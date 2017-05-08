004dfa93d4e486fc4146e4c338d8455ff9ff5a05
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-05-07 22:29:45 -0700

    Fix versioning

--- pygdf/_version.py
+++ pygdf/_version.py
@@ -41,8 +41,8 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = "'v'"
-    cfg.parentdir_prefix = "'pygdf-'"
+    cfg.tag_prefix = "v"
+    cfg.parentdir_prefix = "pygdf-"
     cfg.versionfile_source = "pygdf/_version.py"
     cfg.verbose = False
     return cfg
