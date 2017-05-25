36e2e99b7ab680e82dcfd3c9b6a4f213655f2f48
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-05-25 12:15:29 -0500

    Find libgdf properly on linux

--- python/libgdf_cffi/__init__.py
+++ python/libgdf_cffi/__init__.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import os, sys
+
 from .wrapper import _libgdf_wrapper
 from .wrapper import GDFError           # re-exported
 
@@ -8,7 +10,16 @@ try:
 except ImportError:
     pass
 else:
-    libgdf_api = ffi.dlopen('libgdf.dylib')
+    def _get_lib_name():
+        if os.name == 'posix':
+            # TODO this will need to be changed when packaged for distribution
+            if sys.platform == 'darwin':
+                return './libgdf.dylib'
+            else:
+                return './libgdf.so'
+        raise NotImplementedError('OS {} not supported'.format(os.name))
+
+    libgdf_api = ffi.dlopen(_get_lib_name())
     libgdf = _libgdf_wrapper(ffi, libgdf_api)
 
     del _libgdf_wrapper
