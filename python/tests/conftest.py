256991da616411af69856074c6689e0b9385f7eb
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-06-01 13:55:25 -0500

    Add a pytest fixture to set a constant seed for RNG for the entire test session. This ensures deterministic testing. Also, fixes a typo

---
+++ python/tests/conftest.py
@@ -0,0 +1,15 @@
+"""
+py.test will automatically detect this file.
+"""
+import pytest
+from . import utils
+
+
+# Setup a fixture that is executed once for every test session to set
+# a constant seed for the RNG.
+@pytest.fixture(scope='session', autouse=True)
+def rand_seed():
+    # To check whether this is applied.
+    # Run with: `py.test -s` to see ensure the following message is printed.
+    print("Seeding np.random")
+    utils.seed_rand()
