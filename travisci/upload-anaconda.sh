3106b64543908c8486e352db897e73bc3591488c
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-06-07 15:39:56 -0500

    Add upload step to anaconda in travisci

---
+++ travisci/upload-anaconda.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Adopted from https://github.com/tmcdonell/travis-scripts/blob/dfaac280ac2082cd6bcaba3217428347899f2975/update-accelerate-buildbot.sh
+
+set -e
+
+SOURCE_BRANCH=master
+
+# Pull requests or commits to other branches shouldn't upload
+if [ ${TRAVIS_PULL_REQUEST} != false -o ${TRAVIS_BRANCH} != ${SOURCE_BRANCH} ]; then
+  echo "Skipping upload"
+  return 0
+fi
+
+if [ -z "$MY_UPLOAD_KEY" ]; then
+    echo "No upload key"
+    return 0
+fi
+
+echo "Upload"
+echo ${UPLOADFILE}
+anaconda -t ${MY_UPLOAD_KEY} upload -u gpuopenanalytics -l dev --force ${UPLOADFILE}
