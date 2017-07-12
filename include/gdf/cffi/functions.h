3e1e592b086683b6f499ff578b4855144a8d08d6
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-07-12 16:45:09 -0500

    Update IPC parser to handle categorical. Now depends on libarrow for parsing the arrow Schema.

--- include/gdf/cffi/functions.h
+++ include/gdf/cffi/functions.h
@@ -12,13 +12,20 @@ const char * gdf_error_get_name(gdf_error errcode);
 
 /* ipc */
 
-ipc_parser_type* gdf_ipc_parser_open(const char *device_bytes);
+ipc_parser_type* gdf_ipc_parser_open(const uint8_t *schema, size_t length);
+void gdf_ipc_parser_open_recordbatches(ipc_parser_type *handle,
+                                       const uint8_t *recordbatches,
+                                       size_t length);
+
 void gdf_ipc_parser_close(ipc_parser_type *handle);
 int gdf_ipc_parser_failed(ipc_parser_type *handle);
 const char* gdf_ipc_parser_to_json(ipc_parser_type *handle);
 const char* gdf_ipc_parser_get_error(ipc_parser_type *handle);
 const void* gdf_ipc_parser_get_data(ipc_parser_type *handle);
 int64_t gdf_ipc_parser_get_data_offset(ipc_parser_type *handle);
+
+const char *gdf_ipc_parser_get_schema_json(ipc_parser_type *handle) ;
+const char *gdf_ipc_parser_get_layout_json(ipc_parser_type *handle) ;
 
 /* unary operators */
 
