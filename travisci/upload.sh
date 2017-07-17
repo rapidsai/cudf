b449ab34a330c592d668a0aca711cc891a880ffe
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-07-17 14:03:17 -0500

    Fix travisci not finding file to upload

--- travisci/upload.sh
+++ travisci/upload.sh
@@ -1,10 +1,11 @@
 set -e
 
 if [ $BUILD_CFFI == 1 ]; then
-    export UPLOADFILE=`conda build conda-recipes/libgdf_cffi --python=${PYTHON} --output`
+    export UPLOADFILE=`conda build conda-recipes/libgdf_cffi -c defaults -c conda-forge --python=${PYTHON} --output`
 else
-    export UPLOADFILE=`conda build conda-recipes/libgdf --output`
+    export UPLOADFILE=`conda build conda-recipes/libgdf -c defaults -c conda-forge --output`
 fi
 
 echo "UPLOADFILE = ${UPLOADFILE}"
+test -e ${UPLOADFILE}
 source ./travisci/upload-anaconda.sh
