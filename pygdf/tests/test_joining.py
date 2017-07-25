7d14780c44d03da57ab51340b08535143ad0e37c
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-07-25 18:02:48 -0500

    Add test and make test run faster

--- pygdf/tests/test_joining.py
+++ pygdf/tests/test_joining.py
@@ -1,4 +1,5 @@
 from itertools import product
+from timeit import default_timer as timer
 
 import pytest
 
@@ -11,6 +12,8 @@ from pygdf.dataframe import DataFrame, Series
 # sort_dtype_args = [np.int32, np.int64, np.float32, np.float64]
 
 def make_params():
+    np.random.seed(0)
+
     hows = 'left,inner'.split(',')
     aa = [0, 0, 4, 5, 5]
     bb = [0, 0, 2, 3, 5]
@@ -22,6 +25,11 @@ def make_params():
     for how in hows:
         yield (aa, bb, how)
 
+    aa = np.random.randint(0, 50, 100)
+    bb = np.random.randint(0, 50, 100)
+    for how in hows:
+        yield (aa, bb, how)
+
 
 @pytest.mark.parametrize('aa,bb,how', make_params())
 def test_dataframe_join(aa, bb, how):
@@ -30,28 +38,31 @@ def test_dataframe_join(aa, bb, how):
     df['b'] = bb
 
     def work(df):
+        ts = timer()
         df1 = df.set_index('a')
         df2 = df.set_index('b')
         joined = df1.join(df2, how=how)
+        te = timer()
+        print(type(df), te - ts)
         return joined
 
     expect = work(df.to_pandas())
     got = work(df)
 
-    print(expect)
-    print(got.to_string(nrows=None))
+    # print(expect)
+    # print(got.to_string(nrows=None))
 
     assert list(expect.columns) == list(got.columns)
-    assert list(expect.index) == list(got.index)
-    assert list(expect['b']) == list(got['b'])
+    assert np.all(expect.index.values == got.index.values)
     _check_series(expect['b'], got['b'])
     _check_series(expect['a'], got['a'])
 
 
 def _check_series(expect, got):
     magic = 0xdeadbeaf
-    direct_equal = list(expect) == list(got)
-    nanfilled_equal = list(expect.fillna(magic)) == list(got.fillna(magic))
+    direct_equal = np.all(expect.values == got.to_array())
+    nanfilled_equal = np.all(expect.fillna(magic).values ==
+                             got.fillna(magic).to_array())
     msg = "direct_equal={}, nanfilled_equal={}".format(direct_equal,
                                                        nanfilled_equal)
     assert direct_equal or nanfilled_equal, msg
