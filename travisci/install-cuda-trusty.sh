3c3b83007e0e1c8d70e38d54574e7375f9b3a57e
Stan Seibert <stan@mtrr.org>
2018-01-19 13:46:56 -0600

    Fix for CUDA 9.1 url

--- travisci/install-cuda-trusty.sh
+++ travisci/install-cuda-trusty.sh
@@ -20,11 +20,16 @@ if [ ${CUDA:0:1} == '8' ]; then
     # travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT}
     travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}
     travis_retry sudo apt-get clean
-else
+elif [ ${CUDA:0:3} == '9.0' ]; then
     # CUDA 9 we use the sh installer
     travis_retry wget --progress=dot:mega https://developer.nvidia.com/compute/cuda/${CUDA:0:3}/Prod/local_installers/cuda_${CUDA}_linux-run
     chmod +x cuda_*_linux-run
     sudo ./cuda_*_linux-run --silent --toolkit
+else
+    # CUDA 9.1 has a different filename pattern
+    travis_retry wget --progress=dot:mega https://developer.nvidia.com/compute/cuda/${CUDA:0:3}/Prod/local_installers/cuda_${CUDA}_linux
+    chmod +x cuda_*_linux
+    sudo ./cuda_*_linux --silent --toolkit
 fi
 export CUDA_HOME=/usr/local/cuda-${CUDA:0:3}
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
