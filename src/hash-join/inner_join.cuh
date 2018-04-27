d9fb59e80095cf860d3b6459b8ef024911e60bdb
Nikolay Sakharnykh <nsakharnykh@nvidia.com>
2018-04-26 20:42:05 -0500

    First prototype of two-column hash-join

--- src/hash-join/inner_join.cuh
+++ src/hash-join/inner_join.cuh
@@ -143,3 +143,111 @@ __global__ void probe_hash_tbl(
     }
 }
 
+template<
+    typename multimap_type,
+    typename key_type1,
+    typename key_type2,
+    typename size_type,
+    typename joined_type,
+    int block_size,
+    int output_cache_size>
+__global__ void probe_hash_tbl(
+    multimap_type * multi_map,
+    const key_type1* probe_col1,
+    const key_type2* probe_col2,
+    const size_type probe_tbl_size,
+    const key_type2* build_col2,
+    joined_type * const joined,
+    size_type* const current_idx,
+    const size_type offset,
+    const bool optimized = false)
+{
+    typedef typename multimap_type::key_equal key_compare_type;
+    __shared__ int current_idx_shared[block_size/warp_size];
+    __shared__ joined_type joined_shared[block_size/warp_size][output_cache_size];
+
+    const int warp_id = threadIdx.x/warp_size;
+    const int lane_id = threadIdx.x%warp_size;
+
+    key_compare_type key_compare;
+
+    if ( 0 == lane_id )
+        current_idx_shared[warp_id] = 0;
+
+#if defined(CUDA_VERSION) && CUDA_VERSION >= 9000
+    __syncwarp();
+#endif
+
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+
+#if defined(CUDA_VERSION) && CUDA_VERSION >= 9000
+    const int activemask = __ballot_sync(0xffffffff, i < probe_tbl_size);
+#endif
+    if ( i < probe_tbl_size ) {
+        //OPT: merging equal_range and writing of output values could avoid redundant memory rountrips
+        auto range = multi_map->equal_range(probe_col1[i]);
+        bool running = (range.first != range.second);
+        auto it = range.first;
+        while ( __any_sync( activemask, running ) )
+        {
+            if ( running )
+            {
+                if (!optimized && (!key_compare( probe_col1[i], it->first ) || (probe_col2[i] != build_col2[it->second]))) {
+                    ++it;
+                    running = (it != range.second);
+                }
+                else {
+                    joined_type joined_val;
+                    joined_val.x = it->second;
+                    joined_val.y = offset+i;
+
+                    int my_current_idx = atomicAdd( current_idx_shared+warp_id, 1 );
+                    //its guranteed to fit into the shared cache
+                    joined_shared[warp_id][my_current_idx] = joined_val;
+
+                    ++it;
+                    running = (it != range.second);
+                }
+            }
+
+#if defined(CUDA_VERSION) && CUDA_VERSION >= 9000
+            __syncwarp(activemask);
+#endif
+            //flush output cache if next iteration does not fit
+            if ( current_idx_shared[warp_id]+warp_size >= output_cache_size ) {
+                // count how many active threads participating here which could be less than warp_size
+                int num_threads = __popc(activemask);
+                int output_offset = 0;
+                if ( 0 == lane_id )
+                    output_offset = atomicAdd( current_idx, current_idx_shared[warp_id] );
+                output_offset = cub::ShuffleIndex(output_offset, 0, warp_size, activemask);
+
+                for ( int shared_out_idx = lane_id; shared_out_idx<current_idx_shared[warp_id]; shared_out_idx+=num_threads ) {
+                    joined[output_offset+shared_out_idx] = joined_shared[warp_id][shared_out_idx];
+                }
+#if defined(CUDA_VERSION) && CUDA_VERSION >= 9000
+                __syncwarp(activemask);
+#endif
+                if ( 0 == lane_id )
+                    current_idx_shared[warp_id] = 0;
+#if defined(CUDA_VERSION) && CUDA_VERSION >= 9000
+                __syncwarp(activemask);
+#endif
+            }
+        }
+
+        //final flush of output cache
+        if ( current_idx_shared[warp_id] > 0 ) {
+            // count how many active threads participating here which could be less than warp_size
+            int num_threads = __popc(activemask);
+            int output_offset = 0;
+            if ( 0 == lane_id )
+                output_offset = atomicAdd( current_idx, current_idx_shared[warp_id] );
+            output_offset = cub::ShuffleIndex(output_offset, 0, warp_size, activemask);
+
+            for ( int shared_out_idx = lane_id; shared_out_idx<current_idx_shared[warp_id]; shared_out_idx+=num_threads ) {
+                joined[output_offset+shared_out_idx] = joined_shared[warp_id][shared_out_idx];
+            }
+        }
+    }
+}
--- src/joining.h
+++ src/joining.h
@@ -39,6 +39,7 @@ _join_bounds compute_join_bounds(a_it a, int a_count, b_it b, int b_count,
     return bounds;
 }
 
+template<typename launch_arg_t = empty_t>
 mem_t<int> scan_join_bounds(const _join_bounds &bounds, int a_count, int b_count,
                             context_t &context, bool isInner,
                             int &out_join_count)
@@ -179,6 +180,7 @@ mem_t<int> inner_join(a_it a, int a_count, b_it b, int b_count,
 typedef int size_type;
 typedef struct { size_type x, y; } joined_type;
 
+// single-column join
 template<typename launch_arg_t = empty_t,
          typename a_it, typename b_it, typename comp_t>
 mem_t<size_type> inner_join_hash(a_it a, size_type a_count, b_it b, size_type b_count,
@@ -202,8 +204,8 @@ mem_t<size_type> inner_join_hash(a_it a, size_type a_count, b_it b, size_type b_
 
     // allocate a counter
     size_type* joined_idx;
-    CUDA_RT_CALL( cudaMallocManaged(&joined_idx, sizeof(key_type)) );
-    CUDA_RT_CALL( cudaMemsetAsync(joined_idx, 0, sizeof(key_type), 0) );
+    CUDA_RT_CALL( cudaMallocManaged(&joined_idx, sizeof(size_type)) );
+    CUDA_RT_CALL( cudaMemsetAsync(joined_idx, 0, sizeof(size_type), 0) );
 
     // step 1: initialize a HT for the smaller buffer A
     typedef concurrent_unordered_multimap<key_type, size_type, -1, -1> multimap_type;
@@ -242,6 +244,72 @@ mem_t<size_type> inner_join_hash(a_it a, size_type a_count, b_it b, size_type b_
                                                join_count, context, true);
 #endif
     return output;
+}
+
+// two-column join
+template<typename launch_arg_t = empty_t,
+         typename a1_it, typename b1_it,
+         typename a2_it, typename b2_it,
+         typename comp_t>
+mem_t<size_type> inner_join_hash(a1_it a1, a2_it a2, size_type a_count,
+                                 b1_it b1, b2_it b2, size_type b_count,
+                                 comp_t comp, context_t& context, bool flip_indices = false)
+{
+#ifdef HASH_JOIN
+  // here follows the custom code for hash-joins
+  typedef typename std::iterator_traits<a1_it>::value_type key_type1;
+  typedef typename std::iterator_traits<a2_it>::value_type key_type2;
+
+  // swap buffers if a_count > b_count to use the smaller table for build
+  if (a_count > b_count)
+    return inner_join_hash(b1, b2, b_count, a1, a2, a_count, comp, context, true);
+
+  // TODO: find an estimate for the output buffer size
+  const double matching_rate = a_count;
+  size_type joined_size = (size_type)(b_count * matching_rate);
+
+  // create a temp output buffer to store pairs
+  joined_type *joined;
+  CUDA_RT_CALL( cudaMallocManaged(&joined, sizeof(joined_type) * joined_size) );
+
+  // allocate a counter
+  size_type* joined_idx;
+  CUDA_RT_CALL( cudaMallocManaged(&joined_idx, sizeof(size_type)) );
+  CUDA_RT_CALL( cudaMemsetAsync(joined_idx, 0, sizeof(size_type), 0) );
+
+  // step 1: initialize a HT for the smaller buffer A
+  typedef concurrent_unordered_multimap<key_type1, size_type, -1, -1> multimap_type;
+  size_type hash_tbl_size = (size_type)(a_count * 100 / HASH_TBL_OCC);
+  std::unique_ptr<multimap_type> hash_tbl(new multimap_type(hash_tbl_size));
+  hash_tbl->prefetch(0);  // FIXME: use GPU device id from the context?
+
+  // step 2: build the HT
+  const int block_size = 128;
+  build_hash_tbl<<<(a_count+block_size-1)/block_size, block_size>>>(hash_tbl.get(), a1, a_count);
+  CUDA_RT_CALL( cudaGetLastError() );
+
+  // step 3: scan B, probe the HT and output the joined indices
+  probe_hash_tbl<multimap_type, key_type1, key_type2, size_type, joined_type, 128, 128>
+                 <<<(b_count+block_size-1)/block_size, block_size>>>
+                  (hash_tbl.get(), b1, b2, b_count, a2, joined, joined_idx, 0);
+  CUDA_RT_CALL( cudaDeviceSynchronize() );
+
+  // TODO: can we avoid this transformation from pairs to decoupled?
+  size_type output_npairs = *joined_idx;
+  mem_t<size_type> output(2 * output_npairs, context);
+  if (output_npairs > 0) {
+    size_type* output_data = output.data();
+    auto k = [=] MGPU_DEVICE(size_type index) {
+      output_data[index] = flip_indices ? joined[index].y : joined[index].x;
+      output_data[index + output_npairs] = flip_indices ? joined[index].x : joined[index].y;
+    };
+    transform(k, output_npairs, context);
+  }
+#else
+  mem_t<int> output(0, context);
+  throw std::runtime_error( "multi-column inner_hash_join() is unsupported for sort-based joins, please set HASH_JOIN" );
+#endif
+  return output;
 }
 
 template<typename launch_arg_t = empty_t,
