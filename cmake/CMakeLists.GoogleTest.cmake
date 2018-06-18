785d4d89b9fd992127a3146638b56e00b068cebd
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-06-18 13:13:59 -0500

    use cmake external project to manage google test

---
+++ cmake/CMakeLists.GoogleTest.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required(VERSION 2.8.12)
+
+cmake_policy(SET CMP0048 NEW)
+
+project(googletest-download NONE)
+
+include(ExternalProject)
+
+ExternalProject_Add(googletest
+    GIT_REPOSITORY    https://github.com/google/googletest.git
+    GIT_TAG           master
+    SOURCE_DIR        "${CMAKE_SOURCE_DIR}/thirdparty/googletest"
+    BINARY_DIR        "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/thirdparty/googletest-build"
+    INSTALL_DIR       "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/thirdparty/googletest-install"
+    CMAKE_ARGS        -Dgtest_build_samples=ON -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/thirdparty/googletest-install
+)
