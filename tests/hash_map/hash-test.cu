3fc5e0e086031b1baf6d465b7284c0b782856a68
Jake Hemstad <jhemstad@nvidia.com>
2018-07-02 16:51:40 -0700

    Created and debugged an initial test template for the concurrent hash table using GTests parameterized typed tests.

--- tests/hash_map/hash-test.cu
+++ tests/hash_map/hash-test.cu
@@ -7,10 +7,59 @@
 #include "gtest/gtest.h"
 #include <gdf/gdf.h>
 #include <gdf/cffi/functions.h>
+#include </home/jhemstad/RAPIDS/libgdf/src/hash-join/concurrent_unordered_multimap.cuh>
+
+// This is necessary to do a parametrized typed-test over multiple template arguments
+template <typename Key, typename Value>
+struct KeyValueTypes
+{
+  using key_type = Key;
+  using value_type = Value;
+};
 
 
-TEST(JakeTest, FirstTest){
-  ASSERT_EQ(6,6);
+template <class T>
+class HashTest : public testing::Test 
+{
+  using key_type = typename T::key_type;
+  using value_type = typename T::value_type;
+
+
+  concurrent_unordered_multimap<key_type, 
+                                value_type, 
+                                std::numeric_limits<key_type>::max(), 
+                                std::numeric_limits<value_type>::max() > the_map;
+
+
+public:
+  HashTest(const int hash_table_size = 100)
+    : the_map(hash_table_size)
+  {
+
+
+  }
+
+  ~HashTest(){
+  }
+
+
+};
+
+// The list of types we want to test.
+typedef ::testing::Types< KeyValueTypes<int,int> > Implementations;
+
+TYPED_TEST_CASE(HashTest, Implementations);
+
+TYPED_TEST(HashTest, jake)
+{
+
+  using key_type = typename TypeParam::key_type;
+  using value_type = typename TypeParam::value_type;
+
+  key_type six = 6;
+
+  ASSERT_EQ(6,six);
+
 }
 
 
