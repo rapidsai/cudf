add81f2da7fc141f63eec0737179626f2774a1da
Yash Vardhan <yashv@nvidia.com>
2018-07-12 16:23:57 -0500

    adding new data types in hashing GTest

--- gtests/hashing/hash-test.cu
+++ gtests/hashing/hash-test.cu
@@ -40,7 +40,7 @@ TEST(gdf_hashing_test, simpleTest) {
 TEST(gdf_hashing_test, twoColTest) {
 
 	int nrows = 5;
-	int ncols = 2;
+	int ncols = 6;
 
 	gdf_column **inputCol;
 	inputCol = (gdf_column**) malloc(sizeof(gdf_column)* ncols);
@@ -49,35 +49,78 @@ TEST(gdf_hashing_test, twoColTest) {
 
 	gdf_column outputCol;
 
-	inputCol[0]->dtype = GDF_INT32;
+	inputCol[0]->dtype = GDF_INT8;
 	inputCol[0]->size = nrows;
 
-	inputCol[1]->dtype = GDF_INT64;
+	inputCol[1]->dtype = GDF_INT16;
 	inputCol[1]->size = nrows;
+
+	inputCol[2]->dtype = GDF_INT32;
+	inputCol[2]->size = nrows;
+
+	inputCol[3]->dtype = GDF_INT64;
+	inputCol[3]->size = nrows;
+
+	inputCol[4]->dtype = GDF_FLOAT32;
+	inputCol[4]->size = nrows;
+
+	inputCol[5]->dtype = GDF_FLOAT64;
+	inputCol[5]->size = nrows;
 
 	outputCol.dtype = GDF_INT32;
 	outputCol.size = nrows;
 
 	// Input Data
-	std::vector<int32_t> inputData1(nrows);
+	std::vector<int8_t> inputData1(nrows);
 	inputData1[0] = 0;
 	inputData1[1] = 1;
 	inputData1[2] = 2;
 	inputData1[3] = 3;
 	inputData1[4] = 0;
 
-	std::vector<int64_t> inputData2(nrows);
-	inputData2[0] = 2;
-	inputData2[1] = 34;
-	inputData2[2] = 7;
-	inputData2[3] = 9;
-	inputData2[4] = 2;
+	std::vector<int16_t> inputData2(nrows);
+	inputData2[0] = 4;
+	inputData2[1] = 3;
+	inputData2[2] = 54;
+	inputData2[3] = 7;
+	inputData2[4] = 4;
 
-	thrust::device_vector<int32_t> intputDataDev1(inputData1);
+	std::vector<int32_t> inputData3(nrows);
+	inputData3[0] = 28;
+	inputData3[1] = 543453;
+	inputData3[2] = 357894;
+	inputData3[3] = 456864;
+	inputData3[4] = 28;
+
+	std::vector<int64_t> inputData4(nrows);
+	inputData4[0] = 28467476447;
+	inputData4[1] = 43348947784;
+	inputData4[2] = 76343746754;
+	inputData4[3] = 91125744743;
+	inputData4[4] = 28467476447;
+
+	std::vector<float> inputData5(nrows);
+	inputData5[0] = 3.0;
+	inputData5[1] = 11.0;
+	inputData5[2] = 24.0;
+	inputData5[3] = 83.0;
+	inputData5[4] = 3.0;
+
+	std::vector<double> inputData6(nrows);
+	inputData6[0] = 343.01;
+	inputData6[1] = 112.04;
+	inputData6[2] = 298.67;
+	inputData6[3] = 786.34;
+	inputData6[4] = 343.01;
+
 	thrust::device_vector<gdf_valid_type> inputValidDev(1,0);
 
-	thrust::device_vector<int64_t> intputDataDev2(inputData2);
-	// thrust::device_vector<gdf_valid_type> inputValidDev2(1,0);
+	thrust::device_vector<int8_t> intputDataDev1(inputData1);
+	thrust::device_vector<int16_t> intputDataDev2(inputData2);
+	thrust::device_vector<int32_t> intputDataDev3(inputData3);
+	thrust::device_vector<int64_t> intputDataDev4(inputData4);
+	thrust::device_vector<float> intputDataDev5(inputData5);
+	thrust::device_vector<double> intputDataDev6(inputData6);
 
 	thrust::device_vector<int32_t> outDataDev(nrows);
 	thrust::device_vector<gdf_valid_type> outputValidDev(1,0);
@@ -87,6 +130,18 @@ TEST(gdf_hashing_test, twoColTest) {
 
 	inputCol[1]->data = thrust::raw_pointer_cast(intputDataDev2.data());
 	inputCol[1]->valid = thrust::raw_pointer_cast(inputValidDev.data());
+
+	inputCol[2]->data = thrust::raw_pointer_cast(intputDataDev3.data());
+	inputCol[2]->valid = thrust::raw_pointer_cast(inputValidDev.data());
+
+	inputCol[3]->data = thrust::raw_pointer_cast(intputDataDev4.data());
+	inputCol[3]->valid = thrust::raw_pointer_cast(inputValidDev.data());
+
+	inputCol[4]->data = thrust::raw_pointer_cast(intputDataDev5.data());
+	inputCol[4]->valid = thrust::raw_pointer_cast(inputValidDev.data());
+
+	inputCol[5]->data = thrust::raw_pointer_cast(intputDataDev6.data());
+	inputCol[5]->valid = thrust::raw_pointer_cast(inputValidDev.data());
 
 	outputCol.data = thrust::raw_pointer_cast(outDataDev.data());
 	outputCol.valid = thrust::raw_pointer_cast(outputValidDev.data());
