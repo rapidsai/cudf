af4595ec65c6f86d1254b7a036cf54db23612298
Jake Hemstad <jhemstad@nvidia.com>
2018-07-17 16:32:16 -0700

    Moved definition of the aggregation operation functors to a seperate header file.

---
+++ src/groupby/hash/aggregation_operations.h
@@ -0,0 +1,33 @@
+#ifndef AGGREGATION_OPERATIONS_H
+#define AGGREGATION_OPERATIONS_H
+enum class aggregation_operation
+{
+  max_op,
+  min_op
+};
+
+template<typename value_type>
+struct max_op
+{
+  constexpr static value_type IDENTITY{std::numeric_limits<value_type>::min()};
+
+  __host__ __device__
+    value_type operator()(value_type a, value_type b)
+    {
+      return (a > b? a : b);
+    }
+};
+
+template<typename value_type>
+struct min_op
+{
+  constexpr static value_type IDENTITY{std::numeric_limits<value_type>::max()};
+
+  __host__ __device__
+    value_type operator()(value_type a, value_type b)
+    {
+      return (a < b? a : b);
+    }
+};
+
+#endif
