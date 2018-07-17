717fa9011ef5a867bd3341121d4ac11f07d4d76b
Jake Hemstad <jhemstad@nvidia.com>
2018-07-17 08:42:22 -0700

    Cleaned up test code for groupby-test.

--- tests/groupby/groupby-test.cu
+++ tests/groupby/groupby-test.cu
@@ -10,12 +10,12 @@
 #include <../../src/groupby/hash/groupby_kernels.cuh>
 
 // This is necessary to do a parametrized typed-test over multiple template arguments
-template <typename Key, typename Value, typename aggregation_type>
+template <typename Key, typename Value, template <typename> typename Aggregation_Operator>
 struct KeyValueTypes
 {
   using key_type = Key;
   using value_type = Value;
-  using op_type = aggregation_type;
+  using op_type = Aggregation_Operator<value_type>;
 };
 
 // Have to use a functor instead of a device lambda because
@@ -24,25 +24,28 @@ struct KeyValueTypes
 // and you can't have a device lambda inside a private member
 // function
 template<typename value_type>
-  struct max_op
-  {
-    __host__ __device__
+struct max_op
+{
+  constexpr static value_type IDENTITY{std::numeric_limits<value_type>::min()};
+
+  __host__ __device__
     value_type operator()(value_type a, value_type b)
     {
       return (a > b? a : b);
     }
-  };
+};
 
 template<typename value_type>
-  struct min_op
-  {
-    __host__ __device__
+struct min_op
+{
+  constexpr static value_type IDENTITY{std::numeric_limits<value_type>::max()};
+
+  __host__ __device__
     value_type operator()(value_type a, value_type b)
     {
       return (a < b? a : b);
     }
-  };
-
+};
 
 // A new instance of this class will be created for each *TEST(GroupByTest, ...)
 // Put all repeated stuff for each test here
@@ -53,13 +56,12 @@ struct GroupByTest : public testing::Test
   using value_type = typename T::value_type;
   using op_type = typename T::op_type;
   using map_type = concurrent_unordered_map<key_type, value_type, std::numeric_limits<key_type>::max()>;
-  using pair_type = thrust::pair<key_type, value_type>;
   using size_type = typename map_type::size_type;
 
   std::unique_ptr<map_type> the_map;
 
   const key_type unused_key = std::numeric_limits<key_type>::max();
-  const value_type unused_value = std::numeric_limits<value_type>::max();
+  const value_type unused_value = op_type::IDENTITY;
 
   size_type size;
 
@@ -74,16 +76,18 @@ struct GroupByTest : public testing::Test
   std::unordered_map<key_type, value_type> expected_values;
 
   GroupByTest(const size_type hash_table_size = 10000)
-    : size(hash_table_size), the_map(new map_type(hash_table_size))
+    : size(hash_table_size), the_map(new map_type(hash_table_size, op_type::IDENTITY))
   {
   }
 
-  void create_input(const int num_unique_keys, const int num_values_per_key, const int ratio = 1, const int max_key = RAND_MAX, const int max_value = RAND_MAX)
+  std::pair<key_type*, value_type*>
+  create_input(const int num_unique_keys, const int num_values_per_key, const int ratio = 1, const int max_key = RAND_MAX, const int max_value = RAND_MAX)
   {
 
     const int TOTAL_PAIRS = num_unique_keys * num_values_per_key;
+    size = TOTAL_PAIRS;
 
-    this->the_map.reset(new map_type(ratio*TOTAL_PAIRS));
+    this->the_map.reset(new map_type(ratio*TOTAL_PAIRS, unused_value));
 
     groupby_column.reserve(TOTAL_PAIRS);
     aggregation_column.reserve(TOTAL_PAIRS);
@@ -110,14 +114,14 @@ struct GroupByTest : public testing::Test
         // Don't use unused_value
         while(current_value == this->unused_value)
         {
-          current_value = std::rand();
+          current_value = std::rand() % max_value;
         }
 
         // Store current key and value
         groupby_column.push_back(current_key);
         aggregation_column.push_back(current_value);
 
-        // Use a STL map to keep track of the max value for each key
+        // Use a STL map to keep track of the aggregation for each key
         auto found = expected_values.find(current_key);
 
         // Key doesn't exist yet, insert it
@@ -137,9 +141,35 @@ struct GroupByTest : public testing::Test
 
     d_groupby_column = groupby_column;
     d_aggregation_column = aggregation_column;
+
+    return std::make_pair(thrust::raw_pointer_cast(d_groupby_column.data()), 
+                          thrust::raw_pointer_cast(d_aggregation_column.data()));
   }
 
-  void check_answer(){
+  void build_aggregation_table_device(std::pair<key_type*, value_type*> input)
+  {
+
+    const dim3 grid_size ((this->size + this->THREAD_BLOCK_SIZE -1) / this->THREAD_BLOCK_SIZE,1,1);
+    const dim3 block_size (this->THREAD_BLOCK_SIZE, 1, 1);
+
+    key_type * d_group = input.first;
+    value_type * d_agg = input.second;
+
+    cudaDeviceSynchronize();
+    build_aggregation_table<<<grid_size, block_size>>>((this->the_map).get(), d_group, d_agg, this->size, op_type());
+    cudaDeviceSynchronize(); 
+
+  }
+
+  void extract_groupby_result_device()
+  {
+
+    const dim3 grid_size ((this->size + this->THREAD_BLOCK_SIZE -1) / this->THREAD_BLOCK_SIZE,1,1);
+    const dim3 block_size (this->THREAD_BLOCK_SIZE, 1, 1);
+
+  }
+
+  void verify_aggregation_table(){
 
     for(auto const &k : this->expected_values)
     {
@@ -149,7 +179,7 @@ struct GroupByTest : public testing::Test
 
       auto found = this->the_map->find(test_key);
 
-      ASSERT_NE(this->the_map->end(), found);
+      ASSERT_NE(this->the_map->end(), found) << "Key is: " << test_key;
 
       value_type test_value = found->second;
 
@@ -167,33 +197,32 @@ struct GroupByTest : public testing::Test
 // to nest multiple types inside of the KeyValueTypes struct above
 // KeyValueTypes<type1, type2> implies key_type = type1, value_type = type2
 // This list is the types across which Google Test will run our tests
-typedef ::testing::Types< KeyValueTypes<int,int,max_op<int>>
-//                          KeyValueTypes<int,float,max_op<int>>,
-//                          KeyValueTypes<int,double,max_op<int>>,
-//                          KeyValueTypes<int,long long int,max_op<int>>,
-//                          KeyValueTypes<int,unsigned long long int,max_op<int>>,
-//                          KeyValueTypes<unsigned long long int, int,max_op<int>>,
-//                          KeyValueTypes<unsigned long long int, float,max_op<int>>,
-//                          KeyValueTypes<unsigned long long int, double,max_op<int>>,
-//                          KeyValueTypes<unsigned long long int, long long int,max_op<int>>,
-//                          KeyValueTypes<unsigned long long int, unsigned long long int,max_op<int>>,
-//                          KeyValueTypes<int,int,min_op<int>>,
-//                          KeyValueTypes<int,float,min_op<int>>,
-//                          KeyValueTypes<int,double,min_op<int>>,
-//                          KeyValueTypes<int,long long int,min_op<int>>,
-//                          KeyValueTypes<int,unsigned long long int,min_op<int>>,
-//                          KeyValueTypes<unsigned long long int, int,min_op<int>>,
-//                          KeyValueTypes<unsigned long long int, float,min_op<int>>,
-//                          KeyValueTypes<unsigned long long int, double,min_op<int>>,
-//                          KeyValueTypes<unsigned long long int, long long int,min_op<int>>,
-//                          KeyValueTypes<unsigned long long int, unsigned long long int,min_op<int>>
+typedef ::testing::Types< KeyValueTypes<int, int, max_op>,
+                          KeyValueTypes<int, float, max_op>,
+                          KeyValueTypes<int, double, max_op>,
+                          KeyValueTypes<int, long long int, max_op>,
+                          KeyValueTypes<int, unsigned long long int, max_op>,
+                          KeyValueTypes<unsigned long long int, int, max_op>,
+                          KeyValueTypes<unsigned long long int, float, max_op>,
+                          KeyValueTypes<unsigned long long int, double, max_op>,
+                          KeyValueTypes<unsigned long long int, long long int, max_op>,
+                          KeyValueTypes<unsigned long long int, unsigned long long int, max_op>,
+                          KeyValueTypes<int, int, min_op>,
+                          KeyValueTypes<int, float, min_op>,
+                          KeyValueTypes<int, double, min_op>,
+                          KeyValueTypes<int, long long int, min_op>,
+                          KeyValueTypes<int, unsigned long long int, min_op>,
+                          KeyValueTypes<unsigned long long int, int, min_op>,
+                          KeyValueTypes<unsigned long long int, float, min_op>,
+                          KeyValueTypes<unsigned long long int, double, min_op>,
+                          KeyValueTypes<unsigned long long int, long long int, min_op>,
+                          KeyValueTypes<unsigned long long int, unsigned long long int, min_op>
                           > Implementations;
 
 TYPED_TEST_CASE(GroupByTest, Implementations);
 
 TYPED_TEST(GroupByTest, AggregationTestHost)
 {
-
   using key_type = typename TypeParam::key_type;
   using value_type = typename TypeParam::value_type;
 
@@ -233,95 +262,35 @@ TYPED_TEST(GroupByTest, AggregationTestHost)
 
   found = this->the_map->find(0);
   EXPECT_EQ(11, found->second);
-
 }
-
 
 
 TYPED_TEST(GroupByTest, AggregationTestDeviceAllSame)
 {
-  using value_type = typename TypeParam::value_type;
-  using pair_type = typename GroupByTest<TypeParam>::pair_type;
-  using op_type = typename GroupByTest<TypeParam>::op_type;
-
-  this->create_input(1, 1<<20);
-
-  const dim3 grid_size ((this->size + this->THREAD_BLOCK_SIZE -1) / this->THREAD_BLOCK_SIZE,1,1);
-  const dim3 block_size (this->THREAD_BLOCK_SIZE, 1, 1);
-
-  value_type * d_group = thrust::raw_pointer_cast(this->d_groupby_column.data());
-  value_type * d_agg = thrust::raw_pointer_cast(this->d_aggregation_column.data());
-
-  cudaDeviceSynchronize();
-  build_aggregation_table<<<grid_size, block_size>>>((this->the_map).get(), d_group, d_agg, this->size, op_type());
-  cudaDeviceSynchronize(); 
-
-  this->check_answer();
-
+  auto input = this->create_input(1, this->THREAD_BLOCK_SIZE);
+  this->build_aggregation_table_device(input);
+  this->verify_aggregation_table();
 }
 
 TYPED_TEST(GroupByTest, AggregationTestDeviceAllUnique)
 {
-  using value_type = typename TypeParam::value_type;
-  using pair_type = typename GroupByTest<TypeParam>::pair_type;
-  using op_type = typename GroupByTest<TypeParam>::op_type;
-  
-
-  this->create_input(1<<16, 1);
-
-  const dim3 grid_size ((this->size + this->THREAD_BLOCK_SIZE -1) / this->THREAD_BLOCK_SIZE,1,1);
-  const dim3 block_size (this->THREAD_BLOCK_SIZE, 1, 1);
-
-  value_type * d_group = thrust::raw_pointer_cast(this->d_groupby_column.data());
-  value_type * d_agg = thrust::raw_pointer_cast(this->d_aggregation_column.data());
-
-  cudaDeviceSynchronize();
-  build_aggregation_table<<<grid_size, block_size>>>((this->the_map).get(), d_group, d_agg, this->size, op_type());
-  cudaDeviceSynchronize(); 
-
-  this->check_answer();
+  auto input = this->create_input(1<<16, 1);
+  this->build_aggregation_table_device(input);
+  this->verify_aggregation_table();
 }
 
 TYPED_TEST(GroupByTest, AggregationTestDeviceWarpSame)
 {
-  using value_type = typename TypeParam::value_type;
-  using pair_type = typename GroupByTest<TypeParam>::pair_type;
-  using op_type = typename GroupByTest<TypeParam>::op_type;
-
-  this->create_input(1<<15, 32);
-
-  const dim3 grid_size ((this->size + this->THREAD_BLOCK_SIZE -1) / this->THREAD_BLOCK_SIZE,1,1);
-  const dim3 block_size (this->THREAD_BLOCK_SIZE, 1, 1);
-
-  value_type * d_group = thrust::raw_pointer_cast(this->d_groupby_column.data());
-  value_type * d_agg = thrust::raw_pointer_cast(this->d_aggregation_column.data());
-
-  cudaDeviceSynchronize();
-  build_aggregation_table<<<grid_size, block_size>>>((this->the_map).get(), d_group, d_agg, this->size, op_type());
-  cudaDeviceSynchronize(); 
-
-  this->check_answer();
+  auto input = this->create_input(1<<15, 32);
+  this->build_aggregation_table_device(input);
+  this->verify_aggregation_table();
 }
 
 TYPED_TEST(GroupByTest, AggregationTestDeviceBlockSame)
 {
-  using value_type = typename TypeParam::value_type;
-  using pair_type = typename GroupByTest<TypeParam>::pair_type;
-  using op_type = typename GroupByTest<TypeParam>::op_type;
-
-  this->create_input(1<<12, this->THREAD_BLOCK_SIZE);
-
-  const dim3 grid_size ((this->size + this->THREAD_BLOCK_SIZE -1) / this->THREAD_BLOCK_SIZE,1,1);
-  const dim3 block_size (this->THREAD_BLOCK_SIZE, 1, 1);
-
-  value_type * d_group = thrust::raw_pointer_cast(this->d_groupby_column.data());
-  value_type * d_agg = thrust::raw_pointer_cast(this->d_aggregation_column.data());
-
-  cudaDeviceSynchronize();
-  build_aggregation_table<<<grid_size, block_size>>>((this->the_map).get(), d_group, d_agg, this->size, op_type());
-  cudaDeviceSynchronize(); 
-
-  this->check_answer();
+  auto input = this->create_input(1<<12, this->THREAD_BLOCK_SIZE);
+  this->build_aggregation_table_device(input);
+  this->verify_aggregation_table();
 }
 
 
