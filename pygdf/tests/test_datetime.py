2385e15774b0d8fff5cd5b5d07027f7ed95ccf07
Ubuntu <ubuntu@ip-172-31-44-190.us-east-2.compute.internal>
2018-07-18 21:05:33 +0000

    stubbed out tests

---
+++ pygdf/tests/test_datetime.py
@@ -0,0 +1,40 @@
+import pytest
+import pandas as pd
+
+from pygdf.dataframe import Series, Index
+
+
+def data1():
+    return pd.date_range('20010101', '20020215', freq='400h')
+
+
+def data2():
+    return pd.date_range('20010101', '20020215',
+                         freq='400h',
+                         tz='US/Eastern')
+
+
+fields = ['year', 'month', 'day',
+          'date', 'hour', 'minute']
+
+
+@pytest.mark.parametrize('data', [data1(), data2()])
+@pytest.mark.parametrize('field', fields)
+def test_dt_series(data, field):
+    pd_data = pd.Series(data.copy())
+    gdf_data = Series(pd_data)
+    np.testing.assert_equal(
+        getattr(gdf_data.dt, field).to_array(),
+        getattr(pd_data.dt, field).values
+    )
+
+
+@pytest.mark.parametrize('data', [data1(), data2()])
+@pytest.mark.parametrize('field', fields)
+def test_dt_index(data, field):
+    pd_data = data.copy()
+    gdf_data = Index(pd_data)
+    np.testing.assert_equal(
+        getattr(gdf_data, field).to_array(),
+        getattr(pd_data, field).values
+    )
