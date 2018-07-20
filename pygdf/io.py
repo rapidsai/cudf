459b5605b76530ef4b2a847fd1e34a8c6eb70545
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-07-20 18:07:41 -0500

    Complete basic binding for CSV reader

--- pygdf/io.py
+++ pygdf/io.py
@@ -1,18 +1,35 @@
 from ._gdf import ffi, libgdf, np_to_gdf_dtype
-
+from .column import Column
+from .numerical import NumericalColumn
+from .dataframe import DataFrame
 
 def _wrap_string(text):
     return ffi.new("char[]", text.encode())
 
 
 def read_csv(path, names, dtypes, delimiter=',', ):
+    ncols = len(names)
     out = libgdf.read_csv(
         _wrap_string(path),
         delimiter.encode(),
-        len(names),
+        ncols,
         [_wrap_string(k) for k in names],
         list(map(lambda x: _wrap_string(str(x)), dtypes)),
     )
     if out == ffi.NULL:
         raise ValueError("Failed to parse CSV")
-    return out
+
+    # Extract parsed columns
+    outcols = []
+    for i in range(ncols):
+        newcol = Column.from_cffi_view(out[i])
+        # XXX: Bit mask is wrong? dropping it for now
+        newcol = newcol.replace(mask=None)
+        outcols.append(newcol.view(NumericalColumn, dtype=newcol.dtype))
+
+    # Build dataframe
+    df = DataFrame()
+    for k, v in zip(names, outcols):
+        df[k] = v
+
+    return df
--- pygdf/tests/test_csvreader.py
+++ pygdf/tests/test_csvreader.py
@@ -4,11 +4,8 @@ import pytest
 import numpy as np
 import pandas as pd
 
-from numba import cuda
-
-from pygdf.dataframe import Series, DataFrame
 from pygdf.io import read_csv
-from pygdf._gdf import libgdf, ffi, _as_numba_devarray
+
 
 def make_dataframe(nrows):
     df = pd.DataFrame()
@@ -17,35 +14,18 @@ def make_dataframe(nrows):
     return df
 
 
-def test_csv_reader():
-    print(cuda.current_context())  # XXX: read_csv seems to use a different context
-
+@pytest.mark.parametrize('nrows', [1, 5, 10, 100])
+def test_csv_reader(nrows):
+    # XXX: use temp file instead
     fname = os.path.abspath('tmp_csvreader_file.csv')
     print(fname)
-    df = make_dataframe(nrows=1000)
+    df = make_dataframe(nrows=10)
     df.to_csv(fname, index=False, header=False)
 
     with open(fname, 'r') as fin:
         print(fin.read())
 
     dtypes = [df[k].dtype for k in df.columns]
-    print(df.columns)
-    print('list(df.columns)', list(df.columns.values))
-    print(list(map(str, dtypes)))
-    gdf = read_csv(fname, names=list(df.columns.values), dtypes=dtypes)
-    cuda.synchronize()
-    print(gdf)
-    print(gdf[0].data)
-    print(gdf[0].valid)
-    print(gdf[0].dtype)
-    print(gdf[0].size)
-
-    intptr = int(ffi.cast('intptr_t', gdf[0].data))
-    da = _as_numba_devarray(intptr, gdf[0].size, dtype=np.int64)
-    print(da[0])
-    print(da.copy_to_host())
-
-    assert gdf[0].dtype == libgdf.GDF_INT64
-
-
-
+    out = read_csv(fname, names=list(df.columns.values), dtypes=dtypes)
+    assert len(out.columns) == len(df.columns)
+    pd.util.testing.assert_frame_equal(df, out.to_pandas())
