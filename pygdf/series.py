8a403d0ee0a33f8e2d1687ac1a01a72f58413799
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-07-27 17:35:55 -0500

    Print only summary

--- pygdf/series.py
+++ pygdf/series.py
@@ -244,10 +244,10 @@ class Series(object):
                                  cols=cols, more_rows=more_rows)
 
     def __str__(self):
-        return self.to_string()
+        return self.to_string(nrows=10)
 
     def __repr__(self):
-        return self.to_string()
+        return "<pygdf.Series nrows={} >".format(len(self))
 
     def _binaryop(self, other, fn):
         """
