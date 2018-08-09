fb6c18e895b7ed7534cc836c166ece64da5ff14b
Siu Kwan Lam <michael.lam.sk@gmail.com>
2018-08-08 21:04:53 -0500

    Fix tests

--- conda-recipes/pygdf/run_test.py
+++ conda-recipes/pygdf/run_test.py
@@ -3,10 +3,11 @@ import os
 import pytest
 from numba import cuda
 
-if cuda.is_available():
-    exitcode = pytest.main('-v', 'pygdf')
-else:
-    print("CUDA GPU unavailable")
-    exitcode = 1 if os.environ['PYGDF_BUILD_NO_GPU_TEST'] else 0
+if __name__ == '__main__':
+    if cuda.is_available():
+        exitcode = pytest.main('--pyargs pygdf.tests'.split())
+    else:
+        print("CUDA GPU unavailable")
+        exitcode = 1 if os.environ['PYGDF_BUILD_NO_GPU_TEST'] else 0
 
-sys.exit(exitcode)
+    sys.exit(exitcode)
