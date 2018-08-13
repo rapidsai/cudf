94dc7ccdd4b5ad443f7b8dbf0aaa8d47dde4e056
Jake Hemstad <jhemstad@nvidia.com>
2018-08-13 10:04:50 -0700

    Added error checking wrapper around all CUDA calls in groupby_compute_api and added workaround for the kernel invocation that fails to work for some instatiations of the template input parameters.

--- src/groupby/hash/groupby_compute_api.h
+++ src/groupby/hash/groupby_compute_api.h
@@ -8,6 +8,19 @@
 #include <cub/util_allocator.cuh>
 #include <cub/device/device_radix_sort.cuh>
 
+// TODO: replace this with CUDA_TRY and propagate the error
+#ifndef CUDA_RT_CALL
+#define CUDA_RT_CALL( call ) 									   \
+{                                                                                                  \
+    cudaError_t cudaStatus = call;                                                                 \
+    if ( cudaSuccess != cudaStatus ) {                                                             \
+        fprintf(stderr, "ERROR: CUDA RT call \"%s\" in line %d of file %s failed with %s (%d).\n", \
+                        #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus), cudaStatus);    \
+        exit(1);										   \
+    }												   \
+}
+#endif
+
 // The occupancy of the hash table determines it's capacity. A value of 50 implies
 // 50% occupancy, i.e., hash_table_size == 2 * input_size
 constexpr unsigned int DEFAULT_HASH_TABLE_OCCUPANCY{50};
@@ -82,9 +95,7 @@ cudaError_t GroupbyHash(const groupby_type * const in_groupby_column,
 
   //the_map->prefetch(0);
 
-  error = cudaGetLastError();
-  if(error != cudaSuccess)
-    return error;
+  CUDA_RT_CALL(cudaGetLastError());
 
   // Inserts (groupby_column[i], aggregation_column[i]) as a key-value pair into the
   // hash table. When a given key already exists in the table, the aggregation operation
@@ -94,47 +105,55 @@ cudaError_t GroupbyHash(const groupby_type * const in_groupby_column,
                                                            in_aggregation_column,
                                                            in_column_size,
                                                            aggregation_op);
-  error = cudaGetLastError();
-  if(error != cudaSuccess)
-    return error;
+  CUDA_RT_CALL(cudaGetLastError());
 
   // Used by threads to coordinate where to write their results
   unsigned int * global_write_index{nullptr};
-  cudaMallocManaged(&global_write_index, sizeof(unsigned int));
-  cudaMemset(global_write_index, 0, sizeof(unsigned int));
-  //*global_write_index = 0;
+  CUDA_RT_CALL(cudaMallocManaged(&global_write_index, sizeof(unsigned int)));
+  CUDA_RT_CALL(cudaMemset(global_write_index, 0, sizeof(unsigned int)));
 
-  error = cudaGetLastError();
-  if(error != cudaSuccess)
-    return error;
+  CUDA_RT_CALL(cudaDeviceSynchronize());
 
   const dim3 extract_grid_size ((the_map->size() + THREAD_BLOCK_SIZE - 1) / THREAD_BLOCK_SIZE, 1, 1);
 
   // Extracts every non-empty key and value into separate contiguous arrays,
   // which provides the result of the groupby operation
-  extract_groupby_result<<<extract_grid_size, block_size>>>(the_map.get(),
-                                                            the_map->size(),
-                                                            out_groupby_column,
-                                                            out_aggregation_column,
-                                                            global_write_index);
-  error = cudaDeviceSynchronize();
-  if(error != cudaSuccess)
-    return error;
+  //extract_groupby_result<<<extract_grid_size, block_size>>>(the_map.get(),
+  //                                                          the_map->size(),
+  //                                                          out_groupby_column,
+  //                                                          out_aggregation_column,
+  //                                                          global_write_index);
+  
+  // FIXME Work around for above kernel failing to launch for some instantiations of the_map template class
+  map_type * map = the_map.get();
+  typename map_type::size_type map_size = the_map->size();
+  void *args[] = { &map, &map_size, &out_groupby_column, &out_aggregation_column, &global_write_index};
+
+  void (*func)(const map_type * const, 
+               const typename map_type::size_type, 
+               typename map_type::key_type * const, 
+               typename map_type::mapped_type * const,
+               unsigned int * const ) = &(extract_groupby_result<map_type>);
+
+  CUDA_RT_CALL(cudaLaunchKernel((const void*) func, extract_grid_size, block_size, args, 0, 0));
+  // FIXME End work around
+
+  CUDA_RT_CALL(cudaDeviceSynchronize());
 
   // At the end of the extraction kernel, the global write index will be equal to
   // the size of the output. Update the output size.
   *out_size = *global_write_index;
-  cudaFree(global_write_index);
+  CUDA_RT_CALL(cudaFree(global_write_index));
 
   // Optionally sort the groupby/aggregation result columns
   if(true == sort_result)
   {
     // Allocate double buffers needed for the cub Radix Sort
     groupby_type * groupby_result_alt;
-    cudaMalloc(&groupby_result_alt, *out_size * sizeof(groupby_type));
+    CUDA_RT_CALL(cudaMalloc(&groupby_result_alt, *out_size * sizeof(groupby_type)));
 
     aggregation_type * aggregation_result_alt;
-    cudaMalloc(&aggregation_result_alt, *out_size * sizeof(aggregation_type));
+    CUDA_RT_CALL(cudaMalloc(&aggregation_result_alt, *out_size * sizeof(aggregation_type)));
 
     cub::DoubleBuffer<groupby_type>     d_keys(out_groupby_column, groupby_result_alt);
     cub::DoubleBuffer<aggregation_type> d_vals(out_aggregation_column, aggregation_result_alt);
@@ -143,28 +162,24 @@ cudaError_t GroupbyHash(const groupby_type * const in_groupby_column,
     // temp_storage_bytes
     void *d_temp_storage = nullptr;
     size_t temp_storage_bytes = 0;
-    cub::DeviceRadixSort::SortPairs(d_temp_storage, temp_storage_bytes, d_keys, d_vals, *out_size);
+    CUDA_RT_CALL(cub::DeviceRadixSort::SortPairs(d_temp_storage, temp_storage_bytes, d_keys, d_vals, *out_size));
     
     // allocate temp storage here and call sort again to actually sort arrays
-    cudaMalloc(&d_temp_storage, temp_storage_bytes);
-    cub::DeviceRadixSort::SortPairs(d_temp_storage, temp_storage_bytes, d_keys, d_vals, *out_size);
+    CUDA_RT_CALL(cudaMalloc(&d_temp_storage, temp_storage_bytes));
+    CUDA_RT_CALL(cub::DeviceRadixSort::SortPairs(d_temp_storage, temp_storage_bytes, d_keys, d_vals, *out_size));
 
-    error = cudaDeviceSynchronize();
-    if(error != cudaSuccess)
-      return error;
+    CUDA_RT_CALL(cudaDeviceSynchronize());
 
     // Update output pointers with sorted result
     // TODO Find a better way to do this. 
     // Sorted output may be in a different buffer than what was originally passed in... need to copy it 
-    cudaMemcpy(out_groupby_column, d_keys.Current(), *out_size * sizeof(groupby_type), cudaMemcpyDefault);
-    cudaMemcpy(out_aggregation_column, d_vals.Current(), *out_size * sizeof(aggregation_type), cudaMemcpyDefault);
+    CUDA_RT_CALL(cudaMemcpy(out_groupby_column, d_keys.Current(), *out_size * sizeof(groupby_type), cudaMemcpyDefault));
+    CUDA_RT_CALL(cudaMemcpy(out_aggregation_column, d_vals.Current(), *out_size * sizeof(aggregation_type), cudaMemcpyDefault));
 
     // Free work buffers
-    cudaFree(d_temp_storage);
-    
-    // FIXME These currently cause segfaults
-    cudaFree(groupby_result_alt);
-    cudaFree(aggregation_result_alt);
+    CUDA_RT_CALL(cudaFree(d_temp_storage));
+    CUDA_RT_CALL(cudaFree(groupby_result_alt));
+    CUDA_RT_CALL(cudaFree(aggregation_result_alt));
   }
 
   return error;
