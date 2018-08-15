69e5877b8368201e9bd159627caa3cf3ab800e7c
Kumar Aatish <kaatish@nvidia.com>
2018-08-15 13:58:35 -0400

    Undo erroneous API for multi_left_join_generic

--- include/gdf/cffi/functions.h
+++ include/gdf/cffi/functions.h
@@ -7,11 +7,6 @@ gdf_size_type gdf_column_sizeof();
 gdf_error gdf_column_view(gdf_column *column, void *data, gdf_valid_type *valid,
                           gdf_size_type size, gdf_dtype dtype);
 
-/* context operations */
-
-gdf_error gdf_context_view(gdf_context *context, int flag_sorted, gdf_method flag_method,
-                           int flag_distinct);
-
 gdf_error gdf_column_view_augmented(gdf_column *column, void *data, gdf_valid_type *valid,
                           gdf_size_type size, gdf_dtype dtype, gdf_size_type null_count);
 /* context operations */
@@ -159,7 +154,7 @@ gdf_error gdf_left_join_generic(gdf_column *leftcol, gdf_column *rightcol,
                              gdf_join_result_type **out_result, gdf_context *ctxt);
 
 gdf_error gdf_multi_left_join_generic(int num_cols, gdf_column **leftcol,
-							gdf_column **rightcol, gdf_join_result_type **out_result, gdf_context *ctxt);
+							gdf_column **rightcol, gdf_join_result_type **out_result);
 
 gdf_error gdf_outer_join_i8(gdf_column *leftcol, gdf_column *rightcol,
                              gdf_join_result_type **out_result);
