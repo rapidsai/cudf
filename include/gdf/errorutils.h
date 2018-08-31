4694cb3c81ca5f43f0f63534601669a4d13bfe91
Jake Hemstad <jhemstad@nvidia.com>
2018-08-31 10:39:02 -0700

    Improved the CUDA_TRY macro to report which cuda error occured and where it occured.

--- include/gdf/errorutils.h
+++ include/gdf/errorutils.h
@@ -1,7 +1,16 @@
 #ifndef GDF_ERRORUTILS_H
 #define GDF_ERRORUTILS_H
 
-#define CUDA_TRY(x) if ((x)!=cudaSuccess) return GDF_CUDA_ERROR;
+#define CUDA_TRY( call ) 									                                                         \
+{                                                                                                  \
+    cudaError_t cudaStatus = call;                                                                 \
+    if ( cudaSuccess != cudaStatus )                                                               \
+    {                                                                                              \
+        fprintf(stderr, "ERROR: CUDA RT call \"%s\" in line %d of file %s failed with %s (%d).\n", \
+                        #call, __LINE__, __FILE__, cudaGetErrorString(cudaStatus), cudaStatus);    \
+        return GDF_CUDA_ERROR;                          										                       \
+    }												                                                                       \
+}                                                                                                  
 
 #define CUDA_CHECK_LAST() CUDA_TRY(cudaPeekAtLastError())
 
