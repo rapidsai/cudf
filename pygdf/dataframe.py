6a35b2e9555e700fe4c47b9e9943baf71b86758d
Keith Kraus <kkraus@nvidia.com>
2018-09-12 17:50:26 -0400

    Use sets to avoid differently ordered columns

--- pygdf/dataframe.py
+++ pygdf/dataframe.py
@@ -426,8 +426,8 @@ class DataFrame(object):
 
     @classmethod
     def _concat(cls, objs, ignore_index=False):
-        if len(set(o.columns for o in objs)) != 1:
-            what = set(o.columns for o in objs)
+        if len(set([tuple(set(o.columns)) for o in objs])) != 1:
+            what = set([tuple(set(o.columns)) for o in objs])
             raise ValueError('columns mismatch: {}'.format(what))
         objs = [o for o in objs]
         if ignore_index:
