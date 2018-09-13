d1eec69559ac5b6081ed91cb6fb6d55dab9ffeba
Jake Hemstad <jhemstad@nvidia.com>
2018-09-12 17:35:26 -0700

    Resolved potential memory leak where memory was being allocated in a loop, but not being freed in the loop.

--- src/join/hash/join_compute_api.h
+++ src/join/hash/join_compute_api.h
@@ -343,6 +343,8 @@ gdf_error compute_hash_join(mgpu::context_t & compute_ctx,
     if(estimated_join_output_size < h_actual_found){
       cont = true;
       estimated_join_output_size *= 2;
+      CUDA_TRY( cudaFree(output_l_ptr) );
+      CUDA_TRY( cudaFree(output_r_ptr) );
     }
     else
     {
