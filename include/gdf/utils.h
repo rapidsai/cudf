535a777c828fdd02f411e427faa05eebe8cb8912
Jake Hemstad <jhemstad@nvidia.com>
2018-09-19 18:43:19 -0700

    Added function to set a specified bit in a validity bit mask.

--- include/gdf/utils.h
+++ include/gdf/utils.h
@@ -12,6 +12,19 @@ bool gdf_is_valid(const gdf_valid_type *valid, gdf_index_type pos) {
 		return true;
 }
 
-inline gdf_size_type gdf_get_num_chars_bitmask(gdf_size_type size) { return (( size + ( GDF_VALID_BITSIZE - 1)) / GDF_VALID_BITSIZE ); }
+inline gdf_size_type gdf_get_num_chars_bitmask(gdf_size_type size) 
+{ 
+  return (( size + ( GDF_VALID_BITSIZE - 1)) / GDF_VALID_BITSIZE ); 
+}
+
+__host__ __device__
+inline void turn_bit_on(gdf_valid_type * masks, gdf_index_type pos)
+{
+  if(nullptr != masks)
+  {
+    masks[pos/8] |= (gdf_valid_type(1) << (pos % 8));
+  }
+}
+
 
 #endif
