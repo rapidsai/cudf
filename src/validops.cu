b52591d6d172f9c2f7a51f4a15c87096f7f2860f
Jake Hemstad <jhemstad@nvidia.com>
2018-09-24 10:46:14 -0700

    Updated implementation to use popc and handled remainder masks on host.

--- src/validops.cu
+++ src/validops.cu
@@ -1,59 +1,151 @@
 #include <gdf/gdf.h>
+#include <gdf/errorutils.h>
 #include <gdf/utils.h>
 #include <thrust/device_ptr.h>
 #include <thrust/transform_reduce.h>
 #include <thrust/functional.h>
 
-gdf_error gdf_count_nonzero_mask(gdf_valid_type * masks, int num_rows, int * count)
+
+using valid32_t = uint32_t;
+
+// To account for if gdf_valid_type is not a 4 byte type,
+// compute the RATIO of the number of bytes in gdf_valid_type
+// to the 4 byte type being used for casting
+constexpr size_t RATIO = sizeof(valid32_t) / sizeof(gdf_valid_type);
+
+/* --------------------------------------------------------------------------*/
+/** 
+ * @Synopsis  Counts the number of valid bits for the specified number of rows
+   in the host vector of gdf_valid_type masks
+ * 
+ * @Param masks The host vector of masks whose bits will be counted
+ * @Param num_rows The number of bits to count
+ * 
+ * @Returns  The number of valid bits in [0, num_rows) in the host vector of masks
+ */
+/* ----------------------------------------------------------------------------*/
+size_t count_valid_bits_host(std::vector<gdf_valid_type> const & masks, const int num_rows)
 {
-  // Number of masks in the array of bitmasks
+  if((0 == num_rows) || (0 == masks.size())){
+    return 0;
+  }
+
+
+  size_t count{0};
+
+  // Count the valid bits for all masks except the last one
+  for(size_t i = 0; i < (masks.size() - 1); ++i)
+  {
+    gdf_valid_type current_mask = masks[i];
+
+    while(current_mask > 0)
+    {
+      current_mask &= (current_mask-1) ;
+      count++;
+    }
+  }
+
+  constexpr int BITS_PER_MASK = sizeof(gdf_valid_type) * 8;
+
+  // Only count the bits in the last mask that correspond to rows
+  const int num_rows_last_mask = num_rows % BITS_PER_MASK;
+
+  gdf_valid_type last_mask = *(masks.end() - 1);
+  for(int i = 0; (i < num_rows_last_mask) && (last_mask > 0); ++i)
+  {
+    count += (last_mask & gdf_valid_type(1));
+    last_mask >>= 1;
+  }
+
+  return count;
+}
+
+/* --------------------------------------------------------------------------*/
+/** 
+ * @Synopsis  Counts the number of valid bits for the specified number of rows
+   in a validity bitmask.
+ * 
+ * @Param[in] masks The validity bitmask buffer in device memory
+ * @Param[in] num_rows The number of bits to count
+ * @Param[out] count The number of valid bits in the buffer from [0, num_rows)
+ * 
+ * @Returns  GDF_SUCCESS upon successful completion 
+ */
+/* ----------------------------------------------------------------------------*/
+gdf_error gdf_count_nonzero_mask(gdf_valid_type const * masks, int num_rows, int * count)
+{
+
+  if((nullptr == masks) || (nullptr == count)){return GDF_DATASET_EMPTY;}
+  if(0 == num_rows) {return GDF_SUCCESS;}
+
+  assert(sizeof(valid32_t) >= sizeof(gdf_valid_type));
+
+  // Number of gdf_valid_types in the validity bitmask
   const size_t num_masks = gdf_get_num_chars_bitmask(num_rows);
 
-  // Device lambda to count the number of valid bits in each mask
-  auto mask_bit_counter = [masks, num_masks, num_rows] __device__ (int index)
+  // Number of 4 byte types in the validity bit mask 
+  const size_t num_masks32 = num_masks / RATIO;
+
+  // If the total number of masks is not a multiple of the RATIO 
+  // between the original mask type and the 4 byte masks type, then 
+  // these "remainder" masks cannot be proccessed in the transform_reduce
+  // and must be handled separately
+  cudaStream_t copy_stream;
+  const size_t num_remainder_masks = num_masks % RATIO;
+  std::vector<gdf_valid_type> remainder_masks(num_remainder_masks); 
+  if(remainder_masks.size() > 0)
   {
 
-    constexpr int BITS_PER_MASK = sizeof(gdf_valid_type) * 8;
+    CUDA_TRY(cudaStreamCreate(&copy_stream));
 
-    gdf_valid_type count = 0;
-    gdf_valid_type mask = masks[index];
-    if(index < num_masks - 1)
-    {
-      // FIXME: If gdf_valid_type was 32 bits, we could use __popc
-      while( mask > 0 )
-      {
-        mask &= (mask-1) ;
-        count++;
-      }
-    }
-    // Not all bits in the last mask correspond to rows,
-    // only count the ones that correspond to rows
-    else if(index == num_masks - 1)
-    {
-      const int num_rows_last_mask = num_rows % BITS_PER_MASK;
-      for(int i = 0; i < num_rows_last_mask; ++i)
-      {
-        count += mask & gdf_valid_type(1);
-        mask >>= 1;
-      }
-    }
-    return count;
+    // Copy the remainder masks to the host
+    // FIXME: Is this endian safe?
+    const gdf_valid_type * first_remainder_mask = (masks + num_masks) - num_remainder_masks;
+    CUDA_TRY( cudaMemcpyAsync(remainder_masks.data(), 
+                              first_remainder_mask, 
+                              num_remainder_masks * sizeof(gdf_valid_type), 
+                              cudaMemcpyDeviceToHost, 
+                              copy_stream) );
+  }
+
+
+  // Device lambda to count the number of valid bits in each mask
+  auto mask_bit_counter = [] __device__ (valid32_t const mask)
+  {
+    // TODO What type will Thrust use for the temporary storage
+    // during the transform_reduce? We could store this result in
+    // an int8_t, but the reduction would overflow an int8_t accumulator
+    return __popc(mask);
   };
 
-  thrust::device_vector<gdf_valid_type> mask_counts(num_masks);
+  // Cast validity buffer to 4 byte type
+  thrust::device_ptr<valid32_t const> masks32 = thrust::device_pointer_cast(reinterpret_cast<valid32_t const *>(masks));
 
-  // FIXME: We could use transform_reduce if we didn't have to 
-  // handle the last mask as a special case
-  thrust::tabulate(thrust::device,
-                   mask_counts.begin(),
-                   mask_counts.end(),
-                   mask_bit_counter);
+  // Count the number of valid bits in all the masks that are a 
+  // multiple of 4 bytes
+  const size_t count32 = thrust::transform_reduce(masks32,
+                                                  masks32 + num_masks32,
+                                                  mask_bit_counter,
+                                                  0,
+                                                  thrust::plus<int>());
 
+  CUDA_TRY( cudaGetLastError() );
 
-  *count = thrust::reduce(mask_counts.begin(), 
-                          mask_counts.end());
+  // Count the number of valid bits in the remainder masks
+  size_t remainder_count = 0;
+  if(remainder_masks.size() > 0)
+  {
+    CUDA_TRY(cudaStreamSynchronize(copy_stream));
+    CUDA_TRY(cudaStreamDestroy(copy_stream));
+    remainder_count = count_valid_bits_host(remainder_masks, num_rows);
+  }
+
+  // The final count of valid bits is the sum of the result from the
+  // transform_reduce and the remainder masks
+  *count = (count32 + remainder_count);
 
   return GDF_SUCCESS;
 }
+
 
 
