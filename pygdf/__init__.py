761f5d33da1d83954f4ed3e2208345dcdd06f1b3
Matthew Rocklin <mrocklin@gmail.com>
2018-09-27 05:03:55 -0700

    remove __all__ from __init__.py
    We were listing all locals in the file, so it doesn't seem to accomplish much.
    It does however require people editing this file to make two changes rather
    than one.

--- pygdf/__init__.py
+++ pygdf/__init__.py
@@ -11,10 +11,3 @@ from .settings import set_options
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-__all__ = [
-    DataFrame,
-    Series,
-    concat,
-    set_options,
-]
