7c72bda9d492c29431f1561342301bb2bc2a3130
Jake Hemstad <jhemstad@nvidia.com>
2018-09-27 20:43:57 -0700

    Replaced the thrust transform reduction with custom kernel to avoid cudaFuncGetAttributes overhead.

--- src/validops.cu
+++ src/validops.cu
@@ -2,9 +2,8 @@
 #include <gdf/errorutils.h>
 #include <gdf/utils.h>
 #include <vector>
-#include <thrust/device_ptr.h>
-#include <thrust/transform_reduce.h>
-#include <thrust/functional.h>
+#include <cassert>
+#include <cub/cub.cuh>
 
 
 using valid32_t = uint32_t;
@@ -13,6 +12,8 @@ using valid32_t = uint32_t;
 // compute the RATIO of the number of bytes in gdf_valid_type
 // to the 4 byte type being used for casting
 constexpr size_t RATIO = sizeof(valid32_t) / sizeof(gdf_valid_type);
+
+constexpr int block_size = 256;
 
 /* --------------------------------------------------------------------------*/
 /** 
@@ -61,6 +62,29 @@ size_t count_valid_bits_host(std::vector<gdf_valid_type> const & masks, const in
   return count;
 }
 
+
+__global__ 
+void count_valid_bits(valid32_t const * const __restrict__ masks32, const int num_masks32, int * const __restrict__ global_count)
+{
+  typedef cub::BlockReduce<int, block_size> BlockReduce;
+  __shared__ typename BlockReduce::TempStorage temp_storage;
+
+  int cur_mask = threadIdx.x + blockIdx.x * gridDim.x;
+
+  int my_count = 0;
+  while(cur_mask < num_masks32)
+  {
+    my_count += __popc(masks32[cur_mask]);
+
+    cur_mask += blockDim.x * gridDim.x;
+  }
+
+  int block_count = BlockReduce(temp_storage).Sum(my_count);
+
+  if(threadIdx.x == 0)
+    atomicAdd(global_count, block_count);
+}
+
 /* --------------------------------------------------------------------------*/
 /** 
  * @Synopsis  Counts the number of valid bits for the specified number of rows
@@ -76,6 +100,9 @@ size_t count_valid_bits_host(std::vector<gdf_valid_type> const & masks, const in
 gdf_error gdf_count_nonzero_mask(gdf_valid_type const * masks, int num_rows, int * count)
 {
 
+  // Why am I getting an unused function warning error if I don't do this?
+  gdf_is_valid(nullptr, 0);
+
   if((nullptr == masks) || (nullptr == count)){return GDF_DATASET_EMPTY;}
   if(0 == num_rows) {return GDF_SUCCESS;}
 
@@ -85,52 +112,54 @@ gdf_error gdf_count_nonzero_mask(gdf_valid_type const * masks, int num_rows, int
   const size_t num_masks = gdf_get_num_chars_bitmask(num_rows);
 
   // Number of 4 byte types in the validity bit mask 
-  const size_t num_masks32 = num_masks / RATIO;
+  size_t num_masks32 = num_masks / RATIO;
 
   // If the total number of masks is not a multiple of the RATIO 
   // between the original mask type and the 4 byte masks type, then 
   // these "remainder" masks cannot be proccessed in the transform_reduce
   // and must be handled separately
   cudaStream_t copy_stream;
-  const size_t num_remainder_masks = num_masks % RATIO;
+  size_t num_remainder_masks = num_masks % RATIO;
+
+  // If there are no remainder masks, the last mask still
+  // needs to be handled separately
+  if(0 == num_remainder_masks){
+    num_remainder_masks = RATIO;
+    num_masks32--;
+  }
+
   std::vector<gdf_valid_type> remainder_masks(num_remainder_masks); 
   if(remainder_masks.size() > 0)
   {
-
     CUDA_TRY(cudaStreamCreate(&copy_stream));
 
     // Copy the remainder masks to the host
     // FIXME: Is this endian safe?
-    const gdf_valid_type * first_remainder_mask = (masks + num_masks) - num_remainder_masks;
+    const gdf_valid_type * first_remainder_mask = &(masks[num_masks - num_remainder_masks]);
+
     CUDA_TRY( cudaMemcpyAsync(remainder_masks.data(), 
                               first_remainder_mask, 
                               num_remainder_masks * sizeof(gdf_valid_type), 
-                              cudaMemcpyDeviceToHost, 
+                              cudaMemcpyDeviceToHost,
                               copy_stream) );
   }
 
-
-  // Device lambda to count the number of valid bits in each mask
-  auto mask_bit_counter = [] __device__ (valid32_t const mask)
+  int * d_count32;
+  cudaStream_t count_stream;
+  if(num_masks32 > 0)
   {
-    // TODO What type will Thrust use for the temporary storage
-    // during the transform_reduce? We could store this result in
-    // an int8_t, but the reduction would overflow an int8_t accumulator
-    return __popc(mask);
-  };
+    CUDA_TRY(cudaStreamCreate(&count_stream));
+    // Cast validity buffer to 4 byte type
+    valid32_t const * masks32 = reinterpret_cast<valid32_t const *>(masks);
 
-  // Cast validity buffer to 4 byte type
-  thrust::device_ptr<valid32_t const> masks32 = thrust::device_pointer_cast(reinterpret_cast<valid32_t const *>(masks));
+    CUDA_TRY(cudaMalloc(&d_count32, sizeof(int)));
+    CUDA_TRY(cudaMemsetAsync(d_count32, 0, sizeof(int),count_stream));
 
-  // Count the number of valid bits in all the masks that are a 
-  // multiple of 4 bytes
-  const size_t count32 = thrust::transform_reduce(masks32,
-                                                  masks32 + num_masks32,
-                                                  mask_bit_counter,
-                                                  0,
-                                                  thrust::plus<int>());
+    const int grid_size = (num_masks32 + block_size - 1)/block_size;
+    count_valid_bits<<<grid_size, block_size,0,count_stream>>>(masks32, num_masks32, d_count32);
 
-  CUDA_TRY( cudaGetLastError() );
+    CUDA_TRY( cudaGetLastError() );
+  }
 
   // Count the number of valid bits in the remainder masks
   size_t remainder_count = 0;
@@ -139,6 +168,14 @@ gdf_error gdf_count_nonzero_mask(gdf_valid_type const * masks, int num_rows, int
     CUDA_TRY(cudaStreamSynchronize(copy_stream));
     CUDA_TRY(cudaStreamDestroy(copy_stream));
     remainder_count = count_valid_bits_host(remainder_masks, num_rows);
+  }
+
+  int count32{0};
+  if(num_masks32 > 0)
+  {
+    CUDA_TRY(cudaMemcpyAsync(&count32, d_count32, sizeof(int), cudaMemcpyDeviceToHost,count_stream));
+    CUDA_TRY(cudaStreamSynchronize(count_stream));
+    CUDA_TRY(cudaStreamDestroy(count_stream));
   }
 
   // The final count of valid bits is the sum of the result from the
