ca9a2e45a9d3bb1107b77a6b40141b057a255702
Jake Hemstad <jhemstad@nvidia.com>
2018-10-01 09:41:35 -0700

    Updated the PUSH_RANGE POP_RANGE to be functions instead of macros. Added COLOR enumerations.

--- src/nvtx_utils.h
+++ src/nvtx_utils.h
@@ -3,26 +3,46 @@
 
 #ifdef USE_NVTX
 #include "nvToolsExt.h"
-
-const uint32_t colors[] = { 0xff00ff00, 0xff0000ff, 0xffffff00, 0xffff00ff, 0xff00ffff, 0xffff0000, 0xffffffff };
-const int num_colors = sizeof(colors)/sizeof(uint32_t);
-
-#define PUSH_RANGE(name,cid) { \
-    int color_id = cid; \
-    color_id = color_id%num_colors;\
-    nvtxEventAttributes_t eventAttrib = {0}; \
-    eventAttrib.version = NVTX_VERSION; \
-    eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE; \
-    eventAttrib.colorType = NVTX_COLOR_ARGB; \
-    eventAttrib.color = colors[color_id]; \
-    eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII; \
-    eventAttrib.message.ascii = name; \
-    nvtxRangePushEx(&eventAttrib); \
-}
-#define POP_RANGE nvtxRangePop();
-#else
-#define PUSH_RANGE(name,cid)
-#define POP_RANGE
 #endif
+
+enum class COLOR : uint32_t
+{
+  GREEN = 0xff00ff00, 
+  BLUE = 0xff0000ff,
+  YELLOW = 0xffffff00, 
+  PURPLE = 0xffff00ff,
+  CYAN = 0xff00ffff, 
+  RED = 0xffff0000, 
+  WHITE = 0xffffffff,
+};
+
+constexpr COLOR JOIN_COLOR = COLOR::CYAN;
+constexpr COLOR GROUPBY_COLOR = COLOR::GREEN;
+constexpr COLOR BINARY_OP_COLOR = COLOR::YELLOW;
+constexpr COLOR PARTITION_COLOR = COLOR::PURPLE;
+
+inline 
+void PUSH_RANGE(std::string const & name, const COLOR color) 
+{
+#ifdef USE_NVTX
+    nvtxEventAttributes_t eventAttrib = {0}; 
+    eventAttrib.version = NVTX_VERSION; 
+    eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE; 
+    eventAttrib.colorType = NVTX_COLOR_ARGB; 
+    eventAttrib.color = static_cast<uint32_t>(color); 
+    eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII; 
+    eventAttrib.message.ascii = name.c_str(); 
+    nvtxRangePushEx(&eventAttrib); 
+#endif
+}
+
+inline 
+void POP_RANGE(void)
+{
+#ifdef USE_NVTX
+  nvtxRangePop();
+#endif
+}
+
 
 #endif
