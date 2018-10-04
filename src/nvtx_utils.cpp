de5070898d0bda2bce77bdc487f58895478f37d0
Jake Hemstad <jhemstad@nvidia.com>
2018-10-04 07:49:22 -0700

    Added additional libgdf function for nvtx range push that accepts a custom ARGB hex color code.

--- src/nvtx_utils.cpp
+++ src/nvtx_utils.cpp
@@ -31,6 +31,31 @@ gdf_error gdf_nvtx_range_push(char const * const name, gdf_color color ){
 
 /* --------------------------------------------------------------------------*/
 /** 
+ * @Synopsis  Start a NVTX range with a custom ARGB color code.
+ *
+ * This function is useful only for profiling with nvvp or Nsight Systems. It
+ * demarcates the begining of a user-defined range with a specified name and
+ * color that will show up in the timeline view of nvvp/Nsight Systems. Can be
+ * nested within other ranges.
+ * 
+ * @Param name The name of the NVTX range
+ * @Param color The ARGB hex color code to use to color this range (e.g., 0xFF00FF00)
+ * 
+ * @Returns   
+ */
+/* ----------------------------------------------------------------------------*/
+gdf_error gdf_nvtx_range_push_hex(char const * const name, unsigned int color ){
+
+  if(nullptr == name)
+    return GDF_NULL_NVTX_NAME;
+
+  PUSH_RANGE(name, color);
+
+  return GDF_SUCCESS;
+}
+
+/* --------------------------------------------------------------------------*/
+/** 
  * @Synopsis Ends the inner-most NVTX range.
  *
  * This function is useful only for profiling with nvvp or Nsight Systems. It
