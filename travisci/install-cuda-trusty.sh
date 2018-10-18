39f7cff4f1509f015e25e3aa0391c9cae1c77f2f
Mike Wendt <mike.wendt@me.com>
2018-10-18 06:10:42 -0400

    FIX Tar permissions issues

--- travisci/install-cuda-trusty.sh
+++ travisci/install-cuda-trusty.sh
@@ -50,7 +50,7 @@ WORK_DIR=`pwd`
 cd /tmp
 travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/libcuda1-396_396.44-0ubuntu1_amd64.deb
 ar -x libcuda*.deb
-tar xvf data.tar.xz
+sudo tar xvf data.tar.xz
 sudo mv usr/lib/x86_64-linux-gnu /usr/lib
 cd $WORK_DIR
 
