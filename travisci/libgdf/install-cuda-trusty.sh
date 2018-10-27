32c3ec7b6ca15b161f834d1437da18b751b97ff4
Mike Wendt <mike.wendt@me.com>
2018-10-26 17:24:14 -0700

    ENH Add installer support for CUDA 10 and refactor
    Make more modular instead of repeating if thens

--- travisci/libgdf/install-cuda-trusty.sh
+++ travisci/libgdf/install-cuda-trusty.sh
@@ -10,30 +10,41 @@
 #
 set -e
 
-if [ ${CUDA:0:3} == '9.0' ]
+# Get CUDA release
+if [ ${CUDA:0:1} == '9' ]
 then
-    # CUDA 9 we use the sh installer
-    travis_retry wget --progress=dot:giga https://developer.nvidia.com/compute/cuda/${CUDA:0:3}/Prod/local_installers/cuda_${CUDA}_linux-run
-    chmod +x cuda_*_linux-run
-    sudo ./cuda_*_linux-run --silent --toolkit
-elif [ ${CUDA:0:3} == '9.1' ]
+    # CUDA 9 release
+    CUDA_REL=${CUDA:0:3}
+elif [ ${CUDA:0:2} == '10' ]
 then
-    # CUDA 9.1 has a different filename pattern
-    travis_retry wget --progress=dot:giga https://developer.nvidia.com/compute/cuda/${CUDA:0:3}/Prod/local_installers/cuda_${CUDA}_linux
-    chmod +x cuda_*_linux
-    sudo ./cuda_*_linux --silent --toolkit
-elif [ ${CUDA:0:3} == '9.2' ]
-then
-    # Cuda 9.2 has a different url pattern
-    travis_retry wget --progress=dot:giga https://developer.nvidia.com/compute/cuda/${CUDA:0:3}/Prod2/local_installers/cuda_${CUDA}_linux
-    chmod +x cuda_*_linux
-    sudo ./cuda_*_linux --silent --toolkit
+    # CUDA 10 release
+    CUDA_REL=${CUDA:0:4}
 else
     # Didn't match one of the expected CUDA builds, exit
     echo "CUDA version not specified or invalid version specified!"
     exit 1
 fi
 
-export CUDA_HOME=/usr/local/cuda-${CUDA:0:3}
+# Default values
+CUDA_PROD="Prod"
+CUDA_FILE="cuda_${CUDA}_linux"
+
+# Handle CUDA releases with non-default vals
+if [ $CUDA_REL == '9.0' ]
+then
+    # CUDA 9 has different file name
+    CUDA_FILE="cuda_${CUDA}_linux-run"
+elif [ ${CUDA:0:3} == '9.2' ]
+then
+    # Cuda 9.2 has a different url pattern
+    CUDA_PROD="Prod2"
+fi
+
+# Download and install CUDA
+travis_retry wget --progress=dot:giga https://developer.nvidia.com/compute/cuda/${CUDA_REL}/${CUDA_PROD}/local_installers/${CUDA_FILE}
+chmod +x cuda_*_linux*
+sudo ./cuda_*_linux* --silent --toolkit
+
+export CUDA_HOME=/usr/local/cuda-${CUDA_REL}
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
 export PATH=${CUDA_HOME}/bin:${PATH}
