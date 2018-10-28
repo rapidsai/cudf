6a046e75365e44e6411b84d2d574015bafc4e023
Mike Wendt <mike.wendt@me.com>
2018-10-28 13:13:52 -0400

    FIX Restrict upload to cudf builds only
    This fixes the issue with missing labels as this was being triggered on CUDA 10 builds and overwriting the labels of the libgdf_cffi job

--- travisci/upload-anaconda.sh
+++ travisci/upload-anaconda.sh
@@ -7,21 +7,21 @@ set -e
 if [ "$BUILD_CUDF" == "1" ]; then
   export UPLOADFILE=`conda build conda-recipes/cudf -c defaults -c conda-forge -c numba -c rapidsai/label/dev --python=$PYTHON --output`
   SOURCE_BRANCH=master
+
+  test -e ${UPLOADFILE}
+
+  # Pull requests or commits to other branches shouldn't upload
+  #if [ ${TRAVIS_PULL_REQUEST} != false -o ${TRAVIS_BRANCH} != ${SOURCE_BRANCH} ]; then
+  #  echo "Skipping upload"
+  #  return 0
+  #fi
+
+  if [ -z "$MY_UPLOAD_KEY" ]; then
+    echo "No upload key"
+    return 0
+  fi
+
+  echo "Upload"
+  echo ${UPLOADFILE}
+  anaconda -t ${MY_UPLOAD_KEY} upload -u rapidsai -l main --force ${UPLOADFILE}
 fi
-
-test -e ${UPLOADFILE}
-
-# Pull requests or commits to other branches shouldn't upload
-#if [ ${TRAVIS_PULL_REQUEST} != false -o ${TRAVIS_BRANCH} != ${SOURCE_BRANCH} ]; then
-#  echo "Skipping upload"
-#  return 0
-#fi
-
-if [ -z "$MY_UPLOAD_KEY" ]; then
-  echo "No upload key"
-  return 0
-fi
-
-echo "Upload"
-echo ${UPLOADFILE}
-anaconda -t ${MY_UPLOAD_KEY} upload -u rapidsai -l main --force ${UPLOADFILE}
