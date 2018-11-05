df518bf3ee2b4ce12e00e1b11d752236d4e1c2cc
Keith Kraus <kkraus@nvidia.com>
2018-11-05 11:27:06 -0500

    Fix invalid escape sequence flake8 errors

--- cudf/tests/test_categorical.py
+++ cudf/tests/test_categorical.py
@@ -125,7 +125,7 @@ def test_categorical_binary_add():
 
     with pytest.raises(TypeError) as raises:
         pdsr + pdsr
-    raises.match('Categorical cannot perform the operation \+')
+    raises.match(r'Categorical cannot perform the operation \+')
 
     with pytest.raises(TypeError) as raises:
         sr + sr
