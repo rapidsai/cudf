3a80fc9a93eb9b0f67aa79b3816c43af7d7b9294
Mike Wendt <mike.wendt@me.com>
2018-11-05 15:54:23 -0500

    FIX Add missing channel for nvstrings

--- travisci/libgdf/build_libgdf.sh
+++ travisci/libgdf/build_libgdf.sh
@@ -9,5 +9,5 @@ fi
 
 if [ "$BUILD_LIBGDF" == '1' -o "$BUILD_CFFI" == '1' ]; then
     echo "Building libgdf"
-    conda build conda-recipes/libgdf -c defaults -c conda-forge -c numba
+    conda build conda-recipes/libgdf -c defaults -c conda-forge -c numba -c rapidsai
 fi
