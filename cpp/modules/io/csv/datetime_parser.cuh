5c645be2537cfc48febed113a66841d43f647899
Matthew Jones <majones@dgx15.aselab.nvidia.com>
2018-11-07 23:29:40 -0800

    fixed trailing issue from reset

--- cpp/modules/io/csv/datetime_parser.cuh
+++ cpp/modules/io/csv/datetime_parser.cuh
@@ -62,7 +62,7 @@
 
 #pragma once
 
-#include <gdf/gdf.h>
+#include "cudf.h"
 #include "type_conversion.cuh"
 
 __host__ __device__ gdf_date32 parseDateFormat(char *data, long start_idx, long end_idx, bool dayfirst);
