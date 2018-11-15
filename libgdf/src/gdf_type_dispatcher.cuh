09594ad4c9d03b6cd00744398ab563227e3d702f
Jake Hemstad <jhemstad@nvidia.com>
2018-11-14 16:37:51 -0800

    Added enum_map to gdf_type_dispatcher. In order to add flexibility to the gdf_type_dispatcher, I've added an enum_map template template parameter. This enum_map can be used to determine the mapping between gdf_dtype enum and underlying datatype.

--- libgdf/src/gdf_type_dispatcher.cuh
+++ libgdf/src/gdf_type_dispatcher.cuh
@@ -4,6 +4,18 @@
 #include <utility>
 #include <gdf/cffi/types.h>
 
+template<gdf_dtype t> struct default_enum_map;
+template <> struct default_enum_map<GDF_INT8>{ using type = int8_t; };
+template <> struct default_enum_map<GDF_INT16>{ using type = int16_t; };
+template <> struct default_enum_map<GDF_INT32>{ using type = int32_t; };
+template <> struct default_enum_map<GDF_INT64>{ using type = int64_t; };
+template <> struct default_enum_map<GDF_FLOAT32>{ using type = float; };
+template <> struct default_enum_map<GDF_FLOAT64>{ using type = double; };
+template <> struct default_enum_map<GDF_DATE32>{ using type = gdf_date32; };
+template <> struct default_enum_map<GDF_DATE64>{ using type = gdf_date64; };
+template <> struct default_enum_map<GDF_TIMESTAMP>{ using type = gdf_timestamp; };
+template <> struct default_enum_map<GDF_CATEGORY>{ using type = gdf_category; };
+
 /* --------------------------------------------------------------------------*/
 /** 
  * @brief  Invokes a templated functor by dispatching the appropriate type from 
@@ -50,46 +62,51 @@
  * parameter *must* be of type "auto" and the purpose of this parameter is solely
  * to retrieve the dispatched type.
  *
+ * The return type for all template instantiations of the functor's "operator()" 
+ * must be the same, otherwise there will be a compiler error as you would be 
+ * trying to return different types from the same function.
+ *
  * @Param dtype The gdf_dtype enum that determines which type will be dispatched
  * @Param f The functor with a templated "operator()" that will be invoked with 
  * the dispatched type
  * @Param args A parameter-pack (i.e., arbitrary number of arguments) that will 
  * be perfectly-forwarded as the arguments of the functor's "operator()".
+ * @tparam enum_map A templated structure that maps a gdf_dtype enum to a C++ type
  *
  * @Returns Whatever is returned by the functor's "operator()". 
  *
- * NOTE: The return type for all template instantiations of the functor's "operator()" 
- * must be the same, otherwise there will be a compiler error as you would be 
- * trying to return different types from the same function.
  */
 /* ----------------------------------------------------------------------------*/
 // This pragma disables a compiler warning that complains about the valid usage
 // of calling a __host__ functor from this function which is __host__ __device__
 #pragma hd_warning_disable
-template <class functor_t, typename... Ts>
+template < template <gdf_dtype> typename enum_map = default_enum_map, 
+           class functor_t, 
+           typename... Ts>
 __host__ __device__ __forceinline__
-decltype(auto) gdf_type_dispatcher(gdf_dtype dtype, functor_t f, Ts&&... args)
+decltype(auto) gdf_type_dispatcher(gdf_dtype dtype, 
+                                   functor_t f, 
+                                   Ts&&... args)
 {
-    switch(dtype)
-    {
-      // The .template is known as a "template disambiguator" 
-      // See here for more information: https://stackoverflow.com/questions/3786360/confusing-template-error
-      case GDF_INT8:      { return f.template operator()<int8_t>(std::forward<Ts>(args)...); }
-      case GDF_INT16:     { return f.template operator()<int16_t>(std::forward<Ts>(args)...); }
-      case GDF_INT32:     { return f.template operator()<int32_t>(std::forward<Ts>(args)...); }
-      case GDF_INT64:     { return f.template operator()<int64_t>(std::forward<Ts>(args)...); }
-      case GDF_FLOAT32:   { return f.template operator()<float>(std::forward<Ts>(args)...); }
-      case GDF_FLOAT64:   { return f.template operator()<double>(std::forward<Ts>(args)...); }
-      case GDF_DATE32:    { return f.template operator()<int32_t>(std::forward<Ts>(args)...); }
-      case GDF_DATE64:    { return f.template operator()<int64_t>(std::forward<Ts>(args)...); }
-      case GDF_TIMESTAMP: { return f.template operator()<int64_t>(std::forward<Ts>(args)...); }
-      case GDF_CATEGORY:  { return f.template operator()<int32_t>(std::forward<Ts>(args)...); }
-    }
-
-    // Need to find out what the return type is in order to have a default return value
-    // and solve the compiler warning for lack of a default return
-    using return_type = decltype(f.template operator()<int8_t>(std::forward<Ts>(args)...));
-    return return_type();
+  switch(dtype)
+  {
+    // The .template is known as a "template disambiguator" 
+    // See here for more information: https://stackoverflow.com/questions/3786360/confusing-template-error
+    case GDF_INT8:      { return f.template operator()<typename enum_map<GDF_INT8>::type>(std::forward<Ts>(args)...); }
+    case GDF_INT16:     { return f.template operator()<typename enum_map<GDF_INT16>::type>(std::forward<Ts>(args)...); }
+    case GDF_INT32:     { return f.template operator()<typename enum_map<GDF_INT32>::type>(std::forward<Ts>(args)...); }
+    case GDF_INT64:     { return f.template operator()<typename enum_map<GDF_INT64>::type>(std::forward<Ts>(args)...); }
+    case GDF_FLOAT32:   { return f.template operator()<typename enum_map<GDF_FLOAT32>::type>(std::forward<Ts>(args)...); }
+    case GDF_FLOAT64:   { return f.template operator()<typename enum_map<GDF_FLOAT64>::type>(std::forward<Ts>(args)...); }
+    case GDF_DATE32:    { return f.template operator()<typename enum_map<GDF_DATE32>::type>(std::forward<Ts>(args)...); }
+    case GDF_DATE64:    { return f.template operator()<typename enum_map<GDF_DATE64>::type>(std::forward<Ts>(args)...); }
+    case GDF_TIMESTAMP: { return f.template operator()<typename enum_map<GDF_TIMESTAMP>::type>(std::forward<Ts>(args)...); }
+    case GDF_CATEGORY:  { return f.template operator()<typename enum_map<GDF_CATEGORY>::type>(std::forward<Ts>(args)...); }
+  }
+  // Need to find out what the return type is in order to have a default return value
+  // and solve the compiler warning for lack of a default return
+  using return_type = decltype(f.template operator()<typename enum_map<GDF_INT8>::type>(std::forward<Ts>(args)...));
+  return return_type();
 }
 
 #endif
