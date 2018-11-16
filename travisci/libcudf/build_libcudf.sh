3041839fdadbab070168d4c69a2cd2374d3852c8
Keith Kraus <kkraus@nvidia.com>
2018-11-16 18:56:29 -0500

    Fix path to sed command

--- travisci/libcudf/build_libcudf.sh
+++ travisci/libcudf/build_libcudf.sh
@@ -4,7 +4,7 @@ set -e
 ## See https://github.com/rapidsai/libgdf/pull/149
 if [ ${CUDA:0:4} == '10.0' ]; then
   echo "CUDA 10 detected, removing hash_map tests"
-  sed -i.bak 's/ConfigureTest(HASH_MAP_TEST "${HASH_MAP_TEST_SRC}")/#ConfigureTest(HASH_MAP_TEST "${HASH_MAP_TEST_SRC}")/g' ./cpp/modules/tests/CMakeLists.txt
+  sed -i.bak 's/ConfigureTest(HASH_MAP_TEST "${HASH_MAP_TEST_SRC}")/#ConfigureTest(HASH_MAP_TEST "${HASH_MAP_TEST_SRC}")/g' ./cpp/tests/CMakeLists.txt
 fi
 
 if [ "$BUILD_LIBCUDF" == '1' -o "$BUILD_CFFI" == '1' ]; then
