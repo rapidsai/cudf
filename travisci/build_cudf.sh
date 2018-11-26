9adbeba733f8f9de3928668aa0f97c9cc4bfac79
aocsa <aocsa.cs@gmail.com>
2018-11-26 15:55:30 -0500

    [wip] rebase to cudf0.3

--- travisci/build_cudf.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-if [ "$BUILD_CUDF" == "1" ]; then
-  echo "Building cudf"
-  export CUDF_BUILD_NO_GPU_TEST=1 
-  travis_retry conda build conda-recipes/cudf -c rapidsai -c nvidia -c numba -c conda-forge -c defaults --python=$PYTHON
-fi
