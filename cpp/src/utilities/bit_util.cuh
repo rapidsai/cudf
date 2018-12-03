86d1482afc8697b11cb27ad6f3f4323035312e9a
Brad Rees <brees@nvidia.com>
2018-12-03 12:31:57 -0500

    updating bitmaps

--- cpp/src/utilities/bit_util.cuh
+++ cpp/src/utilities/bit_util.cuh
@@ -21,28 +21,31 @@ namespace gdf {
 namespace util {
 
 static constexpr int ValidSize = 32;
-using ValidType = uint32_t;
+using ValidType = gdf_valid_type;
+
+__device__ int which_bitmap_record(int record_idx) { return (record_idx/ValidSize);  }
+__device__ int which_bit(int record_idx) { return (record_idx % ValidSize);  }
+
+__device__ gdf_error setBitValue(gdf_valid_type * valid, int record_idx, unsigned int value) {
+
+	int status = 0;
+
+	if ( value > 1)
+		return GDF_INVALID_API_CALL;
+
+	int rec = which_bitmap_record(record_idx);
+	int bit = which_bit(record_idx);
+
+	if (value == 0)
+		status = atomicAnd( &valid[rec], (value << bit));
+	else
+		status = atomicOr( &valid[rec], (value << bit));
 
 
-
-
-/**
- * Allocate device space for the valid bitmap
- *
- * @param[out] gdf_valid_type *      pointer to where device memory will be allocated and returned
- * @param[in]  number_of_records     number of records
- * @param[in]  fill_value            optional, should the memory be initialized to all 0 or 1s. All other values indicate un-initialized
- */
-gdf_error create_bitmap(gdf_valid_type *valid, int number_of_records, int fill_value = -1) {
-
-	int num_bits_rec = (number_of_records + (ValidSize - 1)) / ValidSize;
-
-	RMM_TRY( RMM_ALLOC((void**)&valid, 	sizeof(ValidType) * num_bits_rec, 0) );
-
-	if (fill_value == 0) {      CUDA_TRY( cudaMemset(valid,	0,           sizeof(ValidType) * num_bits_rec));  }
-	else if (fill_value == 1) { CUDA_TRY( cudaMemset(valid,	0xFFFFFFFF, sizeof(ValidType) * num_bits_rec));  }
-
-	return GDF_SUCCESS;
+	if ( status == valid[rec])
+		return GDF_SUCCESS;
+	else
+		return GDF_CUDA_ERROR;
 }
 
 
@@ -55,45 +58,46 @@ gdf_error create_bitmap(gdf_valid_type *valid, int number_of_records, int fill_v
  */
 __device__ bool is_valid(gdf_valid_type * valid_masks, int record_idx) {
 
-	int rec = whichBitmapRecord(record_idx);
-	int bit = whichBit(record_idx);
+	int rec = which_bitmap_record(record_idx);
+	int bit = which_bit(record_idx);
 
-	int status = atomicAnd(valid_masks[rec], (1 << bit));
+	int status = atomicAnd(&valid_masks[rec], (1 << bit));
 
 	return (status == 0) ? false : true;
 }
 
 
-__device__ gdf_error set_bit(int record_idx) {
-	return setBitValue(record_idx, 1);
+__device__ gdf_error set_bit(gdf_valid_type * valid, int record_idx) {
+	return setBitValue(valid, record_idx, 1U);
 }
 
-__device__ gdf_error clear_bit(int record_idx) {
-	return setBitValue(record_idx, 0);
-}
-
-
-__device__ gdf_error setBitValue(int record_idx, int value) {
-
-	if ( value < 0 || value > 1)
-		return GDF_INVALID_API_CALL;
-
-	int rec = whichBitmapRecord(record_idx);
-	int bit = whichBit(record_idx);
-
-	int status = atomicOr(valid_masks[rec], (value << bit));
-
-	if ( status == valid_masks[rec])
-		return GDF_SUCCESS;
-	else
-		GDF_CUDA_ERROR;
+__device__ gdf_error clear_bit(gdf_valid_type * valid, int record_idx) {
+	return setBitValue(valid, record_idx, 0U);
 }
 
 
+/**
+ * Allocate device space for the valid bitmap.  Memory is padded to a multiple of 64-bytes
+ *
+ * @param[out] gdf_valid_type *      pointer to where device memory will be allocated and returned
+ * @param[in]  number_of_records     number of records
+ * @param[in]  fill_value            optional, should the memory be initialized to all 0 or 1s. All other values indicate un-initialized
+ */
+gdf_error create_bitmap(gdf_valid_type *valid, int number_of_records, int fill_value = -1) {
+
+	int num_bits_rec = (number_of_records + 63) / ValidSize;
+
+	RMM_TRY( RMM_ALLOC((void**)&valid, 	sizeof(ValidType) * num_bits_rec, 0) );
+
+	if (fill_value == 0) {      CUDA_TRY( cudaMemset(valid,	0,           sizeof(ValidType) * num_bits_rec));  }
+	else if (fill_value == 1) { CUDA_TRY( cudaMemset(valid,	0xEFFFFFFF, sizeof(ValidType) * num_bits_rec));  }
+
+	return GDF_SUCCESS;
+}
 
 
-__device__ int whichBitmapRecord(int record_idx) { return (record_idx/ValidSize);  }
-__device__ int whichBit(int record_idx) { return (bit % ValidSize);  }
+
+
 
 
 
@@ -145,10 +149,10 @@ __device__ int whichBit(int record_idx) { return (bit % ValidSize);  }
 //  bits[i / 8] &= flipped_bitmask(i % 8);
 //}
 //
-//__host__ __device__ __forceinline__ size_t last_byte_index(size_t column_size)
-//{
-//  return (column_size + 8 - 1) / 8;
-//}
+__host__ __device__ __forceinline__ size_t last_byte_index(size_t column_size)
+{
+  return (column_size + 8 - 1) / 8;
+}
 
 static inline std::string chartobin(gdf_valid_type c, size_t size = 8)
 {
