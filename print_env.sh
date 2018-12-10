9059140aa370637a0da6dfa7a2c85b6f6022a0e6
Jake Hemstad <jhemstad@nvidia.com>
2018-12-10 08:41:23 -0800

    Create script to print relevant environment details for cuDF.

---
+++ print_env.sh
@@ -0,0 +1,36 @@
+echo "***cat /etc/*-release***"
+cat /etc/*-release
+
+echo " "
+
+echo "***uname -a***"
+uname -a
+
+echo " "
+
+echo "***nvidia-smi***"
+nvidia-smi
+
+echo " "
+
+echo '***conda list "pandas|arrow|numpy|^python$"***'
+conda list 'pandas|arrow|numpy|^python$'
+
+echo " "
+
+echo "***which cmake && cmake --version***"
+which cmake && cmake --version
+
+echo " "
+
+echo "***which g++ && g++ --version***"
+which g++ && g++ --version
+
+echo " "
+
+echo "***which nvcc && nvcc --version***"
+which nvcc && nvcc --version
+
+
+
+
