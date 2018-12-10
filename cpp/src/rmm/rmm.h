12e2582846fb251e7fb0df4569a7d06666c29105
Jake Hemstad <jhemstad@nvidia.com>
2018-12-10 06:52:14 -0800

    Moved definitions of RMM C++ API to memory.hpp

--- cpp/src/rmm/rmm.h
+++ cpp/src/rmm/rmm.h
@@ -12,10 +12,12 @@ extern "C" {
  * @brief Device memory alloc / realloc / free macros that pass the calling file
  * and line number to RMM for tracking.
  * ---------------------------------------------------------------------------**/
-#define RMM_ALLOC(ptr, sz, stream) rmm::alloc((ptr), (sz), (stream),  __FILE__, __LINE__)
+#define RMM_ALLOC(ptr, sz, stream) \
+  rmm::alloc((ptr), (sz), (stream), __FILE__, __LINE__)
 
-#define RMM_REALLOC(ptr, new_sz, stream) rmm::realloc((ptr), (new_sz), (stream),  __FILE__, __LINE__)
+#define RMM_REALLOC(ptr, new_sz, stream) \
+  rmm::realloc((ptr), (new_sz), (stream), __FILE__, __LINE__)
 
-#define RMM_FREE(ptr, stream) rmmFree((ptr), (stream), __FILE__, __LINE__)
+#define RMM_FREE(ptr, stream) rmm::free((ptr), (stream), __FILE__, __LINE__)
 
-#endif // RMM_H
+#endif  // RMM_H
