b7efb6d7136a897eed142c86771d0c05321b6f56
Jake Hemstad <jhemstad@nvidia.com>
2018-12-10 08:49:25 -0800

    Added printing of most recent git commit.

--- print_env.sh
+++ print_env.sh
@@ -1,31 +1,29 @@
+echo "***git log --decorate -n 1***"
+git log --decorate -n 1
+echo " "
+
 echo "***cat /etc/*-release***"
 cat /etc/*-release
-
 echo " "
 
 echo "***uname -a***"
 uname -a
-
 echo " "
 
 echo "***nvidia-smi***"
 nvidia-smi
-
 echo " "
 
 echo '***conda list "pandas|arrow|numpy|^python$"***'
 conda list 'pandas|arrow|numpy|^python$'
-
 echo " "
 
 echo "***which cmake && cmake --version***"
 which cmake && cmake --version
-
 echo " "
 
 echo "***which g++ && g++ --version***"
 which g++ && g++ --version
-
 echo " "
 
 echo "***which nvcc && nvcc --version***"
