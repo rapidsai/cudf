ddce6f7069f0d348a1a60b671e48a86079230205
Mark Harris <mharris@nvidia.com>
2018-12-18 10:21:15 -0800

    Update cpp/src/io/csv/csv_reader.cu
    Co-Authored-By: j-ieong <45218324+j-ieong@users.noreply.github.com>

--- cpp/src/io/csv/csv_reader.cu
+++ cpp/src/io/csv/csv_reader.cu
@@ -1296,7 +1296,7 @@ gdf_error launch_dataTypeDetection(
 	opts.trueValues			= thrust::raw_pointer_cast(raw_csv->d_trueValues.data());
 	opts.trueValuesCount	= raw_csv->d_trueValues.size();
 	opts.falseValues		= thrust::raw_pointer_cast(raw_csv->d_falseValues.data());
-	opts.falseValuesCount	= raw_csv->d_falseValues.size();
+	opts.falseValuesCount		= raw_csv->d_falseValues.size();
 
 	dataTypeDetection <<< gridSize, blockSize >>>(
 		raw_csv->data,
