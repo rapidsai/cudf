d75b71bffdbad1ffc88c8bf39916c0e629727902
Matthew Rocklin <mrocklin@gmail.com>
2019-01-03 10:21:35 -0800

    apply black to cudf/dataframe/core.py

--- python/cudf/dataframe/core.py
+++ python/cudf/dataframe/core.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+
 def get_renderable_pandas_dataframe(gdf):
     n = pd.core.config.get_option("display.max_rows")
     if len(gdf) <= n:
         return gdf.to_pandas()
     else:
-        return pd.concat([gdf.head(n + 1), gdf.tail(n + 1)])  # enough head and tail to look the same, plus some extra
-
+        return pd.concat(
+            [gdf.head(n + 1), gdf.tail(n + 1)]
+        )  # enough head and tail to look the same, plus some extra
