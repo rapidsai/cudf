a09a0944ae9d15ff6e81e2b484b1bc742207629c
Raymond Douglass <ray@raydouglass.com>
2019-01-10 16:47:12 -0800

    BLD Add script to fake manylinux support

--- setup_pip.py
+++ setup_pip.py
@@ -6,7 +6,10 @@
 #   * librmm_cffi
 #   * cudf
 
-# cython, cffi, & numpy must be installed to run this
+# cython, cffi, nvstrings, & numpy must be installed to run this
+# conda create -n build python=X.Y cython cffi numpy -c nvidia nvstrings
+# python setup_pip.py bdist_wheel
+
 import os
 import subprocess
 import shutil
@@ -18,11 +21,18 @@ from distutils.command.install_headers import install_headers
 from distutils.sysconfig import get_python_lib
 from Cython.Build import cythonize
 import numpy
+import zipfile
+from hashlib import sha256
+from base64 import urlsafe_b64encode
 
 os.environ['RMM_HEADER'] = 'cpp/src/rmm/memory.h'
 os.environ['CUDF_INCLUDE_DIR'] = 'cpp/include/cudf'
 
 CMAKE_EXE = os.environ.get('CMAKE_EXE', shutil.which('cmake'))
+if not CMAKE_EXE:
+    print('cmake executable not found. '
+          'Set CMAKE_EXE environment or update your path')
+    sys.exit(1)
 
 
 def distutils_dir_name(dname):
@@ -96,7 +106,7 @@ class InstallHeaders(install_headers):
 # setup does not clean up the build directory, so do it manually
 shutil.rmtree('build', ignore_errors=True)
 
-cuda_version=''.join(os.environ.get('CUDA', 'unknown').split('.')[:2])
+cuda_version = ''.join(os.environ.get('CUDA', 'unknown').split('.')[:2])
 
 install_requires = [
     'pandas>=0.20,<0.21',
@@ -105,9 +115,12 @@ install_requires = [
     'pyarrow>=0.10,<0.11',
     'cffi>=1.0.0',
     'cython>=0.28,<0.29',
-    'numpy>=1.14',
-    'nvstrings-cuda{}'.format(cuda_version)
+    'numpy>=1.14'
 ]
+setup_requires = install_requires.copy()
+
+install_requires.append('nvstrings-cuda{}'.format(cuda_version))
+setup_requires.append('nvstrings')
 
 try:
     numpy_include = numpy.get_include()
@@ -134,12 +147,14 @@ packages = [
 ]
 packages += find_packages(where='python')
 
-setup(name='cudf-cuda{}'.format(cuda_version),
+name = 'cudf-cuda{}'.format(cuda_version)
+version = os.environ.get('GIT_DESCRIBE_TAG', '0.0.0.dev').lstrip('v')
+setup(name=name,
       description='cuDF - GPU Dataframe',
       long_description=open('README.md', encoding='UTF-8').read(),
       long_description_content_type='text/markdown',
       url='https://github.com/rapidsai/cudf',
-      version=os.environ.get('GIT_DESCRIBE_TAG', '0.0.0.dev').lstrip('v'),
+      version=version,
       classifiers=[
           "Intended Audience :: Developers",
           "Programming Language :: Python",
@@ -158,7 +173,7 @@ setup(name='cudf-cuda{}'.format(cuda_version),
           'cudf.tests': ['data/*'],
       },
       install_requires=install_requires,
-      setup_requires=install_requires,
+      setup_requires=setup_requires,
       python_requires='>=3.5,<3.7',
       cffi_modules=[
           'cpp/python/libgdf_cffi/libgdf_build.py:ffibuilder',
@@ -172,3 +187,84 @@ setup(name='cudf-cuda{}'.format(cuda_version),
       headers=['cpp/include/'],
       zip_safe=False
       )
+
+
+def convert_to_manylinux(name, version):
+    """
+    Modifies the arch metadata of a pip package linux_x86_64=>manylinux1_x86_64
+    :param name:
+    :param version:
+    :return:
+    """
+    # Get python version as XY (27, 35, 36, etc)
+    python_version = str(sys.version_info.major) + str(sys.version_info.minor)
+    name_version = '{}-{}'.format(name.replace('-', '_'), version)
+
+    # linux wheel package
+    dist_zip = '{0}-cp{1}-cp{1}m-linux_x86_64.whl'.format(name_version,
+                                                          python_version)
+    dist_zip_path = os.path.join('dist', dist_zip)
+    if not os.path.exists(dist_zip_path):
+        return
+
+    unzip_dir = 'dist/unzip'
+    os.makedirs(unzip_dir, exist_ok=True)
+    with zipfile.ZipFile(dist_zip_path, 'r') as zip_ref:
+        zip_ref.extractall(unzip_dir)
+
+    wheel_file = '{}.dist-info/WHEEL'.format(name_version)
+    new_wheel_str = ''
+    with open(os.path.join(unzip_dir, wheel_file)) as f:
+        for line in f.readlines():
+            if line.startswith('Tag'):
+                # Replace the linux tag
+                new_wheel_str += line.replace('linux', 'manylinux1')
+            else:
+                new_wheel_str += line
+
+    # compute hash & size of the new WHEEL file
+    # Follows https://www.python.org/dev/peps/pep-0376/#record
+    m = sha256()
+    m.update(new_wheel_str.encode('utf-8'))
+    hash = urlsafe_b64encode(m.digest()).decode('utf-8')
+    hash = hash.replace('=', '')
+
+    with open(os.path.join(unzip_dir, wheel_file), 'w') as f:
+        f.write(new_wheel_str)
+    statinfo = os.stat(os.path.join(unzip_dir, wheel_file))
+    byte_size = statinfo.st_size
+
+    record_file = os.path.join(unzip_dir,
+                               '{}.dist-info/RECORD'.format(name_version))
+    new_record_str = ''
+    with open(record_file) as f:
+        for line in f.readlines():
+            if line.startswith(wheel_file):
+                # Update the record for the WHEEL file
+                new_record_str += '{},sha256={},{}'.format(wheel_file, hash,
+                                                           str(byte_size))
+                new_record_str += os.linesep
+            else:
+                new_record_str += line
+
+    with open(record_file, 'w') as f:
+        f.write(new_record_str)
+
+    def zipdir(path, ziph):
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                ziph.write(os.path.join(root, file),
+                           os.path.join(root, file).replace(path, ''))
+
+    new_zip_name = dist_zip.replace('linux', 'manylinux1')
+    print('Generating new zip {}...'.format(new_zip_name))
+    zipf = zipfile.ZipFile(os.path.join('dist', new_zip_name),
+                           'w', zipfile.ZIP_DEFLATED)
+    zipdir(unzip_dir, zipf)
+    zipf.close()
+
+    shutil.rmtree(unzip_dir, ignore_errors=True)
+    os.remove(dist_zip_path)
+
+
+convert_to_manylinux(name, version)
