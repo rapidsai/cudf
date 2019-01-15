e2215925a38ec8e5a2f5fad5315a56f2763b77c1
Keith Kraus <kkraus@nvidia.com>
2019-01-15 18:14:16 -0500

    Try conda clean to fix package index cache?

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -31,6 +31,7 @@ logger "Activate conda env..."
 source activate gdf
 
 logger "Bump pyarrow"
+conda clean
 conda install -c conda-forge pyarrow=0.11.1 arrow-cpp=0.11.1 pandas>=0.23.4 numpy>=1.15.3
 
 logger "Check versions..."
