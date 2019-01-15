4a1d0167488635806bd0470f90193af829b6d4a2
Keith Kraus <kkraus@nvidia.com>
2019-01-15 18:16:12 -0500

    Forgot --index-cache

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -35,7 +35,7 @@ logger "Activate conda env..."
 source activate gdf
 
 logger "Bump pyarrow"
-conda clean
+conda clean --index-cache
 conda install -c conda-forge pyarrow=0.11.1 arrow-cpp=0.11.1 pandas>=0.23.4 numpy>=1.15.3
 
 logger "Check versions..."
