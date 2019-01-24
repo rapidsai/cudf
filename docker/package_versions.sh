9e30fb8ad23f7cb501862f91ae51f69f33b6829c
Keith Kraus <kkraus@nvidia.com>
2019-01-24 18:54:29 -0500

    Reorder conda channels for priority, fix edge case of package ending in a number and CUDA_VERSION being overloaded, fix variable check to fire when set instead of when not set

--- docker/package_versions.sh
+++ docker/package_versions.sh
@@ -5,53 +5,59 @@
 FILENAME=$1
 
 set_version() {
-    sed -i "/\- $1[<>=][^a-Z]*/\- $1=$2/" $FILENAME
+    sed -i "s/\- $1\([<>=][^a-zA-Z]*\)\?$/\- $1=$2/" $FILENAME
 }
 
 replace_text() {
-    sed -i "/$1/$2/" $FILENAME
+    sed -i "s/$1/$2/" $FILENAME
 }
 
 add_package() {
-    sed -i "/\- $1[<>=][^a-Z]*/a \- $2=$3/" $FILENAME
+    sed -i "s/\- $1\([<>=][^a-zA-Z]*\)\?$/a \- $2=$3/" $FILENAME
 }
 
-if [ -z "$PYTHON_VERSION" ]; then
+if [ "$PYTHON_VERSION" ]; then
     PACKAGE_NAME="python"
     set_version "$PACKAGE_NAME" "$PYTHON_VERSION"
 fi
 
-if [ -z "$NUMBA_VERSION" ]; then
+if [ "$NUMBA_VERSION" ]; then
     PACKAGE_NAME="numba"
     set_version "$PACKAGE_NAME" "$NUMBA_VERSION"
 fi
 
-if [ -z "$PANDAS_VERSION" ]; then
+if [ "$PANDAS_VERSION" ]; then
     PACKAGE_NAME="pandas"
     set_version "$PACKAGE_NAME" "$PANDAS_VERSION"
 fi
 
-if [ -z "$PYARROW_VERSION" ]; then
+if [ "$PYARROW_VERSION" ]; then
     PACKAGE_NAME="pyarrow"
     set_version "$PACKAGE_NAME" "$PYARROW_VERSION"
 fi
 
-if [ -z "$CYTHON_VERSION" ]; then
+if [ "$CYTHON_VERSION" ]; then
     PACKAGE_NAME="cython"
     set_version "$PACKAGE_NAME" "$CYTHON_VERSION"
 fi
 
-if [ -z "$CMAKE_VERSION" ]; then
+if [ "$CMAKE_VERSION" ]; then
     PACKAGE_NAME="cmake"
     set_version "$PACKAGE_NAME" "$CMAKE_VERSION"
 fi
 
-if [ -z "$CUDA_VERSION" ]; then
-    replace_text "nvidia" "nvidia\/label\/cuda$CUDA_VERSION"
-    replace_text "rapidsai" "rapidsai\/label\/cuda$CUDA_VERSION"
+if [ "${CUDA_VERSION:0:3}" == "9.2" ]; then
+    RAPIDS_CUDA_VERSION="9.2"
+elif [ "${CUDA_VERSION:0:4}" == "10.0" ]; then
+    RAPIDS_CUDA_VERSION="10.0"
 fi
 
-if [ -z "$NUMPY_VERSION" ]; then
+if [ "$RAPIDS_CUDA_VERSION" ]; then
+    replace_text "nvidia" "nvidia\/label\/cuda$RAPIDS_CUDA_VERSION"
+    replace_text "rapidsai" "rapidsai\/label\/cuda$RAPIDS_CUDA_VERSION"
+fi
+
+if [ "$NUMPY_VERSION" ]; then
     ABOVE_PACKAGE="pandas"
     PACKAGE_NAME="numpy"
     add_package "$ABOVE_PACKAGE" "$PACKAGE_NAME" "$NUMPY_VERSION"
