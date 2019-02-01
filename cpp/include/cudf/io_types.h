a0249fbfe24933514400a93364852276bb4a6c37
Vukasin Milovanovic <vukasin.milovanovic.87@gmail.com>
2019-02-01 12:08:21 -0800

    Fix doxygen comment format from //< to ///<

--- cpp/include/cudf/io_types.h
+++ cpp/include/cudf/io_types.h
@@ -27,8 +27,8 @@
    */
 typedef enum 
 {
-  FILE_PATH,								//< Indicates that the input is specified with a file path
-  HOST_BUFFER								//< Indicates that the input is passed as a buffer in host memory
+  FILE_PATH,								///< Indicates that the input is specified with a file path
+  HOST_BUFFER								///< Indicates that the input is passed as a buffer in host memory
 } gdf_csv_input_form;
 
 /**---------------------------------------------------------------------------*
@@ -44,63 +44,63 @@ typedef struct {
   /*
    * Output Arguments - allocated in reader.
    */
-  int           num_cols_out;               //< Out: return the number of columns read in
-  int           num_rows_out;               //< Out: return the number of rows read in
-  gdf_column    **data;                     //< Out: return the array of *gdf_columns
+  int           num_cols_out;               ///< Out: return the number of columns read in
+  int           num_rows_out;               ///< Out: return the number of rows read in
+  gdf_column    **data;                     ///< Out: return the array of *gdf_columns
 
   /*
    * Input arguments - all data is in the host memory
    */
-  gdf_csv_input_form	input_data_form;	//< Type of source of CSV data
-  const char			*filepath_or_buffer;//< If input_data_form is FILE_PATH, contains the filepath. If input_data_type is HOST_BUFFER, points to the host memory buffer
-  size_t				buffer_size;		//< If input_data_form is HOST_BUFFER, represents the size of the buffer in bytes. Unused otherwise
+  gdf_csv_input_form	input_data_form;	///< Type of source of CSV data
+  const char			*filepath_or_buffer;///< If input_data_form is FILE_PATH, contains the filepath. If input_data_type is HOST_BUFFER, points to the host memory buffer
+  size_t				buffer_size;		///< If input_data_form is HOST_BUFFER, represents the size of the buffer in bytes. Unused otherwise
 
-  bool          windowslinetermination;     //< States if we should \r\n as our line termination
-  char          lineterminator;             //< define the line terminator character. Default is  '\n'
-  char          delimiter;                  //< define the field separator, default is ',' This argument is also called 'sep' 
+  bool          windowslinetermination;     ///< States if we should \r\n as our line termination
+  char          lineterminator;             ///< define the line terminator character. Default is  '\n'
+  char          delimiter;                  ///< define the field separator, default is ',' This argument is also called 'sep' 
   bool          delim_whitespace;           //< Use white space as the delimiter - default is false. This overrides the delimiter argument
-  bool          skipinitialspace;           //< Skip white spaces after the delimiter - default is false
+  bool          skipinitialspace;           ///< Skip white spaces after the delimiter - default is false
 
-  gdf_size_type nrows;                      //< Number of rows to read, -1 indicates all
-  gdf_size_type header;                     //< Row of the header data, zero based counting. Default states that header should not be read from file.
+  gdf_size_type nrows;                      ///< Number of rows to read, -1 indicates all
+  gdf_size_type header;                     ///< Row of the header data, zero based counting. Default states that header should not be read from file.
 
-  int           num_cols;                   //< Number of columns in the names and dtype arrays
-  const char    **names;                    //< Ordered List of column names
-  const char    **dtype;                    //< Ordered List of data types
+  int           num_cols;                   ///< Number of columns in the names and dtype arrays
+  const char    **names;                    ///< Ordered List of column names
+  const char    **dtype;                    ///< Ordered List of data types
 
-  int           *index_col;                 //< Indexes of columns to use as the row labels of the DataFrame.
-  int           *use_cols_int;              //< Indexes of columns to be returned. CSV reader will only process those columns, another read is needed to get full data
-  int           use_cols_int_len;           //< Number of elements in use_cols_int
-  const char    **use_cols_char;            //< Names of columns to be returned. CSV reader will only process those columns, another read is needed to get full data
-  int           use_cols_char_len;          //< Number of elements in use_cols_char_len
+  int           *index_col;                 ///< Indexes of columns to use as the row labels of the DataFrame.
+  int           *use_cols_int;              ///< Indexes of columns to be returned. CSV reader will only process those columns, another read is needed to get full data
+  int           use_cols_int_len;           ///< Number of elements in use_cols_int
+  const char    **use_cols_char;            ///< Names of columns to be returned. CSV reader will only process those columns, another read is needed to get full data
+  int           use_cols_char_len;          ///< Number of elements in use_cols_char_len
 
-  gdf_size_type skiprows;                   //< Number of rows at the start of the files to skip, default is 0
-  gdf_size_type skipfooter;                 //< Number of rows at the bottom of the file to skip - default is 0
+  gdf_size_type skiprows;                   ///< Number of rows at the start of the files to skip, default is 0
+  gdf_size_type skipfooter;                 ///< Number of rows at the bottom of the file to skip - default is 0
 
   bool          skip_blank_lines;           // Whether or not to ignore blank lines
 
-  const char    **true_values;              //< List of values to recognize as boolean True
-  int           num_true_values;            //< Number of values in the true_values list
-  const char    **false_values;             //< List of values to recognize as boolean False
-  int           num_false_values;           //< Number of values in the true_values list
+  const char    **true_values;              ///< List of values to recognize as boolean True
+  int           num_true_values;            ///< Number of values in the true_values list
+  const char    **false_values;             ///< List of values to recognize as boolean False
+  int           num_false_values;           ///< Number of values in the true_values list
 
   const char    **na_values;                // Array of strings that should be considered as NA
 
   char          *prefix;                    // If there is no header or names, append this to the column ID as the name
-  bool          mangle_dupe_cols;           //< If true, duplicate columns get a suffix. If false, data will be overwritten if there are columns with duplicate names
+  bool          mangle_dupe_cols;           ///< If true, duplicate columns get a suffix. If false, data will be overwritten if there are columns with duplicate names
 
   bool          parse_dates;                // Parse date field into date32 or date64.  If false then date fields are saved as a string. Specifying a date dtype overrides this
   bool          infer_datetime_format;      // Try and determine the date format
-  bool          dayfirst;                   //< Is the first value in the date formatthe day?  DD/MM  versus MM/DD
+  bool          dayfirst;                   ///< Is the first value in the date formatthe day?  DD/MM  versus MM/DD
 
-  char          *compression;               //< Specify the type of compression (nullptr,"none","infer","gzip","zip"), "infer" infers the compression from the file extension, default(nullptr) is uncompressed
-  char          thousands;                  //< Single character that separates thousands in numeric data. If this matches the delimiter then system will return GDF_INVALID_API_CALL
+  char          *compression;               ///< Specify the type of compression (nullptr,"none","infer","gzip","zip"), "infer" infers the compression from the file extension, default(nullptr) is uncompressed
+  char          thousands;                  ///< Single character that separates thousands in numeric data. If this matches the delimiter then system will return GDF_INVALID_API_CALL
 
-  char          decimal;                    //< The decimal point character. If this matches the delimiter then system will return GDF_INVALID_API_CALL
+  char          decimal;                    ///< The decimal point character. If this matches the delimiter then system will return GDF_INVALID_API_CALL
 
-  char          quotechar;                  //< Define the character used to denote start and end of a quoted item
-  bool          quoting;                    //< Treat string fields as quoted item and remove the first and last quotechar
-  bool          doublequote;                //< Indicates whether to interpret two consecutive quotechar inside a field as a single quotechar
+  char          quotechar;                  ///< Define the character used to denote start and end of a quoted item
+  bool          quoting;                    ///< Treat string fields as quoted item and remove the first and last quotechar
+  bool          doublequote;                ///< Indicates whether to interpret two consecutive quotechar inside a field as a single quotechar
 
   char          escapechar;                 // Single character used as the escape character
 
