112827cfb1652a6cadaf5a6d6c9fd4290b0fa8ed
Mark Harris <mharris@nvidia.com>
2019-02-02 22:44:58 -0800

    Added test fixture for testing RMM allocation modes

--- cpp/tests/utilities/cudf_test_fixtures.h
+++ cpp/tests/utilities/cudf_test_fixtures.h
@@ -15,3 +15,31 @@ struct GdfTest : public ::testing::Test
         ASSERT_EQ( RMM_SUCCESS, rmmFinalize() );
     }
 };
+
+template <typename T>
+struct cudfTest : public ::testing::Test
+{
+    static rmmAllocationMode_t allocationMode() { return T::alloc_mode; }
+
+    static void SetUpTestCase() {
+        rmmOptions_t options {allocationMode(), 0, false};
+        ASSERT_EQ( RMM_SUCCESS, rmmInitialize(&options) );
+    }
+
+    static void TearDownTestCase() {
+        ASSERT_EQ( RMM_SUCCESS, rmmFinalize() );
+    }
+};
+
+template <rmmAllocationMode_t mode>
+struct ModeType {
+    static constexpr rmmAllocationMode_t alloc_mode{mode};
+};
+
+using allocation_modes =
+    ::testing::Types< ModeType<CudaDefaultAllocation>,
+                      ModeType<PoolAllocation>,
+                      ModeType<CudaManagedMemory>,
+                      ModeType<static_cast<rmmAllocationMode_t>(
+                          PoolAllocation | CudaManagedMemory)>
+                    >;
