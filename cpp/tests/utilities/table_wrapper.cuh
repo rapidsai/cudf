86890ec6b876f0b6f5ee4c9e4b78350963278a77
Jake Hemstad <jhemstad@nvidia.com>
2019-02-08 12:50:20 -0800

    Updated table_wrapper with a for_each over tuple elements.

--- cpp/tests/utilities/table_wrapper.cuh
+++ cpp/tests/utilities/table_wrapper.cuh
@@ -27,47 +27,32 @@
 
 namespace cudf {
 namespace test {
-
 namespace detail {
-/**---------------------------------------------------------------------------*
- * @brief Invokes a callable with a specified index sequence.
- *
- * This is a helper function for `index_apply` that will invoke the callable `f`
- * with the expansion of the specified `index_sequence` as
- * `std::integral_constant`s.
- *
- * @tparam F The callable's type
- * @tparam Is The index_sequence
- * @param f The callable
- * @return constexpr auto Returns whatever is returned by the callable
- *---------------------------------------------------------------------------**/
-template <class F, size_t... Is>
-constexpr auto index_apply_impl(F f, std::index_sequence<Is...>) {
-  return f(std::integral_constant<size_t, Is>{}...);
+template <typename Tuple, typename F, std::size_t... Indices>
+void for_each_impl(Tuple&& tuple, F&& f, std::index_sequence<Indices...>) {
+  using swallow = int[];
+  (void)swallow{
+      1, (f(std::get<Indices>(std::forward<Tuple>(tuple))), void(), int{})...};
 }
 
 /**---------------------------------------------------------------------------*
- * @brief Invokes a callable with arguments consisting of an index sequence of a
- * specified sized.
+ * @brief A `for_each` over the elements of a tuple.
  *
- * Given a callable `f`, and a size `N`, this function will invoke
- * `f(0, 1, ..., N-1)` where the index_sequence are integral_constant values.
+ * For every element in a tuple, invokes a callable and passes the tuple element
+ * into the callable.
  *
- * @tparam N The size of the index sequence
- * @tparam F The callable's type
- * @param f The callable
+ * @tparam Tuple The type of the tuple
+ * @tparam F The type of the callable
+ * @param tuple The tuple to iterate over
+ * @param f The unary callable
  *---------------------------------------------------------------------------**/
-template <size_t N, class F>
-constexpr auto index_apply(F f) {
-  return index_apply_impl(f, std::make_index_sequence<N>{});
+template <typename Tuple, typename F>
+void for_each(Tuple&& tuple, F&& f) {
+  constexpr std::size_t N =
+      std::tuple_size<std::remove_reference_t<Tuple>>::value;
+  for_each_impl(std::forward<Tuple>(tuple), std::forward<F>(f),
+                std::make_index_sequence<N>{});
 }
-
-template <class Tuple, class F>
-constexpr auto apply(Tuple const &t, F f) {
-  return index_apply<std::tuple_size<Tuple>::value>(
-      [&](auto... Is) { return f(std::get<Is>(t)...); });
-}
-
 }  // namespace detail
 
 template <typename>
@@ -78,59 +63,22 @@ struct table_wrapper<std::tuple<Ts...>> {
   table_wrapper(gdf_size_type num_rows)
       : column_wrappers{std::unique_ptr<column_wrapper<Ts>>(
             new column_wrapper<Ts>(num_rows))...}
-
   {
-    initialize_columns();
-  }
-
-  gdf_column **get() { return gdf_columns.data(); }
-
- private:
-
-  void init_columns(std::unique_ptr<column_wrapper<Ts>> &... cols) {
-    (void)std::initializer_list<int>{
-        ((void)gdf_columns.push_back(cols->get()), 0)...};
-  }
-
-  void initialize_columns() {
-    return detail::index_apply<sizeof...(Ts)>([this](auto... Is) {
-      return this->init_columns(std::get<Is>(column_wrappers)...);
+    // Iterate the tuple of column_wrappers and store each underlying
+    // `gdf_column` in a vector
+    detail::for_each(column_wrappers, [this](auto const& col) {
+      gdf_columns.push_back(col->get());
     });
   }
 
-  /*
-    template <typename ValueGenerator, typename BitmaskGenerator,
-              std::size_t... Is>
-    void initialize_columns_impl(gdf_size_type num_rows, ValueGenerator v,
-                                 BitmaskGenerator b, std::index_sequence<Is...>)
-    {
-      // This is some nasty hackery, but essentially we're just iterating
-      // over the tuple of column wrappers, initializing each column_wrapper,
-    and
-      // pushing a pointer to the underlying `gdf_column` onto the `gdf_columns`
-      // vector
-      // For more details on the hackery, see:
-      // https://codereview.stackexchange.com/a/67394
-      using dummy = int[];
-      (void)dummy{
-          1, (initialize_column(std::get<Is>(column_wrappers), num_rows, v, b),
-              void(), int{})...};
-    }
+  gdf_column** get() { return gdf_columns.data(); }
 
-    template <typename T, typename ValueGenerator, typename BitmaskGenerator>
-    void initialize_column(std::unique_ptr<column_wrapper<T>> &column,
-                           gdf_size_type num_rows, ValueGenerator v,
-                           BitmaskGenerator b) {
-      column.reset(new column_wrapper<T>(num_rows, v, b));
-      gdf_columns.push_back(column->get());
-    }
-    */
-
+ private:
   gdf_size_type const num_columns{
       sizeof...(Ts)};  ///< number of columns in the table
   std::tuple<std::unique_ptr<column_wrapper<Ts>>...>
       column_wrappers;  ///< Collection of column_wrapper s of different types
-  std::vector<gdf_column *>
+  std::vector<gdf_column*>
       gdf_columns;  ///< Pointers to each column_wrapper's underlying gdf_column
 };
 }  // namespace test
