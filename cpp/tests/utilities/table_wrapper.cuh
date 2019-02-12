9e2821221a66b7edaaadf293b3431ea763b9a1b7
Jake Hemstad <jhemstad@nvidia.com>
2019-02-12 11:02:14 -0800

    For_each over tuple elements and documentation.

--- cpp/tests/utilities/table_wrapper.cuh
+++ cpp/tests/utilities/table_wrapper.cuh
@@ -27,50 +27,126 @@
 
 namespace cudf {
 namespace test {
+namespace detail {
+template <typename Tuple, typename F, std::size_t... Indices>
+void for_each_impl(Tuple&& tuple, F&& f, std::index_sequence<Indices...>) {
+  using swallow = int[];
+  (void)swallow{
+      1, (f(std::get<Indices>(std::forward<Tuple>(tuple))), void(), int{})...};
+}
+
+/**---------------------------------------------------------------------------*
+ * @brief A `for_each` over the elements of a tuple.
+ *
+ * For every element in a tuple, invokes a unary callable and passes the tuple
+ *element into the callable.
+ *
+ * @tparam Tuple The type of the tuple
+ * @tparam F The type of the callable
+ * @param tuple The tuple to iterate over
+ * @param f The unary callable
+ *---------------------------------------------------------------------------**/
+template <typename Tuple, typename F>
+void for_each(Tuple&& tuple, F&& f) {
+  constexpr std::size_t N =
+      std::tuple_size<std::remove_reference_t<Tuple>>::value;
+  for_each_impl(std::forward<Tuple>(tuple), std::forward<F>(f),
+                std::make_index_sequence<N>{});
+}
+}  // namespace detail
 
 template <typename>
 struct table_wrapper;
 
+/**---------------------------------------------------------------------------*
+ * @brief A wrapper for constructing a cudf::table with columns of arbitrary
+ * types.
+ *
+ * Accepts a std::tuple of types as a template parameter. The type of element `i`
+ * in the tuple determines the type of column `i` in the cudf::table.
+ * 
+ * For example,
+ * 
+ * `table_wrapper< std::tuple< int, float, double > >` will construct a cudf::table
+ * with 3 columns of types GDF_INT32, GDF_FLOAT32, and GDF_FLOAT64, respectively.
+ *
+ * @tparam Ts The pack of types to use to create the set of columns
+ *---------------------------------------------------------------------------**/
 template <typename... Ts>
 struct table_wrapper<std::tuple<Ts...>> {
-  table_wrapper(gdf_size_type num_rows) {}
+  /**---------------------------------------------------------------------------*
+   * @brief Construct a new table wrapper object
+   *
+   * Constructor that zero-intiailizes each column and it's bitmasks.
+   *
+   * @param[in] num_rows The number of rows in each column
+   *---------------------------------------------------------------------------**/
+  table_wrapper(gdf_size_type num_rows)
+      : column_wrappers{std::unique_ptr<column_wrapper<Ts>>(
+            new column_wrapper<Ts>(num_rows))...} {
+    initialize_structures();
+  }
 
-  gdf_column **get() { return gdf_columns.data(); }
+  /**---------------------------------------------------------------------------*
+   * @brief Construct a new table wrapper object
+   *
+   * Uses a unary callable to initialize each row's value and validity bitmask.
+   *
+   * Each column's value at row `i` will be equal to `v(i)` and bit will be
+   *equal to `b(i)`.
+   *
+   * @tparam ValueIntializerType The type of the callable to initialize column
+   *values
+   * @tparam BitInitializeType  The type of the callable to initialize column
+   *bitmasks
+   * @param num_rows The number of rows in each column
+   * @param v The value intializer callable
+   * @param b The bitmask initializer callable
+   *---------------------------------------------------------------------------**/
+  template <typename ValueIntializerType, typename BitInitializeType>
+  table_wrapper(gdf_size_type num_rows, ValueIntializerType v,
+                BitInitializeType b)
+      : column_wrappers{std::unique_ptr<column_wrapper<Ts>>(
+            new column_wrapper<Ts>(num_rows, v, b))...} {
+    initialize_structures();
+  }
+
+  /**---------------------------------------------------------------------------*
+   * @brief Return pointer to array of underlying `gdf_column`s
+   *
+   *---------------------------------------------------------------------------**/
+  gdf_column** get_columns() { return gdf_columns.data(); }
+  gdf_column const* const* get_columns() const { return gdf_columns.data(); }
+
+  /**---------------------------------------------------------------------------*
+   * @brief Returns pointer to underlying cudf::table
+   *
+   *---------------------------------------------------------------------------**/
+  cudf::table* get() { return the_table.get(); }
+  cudf::table const* get() const { return the_table.get(); }
 
  private:
-  template <typename ValueGenerator, typename BitmaskGenerator>
-  void initialize_columns(gdf_size_type num_rows, ValueGenerator v,
-                          BitmaskGenerator b) {
-    initialize_columns_impl(num_rows, v, b, std::index_sequence_for<Ts...>{});
+  /**---------------------------------------------------------------------------*
+   * @brief Initializes the underlying gdf_column's and cudf::table
+   * with the corresponding data from the tuple of column_wrappers.
+   *
+   *---------------------------------------------------------------------------**/
+  void initialize_structures() {
+    // Iterate the tuple of column_wrappers and store each underlying
+    // `gdf_column` in a vector
+    detail::for_each(column_wrappers, [this](auto const& col) {
+      gdf_columns.push_back(col->get());
+    });
+    the_table.reset(new cudf::table(gdf_columns.data(), gdf_columns.size()));
   }
 
-  template <typename ValueGenerator, typename BitmaskGenerator,
-            std::size_t... Is>
-  void initialize_columns_impl(gdf_size_type num_rows, ValueGenerator v,
-                               BitmaskGenerator b, std::index_sequence<Is...>) {
-    // This is some nasty hackery, but essentially we're just iterating
-    // over the tuple of column wrappers, initializing each column_wrapper, and
-    // pushing a pointer to the underlying `gdf_column` onto the `gdf_columns`
-    // vector
-    // For more details on the hackery, see:
-    // https://codereview.stackexchange.com/a/67394
-    using dummy = int[];
-    (void)dummy{
-        1, (initialize_column(std::get<Is>(column_wrappers), num_rows, v, b),
-            void(), int{})...};
-  }
-
-  template <typename T, typename ValueGenerator, typename BitmaskGenerator>
-  void initialize_column(std::unique_ptr<column_wrapper<T>> &column,
-                         gdf_size_type num_rows, ValueGenerator v,
-                         BitmaskGenerator b) {
-    column.reset(new column_wrapper<T>(num_rows, v, b));
-    gdf_columns.push_back(column->get());
-  }
-
-  gdf_size_type const num_columns{sizeof...(Ts)};
-  std::tuple<std::unique_ptr<column_wrapper<Ts>>...> column_wrappers;
-  std::vector<gdf_column *> gdf_columns;
+  gdf_size_type const num_columns{
+      sizeof...(Ts)};  ///< number of columns in the table
+  std::tuple<std::unique_ptr<column_wrapper<Ts>>...>
+      column_wrappers;  ///< Collection of column_wrapper s of different types
+  std::vector<gdf_column*>
+      gdf_columns;  ///< Pointers to each column_wrapper's underlying gdf_column
+  std::unique_ptr<cudf::table> the_table;  ///< The wrapped cudf::table
 };
 }  // namespace test
 }  // namespace cudf
