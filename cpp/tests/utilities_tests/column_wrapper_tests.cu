578fbb69e6444c18155251f7c55da449e8a1668a
Jake Hemstad <jhemstad@nvidia.com>
2019-02-12 16:17:14 -0800

    Added first test of column_wrapper.

--- cpp/tests/utilities_tests/column_wrapper_tests.cu
+++ cpp/tests/utilities_tests/column_wrapper_tests.cu
@@ -14,19 +14,44 @@
  * limitations under the License.
  */
 
-
 #include "tests/utilities/column_wrapper.cuh"
 #include "tests/utilities/cudf_test_fixtures.h"
+#include "utilities/type_dispatcher.hpp"
+#include "utilities/wrapper_types.hpp"
 
-#include "gtest/gtest.h"
+#include <cstdint>
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
 template <typename T>
-struct ColumnWrapperTest : public GdfTest
-{
+struct ColumnWrapperTest : public GdfTest {};
 
-};
-
-using TestingTypes = ::testing::Types<int8_t, int16_t, int32_t, int64_t, float, double, cudf::date32, cudf::date64, cudf::timestamp>;
+using TestingTypes = ::testing::Types<int8_t, int16_t, int32_t, int64_t, float,
+                                      double, cudf::date32, cudf::date64,
+                                      cudf::timestamp, cudf::category>;
 
 TYPED_TEST_CASE(ColumnWrapperTest, TestingTypes);
+
+TYPED_TEST(ColumnWrapperTest, SizeConstructor) {
+  gdf_size_type const size{1000};
+  cudf::test::column_wrapper<TypeParam> const col(size);
+
+  gdf_column const* underlying_column = col.get();
+  ASSERT_NE(nullptr, underlying_column);
+  EXPECT_NE(nullptr, underlying_column->data);
+  EXPECT_EQ(nullptr, underlying_column->valid);
+  EXPECT_EQ(size, underlying_column->size);
+  gdf_dtype expected = cudf::type_to_gdf_dtype<TypeParam>::value;
+  EXPECT_EQ(expected, underlying_column->dtype);
+
+  std::vector<TypeParam> col_data;
+  std::vector<gdf_valid_type> col_bitmask;
+  std::tie(col_data, col_bitmask) = col.to_host();
+  EXPECT_EQ(static_cast<size_t>(size), col_data.size());
+  EXPECT_EQ(0u, col_bitmask.size());
+
+  bool const all_default_values{
+      std::all_of(col_data.begin(), col_data.end(),
+                  [](TypeParam element) { return (TypeParam{} == element); })};
+  EXPECT_TRUE(all_default_values);
+}
