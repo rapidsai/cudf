db6765314c63d0227d948114eadab367a04005f4
Matthew Rocklin <mrocklin@gmail.com>
2019-02-20 09:31:38 -0800

    flake8

--- python/cudf/comm/serialize.py
+++ python/cudf/comm/serialize.py
@@ -1,12 +1,8 @@
-import os
-import sys
-from types import MethodType
-
 # A flag to allow dask_gdf to detect and warn if
 # IPC serialization is unavailable
 CUSTOM_SERIALIZATION_AVAILABLE = False
 
+
 def register_distributed_serializer(cls):
-    """Dummy no-op function.
-    """
+    """ Dummy no-op function """
     pass
