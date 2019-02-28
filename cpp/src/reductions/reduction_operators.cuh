20fb84434a107d5bcbfb8be42a4847f35f25e32a
takobayashi <takobayashi@nvidia.com>
2019-02-28 20:46:11 +0900

    Update operator

--- cpp/src/reductions/reduction_operators.cuh
+++ cpp/src/reductions/reduction_operators.cuh
@@ -5,6 +5,102 @@
 namespace cudf {
 namespace reduction {
 
+
+	template <typename T, typename T_equivalent, typename Op>
+    __forceinline__  __device__
+    void update_existing_value(T_equivalent & existing_value, value_type const & insert_pair, Op op)
+    {
+      const mapped_type insert_value = insert_pair.second;
+
+      mapped_type old_value = existing_value;
+
+      mapped_type expected{old_value};
+
+      // Attempt to perform the aggregation with existing_value and
+      // store the result atomically
+      do 
+      {
+        expected = old_value;
+
+        const mapped_type new_value = op(insert_value, old_value);
+
+        old_value = atomicCAS(&existing_value, expected, new_value);
+      }
+      // Guard against another thread's update to existing_value
+      while( expected != old_value );
+    }
+
+    // TODO Overload atomicAdd for 1 byte and 2 byte types, until then, overload specifically for the types
+    // where atomicAdd already has an overload. Otherwise the generic update_existing_value will be used.
+    // Specialization for COUNT aggregator
+    __forceinline__ __host__ __device__
+    void update_existing_value(mapped_type & existing_value, value_type const & insert_pair, count_op<int32_t> op)
+    {
+      atomicAdd(&existing_value, static_cast<mapped_type>(1));
+    }
+    // Specialization for COUNT aggregator
+    __forceinline__ __host__ __device__
+    void update_existing_value(mapped_type & existing_value, value_type const & insert_pair, count_op<int64_t> op)
+    {
+      atomicAdd(&existing_value, static_cast<mapped_type>(1));
+    }
+    // Specialization for COUNT aggregator
+    __forceinline__ __host__ __device__
+    void update_existing_value(mapped_type & existing_value, value_type const & insert_pair, count_op<float> op)
+    {
+      atomicAdd(&existing_value, static_cast<mapped_type>(1));
+    }
+    // Specialization for COUNT aggregator
+    __forceinline__ __host__ __device__
+    void update_existing_value(mapped_type & existing_value, value_type const & insert_pair, count_op<double> op)
+    {
+      atomicAdd(&existing_value, static_cast<mapped_type>(1));
+    }
+
+    // Specialization for SUM aggregator (int32)
+    __forceinline__  __device__
+    void update_existing_value(mapped_type & existing_value, value_type const & insert_pair, sum_op<int32_t> op)
+    {
+      atomicAdd(&existing_value, insert_pair.second);
+    }
+
+    // Specialization for SUM aggregator (int64)
+    __forceinline__  __device__
+    void update_existing_value(mapped_type & existing_value, value_type const & insert_pair, sum_op<int64_t> op)
+    {
+      atomicAdd(&existing_value, insert_pair.second);
+    }
+
+    // Specialization for SUM aggregator (fp32)
+    __forceinline__  __device__
+    void update_existing_value(mapped_type & existing_value, value_type const & insert_pair, sum_op<float> op)
+    {
+      atomicAdd(&existing_value, insert_pair.second);
+    }
+
+    // Specialization for SUM aggregator (fp64)
+    __forceinline__  __device__
+    void update_existing_value(mapped_type & existing_value, value_type const & insert_pair, sum_op<double> op)
+    {
+      atomicAdd(&existing_value, insert_pair.second);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct IdentityLoader {
     template<typename T>
     __device__
