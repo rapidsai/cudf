e8120d3ce00468be0b86d930c5c1c21d8bd75543
Benjamin Zaitlen <quasiben@gmail.com>
2019-03-02 08:18:56 -0800

    flush out dataset generation and add tests

--- python/cudf/tests/test_datasets.py
+++ python/cudf/tests/test_datasets.py
@@ -1,23 +1,35 @@
 import pytest
 import pandas as pd
 
-import cudf
+import cudf as gd
 from cudf.tests.utils import assert_eq
 
 def test_dataset_timeseries():
-    a = cudf.datasets.timeseries(dtypes={"x": int, "y": float}, freq="120s", seed=1)
-    b = cudf.datasets.timeseries(dtypes={"x": int, "y": float}, freq="120s", seed=1)
+    gdf1 = gd.datasets.timeseries(dtypes={"x": int, "y": float}, freq="120s", seed=1)
+    gdf2 = gd.datasets.timeseries(dtypes={"x": int, "y": float}, freq="120s", seed=1)
 
-    assert_eq(a, b)
+    assert_eq(gdf1, gdf2)
 
-    assert a['x'].head().dtype == int
-    assert a['y'].head().dtype == float
-    assert a.index.name == 'timestamp'
+    assert gdf1['x'].head().dtype == int
+    assert gdf1['y'].head().dtype == float
+    assert gdf1.index.name == 'timestamp'
 
-    cdf = cudf.datasets.make_timeseries('2000', '2010',
-                                       {'value': float, 'name': 'category', 'id': int},
-                                       freq='2H', partition_freq='1D', seed=1)
+    gdf = gd.datasets.timeseries('2000', '2010', freq='2H',
+                                       dtypes={'value': float, 'name': 'category', 'id': int},
+                                       seed=1)
 
-    assert cdf['value'].head().dtype == float
-    assert cdf['id'].head().dtype == int
-    assert cdf['name'].head().dtype == pd.api.types.CategoricalDtype()
+    assert gdf['value'].head().dtype == float
+    assert gdf['id'].head().dtype == int
+    assert gdf['name'].head().dtype == pd.api.types.CategoricalDtype()
+
+    gdf = gd.datasets.randomdata()
+    assert gdf['id'].head().dtype == int
+    assert gdf['x'].head().dtype == float
+    assert gdf['y'].head().dtype == float
+    assert len(gdf) == 10
+
+    gdf = gd.datasets.randomdata(nrows=20, dtypes={'id': int, 'a': int, 'b': float},)
+    assert gdf['id'].head().dtype == int
+    assert gdf['a'].head().dtype == int
+    assert gdf['b'].head().dtype == float
+    assert len(gdf) == 20
