8aad3c6664ee3b037c0ebdd0a54da6d2e328f7ba
takobayashi <takobayashi@nvidia.com>
2019-03-04 22:12:17 +0900

    Simplify the code
    Rename: ReduceOp::launch() -> Reduce()
    Remove: ReduceOp::launch_onece()

--- cpp/src/reductions/reductions.cu
+++ cpp/src/reductions/reductions.cu
@@ -59,57 +59,39 @@ void gpu_reduction_op(const T *data, const gdf_valid_type *mask,
 
 
 
-template<typename T, typename F>
-struct ReduceOp {
-    static
-    gdf_error launch(gdf_column *input, T identity, T *output,
-                     gdf_size_type output_size) {
-        // 1st round
-        //    Partially reduce the input into *output_size* length.
-        //    Each block computes one output in *output*.
-        //    output_size == gridsize
-        typedef typename F::Loader Ld1;
-        F functor1;
-        Ld1 loader1;
-        launch_once((const T*)input->data, input->valid, input->size,
-                    (T*)output, output_size, identity, functor1, loader1);
-        CUDA_CHECK_LAST();
+template<typename T, typename Op>
+gdf_error ReduceOp(gdf_column *input, T identity, T *output,
+                     gdf_size_type output_size)
+{
+    // fake single step (single grid), the perf is bad.
+    // Todo: using atomics (multi grid)
+    output_size = 1;
 
-        return GDF_SUCCESS;
-    }
+    // initialize by identity value
+    CUDA_TRY(cudaMemcpyAsync(output, &identity,
+            sizeof(T), cudaMemcpyHostToDevice, 0));
+    CUDA_CHECK_LAST();
 
-    template <typename Functor, typename Loader>
-    static
-    void launch_once(const T *data, gdf_valid_type *valid, gdf_size_type size,
-                     T *output, gdf_size_type output_size, T identity,
-                     Functor functor, Loader loader) {
+    // find needed gridsize
+    // use atmost REDUCTION_BLOCK_SIZE blocks
+    int blocksize = REDUCTION_BLOCK_SIZE;
+    int gridsize = (output_size < REDUCTION_BLOCK_SIZE?
+                    output_size : REDUCTION_BLOCK_SIZE);
 
-        // fake single step (single grid), the perf is bad.
-        // Todo: using atomics (multi grid)
-        output_size = 1;
+    typename Op::Loader loader;
+    Op functor;
 
-        // find needed gridsize
-        // use atmost REDUCTION_BLOCK_SIZE blocks
-        int blocksize = REDUCTION_BLOCK_SIZE;
-        int gridsize = (output_size < REDUCTION_BLOCK_SIZE?
-                        output_size : REDUCTION_BLOCK_SIZE);
+    // launch kernel
+    gpu_reduction_op<<<gridsize, blocksize>>>(
+        (const T*)input->data, input->valid, input->size, (T*)output,
+        functor, identity, loader
+    );
 
-        // launch kernel
-        gpu_reduction_op<<<gridsize, blocksize>>>(
-            // inputs
-            data, valid, size,
-            // output
-            output,
-            // action
-            functor,
-            // identity
-            identity,
-            // loader
-            loader
-        );
-    }
+    CUDA_CHECK_LAST();
 
+    return GDF_SUCCESS;
 };
+
 
 template <template <typename Ti> class Op>
 struct ReduceDispatcher {
@@ -121,7 +103,7 @@ struct ReduceDispatcher {
     {
         GDF_REQUIRE(col->size > col->null_count, GDF_DATASET_EMPTY);
         T identity = Op<T>::identity();
-        return ReduceOp<T, Op<T>>::launch(col, identity,
+        return ReduceOp<T, Op<T>>(col, identity,
                                        static_cast<T*>(dev_result),
                                        dev_result_size);
     }
