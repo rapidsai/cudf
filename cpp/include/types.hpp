228d06a6f3f57c8f15db5f277e2ed5a7733a6af3
Jake Hemstad <jhemstad@nvidia.com>
2019-03-05 16:42:25 -0800

    Added unit tests for cudf::table.

--- cpp/include/types.hpp
+++ cpp/include/types.hpp
@@ -27,34 +27,61 @@ namespace cudf {
  *
  */
 struct table {
+  /**---------------------------------------------------------------------------*
+   * @brief Constructs a table object from an array of `gdf_column`s
+   *
+   * @param cols The array of columns wrapped by the table
+   * @param num_cols  The number of columns in the array
+   *---------------------------------------------------------------------------**/
   table(gdf_column* cols[], gdf_size_type num_cols)
-      : columns{cols}, num_columns{num_cols} {
+      : columns{cols}, _num_columns{num_cols} {
     assert(nullptr != cols[0]);
 
     gdf_size_type const num_rows{cols[0]->size};
 
-    std::for_each(columns, columns + num_columns, [num_rows](gdf_column* col) {
+    std::for_each(columns, columns + _num_columns, [num_rows](gdf_column* col) {
       assert(nullptr != col);
       assert(num_rows == col->size);
     });
   }
 
+  /**---------------------------------------------------------------------------*
+   * @brief Returns pointer to the first `gdf_column` in the table.
+   *
+   *---------------------------------------------------------------------------**/
   gdf_column const* const* begin() const { return columns; }
   gdf_column** begin() { return columns; }
 
-  gdf_column const* const* end() const { return columns + num_columns; }
-  gdf_column** end() const { return columns + num_columns; }
+  /**---------------------------------------------------------------------------*
+   * @brief Returns pointer to one past the last `gdf_column` in the table
+   *
+   *---------------------------------------------------------------------------**/
+  gdf_column const* const* end() const { return columns + _num_columns; }
+  gdf_column** end() { return columns + _num_columns; }
 
-  gdf_column* get_column(gdf_index_type index) { return columns[index]; }
-  gdf_column const* get_column const(gdf_index_type index) {
+  /**---------------------------------------------------------------------------*
+   * @brief Returns pointer to the column specified by an index.
+   *
+   * @param index The index of the desired column
+   * @return gdf_column* Pointer to the column at `index`
+   *---------------------------------------------------------------------------**/
+  gdf_column* get_column(gdf_index_type index) {
+    assert(index < _num_columns);
+    return columns[index];
+  }
+  gdf_column const* get_column(gdf_index_type index) const {
     return columns[index];
   }
 
-  gdf_size_type num_columns() const { return num_columns; }
+  /**---------------------------------------------------------------------------*
+   * @brief Returns the number of columns in the table
+   *
+   *---------------------------------------------------------------------------**/
+  gdf_size_type num_columns() const { return _num_columns; }
 
  private:
   gdf_column** columns;            /**< The set of gdf_columns*/
-  gdf_size_type const num_columns; /**< The number of columns in the set */
+  gdf_size_type const _num_columns; /**< The number of columns in the set */
 };
 
 }  // namespace cudf
