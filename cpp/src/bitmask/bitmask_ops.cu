37cf962da0942a568679ddd24a87b18d7a27e5b4
Jake Hemstad <jhemstad@nvidia.com>
2019-03-11 19:38:33 -0700

    Renamed all references to num_chars_bitmask.

--- cpp/src/bitmask/bitmask_ops.cu
+++ cpp/src/bitmask/bitmask_ops.cu
@@ -16,12 +16,12 @@
 
 
 gdf_error all_bitmask_on(gdf_valid_type * valid_out, gdf_size_type & out_null_count, gdf_size_type num_values, cudaStream_t stream){
-	gdf_size_type num_chars_bitmask = gdf_num_bitmask_elements( num_values );
+	gdf_size_type num_bitmask_elements = gdf_num_bitmask_elements( num_values );
 
 	gdf_valid_type max_char = 255;
 	thrust::fill(rmm::exec_policy(stream)->on(stream),
 				 valid_out,
-				 valid_out + num_chars_bitmask,
+				 valid_out + num_bitmask_elements,
 				 max_char);
 	//we have no nulls so set all the bits in gdf_valid_type to 1
 	out_null_count = 0;
@@ -31,11 +31,11 @@ gdf_error all_bitmask_on(gdf_valid_type * valid_out, gdf_size_type & out_null_co
 gdf_error apply_bitmask_to_bitmask(gdf_size_type & out_null_count, gdf_valid_type * valid_out, gdf_valid_type * valid_left, gdf_valid_type * valid_right,
 		cudaStream_t stream, gdf_size_type num_values){
 
-	gdf_size_type num_chars_bitmask = gdf_num_bitmask_elements( num_values );
+	gdf_size_type num_bitmask_elements = gdf_num_bitmask_elements( num_values );
 
 	thrust::transform(rmm::exec_policy(stream)->on(stream),
 					  valid_left,
-					  valid_left + num_chars_bitmask,
+					  valid_left + num_bitmask_elements,
 					  valid_right,
 					  valid_out,
 					  thrust::bit_and<gdf_valid_type>());
