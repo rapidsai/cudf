bb16060fabf41932a15211a50ffd3ae1c8af2e2e
Jake Hemstad <jhemstad@nvidia.com>
2019-03-11 18:26:33 -0700

    Documentation.

--- cpp/src/bitmask/legacy_bitmask.hpp
+++ cpp/src/bitmask/legacy_bitmask.hpp
@@ -20,6 +20,15 @@
 #include <cudf.h>
 #include <utilities/cudf_utils.h>
 
+/**---------------------------------------------------------------------------*
+ * @brief Returns if the specified bit in a validity bit mask is set.
+ *
+ * @param valid The validity bitmask. If equal to `nullptr`, this function
+ * always returns true
+ * @param pos The specified bit
+ * @return true If the bit is set (equal to 1), or if @p valid is nullptr
+ * @return false If the bit is not set (equal to 0)
+ *---------------------------------------------------------------------------**/
 CUDA_HOST_DEVICE_CALLABLE
 bool gdf_is_valid(const gdf_valid_type *valid, gdf_index_type pos) {
   if (valid)
@@ -29,17 +38,19 @@ bool gdf_is_valid(const gdf_valid_type *valid, gdf_index_type pos) {
 }
 
 /**
- * Calculates the index of the last `gdf_valid_type` element in the validity
- * bitmask for a given column's size.
+ * @brief Computes the number of `gdf_valid_type` elements required to provide
+ * enough bits to represent the specified number of column elements.
  *
- * @note Note that this function assumes that `gdf_valid_type` is unsigned char
- * @note This function is different gdf_get_num_bytes_for_valids_allocation
- * because it refers to the last `gdf_valid_type` element that refers to
- * elements in the column, NOT the last element in the allocation
+ * @note Note that this function assumes that the size of `gdf_valid_type` is 1
+ * byte
+ * @note This function is different gdf_valid_allocation_size
+ * because gdf_valid_allocation_size returns the number of bytes required to
+ * satisfy 64B padding. This function should be used when needing to access the
+ * last `gdf_valid_type` element in the validity bitmask.
  *
  * @param[in] column_size the number of elements
- * @return The index of the last `gdf_valid_type` element that refers to
- * elements in a column of size @p column_size
+ * @return The minimum number of `gdf_valid_type` elements to provide sufficient
+ * bits to represent elements in a column of size @p column_size
  */
 CUDA_HOST_DEVICE_CALLABLE
 gdf_size_type gdf_last_bitmask_index(gdf_size_type column_size) {
