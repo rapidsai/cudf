dbce617000796ed075b8467d33590a8a7c994cfe
Keith Kraus <kkraus@nvidia.com>
2019-03-13 17:21:24 -0400

    Update to have the normal `rapidsai` channel before the nightly so it can be easily adjusted for testing

--- ci/cpu/cudf/build_cudf.sh
+++ ci/cpu/cudf/build_cudf.sh
@@ -5,8 +5,8 @@ if [ "$BUILD_CUDF" == "1" ]; then
   export CUDF_BUILD_NO_GPU_TEST=1
 
   if [ "$BUILD_ABI" == "1" ]; then
-    conda build conda/recipes/cudf -c rapidsai-nightly -c nvidia -c numba -c conda-forge -c defaults --python=$PYTHON
+    conda build conda/recipes/cudf -c rapidsai -c rapidsai-nightly -c nvidia -c numba -c conda-forge -c defaults --python=$PYTHON
   else
-    conda build conda/recipes/cudf -c rapidsai-nightly/label/cf201901 -c nvidia/label/cf201901 -c numba -c conda-forge/label/cf201901 -c defaults --python=$PYTHON
+    conda build conda/recipes/cudf -c rapidsai/label/cf201901 -c rapidsai-nightly/label/cf201901 -c nvidia/label/cf201901 -c numba -c conda-forge/label/cf201901 -c defaults --python=$PYTHON
   fi
 fi
--- ci/cpu/libcudf/upload.sh
+++ ci/cpu/libcudf/upload.sh
@@ -19,9 +19,9 @@ fi
 if [ "$BUILD_LIBCUDF" == "1" ]; then
   # Upload libcudf
   if [ "$BUILD_ABI" == "1" ]; then
-    export UPLOADFILE=`conda build conda/recipes/libcudf -c nvidia/label/cuda${CUDA_REL} -c rapidsai-nightly/label/cuda${CUDA_REL} -c numba -c conda-forge -c defaults --output`
+    export UPLOADFILE=`conda build conda/recipes/libcudf -c nvidia/label/cuda${CUDA_REL} -c rapidsai/label/cuda${CUDA_REL} -c rapidsai-nightly/label/cuda${CUDA_REL} -c numba -c conda-forge -c defaults --output`
   else
-    export UPLOADFILE=`conda build conda/recipes/libcudf -c nvidia/label/cf201901-cuda${CUDA_REL} -c rapidsai-nightly/label/cf201901-cuda${CUDA_REL} -c numba -c conda-forge/label/cf201901 -c defaults --output`
+    export UPLOADFILE=`conda build conda/recipes/libcudf -c nvidia/label/cf201901-cuda${CUDA_REL} -c rapidsai/label/cf201901-cuda${CUDA_REL} -c rapidsai-nightly/label/cf201901-cuda${CUDA_REL} -c numba -c conda-forge/label/cf201901 -c defaults --output`
   fi
   upload
 fi
@@ -29,9 +29,9 @@ fi
 if [ "$BUILD_CFFI" == "1" ]; then
   # Upload libcudf_cffi
   if [ "$BUILD_ABI" == "1" ]; then
-    export UPLOADFILE=`conda build conda/recipes/libcudf_cffi -c nvidia/label/cuda${CUDA_REL} -c rapidsai-nightly/label/cuda${CUDA_REL} -c numba -c conda-forge -c defaults --python=$PYTHON --output`
+    export UPLOADFILE=`conda build conda/recipes/libcudf_cffi -c nvidia/label/cuda${CUDA_REL} -c rapidsai/label/cuda${CUDA_REL} -c rapidsai-nightly/label/cuda${CUDA_REL} -c numba -c conda-forge -c defaults --python=$PYTHON --output`
   else
-    export UPLOADFILE=`conda build conda/recipes/libcudf_cffi -c nvidia/label/cf201901-cuda${CUDA_REL} -c rapidsai-nightly/label/cf201901-cuda${CUDA_REL} -c numba -c conda-forge/label/cf201901 -c defaults --python=$PYTHON --output`
+    export UPLOADFILE=`conda build conda/recipes/libcudf_cffi -c nvidia/label/cf201901-cuda${CUDA_REL} -c rapidsai/label/cf201901-cuda${CUDA_REL} -c rapidsai-nightly/label/cf201901-cuda${CUDA_REL} -c numba -c conda-forge/label/cf201901 -c defaults --python=$PYTHON --output`
   fi
   upload
 fi
