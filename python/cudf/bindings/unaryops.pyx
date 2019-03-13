f923dda8964c46305f1f30626279ff86cda7bc15
Devavret Makkar <dmakkar@nvidia.com>
2019-03-14 04:27:25 +0530

    more cleanup of api added cython binding for cast. use cython binding for numerical unary ops.

--- python/cudf/bindings/unaryops.pyx
+++ python/cudf/bindings/unaryops.pyx
@@ -52,3 +52,27 @@ def apply_math_op(incol, outcol, op):
     free(c_outcol)
 
     check_gdf_error(result)
+
+
+def apply_cast(incol, outcol):
+    """
+      Cast from incol.dtype to outcol.dtype
+    """
+
+    check_gdf_compatibility(incol)
+    check_gdf_compatibility(outcol)
+    
+    cdef gdf_column* c_incol = column_view_from_column(incol)
+    cdef gdf_column* c_outcol = column_view_from_column(outcol)
+
+    cdef gdf_error result
+    with nogil:    
+        result = gdf_cast(
+            <gdf_column*>c_incol,
+            <gdf_column*>c_outcol)
+    
+    free(c_incol)
+    free(c_outcol)
+
+    check_gdf_error(result)
+
