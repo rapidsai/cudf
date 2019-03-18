52c892ed7e86551d59cdbf230ce626d243f11a54
takobayashi <takobayashi@nvidia.com>
2019-03-18 18:09:27 +0900

    Add: `atomicMin`, `atomicMax` for cudf types
    Add `atomicMin`, `atomicMax` for all possible cudf types.
    Add gtests for `atomicMin`, `atomicMax`

--- cpp/src/utilities/device_atomics.cuh
+++ cpp/src/utilities/device_atomics.cuh
@@ -18,20 +18,19 @@
  * @brief overloads for CUDA atomic operations
  * @file device_atomics.cuh
  *
- * Provides the overloads for all of possible cudf's data types:
- * int8_t, int16_t, int32_t, int64_t, float, double, cudf::date32, cudf::date64,
- * cudf::timestamp, cudf::category
+ * Provides the overloads for all of possible cudf's data types,
+ * where cudf's data types are, int8_t, int16_t, int32_t, int64_t, float, double,
+ * cudf::date32, cudf::date64, cudf::timestamp, cudf::category.
+ * where CUDA atomic operations are, `atomicAdd`, `atomicMin`, `atomicMax`,
+ * `atomicCAS` (* see note).
  *
  * @note atomicCAS never provides overloads for int8_t, int16_t
  * ---------------------------------------------------------------------------**/
-
 
 #include "cudf.h"
 #include "utilities/cudf_utils.h"
 #include "utilities/wrapper_types.hpp"
 #include "utilities/error_utils.hpp"
-
-
 
 namespace cudf {
 
@@ -57,8 +56,8 @@ namespace detail {
             using T_int = unsigned int;
 
             T_int * address_uint32 = reinterpret_cast<T_int *>
-                (addr - ((size_t)addr & 3));
-            unsigned int shift = (((size_t)addr & 3) * 8);
+                (addr - (reinterpret_cast<size_t>(addr) & 3));
+            unsigned int shift = ((reinterpret_cast<size_t>(addr) & 3) * 8);
 
             T_int old = *address_uint32;
             T_int assumed ;
@@ -66,9 +65,9 @@ namespace detail {
             do {
                 assumed = old;
                 T target_value = T((old >> shift) & 0xff);
-                T new_value = op(update_value, target_value);
-
-                old = (old & ~(0x000000ff << shift)) | (new_value << shift);
+                uint8_t new_value = type_reinterpret<uint8_t, T>
+                    ( op(target_value, update_value) );
+                old = (old & ~(0x000000ff << shift)) | (T_int(new_value) << shift);
                 old = atomicCAS(address_uint32, assumed, old);
             } while (assumed != old);
 
@@ -83,25 +82,25 @@ namespace detail {
         T operator()(T* addr, T const & update_value, Op op)
         {
             using T_int = unsigned int;
-
+            bool is_32_align = (reinterpret_cast<size_t>(addr) & 2) ? false : true;
             T_int * address_uint32 = reinterpret_cast<T_int *>
-                (addr - ((size_t)addr & 2));
-            bool is_32_align = (reinterpret_cast<size_t>(addr) & 2) ? true : false;
+                (reinterpret_cast<size_t>(addr) - (is_32_align ? 0 : 2));
 
             T_int old = *address_uint32;
             T_int assumed ;
 
             do {
                 assumed = old;
-                T target_value = (is_32_align) ? T(old >> 16) : T(old & 0xffff);
-                T new_value = op(update_value, target_value);
+                T target_value = (is_32_align) ? T(old & 0xffff) : T(old >> 16);
+                uint16_t new_value = type_reinterpret<uint16_t, T>
+                    ( op(target_value, update_value) );
 
-                old = (is_32_align) ? (old & 0xffff) | (new_value << 16)
-                                    : (old & 0xffff0000) | new_value;
+                old = (is_32_align) ? (old & 0xffff0000) | new_value
+                                    : (old & 0xffff) | (T_int(new_value) << 16);
                 old = atomicCAS(address_uint32, assumed, old);
             } while (assumed != old);
 
-            return (is_32_align) ? T(old >> 16) : T(old & 0xffff);
+            return (is_32_align) ? T(old & 0xffff) : T(old >> 16);;
         }
     };
 
@@ -118,7 +117,7 @@ namespace detail {
 
             do {
                 assumed  = old_value;
-                const T new_value = op(update_value, old_value);
+                const T new_value = op(old_value, update_value);
 
                 T_int ret = atomicCAS(
                     reinterpret_cast<T_int*>(addr),
@@ -145,7 +144,7 @@ namespace detail {
 
             do {
                 assumed  = old_value;
-                const T new_value = op(update_value, old_value);
+                const T new_value = op(old_value, update_value);
 
                 T_int ret = atomicCAS(
                     reinterpret_cast<T_int*>(addr),
@@ -233,16 +232,17 @@ struct DeviceMax{
 
 // ------------------------------------------------------------------------
 // ------------------------------------------------------------------------
-// Overloads for `atomicAdd`
+// Overloads for `atomicAdd`.
+// Cuda supports `sint32`, `uint32`, `uint64`, `float`, `double`
 
 __forceinline__ __device__
-uint8_t atomicAdd(int8_t* address, int8_t val)
+int8_t atomicAdd(int8_t* address, int8_t val)
 {
     return cudf::genericAtomicOperation(address, val, cudf::DeviceSum{});
 }
 
 __forceinline__ __device__
-uint16_t atomicAdd(int16_t* address, int16_t val)
+int16_t atomicAdd(int16_t* address, int16_t val)
 {
     return cudf::genericAtomicOperation(address, val, cudf::DeviceSum{});
 }
@@ -283,5 +283,140 @@ cudf::timestamp atomicAdd(cudf::timestamp* address, cudf::timestamp val)
     return cudf::genericAtomicOperation(address, val, cudf::DeviceSum{});
 }
 
+// ------------------------------------------------------------------------
+// Overloads for `atomicMin`.
+// Cuda supports `sint32`, `uint32`, `sint64`, `uint64`
 
+__forceinline__ __device__
+int8_t atomicMin(int8_t* address, int8_t val)
+{
+    return cudf::genericAtomicOperation(address, val, cudf::DeviceMin{});
+}
 
+__forceinline__ __device__
+int16_t atomicMin(int16_t* address, int16_t val)
+{
+    return cudf::genericAtomicOperation(address, val, cudf::DeviceMin{});
+}
+
+__forceinline__ __device__
+int64_t atomicMin(int64_t* address, int64_t val)
+{
+    using T = long long int;
+    return cudf::typesAtomicOperation64
+        (address, val, [](T* a, T v){return atomicMin(a, v);});
+}
+
+__forceinline__ __device__
+float atomicMin(float* address, float val)
+{
+    return cudf::genericAtomicOperation(address, val, cudf::DeviceMin{});
+}
+
+__forceinline__ __device__
+double atomicMin(double* address, double val)
+{
+    return cudf::genericAtomicOperation(address, val, cudf::DeviceMin{});
+}
+
+// for wrappers
+inline  __device__
+cudf::date32 atomicMin(cudf::date32* address, cudf::date32 val)
+{
+    using T = int;
+    return cudf::typesAtomicOperation32
+        (address, val, [](T* a, T v){return atomicMin(a, v);});
+}
+
+__forceinline__ __device__
+cudf::category atomicMin(cudf::category* address, cudf::category val)
+{
+    using T = int;
+    return cudf::typesAtomicOperation32
+        (address, val, [](T* a, T v){return atomicMin(a, v);});
+}
+
+__forceinline__ __device__
+cudf::date64 atomicMin(cudf::date64* address, cudf::date64 val)
+{
+    using T = long long int;
+    return cudf::typesAtomicOperation64
+        (address, val, [](T* a, T v){return atomicMin(a, v);});
+}
+
+__forceinline__ __device__
+cudf::timestamp atomicMin(cudf::timestamp* address, cudf::timestamp val)
+{
+    using T = long long int;
+    return cudf::typesAtomicOperation64
+        (address, val, [](T* a, T v){return atomicMin(a, v);});
+}
+
+// ------------------------------------------------------------------------
+// Overloads for `atomicMax`.
+// Cuda supports `sint32`, `uint32`, `sint64`, `uint64`
+
+__forceinline__ __device__
+int8_t atomicMax(int8_t* address, int8_t val)
+{
+    return cudf::genericAtomicOperation(address, val, cudf::DeviceMax{});
+}
+
+__forceinline__ __device__
+int16_t atomicMax(int16_t* address, int16_t val)
+{
+    return cudf::genericAtomicOperation(address, val, cudf::DeviceMax{});
+}
+
+__forceinline__ __device__
+int64_t atomicMax(int64_t* address, int64_t val)
+{
+    using T = long long int;
+    return cudf::typesAtomicOperation64
+        (address, val, [](T* a, T v){return atomicMax(a, v);});
+}
+
+__forceinline__ __device__
+float atomicMax(float* address, float val)
+{
+    return cudf::genericAtomicOperation(address, val, cudf::DeviceMax{});
+}
+
+__forceinline__ __device__
+double atomicMax(double* address, double val)
+{
+    return cudf::genericAtomicOperation(address, val, cudf::DeviceMax{});
+}
+
+// for wrappers
+inline  __device__
+cudf::date32 atomicMax(cudf::date32* address, cudf::date32 val)
+{
+    using T = int;
+    return cudf::typesAtomicOperation32
+        (address, val, [](T* a, T v){return atomicMax(a, v);});
+}
+
+__forceinline__ __device__
+cudf::category atomicMax(cudf::category* address, cudf::category val)
+{
+    using T = int;
+    return cudf::typesAtomicOperation32
+        (address, val, [](T* a, T v){return atomicMax(a, v);});
+}
+
+__forceinline__ __device__
+cudf::date64 atomicMax(cudf::date64* address, cudf::date64 val)
+{
+    using T = long long int;
+    return cudf::typesAtomicOperation64
+        (address, val, [](T* a, T v){return atomicMax(a, v);});
+}
+
+__forceinline__ __device__
+cudf::timestamp atomicMax(cudf::timestamp* address, cudf::timestamp val)
+{
+    using T = long long int;
+    return cudf::typesAtomicOperation64
+        (address, val, [](T* a, T v){return atomicMax(a, v);});
+}
--- cpp/tests/utilities_tests/device_atomics_test.cu
+++ cpp/tests/utilities_tests/device_atomics_test.cu
@@ -31,13 +31,16 @@
 
 template<typename T>
 __global__
-void gpu_atomicadd_test(T *result, T *data, size_t size)
+void gpu_atomic_test(T *result, T *data, size_t size)
 {
     size_t id   = blockIdx.x * blockDim.x + threadIdx.x;
     size_t step = blockDim.x * gridDim.x;
 
     for (; id < size; id += step) {
-        atomicAdd(result, data[id]);
+        atomicAdd(&result[0], data[id]);
+        atomicMin(&result[1], data[id]);
+        atomicMax(&result[2], data[id]);
+        atomicAdd(&result[3], data[id]);
     }
 }
 
@@ -47,7 +50,6 @@ template <typename T>
 struct AtomicsTest : public GdfTest {
 };
 
-
 using TestingTypes = ::testing::Types<
     int8_t, int16_t, int32_t, int64_t, float, double,
     cudf::date32, cudf::date64, cudf::timestamp, cudf::category
@@ -56,10 +58,13 @@ using TestingTypes = ::testing::Types<
 TYPED_TEST_CASE(AtomicsTest, TestingTypes);
 
 template <typename T>
-void AtomicsTest_atomicAdd()
+void AtomicsTest_atomicOps()
 {
-   std::vector<int> v({6, -14, 13, 64, -13, -20, 45});
-    int exact = std::accumulate(v.begin(), v.end(), 0);
+    std::vector<int> v({6, -14, 13, 64, -13, -20, 45});
+    int exact[3];
+    exact[0] = std::accumulate(v.begin(), v.end(), 0);
+    exact[1] = *( std::min_element(v.begin(), v.end()) );
+    exact[2] = *( std::max_element(v.begin(), v.end()) );
     size_t vec_size = v.size();
 
     // std::vector<T> v_type({6, -14, 13, 64, -13, -20, 45}));
@@ -68,15 +73,20 @@ void AtomicsTest_atomicAdd()
     std::transform(v.begin(), v.end(), v_type.begin(),
         [](int x) { T t(x) ; return t; } );
 
-    thrust::device_vector<T> dev_result(1);
+    std::vector<T> result_init(4);
+    result_init[0] = T{0};
+    result_init[1] = std::numeric_limits<T>::max();
+    result_init[2] = std::numeric_limits<T>::min();
+    result_init[3] = T{0};
+
+    thrust::device_vector<T> dev_result(result_init);
     thrust::device_vector<T> dev_data(v_type);
-    dev_result[0] = T{0};
 
     cudaDeviceSynchronize();
     CUDA_CHECK_LAST();
 
 
-    gpu_atomicadd_test<T> <<<1, vec_size>>> (
+    gpu_atomic_test<T> <<<1, vec_size>>> (
         reinterpret_cast<T*>( dev_result.data().get() ),
         reinterpret_cast<T*>( dev_data.data().get() ),
         vec_size);
@@ -87,20 +97,25 @@ void AtomicsTest_atomicAdd()
     thrust::host_vector<T> host_result(dev_result);
     cudaDeviceSynchronize();
 
-    EXPECT_EQ(host_result[0], T(exact));
+    CUDA_CHECK_LAST();
+
+    EXPECT_EQ(host_result[0], T(exact[0])) << "atomicAdd test failed";
+    EXPECT_EQ(host_result[1], T(exact[1])) << "atomicMin test failed";
+    EXPECT_EQ(host_result[2], T(exact[2])) << "atomicMax test failed";
+    EXPECT_EQ(host_result[3], T(exact[0])) << "atomicAdd test(2) failed";
 }
 
 
-TYPED_TEST(AtomicsTest, atomicAdd)
+TYPED_TEST(AtomicsTest, atomicOps)
 {
     // TODO: remove this workaround
     // At TYPED_TEST, the kernel for TypeParam of `wrapper` types won't be compiled,
     // then kenrel call failed by `cudaErrorInvalidDeviceFunction`
     // Explicit typename is requried at this point.
-    if( std::is_arithmetic<TypeParam>::value ){                 AtomicsTest_atomicAdd<TypeParam>(); }
-    else if( std::is_same<TypeParam, cudf::date32>::value ){    AtomicsTest_atomicAdd<cudf::date32>(); }
-    else if( std::is_same<TypeParam, cudf::date64>::value ){    AtomicsTest_atomicAdd<cudf::date64>(); }
-    else if( std::is_same<TypeParam, cudf::category>::value){   AtomicsTest_atomicAdd<cudf::category>(); }
-    else if( std::is_same<TypeParam, cudf::timestamp>::value ){ AtomicsTest_atomicAdd<cudf::timestamp>(); }
+    if( std::is_arithmetic<TypeParam>::value ){                 AtomicsTest_atomicOps<TypeParam>(); }
+    else if( std::is_same<TypeParam, cudf::date32>::value ){    AtomicsTest_atomicOps<cudf::date32>(); }
+    else if( std::is_same<TypeParam, cudf::date64>::value ){    AtomicsTest_atomicOps<cudf::date64>(); }
+    else if( std::is_same<TypeParam, cudf::category>::value){   AtomicsTest_atomicOps<cudf::category>(); }
+    else if( std::is_same<TypeParam, cudf::timestamp>::value ){ AtomicsTest_atomicOps<cudf::timestamp>(); }
 
 }
