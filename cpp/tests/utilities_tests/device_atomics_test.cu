64004a1f955dc7fb3237af13d89842dd6156b9a5
takobayashi <takobayashi@nvidia.com>
2019-03-18 15:36:00 +0900

    Add: atomicAdd test for wrappers
    At this point, it has explicit typename setting for a workaround.

--- cpp/tests/utilities_tests/device_atomics_test.cu
+++ cpp/tests/utilities_tests/device_atomics_test.cu
@@ -16,6 +16,7 @@
 
 #include <tests/utilities/column_wrapper.cuh>
 #include <tests/utilities/cudf_test_fixtures.h>
+#include <utilities/wrapper_types.hpp>
 #include <utilities/device_atomics.cuh>
 
 #include <gmock/gmock.h>
@@ -48,47 +49,58 @@ struct AtomicsTest : public GdfTest {
 
 
 using TestingTypes = ::testing::Types<
-#if 0
     int8_t, int16_t, int32_t, int64_t, float, double,
     cudf::date32, cudf::date64, cudf::timestamp, cudf::category
-#else
-    int8_t, int16_t, int32_t, int64_t, float, double
-#endif
     >;
-
 
 TYPED_TEST_CASE(AtomicsTest, TestingTypes);
 
-TYPED_TEST(AtomicsTest, atomicAdd)
+template <typename T>
+void AtomicsTest_atomicAdd()
 {
-    std::vector<int> v({6, -14, 13, 64, -13, -20, 45});
+   std::vector<int> v({6, -14, 13, 64, -13, -20, 45});
     int exact = std::accumulate(v.begin(), v.end(), 0);
     size_t vec_size = v.size();
 
-    // std::vector<TypeParam> v_type({6, -14, 13, 64, -13, -20, 45}));
+    // std::vector<T> v_type({6, -14, 13, 64, -13, -20, 45}));
     // use transform from std::vector<int> instead.
-    std::vector<TypeParam> v_type(vec_size);
+    std::vector<T> v_type(vec_size);
     std::transform(v.begin(), v.end(), v_type.begin(),
-        [](int x) { TypeParam t(x) ; return t; } );
+        [](int x) { T t(x) ; return t; } );
 
-    thrust::device_vector<TypeParam> dev_result(1);
-    thrust::device_vector<TypeParam> dev_data(v_type);
-    dev_result[0] = TypeParam{0};
+    thrust::device_vector<T> dev_result(1);
+    thrust::device_vector<T> dev_data(v_type);
+    dev_result[0] = T{0};
 
     cudaDeviceSynchronize();
     CUDA_CHECK_LAST();
 
-    gpu_atomicadd_test <<<1, vec_size>>> (
-        static_cast<TypeParam*>( dev_result.data().get() ),
-        static_cast<TypeParam*>( dev_data.data().get() ),
+
+    gpu_atomicadd_test<T> <<<1, vec_size>>> (
+        reinterpret_cast<T*>( dev_result.data().get() ),
+        reinterpret_cast<T*>( dev_data.data().get() ),
         vec_size);
 
+    cudaDeviceSynchronize();
     CUDA_CHECK_LAST();
+
+    thrust::host_vector<T> host_result(dev_result);
     cudaDeviceSynchronize();
 
-    thrust::host_vector<TypeParam> host_result(dev_result);
-    cudaDeviceSynchronize();
+    EXPECT_EQ(host_result[0], T(exact));
+}
 
-    EXPECT_EQ(host_result[0], TypeParam(exact));
+
+TYPED_TEST(AtomicsTest, atomicAdd)
+{
+    // TODO: remove this workaround
+    // At TYPED_TEST, the kernel for TypeParam of `wrapper` types won't be compiled,
+    // then kenrel call failed by `cudaErrorInvalidDeviceFunction`
+    // Explicit typename is requried at this point.
+    if( std::is_arithmetic<TypeParam>::value ){                 AtomicsTest_atomicAdd<TypeParam>(); }
+    else if( std::is_same<TypeParam, cudf::date32>::value ){    AtomicsTest_atomicAdd<cudf::date32>(); }
+    else if( std::is_same<TypeParam, cudf::date64>::value ){    AtomicsTest_atomicAdd<cudf::date64>(); }
+    else if( std::is_same<TypeParam, cudf::category>::value){   AtomicsTest_atomicAdd<cudf::category>(); }
+    else if( std::is_same<TypeParam, cudf::timestamp>::value ){ AtomicsTest_atomicAdd<cudf::timestamp>(); }
 
 }
