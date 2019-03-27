71caf0b15c124614f4c3c183d2a47059f7273d9f
Nick Becker <nickb500@gmail.com>
2019-03-27 10:24:54 -0400

    add handling for when only exclude is passed, leveraging logic in select_dtypes

--- python/cudf/dataframe/dataframe.py
+++ python/cudf/dataframe/dataframe.py
@@ -2156,8 +2156,12 @@ class DataFrame(object):
                             "DatetimeColumns.")
 
         else:
-            included_data = self.select_dtypes(include=include,
-                                               exclude=exclude)
+            if include:
+                included_data = self.select_dtypes(include=include,
+                                                   exclude=exclude)
+            else:
+                included_data = self.select_dtypes(include=np.number,
+                                                   exclude=exclude)
             if included_data.empty:
                 raise ValueError("No data of included types.")
             output_frame = _create_output_frame(included_data, percentiles)
