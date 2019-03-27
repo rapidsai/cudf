7f18cabadd85a714b6071bd8a5af936aad2d8c1b
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2019-03-27 13:08:27 -0700

    Review suggestions + templated findAllFromSet to enable returning (position,character) pairs.

--- cpp/src/io/utilities/parsing_utils.cu
+++ cpp/src/io/utilities/parsing_utils.cu
@@ -1,71 +1,85 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+	 * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file parsing_utils.cu Utility functions for parsing plain-text files
+ *
+ */
+
 #include "parsing_utils.cuh"
 
 #include <cuda_runtime.h>
 
 #include <vector>
+#include <memory>
 
 #include "rmm/rmm.h"
 #include "rmm/thrust_rmm_allocator.h"
 #include "utilities/error_utils.hpp"
 
+// When processing the input in chunks, this is the maximum size of each chunk.
+// Only one chunk is loaded on the GPU at a time, so this value is chosen to
+// be small enough to fit on the GPU in most cases.
 constexpr size_t max_chunk_bytes = 256*1024*1024; // 256MB
 constexpr int bytes_per_find_thread = 64;
 
-/**---------------------------------------------------------------------------*
- * @brief Searches the input character array for each of characters in a set
- * and sums up the number of occurences.
- *
- * Does not load the entire buffer into the GPU memory at any time, so it can 
- * be used with buffers of any size.
- *
- * @param[in] h_data Pointer to the data in host memory
- * @param[in] h_size Size of the input data, in bytes
- * @param[in] keys Vector containing the keys to count in the buffer
- * @param[out] count Total number of occurences of all keys
- *
- * @return gdf_error
- *---------------------------------------------------------------------------**/
-gdf_error countAllFromSet(const char *h_data, size_t h_size, std::vector<char> keys, gdf_size_type &count)
-{
-	const size_t chunk_count = (h_size + max_chunk_bytes - 1) / max_chunk_bytes;
+template <typename T>
+struct rmm_deleter {
+ void operator()(T *ptr) { RMM_FREE(ptr, 0); }
+};
+template <typename T>
+using device_ptr = std::unique_ptr<T, rmm_deleter<T>>;
 
-	char* d_chunk = nullptr;
-	RMM_TRY(RMM_ALLOC (&d_chunk, min(max_chunk_bytes, h_size), 0)); 
+using position_key_pair = thrust::pair<uint64_t,char>;
 
-	for (size_t ci = 0; ci < chunk_count; ++ci) {
-		const auto h_chunk = h_data + ci * max_chunk_bytes;
-		const auto chunk_bytes = std::min((size_t)(h_size - ci * max_chunk_bytes), max_chunk_bytes);
+//doxy
+template<class P, class K>
+__device__ __forceinline__
+void updatePosition(P* positions, long idx, P position, K key){
+	positions[idx] = position;
+}
 
-		// Copy chunk to device
-		CUDA_TRY(cudaMemcpyAsync(d_chunk, h_chunk, chunk_bytes, cudaMemcpyDefault));
+template<class P, class K>
+__device__ __forceinline__
+void updatePosition(thrust::pair<P, K>* positions, long idx, P position, K key) {
+	positions[idx] = {position, key};
+}
 
-		for (char key: keys) {
-			count += thrust::count(rmm::exec_policy()->on(0), d_chunk, d_chunk + chunk_bytes, key);
-		}
-	}
-
-	RMM_TRY( RMM_FREE(d_chunk, 0) );
-
-	CUDA_TRY(cudaGetLastError());
-
-	return GDF_SUCCESS;
+template<class P, class K>
+__device__ __forceinline__
+void updatePosition(void* positions, long idx, P position, K key) {
 }
 
 /**---------------------------------------------------------------------------*
- * @brief CUDA kernel that finds all occurences of a character in the given 
+ * @brief CUDA kernel that finds all occurrences of a character in the given 
  * character array. The positions are stored in the output array.
  * 
  * @param[in] data Pointer to the input character array
  * @param[in] size Number of bytes in the input array
  * @param[in] offset Offset to add to the output positions
  * @param[in] key Character to find in the array
- * @param[in,out] count Pointer to the number of found occurences
+ * @param[in,out] count Pointer to the number of found occurrences
  * @param[out] positions Array containing the output positions
  * 
  * @return void
  *---------------------------------------------------------------------------**/
- __global__ void findAll(char *data, size_t size, size_t offset, const char key,
-	gdf_size_type* count, ll_uint_t* positions) {
+template<class T>
+ __global__ 
+ void countAndSetPositions(char *data, uint64_t size, uint64_t offset, const char key, gdf_size_type* count,
+	T* positions) {
 
 	// thread IDs range per block, so also need the block id
 	const long tid = threadIdx.x + (blockDim.x * blockIdx.x);
@@ -80,15 +94,69 @@ gdf_error countAllFromSet(const char *h_data, size_t h_size, std::vector<char> k
 	// Process the data
 	for (long i = 0; i < byteToProcess; i++) {
 		if (raw[i] == key) {
-			const auto pos = atomicAdd(count, 1ull);
-			positions[pos] = did + offset + i;
+			const auto idx = atomicAdd(count, (gdf_size_type)1);
+			updatePosition(positions, idx, did + offset + i, key);
 		}
 	}
 }
 
 /**---------------------------------------------------------------------------*
+ * @brief Searches the input character array for each of characters in a set
+ * and sums up the number of occurrences.
+ *
+ * Does not load the entire buffer into the GPU memory at any time, so it can 
+ * be used with buffers of any size.
+ *
+ * @param[in] h_data Pointer to the data in host memory
+ * @param[in] h_size Size of the input data, in bytes
+ * @param[in] keys Vector containing the keys to count in the buffer
+ * @param[out] count Total number of occurrences of all keys
+ *
+ * @return gdf_error
+ *---------------------------------------------------------------------------**/
+gdf_size_type countAllFromSet(const char *h_data, size_t h_size, std::vector<char> keys)
+{
+	char* d_chunk = nullptr;
+	RMM_TRY(RMM_ALLOC (&d_chunk, min(max_chunk_bytes, h_size), 0));
+	device_ptr<char> chunk_data(d_chunk);
+	
+	gdf_size_type*	d_count;
+	RMM_TRY(RMM_ALLOC((void**)&d_count, sizeof(gdf_size_type), 0) );
+	device_ptr<gdf_size_type> count_data(d_count);
+	CUDA_TRY(cudaMemsetAsync(d_count, 0ull, sizeof(gdf_size_type)));
+ 
+	int blockSize;		// suggested thread count to use
+	int minGridSize;	// minimum block count required
+	CUDA_TRY(cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, countAndSetPositions<void>) );
+ 
+	const size_t chunk_count = (h_size + max_chunk_bytes - 1) / max_chunk_bytes;
+	for (size_t ci = 0; ci < chunk_count; ++ci) {	
+		const auto chunk_offset = ci * max_chunk_bytes;	
+		const auto h_chunk = h_data + chunk_offset;
+		const auto chunk_bytes = std::min((size_t)(h_size - ci * max_chunk_bytes), max_chunk_bytes);
+		const auto chunk_bits = (chunk_bytes + bytes_per_find_thread - 1) / bytes_per_find_thread;
+		const int gridSize = (chunk_bits + blockSize - 1) / blockSize;
+ 
+		// Copy chunk to device
+		CUDA_TRY(cudaMemcpyAsync(d_chunk, h_chunk, chunk_bytes, cudaMemcpyDefault));
+ 
+		for (char key: keys) {
+		 countAndSetPositions<void> <<< gridSize, blockSize >>> (
+				d_chunk, chunk_bytes, 0, key,
+				d_count, nullptr);
+		}
+	}
+ 
+	gdf_size_type h_count = 0;
+	CUDA_TRY(cudaMemcpy(&h_count, d_count, sizeof(gdf_size_type), cudaMemcpyDefault));
+
+
+	return h_count;
+ }
+
+/**---------------------------------------------------------------------------*
  * @brief For each of the characters in the input set, find and saves all 
- * cccurences in the input host array of characters.
+ * occurrences in the input host array of characters.
  * The positions are stored in the output device array.
  * 
  * Does not load the entire file into the GPU memory at any time, so it can 
@@ -102,8 +170,9 @@ gdf_error countAllFromSet(const char *h_data, size_t h_size, std::vector<char> k
  * 
  * @return gdf_error with error code on failure, otherwise GDF_SUCCESS
  *---------------------------------------------------------------------------**/
-gdf_error findAllFromSet(const char *h_data, size_t h_size, std::vector<char> keys, ll_uint_t result_offset,
-	ll_uint_t *positions) {
+template<class T>
+gdf_error findAllFromSet(const char *h_data, size_t h_size, std::vector<char> keys, uint64_t result_offset,
+	T *positions) {
 
 	char* d_chunk = nullptr;
 	RMM_TRY(RMM_ALLOC (&d_chunk, min(max_chunk_bytes, h_size), 0)); 
@@ -114,7 +183,7 @@ gdf_error findAllFromSet(const char *h_data, size_t h_size, std::vector<char> ke
 
 	int blockSize;		// suggested thread count to use
 	int minGridSize;	// minimum block count required
-	CUDA_TRY(cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, findAll) );
+	CUDA_TRY(cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, countAndSetPositions<T>) );
 
 	const size_t chunk_count = (h_size + max_chunk_bytes - 1) / max_chunk_bytes;
 	for (size_t ci = 0; ci < chunk_count; ++ci) {	
@@ -128,16 +197,26 @@ gdf_error findAllFromSet(const char *h_data, size_t h_size, std::vector<char> ke
 		CUDA_TRY(cudaMemcpyAsync(d_chunk, h_chunk, chunk_bytes, cudaMemcpyDefault));
 
 		for (char key: keys) {
-			findAll <<< gridSize, blockSize >>> (
+			countAndSetPositions<T> <<< gridSize, blockSize >>> (
 				d_chunk, chunk_bytes, chunk_offset + result_offset, key,
 				d_count, positions);
 		}
 	}
 
-	RMM_TRY( RMM_FREE( d_count, 0 ) ); 
-	RMM_TRY( RMM_FREE( d_chunk, 0 ) );
+	gdf_size_type h_count = 0;
+	CUDA_TRY(cudaMemcpy(&h_count, d_count, sizeof(gdf_size_type), cudaMemcpyDefault));
+	thrust::sort(rmm::exec_policy()->on(0), positions, positions + h_count);
 
-	CUDA_TRY( cudaGetLastError() );
+	RMM_TRY(RMM_FREE(d_count, 0)); 
+	RMM_TRY(RMM_FREE(d_chunk, 0));
+
+	CUDA_TRY(cudaGetLastError());
 
 	return GDF_SUCCESS;
 }
+
+template gdf_error findAllFromSet<uint64_t>(const char *h_data, size_t h_size, std::vector<char> keys, uint64_t result_offset,
+	uint64_t *positions);
+
+template gdf_error findAllFromSet<thrust::pair<uint64_t,char>>(const char *h_data, size_t h_size, std::vector<char> keys, uint64_t result_offset,
+	thrust::pair<uint64_t,char> *positions);
--- cpp/src/io/utilities/parsing_utils.cuh
+++ cpp/src/io/utilities/parsing_utils.cuh
@@ -1,13 +1,33 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+	 * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file parsing_utils.cuh Declarations of utility functions for parsing plain-text files
+ *
+ */
+
+
 #pragma once
 
 #include <vector>
 
 #include "cudf.h"
 
-using ll_uint_t = unsigned long long int;
+gdf_size_type countAllFromSet(const char *h_data, size_t h_size, std::vector<char> keys);
 
-gdf_error countAllFromSet(const char *h_data, size_t h_size, std::vector<char> keys, 
-	gdf_size_type &rec_cnt);
-
-gdf_error findAllFromSet(const char *h_data, size_t h_size, std::vector<char> keys, ll_uint_t result_offset,
-	ll_uint_t *recStart);
+template<class T>
+gdf_error findAllFromSet(const char *h_data, size_t h_size, std::vector<char> keys, uint64_t result_offset,
+	T *positions);
