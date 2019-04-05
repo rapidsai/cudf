40bf2848b8e5794c8b51d1dd18ec6730d314f2e3
Tadahito Kobayashi <takobayashi@nvidia.com>
2019-04-05 13:50:35 +0900

    Fix from review: _REDUCTION_OP and _SCAN_OP
    Changed the definition of _REDUCTION_OP and _SCAN_OP

--- python/cudf/bindings/reduce.pyx
+++ python/cudf/bindings/reduce.pyx
@@ -24,18 +24,21 @@ from libcpp.map cimport map as cmap
 from libcpp.string  cimport string as cstring
 
 
-_REDUCTION_OP = {}
-_REDUCTION_OP['max'] = GDF_REDUCTION_MAX
-_REDUCTION_OP['min'] = GDF_REDUCTION_MIN
-_REDUCTION_OP['sum'] = GDF_REDUCTION_SUM
-_REDUCTION_OP['product'] = GDF_REDUCTION_PRODUCT
-_REDUCTION_OP['sum_of_squares'] = GDF_REDUCTION_SUMOFSQUARES
+_REDUCTION_OP = {
+  'max': GDF_REDUCTION_MAX,
+  'min': GDF_REDUCTION_MIN,
+  'sum': GDF_REDUCTION_SUM,
+  'product': GDF_REDUCTION_PRODUCT,
+  'sum_of_squares': GDF_REDUCTION_SUMOFSQUARES,
+}
 
-_SCAN_OP = {}
-_SCAN_OP['sum'] = GDF_SCAN_SUM
-_SCAN_OP['min'] = GDF_SCAN_MIN
-_SCAN_OP['max'] = GDF_SCAN_MAX
-_SCAN_OP['product'] = GDF_SCAN_PRODUCT
+_SCAN_OP = {
+  'sum': GDF_SCAN_SUM,
+  'min': GDF_SCAN_MIN,
+  'max': GDF_SCAN_MAX,
+  'product': GDF_SCAN_PRODUCT,
+}
+
 
 cdef get_scalar_value(gdf_scalar scalar):
     return {
@@ -49,7 +52,6 @@ cdef get_scalar_value(gdf_scalar scalar):
         GDF_DATE64:  scalar.data.dt64,
         GDF_TIMESTAMP: scalar.data.tmst,
     }[scalar.dtype]
-
 
 
 def apply_reduce(reduction_op, col):
@@ -67,19 +69,22 @@ def apply_reduce(reduction_op, col):
             return col.dtype.type(1)
         return np.nan
 
+    col_dtype = col.dtype
+
     cdef gdf_column* c_col = column_view_from_column(col)
     cdef gdf_reduction_op c_op = _REDUCTION_OP[reduction_op]
+    cdef gdf_dtype c_out_dtype = c_col[0].dtype
     cdef gdf_scalar c_result
 
     with nogil:    
         c_result = reduction(
             <gdf_column*>c_col,
             c_op,
-            c_col[0].dtype
+            c_out_dtype
             )
 
     free(c_col)
-    result = np.array([get_scalar_value(c_result)], dtype=col.dtype)
+    result = np.array([get_scalar_value(c_result)], dtype=col_dtype)
 
     return result
 
