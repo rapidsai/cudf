8cd0167e91bc64a10c7a35313eb660a8c02d9193
Nick Becker <nickb500@gmail.com>
2019-04-17 11:14:16 -0400

    standard and masked tests for cumprod

--- python/cudf/tests/test_scan.py
+++ python/cudf/tests/test_scan.py
@@ -10,7 +10,7 @@ from cudf.dataframe.buffer import Buffer
 from cudf.dataframe.numerical import NumericalColumn
 from cudf.dataframe.dataframe import Series, DataFrame
 from cudf.tests import utils
-from cudf.tests.utils import gen_rand
+from cudf.tests.utils import gen_rand, assert_eq
 
 from librmm_cffi import librmm as rmm
 
@@ -140,7 +140,19 @@ def test_cumsum(dtype, nelem):
 
 
 def test_cumsum_masked():
-    pass
+    data = [1, 2, None, 4, 5]
+    float_types = ['float32', 'float64']
+    int_types = ['int8', 'int16', 'int32', 'int64']
+
+    for type_ in float_types:
+        gs = Series(data).astype(type_)
+        ps = pd.Series(data).astype(type_)
+        assert_eq(gs.cumsum(), ps.cumsum())
+
+    for type_ in int_types:
+        expected = pd.Series([1, 3, -1, 7, 12]).astype('int64')
+        gs = Series(data).astype(type_)
+        assert_eq(gs.cumsum(), expected)
 
 
 def test_cummin():
@@ -159,9 +171,42 @@ def test_cummax_masked():
     pass
 
 
-def test_cumprod():
-    pass
+@pytest.mark.parametrize('dtype,nelem', list(_gen_params()))
+def test_cumprod(dtype, nelem):
+    if dtype == np.int8:
+        # to keep data in range
+        data = gen_rand(dtype, nelem, low=-2, high=2)
+    else:
+        data = gen_rand(dtype, nelem)
+
+    decimal = 4 if dtype == np.float32 else 6
+
+    # series
+    gs = Series(data)
+    ps = pd.Series(data)
+    np.testing.assert_array_almost_equal(gs.cumprod(), ps.cumprod(),
+                                         decimal=decimal)
+
+    # dataframe series (named series)
+    gdf = DataFrame()
+    gdf['a'] = Series(data)
+    pdf = pd.DataFrame()
+    pdf['a'] = pd.Series(data)
+    np.testing.assert_array_almost_equal(gdf.a.cumprod(), pdf.a.cumprod(),
+                                         decimal=decimal)
 
 
 def test_cumprod_masked():
-    pass
+    data = [1, 2, None, 4, 5]
+    float_types = ['float32', 'float64']
+    int_types = ['int8', 'int16', 'int32', 'int64']
+
+    for type_ in float_types:
+        gs = Series(data).astype(type_)
+        ps = pd.Series(data).astype(type_)
+        assert_eq(gs.cumprod(), ps.cumprod())
+
+    for type_ in int_types:
+        expected = pd.Series([1, 2, -1, 8, 40]).astype('int64')
+        gs = Series(data).astype(type_)
+        assert_eq(gs.cumprod(), expected)
