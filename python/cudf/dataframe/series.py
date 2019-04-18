e448fbde963d761a4f41f88bb0efb9630791d81e
Nick Becker <nickb500@gmail.com>
2019-04-18 13:57:13 -0400

    docstring grammar (fix plural)

--- python/cudf/dataframe/series.py
+++ python/cudf/dataframe/series.py
@@ -1487,7 +1487,7 @@ class Series(object):
 
         Notes
         -----
-        Diff currently only supports float and integer dtypes columns with
+        Diff currently only supports float and integer dtype columns with
         no null values.
         """
         if self.null_count != 0:
