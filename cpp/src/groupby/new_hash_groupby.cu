1412d71128c68cbd2d8ea98d0ab31338a24a18fc
Jake Hemstad <jhemstad@nvidia.com>
2019-04-22 16:02:11 -0700

    Added new hash groupby impl.

---
+++ cpp/src/groupby/new_hash_groupby.cu
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cudf.h>
+#include <groupby.hpp>
+#include <hash/concurrent_unordered_map.cuh>
+#include <types.hpp>
+#include "new_hash_groupby.hpp"
+
+#include <vector>
+
+namespace cudf {
+namespace detail {
+
+std::tuple<cudf::table, cudf::table> hash_groupby(
+    cudf::table const& keys, cudf::table const& values,
+    std::vector<cudf::groupby::distributive_operators> const& operators,
+    std::vector<gdf_dtype> const& output_dtypes) {
+
+  // Create the output key and value tables
+  std::vector<gdf_dtype> key_dtypes(keys.num_columns());
+  std::transform(keys.begin(), keys.end(), key_dtypes.begin(),
+                 [](gdf_column* col) { return col->dtype; });
+  cudf::table output_keys{keys.num_rows(), key_dtypes};
+  cudf::table output_values{keys.num_rows(), output_dtypes};
+
+  return std::make_tuple(output_keys, output_values);
+}
+
+}  // namespace detail
+
+}  // namespace cudf
---
+++ cpp/src/groupby/new_hash_groupby.hpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NEW_HASH_GROUPBY_HPP
+#define NEW_HASH_GROUPBY_HPP
+
+#include <cudf.h>
+#include <groupby.hpp>
+
+#include <vector>
+
+namespace cudf {
+
+class table;
+
+namespace detail {
+
+std::tuple<cudf::table, cudf::table> hash_groupby(
+    cudf::table const& keys, cudf::table const& values,
+    std::vector<cudf::groupby::distributive_operators> const& operators,
+    std::vector<gdf_dtype> const& output_dtypes);
+}  // namespace detail
+
+}  // namespace cudf
+
+#endif
