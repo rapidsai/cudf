b99ea018c4587dc08f6af61de9ae0cd7f4d71878
Jake Hemstad <jhemstad@nvidia.com>
2019-04-22 18:03:19 -0700

    Added stream param to hash_groupby. Added initialization of output values with identity values.

--- cpp/src/groupby/new_hash_groupby.cu
+++ cpp/src/groupby/new_hash_groupby.cu
@@ -18,24 +18,87 @@
 #include <groupby.hpp>
 #include <hash/concurrent_unordered_map.cuh>
 #include <types.hpp>
+#include <utilities/type_dispatcher.hpp>
+#include "aggregation_operations.hpp"
 #include "new_hash_groupby.hpp"
 
+#include <rmm/thrust_rmm_allocator.h>
+#include <thrust/fill.h>
 #include <vector>
 
 namespace cudf {
 namespace detail {
 
+namespace {
+
+struct identity_initializer {
+  template <typename T>
+  T get_identity(groupby::distributive_operators op) {
+    switch (op) {
+      case groupby::distributive_operators::SUM:
+        return sum_op<T>::IDENTITY;
+      case groupby::distributive_operators::MIN:
+        return min_op<T>::IDENTITY;
+      case groupby::distributive_operators::MAX:
+        return max_op<T>::IDENTITY;
+      case groupby::distributive_operators::COUNT:
+        return sum_op<T>::IDENTITY;
+      default:
+        CUDF_FAIL("Invalid aggregation operation.");
+    }
+  }
+
+  template <typename T>
+  void operator()(gdf_column const& col, groupby::distributive_operators op,
+                  cudaStream_t stream = 0) {
+    T* typed_data = static_cast<T*>(col.data);
+    thrust::fill(rmm::exec_policy(stream)->on(stream), typed_data,
+                 typed_data + col.size, get_identity<T>(op));
+  }
+};
+
+/**---------------------------------------------------------------------------*
+ * @brief Initializes each column in a table with a corresponding identity value
+ * of an aggregation operation.
+ *
+ * The `i`th column will be initialized with the identity value of the `i`th
+ * aggregation operation.
+ *
+ * @param table The table of columns to initialize.
+ * @param operators The aggregation operations whose identity values will be
+ *used to initialize the columns.
+ *---------------------------------------------------------------------------**/
+void initialize_with_identity(
+    cudf::table const& table,
+    std::vector<cudf::groupby::distributive_operators> const& operators,
+    cudaStream_t stream = 0) {
+  for (gdf_size_type i = 0; i < table.num_columns(); ++i) {
+    gdf_column const* col = table.get_column(i);
+    cudf::type_dispatcher(col->dtype, identity_initializer{}, *col,
+                          operators[i]);
+  }
+}
+}  // namespace
+
 std::tuple<cudf::table, cudf::table> hash_groupby(
     cudf::table const& keys, cudf::table const& values,
     std::vector<cudf::groupby::distributive_operators> const& operators,
-    std::vector<gdf_dtype> const& output_dtypes) {
-
+    std::vector<gdf_dtype> const& output_dtypes, cudaStream_t stream) {
   // Create the output key and value tables
   std::vector<gdf_dtype> key_dtypes(keys.num_columns());
   std::transform(keys.begin(), keys.end(), key_dtypes.begin(),
                  [](gdf_column const* col) { return col->dtype; });
-  cudf::table output_keys{keys.num_rows(), key_dtypes};
-  cudf::table output_values{keys.num_rows(), output_dtypes};
+  cudf::table output_keys{keys.num_rows(), key_dtypes, true, stream};
+  cudf::table output_values{keys.num_rows(), output_dtypes, true, stream};
+  initialize_with_identity(output_values, operators, stream);
+
+  using map_type = concurrent_unordered_map<
+      gdf_size_type, gdf_size_type, std::numeric_limits<gdf_size_type>::max(),
+      default_hash<gdf_size_type>, equal_to<gdf_size_type>,
+      legacy_allocator<thrust::pair<gdf_size_type, gdf_size_type> > >;
+
+  std::unique_ptr<map_type> map =
+      std::make_unique<map_type>(compute_hash_table_size(keys.num_rows()), 0);
 
   return std::make_tuple(output_keys, output_values);
 }
