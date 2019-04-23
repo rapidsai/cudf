7cd42a5d8f172f44cbf04ff01711c70e2f73074d
Jake Hemstad <jhemstad@nvidia.com>
2019-04-22 18:28:00 -0700

    Added default constructor.

--- cpp/include/types.hpp
+++ cpp/include/types.hpp
@@ -99,6 +99,8 @@ struct table {
     }
   }
 
+  table() = default;
+
   /**---------------------------------------------------------------------------*
    * @brief Returns const pointer to const of the first `gdf_column` in the
    * table.
@@ -158,15 +160,12 @@ struct table {
    *---------------------------------------------------------------------------**/
   gdf_size_type num_rows() const { return _num_rows; }
 
-  gdf_column ** columns() const {
-      return _columns;
-  }
+  gdf_column** columns() const { return _columns; }
 
  private:
-  gdf_column** _columns;              ///< The set of gdf_columns
-  gdf_size_type const _num_columns;  ///< The number of columns in the set
-  gdf_size_type _num_rows;           ///< The number of elements in each column
-
+  gdf_column** _columns{nullptr};       ///< The set of gdf_columns
+  gdf_size_type _num_columns{0};  ///< The number of columns in the set
+  gdf_size_type _num_rows{0};  ///< The number of elements in each column
 };
 
 }  // namespace cudf
