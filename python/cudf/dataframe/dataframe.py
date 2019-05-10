9ec7f5d903bc70a63648aebfab5fd5805830dcc6
Ashwin Srinath <shwina@users.noreply.github.com>
2019-05-10 14:01:28 -0700

    Some refactoring of Loc class to make it more readable

--- python/cudf/dataframe/dataframe.py
+++ python/cudf/dataframe/dataframe.py
@@ -2812,47 +2812,46 @@ class Loc(object):
         if type(arg) is not tuple:
             arg = (arg, self._df.columns)
 
-        if self._is_multiindex_selection(arg):
-            return self._get_multiindex_selection(arg)
+        if self._is_multiindex_arg(arg):
+            return self._getitem_multiindex_arg(arg)
+        elif self._is_scalar_access(arg):
+            return self._getitem_scalar(arg)
 
-        if self._is_scalar_access(arg):
-            return self._df[arg[1]].loc[arg[0]]
+        df = self._getitem_tuple_arg(arg)
 
+        if self._can_downcast_to_series(df):
+            return self._downcast_to_series(df, arg)
+
+        return df
+
+    def _getitem_tuple_arg(self, arg):
         columns = self._get_column_selection(arg[1])
         df = DataFrame()
         for col in columns:
             df.add_column(name=col, data=self._df[col].loc[arg[0]])
-
-        if df.shape[0] == 1:
-            dtypes = df.dtypes.values.tolist()
-
-            if all([pd.api.types.is_numeric_dtype(t) for t in dtypes]):
-                dtype = np.result_type(*dtypes)
-                for name, col in df.iteritems():
-                    df[name] = col.astype(dtype)
-
-            sr = df.T
-            sr._rename_columns([arg[0]])
-            sr = sr.set_index(df.columns)
-            return sr[arg[0]]
-
-        if df.shape[1] == 1:
-            return df[arg[1]]
-
         return df
 
-    def _is_multiindex_selection(self, arg):
+    def _getitem_scalar(self, arg):
+        return self._df[arg[1]].loc[arg[0]]
+
+    def _getitem_multiindex_arg(self, arg):
+        # Explicitly ONLY support tuple indexes into MultiIndex.
+        # Pandas allows non tuple indices and warns "results may be
+        # undefined."
+        return self._df._index._get_row_major(self._df, arg)
+
+    def _normalize_dtypes(self, df):
+        dtypes = df.dtypes.values.tolist()
+        normalized_dtype = np.result_type(*dtypes)
+        for name, col in df.iteritems():
+            df[name] = col.astype(normalized_dtype)
+        return df
+
+    def _is_multiindex_arg(self, arg):
         return (
             isinstance(self._df.index, cudf.dataframe.multiindex.MultiIndex)
             and isinstance(arg, tuple)
         )
-
-    def _get_multiindex_selection(self, arg):
-        if self._is_multiindex_selection(arg):
-            # Explicitly ONLY support tuple indexes into MultiIndex.
-            # Pandas allows non tuple indices and warns "results may be
-            # undefined."
-            return self._df._index._get_row_major(self._df, arg)
 
     def _get_column_selection(self, arg):
         if self._is_single_value(arg):
@@ -2887,6 +2886,30 @@ class Loc(object):
             if not self._is_single_value(obj):
                 return False
         return True
+
+    def _can_downcast_to_series(self, df):
+        nrows, ncols = df.shape
+        if nrows == 1:
+            dtypes = df.dtypes.values.tolist()
+            all_numeric = all([pd.api.types.is_numeric_dtype(t) for t in dtypes])
+            all_identical = dtypes.count(dtypes[0]) == len(dtypes)
+            if all_numeric or all_identical:
+                return True
+        if ncols == 1:
+            return True
+        return False
+
+    def _downcast_to_series(self, df, args):
+        if df.shape[0] == 1:
+            indices = df.columns
+            df = self._normalize_dtypes(df)
+            sr = df.T
+            sr._rename_columns([args[0]])
+            sr = sr.set_index(df.columns)
+            return sr[args[0]]
+
+        if df.shape[1] == 1:
+            return df[args[1]]
 
 
 class Iloc(object):
