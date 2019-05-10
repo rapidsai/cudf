414513e9acc9e60f3721373f132488ca0ea701d2
Keith Kraus <kkraus@nvidia.com>
2019-05-10 16:47:38 -0400

    Fix handling of categoricals and datetimes

--- python/cudf/utils/utils.py
+++ python/cudf/utils/utils.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 
 import numpy as np
+import pandas as pd
 import pyarrow as pa
 from math import isnan, isinf, ceil
 
@@ -193,7 +194,9 @@ def cudf_dtype_from_pydata_dtype(dtype):
         from pandas.core.dtypes.common import \
             _get_dtype_from_object as infer_dtype_from_object
 
-    if np.issubdtype(dtype, np.datetime64):
+    if pd.api.types.is_categorical_dtype(dtype):
+        pass
+    elif np.issubdtype(dtype, np.datetime64):
         dtype = np.datetime64
 
     return infer_dtype_from_object(dtype)
