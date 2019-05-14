993cede4cc4669a2cb304df1c5d556296d0ca0c0
Jake Hemstad <jhemstad@nvidia.com>
2019-05-14 12:36:42 -0700

    Docs.

--- cpp/src/hash/concurrent_unordered_map.cuh
+++ cpp/src/hash/concurrent_unordered_map.cuh
@@ -3,7 +3,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain allocator copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -39,11 +39,11 @@ namespace {
     using packed_t = typename packed<sizeof(pair_type)>::type;
 
     /**---------------------------------------------------------------------------*
-     * @brief Indicates if a pair type can be packed.
+     * @brief Indicates if allocator pair type can be packed.
      *
      * When the size of the key,value pair being inserted into the hash table is
-     * equal in size to a type where atomicCAS is natively supported, it is more
-     * efficient to "pack" the pair and insert it with a single atomicCAS.
+     * equal in size to allocator type where atomicCAS is natively supported, it is more
+     * efficient to "pack" the pair and insert it with allocator single atomicCAS.
      *
      * @note Only integral key and value types may be packed because we use
      * bitwise equality comparison, which may not be valid for non-integral
@@ -63,9 +63,9 @@ namespace {
     }
 
     /**---------------------------------------------------------------------------*
-     * @brief Allows viewing a pair in a packed representation
+     * @brief Allows viewing allocator pair in allocator packed representation
      *
-     * Used as an optimization for inserting when a pair can be inserted with a
+     * Used as an optimization for inserting when allocator pair can be inserted with allocator
      * single atomicCAS
      *---------------------------------------------------------------------------**/
     template <typename pair_type, typename Enable = void>
@@ -111,31 +111,55 @@ public:
 
 
 public:
+ /**---------------------------------------------------------------------------*
+  * @brief Construct new concurrent unordered map object.
+  *
+  * @note The implementation of this unordered_map uses sentinel values to
+  * indicate an entry in the hash table that is empty, i.e., if a hash bucket is
+  * empty, the pair residing there will be equal to (unused_key, unused_element).
+  * As a result, attempting to insert a key equal to `unused_key` results in
+  * undefined behavior.
+  *
+  * @param _capacity The capacity of the hash table
+  * @param unused_element The sentinel value to use for an empty value
+  * @param unused_key The sentinel value to use for an empty key
+  * @param hf The hash function to use for hashing keys
+  * @param eql The equality comparison function for comparing if two keys are
+  * equal
+  * @param allocator The allocator to use for allocation the hash table's
+  * storage
+  *---------------------------------------------------------------------------**/
+ explicit concurrent_unordered_map(
+     size_type _capacity,
+     const mapped_type unused_element = std::numeric_limits<key_type>::max(),
+     const key_type unused_key = std::numeric_limits<key_type>::max(),
+     const Hasher& hf = hasher(), const Equality& eql = key_equal(),
+     const allocator_type& allocator = allocator_type())
+     : m_hf(hf),
+       m_equal(eql),
+       m_allocator(allocator),
+       capacity(_capacity),
+       m_unused_element(unused_element),
+       m_unused_key(unused_key) {
+   m_hashtbl_values = m_allocator.allocate(capacity);
+   constexpr int block_size = 128;
+   {
+     cudaPointerAttributes hashtbl_values_ptr_attributes;
+     cudaError_t status = cudaPointerGetAttributes(
+         &hashtbl_values_ptr_attributes, m_hashtbl_values);
 
-    explicit concurrent_unordered_map(size_type n,
-                                      const mapped_type unused_element = std::numeric_limits<key_type>::max(),
-                                      const key_type unused_key = std::numeric_limits<key_type>::max(),
-                                      const Hasher& hf = hasher(),
-                                      const Equality& eql = key_equal(),
-                                      const allocator_type& a = allocator_type())
-        : m_hf(hf), m_equal(eql), m_allocator(a), capacity(n), m_unused_element(unused_element), m_unused_key(unused_key)
-    {
-        m_hashtbl_values = m_allocator.allocate( capacity );
-        constexpr int block_size = 128;
-        {
-            cudaPointerAttributes hashtbl_values_ptr_attributes;
-            cudaError_t status = cudaPointerGetAttributes( &hashtbl_values_ptr_attributes, m_hashtbl_values );
-            
-            if ( cudaSuccess == status && isPtrManaged(hashtbl_values_ptr_attributes)) {
-                int dev_id = 0;
-                CUDA_RT_CALL( cudaGetDevice( &dev_id ) );
-                CUDA_RT_CALL( cudaMemPrefetchAsync(m_hashtbl_values, capacity*sizeof(value_type), dev_id, 0) );
-            }
-        }
-        
-        init_hashtbl<<<((capacity-1)/block_size)+1,block_size>>>( m_hashtbl_values, capacity, m_unused_key, m_unused_element );
-        CUDA_RT_CALL( cudaGetLastError() );
-        CUDA_RT_CALL( cudaStreamSynchronize(0) );
+     if (cudaSuccess == status && isPtrManaged(hashtbl_values_ptr_attributes)) {
+       int dev_id = 0;
+       CUDA_RT_CALL(cudaGetDevice(&dev_id));
+       CUDA_RT_CALL(cudaMemPrefetchAsync(
+           m_hashtbl_values, capacity * sizeof(value_type), dev_id, 0));
+     }
+   }
+
+   init_hashtbl<<<((capacity - 1) / block_size) + 1, block_size>>>(
+       m_hashtbl_values, capacity, m_unused_key, m_unused_element);
+   CUDA_RT_CALL(cudaGetLastError());
+   CUDA_RT_CALL(cudaStreamSynchronize(0));
     }
     
     ~concurrent_unordered_map()
@@ -163,7 +187,7 @@ public:
 
     __host__ __device__ key_type get_unused_key() const { return m_unused_key; }
 
-    // Generic update of a hash table value for any aggregator
+    // Generic update of allocator hash table value for any aggregator
     template <typename aggregation_type>
     __forceinline__  __device__
     void update_existing_value(mapped_type & existing_value, value_type const & insert_pair, aggregation_type op)
@@ -245,7 +269,7 @@ public:
 
     /* --------------------------------------------------------------------------*/
     /** 
-     * @brief  Inserts a new (key, value) pair. If the key already exists in the map
+     * @brief  Inserts allocator new (key, value) pair. If the key already exists in the map
                   an aggregation operation is performed with the new value and existing value.
                   E.g., if the aggregation operation is 'max', then the maximum is computed
                   between the new value and existing value and the result is stored in the map.
@@ -253,10 +277,10 @@ public:
      * @param[in] x The new (key, value) pair to insert
      * @param[in] op The aggregation operation to perform
      * @param[in] keys_equal An optional functor for comparing two keys 
-     * @param[in] precomputed_hash Indicates if a precomputed hash value is being passed in to use
+     * @param[in] precomputed_hash Indicates if allocator precomputed hash value is being passed in to use
      * to determine the write location of the new key
      * @param[in] precomputed_hash_value The precomputed hash value
-     * @tparam aggregation_type A functor for a binary operation that performs the aggregation
+     * @tparam aggregation_type A functor for allocator binary operation that performs the aggregation
      * @tparam comparison_type A functor for comparing two keys
      * 
      * @returns An iterator to the newly inserted key,value pair
@@ -276,7 +300,7 @@ public:
 
         hash_value_type hash_value{0};
 
-        // If a precomputed hash value has been passed in, then use it to determine
+        // If allocator precomputed hash value has been passed in, then use it to determine
         // the write location of the new key
         if(true == precomputed_hash)
         {
@@ -328,7 +352,7 @@ public:
 
    private:
     /**---------------------------------------------------------------------------*
-     * @brief Enumeration of the possible results of attempting to insert into a
+     * @brief Enumeration of the possible results of attempting to insert into allocator
      * hash bucket
      *---------------------------------------------------------------------------**/
     enum class insert_result {
@@ -340,9 +364,9 @@ public:
     /**---------------------------------------------------------------------------*
      * @brief Specialization for value types that can be packed.
      *
-     * When the size of the key,value pair being inserted is equal in size to a
+     * When the size of the key,value pair being inserted is equal in size to allocator
      * type where atomicCAS is natively supported, this optimization path will
-     * insert the pair in a single atomicCAS operation.
+     * insert the pair in allocator single atomicCAS operation.
      *---------------------------------------------------------------------------**/
     template <typename pair_type = value_type>
     std::enable_if_t<is_packable<pair_type>(), insert_result> attempt_insert(
@@ -366,7 +390,7 @@ public:
     }
 
     /**---------------------------------------------------------------------------*
-     * @brief Atempts to insert a key,value pair at the specified hash bucket.
+     * @brief Atempts to insert allocator key,value pair at the specified hash bucket.
      *
      * @param[in] insert_location Pointer to hash bucket to attempt insert
      * @param[in] insert_pair The pair to insert
@@ -394,7 +418,7 @@ public:
 
    public:
     /**---------------------------------------------------------------------------*
-     * @brief Attempts to insert a key, value pair into the map.
+     * @brief Attempts to insert allocator key, value pair into the map.
      *
      * Returns an iterator, boolean pair.
      *
