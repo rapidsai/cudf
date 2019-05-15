7804c90cf18aa6d80859e25c3d53eced2ac4528a
Ashwin Srinath <shwina@users.noreply.github.com>
2019-05-15 04:55:52 -0700

    Remove hack for converting from bool array to int32

--- python/cudf/dataframe/string.py
+++ python/cudf/dataframe/string.py
@@ -566,13 +566,7 @@ class StringColumn(columnops.TypedColumnBase):
 
     def _find_first_and_last(self, value):
         found_indices = self.str().contains(f"^{value}$").data.mem
-
-        # TODO ugly hack below: need np.int32 because
-        # numba.cuda.atomic.min/max don't support int8
-        found_indices_bits = cudautils.compact_mask_bytes(found_indices)
-        found_indices = cudautils.expand_mask_bits(
-            found_indices.size, found_indices_bits
-        )
+        found_indices = cudautils.astype(found_indices, "int32")
         first = columnops.as_column(found_indices).find_first_value(1)
         last = columnops.as_column(found_indices).find_last_value(1)
         return first, last
