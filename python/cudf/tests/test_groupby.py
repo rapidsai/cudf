099e33bd909af4e7d2f0c2e88ea7f3f8c9dfc4b7
Keith Kraus <kkraus@nvidia.com>
2019-05-17 15:14:52 -0400

    fix groupby count dtype issue

--- python/cudf/tests/test_groupby.py
+++ python/cudf/tests/test_groupby.py
@@ -504,4 +504,4 @@ def test_groupby_use_agg_column_as_index():
     gdf['a'] = [1, 1, 1, 3, 5]
     pdg = pdf.groupby('a').agg({'a': 'count'})
     gdg = gdf.groupby('a').agg({'a': 'count'})
-    assert_eq(pdg, gdg)
+    assert_eq(pdg, gdg, check_dtype=False)
