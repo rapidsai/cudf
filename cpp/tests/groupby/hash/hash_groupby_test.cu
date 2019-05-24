8385cf1edddaf64b638a716d0814ce3201ac3972
Jake Hemstad <jhemstad@nvidia.com>
2019-05-24 08:41:42 -0700

    Added stub for new groupby tests.

---
+++ cpp/tests/groupby/hash/hash_groupby_test.cu
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <tests/utilities/cudf_test_fixtures.h>
+#include <table.hpp>
+#include <tests/utilities/column_wrapper.cuh>
+#include <utilities/type_dispatcher.hpp>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <random>
+
+template <typename T>
+struct GroupbyTest : public GdfTest {
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution{1000, 10000};
+  int random_size() { return distribution(generator); }
+};
+
+using TestingTypes = ::testing::Types<int8_t, int16_t, int32_t, int64_t, float,
+                                      double, cudf::date32, cudf::date64,
+                                      cudf::timestamp, cudf::category, cudf::bool8>;
