a33134d22569e32211b343cd33221ce625754490
Matthew Rocklin <mrocklin@gmail.com>
2019-06-06 13:33:39 -0700

    Cleanup assertions in test_serialize

--- python/cudf/tests/test_serialize.py
+++ python/cudf/tests/test_serialize.py
@@ -2,21 +2,16 @@
 
 import numpy as np
 import pandas as pd
-try:
-    from distributed.protocol import serialize, deserialize
-    _have_distributed = True
-except ImportError:
-    _have_distributed = False
 import pytest
 import cudf
 from cudf.tests import utils
+from cudf.tests.utils import assert_eq
+
+pytest.importorskip('dask.distributed')
+
+from distributed.protocol import serialize, deserialize  # noqa: E402
 
 
-require_distributed = pytest.mark.skipif(not _have_distributed,
-                                         reason='no distributed')
-
-
-@require_distributed
 def test_serialize_dataframe():
     df = cudf.DataFrame()
     df['a'] = np.arange(100)
@@ -24,10 +19,9 @@ def test_serialize_dataframe():
     df['c'] = pd.Categorical(['a', 'b', 'c', '_', '_'] * 20,
                              categories=['a', 'b', 'c'])
     outdf = deserialize(*serialize(df))
-    pd.util.testing.assert_frame_equal(df.to_pandas(), outdf.to_pandas())
+    assert_eq(df, outdf)
 
 
-@require_distributed
 def test_serialize_dataframe_with_index():
     df = cudf.DataFrame()
     df['a'] = np.arange(100)
@@ -36,31 +30,27 @@ def test_serialize_dataframe_with_index():
                              categories=['a', 'b', 'c'])
     df = df.sort_values('b')
     outdf = deserialize(*serialize(df))
-    pd.util.testing.assert_frame_equal(df.to_pandas(), outdf.to_pandas())
+    assert_eq(df, outdf)
 
 
-@require_distributed
 def test_serialize_series():
     sr = cudf.Series(np.arange(100))
     outsr = deserialize(*serialize(sr))
-    pd.util.testing.assert_series_equal(sr.to_pandas(), outsr.to_pandas())
+    assert_eq(sr, outsr)
 
 
-@require_distributed
 def test_serialize_range_index():
     index = cudf.dataframe.index.RangeIndex(10, 20)
     outindex = deserialize(*serialize(index))
-    assert index == outindex
+    assert_eq(index, outindex)
 
 
-@require_distributed
 def test_serialize_generic_index():
     index = cudf.dataframe.index.GenericIndex(cudf.Series(np.arange(10)))
     outindex = deserialize(*serialize(index))
-    assert index == outindex
+    assert_eq(index, outindex)
 
 
-@require_distributed
 def test_serialize_masked_series():
     nelem = 50
     data = np.random.random(nelem)
@@ -70,10 +60,9 @@ def test_serialize_masked_series():
     assert null_count >= 0
     sr = cudf.Series.from_masked_array(data, mask, null_count=null_count)
     outsr = deserialize(*serialize(sr))
-    pd.util.testing.assert_series_equal(sr.to_pandas(), outsr.to_pandas())
+    assert_eq(sr, outsr)
 
 
-@require_distributed
 def test_serialize_groupby():
     df = cudf.DataFrame()
     df['key'] = np.random.randint(0, 20, 100)
@@ -83,10 +72,9 @@ def test_serialize_groupby():
 
     got = gb.mean()
     expect = outgb.mean()
-    pd.util.testing.assert_frame_equal(got.to_pandas(), expect.to_pandas())
+    assert_eq(got, expect)
 
 
-@require_distributed
 def test_serialize_datetime():
     # Make frame with datetime column
     df = pd.DataFrame({'x': np.random.randint(0, 5, size=20),
@@ -97,10 +85,9 @@ def test_serialize_datetime():
     # (De)serialize roundtrip
     recreated = deserialize(*serialize(gdf))
     # Check
-    pd.util.testing.assert_frame_equal(recreated.to_pandas(), df)
+    assert_eq(recreated, df)
 
 
-@require_distributed
 def test_serialize_string():
     # Make frame with string column
     df = pd.DataFrame({'x': np.random.randint(0, 5, size=5),
@@ -111,23 +98,21 @@ def test_serialize_string():
     # (De)serialize roundtrip
     recreated = deserialize(*serialize(gdf))
     # Check
-    pd.util.testing.assert_frame_equal(recreated.to_pandas(), df)
+    assert_eq(recreated, df)
 
 
-@require_distributed
 def test_serialize_empty_string():
     pd_series = pd.Series([], dtype='str')
     gd_series = cudf.Series([], dtype='str')
 
     recreated = deserialize(*serialize(gd_series))
-    pd.util.testing.assert_series_equal(recreated.to_pandas(), pd_series)
+    assert_eq(recreated, pd_series)
 
 
-@require_distributed
 def test_serialize_all_null_string():
     data = [None, None, None, None, None]
     pd_series = pd.Series(data, dtype='str')
     gd_series = cudf.Series(data, dtype='str')
 
     recreated = deserialize(*serialize(gd_series))
-    pd.util.testing.assert_series_equal(recreated.to_pandas(), pd_series)
+    assert_eq(recreated, pd_series)
