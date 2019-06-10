8ecbc81378e8cd49c87fa3d542232f760debc988
Tadahito Kobayashi <takobayashi@nvidia.com>
2019-06-10 17:00:29 +0900

    Fix the header file path

--- cpp/src/utilities/device_operators.cuh
+++ cpp/src/utilities/device_operators.cuh
@@ -23,7 +23,7 @@
  *
  * ---------------------------------------------------------------------------**/
 
-#include <cudf.h>
+#include <cudf/cudf.h>
 #include <utilities/cudf_utils.h>       // need for CUDA_HOST_DEVICE_CALLABLE
 #include <utilities/wrapper_types.hpp>
 #include <utilities/error_utils.hpp>
