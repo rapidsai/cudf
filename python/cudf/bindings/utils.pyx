f2357b54ea6166e2942ea7f2843aa609f3d6f932
Ashwin Srinath <shwina@users.noreply.github.com>
2019-06-19 12:27:42 -0400

    Fix groupby for empty nvcategory case

--- python/cudf/bindings/utils.pyx
+++ python/cudf/bindings/utils.pyx
@@ -29,25 +29,35 @@ cdef dataframe_from_table(cudf_table* table, colnames):
             data=col
         )
         free(c_col)
-    del table
     return df
 
 
 cdef columns_from_table(cudf_table* table):
     from cudf.dataframe.column import Column
+    cdef gdf_column* c_col
     columns = []
     for i in range(table[0].num_columns()):
-        data, mask = gdf_column_to_column_mem(table[0].get_column(i))
+        c_col = table[0].get_column(i)
+        data, mask = gdf_column_to_column_mem(c_col)
         columns.append(
             Column.from_mem_views(data, mask)
         )
+        free(c_col)
     return columns
 
 
 cdef cudf_table* table_from_columns(columns):
     cdef cudf_table* c_table
     cdef vector[gdf_column*] c_columns
+    cdef gdf_column* c_col
     for col in columns:
-        c_columns.push_back(column_view_from_column(col))
+        c_col = column_view_from_column(col)
+        c_columns.push_back(c_col)
     c_table = new cudf_table(c_columns)
     return c_table
+
+
+
+def test(column):
+    cdef gdf_column* c_col
+    c_col = column_view_from_column(column)
