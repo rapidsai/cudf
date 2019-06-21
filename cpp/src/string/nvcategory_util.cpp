b6dcd433dd437d43cad995dd9ffa096d99e27c39
Ashwin Srinath <shwina@users.noreply.github.com>
2019-06-21 17:57:04 -0400

    Revert to using out-of-place replace_nulls in nvcategory_utils.cpp

--- cpp/src/string/nvcategory_util.cpp
+++ cpp/src/string/nvcategory_util.cpp
@@ -96,7 +96,19 @@ gdf_error nvcategory_gather(gdf_column * column, NVCategory * nv_category){
     null_index_scalar.is_valid = true;
     null_index_scalar.dtype = GDF_STRING_CATEGORY;
 
-    cudf::replace_nulls(*column, null_index_scalar, true);
+    const auto byte_width = (column->dtype == GDF_STRING)
+      ? sizeof(std::pair<const char *, size_t>)
+      : cudf::size_of(column->dtype);
+
+    gdf_column column_nulls_replaced = cudf::replace_nulls(*column, null_index_scalar);
+    CUDA_TRY(cudaMemcpyAsync(
+                             column->data,
+                             column_nulls_replaced.data,
+                             column->size * byte_width,
+                             cudaMemcpyDefault,
+                             0));
+
+    gdf_column_free(&column_nulls_replaced);
   }
 
   CUDF_EXPECTS(column->data != nullptr, "Trying to gather nullptr data in nvcategory_gather");
