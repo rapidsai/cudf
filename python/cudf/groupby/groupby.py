842138ea5475af885d8beaa6ffbd9acd17cd8afc
Ashwin Srinath <shwina@users.noreply.github.com>
2019-06-24 11:59:49 -0400

    Move non-Cython code out of groupby.pyx and into groupby.py

--- python/cudf/groupby/groupby.py
+++ python/cudf/groupby/groupby.py
@@ -1,19 +1,30 @@
 # Copyright (c) 2018, NVIDIA CORPORATION.
 
 from numbers import Number
+import collections
 
+import cudf
 from cudf.dataframe.dataframe import DataFrame
 from cudf.dataframe.series import Series
 from cudf import MultiIndex
+from cudf.bindings.nvtx import nvtx_range_pop
 
-from cudf.bindings.groupby import (
-    agg as cpp_agg,
-    apply_basic_agg as cpp_apply_basic_agg
-)
+from cudf.bindings.groupby import apply_groupby as cpp_apply_groupby
 
 
 _LEVEL_0_INDEX_NAME = 'cudf_groupby_level_index'
 _LEVEL_0_DATA_NAME = 'cudf_groupby_data_name'
+
+
+def columns_from_dataframe(df):
+    return [sr._column for sr in df._cols.values()]
+
+
+def dataframe_from_columns(cols, index=None, columns=None):
+    df = cudf.DataFrame(dict(zip(range(len(cols)), cols)), index=index)
+    if columns is not None:
+        df.columns = columns
+    return df
 
 
 class SeriesGroupBy(object):
@@ -234,7 +245,7 @@ class Groupby(object):
                 for by in self._by:
                     agg_groupby._df._cols[by] = self._df._cols[by]
         return cpp_apply_basic_agg(agg_groupby, agg_type,
-                                   sort_results=sort_results)
+                                sort_results=sort_results)
 
     def apply_multiindex_or_single_index(self, result):
         if len(result) == 0:
@@ -460,3 +471,232 @@ class Groupby(object):
         """
         agg_groupby = self.copy(deep=False)
         return cpp_agg(agg_groupby, args)
+
+
+def cpp_agg(groupby_class, args):
+    """ Invoke aggregation functions on the groups.
+
+    Parameters
+    ----------
+    groupby_class : :class:`~cudf.groupby.Groupby`
+        Instance of :class:`~cudf.groupby.Groupby`.
+    args : dict, list, str, callable
+        - str
+            The aggregate function name.
+        - list
+            List of *str* of the aggregate function.
+        - dict
+            key-value pairs of source column name and list of
+            aggregate functions as *str*.
+
+    Returns
+    -------
+    result : DataFrame
+    """
+    sort_results = True
+
+    key_columns = []
+    value_columns = []
+    result_key_names = []
+    result_value_names = []
+    agg_names = []
+
+    for colname in groupby_class._by:
+        key_columns.append(groupby_class._df._cols[colname]._column)
+        result_key_names = groupby_class._by
+
+    use_prefix = (
+        len(groupby_class._val_columns) > 1
+        or len(args) > 1
+    )
+    if not isinstance(args, str) and isinstance(
+            args, collections.abc.Sequence):
+        # call apply_groupby and then rename the columns
+        for colname in groupby_class._val_columns:
+            in_column = groupby_class._df._cols[colname]._column
+            for agg_type in args:
+                value_columns.append(in_column)
+                if use_prefix:
+                    result_value_names.append(
+                        agg_type + '_' + colname)
+                agg_names.append(agg_type)
+            if not use_prefix:
+                result_value_names.extend(groupby_class._val_columns)
+
+        result_keys, result_values = cpp_apply_groupby(
+            key_columns,
+            value_columns,
+            agg_names
+        )
+
+        result = cudf.concat(
+            [
+                dataframe_from_columns(result_keys, columns=result_key_names),
+                dataframe_from_columns(result_values, columns=result_value_names)
+            ],
+            axis=1
+        )
+
+        if (
+                result_key_names == result_value_names
+                and len(result_key_names) == 1
+        ):
+            # Special case as index and column have the same name,
+            # which `concat` cannot deal with
+            result = cudf.DataFrame(
+                {result_key_names[0]: result_values[0]},
+                index=cudf.Series(result_keys[0], name=result_key_names[0])
+            )
+            if sort_results:
+                result = result.sort_index()
+            return result
+
+        if sort_results:
+            result = result.sort_values(result_key_names)
+
+        if(groupby_class._as_index):
+            result = groupby_class.apply_multiindex_or_single_index(result)
+        if use_prefix and groupby_class._as_index:
+            result = groupby_class.apply_multicolumn(result, args)
+
+    elif isinstance(args, collections.abc.Mapping):
+        if len(args) == 1:
+            for key, value in args.items():
+                if isinstance(value, str):
+                    use_prefix = False
+                else:
+                    if len(value) == 1:
+                        use_prefix = False
+        for colname, agg_type in args.items():
+            in_column = groupby_class._df._cols[colname]._column
+            if not isinstance(agg_type, str) and \
+                    isinstance(agg_type, collections.abc.Sequence):
+                for sub_agg_type in agg_type:
+                    agg_names.append(sub_agg_type)
+                    value_columns.append(in_column)
+                    if use_prefix:
+                        result_value_names.append(
+                            sub_agg_type + '_' + colname
+                        )
+                    if not use_prefix:
+                        result_value_names.append(colname)
+            elif isinstance(agg_type, str):
+                value_columns.append(in_column)
+                agg_names.append(agg_type)
+                if use_prefix:
+                    result_value_names.append(
+                        agg_type + '_' + colname
+                    )
+                if not use_prefix:
+                    result_value_names.append(colname)
+
+        result_keys, result_values = cpp_apply_groupby(
+            key_columns,
+            value_columns,
+            agg_names
+        )
+
+        result = cudf.concat(
+            [
+                dataframe_from_columns(result_keys, columns=result_key_names),
+                dataframe_from_columns(result_values, columns=result_value_names)
+            ],
+            axis=1
+        )
+
+        if (
+                result_key_names == result_value_names
+                and len(result_key_names) == 1
+        ):
+            # Special case as index and column have the same name,
+            # which `concat` cannot deal with
+            result = cudf.DataFrame(
+                {result_key_names[0]: result_values[0]},
+                index=cudf.Series(result_keys[0], name=result_key_names[0])
+            )
+            if sort_results:
+                result = result.sort_index()
+            return result
+
+        if sort_results:
+            result = result.sort_values(result_key_names)
+
+        if groupby_class._as_index:
+            result = groupby_class.apply_multiindex_or_single_index(result)
+        if use_prefix and groupby_class._as_index:
+            result = groupby_class.apply_multicolumn_mapped(result, args)
+    else:
+        result = groupby_class.agg([args])
+
+    nvtx_range_pop()
+    return result
+
+
+def cpp_apply_basic_agg(groupby_class, agg_type, sort_results=False):
+    """
+    Parameters
+    ----------
+    groupby_class : :class:`~cudf.groupby.Groupby`
+        Instance of :class:`~cudf.groupby.Groupby`.
+    agg_type : str
+        The aggregation function to run.
+    """
+    sort_results = True
+
+    key_columns = columns_from_dataframe(groupby_class._df[groupby_class._by])
+    value_columns = columns_from_dataframe(groupby_class._df[groupby_class._val_columns])
+    result_key_names = groupby_class._by
+    result_value_names = groupby_class._val_columns
+
+    result_keys, result_values = cpp_apply_groupby(
+        key_columns,
+        value_columns,
+        agg_type
+    )
+
+    result = cudf.concat(
+        [
+            dataframe_from_columns(result_keys, columns=result_key_names),
+            dataframe_from_columns(result_values, columns=result_value_names)
+        ],
+        axis=1
+    )
+
+    if (
+            result_key_names == result_value_names
+            and len(result_key_names) == 1
+    ):
+        # Special case as index and column have the same name,
+        # which `concat` cannot deal with
+        result = cudf.DataFrame(
+            {result_key_names[0]: result_values[0]},
+            index=cudf.Series(result_keys[0], name=result_key_names[0])
+        )
+        if sort_results:
+            result = result.sort_index()
+        return result
+
+    if sort_results:
+        result = result.sort_values(result_key_names)
+
+    # If a Groupby has one index column and one value column
+    # and as_index is set, return a Series instead of a df
+    if isinstance(result_value_names, (str, Number)) and groupby_class._as_index:
+        result_series = result[result_value_names]
+        idx = index.as_index(result[groupby_class._by[0]])
+        if groupby_class.level == 0:
+            idx.name = groupby_class._original_index_name
+        else:
+            idx.name = groupby_class._by[0]
+        result_series = result_series.set_index(idx)
+        if groupby_class._as_index:
+            result = groupby_class.apply_multiindex_or_single_index(result)
+            result_series.index = result.index
+        return result_series
+
+    if groupby_class._as_index:
+        result = groupby_class.apply_multiindex_or_single_index(result)
+
+    nvtx_range_pop()
+
+    return result
