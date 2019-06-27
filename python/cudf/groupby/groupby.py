aa506dc4bcf9038b4ab20b41813c4275876dc1d3
Ashwin Srinath <shwina@users.noreply.github.com>
2019-06-27 14:50:29 -0400

    Add a GroupbyHelper class to handle groupby messiness

--- python/cudf/groupby/groupby.py
+++ python/cudf/groupby/groupby.py
@@ -28,9 +28,6 @@ def dataframe_from_columns(cols, index_cols=None, index=None, columns=None):
 
 
 class _Groupby(object):
-
-    _NAMED_AGGS = ('sum', 'mean', 'min', 'max', 'count')
-
     def sum(self):
         return self._apply_aggregation("sum")
 
@@ -55,111 +52,10 @@ class SeriesGroupBy(_Groupby):
         self._sr = sr
         self._by = by
         self._sort = sort
-        self._normalize_keys()
-
-    def _normalize_keys(self):
-        """
-        Normalizes the input key names and columns
-        """
-        if isinstance(self._by, (list, tuple)):
-            self._key_names = []
-            self._key_columns = []
-            for by in self._by:
-                name, col = self._key_from_by(by)
-                self._key_names.append(name)
-                self._key_columns.append(col)
-        else:
-            # grouping by a single label or Series
-            name, col = self._key_from_by(self._by)
-            self._key_names = [name]
-            self._key_columns = [col]
-
-    def _key_from_by(self, by):
-        try:
-            by = cudf.Series(by)
-        except:
-            raise ValueError("Cannot convert by argument to a Series")
-        if len(by) != len(self._sr):
-            raise NotImplementedError("cuDF does not support arbitrary series index lengths "
-                                      "for groupby")
-        key_name = by.name
-        key_column = by._column
-        return key_name, key_column
+        self._groupby = _GroupbyHelper(sr, by, level=level, sort=sort)
 
     def _apply_aggregation(self, agg):
-        """
-        Applies the aggregation function(s) ``agg`` on all columns
-        """
-        self._normalize_values(agg)
-
-        out_key_columns, out_value_columns = _groupby_engine(
-            self._key_columns,
-            self._value_columns,
-            self._aggs,
-            self._sort
-        )
-
-        return self._construct_result(out_key_columns, out_value_columns)
-
-    def _normalize_values(self, agg):
-        unknown_agg_err = lambda agg: "Uknown aggregation function {}".format(agg)
-
-        if isinstance(agg, str):
-            if agg not in self._NAMED_AGGS:
-                raise ValueError(unknown_arg_err(agg))
-            self._aggs = agg
-            self._value_columns = [self._sr._column]
-            self._value_names = [self._sr.name]
-
-        elif isinstance(agg, list):
-            for agg_name in agg:
-                if agg_name not in self._NAMED_AGGS:
-                    raise ValueError(unknown_arg_err(agg))
-            # repeat each element of self._value_columns len(agg) times,
-            self._value_columns = [self._sr._column]*len(agg)
-            self._value_names = agg
-            self._aggs = agg
-        else:
-            raise ValueError("Invalid type for agg")
-
-    def _construct_result(self, out_key_columns, out_value_columns):
-        result = dataframe_from_columns(
-                out_value_columns,
-                columns=self._compute_result_column_index()
-            )
-        result.index = self._compute_result_index(out_key_columns, out_value_columns)
-
-        if isinstance(self._aggs, str):
-            result = result[result.columns[0]]
-            result.name = self._value_names[0]
-        return result
-
-    def _compute_result_index(self, key_columns, value_columns):
-        """
-        Computes the index of the result
-        """
-        key_names = self._key_names
-        if (len(key_columns)) == 1:
-            return cudf.dataframe.index.as_index(key_columns[0],
-                                                 name=key_names[0])
-        else:
-            empty_results = all([len(x)==0 for x in key_columns])
-            if len(value_columns) == 0  and empty_results:
-                return cudf.dataframe.index.GenericIndex(cudf.Series([], dtype='object'))
-            return MultiIndex(source_data=dataframe_from_columns(key_columns,
-                                                                 columns=key_names))
-
-    def _compute_result_column_index(self):
-        """
-        Computes the column index of the result
-        """
-        value_names = self._value_names
-        aggs = self._aggs
-        if isinstance(aggs, str):
-            return self._sr.name
-        else:
-            return self._aggs
-
+        return self._groupby.compute_result(agg)
 
 class DataFrameGroupBy(_Groupby):
 
@@ -181,161 +77,23 @@ class DataFrameGroupBy(_Groupby):
         self._by = by
         self._as_index = as_index
         self._sort = sort
-        self._normalize_keys()
-
-    def _normalize_keys(self):
-        """
-        Normalizes the input key names and columns
-        """
-        if isinstance(self._by, (list, tuple)):
-            self._key_names = []
-            self._key_columns = []
-            for by in self._by:
-                name, col = self._key_from_by(by)
-                self._key_names.append(name)
-                self._key_columns.append(col)
-        else:
-            # grouping by a single label or Series
-            name, col = self._key_from_by(self._by)
-            self._key_names = [name]
-            self._key_columns = [col]
-
-    def _key_from_by(self, by):
-        if isinstance(by, str):
-            key_name = by
-            key_column = self._df[by]._column
-        elif isinstance(by, cudf.Series):
-            key_name = by.name
-            key_column = by._column
-        else:
-            raise ValueError("Cannot group by object of type {}".format(
-                type(by).__name__)
-            )
-        return key_name, key_column
-
-    def _normalize_values(self, agg):
-        """
-        Normalizes the groupby object based on agg.
-        """
-        unknown_agg_err = lambda agg: "Uknown aggregation function {}".format(agg)
-
-        self._value_names = [col for col in self._df.columns if col not in self._key_names]
-        self._value_columns = columns_from_dataframe(self._df[self._value_names])
-
-        if isinstance(agg, str):
-            if agg not in self._NAMED_AGGS:
-                raise ValueError(unknown_arg_err(agg))
-            self._aggs = agg
-        elif isinstance(agg, list):
-            for agg_name in agg:
-                if agg_name not in self._NAMED_AGGS:
-                    raise ValueError(unknown_arg_err(agg))
-            agg_list = agg * len(self._value_columns)
-            # repeat each element of self._value_columns len(agg) times,
-            # i.e., [A, B, C] -> [A, A, A..., B, B, B, ..., C, C, C, ...]
-            self._value_columns = list(itertools.chain.from_iterable(
-                len(agg)*[col] for col in self._value_columns
-            ))
-            self._value_names = list(itertools.chain.from_iterable(
-                len(agg)*[col] for col in self._value_names
-            ))
-            self._aggs = agg_list
-        elif isinstance(agg, dict):
-            agg_list = []
-            self._value_columns = []
-            self._value_names = []
-            for col_name, col_agg in agg.items():
-                col = self._df[col_name]._column
-                if isinstance(col_agg, str):
-                    if col_agg not in self._NAMED_AGGS:
-                        raise ValueError(unknown_agg_err(col_agg))
-                    self._value_columns.append(col)
-                    self._value_names.append(col_name)
-                    agg_list.append(col_agg)
-                elif isinstance(col_agg, list):
-                    for col_sub_agg in col_agg:
-                        if col_sub_agg not in self._NAMED_AGGS:
-                            raise ValueError(unknown_agg_err(col_agg))
-                    self._value_columns.extend([col]*len(col_agg))
-                    self._value_names.extend([col_name]*len(col_agg))
-                    agg_list.extend(col_agg)
-            self._aggs = agg_list
-        else:
-            raise ValueError("Invalid type for agg")
+        self._groupby = _GroupbyHelper(df, by, as_index=as_index, level=level, sort=sort)
 
     def _apply_aggregation(self, agg):
         """
         Applies the aggregation function(s) ``agg`` on all columns
         """
-        self._normalize_values(agg)
-
-        out_key_columns, out_value_columns = _groupby_engine(
-            self._key_columns,
-            self._value_columns,
-            self._aggs,
-            self._sort
-        )
-
-        return self._construct_result(out_key_columns, out_value_columns)
-
-
-    def _construct_result(self, out_key_columns, out_value_columns):
-        if self._as_index:
-            result = dataframe_from_columns(
-                out_value_columns,
-                columns=self._compute_result_column_index()
-            )
-            result.index = self._compute_result_index(out_key_columns, out_value_columns)
-        else:
-            result = cudf.concat(
-                [
-                    dataframe_from_columns(out_key_columns, columns=self._key_names),
-                    dataframe_from_columns(out_value_columns, columns=self._value_names)
-                ],
-                axis=1
-            )
-        return result
-
-    def _compute_result_index(self, key_columns, value_columns):
-        """
-        Computes the index of the result
-        """
-        key_names = self._key_names
-        if (len(key_columns)) == 1:
-            return cudf.dataframe.index.as_index(key_columns[0],
-                                                 name=key_names[0])
-        else:
-            empty_results = all([len(x)==0 for x in key_columns])
-            if len(value_columns) == 0  and empty_results:
-                return cudf.dataframe.index.GenericIndex(cudf.Series([], dtype='object'))
-            return MultiIndex(source_data=dataframe_from_columns(key_columns,
-                                                                 columns=key_names))
-
-    def _compute_result_column_index(self):
-        """
-        Computes the column index of the result
-        """
-        value_names = self._value_names
-        aggs = self._aggs
-        if isinstance(aggs, str):
-            return value_names
-        else:
-            if (
-                    len(aggs) == 1
-                    or len(set(value_names)) == len(value_names)
-            ):
-                return value_names
-            else:
-                return MultiIndex.from_tuples(zip(value_names, aggs))
+        return self._groupby.compute_result(agg)
 
     def __getitem__(self, arg):
         if isinstance(arg, str):
             return self.__getattr__(arg)
         else:
             arg = list(arg)
-            import pdb
-            pdb.set_trace()
-            return self._df[arg].groupby(self._by,
+            by_list = []
+            for by_name, by in zip(self._groupby.key_names, self._groupby.key_columns):
+                by_list.append(cudf.Series(by, name=by_name))
+            return self._df[arg].groupby(by_list,
                                          sort=self._sort)
 
     def __getattr__(self, key):
@@ -343,9 +101,207 @@ class DataFrameGroupBy(_Groupby):
             raise AttributeError("'DataFrameGroupBy' object has no attribute "
                                  "'{}'".format(key))
         by_list = []
-        for by_name, by in zip(self._key_names, self._key_columns):
+        for by_name, by in zip(self._groupby.key_names, self._groupby.key_columns):
             by_list.append(cudf.Series(by, name=by_name))
-        return self._df[key].groupby(by_list, sort=self._sort)
+        return self._df[key].groupby(by_list,
+                                     sort=self._sort)
+
+
+class _GroupbyHelper(object):
+
+    NAMED_AGGS = ('sum', 'mean', 'min', 'max', 'count')
+
+    def __init__(self, obj, by, as_index=True, level=None, sort=None):
+        """
+        Helper class for both SeriesGroupBy and DataFrameGroupBy classes.
+        """
+        self.obj = obj
+        self.by = by
+        self.as_index = as_index
+        self.level = level
+        self.sort = sort
+        self.normalize_keys()
+
+    def normalize_keys(self):
+        """
+        Sets self.key_names and self.key_columns
+        """
+        if isinstance(self.by, (list, tuple)):
+            self.key_names = []
+            self.key_columns = []
+            for by in self.by:
+                name, col = self.key_from_by(by)
+                self.key_names.append(name)
+                self.key_columns.append(col)
+        else:
+            # grouping by a single label or Series
+            name, col = self.key_from_by(self.by)
+            self.key_names = [name]
+            self.key_columns = [col]
+
+    def key_from_by(self, by):
+        """
+        Get (key_name, key_column) pair from a single *by* argument
+        """
+        if isinstance(by, str):
+            key_name = by
+            key_column = self.obj[by]._column
+        else:
+            try:
+                by = cudf.Series(by)
+            except:
+                raise ValueError("Cannot convert by argument to a Series")
+            if len(by) != len(self.obj):
+                raise NotImplementedError("cuDF does not support arbitrary series index lengths "
+                                        "for groupby")
+            key_name = by.name
+            key_column = by._column
+        return key_name, key_column
+
+    def compute_result(self, agg):
+        """
+        Computes the groupby result
+        """
+        self.normalize_agg(agg)
+        self.normalize_values()
+        aggs_as_list = self.get_aggs_as_list()
+
+        out_key_columns, out_value_columns = _groupby_engine(
+            self.key_columns,
+            self.value_columns,
+            aggs_as_list,
+            self.sort
+        )
+
+        return self.construct_result(out_key_columns, out_value_columns)
+
+    def normalize_agg(self, agg):
+        """
+        Normalize agg to a dictionary with column names
+        as keys and lists of aggregations as values.
+
+        For a Series, the dictionary has a single key ``None``
+        """
+        if isinstance(agg, collections.Mapping):
+            for key, val in agg.items():
+                if not isinstance(val, list):
+                    agg[key] = [val]
+            self.aggs = agg
+            return
+        if isinstance(self.obj, cudf.Series):
+            keys = [None]
+        else:
+            keys = []
+            for col_name in self.obj.columns:
+                if col_name not in self.key_names:
+                    keys.append(col_name)
+        if isinstance(agg, str):
+            agg = [agg]
+        values = [agg] * len(keys)
+        self.aggs = dict(zip(keys, values))
+        self.validate_aggs()
+
+    def validate_aggs(self):
+        for col_name, agg_list in self.aggs.items():
+            for agg in agg_list:
+                if agg not in self.NAMED_AGGS:
+                    raise ValueError(
+                        f"Aggregation function name {agg} not recognized"
+                    )
+
+    def normalize_values(self):
+        """
+        Sets self.value_names and self.value_columns
+        """
+        if isinstance(self.obj, cudf.Series):
+            # SeriesGroupBy
+            col = self.obj._column
+            agg_list = self.aggs[None]
+            if len(agg_list) == 1:
+                self.value_columns = [col]
+                self.value_names = [self.obj.name]
+            else:
+                self.value_columns = [col] * len(agg_list)
+                self.value_names = agg_list
+        else:
+            # DataFraneGroupBy
+            self.value_columns = []
+            self.value_names = []
+            for col_name, agg_list in self.aggs.items():
+                col = self.obj[col_name]._column
+                if len(agg_list) == 1:
+                    agg_name = agg_list[0]
+                    self.value_columns.append(col)
+                    self.value_names.append(col_name)
+                else:
+                    self.value_columns.extend([col]*len(agg_list))
+                    self.value_names.extend([col_name]*len(agg_list))
+
+    def construct_result(self, out_key_columns, out_value_columns):
+        if not self.as_index:
+            result = cudf.concat(
+                [
+                    dataframe_from_columns(out_key_columns, columns=self.key_names),
+                    dataframe_from_columns(out_value_columns, columns=self.value_names)
+                ],
+                axis=1
+            )
+            return result
+
+        result = dataframe_from_columns(
+                out_value_columns,
+                columns=self.compute_result_column_index()
+            )
+        result.index = self.compute_result_index(out_key_columns, out_value_columns)
+
+        if isinstance(self.obj, cudf.Series):
+            # May need to downcast from DataFrame to Series:
+            if len(self.aggs[None]) == 1:
+                result = result[result.columns[0]]
+                result.name = self.value_names[0]
+
+        return result
+
+
+    def compute_result_index(self, key_columns, value_columns):
+        """
+        Computes the index of the result
+        """
+        key_names = self.key_names
+        if (len(key_columns)) == 1:
+            return cudf.dataframe.index.as_index(key_columns[0],
+                                                 name=key_names[0])
+        else:
+            empty_keys = all([len(x)==0 for x in key_columns])
+            if len(value_columns) == 0  and empty_keys:
+                return cudf.dataframe.index.GenericIndex(cudf.Series([], dtype='object'))
+            return MultiIndex(source_data=dataframe_from_columns(key_columns,
+                                                                 columns=key_names))
+
+    def compute_result_column_index(self):
+        """
+        Computes the column index of the result
+        """
+        value_names = self.value_names
+        aggs_as_list = self.get_aggs_as_list()
+
+        if isinstance(self.obj, cudf.Series):
+            if len(aggs_as_list) == 1:
+                return self.obj.name
+            else:
+                return aggs_as_list
+        else:
+            if len(aggs_as_list) == len(self.aggs):
+                return value_names
+            else:
+                return MultiIndex.from_tuples(zip(value_names, aggs_as_list))
+
+    def get_aggs_as_list(self):
+        """
+        Returns self.aggs as a list of aggs
+        """
+        aggs_as_list = list(itertools.chain.from_iterable(self.aggs.values()))
+        return aggs_as_list
 
 
 def _groupby_engine(key_columns, value_columns, aggs, sort):
@@ -404,4 +360,3 @@ def _add_prefixes(names, prefixes):
         for i, (prefix, col_name) in enumerate(zip(prefixes, names)):
             prefixed_names[i] = f"{prefix}_{col_name}"
     return prefixed_names
-
