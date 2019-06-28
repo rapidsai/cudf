6d202c66657e3fafc8f28515e451af1388f89499
Mark Harris <mharris@nvidia.com>
2019-06-27 23:41:26 -0700

    Added cudf::benchmark fixture and improved apply_boolean_mask_benchmark

--- cpp/benchmarks/stream_compaction/apply_boolean_mask_benchmark.cu
+++ cpp/benchmarks/stream_compaction/apply_boolean_mask_benchmark.cu
@@ -17,9 +17,11 @@
 #include <cudf/stream_compaction.hpp>
 #include <cudf/table.hpp>
 #include <tests/utilities/column_wrapper.cuh>
+#include <fixture/benchmark_fixtures.hpp>
 
 #include <benchmark/benchmark.h>
 #include <random>
+#include <chrono>
 
 template <typename T>
 T random_int(T min, T max)
@@ -31,12 +33,20 @@ T random_int(T min, T max)
   return uniform(engine);
 }
 
-template<class TypeParam, int num_columns, int column_size>
-void BM_apply_boolean_mask(benchmark::State& state) {
+template <class T>
+class ApplyBooleanMask : public cudf::benchmark {
+public:
+  using TypeParam = T;
+};
+
+
+template <class TypeParam>
+void BM_apply_boolean_mask(benchmark::State& state, gdf_size_type num_columns) {
   using wrapper = cudf::test::column_wrapper<TypeParam>;
   using mask_wrapper = cudf::test::column_wrapper<cudf::bool8>;
 
-  const gdf_size_type percent_true{static_cast<gdf_size_type>(state.range(0))}; 
+  const gdf_size_type column_size{static_cast<gdf_size_type>(state.range(0))}; 
+  const gdf_size_type percent_true{static_cast<gdf_size_type>(state.range(1))}; 
 
   std::vector<cudf::test::column_wrapper<TypeParam> > columns;
 
@@ -58,21 +68,79 @@ void BM_apply_boolean_mask(benchmark::State& state) {
                  [](wrapper &c) { return c.get(); });  
 
   cudf::table source_table{raw_columns.data(), num_columns};
-    
+
   for(auto _ : state){
     cudf::table result = cudf::apply_boolean_mask(source_table, *mask.get());
-    //TODO: cudf::table::destroy(result);
+    result.destroy();
   }
+
+  float fraction = percent_true / 100.f;
+  gdf_size_type column_size_out = fraction * column_size;
+  int64_t mask_size = sizeof(cudf::bool8) * column_size + gdf_valid_allocation_size(column_size);
+  int64_t validity_bytes_in  = (fraction >= 1.0f/32) ? 
+    gdf_valid_allocation_size(column_size) :
+    4 * column_size_out;
+  int64_t validity_bytes_out = gdf_valid_allocation_size(column_size_out);
+  int64_t column_bytes_out = sizeof(TypeParam) * column_size_out;
+  int64_t column_bytes_in = column_bytes_out;
+
+  int64_t bytes_read = (column_bytes_in + validity_bytes_in) * num_columns + // reading columns
+    mask_size; // reading boolean mask
+  int64_t bytes_written = (column_bytes_out + validity_bytes_out) * num_columns; // writing columns
+
+  state.SetItemsProcessed(state.iterations() * column_size * num_columns);
+  state.SetBytesProcessed(static_cast<int64_t>(state.iterations()) * 
+                          bytes_read + bytes_written);
 }
+
+
+
+constexpr gdf_size_type hundredM = 1e8;
+constexpr gdf_size_type tenM = 1e7;
+constexpr gdf_size_type tenK = 1e4;
 
 static void percent_range(benchmark::internal::Benchmark* b) {
-//  for (int num_columns = 1; num_columns <= 4; ++num_columns)
-//    for (int column_size = 1e5; column_size <= 1e8; column_size *= 10)
-      for (int percent = 0; percent <= 100; percent += 10)
-        b->Args({percent});
+  b->Unit(benchmark::kMillisecond);
+  for (int percent = 0; percent <= 100; percent += 10)
+    b->Args({hundredM, percent});
 }
 
-BENCHMARK_TEMPLATE(BM_apply_boolean_mask, int8_t,  1, 1000000)->Apply(percent_range);
-BENCHMARK_TEMPLATE(BM_apply_boolean_mask, int32_t, 1, 1000000)->Apply(percent_range);
-BENCHMARK_TEMPLATE(BM_apply_boolean_mask, float,   1, 1000000)->Apply(percent_range);
-BENCHMARK_TEMPLATE(BM_apply_boolean_mask, double,  1, 1000000)->Apply(percent_range);
+constexpr gdf_size_type fifty_percent = 50;
+
+static void size_range(benchmark::internal::Benchmark* b) {
+  b->Unit(benchmark::kMillisecond);
+  for (int size = tenK; size <= hundredM; size *= 10)
+    b->Args({size, fifty_percent});
+}
+
+#define ABM_BENCHMARK_DEFINE(name, type, n_columns)       \
+BENCHMARK_TEMPLATE_DEFINE_F(ApplyBooleanMask, name, type) \
+(::benchmark::State& st) {                                \
+  BM_apply_boolean_mask<TypeParam>(st, n_columns);        \
+}
+
+ABM_BENCHMARK_DEFINE(float_1_col, float, 1);
+ABM_BENCHMARK_DEFINE(float_2_col, float, 2);
+ABM_BENCHMARK_DEFINE(float_4_col, float, 4);
+
+// shmoo 1, 2, 4 column float across percentage true
+BENCHMARK_REGISTER_F(ApplyBooleanMask, float_1_col)->Apply(percent_range);
+BENCHMARK_REGISTER_F(ApplyBooleanMask, float_2_col)->Apply(percent_range);
+BENCHMARK_REGISTER_F(ApplyBooleanMask, float_4_col)->Apply(percent_range);
+
+// shmoo 1, 2, 4 column float across column sizes with 50% true
+BENCHMARK_REGISTER_F(ApplyBooleanMask, float_1_col)->Apply(size_range);
+BENCHMARK_REGISTER_F(ApplyBooleanMask, float_2_col)->Apply(size_range);
+BENCHMARK_REGISTER_F(ApplyBooleanMask, float_4_col)->Apply(size_range);
+
+// spot benchmark other types
+ABM_BENCHMARK_DEFINE(int8_1_col,   int8_t,  1);
+ABM_BENCHMARK_DEFINE(int16_1_col,  int16_t, 1);
+ABM_BENCHMARK_DEFINE(int32_1_col,  int32_t, 1);
+ABM_BENCHMARK_DEFINE(int64_1_col,  int64_t, 1);
+ABM_BENCHMARK_DEFINE(double_1_col, double,  1);
+BENCHMARK_REGISTER_F(ApplyBooleanMask, int8_1_col  )->Args({tenM, fifty_percent});
+BENCHMARK_REGISTER_F(ApplyBooleanMask, int16_1_col )->Args({tenM, fifty_percent});
+BENCHMARK_REGISTER_F(ApplyBooleanMask, int32_1_col )->Args({tenM, fifty_percent});
+BENCHMARK_REGISTER_F(ApplyBooleanMask, int64_1_col )->Args({tenM, fifty_percent});
+BENCHMARK_REGISTER_F(ApplyBooleanMask, double_1_col)->Args({tenM, fifty_percent});
