67ca3d17ddc67def6fdfe967fe20293b24d242a3
ptaylor <paul.e.taylor@me.com>
2019-07-05 13:56:47 -0700

    use Column.unordered_compare instead of Series.equals()

--- python/cudf/cudf/dataframe/column.py
+++ python/cudf/cudf/dataframe/column.py
@@ -168,8 +168,7 @@ class Column(object):
             if isinstance(val, np.ndarray):
                 return val.all()
             return bool(val)
-        from cudf.dataframe.series import Series
-        return Series(self).equals(other)
+        return self.unordered_compare('eq', other).min()
 
     def _update_null_count(self, null_count=None):
         assert null_count is None or null_count >= 0
