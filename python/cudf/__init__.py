fd35e45798692959c39cd1021e2682d327c31b2b
OlivierNV <45697062+OlivierNV@users.noreply.github.com>
2019-07-08 12:07:59 -0700

    Temporary revert of __init__.py
    Reverting to allow auto-merge

--- python/cudf/__init__.py
+++ python/cudf/__init__.py
@@ -6,8 +6,8 @@ from cudf.dataframe import DataFrame, from_pandas, merge
 from cudf.dataframe import Index, MultiIndex
 from cudf.dataframe import Series
 from cudf.multi import concat
-from cudf.io import (read_avro, read_csv, read_feather, read_hdf, read_json,
-                     read_orc, read_parquet, from_dlpack)
+from cudf.io import (read_csv, read_parquet, read_feather, read_json,
+                     read_hdf, read_orc, from_dlpack)
 from cudf.settings import set_options
 from cudf.reshape import melt
 from cudf.ops import (sqrt, sin, cos, tan, arcsin, arccos, arctan, exp, log,
