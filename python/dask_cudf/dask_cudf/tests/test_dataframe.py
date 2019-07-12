ffc79313d787161f4a5af7977b1fd30780498197
galipremsagar <sagarprem75@gmail.com>
2019-07-12 19:46:26 +0000

    remove raises

--- python/dask_cudf/dask_cudf/tests/test_dataframe.py
+++ python/dask_cudf/dask_cudf/tests/test_dataframe.py
@@ -6,6 +6,7 @@ import dask_cudf
 
 import pytest
 
+
 def test_get_dummies(data):
     df = pd.DataFrame({"A": ["a", "b", "c", "a", "z"], "C": [1, 2, 3, 4, 5]})
     df["B"] = df["A"].astype('category')
@@ -31,11 +32,11 @@ def test_get_dummies(data):
     df = pd.DataFrame({"C": pd.Series([1, 2, 3, 4, 5])})
     ddf = dd.from_pandas(df, npartitions=10)
     dd.assert_eq(dd.get_dummies(ddf).compute(), pd.get_dummies(df))
-    with pytest.raises(NotImplementedError) as raises:
+    with pytest.raises(NotImplementedError):
         dd.get_dummies(ddf, columns=['C']).compute()
     gdf = cudf.from_pandas(df)
     gddf = dask_cudf.from_cudf(gdf, npartitions=10)
-    with pytest.raises(NotImplementedError) as raises:
+    with pytest.raises(NotImplementedError):
         dd.get_dummies(gddf, columns=['C']).compute()
     dd.assert_eq(dd.get_dummies(ddf).compute(), dd.get_dummies(gddf),
                  check_dtype=False)
