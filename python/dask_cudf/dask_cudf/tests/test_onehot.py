ac3084b2a4d8176f849c074e50a80f8b0a158ad9
galipremsagar <sagarprem75@gmail.com>
2019-07-15 16:23:12 +0000

    fix flake8

--- python/dask_cudf/dask_cudf/tests/test_onehot.py
+++ python/dask_cudf/dask_cudf/tests/test_onehot.py
@@ -15,6 +15,7 @@ def test_get_dummies_cat():
     gddf = dask_cudf.from_cudf(gdf, npartitions=10)
     dd.assert_eq(dd.get_dummies(ddf).compute(), dd.get_dummies(gddf).compute(),
                  check_dtype=False)
+
     df = pd.DataFrame({"A": ["a", "b", "c", "a", "z"], "C": [1, 2, 3, 4, 5]})
     df["B"] = df["A"].astype('category')
     df["A"] = df["A"].astype('category')
@@ -24,6 +25,7 @@ def test_get_dummies_cat():
     gddf = dask_cudf.from_cudf(gdf, npartitions=10)
     dd.assert_eq(dd.get_dummies(ddf).compute(), dd.get_dummies(gddf).compute(),
                  check_dtype=False)
+
     df = pd.DataFrame({"A": ["a", "b", "c", "a", "z"],
                        "C": pd.Series([1, 2, 3, 4, 5], dtype='category')})
     df["B"] = df["A"].astype('category')
@@ -34,6 +36,7 @@ def test_get_dummies_cat():
     gddf = dask_cudf.from_cudf(gdf, npartitions=10)
     dd.assert_eq(dd.get_dummies(ddf).compute(), dd.get_dummies(gddf).compute(),
                  check_dtype=False)
+
 
 def test_get_dummies_non_cat():
     df = pd.DataFrame({"C": pd.Series([1, 2, 3, 4, 5])})
@@ -48,6 +51,7 @@ def test_get_dummies_non_cat():
     dd.assert_eq(dd.get_dummies(ddf).compute(), dd.get_dummies(gddf).compute(),
                  check_dtype=False)
 
+
 def test_get_dummies_cat_index():
     df = pd.DataFrame({"C": pd.CategoricalIndex([1, 2, 3, 4, 5])})
     ddf = dd.from_pandas(df, npartitions=10)
@@ -59,17 +63,13 @@ def test_get_dummies_cat_index():
 
 
 def test_get_dummies_large():
-    from dask_cuda import LocalCUDACluster
-    from dask.distributed import Client
-    cluster = LocalCUDACluster()
-    client = Client(cluster)
     gdf = cudf.datasets.randomdata(nrows=200000, dtypes={"C": int,
-                                                             "first": 'category',
-                                                             "b": float,
-                                                             "second": 'category'})
+                                                         "first": 'category',
+                                                         "b": float,
+                                                         "second": 'category'})
     df = gdf.to_pandas()
     ddf = dd.from_pandas(df, npartitions=25)
     dd.assert_eq(dd.get_dummies(ddf).compute(), pd.get_dummies(df))
     gddf = dask_cudf.from_cudf(gdf, npartitions=25)
     dd.assert_eq(dd.get_dummies(ddf).compute(), dd.get_dummies(gddf).compute(),
-                     check_dtype=False)
+                 check_dtype=False)
