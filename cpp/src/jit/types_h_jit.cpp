df10982bee1dde7d02a4bbfd57929c19271f5807
Jiqun Tu <jtu@nvidia.com>
2019-07-17 16:27:40 -0700

    fix the extern problem.

--- cpp/src/jit/types_h_jit.cpp
+++ cpp/src/jit/types_h_jit.cpp
@@ -1,5 +1,4 @@
 // From Nik's answer in
 // https://stackoverflow.com/questions/10422034/when-to-use-extern-in-c/10422050#10422050
 // In C++ the extern specifier has to be applied to the variable definition as well.
-extern 
 #include <types.h.jit>
