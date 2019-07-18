0dfe83c917b657622804f1badf260603db9aee01
Keith Kraus <kkraus@nvidia.com>
2019-07-18 11:47:07 -0700

    cython flake8 style fixes

--- python/cudf/cudf/bindings/typecast.pyx
+++ python/cudf/cudf/bindings/typecast.pyx
@@ -18,18 +18,18 @@ def apply_cast(incol, outcol):
 
     check_gdf_compatibility(incol)
     check_gdf_compatibility(outcol)
-    
+
     cdef gdf_column* c_incol = column_view_from_column(incol)
     cdef gdf_column* c_outcol = column_view_from_column(outcol)
 
     cdef gdf_error result
-    with nogil:    
+    with nogil:
         result = gdf_cast(
             <gdf_column*>c_incol,
-            <gdf_column*>c_outcol)
-    
+            <gdf_column*>c_outcol
+        )
+
     free(c_incol)
     free(c_outcol)
 
     check_gdf_error(result)
-
