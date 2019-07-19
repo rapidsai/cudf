7c71474ad80743b16e5f237ecafb4032a61d52e0
Christopher Harris <charris@nvidia.com>
2019-07-19 01:26:04 -0500

    fix(code formatting)

--- python/cudf/cudf/tests/test_apply_rows.py
+++ python/cudf/cudf/tests/test_apply_rows.py
@@ -10,13 +10,10 @@ def _kernel_multiply(a, b, out):
 
 
 def _assert_multiplication(a, b, out):
-    df_original = cudf.DataFrame({'a': a, 'b': b})
-    df_expected = cudf.DataFrame({'a': a, 'b': b, 'out': out})
+    df_original = cudf.DataFrame({"a": a, "b": b})
+    df_expected = cudf.DataFrame({"a": a, "b": b, "out": out})
     df_actual = df_original.apply_rows(
-        _kernel_multiply,
-        ['a', 'b'],
-        {'out': float},
-        {}
+        _kernel_multiply, ["a", "b"], {"out": float}, {}
     )
 
     assert_eq(df_expected, df_actual)
@@ -26,7 +23,7 @@ def test_dataframe_apply_rows_with_null():
     _assert_multiplication(
         [3.0, None, 9.0, None, 4.0],
         [3.0, 7.0, None, None, 2.0],
-        [9.0, None, None, None, 8.0]
+        [9.0, None, None, None, 8.0],
     )
 
 
@@ -34,21 +31,13 @@ def test_dataframe_apply_rows_with_nan():
     _assert_multiplication(
         [3.0, np.nan, 5.0, np.nan, 4.0],
         [3.0, 9.0, np.nan, np.nan, 2.0],
-        [9.0, np.nan, np.nan, np.nan, 8.0]
+        [9.0, np.nan, np.nan, np.nan, 8.0],
     )
 
 
 def test_dataframe_apply_rows_empty():
-    _assert_multiplication(
-        [],
-        [],
-        []
-    )
+    _assert_multiplication([], [], [])
 
 
 def test_dataframe_apply_rows_only_none():
-    _assert_multiplication(
-        [None],
-        [None],
-        [None]
-    )
+    _assert_multiplication([None], [None], [None])
