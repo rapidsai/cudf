cfe60711c637a98c7fca121bd653dfd114ad3bc0
rgsl888prabhu <ramakrishnap@nvidia.com>
2019-07-24 16:35:53 -0500

    cython CI issues fixed

--- python/cudf/cudf/bindings/issorted.pyx
+++ python/cudf/cudf/bindings/issorted.pyx
@@ -37,7 +37,7 @@ cpdef issorted(columns, descending, nulls_are_smallest=False):
     cdef bool c_result = False
     with nogil:
         c_result = is_sorted(c_values_table[0],
-                  c_descending_vector, c_nulls_are_smallest)
+                             c_descending_vector, c_nulls_are_smallest)
 
     del c_values_table
 
