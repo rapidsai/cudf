9182d76d773b60c436d965bfcb543d7113ec1d64
galipremsagar <sagarprem75@gmail.com>
2019-07-30 22:55:55 +0000

    refactor

--- python/cudf/cudf/utils/utils.py
+++ python/cudf/cudf/utils/utils.py
@@ -79,11 +79,10 @@ def scalar_broadcast_to(scalar, shape, dtype):
     if np.dtype(dtype) == np.dtype("object"):
         import nvstrings
         from cudf.dataframe.string import StringColumn
-
-        scalar_str_col = StringColumn(nvstrings.to_device([scalar]))
         from cudf.utils.cudautils import zeros
 
         gather_map = zeros(shape[0], dtype="int32")
+        scalar_str_col = StringColumn(nvstrings.to_device([scalar]))
         return scalar_str_col[gather_map]
     else:
         da = rmm.device_array(shape, dtype=dtype)
