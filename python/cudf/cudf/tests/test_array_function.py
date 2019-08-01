f17a961d3456b726cf3290c62d814fcc33ac6ad5
vibhujawa <vibhujawa@gmail.com>
2019-07-31 18:43:37 -0700

    added array function for index

--- python/cudf/cudf/tests/test_array_function.py
+++ python/cudf/cudf/tests/test_array_function.py
@@ -86,3 +86,53 @@ def test_array_func_missing_cudf_dataframe(pd_df, func):
     cudf_df = cudf.from_pandas(pd_df)
     with pytest.raises(TypeError):
         func(cudf_df)
+
+# we only impliment sum among all non numpy ufuncs
+@pytest.mark.skipif(missing_arrfunc_cond, reason=missing_arrfunc_reason)
+@pytest.mark.parametrize("np_ar", [np.random.random(100)])
+@pytest.mark.parametrize("func", [lambda x: np.sum(x)])
+def test_array_func_cudf_index(np_ar, func):
+    cudf_index = cudf.dataframe.index.as_index(cudf.Series(np_ar))
+    expect = func(np_ar)
+    got = func(cudf_index)
+
+    if np.isscalar(expect):
+        np.testing.assert_approx_equal(expect, got)
+    else:
+        np.testing.assert_array_equal(expect, got.to_pandas().values)
+
+
+@pytest.mark.skipif(missing_arrfunc_cond, reason=missing_arrfunc_reason)
+@pytest.mark.parametrize("np_ar", [np.random.random(100)])
+@pytest.mark.parametrize(
+    "func",
+    [
+        lambda x: np.cov(x, x),
+        lambda x: np.dot(x, x),
+        lambda x: np.linalg.norm(x),
+        lambda x: np.linalg.det(x),
+    ],
+)
+def test_array_func_missing_cudf_index(np_ar, func):
+    cudf_index = cudf.dataframe.index.as_index(cudf.Series(np_ar))
+    with pytest.raises(TypeError):
+        func(cudf_index)
+
+
+@pytest.mark.skipif(missing_arrfunc_cond, reason=missing_arrfunc_reason)
+@pytest.mark.parametrize(
+    "func",
+    [
+        lambda x: np.cov(x, x),
+        lambda x: np.dot(x, x),
+        lambda x: np.linalg.norm(x),
+        lambda x: np.linalg.det(x),
+    ],
+)
+def test_array_func_missing_cudf_multi_index(func):
+    levels = [["a", "b"], ["c", "d"]]
+    codes = [[0, 1], [1, 0]]
+
+    cudf_multi_index = cudf.MultiIndex(levels, codes)
+    with pytest.raises(TypeError):
+        func(cudf_multi_index)
