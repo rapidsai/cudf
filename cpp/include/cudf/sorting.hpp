ffb2726f44687c2d1981ad74cb6721f46b71ae05
Jake Hemstad <jhemstad@nvidia.com>
2019-08-01 13:34:58 -0500

    Added stub for new sorted_order.

---
+++ cpp/include/cudf/sorting.hpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cudf/types.hpp>
+
+#include <memory>
+#include <vector>
+
+namespace cudf {
+namespace exp {
+
+/**---------------------------------------------------------------------------*
+ * @brief Indicates the order in which a column should be sorted.
+ *---------------------------------------------------------------------------**/
+enum class order : bool { ASCENDING, DESCENDING };
+
+/**---------------------------------------------------------------------------*
+ * @brief Indicates how null values compare against all other values.
+ *
+ * If `HIGHEST`, then `null > x` for all `x`
+ *
+ * If `LOWEST`, then `null < x` for all `x`
+ *---------------------------------------------------------------------------**/
+enum class null_size : bool { HIGHEST, LOWEST };
+
+/**---------------------------------------------------------------------------*
+ * @brief Computes the row indices that would produce `input`  in a
+ * lexicographical sorted order.
+ *
+ * @param input The table to sort
+ * @param column_order The desired sort order for each column
+ * @param size_of_nulls The size of a NULL value in comparison to all other
+ * values
+ * @return std::unique_ptr<column> A non-nullable column of INT32 elements
+ * containing the permuted row indices of `input` if it were sorted
+ *---------------------------------------------------------------------------**/
+std::unique_ptr<column> sorted_order(table_view input,
+                                     std::vector<order> const& column_order,
+                                     null_size size_of_nulls);
+}  // namespace exp
+}  // namespace cudf
---
+++ cpp/src/sort/sort.cu
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cudf/column/column.hpp>
+#include <cudf/column/column_factories.hpp>
+#include <cudf/sorting.hpp>
+#include <cudf/table/table_view.hpp>
+
+namespace cudf {
+namespace exp {
+
+// Create permuted row indices if the input were sorted
+std::unique_ptr<column> sorted_order(table_view input,
+                                     std::vector<order> const& column_order,
+                                     null_size size_of_nulls) {
+  auto sorted_indices =
+      cudf::make_numeric_column(data_type{INT32}, input.num_rows());
+  return sorted_indices;
+}
+}  // namespace exp
+}  // namespace cudf
