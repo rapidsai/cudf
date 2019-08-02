464172bcc188655b8468c43a358775d8d28ce0df
Ashwin Srinath <shwina@users.noreply.github.com>
2019-08-02 11:56:26 -0400

    flake8

--- python/cudf/cudf/dataframe/buffer.py
+++ python/cudf/cudf/dataframe/buffer.py
@@ -126,10 +126,10 @@ class Buffer(object):
         self.extend(np.asarray(element, dtype=self.dtype))
 
     def extend(self, array):
+        from cudf.dataframe import columnops
+
         needed = array.size
         self._sentry_capacity(needed)
-        import cudf.bindings.typecast as typecast
-        from cudf.dataframe import columnops
 
         array = columnops.as_column(array).astype(self.dtype).data.mem
 
