946a3c1c70fdbd341f40052224461b9e2274083f
Ashwin Srinath <shwina@users.noreply.github.com>
2019-08-08 19:08:56 -0400

    Avoid typecast when casting to the same dtype

--- python/cudf/cudf/bindings/typecast.pyx
+++ python/cudf/cudf/bindings/typecast.pyx
@@ -12,27 +12,27 @@ from cudf.dataframe.column import Column
 from libc.stdlib cimport free
 
 import numpy as np
-
+import pandas as pd
 
 def apply_cast(incol, dtype=np.float64):
     """
     Return a Column with values in `incol` casted to `dtype`.
     Currently supports numeric and datetime dtypes.
     """
+    dtype = np.dtype(np.float64 if dtype is None else dtype)
 
     check_gdf_compatibility(incol)
 
-    cdef gdf_column* c_incol = column_view_from_column(incol)
+    if pd.api.types.is_dtype_equal(incol.dtype, dtype):
+        return incol
 
-    dtype = np.dtype(np.float64 if dtype is None else dtype)
+    cdef gdf_column* c_incol = column_view_from_column(incol)
     cdef gdf_dtype c_out_dtype = gdf_dtype_from_value(incol, dtype)
     cdef uintptr_t c_category
-
     cdef gdf_dtype_extra_info c_out_info = gdf_dtype_extra_info(
         time_unit=np_dtype_to_gdf_time_unit(dtype),
         category=<void*>c_category
     )
-
     cdef gdf_column result
 
     with nogil:
