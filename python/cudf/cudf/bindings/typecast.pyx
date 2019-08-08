428d909118b9cabeb452fcfac6ff6dace27246e0
Ashwin Srinath <shwina@users.noreply.github.com>
2019-08-08 19:12:39 -0400

    flake8

--- python/cudf/cudf/bindings/typecast.pyx
+++ python/cudf/cudf/bindings/typecast.pyx
@@ -14,6 +14,7 @@ from libc.stdlib cimport free
 import numpy as np
 import pandas as pd
 
+
 def apply_cast(incol, dtype=np.float64):
     """
     Return a Column with values in `incol` casted to `dtype`.
