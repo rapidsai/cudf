89fcad989f0a5bedf50d7da11691bef4473081d9
rgsl888prabhu <ramakrishnap@nvidia.com>
2019-08-19 17:44:06 -0500

    changes with find_first_value

--- python/cudf/cudf/dataframe/numerical.py
+++ python/cudf/cudf/dataframe/numerical.py
@@ -77,12 +77,14 @@ class NumericalColumn(columnops.TypedColumnBase):
         if isinstance(rhs, NumericalColumn) or np.isscalar(rhs):
             out_dtype = np.result_type(self.dtype, rhs.dtype)
             if binop in ["mod", "floordiv"]:
-                if (
-                    (
-                        isinstance(tmp, NumericalColumn)
-                        and (tmp.sort_by_values()[0].searchsorted(0))
-                    )
-                    or (np.isscalar(tmp) and (0 == tmp))
+                found_zero_in_column = False
+                if (isinstance(tmp, NumericalColumn)):
+                    try:
+                        if (tmp.find_first_value(0.0)):
+                            found_zero_in_column = True
+                    except ValueError:
+                        """This means value not found"""
+                if ((found_zero_in_column) or (np.isscalar(tmp) and (0 == tmp))
                 ) and (tmp.dtype in int_dtypes):
                     out_dtype = np.dtype("float_")
             return numeric_column_binop(
