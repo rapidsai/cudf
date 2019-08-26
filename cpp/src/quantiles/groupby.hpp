1d7016619280a7316e2c8f6c02c6020a3b3346ad
Devavret Makkar <dmakkar@nvidia.com>
2019-08-27 04:16:55 +0530

    Docs and cleanup of groupby object

--- cpp/src/quantiles/groupby.hpp
+++ cpp/src/quantiles/groupby.hpp
@@ -27,21 +27,6 @@ namespace cudf {
 
 namespace detail {
 
-template <typename T>
-void print(rmm::device_vector<T> const& d_vec, std::string label = "") {
-  thrust::host_vector<T> h_vec = d_vec;
-  printf("%s \t", label.c_str());
-  for (auto &&i : h_vec)  std::cout << i << " ";
-  printf("\n");
-}
-
-template <typename T>
-void print(gdf_column const& col, std::string label = "") {
-  auto col_data = reinterpret_cast<T*>(col.data);
-  auto d_vec = rmm::device_vector<T>(col_data, col_data+col.size);
-  print(d_vec, label);
-}
-
 struct groupby {
   using index_vector = rmm::device_vector<gdf_size_type>;
 
@@ -65,22 +50,73 @@ struct groupby {
     gdf_column_free(&_unsorted_labels);
   }
 
+  /**
+   * @brief Returns a grouped and sorted values column and a count of valid
+   * values within each group
+   * 
+   * Sorts and groups the @p val_col where the groups are dictated by key table
+   * and the elements are sorted ascending within the groups. Calculates the
+   * number of valid values within each group and also returns this
+   * 
+   * @param val_col The value column to group and sort
+   * @return std::pair<gdf_column, rmm::device_vector<gdf_size_type> > 
+   *  The sorted and grouped column, and per group valid count
+   */
   std::pair<gdf_column, rmm::device_vector<gdf_size_type> >
   sort_values(gdf_column const& val_col);
 
+  /**
+   * @brief Returns a table of sorted unique keys
+   * 
+   * The result contains a new table where each row is a unique row in the
+   * sorted key table
+   */
   cudf::table unique_keys();
 
+  /**
+   * @brief Returns the number of groups in the key table
+   * 
+   */
   gdf_size_type num_groups() { return _group_ids.size(); }
 
+  /**
+   * @brief Returns a device vector of group indices
+   * 
+   */
   index_vector& group_indices() { return _group_ids; }
 
  private:
+  /**
+   * @brief Set the member _key_sorted_order.
+   * 
+   * This member contains the sort order indices for _key_table. Gathering the
+   * _key_table by _key_sorted_order would produce the sorted key table
+   */
   void set_key_sort_order();
 
+  /**
+   * @brief Set the member _group_ids.
+   * 
+   * _group_ids contains the indices for the starting points of each group in
+   * the sorted key table
+   */
   void set_group_ids();
 
+  /**
+   * @brief Set the member _group_labels
+   * 
+   * _group_labels contains a value for each row in the sorted key column
+   * signifying which group in _group_ids it belongs to
+   */
   void set_group_labels();
 
+  /**
+   * @brief Set the member _unsorted_labels
+   * 
+   * _unsorted_labels contains the group labels but in nthe order of the 
+   * unsorted _key_table so that for each row in _key_table, _unsorted_labels
+   * contains the group it would belong to, after sorting
+   */
   void set_unsorted_labels();
 
  private:
