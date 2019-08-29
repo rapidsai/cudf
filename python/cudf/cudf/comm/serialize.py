3bc3d289beaa30d3b03012fb28b2166744c0b770
Benjamin Zaitlen <quasiben@gmail.com>
2019-08-28 19:51:06 -0700

    only load distributed if it is installed

--- python/cudf/cudf/comm/serialize.py
+++ python/cudf/cudf/comm/serialize.py
@@ -1,29 +1,32 @@
 import pickle
 
-from distributed.protocol.cuda import cuda_deserialize, cuda_serialize
-from distributed.utils import log_errors
-
 import cudf
 import cudf.core.groupby.groupby
 
+try:
+    from distributed.protocol.cuda import cuda_deserialize, cuda_serialize
+    from distributed.utils import log_errors
 
-# all (de-)serializtion code lives in the cudf codebase
-# here we ammend the returned headers with `is_gpu` for
-# UCX buffer consumption
-@cuda_serialize.register(
-    (cudf.DataFrame, cudf.Series, cudf.core.groupby.groupby._Groupby)
-)
-def serialize_cudf_dataframe(x):
-    with log_errors():
-        header, frames = x.serialize()
-        return header, frames
+    # all (de-)serializtion are attached to cudf Objects:
+    # Series/DataFrame/Index/Column/Buffer/etc
+    @cuda_serialize.register(
+        (cudf.DataFrame, cudf.Series, cudf.core.groupby.groupby._Groupby)
+    )
+    def serialize_cudf_dataframe(x):
+        with log_errors():
+            header, frames = x.serialize()
+            return header, frames
+
+    @cuda_deserialize.register(
+        (cudf.DataFrame, cudf.Series, cudf.core.groupby.groupby._Groupby)
+    )
+    def deserialize_cudf_dataframe(header, frames):
+        with log_errors():
+            cudf_typ = pickle.loads(header["type"])
+            cudf_obj = cudf_typ.deserialize(header, frames)
+            return cudf_obj
 
 
-@cuda_deserialize.register(
-    (cudf.DataFrame, cudf.Series, cudf.core.groupby.groupby._Groupby)
-)
-def deserialize_cudf_dataframe(header, frames):
-    with log_errors():
-        cudf_typ = pickle.loads(header["type"])
-        cudf_obj = cudf_typ.deserialize(header, frames)
-        return cudf_obj
+except ImportError:
+    # distributed is probably not installed on the system
+    pass
