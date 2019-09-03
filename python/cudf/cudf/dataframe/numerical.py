f79c2cd01286b9ade27af786305706a166149f7a
rgsl888prabhu <ramakrishnap@nvidia.com>
2019-09-03 10:44:31 -0500

    style changes

--- python/cudf/cudf/dataframe/numerical.py
+++ python/cudf/cudf/dataframe/numerical.py
@@ -61,7 +61,7 @@ class NumericalColumn(columnops.TypedColumnBase):
         if self.data.mem.dtype == np.bool:
             return (
                 cudautils.find_first(
-                    self.data.mem.view('int8'), item.view('int8')
+                    self.data.mem.view("int8"), item.view("int8")
                 )
                 != -1
             )
