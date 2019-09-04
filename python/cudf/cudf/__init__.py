100fb29d13fa571787ba48c433ecfe9f832514b2
Rick Ratzel <rratzel@nvidia.com>
2019-09-04 16:20:33 -0700

    Added a lazy import mechanism and unit tests.

--- python/cudf/cudf/__init__.py
+++ python/cudf/cudf/__init__.py
@@ -1,35 +1,104 @@
+##################################
 # Copyright (c) 2018-2019, NVIDIA CORPORATION.
 
-from librmm_cffi import librmm as rmm
+from importlib import import_module
 
-from cudf import core, datasets
-from cudf._version import get_versions
-from cudf.core import DataFrame, Index, MultiIndex, Series, from_pandas, merge
-from cudf.core.ops import (
-    arccos,
-    arcsin,
-    arctan,
-    cos,
-    exp,
-    log,
-    logical_and,
-    logical_not,
-    logical_or,
-    sin,
-    sqrt,
-    tan,
-)
-from cudf.core.reshape import concat, get_dummies, melt
-from cudf.io import (
-    from_dlpack,
-    read_avro,
-    read_csv,
-    read_feather,
-    read_hdf,
-    read_json,
-    read_orc,
-    read_parquet,
-)
+# Define a mapping between cudf namespace variables and the corresponding
+# package/module they're imported from. Actual imports of names into this
+# namespace occur when the names are accessed (eg. "import cudf; cudf.<name>")
+# or by explicitely importing them (eg. "from cudf import <name>"). Calls to
+# "import cudf" will not incur an import cost for variables that are never used.
+#
+# The map definition is based on the following examples:
+#
+#    "datasets": (None,)
+#  is treated as:
+#    from cudf import datasets
+#
+#    "DataFrame": ("cudf.core",)
+#  is treated as:
+#    from cudf.core import DataFrame
+#
+#    "rmm": ("librmm_cffi", "librmm")
+#  is treated as:
+#    from librmm_cffi import librmm as rmm
+#
+__namespaceVarModMap = {
+    "rmm": ("librmm_cffi", "librmm"),
 
-__version__ = get_versions()["version"]
-del get_versions
+    "core": (None,),
+    "datasets": (None,),
+
+    "DataFrame": ("cudf.core",),
+    "Index": ("cudf.core",),
+    "MultiIndex": ("cudf.core",),
+    "Series": ("cudf.core",),
+    "from_pandas": ("cudf.core",),
+    "merge": ("cudf.core",),
+
+    "arccos": ("cudf.core.ops",),
+    "arcsin": ("cudf.core.ops",),
+    "arctan": ("cudf.core.ops",),
+    "cos": ("cudf.core.ops",),
+    "exp": ("cudf.core.ops",),
+    "log": ("cudf.core.ops",),
+    "logical_and": ("cudf.core.ops",),
+    "logical_not": ("cudf.core.ops",),
+    "logical_or": ("cudf.core.ops",),
+    "sin": ("cudf.core.ops",),
+    "sqrt": ("cudf.core.ops",),
+    "tan": ("cudf.core.ops",),
+
+    "concat": ("cudf.core.reshape",),
+    "get_dummies": ("cudf.core.reshape",),
+    "melt": ("cudf.core.reshape",),
+
+    "from_dlpack": ("cudf.io",),
+    "read_avro": ("cudf.io",),
+    "read_csv": ("cudf.io",),
+    "read_feather": ("cudf.io",),
+    "read_hdf": ("cudf.io",),
+    "read_json": ("cudf.io",),
+    "read_orc": ("cudf.io",),
+    "read_parquet": ("cudf.io",),
+}
+
+# Note: dunder vars such as __version__ are not part of __all__
+__all__ = list(__namespaceVarModMap.keys())
+__locals = locals()
+
+# Unconditionally create a CUDA context at import-time
+import numba.cuda
+numba.cuda.current_context()
+
+def __getattr__(var):
+    """
+    Return the value of var, updating the the local namespace as a side effect.
+    This is only called if 'var' is not defined in the local namespace.
+    """
+    varInfo = __namespaceVarModMap.get(var)
+    val = None
+
+    if varInfo:
+        if varInfo[0] is None:
+            val = import_module(".%s" % var, package="cudf")
+        else:
+            mod = import_module(varInfo[0])
+            if len(varInfo) == 2:
+                val = getattr(mod, varInfo[1])
+            else:
+                val = getattr(mod, var)
+
+    # special case: compute __version__
+    elif var == "__version__":
+        from cudf._version import get_versions
+        val = get_versions()["version"]
+
+    else:
+        raise AttributeError("module: 'cudf' has no attribute '%s'" % var)
+
+    # Save the value in the namespace under the var name.
+    # __getattr__() will not be called for this var again.
+    __locals[var] = val
+
+    return val
---
+++ python/cudf/cudf/tests/test_package_imports.py
@@ -0,0 +1,113 @@
+import tempfile
+import importlib
+from os import path
+import sys
+
+
+def setup_module(module):
+    """
+    Retrieve and save all public symbols in the cudf namespace for comparison
+    in tests.
+    """
+    import cudf
+    module.allPossibleCudfNamespaceVars = cudf.__all__
+    del cudf
+    sys.modules.pop("cudf")
+
+
+def teardown_function(function):
+    """
+    "unimport" cudf
+    """
+    globals().pop("cudf", None)
+    sys.modules.pop("cudf", None)
+
+
+def __getVarsLoaded(module):
+    """
+    Return the set of vars loaded in the module, minus "extra" vars used by the
+    lazy-loading mechanism.
+    """
+    extraVars = ["import_module", "numba"]
+    return set([k for k in module.__dict__.keys()
+                if not(k.startswith("__") or (k in extraVars))])
+
+
+########################################
+def test_import_package_module():
+    """
+    Ensure accessing a namespace var that represents a module in the package
+    works. Example: import cudf; cudf.datasets (datasets.py is a mod in the
+    cudf package)
+    """
+    exceptionRaised = None
+    import cudf
+    try:
+        cudf.core
+        cudf.datasets
+    except Exception as e:
+        exceptionRaised = e
+    assert exceptionRaised is None
+
+
+def test_from_import_star():
+    """
+    Ensure a "from cudf import *" does an "actual" import of everything in the
+    cudf namespace.
+    """
+    # "import *" can only be done at the module level, so create a temp .py
+    # file and manually import it.
+    fakeModuleFile = tempfile.NamedTemporaryFile(mode="w+", suffix=".py")
+    fakeModuleFile.write("from cudf import *\n")
+    fakeModuleFile.file.flush()
+    spec = importlib.util.spec_from_file_location(
+        path.splitext(path.basename(fakeModuleFile.name))[0],
+        fakeModuleFile.name)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    varsLoaded = __getVarsLoaded(module)
+    assert varsLoaded == set(allPossibleCudfNamespaceVars)
+
+
+def test_from_import_names():
+    """
+    Ensure "from cudf import <name>" works
+    """
+    from cudf import DataFrame, from_dlpack, arccos
+    import cudf
+
+    # Only names explicitly imported above should be present
+    varsLoaded = __getVarsLoaded(cudf)
+    assert varsLoaded == set(["DataFrame", "from_dlpack", "arccos"])
+
+
+def test_access_names():
+    """
+    Ensure "import cudf;cudf.<name>" works as expected.
+    """
+    import cudf
+    cudf.Index
+    cudf.read_avro
+    cudf.arcsin
+
+    # Only names explicitly imported above should be present
+    varsLoaded = __getVarsLoaded(cudf)
+    assert varsLoaded == set(["Index", "read_avro", "arcsin"])
+
+
+def test_cuda_context_created():
+    """
+    Ensure a CUDA context is created even from a lazy import.
+    NOTE: This test may need to be run separately and with the --noconftest
+    option in order to avoid importing modules that may create a CUDA context
+    (via other tests and/or conftest.py)
+    """
+    import cudf
+    import numba
+    try :
+        numba.cuda.current_context()
+    # the "cuda" attr of the numba module will not exist if a CUDA context was
+    # not created
+    except AttributeError:
+        assert False, "CUDA context not created"
