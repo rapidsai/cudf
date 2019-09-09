10ccfa0cb6a5e2d4a7ce04d86534c32e1eef8ef4
Jake Hemstad <jhemstad@nvidia.com>
2019-09-09 11:45:38 -0500

    Added tests for numeric and relationally_comparable traits

--- cpp/tests/types/traits_test.cpp
+++ cpp/tests/types/traits_test.cpp
@@ -19,6 +19,73 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-class TraitsTest : public ::testing::Test {
+#include <algorithm>
+#include <tuple>
 
-};
+template <typename Tuple, typename F, std::size_t... Indices>
+void tuple_for_each_impl(Tuple&& tuple, F&& f,
+                         std::index_sequence<Indices...>) {
+  (void)std::initializer_list<int>{
+      ((void)(f(std::get<Indices>(std::forward<Tuple>(tuple)))), int{})...};
+}
+
+template <typename F, typename... Args>
+void tuple_for_each(const std::tuple<Args...>& tuple, F&& f) {
+  tuple_for_each_impl(tuple, std::forward<F>(f),
+                      std::index_sequence_for<Args...>{});
+}
+
+class TraitsTest : public ::testing::Test {};
+
+template <typename T>
+class TypedTraitsTest : public TraitsTest {};
+
+using TestTypes =
+    ::testing::Types<int8_t, int16_t, int32_t, int64_t, float, double>;
+
+static constexpr std::array<cudf::data_type, 6> numeric_data_types{
+    cudf::data_type{cudf::INT8},    cudf::data_type{cudf::INT16},
+    cudf::data_type{cudf::INT32},   cudf::data_type{cudf::INT64},
+    cudf::data_type{cudf::FLOAT32}, cudf::data_type{cudf::FLOAT64}};
+
+static constexpr std::array<cudf::data_type, 6> non_numeric_data_types{
+    cudf::data_type{cudf::EMPTY},    cudf::data_type{cudf::BOOL8},
+    cudf::data_type{cudf::DATE32},   cudf::data_type{cudf::TIMESTAMP},
+    cudf::data_type{cudf::CATEGORY}, cudf::data_type{cudf::STRING}};
+
+static_assert(cudf::type_id::NUM_TYPE_IDS ==
+                  (non_numeric_data_types.size() + numeric_data_types.size()),
+              "Mismatch in number of types");
+
+TYPED_TEST_CASE(TypedTraitsTest, TestTypes);
+
+TEST_F(TraitsTest, NumericDataTypesAreNumeric) {
+  EXPECT_TRUE(std::all_of(
+      numeric_data_types.begin(), numeric_data_types.end(),
+      [](cudf::data_type dtype) { return cudf::is_numeric(dtype); }));
+}
+
+/*
+These types are not yet supported by the type dispatcher
+TEST_F(TraitsTest, NonNumericDataTypesAreNotNumeric) {
+  EXPECT_TRUE(std::none_of(
+      non_numeric_data_types.begin(), non_numeric_data_types.end(),
+      [](cudf::data_type dtype) { return cudf::is_numeric(dtype); }));
+}
+*/
+
+TYPED_TEST(TypedTraitsTest, RelationallyComparable) {
+  // All the test types should be comparable with themselves
+  bool comparable = cudf::is_relationally_comparable<TypeParam, TypeParam>();
+  EXPECT_TRUE(comparable);
+}
+
+TYPED_TEST(TypedTraitsTest, NotRelationallyComparable) {
+  // No type should be comparable with an empty dummy type
+  struct foo {};
+  bool comparable = cudf::is_relationally_comparable<foo, TypeParam>();
+  EXPECT_FALSE(comparable);
+
+  comparable = cudf::is_relationally_comparable<TypeParam, foo>();
+  EXPECT_FALSE(comparable);
+}
