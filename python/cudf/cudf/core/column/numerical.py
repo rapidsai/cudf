fe55ee7f734f5822f0fdb08edcd86faffd9b24b2
rgsl888prabhu <ramakrishnap@nvidia.com>
2019-09-11 16:19:32 -0500

    changes

--- python/cudf/cudf/core/column/numerical.py
+++ python/cudf/cudf/core/column/numerical.py
@@ -391,17 +391,19 @@ class NumericalColumn(column.TypedColumnBase):
     @property
     def is_monotonic_increasing(self):
         if not hasattr(self, "_is_monotonic_increasing"):
-            self._is_monotonic_increasing = _numeric_column_binop(
-                self[1:], self[:-1], "ge", "bool"
-            ).all()
+            if self.has_null_mask:
+                self._is_monotonic_increasing = False
+            else:
+                self._is_monotonic_increasing = libcudf.issorted.issorted ([self])
         return self._is_monotonic_increasing
 
     @property
     def is_monotonic_decreasing(self):
         if not hasattr(self, "_is_monotonic_decreasing"):
-            self._is_monotonic_decreasing = _numeric_column_binop(
-                self[1:], self[:-1], "le", "bool"
-            ).all()
+            if self.has_null_mask:
+                self._is_monotonic_decreasing = False
+            else:
+                self._is_monotonic_decreasing = libcudf.issorted.issorted ([self], [1])
         return self._is_monotonic_decreasing
 
 
