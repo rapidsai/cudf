d03d703d2e62301e909506f0ef0246a0dccdb82e
Jake Hemstad <jhemstad@nvidia.com>
2019-09-14 10:09:54 -0500

    Update tests.

--- cpp/tests/experimental/TypeListTests.cpp
+++ cpp/tests/experimental/TypeListTests.cpp
@@ -11,30 +11,34 @@
 #include "GTest.hpp"
 #include "TypeList.hpp"
 
-using namespace util; // this well make reading code way easier
+using namespace util;  // this will make reading code way easier
 
 namespace std {
 template <class T, class U>
 constexpr bool is_same_v = std::is_same<T, U>::value;
 }
 
-namespace{
+namespace {
 // Work around to remove paranthesis surrounding a type
-template<typename T> struct argument_type;
-template<typename T, typename U> struct argument_type<T(U)> { using type = U; };
-}
+template <typename T>
+struct argument_type;
+template <typename T, typename U>
+struct argument_type<T(U)> {
+  using type = U;
+};
+}  // namespace
 /**---------------------------------------------------------------------------*
  * @brief Performs a compile-time check that two types are equivalent.
  *
  * @note In order to work around commas in macros, any type containing commas
  * should be wrapped in paranthesis.
- * 
+ *
  * Example:
  * ```
  * EXPECT_SAME_TYPE(int, int);
- * 
+ *
  * EXPECT_SAME_TYPE(int, float); // compile error
- * 
+ *
  * // Paranthesis around types with commas
  * EXPECT_SAME_TYPE((std::map<int, float), (std::map<int, float>));
  * ```
@@ -77,119 +81,120 @@ TEST(TypeList, Flatten) {
                    (Types<int, double, float>));
 }
 
-// CrossJoin -----------------------------------------------------
-static_assert(std::is_same<CrossJoin<>, Types<>>::value, "");
-static_assert(std::is_same<CrossJoin<Types<>, Types<>>, Types<>>::value, "");
-static_assert(std::is_same<CrossJoin<Types<>, Types<int>>, Types<>>::value, "");
-static_assert(std::is_same<CrossJoin<Types<>, Types<int, double>>, Types<>>::value, "");
+TEST(TypeList, CrossJoin) {
+  EXPECT_SAME_TYPE(CrossJoin<>, Types<>);
+  EXPECT_SAME_TYPE((CrossJoin<Types<>, Types<>>), Types<>);
+  EXPECT_SAME_TYPE((CrossJoin<Types<>, Types<int>>), Types<>);
+  EXPECT_SAME_TYPE((CrossJoin<Types<>, Types<int, double>>), Types<>);
+  EXPECT_SAME_TYPE((CrossJoin<Types<>, Types<int, double>, Types<>>), Types<>);
+  EXPECT_SAME_TYPE((CrossJoin<Types<>, Types<>, Types<>>), Types<>);
+  EXPECT_SAME_TYPE((CrossJoin<Types<int, double>, Types<>, Types<>>), Types<>);
+  EXPECT_SAME_TYPE((CrossJoin<Types<int>, Types<>>), Types<>);
+  EXPECT_SAME_TYPE((CrossJoin<Types<int, double>, Types<>>), Types<>);
+  EXPECT_SAME_TYPE((CrossJoin<Types<int>, Types<int>>),
+                   (Types<Types<int, int>>));
+  EXPECT_SAME_TYPE((CrossJoin<Types<int, double>, Types<int>>),
+                   (Types<Types<int, int>, Types<double, int>>));
+  EXPECT_SAME_TYPE((CrossJoin<Types<int>, Types<double, char>>),
+                   (Types<Types<int, double>, Types<int, char>>));
+  EXPECT_SAME_TYPE((CrossJoin<Types<int, double>, Types<short, char>>),
+                   (Types<Types<int, short>, Types<int, char>,
+                          Types<double, short>, Types<double, char>>));
+  EXPECT_SAME_TYPE((CrossJoin<Types<int, double>, Types<int>, Types<float>>),
+                   (Types<Types<int, int, float>, Types<double, int, float>>));
 
-static_assert(std::is_same<CrossJoin<Types<>, Types<int, double>, Types<>>, Types<>>::value, "");
+  EXPECT_SAME_TYPE(
+      (CrossJoin<Types<int, double>, Types<int>, Types<float, char>>),
+      (Types<Types<int, int, float>, Types<int, int, char>,
+             Types<double, int, float>, Types<double, int, char>>));
+  EXPECT_SAME_TYPE(
+      (CrossJoin<Types<int, double>, Types<int, short>, Types<float, char>>),
+      (Types<Types<int, int, float>, Types<int, int, char>,
+             Types<int, short, float>, Types<int, short, char>,
+             Types<double, int, float>, Types<double, int, char>,
+             Types<double, short, float>, Types<double, short, char>>));
+  EXPECT_SAME_TYPE((CrossJoin<Types<int, float>, int>),
+                   (Types<Types<int, int>, Types<float, int>>));
+  EXPECT_SAME_TYPE((CrossJoin<int, Types<int, float>>),
+                   (Types<Types<int, int>, Types<int, float>>));
+}
 
-static_assert(std::is_same<CrossJoin<Types<>, Types<>, Types<>>, Types<>>::value, "");
+TEST(TypeList, AllSame) {
+  static_assert(AllSame::Call<Types<int, int>>::value, "");
+  static_assert(AllSame::Call<Types<int, int>>::value, "");
+  static_assert(!AllSame::Call<Types<bool, int>>::value, "");
 
-static_assert(std::is_same<CrossJoin<Types<int, double>, Types<>, Types<>>, Types<>>::value, "");
+  static_assert(AllSame::Call<int, int>::value, "");
+  static_assert(!AllSame::Call<int, bool>::value, "");
 
-static_assert(std::is_same<CrossJoin<Types<int>, Types<>>, Types<>>::value, "");
-static_assert(std::is_same<CrossJoin<Types<int, double>, Types<>>, Types<>>::value, "");
+  static_assert(AllSame::Call<int, int, int>::value, "");
+  static_assert(!AllSame::Call<int, float, int>::value, "");
+  static_assert(!AllSame::Call<int, int, float>::value, "");
+}
 
-static_assert(std::is_same<CrossJoin<Types<int>, Types<int>>, Types<Types<int, int>>>::value, "");
+TEST(TypeList, Exists) {
+  static_assert(Exists<int, Types<int, char, float>>, "");
+  static_assert(!Exists<int, Types<double, char, float>>, "");
+  static_assert(!Exists<int, Types<>>, "");
+  static_assert(Exists<int, Types<double, char, float, int>>, "");
+  static_assert(!Exists<int, Types<double>>, "");
+  static_assert(Exists<int, Types<int>>, "");
+}
 
-static_assert(
-    std::is_same<CrossJoin<Types<int, double>, Types<int>>, Types<Types<int, int>, Types<double, int>>>::value, "");
+TEST(TypeList, ContainedIn) {
+  static_assert(
+      ContainedIn<Types<Types<int, char>>>::Call<Types<int, char>>::value, "");
+  static_assert(
+      !ContainedIn<Types<Types<int, char>>>::Call<Types<int, float>>::value,
+      "");
+  static_assert(!ContainedIn<Types<>>::Call<Types<int, float>>::value, "");
+  static_assert(ContainedIn<Types<Types<int, float>, Types<char, char>>>::Call<
+                    Types<int, float>>::value,
+                "");
+  static_assert(!ContainedIn<Types<Types<int, float>, Types<char, char>>>::Call<
+                    Types<int, double>>::value,
+                "");
+  static_assert(
+      ContainedIn<Types<Types<int, float>, Types<>>>::Call<Types<>>::value, "");
+  static_assert(
+      !ContainedIn<Types<Types<int, float>, Types<int>>>::Call<Types<>>::value,
+      "");
+}
 
-static_assert(
-    std::is_same<CrossJoin<Types<int>, Types<double, char>>, Types<Types<int, double>, Types<int, char>>>::value, "");
-static_assert(
-    std::is_same<CrossJoin<Types<int, double>, Types<short, char>>,
-                 Types<Types<int, short>, Types<int, char>, Types<double, short>, Types<double, char>>>::value,
-    "");
+TEST(TypeList, RemoveIf) {
+  EXPECT_SAME_TYPE((RemoveIf<AllSame, Types<>>), Types<>);
 
-static_assert(std::is_same<CrossJoin<Types<int, double>, Types<int>, Types<float>>,
-                           Types<Types<int, int, float>, Types<double, int, float>>>::value,
-              "");
+  EXPECT_SAME_TYPE((RemoveIf<AllSame, Types<Types<int, int, int>>>), Types<>);
 
-static_assert(std::is_same<CrossJoin<Types<int, double>, Types<int>, Types<float, char>>,
-                           Types<Types<int, int, float>, Types<int, int, char>, Types<double, int, float>,
-                                 Types<double, int, char>>>::value,
-              "");
+  EXPECT_SAME_TYPE((RemoveIf<AllSame, Types<Types<int, float, int>>>),
+                   (Types<Types<int, float, int>>));
 
-static_assert(std::is_same<CrossJoin<Types<int, double>, Types<int, short>, Types<float, char>>,
-                           Types<Types<int, int, float>, Types<int, int, char>, Types<int, short, float>,
-                                 Types<int, short, char>, Types<double, int, float>, Types<double, int, char>,
-                                 Types<double, short, float>, Types<double, short, char>>>::value,
-              "");
+  EXPECT_SAME_TYPE(
+      (RemoveIf<AllSame, Types<Types<int, float, char>, Types<int, int, int>,
+                               Types<int, int, char>>>),
+      (Types<Types<int, float, char>, Types<int, int, char>>));
 
-static_assert(std::is_same<CrossJoin<Types<int, float>, int>, Types<Types<int, int>, Types<float, int>>>::value, "");
-static_assert(std::is_same<CrossJoin<int, Types<int, float>>, Types<Types<int, int>, Types<int, float>>>::value, "");
+  EXPECT_SAME_TYPE(
+      (RemoveIf<AllSame, Types<Types<int, float, char>, Types<int, float, char>,
+                               Types<int, int, char>>>),
+      (Types<Types<int, float, char>, Types<int, float, char>,
+             Types<int, int, char>>));
 
-TEST(TypeList, CrossJoin) {}
-
-// AllSame -----------------------------------------------------
-static_assert(AllSame::Call<Types<int, int>>::value, "");
-static_assert(!AllSame::Call<Types<bool, int>>::value, "");
-
-static_assert(AllSame::Call<int, int>::value, "");
-static_assert(!AllSame::Call<int, bool>::value, "");
-
-static_assert(AllSame::Call<int, int, int>::value, "");
-static_assert(!AllSame::Call<int, float, int>::value, "");
-static_assert(!AllSame::Call<int, int, float>::value, "");
-
-TEST(TypeList, AllSame) {}
+  EXPECT_SAME_TYPE(
+      (RemoveIf<ContainedIn<Types<Types<int, char>, Types<float, int>>>,
+                Types<Types<char, char>, Types<float, int>, Types<int, int>>>),
+      (Types<Types<char, char>, Types<int, int>>));
+}
 
 /*
-// Exists -----------------------------------------------------
-static_assert(Exists<int, Types<int, char, float>>, "");
-static_assert(!Exists<int, Types<double, char, float>>, "");
-static_assert(!Exists<int, Types<>>, "");
-static_assert(Exists<int, Types<double, char, float, int>>, "");
-static_assert(!Exists<int, Types<double>>, "");
-static_assert(Exists<int, Types<int>>, "");
-*/
-
-TEST(TypeList, Exists) {}
-
-// ContainedIn -----------------------------------------------------
-static_assert(ContainedIn<Types<Types<int, char>>>::Call<Types<int, char>>::value, "");
-static_assert(!ContainedIn<Types<Types<int, char>>>::Call<Types<int, float>>::value, "");
-static_assert(!ContainedIn<Types<>>::Call<Types<int, float>>::value, "");
-static_assert(ContainedIn<Types<Types<int, float>, Types<char, char>>>::Call<Types<int, float>>::value, "");
-static_assert(!ContainedIn<Types<Types<int, float>, Types<char, char>>>::Call<Types<int, double>>::value, "");
-static_assert(ContainedIn<Types<Types<int, float>, Types<>>>::Call<Types<>>::value, "");
-static_assert(!ContainedIn<Types<Types<int, float>, Types<int>>>::Call<Types<>>::value, "");
-
-TEST(TypeList, ContainedIn) {}
-/*
-
-// RemoveIf -----------------------------------------------------
-static_assert(std::is_same<RemoveIf<AllSame, Types<>>, Types<>>::value, "");
-
-static_assert(std::is_same<RemoveIf<AllSame, Types<Types<int, int, int>>>, Types<>>::value, "");
-
-static_assert(std::is_same<RemoveIf<AllSame, Types<Types<int, float, int>>>, Types<Types<int, float, int>>>::value, "");
-
-static_assert(
-    std::is_same<RemoveIf<AllSame, Types<Types<int, float, char>, Types<int, int, int>, Types<int, int, char>>>,
-                 Types<Types<int, float, char>, Types<int, int, char>>>::value,
-    "");
-
-static_assert(
-    std::is_same<RemoveIf<AllSame, Types<Types<int, float, char>, Types<int, float, char>, Types<int, int, char>>>,
-                 Types<Types<int, float, char>, Types<int, float, char>, Types<int, int, char>>>::value,
-    "");
-
-static_assert(std::is_same_v<RemoveIf<ContainedIn<Types<Types<int, char>, Types<float, int>>>,
-                                      Types<Types<char, char>, Types<float, int>, Types<int, int>>>,
-                             Types<Types<char, char>, Types<int, int>>>,
-              "");
-
-TEST(TypeList, RemoveIn) {}
-
 // Transform -----------------------------------------------------
-static_assert(std::is_same_v<Transform<Rep<2>, Types<int, float>>, Types<Types<int, int>, Types<float, float>>>);
-static_assert(std::is_same_v<Transform<Rep<1>, Types<int, float>>, Types<Types<int>, Types<float>>>);
-static_assert(std::is_same_v<Transform<Rep<0>, Types<int, float>>, Types<Types<>, Types<>>>);
-static_assert(std::is_same_v<Transform<Rep<2>, Types<int>>, Types<Types<int, int>>>);
+static_assert(std::is_same_v<Transform<Rep<2>, Types<int, float>>,
+Types<Types<int, int>, Types<float, float>>>);
+static_assert(std::is_same_v<Transform<Rep<1>, Types<int, float>>,
+Types<Types<int>, Types<float>>>);
+static_assert(std::is_same_v<Transform<Rep<0>, Types<int, float>>,
+Types<Types<>, Types<>>>); static_assert(std::is_same_v<Transform<Rep<2>,
+Types<int>>, Types<Types<int, int>>>);
 static_assert(std::is_same_v<Transform<Rep<1>, Types<int>>, Types<Types<int>>>);
 static_assert(std::is_same_v<Transform<Rep<0>, Types<>>, Types<>>);
 
@@ -200,24 +205,28 @@ static_assert(std::is_same_v<Append<Types<>>, Types<>>);
 static_assert(std::is_same_v<Append<Types<>, int>, Types<int>>);
 static_assert(std::is_same_v<Append<Types<int>>, Types<int>>);
 static_assert(std::is_same_v<Append<Types<int>, float>, Types<int, float>>);
-static_assert(std::is_same_v<Append<Types<int>, float, char>, Types<int, float, char>>);
+static_assert(std::is_same_v<Append<Types<int>, float, char>, Types<int, float,
+char>>);
 
 TEST(TypeList, Append) {}
 
 // Unique -----------------------------------------------------
 static_assert(std::is_same_v<Unique<Types<>>, Types<>>);
-static_assert(std::is_same_v<Unique<Types<int, char, float>>, Types<int, char, float>>);
-static_assert(std::is_same_v<Unique<Types<int, int, float>>, Types<int, float>>);
-static_assert(std::is_same_v<Unique<Types<int, char, char, float>>, Types<int, char, float>>);
-static_assert(std::is_same_v<Unique<Types<int, char, float, float>>, Types<int, char, float>>);
+static_assert(std::is_same_v<Unique<Types<int, char, float>>, Types<int, char,
+float>>); static_assert(std::is_same_v<Unique<Types<int, int, float>>,
+Types<int, float>>); static_assert(std::is_same_v<Unique<Types<int, char, char,
+float>>, Types<int, char, float>>);
+static_assert(std::is_same_v<Unique<Types<int, char, float, float>>, Types<int,
+char, float>>);
 
 TEST(TypeList, Unique) {}
 
 // Contains -------------------------------------------------
 
 static_assert(Contains(Types<Size2D<1, 3>>(), nv::vpi::priv::Size2D{1, 3}));
-static_assert(Contains(Types<Size2D<5, 2>, Size2D<3, 5>, Size2D<1, 3>>(), nv::vpi::priv::Size2D{1, 3}));
-static_assert(!Contains(Types<Size2D<5, 2>, Size2D<3, 5>, Size2D<1, 3>>(), nv::vpi::priv::Size2D{1, 7}));
+static_assert(Contains(Types<Size2D<5, 2>, Size2D<3, 5>, Size2D<1, 3>>(),
+nv::vpi::priv::Size2D{1, 3})); static_assert(!Contains(Types<Size2D<5, 2>,
+Size2D<3, 5>, Size2D<1, 3>>(), nv::vpi::priv::Size2D{1, 7}));
 static_assert(!Contains(Types<Size2D<1, 4>>(), nv::vpi::priv::Size2D{1, 3}));
 static_assert(!Contains(Types<>(), nv::vpi::priv::Size2D{1, 3}));
 
@@ -225,12 +234,12 @@ TEST(TypeList, Contains) {}
 
 // Remove ---------------------------------------------------
 
-static_assert(std::is_same<Remove<Types<int, float, char>, 1>, Types<int, char>>::value);
-static_assert(std::is_same<Remove<Types<int, float, char>, 0, 2>, Types<float>>::value);
-static_assert(std::is_same<Remove<Types<int, char>>, Types<int, char>>::value);
-static_assert(std::is_same<Remove<Types<int>, 0>, Types<>>::value);
-static_assert(std::is_same<Remove<Types<int, char>, 0, 1>, Types<>>::value);
-static_assert(std::is_same<Remove<Types<>>, Types<>>::value);
+static_assert(std::is_same<Remove<Types<int, float, char>, 1>, Types<int,
+char>>::value); static_assert(std::is_same<Remove<Types<int, float, char>, 0,
+2>, Types<float>>::value); static_assert(std::is_same<Remove<Types<int, char>>,
+Types<int, char>>::value); static_assert(std::is_same<Remove<Types<int>, 0>,
+Types<>>::value); static_assert(std::is_same<Remove<Types<int, char>, 0, 1>,
+Types<>>::value); static_assert(std::is_same<Remove<Types<>>, Types<>>::value);
 
 TEST(TypeList, Remove) {}
 
