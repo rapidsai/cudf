e1458ca054225e790185cd471a501b1cc3381391
Mike Wendt <mike.wendt@me.com>
2019-09-17 02:01:22 -0400

    TST Revert bogus commit

--- print_env.sh
+++ print_env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Reports relevant environment information useful for diagnosing and
 # debugging cuDF issues.
-# Usage: test
+# Usage: 
 # "./print_env.sh" - prints to stdout
 # "./print_env.sh > env.txt" - prints to file "env.txt"
 
