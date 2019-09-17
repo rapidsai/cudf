af18d5f3807d2935d5a0aa4828d809a74e5bb940
Jake Hemstad <jhemstad@nvidia.com>
2019-09-16 21:10:31 -0500

    Update includes.

--- cpp/tests/experimental/experimental_tests.cpp
+++ cpp/tests/experimental/experimental_tests.cpp
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#include <tests/utilities/GTest.hpp>
-#include <tests/utilities/TypeList.hpp>
+#include <tests/utilities/cudf_gtest.hpp>
+#include <tests/utilities/type_list.hpp>
 
 #include <gmock/gmock.h>
 
