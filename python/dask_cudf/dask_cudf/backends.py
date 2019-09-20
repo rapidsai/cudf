a87edec568e3d1cb0e84f4e87d01f5649dfe8be5
rjzamora <rzamora217@gmail.com>
2019-09-20 09:46:14 -0700

    fix try block mistake

--- python/dask_cudf/dask_cudf/backends.py
+++ python/dask_cudf/dask_cudf/backends.py
@@ -1,4 +1,5 @@
 from dask.dataframe.core import get_parallel_type, make_meta, meta_nonempty
+from dask.dataframe.methods import concat_dispatch
 
 import cudf
 
@@ -15,31 +16,29 @@ def meta_nonempty_cudf(x, index=None):
     return cudf.from_pandas(y)
 
 
+@make_meta.register((cudf.Series, cudf.DataFrame))
+def make_meta_cudf(x, index=None):
+    return x.head(0)
+
+
+@make_meta.register(cudf.Index)
+def make_meta_cudf_index(x, index=None):
+    return x[:0]
+
+
+@concat_dispatch.register((cudf.DataFrame, cudf.Series, cudf.Index))
+def concat_cudf(
+    dfs, axis=0, join="outer", uniform=False, filter_warning=True, sort=None
+):
+    assert axis == 0
+    assert join == "outer"
+    return cudf.concat(dfs)
+
+
 try:
 
-    from dask.dataframe.methods import concat_dispatch, group_split, hash_df
+    from dask.dataframe.methods import group_split, hash_df
     import cudf._lib as libcudf
-
-    @make_meta.register((cudf.Series, cudf.DataFrame))
-    def make_meta_cudf(x, index=None):
-        return x.head(0)
-
-    @make_meta.register(cudf.Index)
-    def make_meta_cudf_index(x, index=None):
-        return x[:0]
-
-    @concat_dispatch.register((cudf.DataFrame, cudf.Series, cudf.Index))
-    def concat_cudf(
-        dfs,
-        axis=0,
-        join="outer",
-        uniform=False,
-        filter_warning=True,
-        sort=None,
-    ):
-        assert axis == 0
-        assert join == "outer"
-        return cudf.concat(dfs)
 
     @hash_df.register(cudf.DataFrame)
     def hash_df_cudf(dfs):
