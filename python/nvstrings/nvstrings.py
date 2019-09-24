ed20158815093e21294293127fd4c52dc32874e0
rgsl888prabhu <ramakrishnap@nvidia.com>
2019-09-24 10:58:30 -0500

    code change to nvstrings and test cases

--- python/nvstrings/nvstrings.py
+++ python/nvstrings/nvstrings.py
@@ -423,6 +423,8 @@ class nvstrings:
             end = self.size() if key.stop is None else key.stop
             step = 1 if key.step is None or key.step == 0 else key.step
             # negative slicing check
+            start = -1 * self.size() if (start < -1 * self.size()) else start
+            end = -1 * self.size() if (end < -1 * self.size()) else end
             end = self.size() + end if end < 0 else end
             start = self.size() + start if start < 0 else start
             rtn = pyniNVStrings.n_sublist(self.m_cptr, start, end, step)
