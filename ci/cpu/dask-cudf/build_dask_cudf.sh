f1897f1ec1f9a5738546816e801a00d6658d727b
Markku Luukkainen <mluukkainen@nvidia.com>
2019-09-25 11:58:17 -0700

    Use YYMMDD tag in dask-cudf nightly build

--- ci/cpu/dask-cudf/build_dask_cudf.sh
+++ ci/cpu/dask-cudf/build_dask_cudf.sh
@@ -6,5 +6,10 @@ function logger() {
   echo -e "\n>>>> $@\n"
 }
 
+# If nightly build, append current YYMMDD to version
+if [[ "$BUILD_MODE" = "branch" && "$SOURCE_BRANCH" = branch-* ]] ; then
+  export VERSION_SUFFIX=`date +%y%m%d`
+fi
+
 logger "Building dask_cudf"
 conda build conda/recipes/dask-cudf --python=$PYTHON
