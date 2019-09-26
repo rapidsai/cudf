23127355949e17287b46b9f9bb50462ca51ef2a2
Ashwin Srinath <shwina@users.noreply.github.com>
2019-09-26 14:06:35 -0400

    More fixes to CategoricalDtype and add tests

--- python/cudf/cudf/core/dtypes.py
+++ python/cudf/cudf/core/dtypes.py
@@ -1,15 +1,27 @@
-from pandas.api.extensions import ExtensionDtype, register_extension_dtype
+import pandas as pd
+from pandas.api.extensions import ExtensionDtype
+
+import cudf
 
 
-@register_extension_dtype
 class CategoricalDtype(ExtensionDtype):
-    def __init__(self, categories=None, ordered=False):
-        self.categories = categories
+    def __init__(self, categories=None, ordered=None):
+        """
+        dtype similar to pd.CategoricalDtype with the categories
+        stored on the GPU.
+        """
+        self._categories = self._init_categories(categories)
         self.ordered = ordered
 
     @property
+    def categories(self):
+        if self._categories is None:
+            return None
+        return cudf.core.index.as_index(self._categories)
+
+    @property
     def type(self):
-        return self.categories.dtype.type
+        return self._categories.dtype.type
 
     @property
     def name(self):
@@ -19,32 +31,36 @@ class CategoricalDtype(ExtensionDtype):
     def str(self):
         return "|O08"
 
-    def __hash__(self):
-        return hash(self.name)
+    def to_pandas(self):
+        if self.categories is None:
+            categories = None
+        else:
+            categories = self.categories.to_pandas()
+        return pd.CategoricalDtype(categories=categories, ordered=self.ordered)
+
+    def _init_categories(self, categories):
+        if categories is None:
+            return categories
+        if len(categories) == 0:
+            dtype = "object"
+        else:
+            dtype = None
+        return cudf.core.column.as_column(categories, dtype=dtype)
 
     def __eq__(self, other):
-        """
-        Rules for equality largely borrowed from pd.CategoricalDtype
-        """
         if isinstance(other, str):
             return other == self.name
         elif other is self:
             return True
-        elif not (hasattr(other, "ordered") and hasattr(other, "categories")):
+        elif not isinstance(other, self.__class__):
             return False
         elif self.categories is None or other.categories is None:
             return True
-        elif self.ordered or other.ordered:
-            return (self.ordered == other.ordered) and self.categories.equals(
-                other.categories
-            )
         else:
-            if (
-                self.categories.dtype == other.categories.dtype
-                and self.categories.equals(other.categories)
-            ):
-                return True
-        return False
+            return (
+                self._categories.dtype == other._categories.dtype
+                and self._categories.equals(other._categories)
+            )
 
-    def construct_from_string():
-        pass
+    def construct_from_string(self):
+        raise NotImplementedError()
