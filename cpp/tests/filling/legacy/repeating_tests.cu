141c12aabfac3f8a59dd13550e112d8ceb068c1b
Seunghwa Kang <seunghwak@nvidia.com>
2019-10-09 15:39:58 -0700

    updated inlcude statement in test files to reflect the new legacy location for filling.hpp

--- cpp/tests/filling/legacy/repeating_tests.cu
+++ cpp/tests/filling/legacy/repeating_tests.cu
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#include <cudf/filling.hpp>
+#include <cudf/legacy/filling.hpp>
 #include <tests/utilities/legacy/column_wrapper.cuh>
 #include <tests/utilities/legacy/column_wrapper_factory.hpp>
 #include <tests/utilities/legacy/scalar_wrapper.cuh>
