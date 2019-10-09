f039b4e9c746173c65163784a218d28c11ec0daf
ptaylor <paul.e.taylor@me.com>
2019-10-09 13:07:41 -0700

    extract bool8 wrapper into separate type, port applicable tests

---
+++ cpp/include/cudf/wrappers/bools.hpp
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <utilities/cudf_utils.h>
+
+#include <cub/util_type.cuh>
+
+#include <cstdint>
+#include <iosfwd>
+
+/**---------------------------------------------------------------------------*
+ * @file bools.hpp
+ * @brief Concrete type definition for uint8_t values !0 and 0 representing
+ * boolean true and false respectively.
+ *---------------------------------------------------------------------------**/
+namespace cudf {
+namespace exp {
+
+struct bool8 {
+
+  uint8_t value;
+
+  template <typename from_type>
+  CUDA_HOST_DEVICE_CALLABLE constexpr explicit bool8(from_type v)
+    : value{static_cast<bool>(v)} {}
+
+  bool8(bool8 const& w) = default;
+  bool8() = default;
+
+  // cudf::exp::bool8 bool conversion operators
+
+  CUDA_HOST_DEVICE_CALLABLE explicit operator uint8_t() const {
+    return this->value;
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE explicit operator bool() const {
+    return static_cast<bool>(this->value);
+  }
+
+  // cudf::exp::bool8 ostream << operator overload
+
+  inline std::ostream& operator<<(std::ostream& os) {
+    return os << static_cast<bool>(this->value);
+  }
+
+  // cudf::exp::bool8 binary operator overloads
+
+  CUDA_HOST_DEVICE_CALLABLE bool operator==(cudf::exp::bool8 const &rhs) const {
+    return static_cast<bool>(*this) == static_cast<bool>(rhs);
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE bool operator!=(cudf::exp::bool8 const &rhs) const {
+    return static_cast<bool>(*this) != static_cast<bool>(rhs);
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE bool operator<=(cudf::exp::bool8 const &rhs) const {
+    return static_cast<bool>(*this) <= static_cast<bool>(rhs); 
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE bool operator>=(cudf::exp::bool8 const &rhs) const {
+    return static_cast<bool>(*this) >= static_cast<bool>(rhs); 
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE bool operator<(cudf::exp::bool8 const &rhs) const {
+    return static_cast<bool>(*this) < static_cast<bool>(rhs);
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE bool operator>(cudf::exp::bool8 const &rhs) const {
+    return static_cast<bool>(*this) > static_cast<bool>(rhs);
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE cudf::exp::bool8 operator+(cudf::exp::bool8 const &rhs) const {
+    return static_cast<cudf::exp::bool8>(static_cast<bool>(*this) +
+                                         static_cast<bool>(rhs));
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE cudf::exp::bool8 operator-(cudf::exp::bool8 const &rhs) const {
+    return static_cast<cudf::exp::bool8>(static_cast<bool>(*this) -
+                                         static_cast<bool>(rhs));
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE cudf::exp::bool8 operator*(cudf::exp::bool8 const &rhs) const {
+    return static_cast<cudf::exp::bool8>(static_cast<bool>(*this) *
+                                         static_cast<bool>(rhs));
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE cudf::exp::bool8 operator/(cudf::exp::bool8 const &rhs) const {
+    return static_cast<cudf::exp::bool8>(static_cast<bool>(*this) /
+                                         static_cast<bool>(rhs));
+  }
+
+  // cudf::exp::bool8 unary operator overloads
+
+  CUDA_HOST_DEVICE_CALLABLE cudf::exp::bool8& operator+=(cudf::exp::bool8 const &rhs) {
+    cudf::exp::bool8 &lhs = *this;
+    lhs = lhs + rhs;
+    return lhs;
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE cudf::exp::bool8& operator-=(cudf::exp::bool8 const &rhs) {
+    cudf::exp::bool8 &lhs = *this;
+    lhs = lhs - rhs;
+    return lhs;
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE cudf::exp::bool8& operator*=(cudf::exp::bool8 const &rhs) {
+    cudf::exp::bool8 &lhs = *this;
+    lhs = lhs * rhs;
+    return lhs;
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE cudf::exp::bool8& operator/=(cudf::exp::bool8 const &rhs) {
+    cudf::exp::bool8 &lhs = *this;
+    lhs = lhs / rhs;
+    return lhs;
+  }
+
+  CUDA_HOST_DEVICE_CALLABLE cudf::exp::bool8 operator!() const {
+    return static_cast<cudf::exp::bool8>(!static_cast<bool>(*this));
+  }
+};
+
+// This is necessary for global, constant, non-fundamental types
+// We can't rely on --expt-relaxed-constexpr here because `bool8` is not a
+// scalar type. See CUDA Programming guide
+// https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#constexpr-variables
+#ifdef __CUDA_ARCH__
+__device__ __constant__ static bool8 true_v{uint8_t{1}};
+__device__ __constant__ static bool8 false_v{uint8_t{0}};
+#else
+static constexpr bool8 true_v{uint8_t{1}};
+static constexpr bool8 false_v{uint8_t{0}};
+#endif
+
+} // exp
+}  // cudf
+
+namespace std {
+/** --------------------------------------------------------------------------*
+  * @brief Specialization of std::numeric_limits for cudf::exp::bool8
+  *
+  * Required since the underlying type, uint8_t, has different limits than bool
+  * --------------------------------------------------------------------------**/
+template <>
+struct numeric_limits<cudf::exp::bool8> {
+  
+  static constexpr cudf::exp::bool8 max() noexcept {
+    // tried using `return cudf::true_v` but it causes a compiler segfault!
+    return cudf::exp::bool8{true};
+  }
+  
+  static constexpr cudf::exp::bool8 lowest() noexcept {
+    return cudf::exp::bool8{false};
+  }
+
+  static constexpr cudf::exp::bool8 min() noexcept {
+    return cudf::exp::bool8{false};
+  }
+};
+
+} // std
+
+namespace cub {
+
+template <> struct NumericTraits<cudf::exp::bool8> :
+  BaseTraits<SIGNED_INTEGER, true, false, uint8_t, int8_t> {};
+
+} // cub
---
+++ cpp/tests/wrappers/bools_test.cpp
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cudf/wrappers/bools.hpp>
+
+#include <tests/utilities/cudf_gtest.hpp>
+
+#include <random>
+
+struct Bool8Test : public ::testing::Test {
+
+  Bool8Test()
+      : dist{std::numeric_limits<uint8_t>::lowest(),
+             std::numeric_limits<uint8_t>::max()} {
+
+    // Use constant seed for deterministic results
+    rng.seed(0);
+  }
+
+  std::mt19937 rng;
+  std::uniform_int_distribution<uint8_t> dist;
+
+  uint8_t rand(){
+      return dist(rng);
+  }
+};
+
+template <typename SourceType>
+struct Bool8CtorTest : public ::testing::Test {
+  Bool8CtorTest() {
+    // Use constant seed for deterministic results
+    rng.seed(0);
+  }
+
+  std::mt19937 rng;
+
+  using uniform_distribution = typename std::conditional_t<
+      std::is_same<SourceType, bool>::value, std::bernoulli_distribution,
+      std::conditional_t<std::is_floating_point<SourceType>::value,
+                         std::uniform_real_distribution<SourceType>,
+                         std::uniform_int_distribution<SourceType>>>;
+
+  uniform_distribution dist{};
+
+  SourceType rand(){
+      return dist(rng);
+  }
+};
+
+/**
+ * @brief The number of test trials
+ */
+static constexpr int NUM_TRIALS{10000};
+
+using Bool8CastSourceTypes = ::testing::Types<char, bool, float, double, int32_t, int64_t>;
+
+TYPED_TEST_CASE(Bool8CtorTest, Bool8CastSourceTypes);
+
+TEST_F(Bool8Test, TestBool8Constructor) {
+  for (int i = 0; i < NUM_TRIALS; ++i) {
+    uint8_t t{this->rand()};
+    cudf::exp::bool8 const w{t};
+    EXPECT_EQ(w.value, static_cast<uint8_t>(static_cast<bool>(t)));
+  }
+}
+
+// Ensure bool8 constructor is correctly casting the input type to a bool
+TYPED_TEST(Bool8CtorTest, TestBool8ConstructorCast) {
+  using SourceType = TypeParam;
+  for (int i = 0; i < NUM_TRIALS; ++i) {
+    SourceType t{this->rand()};
+    cudf::exp::bool8 const w{t};
+    EXPECT_EQ(w.value, static_cast<uint8_t>(static_cast<bool>(t)));
+  }
+}
+
+TEST_F(Bool8Test, TestBool8Assignment) {
+    for (int i = 0; i < NUM_TRIALS; ++i) {
+      uint8_t const t0{this->rand()};
+      uint8_t const t1{this->rand()};
+      cudf::exp::bool8 w0{t0};
+      cudf::exp::bool8 w1{t1};
+
+      w0 = w1;
+
+      EXPECT_EQ(w0.value, static_cast<bool>(t1));
+    }
+}
+
+TEST_F(Bool8Test, TestBool8ArithmeticOperators) {
+    for(int i = 0; i < NUM_TRIALS; ++i) {
+        uint8_t const t0{static_cast<bool>(this->rand())};
+        uint8_t const t1{static_cast<bool>(this->rand())};
+
+        cudf::exp::bool8 const w0{t0};
+        cudf::exp::bool8 const w1{t1};
+
+        // Types smaller than int are implicitly promoted to `int` for
+        // arithmetic operations. Therefore, need to convert it back to the
+        // original type
+        EXPECT_EQ(static_cast<bool>(cudf::exp::bool8{w0 + w1}.value),
+                  static_cast<bool>(t0 + t1));
+        EXPECT_EQ(static_cast<bool>(cudf::exp::bool8{w0 - w1}.value),
+                  static_cast<bool>(t0 - t1));
+        EXPECT_EQ(static_cast<bool>(cudf::exp::bool8{w0 * w1}.value),
+                  static_cast<bool>(t0 * t1));
+        if (0 != t1) {
+            EXPECT_EQ(static_cast<bool>(cudf::exp::bool8{w0 / w1}.value),
+                      static_cast<bool>(t0 / t1));
+        }
+    }
+}
+
+TEST_F(Bool8Test, TestBool8BinaryOperators) {
+    for(int i = 0; i < NUM_TRIALS; ++i) {
+        bool const t0{this->rand()};
+        bool const t1{this->rand()};
+
+        cudf::exp::bool8 const w0{t0};
+        cudf::exp::bool8 const w1{t1};
+
+        EXPECT_EQ(w0 > w1, t0 > t1);
+        EXPECT_EQ(w0 < w1, t0 < t1);
+        EXPECT_EQ(w0 <= w1, t0 <= t1);
+        EXPECT_EQ(w0 >= w1, t0 >= t1);
+        EXPECT_EQ(w0 == w1, t0 == t1);
+        EXPECT_EQ(w0 != w1, t0 != t1);
+    }
+
+    cudf::exp::bool8 w2{42};
+    cudf::exp::bool8 w3{43};
+
+    EXPECT_TRUE(w2 == w2);
+    EXPECT_TRUE(w2 == w3);
+    EXPECT_FALSE(w2 < w3);
+    EXPECT_FALSE(w2 > w3);
+    EXPECT_FALSE(w2 != w3);
+    EXPECT_TRUE(w2 >= w2);
+    EXPECT_TRUE(w2 <= w2);
+    EXPECT_TRUE(w2 >= w3);
+    EXPECT_TRUE(w2 <= w3);
+
+    cudf::exp::bool8 w4{static_cast<char>(-42)};
+    cudf::exp::bool8 w5{43};
+
+    EXPECT_TRUE(w4 == w4);
+    EXPECT_TRUE(w5 == w5);
+    EXPECT_FALSE(w4 < w5);
+    EXPECT_FALSE(w4 > w5);
+    EXPECT_FALSE(w4 != w5);
+    EXPECT_TRUE(w4 >= w4);
+    EXPECT_TRUE(w4 <= w4);
+    EXPECT_TRUE(w4 >= w5);
+    EXPECT_TRUE(w4 <= w5);
+
+    cudf::exp::bool8 w6{0};
+    cudf::exp::bool8 w7{43};
+
+    EXPECT_FALSE(w6 == w7);
+    EXPECT_TRUE(w6 < w7);
+    EXPECT_TRUE(w7 > w6);
+    EXPECT_FALSE(w6 > w7);
+    EXPECT_TRUE(w6 != w7);
+    EXPECT_TRUE(w6 >= w6);
+    EXPECT_TRUE(w6 <= w6);
+    EXPECT_FALSE(w6 >= w7);
+    EXPECT_TRUE(w6 <= w7);
+}
+
+// This ensures that casting cudf::exp::bool8 to int, doing arithmetic, and casting
+// the result to bool results in the right answer. If the arithmetic is done
+// on random underlying values you can get the wrong answer.
+TEST_F(Bool8Test, TestBool8ArithmeticCast) {
+    cudf::exp::bool8 w1{42};
+    cudf::exp::bool8 w2{static_cast<char>(-42)};
+
+    bool t1{42};
+    bool t2{-42};
+
+    EXPECT_EQ(static_cast<bool>(static_cast<uint8_t>(w1) + static_cast<uint8_t>(w2)),
+              static_cast<bool>(static_cast<uint8_t>(t1) + static_cast<uint8_t>(t2)));
+}
+
+TEST_F(Bool8Test, TestBool8CompoundAssignmentOperators) {
+    for(int i = 0; i < NUM_TRIALS; ++i) {
+        bool t0{static_cast<bool>(this->rand())};
+        bool const t1{static_cast<bool>(this->rand())};
+
+        cudf::exp::bool8 w0{t0};
+        cudf::exp::bool8 const w1{t1};
+
+        t0 += t1;
+        w0 += w1;
+        EXPECT_EQ(static_cast<bool>(w0), t0);
+
+        t0 -= t1;
+        w0 -= w1;
+        EXPECT_EQ(static_cast<bool>(w0), t0);
+
+
+        t0 *= t1;
+        w0 *= w1;
+        EXPECT_EQ(static_cast<bool>(w0), t0);
+
+        if (t1) {
+            t0 /= t1;
+            w0 /= w1;
+            EXPECT_EQ(static_cast<bool>(w0), t0);
+        }
+    }
+}
+
+TEST_F(Bool8Test, TestBool8NumericLimitsTest) {
+    EXPECT_EQ(static_cast<uint8_t>(std::numeric_limits<cudf::exp::bool8>::max()),
+              static_cast<uint8_t>(static_cast<bool>(std::numeric_limits<uint8_t>::max())));
+    EXPECT_EQ(static_cast<uint8_t>(std::numeric_limits<cudf::exp::bool8>::min()),
+              static_cast<uint8_t>(static_cast<bool>(std::numeric_limits<uint8_t>::min())));
+    EXPECT_EQ(static_cast<uint8_t>(std::numeric_limits<cudf::exp::bool8>::lowest()),
+              static_cast<uint8_t>(static_cast<bool>(std::numeric_limits<uint8_t>::lowest())));
+}
