6b5b3c7bdeaf130ac8c7b6275224f07f5d1a0f39
Olivier Lapicque <olapicque@nvidia.com>
2019-10-11 18:01:49 -0700

    Fix Int128ToDouble_rn

--- cpp/src/io/utilities/block_utils.cuh
+++ cpp/src/io/utilities/block_utils.cuh
@@ -55,7 +55,15 @@ template <typename T> inline __device__ T WarpReducePos32(T pos, uint32_t t) { T
 
 inline __device__ double Int128ToDouble_rn(uint64_t lo, int64_t hi)
 {
-    return __fma_rn(__ll2double_rn(hi), 4294967296.0, __ull2double_rn(lo));
+    double sign;
+    if (hi < 0) {
+        sign = -1.0;
+        lo = (~lo) + 1;
+        hi = (~hi) + (lo == 0);
+    } else {
+        sign = 1.0;
+    }
+    return sign * __fma_rn(__ll2double_rn(hi), 4294967296.0 * 4294967296.0, __ull2double_rn(lo));
 }
 
 } // namespace io
