a919c0a3a0b9014036e9275d35daf89e522a686d
Jake Hemstad <jhemstad@nvidia.com>
2019-10-14 15:01:22 -0500

    Initial implementation of new valid_if.

--- cpp/src/bitmask/valid_if.cuh
+++ cpp/src/bitmask/valid_if.cuh
@@ -12,173 +12,93 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
-#ifndef __BITMASK_VALID_IF_CUH__
-#define __BITMASK_VALID_IF_CUH__
+#pragma once
 
-#include <cudf/types.h>
-#include <utilities/error_utils.hpp>
-#include <utilities/bit_util.cuh>
-#include <utilities/cuda_utils.hpp>
-#include <bitmask/legacy/bit_mask.cuh>
+#include <cudf/null_mask.hpp>
+#include <cudf/types.hpp>
 
-#include <cub/cub.cuh>
-
-using bit_mask::bit_mask_t;
+#include <rmm/device_scalar.hpp>
 
 namespace cudf {
-
 namespace detail {
+constexpr size_type warp_size{32};
 
-constexpr int warp_size = 32;
+template <std::size_t block_size, std::size_t lane = 0, typename T>
+__device__ T single_lane_block_sum_reduce(T warp_sum) {
+  static_assert(block_size <= 1024, "Invalid block size.");
+  constexpr auto warps_per_block{block_size / warp_size};
+  auto const lane_id{threadIdx.x % warp_size};
+  auto const warp_id{threadIdx.x / warp_size};
+  __shared__ T warp_sums[warp_size];
 
-constexpr int block_size = 256;
-
-/**
- * @brief for each warp in the block do a reduction (summation) of the
- * `__popc(bit_mask)` on a certain lane (default is lane 0).
- * @param[in] bit_mask The bit_mask to be reduced.
- * @return[out] result of each block is returned in thread 0.
- */
-template <class bit_container, int lane = 0>
-__device__ __inline__ gdf_size_type single_lane_popc_block_reduce(bit_container bit_mask) {
-  
-  static __shared__ gdf_size_type warp_count[warp_size];
-  
-  int lane_id = (threadIdx.x % warp_size);
-  int warp_id = (threadIdx.x / warp_size);
-
-  // Assuming one lane of each warp holds the value that we want to perform
-  // reduction
   if (lane_id == lane) {
-    warp_count[warp_id] = __popc(bit_mask);
+    warp_sums[warp_id] = (warp_id < warps_per_block) ? warp_sum : 0;
   }
+
   __syncthreads();
 
-  gdf_size_type block_count = 0;
+  T result{0};
+  if (warp_id == 0) {
+    __shared__ typename cub::WarpReduce<T>::TempStorage temp;
+    result = cub::WarpReduce<T>(temp).sum(warp_sums[lane_id]);
+  }
+
+  return result;
+}
+
+template <size_type block_size, typename InputIterator, typename Predicate>
+valid_if_kernel(bitmask_type* output, InputIterator begin, InputIterator end,
+                Predicate p, size_type* valid_count) {
+  auto const lane_id{threadIdx.x % warp_size};
+  auto const warp_id{threadIdx.x / warp_size};
+  auto const tid = threadIdx.x + blockIdx.x * gridDim.x;
+  auto i = begin + tid;
+  size_type warp_valid_count{0};
+
+  auto active_mask = __ballot_sync(0xFFFF'FFFF, i < end);
+  while (i < end) {
+    bitmask_type ballot = __ballot_sync(active_mask, p(*i));
+    if (lane_id == 0) {
+      auto bit_index = thrust::distance(begin, i);
+      output[cudf::detail::word_index(bit_index)] = ballot;
+      warp_valid_count += __popc(ballot);
+    }
+    i += blockDim.x * gridDim.x;
+    active_mask = __ballot_sync(active_mask, i < end);
+  }
 
   if (warp_id == 0) {
-    
-    static_assert(block_size <= 1024,
-      "Reduction code only works with a block size less or equal to 1024.");
-
-    // Maximum block size is 1024 and 1024 / 32 = 32
-    // so one single warp is enough to do the reduction over different warps
-    gdf_size_type count = 
-      (lane_id < (blockDim.x / warp_size)) ? warp_count[lane_id] : 0;
-    
-    __shared__
-        typename cub::WarpReduce<gdf_size_type>::TempStorage temp_storage;
-    block_count = cub::WarpReduce<gdf_size_type>(temp_storage).Sum(count);
-
+    atomicAdd(valid_count,
+              single_lane_block_sum_reduce<block_size>(warp_valid_count));
   }
+}  // namespace detail
 
-  return block_count;
+/**
+ * @brief Generate a new bitmask where every bit is set for which a predicate is
+ * `true` over the elements in `[begin,end)`.
+ *
+ * Bit `i` in the output mask will be set if `p(*(begin+i)) == true`.
+ *
+ * @param begin The beginning of the sequence
+ * @param end The end of the sequence
+ * @param p The predicate
+ * @param stream Stream on which to execute all GPU activity and device memory
+ * allocations.
+ * @return A `device_buffer` containing the new bitmask and it's null count
+ */
+template <typename InputIterator, typename Predicate>
+std::pair<rmm::device_buffer, size_type> valid_if(
+    InputIterator begin, InputIterator end, Predicate&& p,
+    cudaStream_t stream = 0,
+    rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource()) {
+  auto size = thrust::distance(begin, end);
 
+  auto null_mask =
+      create_null_mask(size, mask_state::UNINITIALIZED, stream, mr);
+
+  rmm::device_scalar<size_type> valid_count{0, stream, mr};
 }
-
-template <bool source_mask_valid, typename bit_container, typename predicate, typename size_type>
-__global__ void valid_if_kernel(
-    const bit_container* source_mask, 
-    bit_container* destination_mask, 
-    predicate p,
-    size_type num_bits,
-    size_type* p_valid_count
-  ){
- 
-  static_assert(warp_size == util::size_in_bits<bit_container>(), 
-      "warp size is different from bit_container size.");
-
-  size_type bit_index_base = blockIdx.x * blockDim.x;
-
-  while (bit_index_base < num_bits) {
-  
-    size_type bit_index = bit_index_base + threadIdx.x;
-   
-    bool thread_active = bit_index < num_bits;
-    bit_container active_threads =
-        __ballot_sync(0xffffffff, thread_active);
-
-    bit_container result_mask = 0;
-
-    if(thread_active){
-      
-      bool const predicate_is_true = p(bit_index);
-      const bit_container ballot_result =
-          __ballot_sync(active_threads, predicate_is_true);
-
-      // Only one thread writes output
-      if (0 == threadIdx.x % warp_size) {
-        const size_type container_index = 
-          util::detail::bit_container_index<bit_container>(bit_index);
-
-        result_mask = source_mask_valid ?
-          source_mask[container_index] & ballot_result : ballot_result;
-        destination_mask[container_index] = result_mask;
-      }
-    
-    }
-    
-    result_mask = single_lane_popc_block_reduce(result_mask);
-    if(0 == threadIdx.x){
-      atomicAdd(p_valid_count, result_mask);
-    }
-    
-    bit_index_base += blockDim.x * gridDim.x;
-  
-  }
-
-}
-
-} // namespace detail
-
- /**
-  * @brief Generate a bitmask where every bit is marked with valid 
-  * if and only if predicate(bit) and source_mask(bit) are both true.
-  * 
-  * @param source_mask The source mask
-  * @param p The predicate that has an operator() member function
-  * @param num_bits Number of bits
-  * @param stream An optional cudaStream_t object
-  * @return The generated bitmask as well as its null_count
-  */
-template <typename bit_container, typename predicate, typename size_type>
-std::pair<bit_container*, size_type> valid_if(
-    const bit_container* source_mask, 
-    const predicate& p,
-    size_type num_bits,
-    cudaStream_t stream = 0
-  ){
-  
-  bit_container* destination_mask = nullptr;
-  CUDF_EXPECTS(GDF_SUCCESS == bit_mask::create_bit_mask(&destination_mask, num_bits), 
-      "Failed to allocate bit_mask buffer.");
-
-  auto kernel = source_mask ? 
-    detail::valid_if_kernel<true,  bit_container, predicate, size_type> :
-    detail::valid_if_kernel<false, bit_container, predicate, size_type> ;
-
-  rmm::device_vector<size_type> valid_count(1);
-  
-  const int grid_size = util::cuda::grid_config_1d(num_bits, detail::block_size).num_blocks;
-  
-  // launch the kernel
-  kernel<<<grid_size, detail::block_size, 0, stream>>>(
-      source_mask, destination_mask, p, num_bits, valid_count.data().get());
-
-  size_type valid_count_host;
-  CUDA_TRY(cudaMemcpyAsync(&valid_count_host, valid_count.data().get(),
-        sizeof(size_type), cudaMemcpyDeviceToHost, stream));
-  
-  // Synchronize the stream before null_count is updated on the host.
-  cudaStreamSynchronize(stream);
-  size_type null_count = num_bits - valid_count_host;
-
-  CHECK_STREAM(stream);
-  return std::pair<bit_container*, size_type>(destination_mask, null_count);
-
-}
-
-} // namespace cudf
-#endif
+}  // namespace detail
+   // namespace cudf
