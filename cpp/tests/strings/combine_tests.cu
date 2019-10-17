9a4a66c76787bcf0d312c00729f11e3eaa16d508
David Wendt <dwendt@nvidia.com>
2019-10-17 17:38:01 -0400

    update tests to use strings_column_wrapper

--- cpp/tests/strings/combine_tests.cu
+++ cpp/tests/strings/combine_tests.cu
@@ -20,6 +20,8 @@
 #include <cudf/types.hpp>
 
 #include <tests/utilities/base_fixture.hpp>
+#include <tests/utilities/column_wrapper.hpp>
+#include <tests/utilities/column_utilities.cuh>
 #include "./utilities.h"
 
 #include <vector>
@@ -31,12 +33,14 @@ struct StringsCombineTest : public cudf::test::BaseFixture {};
 TEST_F(StringsCombineTest, Concatenate)
 {
     std::vector<const char*> h_strings1{ "eee", "bb", nullptr, "", "aa", "bbb", "ééé" };
+    cudf::test::strings_column_wrapper strings1( h_strings1.begin(), h_strings1.end(),
+        thrust::make_transform_iterator( h_strings1.begin(), [] (auto str) { return str!=nullptr; }));
     std::vector<const char*> h_strings2{ "xyz", "abc", "d", "éa", "", nullptr, "f" };
+    cudf::test::strings_column_wrapper strings2( h_strings2.begin(), h_strings2.end(),
+        thrust::make_transform_iterator( h_strings2.begin(), [] (auto str) { return str!=nullptr; }));
 
-    auto d_strings1 = cudf::test::create_strings_column(h_strings1);
-    auto view1 = cudf::strings_column_view(d_strings1->view());
-    auto d_strings2 = cudf::test::create_strings_column(h_strings2);
-    auto view2 = cudf::strings_column_view(d_strings2->view());
+    auto view1 = cudf::strings_column_view(strings1);
+    auto view2 = cudf::strings_column_view(strings2);
 
     std::vector<cudf::strings_column_view> strings_columns;
     strings_columns.push_back(view1);
@@ -44,27 +48,27 @@ TEST_F(StringsCombineTest, Concatenate)
 
     {
         std::vector<const char*> h_expected{ "eeexyz", "bbabc", nullptr, "éa", "aa", nullptr, "éééf" };
-        auto d_expected = cudf::test::create_strings_column(h_expected);
-        auto expected_view = cudf::strings_column_view(d_expected->view());
+        cudf::test::strings_column_wrapper expected( h_expected.begin(), h_expected.end(),
+            thrust::make_transform_iterator( h_expected.begin(), [] (auto str) { return str!=nullptr; }));
 
         auto results = cudf::strings::concatenate(strings_columns);
-        cudf::test::expect_strings_equal(*results, h_expected);
+        cudf::test::expect_columns_equal(*results,expected);
     }
     {
         std::vector<const char*> h_expected{ "eee:xyz", "bb:abc", nullptr, ":éa", "aa:", nullptr, "ééé:f" };
-        auto d_expected = cudf::test::create_strings_column(h_expected);
-        auto expected_view = cudf::strings_column_view(d_expected->view());
+        cudf::test::strings_column_wrapper expected( h_expected.begin(), h_expected.end(),
+            thrust::make_transform_iterator( h_expected.begin(), [] (auto str) { return str!=nullptr; }));
 
         auto results = cudf::strings::concatenate(strings_columns,":");
-        cudf::test::expect_strings_equal(*results, h_expected);
+        cudf::test::expect_columns_equal(*results,expected);
     }
     {
         std::vector<const char*> h_expected{ "eee:xyz", "bb:abc", "_:d", ":éa", "aa:", "bbb:_", "ééé:f" };
-        auto d_expected = cudf::test::create_strings_column(h_expected);
-        auto expected_view = cudf::strings_column_view(d_expected->view());
+        cudf::test::strings_column_wrapper expected( h_expected.begin(), h_expected.end() );
+            //thrust::make_transform_iterator( h_expected.begin(), [] (auto str) { return str!=nullptr; }));
 
         auto results = cudf::strings::concatenate(strings_columns,":","_");
-        cudf::test::expect_strings_equal(*results, h_expected);
+        cudf::test::expect_columns_equal(*results,expected);
     }
 }
 
@@ -72,11 +76,9 @@ TEST_F(StringsCombineTest, ConcatZeroSizeStringsColumns)
 {
     cudf::column_view zero_size_strings_column( cudf::data_type{cudf::STRING}, 0, nullptr, nullptr, 0);
     auto strings_view = cudf::strings_column_view(zero_size_strings_column);
-
     std::vector<cudf::strings_column_view> strings_columns;
     strings_columns.push_back(strings_view);
     strings_columns.push_back(strings_view);
-
     auto results = cudf::strings::concatenate(strings_columns);
     cudf::test::expect_strings_empty(results->view());
 }
@@ -85,43 +87,35 @@ TEST_F(StringsCombineTest, ConcatInvalidStringsColumns)
 {
     cudf::column_view zero_size_strings_column( cudf::data_type{cudf::STRING}, 0, nullptr, nullptr, 0);
     auto strings_view = cudf::strings_column_view(zero_size_strings_column);
-
     std::vector<cudf::strings_column_view> strings_columns;
-    strings_columns.push_back(strings_view);
-
+    strings_columns.push_back(strings_view); // not enough columns
     EXPECT_THROW( cudf::strings::concatenate(strings_columns), cudf::logic_error );
 }
 
 TEST_F(StringsCombineTest, Join)
 {
-    std::vector<const char*> h_strings1{ "eee", "bb", nullptr, "zzzz", "", "aaa", "ééé" };
-
-    auto d_strings1 = cudf::test::create_strings_column(h_strings1);
-    auto view1 = cudf::strings_column_view(d_strings1->view());
+    std::vector<const char*> h_strings{ "eee", "bb", nullptr, "zzzz", "", "aaa", "ééé" };
+    cudf::test::strings_column_wrapper strings( h_strings.begin(), h_strings.end(),
+        thrust::make_transform_iterator( h_strings.begin(), [] (auto str) { return str!=nullptr; }));
+    auto view1 = cudf::strings_column_view(strings);
 
     {
-        std::vector<const char*> h_expected{ "eeebbzzzzaaaééé" };
-        auto d_expected = cudf::test::create_strings_column(h_expected);
-        auto expected_view = cudf::strings_column_view(d_expected->view());
-
         auto results = cudf::strings::join_strings(view1);
-        cudf::test::expect_strings_equal(*results, h_expected);
+
+        cudf::test::strings_column_wrapper expected{"eeebbzzzzaaaééé"};
+        cudf::test::expect_columns_equal(*results,expected);
     }
     {
-        std::vector<const char*> h_expected{ "eee+bb+zzzz++aaa+ééé" };
-        auto d_expected = cudf::test::create_strings_column(h_expected);
-        auto expected_view = cudf::strings_column_view(d_expected->view());
-
         auto results = cudf::strings::join_strings(view1,"+");
-        cudf::test::expect_strings_equal(*results, h_expected);
+
+        cudf::test::strings_column_wrapper expected{"eee+bb+zzzz++aaa+ééé"};
+        cudf::test::expect_columns_equal(*results,expected);
     }
     {
-        std::vector<const char*> h_expected{ "eee+bb+___+zzzz++aaa+ééé" };
-        auto d_expected = cudf::test::create_strings_column(h_expected);
-        auto expected_view = cudf::strings_column_view(d_expected->view());
-
         auto results = cudf::strings::join_strings(view1,"+","___");
-        cudf::test::expect_strings_equal(*results, h_expected);
+
+        cudf::test::strings_column_wrapper expected{"eee+bb+___+zzzz++aaa+ééé"};
+        cudf::test::expect_columns_equal(*results,expected);
     }
 }
 
