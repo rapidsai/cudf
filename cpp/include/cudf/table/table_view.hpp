3ef0bf897398874c83e3ff1f5de542081caf2bee
Ashwin Srinath <shwina@users.noreply.github.com>
2019-10-22 10:17:00 -0400

    Fix typo

--- cpp/include/cudf/table/table_view.hpp
+++ cpp/include/cudf/table/table_view.hpp
@@ -150,7 +150,7 @@ class mutable_table_view : public detail::table_view_base<mutable_column_view> {
   using detail::table_view_base<mutable_column_view>::table_view_base;
 
   mutable_column_view& column(size_type column_index) const noexcept {
-    return const_cast<mutable_column_view&>(table_view_base::column(column_index));;
+    return const_cast<mutable_column_view&>(table_view_base::column(column_index));
   }
   /**---------------------------------------------------------------------------*
    * @brief Creates an immutable `table_view` of the columns
