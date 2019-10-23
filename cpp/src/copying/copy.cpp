b4f2379ad57f3cf4f8ee56939f4b09261c39f23c
Ram (Ramakrishna Prabhu) <ramakrishnap@nvidia.com>
2019-10-23 11:07:39 -0500

    moving detail header to include/detail

--- cpp/src/copying/copy.cpp
+++ cpp/src/copying/copy.cpp
@@ -20,7 +20,7 @@
 #include <cudf/column/column.hpp>
 #include <cudf/table/table.hpp>
 #include <cudf/utilities/traits.hpp>
-#include "copy_detail.hpp"
+#include <detail/copy_detail.hpp>
 
 #include <algorithm>
 
