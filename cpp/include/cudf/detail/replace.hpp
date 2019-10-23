70bf8cf345163e5b355fe4935b7d1d4297961ecf
James Wyles <jwyles@nvidia.com>
2019-10-23 13:38:15 -0600

    Moved ::detail functions to private header file

--- cpp/include/cudf/detail/replace.hpp
+++ cpp/include/cudf/detail/replace.hpp
@@ -23,59 +23,62 @@
 typedef struct CUstream_st* cudaStream_t;
 
 namespace cudf {
-namespace experimental {
+namespace detail {
+
 /**
-  * @brief Replaces all null values in a column with corresponding values of another column.
-  *
-  * Returns a column `output` such that if `input[i]` is valid, its value will be copied to
-  * `output[i]`. Otherwise, `replacements[i]` will be copied to `output[i]`.
-  *
-  * The `input` and `replacement` columns must be of same size and have the same
-  * data type.
-  *
-  * @param[in] input A gdf_column containing null values
-  * @param[in] replacement A gdf_column whose values will replace null values in input
-  *
-  * @returns Column with nulls replaced
-  */
+ * @brief Replaces all null values in a column with corresponding values of another column
+ *
+ * The first column is expected to be a cudf::column. The second column
+ * must be of the same type and same size as the first.
+ *
+ * The function replaces all nulls of the first column with the
+ * corresponding elements of the second column
+ *
+ * @param[in] input A cudf::column containing null values
+ * @param[in] replacement A cudf::column whose values will replace null values in input
+ * @param[in] stream Optional stream in which to perform allocations
+ *
+ * @returns Column with nulls replaced
+ */
 std::unique_ptr<cudf::column> replace_nulls(cudf::column_view const& input,
                                             cudf::column_view const& replacement,
+                                            cudaStream_t stream = 0,
                                             device_memory_resource* mr = rmm::get_default_resource());
 
 /**
   * @brief Replaces all null values in a column with a scalar.
   *
-  * Returns a column `output` such that if `input[i]` is valid, its value will be copied to
-  * `output[i]`. Otherise, `replacement` will be coped to `output[i]`.
+  * The column is expected to be a cudf::column. The scalar is expected to be
+  * a gdf_scalar of the same data type as the column.
   *
-  * `replacement` must have the same data type as `input`.
+  * The function will replace all nulls of the column with the scalar value.
   *
   * @param[in] input A gdf_column containing null values
   * @param[in] replacement A gdf_scalar whose value will replace null values in input
+  * @param[in] stream Optional stream in which to perform allocations
   *
   * @returns Column with nulls replaced
   */
 std::unique_ptr<cudf::column> replace_nulls(cudf::column_view const& input,
                                             const gdf_scalar& replacement,
+                                            cudaStream_t stream = 0,
                                             device_memory_resource* mr = rmm::get_default_resource());
-
 
 /**
  * @brief Replace elements from `input_col` according to the mapping `old_values` to
  *        `new_values`, that is, replace all `old_values[i]` present in `col`
  *        with `new_values[i]` and return a new gdf_column `output`.
  *
- * @param[in] col gdf_column with the data to be modified
- * @param[in] values_to_replace gdf_column with the old values to be replaced
- * @param[in] replacement_values gdf_column with the new replacement values
- *
- * @return Column with the modified data
- *
+ * @param input_col The column to find and replace values in.
+ * @param values_to_replace The values to replace
+ * @param replacement_values The values to replace with
+ * @param stream The CUDA stream to use for operations
+ * @return The input column with specified values replaced.
  */
 std::unique_ptr<cudf::column> find_and_replace_all(cudf::column_view const& input_col,
                                                    cudf::column_view const& values_to_replace,
                                                    cudf::column_view const& replacement_values,
+                                                   cudaStream_t stream = 0,
                                                    device_memory_resource* mr = rmm::get_default_resource());
-
-} // namespace experimental
+}  // namespace detail
 } // namespace cudf
