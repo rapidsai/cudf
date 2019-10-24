e7a4c2681395422e4b01cbb071695b85d714b139
Ram (Ramakrishna Prabhu) <42624703+rgsl888prabhu@users.noreply.github.com>
2019-10-24 10:09:08 -0500

    Update copy.cpp

--- cpp/src/copying/copy.cpp
+++ cpp/src/copying/copy.cpp
@@ -20,7 +20,7 @@
 #include <cudf/column/column.hpp>
 #include <cudf/table/table.hpp>
 #include <cudf/utilities/traits.hpp>
-#include <cudf/detail/copy_detail.hpp>
+#include <cudf/detail/copy.hpp>
 
 #include <algorithm>
 
