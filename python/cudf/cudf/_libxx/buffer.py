bc9c01f2e2d6cd9843043f25e16982f5421ebc92
Ashwin Srinath <shwina@users.noreply.github.com>
2019-10-29 09:37:35 -0400

    Add Buffer.from_array_like()

--- python/cudf/cudf/_libxx/buffer.py
+++ python/cudf/cudf/_libxx/buffer.py
@@ -1,5 +1,32 @@
 class Buffer:
-    def __init__(self, ptr, size, owner=None):
+    def __init__(self, ptr=None, size=None, owner=None):
         self.ptr = ptr
         self.size = size
         self._owner = owner
+
+    @classmethod
+    def from_array_like(cls, data):
+        if hasattr(data, "__cuda_array_interface__"):
+            ptr, size = _buffer_data_from_array_interface(
+                data.__cuda_array_interface__
+            )
+            return cls.__new__(cls, ptr, size, owner=data)
+        elif isinstance(data, "__array_interface__"):
+            ptr, size = _buffer_data_from_array_interface(
+                data.__array_interface__
+            )
+            dbuf = DeviceBuffer(ptr, size)
+            return cls.__new__(cls, dbuf.ptr, dbuf.size, owner=dbuf)
+        elif isinstance(data, DeviceBuffer):
+            return cls.__new__(cls, data.ptr, data.size, owner=data)
+        else:
+            raise TypeError(
+                f"Cannot construct Buffer from {data.__class__.__name__}"
+            )
+
+
+def _buffer_data_from_array_interface(array_interface):
+    ptr = array_interface["data"][0]
+    itemsize = np.dtype(desc["typestr"]).itemsize
+    size = functools.reduce(operator.mul, array_interface["shape"])
+    return ptr, size
