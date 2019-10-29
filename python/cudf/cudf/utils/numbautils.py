f2e1b753920dde327fe6482505f9b70e8dee115e
brandon-b-miller <brmiller@nvidia.com>
2019-10-29 07:36:19 -0700

    style

--- python/cudf/cudf/utils/numbautils.py
+++ python/cudf/cudf/utils/numbautils.py
@@ -1,13 +1,12 @@
 class PatchedNumbaDeviceArray(object):
     def __init__(self, numba_ary):
         self.parent = numba_ary
-        
+
     def __getattr__(self, name):
-        if name is not '__cuda_array_interface__':
+        if name != "__cuda_array_interface__":
             return getattr(self.parent, name)
         else:
             rtn = self.parent.__cuda_array_interface__
-            if rtn.get('strides') is None:
-                rtn.pop('strides')
+            if rtn.get("strides") is None:
+                rtn.pop("strides")
             return rtn
-            
