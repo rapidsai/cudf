d100230ac10252b9731d97a0f863280b63ac5678
Nick Becker <nickb500@gmail.com>
2019-11-01 12:05:12 -0400

    changed skipna back to being kwarg only; let underlying cudf methods decide how to use skipna if it's passed in axis=0, and explicitly check for it in axis=1

--- python/cudf/cudf/core/dataframe.py
+++ python/cudf/cudf/core/dataframe.py
@@ -3772,9 +3772,7 @@ class DataFrame(object):
             )
         return self._apply_support_method("any", **kwargs)
 
-    def _apply_support_method(
-        self, method, axis=0, skipna=None, *args, **kwargs
-    ):
+    def _apply_support_method(self, method, axis=0, *args, **kwargs):
         assert axis in (None, 0, 1)
 
         if axis in (None, 0):
@@ -3794,6 +3792,8 @@ class DataFrame(object):
             return result
 
         elif axis == 1:
+            # for dask metadata compatibility
+            skipna = kwargs.pop("skipna", None)
             if skipna not in (None, True, 1):
                 msg = (
                     "Row-wise operations do not current support skipna=False."
