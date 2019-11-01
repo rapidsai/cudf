69c4ffe195f81b57065598abfd4ff50dce1f07f6
David Wendt <dwendt@nvidia.com>
2019-11-01 17:05:06 -0400

    Add to_host column utilities specialization for strings

--- cpp/tests/utilities/column_utilities.hpp
+++ cpp/tests/utilities/column_utilities.hpp
@@ -20,35 +20,36 @@
 #include <cudf/column/column_view.hpp>
 #include <cudf/column/column.hpp>
 #include <cudf/null_mask.hpp>
+#include <cudf/strings/strings_column_view.hpp>
 
 namespace cudf {
 namespace test {
 
-/**---------------------------------------------------------------------------*
+/**
  * @brief Verifies the property equality of two columns.
  *
  * @param lhs The first column
  * @param rhs The second column
- *---------------------------------------------------------------------------**/
+ */
 void expect_column_properties_equal(cudf::column_view lhs, cudf::column_view rhs);
 
-/**---------------------------------------------------------------------------*
+/**
  * @brief Verifies the element-wise equality of two columns.
  *
  * Treats null elements as equivalent.
  *
  * @param lhs The first column
  * @param rhs The second column
- *---------------------------------------------------------------------------**/
+ */
 void expect_columns_equal(cudf::column_view lhs, cudf::column_view rhs);
 
-/**---------------------------------------------------------------------------*
+/**
  * @brief Verifies the bitwise equality of two device memory buffers.
  *
  * @param lhs The first buffer
  * @param rhs The second buffer
  * @param size_bytes The number of bytes to check for equality
- *---------------------------------------------------------------------------**/
+ */
 void expect_equal_buffers(void const* lhs, void const* rhs,
                           std::size_t size_bytes);
 
@@ -76,11 +77,54 @@ std::pair<std::vector<T>, std::vector<bitmask_type>> to_host(column_view c) {
   }
 
   if (col.nullable()) {
-    size_t num_mask_elements = 
+    size_t num_mask_elements =
       std::ceil(static_cast<float>(mask_bytes) / sizeof(bitmask_type));
     host_bitmask.resize(num_mask_elements);
     CUDA_TRY(cudaMemcpy(host_bitmask.data(), col.view().null_mask(),
                         mask_bytes, cudaMemcpyDeviceToHost));
+  }
+
+  return std::make_pair(host_data, host_bitmask);
+}
+
+/**
+ * @brief Copies the data and bitmask of a `column_view` of strings
+ * column to the host.
+ *
+ * @param c the `column_view` of strings to copy from
+ * @return std::pair first is `std::vector` of `std::string`
+ * and second is the column's bitmask.
+ */
+template <>
+inline std::pair<std::vector<std::string>, std::vector<bitmask_type>> to_host(column_view c) {
+  std::size_t const mask_bytes{bitmask_allocation_size_bytes(c.size())};
+  std::vector<std::string> host_data;
+  std::vector<bitmask_type> host_bitmask;
+
+  auto strings = strings_column_view(c);
+
+  auto strings_data = cudf::strings::create_offsets(strings);
+  thrust::host_vector<char> h_chars(strings_data.first);
+  thrust::host_vector<size_type> h_offsets(strings_data.second);
+
+  if( c.has_nulls() )
+  {
+    size_t num_mask_elements =
+      std::ceil(static_cast<float>(mask_bytes) / sizeof(bitmask_type));
+    host_bitmask.resize(num_mask_elements);
+    CUDA_TRY(cudaMemcpy(host_bitmask.data(), c.null_mask(),
+                         mask_bytes, cudaMemcpyDeviceToHost));
+  }
+
+  for( size_type idx=0; idx < strings.size(); ++idx )
+  {
+      auto offset = h_offsets[idx];
+      auto length = h_offsets[idx+1] - offset;
+      auto mask_idx = idx / (sizeof(bitmask_type)*8);
+      if( !host_bitmask.empty() && (host_bitmask[mask_idx] & (1 << (idx % (sizeof(bitmask_type)*8))) > 0 ))
+          host_data.push_back( std::string( h_chars.data()+offset, length));
+      else
+          host_data.push_back( "<null>" ); // this string doesn't really matter
   }
 
   return std::make_pair(host_data, host_bitmask);
