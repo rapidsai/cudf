bb0e267ce9cb45889be9c10156a2d31aa3c4dcfe
Jake Hemstad <jhemstad@nvidia.com>
2019-11-06 10:10:17 -0600

    Define groupby object interface.

--- cpp/include/cudf/groupby.hpp
+++ cpp/include/cudf/groupby.hpp
@@ -191,5 +191,92 @@ std::pair<std::unique_ptr<table>, std::vector<std::unique_ptr<column>>> groupby(
     rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
 
 }  // namespace groupby
+
+/**
+ * @brief Groups values by keys and computes aggregations on those groups.
+ */
+class groupby {
+ public:
+  groupby() = delete;
+  ~groupby() = default;
+  groupby(groupby const&) = delete;
+  groupby(groupby&&) = delete;
+  groupby& operator=(groupby const&) = delete;
+  groupby& operator=(groupby&&) = delete;
+
+  /**
+   * @brief Construct a groupby object with the specified `keys`
+   *
+   * @note This object does *not* maintain the lifetime of `keys`. It is the
+   * users responsibility to ensure the `groupby` object does not outlive
+   * `keys`.
+   *
+   * @param keys Table whose rows act as the groupby keys
+   * @param ignore_null_keys Indicates whether rows in `keys` that contain NULL
+   * values should be ignored
+   * @param keys_are_sorted Indicates whether rows in `keys` are already sorted
+   * @param column_order If `keys_are_sorted == true`, indicates whether each
+   * column is ascending/descending. If empty, assumes all  columns are
+   * ascending. Ignored if `keys_are_sorted == false`.
+   * @param null_precedence If `keys_are_sorted == true`, indicates the ordering
+   * of null values in each column. Else, ignored. If empty, assumes all columns
+   * use `null_order::BEFORE`. Ignored if `keys_are_sorted == false`.
+   */
+  explicit groupby(table_view const& keys, bool ignore_null_keys = true,
+                   bool keys_are_sorted = false,
+                   std::vector<order> const& column_order = {},
+                   std::vector<null_order> const& null_precedence = {});
+
+  /**
+   * @brief Performs grouped aggregations on the specified values.
+   *
+   * The values to aggregate and the aggregations to perform are specifed in an
+   * `aggregation_request`. Each request contains a `column_view` of values to
+   * aggregate and a set of `aggregation`s to perform on those elements.
+   *
+   * For each `aggregation` in a request, `values[i]` will be aggregated with
+   * all other `values[j]` where rows `i` and `j` in `keys` are equivalent.
+   *
+   * The `size()` of the request column must equal `keys.num_rows()`.
+   *
+   * @throws cudf::logic_error If `requests[i].values.size() !=
+   * keys.num_rows()`.
+   *
+   * Example:
+   * ```
+   * Input:
+   * keys:     {1 2 1 3 1}
+   *           {1 2 1 4 1}
+   * request:
+   *   values: {3 1 4 9 2}
+   *   aggregations: {{SUM}, {MIN}}
+   *
+   * result:
+   *
+   * keys:  {3 1 2}
+   *        {4 1 2}
+   * values:
+   *   SUM: {9 9 1}
+   *   MIN: {9 2 1}
+   * ```
+   *
+   * @param requests The set of columns to aggregate and the aggregations to
+   * perform
+   * @param mr Memory resource used to allocate the returned table and columns
+   * @return Pair containing the table with each groups unique key and
+   * columns containing the results of the requested aggregation for each group
+   * in the same order as was specified in `requests`.
+   */
+  std::pair<std::unique_ptr<table>, std::vector<std::unique_ptr<column>>>
+  aggregate(
+      std::vector<aggregation_request> const& requests,
+      rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
+
+ private:
+  table_view _keys;
+  bool drop_null_keys{true};
+  bool keys_are_sorted{false};
+};
+
 }  // namespace experimental
 }  // namespace cudf
