fcb940e402e7db4573c31f907ec274115c80bec7
Devavret Makkar <dmakkar@nvidia.com>
2019-11-08 04:12:07 +0530

    Port rest of the binops integration tests

--- cpp/tests/binaryop/integration/assert-binops.h
+++ cpp/tests/binaryop/integration/assert-binops.h
@@ -43,9 +43,9 @@ void ASSERT_BINOP(column_view const& out,
     auto out_h = cudf::test::to_host<TypeOut>(out);
     auto out_data = out_h.first;
 
-    ASSERT_TRUE(out_data.size() == rhs_data.size());
+    ASSERT_EQ(out_data.size(), rhs_data.size());
     for (size_t index = 0; index < out_data.size(); ++index) {
-        ASSERT_TRUE(out_data[index] == (TypeOut)(ope(lhs_h, rhs_data[index])));
+        ASSERT_EQ(out_data[index], (TypeOut)(ope(lhs_h, rhs_data[index])));
     }
 
     auto rhs_valid = rhs_h.second;
@@ -53,9 +53,9 @@ void ASSERT_BINOP(column_view const& out,
 
     uint32_t lhs_valid = (lhs.is_valid() 
                          ? std::numeric_limits<bitmask_type>::max() : 0);
-    ASSERT_TRUE(out_valid.size() == rhs_valid.size());
+    ASSERT_EQ(out_valid.size(), rhs_valid.size());
     for (decltype(out_valid.size()) index = 0; index < out_valid.size(); ++index) {
-        ASSERT_TRUE(out_valid[index] == (lhs_valid & rhs_valid[index]));
+        ASSERT_EQ(out_valid[index], (lhs_valid & rhs_valid[index]));
     }
 }
 
@@ -71,9 +71,9 @@ void ASSERT_BINOP(column_view const& out,
     auto out_h = cudf::test::to_host<TypeOut>(out);
     auto out_data = out_h.first;
 
-    ASSERT_TRUE(out_data.size() == lhs_data.size());
+    ASSERT_EQ(out_data.size(), lhs_data.size());
     for (size_t index = 0; index < out_data.size(); ++index) {
-        ASSERT_TRUE(out_data[index] == (TypeOut)(ope(lhs_data[index], rhs_h)));
+        ASSERT_EQ(out_data[index], (TypeOut)(ope(lhs_data[index], rhs_h)));
     }
 
     auto lhs_valid = lhs_h.second;
@@ -81,9 +81,9 @@ void ASSERT_BINOP(column_view const& out,
 
     uint32_t rhs_valid = (rhs.is_valid() 
                          ? std::numeric_limits<bitmask_type>::max() : 0);
-    ASSERT_TRUE(out_valid.size() == lhs_valid.size());
+    ASSERT_EQ(out_valid.size(), lhs_valid.size());
     for (decltype(out_valid.size()) index = 0; index < out_valid.size(); ++index) {
-        ASSERT_TRUE(out_valid[index] == (lhs_valid[index] & rhs_valid));
+        ASSERT_EQ(out_valid[index], (lhs_valid[index] & rhs_valid));
     }
 }
 
@@ -99,57 +99,57 @@ void ASSERT_BINOP(column_view const& out,
     auto out_h = cudf::test::to_host<TypeOut>(out);
     auto out_data = out_h.first;
 
-    ASSERT_TRUE(out_data.size() == lhs_data.size());
-    ASSERT_TRUE(out_data.size() == rhs_data.size());
+    ASSERT_EQ(out_data.size(), lhs_data.size());
+    ASSERT_EQ(out_data.size(), rhs_data.size());
     for (size_t index = 0; index < out_data.size(); ++index) {
-        ASSERT_TRUE(out_data[index] == (TypeOut)(ope(lhs_data[index], rhs_data[index])));
+        ASSERT_EQ(out_data[index], (TypeOut)(ope(lhs_data[index], rhs_data[index])));
     }
 
     auto lhs_valid = lhs_h.second;
     auto rhs_valid = rhs_h.second;
     auto out_valid = out_h.second;
 
-    ASSERT_TRUE(out_valid.size() == lhs_valid.size());
-    ASSERT_TRUE(out_valid.size() == rhs_valid.size());
+    ASSERT_EQ(out_valid.size(), lhs_valid.size());
+    ASSERT_EQ(out_valid.size(), rhs_valid.size());
     for (decltype(out_valid.size()) index = 0; index < out_valid.size(); ++index) {
-        ASSERT_TRUE(out_valid[index] == (lhs_valid[index] & rhs_valid[index]));
+        ASSERT_EQ(out_valid[index], (lhs_valid[index] & rhs_valid[index]));
     }
 }
 
-    // /**
-    //  * According to CUDA Programming Guide, 'E.1. Standard Functions', 'Table 7 - Double-Precision
-    //  * Mathematical Standard Library Functions with Maximum ULP Error'
-    //  * The pow function has 2 (full range) maximum ulp error.
-    //  */
-    // template <typename TypeOut, typename TypeLhs, typename TypeRhs>
-    // void ASSERT_BINOP(cudf::test::column_wrapper<TypeOut>& out,
-    //                   cudf::test::column_wrapper<TypeLhs>& lhs,
-    //                   cudf::test::column_wrapper<TypeRhs>& rhs,
-    //                   cudf::library::operation::Pow<TypeOut, TypeLhs, TypeRhs>&& ope) {
-    //     auto lhs_h = lhs.to_host();
-    //     auto lhs_data = std::get<0>(lhs_h);
-    //     auto rhs_h = rhs.to_host();
-    //     auto rhs_data = std::get<0>(rhs_h);
-    //     auto out_h = out.to_host();
-    //     auto out_data = std::get<0>(out_h);
+/**
+ * According to CUDA Programming Guide, 'E.1. Standard Functions', 'Table 7 - Double-Precision
+ * Mathematical Standard Library Functions with Maximum ULP Error'
+ * The pow function has 2 (full range) maximum ulp error.
+ */
+template <typename TypeOut, typename TypeLhs, typename TypeRhs>
+void ASSERT_BINOP(column_view const& out,
+                  column_view const& lhs,
+                  column_view const& rhs,
+                  cudf::library::operation::Pow<TypeOut, TypeLhs, TypeRhs>&& ope) {
+    auto lhs_h = cudf::test::to_host<TypeLhs>(lhs);
+    auto lhs_data = lhs_h.first;
+    auto rhs_h = cudf::test::to_host<TypeRhs>(rhs);
+    auto rhs_data = rhs_h.first;
+    auto out_h = cudf::test::to_host<TypeOut>(out);
+    auto out_data = out_h.first;
 
-    //     const int ULP = 2.0;
-    //     ASSERT_TRUE(out_data.size() == lhs_data.size());
-    //     ASSERT_TRUE(out_data.size() == rhs_data.size());
-    //     for (decltype(out_data.size()) index = 0; index < out_data.size(); ++index) {
-    //         ASSERT_TRUE(abs(out_data[index] - (TypeOut)(ope(lhs_data[index], rhs_data[index]))) < ULP);
-    //     }
+    const int ULP = 2.0;
+    ASSERT_EQ(out_data.size(), lhs_data.size());
+    ASSERT_EQ(out_data.size(), rhs_data.size());
+    for (decltype(out_data.size()) index = 0; index < out_data.size(); ++index) {
+        ASSERT_TRUE(abs(out_data[index] - (TypeOut)(ope(lhs_data[index], rhs_data[index]))) < ULP);
+    }
 
-    //     auto lhs_valid = std::get<1>(lhs_h);
-    //     auto rhs_valid = std::get<1>(rhs_h);
-    //     auto out_valid = std::get<1>(out_h);
+    auto lhs_valid = lhs_h.second;
+    auto rhs_valid = rhs_h.second;
+    auto out_valid = out_h.second;
 
-    //     ASSERT_TRUE(out_valid.size() == lhs_valid.size());
-    //     ASSERT_TRUE(out_valid.size() == rhs_valid.size());
-    //     for (decltype(out_valid.size()) index = 0; index < out_valid.size(); ++index) {
-    //         ASSERT_TRUE(out_valid[index] == (lhs_valid[index] & rhs_valid[index]));
-    //     }
-    // }
+    ASSERT_EQ(out_valid.size(), lhs_valid.size());
+    ASSERT_EQ(out_valid.size(), rhs_valid.size());
+    for (decltype(out_valid.size()) index = 0; index < out_valid.size(); ++index) {
+        ASSERT_EQ(out_valid[index], (lhs_valid[index] & rhs_valid[index]));
+    }
+}
 
 } // namespace binop
 } // namespace test
