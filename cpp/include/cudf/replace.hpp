3e1aead6f0702128deecf584bafa0cf05ff54bb6
James Wyles <jwyles@nvidia.com>
2019-11-08 16:18:47 -0700

    Adding back in normalize_nans_and_zeroes functions removed in merge conflict

--- cpp/include/cudf/replace.hpp
+++ cpp/include/cudf/replace.hpp
@@ -80,4 +80,32 @@ std::unique_ptr<cudf::column> find_and_replace_all(cudf::column_view const& inpu
                                                    rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
 
 } // namespace experimental
+
+/**
+ * @brief Copies from a column of floating-point elements and replaces `-NaN` and `-0.0` with `+NaN` and `+0.0`, respectively.
+ *
+ * Converts floating point values from @p input using the following rules:
+ *        Convert  -NaN  -> NaN
+ *        Convert  -0.0  -> 0.0
+ *
+ * @throws cudf::logic_error if column does not have floating point data type.
+ * @param[in] Column of floating-point elements to copy and normalize
+ * @param[in] device_memory_resource allocator for allocating output data
+ *
+ * @returns new column with the modified data
+ */
+std::unique_ptr<column> normalize_nans_and_zeros( column_view const& input,
+                                                  rmm::mr::device_memory_resource *mr = rmm::mr::get_default_resource());
+
+/**
+ * @brief Modifies a column of floating-point elements to replace all `-NaN` and `-0.0` with `+NaN` and `+0.0`, respectively.
+ *
+ * Converts floating point values from @p in_out using the following rules:
+ *        Convert  -NaN  -> NaN
+ *        Convert  -0.0  -> 0.0
+ *
+ * @throws cudf::logic_error if column does not have floating point data type.
+ * @param[in, out] Column of floating-point elements to normalize
+ */
+void normalize_nans_and_zeros(mutable_column_view& in_out);
 } // namespace cudf
--- cpp/src/replace/replace.cu
+++ cpp/src/replace/replace.cu
@@ -37,6 +37,7 @@
 #include <cudf/column/column.hpp>
 #include <bitmask/legacy/legacy_bitmask.hpp>
 #include <bitmask/legacy/bit_mask.cuh>
+#include <cudf/column/column_device_view.cuh>
 
 using bit_mask::bit_mask_t;
 
@@ -561,3 +562,128 @@ std::unique_ptr<cudf::column> replace_nulls(cudf::column_view const& input,
 }
 } //end experimental
 }  // namespace cudf
+
+namespace {  // anonymous
+
+using namespace cudf;
+
+template <typename T>
+struct normalize_nans_and_zeros_lambda {
+   column_device_view in;
+   T __device__ operator()(size_type i)
+   {
+      auto e = in.element<T>(i);
+      if (isnan(e)) {
+         return std::numeric_limits<T>::quiet_NaN();
+      }
+      if (T{0.0} == e) {
+         return T{0.0};
+      }
+      return e;
+   }
+};
+
+/* --------------------------------------------------------------------------*/
+/**
+* @brief Functor called by the `type_dispatcher` in order to invoke and instantiate
+*        `normalize_nans_and_zeros` with the appropriate data types.
+*/
+/* ----------------------------------------------------------------------------*/
+struct normalize_nans_and_zeros_kernel_forwarder {
+   // floats and doubles. what we really care about.
+   template <typename T, std::enable_if_t<std::is_floating_point<T>::value>* = nullptr>
+   void operator()(  column_device_view in,
+                     mutable_column_device_view out,
+                     cudaStream_t stream)
+   {
+      thrust::transform(rmm::exec_policy(stream)->on(stream),
+                        thrust::make_counting_iterator(0),
+                        thrust::make_counting_iterator(in.size()),
+                        out.head<T>(), normalize_nans_and_zeros_lambda<T>{in});
+   }
+
+   // if we get in here for anything but a float or double, that's a problem.
+   template <typename T, std::enable_if_t<not std::is_floating_point<T>::value>* = nullptr>
+   void operator()(  column_device_view in,
+                     mutable_column_device_view out,
+                     cudaStream_t stream)
+   {
+      CUDF_FAIL("Unexpected non floating-point type.");
+   }
+};
+
+} // end anonymous namespace
+
+namespace cudf {
+namespace detail {
+
+void normalize_nans_and_zeros(mutable_column_view in_out,
+                              cudaStream_t stream = 0)
+{
+   if(in_out.size() == 0){
+      return;
+   }
+   CUDF_EXPECTS(in_out.type() == data_type(FLOAT32) || in_out.type() == data_type(FLOAT64), "Expects float or double input");
+
+   // wrapping the in_out data in a column_view so we can call the same lower level code.
+   // that we use for the non in-place version.
+   column_view input = in_out;
+
+   // to device. unique_ptr which gets automatically cleaned up when we leave
+   auto device_in = column_device_view::create(input);
+
+   // from device. unique_ptr which gets automatically cleaned up when we leave.
+   auto device_out = mutable_column_device_view::create(in_out);
+
+    // invoke the actual kernel.
+   cudf::experimental::type_dispatcher(input.type(),
+                                       normalize_nans_and_zeros_kernel_forwarder{},
+                                       *device_in,
+                                       *device_out,
+                                       stream);
+}
+
+}  // namespace detail
+
+/*
+ * @brief Makes all NaNs and zeroes positive.
+ *
+ * Converts floating point values from @p input using the following rules:
+ *        Convert  -NaN  -> NaN
+ *        Convert  -0.0  -> 0.0
+ *
+ * @throws cudf::logic_error if column does not have floating point data type.
+ * @param[in] column_view representing input data
+ * @param[in] device_memory_resource allocator for allocating output data
+ *
+ * @returns new column with the modified data
+ */
+std::unique_ptr<column> normalize_nans_and_zeros( column_view const& input,
+                                                  rmm::mr::device_memory_resource *mr)
+{
+   // output. copies the input
+   std::unique_ptr<column> out = std::make_unique<column>(input, (cudaStream_t)0, mr);
+   // from device. unique_ptr which gets automatically cleaned up when we leave.
+   auto out_view = out->mutable_view();
+
+   detail::normalize_nans_and_zeros(out_view, 0);
+
+   return out;
+}
+
+/*
+ * @brief Makes all Nans and zeroes positive.
+ *
+ * Converts floating point values from @p in_out using the following rules:
+ *        Convert  -NaN  -> NaN
+ *        Convert  -0.0  -> 0.0
+ *
+ * @throws cudf::logic_error if column does not have floating point data type.
+ * @param[in, out] mutable_column_view representing input data. data is processed in-place
+ */
+void normalize_nans_and_zeros(mutable_column_view& in_out)
+{
+   detail::normalize_nans_and_zeros(in_out, 0);
+}
+
+}
