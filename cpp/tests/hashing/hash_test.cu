745ce9253eef593f86033c0732162ef93af372b4
Trevor Smith <trevorsm7@gmail.com>
2019-11-07 20:41:41 -0800

    Add multi-type hash test

--- cpp/tests/hashing/hash_test.cu
+++ cpp/tests/hashing/hash_test.cu
@@ -14,3 +14,110 @@
  * limitations under the License.
  */
 #include <cudf/hashing.hpp>
+#include <tests/utilities/base_fixture.hpp>
+#include <tests/utilities/type_lists.hpp>
+#include <tests/utilities/column_utilities.hpp>
+#include <tests/utilities/column_wrapper.hpp>
+
+namespace {
+
+template <typename T>
+void expect_symmetry(cudf::column_view const& col)
+{
+  std::vector<T> host(col.size());
+  cudaMemcpy(host.data(), col.data<T>(),
+    col.size() * sizeof(T), cudaMemcpyDefault);
+  CUDF_EXPECTS(std::equal(host.begin(), host.begin() + (col.size() / 2),
+    host.rbegin()), "Expected hash to have symmetrical equality");
+}
+
+}
+
+class HashTest : public cudf::test::BaseFixture {};
+
+TEST_F(HashTest, MultiValueMurmur)
+{
+  using cudf::test::fixed_width_column_wrapper;
+  using cudf::test::strings_column_wrapper;
+  using cudf::experimental::bool8;
+
+  std::vector<cudf::column_view> columns;
+
+  auto const strings_col = strings_column_wrapper(
+    {"",
+    "The quick brown fox",
+    "jumps over the lazy dog.",
+    "All work and no play makes Jack a dull boy",
+    "All work and no play makes Jack a dull boy",
+    "jumps over the lazy dog.",
+    "The quick brown fox",
+    ""});
+  columns.push_back(strings_col);
+
+  auto const ints_col = fixed_width_column_wrapper<int32_t>(
+    {0, 123, -456789, 123456789, 123456789, -456789, 123, 0});
+  columns.push_back(ints_col);
+
+  auto const bools_col = fixed_width_column_wrapper<bool8>(
+    {0, 1, 10, 255, 255, 1, 1, 0});
+  columns.push_back(bools_col);
+
+  auto const secs_col = fixed_width_column_wrapper<cudf::timestamp_s>(
+    {0, -123, 456, 123456, 123456, 456, -123, 0});
+  columns.push_back(secs_col);
+
+  // Expect output to have symmetrical equality
+  auto const input = cudf::table_view(columns);
+  auto const output = cudf::hash(input, cudf::hash_func::MURMUR3, {});
+  expect_symmetry<int32_t>(output->view());
+}
+
+TEST_F(HashTest, MultiValueNullsMurmur)
+{
+  using cudf::test::fixed_width_column_wrapper;
+  using cudf::test::strings_column_wrapper;
+  using cudf::experimental::bool8;
+
+  std::vector<cudf::column_view> columns;
+
+  auto const strings_col = strings_column_wrapper(
+    {"",
+    "The quick brown fox",
+    "jumps over the lazy dog.",
+    "All work and no play makes Jack a dull boy",
+    "All work and no play makes Jack a dull boy",
+    "jumps over the lazy dog.",
+    "The quick brown fox",
+    ""},
+    {0, 1, 1, 0, 0, 1, 1, 0});
+  columns.push_back(strings_col);
+
+  auto const ints_col = fixed_width_column_wrapper<int32_t>(
+    {0, 123, -456789, 123456789, 123456789, -456789, 123, 0},
+    {1, 0, 1, 0, 0, 1, 0, 1});
+  columns.push_back(ints_col);
+
+  auto const bools_col = fixed_width_column_wrapper<bool8>(
+    {0, 1, 10, 255, 255, 1, 1, 0},
+    {1, 1, 1, 0, 0, 1, 1, 1});
+  columns.push_back(bools_col);
+
+  auto const secs_col = fixed_width_column_wrapper<cudf::timestamp_s>(
+    {0, -123, 456, 123456, 123456, 456, -123, 0},
+    {1, 0, 1, 1, 1, 1, 0, 1});
+  columns.push_back(secs_col);
+
+  // Expect output to have symmetrical equality
+  auto const input = cudf::table_view(columns);
+  auto const output = cudf::hash(input, cudf::hash_func::MURMUR3, {});
+  expect_symmetry<int32_t>(output->view());
+}
+
+/*template <typename T>
+class HashTestTyped : public cudf::test::BaseFixture {};
+
+TYPED_TEST_CASE(HashTestTyped, cudf::test::FixedWidthTypes);
+
+TYPED_TEST(HashTestTyped, SingleValue)
+{
+}*/
