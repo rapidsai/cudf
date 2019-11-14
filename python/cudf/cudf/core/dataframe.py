9cd0bdad13a1aa49137469ee18b652614fa39dfd
rjzamora <rzamora217@gmail.com>
2019-11-14 07:36:35 -0800

    avoid iloc call

--- python/cudf/cudf/core/dataframe.py
+++ python/cudf/cudf/core/dataframe.py
@@ -4069,9 +4069,13 @@ class DataFrame(object):
         else:
             index = None
 
+        # Make sure every column has the correct name
+        for col, name in zip(self._columns, self.columns):
+            col.name = name
+
         # scatter_to_frames wants a list of columns
         tables = libcudf.copying.scatter_to_frames(
-            self.iloc[:]._columns, map_index._column, index
+            self._columns, map_index._column, index
         )
 
         if map_size:
