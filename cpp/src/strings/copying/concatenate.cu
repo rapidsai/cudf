c26eaa7927dc57aad35bb051e6be9e4243476f45
David Wendt <dwendt@nvidia.com>
2019-11-14 12:33:56 -0500

    Concatenate columns of strings function

---
+++ cpp/src/strings/copying/concatenate.cu
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <bitmask/legacy/valid_if.cuh>
+#include <cudf/column/column_factories.hpp>
+#include <cudf/column/column_device_view.cuh>
+#include <cudf/strings/detail/concatenate.hpp>
+#include <cudf/strings/strings_column_view.hpp>
+#include <cudf/strings/string_view.cuh>
+#include "../utilities.hpp"
+#include "../utilities.cuh"
+
+#include <rmm/thrust_rmm_allocator.h>
+#include <thrust/for_each.h>
+#include <thrust/transform_scan.h>
+#include <thrust/transform_reduce.h>
+
+namespace cudf
+{
+namespace strings
+{
+namespace detail
+{
+
+std::unique_ptr<column> concatenate_vertically( std::vector<strings_column_view> const& strings_columns,
+                                                rmm::mr::device_memory_resource* mr,
+                                                cudaStream_t stream )
+{
+    if( strings_columns.size() == 0 )
+        return make_empty_strings_column(mr,stream);
+
+    size_t strings_count = thrust::transform_reduce( strings_columns.begin(), strings_columns.end(),
+        [] (auto scv) { return scv.size(); }, static_cast<size_t>(0), thrust::plus<size_t>());
+    CUDF_EXPECTS( strings_count < std::numeric_limits<size_type>::max(), "total number of strings is too large for cudf column" );
+    if( strings_count == 0 )
+        return make_empty_strings_column(mr,stream);
+    size_t total_bytes = thrust::transform_reduce( strings_columns.begin(), strings_columns.end(),
+        [] (auto scv) { return scv.chars().size(); }, static_cast<size_t>(0), thrust::plus<size_t>());
+    CUDF_EXPECTS( total_bytes < std::numeric_limits<size_type>::max(), "total size of strings is too large for cudf column" );
+
+    auto execpol = rmm::exec_policy(stream);
+
+    // create chars column
+    auto chars_column = make_numeric_column( data_type{INT8}, total_bytes, mask_state::UNALLOCATED, stream, mr);
+    auto chars_view = chars_column->mutable_view();
+    auto d_new_chars = chars_view.data<char>();
+
+    // create offsets column
+    auto offsets_column = make_numeric_column( data_type{INT32}, strings_count + 1, mask_state::UNALLOCATED, stream, mr);
+    auto offsets_view = offsets_column->mutable_view();
+    auto d_new_offsets = offsets_view.data<int32_t>();
+
+    // copy over the data for all the columns
+    ++d_new_offsets;
+    size_type offset_adjust = 0;
+    for( auto column = strings_columns.begin(); column != strings_columns.end(); ++column )
+    {
+        column_view offsets_child = column->offsets();
+        column_view chars_child = column->chars();
+        size_type size = column->size();
+
+        auto d_offsets = offsets_child.data<int32_t>();
+        CUDA_TRY(cudaMemcpyAsync( d_new_offsets, d_offsets+1, size*sizeof(int32_t), cudaMemcpyDeviceToDevice, stream ));
+
+        // adjust offsets
+        thrust::for_each_n( execpol->on(stream), thrust::make_counting_iterator<size_type>(0), size,
+            [d_new_offsets, offset_adjust] __device__ (size_type idx) { d_new_offsets[idx] += offset_adjust; });
+
+        auto d_chars = chars_child.data<char*>();
+        auto bytes = chars_child.size();
+        CUDA_TRY(cudaMemcpyAsync( d_new_chars, d_chars, bytes, cudaMemcpyDeviceToDevice, stream ));
+        offset_adjust += bytes;
+        d_new_chars += bytes;
+        d_new_offsets += size;
+    }
+    CUDA_TRY(cudaMemsetAsync( offsets_view.data<int32_t>(), 0, sizeof(int32_t), stream));
+
+    // create empty null mask -- caller should be setting this
+    rmm::device_buffer null_mask = create_null_mask( strings_count, ALL_VALID, stream,mr );
+
+    return make_strings_column(strings_count, std::move(offsets_column), std::move(chars_column),
+                               0, std::move(null_mask), stream, mr);
+}
+
+} // namespace detail
+} // namespace strings
+} // namespace cudf
---
+++ cpp/tests/strings/concatenate_tests.cu
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cudf/column/column_factories.hpp>
+#include <cudf/scalar/scalar.hpp>
+#include <cudf/strings/strings_column_view.hpp>
+#include <cudf/strings/detail/concatenate.hpp>
+
+#include <tests/utilities/base_fixture.hpp>
+#include <tests/utilities/column_wrapper.hpp>
+#include <tests/utilities/column_utilities.hpp>
+#include "./utilities.h"
+
+#include <vector>
+#include <gmock/gmock.h>
+
+
+struct StringsConcatenateTest : public cudf::test::BaseFixture {};
+
+TEST_F(StringsConcatenateTest, Concatenate)
+{
+    std::vector<const char*> h_strings1{ "aaa", "bb", "", "cccc", "d", "ééé" };
+    cudf::test::strings_column_wrapper strings1( h_strings1.begin(), h_strings1.end() );
+    std::vector<const char*> h_strings2{ "ff", "gggg", "", "h" };
+    cudf::test::strings_column_wrapper strings2( h_strings2.begin(), h_strings2.end() );
+
+    std::vector<cudf::strings_column_view> strings_columns;
+    strings_columns.push_back(cudf::strings_column_view(strings1));
+    strings_columns.push_back(cudf::strings_column_view(strings2));
+
+    {
+        auto results = cudf::strings::detail::concatenate_vertically(strings_columns);
+
+        std::vector<const char*> h_expected{ "aaa", "bb", "", "cccc", "d", "ééé", "ff", "gggg", "", "h" };
+        cudf::test::strings_column_wrapper expected( h_expected.begin(), h_expected.end(),
+            thrust::make_transform_iterator( h_expected.begin(), [] (auto str) { return str!=nullptr; }));
+        cudf::test::expect_columns_equal(*results,expected);
+    }
+}
+
+TEST_F(StringsConcatenateTest, ZeroSizeStringsColumns)
+{
+    cudf::column_view zero_size_strings_column( cudf::data_type{cudf::STRING}, 0, nullptr, nullptr, 0);
+    std::vector<cudf::strings_column_view> strings_columns;
+    strings_columns.push_back(cudf::strings_column_view(zero_size_strings_column));
+    strings_columns.push_back(cudf::strings_column_view(zero_size_strings_column));
+    auto results = cudf::strings::detail::concatenate_vertically(strings_columns);
+    cudf::test::expect_strings_empty(results->view());
+}
