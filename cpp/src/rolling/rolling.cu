ca1de47245dbab4c83f501fb141ab42774abf32f
Mark Harris <mharris@nvidia.com>
2019-11-14 19:29:42 -0800

    Deduplicate by using iterators for window sizes

--- cpp/src/rolling/rolling.cu
+++ cpp/src/rolling/rolling.cu
@@ -26,46 +26,26 @@ namespace experimental {
 
 namespace detail {
 
-// Applies a fixed-size rolling window function to the values in a column.
+// Applies a rolling window function to the values in a column.
+template <typename WindowIterator>
 std::unique_ptr<column> rolling_window(column_view const& input,
-                                       size_type window,
-                                       size_type forward_window,
+                                       WindowIterator window_begin,
+                                       WindowIterator window_end,
+                                       WindowIterator forward_window_begin,
                                        size_type min_periods,
                                        rolling_operator op,
                                        rmm::mr::device_memory_resource* mr,
                                        cudaStream_t stream = 0)
 {
-  if (input.size() == 0)
-    return cudf::make_numeric_column(data_type{INT32}, 0);
-
   return cudf::make_numeric_column(data_type{INT32}, 0);
 }
 
-// Applies a variable-size rolling window function to the values in a column.
-std::unique_ptr<column> rolling_window(column_view const& input,
-                                       column_view const& window,
-                                       column_view const& forward_window,
-                                       size_type min_periods,
-                                       rolling_operator op,
-                                       rmm::mr::device_memory_resource* mr,
-                                       cudaStream_t stream = 0)
-{
-  if (input.size() == 0 || window.size() == 0)
-    return cudf::make_numeric_column(data_type{INT32}, 0);
-  
-  CUDF_EXPECTS(window.type().id() == INT32 && forward_window.type().id() == INT32,
-               "window/forward_window must have INT32 type");
-
-  CUDF_EXPECTS(window.size() != input.size() && forward_window.size() != input.size(),
-               "window/forward_window size must match input size");
-
-  return cudf::make_numeric_column(data_type{INT32}, 0);
-}
-
-// Applies a fixed-size user-defined rolling window function to the values in a column.
+// Applies a user-defined rolling window function to the values in a column.
+template <typename WindowIterator>
 std::unique_ptr<column> rolling_window(column_view const &input,
-                                       size_type window,
-                                       size_type forward_window,
+                                       WindowIterator window_begin,
+                                       WindowIterator window_end,
+                                       WindowIterator forward_window_begin,
                                        size_type min_periods,
                                        std::string const& user_defined_aggregator,
                                        rolling_operator agg_op,
@@ -73,32 +53,6 @@ std::unique_ptr<column> rolling_window(column_view const &input,
                                        rmm::mr::device_memory_resource* mr,
                                        cudaStream_t stream = 0)
 {
-  if (input.size() == 0)
-    return cudf::make_numeric_column(data_type{INT32}, 0);
-
-  return cudf::make_numeric_column(data_type{INT32}, 0);
-}
-
-// Applies a variable-size user-defined rolling window function to the values in a column.
-std::unique_ptr<column> rolling_window(column_view const &input,
-                                       column_view const& window,
-                                       column_view const& forward_window,
-                                       size_type min_periods,
-                                       std::string const& user_defined_aggregator,
-                                       rolling_operator agg_op,
-                                       data_type output_type,
-                                       rmm::mr::device_memory_resource* mr,
-                                       cudaStream_t stream = 0)
-{
-  if (input.size() == 0 || window.size() == 0 || forward_window.size() == 0)
-    return cudf::make_numeric_column(data_type{INT32}, 0);
-  
-  CUDF_EXPECTS(window.type().id() == INT32 && forward_window.type().id() == INT32,
-               "window/forward_window must have INT32 type");
-
-  CUDF_EXPECTS(window.size() != input.size() && forward_window.size() != input.size(),
-               "window/forward_window size must match input size");
-
   return cudf::make_numeric_column(data_type{INT32}, 0);
 }
 
@@ -112,8 +66,12 @@ std::unique_ptr<column> rolling_window(column_view const& input,
                                        rolling_operator op,
                                        rmm::mr::device_memory_resource* mr)
 {
-  return cudf::experimental::detail::rolling_window(input, window, forward_window, min_periods,
-                                                    op, mr, 0);
+  auto window_begin = thrust::make_constant_iterator(window);
+  auto window_end = thrust::make_constant_iterator(window);
+  auto forward_window_begin = thrust::make_constant_iterator(forward_window);
+
+  return cudf::experimental::detail::rolling_window(input, window_begin, window_end,
+                                                    forward_window_begin, min_periods, op, mr, 0);
 }
 
 // Applies a variable-size rolling window function to the values in a column.
@@ -124,7 +82,18 @@ std::unique_ptr<column> rolling_window(column_view const& input,
                                        rolling_operator op,
                                        rmm::mr::device_memory_resource* mr)
 {
-  return cudf::experimental::detail::rolling_window(input, window, forward_window, min_periods,
+  if (input.size() == 0 || window.size() == 0)
+    return cudf::make_numeric_column(data_type{INT32}, 0);
+  
+  CUDF_EXPECTS(window.type().id() == INT32 && forward_window.type().id() == INT32,
+               "window/forward_window must have INT32 type");
+
+  CUDF_EXPECTS(window.size() != input.size() && forward_window.size() != input.size(),
+               "window/forward_window size must match input size");
+
+  return cudf::experimental::detail::rolling_window(input, window.begin<size_type>(),
+                                                    window.end<size_type>(),
+                                                    forward_window.begin<size_type>(), min_periods,
                                                     op, mr, 0);
 }
 
@@ -138,7 +107,12 @@ std::unique_ptr<column> rolling_window(column_view const &input,
                                        data_type output_type,
                                        rmm::mr::device_memory_resource* mr)
 {
-  return cudf::experimental::detail::rolling_window(input, window, forward_window, min_periods,
+  auto window_begin = thrust::make_constant_iterator(window);
+  auto window_end = thrust::make_constant_iterator(window);
+  auto forward_window_begin = thrust::make_constant_iterator(forward_window);
+
+  return cudf::experimental::detail::rolling_window(input, window_begin, window_end, 
+                                                    forward_window_begin, min_periods,
                                                     user_defined_aggregator, op, output_type, mr, 0);
 }
 
@@ -152,7 +126,18 @@ std::unique_ptr<column> rolling_window(column_view const &input,
                                        data_type output_type,
                                        rmm::mr::device_memory_resource* mr)
 {
-  return cudf::experimental::detail::rolling_window(input, window, forward_window, min_periods,
+  if (input.size() == 0 || window.size() == 0)
+    return cudf::make_numeric_column(data_type{INT32}, 0);
+  
+  CUDF_EXPECTS(window.type().id() == INT32 && forward_window.type().id() == INT32,
+               "window/forward_window must have INT32 type");
+
+  CUDF_EXPECTS(window.size() != input.size() && forward_window.size() != input.size(),
+               "window/forward_window size must match input size");
+
+  return cudf::experimental::detail::rolling_window(input, window.begin<size_type>(),
+                                                    window.end<size_type>(),
+                                                    forward_window.begin<size_type>(), min_periods,
                                                     user_defined_aggregator, op, output_type, mr, 0);
 }
 
