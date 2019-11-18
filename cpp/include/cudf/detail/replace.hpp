b73b9c83c4c309f1768934dbf83a18cb8599d954
James Wyles <37152791+jwyles@users.noreply.github.com>
2019-11-18 10:28:43 -0700

    Update cpp/include/cudf/detail/replace.hpp
    Co-Authored-By: Mark Harris <mharris@nvidia.com>

--- cpp/include/cudf/detail/replace.hpp
+++ cpp/include/cudf/detail/replace.hpp
@@ -56,7 +56,7 @@ std::unique_ptr<column> replace_nulls(column_view const& input,
   *
   * @returns Copy of `input` with null values replaced by `replacement`.
   */
-std::unique_ptr<cudf::column> replace_nulls(cudf::column_view const& input,
+std::unique_ptr<column> replace_nulls(column_view const& input,
                                             cudf::scalar const* replacement,
                                             rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource(),
                                             cudaStream_t stream = 0);
