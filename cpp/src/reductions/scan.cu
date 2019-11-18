aefd1c5cd92a065c9015eafa98d69504276920fb
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2019-11-18 16:57:58 +0530

    restructure scan code

--- cpp/src/reductions/scan.cu
+++ cpp/src/reductions/scan.cu
@@ -18,65 +18,8 @@ namespace experimental {
 
 namespace detail {
 
-template <typename T, typename Op>
-struct Scan {
-    static
-    void call(const column_view input, mutable_column_view output,
-                  bool inclusive, cudaStream_t stream,
-                  rmm::mr::device_memory_resource* mr)
-    {
-        size_t size = input.size();
-
-        // Prepare temp storage
-        void *temp_storage = NULL;
-        size_t temp_storage_bytes = 0;
-
-        if (input.has_nulls()) {
-          auto d_input = column_device_view::create(input, stream);
-          auto it = make_null_replacement_iterator(*d_input, Op::template identity<T>());
-          auto scan_function = (inclusive ? inclusive_scan<decltype(it)> : exclusive_scan<decltype(it)>);
-          scan_function(temp_storage, temp_storage_bytes,
-              it, output.data<T>(), size, stream);
-          rmm::device_buffer buff(temp_storage_bytes, stream, mr);
-          temp_storage = buff.data(); 
-
-          scan_function(temp_storage, temp_storage_bytes,
-              it, output.data<T>(), size, stream);
-        } else {
-          auto it = input.data<T>();  //since scan is for arithmetic types only
-          auto scan_function = (inclusive ? inclusive_scan<decltype(it)> : exclusive_scan<decltype(it)>);
-          scan_function(temp_storage, temp_storage_bytes,
-              it, output.data<T>(), size, stream);
-          rmm::device_buffer buff(temp_storage_bytes, stream, mr);
-          temp_storage = buff.data(); 
-
-          scan_function(temp_storage, temp_storage_bytes,
-              it, output.data<T>(), size, stream);
-        }
-    }
-
-    template <typename InputIterator>
-    static void exclusive_scan(void *&temp_storage, size_t &temp_storage_bytes,
-        const InputIterator input, T* output, size_t size, cudaStream_t stream)
-    {
-        //TODO benchmark between thrust and cub reduce and scan
-        cub::DeviceScan::ExclusiveScan(temp_storage, temp_storage_bytes,
-            input, output, Op{}, Op::template identity<T>(), size, stream);
-        CUDA_CHECK_LAST();
-    }
-
-    template <typename InputIterator>
-    static void inclusive_scan(void *&temp_storage, size_t &temp_storage_bytes,
-        const InputIterator input, T* output, size_t size, cudaStream_t stream)
-    {
-      cub::DeviceScan::InclusiveScan(temp_storage, temp_storage_bytes, input,
-                                     output, Op{}, size, stream);
-      CUDA_CHECK_LAST();
-    }
-};
-
 template <typename Op>
-struct PrefixSumDispatcher {
+struct ScanDispatcher {
   private:
   // return true if T is arithmetic type (including cudf::experimental::bool8)
   template <typename T>
@@ -84,12 +27,46 @@ struct PrefixSumDispatcher {
     return std::is_arithmetic<T>::value;
   }
 
+  template <typename T, typename InputIterator>
+  void exclusive_scan(const InputIterator input, T* output, size_t size,
+                      rmm::mr::device_memory_resource* mr, cudaStream_t stream)
+  {
+    // TODO benchmark between thrust and cub reduce and scan
+    rmm::device_buffer temp_storage;
+    size_t temp_storage_bytes = 0;
+    cub::DeviceScan::ExclusiveScan(temp_storage.data(), temp_storage_bytes,
+                                   input, output, Op{},
+                                   Op::template identity<T>(), size, stream);
+    // Prepare temp storage
+    temp_storage = rmm::device_buffer{temp_storage_bytes, stream, mr};
+
+    cub::DeviceScan::ExclusiveScan(temp_storage.data(), temp_storage_bytes,
+                                   input, output, Op{},
+                                   Op::template identity<T>(), size, stream);
+    CUDA_CHECK_LAST();
+  }
+
+  template <typename T, typename InputIterator>
+  void inclusive_scan(const InputIterator input, T* output, size_t size,
+                      rmm::mr::device_memory_resource* mr, cudaStream_t stream)
+  {
+    rmm::device_buffer temp_storage;
+    size_t temp_storage_bytes = 0;
+    cub::DeviceScan::InclusiveScan(temp_storage.data(), temp_storage_bytes,
+                                   input, output, Op{}, size, stream);
+    // Prepare temp storage
+    temp_storage = rmm::device_buffer{temp_storage_bytes, stream, mr};
+    cub::DeviceScan::InclusiveScan(temp_storage.data(), temp_storage_bytes,
+                                   input, output, Op{}, size, stream);
+    CUDA_CHECK_LAST();
+  }
+
   public:
   template <typename T,
-            typename std::enable_if_t<is_supported<T>(), T> * = nullptr>
+            typename std::enable_if_t<is_supported<T>(), T>* = nullptr>
   void operator()(const column_view& input, mutable_column_view& output,
-                  bool inclusive, cudaStream_t stream,
-                  rmm::mr::device_memory_resource* mr)
+                  bool inclusive, 
+                  rmm::mr::device_memory_resource* mr, cudaStream_t stream)
   {
     CUDF_EXPECTS(input.size() == output.size(),
                  "input and output data size must be same");
@@ -100,24 +77,40 @@ struct PrefixSumDispatcher {
                  "Input column and Output column nullable mismatch (either one "
                  "cannot be nullable)");
 
-    Scan<T, Op>::call(input, output, inclusive, stream, mr);
+    size_t size = input.size();
+
+    if (input.has_nulls()) {
+      auto d_input = column_device_view::create(input, stream);
+      auto it = make_null_replacement_iterator(*d_input, Op::template identity<T>());
+      if (inclusive)
+        inclusive_scan(it, output.data<T>(), size, mr, stream);
+      else
+        exclusive_scan(it, output.data<T>(), size, mr, stream);
+    } else {
+      auto it = input.data<T>();  // since scan is for arithmetic types only
+      if (inclusive)
+        inclusive_scan(it, output.data<T>(), size, mr, stream);
+      else
+        exclusive_scan(it, output.data<T>(), size, mr, stream);
+    }
     CUDF_EXPECTS(input.null_count() == output.null_count(),
                  "Input / output column null count mismatch");
   }
 
   template <typename T,
-            typename std::enable_if_t<!is_supported<T>(), T> * = nullptr>
+            typename std::enable_if_t<!is_supported<T>(), T>* = nullptr>
   void operator()(const column_view& input, mutable_column_view& output,
-                  bool inclusive, cudaStream_t stream,
-                  rmm::mr::device_memory_resource* mr) {
+                  bool inclusive, 
+                  rmm::mr::device_memory_resource* mr, cudaStream_t stream)
+  {
     CUDF_FAIL("Non-arithmetic types not supported for `cudf::scan`");
   }
 };
 
 std::unique_ptr<column> scan(const column_view& input,
                              scan_operators op, bool inclusive,
-                             cudaStream_t stream=0,
-                             rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource())
+                             rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource(),
+                             cudaStream_t stream=0)
 {
   CUDF_EXPECTS(is_numeric(input.type()), "Unexpected non-numeric type.");
 
@@ -131,19 +124,19 @@ std::unique_ptr<column> scan(const column_view& input,
   switch (op) {
     case scan_operators::SCAN_SUM:
         cudf::experimental::type_dispatcher(input.type(),
-            PrefixSumDispatcher<cudf::DeviceSum>(), input, output, inclusive, stream, mr);
+            ScanDispatcher<cudf::DeviceSum>(), input, output, inclusive, mr, stream);
         break;
     case scan_operators::SCAN_MIN:
         cudf::experimental::type_dispatcher(input.type(),
-            PrefixSumDispatcher<cudf::DeviceMin>(), input, output, inclusive, stream, mr);
+            ScanDispatcher<cudf::DeviceMin>(), input, output, inclusive, mr, stream);
         break;
     case scan_operators::SCAN_MAX:
         cudf::experimental::type_dispatcher(input.type(),
-            PrefixSumDispatcher<cudf::DeviceMax>(), input, output, inclusive, stream, mr);
+            ScanDispatcher<cudf::DeviceMax>(), input, output, inclusive, mr, stream);
         break;
     case scan_operators::SCAN_PRODUCT:
         cudf::experimental::type_dispatcher(input.type(),
-            PrefixSumDispatcher<cudf::DeviceProduct>(), input, output, inclusive, stream, mr);
+            ScanDispatcher<cudf::DeviceProduct>(), input, output, inclusive, mr, stream);
         break;
     default:
         CUDF_FAIL("The input enum `scan::operators` is out of the range");
@@ -156,7 +149,7 @@ std::unique_ptr<column> scan(const column_view& input,
                              scan_operators op, bool inclusive,
                              rmm::mr::device_memory_resource* mr)
 {
-  return detail::scan(input, op, inclusive, 0, mr);
+  return detail::scan(input, op, inclusive, mr);
 }
 
 }  // namespace experimental
