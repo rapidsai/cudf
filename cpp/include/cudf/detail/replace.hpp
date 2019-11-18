53bb23a031f376e1be93c2d11fdc8d56062c0d5e
James Wyles <37152791+jwyles@users.noreply.github.com>
2019-11-18 10:31:11 -0700

    Update cpp/include/cudf/detail/replace.hpp
    Co-Authored-By: Mark Harris <mharris@nvidia.com>

--- cpp/include/cudf/detail/replace.hpp
+++ cpp/include/cudf/detail/replace.hpp
@@ -62,7 +62,6 @@ std::unique_ptr<column> replace_nulls(column_view const& input,
                                             cudaStream_t stream = 0);
 
 /**
- * @brief Replace elements from `input_col` according to the mapping `old_values` to
  *        `new_values`, that is, replace all `old_values[i]` present in `col`
  *        with `new_values[i]`.
  *
