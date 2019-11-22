3fa80cabcafdafd7a90623956430f0a442b93dbf
David Wendt <dwendt@nvidia.com>
2019-11-22 13:28:42 -0500

    fixed include header

--- cpp/src/strings/convert/convert_urls.cu
+++ cpp/src/strings/convert/convert_urls.cu
@@ -19,8 +19,8 @@
 #include <cudf/strings/convert/convert_urls.hpp>
 #include <cudf/strings/strings_column_view.hpp>
 #include <cudf/strings/string_view.cuh>
-#include "../utilities.hpp"
-#include "../utilities.cuh"
+#include <strings/utilities.hpp>
+#include <strings/utilities.cuh>
 
 
 namespace cudf
