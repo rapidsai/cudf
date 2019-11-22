25c425c76244496c4cd0a93fdf9a0923ae9c21a2
Seunghwa Kang <seunghwak@nvidia.com>
2019-11-22 10:28:14 -0800

    removed the copy_range for strings function taking strings_column_view as source (take iterators for source instead)

--- cpp/include/cudf/strings/detail/copy_range.cuh
+++ cpp/include/cudf/strings/detail/copy_range.cuh
@@ -154,47 +154,6 @@ std::unique_ptr<column> copy_range(SourceValueIterator source_value_begin,
   }
 }
 
-// TODO: this may no longer be necessary
-/**
- * @brief Internal API to copy a range of string elements out-of-place from one
- * column to another.
- *
- * Creates a new column as if an in-place copy was performed into @p target.
- * A copy of @p target is created first and then the elements indicated by the
- * indices [@p target_begin, @p target_begin + N) were copied from the elements
- * indicated by the indices [@p source_begin, @p source_end) of @p source
- * (where N = (@p source_end - @p source_begin)). Elements outside the range are
- * copied from @p target into the returned new column target.
- *
- * If @p source and @p target refer to the same elements and the ranges overlap,
- * the behavior is undefined.
- *
- * @throws `cudf::logic_error` for invalid range (if
- * @p source_begin > @p source_end, @p source_begin < 0,
- * @p source_begin >= @p source.size(), @p source_end > @p source.size(),
- * @p target_begin < 0, target_begin >= @p target.size(), or
- * @p target_begin + (@p source_end - @p source_begin) > @p target.size()).
- * @throws `cudf::logic_error` if @p target and @p source have different types.
- *
- * @param source The strings column to copy from inside the range.
- * @param target The strings column to copy from outside the range.
- * @param source_begin The starting index of the source range (inclusive)
- * @param source_end The index of the last element in the source range
- * (exclusive)
- * @param target_begin The starting index of the target range (inclusive)
- * @param mr Memory resource to allocate the result target column.
- * @param stream CUDA stream to run this function
- * @return std::unique_ptr<column> The result target column
- */
-std::unique_ptr<column> copy_range(
-  strings_column_view const& source,
-  strings_column_view const& target,
-  size_type source_begin, size_type source_end,
-  size_type target_begin,
-  rmm::mr::device_memory_resource* mr =
-    rmm::mr::get_default_resource(),
-  cudaStream_t stream = 0);
-
 } // namespace detail
 } // namespace strings
 } // namespace cudf
