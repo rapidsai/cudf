d56d0f6f3605f134e6e812fe3cc0ebf1cfeb9d3b
Trevor Smith <trevorsm7@gmail.com>
2019-12-02 19:34:14 -0800

    Add error condition tests

--- cpp/tests/hashing/hash_partition_test.cpp
+++ cpp/tests/hashing/hash_partition_test.cpp
@@ -26,6 +26,32 @@ using cudf::test::expect_tables_equal;
 
 class HashPartition : public cudf::test::BaseFixture {};
 
+TEST_F(HashPartition, NoColumnsToHash)
+{
+  auto floats = fixed_width_column_wrapper<float>({1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f});
+  auto integers = fixed_width_column_wrapper<int16_t>({1, 2, 3, 4, 5, 6, 7, 8});
+  auto strings = strings_column_wrapper({"a", "bb", "ccc", "d", "ee", "fff", "gg", "h"});
+  auto input = cudf::table_view({floats, integers, strings});
+
+  auto columns_to_hash = std::vector<cudf::size_type>();
+
+  cudf::size_type const num_partitions = 3;
+  EXPECT_THROW(cudf::hash_partition(input, columns_to_hash, num_partitions), cudf::logic_error);
+}
+
+TEST_F(HashPartition, ZeroPartitions)
+{
+  auto floats = fixed_width_column_wrapper<float>({1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f});
+  auto integers = fixed_width_column_wrapper<int16_t>({1, 2, 3, 4, 5, 6, 7, 8});
+  auto strings = strings_column_wrapper({"a", "bb", "ccc", "d", "ee", "fff", "gg", "h"});
+  auto input = cudf::table_view({floats, integers, strings});
+
+  auto columns_to_hash = std::vector<cudf::size_type>({2});
+
+  cudf::size_type const num_partitions = 0;
+  EXPECT_THROW(cudf::hash_partition(input, columns_to_hash, num_partitions), cudf::logic_error);
+}
+
 TEST_F(HashPartition, MixedColumnTypes)
 {
   auto floats = fixed_width_column_wrapper<float>({1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f});
