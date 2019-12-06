3ed0ce65236bbfd0eba367a1632ec3d6c03942a1
brandon-b-miller <brmiller@nvidia.com>
2019-12-06 12:41:43 -0800

    adding two dates makes no sense

--- python/cudf/cudf/tests/test_binops.py
+++ python/cudf/cudf/tests/test_binops.py
@@ -670,7 +670,7 @@ def test_int8_float16_binop():
     got = a / b
     utils.assert_eq(expect, got, check_dtype=False)
 
-@pytest.mark.parametrize('dtype', ['int64', 'float64', 'string', 'datetime64[ns]'])
+@pytest.mark.parametrize('dtype', ['int64', 'float64', 'string'])
 def test_vector_to_none_binops(dtype):
     data = Series([1,2,3,None], dtype=dtype)
 
