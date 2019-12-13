2e8b078a69125dac2fb7f54247a24bdb218699a8
H. Thomson Comer <thomcom@gmail.com>
2019-12-13 12:04:05 -0800

    Revert table view changes for now.

--- cpp/include/cudf/table/table_view.hpp
+++ cpp/include/cudf/table/table_view.hpp
@@ -167,7 +167,6 @@ public:
    * @param views The vector of table views to construct the table from
    *---------------------------------------------------------------------------**/
   table_view(std::vector<table_view> const &views);
-  table_view() {}
 
   /**---------------------------------------------------------------------------*
    * @brief Returns a table_view with set of specified columns.
@@ -221,7 +220,6 @@ public:
    * @param views The vector of table views to construct the table from
    *---------------------------------------------------------------------------**/
   mutable_table_view(std::vector<mutable_table_view> const &views);
-  mutable_table_view() {}
 };
 
 inline bool has_nulls(table_view view) {
