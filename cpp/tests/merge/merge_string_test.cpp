9dc043863de4f1ddb28ac27c9d79da881658df18
Andrei Schaffer <aschaffer@nvidia.com>
2019-12-13 14:38:00 -0600

    Added nullable string key column test. And removed the constraint that mutable_column_view::create() must have num_children==0.

--- cpp/tests/merge/merge_string_test.cpp
+++ cpp/tests/merge/merge_string_test.cpp
@@ -178,3 +178,60 @@ TYPED_TEST(MergeStringTest, Merge2StringKeyColumns) {
   cudf::test::expect_columns_equal(expected_column_view3, output_column_view3);
 }
 
+TYPED_TEST(MergeStringTest, Merge1StringKeyNullColumns) {
+  // data: "ab", "bc", "cd", "de" | valid: 1 1 1 0
+  strings_column_wrapper leftColWrap1({"ab", "bc", "cd", "de", "ef", "fg", "gh", "hi"}, {1,1,1,1,1,1,1,0});
+
+  cudf::size_type inputRows = static_cast<cudf::column_view const&>(leftColWrap1).size();
+
+  auto sequence0 = cudf::test::make_counting_transform_iterator(0, [](auto row) {
+      if (cudf::experimental::type_to_id<TypeParam>() == cudf::BOOL8)
+        return 0;
+      else
+        return row; });
+        
+  fixed_width_column_wrapper<TypeParam> leftColWrap2(sequence0, sequence0+inputRows);
+  cudf::table_view left_view{{leftColWrap1, leftColWrap2}};
+
+  // data: "ac", "bd", "ce", "df" | valid: 1 1 1 0
+  strings_column_wrapper rightColWrap1({"ac", "bd", "ce", "df", "eg", "fh", "gi", "hj"}, {1,1,1,1,1,1,1,0});
+  fixed_width_column_wrapper<TypeParam> rightColWrap2(sequence0, sequence0+inputRows);
+
+  cudf::table_view right_view{{rightColWrap1, rightColWrap2}};
+  
+  std::vector<cudf::size_type> key_cols{0};
+  std::vector<cudf::order> column_order {cudf::order::ASCENDING};
+  std::vector<cudf::null_order> null_precedence{cudf::null_order::AFTER};
+
+  std::unique_ptr<cudf::experimental::table> p_outputTable;
+  EXPECT_NO_THROW( p_outputTable = cudf::experimental::merge(left_view,
+                                                            right_view,
+                                                            key_cols,
+                                                            column_order,
+                                                            null_precedence));
+
+  cudf::column_view const& a_left_tbl_cview{static_cast<cudf::column_view const&>(leftColWrap1)};
+  cudf::column_view const& a_right_tbl_cview{static_cast<cudf::column_view const&>(rightColWrap1)};
+  const cudf::size_type outputRows = a_left_tbl_cview.size() + a_right_tbl_cview.size();
+  const cudf::size_type column1TotalNulls = a_left_tbl_cview.null_count() + a_right_tbl_cview.null_count();
+
+  // data: "ab", "ac", "bc", "bd", "cd", "ce", "de", "df" | valid: 1 1 1 1 1 1 0 0
+  strings_column_wrapper expectedDataWrap1({"ab", "ac", "bc", "bd", "cd", "ce", "de", "df", "ef", "eg", "fg", "fh", "gh", "gi", "hi", "hj"},
+                                           {1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0});
+  auto seq_out2 = cudf::test::make_counting_transform_iterator(0, [outputRows](auto row) {
+      if (cudf::experimental::type_to_id<TypeParam>() == cudf::BOOL8)
+        return 0;
+      else
+        return row / 2; });
+  fixed_width_column_wrapper<TypeParam> expectedDataWrap2(seq_out2, seq_out2+outputRows);
+
+    
+  auto expected_column_view1{static_cast<cudf::column_view const&>(expectedDataWrap1)};
+  auto expected_column_view2{static_cast<cudf::column_view const&>(expectedDataWrap2)};
+  
+  auto output_column_view1{p_outputTable->view().column(0)};
+  auto output_column_view2{p_outputTable->view().column(1)};
+    
+  cudf::test::expect_columns_equal(expected_column_view1, output_column_view1);
+  cudf::test::expect_columns_equal(expected_column_view2, output_column_view2);
+}
