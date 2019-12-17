e3c469d5b50f5e2fb01527a7d6f3adbfa1638f68
Christopher Harris <charris@nvidia.com>
2019-12-16 22:24:18 -0600

    quantiles: add EXPECT_NO_THROW to implicit unsortedness test.

--- cpp/tests/quantiles/quantiles_test.cu
+++ cpp/tests/quantiles/quantiles_test.cu
@@ -19,6 +19,7 @@
 #include <cudf/types.hpp>
 #include <cudf/quantiles.hpp>
 #include <limits>
+#include <memory>
 #include <tests/utilities/base_fixture.hpp>
 #include <tests/utilities/column_wrapper.hpp>
 #include <tests/utilities/scalar_utilities.hpp>
@@ -564,8 +565,8 @@ TYPED_TEST(QuantilesTest, TestImplicitlyUnsortedInputs)
     fixed_width_column_wrapper<TypeParam> a ({ b_val, a_val });
 
     cudf::table_view input{{ a }};
-
-    auto q_values = cudf::experimental::quantiles(input, 0);
+    std::vector<std::unique_ptr<cudf::scalar>> q_values{};
+    EXPECT_NO_THROW(q_values = cudf::experimental::quantiles(input, 0));
     auto q_expected = q_res(a_val);
     cudf::scalar * q_actual = q_values.at(0).get();
     expect_scalars_equal(q_expected, *q_actual);
