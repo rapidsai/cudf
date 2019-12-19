863a31dceb021b23bac730d3f8f6e8ad19b62851
Trevor Smith <trevorsm7@gmail.com>
2019-12-18 20:26:36 -0800

    Remove definition guards around *_DEVICE_CALLABLE

--- cpp/include/cudf/types.hpp
+++ cpp/include/cudf/types.hpp
@@ -16,14 +16,12 @@
 
 #pragma once
 
-#ifndef CUDA_HOST_DEVICE_CALLABLE
 #ifdef __CUDACC__
 #define CUDA_HOST_DEVICE_CALLABLE __host__ __device__ inline
 #define CUDA_DEVICE_CALLABLE __device__ inline
 #else
 #define CUDA_HOST_DEVICE_CALLABLE inline
 #define CUDA_DEVICE_CALLABLE inline
-#endif
 #endif
 
 #include <cstddef>
