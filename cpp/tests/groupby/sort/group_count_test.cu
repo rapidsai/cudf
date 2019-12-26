19d859afead69afe0403a524f76acac14c7fd2e9
Devavret Makkar <dmakkar@nvidia.com>
2019-12-27 01:11:58 +0530

    Groupby count test

--- cpp/tests/groupby/sort/group_count_test.cu
+++ cpp/tests/groupby/sort/group_count_test.cu
@@ -27,31 +27,31 @@ namespace test {
 
 
 template <typename V>
-struct groupby_sum_test : public cudf::test::BaseFixture {};
+struct groupby_count_test : public cudf::test::BaseFixture {};
 
-TYPED_TEST_CASE(groupby_sum_test, cudf::test::NumericTypes);
+TYPED_TEST_CASE(groupby_count_test, cudf::test::NumericTypes);
 
-TYPED_TEST(groupby_sum_test, basic)
+TYPED_TEST(groupby_count_test, basic)
 {
     using K = int32_t;
     using V = TypeParam;
-    using R = experimental::detail::target_type_t<V, experimental::aggregation::SUM>;
+    using R = experimental::detail::target_type_t<V, experimental::aggregation::COUNT>;
 
     fixed_width_column_wrapper<K> keys        { 1, 2, 3, 1, 2, 2, 1, 3, 3, 2};
     fixed_width_column_wrapper<V> vals        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    fixed_width_column_wrapper<K> expect_keys { 1, 2,  3 };
-    fixed_width_column_wrapper<R> expect_vals { 9, 19, 17};
+    fixed_width_column_wrapper<K> expect_keys { 1, 2, 3 };
+    fixed_width_column_wrapper<R> expect_vals { 3, 4, 3 };
 
-    auto agg = cudf::experimental::make_sum_aggregation();
+    auto agg = cudf::experimental::make_count_aggregation();
     test_single_agg(keys, vals, expect_keys, expect_vals, std::move(agg));
 }
 
-TYPED_TEST(groupby_sum_test, zero_valid_keys)
+TYPED_TEST(groupby_count_test, zero_valid_keys)
 {
     using K = int32_t;
     using V = TypeParam;
-    using R = experimental::detail::target_type_t<V, experimental::aggregation::SUM>;
+    using R = experimental::detail::target_type_t<V, experimental::aggregation::COUNT>;
 
     fixed_width_column_wrapper<K> keys      ( { 1, 2, 3}, all_null() );
     fixed_width_column_wrapper<V> vals        { 3, 4, 5};
@@ -59,31 +59,31 @@ TYPED_TEST(groupby_sum_test, zero_valid_keys)
     fixed_width_column_wrapper<K> expect_keys { };
     fixed_width_column_wrapper<R> expect_vals { };
 
-    auto agg = cudf::experimental::make_sum_aggregation();
+    auto agg = cudf::experimental::make_count_aggregation();
     test_single_agg(keys, vals, expect_keys, expect_vals, std::move(agg));
 }
 
-TYPED_TEST(groupby_sum_test, zero_valid_values)
+TYPED_TEST(groupby_count_test, zero_valid_values)
 {
     using K = int32_t;
     using V = TypeParam;
-    using R = experimental::detail::target_type_t<V, experimental::aggregation::SUM>;
+    using R = experimental::detail::target_type_t<V, experimental::aggregation::COUNT>;
 
     fixed_width_column_wrapper<K> keys        { 1, 1, 1};
     fixed_width_column_wrapper<V> vals      ( { 3, 4, 5}, all_null() );
 
     fixed_width_column_wrapper<K> expect_keys { 1 };
-    fixed_width_column_wrapper<R> expect_vals({ 0 }, all_null());
+    fixed_width_column_wrapper<R> expect_vals { 0 };
 
-    auto agg = cudf::experimental::make_sum_aggregation();
+    auto agg = cudf::experimental::make_count_aggregation();
     test_single_agg(keys, vals, expect_keys, expect_vals, std::move(agg));
 }
 
-TYPED_TEST(groupby_sum_test, null_keys_and_values)
+TYPED_TEST(groupby_count_test, null_keys_and_values)
 {
     using K = int32_t;
     using V = TypeParam;
-    using R = experimental::detail::target_type_t<V, experimental::aggregation::SUM>;
+    using R = experimental::detail::target_type_t<V, experimental::aggregation::COUNT>;
 
     fixed_width_column_wrapper<K> keys(       { 1, 2, 3, 1, 2, 2, 1, 3, 3, 2, 4},
                                               { 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1});
@@ -93,10 +93,9 @@ TYPED_TEST(groupby_sum_test, null_keys_and_values)
                                           //  { 1, 1,     2, 2, 2,   3, 3,    4}
     fixed_width_column_wrapper<K> expect_keys({ 1,        2,         3,       4}, all_valid());
                                           //  { 3, 6,     1, 4, 9,   2, 8,    -}
-    fixed_width_column_wrapper<R> expect_vals({ 9,        14,        10,      0},
-                                              { 1,         1,         1,      0});
+    fixed_width_column_wrapper<R> expect_vals { 2,        3,         2,       0};
 
-    auto agg = cudf::experimental::make_sum_aggregation();
+    auto agg = cudf::experimental::make_count_aggregation();
     test_single_agg(keys, vals, expect_keys, expect_vals, std::move(agg));
 }
 
