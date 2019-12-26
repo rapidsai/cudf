f9df5a55c05d7718704e326f2ee23a94b3a6b803
Devavret Makkar <dmakkar@nvidia.com>
2019-12-27 05:05:25 +0530

    group quantile specific test cases

--- cpp/tests/groupby/sort/group_quantile_test.cu
+++ cpp/tests/groupby/sort/group_quantile_test.cu
@@ -106,6 +106,70 @@ TYPED_TEST(groupby_quantile_test, null_keys_and_values)
     test_single_agg(keys, vals, expect_keys, expect_vals, std::move(agg));
 }
 
+TYPED_TEST(groupby_quantile_test, multiple_quantile)
+{
+    using K = int32_t;
+    using V = TypeParam;
+    using R = experimental::detail::target_type_t<V, experimental::aggregation::QUANTILE>;
+
+    fixed_width_column_wrapper<K> keys        { 1, 2, 3, 1, 2, 2, 1, 3, 3, 2};
+    fixed_width_column_wrapper<V> vals        { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+                                          //  { 1, 1, 1, 2, 2, 2, 2, 3, 3, 3}
+    fixed_width_column_wrapper<K> expect_keys { 1,       2,          3      };
+                                          //  { 0, 3, 6, 1, 4, 5, 9, 2, 7, 8}
+    fixed_width_column_wrapper<R> expect_vals({  1.5,4.5, 3.25, 6,    4.5,7.5}, all_valid());
+
+    auto agg = cudf::experimental::make_quantile_aggregation({0.25, 0.75},
+                                        experimental::interpolation::LINEAR);
+    test_single_agg(keys, vals, expect_keys, expect_vals, std::move(agg));
+}
+
+TYPED_TEST(groupby_quantile_test, interpolation_types)
+{
+    using K = int32_t;
+    using V = TypeParam;
+    using R = experimental::detail::target_type_t<V, experimental::aggregation::QUANTILE>;
+
+    fixed_width_column_wrapper<K> keys        { 1, 2, 3, 1, 2, 2, 1, 3, 2};
+    fixed_width_column_wrapper<V> vals        { 0, 1, 2, 3, 4, 5, 6, 7, 9};
+
+                                          //  { 1, 1, 1,  2, 2, 2, 2,  3, 3}
+    fixed_width_column_wrapper<K> expect_keys { 1,        2,           3   };
+
+
+                                           //  { 0, 3, 6,  1, 4, 5, 9,  2, 7}
+    fixed_width_column_wrapper<R> expect_vals1({  2.4,         4.2,       4 }, all_valid());
+    auto agg1 = cudf::experimental::make_quantile_aggregation({0.4},
+                                        experimental::interpolation::LINEAR);
+    test_single_agg(keys, vals, expect_keys, expect_vals1, std::move(agg1));
+
+                                           //  { 0, 3, 6,  1, 4, 5, 9,  2, 7}
+    fixed_width_column_wrapper<R> expect_vals2({    3,          4,        2 }, all_valid());
+    auto agg2 = cudf::experimental::make_quantile_aggregation({0.4},
+                                        experimental::interpolation::NEAREST);
+    test_single_agg(keys, vals, expect_keys, expect_vals2, std::move(agg2));
+
+                                           //  { 0, 3, 6,  1, 4, 5, 9,  2, 7}
+    fixed_width_column_wrapper<R> expect_vals3({    0,          4,        2 }, all_valid());
+    auto agg3 = cudf::experimental::make_quantile_aggregation({0.4},
+                                        experimental::interpolation::LOWER);
+    test_single_agg(keys, vals, expect_keys, expect_vals3, std::move(agg3));
+
+                                           //  { 0, 3, 6,  1, 4, 5, 9,  2, 7}
+    fixed_width_column_wrapper<R> expect_vals4({    3,          5,        7 }, all_valid());
+    auto agg4 = cudf::experimental::make_quantile_aggregation({0.4},
+                                        experimental::interpolation::HIGHER);
+    test_single_agg(keys, vals, expect_keys, expect_vals4, std::move(agg4));
+
+                                           //  { 0, 3, 6,  1, 4, 5, 9,  2, 7}
+    fixed_width_column_wrapper<R> expect_vals5({  1.5,         4.5,      4.5}, all_valid());
+    auto agg5 = cudf::experimental::make_quantile_aggregation({0.4},
+                                        experimental::interpolation::MIDPOINT);
+    test_single_agg(keys, vals, expect_keys, expect_vals5, std::move(agg5));
+
+}
+
 
 } // namespace test
 } // namespace cudf
