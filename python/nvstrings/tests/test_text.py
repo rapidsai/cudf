c5ae74624c7f9a3409126418d357fd2e08b95123
AK-ayush <ayushkumar@protonmail.com>
2020-01-03 15:06:08 +0530

    fixed tests again

--- python/nvstrings/tests/test_text.py
+++ python/nvstrings/tests/test_text.py
@@ -75,9 +75,12 @@ def test_token_count():
     assert got == expected
 
     # test device pointer
-    nvtext.token_count(strs, delimiter=["a", "e", "i", "o"])
     got_darray = rmm.device_array(strs.size(), dtype=np.int32)
-    nvtext.token_count(strs, devptr=got_darray.device_ctypes_pointer.value)
+    nvtext.token_count(
+        strs,
+        delimiter=["a", "e", "i", "o"],
+        devptr=got_darray.device_ctypes_pointer.value,
+    )
     expected = [12, 13, 0, 0]
     assert np.array_equal(got_darray.copy_to_host(), expected)
 
