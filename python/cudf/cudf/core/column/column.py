201e9eba28b1fda580ebe8cb8901b6a8eb3fc583
Ashwin Srinath <3190405+shwina@users.noreply.github.com>
2020-01-07 12:49:28 -0500

    Update python/cudf/cudf/core/column/column.py
    Co-Authored-By: jakirkham <jakirkham@gmail.com>

--- python/cudf/cudf/core/column/column.py
+++ python/cudf/cudf/core/column/column.py
@@ -1370,7 +1370,6 @@ def _data_from_cuda_array_interface_desc(obj):
     nelem = desc["shape"][0]
     dtype = np.dtype(desc["typestr"])
 
-    # TODO: this can be done more efficiently
     data = Buffer(data=ptr, size=nelem * dtype.itemsize, owner=obj)
     return data
 
