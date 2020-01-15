be443fba5cf08fae3bfd56b0a76cb03e3df1fb5e
Ram (Ramakrishna Prabhu) <ramakrishnap@nvidia.com>
2020-01-15 13:49:06 -0600

    license year

--- cpp/tests/replace/clamp_test.cpp
+++ cpp/tests/replace/clamp_test.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright 2018 BlazingDB, Inc.
- *     Copyright 2018 Cristhian Alberto Gonzales Castillo <cristhian@blazingdb.com>
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
