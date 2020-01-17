9847c1c18d322c1a3c45e3f761bbd2a38af11e22
Andrei Schaffer <aschaffer@nvidia.com>
2020-01-17 12:14:06 -0600

    Added the degenerate case to the round-robin documentation.

--- cpp/include/cudf/round_robin.hpp
+++ cpp/include/cudf/round_robin.hpp
@@ -29,8 +29,33 @@ namespace experimental {
  * Returns a new table with rows re-arranged into partition groups and
  * a vector of row offsets to the start of each partition in the output table.
  * Rows are assigned partitions based on their row index in the table,
- * based on the following formula:
- * partition = (row_index + start_partition) % num_partitions
+ * in a round robin fashion.
+ *
+ * A good analogy for the algorithm is dealing out cards:
+ *
+ *  1. The deck of cards is represented as the rows in the table.
+ *  2. The number of partitions is the number of players being dealt cards.
+ *  3. the start_partition indicates which player starts getting cards first.
+ *
+ * The algorithm has two outcomes:
+ *
+ *  (a) Another deck of cards formed by stacking each 
+ *      player's cards back into a deck again, 
+ *      preserving the order of cards dealt to each player, 
+ *      starting with player 0.
+ *  (b) A vector into the output deck indicating where a player's cards start.
+ *
+ * A player's deck (partition) is the range of cards starting 
+ * at the corresponding offset and ending at the next player's 
+ * starting offset or the last card in the deck if it's the last player.
+ *
+ * When num_partitions > nrows, we have more players than decks. 
+ * We start dealing to the first indicated player and continuing 
+ * around the players until we run out of cards before we run out of players. 
+ * Players that did not get any cards are represented by
+ * offset[i] == offset[i+1] or
+ * offset[i] == table.num_rows() if i == num_partitions-1
+ * meaning there are no cards (rows) in their deck (partition).
  *
  * Example 1:
  * input:
@@ -81,6 +106,46 @@ namespace experimental {
  * output: pair<table, partition_offsets>
  * table => col 1 {1,4,7,10,2,5,8,0,3,6,9}
  * partition_offsets => {0,4,7}
+ *
+ * Example 6:
+ * input:
+ * table => col 1 {0, ..., 10}
+ * num_partitions = 15 > num_rows = 11
+ * start_partition = 2
+ * 
+ * output: pair<table, partition_offsets>
+ * table => col 1 {0,1,2,3,4,5,6,7,8,9,10}
+ * partition_offsets => {0,0,0,1,2,3,4,5,6,7,8,9,10,11,11}
+ *
+ * Example 7:
+ * input:
+ * table => col 1 {0, ..., 10}
+ * num_partitions = 15 > num_rows = 11
+ * start_partition = 10
+ * 
+ * output: pair<table, partition_offsets>
+ * table => col 1 {5,6,7,8,9,10,0,1,2,3,4}
+ * partition_offsets => {0,1,2,3,4,5,6,6,6,6,6,7,8,9,10}
+ *
+ * Example 8:
+ * input:
+ * table => col 1 {0, ..., 10}
+ * num_partitions = 15 > num_rows = 11
+ * start_partition = 14
+ * 
+ * output: pair<table, partition_offsets>
+ * table => col 1 {1,2,3,4,5,6,7,8,9,10,0}
+ * partition_offsets => {0,1,2,3,4,5,6,7,8,9,10,10,10,10,10}
+ *
+ * Example 9:
+ * input:
+ * table => col 1 {0, ..., 10}
+ * num_partitions = 11 == num_rows = 11
+ * start_partition = 2
+ * 
+ * output: pair<table, partition_offsets>
+ * table => col 1 {9,10,0,1,2,3,4,5,6,7,8}
+ * partition_offsets => {0,1,2,3,4,5,6,7,8,9,10}
  *
  * @Param[in] input The input table to be round-robin partitioned
  * @Param[in] num_partitions Number of partitions for the table
