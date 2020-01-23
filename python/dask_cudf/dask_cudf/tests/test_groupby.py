d8f1e3758b9015595564eb2e539bb2d77e4223da
H. Thomson Comer <thomcom@gmail.com>
2020-01-23 09:15:33 -0800

    Black, pre-commit testing

--- python/dask_cudf/dask_cudf/tests/test_groupby.py
+++ python/dask_cudf/dask_cudf/tests/test_groupby.py
@@ -243,15 +243,22 @@ def test_groupby_string_index_name(myindex):
     assert gdf.compute().index.name == gdf.index.name
 
 
-@pytest.mark.parametrize("agg_func", [
-    lambda gb: gb.agg({"c": "count"}, split_out=2),
-    lambda gb: gb.agg({"c": ["count", "sum"]}, split_out=2),
-    lambda gb: gb.count(split_out=2),
-    lambda gb: gb.c.count(split_out=2),
-])
+@pytest.mark.parametrize(
+    "agg_func",
+    [
+        lambda gb: gb.agg({"c": "count"}, split_out=2),
+        lambda gb: gb.agg({"c": ["count", "sum"]}, split_out=2),
+        lambda gb: gb.count(split_out=2),
+        lambda gb: gb.c.count(split_out=2),
+    ],
+)
 def test_groupby_split_out_multiindex(agg_func):
     df = cudf.DataFrame(
-        {"a": np.random.randint(0, 10, 100), "b": np.random.randint(0, 5, 100), "c": np.random.random(100)}
+        {
+            "a": np.random.randint(0, 10, 100),
+            "b": np.random.randint(0, 5, 100),
+            "c": np.random.random(100),
+        }
     )
     ddf = dask_cudf.from_cudf(df, 5)
     pddf = dd.from_pandas(df.to_pandas(), 5)
