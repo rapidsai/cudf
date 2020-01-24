843ba1030a2dae1f6a994990dc025436b699b161
Conor Hoekstra <codereport@outlook.com>
2020-01-24 17:03:25 -0500

    Adding unit test to tricky division

--- cpp/tests/fixed_point/fixed_point_tests.cpp
+++ cpp/tests/fixed_point/fixed_point_tests.cpp
@@ -103,6 +103,11 @@ TEST_F(FixedPointTest, Decimal32TrickyDivision) {
     EXPECT_EQ(SIXTY_1 / TEN_0, ZERO);
     EXPECT_EQ(SIXTY_1 / TEN_1, SIX_0);
 
+    decimal32 A{34.56,  scale_type{-2}};
+    decimal32 B{1.234,  scale_type{-3}};
+
+    EXPECT_EQ((A / B).get(), 20);
+
 }
 
 TEST_F(FixedPointTest, OverflowDecimal32) {
