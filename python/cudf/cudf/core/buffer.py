ab46f28ddeace7eeb7077f26590bc0e7222fe766
John Kirkham <jakirkham@gmail.com>
2020-01-28 14:11:20 -0800

    Reorganize `data` type checks and exceptions
    Turn the unexpected `data` type case into a flat out exception. Make a
    case for `data` being an address where `size`'s type is also checked.
    Should make this a bit clearer on what the cases are and when the
    exceptions occur. Also should simplify the process for adding new type
    cases.

--- python/cudf/cudf/core/buffer.py
+++ python/cudf/cudf/core/buffer.py
@@ -40,20 +40,20 @@ class Buffer:
             self._init_from_array_like(data)
         elif isinstance(data, memoryview):
             self._init_from_array_like(np.asarray(data))
-        else:
-            if data is None:
-                self.ptr = 0
-                self.size = 0
-            else:
-                if not isinstance(data, int):
-                    raise TypeError(
-                        "data must be Buffer, array-like or integer"
-                    )
-                if not isinstance(size, int):
-                    raise TypeError("size must be integer")
-                self.ptr = data
-                self.size = size
+        elif isinstance(data, int):
+            if not isinstance(size, int):
+                raise TypeError("size must be integer")
+            self.ptr = data
+            self.size = size
             self._owner = owner
+        elif data is None:
+            self.ptr = 0
+            self.size = 0
+            self._owner = None
+        else:
+            raise TypeError(
+                "data must be Buffer, array-like or integer"
+            )
 
     def __reduce__(self):
         return self.__class__, (self.to_host_array(),)
