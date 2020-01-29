41259d0f3b3bfd3d6d20ddff8c3468f45008d5ed
rjzamora <rzamora217@gmail.com>
2020-01-29 13:53:35 -0800

    update tests

--- python/dask_cudf/dask_cudf/tests/test_sort.py
+++ python/dask_cudf/dask_cudf/tests/test_sort.py
@@ -8,7 +8,7 @@ import dask.dataframe as dd
 import cudf
 
 
-@pytest.mark.parametrize("by", ["a", "b"])
+@pytest.mark.parametrize("by", ["a", "b", ["a", "b"]])
 @pytest.mark.parametrize("nelem", [10, 100, 1000])
 @pytest.mark.parametrize("nparts", [1, 2, 5, 10])
 def test_sort_values(nelem, nparts, by):
@@ -53,3 +53,17 @@ def test_sort_binned_meta():
     ddf = dd.from_pandas(df, npartitions=2).persist()
 
     ddf.sort_values_binned(by="b")
+
+
+@pytest.mark.parametrize("by", ["a", "b", ["a", "b"]])
+def test_sort_values_single_partition(by):
+    df = cudf.DataFrame()
+    nelem = 1000
+    df["a"] = np.ascontiguousarray(np.arange(nelem)[::-1])
+    df["b"] = np.arange(100, nelem + 100)
+    ddf = dd.from_pandas(df, npartitions=1)
+
+    with dask.config.set(scheduler="single-threaded"):
+        got = ddf.sort_values(by=by)
+    expect = df.sort_values(by=by)
+    dd.assert_eq(got, expect)
