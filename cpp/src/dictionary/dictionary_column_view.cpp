4c5599cf1525a52db364eaf7c4b0160175565458
David Wendt <dwendt@nvidia.com>
2020-01-30 08:44:16 -0500

    fix parameter name

--- cpp/src/dictionary/dictionary_column_view.cpp
+++ cpp/src/dictionary/dictionary_column_view.cpp
@@ -21,8 +21,8 @@ namespace cudf
 {
 
 //
-dictionary_column_view::dictionary_column_view( column_view const& strings_column )
-    : column_view(strings_column)
+dictionary_column_view::dictionary_column_view( column_view const& dictionary_column )
+    : column_view(dictionary_column)
 {
     CUDF_EXPECTS( type().id()==DICTIONARY32, "dictionary_column_view only supports DICTIONARY type");
 }
