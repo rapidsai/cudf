ef777e0b9d40d4472c2c6fb8fbbea735faf4f3db
Andrei Schaffer <aschaffer@nvidia.com>
2020-02-04 16:52:07 -0600

    Addressed API review requirements on header organization.

--- cpp/include/cudf/strings/capitalize.hpp
+++ cpp/include/cudf/strings/capitalize.hpp
@@ -64,35 +64,6 @@ std::unique_ptr<column> capitalize( strings_column_view const& strings,
  */
 std::unique_ptr<column> title( strings_column_view const& strings,
                                rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
-
-
-/**
- * @brief Inserts new-line characters (ASCII 0x0A) into each string in place of spaces.  
- *
- * Returns a column of strings where, for each string row in the input, 
- * words separated by spaces will become separated by newline characters.
- * Attempts to make each line less than or equal to width characters. 
- * If a string or characters is longer than width, 
- * the line is split on the next closest space character. 
- *
- * Any null string entries return corresponding null output column entries.
- *
- * Example:
- * ```
- * width = 5
- * input_string_tbl = ["tesT1 test2", "more longtest short1", " other test "];
- *
- * wrapped_string_tbl = ["test1\ntest2", ""more\nlongt\nest\nshort1", "other\ntest"]
- * ```
- *
- * @param[in] strings String column.
- * @param[in] width the maximum width of a line. 
- * @param[in] mr Resource for allocating device memory.
- * @return Column of wrapped strings.
- */
-std::unique_ptr<column> wrap( strings_column_view const& strings,
-                              size_type width,
-                              rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
   
 }//namespace strings
 }//namespace cudf
