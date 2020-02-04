891087d159b69c8b9d09dd8079ec6dfafcfb8c67
David Wendt <dwendt@nvidia.com>
2020-02-04 12:13:34 -0500

    update doxygen

--- cpp/include/cudf/text/tokenize.hpp
+++ cpp/include/cudf/text/tokenize.hpp
@@ -25,11 +25,18 @@ namespace nvtext
 {
 
 /**
- * @brief Returns a single column of strings by tokenizing the strings
- * in the given column using the provided characters as delimiters.
+ * @brief Returns a single column of strings by tokenizing the input strings
+ * column using the provided characters as delimiters.
  *
- * As each string is tokenized, the tokens are appended in row order
- * to build the output column.
+ * The `delimiter` may be zero or more characters. If the `delimiter` is empty,
+ * whitespace (character code-point <= ' ') is used for identifying tokens.
+ * Also, any consecutive delimiters found in a string are ignored.
+ * This means only non-empty tokens are returned.
+ *
+ * Tokens are found by locating delimiter(s) starting at the beginning of each string.
+ * As each string is tokenized, the tokens are appended using input column row order
+ * to build the output column. That is, tokens found in input row[i] will be placed in
+ * the output column directly before tokens found in input row[i+1].
  *
  * Example:
  * ```
@@ -40,7 +47,7 @@ namespace nvtext
  *
  * All null row entries are ignored and the output contains all valid rows.
  *
- * @param strings Strings column to use for this operation.
+ * @param strings Strings column tokenize.
  * @param delimiter UTF-8 characters used to separate each string into tokens.
  *                  The default of empty string will separate tokens using whitespace.
  * @param mr Resource for allocating device memory.
@@ -51,11 +58,16 @@ std::unique_ptr<column> tokenize( strings_column_view const& strings,
                                   rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
 
 /**
- * @brief Returns a single column of strings by tokenizing the strings
- * in the given column using multiple strings delimiters.
+ * @brief Returns a single column of strings by tokenizing the input strings
+ * column using multiple strings as delimiters.
  *
- * As each string is tokenized, the tokens are appended in row order
- * to build the output column.
+ * Tokens are found by locating delimiter(s) starting at the beginning of each string.
+ * Any consecutive delimiters found in a string are ignored.
+ * This means only non-empty tokens are returned.
+ *
+ * As each string is tokenized, the tokens are appended using input column row order
+ * to build the output column. That is, tokens found in input row[i] will be placed in
+ * the output column directly before tokens found in input row[i+1].
  *
  * Example:
  * ```
@@ -66,10 +78,10 @@ std::unique_ptr<column> tokenize( strings_column_view const& strings,
  * ```
  *
  * All null row entries are ignored and the output contains all valid rows.
- * 
+ *
  * @throw cudf::logic_error if the delimiters column is empty or contains nulls.
  *
- * @param strings Strings column to use for this operation.
+ * @param strings Strings column to tokenize.
  * @param delimiters Strings used to separate individual strings into tokens.
  * @param mr Resource for allocating device memory.
  * @return New strings columns of tokens.
@@ -81,6 +93,11 @@ std::unique_ptr<column> tokenize( strings_column_view const& strings,
 /**
  * @brief Returns the number of tokens in each string of a strings column.
  *
+ * The `delimiter` may be zero or more characters. If the `delimiter` is empty,
+ * whitespace (character code-point <= ' ') is used for identifying tokens.
+ * Also, any consecutive delimiters found in a string are ignored.
+ * This means that only empty strings or null rows will result in a token count of 0.
+ *
  * Example:
  * ```
  * s = ["a", "b c", " ", "d e f"]
@@ -89,6 +106,7 @@ std::unique_ptr<column> tokenize( strings_column_view const& strings,
  * ```
  *
  * All null row entries are ignored and the output contains all valid rows.
+ * The number of tokens for a null element is set to 0 in the output column.
  *
  * @param strings Strings column to use for this operation.
  * @param delimiter Strings used to separate each string into tokens.
@@ -101,25 +119,29 @@ std::unique_ptr<column> token_count( strings_column_view const& strings,
                                      rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
 
 /**
- * @brief Returns the number of tokens in a strings column
- * using multiple delimiters to tokenize each string.
+ * @brief Returns the number of tokens in each string of a strings column
+ * by using multiple strings delimiters to identify tokens in each string.
+ *
+ * Also, any consecutive delimiters found in a string are ignored.
+ * This means that only empty strings or null rows will result in a token count of 0.
  *
  * Example:
  * ```
- * s = ["a", "b c", "d,e:f;"]
- * d = [",",":",";"]
+ * s = ["a", "b c", "d.e:f;"]
+ * d = [".",":",";"]
  * t = token_count(s,d)
  * t is now [1,1,3]
  * ```
  *
  * All null row entries are ignored and the output contains all valid rows.
+ * The number of tokens for a null element is set to 0 in the output column.
  *
  * @throw cudf::logic_error if the delimiters column is empty or contains nulls.
  *
  * @param strings Strings column to use for this operation.
  * @param delimiters Strings used to separate each string into tokens.
  * @param mr Resource for allocating device memory.
- * @return New INT32 column of counts.
+ * @return New INT32 column of token counts.
  */
 std::unique_ptr<column> token_count( strings_column_view const& strings,
                                      strings_column_view const& delimiters,
