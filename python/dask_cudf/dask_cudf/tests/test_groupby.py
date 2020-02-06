4dffcfc4aa1c8036b993d6ea7f06b465c52ee1db
Ram (Ramakrishna Prabhu) <ramakrishnap@nvidia.com>
2020-02-05 22:58:17 -0600

    review changes

--- python/dask_cudf/dask_cudf/tests/test_groupby.py
+++ python/dask_cudf/dask_cudf/tests/test_groupby.py
@@ -274,15 +274,12 @@ def test_groupby_multiindex_reset_index():
     )
     ddf = dask_cudf.from_cudf(df, npartitions=1)
     pddf = dd.from_pandas(df.to_pandas(), npartitions=1)
-    gr = (
-        ddf.groupby(["a", "c"])
-        .agg({"b": ["count"]})
-        .reset_index()
-        .sort_values(by=["a", "c"])
-        .reset_index(drop=True)
-    )
+    gr = ddf.groupby(["a", "c"]).agg({"b": ["count"]}).reset_index()
     pr = pddf.groupby(["a", "c"]).agg({"b": ["count"]}).reset_index()
-    dd.assert_eq(gr.compute(), pr.compute())
+    dd.assert_eq(
+        gr.compute().sort_values(by=["a", "c"]).reset_index(drop=True),
+        pr.compute().sort_values(by=["a", "c"]).reset_index(drop=True),
+    )
 
 
 @pytest.mark.parametrize(
