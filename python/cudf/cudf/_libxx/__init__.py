0b7ae2b2c0cbf70ebbd59d57af48312c68b9a24c
Ram (Ramakrishna Prabhu) <ramakrishnap@nvidia.com>
2020-02-11 11:32:21 -0600

    changes

--- python/cudf/cudf/_libxx/__init__.py
+++ python/cudf/cudf/_libxx/__init__.py
@@ -8,6 +8,7 @@ from cudf._libxx.stream_compaction import (
     unique_count,
 )
 from cudf._libxx.table import Table
+from cudf._libxx.rolling import rolling
 
 MAX_COLUMN_SIZE = np.iinfo(np.int32).max
 MAX_COLUMN_SIZE_STR = "INT32_MAX"
