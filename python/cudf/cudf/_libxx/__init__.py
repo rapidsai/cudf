0bcc940c68a61cb237f9ecabab848f83bb8be5af
Ram (Ramakrishna Prabhu) <ramakrishnap@nvidia.com>
2020-02-19 12:34:27 -0600

    style changes

--- python/cudf/cudf/_libxx/__init__.py
+++ python/cudf/cudf/_libxx/__init__.py
@@ -1,6 +1,14 @@
 import numpy as np
 
-from . import copying, null_mask, rolling, sort, stream_compaction, table, transpose
+from . import (
+    copying,
+    null_mask,
+    rolling,
+    sort,
+    stream_compaction,
+    table,
+    transpose,
+)
 
 MAX_COLUMN_SIZE = np.iinfo(np.int32).max
 MAX_COLUMN_SIZE_STR = "INT32_MAX"
