79e3f29c7c65801ea297ec59491fbae90a0efd0b
GALI PREM SAGAR <sagarprem75@gmail.com>
2020-02-26 09:46:31 -0600

    Update python/cudf/cudf/core/column/string.py
    Co-Authored-By: Keith Kraus <keith.j.kraus@gmail.com>

--- python/cudf/cudf/core/column/string.py
+++ python/cudf/cudf/core/column/string.py
@@ -65,7 +65,7 @@ class StringMethods(object):
     """
 
     def __init__(self, column, parent=None):
-        self._parent = parent
+        self._column = column
         self._index = index
         self._name = name
         self._owner = owner
