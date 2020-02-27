791bf00bc87812f8cb71a6c8ec0d5ab0ddca80a9
Devavret Makkar <dmakkar@nvidia.com>
2020-02-28 02:24:38 +0530

    Doc update

--- cpp/include/cudf/detail/sorting.hpp
+++ cpp/include/cudf/detail/sorting.hpp
@@ -47,6 +47,22 @@ std::unique_ptr<column> sorted_order(
     rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource(),
     cudaStream_t stream = 0);
 
+/**
+ * @brief Computes the row indices that would produce `input` in a stable
+ * lexicographical sorted order.
+ *
+ * @param input The table to sort
+ * @param column_order The desired sort order for each column. Size must be
+ * equal to `input.num_columns()` or empty. If empty, all columns will be sorted
+ * in ascending order.
+ * @param null_precedence The desired order of null compared to other elements
+ * for each column.  Size must be equal to `input.num_columns()` or empty.
+ * If empty, all columns will be sorted in `null_order::BEFORE`.
+ * @param[in] mr Optional, The resource to use for all allocations
+ * @param[in] stream Optional CUDA stream on which to execute kernels
+ * @return std::unique_ptr<column> A non-nullable column of INT32 elements
+ * containing the permuted row indices of `input` if it were sorted
+ */
 std::unique_ptr<column> stable_sorted_order(
     table_view input, std::vector<order> const& column_order = {},
     std::vector<null_order> const& null_precedence = {},
