80ddf867be4f18ca4cc29e722222179c7bf0f86c
Ashwin Srinath <shwina@users.noreply.github.com>
2020-02-29 08:59:53 -0500

    More test for grouping

--- python/cudf/cudf/tests/test_groupbyxx.py
+++ python/cudf/cudf/tests/test_groupbyxx.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import pytest
 
@@ -16,11 +17,22 @@ def pdf():
     )
 
 
-def test_groupby_groups(pdf):
+@pytest.mark.parametrize(
+    "grouper",
+    [
+        "a",
+        ["a"],
+        ["a", "b"],
+        np.array([0, 1, 1, 2, 3, 2]),
+        {0: "a", 1: "a", 2: "b", 3: "a", 4: "b", 5: "c"},
+        lambda x: x + 1,
+    ],
+)
+def test_groupby_groups(pdf, grouper):
     gdf = cudf.from_pandas(pdf)
 
     for pdf_group, gdf_group in zip(
-        pdf.groupby("a"), gdf.groupby("a", method="libxx")
+        pdf.groupby(grouper), gdf.groupby(grouper, method="libxx")
     ):
         assert pdf_group[0] == gdf_group[0]
         assert_eq(pdf_group[1], gdf_group[1])
