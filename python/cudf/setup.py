f491174c6a9b2dafa5cd69cfb6b5b908e3e96f94
Ashwin Srinath <shwina@users.noreply.github.com>
2020-03-02 15:17:33 -0500

    Typo

--- python/cudf/setup.py
+++ python/cudf/setup.py
@@ -86,8 +86,7 @@ setup(
     ),
     packages=find_packages(include=["cudf", "cudf.*"]),
     package_data=dict.fromkeys(
-        find_packages(include=["cudf._lib*"],
-        ["*.pxd"],
+        find_packages(include=["cudf._lib*"]), ["*.pxd"],
     ),
     cmdclass=versioneer.get_cmdclass(),
     install_requires=install_requires,
