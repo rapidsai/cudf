1f5b3ce7539ae8b60414e5c9b3aa2fae6f0c3ee8
Jason Lowe <jlowe@nvidia.com>
2020-03-02 09:15:40 -0600

    Pass original memory resource as a constructor argument

--- java/src/main/native/src/RmmJni.cpp
+++ java/src/main/native/src/RmmJni.cpp
@@ -18,11 +18,22 @@
 
 #include "jni_utils.hpp"
 
+using rmm::mr::device_memory_resource;
+
 namespace {
 
-class java_event_handler_memory_resource : public rmm::mr::device_memory_resource {
+constexpr char const* RMM_EXCEPTION_CLASS = "ai/rapids/cudf/RmmException";
+
+/**
+ * @brief An RMM device memory resource adaptor that delegates to the wrapped resource
+ * for most operations but will call Java to handle certain situations (e.g.: allocation faillure).
+ **/
+class java_event_handler_memory_resource final : public device_memory_resource {
 public:
-  java_event_handler_memory_resource(JNIEnv* env, jobject jhandler) {
+  java_event_handler_memory_resource(
+      JNIEnv* env,
+      jobject jhandler,
+      device_memory_resource* resource_to_wrap) : resource(resource_to_wrap) {
     if (env->GetJavaVM(&jvm) < 0) {
       throw cudf::jni::jni_exception("GetJavaVM failed");
     }
@@ -38,26 +49,20 @@ public:
     if (handler_obj == nullptr) {
       throw cudf::jni::jni_exception("global ref");
     }
-
-    // The previous resource could be null, but getting the current resource never is.
-    resource = rmm::mr::get_default_resource();
-    rmm::mr::device_memory_resource* old = rmm::mr::set_default_resource(this);
-    if (old != nullptr) {
-      resource = old;
-    }
   }
 
   virtual ~java_event_handler_memory_resource() {
-    // TODO: This is problematic if the memory manager was updated in the interim.
-    rmm::mr::set_default_resource(resource);
-
     JNIEnv* env = cudf::jni::get_jni_env(jvm);
     env->DeleteGlobalRef(handler_obj);
     handler_obj = nullptr;
   }
 
+  device_memory_resource* get_wrapped_resource() {
+    return resource;
+  }
+
 private:
-  rmm::mr::device_memory_resource* resource;
+  device_memory_resource* const resource;
   JavaVM* jvm;
   jobject handler_obj;
   jmethodID on_alloc_fail_method;
@@ -114,18 +119,44 @@ private:
   }
 };
 
+std::unique_ptr<java_event_handler_memory_resource> Java_memory_resource{};
 
-std::unique_ptr<java_event_handler_memory_resource> Memory_resource{};
+void set_java_device_memory_resource(JNIEnv* env, jobject handler_obj) {
+  if (Java_memory_resource && handler_obj != nullptr) {
+    JNI_THROW_NEW(env, RMM_EXCEPTION_CLASS, "Another event handler is already set", )
+  }
+  if (Java_memory_resource) {
+    auto java_resource = Java_memory_resource.get();
+    auto old_resource = rmm::mr::set_default_resource(Java_memory_resource->get_wrapped_resource());
+    Java_memory_resource.reset(nullptr);
+    if (old_resource != java_resource) {
+      rmm::mr::set_default_resource(old_resource);
+      JNI_THROW_NEW(env, RMM_EXCEPTION_CLASS,
+          "Concurrent modification detected while removing memory resource", );
+    }
+  }
+  if (handler_obj != nullptr) {
+    auto resource = rmm::mr::get_default_resource();
+    Java_memory_resource.reset(new java_event_handler_memory_resource(env, handler_obj, resource));
+    auto replaced_resource = rmm::mr::set_default_resource(Java_memory_resource.get());
+    if (resource != replaced_resource) {
+      rmm::mr::set_default_resource(replaced_resource);
+      Java_memory_resource.reset(nullptr);
+      JNI_THROW_NEW(env, RMM_EXCEPTION_CLASS,
+          "Concurrent modification detected while installing memory resource", );
+    }
+  }
+}
 
 } // anonymous namespace
 
 extern "C" {
 
 JNIEXPORT void JNICALL Java_ai_rapids_cudf_Rmm_initializeInternal(JNIEnv *env, jclass clazz,
-                                                          jint allocation_mode,
-                                                          jboolean enable_logging,
-                                                          jlong pool_size,
-                                                          jobject handler_obj) {
+                                                                  jint allocation_mode,
+                                                                  jboolean enable_logging,
+                                                                  jlong pool_size,
+                                                                  jobject handler_obj) {
   if (rmmIsInitialized(nullptr)) {
     JNI_THROW_NEW(env, "java/lang/IllegalStateException", "RMM already initialized", );
   }
@@ -134,9 +165,7 @@ JNIEXPORT void JNICALL Java_ai_rapids_cudf_Rmm_initializeInternal(JNIEnv *env, j
   opts.enable_logging = enable_logging == JNI_TRUE;
   opts.initial_pool_size = pool_size;
   JNI_RMM_TRY(env, , rmmInitialize(&opts));
-  if (handler_obj != nullptr) {
-    Memory_resource.reset(new java_event_handler_memory_resource(env, handler_obj));
-  }
+  set_java_device_memory_resource(env, handler_obj);
 }
 
 JNIEXPORT jboolean JNICALL Java_ai_rapids_cudf_Rmm_isInitializedInternal(JNIEnv *env, jclass clazz) {
@@ -144,7 +173,7 @@ JNIEXPORT jboolean JNICALL Java_ai_rapids_cudf_Rmm_isInitializedInternal(JNIEnv 
 }
 
 JNIEXPORT void JNICALL Java_ai_rapids_cudf_Rmm_shutdown(JNIEnv *env, jclass clazz) {
-  Memory_resource.reset(nullptr);
+  set_java_device_memory_resource(env, nullptr);
   JNI_RMM_TRY(env, , rmmFinalize());
 }
 
@@ -171,14 +200,7 @@ JNIEXPORT void JNICALL Java_ai_rapids_cudf_Rmm_freeDeviceBuffer(JNIEnv *env, jcl
 
 JNIEXPORT void JNICALL Java_ai_rapids_cudf_Rmm_setEventHandlerInternal(JNIEnv* env, jclass,
                                                                        jobject handler_obj) {
-  if (Memory_resource && handler_obj != nullptr) {
-    JNI_THROW_NEW(env, "ai/rapids/cudf/RmmException", "Another event handler is already set", )
-    return;
-  }
-  Memory_resource.reset(nullptr);
-  if (handler_obj != nullptr) {
-    Memory_resource.reset(new java_event_handler_memory_resource(env, handler_obj));
-  }
+  set_java_device_memory_resource(env, handler_obj);
 }
 
 JNIEXPORT jstring JNICALL Java_ai_rapids_cudf_Rmm_getLog(JNIEnv *env, jclass clazz, jlong size,
