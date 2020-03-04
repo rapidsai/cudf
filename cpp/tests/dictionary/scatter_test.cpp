f7d29cc14dc3fac1fb30da1070f09291a5cb376a
David Wendt <dwendt@nvidia.com>
2020-03-04 09:30:21 -0500

    add dictionary scatter test

---
+++ cpp/tests/dictionary/scatter_test.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cudf/dictionary/dictionary_column_view.hpp>
+#include <cudf/dictionary/encode.hpp>
+#include <cudf/copying.hpp>
+#include <cudf/sorting.hpp>
+#include <tests/utilities/base_fixture.hpp>
+#include <tests/utilities/column_utilities.hpp>
+#include <tests/utilities/column_wrapper.hpp>
+
+#include <vector>
+
+struct DictionaryScatterTest : public cudf::test::BaseFixture {};
+
+TEST_F(DictionaryScatterTest, Scatter)
+{
+    cudf::test::strings_column_wrapper strings{ "eee", "aaa", "ddd", "bbb", "ccc", "ccc", "ccc", "eee", "aaa" };
+    auto dictionary = cudf::dictionary::encode( strings );
+    cudf::dictionary_column_view view(dictionary->view());
+
+    cudf::test::fixed_width_column_wrapper<int32_t> gather_map{0,4,3,1};
+    auto table_result = cudf::experimental::gather(cudf::table_view{{view.parent()}}, gather_map)->release();
+    auto result = cudf::dictionary_column_view(table_result.front()->view());
+
+    cudf::test::strings_column_wrapper expected{ "eee", "ccc", "bbb", "aaa" };
+    auto decoded = cudf::dictionary::decode(result);
+    cudf::test::expect_columns_equal(expected,decoded->view());
+}
+
+#if 0
+TEST_F(DictionaryScatterTest, ScatterWithNulls)
+{
+    cudf::test::fixed_width_column_wrapper<int64_t> data{ {1,5,5,3,7,1},{0,1,0,1,1,1} };
+
+    auto dictionary = cudf::dictionary::encode( data );
+    cudf::dictionary_column_view view(dictionary->view());
+
+    cudf::test::fixed_width_column_wrapper<int16_t> gather_map{{4, 1, 2, 4}};
+    auto table_result = cudf::experimental::gather(cudf::table_view{{dictionary->view()}}, gather_map);
+    auto result = cudf::dictionary_column_view(table_result->view().column(0));
+
+    cudf::test::fixed_width_column_wrapper<int64_t> expected{ {7,5,5,7},{1,1,0,1} };
+    auto result_decoded = cudf::dictionary::decode(result);
+    cudf::test::expect_columns_equal( expected, result_decoded->view() );
+}
+#endif
