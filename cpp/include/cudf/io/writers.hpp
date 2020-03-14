7beb45c39095517fbc3fa2e007422bdbed5537b4
OlivierNV <45697062+OlivierNV@users.noreply.github.com>
2020-03-13 22:55:14 -0700

    Remove extra newline in ORC writer

--- cpp/include/cudf/io/writers.hpp
+++ cpp/include/cudf/io/writers.hpp
@@ -115,8 +115,7 @@ class writer {
    * @param metadata Table metadata and column names
    * @param stream Optional stream to use for device memory alloc and kernels
    */
-  void write_all(table_view const& table, const table_metadata *metadata = nullptr,
-                 cudaStream_t stream = 0);
+  void write_all(table_view const& table, const table_metadata *metadata = nullptr, cudaStream_t stream = 0);
 
   /**
    * @brief Begins the chunked/streamed write process.
