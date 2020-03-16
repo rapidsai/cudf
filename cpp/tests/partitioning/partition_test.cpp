c82ca8fd6f0233a006105343a64df4465c85cccc
Jake Hemstad <jhemstad@nvidia.com>
2020-03-16 11:58:40 -0500

    Add initial partitioning tests.

---
+++ cpp/tests/partitioning/partition_test.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <cudf/partitioning.hpp>
+#include <cudf/table/table.hpp>
+#include <tests/utilities/base_fixture.hpp>
+#include <tests/utilities/column_utilities.hpp>
+#include <tests/utilities/column_wrapper.hpp>
+#include <tests/utilities/table_utilities.hpp>
+#include <tests/utilities/type_lists.hpp>
+
+class PartitionTest : public cudf::test::BaseFixture{};
+
+
+using cudf::test::fixed_width_column_wrapper;
+
+TEST_F(PartitionTest, First){
+    fixed_width_column_wrapper<int32_t> ints{0,1,2,3,4,5};
+    fixed_width_column_wrapper<int32_t> map{0,1,2,3,4,5};
+
+    auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
+
+    auto offsets = cudf::test::to_host<int32_t>(map);
+    std::vector<int32_t> expected_offsets{0,1,2,3,4,5};
+
+    EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(), offsets.first.begin()));
+    cudf::test::expect_columns_equal(ints, result.first->get_column(0));
+}
+
+TEST_F(PartitionTest, Second){
+    fixed_width_column_wrapper<int32_t> ints{13,5,7,3,1,0};
+    fixed_width_column_wrapper<int32_t> map{5,4,3,2,1,0};
+
+    auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
+
+    auto offsets = cudf::test::to_host<int32_t>(map);
+    std::vector<int32_t> expected_offsets{0,1,2,3,4,5,6};
+    fixed_width_column_wrapper<int32_t> expected_ints{0,1,3,7,5,13};
+
+    auto result_offsets = result.second;
+
+    std::copy(result_offsets.begin(), result_offsets.end(), std::ostream_iterator<int32_t>(std::cout, ","));
+    std::cout << std::endl;
+
+    EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(), result_offsets.begin()));
+
+    cudf::test::print(result.first->get_column(0));
+    std::cout << std::endl;
+
+    cudf::test::expect_columns_equal(expected_ints, result.first->get_column(0));
+}
