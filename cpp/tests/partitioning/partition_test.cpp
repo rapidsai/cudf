e420e692d88a7c1b6620f7138ab1e69ba5a5fd9a
Jake Hemstad <jhemstad@nvidia.com>
2020-03-16 17:54:59 -0500

    Add test for error case where input and map size are mismatched.

--- cpp/tests/partitioning/partition_test.cpp
+++ cpp/tests/partitioning/partition_test.cpp
@@ -21,52 +21,63 @@
 #include <tests/utilities/table_utilities.hpp>
 #include <tests/utilities/type_lists.hpp>
 
-class PartitionTest : public cudf::test::BaseFixture{};
-
+class PartitionTest : public cudf::test::BaseFixture {};
 
 using cudf::test::fixed_width_column_wrapper;
 
-TEST_F(PartitionTest, First){
-    fixed_width_column_wrapper<int32_t> ints{0,1,2,3,4,5};
-    fixed_width_column_wrapper<int32_t> map{0,1,2,3,4,5};
+TEST_F(PartitionTest, First) {
+  fixed_width_column_wrapper<int32_t> ints{0, 1, 2, 3, 4, 5};
+  fixed_width_column_wrapper<int32_t> map{0, 1, 2, 3, 4, 5};
 
-    auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
+  auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
 
-    auto offsets = cudf::test::to_host<int32_t>(map);
-    std::vector<int32_t> expected_offsets{0,1,2,3,4,5};
+  auto offsets = cudf::test::to_host<int32_t>(map);
+  std::vector<int32_t> expected_offsets{0, 1, 2, 3, 4, 5};
 
-    EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(), offsets.first.begin()));
-    cudf::test::expect_columns_equal(ints, result.first->get_column(0));
+  EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(),
+                         offsets.first.begin()));
+  cudf::test::expect_columns_equal(ints, result.first->get_column(0));
 }
 
-TEST_F(PartitionTest, Second){
-    fixed_width_column_wrapper<int32_t> ints{13,5,7,3,1,0};
-    fixed_width_column_wrapper<int32_t> map{5,4,3,2,1,0};
+TEST_F(PartitionTest, Second) {
+  fixed_width_column_wrapper<int32_t> ints{13, 5, 7, 3, 1, 0};
+  fixed_width_column_wrapper<int32_t> map{5, 4, 3, 2, 1, 0};
 
-    auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
+  auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
 
-    auto offsets = cudf::test::to_host<int32_t>(map);
-    std::vector<int32_t> expected_offsets{0,1,2,3,4,5,6};
-    fixed_width_column_wrapper<int32_t> expected_ints{0,1,3,7,5,13};
+  auto offsets = cudf::test::to_host<int32_t>(map);
+  std::vector<int32_t> expected_offsets{0, 1, 2, 3, 4, 5, 6};
+  fixed_width_column_wrapper<int32_t> expected_ints{0, 1, 3, 7, 5, 13};
 
-    auto result_offsets = result.second;
+  auto result_offsets = result.second;
 
-    EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(), result_offsets.begin()));
+  EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(),
+                         result_offsets.begin()));
 
-    cudf::test::expect_columns_equal(expected_ints, result.first->get_column(0));
+  cudf::test::expect_columns_equal(expected_ints, result.first->get_column(0));
 }
 
-TEST_F(PartitionTest, EmptyInputs){
-    fixed_width_column_wrapper<int32_t> empty_column{};
-    fixed_width_column_wrapper<int32_t> empty_map{};
+TEST_F(PartitionTest, EmptyInputs) {
+  fixed_width_column_wrapper<int32_t> empty_column{};
+  fixed_width_column_wrapper<int32_t> empty_map{};
 
-    auto result = cudf::experimental::partition(cudf::table_view{{empty_column}}, empty_map, 10);
+  auto result = cudf::experimental::partition(cudf::table_view{{empty_column}},
+                                              empty_map, 10);
 
-    auto result_offsets = result.second;
+  auto result_offsets = result.second;
 
-    EXPECT_TRUE(result_offsets.empty());
+  EXPECT_TRUE(result_offsets.empty());
 
-    std::copy(result_offsets.begin(), result_offsets.end(), std::ostream_iterator<int32_t>(std::cout, ","));
+  std::copy(result_offsets.begin(), result_offsets.end(),
+            std::ostream_iterator<int32_t>(std::cout, ","));
 
-    cudf::test::expect_columns_equal(empty_column, result.first->get_column(0));
+  cudf::test::expect_columns_equal(empty_column, result.first->get_column(0));
+}
+
+TEST_F(PartitionTest, MapInputSizeMismatch) {
+  fixed_width_column_wrapper<int32_t> input{1, 2, 3};
+  fixed_width_column_wrapper<int32_t> map{1, 2};
+
+  EXPECT_THROW(cudf::experimental::partition(cudf::table_view{{input}}, map, 3),
+               cudf::logic_error);
 }
