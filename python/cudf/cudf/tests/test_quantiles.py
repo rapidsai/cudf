8b42bdb21cc4d833b64c5be3d9d6919a1333b85e
Christopher Harris <charris@nvidia.com>
2020-03-18 14:55:31 -0500

    quantiles: add python unit test for presorted input

--- python/cudf/cudf/tests/test_quantiles.py
+++ python/cudf/cudf/tests/test_quantiles.py
@@ -16,6 +16,18 @@ def test_single_q():
     assert_eq(pdf_q, gdf_q, check_index_type=False)
 
 
+def test_single_q_presorted():
+    q = 0.5
+
+    pdf = pd.DataFrame({"a": [4, 7, 8, 13, 24]})
+    gdf = cudf.from_pandas(pdf)
+
+    pdf_q = pdf.quantile(q, interpolation="nearest")
+    gdf_q = gdf.quantiles(q, interpolation="nearest")
+
+    assert_eq(pdf_q, gdf_q, check_index_type=False)
+
+
 def test_with_index():
     q = [0, 0.5, 1]
 
