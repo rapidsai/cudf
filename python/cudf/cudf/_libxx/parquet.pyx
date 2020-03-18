1bfe23147b338fb3340963936fecd03d8328a7c5
galipremsagar <sagarprem75@gmail.com>
2020-03-18 09:27:43 -0700

    add categorical validation

--- python/cudf/cudf/_libxx/parquet.pyx
+++ python/cudf/cudf/_libxx/parquet.pyx
@@ -8,7 +8,7 @@ import os
 import pyarrow as pa
 import json
 
-from cudf.utils.dtypes import np_to_pa_dtype
+from cudf.utils.dtypes import np_to_pa_dtype, is_categorical_dtype
 from libc.stdlib cimport free
 from libcpp.memory cimport unique_ptr, make_unique
 from libcpp.string cimport string
@@ -44,7 +44,13 @@ cpdef generate_pandas_metadata(Table table, index):
     # Columns
     for name, col in table._data.items():
         col_names.append(name)
-        types.append(np_to_pa_dtype(col.dtype))
+        if is_categorical_dtype(col):
+            raise ValueError(
+                "'category' column dtypes are currently not "
+                + "supported by the gpu accelerated parquet writer"
+            )
+        else:
+            types.append(np_to_pa_dtype(col.dtype))
 
     # Indexes
     if index is not False:
@@ -66,7 +72,13 @@ cpdef generate_pandas_metadata(Table table, index):
                 else:
                     descr = name
                     col_names.append(name)
-                    types.append(np_to_pa_dtype(idx.dtype))
+                    if is_categorical_dtype(idx):
+                        raise ValueError(
+                            "'category' column dtypes are currently not "
+                            + "supported by the gpu accelerated parquet writer"
+                        )
+                    else:
+                        types.append(np_to_pa_dtype(idx.dtype))
                     index_levels.append(idx)
                 index_descriptors.append(descr)
             else:
