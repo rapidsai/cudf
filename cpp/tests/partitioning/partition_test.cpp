0fd7f4772242b41f004ef81809edf702ecedfa5c
Jake Hemstad <jhemstad@nvidia.com>
2020-03-19 08:35:00 -0500

    Correct checking against returned offsets.

--- cpp/tests/partitioning/partition_test.cpp
+++ cpp/tests/partitioning/partition_test.cpp
@@ -25,26 +25,35 @@ class PartitionTest : public cudf::test::BaseFixture {};
 
 using cudf::test::fixed_width_column_wrapper;
 
-TEST_F(PartitionTest, First) {
+TEST_F(PartitionTest, Identity) {
   fixed_width_column_wrapper<int32_t> ints{0, 1, 2, 3, 4, 5};
   fixed_width_column_wrapper<int32_t> map{0, 1, 2, 3, 4, 5};
 
   auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
 
-  auto offsets = cudf::test::to_host<int32_t>(map);
-  std::vector<int32_t> expected_offsets{0, 1, 2, 3, 4, 5};
+  auto actual_offsets = result.second;
+  std::vector<int32_t> expected_offsets{0, 1, 2, 3, 4, 5, 6};
 
   EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(),
-                         offsets.first.begin()));
+                         actual_offsets.begin()));
   cudf::test::expect_columns_equal(ints, result.first->get_column(0));
 }
 
-TEST_F(PartitionTest, Second) {
+TEST_F(PartitionTest, Reverse) {
   fixed_width_column_wrapper<int32_t> ints{13, 5, 7, 3, 1, 0};
   fixed_width_column_wrapper<int32_t> map{5, 4, 3, 2, 1, 0};
 
   auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
 
+  auto actual_offsets = result.second;
+  std::vector<int32_t> expected_offsets{0, 1, 2, 3, 4, 5, 6};
+  fixed_width_column_wrapper<int32_t> expected_ints{0, 1, 3, 7, 5, 13};
+
+  EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(),
+                         actual_offsets.begin()));
+
+  cudf::test::expect_columns_equal(expected_ints, result.first->get_column(0));
+}
   auto offsets = cudf::test::to_host<int32_t>(map);
   std::vector<int32_t> expected_offsets{0, 1, 2, 3, 4, 5, 6};
   fixed_width_column_wrapper<int32_t> expected_ints{0, 1, 3, 7, 5, 13};
