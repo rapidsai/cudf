858e6fcfb288a0e176d98a350cafc1c9417781a9
Jake Hemstad <jhemstad@nvidia.com>
2020-03-19 11:09:02 -0500

    Make unit tests parameterized on input/map types.

--- cpp/tests/partitioning/partition_test.cpp
+++ cpp/tests/partitioning/partition_test.cpp
@@ -21,54 +21,28 @@
 #include <tests/utilities/table_utilities.hpp>
 #include <tests/utilities/type_lists.hpp>
 
-class PartitionTest : public cudf::test::BaseFixture {};
+template <typename T>
+class PartitionTest : public cudf::test::BaseFixture {
+  using value_type = cudf::test::GetType<T, 0>;
+  using map_type = cudf::test::GetType<T, 1>;
+};
+
+using types = cudf::test::CrossProduct<cudf::test::FixedWidthTypes, cudf::test::IntegralTypes>;
+
+//using types = cudf::test::Types<cudf::test::Types<int32_t, int32_t> >;
+
+TYPED_TEST_CASE(PartitionTest, types);
 
 using cudf::test::fixed_width_column_wrapper;
+using cudf::test::strings_column_wrapper;
 
-TEST_F(PartitionTest, Identity) {
-  fixed_width_column_wrapper<int32_t> ints{0, 1, 2, 3, 4, 5};
-  fixed_width_column_wrapper<int32_t> map{0, 1, 2, 3, 4, 5};
+// Exceptional cases
+TYPED_TEST(PartitionTest, EmptyInputs) {
+  using value_type = cudf::test::GetType<TypeParam, 0>;
+  using map_type = cudf::test::GetType<TypeParam, 1>;
 
-  auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
-
-  auto actual_offsets = result.second;
-  std::vector<int32_t> expected_offsets{0, 1, 2, 3, 4, 5, 6};
-
-  EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(),
-                         actual_offsets.begin()));
-  cudf::test::expect_columns_equal(ints, result.first->get_column(0));
-}
-
-TEST_F(PartitionTest, Reverse) {
-  fixed_width_column_wrapper<int32_t> ints{13, 5, 7, 3, 1, 0};
-  fixed_width_column_wrapper<int32_t> map{5, 4, 3, 2, 1, 0};
-
-  auto result = cudf::experimental::partition(cudf::table_view{{ints}}, map, 6);
-
-  auto actual_offsets = result.second;
-  std::vector<int32_t> expected_offsets{0, 1, 2, 3, 4, 5, 6};
-  fixed_width_column_wrapper<int32_t> expected_ints{0, 1, 3, 7, 5, 13};
-
-  EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(),
-                         actual_offsets.begin()));
-
-  cudf::test::expect_columns_equal(expected_ints, result.first->get_column(0));
-}
-  auto offsets = cudf::test::to_host<int32_t>(map);
-  std::vector<int32_t> expected_offsets{0, 1, 2, 3, 4, 5, 6};
-  fixed_width_column_wrapper<int32_t> expected_ints{0, 1, 3, 7, 5, 13};
-
-  auto result_offsets = result.second;
-
-  EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(),
-                         result_offsets.begin()));
-
-  cudf::test::expect_columns_equal(expected_ints, result.first->get_column(0));
-}
-
-TEST_F(PartitionTest, EmptyInputs) {
-  fixed_width_column_wrapper<int32_t> empty_column{};
-  fixed_width_column_wrapper<int32_t> empty_map{};
+  fixed_width_column_wrapper<value_type> empty_column{};
+  fixed_width_column_wrapper<map_type> empty_map{};
 
   auto result = cudf::experimental::partition(cudf::table_view{{empty_column}},
                                               empty_map, 10);
@@ -77,24 +51,78 @@ TEST_F(PartitionTest, EmptyInputs) {
 
   EXPECT_TRUE(result_offsets.empty());
 
-  std::copy(result_offsets.begin(), result_offsets.end(),
-            std::ostream_iterator<int32_t>(std::cout, ","));
-
   cudf::test::expect_columns_equal(empty_column, result.first->get_column(0));
 }
 
-TEST_F(PartitionTest, MapInputSizeMismatch) {
-  fixed_width_column_wrapper<int32_t> input{1, 2, 3};
-  fixed_width_column_wrapper<int32_t> map{1, 2};
+TYPED_TEST(PartitionTest, MapInputSizeMismatch) {
+  using value_type = cudf::test::GetType<TypeParam, 0>;
+  using map_type = cudf::test::GetType<TypeParam, 1>;
+
+  fixed_width_column_wrapper<value_type> input{1, 2, 3};
+  fixed_width_column_wrapper<map_type> map{1, 2};
 
   EXPECT_THROW(cudf::experimental::partition(cudf::table_view{{input}}, map, 3),
                cudf::logic_error);
 }
 
-TEST_F(PartitionTest, MapWithNullsThrows) {
-  fixed_width_column_wrapper<int32_t> input{1, 2, 3};
-  fixed_width_column_wrapper<int32_t> map{{1, 2},{1,0}};
+TYPED_TEST(PartitionTest, MapWithNullsThrows) {
+  using value_type = cudf::test::GetType<TypeParam, 0>;
+  using map_type = cudf::test::GetType<TypeParam, 1>;
+
+  fixed_width_column_wrapper<value_type> input{1, 2, 3};
+  fixed_width_column_wrapper<map_type> map{{1, 2}, {1, 0}};
 
   EXPECT_THROW(cudf::experimental::partition(cudf::table_view{{input}}, map, 3),
                cudf::logic_error);
+}
+
+// Normal cases
+TYPED_TEST(PartitionTest, Identity) {
+  using value_type = cudf::test::GetType<TypeParam, 0>;
+  using map_type = cudf::test::GetType<TypeParam, 1>;
+
+  fixed_width_column_wrapper<value_type> first{0, 1, 2, 3, 4, 5};
+  strings_column_wrapper strings{"this", "is", "a", "column", "of", "strings"};
+  auto table_to_partition = cudf::table_view{{first, strings}};
+
+  fixed_width_column_wrapper<map_type> map{0, 1, 2, 3, 4, 5};
+
+  auto result = cudf::experimental::partition(table_to_partition, map, 6);
+
+  auto const& actual_offsets = result.second;
+  std::vector<cudf::size_type> expected_offsets{0, 1, 2, 3, 4, 5, 6};
+
+  EXPECT_TRUE(std::equal(expected_offsets.begin(), expected_offsets.end(),
+                         actual_offsets.begin()));
+
+  auto const& partitioned_table = result.first;
+
+  cudf::test::expect_tables_equal(*partitioned_table, table_to_partition);
+}
+
+TYPED_TEST(PartitionTest, Reverse) {
+  using value_type = cudf::test::GetType<TypeParam, 0>;
+  using map_type = cudf::test::GetType<TypeParam, 1>;
+
+  fixed_width_column_wrapper<value_type> first{0, 1, 3, 7, 5, 13};
+  strings_column_wrapper strings{"this", "is", "a", "column", "of", "strings"};
+  auto table_to_partition = cudf::table_view{{first, strings}};
+
+  fixed_width_column_wrapper<map_type> map{5, 4, 3, 2, 1, 0};
+
+  auto result = cudf::experimental::partition(table_to_partition, map, 6);
+
+  auto const& actual_offsets = result.second;
+  std::vector<cudf::size_type> expected_offsets{0, 1, 2, 3, 4, 5, 6};
+  EXPECT_EQ(expected_offsets, actual_offsets);
+
+  fixed_width_column_wrapper<value_type> expected_first{13, 5, 7, 3, 1, 0};
+  strings_column_wrapper expected_strings{"strings", "of", "column",
+                                          "a",       "is", "this"};
+  auto expected_partitioned_table =
+      cudf::table_view{{expected_first, expected_strings}};
+
+  auto const& partitioned_table = result.first;
+  cudf::test::expect_tables_equal(*partitioned_table,
+                                  expected_partitioned_table);
 }
