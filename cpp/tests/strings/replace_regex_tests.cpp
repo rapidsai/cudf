de037af41c08c2fc179e4410cbf8e13eed434fba
davidwendt <dwendt@nvidia.com>
2020-03-25 09:49:01 -0400

    add some tests for invalid regex cases

--- cpp/tests/strings/replace_regex_tests.cpp
+++ cpp/tests/strings/replace_regex_tests.cpp
@@ -84,6 +84,19 @@ TEST_F(StringsReplaceTests, ReplaceMultiRegexTest)
     cudf::test::expect_columns_equal(*results,expected);
 }
 
+TEST_F(StringsReplaceTests, InvalidRegex)
+{
+    cudf::test::strings_column_wrapper strings( {"abc*def|ghi+jkl",""} ); // these do not really matter
+    auto strings_view = cudf::strings_column_view(strings);
+
+    // these are quantifiers that do not have a preceding character/class
+    EXPECT_THROW( cudf::strings::replace_re(strings_view,"*",cudf::string_scalar("")), cudf::logic_error );
+    EXPECT_THROW( cudf::strings::replace_re(strings_view,"|",cudf::string_scalar("")), cudf::logic_error );
+    EXPECT_THROW( cudf::strings::replace_re(strings_view,"+",cudf::string_scalar("")), cudf::logic_error );
+    EXPECT_THROW( cudf::strings::replace_re(strings_view,"ab(*)",cudf::string_scalar("")), cudf::logic_error );
+}
+
+
 TEST_F(StringsReplaceTests, ReplaceBackrefsRegexTest)
 {
     std::vector<const char*> h_strings{ "the quick brown fox jumps over the lazy dog",
