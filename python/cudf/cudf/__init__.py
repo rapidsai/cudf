5961872637b81322579b4535c893ec57addd8662
galipremsagar <sagarprem75@gmail.com>
2020-03-25 10:44:56 -0700

    add GPU support, runtime & driver checks

--- python/cudf/cudf/__init__.py
+++ python/cudf/cudf/__init__.py
@@ -1,9 +1,58 @@
 # Copyright (c) 2018-2019, NVIDIA CORPORATION.
+""" __init__.py
 
+   isort:skip_file
+"""
 import cupy
 
-import rmm
+from cudf.errors import UnSupportedGPUError, UnSupportedCUDAError
 
+gpus_count = cupy.cuda.runtime.getDeviceCount()
+
+
+if gpus_count > 0:
+    for device in range(0, gpus_count):
+        # cudaDevAttrComputeCapabilityMajor - 75
+        major_version = cupy.cuda.runtime.deviceGetAttribute(75, device)
+
+        if major_version >= 6:
+            # You have a GPU with NVIDIA Pascal™ architecture or better
+            pass
+        else:
+            raise UnSupportedGPUError(
+                "You will need a GPU with NVIDIA Pascal™ architecture or better"
+            )
+
+    cuda_runtime_version = cupy.cuda.runtime.runtimeGetVersion()
+
+    if cuda_runtime_version > 10000:
+        # CUDA Runtime Version Check: Runtime version is greater than 10000
+        pass
+    else:
+        raise UnSupportedCUDAError(
+            "Please update your CUDA Runtime to 10.0 or above"
+        )
+
+    cuda_driver_version = cupy.cuda.runtime.driverGetVersion()
+
+    if cuda_driver_version == 0:
+        raise UnSupportedCUDAError("Please install CUDA Driver")
+    elif cuda_driver_version >= cuda_runtime_version:
+        # CUDA Driver Version Check: Driver Runtime version is >= Runtime version
+        pass
+    else:
+        raise UnSupportedCUDAError(
+            "Please update your CUDA Driver to 10.0 or above"
+        )
+
+else:
+    import warnings
+
+    warnings.warn(
+        "You donot have an NVIDIA GPU, please install one and try again"
+    )
+
+import rmm
 from cudf import core, datasets
 from cudf._version import get_versions
 from cudf.core import DataFrame, Index, MultiIndex, Series, from_pandas, merge
