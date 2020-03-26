dc4f7820459f78bec71e2a7a3560aa6ac64bdd16
Trevor Smith <trevorsm7@gmail.com>
2020-03-26 07:33:05 -0700

    Add optimized strings concatenate kernels

--- cpp/src/copying/concatenate.cu
+++ cpp/src/copying/concatenate.cu
@@ -17,6 +17,7 @@
 
 #include <cudf/column/column.hpp>
 #include <cudf/column/column_device_view.cuh>
+#include <cudf/column/column_factories.hpp>
 #include <cudf/table/table.hpp>
 #include <cudf/table/table_device_view.cuh>
 #include <cudf/detail/copy.hpp>
@@ -81,6 +82,83 @@ auto create_device_views(
       std::move(d_views),
       std::move(d_offsets),
       output_size);
+}
+
+// Using a functor instead of a lambda as a workaround for:
+// error: The enclosing parent function ("create_strings_device_views") for an
+// extended __device__ lambda must not have deduced return type
+struct chars_size_transform {
+  __device__ size_type operator()(column_device_view const& col) const {
+    if (col.size() > 0) {
+      constexpr auto offsets_index = strings_column_view::offsets_column_index;
+      auto d_offsets = col.child(offsets_index).data<int32_t>();
+      return d_offsets[col.size() + col.offset()] - d_offsets[col.offset()];
+    } else {
+      return 0;
+    }
+  }
+};
+
+auto create_strings_device_views(
+    std::vector<column_view> const& views, cudaStream_t stream) {
+
+  // Create device views for each input view
+  using CDViewPtr = decltype(column_device_view::create(
+      std::declval<column_view>(), std::declval<cudaStream_t>()));
+  auto device_view_owners = std::vector<CDViewPtr>(views.size());
+  std::transform(views.cbegin(), views.cend(),
+      device_view_owners.begin(),
+      [stream](auto const& col) {
+        return column_device_view::create(col, stream);
+      });
+
+  // Assemble contiguous array of device views
+  auto device_views = thrust::host_vector<column_device_view>();
+  device_views.reserve(views.size());
+  std::transform(device_view_owners.cbegin(), device_view_owners.cend(),
+      std::back_inserter(device_views),
+      [](auto const& col) {
+        return *col;
+      });
+  auto d_views = rmm::device_vector<column_device_view>{device_views};
+
+  // Compute the partition offsets and size of offset column
+  auto input_offsets = thrust::host_vector<size_type>(views.size() + 1);
+  thrust::transform_inclusive_scan(thrust::host,
+      device_views.cbegin(), device_views.cend(),
+      std::next(input_offsets.begin()),
+      [](auto const& col) {
+        return col.size();
+      },
+      thrust::plus<size_type>{});
+  auto const d_input_offsets = rmm::device_vector<size_type>{input_offsets};
+  auto const output_size = input_offsets.back();
+
+  // Compute the partition offsets and size of chars column
+  // Using separate transform and inclusive_scan because
+  // transform_inclusive_scan fails to compile with:
+  // error: the default constructor of "cudf::column_device_view" cannot be
+  // referenced -- it is a deleted function
+  auto d_partition_offsets = rmm::device_vector<size_type>(views.size() + 1);
+  thrust::transform(rmm::exec_policy(stream)->on(stream),
+      d_views.cbegin(), d_views.cend(),
+      std::next(d_partition_offsets.begin()),
+      chars_size_transform{});
+  thrust::inclusive_scan(rmm::exec_policy(stream)->on(stream),
+      d_partition_offsets.cbegin(), d_partition_offsets.cend(),
+      d_partition_offsets.begin());
+
+  // TODO just copy the last element back to host
+  auto const partition_offsets = thrust::host_vector<size_type>{d_partition_offsets};
+  auto const output_chars_size = partition_offsets.back();
+
+  return std::make_tuple(
+      std::move(device_view_owners),
+      std::move(d_views),
+      std::move(d_input_offsets),
+      std::move(d_partition_offsets),
+      output_size,
+      output_chars_size);
 }
 
 /**---------------------------------------------------------------------------*
@@ -253,19 +331,17 @@ __global__ void fused_concatenate_kernel(column_device_view const* input_views,
                             input_offsets + num_input_views, output_index);
     size_type const partition_index = offset_it - input_offsets;
 
-    // Copy input data to output and read bitmask
-    bool bit_is_set = false;
+    // Copy input data to output
     auto const offset_index = output_index - *offset_it;
     auto const& input_view = input_views[partition_index];
     auto const* input_data = input_view.data<T>();
     output_data[output_index] = input_data[offset_index];
-    if (Nullable) {
-      bit_is_set = input_view.is_valid(offset_index);
-    }
 
     if (Nullable) {
-      // First thread writes bitmask word
+      bool const bit_is_set = input_view.is_valid(offset_index);
       bitmask_type const new_word = __ballot_sync(active_mask, bit_is_set);
+
+      // First thread writes bitmask word
       if (threadIdx.x % experimental::detail::warp_size == 0) {
         output_view.null_mask()[word_index(output_index)] = new_word;
       }
@@ -285,6 +361,106 @@ __global__ void fused_concatenate_kernel(column_device_view const* input_views,
     if (threadIdx.x == 0) {
       atomicAdd(out_valid_count, block_valid_count);
     }
+  }
+}
+
+template <size_type block_size, bool Nullable>
+__global__ void
+fused_concatenate_string_offset_kernel(column_device_view const* input_views,
+                                       size_type const* input_offsets,
+                                       size_type const* partition_offsets,
+                                       size_type const num_input_views,
+                                       size_type const output_size,
+                                       size_type* output_data,
+                                       bitmask_type* output_mask,
+                                       size_type* out_valid_count) {
+  size_type output_index = threadIdx.x + blockIdx.x * blockDim.x;
+  size_type warp_valid_count = 0;
+
+  unsigned active_mask;
+  if (Nullable) {
+    active_mask = __ballot_sync(0xFFFF'FFFF, output_index < output_size);
+  }
+  while (output_index < output_size) {
+
+    // Lookup input index by searching for output index in offsets
+    // thrust::prev isn't in CUDA 10.0, so subtracting 1 here instead
+    auto const offset_it = -1 +
+        thrust::upper_bound(thrust::seq, input_offsets,
+                            input_offsets + num_input_views, output_index);
+    size_type const partition_index = offset_it - input_offsets;
+
+    auto const offset_index = output_index - *offset_it;
+    auto const& input_view = input_views[partition_index];
+    constexpr auto offsets_child = strings_column_view::offsets_column_index;
+    auto const* input_data = input_view.child(offsets_child).data<int32_t>();
+    output_data[output_index] =
+        input_data[offset_index + input_view.offset()] // handle parent offset
+        - input_data[input_view.offset()] // subract first offset if non-zero
+        + partition_offsets[partition_index]; // add cumulative chars offset
+
+    if (Nullable) {
+      bool const bit_is_set = input_view.is_valid(offset_index);
+      bitmask_type const new_word = __ballot_sync(active_mask, bit_is_set);
+
+      // First thread writes bitmask word
+      if (threadIdx.x % experimental::detail::warp_size == 0) {
+        output_mask[word_index(output_index)] = new_word;
+      }
+
+      warp_valid_count += __popc(new_word);
+    }
+
+    output_index += blockDim.x * gridDim.x;
+    if (Nullable) {
+      active_mask = __ballot_sync(active_mask, output_index < output_size);
+    }
+  }
+
+  // Fill final offsets index with total size of char data
+  if (output_index == output_size) {
+    output_data[output_size] = partition_offsets[num_input_views];
+  }
+
+  if (Nullable) {
+    using experimental::detail::single_lane_block_sum_reduce;
+    auto block_valid_count = single_lane_block_sum_reduce<block_size, 0>(warp_valid_count);
+    if (threadIdx.x == 0) {
+      atomicAdd(out_valid_count, block_valid_count);
+    }
+  }
+}
+
+__global__ void
+fused_concatenate_string_chars_kernel(column_device_view const* input_views,
+                                      size_type const* partition_offsets,
+                                      size_type const num_input_views,
+                                      size_type const output_size,
+                                      int8_t* output_data) {
+  size_type output_index = threadIdx.x + blockIdx.x * blockDim.x;
+
+  while (output_index < output_size) {
+
+    // Lookup input index by searching for output index in offsets
+    // thrust::prev isn't in CUDA 10.0, so subtracting 1 here instead
+    auto const offset_it = -1 +
+        thrust::upper_bound(thrust::seq, partition_offsets,
+                            partition_offsets + num_input_views, output_index);
+    size_type const partition_index = offset_it - partition_offsets;
+
+    auto const offset_index = output_index - *offset_it;
+    auto const& input_view = input_views[partition_index];
+
+    constexpr auto offsets_child = strings_column_view::offsets_column_index;
+    auto const* input_offsets_data = input_view.child(offsets_child).data<int32_t>();
+
+    constexpr auto chars_child = strings_column_view::chars_column_index;
+    auto const* input_chars_data = input_view.child(chars_child).data<int8_t>();
+
+    auto const first_char = input_offsets_data[input_view.offset()];
+    output_data[output_index] = input_chars_data[offset_index + first_char];
+
+    output_index += blockDim.x * gridDim.x;
   }
 }
 
@@ -344,7 +520,69 @@ struct fused_concatenate {
   template <typename T,
       std::enable_if_t<std::is_same<T, cudf::string_view>::value>* = nullptr>
   std::unique_ptr<column> operator()() {
-    CUDF_FAIL("strings concatenate not yet supported");
+    using mask_policy = cudf::experimental::mask_allocation_policy;
+
+    // Preprocess and upload inputs to device memory
+    auto const device_views = create_strings_device_views(views, stream);
+    auto const& d_views = std::get<1>(device_views);
+    auto const& d_input_offsets = std::get<2>(device_views);
+    auto const& d_partition_offsets = std::get<3>(device_views);
+    auto const output_size = std::get<4>(device_views);
+    auto const output_chars_size = std::get<5>(device_views);
+    auto const output_offsets_size = output_size + 1;
+
+    // Allocate child columns and null mask
+    auto chars_column = make_numeric_column(data_type{INT8},
+        output_chars_size, mask_state::UNALLOCATED, stream, mr);
+    auto offsets_column = make_numeric_column(data_type{INT32},
+        output_offsets_size, mask_state::UNALLOCATED, stream, mr);
+    rmm::device_buffer null_mask;
+    if (has_nulls) {
+      null_mask = create_null_mask(output_size, mask_state::UNINITIALIZED, stream, mr);
+    }
+    size_type null_count{0};
+
+    auto chars_view = chars_column->mutable_view();
+    auto offsets_view = offsets_column->mutable_view();
+
+    { // Launch offsets kernel
+      rmm::device_scalar<size_type> d_valid_count(0);
+
+      constexpr size_type block_size{256};
+      cudf::experimental::detail::grid_1d config(output_offsets_size, block_size);
+      auto const kernel = has_nulls
+          ? fused_concatenate_string_offset_kernel<block_size, true>
+          : fused_concatenate_string_offset_kernel<block_size, false>;
+      kernel<<<config.num_blocks, config.num_threads_per_block, 0, stream>>>(
+          d_views.data().get(),
+          d_input_offsets.data().get(),
+          d_partition_offsets.data().get(),
+          static_cast<size_type>(d_views.size()),
+          output_size,
+          offsets_view.data<size_type>(),
+          reinterpret_cast<bitmask_type*>(null_mask.data()),
+          d_valid_count.data());
+
+      if (has_nulls) {
+        null_count = output_size - d_valid_count.value(stream);
+      }
+    }
+
+    { // Launch chars kernel
+      constexpr size_type block_size{256};
+      cudf::experimental::detail::grid_1d config(output_chars_size, block_size);
+      auto const kernel = fused_concatenate_string_chars_kernel;
+      kernel<<<config.num_blocks, config.num_threads_per_block, 0, stream>>>(
+          d_views.data().get(),
+          d_partition_offsets.data().get(),
+          static_cast<size_type>(d_views.size()),
+          output_chars_size,
+          chars_view.data<int8_t>());
+    }
+
+    return make_strings_column(output_size,
+        std::move(offsets_column), std::move(chars_column),
+        null_count, std::move(null_mask), stream, mr);
   }
 };
 
@@ -371,9 +609,12 @@ concatenate(std::vector<column_view> const& columns_to_concat,
       columns_to_concat.begin(), columns_to_concat.end(),
       [](auto const& col) { return col.has_nulls(); });
   bool const fixed_width = cudf::is_fixed_width(type);
+  bool const strings_col = (type.id() == type_id::STRING);
 
   // Select fused kernel when it can improve performance
-  if (fixed_width && (has_nulls || columns_to_concat.size() > 4)) {
+  // TODO benchmark strings performance and update heuristic
+  if (strings_col ||
+      (fixed_width && (has_nulls || columns_to_concat.size() > 4))) {
     return experimental::type_dispatcher(type,
         detail::fused_concatenate{columns_to_concat, has_nulls, mr, stream});
   } else {
