dfc364722f3ca4c221d2f4ed0f5d732adae9921b
Christopher Harris <charris@nvidia.com>
2020-03-26 11:04:59 -0500

    libcudfxx quantile cython

--- python/cudf/cudf/_libxx/cpp/quantiles.pxd
+++ python/cudf/cudf/_libxx/cpp/quantiles.pxd
@@ -1,20 +1,37 @@
 # Copyright (c) 2020, NVIDIA CORPORATION.
 
+from libcpp cimport bool
 from libcpp.vector cimport vector
 from libcpp.memory cimport unique_ptr
 
+from cudf._libxx.cpp.column.column_view cimport column_view
+from cudf._libxx.cpp.scalar.scalar cimport scalar
 from cudf._libxx.cpp.table.table cimport table
 from cudf._libxx.cpp.table.table_view cimport table_view
 
-cimport cudf._libxx.cpp.types as libcudf_types
+from cudf._libxx.cpp.types cimport (
+    interpolation,
+    null_order,
+    order_info,
+    order,
+    sorted,
+)
 
 
 cdef extern from "cudf/quantiles.hpp" namespace "cudf::experimental" nogil:
+
+    cdef unique_ptr[scalar] quantile (
+        column_view input,
+        double q,
+        interpolation interp,
+        order_info column_order
+    ) except +
+
     cdef unique_ptr[table] quantiles (
         table_view source_table,
         vector[double] q,
-        libcudf_types.interpolation interp,
-        libcudf_types.sorted is_input_sorted,
-        vector[libcudf_types.order] column_order,
-        vector[libcudf_types.null_order] null_precedence
+        interpolation interp,
+        sorted is_input_sorted,
+        vector[order] column_order,
+        vector[null_order] null_precedence
     ) except +
--- python/cudf/cudf/_libxx/quantiles.pyx
+++ python/cudf/cudf/_libxx/quantiles.pyx
@@ -1,22 +1,77 @@
 # Copyright (c) 2020, NVIDIA CORPORATION.
 
+from libcpp cimport bool
 from libcpp.vector cimport vector
 from libcpp.memory cimport unique_ptr
 
 from cudf._libxx.column cimport Column
+from cudf._libxx.scalar cimport Scalar
 from cudf._libxx.table cimport Table
 from cudf._libxx.move cimport move
 from cudf._libxx.types cimport (
     underlying_type_t_order,
     underlying_type_t_null_order,
     underlying_type_t_sorted,
-    underlying_type_t_interpolation
+    underlying_type_t_interpolation,
 )
 
+from cudf._libxx.cpp.column.column_view cimport column_view
+from cudf._libxx.cpp.scalar.scalar cimport scalar
 from cudf._libxx.cpp.table.table cimport table
 from cudf._libxx.cpp.table.table_view cimport table_view
-cimport cudf._libxx.cpp.types as libcudf_types
-cimport cudf._libxx.cpp.quantiles as cpp_quantiles
+from cudf._libxx.cpp.types cimport (
+    interpolation,
+    null_order,
+    order,
+    sorted,
+    order_info,
+)
+from cudf._libxx.cpp.quantiles cimport (
+    quantile as cpp_quantile,
+    quantiles as cpp_quantiles,
+)
+
+
+def quantile(
+    Column input,
+    double q,
+    object interp,
+    object is_sorted,
+    object column_order,
+    object null_precedence
+
+):
+    cdef column_view c_input = input.view()
+    cdef double c_q = q
+    cdef interpolation c_interp = <interpolation>(
+        <underlying_type_t_interpolation> interp
+    )
+    cdef bool c_is_sorted = column_order.is_sorted
+    cdef order_info c_column_order
+
+    c_column_order.ordering = <order>(
+        <underlying_type_t_order> column_order.ordering
+    )
+    c_column_order.ordering = <order>(
+        <underlying_type_t_order> column_order
+    )
+    c_column_order.null_ordering = <null_order>(
+        <underlying_type_t_null_order> null_precedence
+    )
+
+    cdef unique_ptr[scalar] c_result
+
+    with nogil:
+        c_result = move(
+            cpp_quantile(
+                c_input,
+                c_q,
+                c_interp,
+                c_column_order
+            )
+        )
+
+    return Scalar.from_unique_ptr(move(c_result))
 
 
 def quantiles(Table source_table,
@@ -27,33 +82,33 @@ def quantiles(Table source_table,
               list null_precedence):
     cdef table_view c_input = source_table.data_view()
     cdef vector[double] c_q = q
-    cdef libcudf_types.interpolation c_interp = <libcudf_types.interpolation>(
+    cdef interpolation c_interp = <interpolation>(
         <underlying_type_t_interpolation> interp
     )
-    cdef libcudf_types.sorted c_is_input_sorted = <libcudf_types.sorted>(
+    cdef sorted c_is_input_sorted = <sorted>(
         <underlying_type_t_sorted> is_input_sorted
     )
-    cdef vector[libcudf_types.order] c_column_order
-    cdef vector[libcudf_types.null_order] c_null_precedence
+    cdef vector[order] c_column_order
+    cdef vector[null_order] c_null_precedence
 
     c_column_order.reserve(len(column_order))
     c_null_precedence.reserve(len(null_precedence))
 
     for value in column_order:
         c_column_order.push_back(
-            <libcudf_types.order>(<underlying_type_t_order> value)
+            <order>(<underlying_type_t_order> value)
         )
 
     for value in null_precedence:
         c_null_precedence.push_back(
-            <libcudf_types.null_order>(<underlying_type_t_null_order> value)
+            <null_order>(<underlying_type_t_null_order> value)
         )
 
     cdef unique_ptr[table] c_result
 
     with nogil:
         c_result = move(
-            cpp_quantiles.quantiles(
+            cpp_quantiles(
                 c_input,
                 c_q,
                 c_interp,
