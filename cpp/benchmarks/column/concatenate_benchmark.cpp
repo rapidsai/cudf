64f6dded77d700a41192c809865d056d6a51bac8
Trevor Smith <trevorsm7@gmail.com>
2020-03-27 02:50:23 -0700

    Add strings benchmark

--- cpp/benchmarks/column/concatenate_benchmark.cpp
+++ cpp/benchmarks/column/concatenate_benchmark.cpp
@@ -21,22 +21,23 @@
 #include <benchmarks/fixture/benchmark_fixture.hpp>
 #include <benchmarks/synchronization/synchronization.hpp>
 
+#include <thrust/iterator/constant_iterator.h>
+
 #include <vector>
 #include <algorithm>
-
-template <typename T>
-using column_wrapper = cudf::test::fixed_width_column_wrapper<T>;
 
 template <typename T, bool Nullable>
 class Concatenate : public cudf::benchmark {};
 
 template<typename T, bool Nullable>
 static void BM_concatenate(benchmark::State& state) {
+  using column_wrapper = cudf::test::fixed_width_column_wrapper<T>;
+
   auto const num_rows = state.range(0);
   auto const num_cols = state.range(1);
 
   // Create owning columns
-  std::vector<column_wrapper<T>> columns;
+  std::vector<column_wrapper> columns;
   columns.reserve(num_cols);
   std::generate_n(std::back_inserter(columns), num_cols,
     [num_rows]() {
@@ -46,9 +47,9 @@ static void BM_concatenate(benchmark::State& state) {
           [](auto i) {
             return i % 3 == 0;
           });
-        return column_wrapper<T>(iter, iter + num_rows, valid_iter);
+        return column_wrapper(iter, iter + num_rows, valid_iter);
       } else {
-        return column_wrapper<T>(iter, iter + num_rows);
+        return column_wrapper(iter, iter + num_rows);
       }
     });
 
@@ -88,12 +89,14 @@ CONCAT_BENCHMARK_DEFINE(concat_columns_int64_nullable, int64_t, true)
 
 template<typename T, bool Nullable>
 static void BM_concatenate_tables(benchmark::State& state) {
+  using column_wrapper = cudf::test::fixed_width_column_wrapper<T>;
+
   auto const num_rows = state.range(0);
   auto const num_cols = state.range(1);
   auto const num_tables = state.range(2);
 
   // Create owning columns
-  std::vector<column_wrapper<T>> columns;
+  std::vector<column_wrapper> columns;
   columns.reserve(num_cols);
   std::generate_n(std::back_inserter(columns), num_cols * num_tables,
     [num_rows]() {
@@ -103,9 +106,9 @@ static void BM_concatenate_tables(benchmark::State& state) {
           [](auto i) {
             return i % 3 == 0;
           });
-        return column_wrapper<T>(iter, iter + num_rows, valid_iter);
+        return column_wrapper(iter, iter + num_rows, valid_iter);
       } else {
-        return column_wrapper<T>(iter, iter + num_rows);
+        return column_wrapper(iter, iter + num_rows);
       }
     });
 
@@ -153,3 +156,69 @@ BENCHMARK_REGISTER_F(Concatenate, name)                         \
 
 CONCAT_TABLES_BENCHMARK_DEFINE(concat_tables_int64_non_null, int64_t, false)
 CONCAT_TABLES_BENCHMARK_DEFINE(concat_tables_int64_nullable, int64_t, true)
+
+
+template <bool Nullable>
+class ConcatenateStrings : public cudf::benchmark {};
+
+template<bool Nullable>
+static void BM_concatenate_strings(benchmark::State& state) {
+  using column_wrapper = cudf::test::strings_column_wrapper;
+
+  auto const num_rows = state.range(0);
+  auto const num_chars = state.range(1);
+  auto const num_cols = state.range(2);
+
+  std::string str(num_chars, 'a');
+
+  // Create owning columns
+  std::vector<column_wrapper> columns;
+  columns.reserve(num_cols);
+  std::generate_n(std::back_inserter(columns), num_cols,
+    [num_rows, c_str=str.c_str()]() {
+      auto iter = thrust::make_constant_iterator(c_str);
+      if (Nullable) {
+        auto count_it = thrust::make_counting_iterator(0);
+        auto valid_iter = thrust::make_transform_iterator(count_it,
+          [](auto i) {
+            return i % 3 == 0;
+          });
+        return column_wrapper(iter, iter + num_rows, valid_iter);
+      } else {
+        return column_wrapper(iter, iter + num_rows);
+      }
+    });
+
+  // Generate column views
+  std::vector<cudf::column_view> column_views;
+  column_views.reserve(columns.size());
+  std::transform(columns.begin(), columns.end(),
+    std::back_inserter(column_views),
+    [](auto const& col) {
+      return static_cast<cudf::column_view>(col);
+    });
+
+  CHECK_CUDA(0);
+
+  for (auto _ : state) {
+    cuda_event_timer raii(state, true, 0);
+    auto result = cudf::concatenate(column_views);
+  }
+
+  state.SetBytesProcessed(state.iterations() * num_cols * num_rows
+      * (sizeof(int32_t) + num_chars)); // offset + chars
+}
+
+#define CONCAT_STRINGS_BENCHMARK_DEFINE(name, nullable)         \
+BENCHMARK_TEMPLATE_DEFINE_F(ConcatenateStrings, name, nullable) \
+(::benchmark::State& state) {                                   \
+  BM_concatenate_strings<nullable>(state);                      \
+}                                                               \
+BENCHMARK_REGISTER_F(ConcatenateStrings, name)                  \
+  ->RangeMultiplier(4)                                          \
+  ->Ranges({{1<<8, 1<<14}, {8, 128}, {2, 256}})                  \
+  ->Unit(benchmark::kMillisecond)                               \
+  ->UseManualTime();
+
+CONCAT_STRINGS_BENCHMARK_DEFINE(concat_string_columns_non_null, false)
+CONCAT_STRINGS_BENCHMARK_DEFINE(concat_string_columns_nullable, true)
