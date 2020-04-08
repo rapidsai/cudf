e6e9d23955b18325babbd5743e8b9828e761e246
davidwendt <dwendt@nvidia.com>
2020-04-08 14:25:24 -0400

    add NVTX range calls to strings APIs

--- cpp/src/strings/findall.cu
+++ cpp/src/strings/findall.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@
 #include <cudf/strings/strings_column_view.hpp>
 #include <cudf/strings/string_view.cuh>
 #include <cudf/strings/findall.hpp>
+#include <cudf/detail/nvtx/ranges.hpp>
 #include <strings/utilities.hpp>
 #include <strings/regex/regex.cuh>
 
@@ -197,6 +198,7 @@ std::unique_ptr<experimental::table> findall_re( strings_column_view const& stri
                                                  std::string const& pattern,
                                                  rmm::mr::device_memory_resource* mr)
 {
+    CUDF_FUNC_RANGE();
     return detail::findall_re(strings, pattern, mr);
 }
 
