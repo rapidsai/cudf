perform additive masking and fix the logic of mask

python/cudf/cudf/tests/test_cuda_array_interface.py
@@ -128,7 +128,9 @@ def test_cuda_array_interface_as_column(dtype, nulls, mask_type):
     sr = cudf.Series(np.arange(10))
 
     if nulls == "some":
-        sr[[1, 3, 4, 7]] = None
+        sr[
+            sr[True, False, True, False, False, True, True, False, True, True]
+        ] = None
     elif nulls == "all":
         sr[:] = None
 
@@ -155,7 +157,7 @@ def test_cuda_array_interface_as_column(dtype, nulls, mask_type):
                 ]
             )
         elif nulls == "all":
-            obj.__cuda_array_interface__["mask"] = cuda.to_device([False] * 10)
+            obj.__cuda_array_interface__["mask"] = cuda.to_device([True] * 10)
 
     expect = sr
     got = cudf.Series(obj)

python/cudf/cudf/tests/test_dataframe.py
@@ -1784,7 +1784,7 @@ def test_dataframe_boolmask(mask_shape):
         pdf_mask[col] = np.random.randint(0, 2, mask_shape[0]) > 0
     gdf = DataFrame.from_pandas(pdf)
     gdf_mask = DataFrame.from_pandas(pdf_mask)
-
+    # import pdb;pdb.set_trace()
     gdf = gdf[gdf_mask]
     pdf = pdf[pdf_mask]
 
@@ -3596,7 +3596,7 @@ def test_series_value_counts():
         mask = arr != -1
         sr = Series.from_masked_array(arr, Series(mask).as_mask())
         sr.name = "col"
-        df = pd.DataFrame(data=arr[mask], columns=["col"])
+        df = pd.DataFrame(data=arr[~mask], columns=["col"])
         expect = df.col.value_counts().sort_index()
         got = sr.value_counts().sort_index()
 

python/cudf/cudf/tests/test_stats.py
@@ -25,7 +25,7 @@ def test_series_reductions(method, dtype):
         mask = arr > 0.5
 
     arr = arr.astype(dtype)
-    arr2 = arr[mask]
+    arr2 = arr[~mask]
     sr = Series.from_masked_array(arr, Series(mask).as_mask())
 
     def call_test(sr):
@@ -78,8 +78,8 @@ def test_series_unique():
         arr = np.random.randint(low=-1, high=10, size=size)
         mask = arr != -1
         sr = Series.from_masked_array(arr, Series(mask).as_mask())
-        assert set(arr[mask]) == set(sr.unique().to_array())
-        assert len(set(arr[mask])) == sr.nunique()
+        assert set(arr[~mask]) == set(sr.unique().to_array())
+        assert len(set(arr[~mask])) == sr.nunique()
 
 
 @pytest.mark.parametrize(
@@ -286,7 +286,7 @@ def test_series_median(dtype, num_na):
 
     arr = arr.astype(dtype)
     sr = Series.from_masked_array(arr, Series(mask).as_mask())
-    arr2 = arr[mask]
+    arr2 = arr[~mask]
     ps = pd.Series(arr2, dtype=dtype)
 
     actual = sr.median(skipna=True)