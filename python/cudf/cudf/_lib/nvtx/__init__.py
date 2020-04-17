74140117adcd74971f15b7bbf98c15267d0b583f
Ashwin Srinath <shwina@users.noreply.github.com>
2020-04-17 16:40:34 -0400

    Rename env variable

--- python/cudf/cudf/_lib/nvtx/__init__.py
+++ python/cudf/cudf/_lib/nvtx/__init__.py
@@ -4,5 +4,5 @@ import os
 
 from cudf._lib.nvtx.nvtx import _annotate_nop, annotate, pop_range, push_range
 
-if os.getenv("CUDF_DISABLE_NVTX"):
+if os.getenv("PYNVTX_DISABLE"):
     annotate = _annotate_nop
