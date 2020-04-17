43fde571363b46ad37a29e205376626a398dad94
davidwendt <dwendt@nvidia.com>
2020-04-17 09:11:09 -0400

    hates compile warnings

--- cpp/src/rolling/rolling.cu
+++ cpp/src/rolling/rolling.cu
@@ -368,7 +368,7 @@ struct rolling_window_launcher
           // The rows that represent null elements will be having negative values in gather map,
           // and that's why nullify_out_of_bounds/ignore_out_of_bounds is true.
           auto output_table = detail::gather(table_view{{input}}, output->view(), false, true, false, mr, stream);
-          return std::make_unique<cudf::column>(std::move(output_table->get_column(0)));;
+          output = std::make_unique<cudf::column>(std::move(output_table->get_column(0)));;
       }
 
       return output;
