0ce09c6a93b61f1ef3f3e64541d05b8cde2c7b97
Conor Hoekstra <codereport@outlook.com>
2020-04-22 04:54:18 -0400

    Fix for "tests" directory in run-clang-format.py script

--- cpp/scripts/run-clang-format.py
+++ cpp/scripts/run-clang-format.py
@@ -32,7 +32,7 @@ DEFAULT_DIRS = ["cpp/benchmarks",
                 "cpp/include/nvtext",
                 "cpp/include/nvstrings",
                 "cpp/src",
-                "cpp/test"]
+                "cpp/tests"]
 
 
 def parse_args():
