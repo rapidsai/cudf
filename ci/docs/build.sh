2b2ad25be0bc1a54be130dcdf5813b29f6b61888
dillon-cullinan <dcullinan92@gmail.com>
2020-04-24 08:29:55 -0700

    Add docs build script

--- ci/docs/build.sh
+++ ci/docs/build.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
+## Copyright (c) 2018, NVIDIA CORPORATION.
+## Docs Build for cuDF and nvStrings
 
-# Copyright (c) 2018, NVIDIA CORPORATION.
-#########################################
-# cuDF GPU build and test script for CI #
-#########################################
-set -ex
+set -e
 
-# Logger function for build status output
-function logger() {
-  echo -e "\n>>>> $@\n"
-}
-
-# Set path and build parallel level
 export PATH=/conda/bin:/usr/local/cuda/bin:$PATH
-export PARALLEL_LEVEL=4
-export CUDA_REL=${CUDA_VERSION%.*}
-
-# Set home to the job's workspace
 export HOME=$WORKSPACE
-
-# Set versions of packages needed to be grabbed
-export NVSTRINGS_VERSION=0.7.*
-export RMM_VERSION=0.7.*
-
-################################################################################
-# SETUP - Check environment
-################################################################################
+export WORKSPACE=/rapids/cudf
+export LIBCUDF_KERNEL_CACHE_PATH="$HOME/.jitify-cache"
+export NIGHTLY_VERSION=$(echo $BRANCH_VERSION | awk -F. '{print $2}')
+export PROJECTS=(libcudf cudf nvstrings libnvstrings)
 
 logger "Check environment..."
 env
@@ -34,42 +18,40 @@ logger "Check GPU usage..."
 nvidia-smi
 
 logger "Activate conda env..."
-source activate gdf
-conda install -c rapidsai -c rapidsai-nightly -c nvidia -c conda-forge \
-    cudatoolkit=${CUDA_REL} rmm=${RMM_VERSION} nvstrings=${NVSTRINGS_VERSION} \
-    sphinx sphinx_rtd_theme numpydoc sphinxcontrib-websupport nbsphinx ipython \
-    pandoc=\<2.0.0 recommonmark doxygen
-
+source activate rapids
+conda install -c anaconda beautifulsoup4 jq
 pip install sphinx-markdown-tables
+
+
 logger "Check versions..."
 python --version
 $CC --version
 $CXX --version
 conda list
 
-################################################################################
-# BUILD - Build libcudf and cuDF from source
-################################################################################
-
-logger "Build libcudf..."
-$WORKSPACE/build.sh clean libcudf cudf
-
-################################################################################
-# BUILD - Build docs
-################################################################################
 
 #libcudf Doxygen build
 logger "Build libcudf docs..."
 cd $WORKSPACE/cpp/doxygen
 doxygen Doxyfile
 
-rm -rf /data/docs/libcudf/html/*
-mv html/* /data/docs/libcudf/html
-
 #cudf Sphinx Build
 logger "Build cuDF docs..."
-cd $WORKSPACE/docs
+cd $WORKSPACE/docs/cudf
 make html
 
-rm -rf /data/docs/cudf/html/*
-mv build/html/* /data/docs/cudf/html
+#libnvstrings Doxygen build
+logger "Build livnvstrings docs..."
+cd $WORKSPACE/cpp/custrings/doxygen
+doxygen Doxyfile
+
+#nvstrings Sphinx Build
+logger "Build nvstrings docs..."
+cd $WORKSPACE/docs/nvstrings
+make html
+
+#Move HTML to project folders
+mv $WORKSPACE/cpp/doxygen/html/* $WORKSPACE/tmp/libcudf/html/*
+mv $WORKSPACE/docs/cudf/build/html/* $WORKSPACE/tmp/cudf/html/*
+mv $WORKSPACE/cpp/custrings/doxygen/html/* $WORKSPACE/tmp/libnvstrings/html/*
+mv $WORKSPACE/docs/nvstrings/build/html/* $WORKSPACE/tmp/nvstrings/html/*
