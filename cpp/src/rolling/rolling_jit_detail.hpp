9ea1696fd7267dcacf0604dd2fe950370278f602
Jiqun Tu <jtu@nvidia.com>
2020-04-29 08:57:02 -0700

    clang-format.

--- cpp/src/rolling/rolling_jit_detail.hpp
+++ cpp/src/rolling/rolling_jit_detail.hpp
@@ -17,48 +17,46 @@
 #ifndef ROLLING_JIT_DETAIL_HPP
 #define ROLLING_JIT_DETAIL_HPP
 
-namespace cudf
-{
+namespace cudf {
 
-namespace detail
-{
+namespace detail {
 
-template<class T>
-T minimum(T a, T b){
+template <class T>
+T minimum(T a, T b)
+{
   return b < a ? b : a;
 }
 
 struct preceding_window_wrapper {
-
   const cudf::size_type *d_group_offsets;
   const cudf::size_type *d_group_labels;
   cudf::size_type preceding_window;
-  
-  cudf::size_type operator[](cudf::size_type idx){
+
+  cudf::size_type operator[](cudf::size_type idx)
+  {
     auto group_label = d_group_labels[idx];
     auto group_start = d_group_offsets[group_label];
-    return minimum(preceding_window, idx - group_start + 1); // Preceding includes current row.
+    return minimum(preceding_window, idx - group_start + 1);  // Preceding includes current row.
   }
-
 };
 
 struct following_window_wrapper {
-
   const cudf::size_type *d_group_offsets;
   const cudf::size_type *d_group_labels;
   cudf::size_type following_window;
-  
-  cudf::size_type operator[](cudf::size_type idx){
+
+  cudf::size_type operator[](cudf::size_type idx)
+  {
     auto group_label = d_group_labels[idx];
-    auto group_end = d_group_offsets[group_label + 1]; // Cannot fall off the end, since offsets is capped with `input.size()`.
+    auto group_end =
+      d_group_offsets[group_label +
+                      1];  // Cannot fall off the end, since offsets is capped with `input.size()`.
     return minimum(following_window, (group_end - 1) - idx);
   }
-
 };
 
-}  // namespace cudf::detail
+}  // namespace detail
 
-} // namespace cudf
+}  // namespace cudf
 
 #endif
-
