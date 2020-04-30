481ec48cc8eb675e7774c9135d05d5bcc02ea5ef
Olivier Lapicque <olapicque@nvidia.com>
2020-04-29 21:44:26 -0700

    clang-format

--- cpp/src/io/csv/csv_gpu.cu
+++ cpp/src/io/csv/csv_gpu.cu
@@ -834,7 +834,8 @@ __global__ void __launch_bounds__(rowofs_block_dim) gather_row_offsets_gpu(uint6
     ctx_map.z &= mask;
   }
 
-  // Convert the long-form {rowmap,outctx}[inctx] version into packed version {rowcount,ouctx}[inctx]
+  // Convert the long-form {rowmap,outctx}[inctx] version into packed version
+  // {rowcount,ouctx}[inctx]
   ctxb = pack_row_contexts(make_row_context(__popc(ctx_map.x), (ctx_map.w >> 0) & 3),
                            make_row_context(__popc(ctx_map.y), (ctx_map.w >> 2) & 3),
                            make_row_context(__popc(ctx_map.z), (ctx_map.w >> 4) & 3));
