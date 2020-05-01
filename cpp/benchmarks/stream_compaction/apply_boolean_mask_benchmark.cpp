c33e88110c519a135c0c8d299852d188f46cf9cc
Mark Harris <mharris@nvidia.com>
2020-05-01 15:33:40 +1000

    clang-format fixes.

--- cpp/benchmarks/stream_compaction/apply_boolean_mask_benchmark.cpp
+++ cpp/benchmarks/stream_compaction/apply_boolean_mask_benchmark.cpp
@@ -14,35 +14,34 @@
  * limitations under the License.
  */
 
+#include <thrust/iterator/constant_iterator.h>
 #include <cudf/stream_compaction.hpp>
 #include <cudf/table/table.hpp>
-#include <tests/utilities/column_wrapper.hpp>
 #include <fixture/benchmark_fixture.hpp>
 #include <synchronization/synchronization.hpp>
-#include <thrust/iterator/constant_iterator.h>
-
+#include <tests/utilities/column_wrapper.hpp>
 
 #include <benchmark/benchmark.h>
-#include <random>
 #include <chrono>
+#include <random>
 
 namespace {
 
-constexpr cudf::size_type hundredM = 1e8;
-constexpr cudf::size_type tenM = 1e7;
-constexpr cudf::size_type tenK = 1e4;
+constexpr cudf::size_type hundredM      = 1e8;
+constexpr cudf::size_type tenM          = 1e7;
+constexpr cudf::size_type tenK          = 1e4;
 constexpr cudf::size_type fifty_percent = 50;
 
-void percent_range(benchmark::internal::Benchmark* b) {
+void percent_range(benchmark::internal::Benchmark* b)
+{
   b->Unit(benchmark::kMillisecond);
-  for (int percent = 0; percent <= 100; percent += 10)
-    b->Args({hundredM, percent});
+  for (int percent = 0; percent <= 100; percent += 10) b->Args({hundredM, percent});
 }
 
-void size_range(benchmark::internal::Benchmark* b) {
+void size_range(benchmark::internal::Benchmark* b)
+{
   b->Unit(benchmark::kMillisecond);
-  for (int size = tenK; size <= hundredM; size *= 10)
-    b->Args({size, fifty_percent});
+  for (int size = tenK; size <= hundredM; size *= 10) b->Args({size, fifty_percent});
 }
 
 template <typename T>
@@ -56,38 +55,42 @@ T random_int(T min, T max)
 }
 
 template <typename T>
-void calculate_bandwidth(benchmark::State& state, cudf::size_type num_columns) {
-  cudf::size_type const column_size{static_cast<cudf::size_type>(state.range(0))}; 
-  cudf::size_type const percent_true{static_cast<cudf::size_type>(state.range(1))}; 
+void calculate_bandwidth(benchmark::State& state, cudf::size_type num_columns)
+{
+  cudf::size_type const column_size{static_cast<cudf::size_type>(state.range(0))};
+  cudf::size_type const percent_true{static_cast<cudf::size_type>(state.range(1))};
 
-  float const fraction = percent_true / 100.f;
+  float const fraction                  = percent_true / 100.f;
   cudf::size_type const column_size_out = fraction * column_size;
-  int64_t const mask_size = sizeof(bool) * column_size + cudf::bitmask_allocation_size_bytes(column_size);
-  int64_t const validity_bytes_in  = (fraction >= 1.0f/32) ? 
-                               cudf::bitmask_allocation_size_bytes(column_size) :
-                               4 * column_size_out;
+  int64_t const mask_size =
+    sizeof(bool) * column_size + cudf::bitmask_allocation_size_bytes(column_size);
+  int64_t const validity_bytes_in = (fraction >= 1.0f / 32)
+                                      ? cudf::bitmask_allocation_size_bytes(column_size)
+                                      : 4 * column_size_out;
   int64_t const validity_bytes_out = cudf::bitmask_allocation_size_bytes(column_size_out);
-  int64_t const column_bytes_out = sizeof(T) * column_size_out;
-  int64_t const column_bytes_in = column_bytes_out; // we only read unmasked inputs
+  int64_t const column_bytes_out   = sizeof(T) * column_size_out;
+  int64_t const column_bytes_in    = column_bytes_out;  // we only read unmasked inputs
 
-  int64_t const bytes_read = (column_bytes_in + validity_bytes_in) * num_columns + // reading columns
-                             mask_size; // reading boolean mask
-  int64_t const bytes_written = (column_bytes_out + validity_bytes_out) * num_columns; // writing columns
+  int64_t const bytes_read =
+    (column_bytes_in + validity_bytes_in) * num_columns +  // reading columns
+    mask_size;                                             // reading boolean mask
+  int64_t const bytes_written =
+    (column_bytes_out + validity_bytes_out) * num_columns;  // writing columns
 
   state.SetItemsProcessed(state.iterations() * column_size * num_columns);
-  state.SetBytesProcessed(static_cast<int64_t>(state.iterations()) * 
-                          bytes_read + bytes_written);
+  state.SetBytesProcessed(static_cast<int64_t>(state.iterations()) * bytes_read + bytes_written);
 }
 
-} // namespace anonymous
+}  // namespace
 
 template <class T>
-void BM_apply_boolean_mask(benchmark::State& state, cudf::size_type num_columns) {
-  using wrapper = cudf::test::fixed_width_column_wrapper<T>;
+void BM_apply_boolean_mask(benchmark::State& state, cudf::size_type num_columns)
+{
+  using wrapper      = cudf::test::fixed_width_column_wrapper<T>;
   using mask_wrapper = cudf::test::fixed_width_column_wrapper<bool>;
 
-  const cudf::size_type column_size{static_cast<cudf::size_type>(state.range(0))}; 
-  const cudf::size_type percent_true{static_cast<cudf::size_type>(state.range(1))}; 
+  const cudf::size_type column_size{static_cast<cudf::size_type>(state.range(0))};
+  const cudf::size_type percent_true{static_cast<cudf::size_type>(state.range(1))};
 
   std::vector<wrapper> columns;
 
@@ -99,22 +102,19 @@ void BM_apply_boolean_mask(benchmark::State& state, cudf::size_type num_columns)
   for (int i = 0; i < num_columns; i++) {
     columns.emplace_back(data.cbegin(), data.cend(), validity.cbegin());
   }
-  
 
-  auto mask_iter = cudf::test::make_counting_transform_iterator(0, [&](auto i){
-    return random_int(0, 100) < percent_true;
-  });
+  auto mask_iter = cudf::test::make_counting_transform_iterator(
+    0, [&](auto i) { return random_int(0, 100) < percent_true; });
   mask_wrapper mask(mask_iter, mask_iter + column_size);
 
   std::vector<cudf::column_view> column_views(num_columns);
 
-  std::transform(columns.begin(), columns.end(),
-                 column_views.begin(), [](auto const& col) {
+  std::transform(columns.begin(), columns.end(), column_views.begin(), [](auto const& col) {
     return static_cast<cudf::column_view>(col);
   });
   cudf::table_view source_table{column_views};
 
-  for(auto _ : state){
+  for (auto _ : state) {
     cuda_event_timer raii(state, true);
     auto result = cudf::experimental::apply_boolean_mask(source_table, mask);
   }
@@ -124,14 +124,15 @@ void BM_apply_boolean_mask(benchmark::State& state, cudf::size_type num_columns)
 
 template <class T>
 class ApplyBooleanMask : public cudf::benchmark {
-public:
+ public:
   using TypeParam = T;
 };
 
-#define ABM_BENCHMARK_DEFINE(name, type, n_columns)                                 \
-BENCHMARK_TEMPLATE_DEFINE_F(ApplyBooleanMask, name, type)(::benchmark::State& st) { \
-  BM_apply_boolean_mask<TypeParam>(st, n_columns);                                  \
-}
+#define ABM_BENCHMARK_DEFINE(name, type, n_columns)                                  \
+  BENCHMARK_TEMPLATE_DEFINE_F(ApplyBooleanMask, name, type)(::benchmark::State & st) \
+  {                                                                                  \
+    BM_apply_boolean_mask<TypeParam>(st, n_columns);                                 \
+  }
 
 ABM_BENCHMARK_DEFINE(float_1_col, float, 1);
 ABM_BENCHMARK_DEFINE(float_2_col, float, 2);
@@ -148,13 +149,13 @@ BENCHMARK_REGISTER_F(ApplyBooleanMask, float_2_col)->Apply(size_range);
 BENCHMARK_REGISTER_F(ApplyBooleanMask, float_4_col)->Apply(size_range);
 
 // spot benchmark other types
-ABM_BENCHMARK_DEFINE(int8_1_col,   int8_t,  1);
-ABM_BENCHMARK_DEFINE(int16_1_col,  int16_t, 1);
-ABM_BENCHMARK_DEFINE(int32_1_col,  int32_t, 1);
-ABM_BENCHMARK_DEFINE(int64_1_col,  int64_t, 1);
-ABM_BENCHMARK_DEFINE(double_1_col, double,  1);
-BENCHMARK_REGISTER_F(ApplyBooleanMask, int8_1_col  )->Args({tenM, fifty_percent});
-BENCHMARK_REGISTER_F(ApplyBooleanMask, int16_1_col )->Args({tenM, fifty_percent});
-BENCHMARK_REGISTER_F(ApplyBooleanMask, int32_1_col )->Args({tenM, fifty_percent});
-BENCHMARK_REGISTER_F(ApplyBooleanMask, int64_1_col )->Args({tenM, fifty_percent});
+ABM_BENCHMARK_DEFINE(int8_1_col, int8_t, 1);
+ABM_BENCHMARK_DEFINE(int16_1_col, int16_t, 1);
+ABM_BENCHMARK_DEFINE(int32_1_col, int32_t, 1);
+ABM_BENCHMARK_DEFINE(int64_1_col, int64_t, 1);
+ABM_BENCHMARK_DEFINE(double_1_col, double, 1);
+BENCHMARK_REGISTER_F(ApplyBooleanMask, int8_1_col)->Args({tenM, fifty_percent});
+BENCHMARK_REGISTER_F(ApplyBooleanMask, int16_1_col)->Args({tenM, fifty_percent});
+BENCHMARK_REGISTER_F(ApplyBooleanMask, int32_1_col)->Args({tenM, fifty_percent});
+BENCHMARK_REGISTER_F(ApplyBooleanMask, int64_1_col)->Args({tenM, fifty_percent});
 BENCHMARK_REGISTER_F(ApplyBooleanMask, double_1_col)->Args({tenM, fifty_percent});
