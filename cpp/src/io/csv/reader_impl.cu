8f3adb509c1845e11b2beab9592b1b00b56bb21a
Olivier Lapicque <olapicque@nvidia.com>
2020-04-30 18:36:28 -0700

    clang-format pass

--- cpp/src/io/csv/reader_impl.cu
+++ cpp/src/io/csv/reader_impl.cu
@@ -362,8 +362,7 @@ table_with_metadata reader::impl::read(size_t range_offset,
         // during the conversion stage
         const std::string quotechar(1, opts.quotechar);
         const std::string dblquotechar(2, opts.quotechar);
-        std::unique_ptr<column> col =
-          make_strings_column(out_buffers[i]._strings, stream);
+        std::unique_ptr<column> col = make_strings_column(out_buffers[i]._strings, stream);
         out_columns.emplace_back(
           cudf::strings::replace(col->view(), dblquotechar, quotechar, -1, mr_));
       } else {
