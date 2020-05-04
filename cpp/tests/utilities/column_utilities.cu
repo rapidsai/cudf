4ccaf336a4570fc9bd8c4d5f2db946dd71124174
Christopher Harris <charris@nvidia.com>
2020-05-03 22:44:18 -0500

    clang style

--- cpp/tests/utilities/column_utilities.cu
+++ cpp/tests/utilities/column_utilities.cu
@@ -290,13 +290,12 @@ std::vector<bitmask_type> bitmask_to_host(cudf::column_view const& c)
   }
 }
 
-template< typename T >
+template <typename T>
 static auto numeric_to_string_precise(T value)
 {
   std::ostringstream o;
 
-  o << std::setprecision(std::numeric_limits<T>::max_digits10)
-    << value;
+  o << std::setprecision(std::numeric_limits<T>::max_digits10) << value;
 
   return o.str();
 }
