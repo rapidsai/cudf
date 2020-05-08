b3710a8a800b7141641b1750b8954a7a867f886d
John Kirkham <jakirkham@gmail.com>
2020-05-08 08:58:24 -0700

    Install `contextvars` at the beginning

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -57,6 +57,13 @@ nvidia-smi
 
 logger "Activate conda env..."
 source activate gdf
+
+# Install contextvars on Python 3.6
+py_ver=$(python -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
+if [ "$py_ver" == "3.6" ];then
+    conda install contextvars
+fi
+
 conda install "rmm=$MINOR_VERSION.*" "cudatoolkit=$CUDA_REL" \
               "dask>=2.15.0" "distributed>=2.15.0" "numpy>=1.16" "double-conversion" \
               "rapidjson" "flatbuffers" "boost-cpp" "fsspec>=0.6.0" "dlpack" \
@@ -116,12 +123,6 @@ else
     if [ "$np_ver" == "1.16" ];then
         logger "export NUMPY_EXPERIMENTAL_ARRAY_FUNCTION=1"
         export NUMPY_EXPERIMENTAL_ARRAY_FUNCTION=1
-    fi
-
-    # Install contextvars on Python 3.6
-    py_ver=$(python -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")
-    if [ "$py_ver" == "3.6" ];then
-        conda install contextvars
     fi
 
     cd $WORKSPACE/python/nvstrings
