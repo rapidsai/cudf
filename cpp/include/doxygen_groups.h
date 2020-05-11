f8a032dff3bf0e3dea5a17adc7b765934bb6c7b3
davidwendt <dwendt@nvidia.com>
2020-05-11 12:22:05 -0400

    add doxygen group/module header

---
+++ cpp/include/doxygen_groups.h
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This header is only processed by doxygen and does
+// not need to be included in any source file.
+// Below are the main groups that doxygen uses to build
+// the Modules page in the specified order.
+// Only add a group if it should appear in the root of
+// Modules page's tree.
+
+/**
+ * @defgroup cudf_classes Classes
+ * @{
+ *   @defgroup column_classes Column
+ *   @{
+ *      @defgroup column_factories Factories
+ *      @defgroup strings_classes Strings
+ *      @defgroup dictionary_classes
+ *      @defgroup timestamp_classes Timestamp
+ *   @}
+ *   @defgroup table_classes Table
+ *   @defgroup scalar_classes Scalar
+ *   @{
+ *      @defgroup scalar_factories Factories
+ *   @}
+ * @}
+ * @defgroup column_apis Column and Table APIs
+ * @{
+ *   @defgroup column_copy Copying
+ *   @{
+ *     @defgroup copy_concatenate Concatenating
+ *     @defgroup copy_gather Gathering
+ *     @defgroup copy_scatter Scattering
+ *     @defgroup copy_slice Slicing
+ *     @defgroup copy_split Splitting
+ *     @defgroup copy_shift Shifting
+ *   @}
+ *   @defgroup column_nullmask Bitmask Operations
+ *   @defgroup column_sort Sorting
+ *   @defgroup column_search Searching
+ *   @defgroup column_hash Hashing
+ *   @defgroup column_merge Merging
+ *   @defgroup column_join Joining
+ *   @defgroup column_quantiles Quantiles
+ *   @defgroup column_aggregation Aggregation
+ *   @{
+ *     @defgroup aggregation_factories Aggregation Factories
+ *     @defgroup aggregation_reduction Reduction
+ *     @defgroup aggregation_groupby GroupBy
+ *     @defgroup aggregation_rolling Rolling Window
+ *   @}
+ *   @defgroup column_transformation Transformation
+ *   @{
+ *     @defgroup transformation_unaryops Unary Operations
+ *     @defgroup transformation_binaryops Binary Operations
+ *     @defgroup transformation_transform Transform
+ *     @defgroup transformation_replace Replacing
+ *     @defgroup transformation_fill Filling
+ *   @}
+ *   @defgroup column_reshape Reshaping
+ *   @{
+ *     @defgroup reshape_transpose Transpose
+ *   @}
+ *   @defgroup column_reorder Reordering
+ *   @{
+ *     @defgroup reorder_partition Partitioning
+ *     @defgroup reorder_compact Stream Compaction
+ *   @}
+ *   @defgroup column_interop Interop
+ *   @{
+ *     @defgroup interop_dlpack DLPack
+ *   @}
+ * @}
+ * @defgroup datetime_apis DateTime column APIs
+ * @{
+ *   @defgroup datetime_extract Extracting
+ *   @defgroup datetime_compute Compute Day
+ * @}
+ * @defgroup strings_apis Strings column APIs
+ * @{
+ *   @defgroup strings_case Case
+ *   @defgroup strings_types Character Types
+ *   @defgroup strings_combine Combining
+ *   @defgroup strings_contains Searching
+ *   @defgroup strings_convert Converting
+ *   @defgroup strings_substring Substring
+ *   @defgroup strings_find Finding
+ *   @defgroup strings_modify Modifying
+ *   @defgroup strings_replace Replacing
+ *   @defgroup strings_split Splitting
+ * @}
+ * @defgroup dictionary_apis Dictionary APIs
+ * @{
+ *   @defgroup dictionary_encode Encoding
+ *   @defgroup dictionary_search Searching
+ *   @defgroup dictionary_update Updating Keys
+ * @}
+ * @defgroup io_apis IO APIs
+ * @{
+ *   @defgroup io_readers Readers
+ *   @defgroup io_writers Writers
+ * @}
+ * @defgroup nvtext_apis NVText APIs
+ * @{
+ *   @defgroup nvtext_encode NGrams
+ *   @defgroup nvtext_normalize Normalizing
+ *   @defgroup nvtext_tokenize Tokenizing
+ * @}
+ */
