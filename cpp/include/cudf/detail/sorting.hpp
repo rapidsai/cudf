08b9ddc924902f7be4e9f626c59c3e151670fc20
Devavret Makkar <dmakkar@nvidia.com>
2020-05-15 02:56:29 +0530

    Remove duplicate docs from sorting detail header

--- cpp/include/cudf/detail/sorting.hpp
+++ cpp/include/cudf/detail/sorting.hpp
@@ -26,20 +26,9 @@ namespace experimental {
 namespace detail {
 
 /**
- * @brief Computes the row indices that would produce `input`  in a
- * lexicographical sorted order.
+ * @copydoc cudf::experimental::sorted_order
  *
- * @param input The table to sort
- * @param column_order The desired sort order for each column. Size must be
- * equal to `input.num_columns()` or empty. If empty, all columns will be sorted
- * in ascending order.
- * @param null_precedence The desired order of null compared to other elements
- * for each column.  Size must be equal to `input.num_columns()` or empty.
- * If empty, all columns will be sorted in `null_order::BEFORE`.
- * @param[in] mr Optional, The resource to use for all allocations
  * @param[in] stream Optional CUDA stream on which to execute kernels
- * @return std::unique_ptr<column> A non-nullable column of `size_type` elements
- * containing the permuted row indices of `input` if it were sorted
  */
 std::unique_ptr<column> sorted_order(
   table_view input,
@@ -49,11 +38,9 @@ std::unique_ptr<column> sorted_order(
   cudaStream_t stream                            = 0);
 
 /**
- * @brief Computes the row indices that would produce `input` in a stable
- * lexicographical sorted order.
- * The order of equivalent elements is guaranteed to be preserved.
+ * @copydoc cudf::experimental::stable_sorted_order
  *
- * @copydetails cudf::experimental::detail::sorted_order
+ * @param[in] stream Optional CUDA stream on which to execute kernels
  */
 std::unique_ptr<column> stable_sorted_order(
   table_view input,
