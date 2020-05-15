d1885334b1ed5af67b94958a64dc79e24577e8c2
Sriram Chandramouli <sriram_chandramouli@hotmail.com>
2020-05-15 19:49:11 +0000

    - change verbiage from string column[s] -> strings column[s]

--- cpp/include/cudf/strings/combine.hpp
+++ cpp/include/cudf/strings/combine.hpp
@@ -138,7 +138,7 @@ std::unique_ptr<column> join_strings(
  * @throw cudf::logic_error if the number of rows from @p separators and @p strings_columns
  *                          do not match
  *
- * @param strings_columns List of string columns to concatenate.
+ * @param strings_columns List of strings columns to concatenate.
  * @param separators Strings column that provides the separator for a given row
  * @param separator_narep String that should be used in place of a null separator for a given
  *        row. Default of invalid-scalar means no row separator value replacements.
