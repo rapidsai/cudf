93c538e4e0057ac575642159e638e0089c3e01e8
Devavret Makkar <dmakkar@nvidia.com>
2020-05-16 06:13:00 +0530

    Port table search benchmark

--- cpp/benchmarks/search/search_benchmark.cu
+++ cpp/benchmarks/search/search_benchmark.cu
@@ -108,55 +108,59 @@ BENCHMARK_REGISTER_F(Search, NullableColumn)
   ->Unit(benchmark::kMillisecond)
   ->Arg(100000000);
 
-// void BM_table(benchmark::State& state)
-// {
-//   using wrapper = cudf::test::column_wrapper<float>;
+void BM_table(benchmark::State& state)
+{
+  using wrapper = cudf::test::fixed_width_column_wrapper<float>;
 
-//   const cudf::size_type num_columns{(cudf::size_type)state.range(0)};
-//   const cudf::size_type column_size{(cudf::size_type)state.range(1)};
-//   const cudf::size_type values_size = column_size;
+  const cudf::size_type num_columns{(cudf::size_type)state.range(0)};
+  const cudf::size_type column_size{(cudf::size_type)state.range(1)};
+  const cudf::size_type values_size = column_size;
 
-//   std::vector<wrapper> columns;
-//   std::vector<wrapper> values;
+  std::vector<wrapper> columns;
+  std::vector<wrapper> values;
 
-//   auto make_table = [&](std::vector<wrapper>& cols, cudf::size_type col_size) -> cudf::table {
-//     for (cudf::size_type i = 0; i < num_columns; i++) {
-//       cols.emplace_back(
-//         col_size,
-//         [=](cudf::size_type row) { return random_int(0, 100); },
-//         [=](cudf::size_type row) { return random_int(0, 100) < 90; });
-//     }
+  auto make_table = [&](std::vector<wrapper>& cols, cudf::size_type col_size) {
+    cudf::test::UniformRandomGenerator<int> random_gen(0, 100);
+    auto data_it = cudf::test::make_counting_transform_iterator(
+      0, [&](cudf::size_type row) { return random_gen.generate(); });
+    auto valid_it = cudf::test::make_counting_transform_iterator(
+      0, [&](cudf::size_type row) { return random_gen.generate() < 90; });
 
-//     std::vector<gdf_column*> raw_cols(num_columns, nullptr);
-//     std::transform(cols.begin(), cols.end(), raw_cols.begin(), [](wrapper& c) { return c.get();
-//     });
+    for (cudf::size_type i = 0; i < num_columns; i++) {
+      cols.emplace_back(data_it, data_it + col_size, valid_it);
+    }
 
-//     return cudf::table{raw_cols.data(), num_columns};
-//   };
+    std::vector<cudf::column_view> col_views;
+    std::transform(
+      cols.begin(), cols.end(), std::back_inserter(col_views), [](wrapper& c) -> cudf::column_view {
+        return c;
+      });
 
-//   auto data_table   = make_table(columns, column_size);
-//   auto values_table = make_table(values, values_size);
+    return cudf::table_view{col_views};
+  };
 
-//   std::vector<bool> desc_flags(num_columns, false);
-//   sort_table(data_table, desc_flags);
+  auto data_table   = make_table(columns, column_size);
+  auto values_table = make_table(values, values_size);
 
-//   for (auto _ : state) {
-//     cuda_event_timer timer(state, true);
-//     auto col = cudf::lower_bound(data_table, values_table, desc_flags);
-//     gdf_column_free(&col);
-//   }
-// }
+  std::vector<cudf::order> orders(num_columns, cudf::order::ASCENDING);
+  std::vector<cudf::null_order> null_orders(num_columns, cudf::null_order::BEFORE);
+  auto sorted = cudf::experimental::sort(data_table);
 
-// BENCHMARK_DEFINE_F(Search, Table)(::benchmark::State& state) { BM_table(state); }
+  for (auto _ : state) {
+    cuda_event_timer timer(state, true);
+    auto col = cudf::experimental::lower_bound(sorted->view(), values_table, orders, null_orders);
+  }
+}
 
-// static void CustomArguments(benchmark::internal::Benchmark* b)
-// {
-//   for (int num_cols = 1; num_cols <= 10; num_cols *= 2)
-//     for (int col_size = 1000; col_size <= 100000000; col_size *= 10) b->Args({num_cols,
-//     col_size});
-// }
+BENCHMARK_DEFINE_F(Search, Table)(::benchmark::State& state) { BM_table(state); }
 
-// BENCHMARK_REGISTER_F(Search, Table)
-//   ->UseManualTime()
-//   ->Unit(benchmark::kMillisecond)
-//   ->Apply(CustomArguments);
+static void CustomArguments(benchmark::internal::Benchmark* b)
+{
+  for (int num_cols = 1; num_cols <= 10; num_cols *= 2)
+    for (int col_size = 1000; col_size <= 100000000; col_size *= 10) b->Args({num_cols, col_size});
+}
+
+BENCHMARK_REGISTER_F(Search, Table)
+  ->UseManualTime()
+  ->Unit(benchmark::kMillisecond)
+  ->Apply(CustomArguments);
