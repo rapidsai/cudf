1f80f3a6817258e64d941f2cdb89ec45bc78339d
Conor Hoekstra <codereport@outlook.com>
2020-05-19 19:31:52 -0400

    Remove TODO and use `template... Args`

--- cpp/src/copying/gather.cu
+++ cpp/src/copying/gather.cu
@@ -62,17 +62,10 @@ struct dispatch_map_type {
 
   // TODO args
   template <typename map_type,
+            typename... Args,
             std::enable_if_t<not std::is_integral<map_type>::value or
                              std::is_same<map_type, bool>::value>* = nullptr>
-  std::unique_ptr<table> operator()(
-    table_view const& source_table,
-    column_view const& gather_map,
-    size_type num_destination_rows,
-    bounds check_bounds,
-    out_of_bounds oob,
-    negative_indices neg_indices        = negative_indices::NOT_ALLOWED,
-    rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource(),
-    cudaStream_t stream                 = 0)
+  std::unique_ptr<table> operator()(Args&&... args)
   {
     CUDF_FAIL("Gather map must be an integral type.");
   }
