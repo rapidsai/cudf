de51491563349ecb86be5ad0ba553f2bcfc268e7
Jeremy Dyer <jdye64@gmail.com>
2020-05-22 13:38:49 -0400

    Altered Python setup script for compiling cuda src files that are located in the cython package and comprise the kernel_wrapper

--- examples/python-kernel-wrapper/cython/setup.py
+++ examples/python-kernel-wrapper/cython/setup.py
@@ -1,114 +1,62 @@
 import os
+import shutil
 import sysconfig
-from distutils.extension import Extension
 from distutils.sysconfig import get_python_lib
-from os.path import join as pjoin
 
 import numpy
 from Cython.Distutils import build_ext
-from setuptools import find_packages, setup
+from setuptools import Extension, find_packages, setup
+
+# Locate CUDA_HOME
+CUDA_HOME = os.environ.get("CUDA_HOME", False)
+if not CUDA_HOME:
+    path_to_cuda_gdb = shutil.which("cuda-gdb")
+    if path_to_cuda_gdb is None:
+        raise OSError(
+            "Could not locate CUDA. "
+            "Please set the environment variable "
+            "CUDA_HOME to the path to the CUDA installation "
+            "and try again."
+        )
+    CUDA_HOME = os.path.dirname(os.path.dirname(path_to_cuda_gdb))
+
+cuda_include_dir = os.path.join(CUDA_HOME, "include")
+cuda_lib_dir = os.path.join(CUDA_HOME, "lib64")
 
 
-def find_in_path(name, path):
-    """Find a file in a search path"""
-
-    # Adapted fom http://code.activestate.com/recipes/52224
-    for dir in path.split(os.pathsep):
-        binpath = pjoin(dir, name)
-        if os.path.exists(binpath):
-            return os.path.abspath(binpath)
-    return None
-
-
-def locate_cuda():
-    """Locate the CUDA environment on the system
-
-    Returns a dict with keys 'home', 'nvcc', 'include', and 'lib64'
-    and values giving the absolute path to each directory.
-
-    Starts by looking for the CUDAHOME env variable. If not found,
-    everything is based on finding 'nvcc' in the PATH.
-    """
-
-    # First check if the CUDAHOME env variable is in use
-    if "CUDAHOME" in os.environ:
-        home = os.environ["CUDAHOME"]
-        nvcc = pjoin(home, "bin", "nvcc")
-    else:
-        # Otherwise, search the PATH for NVCC
-        nvcc = find_in_path("nvcc", os.environ["PATH"])
-        if nvcc is None:
-            raise EnvironmentError(
-                "The nvcc binary could not be "
-                "located in your $PATH. Either add it to your path, "
-                "or set $CUDAHOME"
-            )
-        home = os.path.dirname(os.path.dirname(nvcc))
-
-    cudaconfig = {
-        "home": home,
-        "nvcc": nvcc,
-        "include": pjoin(home, "include"),
-        "lib64": pjoin(home, "lib64"),
-    }
-    for k, v in iter(cudaconfig.items()):
-        if not os.path.exists(v):
-            raise EnvironmentError(
-                "The CUDA %s path could not be " "located in %s" % (k, v)
-            )
-
-    return cudaconfig
-
-
-def customize_compiler_for_nvcc(self):
-    """Inject deep into distutils to customize how the dispatch
-    to gcc/nvcc works.
-
-    If you subclass UnixCCompiler, it's not trivial to get your subclass
-    injected in, and still have the right customizations (i.e.
-    distutils.sysconfig.customize_compiler) run on it. So instead of going
-    the OO route, I have this. Note, it's kindof like a wierd functional
-    subclassing going on.
-    """
-
-    # Tell the compiler it can processes .cu
+# Include the ability to compile using both GCC and NVCC
+def compile_cuda(self):
     self.src_extensions.append(".cu")
-
-    # Save references to the default compiler_so and _comple methods
-    default_compiler_so = self.compiler_so
+    default_so = self.compiler_so
     super = self._compile
 
-    # Now redefine the _compile method. This gets executed for each
-    # object but distutils doesn't have the ability to change compilers
-    # based on source extension: we add it.
-    def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+    # Adjust the _compile method.
+    def _compile(obj, src, ext, cc_args, extra_args, pp_opts):
         if os.path.splitext(src)[1] == ".cu":
-            # use the cuda for .cu files
-            self.set_executable("compiler_so", CUDA["nvcc"])
-            # use only a subset of the extra_postargs, which are 1-1
-            # translated from the extra_compile_args in the Extension class
-            postargs = extra_postargs["nvcc"]
+            # cude source file, lets compile it accordingly
+            self.set_executable(
+                "compiler_so",
+                os.path.join(os.path.join(CUDA_HOME, "bin"), "nvcc"),
+            )
+            postargs = extra_args["nvcc"]
         else:
-            postargs = extra_postargs["gcc"]
+            postargs = extra_args["gcc"]
 
         super(obj, src, ext, cc_args, postargs, pp_opts)
-        # Reset the default compiler_so, which we might have changed for cuda
-        self.compiler_so = default_compiler_so
 
-    # Inject our redefined _compile method into the class
+        # Reset defaults
+        self.compiler_so = default_so
+
     self._compile = _compile
 
 
-# Run the customize_compiler
+# Stub for custom compiler
 class custom_build_ext(build_ext):
     def build_extensions(self):
-        customize_compiler_for_nvcc(self.compiler)
+        compile_cuda(self.compiler)
         build_ext.build_extensions(self)
 
 
-CUDA = locate_cuda()
-
-# Obtain the numpy include directory. This logic works across numpy versions.
 try:
     numpy_include = numpy.get_include()
 except AttributeError:
@@ -119,25 +67,21 @@ ext = Extension(
     "cudfkernel",
     sources=["src/kernel_wrapper.cu", "kernel.pyx"],
     library_dirs=[
-        CUDA["lib64"],
+        cuda_lib_dir,
         get_python_lib(),
         os.path.join(os.sys.prefix, "lib"),
     ],
     libraries=["cudf", "cudart"],
     language="c++",
-    runtime_library_dirs=[CUDA["lib64"]],
+    runtime_library_dirs=[cuda_lib_dir],
     include_dirs=[
         os.path.dirname(sysconfig.get_path("include")),
         numpy_include,
-        CUDA["include"],
+        cuda_include_dir,
         "src",
         "../../../cpp/thirdparty/libcudacxx/include",
         "../../../cpp/thirdparty/cub",
     ],
-    # This syntax is specific to this build system
-    # we're only going to use certain compiler args with nvcc
-    # and not with gcc the implementation of this trick is in
-    # customize_compiler()
     extra_compile_args={
         "gcc": [],
         "nvcc": ["--ptxas-options=-v", "-c", "--compiler-options", "'-fPIC'"],
