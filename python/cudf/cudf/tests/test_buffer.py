cfa5c8d4b1cf626dbf04e3b1beb13791ecaf4f28
brandon-b-miller <brmiller@nvidia.com>
2020-06-05 12:26:08 -0700

    test for dtype as well

--- python/cudf/cudf/tests/test_buffer.py
+++ python/cudf/cudf/tests/test_buffer.py
@@ -18,10 +18,30 @@ arr_len = 10
         (cp.zeros((arr_len, arr_len))[:, 0], False),
     ],
 )
-def test_buffer_from_cuda_array_interface(data):
+def test_buffer_from_cuda_iface_contiguous(data):
     data, expect_success = data
     if expect_success:
-        buf = Buffer(data=data.view('|u1'), size=data.size)
+        buf = Buffer(data=data.view("|u1"), size=data.size)
     else:
         with pytest.raises(ValueError):
-            buf = Buffer(data=data.view('|u1'), size=data.size)
+            buf = Buffer(data=data.view("|u1"), size=data.size)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        (cp.zeros(arr_len)),
+        (cp.zeros((1, arr_len))),
+        (cp.zeros((1, arr_len, 1))),
+        (cp.zeros((arr_len, arr_len))),
+        (cp.zeros((arr_len, arr_len)).reshape(arr_len * arr_len)),
+    ],
+)
+@pytest.mark.parametrize("dtype", ["uint8", "int8", "float32"])
+def test_buffer_from_cuda_iface_dtype(data, dtype):
+    data = data.astype(dtype)
+    if dtype != "uint8":
+        with pytest.raises(
+            TypeError, match="Buffer data must be of uint8 type"
+        ):
+            bug = Buffer(data=data, size=data.size)
