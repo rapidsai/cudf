5adf671fd79f562c0092d212712b29354d42e67b
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2020-06-07 23:08:44 +0530

    enable all numeric operators for duration types in rolling

--- cpp/src/rolling/rolling_detail.hpp
+++ cpp/src/rolling/rolling_detail.hpp
@@ -34,7 +34,7 @@ static constexpr bool is_rolling_supported()
     return false;
   } else
 
-    if (cudf::is_numeric<ColumnType>()) {
+    if (cudf::is_numeric<ColumnType>() or cudf::is_duration<ColumnType>()) {
     constexpr bool is_comparable_countable_op = std::is_same<AggOp, DeviceMin>::value or
                                                 std::is_same<AggOp, DeviceMax>::value or
                                                 std::is_same<AggOp, DeviceCount>::value;
@@ -45,7 +45,9 @@ static constexpr bool is_rolling_supported()
       (op == aggregation::MEAN) or (op == aggregation::ROW_NUMBER);
 
     constexpr bool is_valid_numeric_agg =
-      (cudf::is_numeric<ColumnType>() or is_comparable_countable_op) and is_operation_supported;
+      (cudf::is_numeric<ColumnType>() or cudf::is_duration<ColumnType>() or
+       is_comparable_countable_op) and
+      is_operation_supported;
 
     return is_valid_numeric_agg;
 
