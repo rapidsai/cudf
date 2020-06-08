6e5da222d772113a18701bad92e76ea3ece97275
galipremsagar <sagarprem75@gmail.com>
2020-06-08 10:51:51 -0500

    change StringColumn._memory_usage to return actual mem usage

--- python/cudf/cudf/core/column/string.py
+++ python/cudf/cudf/core/column/string.py
@@ -3669,11 +3669,8 @@ class StringColumn(column.ColumnBase):
             n += self.base_mask.size
         return n
 
-    def _memory_usage(self, deep=False):
-        if deep:
-            return self.__sizeof__()
-        else:
-            return len(self) * self.dtype.itemsize
+    def _memory_usage(self, **kwargs):
+        return self.__sizeof__()
 
     def unary_operator(self, unaryop):
         raise TypeError(
