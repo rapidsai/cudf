93309aa4e887cf8bf0e8f124190fa0a7bb4d9cbe
ptaylor <paul.e.taylor@me.com>
2020-06-08 13:04:53 -0700

    update librdkafka version to >= 1.3

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -69,7 +69,7 @@ conda install "rmm=$MINOR_VERSION.*" "cudatoolkit=$CUDA_REL" \
               "rapidjson" "flatbuffers" "boost-cpp=1.72.0" "fsspec>=0.6.0" "dlpack" \
               "feather-format" "cupy>=6.6.0,<8.0.0a0,!=7.1.0" "arrow-cpp=0.17.1" "arrow-cpp-proc=*=cuda" "pyarrow=0.17.1" \
               "fastavro>=0.22.0" "pandas>=0.25,<0.26" "hypothesis" "s3fs" "gcsfs" \
-              "boto3" "moto" "httpretty" "streamz" "ipython=7.3*" "jupyterlab"
+              "boto3" "moto" "httpretty" "streamz" "ipython=7.3*" "jupyterlab" "librdkafka>=1.3"
 
 # Install the master version of dask, distributed, and streamz
 logger "pip install git+https://github.com/dask/distributed.git --upgrade --no-deps"
