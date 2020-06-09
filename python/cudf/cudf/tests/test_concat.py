00e2d4cec1bef8e2d54bb8e009573f55a5ad60a1
Miller Hooks <>
2020-06-09 16:38:06 -0500

    added xfail test for DataFrame.concat to demonstrate expected error.

--- python/cudf/cudf/tests/test_concat.py
+++ python/cudf/cudf/tests/test_concat.py
@@ -272,7 +272,7 @@ def test_pandas_concat_compatibility_axis1():
     assert_eq(got, expect)
 
 
-@pytest.mark.parametrize("index", [[0, 1, 2], [2, 1, 0], [5, 9, 10]])
+@pytest.mark.parametrize("index", [[1, 1, 1], [0, 1, 2], [2, 1, 0], [5, 9, 10]])
 @pytest.mark.parametrize("names", [False, (0, 1)])
 @pytest.mark.parametrize(
     "data",
@@ -294,6 +294,16 @@ def test_pandas_concat_compatibility_axis1_overlap(index, names, data):
 
     assert_eq(got, expect)
 
+@pytest.mark.xfail(raises=ValueError)
+def test_pandas_concat_compatibility_axis1_eq_index():
+    s1 = gd.Series(["a", "b", "c"], index=[0, 1, 2])
+    s2 = gd.Series(["a", "b", "c"], index=[1, 1, 1])
+    ps1 = s1.to_pandas()
+    ps2 = s2.to_pandas()
+    got = gd.concat([s1, s2], axis=1)
+    expect = pd.concat([ps1, ps2], axis=1)
+
+    assert_eq(got, expect)
 
 def test_concat_duplicate_columns():
     cdf = gd.DataFrame(
