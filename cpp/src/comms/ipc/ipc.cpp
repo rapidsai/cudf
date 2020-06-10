83df9c2b735025288e17b9a43d2a415243abfa4e
ptaylor <paul.e.taylor@me.com>
2020-06-10 12:10:31 -0700

    add GpuArrowReader pytests for schema + data on GPU, and schema + data on GPU with bad CPU schema

--- cpp/src/comms/ipc/ipc.cpp
+++ cpp/src/comms/ipc/ipc.cpp
@@ -10,8 +10,7 @@ arrow::Result<std::unique_ptr<arrow::ipc::Message>> CudaMessageReader::ReadNextM
   if (host_schema_reader_ != nullptr) {
     auto message        = arrow::ipc::ReadMessage(host_schema_reader_);
     host_schema_reader_ = nullptr;
-    if (!message.ok()) { return arrow::Status::Invalid("Failed to decode message"); }
-    if (*message != nullptr) { return std::move(message); }
+    if (message.ok() && *message != nullptr) { return std::move(message); }
   }
   std::unique_ptr<arrow::ipc::Message> message;
   auto status = arrow::cuda::ReadMessage(stream_, arrow::default_memory_pool(), &message);
