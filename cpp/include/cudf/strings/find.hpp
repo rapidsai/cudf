609c364a5cd80d4aa9f76ed39b329b2957d1f55a
GALI PREM SAGAR <sagarprem75@gmail.com>
2020-06-10 10:39:54 -0500

    Update cpp/include/cudf/strings/find.hpp
    Co-authored-by: David <45795991+davidwendt@users.noreply.github.com>

--- cpp/include/cudf/strings/find.hpp
+++ cpp/include/cudf/strings/find.hpp
@@ -128,7 +128,7 @@ std::unique_ptr<column> starts_with(
  *
  * If `targets[i]` is not found at the beginning of a string in `strings[i]`, false is set for
  * that row entry in the output column.
- * If `targets[i]` is an empty string, true is returned for corresponding non-null entry in the
+ * If `targets[i]` is an empty string, true is returned for corresponding entry in the
  * output column.
  *
  * Any null string entries in `targets` return corresponding null entries in the output columns.
