04e54850180e39fa999a70ad0c9418b84b53a732
galipremsagar <sagarprem75@gmail.com>
2020-06-12 08:39:36 -0700

    add dict tests related to Series.__init__

---
+++ python/cudf/cudf/tests/test_series.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2018-2020, NVIDIA CORPORATION.
+
+
+import pandas as pd
+import pytest
+
+import cudf
+from cudf.tests.utils import assert_eq
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        {"a": 1, "b": 2, "c": 24, "d": 1010},
+        {"a": 1},
+        {1: "a", 2: "b", 24: "c", 1010: "d"},
+        {1: "a"},
+    ],
+)
+def test_series_init_dict(data):
+    pandas_series = pd.Series(data)
+    cudf_series = cudf.Series(data)
+
+    assert_eq(pandas_series, cudf_series)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        {
+            "a": [1, 2, 3],
+            "b": [2, 3, 5],
+            "c": [24, 12212, 22233],
+            "d": [1010, 101010, 1111],
+        },
+        {"a": [1]},
+    ],
+)
+def test_series_init_dict_lists(data):
+
+    with pytest.raises(NotImplementedError):
+        cudf.Series(data)
