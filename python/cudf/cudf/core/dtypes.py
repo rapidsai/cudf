b6d252248b2a332844301c64adaa90292ddd8285
brandon-b-miller <brmiller@nvidia.com>
2020-06-12 13:32:57 -0700

    remove dtype changes from earlier pr

--- python/cudf/cudf/core/dtypes.py
+++ python/cudf/cudf/core/dtypes.py
@@ -78,7 +78,6 @@ class CategoricalDtype(ExtensionDtype):
             return (
                 self._categories.dtype == other._categories.dtype
                 and self._categories.equals(other._categories)
-                and self.ordered == other.ordered
             )
 
     def construct_from_string(self):
