4b50cd587b7601ac347545a111c8d563a15e3e1e
brandon-b-miller <brmiller@nvidia.com>
2020-06-16 08:42:58 -0700

    -f -> --force

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -69,7 +69,7 @@ conda install "rmm=$MINOR_VERSION.*" "cudatoolkit=$CUDA_REL" \
               "rapids-notebook-env=$MINOR_VERSION.*"
 
 # https://docs.rapids.ai/maintainers/depmgmt/ 
-conda remove -f rapids-build-env rapids-notebook-env
+conda remove --force rapids-build-env rapids-notebook-env
 conda install "pandas>=1.0,<1.1.0a0"
 
 # Install the master version of dask, distributed, and streamz
