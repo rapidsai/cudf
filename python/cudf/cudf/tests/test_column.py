9e03e7d09a04c7473921e18ab68a160173acb5ba
brandon-b-miller <brmiller@nvidia.com>
2020-06-16 20:28:58 -0700

    fix tests

--- python/cudf/cudf/tests/test_column.py
+++ python/cudf/cudf/tests/test_column.py
@@ -181,8 +181,8 @@ def test_column_view_invalid_numeric_to_numeric(data, from_dtype, to_dtype):
     except ValueError as error:
         if "size must be a divisor" in str(error):
             with pytest.raises(
-                ValueError,
-                match="Buffer size must be divisible by element size",
+                TypeError,
+                match="Source data size must be an even multiple of view data size",
             ):
                 gpu_data = gpu_data.view(to_dtype)
         else:
