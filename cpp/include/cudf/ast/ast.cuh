f63d525cfe002b03f0cb89f9a8cb5ee547ef8373
Bradley Dice <bdice@bradleydice.com>
2020-06-26 14:30:44 -0700

    Interim work: added visitor pattern for tree parsing, improved data references, removed unnecessary templating.

--- cpp/include/cudf/ast/ast.cuh
+++ cpp/include/cudf/ast/ast.cuh
@@ -15,163 +15,187 @@
  */
 #pragma once
 
-#include <cudf/binaryop.hpp>
 #include <cudf/detail/utilities/cuda.cuh>
+#include <cudf/scalar/scalar.hpp>
 #include <cudf/table/table.hpp>
 #include <cudf/table/table_view.hpp>
-#include <cudf/unary.hpp>
 #include <cudf/utilities/error.hpp>
 #include "cudf/column/column_factories.hpp"
 #include "cudf/table/table_device_view.cuh"
 #include "cudf/types.hpp"
+#include "operators.cuh"
 
 namespace cudf {
 
 namespace ast {
 
-enum class data_source {
-  COLUMN,       // A value from a column in a table
-  LITERAL,      // A constant value
-  INTERMEDIATE  // An internal node (not a leaf) in the AST
+enum class table_reference {
+  LEFT,   // Column index in the left table
+  RIGHT,  // Column index in the right table
+  OUTPUT  // Column index in the output table
 };
 
-enum class binary_operator {
-  ADD,      // Addition
-  SUBTRACT  // Subtraction
+namespace detail {
+
+enum class device_data_reference_type {
+  COLUMN,       // A value in a table column
+  LITERAL,      // A literal value
+  INTERMEDIATE  // An internal temporary value
 };
 
-enum class comparator {
-  LESS,    // x < y
-  GREATER  // x > y
+struct device_data_reference {
+  device_data_reference_type reference_type;  // Source of data
+  cudf::size_type
+    data_index;  // The column index of a table, index of a literal, or index of an intermediate
+  table_reference table_reference = table_reference::LEFT;
 };
 
-enum class table_source {
-  LEFT,  // Column index in the left table
-  RIGHT  // Column index in the right table
+}  // namespace detail
+
+enum class data_reference {
+  COLUMN,  // A value from a column in a table
+  LITERAL  // A literal value
 };
 
 struct expression_source {
-  data_source source;  // Source of data
+  data_reference source;  // Source of data
   cudf::size_type
     data_index;  // The column index of a table, index of a literal, or index of an intermediate
-  table_source table_source = table_source::LEFT;  // Left or right table (if applicable)
+  table_reference table_source = table_reference::LEFT;  // Left or right table (if applicable)
+};
+
+// Forward declaration
+class abstract_visitor;
+class literal;
+class operator_expression;
+
+// Visitor pattern. The visitor tracks a node index as it traverses the tree.
+class expression {
+ public:
+  virtual cudf::size_type accept(abstract_visitor& visitor) const = 0;
+};
+
+class abstract_visitor {
+ public:
+  virtual cudf::size_type visit(const literal* expr)             = 0;
+  virtual cudf::size_type visit(const operator_expression* expr) = 0;
+};
+
+class literal : public expression {
+ public:
+  literal(const cudf::scalar& value) : value(std::cref(value)) {}
+  std::reference_wrapper<const cudf::scalar> get_value() const { return this->value; }
+  cudf::size_type accept(abstract_visitor& visitor) const override { return visitor.visit(this); }
+
+ private:
+  std::reference_wrapper<const cudf::scalar> value;
+};
+
+class operator_expression : public expression {
+ public:
+  ast_operator get_operator() const { return this->op; }
+  virtual std::vector<std::shared_ptr<expression>> get_operands() const = 0;
+
+ protected:
+  operator_expression(ast_operator op) : op(op) {}
+  const ast_operator op;
+};
+
+class binary_expression : public operator_expression {
+ public:
+  binary_expression(ast_operator op,
+                    std::shared_ptr<expression> left,
+                    std::shared_ptr<expression> right)
+    : operator_expression(op), left(left), right(right)
+  {
+  }
+  std::shared_ptr<expression> get_left() const { return this->left; }
+  std::shared_ptr<expression> get_right() const { return this->right; }
+  std::vector<std::shared_ptr<expression>> get_operands() const override
+  {
+    return std::vector<std::shared_ptr<expression>>{this->get_left(), this->get_right()};
+  }
+  cudf::size_type accept(abstract_visitor& visitor) const override { return visitor.visit(this); }
+
+ private:
+  const std::shared_ptr<expression> left;
+  const std::shared_ptr<expression> right;
+};
+
+class linearizer : public abstract_visitor {
+ public:
+  linearizer() : node_counter(0) {}
+
+  cudf::size_type visit(const literal* expr) override
+  {
+    std::cout << "visiting literal value" << std::endl;
+    auto data_type = expr->get_value().get().type();
+    this->literals.push_back(expr->get_value());
+    auto source = detail::device_data_reference{
+      detail::device_data_reference_type::LITERAL,
+      0  // TODO: Use correct index
+    };
+    this->add_source(data_type, source);
+    node_counter++;
+    return node_counter;
+  }
+  cudf::size_type visit(const operator_expression* expr) override
+  {
+    std::cout << "visiting operator_expression" << std::endl;
+    this->visit_operands(expr->get_operands());
+    node_counter++;
+    return node_counter;
+  }
+
+  cudf::data_type get_root_data_type() const
+  {
+    if (this->node_data_types.size() == 0) {
+      return cudf::data_type(cudf::type_id::EMPTY);
+    } else {
+      return this->node_data_types.back();
+    }
+  }
+
+ private:
+  void visit_operands(std::vector<std::shared_ptr<expression>> operands)
+  {
+    std::vector<cudf::size_type> reference_indices;
+    for (auto& operand : operands) {
+      if (operand != nullptr) {
+        reference_index = operand->accept(*this);
+        node_counter++;
+      }
+    }
+  }
+  void add_source(cudf::data_type data_type, detail::device_data_reference source)
+  {
+    this->node_data_types.push_back(data_type);
+    this->data_references.push_back(source);
+  }
+  // State information about the "linearized" GPU execution plan
+  cudf::size_type node_counter;
+  std::vector<cudf::data_type> node_data_types;
+  std::vector<detail::device_data_reference> data_references;
+  std::vector<ast_operator> operators;
+  std::vector<cudf::size_type> operator_source_indices;
+  std::vector<std::reference_wrapper<const scalar>> literals;
 };
 
 template <typename Element>
-struct binary_expression {
-  binary_operator op;
-  expression_source lhs;
-  expression_source rhs;
-};
-
-template <typename Element>
-struct comparator_expression {
-  comparator op;
-  expression_source lhs;
-  expression_source rhs;
-};
-
-template <binary_operator>
-struct binop {
-};
-
-template <>
-struct binop<binary_operator::ADD> {
-  template <typename T>
-  __device__ T operator()(T const& lhs, T const& rhs)
-  {
-    return lhs + rhs;
-  }
-};
-
-template <>
-struct binop<binary_operator::SUBTRACT> {
-  template <typename T>
-  __device__ T operator()(T const& lhs, T const& rhs)
-  {
-    return lhs - rhs;
-  }
-};
-
-template <typename F, typename... Ts>
-__device__ decltype(auto) binop_dispatcher(binary_operator op, F&& f, Ts&&... args)
-{
-  switch (op) {
-    case binary_operator::ADD:
-      return f.template operator()<binary_operator::ADD>(std::forward<Ts>(args)...);
-    case binary_operator::SUBTRACT:
-      return f.template operator()<binary_operator::SUBTRACT>(std::forward<Ts>(args)...);
-    default: return 0;  // TODO: Error handling
-  }
-}
-
-template <typename T>
-struct do_binop {
-  template <binary_operator OP>
-  __device__ T operator()(T const& lhs, T const& rhs)
-  {
-    return binop<OP>{}(lhs, rhs);
-  }
-};
-
-template <comparator>
-struct compareop {
-};
-
-template <>
-struct compareop<comparator::LESS> {
-  template <typename T>
-  __device__ bool operator()(T const& lhs, T const& rhs)
-  {
-    return lhs < rhs;
-  }
-};
-
-template <>
-struct compareop<comparator::GREATER> {
-  template <typename T>
-  __device__ bool operator()(T const& lhs, T const& rhs)
-  {
-    return lhs > rhs;
-  }
-};
-
-template <typename F, typename... Ts>
-__device__ decltype(auto) compareop_dispatcher(comparator op, F&& f, Ts&&... args)
-{
-  switch (op) {
-    case comparator::LESS:
-      return f.template operator()<comparator::LESS>(std::forward<Ts>(args)...);
-    case comparator::GREATER:
-      return f.template operator()<comparator::GREATER>(std::forward<Ts>(args)...);
-    default: return false;  // TODO: Error handling
-  }
-}
-
-template <typename T>
-struct do_compareop {
-  template <comparator OP>
-  __device__ bool operator()(T const& lhs, T const& rhs)
-  {
-    return compareop<OP>{}(lhs, rhs);
-  }
-};
-
-template <typename Element>
-__device__ Element resolve_data_source(expression_source expression_source,
+__device__ Element resolve_data_source(detail::device_data_reference device_data_reference,
                                        table_device_view const& table,
                                        cudf::size_type row_index)
 {
-  switch (expression_source.source) {
-    case data_source::COLUMN: {
-      auto column = table.column(expression_source.data_index);
+  switch (device_data_reference.reference_type) {
+    case detail::device_data_reference_type::COLUMN: {
+      auto column = table.column(device_data_reference.data_index);
       return column.data<Element>()[row_index];
     }
-    case data_source::LITERAL: {
+    case detail::device_data_reference_type::LITERAL: {
       // TODO: Fetch and return literal.
       return static_cast<Element>(0);
     }
-    case data_source::INTERMEDIATE: {
+    case detail::device_data_reference_type::INTERMEDIATE: {
       // TODO: Fetch and return intermediate.
       return static_cast<Element>(0);
     }
@@ -183,27 +207,27 @@ __device__ Element resolve_data_source(expression_source expression_source,
 }
 
 template <typename Element>
-__device__ Element resolve_data_source(expression_source expression_source,
+__device__ Element resolve_data_source(detail::device_data_reference device_data_reference,
                                        table_device_view const& left_table,
                                        table_device_view const& right_table,
                                        cudf::size_type left_row_index,
                                        cudf::size_type right_row_index)
 {
-  switch (expression_source.source) {
-    case data_source::COLUMN: {
-      if (expression_source.table_source == table_source::LEFT) {
-        auto column = left_table.column(expression_source.data_index);
+  switch (device_data_reference.reference_type) {
+    case detail::device_data_reference_type::COLUMN: {
+      if (device_data_reference.table_reference == table_reference::LEFT) {
+        auto column = left_table.column(device_data_reference.data_index);
         return column.data<Element>()[left_row_index];
-      } else {
-        auto column = right_table.column(expression_source.data_index);
+      } else if (device_data_reference.table_reference == table_reference::RIGHT) {
+        auto column = right_table.column(device_data_reference.data_index);
         return column.data<Element>()[right_row_index];
       }
     }
-    case data_source::LITERAL: {
+    case detail::device_data_reference_type::LITERAL: {
       // TODO: Fetch and return literal.
       return static_cast<Element>(0);
     }
-    case data_source::INTERMEDIATE: {
+    case detail::device_data_reference_type::INTERMEDIATE: {
       // TODO: Fetch and return intermediate.
       return static_cast<Element>(0);
     }
@@ -215,56 +239,56 @@ __device__ Element resolve_data_source(expression_source expression_source,
 }
 
 template <typename Element>
-__device__ Element evaluate_expression(binary_expression<Element> binary_expression,
+__device__ Element evaluate_expression(ast_expression expr,
                                        table_device_view table,
                                        cudf::size_type row_index)
 {
-  const Element lhs = resolve_data_source<Element>(binary_expression.lhs, table, row_index);
-  const Element rhs = resolve_data_source<Element>(binary_expression.rhs, table, row_index);
-  return binop_dispatcher(binary_expression.op, do_binop<Element>{}, lhs, rhs);
+  const Element lhs = resolve_data_source<Element>(expr.lhs, table, row_index);
+  const Element rhs = resolve_data_source<Element>(expr.rhs, table, row_index);
+  return binop_dispatcher(expr.op, do_binop<Element>{}, lhs, rhs);
 }
 
 template <typename Element>
-__device__ Element evaluate_expression(binary_expression<Element> expression,
+__device__ Element evaluate_expression(binary_expression expr,
                                        table_device_view left_table,
                                        table_device_view right_table,
                                        cudf::size_type left_row_index,
                                        cudf::size_type right_row_index)
 {
   const Element lhs = resolve_data_source<Element>(
-    expression.lhs, left_table, right_table, left_row_index, right_row_index);
+    expr.lhs, left_table, right_table, left_row_index, right_row_index);
   const Element rhs = resolve_data_source<Element>(
-    expression.rhs, left_table, right_table, left_row_index, right_row_index);
-  return binop_dispatcher(expression.op, do_binop<Element>{}, lhs, rhs);
+    expr.rhs, left_table, right_table, left_row_index, right_row_index);
+  return binop_dispatcher(expr.op, do_binop<Element>{}, lhs, rhs);
 }
 
 template <typename Element>
-__device__ bool evaluate_expression(comparator_expression<Element> expression,
+__device__ bool evaluate_expression(comparator_expression expr,
                                     table_device_view table,
                                     cudf::size_type row_index)
 {
-  const Element lhs = resolve_data_source<Element>(expression.lhs, table, row_index);
-  const Element rhs = resolve_data_source<Element>(expression.rhs, table, row_index);
-  return compareop_dispatcher(expression.op, do_compareop<Element>{}, lhs, rhs);
+  const Element lhs = resolve_data_source<Element>(expr.lhs, table, row_index);
+  const Element rhs = resolve_data_source<Element>(expr.rhs, table, row_index);
+  return compareop_dispatcher(expr.op, do_compareop<Element>{}, lhs, rhs);
 }
 
 template <typename Element>
-__device__ bool evaluate_expression(comparator_expression<Element> expression,
+__device__ bool evaluate_expression(comparator_expression expr,
                                     table_device_view left_table,
                                     table_device_view right_table,
                                     cudf::size_type left_row_index,
                                     cudf::size_type right_row_index)
 {
   const Element lhs = resolve_data_source<Element>(
-    expression.lhs, left_table, right_table, left_row_index, right_row_index);
+    expr.lhs, left_table, right_table, left_row_index, right_row_index);
   const Element rhs = resolve_data_source<Element>(
-    expression.rhs, left_table, right_table, left_row_index, right_row_index);
-  return compareop_dispatcher(expression.op, do_compareop<Element>{}, lhs, rhs);
+    expr.rhs, left_table, right_table, left_row_index, right_row_index);
+  return compareop_dispatcher(expr.op, do_compareop<Element>{}, lhs, rhs);
 }
 
 template <typename Element>
 __global__ void compute_column_kernel(table_device_view table,
-                                      binary_expression<Element> expression,
+                                      binary_expression expr,
                                       mutable_column_device_view output)
 {
   const cudf::size_type start_idx = threadIdx.x + blockIdx.x * blockDim.x;
@@ -272,13 +296,13 @@ __global__ void compute_column_kernel(table_device_view table,
   const auto num_rows             = table.num_rows();
 
   for (cudf::size_type row_index = start_idx; row_index < num_rows; row_index += stride) {
-    output.element<Element>(row_index) = evaluate_expression(expression, table, row_index);
+    output.element<Element>(row_index) = evaluate_expression<Element>(expr, table, row_index);
   }
 }
 
 template <typename Element>
 __global__ void compute_column_kernel(table_device_view table,
-                                      comparator_expression<Element> expression,
+                                      comparator_expression expr,
                                       mutable_column_device_view output)
 {
   const cudf::size_type start_idx = threadIdx.x + blockIdx.x * blockDim.x;
@@ -286,51 +310,34 @@ __global__ void compute_column_kernel(table_device_view table,
   const auto num_rows             = table.num_rows();
 
   for (cudf::size_type row_index = start_idx; row_index < num_rows; row_index += stride) {
-    output.element<bool>(row_index) = evaluate_expression(expression, table, row_index);
+    output.element<bool>(row_index) = evaluate_expression<Element>(expr, table, row_index);
   }
 }
 
 template <typename Element>
 std::unique_ptr<column> compute_column(
   table_view const& table,
-  binary_expression<Element> expression,
+  std::shared_ptr<expression> expr,
   cudaStream_t stream                 = 0,  // TODO use detail API
   rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource())
 {
-  auto table_device      = table_device_view::create(table, stream);
-  auto element_data_type = cudf::data_type(cudf::type_to_id<Element>());
-  auto table_num_rows    = table.num_rows();
+  // Linearize the AST
+  auto expr_linearizer = linearizer();
+  expr->accept(expr_linearizer);
+  auto expr_data_type = expr_linearizer.get_root_data_type();
+
+  auto table_device = table_device_view::create(table, stream);
+  // auto expr_data_type = cudf::data_type(cudf::type_to_id<Element>());
+  auto table_num_rows = table.num_rows();
   auto output_column =
-    make_fixed_width_column(element_data_type, table_num_rows, mask_state::UNALLOCATED, stream, mr);
+    make_fixed_width_column(expr_data_type, table_num_rows, mask_state::UNALLOCATED, stream, mr);
   auto block_size = 1024;  // TODO dynamically determine block size, use shared memory
   auto mutable_output_device =
     cudf::mutable_column_device_view::create(output_column->mutable_view(), stream);
 
-  detail::grid_1d config(table_num_rows, block_size);
+  cudf::detail::grid_1d config(table_num_rows, block_size);
   compute_column_kernel<Element><<<config.num_blocks, config.num_threads_per_block, 0, stream>>>(
-    *table_device, expression, *mutable_output_device);
-  return output_column;
-}
-
-template <typename Element>
-std::unique_ptr<column> compute_column(
-  table_view const& table,
-  comparator_expression<Element> expression,
-  cudaStream_t stream                 = 0,  // TODO use detail API
-  rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource())
-{
-  auto table_device      = table_device_view::create(table, stream);
-  auto element_data_type = cudf::data_type(cudf::type_to_id<bool>());
-  auto table_num_rows    = table.num_rows();
-  auto output_column =
-    make_fixed_width_column(element_data_type, table_num_rows, mask_state::UNALLOCATED, stream, mr);
-  auto block_size = 1024;  // TODO dynamically determine block size, use shared memory
-  auto mutable_output_device =
-    cudf::mutable_column_device_view::create(output_column->mutable_view(), stream);
-
-  detail::grid_1d config(table_num_rows, block_size);
-  compute_column_kernel<Element><<<config.num_blocks, config.num_threads_per_block, 0, stream>>>(
-    *table_device, expression, *mutable_output_device);
+    *table_device, expr, *mutable_output_device);
   return output_column;
 }
 
