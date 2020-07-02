fa3a659314324d06d1979d6b9785a4ff20e7a2ee
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2020-07-02 23:21:29 +0530

    add string conversion API from_durations()

---
+++ cpp/include/cudf/strings/convert/convert_durations.hpp
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <cudf/column/column.hpp>
+#include <cudf/strings/strings_column_view.hpp>
+
+namespace cudf {
+namespace strings {
+/**
+ * @addtogroup strings_convert
+ * @{
+ */
+
+/**
+ * @brief Returns a new duration column converting a strings column into
+ * durations using the provided format pattern.
+ *
+ * The format pattern can include the following specifiers: "%Y,%y,%m,%d,%H,%I,%p,%M,%S,%f,%z"
+ *
+ * | Specifier | Description |
+ * | :-------: | ----------- |
+ * | %%d | Day of the month: 01-31 |
+ * | %%m | Month of the year: 01-12 |
+ * | %%y | Year without century: 00-99 |
+ * | %%Y | Year with century: 0001-9999 |
+ * | %%H | 24-hour of the day: 00-23 |
+ * | %%I | 12-hour of the day: 01-12 |
+ * | %%M | Minute of the hour: 00-59|
+ * | %%S | Second of the minute: 00-59 |
+ * | %%f | 6-digit microsecond: 000000-999999 |
+ * | %%z | UTC offset with format ±HHMM Example +0500 |
+ * | %%j | Day of the year: 001-366 |
+ * | %%p | Only 'AM', 'PM' or 'am', 'pm' are recognized |
+ *
+ * Other specifiers are not currently supported.
+ *
+ * Invalid formats are not checked. If the string contains unexpected
+ * or insufficient characters, that output row entry's timestamp value is undefined.
+ *
+ * Any null string entry will result in a corresponding null row in the output column.
+ *
+ * The resulting time units are specified by the `timestamp_type` parameter.
+ * The time units are independent of the number of digits parsed by the "%f" specifier.
+ * The "%f" supports a precision value to read the numeric digits. Specify the
+ * precision with a single integer value (1-9) as follows:
+ * use "%3f" for milliseconds, "%6f" for microseconds and "%9f" for nanoseconds.
+ *
+ * @throw cudf::logic_error if timestamp_type is not a timestamp type.
+ *
+ * @param strings Strings instance for this operation.
+ * @param timestamp_type The timestamp type used for creating the output column.
+ * @param format String specifying the timestamp format in strings.
+ * @param mr Device memory resource used to allocate the returned column's device memory.
+ * @return New datetime column.
+ */
+std::unique_ptr<column> to_durations(
+  strings_column_view const& strings,
+  data_type duration_type,
+  std::string const& format,
+  rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
+
+/**
+ * @brief Returns a new strings column converting a duration column into
+ * strings using the provided format pattern.
+ *
+ * The format pattern can include the following specifiers: "%Y,%y,%m,%d,%H,%I,%p,%M,%S,%f,%z,%Z"
+ *
+ * | Specifier | Description |
+ * | :-------: | ----------- |
+ * | %%d | Day of the month: 01-31 |
+ * | %%m | Month of the year: 01-12 |
+ * | %%y | Year without century: 00-99 |
+ * | %%Y | Year with century: 0001-9999 |
+ * | %%H | 24-hour of the day: 00-23 |
+ * | %%I | 12-hour of the day: 01-12 |
+ * | %%M | Minute of the hour: 00-59|
+ * | %%S | Second of the minute: 00-59 |
+ * | %%f | 6-digit microsecond: 000000-999999 |
+ * | %%z | Always outputs "+0000" |
+ * | %%Z | Always outputs "UTC" |
+ * | %%j | Day of the year: 001-366 |
+ * | %%p | Only 'AM' or 'PM' |
+ *
+ * No checking is done for invalid formats or invalid timestamp values.
+ * All timestamps values are formatted to UTC.
+ *
+ * Any null input entry will result in a corresponding null entry in the output column.
+ *
+ * The time units of the input column do not influence the number of digits written by
+ * the "%f" specifier.
+ * The "%f" supports a precision value to write out numeric digits for the subsecond value.
+ * Specify the precision with a single integer value (1-9) between the "%" and the "f" as follows:
+ * use "%3f" for milliseconds, "%6f" for microseconds and "%9f" for nanoseconds.
+ * If the precision is higher than the units, then zeroes are padded to the right of
+ * the subsecond value.
+ * If the precision is lower than the units, the subsecond value may be truncated.
+ *
+ * @throw cudf::logic_error if `timestamps` column parameter is not a timestamp type.
+ *
+ * @param timestamps Timestamp values to convert.
+ * @param format The string specifying output format.
+ *        Default format is "%Y-%m-%dT%H:%M:%SZ".
+ * @param mr Device memory resource used to allocate the returned column's device memory.
+ * @return New strings column with formatted timestamps.
+ */
+std::unique_ptr<column> from_durations(
+  column_view const& durations,
+  std::string const& format           = "%d days %H:%M:%S",
+  //"P%YY%MM%DDT%HH%MM%SS" is_iso_format() for skipping leading zeros.
+  rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
+
+/** @} */  // end of doxygen group
+}  // namespace strings
+}  // namespace cudf
---
+++ cpp/src/strings/convert/convert_durations.cu
@@ -0,0 +1,439 @@
+/*
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <cudf/column/column_device_view.cuh>
+#include <cudf/types.hpp>
+#include <strings/utilities.cuh>
+
+#include <map>
+#include <vector>
+#include "cudf/utilities/traits.hpp"
+
+namespace cudf {
+namespace strings {
+namespace detail {
+
+// algo
+// split internal rep to D,H,M,S,ns
+// ask struct {int64_t, int8_t, int8_t, int8_t, int64_t}, then type specific function to convert.
+// reuse conver_integers.cu code for size, string_conversion for days.
+// move functions int->str, str->int to .cuh file, & reuse
+namespace {
+
+// used to index values in a timeparts array
+enum timestamp_parse_component {
+  TP_DAY       = 0,
+  TP_HOUR      = 1,
+  TP_MINUTE    = 2,
+  TP_SECOND    = 3,
+  TP_SUBSECOND = 4,
+  TP_ARRAYSIZE = 5
+};
+
+enum class format_char_type : int8_t {
+  literal,   // literal char type passed through
+  specifier  // duration format specifier
+};
+
+/**
+ * @brief Represents a format specifier or literal from a timestamp format string.
+ *
+ * Created by the format_compiler when parsing a format string.
+ */
+struct alignas(4) format_item {
+  format_char_type item_type;  // specifier or literal indicator
+  char value;                  // specifier or literal value
+  int8_t length;               // item length in bytes
+
+  static format_item new_specifier(char format_char, int8_t length)
+  {
+    return format_item{format_char_type::specifier, format_char, length};
+  }
+  static format_item new_delimiter(char literal)
+  {
+    return format_item{format_char_type::literal, literal, 1};
+  }
+};
+
+/**
+ * @brief The format_compiler parses a timestamp format string into a vector of
+ * format_items.
+ *
+ * The vector of format_items are used when parsing a string into timestamp
+ * components and when formatting a string from timestamp components.
+ */
+struct format_compiler {
+  std::string format;
+  type_id units;  // is this required?
+  rmm::device_vector<format_item> d_items;
+
+  std::map<char, int8_t> specifier_lengths = {{'d', -1},
+                                              {'+', -1},
+                                              {'H', 2},
+                                              {'M', 2},
+                                              {'S', 2},
+                                              {'u', -1},
+                                              {'f', 9}};  // TODO: -1 or 9?
+
+  format_compiler(const char* format, type_id units) : format(format), units(units) {}
+
+  format_item const* compile_to_device()
+  {
+    std::vector<format_item> items;
+    const char* str = format.c_str();
+    auto length     = format.length();
+    while (length > 0) {
+      char ch = *str++;
+      length--;
+      if (ch != '%') {
+        items.push_back(format_item::new_delimiter(ch));
+        continue;
+      }
+      CUDF_EXPECTS(length > 0, "Unfinished specifier in timestamp format");
+
+      ch = *str++;
+      length--;
+      if (ch == '%')  // escaped % char
+      {
+        items.push_back(format_item::new_delimiter(ch));
+        continue;
+      }
+      if (ch >= '0' && ch <= '9') {
+        CUDF_EXPECTS(*str == 'f', "precision not supported for specifier: " + std::string(1, *str));
+        specifier_lengths[*str] = static_cast<int8_t>(ch - '0');
+        ch                      = *str++;
+        length--;
+      }
+      CUDF_EXPECTS(specifier_lengths.find(ch) != specifier_lengths.end(),
+                   "invalid format specifier: " + std::string(1, ch));
+
+      int8_t spec_length = specifier_lengths[ch];
+      items.push_back(format_item::new_specifier(ch, spec_length));
+    }
+    // create program in device memory
+    d_items.resize(items.size());
+    CUDA_TRY(cudaMemcpyAsync(
+      d_items.data().get(), items.data(), items.size() * sizeof(items[0]), cudaMemcpyHostToDevice));
+    return d_items.data().get();
+  }
+
+  // these calls are only valid after compile_to_device is called
+  size_type items_count() const { return static_cast<size_type>(d_items.size()); }
+  //int8_t subsecond_precision() const { return specifier_lengths.at('f'); }
+};
+
+__device__ void dissect_duration(int64_t timestamp, int32_t* timeparts, type_id units)
+{
+  if (units == type_id::DURATION_DAYS) {
+    timeparts[TP_DAY] = static_cast<int32_t>(timestamp);
+    return;
+  }
+
+  // Specialized modulo expression that handles negative values.
+  // Examples:
+  //     modulo(1,60)    1
+  //     modulo(-1,60)  59
+  auto modulo_time = [](int64_t time, int64_t base) {
+    return static_cast<int32_t>(((time % base) + base) % base);
+  };
+
+  // This function handles converting units by dividing and adjusting for negative values.
+  // Examples:
+  //     scale(-61,60)  -2
+  //     scale(-60,60)  -1
+  //     scale(-59,60)  -1
+  //     scale( 59,60)   0
+  //     scale( 60,60)   1
+  //     scale( 61,60)   1
+  auto scale_time = [](int64_t time, int64_t base) {
+    return static_cast<int32_t>((time - ((time < 0) * (base - 1L))) / base);
+  };
+
+  int64_t seconds{0};
+  if (units == type_id::DURATION_SECONDS) {
+    seconds = timestamp;
+  } else if (units == type_id::DURATION_MILLISECONDS) {
+    seconds                 = scale_time(timestamp, 1000);  // TODO scale_time or timestamp/1000;
+    timeparts[TP_SUBSECOND] = modulo_time(timestamp, 1000);   // * 1000 * 1000;
+  } else if (units == type_id::DURATION_MICROSECONDS) {
+    seconds                 = scale_time(timestamp, 1000 * 1000);  // TODO above
+    timeparts[TP_SUBSECOND] = modulo_time(timestamp, 1000 * 1000);   // * 1000;
+  } else if (units == type_id::DURATION_NANOSECONDS) {
+    seconds                 = scale_time(timestamp, 1000 * 1000 * 1000);  // TODO above
+    timeparts[TP_SUBSECOND] = modulo_time(timestamp, 1000 * 1000 * 1000);
+  }
+  timeparts[TP_DAY]    = scale_time(seconds, 24 * 60 * 60);
+  timeparts[TP_HOUR]   = modulo_time(scale_time(seconds, 60 * 60), 24);
+  timeparts[TP_MINUTE] = modulo_time(scale_time(seconds, 60), 60);
+  timeparts[TP_SECOND] = modulo_time(seconds, 60);
+}
+
+template <typename T>
+struct duration_to_string_size_fn {
+  const column_device_view d_timestamps;
+  const format_item* d_format_items;
+  size_type items_count;
+  type_id type;
+  // TODO: add boolean for isoformat
+
+  __device__ int8_t format_length(char format_char, int32_t* timeparts)
+  {
+    switch (format_char) {
+      case 'd': return count_digits(timeparts[TP_DAY]); break;
+      case '+': return timeparts[TP_DAY] < 0 ? 1 : 0; break;
+      case 'H': return count_digits(timeparts[TP_HOUR]); break;
+      case 'M': return count_digits(timeparts[TP_MINUTE]); break;
+      case 'S': return count_digits(timeparts[TP_SECOND]); break;
+      // TODO:  multiples of 3. (  + 2) / 3 * 3 (pandas needs this<6, isoformat doesn't)
+      // TODO: include dot only if non-zero.
+      // us specifier for pandas
+      case 'u': return count_digits(timeparts[TP_SUBSECOND])>0 ? 6 : 0; break;
+      case 'f': return count_digits(timeparts[TP_SUBSECOND]); break;
+      default: return 2;
+    }
+  }
+
+  __device__ size_type operator()(size_type idx)
+  {
+    if (d_timestamps.is_null(idx)) return 0;
+    auto duration                   = d_timestamps.element<T>(idx);
+    int32_t timeparts[TP_ARRAYSIZE] = {0};  // days, hours, minutes, seconds, subseconds(9)
+    dissect_duration(duration.count(), timeparts, type);
+    size_type string_length{0};
+    for (auto i = 0; i < items_count; i++) {
+      format_item item = d_format_items[i];
+      if (item.item_type == format_char_type::literal)
+        string_length++;
+      else if (item.length != -1)
+        string_length += item.length;
+      else
+        string_length += format_length(item.value, timeparts);
+    }
+    return string_length;
+    // convert to characters
+    // format_from_parts(timeparts, d_chars + d_offsets[idx]);
+  }
+};
+
+template <typename T>
+struct duration_to_string_fn : public duration_to_string_size_fn<T> {
+  const int32_t* d_offsets;
+  char* d_chars;
+  using duration_to_string_size_fn<T>::d_timestamps;
+  using duration_to_string_size_fn<T>::d_format_items;
+  using duration_to_string_size_fn<T>::items_count;
+  using duration_to_string_size_fn<T>::type;
+
+  duration_to_string_fn(const column_device_view d_timestamps,
+                        const format_item* d_format_items,
+                        size_type items_count,
+                        type_id type,
+                        const int32_t* d_offsets,
+                        char* d_chars)
+    : duration_to_string_size_fn<T>{d_timestamps, d_format_items, items_count, type},
+      d_offsets(d_offsets),
+      d_chars(d_chars)
+  {
+  }
+
+  // utility to create (optionally) 0-padded integers (up to 10 chars) with negative sign.
+  // min_digits==-1 indicates no 0-padding.
+  __device__ char* int2str(char* str, int min_digits, int32_t value)
+  {
+    constexpr int MAX_DIGITS = 10;  // largest 32-bit integer is 10 digits
+    assert(min_digits <= MAX_DIGITS);
+    if (value == 0) {
+      do {
+        *str++ = '0';
+      } while (--min_digits > 0);
+      return str;
+    }
+    bool is_negative = (value < 0);
+
+    char digits[MAX_DIGITS]  = {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0'};
+    int digits_idx           = 0;
+    while (value != 0) {
+      assert(digits_idx < MAX_DIGITS);
+      digits[digits_idx++] = '0' + std::abs(value % 10);
+      // next digit
+      value = value / 10;
+    }
+    if (is_negative) {
+      *str++ = '-';
+      min_digits--;
+    }
+    digits_idx = std::max(digits_idx, min_digits);
+    // digits are backwards, reverse the string into the output
+    while (digits_idx-- > 0) *str++ = digits[digits_idx];
+    return str;
+  }
+
+  __device__ char* format_from_parts(int32_t const* timeparts, char* ptr)
+  {
+    for (size_t idx = 0; idx < items_count; ++idx) {
+      auto item = d_format_items[idx];
+      if (item.item_type == format_char_type::literal) {
+        *ptr++ = item.value;
+        continue;
+      }
+      // special logic for each specifier
+      switch (item.value) {
+        case 'd':  // days
+          ptr = int2str(ptr, item.length, timeparts[TP_DAY]);
+          break;
+        case '+':  // + if day is negative
+          if(timeparts[TP_DAY]<0) *ptr++='+';
+          break;
+        case 'H':  // 24-hour
+          ptr = int2str(ptr, item.length, timeparts[TP_HOUR]);
+          break;
+        case 'M':  // minute
+          ptr = int2str(ptr, item.length, timeparts[TP_MINUTE]);
+          break;
+        case 'S':  // second
+          ptr = int2str(ptr, item.length, timeparts[TP_SECOND]);
+          break;
+        case 'f':  // sub-second
+        {
+          char subsecond_digits[] = "000000000";  // 9 max digits
+          const int digits        = [units = type] {
+            if (units == type_id::DURATION_MILLISECONDS) return 3;
+            if (units == type_id::DURATION_MICROSECONDS) return 6;
+            if (units == type_id::DURATION_NANOSECONDS) return 9;
+            return 0;
+          }();
+          int2str(subsecond_digits, digits, timeparts[TP_SUBSECOND]);
+          ptr = copy_and_increment(ptr, subsecond_digits, item.length);
+          break;
+        }
+        default:  // ignore everything else
+          break;
+      }
+    }
+    return ptr;
+  }
+
+  __device__ void operator()(size_type idx)
+  {
+    if (d_timestamps.is_null(idx)) return;
+    auto duration                   = d_timestamps.template element<T>(idx);
+    int32_t timeparts[TP_ARRAYSIZE] = {0};  // days, hours, minutes, seconds, subseconds(9)
+    dissect_duration(duration.count(), timeparts, type);
+    // convert to characters
+    format_from_parts(timeparts, d_chars + d_offsets[idx]);
+  }
+};
+
+/**
+ * @brief This dispatch method is for converting integers into strings.
+ * The template function declaration ensures only integer types are used.
+ */
+struct dispatch_from_durations_fn {
+  template <typename T, std::enable_if_t<cudf::is_duration<T>()>* = nullptr>
+  std::unique_ptr<column> operator()(column_view const& durations,
+                                     std::string const& format,
+                                     rmm::mr::device_memory_resource* mr,
+                                     cudaStream_t stream) const
+  {
+    CUDF_EXPECTS(!format.empty(), "Format parameter must not be empty.");
+
+    format_compiler compiler(format.c_str(), durations.type().id());
+    auto d_format_items = compiler.compile_to_device();
+
+    size_type strings_count = durations.size();
+    auto column   = column_device_view::create(durations, stream);
+    auto d_column = *column;
+
+    // copy null mask
+    rmm::device_buffer null_mask = copy_bitmask(durations, stream, mr);
+    // build offsets column
+    auto offsets_transformer_itr = thrust::make_transform_iterator(
+      thrust::make_counting_iterator<int32_t>(0),
+      duration_to_string_size_fn<T>{
+        d_column, d_format_items, compiler.items_count(), durations.type().id()});
+    auto offsets_column = detail::make_offsets_child_column(
+      offsets_transformer_itr, offsets_transformer_itr + strings_count, mr, stream);
+    auto offsets_view  = offsets_column->view();
+    auto d_new_offsets = offsets_view.template data<int32_t>();
+
+    // build chars column
+    size_type bytes = thrust::device_pointer_cast(d_new_offsets)[strings_count];
+    auto chars_column =
+      detail::create_chars_child_column(strings_count, durations.null_count(), bytes, mr, stream);
+    auto chars_view = chars_column->mutable_view();
+    auto d_chars    = chars_view.template data<char>();
+    thrust::fill_n(rmm::exec_policy(stream)->on(stream), d_chars, bytes, '0');
+
+    thrust::for_each_n(rmm::exec_policy(stream)->on(stream),
+                       thrust::make_counting_iterator<size_type>(0),
+                       strings_count,
+                       duration_to_string_fn<T>{d_column,
+                                                d_format_items,
+                                                compiler.items_count(),
+                                                durations.type().id(),
+                                                d_new_offsets,
+                                                d_chars});
+
+    //
+    return make_strings_column(strings_count,
+                               std::move(offsets_column),
+                               std::move(chars_column),
+                               durations.null_count(),
+                               std::move(null_mask),
+                               stream,
+                               mr);
+  }
+
+  // non-duration types throw an exception
+  template <typename T, std::enable_if_t<not cudf::is_duration<T>()>* = nullptr>
+  std::unique_ptr<column> operator()(column_view const&,
+                                     std::string const& format,
+                                     rmm::mr::device_memory_resource*,
+                                     cudaStream_t) const
+  {
+    CUDF_FAIL("Values for from_duration function must be a duration type.");
+  }
+};
+
+}  // namespace
+
+std::unique_ptr<column> from_durations(
+  column_view const& durations,
+  std::string const& format,
+  // "%d days %H:%M:%S.%6f",
+  //"P%YY%MM%DDT%HH%MM%SS" is_iso_format() for skipping leading zeros.
+  // TODO
+  // common for all: check if non-zero days,
+  // per item: non-zero subseconds present. 1(.)+(width 3/6/9 based on non-zero ms/us/ns)
+  // P%YY%MM%DDT%HH%MM%S%fS
+  cudaStream_t stream,
+  rmm::mr::device_memory_resource* mr)
+{
+  size_type strings_count = durations.size();
+  if (strings_count == 0) return make_empty_strings_column(mr, stream);
+
+  return type_dispatcher(durations.type(), dispatch_from_durations_fn{}, durations, format, mr, stream);
+}
+
+}  // namespace detail
+
+std::unique_ptr<column> from_durations(column_view const& durations,
+                                       std::string const& format,
+                                       rmm::mr::device_memory_resource* mr)
+{
+  return detail::from_durations(durations, format, cudaStream_t{}, mr);
+}
+}  // namespace strings
+}  // namespace cudf
