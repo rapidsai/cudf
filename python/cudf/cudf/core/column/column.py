bb523c74f55cc18c22a420ec6d5137738b0e9020
brandon-b-miller <brmiller@nvidia.com>
2020-07-02 13:28:09 -0700

    handle direct series case

--- python/cudf/cudf/core/column/column.py
+++ python/cudf/cudf/core/column/column.py
@@ -1432,6 +1432,8 @@ def as_column(arbitrary, nan_as_null=None, dtype=None, length=None):
         data = ColumnBase._concat(gpu_cols, dtype=new_dtype)
 
     elif isinstance(arbitrary, (pd.Series, pd.Categorical)):
+        if isinstance(arbitrary.array, pd.core.arrays.masked.BaseMaskedArray):
+            return as_column(arbitrary.array)
         if is_categorical_dtype(arbitrary):
             data = as_column(pa.array(arbitrary, from_pandas=True))
         elif arbitrary.dtype == np.bool:
