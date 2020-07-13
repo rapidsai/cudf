16bdb08d16c9b21830005c375207596b291501c4
Sriram Chandramouli <sriram_chandramouli@hotmail.com>
2020-07-13 20:55:18 +0000

    - review comments

--- cpp/tests/utilities/column_wrapper.hpp
+++ cpp/tests/utilities/column_wrapper.hpp
@@ -105,31 +105,31 @@ class column_wrapper {
 /**
  * @brief Convert between source and target types when they differ and where possible.
  **/
-template <typename SourceT, typename TargetT>
+template <typename From, typename To>
 struct fixed_width_type_converter {
   // Are the types same - simply copy elements from [begin, end) to out
-  template <typename SrcT  = SourceT,
-            typename TargT = TargetT,
+  template <typename FromT = From,
+            typename ToT   = To,
             typename InputIterator,
             typename OutputIterator,
-            typename std::enable_if<std::is_same<SrcT, TargT>::value, void>::type* = nullptr>
+            typename std::enable_if<std::is_same<FromT, ToT>::value, void>::type* = nullptr>
   void operator()(InputIterator begin, InputIterator end, OutputIterator out) const
   {
     std::copy(begin, end, out);
   }
 
   // Are the types convertible or can target be constructed from source?
-  template <typename SrcT  = SourceT,
-            typename TargT = TargetT,
+  template <typename FromT = From,
+            typename ToT   = To,
             typename InputIterator,
             typename OutputIterator,
-            typename std::enable_if<!std::is_same<SrcT, TargT>::value &&
-                                      (cudf::is_convertible<SrcT, TargT>::value ||
-                                       std::is_constructible<TargT, SrcT>::value),
+            typename std::enable_if<!std::is_same<FromT, ToT>::value &&
+                                      (cudf::is_convertible<FromT, ToT>::value ||
+                                       std::is_constructible<ToT, FromT>::value),
                                     void>::type* = nullptr>
   void operator()(InputIterator begin, InputIterator end, OutputIterator out) const
   {
-    std::transform(begin, end, out, [](auto const& e) { return static_cast<TargT>(e); });
+    std::transform(begin, end, out, [](auto const& e) { return static_cast<ToT>(e); });
   }
 
 #if 0
@@ -137,10 +137,10 @@ struct fixed_width_type_converter {
   // this conflicts with the convertible/constructible overload
   // Convert integral values to timestamps
   template <
-    typename SrcT                        = SourceT,
-    typename TargT                       = TargetT,
+    typename FromT                        = From,
+    typename ToT                       = To,
     typename InputIterator, typename OutputIterator,
-    typename std::enable_if<std::is_integral<SrcT>::value && cudf::is_timestamp_t<TargT>::value,
+    typename std::enable_if<std::is_integral<FromT>::value && cudf::is_timestamp_t<ToT>::value,
                             void>::type* = nullptr>
   void operator()(InputIterator begin,
                   InputIterator end,
@@ -148,50 +148,9 @@ struct fixed_width_type_converter {
                   ) const
   {
       std::transform(
-      begin, end, out, [](auto const& e) { return TargT{typename TargT::duration{e}}; });
+      begin, end, out, [](auto const& e) { return ToT{typename ToT::duration{e}}; });
   }
 #endif
-
-  // Convert timestamps to arithmetic values
-  template <
-    typename SrcT  = SourceT,
-    typename TargT = TargetT,
-    typename InputIterator,
-    typename OutputIterator,
-    typename std::enable_if<cudf::is_timestamp_t<SrcT>::value && std::is_arithmetic<TargT>::value,
-                            void>::type* = nullptr>
-  void operator()(InputIterator begin, InputIterator end, OutputIterator out) const
-  {
-    std::transform(begin, end, out, [](auto const& e) {
-      return static_cast<TargT>(e.time_since_epoch().count());
-    });
-  }
-
-  // Convert timestamps to duration values
-  template <
-    typename SrcT  = SourceT,
-    typename TargT = TargetT,
-    typename InputIterator,
-    typename OutputIterator,
-    typename std::enable_if<cudf::is_timestamp_t<SrcT>::value && cudf::is_duration_t<TargT>::value,
-                            void>::type* = nullptr>
-  void operator()(InputIterator begin, InputIterator end, OutputIterator out) const
-  {
-    std::transform(begin, end, out, [](auto const& e) { return TargT{e.time_since_epoch()}; });
-  }
-
-  // Convert duration to arithmetic values
-  template <
-    typename SrcT  = SourceT,
-    typename TargT = TargetT,
-    typename InputIterator,
-    typename OutputIterator,
-    typename std::enable_if<cudf::is_duration_t<SrcT>::value && std::is_arithmetic<TargT>::value,
-                            void>::type* = nullptr>
-  void operator()(InputIterator begin, InputIterator end, OutputIterator out) const
-  {
-    std::transform(begin, end, out, [](auto const& e) { return static_cast<TargT>(e.count()); });
-  }
 };
 
 /**
