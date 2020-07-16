cc7436cec9981b4db1dce50756d5ff97077ba5af
galipremsagar <sagarprem75@gmail.com>
2020-07-15 21:00:57 -0700

    add Frame.repeat docs

--- python/cudf/cudf/core/frame.py
+++ python/cudf/cudf/core/frame.py
@@ -498,6 +498,7 @@ class Frame(libcudf.table.Table):
         Clipped DataFrame/Series/Index/MultiIndex
 
         Examples
+        --------
         >>> import cudf
         >>> df = cudf.DataFrame({"a":[1, 2, 3, 4], "b":['a', 'b', 'c', 'd']})
         >>> df.clip(lower=[2, 'b'], upper=[3, 'c'])
@@ -1216,17 +1217,49 @@ class Frame(libcudf.table.Table):
         return self._from_table(out_rank_table).astype(np.float64)
 
     def repeat(self, repeats, axis=None):
-        """Repeats elements consecutively
+        """Repeats elements consecutively.
+
+        Returns a new object of caller type(DataFrame/Series/Index) where each
+        element of the current object is repeated consecutively a given
+        number of times.
 
         Parameters
         ----------
-        repeats : int, array, numpy array, or Column
-            the number of times to repeat each element
+        repeats : int, or array of ints
+            The number of repetitions for each element. This should
+            be a non-negative integer. Repeating 0 times will return
+            an empty object.
+
+        Returns
+        -------
+        Series/DataFrame/Index
+            A newly created object of same type as caller
+            with repeated elements.
 
         Examples
         --------
-        >>> import cudf as cudf
-        >>> s = cudf.Series([0, 2]) # or DataFrame
+        >>> import cudf
+        >>> df = cudf.DataFrame({'a': [1, 2, 3], 'b': [10, 20, 30]})
+        >>> df
+           a   b
+        0  1  10
+        1  2  20
+        2  3  30
+        >>> df.repeat(3)
+           a   b
+        0  1  10
+        0  1  10
+        0  1  10
+        1  2  20
+        1  2  20
+        1  2  20
+        2  3  30
+        2  3  30
+        2  3  30
+
+        Repeat on Series
+
+        >>> s = cudf.Series([0, 2])
         >>> s
         0    0
         1    2
@@ -1246,7 +1279,16 @@ class Frame(libcudf.table.Table):
         1    2
         1    2
         dtype: int64
-        >>>
+
+        Repeat on Index
+
+        >>> index = cudf.Index([10, 22, 33, 55])
+        >>> index
+        Int64Index([10, 22, 33, 55], dtype='int64')
+        >>> index.repeat(5)
+        Int64Index([10, 10, 10, 10, 10, 22, 22, 22, 22, 22, 33,
+                    33, 33, 33, 33, 55, 55, 55, 55, 55],
+                dtype='int64')
         """
         if axis is not None:
             raise NotImplementedError(
@@ -1632,6 +1674,7 @@ class Frame(libcudf.table.Table):
         Interleave Series columns of a table into a single column.
 
         Converts the column major table `cols` into a row major column.
+
         Parameters
         ----------
         cols : input Table containing columns to interleave.
