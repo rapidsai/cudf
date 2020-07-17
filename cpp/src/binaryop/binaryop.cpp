0256cd5b87439b873687b9e4a5b4ea736cc604a5
Conor Hoekstra <codereport@outlook.com>
2020-07-17 13:17:58 -0400

    Add back necessary jitify changes (seems to work w/o b/c of caching bug)

--- cpp/src/binaryop/binaryop.cpp
+++ cpp/src/binaryop/binaryop.cpp
@@ -70,8 +70,12 @@ namespace jit {
 
 const std::string hash = "prog_binop";
 
-const std::vector<std::string> header_names{
-  "operation.h", "traits.h", cudf_types_hpp, cudf_utilities_bit_hpp, cudf_wrappers_timestamps_hpp};
+const std::vector<std::string> header_names{"operation.h",
+                                            "traits.h",
+                                            cudf_types_hpp,
+                                            cudf_utilities_bit_hpp,
+                                            cudf_wrappers_timestamps_hpp,
+                                            cudf_fixed_point_fixed_point_hpp};
 
 std::istream* headers_code(std::string filename, std::iostream& stream)
 {
