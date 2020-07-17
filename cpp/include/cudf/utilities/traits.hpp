332e2d04b9709ba3a27cfbe75abc35c483d15d7c
Sriram Chandramouli <sriram_chandramouli@hotmail.com>
2020-07-17 17:16:05 +0000

    - remove specialization of `is_trivially_copyable`   - this was required in 10.0 builds due to nvcc issues   - now that 10.0 builds have been removed, remove the specialization as well

--- cpp/include/cudf/utilities/traits.hpp
+++ cpp/include/cudf/utilities/traits.hpp
@@ -25,8 +25,6 @@
 
 #include <type_traits>
 
-#include <cuda_runtime_api.h>
-
 namespace cudf {
 
 /**
@@ -541,14 +539,3 @@ struct is_convertible<cudf::detail::timestamp<Duration1>, cudf::detail::timestam
 };
 
 }  // namespace cudf
-
-#if (CUDART_VERSION == 10000)
-namespace std {
-// This specialization is required due to an nvcc bug. This works in 10.1.
-// We can remove this once the build pipeline removes 10.0.
-template <typename Duration>
-struct is_trivially_copyable<cudf::detail::timestamp<Duration>>
-  : std::is_trivially_copyable<cudf::detail::time_point<Duration>> {
-};
-}  // namespace std
-#endif
