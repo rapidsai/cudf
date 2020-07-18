7d3556c065e45f95f6514262e3a4d66dfe9697cd
galipremsagar <sagarprem75@gmail.com>
2020-07-17 19:45:23 -0500

    remove redundant Series._mimic_inplace

--- python/cudf/cudf/core/series.py
+++ python/cudf/cudf/core/series.py
@@ -1810,15 +1810,6 @@ class Series(Frame, Serializable):
 
         return self._mimic_inplace(result, inplace=inplace)
 
-    def _mimic_inplace(self, result, inplace=False):
-        if inplace:
-            self._column._mimic_inplace(result._column, inplace=True)
-            self._index = as_index(result.index)
-            self._size = len(self._index)
-            self.name = result.name
-        else:
-            return result
-
     def fill(self, fill_value, begin=0, end=-1, inplace=False):
         return self._fill([fill_value], begin, end, inplace)
 
