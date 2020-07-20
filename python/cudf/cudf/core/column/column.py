50505b718d650c723148b511d8089e9d79529b13
brandon-b-miller <brmiller@nvidia.com>
2020-07-20 07:22:07 -0700

    fix list tests

--- python/cudf/cudf/core/column/column.py
+++ python/cudf/cudf/core/column/column.py
@@ -121,7 +121,7 @@ class ColumnBase(Column, Serializable):
     def __len__(self):
         return self.size
 
-    def to_pandas(self, index=None, **kwargs):
+    def to_pandas(self, index=None, nullable_pd_dtype=False, **kwargs):
         pd_series = self.to_arrow().to_pandas(**kwargs)
         if index is not None:
             pd_series.index = index
