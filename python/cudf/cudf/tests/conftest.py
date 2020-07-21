f4dd3de79aec8f0b05b70d47564b2f9b40571bf5
brandon-b-miller <brmiller@nvidia.com>
2020-07-20 17:25:02 -0700

    do not modify conftest

--- python/cudf/cudf/tests/conftest.py
+++ python/cudf/cudf/tests/conftest.py
@@ -8,11 +8,3 @@ import rmm  # noqa: F401
 @pytest.fixture(scope="session")
 def datadir():
     return pathlib.Path(__file__).parent / "data"
-
-
-@pytest.fixture(scope="session", autouse=True)
-def patch_df_series_to_pandas():
-    from cudf import DataFrame, Series
-
-    DataFrame.to_pandas.__defaults__ = (False,)
-    Series.to_pandas.__defaults__ = (True, False)
