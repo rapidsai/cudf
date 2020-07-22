c163150abeb85e7410f090e0561f65acc1fcefaa
Ashwin Srinath <shwina@users.noreply.github.com>
2020-07-22 10:42:53 -0400

    Doc for detail::encode

--- cpp/include/cudf/detail/transform.hpp
+++ cpp/include/cudf/detail/transform.hpp
@@ -53,6 +53,11 @@ std::pair<std::unique_ptr<rmm::device_buffer>, cudf::size_type> bools_to_mask(
   rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource(),
   cudaStream_t stream                 = 0);
 
+/**
+ * @copydoc cudf::encode
+ *
+ * @param stream CUDA stream used for device memory operations and kernel launches.
+ **/
 std::pair<std::unique_ptr<cudf::column>, std::unique_ptr<cudf::column>> encode(
   cudf::column_view const& input,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource(),
