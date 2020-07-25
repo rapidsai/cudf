0cd292c32fee3fc20b67b34aa5dd14de8ec3e8cc
galipremsagar <sagarprem75@gmail.com>
2020-07-25 09:17:24 -0700

    add latest dask and distributed install in run_test.sh

--- conda/recipes/dask-cudf/run_test.sh
+++ conda/recipes/dask-cudf/run_test.sh
@@ -1,5 +1,13 @@
-# Install the master version of dask, distributed, and streamz
+#!/bin/bash
 
+set -e
+
+# Logger function for build status output
+function logger() {
+  echo -e "\n>>>> $@\n"
+}
+
+# Install the master version of dask and distributed
 pip install "git+https://github.com/dask/distributed.git" --upgrade --no-deps
 
 pip install "git+https://github.com/dask/dask.git" --upgrade --no-deps
