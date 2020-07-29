f788e140eec7347c3b35326646cf22d0db319a36
galipremsagar <sagarprem75@gmail.com>
2020-07-29 10:57:01 -0700

    add to type-cast tests

--- python/cudf/cudf/tests/test_timedelta.py
+++ python/cudf/cudf/tests/test_timedelta.py
@@ -34,3 +34,27 @@ def test_timedelta_series_create(data, dtype):
     gsr = cudf.Series(data, dtype=dtype)
 
     assert_eq(psr, gsr)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        [1000000, 200000, 3000000],
+        [12, 12, 22, 343, 4353534, 435342],
+        [0.3534, 12, 22, 343, 43.53534, 4353.42],
+        cp.asarray([10, 20, 30, 100]),
+    ],
+)
+@pytest.mark.parametrize("dtype", dtypeutils.TIMEDELTA_TYPES)
+@pytest.mark.parametrize("cast_dtype", ["int64", "category", "object"])
+def test_timedelta_typecast(data, dtype, cast_dtype):
+    if dtype not in ("timedelta64[ns]"):
+        pytest.skip(
+            "Bug in pandas" "https://github.com/pandas-dev/pandas/issues/35465"
+        )
+    psr = pd.Series(
+        cp.asnumpy(data) if isinstance(data, cp.ndarray) else data, dtype=dtype
+    )
+    gsr = cudf.Series(data, dtype=dtype)
+
+    assert_eq(psr.astype(cast_dtype), gsr.astype(cast_dtype))
