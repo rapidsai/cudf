348a4c380fd1a9fb53e8eb35dde16be4a8458d15
galipremsagar <sagarprem75@gmail.com>
2020-07-29 19:13:16 -0700

    add ops tests between duration types

--- python/cudf/cudf/tests/test_timedelta.py
+++ python/cudf/cudf/tests/test_timedelta.py
@@ -155,3 +155,105 @@ def test_timedelta_series_to_pandas(data, dtype):
     actual = gsr.to_pandas()
 
     assert_eq(expected, actual)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        [1000000, 200000, 3000000],
+        [1000000, 200000, None],
+        [],
+        [None],
+        [None, None, None, None, None],
+        [12, 12, 22, 343, 4353534, 435342],
+        np.array([10, 20, 30, None, 100]),
+        cp.asarray([10, 20, 30, 100]),
+    ],
+)
+@pytest.mark.parametrize("dtype", dtypeutils.TIMEDELTA_TYPES)
+@pytest.mark.parametrize(
+    "ops", ["add", "radd", "sub", "rsub", "floordiv", "truediv"]
+)
+def test_timedelta_basic_ops(data, dtype, ops):
+    gsr = cudf.Series(data, dtype=dtype)
+    psr = gsr.to_pandas(nullable_pd_dtype=True)
+
+    print("act", psr)
+    expected = getattr(psr, ops)(psr)
+    actual = getattr(gsr, ops)(gsr)
+    if expected.dtype in cudf.utils.dtypes.cudf_dtypes_to_pandas_dtypes:
+        expected = expected.astype(
+            cudf.utils.dtypes.cudf_dtypes_to_pandas_dtypes[expected.dtype]
+        )
+
+    if ops == "floordiv":
+        expected[actual.isna().to_pandas()] = pd.NA
+
+    assert_eq(expected, actual, nullable_pd_dtype=True)
+
+
+@pytest.mark.parametrize(
+    "data,other",
+    [
+        ([1000000, 200000, 3000000], [1000000, 200000, 3000000]),
+        ([1000000, 200000, None], [1000000, 200000, None]),
+        ([], []),
+        ([None], [None]),
+        ([None, None, None, None, None], [None, None, None, None, None]),
+        (
+            [12, 12, 22, 343, 4353534, 435342],
+            [12, 12, 22, 343, 4353534, 435342],
+        ),
+        (np.array([10, 20, 30, None, 100]), np.array([10, 20, 30, None, 100])),
+        (cp.asarray([10, 20, 30, 100]), cp.asarray([10, 20, 30, 100])),
+        ([1000000, 200000, 3000000], [200000, 34543, 3000000]),
+        ([1000000, 200000, None], [1000000, 200000, 3000000]),
+        ([None], [1]),
+        (
+            [12, 12, 22, 343, 4353534, 435342],
+            [None, 1, 220, 3, 34, 4353423287],
+        ),
+        (np.array([10, 20, 30, None, 100]), np.array([10, 20, 30, None, 100])),
+        (cp.asarray([10, 20, 30, 100]), cp.asarray([10, 20, 30, 100])),
+    ],
+)
+@pytest.mark.parametrize("dtype", dtypeutils.TIMEDELTA_TYPES)
+@pytest.mark.parametrize(
+    "ops",
+    [
+        "eq",
+        "ne",
+        "lt",
+        "gt",
+        "le",
+        "ge",
+        "add",
+        "radd",
+        "sub",
+        "rsub",
+        "floordiv",
+    ],
+)
+def test_timedelta_ops_misc_inputs(data, other, dtype, ops):
+    gsr = cudf.Series(data, dtype=dtype)
+    other_gsr = cudf.Series(other, dtype=dtype)
+
+    psr = gsr.to_pandas(nullable_pd_dtype=True)
+    other_psr = other_gsr.to_pandas(nullable_pd_dtype=True)
+
+    expected = getattr(psr, ops)(other_psr)
+    actual = getattr(gsr, ops)(other_gsr)
+    if ops in ("eq", "lt", "gt", "le", "ge"):
+        actual = actual.fillna(False)
+    elif ops == "ne":
+        actual = actual.fillna(True)
+
+    if expected.dtype in cudf.utils.dtypes.cudf_dtypes_to_pandas_dtypes:
+        expected = expected.astype(
+            cudf.utils.dtypes.cudf_dtypes_to_pandas_dtypes[expected.dtype]
+        )
+
+    if ops == "floordiv":
+        expected[actual.isna().to_pandas()] = pd.NA
+
+    assert_eq(expected, actual, nullable_pd_dtype=True)
