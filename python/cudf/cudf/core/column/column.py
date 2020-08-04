f69cc0d9e5895f426e2e9c52e0a87822c8268917
Ashwin Srinath <shwina@users.noreply.github.com>
2020-08-04 13:47:40 -0400

    Check first for lists in astype()

--- python/cudf/cudf/core/column/column.py
+++ python/cudf/cudf/core/column/column.py
@@ -880,15 +880,15 @@ class ColumnBase(Column, Serializable):
     def astype(self, dtype, **kwargs):
         if is_categorical_dtype(dtype):
             return self.as_categorical_column(dtype, **kwargs)
-        elif np.issubdtype(dtype, np.datetime64):
-            return self.as_datetime_column(dtype, **kwargs)
-        elif pd.api.types.pandas_dtype(dtype).type in (np.str_, np.object_):
-            return self.as_string_column(dtype, **kwargs)
         elif is_list_dtype(dtype):
             if not is_list_dtype(self.dtype):
                 raise ValueError(f"Cannot cast list column to {dtype}")
             else:
                 return self
+        elif np.issubdtype(dtype, np.datetime64):
+            return self.as_datetime_column(dtype, **kwargs)
+        elif pd.api.types.pandas_dtype(dtype).type in (np.str_, np.object_):
+            return self.as_string_column(dtype, **kwargs)
         else:
             return self.as_numerical_column(dtype, **kwargs)
 
