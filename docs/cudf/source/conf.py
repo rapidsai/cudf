a15dc4ca6e5bc1b3d3163512fd54ec2281a9812b
galipremsagar <sagarprem75@gmail.com>
2020-08-05 11:28:14 -0700

    change copyright year

--- docs/cudf/source/conf.py
+++ docs/cudf/source/conf.py
@@ -66,7 +66,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "cudf"
-copyright = "2018, NVIDIA"
+copyright = "2020, NVIDIA"
 author = "NVIDIA"
 
 # The version info for the project you're documenting, acts as replacement for
