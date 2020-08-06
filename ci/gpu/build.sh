6ffc480a08ae9725f57e5b22a91d623d59ab5dbb
John Kirkham <jakirkham@gmail.com>
2020-08-06 09:07:40 -0700

    Explicitly require `dask-cuda` on CI as well

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -67,6 +67,7 @@ fi
 conda install "rmm=$MINOR_VERSION.*" "cudatoolkit=$CUDA_REL" \
               "rapids-build-env=$MINOR_VERSION.*" \
               "rapids-notebook-env=$MINOR_VERSION.*" \
+              "dask-cuda=${MINOR_VERSION}" \
               "ucx-py=${MINOR_VERSION}" \
 
 # https://docs.rapids.ai/maintainers/depmgmt/
