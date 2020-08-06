fb4ccd98e3872a2053f560e83ace72f61b238e1b
Caleb Winston <calebhwin@gmail.com>
2020-08-06 14:28:22 -0700

    Undo

--- cpp/src/comms/ipc/ipc.cpp
+++ cpp/src/comms/ipc/ipc.cpp
@@ -12,8 +12,9 @@ arrow::Result<std::unique_ptr<arrow::ipc::Message>> CudaMessageReader::ReadNextM
     host_schema_reader_ = nullptr;
     if (message.ok() && *message != nullptr) { return std::move(message); }
   }
-  auto message = arrow::ipc::ReadMessage(stream_);
-  if (!message.ok()) { return message; }
+  std::unique_ptr<arrow::ipc::Message> message;
+  auto status = arrow::cuda::ReadMessage(stream_, arrow::default_memory_pool(), &message);
+  if (!status.ok()) { return status; }
   return std::move(message);
 }
 
