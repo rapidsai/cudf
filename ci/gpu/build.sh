713a2691f9523aeafccc6dd23cc8e32e49955d2a
John Kirkham <jakirkham@gmail.com>
2020-08-05 21:16:47 -0700

    Explicitly require `ucx-py` on CI

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -66,7 +66,8 @@ fi
 
 conda install "rmm=$MINOR_VERSION.*" "cudatoolkit=$CUDA_REL" \
               "rapids-build-env=$MINOR_VERSION.*" \
-              "rapids-notebook-env=$MINOR_VERSION.*"
+              "rapids-notebook-env=$MINOR_VERSION.*" \
+              "ucx-py=${MINOR_VERSION}" \
 
 # https://docs.rapids.ai/maintainers/depmgmt/
 # conda remove -f rapids-build-env rapids-notebook-env
