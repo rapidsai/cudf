fc432e68485872de0a1691539855a15e38bf8b52
galipremsagar <sagarprem75@gmail.com>
2020-08-10 14:42:46 -0500

    Letting CUDA insufficient driver error propagate to user

--- python/cudf/cudf/utils/gpu_utils.py
+++ python/cudf/cudf/utils/gpu_utils.py
@@ -24,7 +24,11 @@ def validate_setup(check_dask=True):
 
     try:
         gpus_count = getDeviceCount()
-    except CUDARuntimeError:
+    except CUDARuntimeError as e:
+        if e.status == 35:
+            # status = 35 Indicates CUDA driver
+            # is older than CUDA runtime library
+            raise e
         # If there is no GPU detected, set `gpus_count` to -1
         gpus_count = -1
 
