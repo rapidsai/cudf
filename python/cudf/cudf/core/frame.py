228134ddf4169967d52f5af6e00bec3e18dbec56
Ram (Ramakrishna Prabhu) <ramakrishnap@nvidia.com>
2020-08-21 13:29:57 -0500

    doc

--- python/cudf/cudf/core/frame.py
+++ python/cudf/cudf/core/frame.py
@@ -1942,6 +1942,10 @@ class Frame(libcudf.table.Table):
         else:
             result = cudf_category_frame
 
+        # In a scenarion where column is of type list/other non
+        # pandas types, there will be no pandas metadata associated with
+        # given arrow table as those types can only originate from
+        # arrow.
         if pandas_dtypes:
             for name in result._data.names:
                 if pandas_dtypes[name] == "categorical":
