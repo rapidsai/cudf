992a03ced17f5e0cdb6e44cc86506cf5aacbcff2
Caleb Winston <calebhwin@gmail.com>
2020-09-02 14:49:23 -0700

    Clean up filter application code

--- python/cudf/cudf/utils/filterutils.py
+++ python/cudf/cudf/utils/filterutils.py
@@ -4,32 +4,23 @@ import pandas as pd
 
 
 def _apply_filter_bool_eq(val, col_stats):
-    if col_stats["kind"] == 0:
+    if "true_count" in col_stats and "false_count" in col_stats:
         if val is True:
-            if (
-                "true_count" in col_stats and col_stats["true_count"] == 0
-            ) or (
-                "false_count" in col_stats
-                and col_stats["false_count"] == col_stats["number_of_values"]
+            if (col_stats["true_count"] == 0) or (
+                col_stats["false_count"] == col_stats["number_of_values"]
             ):
                 return False
         elif val is False:
-            if (
-                "false_count" in col_stats and col_stats["false_count"] == 0
-            ) or (
-                "true_count" in col_stats
-                and col_stats["true_count"] == col_stats["number_of_values"]
+            if (col_stats["false_count"] == 0) or (
+                col_stats["true_count"] == col_stats["number_of_values"]
             ):
                 return False
     return True
 
 
 def _apply_filter_not_eq(val, col_stats):
-    return (
-        ("minimum" in col_stats and val < col_stats["minimum"])
-        or ("lower_bound" in col_stats and val < col_stats["lower_bound"])
-        or ("maximum" in col_stats and val > col_stats["maximum"])
-        or ("upper_bound" in col_stats and val > col_stats["upper_bound"])
+    return ("minimum" in col_stats and val < col_stats["minimum"]) or (
+        "maximum" in col_stats and val > col_stats["maximum"]
     )
 
 
@@ -42,81 +33,57 @@ def _apply_predicate(col, op, val, col_stats):
             )
         )
 
+    has_min = "minimum" in col_stats
+    has_max = "maximum" in col_stats
+    has_sum = "sum" in col_stats
+    col_min = col_stats["minimum"]
+    col_max = col_stats["maximum"]
+    col_sum = col_stats["maximum"]
+
     # Apply operator
     if op == "=" or op == "==":
         if _apply_filter_not_eq(val, col_stats):
             return False
-        if (
-            "has_null" in col_stats
-            and pd.isnull(val)
-            and col_stats["has_null"]
-        ):
+        if pd.isnull(val) and col_stats["has_null"]:
             return False
         if not _apply_filter_bool_eq(val, col_stats):
             return False
     elif op == "!=":
-        if (
-            "minimum" in col_stats
-            and "maximum" in col_stats
-            and val == col_stats["minimum"]
-            and val == col_stats["maximum"]
-        ):
+        if has_min and has_max and val == col_min and val == col_max:
             return False
         if _apply_filter_bool_eq(val, col_stats):
             return False
-    elif (
-        op == "<" and "minimum" in col_stats and val <= col_stats["minimum"]
-    ) or (
-        op == "<=" and "minimum" in col_stats and val < col_stats["minimum"]
+    elif has_min and (
+        (op == "<" and val <= col_min) or (op == "<=" and val < col_min)
     ):
         return False
-    elif op == ">":
-        if "maximum" in col_stats and val >= col_stats["maximum"]:
-            return False
-        if (
-            "sum" in col_stats
-            and "minimum" in col_stats
-            and col_stats["minimum"] >= 0
-            and col_stats["sum"] <= val
-        ) or (
-            "sum" in col_stats
-            and "minimum" in col_stats
-            and col_stats["maximum"] <= 0
-            and col_stats["sum"] >= val
+    elif has_max and (
+        (op == ">" and val >= col_max) or (op == ">=" and val > col_max)
+    ):
+        return False
+    elif has_sum:
+        if op == ">" and (
+            (has_min and col_min >= 0 and col_sum <= val)
+            or (has_max and col_max <= 0 and col_sum >= val)
         ):
             return False
-    elif op == ">=":
-        if "maximum" in col_stats and val > col_stats["maximum"]:
-            return False
-        if (
-            "sum" in col_stats
-            and "minimum" in col_stats
-            and col_stats["minimum"] >= 0
-            and col_stats["sum"] < val
-        ) or (
-            "sum" in col_stats
-            and "maximum" in col_stats
-            and col_stats["maximum"] <= 0
-            and col_stats["sum"] > val
+        elif op == ">=" and (
+            (has_min and col_min >= 0 and col_sum < val)
+            or (has_max and col_max <= 0 and col_sum > val)
         ):
             return False
     elif op == "in":
-        if ("maximum" in col_stats and col_stats["maximum"] < min(val)) or (
-            "minimum" in col_stats and col_stats["minimum"] > max(val)
+        if (has_max and col_max < min(val)) or (
+            has_min and col_min > max(val)
         ):
             return False
         if all([_apply_filter_not_eq(elem, col_stats) for elem in val]):
             return False
     elif (
         op == "not in"
-        and "minimum" in col_stats
-        and "maximum" in col_stats
-        and any(
-            [
-                elem >= col_stats["minimum"] and elem <= col_stats["maximum"]
-                for elem in val
-            ]
-        )
+        and has_min
+        and has_max
+        and any([elem >= col_min and elem <= col_max for elem in val])
         # TODO: Change this to only accept val is range or date range
     ):
         # return False if any elem in val == min == max
@@ -130,18 +97,11 @@ def _apply_predicate(col, op, val, col_stats):
 
 def _apply_filters(filters, stats):
     for conjunction in filters:
-        res = True
-        for col, op, val in conjunction:
-            # Get stats
-            col_stats = stats[col]
-            if "lower_bound" in col_stats:
-                col_stats["minimum"] = col_stats["lower_bound"]
-            if "upper_bound" in col_stats:
-                col_stats["maximum"] = col_stats["upper_bound"]
-
-            # Apply operators
-            if not _apply_predicate(col, op, val, col_stats):
-                res = False
-        if res:
+        if all(
+            [
+                _apply_predicate(col, op, val, stats[col])
+                for col, op, val in conjunction
+            ]
+        ):
             return True
     return False
