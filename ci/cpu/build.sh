4b070b3df8f523e0f05c2a509f301f259091de0d
ptaylor <paul.e.taylor@me.com>
2020-09-03 15:08:00 -0700

    remove conda clean, changes to cpu/build.sh

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -41,11 +41,6 @@ gcc --version
 g++ --version
 conda list
 
-# https://docs.rapids.ai/maintainers/depmgmt/
-conda remove --force rapids-build-env rapids-notebook-env || true
-conda install "pyarrow=1.0.1" "arrow-cpp=1.0.1" "arrow-cpp-proc=*=cuda"
-conda clean --all
-
 # FIX Added to deal with Anancoda SSL verification issues during conda builds
 conda config --set ssl_verify False
 
