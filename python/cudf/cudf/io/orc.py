ccdad5c69f18548bcfd6d200631a89dc6420c576
Caleb Winston <calebhwin@gmail.com>
2020-09-04 10:29:40 -0700

    Make _filter_stripes parameters optional

--- python/cudf/cudf/io/orc.py
+++ python/cudf/cudf/io/orc.py
@@ -148,7 +148,7 @@ def read_orc_statistics(
     return file_statistics, stripes_statistics
 
 
-def _filter_stripes(filters, filepath_or_buffer, stripes, skip_rows, num_rows):
+def _filter_stripes(filters, filepath_or_buffer, stripes=None, skip_rows=None, num_rows=None):
     # Coerce filters into list of lists of tuples
     if isinstance(filters[0][0], str):
         filters = [filters]
