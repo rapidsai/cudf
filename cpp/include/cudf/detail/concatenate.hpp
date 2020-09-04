98b93fa97e4e72e5213d965fab700187b13b38d6
ptaylor <paul.e.taylor@me.com>
2020-09-03 19:11:05 -0700

    split detail/concatenate.cuh into two headers, rename from_arrow.cu -> from_arrow.cpp

--- cpp/include/cudf/detail/concatenate.hpp
+++ cpp/include/cudf/detail/concatenate.hpp
@@ -15,10 +15,8 @@
  */
 #pragma once
 
-#include <cudf/column/column_device_view.cuh>
 #include <cudf/column/column_view.hpp>
 #include <cudf/concatenate.hpp>
-#include <cudf/detail/concatenate.hpp>
 #include <cudf/table/table_view.hpp>
 
 #include <vector>
@@ -27,25 +25,24 @@ namespace cudf {
 //! Inner interfaces and implementations
 namespace detail {
 /**
- * @copydoc cudf::concatenate_masks(std::vector<column_view>
- * const&,rmm::mr::device_memory_resource*)
+ * @copydoc cudf::concatenate(std::vector<column_view> const&,rmm::mr::device_memory_resource*)
  *
  * @param stream CUDA stream used for device memory operations and kernel launches.
  */
-void concatenate_masks(rmm::device_vector<column_device_view> const& d_views,
-                       rmm::device_vector<size_t> const& d_offsets,
-                       bitmask_type* dest_mask,
-                       size_type output_size,
-                       cudaStream_t stream);
+std::unique_ptr<column> concatenate(
+  std::vector<column_view> const& columns_to_concat,
+  rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource(),
+  cudaStream_t stream                 = 0);
 
 /**
- * @copydoc cudf::concatenate_masks(std::vector<column_view> const&,bitmask_type*)
+ * @copydoc cudf::concatenate(std::vector<table_view> const&,rmm::mr::device_memory_resource*)
  *
  * @param stream CUDA stream used for device memory operations and kernel launches.
  */
-void concatenate_masks(std::vector<column_view> const& views,
-                       bitmask_type* dest_mask,
-                       cudaStream_t stream);
+std::unique_ptr<table> concatenate(
+  std::vector<table_view> const& tables_to_concat,
+  rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource(),
+  cudaStream_t stream                 = 0);
 
 }  // namespace detail
 }  // namespace cudf
