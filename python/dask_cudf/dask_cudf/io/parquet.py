3d09755b28e78fc1f82bd97932ea804937e5de52
rjzamora <rzamora217@gmail.com>
2020-09-10 16:29:31 -0700

    handle partitioned data

--- python/dask_cudf/dask_cudf/io/parquet.py
+++ python/dask_cudf/dask_cudf/io/parquet.py
@@ -102,6 +102,10 @@ class CudfEngine(ArrowEngine):
         **kwargs,
     ):
         preserve_index = False
+        if set(index_cols).issubset(set(df.columns)):
+            df.index = df[index_cols].copy(deep=False)
+            df.drop(columns=index_cols, inplace=True)
+            preserve_index = True
         if partition_on:
             md = write_to_dataset(
                 df,
@@ -114,9 +118,6 @@ class CudfEngine(ArrowEngine):
                 **kwargs,
             )
         else:
-            if set(index_cols).issubset(set(df.columns)):
-                df.index = df[index_cols].copy(deep=False)
-                df.drop(columns=index_cols, inplace=True)
             with fs.open(fs.sep.join([path, filename]), mode="wb") as out_file:
                 if not isinstance(out_file, IOBase):
                     out_file = BufferedWriter(out_file)
