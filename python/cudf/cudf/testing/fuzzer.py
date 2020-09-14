9fc0ca753a96f3c4a97b6c08c7f51baaa9faaf33
galipremsagar <sagarprem75@gmail.com>
2020-09-14 18:24:10 -0500

    change param file extension to json from xml

--- python/cudf/cudf/testing/fuzzer.py
+++ python/cudf/cudf/testing/fuzzer.py
@@ -52,13 +52,13 @@ class Fuzzer(object):
         error_file_name = datetime.datetime.now().__str__()
         if self._crash_dir:
             crash_path = os.path.join(
-                self._crash_dir, error_file_name + "_crash.xml",
+                self._crash_dir, error_file_name + "_crash.json",
             )
             crash_log_path = os.path.join(
                 self._crash_dir, error_file_name + "_crash.log",
             )
         else:
-            crash_path = error_file_name + "_crash.xml"
+            crash_path = error_file_name + "_crash.json"
             crash_log_path = error_file_name + "_crash.log"
 
         with open(crash_path, "w") as f:
