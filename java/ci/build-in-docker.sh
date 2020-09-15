45d66903e708b79d4ca1d1aaf9bceee168827d5a
gashen <gashen@nvidia.com>
2020-09-16 07:56:26 +0800

    Remove profile abiOff

--- java/ci/build-in-docker.sh
+++ java/ci/build-in-docker.sh
@@ -68,7 +68,7 @@ make -j$PARALLEL_LEVEL
 make install DESTDIR=$INSTALL_PREFIX
 
 ###### Build cudf jar ######
-BUILD_ARG="-Dmaven.repo.local=$WORKSPACE/.m2 -DskipTests=$SKIP_JAVA_TESTS -PabiOff -DPER_THREAD_DEFAULT_STREAM=$ENABLE_PTDS"
+BUILD_ARG="-Dmaven.repo.local=$WORKSPACE/.m2 -DskipTests=$SKIP_JAVA_TESTS -DPER_THREAD_DEFAULT_STREAM=$ENABLE_PTDS"
 if [ "$SIGN_FILE" == true ]; then
     # Build javadoc and sources only when SIGN_FILE is true
     BUILD_ARG="$BUILD_ARG -Prelease"
