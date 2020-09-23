87248cc05fe3197b0a839402d0f8b30a62acb57f
Ryan Lee <ryanlee@nvidia.com>
2020-09-23 18:25:19 +0000

    template the iterator

--- cpp/src/reshape/byte_cast.cu
+++ cpp/src/reshape/byte_cast.cu
@@ -17,66 +17,85 @@
 #include <cudf/column/column_device_view.cuh>
 #include <cudf/copying.hpp>
 #include <cudf/detail/nvtx/ranges.hpp>
-#include <cudf/replace.hpp>
 #include <cudf/reshape.hpp>
 #include <cudf/strings/detail/utilities.cuh>
-#include <cudf/types.hpp>
-#include <cudf/utilities/traits.hpp>
 #include <cudf/utilities/type_dispatcher.hpp>
 
 namespace cudf {
 namespace {
+/**
+ * @brief Function object for converting primitive types and string columns to lists of bytes.
+ */
 
 struct byte_list_conversion {
-  /**
-   * @brief Function object for converting primitive types and string columns to lists of bytes,
-   * mimics Spark's cast to binary type.
-   */
+  template <typename T>
+  struct normalize_lambda : public thrust::unary_function<T, T> {
+    __host__ __device__ T operator()(T input) const
+    {
+      if (isnan(input)) {
+        return std::numeric_limits<T>::quiet_NaN();
+      } else if (T{0.0} == input) {
+        return T{0.0};
+      } else
+        return input;
+    }
+  };
+
+  template <typename T, typename Iter>
   struct flip_endianness_lambda {
     char* d_chars;
-    const char* d_data;
-    uint32_t mask;
-    __device__ void operator()(int byte_index)
-    {
-      d_chars[byte_index] = d_data[byte_index + mask - ((byte_index & mask) << 1)];
-    }
-  };
-
-  template <typename T>
-  struct normalize_lambda {
-    T* d_result;
-    const T* d_data;
+    Iter const d_data;
     __device__ void operator()(int index)
     {
-      auto e = d_data[index];
-      if (isnan(e)) {
-        d_result[index] = std::numeric_limits<T>::quiet_NaN();
-      } else if (T{0.0} == e) {
-        d_result[index] = T{0.0};
-      } else
-        d_result[index] = e;
+      T value   = d_data[index];
+      char* val = reinterpret_cast<char*>(&value);
+      for (int i = 0; i < sizeof(T); i++) {
+        d_chars[index * sizeof(T) + i] = val[sizeof(T) - 1 - i];
+      }
     }
   };
 
-  template <typename T>
-  struct normalize_flip_endianess_lambda {
-    char* d_chars;
-    const T* d_data;
-    size_t size;
-    __device__ void operator()(int index)
-    {
-      T normal           = normalize(d_data[index]);
-      char* normal_bytes = reinterpret_cast<char*>(&normal);
-      for (int i = 0; i < size; i++) { d_chars[index * size + i] = normal_bytes[size - 1 - i]; }
+  template <typename T, typename Iter>
+  std::unique_ptr<column> process(column_view const& input_column,
+                                  Iter input_begin,
+                                  flip_endianness configuration,
+                                  rmm::mr::device_memory_resource* mr,
+                                  cudaStream_t stream) const
+  {
+    auto byte_column = make_numeric_column(data_type{type_id::UINT8},
+                                           input_column.size() * cudf::size_of(input_column.type()),
+                                           mask_state::UNALLOCATED,
+                                           stream,
+                                           mr);
+
+    if (configuration == flip_endianness::YES) {
+      thrust::for_each(
+        rmm::exec_policy(stream)->on(stream),
+        thrust::make_counting_iterator(0),
+        thrust::make_counting_iterator(input_column.size()),
+        flip_endianness_lambda<T, Iter>{
+          reinterpret_cast<char*>(byte_column->mutable_view().data<T>()), input_begin});
+    } else {
+      thrust::copy(rmm::exec_policy(stream)->on(stream),
+                   input_begin,
+                   input_begin + input_column.size(),
+                   byte_column->mutable_view().begin<T>());
     }
 
-    __device__ T normalize(T in)
-    {
-      if (isnan(in)) { return std::numeric_limits<T>::quiet_NaN(); }
-      if (T{0.0} == in) { return T{0.0}; }
-      return in;
-    }
-  };
+    auto begin          = thrust::make_constant_iterator(cudf::size_of(input_column.type()));
+    auto offsets_column = cudf::strings::detail::make_offsets_child_column(
+      begin, begin + input_column.size(), mr, stream);
+
+    rmm::device_buffer null_mask = copy_bitmask(input_column, stream, mr);
+
+    return make_lists_column(input_column.size(),
+                             std::move(offsets_column),
+                             std::move(byte_column),
+                             input_column.null_count(),
+                             std::move(null_mask),
+                             stream,
+                             mr);
+  }
 
   template <typename T,
             std::enable_if_t<!std::is_integral<T>::value and !is_floating_point<T>()>* = nullptr>
@@ -94,41 +113,12 @@ struct byte_list_conversion {
                                      rmm::mr::device_memory_resource* mr,
                                      cudaStream_t stream) const
   {
-    auto byte_column = make_numeric_column(data_type{type_id::UINT8},
-                                           input_column.size() * cudf::size_of(input_column.type()),
-                                           mask_state::UNALLOCATED,
-                                           stream,
-                                           mr);
-
-    if (configuration == flip_endianness::YES) {
-      thrust::for_each(rmm::exec_policy(stream)->on(stream),
-                       thrust::make_counting_iterator(0),
-                       thrust::make_counting_iterator(input_column.size()),
-                       normalize_flip_endianess_lambda<T>{
-                         reinterpret_cast<char*>(byte_column->mutable_view().data<T>()),
-                         input_column.data<T>(),
-                         cudf::size_of(input_column.type())});
-    } else {
-      thrust::for_each(
-        rmm::exec_policy(stream)->on(stream),
-        thrust::make_counting_iterator(0),
-        thrust::make_counting_iterator(input_column.size()),
-        normalize_lambda<T>{byte_column->mutable_view().data<T>(), input_column.data<T>()});
-    }
-
-    auto begin          = thrust::make_constant_iterator(cudf::size_of(input_column.type()));
-    auto offsets_column = cudf::strings::detail::make_offsets_child_column(
-      begin, begin + input_column.size(), mr, stream);
-
-    rmm::device_buffer null_mask = copy_bitmask(input_column, stream, mr);
-
-    return make_lists_column(input_column.size(),
-                             std::move(offsets_column),
-                             std::move(byte_column),
-                             input_column.null_count(),
-                             std::move(null_mask),
-                             stream,
-                             mr);
+    return process<T, thrust::transform_iterator<normalize_lambda<T>, T const*>>(
+      input_column,
+      thrust::make_transform_iterator(input_column.data<T>(), normalize_lambda<T>{}),
+      configuration,
+      mr,
+      stream);
   }
 
   template <typename T, std::enable_if_t<std::is_integral<T>::value>* = nullptr>
@@ -137,39 +127,7 @@ struct byte_list_conversion {
                                      rmm::mr::device_memory_resource* mr,
                                      cudaStream_t stream) const
   {
-    size_type num_output_elements = input_column.size() * cudf::size_of(input_column.type());
-
-    auto byte_column = make_numeric_column(
-      data_type{type_id::UINT8}, num_output_elements, mask_state::UNALLOCATED, stream, mr);
-    auto d_chars = reinterpret_cast<char*>(byte_column->mutable_view().data<T>());
-
-    if (configuration == flip_endianness::YES) {
-      uint32_t mask = cudf::size_of(input_column.type()) - 1;
-      thrust::for_each(rmm::exec_policy(stream)->on(stream),
-                       thrust::make_counting_iterator(0),
-                       thrust::make_counting_iterator(num_output_elements),
-                       flip_endianness_lambda{
-                         d_chars, reinterpret_cast<const char*>(input_column.data<T>()), mask});
-    } else {
-      thrust::copy_n(rmm::exec_policy(stream)->on(stream),
-                     reinterpret_cast<const char*>(input_column.data<T>()),
-                     num_output_elements,
-                     d_chars);
-    }
-
-    auto begin          = thrust::make_constant_iterator(cudf::size_of(input_column.type()));
-    auto offsets_column = cudf::strings::detail::make_offsets_child_column(
-      begin, begin + input_column.size(), mr, stream);
-
-    rmm::device_buffer null_mask = copy_bitmask(input_column, stream, mr);
-
-    return make_lists_column(input_column.size(),
-                             std::move(offsets_column),
-                             std::move(byte_column),
-                             input_column.null_count(),
-                             std::move(null_mask),
-                             stream,
-                             mr);
+    return process<T, T const*>(input_column, input_column.begin<T>(), configuration, mr, stream);
   }
 };
 
