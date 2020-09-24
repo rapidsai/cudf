63026673d947622f7a43ea4cc2ef97242205fcf8
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2020-09-24 15:39:02 -0500

    review changes

--- python/cudf/cudf/benchmarks/bench_cudf_io.py
+++ python/cudf/cudf/benchmarks/bench_cudf_io.py
@@ -3,22 +3,37 @@
 import pytest
 import cudf
 import glob
+import io
 
-@pytest.mark.parametrize("file_path", glob.glob("cudf/benchmarks//cuio_data/datasets/avro_*"))
-def bench_avro(benchmark, file_path):
+
+@pytest.mark.parametrize(
+    "file_path", glob.glob("cudf/benchmarks/cuio_data/datasets/avro_*")
+)
+def bench_avro(benchmark, file_path, use_file):
+
+    if use_file == "False":
+        with open(file_path, "rb") as f:
+            file_path = io.BytesIO(f.read())
     benchmark(cudf.read_avro, file_path)
 
 
-@pytest.mark.parametrize("file_path", glob.glob("cudf/benchmarks//cuio_data/datasets/json_*"))
-def bench_json(benchmark, file_path):
+@pytest.mark.parametrize(
+    "file_path", glob.glob("cudf/benchmarks/cuio_data/datasets/json_*")
+)
+def bench_json(benchmark, file_path, use_file):
     if "bz2" in file_path:
-        compression="bz2"
+        compression = "bz2"
     elif "gzip" in file_path:
-        compression="gzip"
+        compression = "gzip"
     elif "infer" in file_path:
-        compression="infer"
+        compression = "infer"
     else:
         raise TypeError("Unsupported compression type")
+
+    if use_file == "False":
+        with open(file_path, "rb") as f:
+            file_path = io.BytesIO(f.read())
+
     benchmark(
         cudf.read_json,
         file_path,
