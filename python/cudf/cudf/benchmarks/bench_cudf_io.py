76da81fdd45e09add17770486f7796480d31e3af
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2020-09-24 13:02:13 -0500

    shell file to get data and benchmark change

--- python/cudf/cudf/benchmarks/bench_cudf_io.py
+++ python/cudf/cudf/benchmarks/bench_cudf_io.py
@@ -1,75 +1,24 @@
 # Copyright (c) 2018-2020, NVIDIA CORPORATION.
 
-from cudf.tests import dataset_generator as dg
-
 import pytest
 import cudf
-import numpy as np
-import pandavro as pav
+import glob
 
-common_dtypes_meta = [
-    [np.dtype("bool"), 0.0, 250],
-    [np.dtype("int8"), 0.1, 481],
-    [np.dtype("int16"), 0.15, 171],
-    [np.dtype("int32"), 0.2, 480],
-    [np.dtype("int64"), 0.3, 177],
-    [np.dtype("uint8"), 0.14, 418],
-    [np.dtype("uint16"), 0.12, 48],
-    [np.dtype("uint32"), 0.17, 177],
-    [np.dtype("float32"), 0.17, 25],
-    [np.dtype("float64"), 0.18, 104],
-    [np.dtype("str"), 0.23, 49],
-    [np.dtype("datetime64[s]"), 0, 178],
-    [np.dtype("datetime64[ms]"), 0, 179],
-    [np.dtype("datetime64[us]"), 0, 180],
-    [np.dtype("datetime64[ns]"), 0, 181],
-]
+@pytest.mark.parametrize(file_path, glob.glob("cudf/benchmarks/datasets/datasets/avro_*"))
+def bench_avro(benchmark, ):
+    benchmark(cudf.read_avro, file_path)
 
 
-def get_random_dataframe(
-    dtype_meta, size_in_bytes=524_288_000, num_columns=64, seed=0
-):
-    dtype, _, _ = dtype_meta
-    item_size = dtype.itemsize
-    if dtype.kind in ("O", "U"):
-        # string sizes are limited to 4 characters
-        item_size = 4
-
-    num_rows = size_in_bytes // num_columns // item_size
-
-    dtype_metas = [dtype_meta] * num_rows
-
-    return dg.rand_dataframe(dtype_metas, num_rows, seed).to_pandas()
-
-
-@pytest.mark.parametrize("dtype_meta", common_dtypes_meta)
-def bench_avro(dtype_meta, tmpdir, benchmark):
-    fname = tmpdir.mkdir("cudf_benchmark").join("avro_sample")
-
-    # pandavro doesn't support timedelta, so skipping that dtype
-    df = get_random_dataframe(dtype_meta)
-    pav.to_avro(str(fname), df)
-
-    benchmark(cudf.read_avro, fname)
-
-
-@pytest.mark.parametrize(
-    "dtype_meta",
-    common_dtypes_meta
-    + [
-        [np.dtype("timedelta64[s]"), 0, 187],
-        [np.dtype("timedelta64[ms]"), 0, 187],
-        [np.dtype("timedelta64[us]"), 0, 187],
-        [np.dtype("timedelta64[ns]"), 0, 187],
-    ],
-)
-@pytest.mark.parametrize("compression", ["infer", "gzip", "bz2"])
-def bench_json(dtype_meta, compression, tmpdir, benchmark):
-    fname = tmpdir.mkdir("cudf_benchmark").join("json_sample")
-
-    df = get_random_dataframe(dtype_meta)
-    df.to_json(fname, compression=compression, lines=True, orient="records")
-
+@pytest.mark.parametrize(file_path, glob.glob("cudf/benchmarks/datasets/datasets/json_*"))
+def bench_json(benchmark, file_path):
+    if "bz2" in file_path:
+        compression="bz2"
+    elif "gzip" in file_path:
+        compression="gzip"
+    elif "infer" in file_path:
+        compression="infer"
+    else:
+        raise TypeError("Unsupported compression type")
     benchmark(
         cudf.read_json,
         fname,
---
+++ python/cudf/cudf/benchmarks/get_dataset.py
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# Update this to add/remove/change a dataset, using the following format:
+#
+#  comment about the dataset
+#  dataset download URL
+#  destination dir to untar to
+#  blank line separator
+#
+# FIXME: some test data needs to be extracted to "benchmarks", which is
+# confusing now that there's dedicated datasets for benchmarks.
+CUIO_BENCHMARK_DATASET_DATA="
+# 10GB File
+https://github.com/rapidsai/cudf/files/5044005/orc_mismatch.orc.zip
+cudf/benchmarks/cuio_data/
+# ~14s download
+"
+
+################################################################################
+# Do not change the script below this line if only adding/updating a dataset
+
+NUMARGS=$#
+ARGS=$*
+function hasArg {
+    (( ${NUMARGS} != 0 )) && (echo " ${ARGS} " | grep -q " $1 ")
+}
+
+if hasArg -h || hasArg --help; then
+    echo "$0 [--subset | --benchmark]"
+    exit 0
+fi
+
+# Select the datasets to install
+# Update this as and when new datasets and requirement arrive
+if hasArg "--cuio_benchmark"; then
+    DATASET_DATA="${CUIO_BENCHMARK_DATASET_DATA}"
+else
+    DATASET_DATA="${CUIO_BENCHMARK_DATASET_DATA}"
+fi
+
+URLS=($(echo "$DATASET_DATA"|awk '{if (NR%4 == 3) print $0}'))  # extract 3rd fields to a bash array
+DESTDIRS=($(echo "$DATASET_DATA"|awk '{if (NR%4 == 0) print $0}'))  # extract 4th fields to a bash array
+
+echo Downloading ...
+
+# Download all tarfiles to a tmp dir
+rm -rf tmp
+mkdir tmp
+cd tmp
+for url in ${URLS[*]}; do
+   time wget --progress=dot:giga ${url}
+done
+cd ..
+
+# Setup the destination dirs, removing any existing ones first!
+for index in ${!DESTDIRS[*]}; do
+    rm -rf ${DESTDIRS[$index]}
+done
+for index in ${!DESTDIRS[*]}; do
+    mkdir -p ${DESTDIRS[$index]}
+done
+
+# Iterate over the arrays and untar the nth tarfile to the nth dest directory.
+# The tarfile name is derived from the download url.
+echo Decompressing ...
+for index in ${!DESTDIRS[*]}; do
+    tfname=$(basename ${URLS[$index]})
+    unzip tmp/${tfname} -d ${DESTDIRS[$index]}
+done
+
+rm -rf tmp
