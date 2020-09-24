a0c2c428d1e455a51de0bbfbd0302d64734e3b11
Ryan Lee <ryanlee@nvidia.com>
2020-09-24 00:32:34 +0000

    fix test resource file path

--- cpp/tests/reshape/byte_cast_tests.cpp
+++ cpp/tests/reshape/byte_cast_tests.cpp
@@ -15,9 +15,9 @@
  */
 
 #include <cudf/reshape.hpp>
-#include <tests/utilities/base_fixture.hpp>
-#include <tests/utilities/column_wrapper.hpp>
-#include <tests/utilities/type_lists.hpp>
+#include <cudf_test/base_fixture.hpp>
+#include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/type_lists.hpp>
 
 using namespace cudf::test;
 
