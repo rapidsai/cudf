335828e33d26a5266e0ccef2b9aeb0a0fb21f0ac
galipremsagar <sagarprem75@gmail.com>
2020-09-30 15:58:01 -0700

    remove redundant deepcopies

--- python/cudf/cudf/tests/test_replace.py
+++ python/cudf/cudf/tests/test_replace.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2020, NVIDIA CORPORATION.
-import copy
+
 
 import numpy as np
 import pandas as pd
@@ -165,33 +165,43 @@ def test_replace_strings():
 
 @pytest.mark.parametrize(
     "psr",
-    [pd.Series([0, 1, None, 2, None]), pd.Series([0, 1, np.nan, 2, np.nan])],
+    [
+        pd.Series([0, 1, None, 2, None], dtype=pd.Int8Dtype()),
+        pd.Series([0, 1, np.nan, 2, np.nan]),
+    ],
 )
 @pytest.mark.parametrize("data_dtype", NUMERIC_TYPES)
 @pytest.mark.parametrize("fill_value", [10, pd.Series([10, 20, 30, 40, 50])])
 @pytest.mark.parametrize("inplace", [True, False])
 def test_series_fillna_numerical(psr, data_dtype, fill_value, inplace):
+    test_psr = psr.copy(deep=True)
     # TODO: These tests should use Pandas' nullable int type
     # when we support a recent enough version of Pandas
     # https://pandas.pydata.org/pandas-docs/stable/user_guide/integer_na.html
-    if np.dtype(data_dtype).kind not in ("i"):
-        psr = psr.astype(data_dtype)
+    if np.dtype(data_dtype).kind not in ("f") and test_psr.dtype.kind == "i":
+        test_psr = test_psr.astype(
+            cudf.utils.dtypes.cudf_dtypes_to_pandas_dtypes[
+                np.dtype(data_dtype)
+            ]
+        )
 
-    gsr = cudf.from_pandas(psr)
+    gsr = cudf.from_pandas(test_psr)
 
     if isinstance(fill_value, pd.Series):
         fill_value_cudf = cudf.from_pandas(fill_value)
     else:
-        fill_value_cudf = copy.deepcopy(fill_value)
+        fill_value_cudf = fill_value
 
-    expected = psr.fillna(fill_value, inplace=inplace)
+    expected = test_psr.fillna(fill_value, inplace=inplace)
     actual = gsr.fillna(fill_value_cudf, inplace=inplace)
 
     if inplace:
-        expected = psr
+        expected = test_psr
         actual = gsr
 
-    assert_eq(expected, actual)
+    # TODO: Remove check_dtype when we have support
+    # to compare with pandas nullable dtypes
+    assert_eq(expected, actual, check_dtype=False)
 
 
 @pytest.mark.parametrize(
@@ -383,7 +393,7 @@ def test_fillna_dataframe(df, value, inplace):
     pdf = df.copy(deep=True)
     gdf = DataFrame.from_pandas(pdf)
 
-    fill_value_pd = copy.deepcopy(value)
+    fill_value_pd = value
     if isinstance(fill_value_pd, (pd.Series, pd.DataFrame)):
         fill_value_cudf = cudf.from_pandas(fill_value_pd)
     elif isinstance(fill_value_pd, dict):
@@ -394,7 +404,7 @@ def test_fillna_dataframe(df, value, inplace):
                 temp_val = cudf.from_pandas(temp_val)
             fill_value_cudf[key] = temp_val
     else:
-        fill_value_cudf = copy.deepcopy(value)
+        fill_value_cudf = value
 
     expect = pdf.fillna(fill_value_pd, inplace=inplace)
     got = gdf.fillna(fill_value_cudf, inplace=inplace)
