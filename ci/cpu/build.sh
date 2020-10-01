f6722f51609dcf731cad17bfa059b545f26466c5
Mike Wendt <mike.wendt@me.com>
2020-10-01 10:00:39 -0400

    FIX Set default PARALLEL_LEVEL to 4, allow override

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -7,7 +7,7 @@ set -e
 
 # Set path and build parallel level
 export PATH=/opt/conda/bin:/usr/local/cuda/bin:$PATH
-export PARALLEL_LEVEL=${PARALLEL_LEVEL:-8}
+export PARALLEL_LEVEL=${PARALLEL_LEVEL:-4}
 
 # Set home to the job's workspace
 export HOME=$WORKSPACE
--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -14,7 +14,7 @@ function hasArg {
 
 # Set path and build parallel level
 export PATH=/opt/conda/bin:/usr/local/cuda/bin:$PATH
-export PARALLEL_LEVEL=${PARALLEL_LEVEL:-8}
+export PARALLEL_LEVEL=${PARALLEL_LEVEL:-4}
 
 # Set home to the job's workspace
 export HOME=$WORKSPACE
