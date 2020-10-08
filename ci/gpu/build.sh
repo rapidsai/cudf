d1deddd5aaf64aa4961bfc72d2ae3886d183499d
Mike Wendt <mike.wendt@me.com>
2020-10-08 03:33:49 -0400

    ENH Reorder install deps and add logger msg

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -67,15 +67,18 @@ gpuci_logger "Activate conda env"
 . /opt/conda/etc/profile.d/conda.sh
 conda activate rapids
 
-gpuci_conda_retry install "rmm=$MINOR_VERSION.*" "cudatoolkit=$CUDA_REL" \
-              "rapids-build-env=$MINOR_VERSION.*" \
-              "rapids-notebook-env=$MINOR_VERSION.*" \
-              "dask-cuda=${MINOR_VERSION}" \
-              "ucx-py=${MINOR_VERSION}" \
+gpuci_logger "Install dependencies"
+gpuci_conda_retry install -y \
+                  "cudatoolkit=$CUDA_REL" \
+                  "rapids-build-env=$MINOR_VERSION.*" \
+                  "rapids-notebook-env=$MINOR_VERSION.*" \
+                  "dask-cuda=${MINOR_VERSION}" \
+                  "rmm=$MINOR_VERSION.*" \
+                  "ucx-py=${MINOR_VERSION}"
 
 # https://docs.rapids.ai/maintainers/depmgmt/
 # gpuci_conda_retry remove --force rapids-build-env rapids-notebook-env
-# gpuci_conda_retry install "your-pkg=1.0.0"
+# gpuci_conda_retry install -y "your-pkg=1.0.0"
 
 # Install the master version of dask, distributed, and streamz
 gpuci_logger "Install the master version of dask, distributed, and streamz"
