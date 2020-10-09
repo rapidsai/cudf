432657395a2f4e95c725ff134dbfbb6029afac21
Marlene <57748216+marlenezw@users.noreply.github.com>
2020-10-09 08:50:39 +0200

    Update python/cudf/cudf/core/reshape.py
    Co-authored-by: Keith Kraus <keith.j.kraus@gmail.com>

--- python/cudf/cudf/core/reshape.py
+++ python/cudf/cudf/core/reshape.py
@@ -252,7 +252,7 @@ def concat(objs, axis=0, join="outer", ignore_index=False, sort=None):
         assert typs.issubset(allowed_typs)
         if not typs.issubset(allowed_typs):
             raise TypeError(
-                "Can only concatenate Series" "and DataFrame objs when axis=1"
+                "Can only concatenate Series and DataFrame objects when axis=1"
             )
         df = cudf.DataFrame()
         _normalize_series_and_dataframe(objs, axis=axis)
