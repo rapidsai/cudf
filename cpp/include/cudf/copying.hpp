0e39bb1059822e6fde09e4690fa66ca4c58b6129
Michael Wang <michelwang0905@icloud.com>
2020-10-15 00:02:01 -0700

    Wrap around control

--- cpp/include/cudf/copying.hpp
+++ cpp/include/cudf/copying.hpp
@@ -74,6 +74,7 @@ std::unique_ptr<table> gather(
   table_view const& source_table,
   column_view const& gather_map,
   out_of_bounds_policy bounds         = out_of_bounds_policy::NULLIFY,
+  bool wrap_around                    = false,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 
 /**
