df9712d64c55a94cfbd5ed2cde8a570c26eefcb2
Ashwin Srinath <shwina@users.noreply.github.com>
2020-10-15 10:51:26 -0400

    Add scalar to typing

--- python/cudf/cudf/_typing.py
+++ python/cudf/cudf/_typing.py
@@ -1,9 +1,21 @@
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, TypeVar, Union
 
 import numpy as np
 
 if TYPE_CHECKING:
+    from pandas import Interval, Period, Timedelta, Timestamp
     from pandas.api.extensions import ExtensionDtype
+
+# Many of these are from
+# https://github.com/pandas-dev/pandas/blob/master/pandas/_typing.py
 
 Dtype = Union["ExtensionDtype", str, np.dtype]
 DtypeObj = Union["ExtensionDtype", np.dtype]
+
+# scalars
+PythonScalar = Union[str, int, float, bool]
+DatetimeLikeScalar = TypeVar(
+    "DatetimeLikeScalar", "Period", "Timestamp", "Timedelta"
+)
+PandasScalar = Union["Period", "Timestamp", "Timedelta", "Interval"]
+ScalarObj = Union[PythonScalar, PandasScalar]
