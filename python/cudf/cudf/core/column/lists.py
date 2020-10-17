899a711a0d526d65390ce533e4c3be508ce0f912
Ben Frederickson <github@benfrederickson.com>
2020-10-16 20:18:01 -0700

    Fix memory_usage calls for list columns
    memory_usage calls were throwing a ```AttributeError: 'ListDtype' object has no attribute 'itemsize'```
    exception for list columns. Fix by implementing ```__sizeof__`` for list columns

--- python/cudf/cudf/core/column/lists.py
+++ python/cudf/cudf/core/column/lists.py
@@ -69,6 +69,9 @@ class ListColumn(ColumnBase):
             pa_type, len(self), buffers, children=[elements]
         )
 
+    def __sizeof__(self):
+        return sum(child.__sizeof__() for child in self.base_children)
+
 
 class ListMethods(ColumnMethodsMixin):
     """
