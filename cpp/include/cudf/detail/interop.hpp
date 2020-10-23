17cd94fa6b6a108b73cfa10af9df8e2882aa0e54
Ram (Ramakrishna Prabhu) <42624703+rgsl888prabhu@users.noreply.github.com>
2020-10-23 13:05:02 -0500

    Add struct type support to `to_arrow` and `from_arrow` (#6430)
    * Primary changes
    
    * CHANGELOG.md
    
    * adding more tests and enabling nested naming support
    
    * minor change
    
    * doc changes
    
    * changes
    
    * Adding tests for sliced struct column
    
    * additional test cases
    
    * Update CHANGELOG.md
    
    * review changes and test cases for list slice
    
    * review changes, fixing typo
    
    * review changes
    
    * review changes

--- cpp/include/cudf/detail/interop.hpp
+++ cpp/include/cudf/detail/interop.hpp
@@ -100,7 +100,7 @@ data_type arrow_to_cudf_type(arrow::DataType const& arrow_type);
  * @param stream CUDA stream used for device memory operations and kernel launches.
  **/
 std::shared_ptr<arrow::Table> to_arrow(table_view input,
-                                       std::vector<std::string> const& column_names = {},
+                                       std::vector<column_metadata> const& metadata = {},
                                        arrow::MemoryPool* ar_mr = arrow::default_memory_pool(),
                                        cudaStream_t stream      = 0);
 
