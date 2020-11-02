29a02f5b44c0c252fe7a8b87cc9abad5026f5fef
Conor Hoekstra <codereport@outlook.com>
2020-11-02 17:45:27 -0500

    Add failing unit tests for unsigned integers

--- cpp/tests/round/round_tests.cpp
+++ cpp/tests/round/round_tests.cpp
@@ -207,8 +207,19 @@ TYPED_TEST(RoundTestsIntegerTypes, SimpleNegativeInteger)
 {
   using fw_wrapper = cudf::test::fixed_width_column_wrapper<TypeParam>;
 
-  auto const input    = fw_wrapper{-12, -135, -1454, -1455, -1500};
-  auto const expected = fw_wrapper{0, -100, -1500, -1500, -1500};
+  auto const input    = fw_wrapper{-12, -135, -1454, -1455, -1500, -140, -150, -160};
+  auto const expected = fw_wrapper{0, -100, -1500, -1500, -1500, -100, -200, -200};
+  auto const result   = cudf::round(input, -2, cudf::rounding_method::HALF_UP);
+
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, result->view());
+}
+
+TEST_F(RoundTests, SimpleNegativeIntegerWithUnsignedNumbers)
+{
+  using fw_wrapper = cudf::test::fixed_width_column_wrapper<uint32_t>;
+
+  auto const input    = fw_wrapper{12, 135, 1454, 1455, 1500, 140, 150, 160};
+  auto const expected = fw_wrapper{0, 100, 1500, 1500, 1500, 100, 200, 200};
   auto const result   = cudf::round(input, -2, cudf::rounding_method::HALF_UP);
 
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, result->view());
