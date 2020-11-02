e01ab96d1d90300b8eb32473de2b222ebef54e0c
GALI PREM SAGAR <sagarprem75@gmail.com>
2020-11-02 12:18:16 -0600

    Update `to_pandas` api docs  (#6622)
    This PR updates documentation of DataFrame.to_pandas and Series.to_pandas.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -44,6 +44,7 @@
 - PR #6612 Update JNI to new RMM cuda_stream_view API
 - PR #6579 Update scatter APIs to use reference wrapper / const scalar
 - PR #6614 Add support for conversion to Pandas nullable dtypes and fix related issue in `cudf.to_json`
+- PR #6622 Update `to_pandas` api docs
 - PR #6623 Add operator overloading to column and clean up error messages
 
 ## Bug Fixes
--- python/cudf/cudf/core/dataframe.py
+++ python/cudf/cudf/core/dataframe.py
@@ -4790,6 +4790,19 @@ class DataFrame(Frame, Serializable):
         """
         Convert to a Pandas DataFrame.
 
+        Parameters
+        ----------
+        nullable : Boolean, Default False
+            If ``nullable`` is ``True``, the resulting columns
+            in the dataframe will be having a corresponding
+            nullable Pandas dtype. If ``nullable`` is ``False``,
+            the resulting columns will either convert null
+            values to ``np.nan`` or ``None`` depending on the dtype.
+
+        Returns
+        -------
+        out : Pandas DataFrame
+
         Examples
         --------
         >>> import cudf
@@ -4802,6 +4815,36 @@ class DataFrame(Frame, Serializable):
         2  2  0
         >>> type(pdf)
         <class 'pandas.core.frame.DataFrame'>
+
+        ``nullable`` parameter can be used to control
+        whether dtype can be Pandas Nullable or not:
+
+        >>> df = cudf.DataFrame({'a': [0, None, 2], 'b': [True, False, None]})
+        >>> df
+              a      b
+        0     0   True
+        1  <NA>  False
+        2     2   <NA>
+        >>> pdf = df.to_pandas(nullable=True)
+        >>> pdf
+              a      b
+        0     0   True
+        1  <NA>  False
+        2     2   <NA>
+        >>> pdf.dtypes
+        a      Int64
+        b    boolean
+        dtype: object
+        >>> pdf = df.to_pandas(nullable=False)
+        >>> pdf
+            a      b
+        0  0.0   True
+        1  NaN  False
+        2  2.0   None
+        >>> pdf.dtypes
+        a    float64
+        b     object
+        dtype: object
         """
 
         out_data = {}
