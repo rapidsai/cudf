bb96aafca06aabe49586f2a416f1cff35a09ac64
Dave Baranec <dbaranec@nvidia.com>
2020-11-04 10:14:17 -0600

    Change implementation of extract_ordered_struct_children to use loops instead of transforms.

--- cpp/src/structs/utilities.cu
+++ cpp/src/structs/utilities.cu
@@ -37,37 +37,31 @@ std::vector<std::vector<column_view>> extract_ordered_struct_children(
                 [](column_view const& col) { return col.type().id() == type_id::STRUCT; }),
     "Encountered non-struct input to extract_ordered_struct_children");
 
+  size_type num_children = struct_cols[0].num_children();
+  size_type num_cols     = static_cast<size_type>(struct_cols.size());
+
   std::vector<std::vector<column_view>> result;
-  result.reserve(struct_cols[0].num_children());
+  result.reserve(num_children);
 
-  auto child_index = thrust::make_counting_iterator(0);
-  std::transform(child_index,
-                 child_index + struct_cols[0].num_children(),
-                 std::back_inserter(result),
-                 [&struct_cols](int i) {
-                   std::vector<column_view> children;
+  for (size_type child_index = 0; child_index < num_children; child_index++) {
+    std::vector<column_view> children;
+    children.reserve(num_cols);
+    for (size_type col_index = 0; col_index < num_cols; col_index++) {
+      structs_column_view scv(struct_cols[col_index]);
 
-                   // extract the i'th child from each input column
-                   auto col_index = thrust::make_counting_iterator(0);
-                   std::transform(
-                     col_index,
-                     col_index + struct_cols.size(),
-                     std::back_inserter(children),
-                     [&struct_cols, i](int col_index) {
-                       structs_column_view scv(struct_cols[col_index]);
+      // all inputs must have the same # of children and they must all be of the
+      // same type.
+      CUDF_EXPECTS(struct_cols[0].num_children() == scv.num_children(),
+                   "Mismatch in number of children during struct concatenate");
+      CUDF_EXPECTS(struct_cols[0].child(child_index).type() == scv.child(child_index).type(),
+                   "Mismatch in child types during struct concatenate");
+      children.push_back(scv.get_sliced_child(child_index));
+    }
 
-                       // all inputs must have the same # of children and they must all be of the
-                       // same type.
-                       CUDF_EXPECTS(struct_cols[0].num_children() == scv.num_children(),
-                                    "Mismatch in number of children during struct concatenate");
-                       CUDF_EXPECTS(struct_cols[0].child(i).type() == scv.child(i).type(),
-                                    "Mismatch in child types during struct concatenate");
-                       return scv.get_sliced_child(i);
-                     });
-                   return std::move(children);
-                 });
+    result.push_back(std::move(children));
+  }
 
-  return std::move(result);
+  return result;
 }
 
 }  // namespace detail
