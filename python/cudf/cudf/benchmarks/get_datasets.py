78c8b65ddc5b2a9fba40980558d4ee4c2a121d4a
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2020-11-04 15:32:00 -0600

    changes

--- python/cudf/cudf/benchmarks/get_datasets.py
+++ python/cudf/cudf/benchmarks/get_datasets.py
@@ -4,14 +4,16 @@ import getopt
 import sys
 import os
 import shutil
+from collections import namedtuple
 
 # Update url and dir where datasets needs to be copied
+Dataset = namedtuple("Dataset", ["url", "dir"])
 datasets = {
-    "cuio_dataset": [
+    "cuio_dataset": Dataset(
         "https://rapidsai-data.s3.us-east-2.amazonaws.com/cudf/benchmark/"
         "avro_json_datasets.zip",
         "cudf/benchmarks/cuio_data/",
-    ],
+    ),
 }
 
 
@@ -23,11 +25,11 @@ def delete_dir(path):
 
 
 def fetch_datasets(urls, dirs):
-    tmp_path = os.getcwd() + "/tmp_benchmark/"
+    tmp_path = os.path.join(os.getcwd()+"tmp_benchmark/")
     delete_dir(tmp_path)
     os.mkdir(tmp_path)
     for url, path in zip(urls, dirs):
-        path = os.getcwd() + "/" + path
+        path = os.path.join(os.getcwd(), path)
 
         delete_dir(path)
         os.mkdir(path)
@@ -67,8 +69,8 @@ for opt, arg in options:
     elif opt in ("-d", "--dir"):
         dirs.append(arg)
     elif opt[2:] in datasets.keys():
-        urls.append(datasets[opt[2:]][0])
-        dirs.append(datasets[opt[2:]][1])
+        urls.append(datasets[opt[2:]].url)
+        dirs.append(datasets[opt[2:]].dir)
 
 if len(urls) > len(dirs):
     raise ValueError("Missed providing path to store dataset from url")
