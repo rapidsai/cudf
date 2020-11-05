d6c03defdbe3c8e1ff01cec45177ee323a2ead89
Conor Hoekstra <codereport@outlook.com>
2020-11-05 14:16:08 -0500

    is_empty

--- cpp/src/round/round.cu
+++ cpp/src/round/round.cu
@@ -343,7 +343,7 @@ std::unique_ptr<column> round(column_view const& input,
   CUDF_EXPECTS(cudf::is_numeric(input.type()) || cudf::is_fixed_point(input.type()),
                "Only integral/floating point/fixed point currently supported.");
 
-  if (input.size() == 0) {
+  if (input.is_empty()) {
     if (is_fixed_point(input.type())) {
       auto const type = data_type{input.type().id(), numeric::scale_type{-decimal_places}};
       return std::make_unique<cudf::column>(type, 0, rmm::device_buffer{});
