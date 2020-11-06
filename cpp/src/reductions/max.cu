022afe02fa27edc7a0a3bbf92664352df5904d82
davidwendt <dwendt@nvidia.com>
2020-11-05 20:16:58 -0500

    do min/max on dictionary indices

--- cpp/src/reductions/max.cu
+++ cpp/src/reductions/max.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,19 +13,95 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// The translation unit for reduction `max`
 
+#include <cudf/column/column_factories.hpp>
+#include <cudf/detail/copy.hpp>
+#include <cudf/detail/indexalator.cuh>
 #include <cudf/detail/reduction_functions.hpp>
+#include <cudf/detail/unary.hpp>
 #include <cudf/dictionary/dictionary_column_view.hpp>
 #include <reductions/simple.cuh>
 
-std::unique_ptr<cudf::scalar> cudf::reduction::max(column_view const& col,
-                                                   cudf::data_type const output_dtype,
-                                                   rmm::mr::device_memory_resource* mr,
-                                                   cudaStream_t stream)
+namespace cudf {
+namespace reduction {
+
+namespace {
+
+template <typename Op>
+struct max_fn_dispatcher {
+ private:
+  template <typename ElementType>
+  static constexpr bool is_supported_v()
+  {
+    return !(cudf::is_fixed_point<ElementType>() ||
+             std::is_same<ElementType, cudf::list_view>::value ||
+             std::is_same<ElementType, cudf::struct_view>::value);
+  }
+
+ public:
+  template <typename ElementType, std::enable_if_t<is_supported_v<ElementType>()>* = nullptr>
+  std::unique_ptr<scalar> operator()(column_view const& col,
+                                     rmm::mr::device_memory_resource* mr,
+                                     cudaStream_t stream)
+  {
+    return cudf::reduction::simple::simple_reduction<ElementType, ElementType, Op>(
+      col, cudf::data_type{cudf::type_to_id<ElementType>()}, mr, stream);
+  }
+
+  template <typename ElementType, std::enable_if_t<not is_supported_v<ElementType>()>* = nullptr>
+  std::unique_ptr<scalar> operator()(column_view const& col,
+                                     rmm::mr::device_memory_resource* mr,
+                                     cudaStream_t stream)
+  {
+    CUDF_FAIL("Reduction operator `max` not supported for this type");
+  }
+};
+
+}  // namespace
+
+std::unique_ptr<cudf::scalar> max(column_view const& col,
+                                  cudf::data_type const output_dtype,
+                                  rmm::mr::device_memory_resource* mr,
+                                  cudaStream_t stream)
 {
-  using reducer = cudf::reduction::simple::element_type_dispatcher<cudf::reduction::op::max>;
+  // using reducer = cudf::reduction::simple::element_type_dispatcher<cudf::reduction::op::max>;
+  using reducer = max_fn_dispatcher<cudf::reduction::op::max>;
+
   auto col_type =
     cudf::is_dictionary(col.type()) ? dictionary_column_view(col).keys().type() : col.type();
-  return cudf::type_dispatcher(col_type, reducer(), col, output_dtype, mr, stream);
+  // return cudf::type_dispatcher(col_type, reducer(), col, output_dtype, mr, stream);
+
+  std::unique_ptr<cudf::scalar> result;
+  if (!cudf::is_dictionary(col.type())) {
+    result = cudf::type_dispatcher(col_type, reducer(), col, mr, stream);
+  } else {
+    cudf::reduction::op::max simple_op{};
+    // TODO: Need pair a indexalator
+    // if (col.has_nulls()) {
+    //  auto it = thrust::make_transform_iterator(
+    //    cudf::dictionary::detail::make_dictionary_pair_iterator<ElementType, true>(*dcol),
+    //    simple_op.template get_null_replacing_element_transformer<ResultType>());
+    //  result = detail::reduce(it, col.size(), Op{}, mr, stream);
+    //} else {
+    auto dict_col = cudf::dictionary_column_view(col);
+    auto indices  = dict_col.get_indices_annotated();
+    auto it       =  // thrust::make_transform_iterator(
+      cudf::detail::indexalator_factory::make_input_iterator(indices);  //,
+    // simple_op.get_element_transformer<size_type>());
+    result         = cudf::reduction::detail::reduce(it, col.size(), simple_op, mr, stream);
+    auto key_index = static_cast<numeric_scalar<size_type>*>(result.get());
+    result = cudf::detail::get_element(dict_col.keys(), key_index->value(stream), stream, mr);
+    //}
+  }
+
+  if (output_dtype == col_type) return result;
+
+  // if the output_dtype does not match, do extra work to cast it here
+  auto input = cudf::make_column_from_scalar(*result, 1, mr, stream);
+  // TODO: should build a scalar cast function
+  auto output = cudf::detail::cast(*input, output_dtype, mr, stream);
+  return cudf::detail::get_element(*output, 0, stream, mr);
 }
+
+}  // namespace reduction
+}  // namespace cudf
