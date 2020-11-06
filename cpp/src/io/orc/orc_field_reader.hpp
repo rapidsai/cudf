f801c0df09e3b2a8bf4a1a248fe65b43452d678c
Kumar Aatish <kaatish@nvidia.com>
2020-11-06 16:24:26 -0500

    PR comment fixes

--- cpp/src/io/orc/orc_field_reader.hpp
+++ cpp/src/io/orc/orc_field_reader.hpp
@@ -27,13 +27,13 @@ struct FunctionSwitchImpl {
   template <typename... Operator>
   static inline bool run(ProtobufReader *pbr,
                          const uint8_t *end,
-                         const int &fld,
+                         const int &field,
                          std::tuple<Operator...> &ops)
   {
-    if (fld == std::get<index>(ops).field) {
+    if (field == std::get<index>(ops).field) {
       return std::get<index>(ops)(pbr, end);
     } else {
-      return FunctionSwitchImpl<index - 1>::run(pbr, end, fld, ops);
+      return FunctionSwitchImpl<index - 1>::run(pbr, end, field, ops);
     }
   }
 };
@@ -43,13 +43,13 @@ struct FunctionSwitchImpl<0> {
   template <typename... Operator>
   static inline bool run(ProtobufReader *pbr,
                          const uint8_t *end,
-                         const int &fld,
+                         const int &field,
                          std::tuple<Operator...> &ops)
   {
-    if (fld == std::get<0>(ops).field) {
+    if (field == std::get<0>(ops).field) {
       return std::get<0>(ops)(pbr, end);
     } else {
-      pbr->skip_struct_field(fld & 7);
+      pbr->skip_struct_field(field & 7);
       return false;
     }
   }
@@ -73,8 +73,8 @@ inline bool ProtobufReader::function_builder(T &s, size_t maxlen, std::tuple<Ope
   constexpr int index = std::tuple_size<std::tuple<Operator...>>::value - 1;
   const uint8_t *end  = std::min(m_cur + maxlen, m_end);
   while (m_cur < end) {
-    int fld            = get_u32();
-    bool exit_function = FunctionSwitchImpl<index>::run(this, end, fld, op);
+    int field          = get_u32();
+    bool exit_function = FunctionSwitchImpl<index>::run(this, end, field, op);
     if (exit_function) { return false; }
   }
   return function_builder_return(s, end);
@@ -82,52 +82,52 @@ inline bool ProtobufReader::function_builder(T &s, size_t maxlen, std::tuple<Ope
 
 struct ProtobufReader::FieldInt32 {
   int field;
-  int32_t &val;
+  int32_t &value;
 
-  FieldInt32(int f, int32_t &v) : field((f * 8) + PB_TYPE_VARINT), val(v) {}
+  FieldInt32(int f, int32_t &v) : field((f * 8) + PB_TYPE_VARINT), value(v) {}
 
   inline bool operator()(ProtobufReader *pbr, const uint8_t *end)
   {
-    val = pbr->get_i32();
+    value = pbr->get_i32();
     return false;
   }
 };
 
 struct ProtobufReader::FieldUInt32 {
   int field;
-  uint32_t &val;
+  uint32_t &value;
 
-  FieldUInt32(int f, uint32_t &v) : field((f * 8) + PB_TYPE_VARINT), val(v) {}
+  FieldUInt32(int f, uint32_t &v) : field((f * 8) + PB_TYPE_VARINT), value(v) {}
 
   inline bool operator()(ProtobufReader *pbr, const uint8_t *end)
   {
-    val = pbr->get_u32();
+    value = pbr->get_u32();
     return false;
   }
 };
 
 struct ProtobufReader::FieldInt64 {
   int field;
-  int64_t &val;
+  int64_t &value;
 
-  FieldInt64(int f, int64_t &v) : field((f * 8) + PB_TYPE_VARINT), val(v) {}
+  FieldInt64(int f, int64_t &v) : field((f * 8) + PB_TYPE_VARINT), value(v) {}
 
   inline bool operator()(ProtobufReader *pbr, const uint8_t *end)
   {
-    val = pbr->get_i64();
+    value = pbr->get_i64();
     return false;
   }
 };
 
 struct ProtobufReader::FieldUInt64 {
   int field;
-  uint64_t &val;
+  uint64_t &value;
 
-  FieldUInt64(int f, uint64_t &v) : field((f * 8) + PB_TYPE_VARINT), val(v) {}
+  FieldUInt64(int f, uint64_t &v) : field((f * 8) + PB_TYPE_VARINT), value(v) {}
 
   inline bool operator()(ProtobufReader *pbr, const uint8_t *end)
   {
-    val = pbr->get_u64();
+    value = pbr->get_u64();
     return false;
   }
 };
@@ -135,43 +135,45 @@ struct ProtobufReader::FieldUInt64 {
 template <typename Enum>
 struct ProtobufReader::FieldEnum {
   int field;
-  Enum &val;
+  Enum &value;
 
-  FieldEnum(int f, Enum &v) : field((f * 8) + PB_TYPE_VARINT), val(v) {}
+  FieldEnum(int f, Enum &v) : field((f * 8) + PB_TYPE_VARINT), value(v) {}
 
   inline bool operator()(ProtobufReader *pbr, const uint8_t *end)
   {
-    val = static_cast<Enum>(pbr->get_u32());
+    value = static_cast<Enum>(pbr->get_u32());
     return false;
   }
 };
 
 struct ProtobufReader::FieldPackedUInt32 {
   int field;
-  std::vector<uint32_t> &val;
+  std::vector<uint32_t> &value;
 
-  FieldPackedUInt32(int f, std::vector<uint32_t> &v) : field((f * 8) + PB_TYPE_FIXEDLEN), val(v) {}
+  FieldPackedUInt32(int f, std::vector<uint32_t> &v) : field((f * 8) + PB_TYPE_FIXEDLEN), value(v)
+  {
+  }
 
   inline bool operator()(ProtobufReader *pbr, const uint8_t *end)
   {
-    uint32_t len           = pbr->get_u32();
-    const uint8_t *fld_end = std::min(pbr->m_cur + len, end);
-    while (pbr->m_cur < fld_end) val.push_back(pbr->get_u32());
+    uint32_t len             = pbr->get_u32();
+    const uint8_t *field_end = std::min(pbr->m_cur + len, end);
+    while (pbr->m_cur < field_end) value.push_back(pbr->get_u32());
     return false;
   }
 };
 
 struct ProtobufReader::FieldString {
   int field;
-  std::string &val;
+  std::string &value;
 
-  FieldString(int f, std::string &v) : field((f * 8) + PB_TYPE_FIXEDLEN), val(v) {}
+  FieldString(int f, std::string &v) : field((f * 8) + PB_TYPE_FIXEDLEN), value(v) {}
 
   inline bool operator()(ProtobufReader *pbr, const uint8_t *end)
   {
     uint32_t n = pbr->get_u32();
     if (n > (size_t)(end - pbr->m_cur)) return true;
-    val.assign((const char *)(pbr->m_cur), n);
+    value.assign((const char *)(pbr->m_cur), n);
     pbr->m_cur += n;
     return false;
   }
@@ -179,10 +181,10 @@ struct ProtobufReader::FieldString {
 
 struct ProtobufReader::FieldRepeatedString {
   int field;
-  std::vector<std::string> &val;
+  std::vector<std::string> &value;
 
   FieldRepeatedString(int f, std::vector<std::string> &v)
-    : field((f * 8) + PB_TYPE_FIXEDLEN), val(v)
+    : field((f * 8) + PB_TYPE_FIXEDLEN), value(v)
   {
   }
 
@@ -190,8 +192,8 @@ struct ProtobufReader::FieldRepeatedString {
   {
     uint32_t n = pbr->get_u32();
     if (n > (size_t)(end - pbr->m_cur)) return true;
-    val.resize(val.size() + 1);
-    val.back().assign((const char *)(pbr->m_cur), n);
+    value.resize(value.size() + 1);
+    value.back().assign((const char *)(pbr->m_cur), n);
     pbr->m_cur += n;
     return false;
   }
@@ -200,10 +202,10 @@ struct ProtobufReader::FieldRepeatedString {
 template <typename Enum>
 struct ProtobufReader::FieldRepeatedStructFunctor {
   int field;
-  std::vector<Enum> &val;
+  std::vector<Enum> &value;
 
   FieldRepeatedStructFunctor(int f, std::vector<Enum> &v)
-    : field((f * 8) + PB_TYPE_FIXEDLEN), val(v)
+    : field((f * 8) + PB_TYPE_FIXEDLEN), value(v)
   {
   }
 
@@ -211,8 +213,8 @@ struct ProtobufReader::FieldRepeatedStructFunctor {
   {
     uint32_t n = pbr->get_u32();
     if (n > (size_t)(end - pbr->m_cur)) return true;
-    val.resize(val.size() + 1);
-    if (!pbr->read(val.back(), n)) return true;
+    value.resize(value.size() + 1);
+    if (!pbr->read(value.back(), n)) return true;
     return false;
   }
 };
@@ -220,10 +222,10 @@ struct ProtobufReader::FieldRepeatedStructFunctor {
 template <typename Enum>
 struct ProtobufReader::FieldRepeatedStructBlobFunctor {
   int field;
-  std::vector<Enum> &val;
+  std::vector<Enum> &value;
 
   FieldRepeatedStructBlobFunctor(int f, std::vector<Enum> &v)
-    : field((f * 8) + PB_TYPE_FIXEDLEN), val(v)
+    : field((f * 8) + PB_TYPE_FIXEDLEN), value(v)
   {
   }
 
@@ -231,8 +233,8 @@ struct ProtobufReader::FieldRepeatedStructBlobFunctor {
   {
     uint32_t n = pbr->get_u32();
     if (n > (size_t)(end - pbr->m_cur)) return true;
-    val.resize(val.size() + 1);
-    val.back().assign(pbr->m_cur, pbr->m_cur + n);
+    value.resize(value.size() + 1);
+    value.back().assign(pbr->m_cur, pbr->m_cur + n);
     pbr->m_cur += n;
     return false;
   }
--- cpp/src/io/orc/orc_field_writer.hpp
+++ cpp/src/io/orc/orc_field_writer.hpp
@@ -16,6 +16,7 @@
 #pragma once
 
 #include <string.h>
+#include <numeric>
 #include "orc.h"
 
 namespace cudf {
@@ -29,75 +30,75 @@ struct ProtobufWriter::ProtobufFieldWriter {
   ProtobufFieldWriter(ProtobufWriter *pbw) : struct_size(0), p(pbw) {}
 
   template <typename T>
-  void field_uint(int id, const T &v)
+  void field_uint(int field, const T &value)
   {
-    struct_size += p->put_uint(id * 8 + PB_TYPE_VARINT);
-    struct_size += p->put_uint(static_cast<uint64_t>(v));
+    struct_size += p->put_uint(field * 8 + PB_TYPE_VARINT);
+    struct_size += p->put_uint(static_cast<uint64_t>(value));
   }
 
   template <typename T>
-  void field_packed_uint(int id, const std::vector<T> &v)
+  void field_packed_uint(int field, const std::vector<T> &value)
   {
-    size_t cnt = v.size(), sz = 0, lpos;
-    struct_size += p->put_uint(id * 8 + PB_TYPE_FIXEDLEN);
-    lpos = p->m_buf->size();
+    struct_size += p->put_uint(field * 8 + PB_TYPE_FIXEDLEN);
+    auto lpos = p->m_buf->size();
     p->putb(0);
-    for (size_t i = 0; i < cnt; i++) sz += p->put_uint(v[i]);
+    auto sz = std::accumulate(
+      value.begin(), value.end(), 0, [&](size_t sum, auto val) { return sum + p->put_uint(val); });
+
     struct_size += sz + 1;
     for (; sz > 0x7f; sz >>= 7, struct_size++)
       p->m_buf->insert(p->m_buf->begin() + (lpos++), static_cast<uint8_t>((sz & 0x7f) | 0x80));
     (*(p->m_buf))[lpos] = static_cast<uint8_t>(sz);
   }
 
-  void field_string(int id, const std::string &v)
+  void field_string(int field, const std::string &value)
   {
-    size_t len = v.length();
-    struct_size += p->put_uint(id * 8 + PB_TYPE_FIXEDLEN);
+    size_t len = value.length();
+    struct_size += p->put_uint(field * 8 + PB_TYPE_FIXEDLEN);
     struct_size += p->put_uint(len) + len;
-    for (size_t i = 0; i < len; i++) p->putb(v[i]);
+    for (size_t i = 0; i < len; i++) p->putb(value[i]);
   }
 
   template <typename T>
-  void field_blob(int id, const std::vector<T> &v)
+  void field_blob(int field, const std::vector<T> &value)
   {
-    size_t len = v.size();
-    struct_size += p->put_uint(id * 8 + PB_TYPE_FIXEDLEN);
+    size_t len = value.size();
+    struct_size += p->put_uint(field * 8 + PB_TYPE_FIXEDLEN);
     struct_size += p->put_uint(len) + len;
-    for (size_t i = 0; i < len; i++) p->putb(v[i]);
+    for (size_t i = 0; i < len; i++) p->putb(value[i]);
   }
 
   template <typename T>
-  void field_struct(int id, const T &v)
+  void field_struct(int field, const T &value)
   {
-    size_t sz, lpos;
-    struct_size += p->put_uint((id)*8 + PB_TYPE_FIXEDLEN);
-    lpos = p->m_buf->size();
+    struct_size += p->put_uint((field)*8 + PB_TYPE_FIXEDLEN);
+    auto lpos = p->m_buf->size();
     p->putb(0);
-    sz = p->write(v);
+    auto sz = p->write(value);
     struct_size += sz + 1;
     for (; sz > 0x7f; sz >>= 7, struct_size++)
       p->m_buf->insert(p->m_buf->begin() + (lpos++), static_cast<uint8_t>((sz & 0x7f) | 0x80));
     (*(p->m_buf))[lpos] = static_cast<uint8_t>(sz);
   }
 
-  void field_repeated_string(int id, const std::vector<std::string> &v)
+  void field_repeated_string(int field, const std::vector<std::string> &value)
   {
-    for (const auto &elem : v) field_string(id, elem);
+    for (const auto &elem : value) field_string(field, elem);
   }
 
   template <typename T>
-  void field_repeated_struct(int id, const std::vector<T> &v)
+  void field_repeated_struct(int field, const std::vector<T> &value)
   {
-    for (const auto &elem : v) field_struct(id, elem);
+    for (const auto &elem : value) field_struct(field, elem);
   }
 
   template <typename T>
-  void field_repeated_struct_blob(int id, const std::vector<T> &v)
+  void field_repeated_struct_blob(int field, const std::vector<T> &value)
   {
-    for (const auto &elem : v) field_blob(id, elem);
+    for (const auto &elem : value) field_blob(field, elem);
   }
 
-  size_t value(void) { return struct_size; }
+  size_t value() { return struct_size; }
 };
 
 }  // namespace orc
