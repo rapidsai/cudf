3246c71ee1c46a2f980bbc7620de1787efd42522
Mark Harris <mharris@nvidia.com>
2020-11-12 18:17:54 +1100

    Fix copyright and include order in dlpack.cpp

--- cpp/src/interop/dlpack.cpp
+++ cpp/src/interop/dlpack.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,10 +18,11 @@
 #include <cudf/utilities/error.hpp>
 #include <cudf/utilities/type_dispatcher.hpp>
 
+#include <rmm/cuda_stream_view.hpp>
+
 #include <dlpack/dlpack.h>
 
 #include <algorithm>
-#include "rmm/cuda_stream_view.hpp"
 
 namespace cudf {
 namespace {
