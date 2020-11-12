e11347de2362d2805521934e6b643da6e8e3bc1f
brandon-b-miller <brmiller@nvidia.com>
2020-11-11 18:40:20 -0800

    rewrite test_scalar.py

--- python/cudf/cudf/tests/test_scalar.py
+++ python/cudf/cudf/tests/test_scalar.py
@@ -15,11 +15,10 @@ from cudf.tests.utils import (
     NUMERIC_TYPES,
     TIMEDELTA_TYPES,
 )
+from cudf._lib.copying import get_element
 
 
-@pytest.mark.parametrize(
-    "value",
-    [
+SCALAR_VALUES = [
         0,
         -1,
         42,
@@ -80,10 +79,21 @@ from cudf.tests.utils import (
         np.datetime64(42, "ns"),
         np.datetime64(np.iinfo(np.int64).max, "ns"),
         np.datetime64(np.iinfo(np.int64).min + 1, "ns"),
-        np.datetime64("NaT", "s"),
-        np.datetime64("NaT", "ms"),
-        np.datetime64("NaT", "us"),
-        np.datetime64("NaT", "ns"),
+        np.timedelta64(0, "s"),
+        np.timedelta64(1, "s"),
+        np.timedelta64(-1, "s"),
+        np.timedelta64(42, "s"),
+        np.timedelta64(np.iinfo(np.int64).max, "s"),
+        np.timedelta64(np.iinfo(np.int64).min + 1, "s"),
+        np.timedelta64(42, "ms"),
+        np.timedelta64(np.iinfo(np.int64).max, "ms"),
+        np.timedelta64(np.iinfo(np.int64).min + 1, "ms"),
+        np.timedelta64(42, "us"),
+        np.timedelta64(np.iinfo(np.int64).max, "us"),
+        np.timedelta64(np.iinfo(np.int64).min + 1, "us"),
+        np.timedelta64(42, "ns"),
+        np.timedelta64(np.iinfo(np.int64).max, "ns"),
+        np.timedelta64(np.iinfo(np.int64).min + 1, "ns"),
         "",
         "one",
         "1",
@@ -93,33 +103,93 @@ from cudf.tests.utils import (
         np.bool_(False),
         np.str_("asdf"),
         np.object_("asdf"),
-    ],
-)
-@pytest.mark.parametrize("constructor", [libcudf_scalar, pycudf_scalar])
-def test_round_trip_scalar(value, constructor):
-    s = constructor(value)
+]
+
+
+@pytest.mark.parametrize("value", SCALAR_VALUES)
+def test_scalar_host_initialization(value):
+    s = cudf.Scalar(value)
 
     np.testing.assert_equal(s.value, value)
     assert s.is_valid() is True
+    assert s._is_host_value_current
+    assert not s._is_device_value_current
 
+@pytest.mark.parametrize("value", SCALAR_VALUES)
+def test_scalar_device_initialization(value):
+    column = cudf.Series([value])._column
+    dev_slr = get_element(column, 0)
+
+    s = cudf.Scalar(dev_slr)
+
+    assert s._is_device_value_current
+    assert not s._is_host_value_current
+
+    assert s.value == value
+
+    assert s._is_device_value_current
+    assert s._is_host_value_current
+
+@pytest.mark.parametrize("value", SCALAR_VALUES)
+def test_scalar_roundtrip(value):
+    s = cudf.Scalar(value)
+
+    assert s._is_host_value_current
+    assert not s._is_device_value_current
+
+    # call this property to sync the scalar
+    s.device_value
+
+    assert s._is_host_value_current
+    assert s._is_device_value_current
+
+    # invalidate the host cache
+    s._host_value = None
+    s._host_dtype = None
+
+    assert not s._is_host_value_current
+    assert s._is_device_value_current
+
+    # this should trigger a host copy
+
+    assert s.value == value
 
 @pytest.mark.parametrize(
     "dtype", NUMERIC_TYPES + DATETIME_TYPES + TIMEDELTA_TYPES + ["object"]
 )
-@pytest.mark.parametrize("constructor", [libcudf_scalar, pycudf_scalar])
-def test_null_scalar(dtype, constructor):
-    s = constructor(None, dtype=dtype)
+def test_null_scalar(dtype):
+    s = cudf.Scalar(None, dtype=dtype)
     assert s.value is cudf.NA
     assert s.dtype == np.dtype(dtype)
     assert s.is_valid() is False
 
+@pytest.mark.parametrize('value', [
+    np.datetime64('NaT', 'ns'),
+    np.datetime64('NaT', 'us'),
+    np.datetime64('NaT', 'ms'),
+    np.datetime64('NaT', 's'),
+    np.timedelta64('NaT', 'ns'),
+    np.timedelta64('NaT', 'us'),
+    np.timedelta64('NaT', 'ms'),
+    np.timedelta64('NaT', 's')
+])
+def test_nat_to_null_scalar_succeeds(value):
+    s = cudf.Scalar(value)
+    assert s.value is cudf.NA
+    assert not s.is_valid()
+    assert s.dtype == value.dtype
+
+@pytest.mark.parametrize('value', [None, np.datetime64('NaT'), np.timedelta64('NaT')])
+def test_generic_null_scalar_construction_fails(value):
+    with pytest.raises(TypeError):
+        cudf.Scalar(value)
+
 
 @pytest.mark.parametrize(
     "dtype", NUMERIC_TYPES + DATETIME_TYPES + TIMEDELTA_TYPES + ["object"]
 )
-@pytest.mark.parametrize("constructor", [libcudf_scalar, pycudf_scalar])
-def test_valid_scalar(dtype, constructor):
-    s = constructor(1, dtype=dtype)
+def test_scalar_dtype_and_validity(dtype):
+    s = cudf.Scalar(1, dtype=dtype)
 
     assert s.dtype == np.dtype(dtype)
     assert s.is_valid() is True
@@ -139,9 +209,8 @@ def test_valid_scalar(dtype, constructor):
         pd.Timedelta(34765, unit="D"),
     ],
 )
-@pytest.mark.parametrize("constructor", [libcudf_scalar, pycudf_scalar])
-def test_date_duration_scalars(value, constructor):
-    s = constructor(value)
+def test_date_duration_scalars(value):
+    s = cudf.Scalar(value)
 
     actual = s.value
 
@@ -159,17 +228,17 @@ def test_date_duration_scalars(value, constructor):
 
 
 def test_scalar_implicit_bool_conversion():
-    assert pycudf_scalar(True)
-    assert not pycudf_scalar(False)
-    assert pycudf_scalar(0) == pycudf_scalar(0)
-    assert pycudf_scalar(1) <= pycudf_scalar(2)
-    assert pycudf_scalar(1) <= 2
+    assert cudf.Scalar(True)
+    assert not cudf.Scalar(False)
+    assert cudf.Scalar(0) == cudf.Scalar(0)
+    assert cudf.Scalar(1) <= cudf.Scalar(2)
+    assert cudf.Scalar(1) <= 2
 
 
 @pytest.mark.parametrize("value", [1, -1, 1.5, 0, "1.5", "1", True, False])
 def test_scalar_implicit_float_conversion(value):
     expect = float(value)
-    got = float(pycudf_scalar(value))
+    got = float(cudf.Scalar(value))
 
     assert expect == got
     assert type(expect) == type(got)
@@ -178,7 +247,7 @@ def test_scalar_implicit_float_conversion(value):
 @pytest.mark.parametrize("value", [1, -1, 1.5, 0, "1", True, False])
 def test_scalar_implicit_int_conversion(value):
     expect = int(value)
-    got = int(pycudf_scalar(value))
+    got = int(cudf.Scalar(value))
 
     assert expect == got
     assert type(expect) == type(got)
@@ -200,3 +269,16 @@ def test_scalar_invalid_implicit_conversion(cls, dtype):
         with pytest.raises(TypeError, match=re.escape(str(error))):
             slr = pycudf_scalar(None, dtype=dtype)
             cls(slr)
+
+@pytest.mark.parametrize('value', SCALAR_VALUES)
+def test_device_scalar_direct_construction(value):
+    value = cudf.utils.utils.to_cudf_compatible_scalar(value)
+    dtype = value.dtype
+
+    s = cudf._lib.scalar.DeviceScalar(value, dtype)
+
+    assert s.value == value
+    if dtype.char == 'U':
+        assert s.dtype == 'object'
+    else:
+        assert s.dtype == dtype
