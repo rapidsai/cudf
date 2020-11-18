d71eba4f69422c2da5769619f752e11469ab5443
Vukasin Milovanovic <vukasin.milovanovic.87@gmail.com>
2020-11-18 11:17:41 -0800

    one last revert

--- cpp/src/io/avro/reader_impl.cu
+++ cpp/src/io/avro/reader_impl.cu
@@ -363,7 +363,7 @@ table_with_metadata reader::impl::read(avro_reader_options const &options, cudaS
   auto num_rows  = options.get_num_rows();
   num_rows       = (num_rows != 0) ? num_rows : -1;
   std::vector<std::unique_ptr<column>> out_columns;
-  table_metadata metadata_out{};
+  table_metadata metadata_out;
 
   // Select and read partial metadata / schema within the subset of rows
   _metadata->init_and_select_rows(skip_rows, num_rows);
