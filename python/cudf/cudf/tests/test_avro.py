4a677c0204b28b4aa3f46807c4a6d847863366da
vuule <vmilovanovic@nvidia.com>
2020-11-18 10:54:29 -0800

    python style; simplify empty schema fix

--- python/cudf/cudf/tests/test_avro.py
+++ python/cudf/cudf/tests/test_avro.py
@@ -72,36 +72,37 @@ def test_avro_reader_basic(datadir, inputfile, columns, engine):
     # (actual order may depend on pandas/python version)
     assert_eq(expect, got[expect.columns], check_categorical=False)
 
+
 def test_empty_dataframe(tmpdir):
-    filepath = tmpdir + 'empty.avro'
+    filepath = tmpdir + "empty.avro"
     # write empty dataframe
-    with open(filepath, 'wb') as out:
+    with open(filepath, "wb") as out:
         fa.writer(out, [], [])
 
     df = cudf.read_avro(filepath)
     assert_eq(df, cudf.DataFrame())
-    
+
 
 def test_no_data(tmpdir):
-    filepath = tmpdir + 'no_data.avro'
+    filepath = tmpdir + "no_data.avro"
     schema = {
-     'name': 'Weather',
-     'type': 'record',
-     'fields': [
-         {'name': 'station', 'type': 'string'},
-         {'name': 'time', 'type': 'long'},
-        {'name': 'temp', 'type': 'int'},
-      ],
+        "name": "Weather",
+        "type": "record",
+        "fields": [
+            {"name": "station", "type": "string"},
+            {"name": "time", "type": "long"},
+            {"name": "temp", "type": "int"},
+        ],
     }
     parsed_schema = fa.parse_schema(schema)
-    with open(filepath, 'wb') as out:
+    with open(filepath, "wb") as out:
         fa.writer(out, parsed_schema, [])
 
     df = cudf.read_avro(filepath)
 
     # fastavro returns an empty dataframe, need to verify manually
-    assert_eq(df.shape, (0,3))
+    assert_eq(df.shape, (0, 3))
     dtypes = df.dtypes.values.tolist()
-    assert_eq(dtypes, [np.dtype('O'), np.dtype('int64'), np.dtype('int32')])
+    assert_eq(dtypes, [np.dtype("O"), np.dtype("int64"), np.dtype("int32")])
     col_names = df.columns.tolist()
-    assert_eq(col_names, ['station', 'time', 'temp'])
+    assert_eq(col_names, ["station", "time", "temp"])
