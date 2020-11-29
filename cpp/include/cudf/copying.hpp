68b2673994df9aeb84c4c8c0736988c6cf9c1923
Michael Wang <michelwang0905@icloud.com>
2020-11-28 19:20:06 -0800

    Address review comments - State DONT_CHECK purpose - Style

--- cpp/include/cudf/copying.hpp
+++ cpp/include/cudf/copying.hpp
@@ -37,12 +37,12 @@ namespace cudf {
  * @brief Policy to account for possible out-of-bounds indices
  *
  * `NULLIFY` means to nullify output values corresponding to out-of-bounds gather_map values.
- * `DONT_CHECK` means do not check whether the indices are out-of-bounds.
+ * `DONT_CHECK` means do not check whether the indices are out-of-bounds, for better performance.
  */
 
 enum class out_of_bounds_policy : int8_t {
   NULLIFY,    /// Output values corresponding to out-of-bounds indices are null
-  DONT_CHECK  /// No bounds checking is performed
+  DONT_CHECK  /// No bounds checking is performed, better performance
 };
 
 /**
