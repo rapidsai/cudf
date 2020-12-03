815e956a59e233fb1beb0bf7298d6788c667d62e
Ayush Dattagupta <ayushdg95@gmail.com>
2020-12-03 11:35:01 -0800

    Update multifile reader exception for read_orc_statistics

--- python/cudf/cudf/io/orc.py
+++ python/cudf/cudf/io/orc.py
@@ -112,7 +112,9 @@ def read_orc_statistics(
         path_or_data=filepath_or_buffer, **kwargs,
     )
     if not is_single_filepath_or_buffer:
-        raise IOError("filepath_or_buffer resolved to multiple files")
+        raise NotImplementedError(
+            "`read_orc_statistics` does not yet support reading multiple files"
+        )
 
     filepath_or_buffer, compression = ioutils.get_filepath_or_buffer(
         path_or_data=filepath_or_buffer, compression=None, **kwargs
