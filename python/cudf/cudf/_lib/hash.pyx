55937182176d261498361072830cc3fd12a97f01
rwlee <rwlee@users.noreply.github.com>
2020-12-02 16:05:00 -0800

    Update python/cudf/cudf/_lib/hash.pyx
    Co-authored-by: GALI PREM SAGAR <sagarprem75@gmail.com>

--- python/cudf/cudf/_lib/hash.pyx
+++ python/cudf/cudf/_lib/hash.pyx
@@ -62,7 +62,6 @@ def hash_partition(Table source_table, object columns_to_hash,
 def hash(Table source_table, object initial_hash_values=None, int seed=0):
     cdef vector[uint32_t] c_initial_hash = initial_hash_values or []
     cdef table_view c_source_view = source_table.data_view()
-    cdef int hash_seed = seed
 
     cdef unique_ptr[column] c_result
     with nogil:
