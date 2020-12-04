de856902956c662157762396353e2264912fff67
Mark Harris <mharris@nvidia.com>
2020-12-04 13:54:40 +1100

    Add null count test

--- cpp/tests/stream_compaction/apply_boolean_mask_tests.cpp
+++ cpp/tests/stream_compaction/apply_boolean_mask_tests.cpp
@@ -247,6 +247,28 @@ TEST_F(ApplyBooleanMask, NoNullInput)
   CUDF_TEST_EXPECT_TABLES_EQUAL(expected, got->view());
 }
 
+TEST_F(ApplyBooleanMask, CorrectNullCount)
+{
+  cudf::size_type inputRows = 75000;
+
+  auto seq1       = cudf::test::make_counting_transform_iterator(0, [](auto i) { return i; });
+  auto valid_seq1 = cudf::test::make_counting_transform_iterator(0, [](auto row) { return true; });
+  cudf::test::fixed_width_column_wrapper<int64_t, typename decltype(seq1)::value_type> col1(
+    seq1, seq1 + inputRows, valid_seq1);
+
+  cudf::table_view input{{col1}};
+
+  auto seq3 =
+    cudf::test::make_counting_transform_iterator(0, [](auto i) { return (i % 277) == 0; });
+  cudf::test::fixed_width_column_wrapper<bool> boolean_mask(seq3, seq3 + inputRows);
+
+  auto got                 = cudf::apply_boolean_mask(input, boolean_mask);
+  auto out_col             = got->get_column(0).view();
+  auto expected_null_count = cudf::count_unset_bits(out_col.null_mask(), 0, out_col.size());
+
+  ASSERT_EQ(out_col.null_count(), expected_null_count);
+}
+
 TEST_F(ApplyBooleanMask, StructFiltering)
 {
   using namespace cudf::test;
