913cba5af8f40218d63a84f9f6647d1017e269ba
vuule <vmilovanovic@nvidia.com>
2020-12-09 15:52:08 -0800

    clean up the new logic, add checks/comments

--- cpp/src/io/orc/timezone.cpp
+++ cpp/src/io/orc/timezone.cpp
@@ -399,18 +399,20 @@ timezone_table build_timezone_transition_table(std::string const &timezone_name)
     if (!earliest_std_idx && !tzf.ttype[idx].isdst) { earliest_std_idx = ttimes.size() - 1; }
   }
 
-  if (tzf.timecnt() == 0) {
-    if (tzf.ttype[0].utcoff == 0) {
+  if (tzf.timecnt() != 0) {
+    if (!earliest_std_idx) { earliest_std_idx = 1; }
+    ttimes[0]  = ttimes[earliest_std_idx];
+    offsets[0] = offsets[earliest_std_idx];
+  } else {
+    if (tzf.typecnt() == 0 || tzf.ttype[0].utcoff == 0) {
       // No transitions, offset is zero; Table would be a no-op.
       // Return an empty table to speed up parsing.
       return {};
     }
+    // No transitions to use for the time/offset - use the first offset and apply to all timestamps
+    CUDF_EXPECTS(tzf.typecnt() != 0, "Need at least one offset");
     ttimes[0]  = std::numeric_limits<int64_t>::max();
     offsets[0] = tzf.ttype[0].utcoff;
-  } else {
-    if (!earliest_std_idx) { earliest_std_idx = 1; }
-    ttimes[0]  = ttimes[earliest_std_idx];
-    offsets[0] = offsets[earliest_std_idx];
   }
 
   // Generate entries for times after the last transition
