2f76fcbfe0b6b5dac290e31982264cf29defc37f
salonijain27 <salj7856@gmail.com>
2020-12-11 11:04:45 -0600

    create code for cuio release deck benchmarks

---
+++ python/cudf/cudf/benchmarks/cuio_release_deck_bench.py
@@ -0,0 +1,162 @@
+import cudf
+import io
+import os
+import pytest
+import pandas as pd
+
+
+path = "gcs://anaconda-public-data/nyc-taxi/csv/"
+cudf_df = cudf.read_csv(path + "2014/yellow_tripdata_2014-01.csv")
+
+
+def obtain_file_path(file_type):
+    file_dir = "cudf/benchmarks/cuio_data/"
+    file_path = os.path.join(file_dir, "file_data" + "." + file_type)
+    return file_path
+
+
+def obtain_json_data():
+    pd_df = cudf_df.to_pandas()
+    file_path = obtain_file_path(file_type="json")
+    pd_df.to_json(file_path)
+    return file_path
+
+
+def bench_to_csv(benchmark):
+    file_path = obtain_file_path(file_type="csv")
+    benchmark(cudf_df.to_csv, file_path)
+
+
+def bench_read_json(benchmark, use_buffer):
+    file_path = obtain_json_data()
+    if use_buffer == "True":
+        with open(file_path, "rb") as f:
+            file = io.BytesIO(f.read())
+    else:
+        file = file_path
+    benchmark(cudf.read_json, file)
+    os.remove(file_path)
+
+
+def bench_read_csv(benchmark, use_buffer):
+    file_path = obtain_file_path(file_type="csv")
+    if use_buffer == "True":
+        with open(file_path, "rb") as f:
+            file = io.BytesIO(f.read())
+    else:
+        file = file_path
+    benchmark(cudf.read_csv, file)
+    os.remove(file_path)
+
+
+def bench_to_orc(benchmark):
+    file_path = obtain_file_path(file_type="orc")
+    benchmark(cudf_df.to_orc, file_path)
+
+
+def bench_read_orc(benchmark, use_buffer, bench_pandas):
+    file_path = obtain_file_path(file_type="orc")
+    if use_buffer == "True":
+        with open(file_path, "rb") as f:
+            file = io.BytesIO(f.read())
+    else:
+        file = file_path
+    benchmark(cudf.read_orc, file)
+    if not bench_pandas:
+        os.remove(file_path)
+
+
+def bench_to_parquet(benchmark):
+    file_path = obtain_file_path(file_type="parquet")
+    benchmark(cudf_df.to_parquet, file_path)
+
+
+def bench_read_parquet(benchmark, use_buffer):
+    file_path = obtain_file_path(file_type="parquet")
+    if use_buffer == "True":
+        with open(file_path, "rb") as f:
+            file = io.BytesIO(f.read())
+    else:
+        file = file_path
+    benchmark(cudf.read_parquet, file)
+    os.remove(file_path)
+
+
+# Run panda benchmarks if bench_pandas=True
+def bench_pandas_to_csv(benchmark, bench_pandas):
+    if not bench_pandas:
+        pytest.skip(
+            "bench_pandas=False, panda functions " " will not be benchmarked"
+        )
+    file_path = obtain_file_path(file_type="csv")
+    print("file_path : ", file_path)
+    pd_df = cudf_df.to_pandas()
+    benchmark(pd_df.to_csv, file_path)
+
+
+def bench_pandas_read_csv(benchmark, use_buffer, bench_pandas):
+    if not bench_pandas:
+        pytest.skip(
+            "bench_pandas=False, panda functions " " will not be benchmarked"
+        )
+    file_path = obtain_file_path(file_type="csv")
+    print("file_path : ", file_path)
+
+    if use_buffer == "True":
+        with open(file_path, "rb") as f:
+            file = io.BytesIO(f.read())
+    else:
+        file = file_path
+    benchmark(pd.read_csv, file)
+    os.remove(file_path)
+
+
+def bench_pandas_read_orc(benchmark, use_buffer, bench_pandas):
+    if not bench_pandas:
+        pytest.skip(
+            "bench_pandas=False, panda functions " " will not be benchmarked"
+        )
+    file_path = obtain_file_path(file_type="orc")
+    if use_buffer == "True":
+        with open(file_path, "rb") as f:
+            file = io.BytesIO(f.read())
+    else:
+        file = file_path
+    benchmark(pd.read_orc, file)
+    os.remove(file_path)
+
+
+def bench_pandas_to_parquet(benchmark, bench_pandas):
+    if not bench_pandas:
+        pytest.skip(
+            "bench_pandas=False, panda functions " " will not be benchmarked"
+        )
+    file_path = obtain_file_path(file_type="parquet")
+    pd_df = cudf_df.to_pandas()
+    benchmark(pd_df.to_parquet, file_path)
+
+
+def bench_pandas_read_parquet(benchmark, use_buffer, bench_pandas):
+    if not bench_pandas:
+        pytest.skip(
+            "bench_pandas=False, panda functions " " will not be benchmarked"
+        )
+    file_path = obtain_file_path(file_type="parquet")
+    if use_buffer == "True":
+        with open(file_path, "rb") as f:
+            file = io.BytesIO(f.read())
+    else:
+        file = file_path
+    benchmark(pd.read_parquet, file)
+    os.remove(file_path)
+
+
+def bench_pandas_read_json(benchmark, use_buffer):
+    file_path = obtain_json_data()
+    if use_buffer == "True":
+        with open(file_path, "rb") as f:
+            file = io.BytesIO(f.read())
+    else:
+        file = file_path
+    benchmark(pd.read_json, file)
+    os.remove(file_path)
