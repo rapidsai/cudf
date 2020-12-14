2f1716e87c4c005cafb600f99709cc7afae90829
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2020-12-15 00:21:53 +0530

    add segmented_gather(list,list)

--- cpp/include/cudf/lists/detail/gather.cuh
+++ cpp/include/cudf/lists/detail/gather.cuh
@@ -288,6 +288,12 @@ std::unique_ptr<column> gather_list_leaf(
   rmm::cuda_stream_view stream,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 
+std::unique_ptr<column> segmented_gather(
+  lists_column_view const& value_column,
+  lists_column_view const& gather_map,
+  rmm::cuda_stream_view stream,
+  rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
+
 }  // namespace detail
 }  // namespace lists
 }  // namespace cudf
