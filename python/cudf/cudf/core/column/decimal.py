b67fb2ebc502a7b52c67c5f053c928665e251f38
Conor Hoekstra <codereport@outlook.com>
2021-01-07 16:57:45 -0500

    TODOs

--- python/cudf/cudf/core/column/decimal.py
+++ python/cudf/cudf/core/column/decimal.py
@@ -10,6 +10,7 @@ from cudf.core.dtypes import DecimalDtype
 class DecimalColumn(ColumnBase):
     @classmethod
     def from_arrow(cls, data: pa.Array):
+        #TODO handle nulls
         bts = data.buffers()[1].to_pybytes()
         bts = b"".join(list(more_itertools.sliced(bts, 8))[::2])
         return cls(
@@ -32,6 +33,7 @@ class DecimalColumn(ColumnBase):
         return pa.Array.from_buffers(
             type=self.dtype.to_arrow(),
             length=self.size,
+            #TODO handle nulls
             buffers=[None, pa.py_buffer(data_buf_128)],
         )
 
@@ -48,3 +50,7 @@ def binop_precision(l_dtype, r_dtype, op):
     s1, s2 = l_dtype.scale, r_dtype.scale
     if op == "add":
         return max(s1, s2) + max(p1 - s1, p2 - s2) + 1
+    #TODO extend with -, *, / + unit tests
+    #TODO think about TRUE_DIV vs FLOOR_DIV vs (DIV only in C++))
+    #TODO DIVISION rule -> p1 - s1 + s2 + max(6, s1 + p2 + 1)
+    #     just do what C++ does -> highlight in review so Keith can comment
