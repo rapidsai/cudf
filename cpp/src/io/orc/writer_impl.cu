31ef090010237e1f669db4366ac0f8b749eab9de
Ramakrishna Prabhu <ramakrishnap@nvidia.com>
2021-01-08 15:09:24 -0600

    style changes

--- cpp/src/io/orc/writer_impl.cu
+++ cpp/src/io/orc/writer_impl.cu
@@ -999,7 +999,7 @@ writer::impl::impl(std::unique_ptr<data_sink> sink,
     stream(stream),
     _mr(mr)
 {
-    init_state();
+  init_state();
 }
 
 writer::impl::impl(std::unique_ptr<data_sink> sink,
@@ -1022,10 +1022,7 @@ writer::impl::impl(std::unique_ptr<data_sink> sink,
   init_state();
 }
 
-writer::impl::~impl()
-{
-  close();
-}
+writer::impl::~impl() { close(); }
 
 void writer::impl::init_state()
 {
