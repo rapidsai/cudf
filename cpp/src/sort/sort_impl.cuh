d53b8cfdca23bab4a8b18b45c45222e549b6c258
davidwendt <dwendt@nvidia.com>
2021-01-15 10:18:22 -0500

    Add fastpath for sorting a single numeric column

--- cpp/src/sort/sort_impl.cuh
+++ cpp/src/sort/sort_impl.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2021, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,14 +23,200 @@
 #include <cudf/utilities/error.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
-#include <rmm/device_vector.hpp>
+#include <rmm/device_uvector.hpp>
 #include <rmm/exec_policy.hpp>
 
+#include <cub/cub.cuh>
+
 #include <thrust/sequence.h>
+#include <thrust/sort.h>
 
 namespace cudf {
 namespace detail {
-// Create permuted row indices that would materialize sorted order
+
+/**
+ * @brief Type-dispatched functor for sorting a single column.
+ */
+template <bool stable = false>
+struct single_column_sort_fn {
+  /**
+   * @brief Comparator functor needed for stable sort.
+   */
+  template <typename T>
+  struct comparator {
+    __device__ bool operator()(size_type lhs, size_type rhs)
+    {
+      if (has_nulls) {
+        bool lhs_null{d_column.is_null(lhs)};
+        bool rhs_null{d_column.is_null(rhs)};
+        if (lhs_null || rhs_null) {
+          if (!ascending) thrust::swap(lhs_null, rhs_null);
+          return (null_precedence == cudf::null_order::BEFORE ? !rhs_null : !lhs_null);
+        }
+      }
+      auto const lh_elem = d_column.element<T>(lhs);
+      auto const rh_elem = d_column.element<T>(rhs);
+      return ascending ? lh_elem < rh_elem : rh_elem < lh_elem;
+    }
+    column_device_view const d_column;
+    bool has_nulls;
+    bool ascending;
+    null_order null_precedence{};
+  };
+
+  /**
+   * @brief Sorts numeric columns using the CUB Radix Sort.
+   *
+   * @param input Column to sort
+   * @param indices Output sorted indices
+   * @param ascending True if sort order is ascending
+   * @param stream CUDA stream used for device memory operations and kernel launches
+   */
+  template <typename T, typename std::enable_if_t<cudf::is_numeric<T>()>* = nullptr>
+  void fast_numeric_sort(column_view const& input,
+                         mutable_column_view& indices,
+                         bool ascending,
+                         rmm::cuda_stream_view stream)
+  {
+    // A non-stable sort on a numeric column with no nulls can use the radix sort but
+    // requires making a copy of the input data and the indices.
+    auto temp_col = std::make_unique<column>(input, stream);
+    auto d_col    = temp_col->mutable_view();
+    auto temp_seq = std::make_unique<column>(indices, stream);
+    auto d_seq    = temp_seq->view();
+    if (ascending) {
+      size_t tbytes = 0;
+      cub::DeviceRadixSort::SortPairs(nullptr,
+                                      tbytes,
+                                      input.begin<T>(),
+                                      d_col.begin<T>(),
+                                      d_seq.begin<size_type>(),
+                                      indices.begin<size_type>(),
+                                      input.size(),
+                                      0,
+                                      sizeof(T) * 8,
+                                      stream.value());
+      rmm::device_uvector<int8_t> buf(tbytes, stream);
+      cub::DeviceRadixSort::SortPairs(buf.data(),
+                                      tbytes,
+                                      input.begin<T>(),
+                                      d_col.begin<T>(),
+                                      d_seq.begin<size_type>(),
+                                      indices.begin<size_type>(),
+                                      input.size(),
+                                      0,
+                                      sizeof(T) * 8,
+                                      stream.value());
+    } else {
+      size_t tbytes = 0;
+      cub::DeviceRadixSort::SortPairsDescending(nullptr,
+                                                tbytes,
+                                                input.begin<T>(),
+                                                d_col.begin<T>(),
+                                                d_seq.begin<size_type>(),
+                                                indices.begin<size_type>(),
+                                                input.size(),
+                                                0,
+                                                sizeof(T) * 8,
+                                                stream.value());
+      rmm::device_uvector<int8_t> buf(tbytes, stream);
+      cub::DeviceRadixSort::SortPairsDescending(buf.data(),
+                                                tbytes,
+                                                input.begin<T>(),
+                                                d_col.begin<T>(),
+                                                d_seq.begin<size_type>(),
+                                                indices.begin<size_type>(),
+                                                input.size(),
+                                                0,
+                                                sizeof(T) * 8,
+                                                stream.value());
+    }
+  }
+  template <typename T, typename std::enable_if_t<!cudf::is_numeric<T>()>* = nullptr>
+  void fast_numeric_sort(column_view const&, mutable_column_view&, bool, rmm::cuda_stream_view)
+  {
+    CUDF_FAIL("Only numeric types are suitable for radix sorting");
+  }
+
+  /**
+   * @brief Sorts a single column with a relationally comparable type.
+   *
+   * This includes numeric, timestamp, duration, and string types.
+   *
+   * @param input Column to sort
+   * @param indices Output sorted indices
+   * @param ascending True if sort order is ascending
+   * @param null_precedence How null rows are to be ordered
+   * @param stream CUDA stream used for device memory operations and kernel launches
+   */
+  template <typename T,
+            typename std::enable_if_t<cudf::is_relationally_comparable<T, T>()>* = nullptr>
+  void operator()(column_view const& input,
+                  mutable_column_view& indices,
+                  bool ascending,
+                  null_order null_precedence,
+                  rmm::cuda_stream_view stream)
+  {
+    // a stable sort or a column with nulls requires the comparator
+    if (stable) {
+      auto keys = column_device_view::create(input, stream);
+      thrust::stable_sort(rmm::exec_policy(stream),
+                          indices.begin<size_type>(),
+                          indices.end<size_type>(),
+                          comparator<T>{*keys, input.has_nulls(), ascending, null_precedence});
+    } else {
+      if (input.has_nulls() || !cudf::is_numeric<T>()) {
+        auto keys = column_device_view::create(input, stream);
+        thrust::sort(rmm::exec_policy(stream),
+                     indices.begin<size_type>(),
+                     indices.end<size_type>(),
+                     comparator<T>{*keys, input.has_nulls(), ascending, null_precedence});
+      } else {
+        fast_numeric_sort<T>(input, indices, ascending, stream);
+      }
+    }
+  }
+
+  template <typename T,
+            typename std::enable_if_t<!cudf::is_relationally_comparable<T, T>()>* = nullptr>
+  void operator()(column_view const&, mutable_column_view&, bool, null_order, rmm::cuda_stream_view)
+  {
+    CUDF_FAIL("Column type must be relationally comparable");
+  }
+};
+
+/**
+ * @brief Sort indices of a single column.
+ *
+ * @param input Column to sort. The column data is not modified.
+ * @param column_order Ascending or descending sort order
+ * @param null_precedence How null rows are to be ordered
+ * @param stream CUDA stream used for device memory operations and kernel launches
+ * @param mr Device memory resource used to allocate the returned column's device memory
+ * @return Sorted indices for the input column.
+ */
+template <bool stable = false>
+std::unique_ptr<column> sorted_order(column_view const& input,
+                                     order column_order,
+                                     null_order null_precedence,
+                                     rmm::cuda_stream_view stream,
+                                     rmm::mr::device_memory_resource* mr)
+{
+  auto sorted_indices = cudf::make_numeric_column(
+    data_type(type_to_id<size_type>()), input.size(), mask_state::UNALLOCATED, stream, mr);
+  mutable_column_view indices_view = sorted_indices->mutable_view();
+  thrust::sequence(
+    rmm::exec_policy(stream), indices_view.begin<size_type>(), indices_view.end<size_type>(), 0);
+  cudf::type_dispatcher(input.type(),
+                        single_column_sort_fn<stable>{},
+                        input,
+                        indices_view,
+                        column_order == order::ASCENDING,
+                        null_precedence,
+                        stream);
+  return sorted_indices;
+}
+
 template <bool stable = false>
 std::unique_ptr<column> sorted_order(table_view input,
                                      std::vector<order> const& column_order,
@@ -54,16 +240,23 @@ std::unique_ptr<column> sorted_order(table_view input,
 
   std::unique_ptr<column> sorted_indices = cudf::make_numeric_column(
     data_type(type_to_id<size_type>()), input.num_rows(), mask_state::UNALLOCATED, stream, mr);
-
   mutable_column_view mutable_indices_view = sorted_indices->mutable_view();
-
-  auto device_table = table_device_view::create(input, stream);
-
   thrust::sequence(rmm::exec_policy(stream),
                    mutable_indices_view.begin<size_type>(),
                    mutable_indices_view.end<size_type>(),
                    0);
 
+  // fast-path for single column sort
+  if (input.num_columns() == 1) {
+    return sorted_order<stable>(
+      input.column(0),
+      column_order.empty() ? order::ASCENDING : column_order.front(),
+      null_precedence.empty() ? null_order::BEFORE : null_precedence.front(),
+      stream,
+      mr);
+  }
+
+  auto device_table = table_device_view::create(input, stream);
   rmm::device_vector<order> d_column_order(column_order);
 
   if (has_nulls(input)) {
