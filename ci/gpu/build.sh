c435e862767611fd84ec5eab80018ad44ce97297
Keith Kraus <keith.j.kraus@gmail.com>
2021-01-15 02:19:38 -0500

    temporarily bump build stack to 9.3.0 for testing

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -84,6 +84,8 @@ gpuci_conda_retry install -y \
 # https://docs.rapids.ai/maintainers/depmgmt/
 # gpuci_conda_retry remove --force rapids-build-env rapids-notebook-env
 # gpuci_conda_retry install -y "your-pkg=1.0.0"
+gpuci_conda_retry remove --force rapids-build-env rapids-notebook-env
+gpuci_conda_retry install "libgcc-ng=9.3.0" "libstdcxx-ng=9.3.0" "libgfortran-ng=9.3.0"
 
 gpuci_logger "Check compiler versions"
 python --version
