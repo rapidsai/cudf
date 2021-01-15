7d7d0c912cffe1f0b64cb675f4b28cdeb100ae98
Ashwin Srinath <shwina@users.noreply.github.com>
2021-01-15 10:43:06 -0500

    Set ScalarObj->Any

--- python/cudf/cudf/_typing.py
+++ python/cudf/cudf/_typing.py
@@ -1,9 +1,9 @@
-from typing import TYPE_CHECKING, TypeVar, Union
+from typing import TYPE_CHECKING, Any, TypeVar, Union
 
 import numpy as np
 
 if TYPE_CHECKING:
-    from pandas import Interval, Period, Timedelta, Timestamp
+    from pandas import Period, Timedelta, Timestamp
     from pandas.api.extensions import ExtensionDtype
 
     import cudf
@@ -15,12 +15,10 @@ Dtype = Union["ExtensionDtype", str, np.dtype]
 DtypeObj = Union["ExtensionDtype", np.dtype]
 
 # scalars
-PythonScalar = Union[str, int, float, bool]
 DatetimeLikeScalar = TypeVar(
-    "DatetimeLikeScalar", "Period", "Timestamp", "Timedelta"
+    "DatetimeLikeScalar", Period, Timestamp, Timedelta
 )
-PandasScalar = Union["Period", "Timestamp", "Timedelta", "Interval"]
-ScalarObj = Union[PythonScalar, PandasScalar]
+ScalarObj = Any
 
 # columns
 AnyColumn = Union[
