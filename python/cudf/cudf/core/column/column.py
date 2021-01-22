78113f5038b6dbf5ee228bd4a3943faaf4f878d8
GALI PREM SAGAR <sagarprem75@gmail.com>
2021-01-22 17:58:03 -0600

    Fix `fillna` & `dropna` to also consider `np.nan` as a missing value (#7019)
    Fixes: #7007 
    
    This PR introduces changes to handle the filling of `np.nan` values in `fillna` code by converting `nan` to `null`. This fix surfaced an issue with `can_cast_safely` where when trying to convert a float column with `nan`'s to `int` column is being allowed - This is incorrect and thus added a check to return False if there is atleast 1 `nan` value in the float column.
    
    `nan` is not being handled in `dropna` aswell but is being handled in `isna`, thus introduced changes to `nan` in `dropna` too.
    
    <!--
    
    Thank you for contributing to cuDF :)
    
    Here are some guidelines to help the review process go smoothly.
    
    1. Please write a description in this text box of the changes that are being
       made.
    
    2. Please ensure that you have written units tests for the changes made/features
       added.
    
    3. There are CI checks in place to enforce that committed code follows our style
       and syntax standards. Please see our contribution guide in `CONTRIBUTING.MD`
       in the project root for more information about the checks we perform and how
       you can run them locally.
    
    4. If you are closing an issue please use one of the automatic closing words as
       noted here: https://help.github.com/articles/closing-issues-using-keywords/
    
    5. If your pull request is not ready for review but you want to make use of the
       continuous integration testing facilities please label it with `[WIP]`.
    
    6. If your pull request is ready to be reviewed without requiring additional
       work on top of it, then remove the `[WIP]` label (if present) and replace
       it with `[REVIEW]`. If assistance is required to complete the functionality,
       for example when the C/C++ code of a feature is complete but Python bindings
       are still required, then add the label `[HELP-REQ]` so that others can triage
       and assist. The additional changes then can be implemented on top of the
       same PR. If the assistance is done by members of the rapidsAI team, then no
       additional actions are required by the creator of the original PR for this,
       otherwise the original author of the PR needs to give permission to the
       person(s) assisting to commit to their personal fork of the project. If that
       doesn't happen then a new PR based on the code of the original PR can be
       opened by the person assisting, which then will be the PR that will be
       merged.
    
    7. Once all work has been done and review has taken place please do not add
       features or make changes out of the scope of those requested by the reviewer
       (doing this just add delays as already reviewed code ends up having to be
       re-reviewed/it is hard to tell what is new etc!). Further, please do not
       rebase your branch on master/force push/rewrite history, doing any of these
       causes the context of any comments made by reviewers to be lost. If
       conflicts occur against master they should be resolved by merging master
       into the branch used for making the pull request.
    
    Many thanks in advance for your cooperation!
    
    -->
    
    Authors:
      - GALI PREM SAGAR (@galipremsagar)
    
    Approvers:
      - Christopher Harris (@cwharris)
      - Keith Kraus (@kkraus14)
    
    URL: https://github.com/rapidsai/cudf/pull/7019

--- python/cudf/cudf/core/column/column.py
+++ python/cudf/cudf/core/column/column.py
@@ -291,8 +291,14 @@ class ColumnBase(Column, Serializable):
 
         return col
 
-    def dropna(self):
-        dropped_col = self.as_frame().dropna()._as_column()
+    def dropna(self, drop_nan=False):
+        if drop_nan:
+            col = self.nans_to_nulls()
+        else:
+            col = self
+        dropped_col = (
+            col.as_frame()._drop_na_rows(drop_nan=drop_nan)._as_column()
+        )
         return dropped_col
 
     def to_arrow(self):
@@ -431,7 +437,7 @@ class ColumnBase(Column, Serializable):
         if fillna:
             return self.fillna(self.default_na_value()).data_array_view
         else:
-            return self.dropna().data_array_view
+            return self.dropna(drop_nan=False).data_array_view
 
     def to_array(self, fillna=None):
         """Get a dense numpy array for the data.
@@ -1253,8 +1259,7 @@ class ColumnBase(Column, Serializable):
 
     def nans_to_nulls(self):
         if self.dtype.kind == "f":
-            col = self.fillna(np.nan)
-            newmask = libcudf.transform.nans_to_nulls(col)
+            newmask = libcudf.transform.nans_to_nulls(self)
             return self.set_mask(newmask)
         else:
             return self
