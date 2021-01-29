bba506ba8e3987a01d1ddffbec47eb0b87398a58
Michael Wang <isVoid@users.noreply.github.com>
2021-01-29 14:10:42 -0800

    Use uvector, supplied stream and default mr

--- cpp/src/replace/nulls.cu
+++ cpp/src/replace/nulls.cu
@@ -387,7 +387,7 @@ std::unique_ptr<cudf::column> replace_nulls_policy_impl(cudf::column_view const&
   auto valid_it  = cudf::detail::make_validity_iterator(*device_in);
   auto in_begin  = thrust::make_zip_iterator(thrust::make_tuple(index, valid_it));
 
-  rmm::device_vector<cudf::size_type> gather_map(input.size());
+  rmm::device_uvector<cudf::size_type> gather_map(input.size(), stream);
   auto gm_begin = thrust::make_zip_iterator(
     thrust::make_tuple(gather_map.begin(), thrust::make_discard_iterator()));
 
