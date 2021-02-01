d81ba85e4b141ad627db62a69b758b912d7c1eb7
Ashwin Srinath <shwina@users.noreply.github.com>
2021-02-01 14:14:28 -0500

    Add failing construction tests

--- python/cudf/cudf/tests/test_offset.py
+++ python/cudf/cudf/tests/test_offset.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2021, NVIDIA CORPORATION.
 
+import numpy as np
 import pytest
 
 from cudf import DateOffset
@@ -11,3 +12,10 @@ def test_construction_invalid(period, freq):
     kwargs = {freq: period}
     with pytest.raises(ValueError):
         DateOffset(**kwargs)
+
+
+@pytest.mark.parametrize(
+    "unit", ["nanoseconds", "microseconds", "milliseconds", "seconds"]
+)
+def test_construct_max_offset(unit):
+    DateOffset(**{unit: np.iinfo("int64").max})
