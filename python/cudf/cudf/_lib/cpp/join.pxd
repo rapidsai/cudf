555d5ec5ad9ca04142e8b1c6a9448637f9d900e8
Ashwin Srinath <shwina@users.noreply.github.com>
2021-02-08 16:08:19 -0500

    Add Cython bindings

--- python/cudf/cudf/_lib/cpp/join.pxd
+++ python/cudf/cudf/_lib/cpp/join.pxd
@@ -19,29 +19,31 @@ cdef extern from "cudf/join.hpp" namespace "cudf" nogil:
         const vector[int] left_on,
         const vector[int] right_on
     ) except +
+
     cdef pair[unique_ptr[column], unique_ptr[column]] left_join(
         const table_view left,
         const table_view right,
         const vector[int] left_on,
         const vector[int] right_on
     ) except +
+
     cdef pair[unique_ptr[column], unique_ptr[column]] full_join(
         const table_view left,
         const table_view right,
         const vector[int] left_on,
         const vector[int] right_on
     ) except +
-    cdef unique_ptr[table] left_semi_join(
+
+    cdef unique_ptr[column] left_semi_join(
         const table_view left,
         const table_view right,
         const vector[int] left_on,
         const vector[int] right_on,
-        const vector[int] return_columns
     ) except +
-    cdef unique_ptr[table] left_anti_join(
+
+    cdef unique_ptr[column] left_anti_join(
         const table_view left,
         const table_view right,
         const vector[int] left_on,
         const vector[int] right_on,
-        const vector[int] return_columns
     ) except +
--- python/cudf/cudf/_lib/join.pyx
+++ python/cudf/cudf/_lib/join.pyx
@@ -18,113 +18,64 @@ from cudf._lib.cpp.table.table_view cimport table_view
 cimport cudf._lib.cpp.join as cpp_join
 
 
-cpdef join(Table lhs,
-           Table rhs,
-           object how,
-           object method,
-           object left_on=None,
-           object right_on=None,
-           bool left_index=False,
-           bool right_index=False
-           ):
-    """
-    Call libcudf++ join for full outer, inner and left joins.
-    """
-    cdef Table c_lhs = lhs
-    cdef Table c_rhs = rhs
-
-    # Views might or might not include index
-    cdef table_view lhs_view
-    cdef table_view rhs_view
-
-    # Will hold the join column indices into L and R tables
-    cdef vector[int] left_on_ind
-    cdef vector[int] right_on_ind
-
-    # If left/right index, will pass a full view
-    # must offset the data column indices by # of index columns
-    num_inds_left = len(left_on) + (lhs._num_indices * left_index)
-    num_inds_right = len(right_on) + (rhs._num_indices * right_index)
-    left_on_ind.reserve(num_inds_left)
-    right_on_ind.reserve(num_inds_right)
-
-    # Only used for semi or anti joins
-    # The result columns are only the left hand columns
-    cdef vector[int] all_left_inds = range(
-        lhs._num_columns + (lhs._num_indices * left_index)
-    )
-
-    if left_index or right_index:
-        # If either true, we need to process both indices as columns
-        lhs_view = c_lhs.view()
-        rhs_view = c_rhs.view()
-
-        left_join_cols = list(lhs._index_names) + list(lhs._data.keys())
-        right_join_cols = list(rhs._index_names) + list(rhs._data.keys())
-
-        if left_index and right_index:
-            # Index columns will be common, on the left, dropped from right
-            # Index name is from the left
-            # Both views, must take index column indices
-            left_on_indices = right_on_indices = range(lhs._num_indices)
-        elif left_index:
-            # Joins left index columns with right 'on' columns
-            left_on_indices = range(lhs._num_indices)
-            right_on_indices = [
-                right_join_cols.index(on_col) for on_col in right_on
-            ]
-        elif right_index:
-            # Joins right index columns with left 'on' columns
-            right_on_indices = range(rhs._num_indices)
-            left_on_indices = [
-                left_join_cols.index(on_col) for on_col in left_on
-            ]
-        for i_l, i_r in zip(left_on_indices, right_on_indices):
-            left_on_ind.push_back(i_l)
-            right_on_ind.push_back(i_r)
-    else:
-        # cuDF's Python layer will create a new RangeIndex for this case
-        lhs_view = c_lhs.data_view()
-        rhs_view = c_rhs.data_view()
-
-        left_join_cols = list(lhs._data.keys())
-        right_join_cols = list(rhs._data.keys())
-
-    # If both left/right_index, joining on indices plus additional cols
-    # If neither, joining on just cols, not indices
-    # In both cases, must match up additional column indices in lhs/rhs
-    if left_index == right_index:
-        for name in left_on:
-            left_on_ind.push_back(left_join_cols.index(name))
-        for name in right_on:
-            right_on_ind.push_back(right_join_cols.index(name))
-
+cpdef join(Table lhs, Table rhs, left_on, right_on, how=None):
+    # left, inner and outer join
+    cdef vector[int] c_left_on = left_on
+    cdef vector[int] c_right_on = right_on
     cdef pair[unique_ptr[column], unique_ptr[column]] c_result
-    if how == 'inner':
-        with nogil:
-            c_result = move(cpp_join.inner_join(
-                lhs_view,
-                rhs_view,
-                left_on_ind,
-                right_on_ind,
-            ))
-    elif how == 'left':
-        with nogil:
-            c_result = move(cpp_join.left_join(
-                lhs_view,
-                rhs_view,
-                left_on_ind,
-                right_on_ind,
-            ))
-    elif how == 'outer':
-        with nogil:
-            c_result = move(cpp_join.full_join(
-                lhs_view,
-                rhs_view,
-                left_on_ind,
-                right_on_ind,
-            ))
+    cdef table_view c_lhs = lhs.view()
+    cdef table_view c_rhs = rhs.view()
+
+    if how == "inner":
+        c_result = move(cpp_join.inner_join(
+            c_lhs,
+            c_rhs,
+            c_left_on,
+            c_right_on,
+        ))
+    elif how == "left":
+        c_result = move(cpp_join.left_join(
+            c_lhs,
+            c_rhs,
+            c_left_on,
+            c_right_on,
+        ))
+    elif how == "outer":
+        c_result = move(cpp_join.outer_join(
+            c_lhs,
+            c_rhs
+            c_left_on,
+            c_right_on
+        ))
+    else:
+        raise ValueError(f"Unkown join type {how}")
     return (
         Column.from_unique_ptr(move(c_result.first)),
         Column.from_unique_ptr(move(c_result.second))
     )
+
+
+cpdef join_semi_anti(Table lhs, Table rhs, left_on, right_on, how=None):
+    # left-semi and left-anti joins
+    cdef vector[int] c_left_on = left_on
+    cdef vector[int] c_right_on = right_on
+    cdef unique_ptr[column] c_result
+    cdef table_view c_lhs = lhs.view()
+    cdef table_view c_rhs = rhs.view()
+
+    if how == "semi":
+        c_result = move(cpp_join.left_semi_join(
+            c_lhs,
+            c_rhs,
+            c_left_on,
+            c_right_on
+        ))
+    elif how == "anti":
+        c_result = move(cpp_join.left_anti_join(
+            c_lhs,
+            c_rhs,
+            c_left_on,
+            c_right_on
+        ))
+    else:
+        raise ValueError(f"Invalid join type {how}")
