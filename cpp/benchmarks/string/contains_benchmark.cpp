289af40788771e97edde8e0a15f328e0667e2189
davidwendt <dwendt@nvidia.com>
2021-02-11 18:07:43 -0500

    add findall_re to benchmark as well

--- cpp/benchmarks/string/contains_benchmark.cpp
+++ cpp/benchmarks/string/contains_benchmark.cpp
@@ -20,12 +20,15 @@
 #include <benchmarks/synchronization/synchronization.hpp>
 
 #include <cudf/strings/contains.hpp>
+#include <cudf/strings/findall.hpp>
 #include <cudf/strings/strings_column_view.hpp>
 
 class StringContains : public cudf::benchmark {
 };
 
-static void BM_contains(benchmark::State& state, bool contains)
+enum contains_type { contains, count, findall };
+
+static void BM_contains(benchmark::State& state, contains_type ct)
 {
   cudf::size_type const n_rows{(cudf::size_type)state.range(0)};
   auto const table = create_random_table({cudf::type_id::STRING}, 1, row_count{n_rows});
@@ -35,20 +38,31 @@ static void BM_contains(benchmark::State& state, bool contains)
     cuda_event_timer raii(state, true, 0);
     // contains_re(), matches_re(), and count_re() all have similar functions
     // with count_re() being the most regex intensive
-    contains ? cudf::strings::contains_re(input, "\\d+") : cudf::strings::count_re(input, "\\d+");
+    switch (ct) {
+      case contains_type::contains:  // contains_re and matches_re use the same main logic
+        cudf::strings::contains_re(input, "\\d+");
+        break;
+      case contains_type::count:  // counts occurrences of pattern
+        cudf::strings::count_re(input, "\\d+");
+        break;
+      case contains_type::findall:  // returns occurrences of matches
+        cudf::strings::findall_re(input, "\\d+");
+        break;
+    }
   }
 
   state.SetBytesProcessed(state.iterations() * input.chars_size());
 }
 
-#define STRINGS_BENCHMARK_DEFINE(name, b)           \
-  BENCHMARK_DEFINE_F(StringContains, name)          \
-  (::benchmark::State & st) { BM_contains(st, b); } \
-  BENCHMARK_REGISTER_F(StringContains, name)        \
-    ->RangeMultiplier(8)                            \
-    ->Ranges({{1 << 12, 1 << 24}})                  \
-    ->UseManualTime()                               \
+#define STRINGS_BENCHMARK_DEFINE(name, b)                          \
+  BENCHMARK_DEFINE_F(StringContains, name)                         \
+  (::benchmark::State & st) { BM_contains(st, contains_type::b); } \
+  BENCHMARK_REGISTER_F(StringContains, name)                       \
+    ->RangeMultiplier(8)                                           \
+    ->Ranges({{1 << 12, 1 << 24}})                                 \
+    ->UseManualTime()                                              \
     ->Unit(benchmark::kMillisecond);
 
-STRINGS_BENCHMARK_DEFINE(contains_re, true)
-STRINGS_BENCHMARK_DEFINE(count_re, false)
+STRINGS_BENCHMARK_DEFINE(contains_re, contains)
+STRINGS_BENCHMARK_DEFINE(count_re, count)
+STRINGS_BENCHMARK_DEFINE(findall_re, findall)
