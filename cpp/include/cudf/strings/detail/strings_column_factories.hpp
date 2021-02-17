99eea9e02856f928a5fe7e97a1f03462053515c7
Mark Harris <mharris@nvidia.com>
2021-02-17 16:32:16 +1100

    Convert chars, offsets, nulls make_strings_column to iterators

--- cpp/include/cudf/strings/detail/strings_column_factories.hpp
+++ cpp/include/cudf/strings/detail/strings_column_factories.hpp
@@ -9,10 +9,19 @@ namespace cudf {
 namespace detail {
 
 std::unique_ptr<column> make_strings_column(
-  const device_span<string_view>& string_views,
-  const string_view null_placeholder,
+  device_span<string_view> const& string_views,
+  string_view const null_placeholder,
   rmm::cuda_stream_view stream        = rmm::cuda_stream_default,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 
-}
+std::unique_ptr<column> make_strings_column(
+  device_span<char> const& chars,
+  device_span<size_type> const& offsets,
+  size_type null_count,
+  rmm::device_buffer&& null_mask,
+  rmm::cuda_stream_view stream        = rmm::cuda_stream_default,
+  rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
+
+}  // namespace detail
+
 }  // namespace cudf
