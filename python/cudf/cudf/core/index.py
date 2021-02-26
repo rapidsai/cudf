6764e0bc59a3254d0feb4e21ff7b3e88f0cdf590
mmhangami <mmhangami@nvidia.com>
2021-02-26 06:24:00 -0800

    fixes for mypy.

--- python/cudf/cudf/core/index.py
+++ python/cudf/cudf/core/index.py
@@ -2736,13 +2736,13 @@ class interval_range(GenericIndex):
             left_col = cupy.arange(start, end, freq)
             if end is not None:
                 end = end + 1
-            if start is not None:
+            if start and freq is not None:
                 new_freq = start + freq
-            elif start is None:
+            elif start is None and freq is not None:
                 new_freq = freq
             right_col = cupy.arange(new_freq, end, freq)
             if len(left_col) != len(right_col):
-                if end is not None:
+                if end and freq is not None:
                     end = end - freq
                 left_col = cupy.arange(start, end, freq)
             if len(right_col) == 0 or len(left_col) == 0:
