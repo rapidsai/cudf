ff97904d9695aa2917104ff629c4ca79f6622d42
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2021-03-03 19:07:34 +0530

    add groupby min_scan, max_scan tests

---
+++ cpp/tests/groupby/group_min_scan_test.cpp
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2021, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <tests/groupby/groupby_test_util.hpp>
+
+#include <cudf_test/base_fixture.hpp>
+#include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/type_lists.hpp>
+
+#include <cudf/detail/aggregation/aggregation.hpp>
+#include <cudf/dictionary/update_keys.hpp>
+
+namespace cudf {
+namespace test {
+template <typename V>
+struct groupby_min_scan_test : public cudf::test::BaseFixture {
+};
+
+TYPED_TEST_CASE(groupby_min_scan_test, cudf::test::FixedWidthTypesWithoutFixedPoint);
+
+// clang-format off
+TYPED_TEST(groupby_min_scan_test, basic)
+{
+    using K = int32_t;
+    using V = TypeParam;
+    using R = cudf::detail::target_type_t<V, aggregation::MIN>;
+
+    fixed_width_column_wrapper<K> keys          {1, 2, 3, 1, 2, 2, 1, 3, 3, 2};
+    fixed_width_column_wrapper<V, int32_t> vals({5, 6, 7, 8, 9, 0, 1, 2, 3, 4});
+
+    fixed_width_column_wrapper<K> expect_keys          {1, 1, 1, 2, 2, 2, 2, 3, 3, 3};
+    fixed_width_column_wrapper<R, int32_t> expect_vals({5, 5, 1, 6, 6, 0, 0, 7, 2, 2});
+
+    auto agg = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg));
+
+    auto agg2 = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg2));
+}
+
+TYPED_TEST(groupby_min_scan_test, empty_cols)
+{
+    using K = int32_t;
+    using V = TypeParam;
+    using R = cudf::detail::target_type_t<V, aggregation::MIN>;
+
+    fixed_width_column_wrapper<K> keys        { };
+    fixed_width_column_wrapper<V> vals        { };
+
+    fixed_width_column_wrapper<K> expect_keys { };
+    fixed_width_column_wrapper<R> expect_vals { };
+
+    auto agg = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg));
+
+    auto agg2 = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg2));
+}
+
+TYPED_TEST(groupby_min_scan_test, zero_valid_keys)
+{
+    using K = int32_t;
+    using V = TypeParam;
+    using R = cudf::detail::target_type_t<V, aggregation::MIN>;
+
+    fixed_width_column_wrapper<K> keys( { 1, 2, 3}, all_null() );
+    fixed_width_column_wrapper<V, int32_t> vals({3, 4, 5});
+
+    fixed_width_column_wrapper<K> expect_keys { };
+    fixed_width_column_wrapper<R> expect_vals { };
+
+    auto agg = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg));
+
+    auto agg2 = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg2));
+}
+
+TYPED_TEST(groupby_min_scan_test, zero_valid_values)
+{
+    using K = int32_t;
+    using V = TypeParam;
+    using R = cudf::detail::target_type_t<V, aggregation::MIN>;
+
+    fixed_width_column_wrapper<K> keys   { 1, 1, 1};
+    fixed_width_column_wrapper<V, int32_t> vals({3, 4, 5}, all_null());
+
+    fixed_width_column_wrapper<K> expect_keys { 1, 1, 1 };
+    fixed_width_column_wrapper<R, int32_t> expect_vals({ -1, -1, -1 }, all_null());
+
+    auto agg = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg));
+
+    auto agg2 = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg2));
+}
+
+TYPED_TEST(groupby_min_scan_test, null_keys_and_values)
+{
+    using K = int32_t;
+    using V = TypeParam;
+    using R = cudf::detail::target_type_t<V, aggregation::MIN>;
+
+    fixed_width_column_wrapper<K> keys(         {1, 2, 3, 1, 2, 2, 1, 3, 3, 2, 4},
+                                                {1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1});
+    fixed_width_column_wrapper<V, int32_t> vals({5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 4},
+                                                {0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0});
+
+                                          //  { 1, 1, 1, 2, 2, 2, 2, 3, _, 3, 4}
+    fixed_width_column_wrapper<K> expect_keys({ 1, 1, 1, 2, 2, 2, 2, 3,/**/3, 4}, all_valid());
+                                          //  { _, 8, 1, 6, 9, _, 4, 7, 2, 3, _}
+    fixed_width_column_wrapper<R, int32_t> expect_vals({-1, 8, 1, 6, 6,-1, 4, 7,/*2,*/3,-1},
+                                                       { 0, 1, 1, 1, 1, 0, 1, 1,/*1,*/1, 0});
+
+    auto agg = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg));
+
+    auto agg2 = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals, std::move(agg2));
+}
+
+// clang-format on
+
+template <typename T>
+struct FixedPointTestBothReps : public cudf::test::BaseFixture {
+};
+
+TYPED_TEST_CASE(FixedPointTestBothReps, cudf::test::FixedPointTypes);
+
+TYPED_TEST(FixedPointTestBothReps, GroupBySortMinScanDecimalAsValue)
+{
+  using namespace numeric;
+  using decimalXX  = TypeParam;
+  using RepType    = cudf::device_storage_type_t<decimalXX>;
+  using fp_wrapper = cudf::test::fixed_point_column_wrapper<RepType>;
+
+  using K = int32_t;
+
+  for (auto const i : {2, 1, 0, -1, -2}) {
+    auto const scale = scale_type{i};
+    auto const keys  = fixed_width_column_wrapper<K>{1, 2, 3, 1, 2, 2, 1, 3, 3, 2};
+    auto const vals  = fp_wrapper{{5, 6, 7, 8, 9, 0, 1, 2, 3, 4}, scale};
+
+    // {5, 8, 1, 6, 9, 0, 4, 7, 2, 3}
+    auto const expect_keys     = fixed_width_column_wrapper<K>{1, 1, 1, 2, 2, 2, 2, 3, 3, 3};
+    auto const expect_vals_min = fp_wrapper{{5, 5, 1, 6, 6, 0, 0, 7, 2, 2}, scale};
+
+    auto agg2 = cudf::make_min_aggregation();
+    test_single_scan(keys, vals, expect_keys, expect_vals_min, std::move(agg2));
+  }
+}
+
+}  // namespace test
+}  // namespace cudf
