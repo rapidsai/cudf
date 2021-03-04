85c1f8faf506c1255929c5dfdc209c7b2224f5c8
Alessandro Bellina <abellina@nvidia.com>
2021-03-04 10:44:31 -0600

    Java cleaner synchronization (#7474)
    Add synchronization in `cleanImpl` and `close` in various places where race conditions could exist, and also within the `MemoryCleaner` to address some concurrent modification issues we've seen in tests while shutting down (i.e. invoking the cleaner) (i.e. https://github.com/NVIDIA/spark-rapids/issues/1797)
    
    Authors:
      - Alessandro Bellina (@abellina)
    
    Approvers:
      - Robert (Bobby) Evans (@revans2)
      - Jason Lowe (@jlowe)
    
    URL: https://github.com/rapidsai/cudf/pull/7474

--- java/src/main/java/ai/rapids/cudf/nvcomp/Decompressor.java
+++ java/src/main/java/ai/rapids/cudf/nvcomp/Decompressor.java
@@ -110,7 +110,7 @@ public class Decompressor {
     }
 
     @Override
-    public void close() {
+    public synchronized void close() {
       if (!closed) {
         cleaner.delRef();
         cleaner.clean(false);
@@ -135,7 +135,7 @@ public class Decompressor {
       }
 
       @Override
-      protected boolean cleanImpl(boolean logErrorIfNotClean) {
+      protected synchronized boolean cleanImpl(boolean logErrorIfNotClean) {
         boolean neededCleanup = false;
         long address = metadata;
         if (metadata != 0) {
