fbb72612505b3d1f76c60037fc3cb89cf418f732
Nghia Truong <nghiatruong.vn@gmail.com>
2021-03-07 14:10:35 -0700

    Rewrite unit tests for drop_list_duplicates

--- cpp/tests/lists/drop_list_duplicates_tests.cpp
+++ cpp/tests/lists/drop_list_duplicates_tests.cpp
@@ -19,6 +19,12 @@
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_wrapper.hpp>
 
+using float_type = float;
+using int_type   = int32_t;
+using INT_LCW    = cudf::test::lists_column_wrapper<int_type>;
+using FLT_LCW    = cudf::test::lists_column_wrapper<float_type>;
+using STR_LCW    = cudf::test::lists_column_wrapper<cudf::string_view>;
+
 template <bool equal_test, class LCW>
 void test_once(cudf::column_view const& input,
                LCW const& expected,
@@ -37,86 +43,79 @@ struct DropListDuplicatesTest : public cudf::test::BaseFixture {
 
 TEST_F(DropListDuplicatesTest, InvalidCasesTests)
 {
-  using ILCW = cudf::test::lists_column_wrapper<int32_t>;
-  using FLCW = cudf::test::lists_column_wrapper<float>;
-  using SLCW = cudf::test::lists_column_wrapper<cudf::string_view>;
-
   /* Lists of nested types are not supported */
-  EXPECT_THROW(cudf::lists::drop_list_duplicates(cudf::lists_column_view{ILCW{ILCW{{1, 2}, {3}}}}),
-               cudf::logic_error);
-  EXPECT_THROW(cudf::lists::drop_list_duplicates(cudf::lists_column_view{FLCW{FLCW{{1, 2}, {3}}}}),
-               cudf::logic_error);
   EXPECT_THROW(
-    cudf::lists::drop_list_duplicates(cudf::lists_column_view{SLCW{SLCW{SLCW{"dummy string"}}}}),
+    cudf::lists::drop_list_duplicates(cudf::lists_column_view{INT_LCW{INT_LCW{{1, 2}, {3}}}}),
     cudf::logic_error);
+  EXPECT_THROW(
+    cudf::lists::drop_list_duplicates(cudf::lists_column_view{FLT_LCW{FLT_LCW{{1, 2}, {3}}}}),
+    cudf::logic_error);
+  EXPECT_THROW(cudf::lists::drop_list_duplicates(
+                 cudf::lists_column_view{STR_LCW{STR_LCW{STR_LCW{"dummy string"}}}}),
+               cudf::logic_error);
 }
 
 TEST_F(DropListDuplicatesTest, FloatingPointTestsNonNull)
 {
-  using float_type = double;
-  using LCW        = cudf::test::lists_column_wrapper<float_type>;
-
   /* Trivial cases */
-  test_once<false>(LCW{{}}, LCW{{}});
-  test_once<false>(LCW{{0, 1, 2, 3, 4, 5}, {}}, LCW{{0, 1, 2, 3, 4, 5}, {}});
+  test_once<false>(FLT_LCW{{}}, FLT_LCW{{}});
+  test_once<false>(FLT_LCW{{0, 1, 2, 3, 4, 5}, {}}, FLT_LCW{{0, 1, 2, 3, 4, 5}, {}});
 
   /* Multiple empty lists */
-  test_once<false>(LCW{{}, {}, {5, 4, 3, 2, 1, 0}, {}, {6}, {}},
-                   LCW{{}, {}, {0, 1, 2, 3, 4, 5}, {}, {6}, {}});
+  test_once<false>(FLT_LCW{{}, {}, {5, 4, 3, 2, 1, 0}, {}, {6}, {}},
+                   FLT_LCW{{}, {}, {0, 1, 2, 3, 4, 5}, {}, {6}, {}});
 
+  auto constexpr inf = std::numeric_limits<float_type>::infinity();
+  auto constexpr nan = std::numeric_limits<float_type>::quiet_NaN();
   /* Lists contain inf/nan */
-  auto const inf = std::numeric_limits<float_type>::infinity();
-  auto const nan = std::numeric_limits<float_type>::quiet_NaN();
-  test_once<false>(LCW{0, 1, 2, 0, 1, 2, 0, 1, 2, inf, inf, inf}, LCW{0, 1, 2, inf});
-  test_once<false>(LCW{0, 1, 2, 0, 1, 2, 0, 1, 2, nan, nan, nan}, LCW{0, 1, 2, nan});
-  test_once<false>(LCW{nan, nan, nan, 0, 1, 2, 0, 1, 2, 0, 1, 2, inf, inf, inf},
-                   LCW{0, 1, 2, nan, inf});
-  test_once<false>(LCW{nan, inf, nan, inf, nan, inf}, LCW{nan, inf});
+  test_once<false>(FLT_LCW{0, 1, 2, 0, 1, 2, 0, 1, 2, inf, inf, inf}, FLT_LCW{0, 1, 2, inf});
+  test_once<false>(FLT_LCW{0, 1, 2, 0, 1, 2, 0, 1, 2, nan, nan, nan}, FLT_LCW{0, 1, 2, nan});
+  test_once<false>(FLT_LCW{nan, nan, nan, 0, 1, 2, 0, 1, 2, 0, 1, 2, inf, inf, inf},
+                   FLT_LCW{0, 1, 2, nan, inf});
+  test_once<false>(FLT_LCW{nan, inf, nan, inf, nan, inf}, FLT_LCW{nan, inf});
 }
 
 TEST_F(DropListDuplicatesTest, IntegerTestsNonNull)
 {
-  using LCW = cudf::test::lists_column_wrapper<int32_t>;
-
   /* Trivial cases */
-  test_once<true>(LCW{{}}, LCW{{}});
-  test_once<true>(LCW{{0, 1, 2, 3, 4, 5}, {}}, LCW{{0, 1, 2, 3, 4, 5}, {}});
+  test_once<true>(INT_LCW{{}}, INT_LCW{{}});
+  test_once<true>(INT_LCW{{0, 1, 2, 3, 4, 5}, {}}, INT_LCW{{0, 1, 2, 3, 4, 5}, {}});
 
   /* Multiple empty lists */
-  test_once<true>(LCW{{}, {}, {5, 4, 3, 2, 1, 0}, {}, {6}, {}},
-                  LCW{{}, {}, {0, 1, 2, 3, 4, 5}, {}, {6}, {}});
+  test_once<true>(INT_LCW{{}, {}, {5, 4, 3, 2, 1, 0}, {}, {6}, {}},
+                  INT_LCW{{}, {}, {0, 1, 2, 3, 4, 5}, {}, {6}, {}});
 
   /* Sliced list column */
-  auto const list0 = LCW{{1, 2, 3, 2, 3, 2, 3, 2, 3}, {3, 2, 1, 4, 1}, {5}, {10, 8, 9}, {6, 7}};
+  auto const list0 = INT_LCW{{1, 2, 3, 2, 3, 2, 3, 2, 3}, {3, 2, 1, 4, 1}, {5}, {10, 8, 9}, {6, 7}};
   auto const list1 = cudf::slice(list0, {1, 5})[0];
-  test_once<true>(list0, LCW{{1, 2, 3}, {1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
-  test_once<true>(list1, LCW{{1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
+  test_once<true>(list0, INT_LCW{{1, 2, 3}, {1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
+  test_once<true>(list1, INT_LCW{{1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
 }
 
 TEST_F(DropListDuplicatesTest, IntegerTestsWithNulls)
 {
-  using LCW       = cudf::test::lists_column_wrapper<int32_t>;
-  auto const null = std::numeric_limits<int32_t>::max();
+  auto constexpr null = std::numeric_limits<int_type>::max();
 
   /* null lists */
-  test_once<true>(
-    LCW{{{3, 2, 1, 4, 1}, {5}, {}, {}, {10, 8, 9}, {6, 7}},
-        cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 2 && i != 3; })},
-    LCW{
-      {{1, 2, 3, 4}, {5}, {}, {}, {8, 9, 10}, {6, 7}},
-      cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 2 && i != 3; })});
+  test_once<true>(INT_LCW{{{3, 2, 1, 4, 1}, {5}, {}, {}, {10, 8, 9}, {6, 7}},
+                          cudf::detail::make_counting_transform_iterator(
+                            0, [](auto i) { return i != 2 && i != 3; })},
+                  INT_LCW{{{1, 2, 3, 4}, {5}, {}, {}, {8, 9, 10}, {6, 7}},
+                          cudf::detail::make_counting_transform_iterator(
+                            0, [](auto i) { return i != 2 && i != 3; })});
 
   /* null entries are equal */
   test_once<true>(
-    LCW{{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-        cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i % 2; })},
-    LCW{{1, 3, 5, 7, 9, null}, std::initializer_list<bool>{true, true, true, true, true, false}});
+    INT_LCW{{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+            cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i % 2; })},
+    INT_LCW{{1, 3, 5, 7, 9, null},
+            std::initializer_list<bool>{true, true, true, true, true, false}});
 
   /* nulls entries are not equal */
   test_once<true>(
-    LCW{{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-        cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i % 2; })},
-    LCW{
+    INT_LCW{{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+            cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i % 2; })},
+    INT_LCW{
       {1, 3, 5, 7, 9, null, null, null, null, null},
       std::initializer_list<bool>{true, true, true, true, true, false, false, false, false, false}},
     cudf::null_equality::UNEQUAL);
@@ -124,23 +123,50 @@ TEST_F(DropListDuplicatesTest, IntegerTestsWithNulls)
 
 TEST_F(DropListDuplicatesTest, StringTestsNonNull)
 {
-  using LCW = cudf::test::lists_column_wrapper<cudf::string_view>;
-
   /* Trivial cases */
-  test_once<true>(LCW{{}}, LCW{{}});
-  test_once<true>(LCW{"this", "is", "a", "string"}, LCW{"a", "is", "string", "this"});
+  test_once<true>(STR_LCW{{}}, STR_LCW{{}});
+  test_once<true>(STR_LCW{"this", "is", "a", "string"}, STR_LCW{"a", "is", "string", "this"});
 
   /* One list column */
-  test_once<true>(LCW{"this", "is", "is", "is", "a", "string", "string"},
-                  LCW{"a", "is", "string", "this"});
+  test_once<true>(STR_LCW{"this", "is", "is", "is", "a", "string", "string"},
+                  STR_LCW{"a", "is", "string", "this"});
 
   /* Multiple lists column */
-  test_once<true>(LCW{LCW{"this", "is", "a", "no duplicate", "string"},
-                      LCW{"this", "is", "is", "a", "one duplicate", "string"},
-                      LCW{"this", "is", "is", "is", "a", "two duplicates", "string"},
-                      LCW{"this", "is", "is", "is", "is", "a", "three duplicates", "string"}},
-                  LCW{LCW{"a", "is", "no duplicate", "string", "this"},
-                      LCW{"a", "is", "one duplicate", "string", "this"},
-                      LCW{"a", "is", "string", "this", "two duplicates"},
-                      LCW{"a", "is", "string", "this", "three duplicates"}});
+  test_once<true>(
+    STR_LCW{STR_LCW{"this", "is", "a", "no duplicate", "string"},
+            STR_LCW{"this", "is", "is", "a", "one duplicate", "string"},
+            STR_LCW{"this", "is", "is", "is", "a", "two duplicates", "string"},
+            STR_LCW{"this", "is", "is", "is", "is", "a", "three duplicates", "string"}},
+    STR_LCW{STR_LCW{"a", "is", "no duplicate", "string", "this"},
+            STR_LCW{"a", "is", "one duplicate", "string", "this"},
+            STR_LCW{"a", "is", "string", "this", "two duplicates"},
+            STR_LCW{"a", "is", "string", "this", "three duplicates"}});
+}
+
+TEST_F(DropListDuplicatesTest, StringTestsWithNulls)
+{
+  auto const null = std::string("");
+
+  /* One list column with null entries */
+  test_once<true>(
+    STR_LCW{{"this", null, "is", "is", "is", "a", null, "string", null, "string"},
+            cudf::detail::make_counting_transform_iterator(
+              0, [](auto i) { return i != 1 && i != 6 && i != 8; })},
+    STR_LCW{{"a", "is", "string", "this", null},
+            cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 4; })});
+
+  /* Multiple lists column with null lists and null entries */
+  test_once<true>(
+    STR_LCW{{STR_LCW{{"this", null, "is", null, "a", null, "no duplicate", null, "string"},
+                     cudf::detail::make_counting_transform_iterator(
+                       0, [](auto i) { return i % 2 == 0; })},
+             STR_LCW{},
+             STR_LCW{"this", "is", "is", "a", "one duplicate", "string"}},
+            cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })},
+    STR_LCW{
+      {STR_LCW{{"a", "is", "no duplicate", "string", "this", null},
+               cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i <= 4; })},
+       STR_LCW{},
+       STR_LCW{"a", "is", "one duplicate", "string", "this"}},
+      cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })});
 }
