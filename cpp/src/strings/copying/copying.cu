8ffd37eb6c1dde67978477cbae7317d3e7a3aba0
davidwendt <dwendt@nvidia.com>
2021-03-08 11:46:32 -0500

    merge optimized code to handle non-zero offset/start

--- cpp/src/strings/copying/copying.cu
+++ cpp/src/strings/copying/copying.cu
@@ -16,9 +16,10 @@
 
 #include <cudf/column/column_factories.hpp>
 #include <cudf/copying.hpp>
-#include <cudf/detail/gather.cuh>
 #include <cudf/detail/get_value.cuh>
-#include <cudf/strings/copying.hpp>
+#include <cudf/detail/null_mask.hpp>
+#include <cudf/strings/detail/copying.hpp>
+#include <cudf/strings/detail/utilities.hpp>
 #include <cudf/strings/strings_column_view.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
@@ -37,38 +38,43 @@ std::unique_ptr<cudf::column> copy_slice(strings_column_view const& strings,
   if (strings.is_empty()) return make_empty_strings_column(stream, mr);
   if (end < 0 || end > strings.size()) end = strings.size();
   CUDF_EXPECTS(((start >= 0) && (start < end)), "Invalid start parameter value.");
-  size_type const strings_count = end - start;
-  if (start == 0 && strings.offset() == 0) {
-    // sliced at the beginning and copying everything, so no need to gather
-    auto offsets_column = std::make_unique<cudf::column>(
-      cudf::slice(strings.offsets(), {0, strings_count + 1}).front(), stream, mr);
-    auto data_size =
-      cudf::detail::get_value<int32_t>(offsets_column->view(), strings_count, stream);
-    auto chars_column = std::make_unique<cudf::column>(
-      cudf::slice(strings.chars(), {0, data_size}).front(), stream, mr);
-    auto null_mask = cudf::detail::copy_bitmask(strings.null_mask(), 0, strings_count, stream, mr);
-    return make_strings_column(strings_count,
-                               std::move(offsets_column),
-                               std::move(chars_column),
-                               UNKNOWN_NULL_COUNT,
-                               std::move(null_mask),
-                               stream,
-                               mr);
+  auto const strings_count  = end - start;
+  auto const offsets_offset = start + strings.offset();
+
+  // slice the offsets child column
+  auto offsets_column = std::make_unique<cudf::column>(
+    cudf::slice(strings.offsets(), {offsets_offset, offsets_offset + strings_count + 1}).front(),
+    stream,
+    mr);
+  auto const chars_offset =
+    offsets_offset == 0 ? 0 : cudf::detail::get_value<int32_t>(offsets_column->view(), 0, stream);
+  if (chars_offset > 0) {
+    // adjust the individual offset values only if needed
+    auto d_offsets = offsets_column->mutable_view();
+    thrust::transform(rmm::exec_policy(stream),
+                      d_offsets.begin<int32_t>(),
+                      d_offsets.end<int32_t>(),
+                      d_offsets.begin<int32_t>(),
+                      [chars_offset] __device__(auto offset) { return offset - chars_offset; });
   }
 
-  // do the full gather instead
-  // TODO: it may be faster to just copy sliced child columns and then fixup the offset values
-  auto sliced_table = cudf::detail::gather(table_view{{strings.parent()}},
-                                           thrust::counting_iterator<size_type>(start),
-                                           thrust::counting_iterator<size_type>(end),
-                                           cudf::out_of_bounds_policy::DONT_CHECK,
-                                           stream,
-                                           mr)
-                        ->release();
-  std::unique_ptr<column> output_column(std::move(sliced_table.front()));
-  if (output_column->null_count() == 0)
-    output_column->set_null_mask(rmm::device_buffer{0, stream, mr}, 0);
-  return output_column;
+  // slice the chars child column
+  auto const data_size =
+    cudf::detail::get_value<int32_t>(offsets_column->view(), strings_count, stream);
+  auto chars_column = std::make_unique<cudf::column>(
+    cudf::slice(strings.chars(), {chars_offset, chars_offset + data_size}).front(), stream, mr);
+
+  // slice the null mask
+  auto null_mask = cudf::detail::copy_bitmask(
+    strings.null_mask(), offsets_offset, offsets_offset + strings_count, stream, mr);
+
+  return make_strings_column(strings_count,
+                             std::move(offsets_column),
+                             std::move(chars_column),
+                             UNKNOWN_NULL_COUNT,
+                             std::move(null_mask),
+                             stream,
+                             mr);
 }
 
 }  // namespace detail
