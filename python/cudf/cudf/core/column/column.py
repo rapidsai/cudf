bd23b19663929678e23b03cdf3603f568c13b99f
brandon-b-miller <brmiller@nvidia.com>
2021-03-09 15:00:37 -0800

    remove na filling in ColumnBase.__setitem__

--- python/cudf/cudf/core/column/column.py
+++ python/cudf/cudf/core/column/column.py
@@ -682,7 +682,7 @@ class ColumnBase(Column, Serializable):
                     raise ValueError(
                         "Boolean mask must be of same length as column"
                     )
-                key = arange(len(self))[key.fillna(False)]
+                key = arange(len(self))[key]
                 if hasattr(value, "__len__") and len(value) == len(self):
                     value = as_column(value)[key]
             nelem = len(key)
