380287ea2b7707966cdfebabd9c981ea00394f69
vuule <vmilovanovic@nvidia.com>
2021-03-12 16:55:01 -0800

    allocate the pinned buffer only if used

--- cpp/src/io/parquet/writer_impl.cu
+++ cpp/src/io/parquet/writer_impl.cu
@@ -1108,12 +1108,7 @@ void writer::impl::write(table_view const &table)
                        num_stats_bfr);
   }
 
-  auto host_bfr = pinned_buffer<uint8_t>{[](size_t size) {
-                                           uint8_t *ptr = nullptr;
-                                           CUDA_TRY(cudaMallocHost(&ptr, size));
-                                           return ptr;
-                                         }(max_chunk_bfr_size),
-                                         cudaFreeHost};
+  pinned_buffer<uint8_t> host_bfr{nullptr, cudaFreeHost};
 
   // Encode row groups in batches
   for (uint32_t b = 0, r = 0, global_r = global_rowgroup_base; b < (uint32_t)batch_list.size();
@@ -1163,6 +1158,14 @@ void writer::impl::write(table_view const &table)
             stream.synchronize();
           }
         } else {
+          if (!host_bfr) {
+            host_bfr = pinned_buffer<uint8_t>{[](size_t size) {
+                                                uint8_t *ptr = nullptr;
+                                                CUDA_TRY(cudaMallocHost(&ptr, size));
+                                                return ptr;
+                                              }(max_chunk_bfr_size),
+                                              cudaFreeHost};
+          }
           // copy the full data
           CUDA_TRY(cudaMemcpyAsync(host_bfr.get(),
                                    dev_bfr,
