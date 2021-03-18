3c7548363e5e0e488c857f86c6e16619dce890a9
Jeremy Dyer <jdye64@gmail.com>
2021-03-18 13:41:26 -0400

    create RDKAFKA target

--- cpp/libcudf_kafka/cmake/thirdparty/CUDF_KAFKA_GetRDKafka.cmake
+++ cpp/libcudf_kafka/cmake/thirdparty/CUDF_KAFKA_GetRDKafka.cmake
@@ -1,2 +1,10 @@
 find_path(RDKAFKA_INCLUDE "librdkafka" HINTS "$ENV{RDKAFKA_ROOT}/include")
 find_library(RDKAFKA++_LIBRARY "rdkafka++" HINTS "$ENV{RDKAFKA_ROOT}/lib" "$ENV{RDKAFKA_ROOT}/build")
+
+if(RDKAFKA_INCLUDE AND RDKAFKA++_LIBRARY)
+  add_target(rdkafka INTERFACE)
+  target_link_libraries(rdkafka INTERFACE "${RDKAFKA++_LIBRARY}")
+  target_include_directories(rdkafka INTERFACE "${RDKAFKA_INCLUDE}")
+
+  add_target(RDKAFKA::RDKAFKA ALIAS rdkafka)
+endif()
