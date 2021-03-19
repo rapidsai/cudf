84b06a3b428f6270f5b9a5816289dc6f9cd9f230
Nghia Truong <nghiatruong.vn@gmail.com>
2021-03-19 15:28:29 -0600

    Rewrite tests: split tests into smaller tests with some improvements

--- cpp/tests/lists/drop_list_duplicates_tests.cpp
+++ cpp/tests/lists/drop_list_duplicates_tests.cpp
@@ -16,80 +16,63 @@
 
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/type_lists.hpp>
 
 #include <cudf/lists/drop_list_duplicates.hpp>
+#include <cudf/utilities/type_dispatcher.hpp>
 
 #include <unordered_set>
 
-using float_type = float;
 using int_type   = int32_t;
-using INT_LCW    = cudf::test::lists_column_wrapper<int_type>;
-using FLT_LCW    = cudf::test::lists_column_wrapper<float_type>;
-using STR_LCW    = cudf::test::lists_column_wrapper<cudf::string_view>;
+using float_type = float;
 
-template <bool equal_test, class LCW>
+using LIST_COL_FLT = cudf::test::lists_column_wrapper<float_type>;
+using LIST_COL_STR = cudf::test::lists_column_wrapper<cudf::string_view>;
+
+auto constexpr m_NaN = -std::numeric_limits<float_type>::quiet_NaN();
+auto constexpr m_inf = -std::numeric_limits<float_type>::infinity();
+auto constexpr p_inf = std::numeric_limits<float_type>::infinity();
+auto constexpr p_NaN = std::numeric_limits<float_type>::quiet_NaN();
+
+template <class LCW>
 void test_once(cudf::column_view const& input,
                LCW const& expected,
                cudf::null_equality nulls_equal = cudf::null_equality::EQUAL)
 {
   auto const results =
     cudf::lists::drop_list_duplicates(cudf::lists_column_view{input}, nulls_equal);
-  if (equal_test) {
-    CUDF_TEST_EXPECT_COLUMNS_EQUAL(results->view(), expected, true);
+  if (cudf::is_floating_point(input.type())) {
+    CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(results->view(), expected);
   } else {
-    CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(results->view(), expected, true);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(results->view(), expected);
   }
 }
 
 struct DropListDuplicatesTest : public cudf::test::BaseFixture {
 };
 
-TEST_F(DropListDuplicatesTest, InvalidCasesTests)
+TEST_F(DropListDuplicatesTest, FloatingPointTestsWithInf)
 {
-  // Lists of nested types are not supported
-  EXPECT_THROW(
-    cudf::lists::drop_list_duplicates(cudf::lists_column_view{INT_LCW{INT_LCW{{1, 2}, {3}}}}),
-    cudf::logic_error);
-  EXPECT_THROW(
-    cudf::lists::drop_list_duplicates(cudf::lists_column_view{FLT_LCW{FLT_LCW{{1, 2}, {3}}}}),
-    cudf::logic_error);
-  EXPECT_THROW(
-    cudf::lists::drop_list_duplicates(cudf::lists_column_view{STR_LCW{STR_LCW{STR_LCW{"string"}}}}),
-    cudf::logic_error);
+  // Lists contain inf
+  test_once(LIST_COL_FLT{0, 1, 2, 0, 1, 2, 0, 1, 2, p_inf, p_inf, p_inf},
+            LIST_COL_FLT{0, 1, 2, p_inf});
+  test_once(LIST_COL_FLT{p_inf, 0, m_inf, 0, p_inf, 0, m_inf, 0, p_inf, 0, m_inf},
+            LIST_COL_FLT{m_inf, 0, p_inf});
 }
 
-TEST_F(DropListDuplicatesTest, FloatingPointTestsNonNull)
+TEST_F(DropListDuplicatesTest, FloatingPointTestsWithNaN)
 {
-  // Trivial cases
-  test_once<false>(FLT_LCW{{}}, FLT_LCW{{}});
-  test_once<false>(FLT_LCW{{0, 1, 2, 3, 4, 5}, {}}, FLT_LCW{{0, 1, 2, 3, 4, 5}, {}});
-
-  // Multiple empty lists
-  test_once<false>(FLT_LCW{{}, {}, {5, 4, 3, 2, 1, 0}, {}, {6}, {}},
-                   FLT_LCW{{}, {}, {0, 1, 2, 3, 4, 5}, {}, {6}, {}});
-
-  auto constexpr p_inf = std::numeric_limits<float_type>::infinity();
-  auto constexpr m_inf = -std::numeric_limits<float_type>::infinity();
-
-  // Lists contain inf
-  test_once<false>(FLT_LCW{0, 1, 2, 0, 1, 2, 0, 1, 2, p_inf, p_inf, p_inf},
-                   FLT_LCW{0, 1, 2, p_inf});
-  test_once<false>(FLT_LCW{p_inf, 0, m_inf, 0, p_inf, 0, m_inf, 0, p_inf, 0, m_inf},
-                   FLT_LCW{m_inf, 0, p_inf});
-
-  // Lists contain NaN
   // The position of NaN is undefined after sorting, thus we need to offload the data to CPU to
   // check for validity
-  auto constexpr m_NaN = -std::numeric_limits<float_type>::quiet_NaN();
-  auto constexpr p_NaN = std::numeric_limits<float_type>::quiet_NaN();
 
   // We will not store NaN in an unordered_set because it can't check for NaN existence
   std::unordered_set<float_type> results_expected{-2, -1, 0, 1, 2};
-  auto results_col = cudf::lists::drop_list_duplicates(cudf::lists_column_view{
-    FLT_LCW{0, -1, 1, p_NaN, 2, 0, m_NaN, 1, -2, 2, 0, 1, 2, m_NaN, p_NaN, p_NaN, p_NaN, m_NaN}});
-  auto results_arr =
+  auto const results_col = cudf::lists::drop_list_duplicates(cudf::lists_column_view{LIST_COL_FLT{
+    0, -1, 1, p_NaN, 2, 0, m_NaN, 1, -2, 2, 0, 1, 2, m_NaN, p_NaN, p_NaN, p_NaN, m_NaN}});
+  auto const results_arr =
     cudf::test::to_host<float_type>(cudf::lists_column_view(results_col->view()).child()).first;
   EXPECT_EQ(results_arr.size(), results_expected.size() + 2);
+
   int NaN_count{0};
   std::unordered_set<float_type> results;
   for (auto const x : results_arr) {
@@ -100,18 +83,22 @@ TEST_F(DropListDuplicatesTest, FloatingPointTestsNonNull)
     }
   }
   EXPECT_TRUE(results_expected.size() == results.size() && NaN_count == 2);
+}
 
+TEST_F(DropListDuplicatesTest, FloatingPointTestsWithInfAndNaN)
+{
   // Lists contain both NaN and inf
   // We will not store NaN in an unordered_set because it can't check for NaN existence
-  results_expected = std::unordered_set<float_type>{-2, -1, 0, 1, 2, m_inf, p_inf};
-  results_col      = cudf::lists::drop_list_duplicates(cudf::lists_column_view{
-    FLT_LCW{m_inf, 0, m_NaN, 1, -1, -2,    p_NaN, p_NaN, p_inf, p_NaN, m_NaN, 2,     -1,   0, m_NaN,
-            1,     2, p_inf, 0, 1,  m_inf, 2,     m_NaN, p_inf, m_NaN, m_NaN, p_NaN, m_inf}});
-  results_arr =
+  std::unordered_set<float_type> results_expected{-2, -1, 0, 1, 2, m_inf, p_inf};
+  auto const results_col = cudf::lists::drop_list_duplicates(cudf::lists_column_view{LIST_COL_FLT{
+    m_inf, 0, m_NaN, 1,     -1, -2, p_NaN, p_NaN, p_inf, p_NaN, m_NaN, 2,     -1,    0,
+    m_NaN, 1, 2,     p_inf, 0,  1,  m_inf, 2,     m_NaN, p_inf, m_NaN, m_NaN, p_NaN, m_inf}});
+  auto const results_arr =
     cudf::test::to_host<float_type>(cudf::lists_column_view(results_col->view()).child()).first;
   EXPECT_EQ(results_arr.size(), results_expected.size() + 2);
-  results.clear();
-  NaN_count = 0;
+
+  int NaN_count{0};
+  std::unordered_set<float_type> results;
   for (auto const x : results_arr) {
     if (std::isnan(x)) {
       ++NaN_count;
@@ -122,84 +109,26 @@ TEST_F(DropListDuplicatesTest, FloatingPointTestsNonNull)
   EXPECT_TRUE(results_expected.size() == results.size() && NaN_count == 2);
 }
 
-TEST_F(DropListDuplicatesTest, IntegerTestsNonNull)
-{
-  // Trivial cases
-  test_once<true>(INT_LCW{{}}, INT_LCW{{}});
-  test_once<true>(INT_LCW{{0, 1, 2, 3, 4, 5}, {}}, INT_LCW{{0, 1, 2, 3, 4, 5}, {}});
-
-  // Multiple empty lists
-  test_once<true>(INT_LCW{{}, {}, {5, 4, 3, 2, 1, 0}, {}, {6}, {}},
-                  INT_LCW{{}, {}, {0, 1, 2, 3, 4, 5}, {}, {6}, {}});
-
-  // Adjacent lists containing the same entries
-  test_once<true>(
-    INT_LCW{{1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 2, 2, 2}, {2, 2, 2, 2, 3, 3, 3, 3}},
-    INT_LCW{{1}, {1, 2}, {2, 3}});
-
-  // Sliced list column
-  auto const list0 = INT_LCW{{1, 2, 3, 2, 3, 2, 3, 2, 3}, {3, 2, 1, 4, 1}, {5}, {10, 8, 9}, {6, 7}};
-  auto const list1 = cudf::slice(list0, {0, 5})[0];
-  auto const list2 = cudf::slice(list0, {1, 5})[0];
-  auto const list3 = cudf::slice(list0, {1, 3})[0];
-  auto const list4 = cudf::slice(list0, {0, 3})[0];
-
-  test_once<true>(list0, INT_LCW{{1, 2, 3}, {1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
-  test_once<true>(list1, INT_LCW{{1, 2, 3}, {1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
-  test_once<true>(list2, INT_LCW{{1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
-  test_once<true>(list3, INT_LCW{{1, 2, 3, 4}, {5}});
-  test_once<true>(list4, INT_LCW{{1, 2, 3}, {1, 2, 3, 4}, {5}});
-}
-
-TEST_F(DropListDuplicatesTest, IntegerTestsWithNulls)
-{
-  auto constexpr null = std::numeric_limits<int_type>::max();
-
-  // null lists
-  test_once<true>(INT_LCW{{{3, 2, 1, 4, 1}, {5}, {}, {}, {10, 8, 9}, {6, 7}},
-                          cudf::detail::make_counting_transform_iterator(
-                            0, [](auto i) { return i != 2 && i != 3; })},
-                  INT_LCW{{{1, 2, 3, 4}, {5}, {}, {}, {8, 9, 10}, {6, 7}},
-                          cudf::detail::make_counting_transform_iterator(
-                            0, [](auto i) { return i != 2 && i != 3; })});
-
-  // null entries are equal
-  test_once<true>(
-    INT_LCW{{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-            cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i % 2; })},
-    INT_LCW{{1, 3, 5, 7, 9, null},
-            std::initializer_list<bool>{true, true, true, true, true, false}});
-
-  // nulls entries are not equal
-  test_once<true>(
-    INT_LCW{{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-            cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i % 2; })},
-    INT_LCW{
-      {1, 3, 5, 7, 9, null, null, null, null, null},
-      std::initializer_list<bool>{true, true, true, true, true, false, false, false, false, false}},
-    cudf::null_equality::UNEQUAL);
-}
-
 TEST_F(DropListDuplicatesTest, StringTestsNonNull)
 {
   // Trivial cases
-  test_once<true>(STR_LCW{{}}, STR_LCW{{}});
-  test_once<true>(STR_LCW{"this", "is", "a", "string"}, STR_LCW{"a", "is", "string", "this"});
+  test_once(LIST_COL_STR{{}}, LIST_COL_STR{{}});
+  test_once(LIST_COL_STR{"this", "is", "a", "string"}, LIST_COL_STR{"a", "is", "string", "this"});
 
   // One list column
-  test_once<true>(STR_LCW{"this", "is", "is", "is", "a", "string", "string"},
-                  STR_LCW{"a", "is", "string", "this"});
+  test_once(LIST_COL_STR{"this", "is", "is", "is", "a", "string", "string"},
+            LIST_COL_STR{"a", "is", "string", "this"});
 
   // Multiple lists column
-  test_once<true>(
-    STR_LCW{STR_LCW{"this", "is", "a", "no duplicate", "string"},
-            STR_LCW{"this", "is", "is", "a", "one duplicate", "string"},
-            STR_LCW{"this", "is", "is", "is", "a", "two duplicates", "string"},
-            STR_LCW{"this", "is", "is", "is", "is", "a", "three duplicates", "string"}},
-    STR_LCW{STR_LCW{"a", "is", "no duplicate", "string", "this"},
-            STR_LCW{"a", "is", "one duplicate", "string", "this"},
-            STR_LCW{"a", "is", "string", "this", "two duplicates"},
-            STR_LCW{"a", "is", "string", "this", "three duplicates"}});
+  test_once(
+    LIST_COL_STR{LIST_COL_STR{"this", "is", "a", "no duplicate", "string"},
+                 LIST_COL_STR{"this", "is", "is", "a", "one duplicate", "string"},
+                 LIST_COL_STR{"this", "is", "is", "is", "a", "two duplicates", "string"},
+                 LIST_COL_STR{"this", "is", "is", "is", "is", "a", "three duplicates", "string"}},
+    LIST_COL_STR{LIST_COL_STR{"a", "is", "no duplicate", "string", "this"},
+                 LIST_COL_STR{"a", "is", "one duplicate", "string", "this"},
+                 LIST_COL_STR{"a", "is", "string", "this", "two duplicates"},
+                 LIST_COL_STR{"a", "is", "string", "this", "three duplicates"}});
 }
 
 TEST_F(DropListDuplicatesTest, StringTestsWithNulls)
@@ -207,25 +136,105 @@ TEST_F(DropListDuplicatesTest, StringTestsWithNulls)
   auto const null = std::string("");
 
   // One list column with null entries
-  test_once<true>(
-    STR_LCW{{"this", null, "is", "is", "is", "a", null, "string", null, "string"},
-            cudf::detail::make_counting_transform_iterator(
-              0, [](auto i) { return i != 1 && i != 6 && i != 8; })},
-    STR_LCW{{"a", "is", "string", "this", null},
-            cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 4; })});
+  test_once(
+    LIST_COL_STR{{"this", null, "is", "is", "is", "a", null, "string", null, "string"},
+                 cudf::detail::make_counting_transform_iterator(
+                   0, [](auto i) { return i != 1 && i != 6 && i != 8; })},
+    LIST_COL_STR{{"a", "is", "string", "this", null},
+                 cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 4; })});
 
   // Multiple lists column with null lists and null entries
-  test_once<true>(
-    STR_LCW{{STR_LCW{{"this", null, "is", null, "a", null, "no duplicate", null, "string"},
+  test_once(
+    LIST_COL_STR{
+      {LIST_COL_STR{
+         {"this", null, "is", null, "a", null, "no duplicate", null, "string"},
+         cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i % 2 == 0; })},
+       LIST_COL_STR{},
+       LIST_COL_STR{"this", "is", "is", "a", "one duplicate", "string"}},
+      cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })},
+    LIST_COL_STR{{LIST_COL_STR{{"a", "is", "no duplicate", "string", "this", null},
+                               cudf::detail::make_counting_transform_iterator(
+                                 0, [](auto i) { return i <= 4; })},
+                  LIST_COL_STR{},
+                  LIST_COL_STR{"a", "is", "one duplicate", "string", "this"}},
+                 cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })});
+}
+
+template <typename T>
+struct DropListDuplicatesTypedTest : public cudf::test::BaseFixture {
+};
+#define LIST_COL cudf::test::lists_column_wrapper<TypeParam>
+
+using TypesForTest =
+  cudf::test::Concat<cudf::test::IntegralTypesNotBool, cudf::test::FloatingPointTypes>;
+TYPED_TEST_CASE(DropListDuplicatesTypedTest, TypesForTest);
+
+TYPED_TEST(DropListDuplicatesTypedTest, InvalidInputTests)
+{
+  // Lists of nested types are not supported
+  EXPECT_THROW(
+    cudf::lists::drop_list_duplicates(cudf::lists_column_view{LIST_COL{LIST_COL{{1, 2}, {3}}}}),
+    cudf::logic_error);
+}
+
+TYPED_TEST(DropListDuplicatesTypedTest, TrivialInputTests)
+{
+  // Empty input
+  test_once(LIST_COL{{}}, LIST_COL{{}});
+
+  // Trivial cases
+  test_once(LIST_COL{0, 1, 2, 3, 4, 5}, LIST_COL{0, 1, 2, 3, 4, 5});
+
+  // Multiple empty lists
+  test_once(LIST_COL{{}, {}, {5, 4, 3, 2, 1, 0}, {}, {6}, {}},
+            LIST_COL{{}, {}, {0, 1, 2, 3, 4, 5}, {}, {6}, {}});
+}
+
+TYPED_TEST(DropListDuplicatesTypedTest, NonNullInputTests)
+{
+  // Adjacent lists containing the same entries
+  test_once(LIST_COL{{1, 1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 2, 2, 2}, {2, 2, 2, 2, 3, 3, 3, 3}},
+            LIST_COL{{1}, {1, 2}, {2, 3}});
+
+  // Sliced list column
+  auto const list0 =
+    LIST_COL{{1, 2, 3, 2, 3, 2, 3, 2, 3}, {3, 2, 1, 4, 1}, {5}, {10, 8, 9}, {6, 7}};
+  auto const list1 = cudf::slice(list0, {0, 5})[0];
+  auto const list2 = cudf::slice(list0, {1, 5})[0];
+  auto const list3 = cudf::slice(list0, {1, 3})[0];
+  auto const list4 = cudf::slice(list0, {0, 3})[0];
+
+  test_once(list0, LIST_COL{{1, 2, 3}, {1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
+  test_once(list1, LIST_COL{{1, 2, 3}, {1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
+  test_once(list2, LIST_COL{{1, 2, 3, 4}, {5}, {8, 9, 10}, {6, 7}});
+  test_once(list3, LIST_COL{{1, 2, 3, 4}, {5}});
+  test_once(list4, LIST_COL{{1, 2, 3}, {1, 2, 3, 4}, {5}});
+}
+
+TYPED_TEST(DropListDuplicatesTypedTest, WithNullInputTests)
+{
+  auto constexpr null = TypeParam{0};
+
+  // null lists
+  test_once(LIST_COL{{{3, 2, 1, 4, 1}, {5}, {}, {}, {10, 8, 9}, {6, 7}},
                      cudf::detail::make_counting_transform_iterator(
-                       0, [](auto i) { return i % 2 == 0; })},
-             STR_LCW{},
-             STR_LCW{"this", "is", "is", "a", "one duplicate", "string"}},
-            cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })},
-    STR_LCW{
-      {STR_LCW{{"a", "is", "no duplicate", "string", "this", null},
-               cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i <= 4; })},
-       STR_LCW{},
-       STR_LCW{"a", "is", "one duplicate", "string", "this"}},
-      cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })});
+                       0, [](auto i) { return i != 2 && i != 3; })},
+            LIST_COL{{{1, 2, 3, 4}, {5}, {}, {}, {8, 9, 10}, {6, 7}},
+                     cudf::detail::make_counting_transform_iterator(
+                       0, [](auto i) { return i != 2 && i != 3; })});
+
+  // null entries are equal
+  test_once(
+    LIST_COL{std::initializer_list<TypeParam>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+             cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i % 2; })},
+    LIST_COL{std::initializer_list<TypeParam>{1, 3, 5, 7, 9, null},
+             cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 5; })});
+
+  // nulls entries are not equal
+  test_once(
+    LIST_COL{std::initializer_list<TypeParam>{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+             cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i % 2; })},
+    LIST_COL{std::initializer_list<TypeParam>{1, 3, 5, 7, 9, null, null, null, null, null},
+             cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i < 5; })},
+    cudf::null_equality::UNEQUAL);
 }
