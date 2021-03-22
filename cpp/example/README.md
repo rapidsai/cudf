c12895719fd7d7ab21a7acbb63ea591a7c7f8891
Michael Wang <isVoid@users.noreply.github.com>
2021-03-22 13:14:49 -0700

    Migrating from link_to_libcudf repo

---
+++ cpp/example/README.md
@@ -0,0 +1,45 @@
+# Basic Standalone libcudf application
+
+This project contains a simple example demonstrating basic libcudf use case. It
+also show case ways to build your own application based on libcudf in
+minimalistic style.
+
+The example source code loads a csv file that contains stock prices from 4
+companies spanning across 5 days, computes the average of the closing price
+for each company and writes the result in csv format.
+
+## How to compile and execute
+
+The compilation process is automated by a Dockerfile included in the project.
+
+Prerequisites:
+- docker (API >= 1.40 to support --gpus)
+- nvidia driver >= 450.80.02 (to support cudatoolkit 11.1)
+
+### Step 1: build environment in docker
+```bash
+docker build . -t rapidsenv
+```
+
+### Step 2: start the container
+```bash
+docker run -t -d -v $PWD:/workspace --gpus all --name rapidsenvrt rapidsenv
+```
+
+### (When active container running) Configure project
+```bash
+docker exec rapidsenvrt sh -c "cmake -S . -B build/"
+```
+
+### (When active container running) Build project
+```bash
+docker exec rapidsenvrt sh -c "cmake --build build/ --parallel $PARALLEL_LEVEL"
+```
+The first time running this command will take a long time because it will build
+libcudf on the host machine. It may be sped up by configuring the proper
+`PARALLEL_LEVEL` number.
+
+### (When active container running) Execute binary
+```bash
+docker exec rapidsenvrt sh -c "build/libcudf_example"
+```
---
+++ cpp/example/src/process_csv.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <rmm/mr/device/cuda_memory_resource.hpp>
+#include <rmm/mr/device/per_device_resource.hpp>
+#include <rmm/mr/device/pool_memory_resource.hpp>
+
+#include <cudf/aggregation.hpp>
+#include <cudf/groupby.hpp>
+#include <cudf/table/table.hpp>
+
+#include <cudf/io/csv.hpp>
+
+std::unique_ptr<cudf::table> read_csv(std::string const& file_path)
+{
+  auto source_info    = cudf::io::source_info(file_path);
+  auto builder        = cudf::io::csv_reader_options::builder(source_info);
+  auto options        = builder.build();
+  auto data_with_meta = cudf::io::read_csv(options);
+  auto data           = std::move(data_with_meta.tbl);
+  return data;
+}
+
+void write_csv(cudf::table_view const& tbl_view, std::string const& file_path)
+{
+  auto sink_info = cudf::io::sink_info(file_path);
+  auto builder   = cudf::io::csv_writer_options::builder(sink_info, tbl_view);
+  auto options   = builder.build();
+  cudf::io::write_csv(options);
+}
+
+std::unique_ptr<cudf::table> get_columns_from_table(cudf::table_view table,
+                                                    std::vector<int> const& indices)
+{
+  std::vector<std::unique_ptr<cudf::column>> cols;
+  for (auto i : indices) {
+    cols.push_back(std::move(std::make_unique<cudf::column>(table.column(i))));
+  }
+  auto res = std::make_unique<cudf::table>(std::move(cols));
+  return res;
+}
+
+int main(int argc, char** argv)
+{
+  // Init memory pool
+  size_t available_mem, total_mem, prealloc_mem;
+  cudaMemGetInfo(&available_mem, &total_mem);
+  prealloc_mem = available_mem * 0.5;
+  prealloc_mem = prealloc_mem - prealloc_mem % 256;
+
+  auto cuda_mr = std::make_unique<rmm::mr::cuda_memory_resource>();
+  auto pool_mr = std::make_unique<rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource>>(
+    cuda_mr.get(), prealloc_mem);
+  rmm::mr::set_current_device_resource(pool_mr.get());
+
+  // Read data
+  auto table = read_csv("4stock_5day.csv");
+
+  // Scheme: | Company | Date | Open | High | Low | Close | Volume |
+  std::vector<int> key_cols{0};  // Company
+  std::vector<int> val_cols{2, 3, 4, 5, 6};
+
+  auto keys = get_columns_from_table(table->view(), key_cols);
+  auto vals = get_columns_from_table(table->view(), val_cols);
+
+  // Compute the average of each company's closing price across 5 day span
+  cudf::groupby::groupby grpby_obj(keys->view());
+  cudf::groupby::aggregation_request agg_req;
+  agg_req.values = vals->view().column(3);
+  agg_req.aggregations.push_back(cudf::make_mean_aggregation());
+  std::vector<cudf::groupby::aggregation_request> reqs;
+  reqs.push_back(std::move(agg_req));
+
+  auto agg_res = grpby_obj.aggregate(reqs);
+
+  // Assembling the result
+  auto res_key = std::move(agg_res.first);
+  auto res_val = std::move(agg_res.second[0].results[0]);
+
+  auto res_key_view = res_key->view();
+  std::vector<cudf::column_view> cols(res_key_view.begin(), res_key_view.end());
+  cols.push_back(res_val->view());
+  auto res = cudf::table_view(cols);
+
+  // Write out result
+  write_csv(res, "4stock_5day_avg.csv");
+
+  return 0;
+}
