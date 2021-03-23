731c0b6fffdfb7eb0b443daab93379e8cace61f0
Mark Harris <mharris@nvidia.com>
2021-03-23 13:33:40 +1100

    Add host_span Cython and change concatenate functions to use it

--- python/cudf/cudf/_lib/concat.pyx
+++ python/cudf/cudf/_lib/concat.pyx
@@ -12,6 +12,7 @@ from cudf._lib.cpp.concatenate cimport (
 )
 from cudf._lib.cpp.column.column cimport column, column_view
 from cudf._lib.cpp.table.table cimport table, table_view
+from cudf._lib.cpp.utilities.host_span cimport host_span
 
 from cudf._lib.column cimport Column
 from cudf._lib.table cimport Table
@@ -29,8 +30,9 @@ cpdef concat_masks(object columns):
     cdef device_buffer c_result
     cdef unique_ptr[device_buffer] c_unique_result
     cdef vector[column_view] c_views = make_column_views(columns)
+    cdef host_span[column_view] c_spans = host_span[column_view](c_views)
     with nogil:
-        c_result = move(libcudf_concatenate_masks(c_views))
+        c_result = move(libcudf_concatenate_masks(c_spans))
         c_unique_result = make_unique[device_buffer](move(c_result))
     return Buffer(DeviceBuffer.c_from_unique_ptr(move(c_unique_result)))
 
@@ -38,20 +40,24 @@ cpdef concat_masks(object columns):
 cpdef concat_columns(object columns):
     cdef unique_ptr[column] c_result
     cdef vector[column_view] c_views = make_column_views(columns)
+    cdef host_span[column_view] c_spans = host_span[column_view](c_views)
     with nogil:
-        c_result = move(libcudf_concatenate_columns(c_views))
+        c_result = move(libcudf_concatenate_columns(c_spans))
     return Column.from_unique_ptr(move(c_result))
 
 
 cpdef concat_tables(object tables, bool ignore_index=False):
     cdef unique_ptr[table] c_result
     cdef vector[table_view] c_views
+    cdef host_span[table_view] c_spans
     if ignore_index is False:
         c_views = make_table_views(tables)
+        c_spans = host_span[table_view](c_views)
     else:
         c_views = make_table_data_views(tables)
+        c_spans = host_span[table_view](c_views)
     with nogil:
-        c_result = move(libcudf_concatenate_tables(c_views))
+        c_result = move(libcudf_concatenate_tables(c_spans))
     return Table.from_unique_ptr(
         move(c_result),
         column_names=tables[0]._column_names,
