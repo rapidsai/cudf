2985b4de7863d55205331e107a08e2b0bdf4264b
Dave Baranec <dbaranec@nvidia.com>
2021-03-23 14:17:16 -0500

    Cleaned up some test function names.  Sprinkled utf8 characters around various tests.

--- cpp/tests/transform/row_bit_count_test.cu
+++ cpp/tests/transform/row_bit_count_test.cu
@@ -154,7 +154,7 @@ struct RowBitCount : public cudf::test::BaseFixture {
 
 TEST_F(RowBitCount, Strings)
 {
-  std::vector<std::string> strings{"abc", "def", "", "z", "bananas", "warp", "", "zing"};
+  std::vector<std::string> strings{"abc", "ï", "", "z", "bananas", "warp", "", "zing"};
 
   cudf::test::strings_column_wrapper col(strings.begin(), strings.end());
 
@@ -173,8 +173,8 @@ TEST_F(RowBitCount, Strings)
 TEST_F(RowBitCount, StringsWithNulls)
 {
   // clang-format off
-  std::vector<std::string> strings { "abc", "def", "", "z", "bananas", "warp", "", "zing" };
-  std::vector<bool>        valids  {  1,     0,    0,  1,   0,         1,      1,  1 };
+  std::vector<std::string> strings { "daïs", "def", "", "z", "bananas", "warp", "", "zing" };
+  std::vector<bool>        valids  {  1,      0,    0,  1,   0,          1,      1,  1 };
   // clang-format on
 
   cudf::test::strings_column_wrapper col(strings.begin(), strings.end(), valids.begin());
@@ -196,7 +196,7 @@ TEST_F(RowBitCount, StringsWithNulls)
 std::pair<std::unique_ptr<column>, std::unique_ptr<column>> build_struct_column()
 {
   std::vector<bool> struct_validity{0, 1, 1, 1, 1, 0};
-  std::vector<std::string> strings{"abc", "def", "", "z", "bananas", "warp"};
+  std::vector<std::string> strings{"abc", "def", "", "z", "bananas", "daïs"};
 
   cudf::test::fixed_width_column_wrapper<float> col0{0, 1, 2, 3, 4, 5};
   cudf::test::fixed_width_column_wrapper<int16_t> col1{{8, 9, 10, 11, 12, 13}, {1, 0, 1, 1, 1, 1}};
@@ -223,7 +223,7 @@ std::pair<std::unique_ptr<column>, std::unique_ptr<column>> build_struct_column(
 
 TEST_F(RowBitCount, StructsNoNulls)
 {
-  std::vector<std::string> strings{"abc", "def", "", "z", "bananas", "warp"};
+  std::vector<std::string> strings{"abc", "daïs", "", "z", "bananas", "warp"};
 
   cudf::test::fixed_width_column_wrapper<float> col0{0, 1, 2, 3, 4, 5};
   cudf::test::fixed_width_column_wrapper<int16_t> col1{8, 9, 10, 11, 12, 13};
@@ -274,7 +274,7 @@ TEST_F(RowBitCount, StructsNested)
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *result);
 }
 
-std::pair<std::unique_ptr<column>, std::unique_ptr<column>> build_nested_column(
+std::pair<std::unique_ptr<column>, std::unique_ptr<column>> build_nested_and_expected_column(
   std::vector<bool> const& struct_validity)
 {
   // tests the "branching" case ->  list<struct<list> ...>>>
@@ -324,7 +324,7 @@ std::pair<std::unique_ptr<column>, std::unique_ptr<column>> build_nested_column(
           expected_sizes.release()};
 }
 
-std::unique_ptr<column> build_nested_column2(std::vector<bool> const& struct_validity)
+std::unique_ptr<column> build_nested_column(std::vector<bool> const& struct_validity)
 {
   // List<Struct<List<List<int>>, Struct<int16>>>
 
@@ -362,11 +362,12 @@ TEST_F(RowBitCount, NestedTypes)
   {
     std::unique_ptr<column> col_no_nulls;
     std::unique_ptr<column> expected_sizes;
-    std::tie(col_no_nulls, expected_sizes) = build_nested_column({1, 1, 1, 1, 1, 1, 1, 1});
+    std::tie(col_no_nulls, expected_sizes) =
+      build_nested_and_expected_column({1, 1, 1, 1, 1, 1, 1, 1});
     table_view no_nulls_t({*col_no_nulls});
     auto no_nulls_result = cudf::row_bit_count(no_nulls_t);
 
-    auto col_nulls = build_nested_column({0, 0, 1, 1, 1, 1, 1, 1}).first;
+    auto col_nulls = build_nested_and_expected_column({0, 0, 1, 1, 1, 1, 1, 1}).first;
     table_view nulls_t({*col_nulls});
     auto nulls_result = cudf::row_bit_count(nulls_t);
 
@@ -392,11 +393,11 @@ TEST_F(RowBitCount, NestedTypes)
 
   // List<Struct<List<List<int>>, Struct<int16>>>
   {
-    auto col_no_nulls = build_nested_column2({1, 1, 1});
+    auto col_no_nulls = build_nested_column({1, 1, 1});
     table_view no_nulls_t({*col_no_nulls});
     auto no_nulls_result = cudf::row_bit_count(no_nulls_t);
 
-    auto col_nulls = build_nested_column2({1, 0, 1});
+    auto col_nulls = build_nested_column({1, 0, 1});
     table_view nulls_t({*col_nulls});
     auto nulls_result = cudf::row_bit_count(nulls_t);
 
@@ -466,7 +467,7 @@ TEST_F(RowBitCount, Table)
   // complex nested column
   std::unique_ptr<column> col0;
   std::unique_ptr<column> col0_sizes;
-  std::tie(col0, col0_sizes) = build_nested_column({1, 1, 1, 1, 1, 1, 1, 1});
+  std::tie(col0, col0_sizes) = build_nested_and_expected_column({1, 1, 1, 1, 1, 1, 1, 1});
 
   // struct column
   std::unique_ptr<column> col1;
@@ -514,7 +515,7 @@ TEST_F(RowBitCount, SlicedColumnsStrings)
 {
   auto const slice_size = 7;
   std::vector<std::string> strings{
-    "banana", "metric", "imperial", "abc", "pears", "", "fire", "def", "cudf", "xyzw"};
+    "banana", "metric", "imperial", "abc", "daïs", "", "fire", "def", "cudf", "xyzw"};
   cudf::test::strings_column_wrapper c0_unsliced(strings.begin(), strings.end());
   auto c0 = cudf::slice(c0_unsliced, {3, 3 + slice_size});
 
@@ -536,7 +537,7 @@ TEST_F(RowBitCount, SlicedColumnsLists)
   auto const slice_size = 2;
   cudf::test::lists_column_wrapper<cudf::string_view> c0_unsliced{
     {{"banana", "v"}, {"cats"}},
-    {{"dogs", "yay"}, {"xyz", ""}, {"ultra"}},
+    {{"dogs", "yay"}, {"xyz", ""}, {"daïs"}},
     {{"fast", "parrot"}, {"orange"}},
     {{"blue"}, {"red", "yellow"}, {"ultraviolet", "", "green"}}};
   auto c0 = cudf::slice(c0_unsliced, {1, 1 + slice_size});
@@ -555,7 +556,7 @@ TEST_F(RowBitCount, SlicedColumnsStructs)
 
   cudf::test::fixed_width_column_wrapper<int16_t> c0{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   std::vector<std::string> strings{
-    "banana", "metric", "imperial", "abc", "pears", "", "fire", "def", "cudf", "xyzw"};
+    "banana", "metric", "imperial", "abc", "daïs", "", "fire", "def", "cudf", "xyzw"};
   cudf::test::strings_column_wrapper c1(strings.begin(), strings.end());
 
   auto struct_col_unsliced = cudf::test::structs_column_wrapper({c0, c1});
