7c7d6746458c546d5952ef94d91a0ed7a2575558
Dave Baranec <dbaranec@nvidia.com>
2021-03-24 10:51:46 -0500

    Add missing header for thrust::optional

--- cpp/src/transform/row_bit_count.cu
+++ cpp/src/transform/row_bit_count.cu
@@ -23,6 +23,8 @@
 #include <cudf/table/table_device_view.cuh>
 #include <cudf/types.hpp>
 
+#include <thrust/optional.h>
+
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/device_uvector.hpp>
 #include <rmm/exec_policy.hpp>
