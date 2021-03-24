4bcecfa49228669ccbdfd49502fad1751c0a92b1
galipremsagar <sagarprem75@gmail.com>
2021-03-23 18:47:30 -0700

    remove 3.6 python usages

--- python/cudf/setup.py
+++ python/cudf/setup.py
@@ -144,8 +144,8 @@ setup(
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     # Include the separately-compiled shared library
     setup_requires=["cython", "protobuf"],
--- python/cudf_kafka/setup.py
+++ python/cudf_kafka/setup.py
@@ -81,8 +81,8 @@ setup(
         "Topic :: Apache Kafka",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     # Include the separately-compiled shared library
     setup_requires=["cython"],
