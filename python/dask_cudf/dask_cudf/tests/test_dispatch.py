ad9212b443167166b4ee83277a117c863506c6be
brandon-b-miller <53796099+brandon-b-miller@users.noreply.github.com>
2021-03-30 15:11:28 -0500

    Enable dask dispatch to cuDF's `is_categorical_dtype` for cuDF objects (#7740)
    Fixes https://github.com/rapidsai/cudf/issues/7111
    Closes https://github.com/rapidsai/cudf/issues/7400
    
    Authors:
      - @brandon-b-miller
      - Keith Kraus (@kkraus14)
    
    Approvers:
      - Keith Kraus (@kkraus14)
      - GALI PREM SAGAR (@galipremsagar)
      - Ray Douglass (@raydouglass)
    
    URL: https://github.com/rapidsai/cudf/pull/7740

---
+++ python/dask_cudf/dask_cudf/tests/test_dispatch.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from dask.dataframe.methods import is_categorical_dtype
+
+import cudf
+
+
+def test_is_categorical_dispatch():
+    assert is_categorical_dtype(pd.CategoricalDtype([1, 2, 3]))
+    assert is_categorical_dtype(cudf.CategoricalDtype([1, 2, 3]))
+
+    assert is_categorical_dtype(cudf.Series([1, 2, 3], dtype="category"))
+    assert is_categorical_dtype(pd.Series([1, 2, 3], dtype="category"))
+
+    assert is_categorical_dtype(pd.Index([1, 2, 3], dtype="category"))
+    assert is_categorical_dtype(cudf.Index([1, 2, 3], dtype="category"))
