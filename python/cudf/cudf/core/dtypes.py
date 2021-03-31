4f17e211af17ddb94368bf77c7ba93ac19e819ad
Ashwin Srinath <shwina@users.noreply.github.com>
2021-03-31 11:10:20 -0400

    Make all cudf dtypes inherit from a _BaseDtype

--- python/cudf/cudf/core/dtypes.py
+++ python/cudf/cudf/core/dtypes.py
@@ -14,7 +14,11 @@ import cudf
 from cudf._typing import Dtype
 
 
-class CategoricalDtype(ExtensionDtype):
+class _BaseDtype(ExtensionDtype):
+    pass
+
+
+class CategoricalDtype(_BaseDtype):
 
     ordered: Optional[bool]
 
@@ -121,7 +125,7 @@ class CategoricalDtype(ExtensionDtype):
         return cls(categories=categories, ordered=ordered)
 
 
-class ListDtype(ExtensionDtype):
+class ListDtype(_BaseDtype):
     _typ: pa.ListType
     name: str = "list"
 
@@ -180,7 +184,7 @@ class ListDtype(ExtensionDtype):
         return hash(self._typ)
 
 
-class StructDtype(ExtensionDtype):
+class StructDtype(_BaseDtype):
 
     name = "struct"
 
@@ -231,7 +235,7 @@ class StructDtype(ExtensionDtype):
         return hash(self._typ)
 
 
-class Decimal64Dtype(ExtensionDtype):
+class Decimal64Dtype(_BaseDtype):
 
     name = "decimal"
     _metadata = ("precision", "scale")
