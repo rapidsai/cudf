51c7eadde7739a14f3cac2a0181404fb96e2e9a6
Jake Hemstad <jhemstad@nvidia.com>
2021-03-31 08:48:13 -0500

    See if unnamed variadic template parameter pack fixes the warning.

--- cpp/include/cudf/detail/scatter.cuh
+++ cpp/include/cudf/detail/scatter.cuh
@@ -81,7 +81,11 @@ auto scatter_to_gather(MapIterator scatter_map_begin,
 
 template <typename Element, typename Enable = void>
 struct column_scatterer_impl {
-  std::unique_ptr<column> operator()(...) const { CUDF_FAIL("Unsupported type for scatter."); }
+  template <typename...>
+  std::unique_ptr<column> operator()(...) const
+  {
+    CUDF_FAIL("Unsupported type for scatter.");
+  }
 };
 
 template <typename Element>
--- cpp/src/copying/copy.cu
+++ cpp/src/copying/copy.cu
@@ -31,7 +31,11 @@ namespace {
 
 template <typename T, typename Enable = void>
 struct copy_if_else_functor_impl {
-  std::unique_ptr<column> operator()(...) { CUDF_FAIL("Unsupported type for copy_if_else."); }
+  template <typename...>
+  std::unique_ptr<column> operator()(...)
+  {
+    CUDF_FAIL("Unsupported type for copy_if_else.");
+  }
 };
 
 template <typename T>
