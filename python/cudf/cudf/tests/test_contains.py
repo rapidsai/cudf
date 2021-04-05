7e4042eaeb83f7a99fa1a1066ba8bdeeec57c310
brandon-b-miller <brmiller@nvidia.com>
2021-04-05 14:41:55 -0700

    tests

--- python/cudf/cudf/tests/test_contains.py
+++ python/cudf/cudf/tests/test_contains.py
@@ -1,11 +1,13 @@
 from datetime import datetime as dt
 
+import numpy as np
 import pandas as pd
 import pytest
 
 from cudf import Series
 from cudf.core.index import RangeIndex, as_index
 from cudf.tests.utils import assert_eq
+from cudf.utils.dtypes import SCALAR_TYPES
 
 
 def cudf_date_series(start, stop, freq):
@@ -72,3 +74,21 @@ def test_index_contains(values, item, expected):
 def test_rangeindex_contains():
     assert_eq(True, 9 in RangeIndex(start=0, stop=10, name="Index"))
     assert_eq(False, 10 in RangeIndex(start=0, stop=10, name="Index"))
+
+
+@pytest.mark.parametrize("dtype", SCALAR_TYPES)
+def test_lists_contains(dtype):
+    dtype = np.dtype(dtype)
+    inner_data = np.array([1, 2, 3], dtype=dtype)
+    data = Series([inner_data])
+
+    if dtype.kind in "mM":
+        unit, _ = np.datetime_data(dtype)
+        contained_scalar = inner_data.dtype.type(2, unit)
+        not_contained_scalar = inner_data.dtype.type(42, unit)
+    else:
+        contained_scalar = inner_data.dtype.type(2)
+        not_contained_scalar = inner_data.dtype.type(42)
+
+    assert data.list.contains(contained_scalar)[0]
+    assert not data.list.contains(not_contained_scalar)[0]
