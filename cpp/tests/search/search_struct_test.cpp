63655d9ee339ad28184724d4948a5b270dae20dc
Nghia Truong <nghiatruong.vn@gmail.com>
2021-04-07 07:49:40 -0600

    Simplify StructSearchTests

--- cpp/tests/search/search_struct_test.cpp
+++ cpp/tests/search/search_struct_test.cpp
@@ -17,6 +17,7 @@
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_utilities.hpp>
 #include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/iterator_utilities.hpp>
 #include <cudf_test/type_lists.hpp>
 
 #include <cudf/detail/iterator.cuh>
@@ -60,6 +61,9 @@ auto search_bounds(std::unique_ptr<cudf::column> const& t_col,
   auto result_upper_bound = cudf::upper_bound(t, values, column_orders, null_precedence);
   return std::make_pair(std::move(result_lower_bound), std::move(result_upper_bound));
 }
+
+auto null_at(cudf::size_type idx) { return cudf::test::iterator_with_null_at(idx); }
+
 }  // namespace
 
 // Test case when all input columns are empty
@@ -107,22 +111,12 @@ TYPED_TEST(TypedStructSearchTest, SimpleInputWithNullsTests)
 {
   using col_wrapper = cudf::test::fixed_width_column_wrapper<TypeParam, int32_t>;
 
-  auto child_col_values =
-    col_wrapper{{1, null, 70, XXX, 2, 100},
-                cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })};
-  auto const structs_values = structs_col{
-    {child_col_values}, cudf::detail::make_counting_transform_iterator(0, [](auto i) {
-      return i != 3;
-    })}.release();
+  auto child_col_values     = col_wrapper{{1, null, 70, XXX, 2, 100}, null_at(1)};
+  auto const structs_values = structs_col{{child_col_values}, null_at(3)}.release();
 
   // Sorted asc, nulls first
-  auto child_col_t =
-    col_wrapper{{XXX, null, 0, 1, 2, 2, 2, 2, 3, 3, 4},
-                cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })};
-  auto structs_t = structs_col{
-    {child_col_t}, cudf::detail::make_counting_transform_iterator(0, [](auto i) {
-      return i != 0;
-    })}.release();
+  auto child_col_t = col_wrapper{{XXX, null, 0, 1, 2, 2, 2, 2, 3, 3, 4}, null_at(1)};
+  auto structs_t   = structs_col{{child_col_t}, null_at(0)}.release();
 
   auto results =
     search_bounds(structs_t, structs_values, {cudf::order::ASCENDING}, {cudf::null_order::BEFORE});
@@ -132,13 +126,8 @@ TYPED_TEST(TypedStructSearchTest, SimpleInputWithNullsTests)
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected_upper_bound, results.second->view(), print_all);
 
   // Sorted asc, nulls last
-  child_col_t =
-    col_wrapper{{0, 1, 2, 2, 2, 2, 3, 3, 4, null, XXX},
-                cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 9; })};
-  structs_t = structs_col{
-    {child_col_t}, cudf::detail::make_counting_transform_iterator(0, [](auto i) {
-      return i != 10;
-    })}.release();
+  child_col_t = col_wrapper{{0, 1, 2, 2, 2, 2, 3, 3, 4, null, XXX}, null_at(9)};
+  structs_t   = structs_col{{child_col_t}, null_at(10)}.release();
   results =
     search_bounds(structs_t, structs_values, {cudf::order::ASCENDING}, {cudf::null_order::AFTER});
   expected_lower_bound = int32s_col{1, 0, 10, 10, 2, 10};
@@ -147,13 +136,8 @@ TYPED_TEST(TypedStructSearchTest, SimpleInputWithNullsTests)
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected_upper_bound, results.second->view(), print_all);
 
   // Sorted dsc, nulls first
-  child_col_t =
-    col_wrapper{{XXX, null, 4, 3, 3, 2, 2, 2, 2, 1, 0},
-                cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })};
-  structs_t = structs_col{
-    {child_col_t}, cudf::detail::make_counting_transform_iterator(0, [](auto i) {
-      return i != 0;
-    })}.release();
+  child_col_t = col_wrapper{{XXX, null, 4, 3, 3, 2, 2, 2, 2, 1, 0}, null_at(1)};
+  structs_t   = structs_col{{child_col_t}, null_at(0)}.release();
   results =
     search_bounds(structs_t, structs_values, {cudf::order::DESCENDING}, {cudf::null_order::BEFORE});
   expected_lower_bound = int32s_col{9, 11, 0, 11, 5, 0};
@@ -162,13 +146,8 @@ TYPED_TEST(TypedStructSearchTest, SimpleInputWithNullsTests)
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected_upper_bound, results.second->view(), print_all);
 
   // Sorted dsc, nulls last
-  child_col_t =
-    col_wrapper{{4, 3, 3, 2, 2, 2, 2, 1, 0, null, XXX},
-                cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 9; })};
-  structs_t = structs_col{
-    {child_col_t}, cudf::detail::make_counting_transform_iterator(0, [](auto i) {
-      return i != 10;
-    })}.release();
+  child_col_t = col_wrapper{{4, 3, 3, 2, 2, 2, 2, 1, 0, null, XXX}, null_at(9)};
+  structs_t   = structs_col{{child_col_t}, null_at(10)}.release();
   results =
     search_bounds(structs_t, structs_values, {cudf::order::DESCENDING}, {cudf::null_order::AFTER});
   expected_lower_bound = int32s_col{7, 11, 0, 0, 3, 0};
@@ -184,27 +163,18 @@ TYPED_TEST(TypedStructSearchTest, ComplexStructTest)
 
   auto names_column_t =
     strings_col{"Cherry", "Kiwi", "Lemon", "Newton", "Tomato", /*NULL*/ "Washington"};
-  auto ages_column_t =
-    col_wrapper{{5, 10, 15, 20, null, XXX},
-                cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 4; })};
+  auto ages_column_t  = col_wrapper{{5, 10, 15, 20, null, XXX}, null_at(4)};
   auto is_human_col_t = bools_col{false, false, false, false, false, /*NULL*/ true};
 
-  auto const structs_t = structs_col{
-    {names_column_t, ages_column_t, is_human_col_t},
-    cudf::detail::make_counting_transform_iterator(0, [](auto i) {
-      return i != 5;
-    })}.release();
+  auto const structs_t =
+    structs_col{{names_column_t, ages_column_t, is_human_col_t}, null_at(5)}.release();
 
   auto names_column_values = strings_col{"Bagel", "Tomato", "Lemonade", /*NULL*/ "Donut", "Butter"};
-  auto ages_column_values =
-    col_wrapper{{10, 15, null, XXX, 17},
-                cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i != 1; })};
-  auto is_human_col_values  = bools_col{false, false, true, /*NULL*/ true, true};
-  auto const structs_values = structs_col{
-    {names_column_values, ages_column_values, is_human_col_values},
-    cudf::detail::make_counting_transform_iterator(0, [](auto i) {
-      return i != 3;
-    })}.release();
+  auto ages_column_values  = col_wrapper{{10, 15, null, XXX, 17}, null_at(1)};
+  auto is_human_col_values = bools_col{false, false, true, /*NULL*/ true, true};
+  auto const structs_values =
+    structs_col{{names_column_values, ages_column_values, is_human_col_values}, null_at(3)}
+      .release();
 
   auto const results =
     search_bounds(structs_t, structs_values, {cudf::order::ASCENDING}, {cudf::null_order::AFTER});
