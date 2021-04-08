a5d2407b93de444a6a7faf9db4b7dbf4ecbfe9ed
Christopher Harris <charris@nvidia.com>
2021-04-08 16:49:23 -0500

    jitify direct-to-cubin compilation and caching. (#7919)
    This changes jitify2 `get_kernel` invocations to pass an `-arch=sm_.` arg which causes jitify to compile and disk-cache direct-to-cubin, rather than caching ptx. This alleviates some compilation issues for specific (possibly unsupported) environments/configurations, but more importantly should provide faster launching from a cached kernel.
    
    Authors:
      - Christopher Harris (https://github.com/cwharris)
    
    Approvers:
      - MithunR (https://github.com/mythrocks)
      - Keith Kraus (https://github.com/kkraus14)
      - Devavret Makkar (https://github.com/devavret)
    
    URL: https://github.com/rapidsai/cudf/pull/7919

--- cpp/src/transform/transform.cpp
+++ cpp/src/transform/transform.cpp
@@ -55,9 +55,10 @@ void unary_operation(mutable_column_view output,
                                                    "GENERIC_UNARY_OP");
 
   cudf::jit::get_program_cache(*transform_jit_kernel_cu_jit)
-    .get_kernel(kernel_name, {}, {{"transform/jit/operation-udf.hpp", cuda_source}})  //
-    ->configure_1d_max_occupancy(0, 0, 0, stream.value())                             //
-    ->launch(output.size(),                                                           //
+    .get_kernel(
+      kernel_name, {}, {{"transform/jit/operation-udf.hpp", cuda_source}}, {"-arch=sm_."})  //
+    ->configure_1d_max_occupancy(0, 0, 0, stream.value())                                   //
+    ->launch(output.size(),                                                                 //
              cudf::jit::get_data_ptr(output),
              cudf::jit::get_data_ptr(input));
 }
