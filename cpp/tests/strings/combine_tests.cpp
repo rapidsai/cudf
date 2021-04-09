95dcdc2ec10d767e527af06b77894de4548fb3f0
Nghia Truong <nghiatruong.vn@gmail.com>
2021-04-09 11:35:12 -0600

    Fix AllNullsStringsInput test

--- cpp/tests/strings/combine_tests.cpp
+++ cpp/tests/strings/combine_tests.cpp
@@ -596,7 +596,7 @@ TEST_F(StringsListsConcatenateTest, AllNullsStringsInput)
   auto results = cudf::strings::concatenate(string_lv);
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected, print_all);
 
-  auto const separators = STR_COL{"+++"}.release();
+  auto const separators = STR_COL{{"", "", ""}, all_nulls()}.release();
   results               = cudf::strings::concatenate(string_lv, separators->view());
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected, print_all);
 }
