30405c52163b46210e00c5ab06b4278bbc40c745
Vibhu Jawa <vibhujawa@gmail.com>
2021-04-09 14:44:12 -0700

    minor bug fixes

--- python/cudf/cudf/core/subword_tokenizer.py
+++ python/cudf/cudf/core/subword_tokenizer.py
@@ -9,32 +9,26 @@ class subword_tokenizer:
 
     def __init__(self,
         hash_file: str,
-        max_length: int = 64,
-        stride: int = 48,
         do_lower: bool = True,
         do_truncate: bool = False,
-        max_rows_tensor: int = 500,
     ):
-        self.max_length = max_length
-        self.stride = stride
         self.do_lower = do_lower
         self.do_truncate = do_truncate
-        self.max_rows_tensor = max_rows_tensor
         self.vocab_file = cpp_hashed_vocabulary(hash_file) 
 
     
-    def encode(self, str_series):
+    def encode(self, str_series, max_length,stride,max_rows_tensor):
         tokens, masks, metadata = cpp_subword_tokenize(
             str_series._column,
             self.vocab_file,
-            self.max_length,
-            self.stride,
+            max_length,
+            stride,
             self.do_lower,
             self.do_truncate,
-            self.max_rows_tensor,
+            max_rows_tensor,
         )
         return (
-            cupy.asarray(tokens).reshape(-1, self.max_length),
-            cupy.asarray(masks).reshape(-1, self.max_length),
+            cupy.asarray(tokens).reshape(-1, max_length),
+            cupy.asarray(masks).reshape(-1, max_length),
             cupy.asarray(metadata).reshape(-1, 3),
         ) 
