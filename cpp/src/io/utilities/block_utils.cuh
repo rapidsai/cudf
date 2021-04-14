7fb6d780539dd6e0fe3a49e6af040d4a90f6bcbc
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2021-04-13 17:45:28 -0700

    fix formatting

--- cpp/src/io/utilities/block_utils.cuh
+++ cpp/src/io/utilities/block_utils.cuh
@@ -39,7 +39,7 @@ inline __device__ uint32_t ballot(int pred) { return __ballot_sync(~0, pred); }
 template <typename T>
 inline __device__ void nanosleep(T d)
 {
-  // TODO: nanosleep temporarily disabled 
+  // TODO: nanosleep temporarily disabled
   clock();
 }
 
