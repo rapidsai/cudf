0843cd78f340673d258992ffa55f6fc3f1864b9a
Vibhu Jawa <vibhujawa@gmail.com>
2021-04-14 20:50:33 -0700

    test cleanup

--- python/cudf/cudf/tests/test_subword_tokenizer.py
+++ python/cudf/cudf/tests/test_subword_tokenizer.py
@@ -1,16 +1,29 @@
-
-
+# Copyright (c) 2020-2021, NVIDIA CORPORATION.
 from transformers import BertTokenizer
+import pytest
+import os
+
+
+@pytest.fixture(scope="module")
+def datadir(datadir):
+    return os.path.join(datadir, "subword_tokenizer_data")
 
 
 def assert_equal_tokenization_outputs(hf_output,cudf_output):
     assert np.sum(hf_output['input_ids']!=cudf_output['input_ids'].get())==0
     assert np.sum(hf_output['attention_mask']!=cudf_output['attention_mask'].get())==0
 
+@pytest.mark.parametrize("seq_len", [32,64])
+@pytest.mark.parametrize("stride", [0,15,32])
+@pytest.mark.parametrize("add_special_tokens", [True, False])
+def test_subword_tokenize(seq_len,stride,add_special_tokens,datadir):
+    with open(os.path.join(datadir,'test_sentences.txt')) as file:
+        input_sentence_ls = [line.strip() for line in file]
+    
 
-def test_subword_tokenize():
     vocab_dir = 'data/bert_base_uncased_sampled'
     hf_tokenizer = BertTokenizerFast.from_pretrained(vocab_dir, do_lower_case)
+
 
     hf_output = hf_tokenizer(input_sentence_ls,
                              max_length=seq_len,
@@ -22,6 +35,7 @@ def test_subword_tokenize():
                             )
 
     
+    str_series = cudf.Series(str_series)
     cudf_tokenizer  = SubwordTokenizer('bert_base_uncased_sampled/vocab-hash.txt', do_lower_case)
     cudf_output = cudf_tokenizer(str_series,
                      max_length=seq_len,
