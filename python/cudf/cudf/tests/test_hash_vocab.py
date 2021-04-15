cecfaa4ed741d11438c96c5add9fed52ca7f096f
Vibhu Jawa <vibhujawa@gmail.com>
2021-04-15 15:12:51 -0700

    fixed style issues

--- python/cudf/cudf/tests/test_hash_vocab.py
+++ python/cudf/cudf/tests/test_hash_vocab.py
@@ -7,16 +7,16 @@ import pytest
 
 @pytest.fixture(scope="module")
 def datadir(datadir):
-    return os.path.join(datadir, "subword_tokenizer_data", "bert_base_cased_sampled")
+    return os.path.join(
+        datadir, "subword_tokenizer_data", "bert_base_cased_sampled"
+    )
 
 
 def test_correct_bert_base_vocab_hash(datadir, tmpdir):
     # The vocabulary is drawn from bert-base-cased
     vocab_path = os.path.join(datadir, "vocab.txt")
 
-    groundtruth_path = os.path.join(
-        datadir, "vocab-hash.txt"
-    )
+    groundtruth_path = os.path.join(datadir, "vocab-hash.txt")
     output_path = tmpdir.join("cudf-vocab-hash.txt")
     hash_vocab(vocab_path, output_path)
 
