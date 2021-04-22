b7330821ed6dcbb0388461a4aae30673ad796c92
Michael Wang <isVoid@users.noreply.github.com>
2021-04-22 16:53:58 -0700

    Add `list_scalar` API (#7584)
    Closes #5887
    
    This PR adds `list_scalar`, a scalar for a `LIST` type column. A `list_scalar` contains a validity field and a `_data` field. The validity field describe whether the list_scalar is valid as a whole, and is accessible via derived `is_valid` member function. The `_data` field is a `cudf::column`, owned by the scalar class. The data column can be either nested or non-nested type, depending on what `LIST` column it originate from. 
    
    For example, a scalar from a `List<int>` column contains a `int` column:
    ```
    C = List<int> column: {{1, 2, 3}, {4}, {5, 6}}
    C[0] = int column: {1, 2, 3}
    ```
    
    A scalar from a `List<List<int>>` column would contain a `List<int>` column instead:
    ```
    C = List<List<int>> column: {{{1, 2}, {}}, {{3}}, {{4, 5}, {6}}}
    C[2] = List<int> column: {{4, 5}, {6}}
    ```
    
    `list_scalar` has a `view` method, which returns a immutable view to the underlying data.
    
    Authors:
      - Michael Wang (https://github.com/isVoid)
      - https://github.com/brandon-b-miller
    
    Approvers:
      - Mark Harris (https://github.com/harrism)
      - Devavret Makkar (https://github.com/devavret)
      - Nghia Truong (https://github.com/ttnghia)
    
    URL: https://github.com/rapidsai/cudf/pull/7584

--- cpp/tests/scalar/scalar_test.cpp
+++ cpp/tests/scalar/scalar_test.cpp
@@ -18,6 +18,7 @@
 #include <cudf/types.hpp>
 #include <cudf/utilities/type_dispatcher.hpp>
 #include <cudf_test/base_fixture.hpp>
+#include <cudf_test/column_wrapper.hpp>
 #include <cudf_test/cudf_gtest.hpp>
 #include <cudf_test/type_list_utilities.hpp>
 #include <cudf_test/type_lists.hpp>
@@ -136,6 +137,86 @@ TEST_F(StringScalarTest, MoveConstructor)
 
   EXPECT_EQ(mask_ptr, s2.validity_data());
   EXPECT_EQ(data_ptr, s2.data());
+}
+
+struct ListScalarTest : public cudf::test::BaseFixture {
+};
+
+TEST_F(ListScalarTest, DefaultValidityNonNested)
+{
+  auto data = cudf::test::fixed_width_column_wrapper<int32_t>{1, 2, 3};
+  auto s    = cudf::list_scalar(data);
+
+  EXPECT_TRUE(s.is_valid());
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(data, s.view());
+}
+
+TEST_F(ListScalarTest, DefaultValidityNested)
+{
+  auto data = cudf::test::lists_column_wrapper<int32_t>{{1, 2}, {2}, {}, {4, 5}};
+  auto s    = cudf::list_scalar(data);
+
+  EXPECT_TRUE(s.is_valid());
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(data, s.view());
+}
+
+TEST_F(ListScalarTest, ConstructNull)
+{
+  auto s = cudf::list_scalar();
+
+  EXPECT_FALSE(s.is_valid());
+}
+
+TEST_F(ListScalarTest, CopyConstructorNonNested)
+{
+  auto data = cudf::test::fixed_width_column_wrapper<int32_t>{1, 2, 3};
+  auto s    = cudf::list_scalar(data);
+  auto s2   = s;
+
+  EXPECT_TRUE(s2.is_valid());
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(data, s2.view());
+  EXPECT_NE(s.view().data<int32_t>(), s2.view().data<int32_t>());
+}
+
+TEST_F(ListScalarTest, CopyConstructorNested)
+{
+  auto data = cudf::test::lists_column_wrapper<int32_t>{{1, 2}, {2}, {}, {4, 5}};
+  auto s    = cudf::list_scalar(data);
+  auto s2   = s;
+
+  EXPECT_TRUE(s2.is_valid());
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(data, s2.view());
+  EXPECT_NE(s.view().child(0).data<int32_t>(), s2.view().child(0).data<int32_t>());
+  EXPECT_NE(s.view().child(1).data<int32_t>(), s2.view().child(1).data<int32_t>());
+}
+
+TEST_F(ListScalarTest, MoveConstructorNonNested)
+{
+  auto data     = cudf::test::fixed_width_column_wrapper<int32_t>{1, 2, 3};
+  auto s        = cudf::list_scalar(data);
+  auto data_ptr = s.view().data<int32_t>();
+  auto mask_ptr = s.validity_data();
+  decltype(s) s2(std::move(s));
+
+  EXPECT_EQ(mask_ptr, s2.validity_data());
+  EXPECT_EQ(data_ptr, s2.view().data<int32_t>());
+  EXPECT_EQ(s.view().data<int32_t>(), nullptr);
+}
+
+TEST_F(ListScalarTest, MoveConstructorNested)
+{
+  auto data       = cudf::test::lists_column_wrapper<int32_t>{{1, 2}, {2}, {}, {4, 5}};
+  auto s          = cudf::list_scalar(data);
+  auto offset_ptr = s.view().child(0).data<cudf::size_type>();
+  auto data_ptr   = s.view().child(1).data<int32_t>();
+  auto mask_ptr   = s.validity_data();
+  decltype(s) s2(std::move(s));
+
+  EXPECT_EQ(mask_ptr, s2.validity_data());
+  EXPECT_EQ(offset_ptr, s2.view().child(0).data<cudf::size_type>());
+  EXPECT_EQ(data_ptr, s2.view().child(1).data<int32_t>());
+  EXPECT_EQ(s.view().data<int32_t>(), nullptr);
+  EXPECT_EQ(s.view().num_children(), 0);
 }
 
 CUDF_TEST_PROGRAM_MAIN()
