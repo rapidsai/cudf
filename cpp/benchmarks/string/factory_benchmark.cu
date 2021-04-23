6ce66434c416ba3f7215cac0642fbc0ae1db2e23
Mark Harris <mharris@nvidia.com>
2021-04-23 13:30:58 +1000

    Fix benchmark compilation in debug build

--- cpp/benchmarks/string/factory_benchmark.cu
+++ cpp/benchmarks/string/factory_benchmark.cu
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 
+#include "rmm/cuda_stream_view.hpp"
 #include "string_bench_args.hpp"
 
 #include <benchmark/benchmark.h>
@@ -55,7 +56,7 @@ static void BM_factory(benchmark::State& state)
   auto const table =
     create_random_table({cudf::type_id::STRING}, 1, row_count{n_rows}, table_profile);
   auto d_column = cudf::column_device_view::create(table->view().column(0));
-  rmm::device_vector<string_pair> pairs(d_column->size());
+  rmm::device_uvector<string_pair> pairs(d_column->size(), rmm::cuda_stream_default);
   thrust::transform(thrust::device,
                     d_column->pair_begin<cudf::string_view, true>(),
                     d_column->pair_end<cudf::string_view, true>(),
