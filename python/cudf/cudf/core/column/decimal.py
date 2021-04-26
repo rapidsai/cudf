94afddafe156c0e25c235959e59872e2ecf07cf7
brandon-b-miller <53796099+brandon-b-miller@users.noreply.github.com>
2021-04-26 16:25:01 -0500

    Adds serialization of Decimal Columns and dtypes (#8041)
    Fixes https://github.com/rapidsai/cudf/issues/8034
    
    Authors:
      - https://github.com/brandon-b-miller
    
    Approvers:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
      - Keith Kraus (https://github.com/kkraus14)
    
    URL: https://github.com/rapidsai/cudf/pull/8041

--- python/cudf/cudf/core/column/decimal.py
+++ python/cudf/cudf/core/column/decimal.py
@@ -1,20 +1,20 @@
 # Copyright (c) 2021, NVIDIA CORPORATION.
 
 from decimal import Decimal
-from typing import cast, Any, Sequence, Union
+from numbers import Number
+from typing import Any, Sequence, Tuple, Union, cast
 
 import cupy as cp
 import numpy as np
 import pyarrow as pa
 from pandas.api.types import is_integer_dtype
-from numbers import Number
 
 import cudf
 from cudf import _lib as libcudf
+from cudf._lib.quantiles import quantile as cpp_quantile
 from cudf._lib.strings.convert.convert_fixed_point import (
     from_decimal as cpp_from_decimal,
 )
-from cudf._lib.quantiles import quantile as cpp_quantile
 from cudf._typing import Dtype
 from cudf.core.buffer import Buffer
 from cudf.core.column import ColumnBase, as_column
@@ -228,6 +228,18 @@ class DecimalColumn(ColumnBase):
             input_col=self, replacement=value, method=method, dtype=dtype
         )
         return self._copy_type_metadata(result)
+
+    def serialize(self) -> Tuple[dict, list]:
+        header, frames = super().serialize()
+        header["dtype"] = self.dtype.serialize()
+        header["size"] = self.size
+        return header, frames
+
+    @classmethod
+    def deserialize(cls, header: dict, frames: list) -> ColumnBase:
+        dtype = cudf.Decimal64Dtype.deserialize(*header["dtype"])
+        header["dtype"] = dtype
+        return super().deserialize(header, frames)
 
 
 def _binop_scale(l_dtype, r_dtype, op):
