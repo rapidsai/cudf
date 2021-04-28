e6c5ad2dad32823449f53c6d4999cbe6f6e49ed7
Mark Harris <mharris@nvidia.com>
2021-04-28 14:12:27 +1000

    Add contains benchmark

--- cpp/benchmarks/search/search_benchmark.cu
+++ cpp/benchmarks/search/search_benchmark.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2021, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,35 +31,6 @@
 class Search : public cudf::benchmark {
 };
 
-void BM_non_null_column(benchmark::State& state)
-{
-  const cudf::size_type column_size{(cudf::size_type)state.range(0)};
-  const cudf::size_type values_size = column_size;
-
-  auto col_data_it = cudf::detail::make_counting_transform_iterator(
-    0, [=](cudf::size_type row) { return static_cast<float>(row); });
-  auto val_data_it = cudf::detail::make_counting_transform_iterator(
-    0, [=](cudf::size_type row) { return static_cast<float>(values_size - row); });
-
-  cudf::test::fixed_width_column_wrapper<float> column(col_data_it, col_data_it + column_size);
-  cudf::test::fixed_width_column_wrapper<float> values(val_data_it, val_data_it + values_size);
-
-  for (auto _ : state) {
-    cuda_event_timer timer(state, true);
-    auto col = cudf::upper_bound(cudf::table_view({column}),
-                                 cudf::table_view({values}),
-                                 {cudf::order::ASCENDING},
-                                 {cudf::null_order::BEFORE});
-  }
-}
-
-BENCHMARK_DEFINE_F(Search, AllValidColumn)(::benchmark::State& state) { BM_non_null_column(state); }
-
-BENCHMARK_REGISTER_F(Search, AllValidColumn)
-  ->UseManualTime()
-  ->Unit(benchmark::kMillisecond)
-  ->Arg(100000000);
-
 auto make_validity_iter()
 {
   static constexpr int r_min = 1;
@@ -71,7 +42,7 @@ auto make_validity_iter()
     0, [mod_base](auto row) { return (row % mod_base) > 0; });
 }
 
-void BM_nullable_column(benchmark::State& state)
+void BM_column(benchmark::State& state, bool nulls)
 {
   const cudf::size_type column_size{(cudf::size_type)state.range(0)};
   const cudf::size_type values_size = column_size;
@@ -81,25 +52,39 @@ void BM_nullable_column(benchmark::State& state)
   auto val_data_it = cudf::detail::make_counting_transform_iterator(
     0, [=](cudf::size_type row) { return static_cast<float>(values_size - row); });
 
-  cudf::test::fixed_width_column_wrapper<float> column(
-    col_data_it, col_data_it + column_size, make_validity_iter());
-  cudf::test::fixed_width_column_wrapper<float> values(
-    val_data_it, val_data_it + values_size, make_validity_iter());
+  auto column = [&]() {
+    return nulls ? cudf::test::fixed_width_column_wrapper<float>(
+                     col_data_it, col_data_it + column_size, make_validity_iter())
+                 : cudf::test::fixed_width_column_wrapper<float>(col_data_it,
+                                                                 col_data_it + column_size);
+  }();
+  auto values = [&]() {
+    return nulls ? cudf::test::fixed_width_column_wrapper<float>(
+                     val_data_it, val_data_it + values_size, make_validity_iter())
+                 : cudf::test::fixed_width_column_wrapper<float>(val_data_it,
+                                                                 val_data_it + values_size);
+  }();
 
-  auto sorted = cudf::sort(cudf::table_view({column}));
+  auto data_table = cudf::sort(cudf::table_view({column}));
 
   for (auto _ : state) {
     cuda_event_timer timer(state, true);
-    auto col = cudf::upper_bound(sorted->view(),
+    auto col = cudf::upper_bound(data_table->view(),
                                  cudf::table_view({values}),
                                  {cudf::order::ASCENDING},
                                  {cudf::null_order::BEFORE});
   }
 }
 
-BENCHMARK_DEFINE_F(Search, NullableColumn)(::benchmark::State& state) { BM_nullable_column(state); }
+BENCHMARK_DEFINE_F(Search, Column_AllValid)(::benchmark::State& state) { BM_column(state, false); }
+BENCHMARK_DEFINE_F(Search, Column_Nulls)(::benchmark::State& state) { BM_column(state, true); }
 
-BENCHMARK_REGISTER_F(Search, NullableColumn)
+BENCHMARK_REGISTER_F(Search, Column_AllValid)
+  ->UseManualTime()
+  ->Unit(benchmark::kMillisecond)
+  ->Arg(100000000);
+
+BENCHMARK_REGISTER_F(Search, Column_Nulls)
   ->UseManualTime()
   ->Unit(benchmark::kMillisecond)
   ->Arg(100000000);
@@ -153,3 +138,53 @@ BENCHMARK_REGISTER_F(Search, Table)
   ->UseManualTime()
   ->Unit(benchmark::kMillisecond)
   ->Apply(CustomArguments);
+
+void BM_contains(benchmark::State& state, bool nulls)
+{
+  const cudf::size_type column_size{(cudf::size_type)state.range(0)};
+  const cudf::size_type values_size = column_size;
+
+  auto col_data_it = cudf::detail::make_counting_transform_iterator(
+    0, [=](cudf::size_type row) { return static_cast<float>(row); });
+  auto val_data_it = cudf::detail::make_counting_transform_iterator(
+    0, [=](cudf::size_type row) { return static_cast<float>(values_size - row); });
+
+  auto column = [&]() {
+    return nulls ? cudf::test::fixed_width_column_wrapper<float>(
+                     col_data_it, col_data_it + column_size, make_validity_iter())
+                 : cudf::test::fixed_width_column_wrapper<float>(col_data_it,
+                                                                 col_data_it + column_size);
+  }();
+  auto values = [&]() {
+    return nulls ? cudf::test::fixed_width_column_wrapper<float>(
+                     val_data_it, val_data_it + values_size, make_validity_iter())
+                 : cudf::test::fixed_width_column_wrapper<float>(val_data_it,
+                                                                 val_data_it + values_size);
+  }();
+
+  for (auto _ : state) {
+    cuda_event_timer timer(state, true);
+    auto col = cudf::contains(column, values);
+  }
+}
+
+BENCHMARK_DEFINE_F(Search, ColumnContains_AllValid)(::benchmark::State& state)
+{
+  BM_column(state, false);
+}
+BENCHMARK_DEFINE_F(Search, ColumnContains_Nulls)(::benchmark::State& state)
+{
+  BM_column(state, true);
+}
+
+BENCHMARK_REGISTER_F(Search, ColumnContains_AllValid)
+  ->RangeMultiplier(8)
+  ->Ranges({{1 << 10, 1 << 26}})
+  ->UseManualTime()
+  ->Unit(benchmark::kMillisecond);
+
+BENCHMARK_REGISTER_F(Search, ColumnContains_Nulls)
+  ->RangeMultiplier(8)
+  ->Ranges({{1 << 10, 1 << 26}})
+  ->UseManualTime()
+  ->Unit(benchmark::kMillisecond);
