09ac333e1a1efe5ca8f5c1ad20bfef6d9b49716d
Nghia Truong <nghiatruong.vn@gmail.com>
2021-04-28 13:17:24 -0600

    Rename variables, switch to use `thrust::any_of` and `thrust::all_of`, and many other improvements

--- cpp/src/lists/concatenate_rows.cu
+++ cpp/src/lists/concatenate_rows.cu
@@ -29,7 +29,9 @@
 #include <rmm/exec_policy.hpp>
 
 #include <thrust/copy.h>
+#include <thrust/logical.h>
 #include <thrust/transform.h>
+#include <thrust/transform_reduce.h>
 
 namespace cudf {
 namespace lists {
@@ -40,17 +42,19 @@ std::unique_ptr<column> concatenate_rows_ignore_null(table_view const& input,
                                                      rmm::cuda_stream_view stream,
                                                      rmm::mr::device_memory_resource* mr)
 {
-  auto const num_cols         = input.num_columns();
   auto const num_output_lists = input.num_rows();
   auto const table_dv_ptr     = table_device_view::create(input);
-  auto interleaved_columns    = detail::interleave_columns(input, has_null_mask, stream);
+
+  // Interleave the list element from the input table, thus all the lists at the same row now stay
+  // next to each other.
+  auto interleaved_columns = detail::interleave_columns(input, has_null_mask, stream);
 
   // Modify the list offsets to combine lists of the same input row.
   static_assert(sizeof(offset_type) == sizeof(int32_t));
   static_assert(sizeof(size_type) == sizeof(int32_t));
   auto list_offsets = make_numeric_column(
     data_type{type_id::INT32}, num_output_lists + 1, mask_state::UNALLOCATED, stream, mr);
-  auto const d_offsets = list_offsets->mutable_view().begin<offset_type>();
+  auto const d_offsets = list_offsets->mutable_view().template begin<offset_type>();
 
   // The array of int8_t to store validities for list elements.
   // Since we combine multiple lists, we need to recompute list validities.
@@ -60,49 +64,42 @@ std::unique_ptr<column> concatenate_rows_ignore_null(table_view const& input,
   // [ i_0, i_1, ..., i_n, i_n+1, ..., i_2n, ... ] then to concatenate them just modify the offsets
   // to be [ i_0, i_n, i_2n, i_3n, ... ].
   auto const d_interleaved_offsets = lists_column_view(interleaved_columns->view()).offsets_begin();
-  thrust::transform(rmm::exec_policy(stream),
-                    thrust::make_counting_iterator<size_type>(0),
-                    thrust::make_counting_iterator<size_type>(num_output_lists + 1),
-                    d_offsets,
-                    [num_cols,
-                     d_interleaved_offsets,
-                     table_dv     = *table_dv_ptr,
-                     d_validities = validities.begin(),
-                     has_null_mask] __device__(auto const idx) {
-                      if (has_null_mask) {
-                        bool is_valid{false};
-                        for (size_type col_id = 0; col_id < num_cols; ++col_id) {
-                          auto const& lists_col = table_dv.column(col_id);
-                          if (lists_col.is_valid(idx)) {
-                            is_valid = true;
-                            break;
-                          }
-                        }
-                        d_validities[idx] = static_cast<int8_t>(is_valid);
-                      }
-                      return d_interleaved_offsets[idx * num_cols];
-                    });
+  thrust::transform(
+    rmm::exec_policy(stream),
+    thrust::make_counting_iterator<size_type>(0),
+    thrust::make_counting_iterator<size_type>(num_output_lists + 1),
+    d_offsets,
+    [d_interleaved_offsets,
+     num_cols     = input.num_columns(),
+     table_dv     = *table_dv_ptr,
+     d_validities = validities.begin(),
+     has_null_mask] __device__(auto const idx) {
+      if (has_null_mask) {
+        auto const any_valid = thrust::any_of(
+          thrust::seq, table_dv.begin(), table_dv.end(), [idx](auto const& list_col) {
+            return list_col.is_valid(idx);
+          });
+        d_validities[idx] = static_cast<int8_t>(any_valid);
+      }
+      return d_interleaved_offsets[idx * num_cols];
+    });
+
+  auto [null_mask, null_count] = [&] {
+    return has_null_mask
+             ? cudf::detail::valid_if(
+                 validities.begin(), validities.end(), thrust::identity<int8_t>{}, stream, mr)
+             : std::make_pair(rmm::device_buffer{}, size_type{0});
+  }();
 
   // The child column containing list entries is taken from the `interleaved_columns` column.
   auto interleaved_columns_content = interleaved_columns->release();
-
-  rmm::device_buffer null_mask{};
-  size_type null_count{0};
-  if (has_null_mask) {
-    std::tie(null_mask, null_count) = cudf::detail::valid_if(
-      validities.begin(), validities.end(), thrust::identity<int8_t>{}, stream, mr);
-    if (null_count == 0) {
-      // The output from `valid_if` is always a full-size buffer.
-      null_mask = rmm::device_buffer{};
-    }
-  }
 
   return make_lists_column(
     num_output_lists,
     std::move(list_offsets),
     std::move(interleaved_columns_content.children[lists_column_view::child_column_index]),
     null_count,
-    std::move(null_mask),
+    null_count > 0 ? std::move(null_mask) : rmm::device_buffer{},
     stream,
     mr);
 }
@@ -111,15 +108,13 @@ std::unique_ptr<column> concatenate_rows_ignore_null(table_view const& input,
  * @brief Generate list offsets and list validities for the output lists column from the table_view
  * of the input lists columns.
  *
- * This function is called only when has_null_mask == true and
- * null_policy == concatenate_null_policy::NULLIFY_OUTPUT_ROW.
+ * This function is called only when (has_null_mask == true and null_policy == NULLIFY_OUTPUT_ROW).
  */
 std::pair<std::unique_ptr<column>, rmm::device_uvector<int8_t>>
 generate_list_offsets_and_validities(table_view const& input,
                                      rmm::cuda_stream_view stream,
                                      rmm::mr::device_memory_resource* mr)
 {
-  auto const num_cols         = input.num_columns();
   auto const num_output_lists = input.num_rows();
   auto const table_dv_ptr     = table_device_view::create(input);
 
@@ -128,7 +123,7 @@ generate_list_offsets_and_validities(table_view const& input,
   static_assert(sizeof(size_type) == sizeof(int32_t));
   auto list_offsets = make_numeric_column(
     data_type{type_id::INT32}, num_output_lists + 1, mask_state::UNALLOCATED, stream, mr);
-  auto const d_offsets = list_offsets->mutable_view().begin<offset_type>();
+  auto const d_offsets = list_offsets->mutable_view().template begin<offset_type>();
 
   // The array of int8_t to store validities for list elements.
   auto validities = rmm::device_uvector<int8_t>(num_output_lists, stream);
@@ -139,24 +134,27 @@ generate_list_offsets_and_validities(table_view const& input,
     thrust::make_counting_iterator<size_type>(0),
     thrust::make_counting_iterator<size_type>(num_output_lists),
     d_offsets,
-    [num_cols, table_dv = *table_dv_ptr, d_validities = validities.begin()] __device__(
-      size_type const idx) {
-      size_type list_size{0};
-      bool is_valid{true};
-      for (size_type col_id = 0; col_id < num_cols; ++col_id) {
-        auto const& lists_col = table_dv.column(col_id);
-        if (lists_col.is_null(idx)) {
-          is_valid  = false;
-          list_size = 0;
-          break;
-        }
-        auto const list_offsets =
-          lists_col.child(lists_column_view::offsets_column_index).data<size_type>() +
-          lists_col.offset();
-        list_size += list_offsets[idx + 1] - list_offsets[idx];
-      }
-      d_validities[idx] = static_cast<int8_t>(is_valid);
-      return list_size;
+    [table_dv = *table_dv_ptr, d_validities = validities.begin()] __device__(size_type const idx) {
+      auto const all_valid =
+        thrust::all_of(thrust::seq, table_dv.begin(), table_dv.end(), [idx](auto const& list_col) {
+          return list_col.is_valid(idx);
+        });
+      d_validities[idx] = static_cast<int8_t>(all_valid);
+      if (not all_valid) return size_type{0};
+
+      // Compute size of the output list as sum of sizes of input lists
+      return thrust::transform_reduce(
+        thrust::seq,
+        table_dv.begin(),
+        table_dv.end(),
+        [idx] __device__(auto const& lists_col) {
+          auto const list_offsets =
+            lists_col.child(lists_column_view::offsets_column_index).template data<size_type>() +
+            lists_col.offset();
+          return list_offsets[idx + 1] - list_offsets[idx];  // list size
+        },
+        size_type{0},
+        thrust::plus<size_type>{});
     });
 
   // Compute offsets from sizes.
@@ -169,9 +167,10 @@ generate_list_offsets_and_validities(table_view const& input,
 /**
  * @brief Compute string sizes, string validities, and concatenate string lists functor.
  *
- * This functor is executed twice. In the first pass, the sizes and validities of the output strings
- * will be computed. In the second pass, this will concatenate the lists of strings of the given
- * table of lists columns in a row-wise manner.
+ * This functor is called only when (has_null_mask == true and null_policy == NULLIFY_OUTPUT_ROW).
+ * It is executed twice. In the first pass, the sizes and validities of the output strings will be
+ * computed. In the second pass, this will concatenate the lists of strings of the given table of
+ * lists columns in a row-wise manner.
  */
 struct compute_string_sizes_and_concatenate_lists_fn {
   table_device_view const table_dv;
@@ -179,7 +178,7 @@ struct compute_string_sizes_and_concatenate_lists_fn {
   // Store list offsets of the output lists column.
   offset_type const* const dst_list_offsets;
 
-  // Store offsets of the strings in the chars column.
+  // Store offsets of the strings.
   offset_type* d_offsets{nullptr};
 
   // If d_chars == nullptr: only compute sizes and validities of the output strings.
@@ -195,43 +194,40 @@ struct compute_string_sizes_and_concatenate_lists_fn {
     // computation.
     if (dst_list_offsets[idx + 1] == dst_list_offsets[idx]) { return; }
 
-    auto const num_cols = table_dv.num_columns();
-
     // read_idx and write_idx are indices of string elements.
     size_type write_idx = dst_list_offsets[idx];
-    for (size_type col_id = 0; col_id < num_cols; ++col_id) {
-      auto const& lists_col = table_dv.column(col_id);
-      assert(lists_col.is_valid(idx));
+    thrust::for_each(
+      thrust::seq, table_dv.begin(), table_dv.end(), [&] __device__(auto const& lists_col) {
+        auto const list_offsets =
+          lists_col.child(lists_column_view::offsets_column_index).template data<offset_type>() +
+          lists_col.offset();
+        auto const& str_col = lists_col.child(lists_column_view::child_column_index);
+        auto const str_offsets =
+          str_col.child(strings_column_view::offsets_column_index).template data<offset_type>();
 
-      auto const list_offsets =
-        lists_col.child(lists_column_view::offsets_column_index).data<size_type>() +
-        lists_col.offset();
-      auto const& child = lists_col.child(lists_column_view::child_column_index);
-      auto const child_offsets =
-        child.child(strings_column_view::offsets_column_index).data<size_type>() + child.offset();
+        // The indices of the strings within the source list.
+        auto const start_str_idx = list_offsets[idx];
+        auto const end_str_idx   = list_offsets[idx + 1];
 
-      if (not d_chars) {  // just compute sizes of strings within a list
-        for (auto read_idx = list_offsets[idx], end_idx = list_offsets[idx + 1]; read_idx < end_idx;
-             ++read_idx, ++write_idx) {
-          d_validities[write_idx] = static_cast<int8_t>(child.is_valid(read_idx));
-          d_offsets[write_idx]    = child_offsets[read_idx + 1] - child_offsets[read_idx];
+        if (not d_chars) {  // just compute sizes of strings within a list
+          for (auto read_idx = start_str_idx; read_idx < end_str_idx; ++read_idx, ++write_idx) {
+            d_validities[write_idx] = static_cast<int8_t>(str_col.is_valid(read_idx));
+            d_offsets[write_idx]    = str_offsets[read_idx + 1] - str_offsets[read_idx];
+          }
+        } else {  // just copy the entire memory region containing all strings in the list
+          // start_byte and end_byte are indices of character of the string elements.
+          auto const start_byte = str_offsets[start_str_idx];
+          auto const end_byte   = str_offsets[end_str_idx];
+          if (start_byte < end_byte) {
+            auto const input_ptr =
+              str_col.child(strings_column_view::chars_column_index).template data<char>() +
+              start_byte;
+            auto const output_ptr = d_chars + d_offsets[write_idx];
+            thrust::copy(thrust::seq, input_ptr, input_ptr + end_byte - start_byte, output_ptr);
+            write_idx += end_str_idx - start_str_idx;
+          }
         }
-      } else {  // just copy the entire memory region containing all strings in the list
-        // start_idx and end_idx are indices of string elements.
-        // start_byte and end_byte are indices of character of the string elements.
-        auto const start_idx  = list_offsets[idx];
-        auto const end_idx    = list_offsets[idx + 1];
-        auto const start_byte = child_offsets[start_idx];
-        auto const end_byte   = child_offsets[end_idx];
-        if (start_byte < end_byte) {
-          auto const input_ptr = child.child(strings_column_view::chars_column_index).data<char>() +
-                                 child.offset() + start_byte;
-          auto const output_ptr = d_chars + d_offsets[write_idx];
-          thrust::copy(thrust::seq, input_ptr, input_ptr + end_byte - start_byte, output_ptr);
-          write_idx += end_idx - start_idx;
-        }
-      }
-    }
+      });
   }
 };
 
@@ -239,8 +235,7 @@ struct compute_string_sizes_and_concatenate_lists_fn {
  * @brief Struct used in type_dispatcher to interleave list entries of the input lists columns and
  * output the results into a destination column.
  *
- * This functor is called only when has_null_mask == true and
- * null_policy == concatenate_null_policy::NULLIFY_OUTPUT_ROW.
+ * This functor is called only when (has_null_mask == true and null_policy == NULLIFY_OUTPUT_ROW).
  */
 struct concatenate_lists_fn {
   template <class T>
@@ -254,10 +249,9 @@ struct concatenate_lists_fn {
   {
     auto const table_dv_ptr = table_device_view::create(input);
     auto const comp_fn      = compute_string_sizes_and_concatenate_lists_fn{
-      *table_dv_ptr, output_list_offsets.begin<offset_type>()};
+      *table_dv_ptr, output_list_offsets.template begin<offset_type>()};
 
-    // Cannot call `cudf::strings::detail::make_strings_children` because that function executes the
-    // functor `comp_fn` on the range equal to `num_output_entries`.
+    // Generate a null mask because the input table has nullable column.
     auto [offsets_column, chars_column, null_mask, null_count] =
       cudf::strings::detail::make_strings_children_with_null_mask(
         comp_fn, num_output_lists, num_output_entries, stream, mr);
@@ -280,8 +274,6 @@ struct concatenate_lists_fn {
     rmm::cuda_stream_view stream,
     rmm::mr::device_memory_resource* mr) const noexcept
   {
-    auto const num_cols     = input.num_columns();
-    auto const num_rows     = input.num_rows();
     auto const table_dv_ptr = table_device_view::create(input);
 
     // The output child column.
@@ -297,34 +289,34 @@ struct concatenate_lists_fn {
       rmm::exec_policy(stream),
       thrust::make_counting_iterator<size_type>(0),
       num_output_lists,
-      [num_cols,
+      [num_cols         = input.num_columns(),
        table_dv         = *table_dv_ptr,
        d_validities     = validities.begin(),
-       dst_list_offsets = output_list_offsets.begin<offset_type>(),
-       d_output         = output_dv_ptr->begin<T>()] __device__(size_type const idx) {
-        // The current row contain null, which has been identified during `dst_list_offsets`
-        // computation.
+       dst_list_offsets = output_list_offsets.template begin<offset_type>(),
+       d_output         = output_dv_ptr->template begin<T>()] __device__(size_type const idx) {
+        // The output row has been identified as a null list during list size computation.
         if (dst_list_offsets[idx + 1] == dst_list_offsets[idx]) { return; }
 
         auto write_start = dst_list_offsets[idx];
         for (size_type col_id = 0; col_id < num_cols; ++col_id) {
           auto const& lists_col = table_dv.column(col_id);
           auto const list_offsets =
-            lists_col.child(lists_column_view::offsets_column_index).data<size_type>() +
+            lists_col.child(lists_column_view::offsets_column_index).template data<offset_type>() +
             lists_col.offset();
-          auto const& child = lists_col.child(lists_column_view::child_column_index);
+          auto const& data_col = lists_col.child(lists_column_view::child_column_index);
 
-          // The indices of the entries in the list to gather from.
+          // The indices of the entries within the source list.
           auto const start_idx = list_offsets[idx];
           auto const end_idx   = list_offsets[idx + 1];
 
-          // Fill the validities array if necessary.
+          // Fill the validities array.
           for (auto read_idx = start_idx, write_idx = write_start; read_idx < end_idx;
                ++read_idx, ++write_idx) {
-            d_validities[write_idx] = static_cast<int8_t>(child.is_valid(read_idx));
+            d_validities[write_idx] = static_cast<int8_t>(data_col.is_valid(read_idx));
           }
           // Do a copy for the entire list entries.
-          auto const input_ptr  = reinterpret_cast<char const*>(child.data<T>() + start_idx);
+          auto const input_ptr =
+            reinterpret_cast<char const*>(data_col.template data<T>() + start_idx);
           auto const output_ptr = reinterpret_cast<char*>(&d_output[write_start]);
           thrust::copy(
             thrust::seq, input_ptr, input_ptr + sizeof(T) * (end_idx - start_idx), output_ptr);
@@ -425,7 +417,7 @@ std::unique_ptr<column> concatenate_rows(table_view const& input,
   }
 
   // Both conditions satisfied: has_null_mask == true and
-  // null_policy == concatenate_null_policy::NULLIFY_OUTPUT_ROW.
+  // null_policy == NULLIFY_OUTPUT_ROW.
   return concatenate_with_nullifying_rows(input, stream, mr);
 }
 
