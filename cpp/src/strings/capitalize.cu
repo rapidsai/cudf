6c66bdcd7afe53415f1458ddef799441d68d5a1c
David Wendt <45795991+davidwendt@users.noreply.github.com>
2021-04-27 21:48:39 -0400

    Move make_strings_children to strings/detail/utilities.cuh (#8060)
    Closes #8059 
    
    The `cudf::strings::detail::make_strings_children` is used by non-strings libcudf internal functions. The declaration/definition should be moved from `cpp/src/strings/utilities.cuh` to `cpp/include/cudf/strings/detail/utilities.cuh`. The remaining functions in the `strings/utilities.cuh` will be moved into `strings/utf8.cuh` which is used only internally in strings functions.
    
    No functions have changed -- just moved in internal details headers.
    
    Authors:
      - David Wendt (https://github.com/davidwendt)
    
    Approvers:
      - Nghia Truong (https://github.com/ttnghia)
      - https://github.com/nvdbaranec
    
    URL: https://github.com/rapidsai/cudf/pull/8060

--- cpp/src/strings/capitalize.cu
+++ cpp/src/strings/capitalize.cu
@@ -15,7 +15,7 @@
  */
 
 #include <strings/char_types/is_flags.h>
-#include <strings/utilities.cuh>
+#include <strings/utf8.cuh>
 #include <strings/utilities.hpp>
 
 #include <cudf/column/column.hpp>
@@ -23,6 +23,7 @@
 #include <cudf/detail/null_mask.hpp>
 #include <cudf/detail/nvtx/ranges.hpp>
 #include <cudf/strings/capitalize.hpp>
+#include <cudf/strings/detail/utilities.cuh>
 #include <cudf/strings/detail/utilities.hpp>
 #include <cudf/strings/string_view.cuh>
 #include <cudf/strings/strings_column_view.hpp>
