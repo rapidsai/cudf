9880081495c9fa520f02a968f4f5c123044230db
brandon-b-miller <brmiller@nvidia.com>
2021-05-05 12:48:21 -0700

    add failing tests for literal return

--- python/cudf/cudf/tests/test_udf_masked_ops.py
+++ python/cudf/cudf/tests/test_udf_masked_ops.py
@@ -72,3 +72,40 @@ def test_apply_NA_conditional():
     obtain = gdf.apply(lambda row: func_gdf(row['a'], row['b']), axis=1)
 
     assert_eq(expect, obtain)
+
+
+def test_apply_return_literal():
+    # 1. Casting rule literal -> Masked
+    #  -> a) make it so numba knows that we can even promote literals to Masked ()
+    #  -> b) implement custom lowering to specify how this actually happens (python only)
+
+
+    # 2. Custom unfication code
+
+
+    # numba/core/type
+    def func_pdf(x, y):
+        if x is pd.NA:
+            return 5
+        else:
+            return x + y
+
+    @nulludf
+    def func_gdf(x, y):
+        if x is cudf.NA:
+            return 5 # Masked(5, True)
+        else:
+            return x + y
+
+
+    gdf = cudf.DataFrame({
+        'a':[1,None,3, None],
+        'b':[4,5,None, None]
+    })
+
+    pdf = gdf.to_pandas()
+
+    expect = pdf.apply(lambda row: func_pdf(row['a'], row['b']), axis=1)
+    obtain = gdf.apply(lambda row: func_gdf(row['a'], row['b']), axis=1)
+
+    assert_eq(expect, obtain)
