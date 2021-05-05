177b9d019e81bd5804f77a8e160f96e53afe5f0f
Michael Wang <isVoid@users.noreply.github.com>
2021-05-05 16:18:19 -0700

    Lists and structs test

--- cpp/tests/groupby/group_replace_nulls_test.cpp
+++ cpp/tests/groupby/group_replace_nulls_test.cpp
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 
+#include <initializer_list>
 #include <tests/groupby/groupby_test_util.hpp>
 
 #include <cudf/table/table.hpp>
@@ -160,6 +161,204 @@ TEST_F(GroupbyReplaceNullsStringsTest, FollowingFillTrailingNull)
   fixed_width_column_wrapper<K> expect_key{0, 0, 1, 1, 1, 1, 1};
   strings_column_wrapper expect_val({"y", "y", "xx", "zzz", "zzz", "", ""},
                                     {true, true, true, true, true, false, false});
+
+  TestReplaceNullsGroupbySingle(key, val, expect_key, expect_val, replace_policy::FOLLOWING);
+}
+
+template <typename T>
+struct GroupbyReplaceNullsListsTest : public BaseFixture {
+};
+
+TYPED_TEST_CASE(GroupbyReplaceNullsListsTest, FixedWidthTypes);
+
+TYPED_TEST(GroupbyReplaceNullsListsTest, PrecedingFillNonNested)
+{
+  using LCW = lists_column_wrapper<TypeParam, int32_t>;
+  // Group 0 value: {{1 2 3} @ {4 5} @} --> {{1 2 3} {1 2 3} {4 5} {4 5}}, w/o leading nulls
+  // Group 1 value: {@ {} @} --> {@ {} {}}, w/ leading nulls
+  fixed_width_column_wrapper<K> key{0, 1, 0, 0, 1, 1, 0};
+
+  std::vector<valid_type> mask{1, 0, 0, 1, 1, 0, 0};
+  LCW val({{1, 2, 3}, {}, {}, {4, 5}, {}, {}, {}}, mask.begin());
+
+  fixed_width_column_wrapper<K> expect_key{0, 0, 0, 0, 1, 1, 1};
+  std::vector<valid_type> expected_mask{1, 1, 1, 1, 0, 1, 1};
+  LCW expect_val({{1, 2, 3}, {1, 2, 3}, {4, 5}, {4, 5}, {-1}, {}, {}}, expected_mask.begin());
+
+  TestReplaceNullsGroupbySingle(key, val, expect_key, expect_val, replace_policy::PRECEDING);
+}
+
+TYPED_TEST(GroupbyReplaceNullsListsTest, FollowingFillNonNested)
+{
+  using LCW = lists_column_wrapper<TypeParam, int32_t>;
+  // Group 0 value: {@ {5 6} @ {-1}} --> {{5 6} {5 6} {-1} {-1}}, w/o trailing nulls
+  // Group 1 value: {@ {} @} --> {{} {} @}}, w/ trailing nulls
+  fixed_width_column_wrapper<K> key{0, 1, 0, 0, 1, 1, 0};
+
+  std::vector<valid_type> mask{0, 0, 1, 0, 1, 0, 1};
+  LCW val({{}, {}, {5, 6}, {}, {}, {}, {-1}}, mask.begin());
+
+  fixed_width_column_wrapper<K> expect_key{0, 0, 0, 0, 1, 1, 1};
+  std::vector<valid_type> expected_mask{1, 1, 1, 1, 1, 1, 0};
+  LCW expect_val({{5, 6}, {5, 6}, {-1}, {-1}, {}, {}, {}}, expected_mask.begin());
+
+  TestReplaceNullsGroupbySingle(key, val, expect_key, expect_val, replace_policy::FOLLOWING);
+}
+
+TYPED_TEST(GroupbyReplaceNullsListsTest, PrecedingFillNested)
+{
+  using LCW    = lists_column_wrapper<TypeParam, int32_t>;
+  using Mask_t = std::vector<valid_type>;
+  // Group 0 value: {{{1 @ 3} @}
+  //                 @
+  //                 {{@} {}}}} -->
+  //                {{{1 @ 3} @}
+  //                 {{1 @ 3} @}
+  //                 {{@} {}}}}, w/o leading nulls
+  // Group 1 value: {@
+  //                 {@ {102 @}}
+  //                 @
+  //                 {{@ 202} {}}}} -->
+  //                {@
+  //                 {@ {102 @}}
+  //                 {@ {102 @}}
+  //                 {{@ 202} {}}}}, w/ leading nulls
+  // Only top level nulls are replaced.
+  fixed_width_column_wrapper<K> key{1, 0, 1, 1, 0, 0, 1};
+
+  // clang-format off
+  LCW val({{},
+           LCW({LCW({1, -1, 3}, Mask_t{1, 0, 1}.begin()), {}}, Mask_t{1, 0}.begin()),
+           LCW({LCW{}, LCW({102, -1}, Mask_t{1, 0}.begin())}, Mask_t{0, 1}.begin()),
+           {},
+           {},
+           {LCW({{}}, Mask_t{0}.begin()), LCW{}},
+           {LCW({-1, 202}, Mask_t{0, 1}.begin()), LCW{}}},
+           Mask_t{0, 1, 1, 0, 0, 1, 1}.begin());
+  // clang-format on
+
+  fixed_width_column_wrapper<K> expect_key{0, 0, 0, 1, 1, 1, 1};
+
+  // clang-format off
+  LCW expect_val({LCW({LCW({1, -1, 3}, Mask_t{1, 0, 1}.begin()), {}}, Mask_t{1, 0}.begin()),
+                  LCW({LCW({1, -1, 3}, Mask_t{1, 0, 1}.begin()), {}}, Mask_t{1, 0}.begin()),
+                  {LCW({{}}, Mask_t{0}.begin()), LCW{}},
+                  {},
+                  LCW({LCW{}, LCW({102, -1}, Mask_t{1, 0}.begin())}, Mask_t{0, 1}.begin()),
+                  LCW({LCW{}, LCW({102, -1}, Mask_t{1, 0}.begin())}, Mask_t{0, 1}.begin()),
+                  {LCW({-1, 202}, Mask_t{0, 1}.begin()), LCW{}}},
+           Mask_t{1, 1, 1, 0, 1 ,1 ,1}.begin());
+  // clang-format on
+
+  TestReplaceNullsGroupbySingle(key, val, expect_key, expect_val, replace_policy::PRECEDING);
+}
+
+TYPED_TEST(GroupbyReplaceNullsListsTest, FollowingFillNested)
+{
+  using LCW    = lists_column_wrapper<TypeParam, int32_t>;
+  using Mask_t = std::vector<valid_type>;
+  // Group 0 value: {{{1 @ 3} @}
+  //                 @
+  //                 {{@} {}}}} -->
+  //                {{{1 @ 3} @}
+  //                 {{@} {}}}}
+  //                 {{@} {}}}}, w/o trailing nulls
+  // Group 1 value: {{@ {102 @}}
+  //                 @
+  //                 {{@ 202} {}}}}
+  //                 @ -->
+  //                {{@ {102 @}}
+  //                 {{@ 202} {}}}
+  //                 {{@ 202} {}}}
+  //                 @}, w/ trailing nulls
+  // Only top level nulls are replaced.
+  fixed_width_column_wrapper<K> key{1, 0, 1, 1, 0, 0, 1};
+
+  // clang-format off
+  LCW val({LCW({LCW{}, LCW({102, -1}, Mask_t{1, 0}.begin())}, Mask_t{0, 1}.begin()),
+           LCW({LCW({1, -1, 3}, Mask_t{1, 0, 1}.begin()), {}}, Mask_t{1, 0}.begin()),
+           {},
+           {LCW({-1, 202}, Mask_t{0, 1}.begin()), LCW{}},
+           {},
+           {LCW({{}}, Mask_t{0}.begin()), LCW{}},
+           {}},
+           Mask_t{1, 1, 0, 1, 0, 1, 0}.begin());
+  // clang-format on
+
+  fixed_width_column_wrapper<K> expect_key{0, 0, 0, 1, 1, 1, 1};
+
+  // clang-format off
+  LCW expect_val({LCW({LCW({1, -1, 3}, Mask_t{1, 0, 1}.begin()), {}}, Mask_t{1, 0}.begin()),
+                 {LCW({{}}, Mask_t{0}.begin()), LCW{}},
+                 {LCW({{}}, Mask_t{0}.begin()), LCW{}},
+                 LCW({LCW{}, LCW({102, -1}, Mask_t{1, 0}.begin())}, Mask_t{0, 1}.begin()),
+                 {LCW({-1, 202}, Mask_t{0, 1}.begin()), LCW{}},
+                 {LCW({-1, 202}, Mask_t{0, 1}.begin()), LCW{}},
+                 {}},
+           Mask_t{1, 1, 1, 1, 1, 1, 0}.begin());
+  // clang-format on
+
+  TestReplaceNullsGroupbySingle(key, val, expect_key, expect_val, replace_policy::FOLLOWING);
+}
+
+struct GroupbyReplaceNullsStructsTest : public BaseFixture {
+  using SCW = structs_column_wrapper;
+
+  SCW data(fixed_width_column_wrapper<int32_t> field0,
+           strings_column_wrapper field1,
+           lists_column_wrapper<int32_t> field2,
+           std::initializer_list<valid_type> mask)
+  {
+    return SCW({field0, field1, field2}, mask.begin());
+  }
+};
+
+TEST_F(GroupbyReplaceNullsStructsTest, PrecedingFill)
+{
+  using LCW    = lists_column_wrapper<int32_t>;
+  using Mask_t = std::vector<valid_type>;
+  fixed_width_column_wrapper<K> key{1, 0, 0, 1, 0, 1, 1};
+
+  // Only null rows are replaced.
+
+  SCW val =
+    this->data({{1, -1, 3, -1, -1, -1, 7}, {1, 0, 1, 0, 0, 0, 1}},
+               {{"x", "yy", "", "", "", "zz", ""}, {true, true, false, false, false, true, false}},
+               LCW({{1, 2, 3}, {-1}, {}, {}, {42}, {}, {}}, Mask_t{1, 1, 0, 0, 1, 0, 0}.begin()),
+               {1, 1, 0, 0, 1, 1, 0});
+
+  fixed_width_column_wrapper<K> expect_key{0, 0, 0, 1, 1, 1, 1};
+
+  SCW expect_val = this->data(
+    {{-1, -1, -1, 1, 1, -1, -1}, {0, 0, 0, 1, 1, 0, 0}},
+    {{"yy", "yy", "", "x", "x", "zz", "zz"}, {true, true, false, true, true, true, true}},
+    LCW({LCW{-1}, {-1}, {42}, {1, 2, 3}, {1, 2, 3}, {}, {}}, Mask_t{1, 1, 1, 1, 1, 0, 0}.begin()),
+    {1, 1, 1, 1, 1, 1, 1});
+
+  TestReplaceNullsGroupbySingle(key, val, expect_key, expect_val, replace_policy::PRECEDING);
+}
+
+TEST_F(GroupbyReplaceNullsStructsTest, FollowingFill)
+{
+  using LCW    = lists_column_wrapper<int32_t>;
+  using Mask_t = std::vector<valid_type>;
+  fixed_width_column_wrapper<K> key{1, 0, 0, 1, 0, 1, 1};
+
+  // Only null rows are replaced.
+
+  SCW val =
+    this->data({{1, -1, 3, -1, -1, -1, 7}, {1, 0, 1, 0, 0, 0, 1}},
+               {{"x", "yy", "", "", "", "zz", ""}, {true, true, false, false, false, true, false}},
+               LCW({{1, 2, 3}, {-1}, {}, {}, {42}, {}, {}}, Mask_t{1, 1, 0, 0, 1, 0, 0}.begin()),
+               {1, 1, 0, 0, 1, 1, 0});
+
+  fixed_width_column_wrapper<K> expect_key{0, 0, 0, 1, 1, 1, 1};
+
+  SCW expect_val = this->data(
+    {{-1, -1, -1, 1, -1, -1, -1}, {0, 0, 0, 1, 0, 0, 0}},
+    {{"yy", "", "", "x", "zz", "zz", ""}, {true, false, false, true, true, true, false}},
+    LCW({LCW{-1}, {42}, {42}, {1, 2, 3}, {}, {}, {}}, Mask_t{1, 1, 1, 1, 0, 0, 0}.begin()),
+    {1, 1, 1, 1, 1, 1, 0});
 
   TestReplaceNullsGroupbySingle(key, val, expect_key, expect_val, replace_policy::FOLLOWING);
 }
