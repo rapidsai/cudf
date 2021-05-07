ec0624fdb17b4854f3597384cf1acb04893bfbf8
davidwendt <dwendt@nvidia.com>
2021-05-07 14:04:56 -0400

    add doxygen comments to new detail header

--- cpp/include/cudf/detail/scan.hpp
+++ cpp/include/cudf/detail/scan.hpp
@@ -23,12 +23,52 @@
 namespace cudf {
 namespace detail {
 
+/**
+ * @brief Computes the exclusive scan of a column.
+ *
+ * The null values are skipped for the operation, and if an input element
+ * at `i` is null, then the output element at `i` will also be null.
+ *
+ * The identity value for the column type as per the aggregation type
+ * is used for the value of the first element in the output column.
+ *
+ * @throws cudf::logic_error if column data_type is not an arithmetic type.
+ *
+ * @param input The input column view for the scan
+ * @param agg unique_ptr to aggregation operator applied by the scan
+ * @param null_handling Exclude null values when computing the result if
+ *                      null_policy::EXCLUDE. Include nulls if null_policy::INCLUDE.
+ *                      Any operation with a null results in a null.
+ * @param stream CUDA stream used for device memory operations and kernel launches.
+ * @param mr Device memory resource used to allocate the returned scalar's device memory
+ * @returns Column with scan results
+ */
 std::unique_ptr<column> scan_exclusive(column_view const& input,
                                        std::unique_ptr<aggregation> const& agg,
                                        null_policy null_handling,
                                        rmm::cuda_stream_view stream,
                                        rmm::mr::device_memory_resource* mr);
 
+/**
+ * @brief Computes the inclusive scan of a column.
+ *
+ * The null values are skipped for the operation, and if an input element
+ * at `i` is null, then the output element at `i` will also be null.
+ *
+ * String columns are allowed with aggregation types Min and Max.
+ *
+ * @throws cudf::logic_error if column data_type is not an arithmetic type
+ *                           or string type but the `agg` is not Min or Max
+ *
+ * @param input The input column view for the scan
+ * @param agg unique_ptr to aggregation operator applied by the scan
+ * @param null_handling Exclude null values when computing the result if
+ *                      null_policy::EXCLUDE. Include nulls if null_policy::INCLUDE.
+ *                      Any operation with a null results in a null.
+ * @param stream CUDA stream used for device memory operations and kernel launches.
+ * @param mr Device memory resource used to allocate the returned scalar's device memory
+ * @returns Column with scan results
+ */
 std::unique_ptr<column> scan_inclusive(column_view const& input,
                                        std::unique_ptr<aggregation> const& agg,
                                        null_policy null_handling,
