e8ba3b79c3555e4b5eaeeeac1ab333d08e32e481
Shane Ding <shane200195@gmail.com>
2021-05-10 18:12:42 +0000

    updated files

---
+++ python/dask_cudf/dask_cudf/accessors.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.
+
+
+class ListMethods():
+    #TODO: Maybe use inheritance instead?
+    def __init__(self, d_series):
+        self.d_series = d_series
+    
+    def len(self):
+        return self.d_series.map_partitions(lambda s: s.list.len(), meta=self.d_series._meta)
+
+    def contains(self, search_key):
+        return self.d_series.map_partitions(lambda s: s.list.contains(search_key), meta=self.d_series._meta)
+
+    def get(self, index):
+        return self.d_series.map_partitions(lambda s: s.list.get(index), meta=self.d_series._meta)
+
+    @property
+    def leaves(self):
+        return self.d_series.map_partitions(lambda s: s.list.leaves, meta=self.d_series._meta)
+
+    def take(self, lists_indices):
+        return self.d_series.map_partitions(lambda s: s.list.take(lists_indices), meta=self.d_series._meta)
+
+    def unique(self):
+        return self.d_series.map_partitions(lambda s: s.list.unique(), meta=self.d_series._meta)
+
+    def sort_values(
+        self,
+        ascending=True,
+        inplace=False,
+        kind="quicksort",
+        na_position="last",
+        ignore_index=False,
+    ):
+        return self.d_series.map_partitions(lambda s: s.list.sort_values(ascending, inplace, kind, na_position, ignore_index), meta=self.d_series._meta)
--- python/dask_cudf/dask_cudf/core.py
+++ python/dask_cudf/dask_cudf/core.py
@@ -22,6 +22,7 @@ from dask.utils import M, OperatorMethodMixin, derived_from, funcname
 import cudf
 from cudf import _lib as libcudf
 from dask_cudf import sorting
+from dask_cudf.accessors import ListMethods
 
 DASK_VERSION = LooseVersion(dask.__version__)
 
@@ -415,41 +416,8 @@ class Series(_Frame, dd.core.Series):
 
     @property
     def list(self):
-        return ListMethod(self)
+        return ListMethods(self)
 
-class ListMethod():
-    #TODO: Maybe use inheritance instead?
-    def __init__(self, d_series):
-        self.d_series = d_series
-    
-    def len(self):
-        return self.d_series.map_partitions(lambda s: s.list.len(), meta=self.d_series._meta)
-
-    def contains(self, search_key):
-        return self.d_series.map_partitions(lambda s: s.list.contains(search_key), meta=self.d_series._meta)
-
-    def get(self, index):
-        return self.d_series.map_partitions(lambda s: s.list.get(index), meta=self.d_series._meta)
-
-    @property
-    def leaves(self):
-        return self.d_series.map_partitions(lambda s: s.list.leaves, meta=self.d_series._meta)
-
-    def take(self, lists_indices):
-        return self.d_series.map_partitions(lambda s: s.list.take(lists_indices), meta=self.d_series._meta)
-
-    def unique(self):
-        return self.d_series.map_partitions(lambda s: s.list.unique(), meta=self.d_series._meta)
-
-    def sort_values(
-        self,
-        ascending=True,
-        inplace=False,
-        kind="quicksort",
-        na_position="last",
-        ignore_index=False,
-    ):
-        return self.d_series.map_partitions(lambda s: s.list.sort_values(ascending, inplace, kind, na_position, ignore_index), meta=self.d_series._meta)
 
 class Index(Series, dd.core.Index):
     _partition_type = cudf.Index
