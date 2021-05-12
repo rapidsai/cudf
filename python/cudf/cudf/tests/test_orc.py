9b6690e0866af8f5ad7214c5640d169397bd0fe6
vuule <vmilovanovic@nvidia.com>
2021-05-12 15:34:12 -0700

    add missing import

--- python/cudf/cudf/tests/test_orc.py
+++ python/cudf/cudf/tests/test_orc.py
@@ -14,6 +14,7 @@ import pytest
 import cudf
 from cudf.io.orc import ORCWriter
 from cudf.tests.utils import assert_eq, gen_rand_series, supported_numpy_dtypes
+from cudf.core.dtypes import Decimal64Dtype
 
 
 @pytest.fixture(scope="module")
