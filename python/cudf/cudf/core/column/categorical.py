8b05f166195aeaa493efb50a78f32eadcb0d0989
Marlene <57748216+marlenezw@users.noreply.github.com>
2021-05-19 16:09:38 +0200

    Update python/cudf/cudf/core/column/categorical.py
    Co-authored-by: Michael Wang <isVoid@users.noreply.github.com>

--- python/cudf/cudf/core/column/categorical.py
+++ python/cudf/cudf/core/column/categorical.py
@@ -1093,7 +1093,7 @@ class CategoricalColumn(column.ColumnBase):
         signed_dtype = min_signed_type(len(col.categories))
         codes = col.cat().codes.astype(signed_dtype).fillna(-1).to_array()
         if is_interval_dtype(col.categories.dtype):
-            # leaving out dropna because it termporarily changes an interval
+            # leaving out dropna because it temporarily changes an interval
             # index into a struct and throws off results.
             # TODO: work on interval index dropna
             categories = col.categories.to_pandas()
