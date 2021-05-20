9e2ced91f645bebc7363d10807a662159b6afd77
Michael Wang <isVoid@users.noreply.github.com>
2021-05-20 11:45:36 -0700

    soft code `child_column_index`

--- cpp/src/copying/get_element.cu
+++ cpp/src/copying/get_element.cu
@@ -125,7 +125,8 @@ struct get_element_functor {
     rmm::cuda_stream_view stream,
     rmm::mr::device_memory_resource *mr = rmm::mr::get_current_device_resource())
   {
-    bool valid = is_element_valid_sync(input, index, stream);
+    bool valid               = is_element_valid_sync(input, index, stream);
+    auto const child_col_idx = lists_column_view::child_column_index;
 
     if (valid) {
       lists_column_view lcv(input);
@@ -134,11 +135,11 @@ struct get_element_functor {
         lists::detail::copy_slice(lcv, index, index + 1, stream, mr)->release();
       // Construct scalar with row data
       return std::make_unique<list_scalar>(
-        std::move(*row_slice_contents.children[1]), valid, stream, mr);
+        std::move(*row_slice_contents.children[child_col_idx]), valid, stream, mr);
     } else {
       auto empty_row_contents = empty_like(input)->release();
       return std::make_unique<list_scalar>(
-        std::move(*empty_row_contents.children[1]), valid, stream, mr);
+        std::move(*empty_row_contents.children[child_col_idx]), valid, stream, mr);
     }
   }
 
