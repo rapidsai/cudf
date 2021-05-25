671a2a5f79bcc2bd296b02c8dd141d270d43f1d8
davidwendt <dwendt@nvidia.com>
2021-05-25 16:48:37 -0400

    Refactor setting stack size in regex code

--- cpp/src/strings/regex/regexec.cu
+++ cpp/src/strings/regex/regexec.cu
@@ -68,9 +68,7 @@ reprog_device::reprog_device(reprog& prog)
     _insts_count{prog.insts_count()},
     _starts_count{prog.starts_count()},
     _classes_count{prog.classes_count()},
-    _relists_mem{nullptr},
-    _stack_mem1{nullptr},
-    _stack_mem2{nullptr}
+    _relists_mem{nullptr}
 {
 }
 
