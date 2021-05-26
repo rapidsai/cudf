97df6be2b32b3f53cfc1222b7f42bfaf75f72274
Sheilah <kirui.sheilah@gmail.com>
2021-05-26 10:20:47 -0700

    addressed review comment

--- python/cudf/cudf/core/groupby/groupby.py
+++ python/cudf/cudf/core/groupby/groupby.py
@@ -615,8 +615,41 @@ class GroupBy(Serializable):
         return self.agg(func)
 
     def describe(self):
-        """Return descriptive stats of the values in each group"""
+        """
+        Generate descriptive statistics that summarizes the central tendency,
+        dispersion and shape of a dataset’s distribution, excluding NaN values.
 
+        Analyzes numeric DataFrames only
+
+        Parameters
+        ----------
+        DataFrame to be summarized
+
+        Returns
+        -------
+        DataFrame
+            Summary statistics of the Dataframe provided.
+
+        Examples
+        --------
+        >>> import cudf
+        >>> gdf = cudf.DataFrame({"Speed": [380.0, 370.0, 24.0, 26.0],
+                                  "Score": [50, 30, 90, 80]})
+        >>> gdf
+        Speed  Score
+        0  380.0     50
+        1  370.0     30
+        2   24.0     90
+        3   26.0     80
+        >>> gdf.groupby('Score').describe()
+                count   mean   std    min    25%    50%    75%    max
+        Score
+        30         1  370.0  <NA>  370.0  370.0  370.0  370.0  370.0
+        50         1  380.0  <NA>  380.0  380.0  380.0  380.0  380.0
+        80         1   26.0  <NA>   26.0   26.0   26.0   26.0   26.0
+        90         1   24.0  <NA>   24.0   24.0   24.0   24.0   24.0
+
+        """
         res = self.agg(
             [
                 "count",
