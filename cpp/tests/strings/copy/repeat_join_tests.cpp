b6748154e1e1e3adaec716033afe02c7351bb1ff
Nghia Truong <nghiatruong.vn@gmail.com>
2021-06-01 16:02:06 -0600

    Complete unit tests

--- cpp/tests/strings/copy/repeat_join_tests.cpp
+++ cpp/tests/strings/copy/repeat_join_tests.cpp
@@ -14,43 +14,47 @@
  * limitations under the License.
  */
 
-#include <cudf/scalar/scalar.hpp>
-#include <cudf/strings/copy.hpp>
-#include <cudf/strings/strings_column_view.hpp>
-
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_wrapper.hpp>
 #include <cudf_test/iterator_utilities.hpp>
+
+#include <cudf/scalar/scalar.hpp>
+#include <cudf/strings/copy.hpp>
+#include <cudf/strings/strings_column_view.hpp>
 
 namespace {
 using STR_COL = cudf::test::strings_column_wrapper;
 
 constexpr bool print_all{false};
 
-// auto all_nulls() { return cudf::test::iterator_all_nulls(); }
+auto all_nulls() { return cudf::test::iterator_all_nulls(); }
 
-// auto null_at(cudf::size_type idx) { return cudf::test::iterator_with_null_at(idx); }
+auto null_at(cudf::size_type idx) { return cudf::test::iterator_with_null_at(idx); }
 
-// auto null_at(std::vector<cudf::size_type> const& indices)
-//{
-//  return cudf::test::iterator_with_null_at(cudf::host_span<cudf::size_type const>{indices});
-//}
-
-// auto nulls_from_nullptr(std::vector<const char*> const& strs)
-//{
-//  return thrust::make_transform_iterator(strs.begin(), [](auto ptr) { return ptr != nullptr; });
-//}
+auto null_at(std::vector<cudf::size_type> const& indices)
+{
+  return cudf::test::iterator_with_null_at(cudf::host_span<cudf::size_type const>{indices});
+}
 
 }  // namespace
 
 struct RepeatJoinStringTest : public cudf::test::BaseFixture {
 };
 
-TEST_F(RepeatJoinStringTest, InvalidRepeatTimes) {}
+TEST_F(RepeatJoinStringTest, InvalidStringScalar)
+{
+  auto const str    = cudf::string_scalar("", false);
+  auto const result = cudf::strings::repeat_join(str, 3);
+  EXPECT_EQ(result.is_valid(), false);
+}
 
-TEST_F(RepeatJoinStringTest, InvalidStringScalar) {}
-
-TEST_F(RepeatJoinStringTest, ZeroSizeStringScalar) {}
+TEST_F(RepeatJoinStringTest, ZeroSizeStringScalar)
+{
+  auto const str    = cudf::string_scalar("");
+  auto const result = cudf::strings::repeat_join(str, 3);
+  EXPECT_EQ(result.is_valid(), true);
+  EXPECT_EQ(result.size(), 0);
+}
 
 TEST_F(RepeatJoinStringTest, ValidStringScalar)
 {
@@ -70,14 +74,47 @@ TEST_F(RepeatJoinStringTest, ValidStringScalar)
 
   // Zero repeat time.
   {
+    auto const result = cudf::strings::repeat_join(str, 0);
+    EXPECT_EQ(result.is_valid(), true);
+    EXPECT_EQ(result.size(), 0);
+  }
+
+  // Negatitve repeat time.
+  {
+    auto const result = cudf::strings::repeat_join(str, -10);
+    EXPECT_EQ(result.is_valid(), true);
+    EXPECT_EQ(result.size(), 0);
   }
 }
 
-TEST_F(RepeatJoinStringTest, ZeroSizeStringsColumn) {}
+TEST_F(RepeatJoinStringTest, ZeroSizeStringsColumn)
+{
+  auto const strs    = STR_COL{};
+  auto const results = cudf::strings::repeat_join(cudf::strings_column_view(strs), 10);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(strs, *results, print_all);
+}
 
-TEST_F(RepeatJoinStringTest, AllNullStringsColumn) {}
+TEST_F(RepeatJoinStringTest, AllEmptyStringsColumn)
+{
+  auto const strs    = STR_COL{"", "", "", "", ""};
+  auto const results = cudf::strings::repeat_join(cudf::strings_column_view(strs), 10);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(strs, *results, print_all);
+}
 
-TEST_F(RepeatJoinStringTest, ZeroSizeAndNullStringsColumn) {}
+TEST_F(RepeatJoinStringTest, AllNullStringsColumn)
+{
+  auto const strs    = STR_COL{{"" /*NULL*/, "" /*NULL*/, "" /*NULL*/}, all_nulls()};
+  auto const results = cudf::strings::repeat_join(cudf::strings_column_view(strs), 10);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(strs, *results, print_all);
+}
+
+TEST_F(RepeatJoinStringTest, ZeroSizeAndNullStringsColumn)
+{
+  auto const strs =
+    STR_COL{{"" /*NULL*/, "", "" /*NULL*/, "", "", "" /*NULL*/}, null_at({0, 2, 5})};
+  auto const results = cudf::strings::repeat_join(cudf::strings_column_view(strs), 10);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(strs, *results, print_all);
+}
 
 TEST_F(RepeatJoinStringTest, StringsColumnNoNull)
 {
@@ -95,8 +132,16 @@ TEST_F(RepeatJoinStringTest, StringsColumnNoNull)
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(strs, *results, print_all);
   }
 
-  // Zero repeat time.
-  {}
+  // Non-positive repeat time.
+  {
+    auto const expected = STR_COL{"", "", "", "", ""};
+
+    auto results = cudf::strings::repeat_join(cudf::strings_column_view(strs), 0);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
+
+    results = cudf::strings::repeat_join(cudf::strings_column_view(strs), -100);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
+  }
 
   // Sliced the first half of the column
   {
@@ -113,28 +158,86 @@ TEST_F(RepeatJoinStringTest, StringsColumnNoNull)
     auto const expected    = STR_COL{"abcxyzabcxyz", "xyzéééxyzééé"};
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
   }
+
+  // Sliced the second half of the column
+  {
+    auto const sliced_strs = cudf::slice(strs, {2, 5})[0];
+    auto const results     = cudf::strings::repeat_join(cudf::strings_column_view(sliced_strs), 2);
+    auto const expected    = STR_COL{"xyzéééxyzééé", "áááááá", "íííí"};
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
+  }
 }
 
 TEST_F(RepeatJoinStringTest, StringsColumnWithNulls)
 {
-  {
+  auto const strs = STR_COL{{"0a0b0c",
+                             "" /*NULL*/,
+                             "abcxyz",
+                             "" /*NULL*/,
+                             "xyzééé",
+                             "" /*NULL*/,
+                             "ááá",
+                             "íí",
+                             "",
+                             "Hello World"},
+                            null_at({1, 3, 5})};
 
+  {
+    auto const results  = cudf::strings::repeat_join(cudf::strings_column_view(strs), 2);
+    auto const expected = STR_COL{{"0a0b0c0a0b0c",
+                                   "" /*NULL*/,
+                                   "abcxyzabcxyz",
+                                   "" /*NULL*/,
+                                   "xyzéééxyzééé",
+                                   "" /*NULL*/,
+                                   "áááááá",
+                                   "íííí",
+                                   "",
+                                   "Hello WorldHello World"},
+                                  null_at({1, 3, 5})};
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
   }
 
   // Repeat once.
   {
-
+    auto const results = cudf::strings::repeat_join(cudf::strings_column_view(strs), 1);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(strs, *results, print_all);
   }
 
-  // Zero repeat time.
-  {}
+  // Non-positive repeat time.
+  {
+    auto const expected = STR_COL{
+      {"", "" /*NULL*/, "", "" /*NULL*/, "", "" /*NULL*/, "", "", "", ""}, null_at({1, 3, 5})};
+
+    auto results = cudf::strings::repeat_join(cudf::strings_column_view(strs), 0);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
+
+    results = cudf::strings::repeat_join(cudf::strings_column_view(strs), -100);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
+  }
 
   // Sliced the first half of the column
   {
-
+    auto const sliced_strs = cudf::slice(strs, {0, 3})[0];
+    auto const results     = cudf::strings::repeat_join(cudf::strings_column_view(sliced_strs), 2);
+    auto const expected    = STR_COL{{"0a0b0c0a0b0c", "" /*NULL*/, "abcxyzabcxyz"}, null_at(1)};
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
   }
 
   // Sliced the middle of the column
   {
+    auto const sliced_strs = cudf::slice(strs, {2, 7})[0];
+    auto const results     = cudf::strings::repeat_join(cudf::strings_column_view(sliced_strs), 2);
+    auto const expected    = STR_COL{
+      {"abcxyzabcxyz", "" /*NULL*/, "xyzéééxyzééé", "" /*NULL*/, "áááááá"}, null_at({1, 3})};
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
+  }
+
+  // Sliced the second half of the column
+  {
+    auto const sliced_strs = cudf::slice(strs, {6, 10})[0];
+    auto const results     = cudf::strings::repeat_join(cudf::strings_column_view(sliced_strs), 2);
+    auto const expected    = STR_COL{"áááááá", "íííí", "", "Hello WorldHello World"};
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results, print_all);
   }
 }
