98e51803c46b037545db473ba25b23f2dea43db8
Nghia Truong <nghiatruong.vn@gmail.com>
2021-06-02 12:18:56 -0600

    Some optimizations

--- cpp/src/strings/copy/repeat_strings.cu
+++ cpp/src/strings/copy/repeat_strings.cu
@@ -47,26 +47,64 @@ string_scalar repeat_strings(string_scalar const& input,
 
   auto buff =
     rmm::device_buffer(repeat_times * input.size(), stream, rmm::mr::get_current_device_resource());
-  auto const iter = thrust::make_counting_iterator(0);
+  auto const iter     = thrust::make_counting_iterator(0);
+  auto const str_size = input.size();
+
+  // Pull data from the input string into each byte of the output string.
   thrust::transform(rmm::exec_policy(stream),
                     iter,
-                    iter + repeat_times * input.size(),
+                    iter + repeat_times * str_size,
                     static_cast<char*>(buff.data()),
-                    [in_ptr = input.data(), str_size = input.size()] __device__(const auto idx) {
+                    [in_ptr = input.data(), str_size] __device__(const auto idx) {
                       return in_ptr[idx % str_size];
                     });
 
+  // TODO: using move constructor when https://github.com/rapidsai/cudf/pull/8428 merged
+  // return string_scalar(std::move(buff));
   return string_scalar(string_view(static_cast<char*>(buff.data()), buff.size()), true, stream, mr);
 }
 
 namespace {
 /**
- * @brief Functor to compute string sizes and repeat the input strings.
+ * @brief Generate a strings column in which each row is an empty or null string.
+ *
+ * The output strings column has the same bitmask as the input column.
  */
-template <bool has_nulls>
+auto generate_empty_output(strings_column_view const& input,
+                           size_type strings_count,
+                           rmm::cuda_stream_view stream,
+                           rmm::mr::device_memory_resource* mr)
+{
+  auto chars_column = create_chars_child_column(strings_count, 0, stream, mr);
+
+  static_assert(std::is_same_v<offset_type, int32_t> && std::is_same_v<size_type, int32_t>);
+  auto offsets_column = make_numeric_column(
+    data_type{type_id::INT32}, strings_count + 1, mask_state::UNALLOCATED, stream, mr);
+  CUDA_TRY(cudaMemsetAsync(offsets_column->mutable_view().template begin<offset_type>(),
+                           0,
+                           (strings_count + 1) * sizeof(offset_type),
+                           stream.value()));
+
+  return make_strings_column(strings_count,
+                             std::move(offsets_column),
+                             std::move(chars_column),
+                             input.null_count(),
+                             input.null_count()
+                               ? cudf::detail::copy_bitmask(input.parent(), stream, mr)
+                               : rmm::device_buffer{},
+                             stream,
+                             mr);
+}
+
+/**
+ * @brief Functor to compute string sizes and repeat the input strings.
+ *
+ * This functor is called only when `repeat_times > 0`.
+ */
 struct compute_size_and_repeat_fn {
   column_device_view const strings_dv;
-  size_type repeat_times;
+  size_type const repeat_times;
+  bool const has_nulls;
 
   offset_type* d_offsets{nullptr};
 
@@ -74,24 +112,27 @@ struct compute_size_and_repeat_fn {
   // If d_chars != nullptr: only repeat strings.
   char* d_chars{nullptr};
 
+  // `idx` will be in the range of [0, repeat_times * strings_count).
   __device__ void operator()(size_type const idx) const noexcept
   {
-    // If the number of repetitions is not positive, the output will be either an empty string,
-    // or a null.
-    if (repeat_times <= 0) {
-      if (!d_chars) { d_offsets[idx] = 0; }
-      return;
+    auto const str_idx    = idx / repeat_times;
+    auto const repeat_idx = idx % repeat_times;
+    auto const is_valid   = !has_nulls || strings_dv.is_valid_nocheck(str_idx);
+
+    // Only `strings_count` threads will compute string sizes.
+    if (!d_chars && repeat_idx == 0) {
+      d_offsets[str_idx] =
+        is_valid ? repeat_times * strings_dv.element<string_view>(str_idx).size_bytes() : 0;
     }
 
-    auto const is_valid = !has_nulls || strings_dv.is_valid_nocheck(idx);
-    if (!d_chars) {
-      d_offsets[idx] =
-        is_valid ? repeat_times * strings_dv.element<string_view>(idx).size_bytes() : 0;
-    } else if (is_valid) {
-      auto const d_str = strings_dv.element<string_view>(idx);
-      auto output_ptr  = d_chars + d_offsets[idx];
-      for (size_type i = 0; i < repeat_times; ++i) {
-        output_ptr = detail::copy_string(output_ptr, d_str);
+    // Each input string will be copied by `repeat_times` threads into the output string.
+    if (d_chars && is_valid) {
+      auto const d_str    = strings_dv.element<string_view>(str_idx);
+      auto const str_size = d_str.size_bytes();
+      if (str_size > 0) {
+        auto const input_ptr  = d_str.data();
+        auto const output_ptr = d_chars + d_offsets[str_idx] + repeat_idx * str_size;
+        std::memcpy(output_ptr, input_ptr, str_size);
       }
     }
   }
@@ -104,31 +145,32 @@ std::unique_ptr<column> repeat_strings(strings_column_view const& input,
                                        rmm::cuda_stream_view stream,
                                        rmm::mr::device_memory_resource* mr)
 {
-  auto const num_rows = input.size();
-  if (num_rows == 0) { return detail::make_empty_strings_column(stream, mr); }
-  if (num_rows == 1) { return std::make_unique<column>(input.parent(), stream, mr); }
+  auto const strings_count = input.size();
+  if (strings_count == 0) { return detail::make_empty_strings_column(stream, mr); }
 
-  if (repeat_times > 0) {
-    auto const size_start =
-      cudf::detail::get_value<size_type>(input.offsets(), input.offset(), stream);
-    auto const size_end =
-      cudf::detail::get_value<size_type>(input.offsets(), input.offset() + num_rows, stream);
-    CUDF_EXPECTS(size_end - size_start <= std::numeric_limits<size_type>::max() / repeat_times,
-                 "The output strings have total size that exceeds the maximum allowed size.");
+  if (repeat_times <= 0) {
+    // If the number of repetitions is not positive, each row of the output strings column will be
+    // either an empty string (if the input row is not null), or a null (if the input row is null).
+    return generate_empty_output(input, strings_count, stream, mr);
   }
 
-  auto [offsets_column, chars_column] = [&] {
-    auto const strings_dv_ptr = column_device_view::create(input.parent(), stream);
-    if (input.has_nulls()) {
-      auto const fn = compute_size_and_repeat_fn<true>{*strings_dv_ptr, repeat_times};
-      return make_strings_children(fn, num_rows, stream, mr);
-    } else {
-      auto const fn = compute_size_and_repeat_fn<false>{*strings_dv_ptr, repeat_times};
-      return make_strings_children(fn, num_rows, stream, mr);
-    }
-  }();
+  // If `repeat_times == 1`, just make a copy of the input.
+  if (repeat_times == 1) { return std::make_unique<column>(input.parent(), stream, mr); }
 
-  return make_strings_column(num_rows,
+  // The output strings column should have size that can be properly handled by cudf.
+  auto const size_start =
+    cudf::detail::get_value<size_type>(input.offsets(), input.offset(), stream);
+  auto const size_end =
+    cudf::detail::get_value<size_type>(input.offsets(), input.offset() + strings_count, stream);
+  CUDF_EXPECTS(size_end - size_start <= std::numeric_limits<size_type>::max() / repeat_times,
+               "The output strings have total size that exceeds the maximum allowed size.");
+
+  auto const strings_dv_ptr = column_device_view::create(input.parent(), stream);
+  auto const fn = compute_size_and_repeat_fn{*strings_dv_ptr, repeat_times, input.has_nulls()};
+  auto [offsets_column, chars_column] =
+    make_strings_children(fn, strings_count * repeat_times, strings_count, stream, mr);
+
+  return make_strings_column(strings_count,
                              std::move(offsets_column),
                              std::move(chars_column),
                              input.null_count(),
