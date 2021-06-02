dbee4bb4a56555c22d78e763dc082b27814867f3
Michael Wang <isVoid@users.noreply.github.com>
2021-06-02 15:23:01 -0700

    reverts stable sorts requirement. Fixes tests

--- python/cudf/cudf/tests/test_groupby.py
+++ python/cudf/cudf/tests/test_groupby.py
@@ -35,12 +35,10 @@ def assert_groupby_results_equal(
     expect, got, sort=True, as_index=True, by=None, **kwargs
 ):
     # Because we don't sort by index by default in groupby,
-    # sort expect and got by index before comparing. We use
-    # merge sort because it's stable, in transform operations
-    # we require rows to maintain their order within groups.
+    # sort expect and got by index before comparing.
     if sort:
         if as_index:
-            expect = expect.sort_index(kind="mergesort")
+            expect = expect.sort_index()
             got = got.sort_index()
         else:
             assert by is not None
@@ -1701,6 +1699,7 @@ def test_groupby_mix_agg_scan():
         gb.agg(func)
 
 
+@pytest.mark.parametrize("nelem", [2, 3, 100, 1000])
 @pytest.mark.parametrize("shift_perc", [0.5, 1.0, 1.5])
 @pytest.mark.parametrize("direction", [1, -1])
 @pytest.mark.parametrize("fill_value", [None, np.nan, 42])
