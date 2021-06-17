078e90ee7c526e06ebff82b45a4af0bf5bdf2fde
Nghia Truong <nghiatruong.vn@gmail.com>
2021-06-17 16:46:12 -0600

    Fix headers

--- cpp/tests/groupby/merge_lists_tests.cpp
+++ cpp/tests/groupby/merge_lists_tests.cpp
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-#include <tests/groupby/groupby_test_util.hpp>
-
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_wrapper.hpp>
 #include <cudf_test/iterator_utilities.hpp>
@@ -24,6 +22,7 @@
 #include <cudf/concatenate.hpp>
 #include <cudf/copying.hpp>
 #include <cudf/detail/aggregation/aggregation.hpp>
+#include <cudf/groupby.hpp>
 #include <cudf/table/table_view.hpp>
 
 using namespace cudf::test::iterators;
