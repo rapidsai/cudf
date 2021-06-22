23f80ba58a1495a362681a1322b34217978adef2
Michael Wang <isVoid@users.noreply.github.com>
2021-06-22 13:51:33 -0700

    docs

--- cpp/include/cudf/utilities/type_checks.hpp
+++ cpp/include/cudf/utilities/type_checks.hpp
@@ -8,17 +8,12 @@ namespace cudf {
  * @brief Compares the type of two `column_view`s
  *
  * This function returns true if the type of `lhs` equals that of `rhs`.
- *
- * Specifically, for fixed point types, the scale is compared.
- *
- * For dictionary types, the type of the keys are compared if both are
- * non-empty columns.
- *
- * For lists types, the type of child columns are compared recursively.
- *
- * For struct types, the type of each field are compared in order.
- *
- * For all other types, the `id` of `data_type` is compared.
+ * - For fixed point types, the scale is compared.
+ * - For dictionary types, the type of the keys are compared if both are
+ *   non-empty columns.
+ * - For lists types, the type of child columns are compared recursively.
+ * - For struct types, the type of each field are compared in order.
+ * - For all other types, the `id` of `data_type` is compared.
  *
  * @param lhs The first `column_view` to compare
  * @param rhs The second `column_view` to compare
