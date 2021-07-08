5cf1cf1afccacd0f7c9b0d47596176926b8b0858
Mike Wilson <knobby@burntsheep.com>
2021-07-08 01:52:36 +0000

    Adding row to column conversion code. Performance falls off a cliff, but starts out reasonably. I haven't looked at this in nsight yet.

--- cpp/benchmarks/row_conversion/row_conversion.cpp
+++ cpp/benchmarks/row_conversion/row_conversion.cpp
@@ -91,7 +91,7 @@ static void BM_new_to_row(benchmark::State& state)
   state.SetBytesProcessed(state.iterations() * total_bytes * 2 * table->num_rows());
 }
 
-/*static void BM_from_row(benchmark::State& state)
+static void BM_old_from_row(benchmark::State& state)
 {
   cudf::size_type const n_rows{(cudf::size_type)state.range(0)};
   auto const table = create_random_table({cudf::type_id::INT8,
@@ -123,36 +123,62 @@ static void BM_new_to_row(benchmark::State& state)
   }
 
   state.SetBytesProcessed(state.iterations() * total_bytes * 2 * table->num_rows());
-}*/
+}
 
-#define OLD_TO_ROW_CONVERSION_BENCHMARK_DEFINE(name, f) \
-  BENCHMARK_DEFINE_F(RowConversion, name)           \
-  (::benchmark::State & st) { f(st); }              \
-  BENCHMARK_REGISTER_F(RowConversion, name)         \
-    ->RangeMultiplier(8)                            \
-    ->Ranges({{1 << 6, 1 << 20}})                   \
-    ->UseManualTime()                               \
+static void BM_new_from_row(benchmark::State& state)
+{
+  cudf::size_type const n_rows{(cudf::size_type)state.range(0)};
+  auto const table = create_random_table({cudf::type_id::INT8,
+                                          cudf::type_id::INT32,
+                                          cudf::type_id::INT16,
+                                          cudf::type_id::INT64,
+                                          cudf::type_id::INT32,
+                                          cudf::type_id::BOOL8,
+                                          cudf::type_id::UINT16,
+                                          cudf::type_id::UINT8,
+                                          cudf::type_id::UINT64},
+                                         256,
+                                         row_count{n_rows});
+
+  std::vector<cudf::data_type> schema;
+  cudf::size_type total_bytes = 0;
+  for (int i = 0; i < table->num_columns(); ++i) {
+    auto t = table->get_column(i).type();
+    schema.push_back(t);
+    total_bytes += cudf::size_of(t);
+  }
+
+  auto rows = cudf::convert_to_rows(table->view());
+
+  for (auto _ : state) {
+    cuda_event_timer raii(state, true, rmm::cuda_stream_default);
+
+    auto out = cudf::convert_from_rows2(rows, schema);
+  }
+
+  state.SetBytesProcessed(state.iterations() * total_bytes * 2 * table->num_rows());
+}
+
+#define TO_ROW_CONVERSION_BENCHMARK_DEFINE(name, f) \
+  BENCHMARK_DEFINE_F(RowConversion, name)               \
+  (::benchmark::State & st) { f(st); }                  \
+  BENCHMARK_REGISTER_F(RowConversion, name)             \
+    ->RangeMultiplier(8)                                \
+    ->Ranges({{1 << 6, 1 << 20}})                       \
+    ->UseManualTime()                                   \
     ->Unit(benchmark::kMillisecond);
 
-#define NEW_TO_ROW_CONVERSION_BENCHMARK_DEFINE(name, f) \
-  BENCHMARK_DEFINE_F(RowConversion, name)           \
-  (::benchmark::State & st) { f(st); }              \
-  BENCHMARK_REGISTER_F(RowConversion, name)         \
-    ->RangeMultiplier(8)                            \
-    ->Ranges({{1 << 6, 1 << 20}})                   \
-    ->UseManualTime()                               \
-    ->Unit(benchmark::kMillisecond);
+TO_ROW_CONVERSION_BENCHMARK_DEFINE(old_to_row_conversion, BM_old_to_row)
+TO_ROW_CONVERSION_BENCHMARK_DEFINE(new_to_row_conversion, BM_new_to_row)
 
-OLD_TO_ROW_CONVERSION_BENCHMARK_DEFINE(old_to_row_conversion, BM_old_to_row)
-NEW_TO_ROW_CONVERSION_BENCHMARK_DEFINE(new_to_row_conversion, BM_new_to_row)
-
-#define FROM_ROW_CONVERSION_BENCHMARK_DEFINE(name) \
+#define FROM_ROW_CONVERSION_BENCHMARK_DEFINE(name, f) \
   BENCHMARK_DEFINE_F(RowConversion, name)          \
-  (::benchmark::State & st) { BM_from_row(st); }   \
+  (::benchmark::State & st) { f(st); }   \
   BENCHMARK_REGISTER_F(RowConversion, name)        \
     ->RangeMultiplier(8)                           \
-    ->Ranges({{1 << 6, 1 << 22}})                  \
+    ->Ranges({{1 << 6, 1 << 20}})                  \
     ->UseManualTime()                              \
     ->Unit(benchmark::kMillisecond);
 
-//FROM_ROW_CONVERSION_BENCHMARK_DEFINE(from_row_conversion)
+FROM_ROW_CONVERSION_BENCHMARK_DEFINE(old_from_row_conversion, BM_old_from_row)
+FROM_ROW_CONVERSION_BENCHMARK_DEFINE(new_from_row_conversion, BM_new_from_row)
