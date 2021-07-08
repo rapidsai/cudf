ab91dd4c2c6ccb68e4360c42a23c03694b922931
Vyas Ramasubramani <vyasr@nvidia.com>
2021-07-08 11:23:36 -0700

    Remove now outdated TODO.

--- python/cudf/cudf/_lib/utils.pyx
+++ python/cudf/cudf/_lib/utils.pyx
@@ -186,9 +186,6 @@ def _index_level_name(index_name, level, column_names):
 
 
 # TODO: Look into simplifying calling APIs that don't use the index from this.
-# TODO: Figure out whether _from_data can be changed to accept a dict rather
-# than a ColumnAccessor, which would allow a much more idiomatic usage of this
-# function via _from_data(*data_from_unique_ptr(...)).
 # TODO: There's a bit of an inconsistency in use cases where calling functions
 # are calling this function once to get the index and once to get the data. The
 # index is converted to an index object, while the data is not. Perhaps this
@@ -294,6 +291,5 @@ cdef data_from_table_view(
         )
         column_idx += 1
         source_column_idx += 1
-    data = dict(zip(column_names, data_columns))
 
-    return data, index
+    return dict(zip(column_names, data_columns)), index
