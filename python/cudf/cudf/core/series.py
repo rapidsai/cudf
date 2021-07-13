c821791d24540a8d6c0afaa81ada5d85eb1f4f60
sft-managed <u00u9d1cxwxama1MwJ357@rl-dgx-r12-u27-rapids-dgx112.raplab.nvidia.com>
2021-07-13 18:54:30 +0000

    Moved 'pad' and 'backfil' support to Frame

--- python/cudf/cudf/core/series.py
+++ python/cudf/cudf/core/series.py
@@ -6050,13 +6050,9 @@ class Series(SingleColumnFrame, Serializable):
             raise NotImplementedError("limit parameter not supported yet.")
         if freq is not None:
             raise NotImplementedError("freq parameter not supported yet.")
-        if fill_method == "pad":
-            fill_method = "ffill"
-        elif fill_method == "backfill":
-            fill_method = "bfill"
-        elif fill_method not in ["ffill", "bfill"]:
+        elif fill_method not in {"ffill", "pad", "bfill", "backfill"}:
             raise ValueError(
-                "fill_method must be either 'ffill', 'pad', "
+                "fill_method must be one of 'ffill', 'pad', "
                 "'bfill', or 'backfill'."
             )
 
