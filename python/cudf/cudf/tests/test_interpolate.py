676388b774786dcd647395cd636c11a47cd782f0
brandon-b-miller <brmiller@nvidia.com>
2021-07-13 11:55:46 -0700

    forgot test

---
+++ python/cudf/cudf/tests/test_interpolate.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import cudf
+import pytest
+from cudf.testing._utils import assert_eq
+
+@pytest.mark.parametrize("data", [
+    {
+        'A': [1, None, 3]
+    }
+])
+@pytest.mark.parametrize("method", ['linear'])
+@pytest.mark.parametrize("axis", [0])
+def test_interpolate_nans(data, method,axis):  
+    # doesn't seem to work with NAs just yet
+    gdf = cudf.DataFrame(data)
+    pdf = gdf.to_pandas()
+    
+    expect = pdf.interpolate(method=method, axis=axis)
+    got = gdf.interpolate(method=method, axis=axis)
+
+    assert_eq(expect, got)
