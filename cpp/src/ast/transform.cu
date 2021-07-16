825a6a319e2c0062e2d967b02c68d4f4911ca4c3
Vyas Ramasubramani <vyasr@nvidia.com>
2021-07-16 11:31:31 -0700

    Move compute_column API to preexisting transform namespace.

--- cpp/src/ast/transform.cu
+++ cpp/src/ast/transform.cu
@@ -17,7 +17,6 @@
 #include <cudf/ast/detail/evaluator.cuh>
 #include <cudf/ast/nodes.hpp>
 #include <cudf/ast/operators.hpp>
-#include <cudf/ast/transform.hpp>
 #include <cudf/column/column_device_view.cuh>
 #include <cudf/column/column_factories.hpp>
 #include <cudf/detail/nvtx/ranges.hpp>
@@ -26,6 +25,7 @@
 #include <cudf/scalar/scalar_device_view.cuh>
 #include <cudf/table/table_device_view.cuh>
 #include <cudf/table/table_view.hpp>
+#include <cudf/transform.hpp>
 #include <cudf/types.hpp>
 #include <cudf/utilities/error.hpp>
 #include <cudf/utilities/type_dispatcher.hpp>
@@ -34,7 +34,6 @@
 #include <rmm/mr/device/device_memory_resource.hpp>
 
 namespace cudf {
-namespace ast {
 namespace detail {
 
 /**
@@ -54,7 +53,7 @@ namespace detail {
 template <cudf::size_type max_block_size, bool has_nulls>
 __launch_bounds__(max_block_size) __global__
   void compute_column_kernel(table_device_view const table,
-                             device_ast_plan plan,
+                             ast::detail::device_ast_plan plan,
                              mutable_column_device_view output_column)
 {
   // The (required) extern storage of the shared memory array leads to
@@ -62,8 +61,8 @@ __launch_bounds__(max_block_size) __global__
   // workaround is to declare an arbitrary (here char) array type then cast it
   // after the fact to the appropriate type.
   extern __shared__ char raw_intermediate_storage[];
-  IntermediateDataType<has_nulls>* intermediate_storage =
-    reinterpret_cast<IntermediateDataType<has_nulls>*>(raw_intermediate_storage);
+  ast::detail::IntermediateDataType<has_nulls>* intermediate_storage =
+    reinterpret_cast<ast::detail::IntermediateDataType<has_nulls>*>(raw_intermediate_storage);
 
   auto thread_intermediate_storage = &intermediate_storage[threadIdx.x * plan.num_intermediates];
   auto const start_idx = static_cast<cudf::size_type>(threadIdx.x + blockIdx.x * blockDim.x);
@@ -72,13 +71,13 @@ __launch_bounds__(max_block_size) __global__
     cudf::ast::detail::expression_evaluator<has_nulls>(table, plan, thread_intermediate_storage);
 
   for (cudf::size_type row_index = start_idx; row_index < table.num_rows(); row_index += stride) {
-    auto output_dest = mutable_column_expression_result<has_nulls>(output_column);
+    auto output_dest = ast::detail::mutable_column_expression_result<has_nulls>(output_column);
     evaluator.evaluate(output_dest, row_index);
   }
 }
 
 std::unique_ptr<column> compute_column(table_view const table,
-                                       expression const& expr,
+                                       ast::expression const& expr,
                                        rmm::cuda_stream_view stream,
                                        rmm::mr::device_memory_resource* mr)
 {
@@ -90,7 +89,7 @@ std::unique_ptr<column> compute_column(table_view const table,
   auto const nullable  = cudf::nullable(table);
   auto const has_nulls = nullable && cudf::has_nulls(table);
 
-  auto const plan = ast_plan{expr, table, has_nulls, stream, mr};
+  auto const plan = ast::detail::ast_plan{expr, table, has_nulls, stream, mr};
 
   auto const output_column_mask_state =
     nullable ? (has_nulls ? mask_state::UNINITIALIZED : mask_state::ALL_VALID)
@@ -119,11 +118,11 @@ std::unique_ptr<column> compute_column(table_view const table,
   // Execute the kernel
   auto table_device = table_device_view::create(table, stream);
   if (has_nulls) {
-    cudf::ast::detail::compute_column_kernel<MAX_BLOCK_SIZE, true>
+    cudf::detail::compute_column_kernel<MAX_BLOCK_SIZE, true>
       <<<config.num_blocks, config.num_threads_per_block, shmem_per_block, stream.value()>>>(
         *table_device, dev_plan, *mutable_output_device);
   } else {
-    cudf::ast::detail::compute_column_kernel<MAX_BLOCK_SIZE, false>
+    cudf::detail::compute_column_kernel<MAX_BLOCK_SIZE, false>
       <<<config.num_blocks, config.num_threads_per_block, shmem_per_block, stream.value()>>>(
         *table_device, dev_plan, *mutable_output_device);
   }
@@ -134,13 +133,11 @@ std::unique_ptr<column> compute_column(table_view const table,
 }  // namespace detail
 
 std::unique_ptr<column> compute_column(table_view const table,
-                                       expression const& expr,
+                                       ast::expression const& expr,
                                        rmm::mr::device_memory_resource* mr)
 {
   CUDF_FUNC_RANGE();
   return detail::compute_column(table, expr, rmm::cuda_stream_default, mr);
 }
-
-}  // namespace ast
 
 }  // namespace cudf
