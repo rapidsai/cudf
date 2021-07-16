6eaf685bc40ed814082c7b2e63a6adb74fa1718a
Vyas Ramasubramani <vyasr@nvidia.com>
2021-07-16 10:01:51 -0700

    Move evaluator into separate header.

--- cpp/src/join/conditional_join_kernels.cuh
+++ cpp/src/join/conditional_join_kernels.cuh
@@ -20,8 +20,8 @@
 #include <join/join_common_utils.hpp>
 
 #include <cub/cub.cuh>
+#include <cudf/ast/detail/evaluator.cuh>
 #include <cudf/ast/detail/linearizer.hpp>
-#include <cudf/ast/detail/transform.cuh>
 #include <cudf/ast/operators.hpp>
 #include <cudf/detail/utilities/cuda.cuh>
 #include <cudf/table/table_device_view.cuh>
