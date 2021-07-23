0e24ae8fbd2d3d5f44a39a0dcc1dc8a209cf71f7
vuule <vmilovanovic@nvidia.com>
2021-07-23 11:44:16 -0700

    re-enable json tests that were accidentally disabled

--- cpp/tests/io/json_test.cpp
+++ cpp/tests/io/json_test.cpp
@@ -145,7 +145,6 @@ void check_float_column(cudf::column_view const& col,
 struct JsonReaderTest : public cudf::test::BaseFixture {
 };
 
-/*
 TEST_F(JsonReaderTest, BasicJsonLines)
 {
   std::string data = "[1, 1.1]\n[2, 2.2]\n[3, 3.3]\n";
@@ -615,10 +614,10 @@ TEST_F(JsonReaderTest, JsonLinesObjectsOutOfOrder)
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(result.tbl->get_column(2),
                                  cudf::test::strings_column_wrapper({"aaa", "bbb"}));
 }
-*/
-/*
+
 // currently, the json reader is strict about having non-empty input.
-TEST_F(JsonReaderTest, EmptyFile) {
+TEST_F(JsonReaderTest, EmptyFile)
+{
   auto filepath = temp_env->get_temp_dir() + "EmptyFile.csv";
   {
     std::ofstream outfile{filepath, std::ofstream::out};
@@ -634,7 +633,8 @@ TEST_F(JsonReaderTest, EmptyFile) {
 }
 
 // currently, the json reader is strict about having non-empty input.
-TEST_F(JsonReaderTest, NoDataFile) {
+TEST_F(JsonReaderTest, NoDataFile)
+{
   auto filepath = temp_env->get_temp_dir() + "NoDataFile.csv";
   {
     std::ofstream outfile{filepath, std::ofstream::out};
@@ -648,8 +648,7 @@ TEST_F(JsonReaderTest, NoDataFile) {
   const auto view = result.tbl->view();
   EXPECT_EQ(0, view.num_columns());
 }
-*/
-/*
+
 TEST_F(JsonReaderTest, ArrowFileSource)
 {
   const std::string fname = temp_env->get_temp_dir() + "ArrowFileSource.csv";
@@ -698,7 +697,8 @@ TEST_F(JsonReaderTest, InvalidFloatingPoint)
 
   const auto col_data = cudf::test::to_host<float>(result.tbl->view().column(0));
   // col_data.first contains the column data
-  for (const auto& elem : col_data.first) ASSERT_TRUE(std::isnan(elem));
+  for (const auto& elem : col_data.first)
+    ASSERT_TRUE(std::isnan(elem));
   // col_data.second contains the bitmasks
   ASSERT_EQ(0u, col_data.second[0]);
 }
@@ -861,7 +861,7 @@ TEST_F(JsonReaderTest, ParseOutOfRangeIntegers)
   CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(input_less_int64_min_append, view.column(8));
   CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(input_mixed_range_append, view.column(9));
 }
-*/
+
 TEST_F(JsonReaderTest, JsonLinesMultipleFileInputs)
 {
   const std::string file1 = temp_env->get_temp_dir() + "JsonLinesFileTest1.json";
