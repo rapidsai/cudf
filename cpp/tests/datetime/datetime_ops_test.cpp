756499ff91c30fbc196397ff55e8f8733f5e1db5
shaneding <shane200195@gmail.com>
2021-07-26 18:42:32 -0400

    Add quarters to libcudf datetime (#8779)
    Partly addresses #8676.
    This PR adds `date_time:: extract_quarter`. The function returns an int `(1,2,3,4)` depending on which quarter of the year the date is in. Also fixed an incorrect date in previous testcase (#8711).
    
    Authors:
      - https://github.com/shaneding
    
    Approvers:
      - Nghia Truong (https://github.com/ttnghia)
      - Conor Hoekstra (https://github.com/codereport)
    
    URL: https://github.com/rapidsai/cudf/pull/8779

--- cpp/tests/datetime/datetime_ops_test.cpp
+++ cpp/tests/datetime/datetime_ops_test.cpp
@@ -17,6 +17,7 @@
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_utilities.hpp>
 #include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/iterator_utilities.hpp>
 #include <cudf_test/timestamp_utilities.cuh>
 #include <cudf_test/type_lists.hpp>
 
@@ -552,7 +553,7 @@ TEST_F(BasicDatetimeOpsTest, TestIsLeapYear)
         915148800L,     // 1999-01-01 00:00:00 GMT - non leap year
         -11663029161L,  // 1600-5-31 05:40:39 GMT - leap year
         707904541L,     // 1992-06-07 08:09:01 GMT - leap year
-        2181048447L,    // 1900-11-20 09:12:33 GMT - non leap year
+        -2181005247L,   // 1900-11-20 09:12:33 GMT - non leap year
         0L,             // UNIX EPOCH 1970-01-01 00:00:00 GMT - non leap year
         -12212553600L,  // First full year of Gregorian Calandar 1583-01-01 00:00:00 - non-leap-year
         0L,             // null
@@ -567,6 +568,41 @@ TEST_F(BasicDatetimeOpsTest, TestIsLeapYear)
     cudf::test::fixed_width_column_wrapper<bool>{
       {true, XXX, false, true, true, false, false, false, XXX, true, false, XXX},
       {true, false, true, true, true, true, true, true, false, true, true, false}});
+}
+
+TEST_F(BasicDatetimeOpsTest, TestQuarter)
+{
+  using namespace cudf::test;
+  using namespace cudf::datetime;
+  using namespace cuda::std::chrono;
+  using namespace cudf::test::iterators;
+
+  // Time in seconds since epoch
+  // Dates converted using epochconverter.com
+  auto timestamps_s =
+    cudf::test::fixed_width_column_wrapper<cudf::timestamp_s, cudf::timestamp_s::rep>{
+      {
+        1594332839L,    // 2020-07-09 10:13:59 GMT
+        0L,             // null
+        915148800L,     // 1999-01-01 00:00:00 GMT
+        -11663029161L,  // 1600-5-31 05:40:39 GMT
+        707904541L,     // 1992-06-07 08:09:01 GMT
+        -2181005247L,   // 1900-11-20 09:12:33 GMT
+        0L,             // UNIX EPOCH 1970-01-01 00:00:00 GMT
+        -12212553600L,  // First full year of Gregorian Calandar 1583-01-01 00:00:00
+        0L,             // null
+        13591632822L,   // 2400-09-13 13:33:42 GMT
+        4539564243L,    // 2113-11-08 06:04:03 GMT
+        0L,             // null
+        1608581568L,    // 2020-12-21 08:12:48 GMT
+        1584821568L,    // 2020-03-21 08:12:48 GMT
+      },
+      nulls_at({1, 8, 11})};
+
+  auto quarter = cudf::test::fixed_width_column_wrapper<int16_t>{
+    {3, 0 /*null*/, 1, 2, 2, 4, 1, 1, 0 /*null*/, 3, 4, 0 /*null*/, 4, 1}, nulls_at({1, 8, 11})};
+
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(*extract_quarter(timestamps_s), quarter);
 }
 
 CUDF_TEST_PROGRAM_MAIN()
