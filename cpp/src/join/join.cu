2270b9035b73a3ef33d631aba332d896f1ba5614
davidwendt <dwendt@nvidia.com>
2021-07-30 15:42:55 -0400

    move more inline utilities to join_utils.cu

--- cpp/src/join/join.cu
+++ cpp/src/join/join.cu
@@ -26,27 +26,6 @@
 namespace cudf {
 namespace detail {
 
-// declared in join/join_common_utils.hpp
-bool is_trivial_join(table_view const& left, table_view const& right, join_kind join_type)
-{
-  // If there is nothing to join, then send empty table with all columns
-  if (left.is_empty() || right.is_empty()) { return true; }
-
-  // If left join and the left table is empty, return immediately
-  if ((join_kind::LEFT_JOIN == join_type) && (0 == left.num_rows())) { return true; }
-
-  // If Inner Join and either table is empty, return immediately
-  if ((join_kind::INNER_JOIN == join_type) && ((0 == left.num_rows()) || (0 == right.num_rows()))) {
-    return true;
-  }
-
-  // If left semi join (contains) and right table is empty,
-  // return immediately
-  if ((join_kind::LEFT_SEMI_JOIN == join_type) && (0 == right.num_rows())) { return true; }
-
-  return false;
-}
-
 std::pair<std::unique_ptr<rmm::device_uvector<size_type>>,
           std::unique_ptr<rmm::device_uvector<size_type>>>
 inner_join(table_view const& left_input,
