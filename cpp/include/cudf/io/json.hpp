8fa26947ec0c8a75ceb2b71186eb0038e871d85a
vuule <vmilovanovic@nvidia.com>
2021-08-03 11:54:51 -0700

    add missing empty line

--- cpp/include/cudf/io/json.hpp
+++ cpp/include/cudf/io/json.hpp
@@ -170,6 +170,7 @@ class json_reader_options {
    * @param types Vector of dtypes
    */
   void set_dtypes(std::vector<data_type> types) { _dtypes = std::move(types); }
+
   /**
    * @brief Set data types for columns to be read.
    *
