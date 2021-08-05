90217944ef7cfbb91cfbf357e0691e9f9d5a5726
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2021-08-05 18:05:16 -0400

    style update

--- python/cudf/cudf/tests/test_dataframe.py
+++ python/cudf/cudf/tests/test_dataframe.py
@@ -8748,7 +8748,9 @@ def test_frame_series_where_other(data):
     expected = gdf.where(gdf["b"] == 1, cudf.NA)
     actual = pdf.where(pdf["b"] == 1, pd.NA)
     for col in actual.columns:  # dtypes will be different
-        assert_eq(actual[col].fillna(-1).values, expected[col].fillna(-1).values)
+        assert_eq(
+            actual[col].fillna(-1).values, expected[col].fillna(-1).values
+        )
 
     expected = gdf.where(gdf["b"] == 1, 0)
     actual = pdf.where(pdf["b"] == 1, 0)
