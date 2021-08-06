7816a3d3714b009ec0bf00f48acb477139e30373
GALI PREM SAGAR <sagarprem75@gmail.com>
2021-08-05 21:17:56 -0500

    Add deprecation warning for `Series.set_mask` API (#8943)
    `Series.set_mask` is more of internal implementation detail that the end-users will not have knowledge about, hence deprecating the API.
    
    Authors:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
    
    Approvers:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/cudf/pull/8943

--- python/cudf/cudf/core/series.py
+++ python/cudf/cudf/core/series.py
@@ -845,6 +845,11 @@ class Series(SingleColumnFrame, Serializable):
         4       5
         dtype: int64
         """
+        warnings.warn(
+            "Series.set_mask is deprecated and will be removed "
+            "in the future.",
+            DeprecationWarning,
+        )
         col = self._column.set_mask(mask)
         return self._copy_construct(data=col)
 
