a62ab3214f8dfe6f1d068eaf24dcbf446d87e73d
Ashwin Srinath <3190405+shwina@users.noreply.github.com>
2021-08-09 18:55:47 -0400

    Update python/cudf/cudf/api/types.py
    Co-authored-by: Vyas Ramasubramani <vyas.ramasubramani@gmail.com>

--- python/cudf/cudf/api/types.py
+++ python/cudf/cudf/api/types.py
@@ -31,8 +31,8 @@ def dtype(arbitrary):
     """
     Return the cuDF-supported dtype corresponding to `arbitrary`.
 
-    Inputs
-    ------
+    Parameters
+    ------------
     arbitrary: dtype or scalar-like
 
     Returns
