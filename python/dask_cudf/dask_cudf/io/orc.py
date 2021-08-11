1ffffe5d0a13c5f13189d7ed64867a9ec39ac497
rjzamora <rzamora217@gmail.com>
2021-08-11 14:09:20 -0700

    saving state

--- python/dask_cudf/dask_cudf/io/orc.py
+++ python/dask_cudf/dask_cudf/io/orc.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2020-2021, NVIDIA CORPORATION.
+import warnings
 from contextlib import ExitStack
-
 from io import BufferedWriter, IOBase
 
 from fsspec.core import get_fs_token_paths
@@ -9,6 +9,7 @@ from pyarrow import orc as orc
 
 from dask import dataframe as dd
 from dask.base import tokenize
+from dask.dataframe.io.parquet.core import apply_filters
 from dask.dataframe.io.utils import _get_pyarrow_dtypes
 
 import cudf
@@ -19,17 +20,44 @@ try:
     from dask.dataframe.io.orc.arrow import ArrowORCEngine
 
     class CudfORCEngine(ArrowORCEngine):
-
         @classmethod
-        def _create_meta(cls, fs, part0, columns, *args, **kwargs):
-            pd_meta = ArrowORCEngine._create_meta(fs, part0, columns, *args, **kwargs)
-            if part0:
-                with fs.open(part0[0], "rb") as f:
+        def create_output_meta(
+            cls,
+            sample=None,
+            columns=None,
+            schema=None,
+            index=None,
+            directory_partition_keys=None,
+            **kwargs,
+        ):
+
+            if kwargs:
+                # We want to allow Dask to add to this function
+                # signature without an error, but we do want to
+                # be aware of any changes.
+                warnings.warn(
+                    f"Unexpected kwargs ({kwargs}) were passed to "
+                    f"`CudfORCEngine.create_output_meta`. This version "
+                    f"of Dask-cuDF may be out of sync with upstream Dask."
+                )
+
+            pd_meta = ArrowORCEngine.create_output_meta(
+                sample=None,
+                columns=columns,
+                schema=schema,
+                index=index,
+                directory_partition_keys=directory_partition_keys,
+            )
+            if sample:
+                part = sample.get("part")
+                fs = sample.get("fs")
+                read_kwargs = sample.get("read_kwargs")
+                with fs.open(part[0], "rb") as f:
                     cudf_meta = cudf.read_orc(
                         f,
-                        stripes=[0] if part0[1] else None,
+                        stripes=[0] if part[1] else None,
                         columns=columns,
-                        **kwargs,
+                        **read_kwargs,
                     )
             else:
                 cudf_meta = cudf.DataFrame(index=pd_meta.index)
@@ -39,6 +67,88 @@ try:
                     cudf_meta[col] = as_column(pd_meta[col])
 
             return cudf_meta
+
+        @classmethod
+        def filter_file_stripes(
+            cls,
+            file_path,
+            orc_file=None,
+            filters=None,
+            stat_columns=None,
+            stat_hive_part=None,
+            fs=None,
+            file_handle=None,
+            gather_statistics=True,
+            **kwargs,
+        ):
+
+            if kwargs:
+                # We want to allow Dask to add to this function
+                # signature without an error, but we do want to
+                # be aware of any changes.
+                warnings.warn(
+                    f"Unexpected kwargs ({kwargs}) were passed to "
+                    f"`CudfORCEngine.filter_file_stripes`. This version "
+                    f"of Dask-cuDF may be out of sync with upstream Dask."
+                )
+
+            # Use cudf to gather stripe statistics
+            cudf_statistics = []
+            if gather_statistics:
+                if file_handle is None:
+                    with fs.open(file_path, "rb") as f:
+                        f.seek(0)
+                        cudf_statistics = cudf.io.orc.read_orc_statistics(
+                            f, columns=stat_columns,
+                        )[1]
+                else:
+                    file_handle.seek(0)
+                    cudf_statistics = cudf.io.orc.read_orc_statistics(
+                        file_handle, columns=stat_columns,
+                    )[1]
+                stripes = list(range(len(cudf_statistics)))
+
+            # Populate statistics with stripe statistics from cudf
+            # and "known" statistics from the directory partitioning
+            statistics = []
+            for i, stripe in enumerate(stripes):
+                stats = {"file-path": file_path}
+                column_stats = []
+                columns_populated = []
+                if cudf_statistics:
+                    for k, v in cudf_statistics[i].items():
+                        if k == "col0":
+                            stats["num-rows"] = v.get("number_of_values", None)
+                        elif k in stat_columns:
+                            columns_populated.append(k)
+                            column_stats.append(
+                                {
+                                    "name": k,
+                                    "count": v.get("number_of_values", None),
+                                    "min": v.get("minimum", None),
+                                    "max": v.get("maximum", None),
+                                }
+                            )
+                for (k, v) in stat_hive_part:
+                    if k not in columns_populated:
+                        column_stats.append(
+                            {
+                                "name": k,
+                                "count": stats["num-rows"],
+                                "min": v,
+                                "max": v,
+                            }
+                        )
+                stats["columns"] = column_stats
+                statistics.append(stats)
+
+            # Apply filters (if necessary)
+            if filters:
+                stripes, statistics = apply_filters(
+                    stripes, statistics, filters
+                )
+
+            return stripes, statistics
 
         @classmethod
         def read_partition(
@@ -54,13 +164,14 @@ try:
             # Create a seperate dataframe for each directory partition.
             # We are only creating a single cudf dataframe if there
             # are no partition columns.
-            tables = []
             partitions = []
             partition_uniques = partition_uniques or {}
             if columns:
                 # Separate file columns and partition columns
                 file_columns = [c for c in columns if c in set(schema)]
-                partition_columns = [c for c in columns if c not in set(schema)]
+                partition_columns = [
+                    c for c in columns if c not in set(schema)
+                ]
             else:
                 file_columns, partition_columns = None, list(partition_uniques)
 
@@ -75,12 +186,7 @@ try:
                 else:
                     dfs.append(
                         cls._read_partition(
-                            fs,
-                            path_list,
-                            file_columns,
-                            filters,
-                            stripe_list,
-                            **kwargs,
+                            fs, path_list, file_columns, stripe_list, **kwargs,
                         )
                     )
                     partitions.append(hive_parts)
@@ -89,12 +195,7 @@ try:
                     hive_parts = next_hive_parts
             dfs.append(
                 cls._read_partition(
-                    fs,
-                    path_list,
-                    file_columns,
-                    filters,
-                    stripe_list,
-                    **kwargs,
+                    fs, path_list, file_columns, stripe_list, **kwargs,
                 )
             )
             partitions.append(hive_parts)
@@ -107,7 +208,12 @@ try:
                         # columns are NOT in our table yet.
                         categories = partition_uniques[part_name]
 
-                        col = as_column(categories.index(cat)).as_frame().repeat(len(df))._data[None]
+                        col = (
+                            as_column(categories.index(cat))
+                            .as_frame()
+                            .repeat(len(dfs[i]))
+                            ._data[None]
+                        )
                         dfs[i][part_name] = build_categorical_column(
                             categories=categories,
                             codes=as_column(col.base_data, dtype=col.dtype),
@@ -117,9 +223,10 @@ try:
                         )
             return cudf.concat(dfs)
 
-
         @classmethod
-        def _read_partition(cls, fs, path_list, columns, filters, stripe_list, **kwargs):
+        def _read_partition(
+            cls, fs, path_list, columns, stripe_list, **kwargs
+        ):
 
             with ExitStack() as stack:
                 if cudf.utils.ioutils._is_local_filesystem(fs):
@@ -135,14 +242,29 @@ try:
                 df = cudf.io.read_orc(
                     _source_list,
                     columns=columns,
-                    filters=filters,
                     stripes=stripe_list,
                     **kwargs,
                 )
             return df
 
+
 except ImportError:
     CudfORCEngine = None
+
+
+def read_orc(*args, legacy=False, **kwargs):
+    if CudfORCEngine is None or legacy is True:
+        return read_orc_legacy(*args, **kwargs)
+
+    # Move `engine` argument to `read_kwargs` to
+    # align with legacy read_orc API
+    read_kwargs = kwargs.pop("read_kwargs", {})
+    if "engine" not in read_kwargs:
+        read_kwargs["engine"] = kwargs.pop("engine", None)
+
+    return dd.read_orc(
+        *args, engine=CudfORCEngine, read_kwargs=read_kwargs, **kwargs,
+    )
 
 
 def _read_orc_stripe(fs, path, stripe, columns, kwargs=None):
@@ -156,17 +278,9 @@ def _read_orc_stripe(fs, path, stripe, columns, kwargs=None):
     return df_stripe
 
 
-def read_orc(*args, legacy=False, **kwargs):
-    if CudfORCEngine is None or legacy is True:
-        return read_orc_legacy(*args, **kwargs)
-
-    engine = kwargs.pop("engine", CudfORCEngine)
-    if engine == "cudf":
-        engine = CudfORCEngine
-    return dd.read_orc(*args, engine=engine, **kwargs)
-
-
-def read_orc_legacy(path, columns=None, filters=None, storage_options=None, **kwargs):
+def read_orc_legacy(
+    path, columns=None, filters=None, storage_options=None, **kwargs
+):
     """Read cudf dataframe from ORC file(s).
 
     Note that this function is mostly borrowed from upstream Dask.
