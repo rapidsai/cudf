ad7f2b206738a246ae4ac2b6e4db9f6ebfb2daa1
galipremsagar <sagarprem75@gmail.com>
2021-08-11 18:36:19 -0700

    fetch correct grouping keys

--- python/dask_cudf/dask_cudf/groupby.py
+++ python/dask_cudf/dask_cudf/groupby.py
@@ -16,6 +16,8 @@ from dask.dataframe.core import (
 from dask.dataframe.groupby import DataFrameGroupBy, SeriesGroupBy
 from dask.highlevelgraph import HighLevelGraph
 
+import cudf
+
 
 class CudfDataFrameGroupBy(DataFrameGroupBy):
     def __init__(self, *args, **kwargs):
@@ -79,9 +81,14 @@ class CudfDataFrameGroupBy(DataFrameGroupBy):
             and isinstance(self.index, (str, list))
             and _is_supported(arg, _supported)
         ):
+            if isinstance(self._meta.grouping.keys, cudf.MultiIndex):
+                keys = self._meta.grouping.keys.names
+            else:
+                keys = self._meta.grouping.keys.name
+
             return groupby_agg(
                 self.obj,
-                self.index,
+                keys,
                 arg,
                 split_every=split_every,
                 split_out=split_out,
