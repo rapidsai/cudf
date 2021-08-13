c2a6286e07b5aefaa2ecaf78128305cce3f6395c
Michael Wang <isVoid@users.noreply.github.com>
2021-08-13 02:18:46 -0700

    style

--- python/cudf/cudf/tests/test_decimal.py
+++ python/cudf/cudf/tests/test_decimal.py
@@ -187,9 +187,8 @@ def test_typecast_from_int_to_decimal(data, from_dtype, to_dtype):
 def test_typecast_to_from_decimal(data, from_dtype, to_dtype):
     if from_dtype.scale > to_dtype.MAX_PRECISION:
         pytest.skip(
-            "In python this is supposed to overflow because "
-            "the representation value in the source exceeds the max representable"
-            "in destination dtype."
+            "This is supposed to overflow because the representation value in "
+            "the source exceeds the max representable in destination dtype."
         )
     s = data.astype(from_dtype)
 
