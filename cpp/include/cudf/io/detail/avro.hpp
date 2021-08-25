cf9eecf02d336dff20db006666fdae238e98eee4
Christopher Harris <xixonia@gmail.com>
2021-08-25 00:38:24 -0500

    adjust copyright in avro.hpp

--- cpp/include/cudf/io/detail/avro.hpp
+++ cpp/include/cudf/io/detail/avro.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2021, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,11 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
-/**
- * @file avro.hpp
- * @brief cuDF-IO reader classes API
  */
 
 #pragma once
