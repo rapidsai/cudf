55b69df2182491b7bbb0e30feaf3b25aa9e01622
Michael Wang <isVoid@users.noreply.github.com>
2021-08-25 13:56:16 -0700

    Use direct months type initialization, not alias

--- cpp/include/cudf/detail/datetime_ops.cuh
+++ cpp/include/cudf/detail/datetime_ops.cuh
@@ -21,14 +21,13 @@ struct add_calendrical_months_functor_impl {
   Timestamp __device__ operator()(Timestamp time_val, MonthType months_val)
   {
     using namespace cuda::std::chrono;
-    using duration_m = duration<int32_t, months::period>;
 
     // Get the days component from the input
     auto days_since_epoch = floor<days>(time_val);
 
     // Add the number of months
     year_month_day ymd{days_since_epoch};
-    ymd += duration_m{months_val};
+    ymd += duration<int32_t, months::period>{months_val};
 
     // If the new date isn't valid, scale it back to the last day of the
     // month.
