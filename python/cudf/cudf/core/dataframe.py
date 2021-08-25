5d085a3312d069cc91849ef21f9a80ae7345e9c5
Michael Wang <isVoid@users.noreply.github.com>
2021-08-24 17:39:40 -0700

    style

--- python/cudf/cudf/core/dataframe.py
+++ python/cudf/cudf/core/dataframe.py
@@ -547,10 +547,7 @@ class DataFrame(Frame, Serializable, GetAttrGetItemMixin):
         column_names = pickle.loads(header["column_names"])
         columns = column.deserialize_columns(header["columns"], column_frames)
 
-        return cls._from_data(
-            dict(zip(column_names, columns)),
-            index=index,
-        )
+        return cls._from_data(dict(zip(column_names, columns)), index=index,)
 
     @property
     def dtypes(self):
