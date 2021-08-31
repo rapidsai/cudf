009857d8a6a98679dcf33109052557805fc5b3d5
galipremsagar <sagarprem75@gmail.com>
2021-08-31 12:24:16 -0700

    add docs

--- python/cudf/cudf/core/groupby/groupby.py
+++ python/cudf/cudf/core/groupby/groupby.py
@@ -104,6 +104,37 @@ class GroupBy(Serializable):
         )
 
     def get_group(self, name, obj=None):
+        """
+        Construct DataFrame from group with provided name.
+
+        Parameters
+        ----------
+        name : object
+            The name of the group to get as a DataFrame.
+        obj : DataFrame, default None
+            The DataFrame to take the DataFrame out of.  If
+            it is None, the object groupby was called on will
+            be used.
+
+        Returns
+        -------
+        group : same type as obj
+
+        Examples
+        --------
+        >>> import cudf
+        >>> df = cudf.DataFrame({"X": ["A", "B", "A", "B"], "Y": [1, 4, 3, 2]})
+        >>> df
+        X  Y
+        0  A  1
+        1  B  4
+        2  A  3
+        3  B  2
+        >>> df.groupby("X").get_group("A")
+        X  Y
+        0  A  1
+        2  A  3
+        """
         if obj is None:
             obj = self.obj
 
