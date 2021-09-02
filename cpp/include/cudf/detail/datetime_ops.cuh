640e4b4da1f3e321909ce248d87ad28b9644aa76
Michael Wang <isVoid@users.noreply.github.com>
2021-09-02 15:22:03 -0700

    Apply suggestions from code review
    Co-authored-by: Jake Hemstad <jhemstad@nvidia.com>

--- cpp/include/cudf/detail/datetime_ops.cuh
+++ cpp/include/cudf/detail/datetime_ops.cuh
@@ -21,26 +21,16 @@ namespace cudf {
 namespace datetime {
 namespace detail {
 
-template <typename Timestamp, typename MonthType>
-__device__ Timestamp add_calendrical_months_with_scale_back(Timestamp time_val,
-                                                            MonthType months_val)
-{
-  using namespace cuda::std::chrono;
-
-  // Get the days component from the input
   auto const days_since_epoch = floor<days>(time_val);
-
-  // Add the number of months
-  year_month_day ymd{days_since_epoch};
-  ymd += duration<int32_t, months::period>{months_val};
-
-  // If the new date isn't valid, scale it back to the last day of the
-  // month.
-  if (!ymd.ok()) ymd = ymd.year() / ymd.month() / last;
-
-  // Put back the time component to the date
-  return sys_days{ymd} + (time_val - days_since_epoch);
-}
+  
+  auto const date = [&](){ 
+     auto const ymd = year_month_day{days_since_epoch} + std::chrono::months{months_val};
+     return ymd.ok() ? ymd : ymd.year() / ymd.month() / last;
+     }();
+     
+  auto const time = (time_val - days_since_epoch);
+  
+  return sys_days{date} + time;
 
 }  // namespace detail
 }  // namespace datetime
