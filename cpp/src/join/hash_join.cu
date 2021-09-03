0af148bf9c978ab3cd5649246059353edadf3b78
Yunsong Wang <yunsongw@nvidia.com>
2021-09-03 11:03:42 -0400

    Cleanups: remove unnecessary nullptr check + insert_if instead of insert_if_n

--- cpp/src/join/hash_join.cu
+++ cpp/src/join/hash_join.cu
@@ -41,7 +41,7 @@ class row_contains_null {
 
   __device__ __inline__ bool operator()(const size_type& i) const noexcept
   {
-    return _row_bitmask == nullptr or cudf::bit_is_set(_row_bitmask, i);
+    return cudf::bit_is_set(_row_bitmask, i);
   }
 
  private:
@@ -91,7 +91,7 @@ void build_join_hash_table(cudf::table_view const& build,
     auto const row_bitmask = cudf::detail::bitmask_and(build, stream);
     row_contains_null pred{static_cast<bitmask_type const*>(row_bitmask.data())};
 
-    hash_table.insert_if_n(iter, first, build_table_num_rows, pred, stream.value());
+    hash_table.insert_if(iter, iter + build_table_num_rows, first, pred, stream.value());
   }
 }
 
