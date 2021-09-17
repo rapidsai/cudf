e520eb6ef3dd567528f3e3b563d6bd483a42384c
Marlene <57748216+marlenezw@users.noreply.github.com>
2021-09-17 12:00:06 +0200

    Update python/cudf/cudf/core/index.py
    Co-authored-by: Ram (Ramakrishna Prabhu) <42624703+rgsl888prabhu@users.noreply.github.com>

--- python/cudf/cudf/core/index.py
+++ python/cudf/cudf/core/index.py
@@ -1618,7 +1618,7 @@ class DatetimeIndex(GenericIndex):
     def isocalendar(self):
         """
         Returns a DataFrame with the year, week, and day
-        calculated according tothe ISO 8601 standard.
+        calculated according to the ISO 8601 standard.
         Returns
         -------
         DataFrame
