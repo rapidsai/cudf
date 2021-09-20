9f19ddfb468fd345df0aef560dd19bf195884a27
Karthikeyan <6488848+karthikeyann@users.noreply.github.com>
2021-09-20 10:09:24 +0530

    Apply suggestions from code review (jake)
    Co-authored-by: Jake Hemstad <jhemstad@nvidia.com>

--- cpp/src/aggregation/result_cache.cpp
+++ cpp/src/aggregation/result_cache.cpp
@@ -38,18 +38,18 @@ void result_cache::add_result(column_view const& input,
 
 column_view result_cache::get_result(column_view const& input, aggregation const& agg) const
 {
-  CUDF_EXPECTS(has_result(input, agg), "Result does not exist in cache");
 
   auto result_it = _cache.find({input, agg});
+  CUDF_EXPECTS(result_it != _cache.end(), "Result does not exist in cache");
   return result_it->second.second->view();
 }
 
 std::unique_ptr<column> result_cache::release_result(column_view const& input,
                                                      aggregation const& agg)
 {
-  CUDF_EXPECTS(has_result(input, agg), "Result does not exist in cache");
 
-  auto result_it = _cache.extract({input, agg});
+  auto node = _cache.extract({input, agg});
+  CUDF_EXPECTS(not node.empty(), "Result does not exist in cache");
   return std::move(result_it.mapped().second);
 }
 
