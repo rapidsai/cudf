e219b3576fd8ac13f468330674a70738379839bc
David Wendt <dwendt@nvidia.com>
2021-09-21 15:15:44 -0400

    refactor common filter function

--- cpp/src/copying/segmented_shift.cu
+++ cpp/src/copying/segmented_shift.cu
@@ -34,63 +34,16 @@ namespace detail {
 namespace {
 
 /**
- * @brief Helper function to invoke general `copy_if_else`
+ * @brief Common filter function to convert index values into copy-if-else left/right result.
+ *
+ * The offset position is used to identify which segment to copy from.
  */
-template <typename PairIterator, typename ScalarIterator>
-std::unique_ptr<column> segmented_shift_rep_impl(PairIterator input_pair_iterator,
-                                                 ScalarIterator fill_pair_iterator,
-                                                 bool nullable,
-                                                 size_type offset,
-                                                 device_span<size_type const> segment_offsets,
-                                                 data_type value_type,
-                                                 size_type column_size,
-                                                 rmm::cuda_stream_view stream,
-                                                 rmm::mr::device_memory_resource* mr)
-{
-  if (offset > 0) {
-    auto filter = [segment_offsets, offset] __device__(auto const& i) {
-      auto segment_bound_idx =
-        thrust::upper_bound(thrust::seq, segment_offsets.begin(), segment_offsets.end(), i) - 1;
-      return not(*segment_bound_idx <= i and i < *segment_bound_idx + offset);
-    };
-    return copy_if_else(nullable,
-                        input_pair_iterator,
-                        input_pair_iterator + column_size,
-                        fill_pair_iterator,
-                        filter,
-                        value_type,
-                        stream,
-                        mr);
-  } else {
-    auto filter = [segment_offsets, offset] __device__(auto const& i) {
-      auto segment_bound_idx =
-        thrust::upper_bound(thrust::seq, segment_offsets.begin(), segment_offsets.end(), i);
-      return not(*segment_bound_idx + offset <= i and i < *segment_bound_idx);
-    };
-    return copy_if_else(nullable,
-                        input_pair_iterator,
-                        input_pair_iterator + column_size,
-                        fill_pair_iterator,
-                        filter,
-                        value_type,
-                        stream,
-                        mr);
-  }
-}
+struct segmented_shift_filter {
+  device_span<size_type const> const segment_offsets;
+  size_type const offset;
 
-/**
- * @brief Helper function to invoke string specialization of `copy_if_else`
- */
-template <typename InputIterator, typename ScalarIterator>
-std::unique_ptr<column> segmented_shift_string_impl(InputIterator input_iterator,
-                                                    ScalarIterator fill_iterator,
-                                                    size_type offset,
-                                                    device_span<size_type const> segment_offsets,
-                                                    size_type column_size,
-                                                    rmm::cuda_stream_view stream,
-                                                    rmm::mr::device_memory_resource* mr)
-{
-  auto filter = [segment_offsets, offset] __device__(auto const& i) {
+  __device__ bool operator()(size_type const i) const
+  {
     auto const segment_bound_idx =
       thrust::upper_bound(thrust::seq, segment_offsets.begin(), segment_offsets.end(), i) -
       (offset > 0);
@@ -98,9 +51,7 @@ std::unique_ptr<column> segmented_shift_string_impl(InputIterator input_iterator
     auto const right_idx = *segment_bound_idx + (offset > 0 ? offset : 0);
     return not(left_idx <= i and i < right_idx);
   };
-  return strings::detail::copy_if_else(
-    input_iterator, input_iterator + column_size, fill_iterator, filter, stream, mr);
-}
+};
 
 template <typename T, typename Enable = void>
 struct segmented_shift_functor {
@@ -129,26 +80,24 @@ struct segmented_shift_functor<T, std::enable_if_t<is_rep_layout_compatible<T>()
 
     if (segmented_values.has_nulls()) {
       auto input_pair_iterator = make_pair_iterator<T, true>(*values_device_view) - offset;
-      return segmented_shift_rep_impl(input_pair_iterator,
-                                      fill_pair_iterator,
-                                      nullable,
-                                      offset,
-                                      segment_offsets,
-                                      segmented_values.type(),
-                                      segmented_values.size(),
-                                      stream,
-                                      mr);
+      return copy_if_else(nullable,
+                          input_pair_iterator,
+                          input_pair_iterator + segmented_values.size(),
+                          fill_pair_iterator,
+                          segmented_shift_filter{segment_offsets, offset},
+                          segmented_values.type(),
+                          stream,
+                          mr);
     } else {
       auto input_pair_iterator = make_pair_iterator<T, false>(*values_device_view) - offset;
-      return segmented_shift_rep_impl(input_pair_iterator,
-                                      fill_pair_iterator,
-                                      nullable,
-                                      offset,
-                                      segment_offsets,
-                                      segmented_values.type(),
-                                      segmented_values.size(),
-                                      stream,
-                                      mr);
+      return copy_if_else(nullable,
+                          input_pair_iterator,
+                          input_pair_iterator + segmented_values.size(),
+                          fill_pair_iterator,
+                          segmented_shift_filter{segment_offsets, offset},
+                          segmented_values.type(),
+                          stream,
+                          mr);
     }
   }
 };
@@ -166,17 +115,18 @@ struct segmented_shift_functor<string_view> {
                                      rmm::mr::device_memory_resource* mr)
   {
     auto values_device_view = column_device_view::create(segmented_values, stream);
-    auto input_iterator     = make_optional_iterator<cudf::string_view>(
-      *values_device_view, contains_nulls::DYNAMIC{}, segmented_values.has_nulls());
+    auto input_iterator =
+      make_optional_iterator<cudf::string_view>(
+        *values_device_view, contains_nulls::DYNAMIC{}, segmented_values.has_nulls()) -
+      offset;
     auto fill_iterator =
       make_optional_iterator<cudf::string_view>(fill_value, contains_nulls::YES{});
-    return segmented_shift_string_impl(input_iterator - offset,
-                                       fill_iterator,
-                                       offset,
-                                       segment_offsets,
-                                       segmented_values.size(),
-                                       stream,
-                                       mr);
+    return strings::detail::copy_if_else(input_iterator,
+                                         input_iterator + segmented_values.size(),
+                                         fill_iterator,
+                                         segmented_shift_filter{segment_offsets, offset},
+                                         stream,
+                                         mr);
   }
 };
 
