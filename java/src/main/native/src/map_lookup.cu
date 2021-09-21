a1ea2d3ca17765a5d99e6dd28f29a102421b619f
Mike Wilson <knobby@burntsheep.com>
2021-09-21 02:43:49 +0000

    Fixing empty input crashing

--- java/src/main/native/src/map_lookup.cu
+++ java/src/main/native/src/map_lookup.cu
@@ -112,6 +112,11 @@ std::unique_ptr<column>
 get_gather_map_for_map_values(column_view const &input, string_scalar &lookup_key,
                               rmm::cuda_stream_view stream, rmm::mr::device_memory_resource *mr) {
   constexpr size_type block_size{256};
+
+  if (input.size() == 0) {
+    return make_empty_column(data_type{cudf::type_to_id<size_type>()});
+  }
+
   cudf::detail::grid_1d grid{input.size(), block_size};
 
   auto input_device_view = cudf::column_device_view::create(input, stream);
@@ -191,6 +196,10 @@ std::unique_ptr<column> map_lookup(column_view const &map_column, string_scalar 
   auto gather_map = has_nulls ?
                         get_gather_map_for_map_values<true>(map_column, lookup_key, stream, mr) :
                         get_gather_map_for_map_values<false>(map_column, lookup_key, stream, mr);
+
+  if (gather_map->size() == 0) {
+    return make_empty_column(cudf::data_type{cudf::type_id::STRING});
+  }
 
   // Gather map is now available.
 
