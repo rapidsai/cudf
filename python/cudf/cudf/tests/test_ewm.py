6d431610f77834ba4a36db08056ceabd73da8108
brandon-b-miller <brmiller@nvidia.com>
2021-09-21 09:41:26 -0700

    refactor tests

--- python/cudf/cudf/tests/test_ewm.py
+++ python/cudf/cudf/tests/test_ewm.py
@@ -3,9 +3,37 @@ import pandas as pd
 import cudf
 import numpy as np
 from cudf.testing._utils import assert_eq
+from dataclasses import dataclass
+
+@dataclass
+class EWMParams:        
+    def __init__(self, data, nulls, adjust, bias=None, com=None, span=None, halflife=None, alpha=None):
+        self.nulls = nulls
+        if self.nulls:
+            data[1] = cudf.NA
+            data[3] = cudf.NA
+            data[4] = cudf.NA
+
+        initial_ewm_args = {
+            'adjust': adjust,
+            'com': com,
+            'span': span,
+            'halflife': halflife,
+            'alpha': alpha
+        }
+        self.ewm_args = {k: v for k, v in initial_ewm_args.items() if v is not None}
+        self.call_args = {'bias': bias} if bias is not None else {}
+
+    def __repr__(self):
+        return str({**{"nulls": self.nulls},**self.ewm_args, **self.call_args})
+
+
+
 
 @pytest.mark.parametrize('data', [
-    [1.0, 2.0, 3.0, 4.0, 5.0]
+    [1.0, 2.0, 3.0, 4.0, 5.0],
+    [5.0, cudf.NA, 3.0, cudf.NA, 8.5],
+    [5.0, cudf.NA, 3.0, cudf.NA, cudf.NA, 4.5]
 ])
 @pytest.mark.parametrize('params', [
     {'com': 0.1},
@@ -37,7 +65,8 @@ def test_ewma(data, params, adjust):
 
 @pytest.mark.parametrize('data', [
     [1.0, 2.0, 3.0, 4.0, 5.0],
-    [1.0, 2.0, float('nan'), 4.0, 5.0]
+    [5.0, cudf.NA, 3.0, cudf.NA, 8.5],
+    [5.0, cudf.NA, 3.0, cudf.NA, cudf.NA, 4.5]
 ])
 @pytest.mark.parametrize('params', [
     {'com': 0.1},
@@ -50,7 +79,7 @@ def test_ewma(data, params, adjust):
     {'alpha': 0.5},
 ])
 @pytest.mark.parametrize('adjust', [True, False])
-@pytest.mark.parametrize('bias', [True])
+@pytest.mark.parametrize('bias', [True, False])
 def test_ewmvar(data, params, adjust, bias):
     """
     the most basic test asserts that we obtain
@@ -69,7 +98,9 @@ def test_ewmvar(data, params, adjust, bias):
     assert_eq(expect, got)
 
 @pytest.mark.parametrize('data', [
-    [1.0, 2.0, 3.0, 4.0, 5.0]
+    [1.0, 2.0, 3.0, 4.0, 5.0],
+    [5.0, cudf.NA, 3.0, cudf.NA, 8.5],
+    [5.0, cudf.NA, 3.0, cudf.NA, cudf.NA, 4.5]
 ])
 @pytest.mark.parametrize('params', [
     {'com': 0.1},
@@ -82,7 +113,7 @@ def test_ewmvar(data, params, adjust, bias):
     {'alpha': 0.5},
 ])
 @pytest.mark.parametrize('adjust', [True, False])
-@pytest.mark.parametrize('bias', [True])
+@pytest.mark.parametrize('bias', [True, False])
 def test_ewmstd(data, params, adjust, bias):
     """
     the most basic test asserts that we obtain
@@ -97,36 +128,5 @@ def test_ewmstd(data, params, adjust, bias):
 
     expect = psr.ewm(**params).std(bias=bias)
     got = gsr.ewm(**params).std(bias=bias)
-
-    assert_eq(expect, got)
-
-@pytest.mark.parametrize('data', [
-    [1.0, 2.0, np.nan, 4.0, 5.0]
-])
-@pytest.mark.parametrize('params', [
-    {'com': 0.1},
-    {'com': 0.5},
-    {'span': 1.5},
-    {'span': 2.5},
-    {'halflife': 0.5},
-    {'halflife': 1.5},
-    {'alpha': 0.1},
-    {'alpha': 0.5},
-])
-@pytest.mark.parametrize('adjust', [True, False])
-def test_ewma_nulls(data, params, adjust):
-    """
-    the most basic test asserts that we obtain
-    the same numerical values as pandas for various
-    sets of keyword arguemnts that effect the raw 
-    coefficients of the formula
-    """
-    params["adjust"] = adjust
-
-    gsr = cudf.Series(data, dtype='float64')
-    psr = gsr.to_pandas()
-
-    expect = psr.ewm(**params).mean()
-    got = gsr.ewm(**params).mean()
 
     assert_eq(expect, got)
