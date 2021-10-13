8fbeaeb27d8260adbd54f38a98729fe963701667
Nghia Truong <nghiatruong.vn@gmail.com>
2021-10-12 20:57:24 -0600

    Remove redundant headers

--- cpp/src/sort/segmented_sort.cu
+++ cpp/src/sort/segmented_sort.cu
@@ -14,32 +14,16 @@
  * limitations under the License.
  */
 
-#include <cudf/column/column.hpp>
-#include <cudf/column/column_device_view.cuh>
-#include <cudf/column/column_factories.hpp>
-#include <cudf/detail/copy.hpp>
 #include <cudf/detail/gather.hpp>
-#include <cudf/detail/null_mask.hpp>
 #include <cudf/detail/nvtx/ranges.hpp>
 #include <cudf/detail/sorting.hpp>
-#include <cudf/lists/list_device_view.cuh>
-#include <cudf/lists/lists_column_view.hpp>
-#include <cudf/table/table_device_view.cuh>
 
-#include <rmm/cuda_stream_view.hpp>
-#include <rmm/device_buffer.hpp>
 #include <rmm/device_uvector.hpp>
 #include <rmm/exec_policy.hpp>
 
 #include <thrust/binary_search.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
-#include <thrust/logical.h>
-
-#include <algorithm>
-#include <iterator>
-#include <memory>
-#include <type_traits>
 
 namespace cudf {
 namespace detail {
