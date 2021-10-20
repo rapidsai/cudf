4222229ed7b28a23db4d8e5aa317089d4a2fd6c6
Vyas Ramasubramani <vyasr@nvidia.com>
2021-10-20 15:47:11 -0700

    Script now works for cmake-format and cmake-lint, exits gracefully if the config is not present, and exits with failure if the linters run and fail.

--- cpp/scripts/run-cmake-format.sh
+++ cpp/scripts/run-cmake-format.sh
@@ -1,16 +1,15 @@
-RAPIDS_CMAKE_FORMAT_FILE=cpp/build/release/_deps/rapids-cmake-src/cmake-format-rapids-cmake.json
-remove_file=false
-if ! [ -f ${RAPIDS_CMAKE_FORMAT_FILE} ]; then
-    echo "RAPIDS-CMake config file not found, downloading now."
-    mkdir -p $(dirname ${RAPIDS_CMAKE_FORMAT_FILE})
-    wget -O ${RAPIDS_CMAKE_FORMAT_FILE} https://github.com/rapidsai/rapids-cmake/blob/branch-21.12/cmake-format-rapids-cmake.json
-    remove_file=true
+#!/bin/bash
+
+# This script invokes cmake-format 
+DEFAULT_FORMAT_FILE=cpp/build/release/_deps/rapids-cmake-src/cmake-format-rapids-cmake.json
+RAPIDS_CMAKE_FORMAT_FILE=${RAPIDS_CMAKE_FORMAT_FILE:-${DEFAULT_FORMAT_FILE}}
+
+if [ -f ${RAPIDS_CMAKE_FORMAT_FILE} ]; then
+  if [[ $1 == "cmake-format" ]]; then
+    cmake-format -i --config-files cpp/cmake/config.json ${RAPIDS_CMAKE_FORMAT_FILE} -- ${@:2}
+  elif [[ $1 == "cmake-lint" ]]; then
+    cmake-lint --config-files cpp/cmake/config.json ${RAPIDS_CMAKE_FORMAT_FILE} -- ${@:2}
+  fi
 else
-    echo "Found file."
-fi
-
-pre-commit run cmake-format --hook-stage manual
-
-if [ ${remove_file} = true ]; then
-    rm ${RAPIDS_CMAKE_FORMAT_FILE}
+  echo "The rapids-cmake cmake-format configuration file is not present at ${RAPIDS_CMAKE_FORMAT_FILE}. Try setting the environment variable RAPIDS_CMAKE_FORMAT_FILE to the path to the config file."
 fi
