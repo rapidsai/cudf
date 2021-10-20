a7773b83b9ea8d3e9037ba5b1eedb152d60f51b2
Bradley Dice <bdice@bradleydice.com>
2021-10-19 17:44:39 -0700

    Move result_location to constructor.

--- cpp/src/hash/sha_hash.cu
+++ cpp/src/hash/sha_hash.cu
@@ -37,6 +37,39 @@ namespace detail {
 
 namespace {
 
+static const __device__ __constant__ uint32_t sha256_hash_constants[64] = {
+  0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+  0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+  0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+  0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+  0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+  0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+  0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+  0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
+
+static const __device__ __constant__ uint64_t sha512_hash_constants[80] = {
+  0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
+  0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
+  0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
+  0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
+  0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+  0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
+  0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
+  0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
+  0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
+  0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
+  0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
+  0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
+  0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
+  0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
+  0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+  0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
+  0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
+  0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
+  0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
+  0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817,
+};
+
 // SHA supported leaf data type check
 bool sha_type_check(data_type dt)
 {
@@ -78,7 +111,78 @@ CUDA_DEVICE_CALLABLE uint64_t swap_endian(uint64_t x)
   return (static_cast<uint64_t>(low_bits) << 32) | (static_cast<uint64_t>(high_bits));
 };
 
-}  // namespace
+template <int capacity, typename hash_step_callable>
+struct hash_circular_buffer {
+  uint8_t storage[capacity];
+  uint8_t* cur;
+  hash_step_callable hash_step;
+
+  CUDA_DEVICE_CALLABLE hash_circular_buffer(hash_step_callable hash_step)
+    : cur{storage}, hash_step{hash_step}
+  {
+  }
+
+  CUDA_DEVICE_CALLABLE uint8_t* begin() { return storage; }
+  CUDA_DEVICE_CALLABLE const uint8_t* begin() const { return storage; }
+
+  CUDA_DEVICE_CALLABLE int size() const
+  {
+    return std::distance(begin(), static_cast<const uint8_t*>(cur));
+  }
+
+  CUDA_DEVICE_CALLABLE int available_space() const { return capacity - size(); }
+
+  CUDA_DEVICE_CALLABLE void put(uint8_t const* in, int size)
+  {
+    int space      = available_space();
+    int copy_start = 0;
+    while (size >= space) {
+      // The buffer will be filled by this chunk of data. Copy a chunk of the
+      // data to fill the buffer and trigger a hash step.
+      memcpy(cur, in + copy_start, space);
+      hash_step(storage);
+      size -= space;
+      copy_start += space;
+      cur   = begin();
+      space = available_space();
+    }
+    // The buffer will not be filled by the remaining data. That is, `size >= 0
+    // && size < capacity`. We copy the remaining data into the buffer but do
+    // not trigger a hash step.
+    memcpy(cur, in + copy_start, size);
+    cur += size;
+  }
+
+  CUDA_DEVICE_CALLABLE void pad(int space_to_leave)
+  {
+    int space = available_space();
+    if (space_to_leave > space) {
+      memset(cur, 0x00, space);
+      hash_step(storage);
+      cur   = begin();
+      space = available_space();
+    }
+    memset(cur, 0x00, space - space_to_leave);
+    cur += space - space_to_leave;
+  }
+
+  CUDA_DEVICE_CALLABLE const uint8_t& operator[](int idx) const { return storage[idx]; }
+};
+
+template <typename Key>
+auto CUDA_DEVICE_CALLABLE get_data(Key const& k)
+{
+  if constexpr (is_fixed_width<Key>() && !is_chrono<Key>()) {
+    return thrust::make_pair(reinterpret_cast<uint8_t const*>(&k), sizeof(Key));
+  } else {
+    cudf_assert(false && "Unsupported type.");
+  }
+}
+
+auto CUDA_DEVICE_CALLABLE get_data(string_view const& k)
+{
+  return thrust::make_pair(reinterpret_cast<uint8_t const*>(k.data()), k.size_bytes());
+}
 
 /**
  * @brief A CRTP helper function
@@ -99,6 +203,10 @@ struct crtp {
 
 template <typename Hasher>
 struct HashBase : public crtp<Hasher> {
+  char* result_location;
+
+  CUDA_DEVICE_CALLABLE HashBase(char* result_location) : result_location(result_location) {}
+
   /**
    * @brief Execute SHA on input data chunks.
    *
@@ -151,7 +259,7 @@ struct HashBase : public crtp<Hasher> {
    * the message length (in another step of the hash, if needed), and performs
    * the final hash step.
    */
-  void CUDA_DEVICE_CALLABLE finalize(char* result_location)
+  void CUDA_DEVICE_CALLABLE finalize()
   {
     auto& state = this->underlying().state;
     // Message length in bits.
@@ -315,39 +423,6 @@ struct sha512_hash_state {
                             0x1f83d9abfb41bd6b,
                             0x5be0cd19137e2179};
   uint8_t buffer[128];
-};
-
-__constant__ uint32_t sha256_hash_constants[64] = {
-  0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-  0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-  0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-  0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-  0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-  0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-  0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-  0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
-
-__constant__ uint64_t sha512_hash_constants[80] = {
-  0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
-  0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
-  0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
-  0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
-  0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
-  0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
-  0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
-  0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
-  0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
-  0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
-  0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
-  0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
-  0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
-  0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
-  0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
-  0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
-  0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
-  0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
-  0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
-  0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817,
 };
 
 /**
@@ -550,6 +625,8 @@ void CUDA_DEVICE_CALLABLE sha512_hash_step(hash_state& state)
 }
 
 struct SHA1Hash : HashBase<SHA1Hash> {
+  CUDA_DEVICE_CALLABLE SHA1Hash(char* result_location) : HashBase<SHA1Hash>(result_location) {}
+
   // Intermediate data type storing the hash state
   using hash_state = sha1_hash_state;
   // The word type used by this hash function
@@ -567,6 +644,8 @@ struct SHA1Hash : HashBase<SHA1Hash> {
 };
 
 struct SHA224Hash : HashBase<SHA224Hash> {
+  CUDA_DEVICE_CALLABLE SHA224Hash(char* result_location) : HashBase<SHA224Hash>(result_location) {}
+
   // Intermediate data type storing the hash state
   using hash_state = sha224_hash_state;
   // The word type used by this hash function
@@ -584,6 +663,8 @@ struct SHA224Hash : HashBase<SHA224Hash> {
 };
 
 struct SHA256Hash : HashBase<SHA256Hash> {
+  CUDA_DEVICE_CALLABLE SHA256Hash(char* result_location) : HashBase<SHA256Hash>(result_location) {}
+
   // Intermediate data type storing the hash state
   using hash_state = sha256_hash_state;
   // The word type used by this hash function
@@ -601,6 +682,8 @@ struct SHA256Hash : HashBase<SHA256Hash> {
 };
 
 struct SHA384Hash : HashBase<SHA384Hash> {
+  CUDA_DEVICE_CALLABLE SHA384Hash(char* result_location) : HashBase<SHA384Hash>(result_location) {}
+
   // Intermediate data type storing the hash state
   using hash_state = sha384_hash_state;
   // The word type used by this hash function
@@ -618,6 +701,8 @@ struct SHA384Hash : HashBase<SHA384Hash> {
 };
 
 struct SHA512Hash : HashBase<SHA512Hash> {
+  CUDA_DEVICE_CALLABLE SHA512Hash(char* result_location) : HashBase<SHA512Hash>(result_location) {}
+
   // Intermediate data type storing the hash state
   using hash_state = sha512_hash_state;
   // The word type used by this hash function
@@ -633,6 +718,8 @@ struct SHA512Hash : HashBase<SHA512Hash> {
 
   hash_state state;
 };
+
+}  // namespace
 
 /**
  * @brief Call a SHA-1 or SHA-2 hash function on a table view.
@@ -685,7 +772,8 @@ std::unique_ptr<column> sha_hash(table_view const& input,
                    thrust::make_counting_iterator(0),
                    thrust::make_counting_iterator(input.num_rows()),
                    [d_chars, device_input = *device_input] __device__(auto row_index) {
-                     Hasher hasher = Hasher{};
+                     auto const result_location = d_chars + (row_index * Hasher::digest_size);
+                     Hasher hasher              = Hasher{result_location};
                      for (auto const& col : device_input) {
                        if (col.is_valid(row_index)) {
                          HasherDispatcher<Hasher> hasher_dispatcher{&hasher, col};
@@ -693,8 +781,7 @@ std::unique_ptr<column> sha_hash(table_view const& input,
                            col.type(), hasher_dispatcher, row_index);
                        }
                      }
-                     auto const result_location = d_chars + (row_index * Hasher::digest_size);
-                     hasher.finalize(result_location);
+                     hasher.finalize();
                    });
 
   return make_strings_column(
