f7d1b3e027aefe80825f5662bd26cf3d186d500a
Nghia Truong <nghiatruong.vn@gmail.com>
2021-11-01 11:13:02 -0600

    Implement groupby scan for struct min/max

--- cpp/src/groupby/sort/group_scan_util.cuh
+++ cpp/src/groupby/sort/group_scan_util.cuh
@@ -16,12 +16,18 @@
 
 #pragma once
 
+#include <groupby/sort/group_util.cuh>
+
 #include <cudf/column/column.hpp>
 #include <cudf/column/column_factories.hpp>
 #include <cudf/column/column_view.hpp>
+#include <cudf/copying.hpp>
 #include <cudf/detail/aggregation/aggregation.cuh>
+#include <cudf/detail/gather.hpp>
 #include <cudf/detail/iterator.cuh>
 #include <cudf/detail/null_mask.hpp>
+#include <cudf/detail/structs/utilities.hpp>
+#include <cudf/detail/utilities/vector_factories.hpp>
 #include <cudf/table/table_device_view.cuh>
 #include <cudf/types.hpp>
 #include <cudf/utilities/span.hpp>
@@ -39,7 +45,7 @@ namespace detail {
 template <aggregation::Kind K>
 struct scan_functor {
   template <typename T>
-  static constexpr bool is_supported()
+  static constexpr bool is_trivially_supported()
   {
     if (K == aggregation::SUM)
       return cudf::is_numeric<T>() || cudf::is_duration<T>() || cudf::is_fixed_point<T>();
@@ -50,7 +56,7 @@ struct scan_functor {
   }
 
   template <typename T>
-  std::enable_if_t<is_supported<T>() and not std::is_same_v<T, cudf::string_view>,
+  std::enable_if_t<is_trivially_supported<T>() and not std::is_same_v<T, cudf::string_view>,
                    std::unique_ptr<column>>
   operator()(column_view const& values,
              size_type num_groups,
@@ -78,34 +84,33 @@ struct scan_functor {
     auto result_view = mutable_column_device_view::create(result->mutable_view(), stream);
     auto values_view = column_device_view::create(values, stream);
 
+    // Perform segmented scan.
+    auto const do_scan = [&](auto const& inp_iter, auto const& out_iter, auto const& binop) {
+      thrust::inclusive_scan_by_key(rmm::exec_policy(stream),
+                                    group_labels.begin(),
+                                    group_labels.end(),
+                                    inp_iter,
+                                    out_iter,
+                                    thrust::equal_to<size_type>{},
+                                    binop);
+    };
+
     if (values.has_nulls()) {
       auto input = thrust::make_transform_iterator(
         make_null_replacement_iterator(*values_view, OpType::template identity<DeviceType>()),
         thrust::identity<ResultDeviceType>{});
-      thrust::inclusive_scan_by_key(rmm::exec_policy(stream),
-                                    group_labels.begin(),
-                                    group_labels.end(),
-                                    input,
-                                    result_view->begin<ResultDeviceType>(),
-                                    thrust::equal_to<size_type>{},
-                                    OpType{});
+      do_scan(input, result_view->begin<ResultDeviceType>(), OpType{});
       result->set_null_mask(cudf::detail::copy_bitmask(values, stream));
     } else {
       auto input = thrust::make_transform_iterator(values_view->begin<DeviceType>(),
                                                    thrust::identity<ResultDeviceType>{});
-      thrust::inclusive_scan_by_key(rmm::exec_policy(stream),
-                                    group_labels.begin(),
-                                    group_labels.end(),
-                                    input,
-                                    result_view->begin<ResultDeviceType>(),
-                                    thrust::equal_to<size_type>{},
-                                    OpType{});
+      do_scan(input, result_view->begin<ResultDeviceType>(), OpType{});
     }
     return result;
   }
 
   template <typename T>
-  std::enable_if_t<is_supported<T>() and std::is_same_v<T, cudf::string_view>,
+  std::enable_if_t<is_trivially_supported<T>() and std::is_same_v<T, cudf::string_view>,
                    std::unique_ptr<column>>
   operator()(column_view const& values,
              size_type num_groups,
@@ -115,33 +120,30 @@ struct scan_functor {
   {
     using OpType = cudf::detail::corresponding_operator_t<K>;
 
-    if (values.is_empty()) {
-      return cudf::make_empty_column(cudf::data_type{cudf::type_id::STRING});
-    }
+    if (values.is_empty()) { return cudf::make_empty_column(cudf::type_id::STRING); }
 
     // create an empty output vector we can fill with string_view instances
     auto results_vector = rmm::device_uvector<string_view>(values.size(), stream);
 
     auto values_view = column_device_view::create(values, stream);
 
+    // Perform segmented scan.
+    auto const do_scan = [&](auto const& inp_iter, auto const& out_iter, auto const& binop) {
+      thrust::inclusive_scan_by_key(rmm::exec_policy(stream),
+                                    group_labels.begin(),
+                                    group_labels.end(),
+                                    inp_iter,
+                                    out_iter,
+                                    thrust::equal_to<size_type>{},
+                                    binop);
+    };
+
     if (values.has_nulls()) {
       auto input = make_null_replacement_iterator(
         *values_view, OpType::template identity<string_view>(), values.has_nulls());
-      thrust::inclusive_scan_by_key(rmm::exec_policy(stream),
-                                    group_labels.begin(),
-                                    group_labels.end(),
-                                    input,
-                                    results_vector.begin(),
-                                    thrust::equal_to<size_type>{},
-                                    OpType{});
+      do_scan(input, results_vector.begin(), OpType{});
     } else {
-      thrust::inclusive_scan_by_key(rmm::exec_policy(stream),
-                                    group_labels.begin(),
-                                    group_labels.end(),
-                                    values_view->begin<string_view>(),
-                                    results_vector.begin(),
-                                    thrust::equal_to<size_type>{},
-                                    OpType{});
+      do_scan(values_view->begin<string_view>(), results_vector.begin(), OpType{});
     }
 
     // turn the string_view vector into a strings column
@@ -151,8 +153,91 @@ struct scan_functor {
     return results;
   }
 
+  template <typename T>
+  std::enable_if_t<std::is_same_v<T, cudf::struct_view> and
+                     (K == aggregation::MIN or K == aggregation::MAX),
+                   std::unique_ptr<column>>
+  operator()(column_view const& values,
+             size_type num_groups,
+             cudf::device_span<cudf::size_type const> group_labels,
+             rmm::cuda_stream_view stream,
+             rmm::mr::device_memory_resource* mr)
+  {
+    if (values.is_empty()) { return cudf::empty_like(values); }
+
+    // When finding MIN, we need to consider nulls as larger than non-null elements.
+    // Thing is opposite when finding MAX.
+    auto const null_precedence  = (K == aggregation::MIN) ? null_order::AFTER : null_order::BEFORE;
+    auto const flattened_values = structs::detail::flatten_nested_columns(
+      table_view{{values}}, {}, std::vector<null_order>{null_precedence});
+    auto const d_flattened_values_ptr = table_device_view::create(flattened_values, stream);
+    auto const flattened_null_precedences =
+      (K == aggregation::MIN)
+        ? cudf::detail::make_device_uvector_async(flattened_values.null_orders(), stream)
+        : rmm::device_uvector<null_order>(0, stream);
+
+    // Create a gather map contaning indices of the prefix min/max elements.
+    auto gather_map      = rmm::device_uvector<size_type>(values.size(), stream);
+    auto const map_begin = gather_map.begin();
+
+    // Perform segmented scan.
+    auto const do_scan = [&](auto const& inp_iter, auto const& out_iter, auto const& binop) {
+      thrust::inclusive_scan_by_key(rmm::exec_policy(stream),
+                                    group_labels.begin(),
+                                    group_labels.end(),
+                                    inp_iter,
+                                    out_iter,
+                                    thrust::equal_to<size_type>{},
+                                    binop);
+    };
+
+    // Find the indices of the prefix min/max elements within each group.
+    auto const count_iter = thrust::make_counting_iterator<size_type>(0);
+    if (values.has_nulls()) {
+      auto const op = row_arg_minmax_fn<T, true, K == aggregation::MIN>(
+        values.size(), *d_flattened_values_ptr, flattened_null_precedences.data());
+      do_scan(count_iter, map_begin, op);
+    } else {
+      auto const op = row_arg_minmax_fn<T, false, K == aggregation::MIN>(
+        values.size(), *d_flattened_values_ptr, flattened_null_precedences.data());
+      do_scan(count_iter, map_begin, op);
+    }
+
+    auto gather_map_view =
+      column_view(data_type{type_to_id<offset_type>()}, gather_map.size(), gather_map.data());
+
+    // Gather the children elements of the prefix min/max struct elements first.
+    auto scanned_children =
+      cudf::detail::gather(
+        table_view(std::vector<column_view>{values.child_begin(), values.child_end()}),
+        gather_map_view,
+        cudf::out_of_bounds_policy::DONT_CHECK,
+        cudf::detail::negative_index_policy::NOT_ALLOWED,
+        stream,
+        mr)
+        ->release();
+
+    // After gathering the children elements, we need to push down nulls from the root structs
+    // column to them (so we will have null row in => null row out).
+    if (values.has_nulls()) {
+      for (std::unique_ptr<column>& child : scanned_children) {
+        structs::detail::superimpose_parent_nulls(
+          values.null_mask(), values.null_count(), *child, stream, mr);
+      }
+    }
+
+    return make_structs_column(values.size(),
+                               std::move(scanned_children),
+                               values.null_count(),
+                               cudf::detail::copy_bitmask(values, stream));
+  }
+
   template <typename T, typename... Args>
-  std::enable_if_t<not is_supported<T>(), std::unique_ptr<column>> operator()(Args&&... args)
+  std::enable_if_t<not is_trivially_supported<T>() and
+                     (not std::is_same_v<T, cudf::struct_view> or
+                      (K != aggregation::MIN and K != aggregation::MAX)),
+                   std::unique_ptr<column>>
+  operator()(Args&&... args)
   {
     CUDF_FAIL("Unsupported groupby scan type-agg combination");
   }
