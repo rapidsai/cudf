5e88c6755b06ee85a99a3e82cd7c65bce9060feb
Bradley Dice <bdice@bradleydice.com>
2021-11-01 20:46:01 -0500

    Intermediate progress.

--- python/cudf/cudf/tests/test_doctests.py
+++ python/cudf/cudf/tests/test_doctests.py
@@ -5,14 +5,34 @@ import pytest
 
 import cudf
 
+# These classes and all subclasses will be doctested
+doctested_classes = [
+    "Frame",
+    "BaseIndex",
+]
+
+
+def find_docstrings_in_module(finder, module):
+    print("Finding in module", module.__name__)
+    for docstring in finder.find(module):
+        print("Finding in docstring", docstring.name, docstring.filename)
+        if docstring.examples:
+            yield docstring
+    for name, member in inspect.getmembers(module):
+        if name not in getattr(module, "__all__", []):
+            if inspect.ismodule(member):
+                print("SKIPPING MODULE", module.__name__, name)
+            else:
+                print("Skipping member", module.__name__, name)
+            continue
+        # print("Investigating", name)
+        if inspect.ismodule(member):
+            yield from find_docstrings_in_module(finder, member)
+
 
 def fetch_doctests():
     finder = doctest.DocTestFinder()
-    for name, member in inspect.getmembers(cudf):
-        if inspect.ismodule(member):
-            for docstring in finder.find(member):
-                if docstring.examples:
-                    yield docstring
+    yield from find_docstrings_in_module(finder, cudf)
 
 
 class TestDoctests:
