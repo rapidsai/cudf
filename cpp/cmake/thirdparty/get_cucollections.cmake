21ecdd1efb31a4c8f97fff54cc991f100fadc207
Vyas Ramasubramani <vyasr@nvidia.com>
2021-11-02 10:12:39 -0700

    Apply formatting.

--- cpp/cmake/thirdparty/get_cucollections.cmake
+++ cpp/cmake/thirdparty/get_cucollections.cmake
@@ -10,7 +10,7 @@
 # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
- # =============================================================================
+# =============================================================================
 
 # This function finds cucollections and sets any additional necessary environment variables.
 function(find_and_configure_cucollections)
