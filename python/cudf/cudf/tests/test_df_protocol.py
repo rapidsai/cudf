117e4321bb2b51c8c543ba5241234125997967ac
iskode <ismael.from.kone@gmail.com>
2021-11-02 08:57:06 +0000

    fix flake8 style checks for 'test_df_protocol.py' file

--- python/cudf/cudf/tests/test_df_protocol.py
+++ python/cudf/cudf/tests/test_df_protocol.py
@@ -1,12 +1,9 @@
-import datetime
 import cupy as cp
-import numpy as np
 import pytest
 from cudf.core.df_protocol import (
-    _from_dataframe, 
+    _from_dataframe,
     _DtypeKind,
     protocol_dtype_to_cupy_dtype,
-    _CuDFDataFrame,
     _CuDFColumn,
     _CuDFBuffer
 )
@@ -14,33 +11,24 @@ from cudf.core.df_protocol import (
 import cudf
 from cudf.core.column import build_column
 from cudf.core.buffer import Buffer
-from cudf.testing import _utils as utils
-from cudf.testing._utils import (
-    ALL_TYPES,
-    DATETIME_TYPES,
-    NUMERIC_TYPES,
-    assert_eq,
-    assert_exceptions_equal,
-    does_not_raise,
-    gen_rand,
-)
+from cudf.testing._utils import assert_eq
 import pandas as pd
 from typing import Any, Tuple
 
 DataFrameObject = Any
+
 
 def assert_buffer_equal(buffer_and_dtype: Tuple[_CuDFBuffer, Any], cudfcol):
     buf, dtype = buffer_and_dtype
     device_id = cp.asarray(cudfcol.data).device.id
     assert buf.__dlpack_device__() == (2, device_id)
     col_from_buf = build_column(Buffer(buf.ptr, buf.bufsize),
-                        protocol_dtype_to_cupy_dtype(dtype)
-                        )
+                                protocol_dtype_to_cupy_dtype(dtype))
     # check that non null values are the equals as null are represented
     # by sentinel values in the buffer.
-    non_null_idxs = cudfcol != None
+    non_null_idxs = cudfcol != cudf.NA
     assert_eq(col_from_buf[non_null_idxs], cudfcol[non_null_idxs])
-    
+
     if dtype[0] != _DtypeKind.BOOL:
         array_from_dlpack = cp.fromDlpack(buf.__dlpack__())
         col_array = cp.asarray(cudfcol.data_array_view)
@@ -49,42 +37,37 @@ def assert_buffer_equal(buffer_and_dtype: Tuple[_CuDFBuffer, Any], cudfcol):
         pytest.raises(TypeError, buf.__dlpack__)
 
 
-
-
 def assert_column_equal(col: _CuDFColumn, cudfcol):
-    assert col.size == cudfcol.size 
+    assert col.size == cudfcol.size
     assert col.offset == 0
     assert col.null_count == cudfcol.null_count
     assert col.num_chunks() == 1
     if col.null_count == 0 :
         pytest.raises(RuntimeError, col._get_validity_buffer)
-        assert col.get_buffers()['validity'] == None
+        assert col.get_buffers()['validity'] is None
     else:
         assert_buffer_equal(col.get_buffers()['validity'],
                             cudfcol._get_mask_as_column().astype(cp.uint8))
-    
+
     if col.dtype[0] == _DtypeKind.CATEGORICAL:
         assert_buffer_equal(col.get_buffers()['data'], cudfcol.codes)
-        assert col.get_buffers()['offsets'] == None
+        assert col.get_buffers()['offsets'] is None
 
     elif col.dtype[0] == _DtypeKind.STRING:
         assert_buffer_equal(col.get_buffers()['data'], cudfcol.children[1])
         assert_buffer_equal(col.get_buffers()['offsets'], cudfcol.children[0])
-        
+
     else:
         assert_buffer_equal(col.get_buffers()['data'], cudfcol)
-        assert col.get_buffers()['offsets'] == None
+        assert col.get_buffers()['offsets'] is None
 
-    null_kind, null_value = col.describe_null
     if col.null_count == 0:
-        assert null_kind == 0
-        assert null_value == None
+        assert col.describe_null == (0, None)
     else:
-        assert null_kind == 3
-        assert null_value == 0
+        assert col.describe_null == (3, 0)
 
 
-def assert_dataframe_equal(dfo: DataFrameObject, df:cudf.DataFrame):
+def assert_dataframe_equal(dfo: DataFrameObject, df: cudf.DataFrame):
     assert dfo.num_columns() == len(df.columns)
     assert dfo.num_rows() == len(df)
     assert dfo.num_chunks() == 1
@@ -108,28 +91,34 @@ def _test_from_dataframe_equals(dfobj):
 
 
 def _test_from_dataframe_exception(dfobj):
-    exception_msg = "This operation must copy data from CPU to GPU. Set `allow_copy=True` to allow it."
+    exception_msg = "This operation must copy data from CPU to GPU."
+    " Set `allow_copy=True` to allow it."
     with pytest.raises(TypeError, match=exception_msg):
-        df2 = _from_dataframe(dfobj)
+        _from_dataframe(dfobj)
+
 
 def _test_datatype(data):
     cdf = cudf.DataFrame(data=data)
     _test_from_dataframe_equals(cdf.__dataframe__(allow_copy=False))
     _test_from_dataframe_equals(cdf.__dataframe__(allow_copy=True))
 
+
 def test_from_dataframe():
     data = dict(a=[1, 2, 3], b=[9, 10, 11])
     df1 = cudf.DataFrame(data=data)
     df2 = cudf.from_dataframe(df1)
     assert_eq(df1, df2)
-    
+
+
 def test_int_dtype():
     data_int = dict(a=[1, 2, 3], b=[9, 10, 11])
     _test_datatype(data_int)
 
+
 def test_float_dtype():
     data_float = dict(a=[1.5, 2.5, 3.5], b=[9.2, 10.5, 11.8])
     _test_datatype(data_float)
+
 
 def test_categorical_dtype():
     cdf = cudf.DataFrame({"A": [1, 2, 5, 1]})
@@ -139,6 +128,7 @@ def test_categorical_dtype():
     assert col.describe_categorical == (False, True, {0: 1, 1: 2, 2: 5})
     _test_from_dataframe_equals(cdf.__dataframe__(allow_copy=False))
     _test_from_dataframe_equals(cdf.__dataframe__(allow_copy=True))
+
 
 def test_bool_dtype():
     data_bool = dict(a=[True, True, False], b=[False, True, False])
@@ -152,22 +142,24 @@ def test_string_dtype():
 
 def test_mixed_dtype():
     data_mixed = dict(int=[1, 2, 3], float=[1.5, 2.5, 3.5],
-                        bool=[True, False, True], categorical=[5, 1, 5],
-                        string=["rapidsai-cudf ", "", "df protocol"])
+                      bool=[True, False, True], categorical=[5, 1, 5],
+                      string=["rapidsai-cudf ", "", "df protocol"])
     _test_datatype(data_mixed)
 
 
 def test_NA_int_dtype():
-    data_int = dict(a=[1, None, 3, None, 5], 
+    data_int = dict(a=[1, None, 3, None, 5],
                     b=[9, 10, None, 7, 8],
-                    c= [6, 19, 20, 100, 1000] )
+                    c=[6, 19, 20, 100, 1000])
     _test_datatype(data_int)
 
+
 def test_NA_float_dtype():
-    data_float = dict(a=[1.4, None, 3.6, None, 5.2], 
-                    b=[9.7, 10.9, None, 7.8, 8.2],
-                    c= [6.1, 19.2, 20.3, 100.4, 1000.5] )
+    data_float = dict(a=[1.4, None, 3.6, None, 5.2],
+                      b=[9.7, 10.9, None, 7.8, 8.2],
+                      c=[6.1, 19.2, 20.3, 100.4, 1000.5])
     _test_datatype(data_float)
+
 
 def test_NA_categorical_dtype():
     df = cudf.DataFrame({"A": [1, 2, 5, 1]})
@@ -184,9 +176,11 @@ def test_NA_categorical_dtype():
     _test_from_dataframe_equals(df.__dataframe__(allow_copy=False))
     _test_from_dataframe_equals(df.__dataframe__(allow_copy=True))
 
+
 def test_NA_bool_dtype():
     data_bool = dict(a=[None, True, False], b=[False, None, None])
     _test_datatype(data_bool)
+
 
 def test_NA_string_dtype():
     df = cudf.DataFrame({"A": ["a", "b", "cdef", "", "g"]})
@@ -204,9 +198,9 @@ def test_NA_string_dtype():
 
 
 def test_NA_mixed_dtype():
-    data_mixed = dict(int=[1, None, 2, 3, 1000], float=[None, 1.5, 2.5, 3.5, None],
-                        bool=[True, None, False, None, None], 
-                        categorical=[5, 1, 5, 3, None],
-                        string=[None, None, None, "df protocol", None])
+    data_mixed = dict(int=[1, None, 2, 3, 1000],
+                      float=[None, 1.5, 2.5, 3.5, None],
+                      bool=[True, None, False, None, None],
+                      categorical=[5, 1, 5, 3, None],
+                      string=[None, None, None, "df protocol", None])
     _test_datatype(data_mixed)
-
