1ed143a4155b07f3c28f5e57fd88d9f3699d3861
Bradley Dice <bdice@bradleydice.com>
2021-12-01 15:06:42 -0600

    Make test methods private.

--- python/cudf/cudf/tests/test_doctests.py
+++ python/cudf/cudf/tests/test_doctests.py
@@ -14,7 +14,7 @@ def _is_public_name(parent, name, member):
     return not name.startswith("_")
 
 
-def find_docstrings_in_obj(finder, obj, criteria=None):
+def _find_docstrings_in_obj(finder, obj, criteria=None):
     for docstring in finder.find(obj):
         if docstring.examples:
             yield docstring
@@ -24,25 +24,25 @@ def find_docstrings_in_obj(finder, obj, criteria=None):
             continue
         # Recurse over the public API of modules (objects defined in __all__)
         if inspect.ismodule(member):
-            yield from find_docstrings_in_obj(
+            yield from _find_docstrings_in_obj(
                 finder, member, criteria=_name_in_all
             )
         # Recurse over the public API of classes (attributes not prefixed with
         # an underscore)
         if inspect.isclass(member):
-            yield from find_docstrings_in_obj(
+            yield from _find_docstrings_in_obj(
                 finder, member, criteria=_is_public_name
             )
 
 
-def fetch_doctests():
+def _fetch_doctests():
     finder = doctest.DocTestFinder()
-    yield from find_docstrings_in_obj(finder, cudf, criteria=_name_in_all)
+    yield from _find_docstrings_in_obj(finder, cudf, criteria=_name_in_all)
 
 
 class TestDoctests:
     @pytest.mark.parametrize(
-        "docstring", fetch_doctests(), ids=lambda docstring: docstring.name
+        "docstring", _fetch_doctests(), ids=lambda docstring: docstring.name
     )
     def test_docstring(self, docstring):
         optionflags = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
