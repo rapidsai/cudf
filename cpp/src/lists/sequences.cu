3c529456b19f95f41313d1dc48cd9e6898a25edb
Nghia Truong <nghiatruong.vn@gmail.com>
2021-12-01 14:10:15 -0700

    Add implementation for numeric-not-bool types

---
+++ cpp/src/lists/sequences.cu
@@ -0,0 +1,227 @@
+/*
+ * Copyright (c) 2021, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cudf/column/column_device_view.cuh>
+#include <cudf/column/column_factories.hpp>
+#include <cudf/detail/get_value.cuh>
+#include <cudf/detail/indexalator.cuh>
+#include <cudf/detail/null_mask.hpp>
+#include <cudf/lists/filling.hpp>
+#include <cudf/types.hpp>
+#include <cudf/utilities/error.hpp>
+
+#include <rmm/cuda_stream_view.hpp>
+#include <rmm/exec_policy.hpp>
+
+#include <thrust/binary_search.h>
+#include <thrust/tabulate.h>
+
+#include <optional>
+
+namespace cudf::lists {
+namespace detail {
+namespace {
+template <typename T>
+struct tabulator {
+  column_device_view const starts;
+  column_device_view const steps;
+  offset_type const* const offsets;
+  size_type const* const labels;
+
+  auto __device__ operator()(size_type idx) const
+  {
+    auto const list_idx    = labels[idx] - 1;  // labels are 1-based indices
+    auto const list_offset = offsets[list_idx];
+    return starts.element<T>(list_idx) +
+           static_cast<T>(idx - list_offset) * steps.element<T>(list_idx);
+  }
+};
+
+template <typename T>
+struct tabulator_fixed_step {
+  column_device_view const starts;
+  offset_type const* const offsets;
+  size_type const* const labels;
+
+  auto __device__ operator()(size_type idx) const
+  {
+    auto const list_idx    = labels[idx] - 1;  // labels are 1-based indices
+    auto const list_offset = offsets[list_idx];
+    return starts.element<T>(list_idx) + static_cast<T>(idx - list_offset);
+  }
+};
+
+template <typename T, typename Enable = void>
+struct sequences_functor {
+  template <typename... Args>
+  static std::unique_ptr<column> invoke(Args&&...)
+  {
+    CUDF_FAIL("Unsupported per-list sequence type-agg combination.");
+  }
+};
+
+struct sequences_dispatcher {
+  template <typename T>
+  std::unique_ptr<column> operator()(size_type n_elements,
+                                     column_view const& starts,
+                                     std::optional<column_view> const& steps,
+                                     offset_type const* offsets,
+                                     size_type const* labels,
+                                     rmm::cuda_stream_view stream,
+                                     rmm::mr::device_memory_resource* mr)
+  {
+    return sequences_functor<T>::invoke(n_elements, starts, steps, offsets, labels, stream, mr);
+  }
+};
+
+template <typename T>
+static constexpr bool is_supported()
+{
+  return (cudf::is_numeric<T>() && !cudf::is_boolean<T>());  // || cudf::is_duration<T>();
+}
+
+template <typename T>
+struct sequences_functor<T, std::enable_if_t<is_supported<T>()>> {
+  static std::unique_ptr<column> invoke(size_type n_elements,
+                                        column_view const& starts,
+                                        std::optional<column_view> const& steps,
+                                        offset_type const* offsets,
+                                        size_type const* labels,
+                                        rmm::cuda_stream_view stream,
+                                        rmm::mr::device_memory_resource* mr)
+  {
+    auto result =
+      make_fixed_width_column(starts.type(), n_elements, mask_state::UNALLOCATED, stream, mr);
+    if (starts.is_empty()) { return result; }
+
+    auto const result_begin  = result->mutable_view().template begin<T>();
+    auto const starts_dv_ptr = column_device_view::create(starts, stream);
+    auto const steps_dv_ptr  = steps ? column_device_view::create(steps.value(), stream) : nullptr;
+
+    if (steps) {
+      auto const op = tabulator<T>{*starts_dv_ptr, *steps_dv_ptr, offsets, labels};
+      thrust::tabulate(rmm::exec_policy(stream), result_begin, result_begin + n_elements, op);
+    } else {
+      auto const op = tabulator_fixed_step<T>{*starts_dv_ptr, offsets, labels};
+      thrust::tabulate(rmm::exec_policy(stream), result_begin, result_begin + n_elements, op);
+    }
+
+    return result;
+  }
+};
+
+// template <typename T>
+// struct sequences_functor<T, std::enable_if_t<cudf::is_timestamp<T>()>> {
+//  static std::unique_ptr<column> invoke(size_type n_elements,
+//                                        column_view const& starts,
+//                                        std::optional<column_view> const& steps,
+//                                        offset_type const* offsets,
+//                                        size_type const* labels,
+//                                        rmm::cuda_stream_view stream,
+//                                        rmm::mr::device_memory_resource* mr)
+//  {
+//    return nullptr;
+//  }
+//};
+
+}  // anonymous namespace
+
+std::unique_ptr<column> sequences(column_view const& starts,
+                                  std::optional<column_view> const& steps,
+                                  column_view const& sizes,
+                                  rmm::cuda_stream_view stream,
+                                  rmm::mr::device_memory_resource* mr)
+{
+  CUDF_EXPECTS(cudf::is_index_type(sizes.type()), "Input sizes column must be of integer types.");
+  if (steps) {
+    CUDF_EXPECTS(starts.size() == steps.value().size() && starts.size() == sizes.size(),
+                 "starts, steps, and sizes input columns must have the same number of rows.");
+    CUDF_EXPECTS(starts.type() == steps.value().type(),
+                 "starts and steps input columns must have the same type.");
+  } else {
+    CUDF_EXPECTS(starts.size() == sizes.size(),
+                 "starts and sizes input columns must have the same number of rows.");
+  }
+
+  auto const n_lists = starts.size();
+
+  // Generate list offsets for the output.
+  auto list_offsets = make_numeric_column(
+    data_type(type_to_id<offset_type>()), n_lists + 1, mask_state::UNALLOCATED, stream, mr);
+  auto const offsets_begin = list_offsets->mutable_view().template begin<offset_type>();
+
+  // Normalize the input sizes: convert any integer type into size_type, and clamp negative values
+  // to zero.
+  auto const sizes_input = cudf::detail::indexalator_factory::make_input_iterator(sizes);
+  auto const sizes_norm  = thrust::make_transform_iterator(
+    sizes_input, [] __device__(size_type idx) { return idx < 0 ? 0 : idx; });
+
+  CUDA_TRY(cudaMemsetAsync(offsets_begin, 0, sizeof(offset_type), stream.value()));
+  thrust::inclusive_scan(
+    rmm::exec_policy(stream), sizes_norm, sizes_norm + n_lists, offsets_begin + 1);
+  auto const n_elements = cudf::detail::get_value<size_type>(list_offsets->view(), n_lists, stream);
+
+  // Generate (temporary) list labels (1-based list indices) for all elements.
+  auto labels = rmm::device_uvector<size_type>(n_elements, stream);
+  thrust::upper_bound(rmm::exec_policy(stream),
+                      offsets_begin,
+                      offsets_begin + n_lists,
+                      thrust::make_counting_iterator(0),
+                      thrust::make_counting_iterator(n_elements),
+                      labels.begin());
+
+  auto child = type_dispatcher(starts.type(),
+                               sequences_dispatcher{},
+                               n_elements,
+                               starts,
+                               steps,
+                               offsets_begin,
+                               labels.begin(),
+                               stream,
+                               mr);
+
+  // Any null of the input columns will result in a null in the output lists column.
+  auto [null_mask, null_count] =
+    steps ? cudf::detail::bitmask_and(table_view{{starts, steps.value(), sizes}}, stream, mr)
+          : cudf::detail::bitmask_and(table_view{{starts, sizes}}, stream, mr);
+
+  return make_lists_column(n_lists,
+                           std::move(list_offsets),
+                           std::move(child),
+                           null_count,
+                           std::move(null_mask),
+                           stream,
+                           mr);
+}
+
+}  // namespace detail
+
+std::unique_ptr<column> sequences(column_view const& starts,
+                                  column_view const& sizes,
+                                  rmm::mr::device_memory_resource* mr)
+{
+  return detail::sequences(starts, std::nullopt, sizes, rmm::cuda_stream_default, mr);
+}
+
+std::unique_ptr<column> sequences(column_view const& starts,
+                                  column_view const& steps,
+                                  column_view const& sizes,
+                                  rmm::mr::device_memory_resource* mr)
+{
+  return detail::sequences(starts, steps, sizes, rmm::cuda_stream_default, mr);
+}
+
+}  // namespace cudf::lists
