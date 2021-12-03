80546474333ca2b5a177ca62405e874d2f6a5816
Sheilah <kirui.sheilah@gmail.com>
2021-12-02 22:02:56 -0800

    adding tests, WIP

--- python/cudf/cudf/tests/test_dataframe.py
+++ python/cudf/cudf/tests/test_dataframe.py
@@ -9039,3 +9039,25 @@ def test_pearson_corr_multiindex_dataframe():
     expected = gdf.to_pandas().groupby(level="a").corr("pearson")
 
     assert_eq(expected, actual)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        np.random.normal(-100, 100, (50, 50)),
+        np.random.randint(-50, 50, (25, 30)),
+        np.random.random_sample((4, 4)),
+        np.random.uniform(10.5, 75.5, (10, 6)),
+        np.array([1.123, 2.343, 5.890, 0.0]),
+    ],
+)
+@pytest.mark.parametrize("periods", range(-5, 5))
+@pytest.mark.parametrize("fill_method", ["ffill", "bfill", "pad", "backfill"])
+def test_dataframe_pct_change(data, periods, fill_method):
+    gdf = cudf.DataFrame(data)
+    pdf = gdf.to_pandas()
+
+    actual = gdf.pct_change(periods=periods, fill_method=fill_method)
+    expected = pdf.pct_change(periods=periods, fill_method=fill_method)
+
+    assert_eq(expected, actual)
