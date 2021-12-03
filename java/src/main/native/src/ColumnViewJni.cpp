3903978893a25bfe343d19a8c595ae4fbf7e596b
MithunR <mythrocks@gmail.com>
2021-12-03 13:36:13 -0800

    JNI bindings for lists::index_of(). With tests.

--- java/src/main/native/src/ColumnViewJni.cpp
+++ java/src/main/native/src/ColumnViewJni.cpp
@@ -540,6 +540,44 @@ JNIEXPORT jlong JNICALL Java_ai_rapids_cudf_ColumnView_listContainsColumn(JNIEnv
   CATCH_STD(env, 0);
 }
 
+JNIEXPORT jlong JNICALL Java_ai_rapids_cudf_ColumnView_listIndexOfScalar(JNIEnv *env, jclass,
+                                                                         jlong column_view,
+                                                                         jlong lookup_key,
+                                                                         jboolean is_find_first) {
+  JNI_NULL_CHECK(env, column_view, "column is null", 0);
+  JNI_NULL_CHECK(env, lookup_key, "lookup scalar is null", 0);
+  try {
+    cudf::jni::auto_set_device(env);
+    auto cv = reinterpret_cast<cudf::column_view*>(column_view);
+    auto lcv = cudf::lists_column_view{*cv};
+    auto lookup_key_scalar = reinterpret_cast<cudf::scalar *>(lookup_key);
+    auto find_option = is_find_first ? cudf::lists::duplicate_find_option::FIND_FIRST
+                                     : cudf::lists::duplicate_find_option::FIND_LAST;
+    auto result = cudf::lists::index_of(lcv, *lookup_key_scalar, find_option);
+    return reinterpret_cast<jlong>(result.release());
+  }
+  CATCH_STD(env, 0);
+}
+
+JNIEXPORT jlong JNICALL Java_ai_rapids_cudf_ColumnView_listIndexOfColumn(JNIEnv *env, jclass,
+                                                                         jlong column_view,
+                                                                         jlong lookup_keys,
+                                                                         jboolean is_find_first) {
+  JNI_NULL_CHECK(env, column_view, "column is null", 0);
+  JNI_NULL_CHECK(env, lookup_keys, "lookup key column is null", 0);
+  try {
+    cudf::jni::auto_set_device(env);
+    auto cv = reinterpret_cast<cudf::column_view *>(column_view);
+    auto lcv = cudf::lists_column_view{*cv};
+    auto lookup_key_column = reinterpret_cast<cudf::column_view*>(lookup_keys);
+    auto find_option = is_find_first ? cudf::lists::duplicate_find_option::FIND_FIRST
+                                     : cudf::lists::duplicate_find_option::FIND_LAST;
+    auto result = cudf::lists::index_of(lcv, *lookup_key_column, find_option);
+    return reinterpret_cast<jlong>(result.release());
+  }
+  CATCH_STD(env, 0);
+}
+
 JNIEXPORT jlong JNICALL Java_ai_rapids_cudf_ColumnView_listSortRows(JNIEnv *env, jclass,
                                                                     jlong column_view,
                                                                     jboolean is_descending,
