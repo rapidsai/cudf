d15daade354693d7749e77df66939e229b844803
Mayank Anand <m7anand@uwaterloo.ca>
2021-12-06 22:45:19 +0000

    add string in pytest

--- python/cudf/cudf/tests/test_series.py
+++ python/cudf/cudf/tests/test_series.py
@@ -1348,7 +1348,24 @@ def test_nullable_bool_dtype_series(data, bool_dtype):
     assert_eq(psr, gsr.to_pandas(nullable=True))
 
 
-@pytest.mark.parametrize("data", [([0, 1, 2, 3])])
+@pytest.mark.parametrize(
+    "data",
+    [
+        ([0, 1, 2, 3]),
+        (
+            [
+                "abc",
+                "a",
+                None,
+                "hello world",
+                "foo buzz",
+                "",
+                None,
+                "rapids ai",
+            ]
+        ),
+    ],
+)
 def test_series_transpose(data):
     psr = pd.Series(data=data)
     csr = cudf.Series(data=data)
@@ -1356,7 +1373,8 @@ def test_series_transpose(data):
     cudf_transposed = csr.transpose()
     pd_transposed = psr.transpose()
     cudf_property = csr.T
+    pd_property = psr.T
 
     assert_eq(pd_transposed, cudf_transposed)
-    assert_eq(pd_transposed, cudf_property)
+    assert_eq(pd_property, cudf_property)
     assert_eq(cudf_transposed, csr)
