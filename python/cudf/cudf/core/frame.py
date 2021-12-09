e2c7c327093cb1bf77ac54cbc09e62a29f162cc0
Michael Wang <michaelwang0905@icloud.com>
2021-12-09 14:43:27 -0800

    Remove make index handling in _copy_type_metadata

--- python/cudf/cudf/core/frame.py
+++ python/cudf/cudf/core/frame.py
@@ -16,7 +16,6 @@ from typing import (
     Tuple,
     TypeVar,
     Union,
-    cast,
 )
 
 import cupy
@@ -2563,23 +2562,6 @@ class Frame:
         if include_index:
             if self._index is not None and other._index is not None:
                 self._index._copy_type_metadata(other._index)  # type: ignore
-                # When other._index is a CategoricalIndex, the current index
-                # will be a NumericalIndex with an underlying CategoricalColumn
-                # (the above _copy_type_metadata call will have converted the
-                # column). Calling cudf.Index on that column generates the
-                # appropriate index.
-                if isinstance(
-                    other._index, cudf.core.index.CategoricalIndex
-                ) and not isinstance(
-                    self._index, cudf.core.index.CategoricalIndex
-                ):
-                    self._index = cudf.Index(
-                        cast(
-                            cudf.core.index.NumericIndex, self._index
-                        )._column,
-                        name=self._index.name,
-                    )
-
         return self
 
     def _copy_interval_data(self, other, include_index=True):
