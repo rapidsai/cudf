fc00e28e34d88591a50cb89e3cf6bb1b41135423
David Wendt <dwendt@nvidia.com>
2021-12-17 08:52:11 -0500

    remove unneeded xml copy

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -77,7 +77,6 @@ if [ "$BUILD_LIBCUDF" == '1' ]; then
   echo "Checking for build time log $LIB_BUILD_DIR/ninja_log.html"
   if [[ -f "$LIB_BUILD_DIR/ninja_log.html" ]]; then
       gpuci_logger "Copying build time results"
-      cp "$LIB_BUILD_DIR/ninja_log.xml" "$WORKSPACE/test-results/buildtimes-junit.xml"
       mkdir -p "$WORKSPACE/build-metrics"
       cp "$LIB_BUILD_DIR/ninja_log.html" "$WORKSPACE/build-metrics/BuildMetrics.html"
   fi
