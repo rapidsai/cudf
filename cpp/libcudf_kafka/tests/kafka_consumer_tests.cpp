4087acc881bb93e86aa323e3a28b132c9d831926
Jeremy Dyer <jdye64@gmail.com>
2022-01-05 13:35:12 -0500

    Update source file years and also adjust import for cudf_kafka

--- cpp/libcudf_kafka/tests/kafka_consumer_tests.cpp
+++ cpp/libcudf_kafka/tests/kafka_consumer_tests.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#include "cudf_kafka/kafka_consumer.hpp"
+#include <cudf_kafka/kafka_consumer.hpp>
 #include <gtest/gtest.h>
 #include <map>
 #include <memory>
