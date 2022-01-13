3cdc00dfd53b7ec6ece83657faaa5efebcc1b7a2
galipremsagar <sagarprem75@gmail.com>
2022-01-13 11:42:30 -0800

    fix a concat issue that only shows up in repr

--- python/cudf/cudf/core/dataframe.py
+++ python/cudf/cudf/core/dataframe.py
@@ -1551,7 +1551,15 @@ class DataFrame(IndexedFrame, Serializable, GetAttrGetItemMixin):
         # Reassign precision for any decimal cols
         for name, col in out._data.items():
             if isinstance(col, cudf.core.column.Decimal64Column):
-                col = col._with_type_metadata(tables[0]._data[name].dtype)
+                out._data[name] = col._with_type_metadata(
+                    tables[0]._data[name].dtype
+                )
+            elif isinstance(col, cudf.core.column.StructColumn):
+                out._data[name] = col._with_type_metadata(
+                    tables[0]._data[name].dtype
+                )
+            else:
+                out._data[name] = col
 
         # Reassign index and column names
         if isinstance(objs[0].columns, pd.MultiIndex):
