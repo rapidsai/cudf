a5f881ff4ae4e40efc9ba2f03cdc9e2f9e4a1304
Yunsong Wang <yunsongw@nvidia.com>
2022-01-19 18:29:49 -0500

    Split benchmarks

--- cpp/benchmarks/stream_compaction/drop_duplicates_nvbench.cpp
+++ cpp/benchmarks/stream_compaction/drop_duplicates_nvbench.cpp
@@ -73,16 +73,12 @@ NVBENCH_DECLARE_ENUM_TYPE_STRINGS(
 
 NVBENCH_DECLARE_TYPE_STRINGS(cudf::timestamp_ms, "cudf::timestamp_ms", "cudf::timestamp_ms");
 
-template <typename Type, algorithm Algo, cudf::duplicate_keep_option Keep>
-void nvbench_compaction(
-  nvbench::state& state,
-  nvbench::type_list<Type, nvbench::enum_type<Algo>, nvbench::enum_type<Keep>>)
+template <typename Type, cudf::duplicate_keep_option Keep>
+void nvbench_sort_and_drop_duplicates(nvbench::state& state,
+                                      nvbench::type_list<Type, nvbench::enum_type<Keep>>)
 {
-  if constexpr ((not std::is_same_v<Type, int32_t> and
-                 Keep != cudf::duplicate_keep_option::KEEP_FIRST and
-                 Algo == algorithm::SORT_BASED) or
-                (Algo == algorithm::HASH_BASED and
-                 Keep != cudf::duplicate_keep_option::KEEP_FIRST)) {
+  if constexpr (not std::is_same_v<Type, int32_t> and
+                Keep != cudf::duplicate_keep_option::KEEP_FIRST) {
     state.skip("Skip unwanted benchmarks.");
   }
 
@@ -102,29 +98,46 @@ void nvbench_compaction(
 
   state.exec(nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
     rmm::cuda_stream_view stream_view{launch.get_stream()};
-    auto const result = [&]() {
-      if constexpr (Algo == algorithm::HASH_BASED) {
-        return cudf::detail::unordered_drop_duplicates(
-          input_table, {0}, cudf::null_equality::EQUAL, stream_view);
-      } else {
-        return cudf::detail::sort_and_drop_duplicates(input_table,
-                                                      {0},
-                                                      Keep,
-                                                      cudf::null_equality::EQUAL,
-                                                      cudf::null_order::BEFORE,
-                                                      stream_view);
-      }
-    }();
+    auto result = cudf::detail::sort_and_drop_duplicates(
+      input_table, {0}, Keep, cudf::null_equality::EQUAL, cudf::null_order::BEFORE, stream_view);
+  });
+}
+
+template <typename Type>
+void nvbench_unordered_drop_duplicates(nvbench::state& state, nvbench::type_list<Type>)
+{
+  cudf::rmm_pool_raii pool_raii;
+
+  auto const num_rows = state.get_int64("NumRows");
+
+  cudf::test::UniformRandomGenerator<long> rand_gen(0, 100);
+  auto elements = cudf::detail::make_counting_transform_iterator(
+    0, [&rand_gen](auto row) { return rand_gen.generate(); });
+  auto valids = cudf::detail::make_counting_transform_iterator(
+    0, [](auto i) { return i % 100 == 0 ? false : true; });
+  cudf::test::fixed_width_column_wrapper<Type, long> values(elements, elements + num_rows, valids);
+
+  auto input_column = cudf::column_view(values);
+  auto input_table  = cudf::table_view({input_column, input_column, input_column, input_column});
+
+  state.exec(nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
+    rmm::cuda_stream_view stream_view{launch.get_stream()};
+    auto result = cudf::detail::unordered_drop_duplicates(
+      input_table, {0}, cudf::null_equality::EQUAL, stream_view);
   });
 }
 
 using data_type   = nvbench::type_list<bool, int8_t, int32_t, int64_t, float, cudf::timestamp_ms>;
-using algo        = nvbench::enum_type_list<algorithm::SORT_BASED, algorithm::HASH_BASED>;
 using keep_option = nvbench::enum_type_list<cudf::duplicate_keep_option::KEEP_FIRST,
                                             cudf::duplicate_keep_option::KEEP_LAST,
                                             cudf::duplicate_keep_option::KEEP_NONE>;
 
-NVBENCH_BENCH_TYPES(nvbench_compaction, NVBENCH_TYPE_AXES(data_type, algo, keep_option))
-  .set_name("drop_duplicates")
-  .set_type_axes_names({"Type", "Algorithm", "KeepOption"})
+NVBENCH_BENCH_TYPES(nvbench_sort_and_drop_duplicates, NVBENCH_TYPE_AXES(data_type, keep_option))
+  .set_name("sort_and_drop_duplicates")
+  .set_type_axes_names({"Type", "KeepOption"})
+  .add_int64_axis("NumRows", {10'000, 100'000, 1'000'000, 10'000'000});
+
+NVBENCH_BENCH_TYPES(nvbench_unordered_drop_duplicates, NVBENCH_TYPE_AXES(data_type))
+  .set_name("unordered_drop_duplicates")
+  .set_type_axes_names({"Type"})
   .add_int64_axis("NumRows", {10'000, 100'000, 1'000'000, 10'000'000});
