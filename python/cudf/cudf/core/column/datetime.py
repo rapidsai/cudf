Fix signatures for methods of numerical columns called by datetime or timedelta columns.

python/cudf/cudf/core/column/datetime.py
@@ -350,22 +350,27 @@ class DatetimeColumn(column.ColumnBase):
         """Returns the default NA value for this column"""
         return np.datetime64("nat", self.time_unit)
 
-    def mean(self, skipna=None, dtype=np.float64) -> ScalarLike:
+    def mean(
+        self, skipna=None, min_count: int = 0, dtype=np.float64
+    ) -> ScalarLike:
         return pd.Timestamp(
-            self.as_numerical.mean(skipna=skipna, dtype=dtype),
+            self.as_numerical.mean(
+                skipna=skipna, min_count=min_count, dtype=dtype
+            ),
             unit=self.time_unit,
         )
 
     def std(
         self,
         skipna: bool = None,
-        ddof: int = 1,
+        min_count: int = 0,
         dtype: Dtype = np.float64,
-        *args,
-        **kwargs,
+        ddof: int = 1,
     ) -> pd.Timedelta:
         return pd.Timedelta(
-            self.as_numerical.std(skipna=skipna, ddof=ddof, dtype=dtype)
+            self.as_numerical.std(
+                skipna=skipna, min_count=min_count, dtype=dtype, ddof=ddof
+            )
             * _numpy_to_pandas_conversion[self.time_unit],
         )
 

python/cudf/cudf/core/column/numerical_base.py
@@ -103,14 +103,24 @@ class NumericalBaseColumn(ColumnBase):
             )
         return result
 
-    def mean(self, dtype=np.float64, *args, **kwargs):
-        return self._reduce("mean", dtype=dtype, *args, **kwargs)
+    def mean(self, skipna: bool = None, min_count: int = 0, dtype=np.float64):
+        return self._reduce(
+            "mean", skipna=skipna, min_count=min_count, dtype=dtype
+        )
 
-    def var(self, dtype=np.float64, *args, **kwargs):
-        return self._reduce("var", dtype=dtype, *args, **kwargs)
+    def var(
+        self, skipna: bool = None, min_count: int = 0, dtype=np.float64, ddof=1
+    ):
+        return self._reduce(
+            "var", skipna=skipna, min_count=min_count, dtype=dtype, ddof=ddof
+        )
 
-    def std(self, dtype=np.float64, *args, **kwargs):
-        return self._reduce("std", dtype=dtype, *args, **kwargs)
+    def std(
+        self, skipna: bool = None, min_count: int = 0, dtype=np.float64, ddof=1
+    ):
+        return self._reduce(
+            "std", skipna=skipna, min_count=min_count, dtype=dtype, ddof=ddof
+        )
 
     def median(self, skipna: bool = None) -> NumericalBaseColumn:
         skipna = True if skipna is None else skipna