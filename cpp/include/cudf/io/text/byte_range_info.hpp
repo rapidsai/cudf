cd51c6e16249a5c7be4135d04dcaed8c7b8ccf7d
Christopher Harris <xixonia@gmail.com>
2022-02-08 12:04:34 -0600

    move byte_range_info static function definitions out of header

--- cpp/include/cudf/io/text/byte_range_info.hpp
+++ cpp/include/cudf/io/text/byte_range_info.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, NVIDIA CORPORATION.
+ * Copyright (c) 2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,13 +18,15 @@
 
 #include <cudf/detail/utilities/integer_utils.hpp>
 
-#include <limits>
 #include <vector>
 
 namespace cudf {
 namespace io {
 namespace text {
 
+/**
+ * @brief stores offset and size used to indicate a byte range
+ */
 struct byte_range_info {
  public:
   int64_t offset;
@@ -33,22 +35,18 @@ struct byte_range_info {
   byte_range_info() : offset(0), size(0) {}
   byte_range_info(int64_t offset, int64_t size) : offset(offset), size(size) {}
 
-  static std::vector<byte_range_info> create_consecutive(int64_t total_bytes, int64_t range_count)
-  {
-    auto range_size = util::div_rounding_up_safe(total_bytes, range_count);
-
-    std::vector<byte_range_info> ranges;
-
-    for (int64_t i = 0; i < range_count; i++) {
-      auto offset = i * range_size;
-      auto size   = std::min(range_size, total_bytes - offset);
-      ranges.push_back(byte_range_info{offset, size});
-    }
-
-    return ranges;
-  }
-
-  static byte_range_info whole_source() { return {0, std::numeric_limits<int64_t>::max()}; }
+  static byte_range_info whole_source();
+  /**
+   * @brief Create a collection of consecutive ranges between [0, total_bytes).
+   *
+   * Each range wil be the same size except if `total_bytes` is not evenly divisible by
+   * `range_count`, in which cas the last range size will be the remainder.
+   *
+   * @param total_bytes total number of bytes in all ranges
+   * @param range_count total number of ranges in which to divide bytes
+   * @return
+   */
+  static std::vector<byte_range_info> create_consecutive(int64_t total_bytes, int64_t range_count);
 };
 
 }  // namespace text
