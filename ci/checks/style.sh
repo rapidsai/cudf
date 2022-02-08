521a94156bc8332b3eb2e98ce70dd3a0a1a3b465
galipremsagar <sagarprem75@gmail.com>
2022-02-08 11:35:56 -0800

    modify style.sh

--- ci/checks/style.sh
+++ ci/checks/style.sh
@@ -22,6 +22,19 @@ wget -O ${RAPIDS_CMAKE_FORMAT_FILE} ${FORMAT_FILE_URL}
 pre-commit run --hook-stage manual --all-files
 PRE_COMMIT_RETVAL=$?
 
+# Check for copyright headers in the files modified currently
+COPYRIGHT=`python ci/checks/copyright.py --git-modified-only 2>&1`
+CR_RETVAL=$?
+
+# Output results if failure otherwise show pass
+if [ "$CR_RETVAL" != "0" ]; then
+  echo -e "\n\n>>>> FAILED: copyright check; begin output\n\n"
+  echo -e "$COPYRIGHT"
+  echo -e "\n\n>>>> FAILED: copyright check; end output\n\n"
+else
+  echo -e "\n\n>>>> PASSED: copyright check\n\n"
+fi
+
 # Run clang-format and check for a consistent code format
 CLANG_FORMAT=`python cpp/scripts/run-clang-format.py 2>&1`
 CLANG_FORMAT_RETVAL=$?
@@ -40,7 +53,7 @@ HEADER_META_RETVAL=$?
 echo -e "$HEADER_META"
 
 RETVALS=(
-  $PRE_COMMIT_RETVAL $CLANG_FORMAT_RETVAL
+  $CR_RETVAL $PRE_COMMIT_RETVAL $CLANG_FORMAT_RETVAL
 )
 IFS=$'\n'
 RETVAL=`echo "${RETVALS[*]}" | sort -nr | head -n1`
