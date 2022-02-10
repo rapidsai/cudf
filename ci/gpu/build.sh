8dfe18a80ef51200d7df1fd5030374bfa427c60d
galipremsagar <sagarprem75@gmail.com>
2022-02-10 07:47:04 -0800

    copyright

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2018-2021, NVIDIA CORPORATION.
+# Copyright (c) 2018-2022, NVIDIA CORPORATION.
 ##############################################
 # cuDF GPU build and test script for CI      #
 ##############################################
