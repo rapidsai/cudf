61d786e6325bdda240da47f345d23d3ed7b6560c
Bradley Dice <bdice@bradleydice.com>
2022-02-16 13:39:35 -0600

    Add device_span constructor from data pointer and size.

--- cpp/include/cudf/utilities/span.hpp
+++ cpp/include/cudf/utilities/span.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -195,6 +195,11 @@ struct device_span : public cudf::detail::span_base<T, Extent, device_span<T, Ex
   using base::base;
 
   constexpr device_span() noexcept : base() {}  // required to compile on centos
+
+  constexpr device_span(typename base::pointer data, typename base::size_type size) noexcept
+    : base(data, size)
+  {
+  }
 
   template <
     typename C,
