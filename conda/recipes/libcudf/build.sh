c4c652165f345ff7a9e67fdccb8e4d835d9a89d7
AJ Schmidt <aschmidt@nvidia.com>
2022-02-16 17:03:35 -0500

    mv libcudf_kafka recipe to libcudf recipe

--- conda/recipes/libcudf/build.sh
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2018-2019, NVIDIA CORPORATION.
-
-if [[ -z "$PROJECT_FLASH" || "$PROJECT_FLASH" == "0" ]]; then
-    # This assumes the script is executed from the root of the repo directory
-    ./build.sh -v libcudf --allgpuarch --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
-else
-    ./build.sh -v libcudf tests --allgpuarch --build_metrics --incl_cache_stats --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
-fi
