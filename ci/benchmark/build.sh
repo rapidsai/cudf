4733f07b13b7815c7837ff88c72918ab4a44e0f9
AJ Schmidt <aschmidt@nvidia.com>
2022-02-16 18:49:47 -0500

    Remove extraneous `build.sh` parameter
    The `-l` flag was introduced in #3504, but I don't see it being used anywhere. Seems safe to remove unless anyone else thinks otherwise.

--- ci/benchmark/build.sh
+++ ci/benchmark/build.sh
@@ -98,11 +98,7 @@ conda list --show-channel-urls
 ################################################################################
 
 logger "Build libcudf..."
-if [[ "${BUILD_MODE}" == "pull-request" ]]; then
-    "$WORKSPACE/build.sh" clean libcudf cudf dask_cudf benchmarks tests --ptds
-else
-    "$WORKSPACE/build.sh" clean libcudf cudf dask_cudf benchmarks tests -l --ptds
-fi
+"$WORKSPACE/build.sh" clean libcudf cudf dask_cudf benchmarks tests --ptds
 
 ################################################################################
 # BENCHMARK - Run and parse libcudf and cuDF benchmarks
