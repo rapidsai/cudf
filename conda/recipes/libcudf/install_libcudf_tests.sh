cc7041cb4ba7dd787bbf60c715b5007c1fb29e83
AJ Schmidt <aschmidt@nvidia.com>
2022-02-17 16:35:54 -0500

    add libcudf_tests pkg to recipe

---
+++ conda/recipes/libcudf/install_libcudf_tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Copyright (c) 2018-2022, NVIDIA CORPORATION.
+
+./build.sh -n -v libcudf libcudf_kafka tests benchmarks --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
+cmake --install build --component testing
