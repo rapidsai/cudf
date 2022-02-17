c37a7855803701a593caf7f9d6e1e5e755806a6a
vuule <vmilovanovic@nvidia.com>
2022-02-17 13:54:04 -0800

    python style :\

--- python/cudf/cudf/_lib/text.pyx
+++ python/cudf/cudf/_lib/text.pyx
@@ -36,14 +36,19 @@ def read_text(object filepaths_or_buffers,
     cdef size_t c_byte_range_offset
     cdef size_t c_byte_range_size
     cdef byte_range_info c_byte_range
-    
+
     if (byte_range is not None):
         c_byte_range_offset = byte_range[0]
         c_byte_range_size = byte_range[1]
         with nogil:
             datasource = move(make_source_from_file(filename))
-            c_byte_range = byte_range_info(c_byte_range_offset, c_byte_range_size)
-            c_col = move(multibyte_split(dereference(datasource), delim, c_byte_range))
+            c_byte_range = byte_range_info(
+                c_byte_range_offset,
+                c_byte_range_size
+            )
+            c_col = move(
+                multibyte_split(dereference(datasource), delim, c_byte_range)
+            )
     else:
         with nogil:
             datasource = move(make_source_from_file(filename))
