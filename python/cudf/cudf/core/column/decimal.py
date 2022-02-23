56987a4d5609bb65befdd0ebc084924be1df9ffc
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-02-23 09:37:28 -0600

    Apply suggestions from code review
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- python/cudf/cudf/core/column/decimal.py
+++ python/cudf/cudf/core/column/decimal.py
@@ -364,28 +364,23 @@ def _get_decimal_type(lhs_dtype, rhs_dtype, op):
     else:
         raise NotImplementedError()
 
-    if isinstance(lhs_dtype, type(rhs_dtype)):
-        # SCENARIO 1: If `lhs_dtype` & `rhs_dtype` are same, then try to
-        # see if `precision` & `scale` can be fit into this type.
-        try:
+    try:
+        if isinstance(lhs_dtype, type(rhs_dtype)):
+            # SCENARIO 1: If `lhs_dtype` & `rhs_dtype` are same, then try to
+            # see if `precision` & `scale` can be fit into this type.
             return lhs_dtype.__class__(precision=precision, scale=scale)
-        except ValueError:
-            # Call to _validate fails, which means we need
-            # to goto SCENARIO 3.
-            pass
-    else:
-        # SCENARIO 2: If `lhs_dtype` & `rhs_dtype` are of different dtypes,
-        # then try to see if `precision` & `scale` can be fit into the type
-        # with greater MAX_PRECISION (i.e., the bigger dtype).
-        try:
+        else:
+            # SCENARIO 2: If `lhs_dtype` & `rhs_dtype` are of different dtypes,
+            # then try to see if `precision` & `scale` can be fit into the type
+            # with greater MAX_PRECISION (i.e., the bigger dtype).
             if lhs_dtype.MAX_PRECISION >= rhs_dtype.MAX_PRECISION:
                 return lhs_dtype.__class__(precision=precision, scale=scale)
             else:
                 return rhs_dtype.__class__(precision=precision, scale=scale)
-        except ValueError:
-            # Call to _validate fails, which means we need
-            # to goto SCENARIO 3.
-            pass
+    except ValueError:
+        # Call to _validate fails, which means we need
+        # to goto SCENARIO 3.
+        pass
 
     # SCENARIO 3: If either of the above two scenarios fail, then get the
     # MAX_PRECISION of `lhs_dtype` & `rhs_dtype` so that we can only check
@@ -403,6 +398,6 @@ def _get_decimal_type(lhs_dtype, rhs_dtype, op):
             except ValueError:
                 # Call to _validate fails, which means we need
                 # to try the next dtype
-                pass
+                continue
 
     raise OverflowError("Maximum supported decimal type is Decimal128")
