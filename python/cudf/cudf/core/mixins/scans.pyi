ee7ccf314d4a75edb3b5f49a416f9862ad11ab9e
Vyas Ramasubramani <vyasr@nvidia.com>
2022-02-24 09:22:34 -0800

    Add type annotations file.

---
+++ python/cudf/cudf/core/mixins/scans.pyi
@@ -0,0 +1,14 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.
+
+class Scannable:
+    def cumsum(self):
+        ...
+
+    def cumprod(self):
+        ...
+
+    def cummin(self):
+        ...
+
+    def cummax(self):
+        ...
