eaae94b533a3cf0830efda7f528c94d439e4c037
Karthikeyan <6488848+karthikeyann@users.noreply.github.com>
2022-02-25 07:48:46 +0530

    Add device create_sequence_table for benchmarks (#10300)
    addresses parts of https://github.com/rapidsai/cudf/issues/5773
    - Add `create_sequence_table` which creates sequences in device (only numeric types supported) with/without nulls.
    - Add `create_random_null_mask` to create random null mask with given probability. (0.0-1.0 null probability)
    ~- add gnu++17 to generate_input.cu (temporarily for  int128 STL support).~
    - renamed `repeat_dtypes` to `cycle_dtypes` and moved out of create_* methods
    - updated ast bench, search, scatter , binary ops bench
    
    
    Splitting PR https://github.com/rapidsai/cudf/pull/10109 for review
    
    Authors:
      - Karthikeyan (https://github.com/karthikeyann)
    
    Approvers:
      - Conor Hoekstra (https://github.com/codereport)
      - Nghia Truong (https://github.com/ttnghia)
      - Robert Maynard (https://github.com/robertmaynard)
      - MithunR (https://github.com/mythrocks)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/cudf/pull/10300

--- cpp/benchmarks/io/parquet/parquet_reader.cpp
+++ cpp/benchmarks/io/parquet/parquet_reader.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,8 +45,8 @@ void BM_parq_read_varying_input(benchmark::State& state)
   data_profile table_data_profile;
   table_data_profile.set_cardinality(cardinality);
   table_data_profile.set_avg_run_length(run_length);
-  auto const tbl =
-    create_random_table(data_types, num_cols, table_size_bytes{data_size}, table_data_profile);
+  auto const tbl = create_random_table(
+    cycle_dtypes(data_types, num_cols), table_size_bytes{data_size}, table_data_profile);
   auto const view = tbl->view();
 
   cuio_source_sink_pair source_sink(source_type);
@@ -96,7 +96,7 @@ void BM_parq_read_varying_options(benchmark::State& state)
                        static_cast<int32_t>(type_group_id::TIMESTAMP),
                        static_cast<int32_t>(cudf::type_id::STRING)}),
     col_sel);
-  auto const tbl  = create_random_table(data_types, data_types.size(), table_size_bytes{data_size});
+  auto const tbl  = create_random_table(data_types, table_size_bytes{data_size});
   auto const view = tbl->view();
 
   cuio_source_sink_pair source_sink(io_type::HOST_BUFFER);
