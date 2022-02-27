0ae60f99ca61c0909cec1789bba5d0c3b67cff67
AJ Schmidt <aschmidt@nvidia.com>
2022-02-27 14:33:33 -0500

    add cudf_ROOT var for libcudf_kafka build

--- conda/recipes/libcudf/build.sh
+++ conda/recipes/libcudf/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Copyright (c) 2018-2022, NVIDIA CORPORATION.
 
-./build.sh -n -v libcudf libcudf_kafka tests benchmarks --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
+export cudf_ROOT="$(realpath ./cpp/build)"
+./build.sh -n -v libcudf libcudf_kafka benchmarks tests --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib\"
