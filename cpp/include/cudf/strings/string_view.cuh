797433d1f5450f5397ecc319caacf3f0fc6fcc22
David Wendt <dwendt@nvidia.com>
2022-02-28 15:25:11 -0500

    Move standalone string functions from string_view.hpp to string.hpp

--- cpp/include/cudf/strings/string_view.cuh
+++ cpp/include/cudf/strings/string_view.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2021, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 #pragma once
 
+#include <cudf/strings/string.hpp>
 #include <cudf/strings/string_view.hpp>
 
 #ifndef __CUDA_ARCH__
