3535ead7d78a3096d59bc3a98b280e0e47ed0d50
Yunsong Wang <yunsongw@nvidia.com>
2022-03-08 16:33:53 -0500

    Fix a bug: do not set logical type for int96 timestamps

--- cpp/src/io/parquet/writer_impl.cu
+++ cpp/src/io/parquet/writer_impl.cu
@@ -392,13 +392,16 @@ struct leaf_schema_fn {
   template <typename T>
   std::enable_if_t<std::is_same_v<T, cudf::timestamp_ns>, void> operator()()
   {
-    col_schema.type                         = (timestamp_is_int96) ? Type::INT96 : Type::INT64;
-    col_schema.converted_type               = ConvertedType::UNKNOWN;
-    col_schema.stats_dtype                  = statistics_dtype::dtype_timestamp64;
-    col_schema.logical_type.isset.TIMESTAMP = true;
-    col_schema.logical_type.TIMESTAMP.unit.isset.NANOS = true;
+    col_schema.type           = (timestamp_is_int96) ? Type::INT96 : Type::INT64;
+    col_schema.converted_type = ConvertedType::UNKNOWN;
+    col_schema.stats_dtype    = statistics_dtype::dtype_timestamp64;
     if (timestamp_is_int96) {
       col_schema.ts_scale = -1000;  // negative value indicates division by absolute value
+    }
+    // set logical type if it's not int96
+    else {
+      col_schema.logical_type.isset.TIMESTAMP            = true;
+      col_schema.logical_type.TIMESTAMP.unit.isset.NANOS = true;
     }
   }
 
