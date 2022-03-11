b1ea304ec7d421c871061fdfab7f2e097fbd4a0f
nvdbaranec <56695930+nvdbaranec@users.noreply.github.com>
2022-03-11 16:54:33 -0600

    Add scan_aggregation and reduce_aggregation derived types. (#10357)
    This PR adds the `scan_aggregation` and `reduce_aggregation` derived types. With it, all concrete aggregation types are now derived from algorithmic specific subtypes.
    
    Authors:
      - https://github.com/nvdbaranec
    
    Approvers:
      - Robert (Bobby) Evans (https://github.com/revans2)
      - Ram (Ramakrishna Prabhu) (https://github.com/rgsl888prabhu)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/cudf/pull/10357

--- cpp/src/reductions/reductions.cpp
+++ cpp/src/reductions/reductions.cpp
@@ -45,7 +45,7 @@ struct reduce_dispatch_functor {
   }
 
   template <aggregation::Kind k>
-  std::unique_ptr<scalar> operator()(std::unique_ptr<aggregation> const& agg)
+  std::unique_ptr<scalar> operator()(std::unique_ptr<reduce_aggregation> const& agg)
   {
     switch (k) {
       case aggregation::SUM: return reduction::sum(col, output_dtype, stream, mr); break;
@@ -125,7 +125,7 @@ struct reduce_dispatch_functor {
 
 std::unique_ptr<scalar> reduce(
   column_view const& col,
-  std::unique_ptr<aggregation> const& agg,
+  std::unique_ptr<reduce_aggregation> const& agg,
   data_type output_dtype,
   rmm::cuda_stream_view stream        = rmm::cuda_stream_default,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource())
@@ -145,7 +145,7 @@ std::unique_ptr<scalar> reduce(
 }  // namespace detail
 
 std::unique_ptr<scalar> reduce(column_view const& col,
-                               std::unique_ptr<aggregation> const& agg,
+                               std::unique_ptr<reduce_aggregation> const& agg,
                                data_type output_dtype,
                                rmm::mr::device_memory_resource* mr)
 {
