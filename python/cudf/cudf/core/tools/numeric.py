08c64a554665e5485fb8af933de6b3ffc28da25b
galipremsagar <sagarprem75@gmail.com>
2022-03-14 13:57:27 -0700

    fix bool dtype issue

--- python/cudf/cudf/core/tools/numeric.py
+++ python/cudf/cudf/core/tools/numeric.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2020, NVIDIA CORPORATION.
+# Copyright (c) 2018-2022, NVIDIA CORPORATION.
 
 import warnings
 
@@ -19,6 +19,17 @@ from cudf.api.types import (
 )
 from cudf.core.column import as_column
 from cudf.utils.dtypes import can_convert_to_column
+
+DOWNCAST_TYPE_MAP = {
+    "integer": list(np.typecodes["Integer"]),
+    "signed": list(np.typecodes["Integer"]),
+    "unsigned": list(np.typecodes["UnsignedInteger"]),
+}
+float_types = list(np.typecodes["Float"])
+# we only support float32 & float64
+min_idx = float_types.index(cudf.dtype(np.float32).char)
+max_idx = float_types.index(cudf.dtype(np.float64).char) + 1
+DOWNCAST_TYPE_MAP["float"] = float_types[min_idx:max_idx]
 
 
 def to_numeric(arg, errors="raise", downcast=None):
@@ -144,16 +155,7 @@ def to_numeric(arg, errors="raise", downcast=None):
         col = col.as_numerical_column("d")
 
     if downcast:
-        downcast_type_map = {
-            "integer": list(np.typecodes["Integer"]),
-            "signed": list(np.typecodes["Integer"]),
-            "unsigned": list(np.typecodes["UnsignedInteger"]),
-        }
-        float_types = list(np.typecodes["Float"])
-        idx = float_types.index(cudf.dtype(np.float32).char)
-        downcast_type_map["float"] = float_types[idx:]
-
-        type_set = downcast_type_map[downcast]
+        type_set = DOWNCAST_TYPE_MAP[downcast]
 
         for t in type_set:
             downcast_dtype = cudf.dtype(t)
