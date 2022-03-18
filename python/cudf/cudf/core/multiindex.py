677a7a26c96d955be9e968d703933a303a4ec7b8
Michael Wang <michaelwang0905@icloud.com>
2022-03-17 17:24:51 -0700

    Use the same static method calling convention

--- python/cudf/cudf/core/multiindex.py
+++ python/cudf/cudf/core/multiindex.py
@@ -1833,5 +1833,5 @@ class MultiIndex(Frame, BaseIndex, NotIterable):
 
     def repeat(self, repeats, axis=None):
         return self._from_columns_like_self(
-            self._repeat([*self._columns], repeats, axis), self._column_names
+            Frame._repeat([*self._columns], repeats, axis), self._column_names
         )
