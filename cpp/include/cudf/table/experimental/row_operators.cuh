rapidsai/cudf

commit 40baeb425df98f9d30a651e483b904525f32366f

Support nanosecond timestamps in parquet (#10063)
Closes https://github.com/rapidsai/cudf/issues/9393

This PR was intended to support nanoseconds for both duration and timestamp types in parquet. It introduces `LogicalType`-handling on both reader and writer sides. This PR also includes code cleanups like moving `CompactProtocolReader` to its own file. Finally, nanosecond durations remain unchanged since it's not fully supported by `pyarrow` i.e. nanosecond durations are truncated to microseconds (see [here](https://github.com/apache/arrow/blob/release-7.0.0/cpp/src/arrow/python/datetime.cc#L259-L264)).

Authors:
  - Yunsong Wang (https://github.com/PointKernel)

Approvers:
  - Robert Maynard (https://github.com/robertmaynard)
  - Vukasin Milovanovic (https://github.com/vuule)
  - Devavret Makkar (https://github.com/devavret)
  - Karthikeyan (https://github.com/karthikeyann)

URL: https://github.com/rapidsai/cudf/pull/10063

cpp/src/io/parquet/compact_protocol_reader.hpp
@@ -1,0 +1,510 @@
+/*
+ * Copyright (c) 2018-2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "parquet.hpp"
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+namespace cudf {
+namespace io {
+namespace parquet {
+/**
+ * @brief Class for parsing Parquet's Thrift Compact Protocol encoded metadata
+ *
+ * This class takes in the starting location of the Parquet metadata, and fills
+ * out Thrift-derived structs and a schema tree.
+ *
+ * In a Parquet, the metadata is separated from the data, both conceptually and
+ * physically. There may be multiple data files sharing a common metadata file.
+ *
+ * The parser handles both V1 and V2 Parquet datasets, although not all
+ * compression codecs are supported yet.
+ */
+class CompactProtocolReader {
+ protected:
+  static const uint8_t g_list2struct[16];
+
+ public:
+  explicit CompactProtocolReader(const uint8_t* base = nullptr, size_t len = 0) { init(base, len); }
+  void init(const uint8_t* base, size_t len)
+  {
+    m_base = m_cur = base;
+    m_end          = base + len;
+  }
+  [[nodiscard]] ptrdiff_t bytecount() const noexcept { return m_cur - m_base; }
+  unsigned int getb() noexcept { return (m_cur < m_end) ? *m_cur++ : 0; }
+  void skip_bytes(size_t bytecnt) noexcept
+  {
+    bytecnt = std::min(bytecnt, (size_t)(m_end - m_cur));
+    m_cur += bytecnt;
+  }
+  uint32_t get_u32() noexcept
+  {
+    uint32_t v = 0;
+    for (uint32_t l = 0;; l += 7) {
+      uint32_t c = getb();
+      v |= (c & 0x7f) << l;
+      if (c < 0x80) break;
+    }
+    return v;
+  }
+  uint64_t get_u64() noexcept
+  {
+    uint64_t v = 0;
+    for (uint64_t l = 0;; l += 7) {
+      uint64_t c = getb();
+      v |= (c & 0x7f) << l;
+      if (c < 0x80) break;
+    }
+    return v;
+  }
+  int32_t get_i16() noexcept { return get_i32(); }
+  int32_t get_i32() noexcept
+  {
+    uint32_t u = get_u32();
+    return (int32_t)((u >> 1u) ^ -(int32_t)(u & 1));
+  }
+  int64_t get_i64() noexcept
+  {
+    uint64_t u = get_u64();
+    return (int64_t)((u >> 1u) ^ -(int64_t)(u & 1));
+  }
+  int32_t get_listh(uint8_t* el_type) noexcept
+  {
+    uint32_t c = getb();
+    int32_t sz = c >> 4;
+    *el_type   = c & 0xf;
+    if (sz == 0xf) sz = get_u32();
+    return sz;
+  }
+  bool skip_struct_field(int t, int depth = 0);
+
+ public:
+  // Generate Thrift structure parsing routines
+  bool read(FileMetaData* f);
+  bool read(SchemaElement* s);
+  bool read(LogicalType* l);
+  bool read(DecimalType* d);
+  bool read(TimeType* t);
+  bool read(TimeUnit* u);
+  bool read(TimestampType* t);
+  bool read(IntType* t);
+  bool read(RowGroup* r);
+  bool read(ColumnChunk* c);
+  bool read(ColumnChunkMetaData* c);
+  bool read(PageHeader* p);
+  bool read(DataPageHeader* d);
+  bool read(DictionaryPageHeader* d);
+  bool read(KeyValue* k);
+
+ public:
+  static int NumRequiredBits(uint32_t max_level) noexcept
+  {
+    return 32 - CountLeadingZeros32(max_level);
+  }
+  bool InitSchema(FileMetaData* md);
+
+ protected:
+  int WalkSchema(FileMetaData* md,
+                 int idx           = 0,
+                 int parent_idx    = 0,
+                 int max_def_level = 0,
+                 int max_rep_level = 0);
+
+ protected:
+  const uint8_t* m_base = nullptr;
+  const uint8_t* m_cur  = nullptr;
+  const uint8_t* m_end  = nullptr;
+
+  friend class ParquetFieldBool;
+  friend class ParquetFieldInt8;
+  friend class ParquetFieldInt32;
+  friend class ParquetFieldInt64;
+  template <typename T>
+  friend class ParquetFieldStructListFunctor;
+  friend class ParquetFieldString;
+  template <typename T>
+  friend class ParquetFieldStructFunctor;
+  template <typename T, bool>
+  friend class ParquetFieldUnionFunctor;
+  template <typename T>
+  friend class ParquetFieldEnum;
+  template <typename T>
+  friend class ParquetFieldEnumListFunctor;
+  friend class ParquetFieldStringList;
+  friend class ParquetFieldStructBlob;
+};
+
+/**
+ * @brief Functor to set value to bool read from CompactProtocolReader
+ *
+ * @return True if field type is not bool
+ */
+class ParquetFieldBool {
+  int field_val;
+  bool& val;
+
+ public:
+  ParquetFieldBool(int f, bool& v) : field_val(f), val(v) {}
+
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    return (field_type != ST_FLD_TRUE && field_type != ST_FLD_FALSE) ||
+           !(val = (field_type == ST_FLD_TRUE), true);
+  }
+
+  int field() { return field_val; }
+};
+
+/**
+ * @brief Functor to set value to 8 bit integer read from CompactProtocolReader
+ *
+ * @return True if field type is not int8
+ */
+class ParquetFieldInt8 {
+  int field_val;
+  int8_t& val;
+
+ public:
+  ParquetFieldInt8(int f, int8_t& v) : field_val(f), val(v) {}
+
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    val = cpr->getb();
+    return (field_type != ST_FLD_BYTE);
+  }
+
+  int field() { return field_val; }
+};
+
+/**
+ * @brief Functor to set value to 32 bit integer read from CompactProtocolReader
+ *
+ * @return True if field type is not int32
+ */
+class ParquetFieldInt32 {
+  int field_val;
+  int32_t& val;
+
+ public:
+  ParquetFieldInt32(int f, int32_t& v) : field_val(f), val(v) {}
+
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    val = cpr->get_i32();
+    return (field_type != ST_FLD_I32);
+  }
+
+  int field() { return field_val; }
+};
+
+/**
+ * @brief Functor to set value to 64 bit integer read from CompactProtocolReader
+ *
+ * @return True if field type is not int32 or int64
+ */
+class ParquetFieldInt64 {
+  int field_val;
+  int64_t& val;
+
+ public:
+  ParquetFieldInt64(int f, int64_t& v) : field_val(f), val(v) {}
+
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    val = cpr->get_i64();
+    return (field_type < ST_FLD_I16 || field_type > ST_FLD_I64);
+  }
+
+  int field() { return field_val; }
+};
+
+/**
+ * @brief Functor to read a vector of structures from CompactProtocolReader
+ *
+ * @return True if field types mismatch or if the process of reading a
+ * struct fails
+ */
+template <typename T>
+class ParquetFieldStructListFunctor {
+  int field_val;
+  std::vector<T>& val;
+
+ public:
+  ParquetFieldStructListFunctor(int f, std::vector<T>& v) : field_val(f), val(v) {}
+
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    if (field_type != ST_FLD_LIST) return true;
+
+    int current_byte = cpr->getb();
+    if ((current_byte & 0xf) != ST_FLD_STRUCT) return true;
+    int n = current_byte >> 4;
+    if (n == 0xf) n = cpr->get_u32();
+    val.resize(n);
+    for (int32_t i = 0; i < n; i++) {
+      if (!(cpr->read(&val[i]))) { return true; }
+    }
+
+    return false;
+  }
+
+  int field() { return field_val; }
+};
+
+template <typename T>
+ParquetFieldStructListFunctor<T> ParquetFieldStructList(int f, std::vector<T>& v)
+{
+  return ParquetFieldStructListFunctor<T>(f, v);
+}
+
+/**
+ * @brief Functor to read a string from CompactProtocolReader
+ *
+ * @return True if field type mismatches or if size of string exceeds bounds
+ * of the CompactProtocolReader
+ */
+class ParquetFieldString {
+  int field_val;
+  std::string& val;
+
+ public:
+  ParquetFieldString(int f, std::string& v) : field_val(f), val(v) {}
+
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    if (field_type != ST_FLD_BINARY) return true;
+    uint32_t n = cpr->get_u32();
+    if (n < (size_t)(cpr->m_end - cpr->m_cur)) {
+      val.assign((const char*)cpr->m_cur, n);
+      cpr->m_cur += n;
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  int field() { return field_val; }
+};
+
+/**
+ * @brief Functor to read a structure from CompactProtocolReader
+ *
+ * @return True if field types mismatch or if the process of reading a
+ * struct fails
+ */
+template <typename T>
+class ParquetFieldStructFunctor {
+  int field_val;
+  T& val;
+
+ public:
+  ParquetFieldStructFunctor(int f, T& v) : field_val(f), val(v) {}
+
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    return (field_type != ST_FLD_STRUCT || !(cpr->read(&val)));
+  }
+
+  int field() { return field_val; }
+};
+
+template <typename T>
+ParquetFieldStructFunctor<T> ParquetFieldStruct(int f, T& v)
+{
+  return ParquetFieldStructFunctor<T>(f, v);
+}
+
+/**
+ * @brief Functor to read a union member from CompactProtocolReader
+ *
+ * @tparam is_empty True if tparam `T` type is empty type, else false.
+ *
+ * @return True if field types mismatch or if the process of reading a
+ * union member fails
+ */
+template <typename T, bool is_empty = false>
+class ParquetFieldUnionFunctor {
+  int field_val;
+  bool& is_set;
+  T& val;
+
+ public:
+  ParquetFieldUnionFunctor(int f, bool& b, T& v) : field_val(f), is_set(b), val(v) {}
+
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    if (field_type != ST_FLD_STRUCT) {
+      return true;
+    } else {
+      is_set = true;
+      return !cpr->read(&val);
+    }
+  }
+
+  int field() { return field_val; }
+};
+
+template <typename T>
+struct ParquetFieldUnionFunctor<T, true> {
+  int field_val;
+  bool& is_set;
+  T& val;
+
+ public:
+  ParquetFieldUnionFunctor(int f, bool& b, T& v) : field_val(f), is_set(b), val(v) {}
+
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    if (field_type != ST_FLD_STRUCT) {
+      return true;
+    } else {
+      is_set = true;
+      cpr->skip_struct_field(field_type);
+      return false;
+    }
+  }
+
+  int field() { return field_val; }
+};
+
+template <typename T>
+ParquetFieldUnionFunctor<T, std::is_empty_v<T>> ParquetFieldUnion(int f, bool& b, T& v)
+{
+  return ParquetFieldUnionFunctor<T, std::is_empty_v<T>>(f, b, v);
+}
+
+/**
+ * @brief Functor to set value to enum read from CompactProtocolReader
+ *
+ * @return True if field type is not int32
+ */
+template <typename Enum>
+class ParquetFieldEnum {
+  int field_val;
+  Enum& val;
+
+ public:
+  ParquetFieldEnum(int f, Enum& v) : field_val(f), val(v) {}
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    val = static_cast<Enum>(cpr->get_i32());
+    return (field_type != ST_FLD_I32);
+  }
+
+  int field() { return field_val; }
+};
+
+/**
+ * @brief Functor to read a vector of enums from CompactProtocolReader
+ *
+ * @return True if field types mismatch or if the process of reading an
+ * enum fails
+ */
+template <typename Enum>
+class ParquetFieldEnumListFunctor {
+  int field_val;
+  std::vector<Enum>& val;
+
+ public:
+  ParquetFieldEnumListFunctor(int f, std::vector<Enum>& v) : field_val(f), val(v) {}
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    if (field_type != ST_FLD_LIST) return true;
+    int current_byte = cpr->getb();
+    if ((current_byte & 0xf) != ST_FLD_I32) return true;
+    int n = current_byte >> 4;
+    if (n == 0xf) n = cpr->get_u32();
+    val.resize(n);
+    for (int32_t i = 0; i < n; i++) {
+      val[i] = static_cast<Enum>(cpr->get_i32());
+    }
+    return false;
+  }
+
+  int field() { return field_val; }
+};
+
+template <typename T>
+ParquetFieldEnumListFunctor<T> ParquetFieldEnumList(int field, std::vector<T>& v)
+{
+  return ParquetFieldEnumListFunctor<T>(field, v);
+}
+
+/**
+ * @brief Functor to read a vector of strings from CompactProtocolReader
+ *
+ * @return True if field types mismatch or if the process of reading a
+ * string fails
+ */
+class ParquetFieldStringList {
+  int field_val;
+  std::vector<std::string>& val;
+
+ public:
+  ParquetFieldStringList(int f, std::vector<std::string>& v) : field_val(f), val(v) {}
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    if (field_type != ST_FLD_LIST) return true;
+    int current_byte = cpr->getb();
+    if ((current_byte & 0xf) != ST_FLD_BINARY) return true;
+    int n = current_byte >> 4;
+    if (n == 0xf) n = cpr->get_u32();
+    val.resize(n);
+    for (int32_t i = 0; i < n; i++) {
+      uint32_t l = cpr->get_u32();
+      if (l < (size_t)(cpr->m_end - cpr->m_cur)) {
+        val[i].assign((const char*)cpr->m_cur, l);
+        cpr->m_cur += l;
+      } else
+        return true;
+    }
+    return false;
+  }
+
+  int field() { return field_val; }
+};
+
+/**
+ * @brief Functor to read a struct from CompactProtocolReader
+ *
+ * @return True if field type mismatches
+ */
+class ParquetFieldStructBlob {
+  int field_val;
+  std::vector<uint8_t>& val;
+
+ public:
+  ParquetFieldStructBlob(int f, std::vector<uint8_t>& v) : field_val(f), val(v) {}
+  inline bool operator()(CompactProtocolReader* cpr, int field_type)
+  {
+    if (field_type != ST_FLD_STRUCT) return true;
+    const uint8_t* start = cpr->m_cur;
+    cpr->skip_struct_field(field_type);
+    if (cpr->m_cur > start) { val.assign(start, cpr->m_cur - 1); }
+    return false;
+  }
+
+  int field() { return field_val; }
+};
+
+}  // namespace parquet
+}  // namespace io
+}  // namespace cudf

cpp/CMakeLists.txt
@@ -300,12 +300,12 @@ add_library(
   src/io/orc/stripe_init.cu
   src/io/orc/timezone.cpp
   src/io/orc/writer_impl.cu
+  src/io/parquet/compact_protocol_reader.cpp
   src/io/parquet/compact_protocol_writer.cpp
   src/io/parquet/page_data.cu
   src/io/parquet/chunk_dict.cu
   src/io/parquet/page_enc.cu
   src/io/parquet/page_hdr.cu
-  src/io/parquet/parquet.cpp
   src/io/parquet/reader_impl.cu
   src/io/parquet/writer_impl.cu
   src/io/statistics/orc_column_statistics.cu

cpp/src/io/parquet/compact_protocol_reader.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, NVIDIA CORPORATION.
+ * Copyright (c) 2018-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,11 @@
  * limitations under the License.
  */
 
-#include "parquet.hpp"
+#include "compact_protocol_reader.hpp"
+
 #include <algorithm>
+#include <cstddef>
+#include <tuple>
 
 namespace cudf {
 namespace io {
@@ -198,7 +201,8 @@ bool CompactProtocolReader::read(TimestampType* t)
 bool CompactProtocolReader::read(TimeUnit* u)
 {
   auto op = std::make_tuple(ParquetFieldUnion(1, u->isset.MILLIS, u->MILLIS),
-                            ParquetFieldUnion(2, u->isset.MICROS, u->MICROS));
+                            ParquetFieldUnion(2, u->isset.MICROS, u->MICROS),
+                            ParquetFieldUnion(3, u->isset.NANOS, u->NANOS));
   return function_builder(this, op);
 }
 

cpp/src/io/parquet/compact_protocol_writer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, NVIDIA CORPORATION.
+ * Copyright (c) 2018-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,6 +48,84 @@ size_t CompactProtocolWriter::write(const FileMetaData& f)
   return c.value();
 }
 
+size_t CompactProtocolWriter::write(const DecimalType& decimal)
+{
+  CompactProtocolFieldWriter c(*this);
+  c.field_int(1, decimal.scale);
+  c.field_int(2, decimal.precision);
+  return c.value();
+}
+
+size_t CompactProtocolWriter::write(const TimeUnit& time_unit)
+{
+  CompactProtocolFieldWriter c(*this);
+  auto const isset = time_unit.isset;
+  if (isset.MILLIS) {
+    c.field_struct(1, time_unit.MILLIS);
+  } else if (isset.MICROS) {
+    c.field_struct(2, time_unit.MICROS);
+  } else if (isset.NANOS) {
+    c.field_struct(3, time_unit.NANOS);
+  }
+  return c.value();
+}
+
+size_t CompactProtocolWriter::write(const TimeType& time)
+{
+  CompactProtocolFieldWriter c(*this);
+  c.field_bool(1, time.isAdjustedToUTC);
+  c.field_struct(2, time.unit);
+  return c.value();
+}
+
+size_t CompactProtocolWriter::write(const TimestampType& timestamp)
+{
+  CompactProtocolFieldWriter c(*this);
+  c.field_bool(1, timestamp.isAdjustedToUTC);
+  c.field_struct(2, timestamp.unit);
+  return c.value();
+}
+
+size_t CompactProtocolWriter::write(const IntType& integer)
+{
+  CompactProtocolFieldWriter c(*this);
+  c.field_int8(1, integer.bitWidth);
+  c.field_bool(2, integer.isSigned);
+  return c.value();
+}
+
+size_t CompactProtocolWriter::write(const LogicalType& logical_type)
+{
+  CompactProtocolFieldWriter c(*this);
+  auto const isset = logical_type.isset;
+  if (isset.STRING) {
+    c.field_struct(1, logical_type.STRING);
+  } else if (isset.MAP) {
+    c.field_struct(2, logical_type.MAP);
+  } else if (isset.LIST) {
+    c.field_struct(3, logical_type.LIST);
+  } else if (isset.ENUM) {
+    c.field_struct(4, logical_type.ENUM);
+  } else if (isset.DECIMAL) {
+    c.field_struct(5, logical_type.DECIMAL);
+  } else if (isset.DATE) {
+    c.field_struct(6, logical_type.DATE);
+  } else if (isset.TIME) {
+    c.field_struct(7, logical_type.TIME);
+  } else if (isset.TIMESTAMP) {
+    c.field_struct(8, logical_type.TIMESTAMP);
+  } else if (isset.INTEGER) {
+    c.field_struct(10, logical_type.INTEGER);
+  } else if (isset.UNKNOWN) {
+    c.field_struct(11, logical_type.UNKNOWN);
+  } else if (isset.JSON) {
+    c.field_struct(12, logical_type.JSON);
+  } else if (isset.BSON) {
+    c.field_struct(13, logical_type.BSON);
+  }
+  return c.value();
+}
+
 size_t CompactProtocolWriter::write(const SchemaElement& s)
 {
   CompactProtocolFieldWriter c(*this);
@@ -66,6 +144,12 @@ size_t CompactProtocolWriter::write(const SchemaElement& s)
       c.field_int(8, s.decimal_precision);
     }
   }
+  auto const isset = s.logical_type.isset;
+  // TODO: add handling for all logical types
+  // if (isset.STRING or isset.MAP or isset.LIST or isset.ENUM or isset.DECIMAL or isset.DATE or
+  //    isset.TIME or isset.TIMESTAMP or isset.INTEGER or isset.UNKNOWN or isset.JSON or isset.BSON)
+  //    {
+  if (isset.TIMESTAMP) { c.field_struct(10, s.logical_type); }
   return c.value();
 }
 
@@ -156,6 +240,19 @@ void CompactProtocolFieldWriter::put_field_header(int f, int cur, int t)
   }
 }
 
+inline void CompactProtocolFieldWriter::field_bool(int field, bool b)
+{
+  put_field_header(field, current_field_value, b ? ST_FLD_TRUE : ST_FLD_FALSE);
+  current_field_value = field;
+}
+
+inline void CompactProtocolFieldWriter::field_int8(int field, int8_t val)
+{
+  put_field_header(field, current_field_value, ST_FLD_BYTE);
+  put_byte(val);
+  current_field_value = field;
+}
+
 inline void CompactProtocolFieldWriter::field_int(int field, int32_t val)
 {
   put_field_header(field, current_field_value, ST_FLD_I32);
@@ -186,7 +283,11 @@ template <typename T>
 inline void CompactProtocolFieldWriter::field_struct(int field, const T& val)
 {
   put_field_header(field, current_field_value, ST_FLD_STRUCT);
-  writer.write(val);
+  if constexpr (not std::is_empty_v<T>) {
+    writer.write(val);  // write the struct if it's not empty
+  } else {
+    put_byte(0);  // otherwise, add a stop field
+  }
   current_field_value = field;
 }
 

cpp/src/io/parquet/compact_protocol_writer.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, NVIDIA CORPORATION.
+ * Copyright (c) 2018-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,12 @@ class CompactProtocolWriter {
   CompactProtocolWriter(std::vector<uint8_t>* output) : m_buf(*output) {}
 
   size_t write(const FileMetaData&);
+  size_t write(const DecimalType&);
+  size_t write(const TimeUnit&);
+  size_t write(const TimeType&);
+  size_t write(const TimestampType&);
+  size_t write(const IntType&);
+  size_t write(const LogicalType&);
   size_t write(const SchemaElement&);
   size_t write(const RowGroup&);
   size_t write(const KeyValue&);
@@ -70,6 +76,10 @@ class CompactProtocolFieldWriter {
   uint32_t put_int(int64_t v);
 
   void put_field_header(int f, int cur, int t);
+
+  inline void field_bool(int field, bool b);
+
+  inline void field_int8(int field, int8_t val);
 
   inline void field_int(int field, int32_t val);
 

cpp/src/io/parquet/page_data.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2021, NVIDIA CORPORATION.
+ * Copyright (c) 2018-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -924,17 +924,18 @@ static __device__ bool setupLocalPageInfo(page_state_s* const s,
         case INT64:
           if (s->col.ts_clock_rate) {
             int32_t units = 0;
-            if (s->col.converted_type == TIME_MICROS || s->col.converted_type == TIMESTAMP_MICROS) {
-              units = cudf::timestamp_us::period::den;
-            }
-
-            else if (s->col.converted_type == TIME_MILLIS ||
-                     s->col.converted_type == TIMESTAMP_MILLIS) {
+            if (s->col.converted_type == TIME_MILLIS or s->col.converted_type == TIMESTAMP_MILLIS) {
               units = cudf::timestamp_ms::period::den;
+            } else if (s->col.converted_type == TIME_MICROS or
+                       s->col.converted_type == TIMESTAMP_MICROS) {
+              units = cudf::timestamp_us::period::den;
+            } else if (s->col.logical_type.TIMESTAMP.unit.isset.NANOS) {
+              units = cudf::timestamp_ns::period::den;
             }
-            if (units && units != s->col.ts_clock_rate)
+            if (units and units != s->col.ts_clock_rate) {
               s->ts_scale = (s->col.ts_clock_rate < units) ? -(units / s->col.ts_clock_rate)
                                                            : (s->col.ts_clock_rate / units);
+            }
           }
           // Fall through to DOUBLE
         case DOUBLE: s->dtype_len = 8; break;

commit 0a2aa98adfc21bc9c5767149445195a3ce52ac74

Include <cstddef> to fix compilation of parquet reader on GCC 11. (#10483)
Includes `<cstddef>` for `ptrdiff_t` in `parquet/compact_protocol_reader.hpp`. Compilation fails on GCC 11 without this include. Targeting 22.04 since this was broken yesterday in #10063.

Error output:
```
cudf/cpp/src/io/parquet/compact_protocol_reader.hpp:51:17: error: 'ptrdiff_t' does not name a type
   51 |   [[nodiscard]] ptrdiff_t bytecount() const noexcept { return m_cur - m_base; }
      |
cudf/cpp/src/io/parquet/compact_protocol_reader.hpp:22:1: note: 'ptrdiff_t' is defined in header '<cstddef>'; did you forget to '#include <cstddef>'?
```

Also includes `<optional>` in `cpp/include/cudf/table/experimental/row_operators.cuh`, which was broken by #10164.

Authors:
  - Bradley Dice (https://github.com/bdice)

Approvers:
  - Conor Hoekstra (https://github.com/codereport)
  - Yunsong Wang (https://github.com/PointKernel)

URL: https://github.com/rapidsai/cudf/pull/10483

cpp/include/cudf/table/experimental/row_operators.cuh
@@ -34,6 +34,7 @@
 
 #include <limits>
 #include <memory>
+#include <optional>
 #include <utility>
 
 namespace cudf {

cpp/src/io/parquet/compact_protocol_reader.hpp
@@ -19,6 +19,7 @@
 #include "parquet.hpp"
 
 #include <algorithm>
+#include <cstddef>
 #include <string>
 #include <vector>
 