15c85284f632e33b4d1ed1e48ef5d5c79d1cf4d1
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-03-30 14:13:13 -0500

    Update python/cudf/cudf/core/series.py
    Co-authored-by: Vyas Ramasubramani <vyas.ramasubramani@gmail.com>

--- python/cudf/cudf/core/series.py
+++ python/cudf/cudf/core/series.py
@@ -857,7 +857,7 @@ class Series(SingleColumnFrame, IndexedFrame, Serializable):
     @_cudf_nvtx_annotate
     def memory_usage(self, index=True, deep=False):
         return self._column.memory_usage + (
-            self.index.memory_usage() if index else 0
+            self._index.memory_usage() if index else 0
         )
 
     @_cudf_nvtx_annotate
