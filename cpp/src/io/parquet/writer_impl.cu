4477cc6cb81abf8e87f9cd2c79955ea754327d0a
Yunsong Wang <yunsongw@nvidia.com>
2022-03-31 11:21:51 -0400

    Fix a bug: add field id when it's set

--- cpp/src/io/parquet/writer_impl.cu
+++ cpp/src/io/parquet/writer_impl.cu
@@ -608,8 +608,10 @@ std::vector<schema_tree_node> construct_schema_tree(LinkedColVector const& linke
         map_schema.converted_type = ConvertedType::MAP;
         map_schema.repetition_type =
           col_nullable ? FieldRepetitionType::OPTIONAL : FieldRepetitionType::REQUIRED;
-        map_schema.name         = col_meta.get_name();
-        map_schema.field_id     = col_meta.get_parquet_field_id();
+        map_schema.name = col_meta.get_name();
+        if (col_meta.is_parquet_field_id_set()) {
+          map_schema.field_id = col_meta.get_parquet_field_id();
+        }
         map_schema.num_children = 1;
         map_schema.parent_idx   = parent_idx;
         schema.push_back(std::move(map_schema));
