d18238feccfc09d98eaa52ce739893b7673e6ed8
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2022-04-04 02:28:30 -0700

    renaming key-value store op to stack_op

--- cpp/src/io/fst/logical_stack.cuh
+++ cpp/src/io/fst/logical_stack.cuh
@@ -441,9 +441,9 @@ void sparse_stack_op_to_top_of_stack(StackSymbolItT d_symbols,
   // Dump info on stack operations: (stack level change + symbol) -> (absolute stack level + symbol)
   test::print::print_array(num_symbols_in,
                            stream,
-                           get_key_it(stack_symbols_in),
+                           get_stack_level_it(stack_symbols_in),
                            get_value_it(stack_symbols_in),
-                           get_key_it(d_kv_operations.Current()),
+                           get_stack_level_it(d_kv_operations.Current()),
                            get_value_it(d_kv_operations.Current()));
 
   // Stable radix sort, sorting by stack level of the operations
