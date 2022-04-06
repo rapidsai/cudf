8309aa9a9aabb3f4969b5679d1358a005eaed4f6
David Wendt <dwendt@nvidia.com>
2022-04-06 18:01:30 -0400

    use more predicatble method for targeting hit-rate

--- cpp/benchmarks/string/contains.cpp
+++ cpp/benchmarks/string/contains.cpp
@@ -21,12 +21,54 @@
 
 #include <cudf_test/column_wrapper.hpp>
 
+#include <cudf/filling.hpp>
 #include <cudf/strings/contains.hpp>
 #include <cudf/strings/findall.hpp>
 #include <cudf/strings/strings_column_view.hpp>
 
 class StringContains : public cudf::benchmark {
 };
+
+std::unique_ptr<cudf::column> build_input_column(cudf::size_type n_rows, int32_t hit_rate)
+{
+  // build input table using the following data
+  auto data      = cudf::test::strings_column_wrapper({
+    "123 abc 4567890 DEFGHI 0987 5W43",  // matches both patterns;
+    "012345 6789 01234 56789 0123 456",  // the rest do not match
+    "abc 4567890 DEFGHI 0987 Wxyz 123",
+    "abcdefghijklmnopqrstuvwxyz 01234",
+    "",
+    "AbcéDEFGHIJKLMNOPQRSTUVWXYZ 01",
+    "9876543210,abcdefghijklmnopqrstU",
+    "9876543210,abcdefghijklmnopqrstU",
+    "123 édf 4567890 DéFG 0987 X5",
+    "1",
+  });
+  auto data_view = cudf::column_view(data);
+
+  // compute number of rows in n_rows that should match
+  auto matches = static_cast<int32_t>(n_rows * hit_rate) / 100;
+
+  // Create a randomized gather-map to build a column out of the strings in data.
+  data_profile gather_profile;
+  gather_profile.set_distribution_params(
+    cudf::type_id::INT32, distribution_id::UNIFORM, 1, data_view.size() - 1);
+  gather_profile.set_null_frequency(0.0);  // no nulls for gather-map
+  gather_profile.set_cardinality(0);
+  auto gather_table =
+    create_random_table({cudf::type_id::INT32}, row_count{n_rows}, gather_profile);
+  gather_table->get_column(0).set_null_mask(rmm::device_buffer{}, 0);
+
+  // Create scatter map by placing 0-index values throughout the gather-map
+  auto scatter_data = cudf::sequence(
+    matches, cudf::numeric_scalar<int32_t>(0), cudf::numeric_scalar<int32_t>(n_rows / matches));
+  auto zero_scalar = cudf::numeric_scalar<int32_t>(0);
+  auto table       = cudf::scatter({zero_scalar}, scatter_data->view(), gather_table->view());
+  auto gather_map  = table->view().column(0);
+  table            = cudf::gather(cudf::table_view({data_view}), gather_map);
+
+  return std::move(table->release().front());
+}
 
 enum contains_type { contains, count, findall };
 
@@ -39,39 +81,8 @@ static void BM_contains(benchmark::State& state, contains_type ct)
   auto const pattern_index = static_cast<int32_t>(state.range(1));
   auto const hit_rate      = static_cast<int32_t>(state.range(2));
 
-  // build 100 rows using the following data
-  std::vector<std::string> rawdata({
-    "123 abc 4567890 DEFGHI 0987 5W43",  // matches both patterns;
-    "012345 6789 01234 56789 0123 456",  // the rest do not match
-    "abc 4567890 DEFGHI 0987 Wxyz 123",
-    "abcdefghijklmnopqrstuvwxyz 01234",
-    "",
-    "AbcéDEFGHIJKLMNOPQRSTUVWXYZ 01",
-    "9876543210,abcdefghijklmnopqrstU",
-    "9876543210,abcdefghijklmnopqrstU",
-    "123 édf 4567890 DéFG 0987 X5",
-    "1",
-  });
-  std::vector<std::string> h_data;
-  for (std::size_t i = 0; i < rawdata.size() * rawdata.size(); ++i) {
-    h_data.push_back(rawdata[i / rawdata.size()]);
-  }
-  auto data      = cudf::test::strings_column_wrapper(h_data.begin(), h_data.end());
-  auto data_view = cudf::column_view(data);
-
-  // Create a randomized gather-map to build a column out of the strings in data.
-  // For hit-rate ~100%, matches is 1 -- only gathers the matching row(s).
-  // For hit-rate ~10%, matches is 10 -- use all rows from data (only 10% will match).
-  auto matches = static_cast<int32_t>(data_view.size() * rawdata.size() / hit_rate);
-  data_profile table_profile;
-  table_profile.set_distribution_params(
-    cudf::type_id::INT32, distribution_id::UNIFORM, 0, matches - 1);
-  table_profile.set_null_frequency(0.0);  // no nulls for gather-map
-
-  auto table      = create_random_table({cudf::type_id::INT32}, row_count{n_rows}, table_profile);
-  auto gather_map = cudf::column_view(table->view().column(0));
-  table           = cudf::gather(cudf::table_view({data_view}), gather_map);
-  auto input      = cudf::strings_column_view(table->view().column(0));
+  auto col   = build_input_column(n_rows, hit_rate);
+  auto input = cudf::strings_column_view(col->view());
 
   auto pattern = patterns[pattern_index];
 
@@ -93,14 +104,14 @@ static void BM_contains(benchmark::State& state, contains_type ct)
   state.SetBytesProcessed(state.iterations() * input.chars_size());
 }
 
-#define STRINGS_BENCHMARK_DEFINE(name, b)                                                   \
-  BENCHMARK_DEFINE_F(StringContains, name)                                                  \
-  (::benchmark::State & st) { BM_contains(st, contains_type::b); }                          \
-  BENCHMARK_REGISTER_F(StringContains, name)                                                \
-    ->ArgsProduct({{4096, 32768, 262144, 2097152, 16777216}, /* row count */                \
-                   {0, 1},                                   /* patterns index */           \
-                   {10, 18, 46, 64, 98}})                    /* 11%, 25%, 50%, 70%, 100% */ \
-    ->UseManualTime()                                                                       \
+#define STRINGS_BENCHMARK_DEFINE(name, b)                                         \
+  BENCHMARK_DEFINE_F(StringContains, name)                                        \
+  (::benchmark::State & st) { BM_contains(st, contains_type::b); }                \
+  BENCHMARK_REGISTER_F(StringContains, name)                                      \
+    ->ArgsProduct({{4096, 32768, 262144, 2097152, 16777216}, /* row count */      \
+                   {0, 1},                                   /* patterns index */ \
+                   {1, 5, 10, 25, 70, 100}})                 /* hit rate */       \
+    ->UseManualTime()                                                             \
     ->Unit(benchmark::kMillisecond);
 
 STRINGS_BENCHMARK_DEFINE(contains_re, contains)
