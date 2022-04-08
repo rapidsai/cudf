0ebcd075811ce1ef8d248ed7f8bfc1e24bfbc532
brandon-b-miller <brmiller@nvidia.com>
2022-04-08 12:11:46 -0700

    remove stale breakpoint

--- python/cudf/cudf/core/udf/utils.py
+++ python/cudf/cudf/core/udf/utils.py
@@ -254,7 +254,6 @@ def _return_col_from_dtype(dt, size):
 
 def _post_process_output_col(col, retty):
     if retty == string_view:
-        breakpoint()
         return from_stringview_array(col)
     else:
         return as_column(col)
