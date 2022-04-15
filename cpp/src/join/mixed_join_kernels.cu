ac68b6195a88df4b2c24048460a4f0d06893a23a
davidwendt <dwendt@nvidia.com>
2022-04-15 18:44:29 -0400

    retest split-up

--- cpp/src/join/mixed_join_kernels.cu
+++ cpp/src/join/mixed_join_kernels.cu
@@ -19,20 +19,6 @@
 namespace cudf {
 namespace detail {
 
-template __global__ void mixed_join<DEFAULT_JOIN_BLOCK_SIZE, true>(
-  table_device_view left_table,
-  table_device_view right_table,
-  table_device_view probe,
-  table_device_view build,
-  row_equality const equality_probe,
-  join_kind const join_type,
-  cudf::detail::mixed_multimap_type::device_view hash_table_view,
-  size_type* join_output_l,
-  size_type* join_output_r,
-  cudf::ast::detail::expression_device_view device_expression_data,
-  cudf::size_type const* join_result_offsets,
-  bool const swap_tables);
-
 template __global__ void mixed_join<DEFAULT_JOIN_BLOCK_SIZE, false>(
   table_device_view left_table,
   table_device_view right_table,
