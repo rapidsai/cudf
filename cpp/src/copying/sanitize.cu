1e8604d92b05c9e844092ee0a858a9413fa265f7
MithunR <mythrocks@gmail.com>
2022-04-20 22:41:20 -0700

    Improved readability of needs_sanitize().

--- cpp/src/copying/sanitize.cu
+++ cpp/src/copying/sanitize.cu
@@ -30,42 +30,59 @@ using cudf::type_id;
 
 namespace {
 
+/// Check if sanitize checks can be skipped for a given type.
 bool cannot_need_sanitize(cudf::type_id const& type)
 {
   return type != type_id::STRING && type != type_id::LIST && type != type_id::STRUCT;
 }
 
+/// Check if the (STRING/LIST) column has any null rows with non-zero length.
+bool has_dirty_rows(cudf::column_view const& input, rmm::cuda_stream_view stream)
+{
+  if (not input.nullable()) { return false; }  // No nulls => no dirty rows.
+
+  // Cross-reference nullmask and offsets.
+  auto const type         = input.type().id();
+  auto const offsets      = (type == type_id::STRING) ? (strings_column_view{input}).offsets()
+                                                      : (lists_column_view{input}).offsets();
+  auto const d_input      = cudf::column_device_view::create(input);
+  auto const is_dirty_row = [d_input = *d_input, offsets = offsets.begin<size_type>()] __device__(
+                              size_type const& row_idx) {
+    return d_input.is_null_nocheck(row_idx) && (offsets[row_idx] != offsets[row_idx + 1]);
+  };
+
+  auto const row_begin = thrust::make_counting_iterator<cudf::size_type>(0);
+  auto const row_end   = row_begin + input.size();
+  return thrust::any_of(rmm::exec_policy(stream), row_begin, row_end, is_dirty_row);
+}
+
+bool has_dirty_children(cudf::column_view const& input, rmm::cuda_stream_view stream)
+{
+  return std::any_of(input.child_begin(), input.child_end(), [stream](auto const& child) {
+    return cudf::detail::needs_sanitize(child, stream);
+  });
+}
+
 }  // namespace
 
+/**
+ * @copydoc cudf::detail::needs_sanitize
+ */
 bool needs_sanitize(cudf::column_view const& input, rmm::cuda_stream_view stream)
 {
   auto const type = input.type().id();
 
   if (cannot_need_sanitize(type)) { return false; }
 
-  // Cross-reference nullmask and offsets.
-  if ((type == type_id::STRING || type == type_id::LIST) && input.nullable()) {
-    auto const offsets      = (type == type_id::STRING) ? (strings_column_view{input}).offsets()
-                                                        : (lists_column_view{input}).offsets();
-    auto const d_input      = cudf::column_device_view::create(input);
-    auto const is_dirty_row = [d_input = *d_input, offsets = offsets.begin<size_type>()] __device__(
-                                size_type const& row_idx) {
-      return d_input.is_null_nocheck(row_idx) && (offsets[row_idx] != offsets[row_idx + 1]);
-    };
-
-    auto const row_begin = thrust::make_counting_iterator<cudf::size_type>(0);
-    auto const row_end   = row_begin + input.size();
-
-    if (thrust::any_of(rmm::exec_policy(stream), row_begin, row_end, is_dirty_row)) { return true; }
+  // For types with variable-length rows, check if any rows are "dirty".
+  // A dirty row is a null row with non-zero length.
+  if ((type == type_id::STRING || type == type_id::LIST) && has_dirty_rows(input, stream)) {
+    return true;
   }
 
-  // For complex types, check child columns.
-  if (type == type_id::STRUCT || type == type_id::LIST) {
-    if (std::any_of(input.child_begin(), input.child_end(), [](auto const& child) {
-          return needs_sanitize(child, rmm::cuda_stream_view{});
-        })) {
-      return true;
-    }
+  // For complex types, check if child columns need sanitization.
+  if ((type == type_id::STRUCT || type == type_id::LIST) && has_dirty_children(input, stream)) {
+    return true;
   }
 
   return false;
@@ -73,6 +90,9 @@ bool needs_sanitize(cudf::column_view const& input, rmm::cuda_stream_view stream
 
 }  // namespace detail
 
+/**
+ * @copydoc cudf::needs_sanitize
+ */
 bool needs_sanitize(column_view const& input) { return detail::needs_sanitize(input); }
 
 }  // namespace cudf
