9aa42a845a0c7847e85da35a2a057fb604b6a51d
Sheilah <kirui.sheilah@gmail.com>
2022-04-29 17:29:41 -0700

    added few more shape mismatch tests

--- python/cudf/cudf/tests/test_indexing.py
+++ python/cudf/cudf/tests/test_indexing.py
@@ -1572,14 +1572,12 @@ def test_dataframe_loc_iloc_inplace_update_with_RHS_dataframe(
     gdf = cudf.DataFrame({"x": [1, 2, 3], "y": [4, 5, 6]})
     pdf = gdf.to_pandas()
 
-    actual = gdf.loc[loc_key] = cudf.DataFrame(data, index=cudf.Index([0, 2]))
-    expected = pdf.loc[loc_key] = pd.DataFrame(data, index=pd.Index([0, 2]))
+    actual = gdf.loc[loc_key] = cudf.DataFrame(data, index=cudf.Index(index))
+    expected = pdf.loc[loc_key] = pd.DataFrame(data, index=pd.Index(index))
     assert_eq(expected, actual)
 
-    actual = gdf.iloc[iloc_key] = cudf.DataFrame(
-        data, index=cudf.Index([0, 2])
-    )
-    expected = pdf.iloc[iloc_key] = pd.DataFrame(data, index=pd.Index([0, 2]))
+    actual = gdf.iloc[iloc_key] = cudf.DataFrame(data, index=cudf.Index(index))
+    expected = pdf.iloc[iloc_key] = pd.DataFrame(data, index=pd.Index(index))
     assert_eq(expected, actual)
 
 
@@ -1597,9 +1595,19 @@ def test_dataframe_loc_inplace_update_with_invalid_RHS_columns():
     assert_eq(expected, actual)
 
 
-def test_dataframe_loc_inplace_update_shape_mismatch():
+def test_dataframe_loc_iloc_inplace_update_shape_mismatch():
     gdf = cudf.DataFrame({"x": [1, 2, 3], "y": [4, 5, 6]})
     with pytest.raises(ValueError, match="shape mismatch:"):
+        gdf.loc[([0, 2], ["x", "y"])] = cudf.DataFrame(
+            {"x": [10, 20]}, index=cudf.Index([0, 2])
+        )
+        gdf.iloc[([0, 2], ["x", "y"])] = cudf.DataFrame(
+            {"x": [10, 20]}, index=cudf.Index([0, 2])
+        )
+
+        gdf.loc[([0], ["x", "y"])] = [[10], [20]]
+        gdf.iloc[([0], ["x", "y"])] = [[10], [20]]
+
         gdf.loc[[0, 2], ["x", "y"]] = [[10, 30, 50], [20, 40, 60]]
         gdf.iloc[[0, 2]] = [[10, 30, 50], [20, 40, 60]]
 
