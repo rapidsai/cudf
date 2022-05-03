c7ab9443a222e2185b4292785d5dc883ffec90ee
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-05-03 13:57:22 -0500

    Update build.sh

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -90,9 +90,9 @@ gpuci_mamba_retry install -y \
 
 # https://docs.rapids.ai/maintainers/depmgmt/
 # gpuci_conda_retry remove --force rapids-build-env rapids-notebook-env
-# gpuci_mamba_retry update -c conda-forge -y "pandas=1.4.2" --force-reinstall
-pip install -I pandas==1.4.2
-pip install -I numpy<=1.21
+gpuci_mamba_retry update -c conda-forge -y "pandas=1.4.2" "numpy<=1.21" --force-reinstall
+#pip install -I pandas==1.4.2
+
 
 
 gpuci_logger "Check compiler versions"
