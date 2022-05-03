0ba815dd2d08a5b696b6d238067a19f39669bc5b
MithunR <mythrocks@gmail.com>
2022-05-02 19:02:04 -0700

    Initial take.

---
+++ cpp/src/lists/apply_boolean_mask.cu
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cudf/column/column_factories.hpp>
+#include <cudf/detail/copy.hpp>
+#include <cudf/detail/fill.hpp>
+#include <cudf/detail/iterator.cuh>
+#include <cudf/detail/null_mask.hpp>
+#include <cudf/detail/reduction_functions.hpp>
+#include <cudf/detail/replace.hpp>
+#include <cudf/detail/stream_compaction.hpp>
+#include <cudf/lists/detail/stream_compaction.hpp>
+#include <cudf/lists/stream_compaction.hpp>
+#include <cudf/utilities/bit.hpp>
+
+#include <rmm/exec_policy.hpp>
+
+#include <thrust/reduce.h>
+
+namespace cudf {
+namespace detail {
+namespace {
+
+class get_list_size {
+ public:
+  explicit get_list_size(lists_column_view const& lcv)
+    : num_rows{lcv.size()},
+      offsets{lcv.offsets().begin<offset_type>() + lcv.offset()},
+      bitmask{lcv.null_mask()}
+  {
+  }
+
+  size_type __device__ operator()(size_type i) const
+  {
+    return bit_value_or(bitmask, i, true) ? (offsets[i + 1] - offsets[i]) : 0;
+  }
+
+ private:
+  size_type num_rows;
+  offset_type const* offsets;
+  bitmask_type const* bitmask;
+};
+
+void assert_same_sizes(lists_column_view const& input,
+                       lists_column_view const& boolean_mask,
+                       rmm::cuda_stream_view stream)
+{
+  CUDF_EXPECTS(input.size() == boolean_mask.size(),
+               "Boolean masks column must have same number of rows as input.");
+
+  auto const begin = make_counting_transform_iterator(
+    0,
+    [get_list_size = get_list_size{input}, get_mask_size = get_list_size{boolean_mask}] __device__(
+      size_type i) -> size_type { return get_list_size(i) != get_mask_size(i); });
+
+  CUDF_EXPECTS(thrust::reduce(rmm::exec_policy(stream), begin, begin + input.size()) == 0,
+               "Each list row must match the corresponding boolean mask row in size.");
+}
+}  // namespace
+
+std::unique_ptr<column> apply_boolean_mask(lists_column_view const& input,
+                                           lists_column_view const& boolean_mask,
+                                           rmm::cuda_stream_view stream,
+                                           rmm::mr::device_memory_resource* mr)
+{
+  // TODO: Test sliced input. Offsets aren't sliced.
+
+  auto const num_rows = input.size();
+
+  if (num_rows == 0) { return cudf::empty_like(input.child()); }
+  // Note: This assert guarantees that no elements are gathered
+  // from nominally NULL input list rows.
+  assert_same_sizes(input, boolean_mask, stream);
+
+  auto constexpr offset_data_type = data_type{type_id::INT32};
+
+  auto filtered_child = [&] {
+    std::unique_ptr<cudf::table> tbl =
+      cudf::detail::apply_boolean_mask(cudf::table_view{{input.get_sliced_child(stream)}},
+                                       boolean_mask.get_sliced_child(stream),
+                                       stream,
+                                       mr);
+    std::vector<std::unique_ptr<cudf::column>> columns = tbl->release();
+    return std::move(columns.front());
+  };
+
+  auto output_offsets = [&] {
+    auto boolean_mask_sliced_offsets =
+      cudf::detail::slice(
+        boolean_mask.offsets(), {boolean_mask.offset(), boolean_mask.size()}, stream)
+        .front();
+    auto const sizes         = cudf::reduction::segmented_sum(boolean_mask.get_sliced_child(stream),
+                                                      boolean_mask_sliced_offsets,
+                                                      offset_data_type,
+                                                      null_policy::EXCLUDE,
+                                                      stream);
+    auto const scalar_0      = cudf::numeric_scalar<offset_type>{0, true, stream};
+    auto const no_null_sizes = cudf::detail::replace_nulls(*sizes, scalar_0, stream);
+
+    auto offsets = cudf::make_numeric_column(
+      offset_data_type, num_rows + 1, mask_state::UNALLOCATED, stream, mr);
+    thrust::exclusive_scan(rmm::exec_policy(stream),
+                           no_null_sizes->view().begin<offset_type>(),
+                           no_null_sizes->view().end<offset_type>() + 1,
+                           offsets->mutable_view().begin<offset_type>());
+    return offsets;
+  };
+
+  return cudf::make_lists_column(input.size(),
+                                 output_offsets(),
+                                 filtered_child(),
+                                 input.null_count(),
+                                 cudf::detail::copy_bitmask(input.parent(), stream, mr),
+                                 stream,
+                                 mr);
+}
+}  // namespace detail
+
+std::unique_ptr<column> apply_boolean_mask(lists_column_view const& input,
+                                           lists_column_view const& boolean_mask,
+                                           rmm::mr::device_memory_resource* mr)
+{
+  return detail::apply_boolean_mask(input, boolean_mask, rmm::cuda_stream_default, mr);
+}
+
+}  // namespace cudf
---
+++ cpp/tests/lists/segmented_test.cu
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "cudf/detail/reduction_functions.hpp"
+#include "cudf/detail/stream_compaction.hpp"
+#include <cudf/column/column_factories.hpp>
+#include <cudf/detail/null_mask.hpp>
+#include <cudf/lists/extract.hpp>
+
+#include <cudf_test/base_fixture.hpp>
+#include <cudf_test/column_utilities.hpp>
+#include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/iterator_utilities.hpp>
+#include <cudf_test/type_lists.hpp>
+#include <tests/strings/utilities.h>
+
+#include <rmm/cuda_stream_view.hpp>
+
+#include <thrust/iterator/constant_iterator.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/iterator/transform_iterator.h>
+
+#include <vector>
+
+namespace cudf::test {
+
+using lists  = lists_column_wrapper<int32_t>;
+using filter = lists_column_wrapper<bool, int32_t>;
+
+struct SegmentedFilterTest : public BaseFixture {
+};
+
+TEST_F(SegmentedFilterTest, TheBasic)
+{
+  std::cout << "CALEB: SegmentedFilterTest.TheBasic" << std::endl;
+  auto filteree = lists{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {9, 0, 1}, {2, 3, 4}, {5, 6, 7}};
+  auto filterer =
+    filter{{1, 0, 1}, {0, 1, 0}, {1, 0, 1}, {0, 1, 0}, {1, 0, 1}, {0, 1, 0}}.release();
+  // auto filterer   = filter{{1,0,1}, {}, {1,0,1}, {0,1,0}, {1,0,1}, {0,1,0}}.release();
+  auto bools_cv = lists_column_view(*filterer);
+  std::cout << "Filteree: " << std::endl;
+  print(filteree);
+  std::cout << "Filterer: " << std::endl;
+  print(*filterer);
+
+  auto output_offsets = cudf::reduction::segmented_sum(
+    bools_cv.child(), bools_cv.offsets(), data_type{type_id::INT32}, null_policy::EXCLUDE);
+  std::cout << "Output offsets:\n";
+  print(*output_offsets);
+
+  auto filtered_child = cudf::detail::apply_boolean_mask(
+    cudf::table_view{{lists_column_view{filteree}.child()}}, bools_cv.child());
+  std::cout << "Filtered child:\n";
+  print(filtered_child->view().column(0));
+}
+
+TEST_F(SegmentedFilterTest, TheBasicForNestedLists)
+{
+  std::cout << "CALEB: SegmentedFilterTest.TheBasicForNestedLists" << std::endl;
+  auto filteree =
+    lists{{{0, 1}, {2}}, {{3}, {4, 5}}, {{6, 7}, {8}}, {{9, 0, 1}}, {{2, 3, 4}}, {{5, 6, 7}}};
+  auto filterer = filter{{1, 0}, {0, 1}, {1, 0}, {0}, {1}, {0}}.release();
+  // auto filterer   = filter{{1,0,1}, {}, {1,0,1}, {0,1,0}, {1,0,1}, {0,1,0}}.release();
+  auto bools_cv = lists_column_view(*filterer);
+  std::cout << "Filteree: " << std::endl;
+  print(filteree);
+  std::cout << "Filterer: " << std::endl;
+  print(*filterer);
+
+  auto output_offsets = cudf::reduction::segmented_sum(
+    bools_cv.child(), bools_cv.offsets(), data_type{type_id::INT32}, null_policy::EXCLUDE);
+  std::cout << "Output offsets:\n";
+  print(*output_offsets);
+
+  auto filtered_child = cudf::detail::apply_boolean_mask(
+    cudf::table_view{{lists_column_view{filteree}.child()}}, bools_cv.child());
+  std::cout << "Filtered child:\n";
+  print(filtered_child->view().column(0));
+}
+
+}  // namespace cudf::test
