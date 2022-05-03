8d59d69b6ee8effa43f02d3ad49339bb1cbfee04
MithunR <mythrocks@gmail.com>
2022-05-02 22:42:05 -0700

    Tests for null elements.

--- cpp/tests/lists/apply_boolean_mask_test.cpp
+++ cpp/tests/lists/apply_boolean_mask_test.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <cstdint>
 #include <cudf/column/column_factories.hpp>
 #include <cudf/detail/null_mask.hpp>
 #include <cudf/lists/extract.hpp>
@@ -23,23 +24,18 @@
 #include <cudf_test/column_wrapper.hpp>
 #include <cudf_test/iterator_utilities.hpp>
 #include <cudf_test/type_lists.hpp>
-#include <tests/strings/utilities.h>
-
-#include <rmm/cuda_stream_view.hpp>
-
-#include <thrust/iterator/constant_iterator.h>
-#include <thrust/iterator/counting_iterator.h>
-#include <thrust/iterator/transform_iterator.h>
-
-#include <vector>
 
 namespace cudf::test {
 
+using namespace iterators;
 using cudf::lists_column_view;
 using cudf::lists::apply_boolean_mask;
 
-using lists_t  = lists_column_wrapper<int32_t>;
+template <typename T>
+using lists    = lists_column_wrapper<T, int32_t>;
 using filter_t = lists_column_wrapper<bool, int32_t>;
+
+auto constexpr X = int32_t{0};  // Placeholder for NULL.
 
 struct ApplyBooleanMaskTest : public BaseFixture {
 };
@@ -48,55 +44,69 @@ template <typename T>
 struct ApplyBooleanMaskTypedTest : ApplyBooleanMaskTest {
 };
 
-using TestTypes = cudf::test::NumericTypes;
+TYPED_TEST_SUITE(ApplyBooleanMaskTypedTest, cudf::test::NumericTypes);
 
-TEST_F(ApplyBooleanMaskTest, StraightLine)
+TYPED_TEST(ApplyBooleanMaskTypedTest, StraightLine)
 {
-  auto input  = lists_t{{0, 1, 2, 3}, {4, 5}, {6, 7, 8, 9}, {0, 1}, {2, 3, 4, 5}, {6, 7}}.release();
+  using T    = TypeParam;
+  auto input = lists<T>{{0, 1, 2, 3}, {4, 5}, {6, 7, 8, 9}, {0, 1}, {2, 3, 4, 5}, {6, 7}}.release();
   auto filter = filter_t{{1, 0, 1, 0}, {1, 0}, {1, 0, 1, 0}, {1, 0}, {1, 0, 1, 0}, {1, 0}};
 
   {
     // Unsliced.
     auto filtered = apply_boolean_mask(lists_column_view{*input}, lists_column_view{filter});
-    auto expected = lists_t{{0, 2}, {4}, {6, 8}, {0}, {2, 4}, {6}};
+    auto expected = lists<T>{{0, 2}, {4}, {6, 8}, {0}, {2, 4}, {6}};
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(*filtered, expected);
   }
   {
     // Sliced input: Remove the first row.
     auto sliced = cudf::slice(*input, {1, input->size()}).front();
-    //         == lists_t {{4, 5}, {6, 7, 8, 9}, {0, 1}, {2, 3, 4, 5}, {6, 7}};
+    //           == lists_t {{4, 5}, {6, 7, 8, 9}, {0, 1}, {2, 3, 4, 5}, {6, 7}};
     auto filter   = filter_t{{0, 1}, {0, 1, 0, 1}, {1, 1}, {0, 1, 0, 1}, {0, 0}};
     auto filtered = apply_boolean_mask(lists_column_view{sliced}, lists_column_view{filter});
-    auto expected = lists_t{{5}, {7, 9}, {0, 1}, {3, 5}, {}};
+    auto expected = lists<T>{{5}, {7, 9}, {0, 1}, {3, 5}, {}};
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(*filtered, expected);
+  }
+}
+
+TYPED_TEST(ApplyBooleanMaskTypedTest, WithNullElements)
+{
+  using T = TypeParam;
+  auto input =
+    lists<T>{
+      {0, 1, 2, 3},
+      lists<T>{{X, 5}, null_at(0)},
+      {6, 7, 8, 9},
+      {0, 1},
+      lists<T>{{X, 3, 4, X}, nulls_at({0, 3})},
+      lists<T>{{X, X}, nulls_at({0, 1})},
+    }
+      .release();
+  auto filter = filter_t{{1, 0, 1, 0}, {1, 0}, {1, 0, 1, 0}, {1, 0}, {1, 0, 1, 0}, {1, 0}};
+
+  {
+    // Unsliced.
+    auto filtered = apply_boolean_mask(lists_column_view{*input}, lists_column_view{filter});
+    auto expected = lists<T>{{0, 2},
+                             lists<T>{{X}, null_at(0)},
+                             {6, 8},
+                             {0},
+                             lists<T>{{X, 4}, null_at(0)},
+                             lists<T>{{X}, null_at(0)}};
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(*filtered, expected);
+  }
+  {
+    // Sliced input: Remove the first row.
+    auto sliced = cudf::slice(*input, {1, input->size()}).front();
+    //           == lists_t {{X, 5}, {6, 7, 8, 9}, {0, 1}, {X, 3, 4, X}, {X, X}};
+    auto filter   = filter_t{{0, 1}, {0, 1, 0, 1}, {1, 1}, {0, 1, 0, 1}, {0, 0}};
+    auto filtered = apply_boolean_mask(lists_column_view{sliced}, lists_column_view{filter});
+    auto expected = lists<T>{{5}, {7, 9}, {0, 1}, lists<T>{{3, X}, null_at(1)}, {}};
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(*filtered, expected);
   }
 }
 
 /*
-TEST_F(SegmentedFilterTest, TheBasic)
-{
-  std::cout << "CALEB: SegmentedFilterTest.TheBasic" << std::endl;
-  auto filteree = lists{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {9, 0, 1}, {2, 3, 4}, {5, 6, 7}};
-  auto filterer =
-    filter{{1, 0, 1}, {0, 1, 0}, {1, 0, 1}, {0, 1, 0}, {1, 0, 1}, {0, 1, 0}}.release();
-  // auto filterer   = filter{{1,0,1}, {}, {1,0,1}, {0,1,0}, {1,0,1}, {0,1,0}}.release();
-  auto bools_cv = lists_column_view(*filterer);
-  std::cout << "Filteree: " << std::endl;
-  print(filteree);
-  std::cout << "Filterer: " << std::endl;
-  print(*filterer);
-
-  auto output_offsets = cudf::reduction::segmented_sum(
-    bools_cv.child(), bools_cv.offsets(), data_type{type_id::INT32}, null_policy::EXCLUDE);
-  std::cout << "Output offsets:\n";
-  print(*output_offsets);
-
-  auto filtered_child = cudf::detail::apply_boolean_mask(
-    cudf::table_view{{lists_column_view{filteree}.child()}}, bools_cv.child());
-  std::cout << "Filtered child:\n";
-  print(filtered_child->view().column(0));
-}
-
 TEST_F(SegmentedFilterTest, TheBasicForNestedLists)
 {
   std::cout << "CALEB: SegmentedFilterTest.TheBasicForNestedLists" << std::endl;
