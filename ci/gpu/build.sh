72faede252aadc821861406c33047b547d6ef26a
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-05-03 15:05:16 -0500

    Update build.sh

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -89,7 +89,7 @@ gpuci_mamba_retry install -y \
                   "ucx-py=${UCX_PY_VERSION}"
 
 # https://docs.rapids.ai/maintainers/depmgmt/
-# gpuci_conda_retry remove --force rapids-build-env rapids-notebook-env
+gpuci_conda_retry remove --force pandas numpy
 gpuci_mamba_retry install -c conda-forge -y "pandas=1.4.2" "numpy<=1.21" --force-reinstall
 #pip install -I pandas==1.4.2
 
