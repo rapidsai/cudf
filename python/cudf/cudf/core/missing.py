5358172202d769243a1c00b77d7dce210dd6b095
brandon-b-miller <53796099+brandon-b-miller@users.noreply.github.com>
2022-05-09 11:39:45 -0500

    Apply suggestions from code review
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- python/cudf/cudf/core/missing.py
+++ python/cudf/cudf/core/missing.py
@@ -4,4 +4,6 @@
 # Pandas NAType enforces a single instance exists at a time
 # instantiating this class will yield the existing instance
 # of pandas._libs.missing.NAType, id(cudf.NA) == id(pd.NA).
-from pandas import NA  # noqa: F401
+from pandas import NA
+
+__all__ = ["NA"]
