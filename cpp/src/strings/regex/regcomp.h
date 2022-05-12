fe9aaebeedce9a870661db2fe547addc8f3388fe
David Wendt <45795991+davidwendt@users.noreply.github.com>
2022-05-12 17:15:32 -0400

    Cleanup regex compile optimize functions (#10825)
    Cleans up the internal `regcomp::optimize1()` function by replacing for-loops with STL functions. Hopefully this will make this part of the code a bit easier to understand and maintain.
    
    No external function or behavior has changed.
    
    Authors:
      - David Wendt (https://github.com/davidwendt)
    
    Approvers:
      - Mark Harris (https://github.com/harrism)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/cudf/pull/10825

--- cpp/src/strings/regex/regcomp.h
+++ cpp/src/strings/regex/regcomp.h
@@ -124,8 +124,7 @@ class reprog {
   void set_start_inst(int32_t id);
   [[nodiscard]] int32_t get_start_inst() const;
 
-  void optimize1();
-  void optimize2();
+  void finalize();
   void check_for_errors();
 #ifndef NDEBUG
   void print(regex_flags const flags);
@@ -139,6 +138,8 @@ class reprog {
   int32_t _num_capturing_groups{};
 
   reprog() = default;
+  void collapse_nops();
+  void build_start_ids();
   void check_for_errors(int32_t id, int32_t next_id);
 };
 
