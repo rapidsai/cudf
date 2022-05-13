8e8ddaee3400ec936268b52fccb312eea7017b84
Nghia Truong <nghiatruong.vn@gmail.com>
2022-05-13 13:39:05 -0700

    Reverse `multibyte_split.cu`

--- cpp/src/io/text/multibyte_split.cu
+++ cpp/src/io/text/multibyte_split.cu
@@ -14,11 +14,6 @@
  * limitations under the License.
  */
 
-// Can be removed once we use Thrust 1.16+
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpragmas"
-#pragma GCC diagnostic ignored "-Wsizeof-array-div"
-
 #include <cudf/column/column.hpp>
 #include <cudf/column/column_factories.hpp>
 #include <cudf/detail/nvtx/ranges.hpp>
@@ -44,8 +39,6 @@
 
 #include <cub/block/block_load.cuh>
 #include <cub/block/block_scan.cuh>
-
-#pragma GCC diagnostic pop
 
 #include <memory>
 #include <optional>
