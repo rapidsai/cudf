cfd4e4b0c96ceeada17e0a4a6e32d9962a12a7a0
Robert Maynard <rmaynard@nvidia.com>
2022-05-16 16:19:21 -0400

    Use rapids-cmake 22.06

--- fetch_rapids.cmake
+++ fetch_rapids.cmake
@@ -12,12 +12,6 @@
 # the License.
 # =============================================================================
 include(FetchContent)
-FetchContent_Declare(
-  rapids-cmake
-  GIT_REPOSITORY https://github.com/robertmaynard/rapids-cmake.git
-  GIT_TAG add_cpm_nvcomp
-)
-
 file(DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-22.06/RAPIDS.cmake
      ${CMAKE_BINARY_DIR}/RAPIDS.cmake
 )
