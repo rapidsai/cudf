934ee731c276398655503f540040cb57be39b5c3
Nghia Truong <nghiatruong.vn@gmail.com>
2022-05-18 13:58:41 -0700

    Switch operands

--- cpp/src/structs/search/contains.cu
+++ cpp/src/structs/search/contains.cu
@@ -60,8 +60,8 @@ bool contains(structs_column_view const& haystack,
 
   auto const found_iter = thrust::find_if(
     rmm::exec_policy(stream), start_iter, end_iter, [d_comp] __device__(auto const idx) {
-      // Compare needle_as_col[0] == haystack[idx].
-      return d_comp(static_cast<rhs_index_type>(0), idx);
+      // Compare haystack[idx] == needle_as_col[0].
+      return d_comp(idx, static_cast<rhs_index_type>(0));
     });
 
   return found_iter != end_iter;
