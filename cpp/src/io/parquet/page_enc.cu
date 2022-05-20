39d027ef87b6fafc7e64274d07340f921410fd11
seidl <seidl2@llnl.gov>
2022-05-20 16:55:28 -0700

    implement suggestion from review, but needed to change type of this_max_page_size to size_t

--- cpp/src/io/parquet/page_enc.cu
+++ cpp/src/io/parquet/page_enc.cu
@@ -336,12 +336,12 @@ __global__ void __launch_bounds__(128)
           ? frag_g.num_leaf_values * 2  // Assume worst-case of 2-bytes per dictionary index
           : frag_g.fragment_data_size;
       // TODO (dm): this convoluted logic to limit page size needs refactoring
-      uint32_t this_max_page_size = (values_in_page * 2 >= ck_g.num_values)   ? 256 * 1024
-                                    : (values_in_page * 3 >= ck_g.num_values) ? 384 * 1024
-                                                                              : 512 * 1024;
+      size_t this_max_page_size = (values_in_page * 2 >= ck_g.num_values)   ? 256 * 1024
+                                  : (values_in_page * 3 >= ck_g.num_values) ? 384 * 1024
+                                                                            : 512 * 1024;
 
       // override this_max_page_size if the requested size is smaller
-      if (this_max_page_size > max_page_size_bytes) this_max_page_size = max_page_size_bytes;
+      this_max_page_size = min(this_max_page_size, max_page_size_bytes);
 
       if (num_rows >= ck_g.num_rows ||
           (values_in_page > 0 && (page_size + fragment_data_size > this_max_page_size)) ||
