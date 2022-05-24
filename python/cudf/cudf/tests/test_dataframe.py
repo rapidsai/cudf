a5dc55f861cb6088244a4d0296a2f13658f108b4
galipremsagar <sagarprem75@gmail.com>
2022-05-24 16:04:47 -0700

    fix single column multiIndex issue

--- python/cudf/cudf/tests/test_dataframe.py
+++ python/cudf/cudf/tests/test_dataframe.py
@@ -3073,6 +3073,11 @@ def test_dataframe_empty_sort_index():
     [
         pd.RangeIndex(0, 3, 1),
         [3.0, 1.0, np.nan],
+        pd.MultiIndex.from_arrays(
+            [
+                [2, 0, 1],
+            ]
+        ),
         pytest.param(
             pd.RangeIndex(2, -1, -1),
             marks=[
