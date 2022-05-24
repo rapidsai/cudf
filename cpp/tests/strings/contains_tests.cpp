581da1f8a781f43699d874264ab0319f491819e6
David Wendt <dwendt@nvidia.com>
2022-05-24 16:58:05 -0400

    remove commented out line

--- cpp/tests/strings/contains_tests.cpp
+++ cpp/tests/strings/contains_tests.cpp
@@ -402,7 +402,6 @@ TEST_F(StringsContainsTests, FixedQuantifier)
     cudf::test::fixed_width_column_wrapper<int32_t> expected({0, 0, 0, 0, 0, 0});
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected);
     EXPECT_THROW(cudf::strings::count_re(sv, "aaaa{1234,5678}"), cudf::logic_error);
-    // CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected);
   }
 }
 
