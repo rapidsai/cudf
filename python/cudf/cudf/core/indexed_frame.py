04bfabcbc239c6de1f294c624b25a92fd0a8ca6c
Vamsi Tallam <vtallam@dt01.aselab.nvidia.com>
2022-05-25 17:45:05 -0700

    removed axes functionality from index_frame

--- python/cudf/cudf/core/indexed_frame.py
+++ python/cudf/cudf/core/indexed_frame.py
@@ -229,53 +229,7 @@ class IndexedFrame(Frame):
 
     def __init__(self, data=None, index=None):
         super().__init__(data=data, index=index)
-
-    
-#     @property
-#     @_cudf_nvtx_annotate
-#     def axes(self)-> list[Index]:
-#         """
-#         Return a list representing the axes of DataFrame and Series.
-#         DataFrame.axes returns a list of two elements: element zero is the row index and element one is the columns.
-#         Series.axes returns a list containing the row index. 
-        
-#         Examples
-#         --------
-#         **DataFrame**
-        
-#         >>> import cudf
-        
-#         >>> cdf1 = cudf.DataFrame()
-#         >>> cdf1["key"] = [0,0,1,1]
-#         >>> cdf1["k2"] = [1,2,2,3]
-#         >>> cdf1["val"] = [1,2,3,4]
-#         >>> cdf1["temp"] = [-1,2,2,3]
-#         >>> print(cdf1.axes)
-#         [RangeIndex(start=0, stop=4, step=1), Index(['key', 'k2', 'val', 'temp'], dtype='object')]
-        
-#         >>> cdf1.groupby(["key", "k2"])
-#         >>> print((cdf1.groupby(["key", "k2"]).sum()).axes)
-#         [MultiIndex([(0, 2),
-#              (1, 2),
-#              (1, 3),
-#              (0, 1)],
-#             names=['key', 'k2']), Index(['val', 'temp'], dtype='object')]
-        
-#         **Series**
-        
-#         >>> csf1 = cudf.Series([1, 2, 3, 4])
-#         >>> print(csf1.axes)
-#         [RangeIndex(start=0, stop=4, step=1)]
-        
-#         """
-#         # DataFrame.axes
-#         if isinstance(self, cudf.core.dataframe.DataFrame): 
-#             return [self.index, self.columns]
-        
-#         # Series.axes
-#         else:
-#             return [self.index]     
-            
+                
     def to_dict(self, *args, **kwargs):  # noqa: D102
         raise TypeError(
             "cuDF does not support conversion to host memory "
