ce2681ce55ccb083ff919498fb7795043ef0d2db
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2022-05-28 05:20:36 +0530

    add missing docs in cudf/ipc.hpp

--- cpp/include/cudf/ipc.hpp
+++ cpp/include/cudf/ipc.hpp
@@ -3,13 +3,35 @@
 #include <arrow/io/memory.h>
 #include <arrow/ipc/api.h>
 
+/**
+ * @brief Reads Message objects from cuda buffer source
+ *
+ */
 class CudaMessageReader : arrow::ipc::MessageReader {
  public:
+  /**
+   * @brief Construct a new Cuda Message Reader object from a cuda buffer stream
+   *
+   * @param stream The cuda buffer reader stream
+   * @param schema The schema of the stream
+   */
   CudaMessageReader(arrow::cuda::CudaBufferReader* stream, arrow::io::BufferReader* schema);
 
+  /**
+   * @brief Open stream from source.
+   *
+   * @param stream The cuda buffer reader stream
+   * @param schema The schema of the stream
+   * @return arrow::ipc::MessageReader object
+   */
   static std::unique_ptr<arrow::ipc::MessageReader> Open(arrow::cuda::CudaBufferReader* stream,
                                                          arrow::io::BufferReader* schema);
 
+  /**
+   * @brief Read next Message from the stream.
+   *
+   * @return arrow::ipc::Message object
+   */
   arrow::Result<std::unique_ptr<arrow::ipc::Message>> ReadNextMessage() override;
 
  private:
