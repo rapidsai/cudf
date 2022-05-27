de2f197d13beb0ce3ce4a7ff66e255dc1be0217b
Nghia Truong <nghiatruong.vn@gmail.com>
2022-05-27 14:52:32 -0700

    Cleanup headers
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/src/groupby/sort/sort_helper.cu
+++ cpp/src/groupby/sort/sort_helper.cu
@@ -33,17 +33,11 @@
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/exec_policy.hpp>
 
-#include <thrust/binary_search.h>
 #include <thrust/distance.h>
 #include <thrust/fill.h>
-#include <thrust/iterator/constant_iterator.h>
 #include <thrust/iterator/counting_iterator.h>
-#include <thrust/iterator/discard_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
-#include <thrust/scan.h>
-#include <thrust/scatter.h>
 #include <thrust/sequence.h>
-#include <thrust/uninitialized_fill.h>
 #include <thrust/unique.h>
 
 #include <algorithm>
