136511d38046c9262ce67617a5cb39ba6f647807
Nghia Truong <nghiatruong.vn@gmail.com>
2022-05-31 16:52:52 -0700

    Rename `out_` iterators into `label_`
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/include/cudf/detail/labeling/label_segments.cuh
+++ cpp/include/cudf/detail/labeling/label_segments.cuh
@@ -53,25 +53,25 @@ namespace cudf::detail {
  *
  * @param offsets_begin The beginning of the offsets that define segments.
  * @param offsets_end The end of the offsets that define segments.
- * @param out_begin The beginning of the output label range.
- * @param out_end The end of the output label range.
+ * @param label_begin The beginning of the output label range.
+ * @param label_end The end of the output label range.
  * @param stream CUDA stream used for device memory operations and kernel launches.
  */
 template <typename InputIterator, typename OutputIterator>
 void label_segments(InputIterator offsets_begin,
                     InputIterator offsets_end,
-                    OutputIterator out_begin,
-                    OutputIterator out_end,
+                    OutputIterator label_begin,
+                    OutputIterator label_end,
                     rmm::cuda_stream_view stream)
 {
   // If the output array is empty, that means we have all empty segments.
   // In such cases, we must terminate immediately. Otherwise, the `for_each` loop below may try to
   // access memory of the output array, resulting in "illegal memory access" error.
-  if (thrust::distance(out_begin, out_end) == 0) { return; }
+  if (thrust::distance(label_begin, label_end) == 0) { return; }
 
   // When the output array is not empty, always fill it with `0` value first.
   using OutputType = typename thrust::iterator_value<OutputIterator>::type;
-  thrust::uninitialized_fill(rmm::exec_policy(stream), out_begin, out_end, OutputType{0});
+  thrust::uninitialized_fill(rmm::exec_policy(stream), label_begin, label_end, OutputType{0});
 
   // If the offsets array has no more than 2 offset values, there will be at max 1 segment.
   // In such cases, the output will just be an array of all `0` values (which we already filled).
@@ -83,7 +83,7 @@ void label_segments(InputIterator offsets_begin,
   thrust::for_each(rmm::exec_policy(stream),
                    offsets_begin + 1,  // exclude the first offset value
                    offsets_end - 1,    // exclude the last offset value
-                   [offsets = offsets_begin, output = out_begin] __device__(auto const idx) {
+                   [offsets = offsets_begin, output = label_begin] __device__(auto const idx) {
                      // Zero-normalized offsets.
                      auto const dst_idx = idx - (*offsets);
 
@@ -93,7 +93,7 @@ void label_segments(InputIterator offsets_begin,
                      // empty segments will be skipped in the output.
                      atomicAdd(&output[dst_idx], OutputType{1});
                    });
-  thrust::inclusive_scan(rmm::exec_policy(stream), out_begin, out_end, out_begin);
+  thrust::inclusive_scan(rmm::exec_policy(stream), label_begin, label_end, label_begin);
 }
 
 }  // namespace cudf::detail
