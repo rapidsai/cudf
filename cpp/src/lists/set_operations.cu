b145e33245ec30b1cb6d21f9ecb9b37e0df75fb8
Nghia Truong <nghiatruong.vn@gmail.com>
2022-06-03 13:27:51 -0700

    Implement `create_map` and `check_contains`
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/src/lists/set_operations.cu
+++ cpp/src/lists/set_operations.cu
@@ -17,9 +17,12 @@
 #include <stream_compaction/stream_compaction_common.cuh>
 #include <stream_compaction/stream_compaction_common.hpp>
 
+#include <cudf/detail/iterator.cuh>
 #include <cudf/lists/set_operations.hpp>
+#include <cudf/table/experimental/row_operators.cuh>
 
 #include <rmm/cuda_stream_view.hpp>
+#include <rmm/device_uvector.hpp>
 #include <rmm/exec_policy.hpp>
 
 namespace cudf::lists {
@@ -32,15 +35,48 @@ namespace {
  */
 auto create_map(column_view const& input, rmm::cuda_stream_view stream)
 {
-  //
+  auto map =
+    detail::hash_map_type{compute_hash_table_size(input.size()),
+                          detail::COMPACTION_EMPTY_KEY_SENTINEL,
+                          detail::COMPACTION_EMPTY_VALUE_SENTINEL,
+                          detail::hash_table_allocator_type{default_allocator<char>{}, stream},
+                          stream.value()};
+
+  auto const input_tv = table_view{{input}};
+  auto const hasher   = cudf::experimental::row::hash::row_hasher(input_tv, stream);
+  auto const d_hasher = detail::experimental::compaction_hash(
+    hasher.device_hasher(nullate::DYNAMIC{input.has_nulls()}));
+  auto const kv_it = cudf::detail::make_counting_transform_iterator(
+    size_type{0}, [] __device__(size_type const i) { return cuco::make_pair(i, i); });
+
+  map.insert(kv_it, kv_it + input.size(), d_hasher, thrust::equal_to<size_type>{}, stream.value());
+  return map;
 }
 
 /**
  * @brief Check the existence of rows in the keys column in the hash map.
  */
-auto check_contains(map_type const& map, column_view const& keys)
+auto check_contains(detail::hash_map_type const& map,
+                    bool map_has_nulls,
+                    column_view const& keys,
+                    rmm::cuda_stream_view stream)
 {
-  //
+  auto contained = rmm::device_uvector<bool>(keys.size(), stream);
+
+  auto const keys_tv  = table_view{{keys}};
+  auto const hasher   = cudf::experimental::row::hash::row_hasher(keys_tv, stream);
+  auto const d_hasher = cudf::experimental::row::hash::negative_index_hasher_adapter{
+    detail::experimental::compaction_hash(
+      hasher.device_hasher(nullate::DYNAMIC{keys.has_nulls()}))};
+  auto const comparator =
+    cudf::experimental::row::equality::two_table_comparator(haystack_tv, keys_tv, stream);
+  auto const d_eqcomp = cudf::experimental::row::equality::negative_index_comparator_adapter{
+    comparator.device_comparator(nullate::DYNAMIC{map_has_nulls || keys.has_nulls()})};
+  auto const keys_it = thrust::make_reverse_iterator(thrust::make_counting_iterator(size_type{0}));
+
+  map.contains(
+    keys_it, keys_it + keys.size(), contained.begin(), d_hasher, d_eqcomp, stream.value());
+  return contained;
 }
 
 /**
@@ -73,9 +109,9 @@ auto reconstruct_offsets()
 }  // namespace
 
 std::unique_ptr<column> overlap(lists_column_view const& lhs,
-                                    lists_column_view const& rhs,
-                                    rmm::cuda_stream_view stream,
-                                    rmm::mr::device_memory_resource* mr)
+                                lists_column_view const& rhs,
+                                rmm::cuda_stream_view stream,
+                                rmm::mr::device_memory_resource* mr)
 {
   // - Insert lhs child elements into map.
   // - Check contains for rhs child elements.
