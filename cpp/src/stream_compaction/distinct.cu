b8832f7632c16593dfbf3ac33ec8f06f5fd42a9d
Nghia Truong <nghiatruong.vn@gmail.com>
2022-06-07 15:26:34 -0700

    Switch to use the old atomic ops

--- cpp/src/stream_compaction/distinct.cu
+++ cpp/src/stream_compaction/distinct.cu
@@ -15,12 +15,12 @@
  */
 
 #include "stream_compaction_common.cuh"
-#include "stream_compaction_common.hpp"
 
 #include <cudf/column/column_device_view.cuh>
 #include <cudf/column/column_factories.hpp>
 #include <cudf/column/column_view.hpp>
 #include <cudf/detail/copy.hpp>
+#include <cudf/detail/copy_if.cuh>
 #include <cudf/detail/gather.cuh>
 #include <cudf/detail/gather.hpp>
 #include <cudf/detail/iterator.cuh>
@@ -48,8 +48,42 @@
 
 namespace cudf {
 namespace detail {
+
+namespace {
+template <duplicate_keep_option keep, typename MapDeviceView, typename Hash, typename KeyEqual>
+struct reduce_index_fn {
+  __device__ void operator()(size_type const key) const
+  {
+    // iter should always be valid, because all keys have been inserted.
+    auto const iter = d_map.find(key, d_hash, d_eqcomp);
+
+    // Here idx is the index of the unique elements that has been inserted into the map.
+    // As such, `find` calling for all duplicate keys will return the same idx value.
+    auto const idx = iter->second.load(cuda::std::memory_order_relaxed);
+
+    if constexpr (keep == duplicate_keep_option::KEEP_FIRST) {
+      // Store the smallest index of all keys that are equal.
+      atomicMin(&d_output[idx], key);
+    } else if constexpr (keep == duplicate_keep_option::KEEP_LAST) {
+      // Store the greatest index of all keys that are equal.
+      atomicMax(&d_output[idx], key);
+    } else {
+      // Count the number of duplicates for key.
+      atomicAdd(&d_output[idx], size_type{1});
+    }
+  }
+
+  size_type* const d_output;
+  MapDeviceView const d_map;
+  Hash const d_hash;
+  KeyEqual const d_eqcomp;
+};
+
+}  // namespace
+
 std::unique_ptr<table> distinct(table_view const& input,
                                 std::vector<size_type> const& keys,
+                                duplicate_keep_option keep,
                                 null_equality nulls_equal,
                                 rmm::cuda_stream_view stream,
                                 rmm::mr::device_memory_resource* mr)
@@ -81,73 +115,101 @@ std::unique_ptr<table> distinct(table_view const& input,
   // insert distinct indices into the map.
   key_map.insert(iter, iter + num_rows, hash_key, key_equal, stream.value());
 
-#if 0
-  auto const output_size{key_map.get_size()};
-  auto distinct_indices = cudf::make_numeric_column(
-    data_type{type_id::INT32}, output_size, mask_state::UNALLOCATED, stream, mr);
-  // write distinct indices to a numeric column
-  key_map.retrieve_all(distinct_indices->mutable_view().begin<cudf::size_type>(),
-                       thrust::make_discard_iterator(),
-                       stream.value());
-  // run gather operation to establish new order
-  return detail::gather(input,
-                        distinct_indices->view(),
-                        out_of_bounds_policy::DONT_CHECK,
-                        detail::negative_index_policy::NOT_ALLOWED,
-                        stream,
-                        mr);
-#else
+  // If we don't care about order, just gather all rows having distinct keys taken from key_map.
+  if (keep == duplicate_keep_option::KEEP_ANY) {
+    auto const output_size = key_map.get_size();
+    auto distinct_indices  = rmm::device_uvector<size_type>(output_size, stream);
 
-  auto const key_size{keys_view.num_rows()};
-  auto distinct_indices = rmm::device_uvector<size_type>(key_size, stream);
+    key_map.retrieve_all(distinct_indices.begin(), thrust::make_discard_iterator(), stream.value());
+    return detail::gather(input,
+                          distinct_indices.begin(),
+                          distinct_indices.end(),
+                          out_of_bounds_policy::DONT_CHECK,
+                          stream,
+                          mr);
+  }
 
-  // Fill `key_size` if keep_first
-  // Fill `INT_MIN` if keep_last
+  auto const key_size  = keys_view.num_rows();
+  auto reduced_indices = rmm::device_uvector<size_type>(key_size, stream);
+
+  auto const init_value = [keep] {
+    if (keep == duplicate_keep_option::KEEP_FIRST) {
+      return std::numeric_limits<size_type>::max();
+    } else if (keep == duplicate_keep_option::KEEP_LAST) {
+      return std::numeric_limits<size_type>::min();
+    }
+    return size_type{0};  // keep == KEEP_NONE
+  }();
+
   thrust::uninitialized_fill(rmm::exec_policy(stream),
-                             distinct_indices.begin(),
-                             distinct_indices.begin() + key_size,
-                             key_size);
+                             reduced_indices.begin(),
+                             reduced_indices.begin() + key_size,
+                             init_value);
+
+  auto const do_reduce = [key_size, stream](auto const& fn) {
+    thrust::for_each(rmm::exec_policy(stream),
+                     thrust::counting_iterator<size_type>(0),
+                     thrust::counting_iterator<size_type>(key_size),
+                     fn);
+  };
 
   auto const d_map = key_map.get_device_view();
-  thrust::for_each(
-    rmm::exec_policy(stream),
-    thrust::make_counting_iterator<size_type>(0),
-    thrust::make_counting_iterator<size_type>(key_size),
-    [distinct_indices = distinct_indices.begin(), d_map, hash_key, key_equal] __device__(auto key) {
-      // iter should always be valid, because all keys have been inserted.
-      // Here idx is the index of the unique elements that has been inserted into the map.
-      // As such, `find` calling for all duplicate keys will return the same idx value.
-      auto const idx =
-        d_map.find(key, hash_key, key_equal)->second.load(cuda::std::memory_order_relaxed);
-
-      // Store the minimum index of all keys that are equal.
-      atomicMin(&distinct_indices[idx], key);
-    });
+  switch (keep) {
+    case duplicate_keep_option::KEEP_FIRST:
+      do_reduce(
+        reduce_index_fn<duplicate_keep_option::KEEP_FIRST,
+                        decltype(d_map),
+                        decltype(hash_key),
+                        decltype(key_equal)>{reduced_indices.begin(), d_map, hash_key, key_equal});
+      break;
+    case duplicate_keep_option::KEEP_LAST:
+      do_reduce(
+        reduce_index_fn<duplicate_keep_option::KEEP_LAST,
+                        decltype(d_map),
+                        decltype(hash_key),
+                        decltype(key_equal)>{reduced_indices.begin(), d_map, hash_key, key_equal});
+      break;
+    case duplicate_keep_option::KEEP_NONE:
+      do_reduce(
+        reduce_index_fn<duplicate_keep_option::KEEP_NONE,
+                        decltype(d_map),
+                        decltype(hash_key),
+                        decltype(key_equal)>{reduced_indices.begin(), d_map, hash_key, key_equal});
+      break;
+    default:;  // KEEP_ANY has already been handled
+  }
 
   // Filter out the invalid indices, which are indices of the duplicate keys
-  // (except the first one that has valid index being written in the previous step).
-  auto gather_map = rmm::device_uvector<size_type>(key_size, stream);
-  auto const gather_map_end =
-    thrust::copy_if(rmm::exec_policy(stream),
-                    distinct_indices.begin(),
-                    distinct_indices.end(),
-                    gather_map.begin(),
-                    [key_size] __device__(auto const idx) { return idx != key_size; });
+  // (the first duplicate key already has valid index being written in the previous step).
+  if (keep == duplicate_keep_option::KEEP_FIRST || keep == duplicate_keep_option::KEEP_LAST) {
+    return cudf::detail::copy_if(
+      table_view{{input}},
+      [init_value, reduced_indices = reduced_indices.begin()] __device__(auto const idx) {
+        return reduced_indices[idx] != init_value;
+      },
+      stream,
+      mr);
+  }
 
-  return cudf::detail::gather(
-    input, gather_map.begin(), gather_map_end, out_of_bounds_policy::DONT_CHECK, stream, mr);
-#endif
+  return cudf::detail::copy_if(
+    table_view{{input}},
+    [reduced_indices = reduced_indices.begin()] __device__(auto const idx) {
+      return reduced_indices[idx] == size_type{1};
+    },
+    stream,
+    mr);
 }
 
 }  // namespace detail
 
 std::unique_ptr<table> distinct(table_view const& input,
                                 std::vector<size_type> const& keys,
+                                duplicate_keep_option keep,
                                 null_equality nulls_equal,
                                 rmm::mr::device_memory_resource* mr)
 {
   CUDF_FUNC_RANGE();
-  return detail::distinct(input, keys, nulls_equal, rmm::cuda_stream_default, mr);
+  return detail::distinct(input, keys, keep, nulls_equal, rmm::cuda_stream_default, mr);
 }
 
 }  // namespace cudf
