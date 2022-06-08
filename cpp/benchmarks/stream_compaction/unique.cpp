2f1ce5a98aea2c5bdb68a48ca18213ef06de6d45
Nghia Truong <nghiatruong.vn@gmail.com>
2022-06-08 16:17:23 -0700

    Reverse benchmark

--- cpp/benchmarks/stream_compaction/unique.cpp
+++ cpp/benchmarks/stream_compaction/unique.cpp
@@ -1,69 +1,83 @@
-#if 1
+/*
+ * Copyright (c) 2020-2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <benchmarks/common/generate_input.hpp>
-#include <benchmarks/fixture/benchmark_fixture.hpp>
-#include <benchmarks/synchronization/synchronization.hpp>
+#include <benchmarks/fixture/rmm_pool_raii.hpp>
 
-#include <cudf/column/column_factories.hpp>
-#include <cudf/concatenate.hpp>
-#include <cudf/sorting.hpp>
-#include <cudf/stream_compaction.hpp>
+#include <cudf/column/column_view.hpp>
+#include <cudf/detail/stream_compaction.hpp>
 #include <cudf/types.hpp>
 
-static constexpr cudf::size_type num_struct_members = 8;
-static constexpr cudf::size_type max_str_length     = 32;
+#include <nvbench/nvbench.cuh>
 
-namespace {
-static auto create_random_structs_column(cudf::size_type n_rows)
+// necessary for custom enum types
+// see: https://github.com/NVIDIA/nvbench/blob/main/examples/enums.cu
+NVBENCH_DECLARE_ENUM_TYPE_STRINGS(
+  // Enum type:
+  cudf::duplicate_keep_option,
+  // Callable to generate input strings:
+  [](cudf::duplicate_keep_option option) {
+    switch (option) {
+      case cudf::duplicate_keep_option::KEEP_FIRST: return "KEEP_FIRST";
+      case cudf::duplicate_keep_option::KEEP_LAST: return "KEEP_LAST";
+      case cudf::duplicate_keep_option::KEEP_NONE: return "KEEP_NONE";
+      default: return "ERROR";
+    }
+  },
+  // Callable to generate descriptions:
+  [](auto) { return std::string{}; })
+
+NVBENCH_DECLARE_TYPE_STRINGS(cudf::timestamp_ms, "cudf::timestamp_ms", "cudf::timestamp_ms");
+
+template <typename Type, cudf::duplicate_keep_option Keep>
+void nvbench_unique(nvbench::state& state, nvbench::type_list<Type, nvbench::enum_type<Keep>>)
 {
-  data_profile table_profile;
-  table_profile.set_distribution_params(cudf::type_id::INT32, distribution_id::UNIFORM, 0, n_rows);
-  table_profile.set_distribution_params(
-    cudf::type_id::STRING, distribution_id::NORMAL, 0, max_str_length);
-
-  // The first two struct members are int32 and string.
-  // The first column is also used as keys in groupby.
-  // The subsequent struct members are int32 and string again.
-  auto table = create_random_table(
-    cycle_dtypes({cudf::type_id::INT32, cudf::type_id::STRING}, num_struct_members),
-    row_count{n_rows},
-    table_profile);
-  return cudf::make_structs_column(n_rows, table->release(), 0, {});
-}
-
-void BM_fn(benchmark::State& state)
-{
-  auto const size{static_cast<cudf::size_type>(state.range(0))};
-
-  constexpr cudf::size_type repeat_times = 4;  // <25% unique rows
-  //  constexpr cudf::size_type repeat_times = 2;  // <50% unique rows
-  //  constexpr int repeat_times = 1;  // <100% unique rows
-
-  auto input        = create_random_structs_column(size / repeat_times);
-  auto const input0 = std::make_unique<cudf::column>(*input);
-
-  for (int i = 0; i < repeat_times - 1; ++i) {
-    input = cudf::concatenate(std::vector<cudf::column_view>{input0->view(), input->view()});
+  if constexpr (not std::is_same_v<Type, int32_t> and
+                Keep != cudf::duplicate_keep_option::KEEP_FIRST) {
+    state.skip("Skip unwanted benchmarks.");
   }
 
-  for ([[maybe_unused]] auto _ : state) {
-    [[maybe_unused]] auto const timer = cuda_event_timer(state, true);
-    auto const sorted_input           = cudf::sort(cudf::table_view{{*input}});
-    auto const result                 = cudf::unique(
-      sorted_input->view(), std::vector<int>{0}, cudf::duplicate_keep_option::KEEP_FIRST);
-  }
+  cudf::rmm_pool_raii pool_raii;
+
+  cudf::size_type const num_rows = state.get_int64("NumRows");
+
+  data_profile profile;
+  profile.set_null_frequency(0.01);
+  profile.set_cardinality(0);
+  profile.set_distribution_params<Type>(cudf::type_to_id<Type>(), distribution_id::UNIFORM, 0, 100);
+
+  auto source_table =
+    create_random_table(cycle_dtypes({cudf::type_to_id<Type>()}, 1), row_count{num_rows}, profile);
+
+  auto input_column = cudf::column_view(source_table->get_column(0));
+  auto input_table  = cudf::table_view({input_column, input_column, input_column, input_column});
+
+  state.exec(nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
+    rmm::cuda_stream_view stream_view{launch.get_stream()};
+    auto result =
+      cudf::detail::unique(input_table, {0}, Keep, cudf::null_equality::EQUAL, stream_view);
+  });
 }
-}  // namespace
 
-class Unique : public cudf::benchmark {
-};
+using data_type   = nvbench::type_list<bool, int8_t, int32_t, int64_t, float, cudf::timestamp_ms>;
+using keep_option = nvbench::enum_type_list<cudf::duplicate_keep_option::KEEP_FIRST,
+                                            cudf::duplicate_keep_option::KEEP_LAST,
+                                            cudf::duplicate_keep_option::KEEP_NONE>;
 
-BENCHMARK_DEFINE_F(Unique, BM)(::benchmark::State& state) { BM_fn(state); }
-
-BENCHMARK_REGISTER_F(Unique, BM)
-  ->RangeMultiplier(8)
-  ->Ranges({{1 << 10, 1 << 26}})
-  ->UseManualTime()
-  ->Unit(benchmark::kMillisecond);
-
-#endif
+NVBENCH_BENCH_TYPES(nvbench_unique, NVBENCH_TYPE_AXES(data_type, keep_option))
+  .set_name("unique")
+  .set_type_axes_names({"Type", "KeepOption"})
+  .add_int64_axis("NumRows", {10'000, 100'000, 1'000'000, 10'000'000});
