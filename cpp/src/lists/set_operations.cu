361464fc598c4bb57150c90e1ddec90575e2eead
Nghia Truong <nghiatruong.vn@gmail.com>
2022-06-10 15:37:50 -0700

    Reimplement `set_overlap`
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/src/lists/set_operations.cu
+++ cpp/src/lists/set_operations.cu
@@ -17,9 +17,10 @@
 #include <stream_compaction/stream_compaction_common.cuh>
 
 #include <cudf/column/column_factories.hpp>
-#include <cudf/detail/gather.cuh>
+#include <cudf/detail/copy_if.cuh>
 #include <cudf/detail/iterator.cuh>
 #include <cudf/detail/labeling/label_segments.cuh>
+#include <cudf/detail/nvtx/ranges.hpp>
 #include <cudf/lists/combine.hpp>
 #include <cudf/lists/set_operations.hpp>
 #include <cudf/table/experimental/row_operators.cuh>
@@ -36,32 +37,52 @@
 #include <rmm/device_uvector.hpp>
 #include <rmm/exec_policy.hpp>
 
+#include <cuco/static_multimap.cuh>
+
 namespace cudf::lists {
 namespace detail {
 
 namespace {
 
+using cudf::experimental::row::lhs_index_type;
+using cudf::experimental::row::rhs_index_type;
+using hash_map = cuco::static_map<lhs_index_type,
+                                  lhs_index_type,
+                                  cuda::thread_scope_device,
+                                  cudf::detail::hash_table_allocator_type>;
+
 /**
  * @brief Create a hash map with keys are indices of all elements in the input column.
  */
-std::unique_ptr<cudf::detail::hash_map_type> create_map(column_view const& input,
-                                                        rmm::cuda_stream_view stream)
+std::unique_ptr<hash_map> create_map(column_view const& input,
+                                     null_equality nulls_equal,
+                                     rmm::cuda_stream_view stream)
 {
-  auto map = std::make_unique<cudf::detail::hash_map_type>(
+  auto map = std::make_unique<hash_map>(
     compute_hash_table_size(input.size()),
-    cudf::detail::COMPACTION_EMPTY_KEY_SENTINEL,
-    cudf::detail::COMPACTION_EMPTY_VALUE_SENTINEL,
+    cuco::sentinel::empty_key{cudf::detail::COMPACTION_EMPTY_KEY_SENTINEL},
+    cuco::sentinel::empty_value{cudf::detail::COMPACTION_EMPTY_VALUE_SENTINEL},
     cudf::detail::hash_table_allocator_type{default_allocator<char>{}, stream},
     stream.value());
 
-  auto const input_tv = table_view{{input}};
-  auto const hasher   = cudf::experimental::row::hash::row_hasher(input_tv, stream);
-  auto const d_hasher = cudf::detail::experimental::compaction_hash(
-    hasher.device_hasher(nullate::DYNAMIC{input.has_nulls()}));
-  auto const kv_it = cudf::detail::make_counting_transform_iterator(
-    size_type{0}, [] __device__(size_type const i) { return cuco::make_pair(i, i); });
+  auto const has_nulls = has_nested_nulls(input);
+  auto const input_tv  = table_view{{input}};
+  auto const preprocessed_input =
+    cudf::experimental::row::hash::preprocessed_table::create(input_tv, stream);
 
-  map->insert(kv_it, kv_it + input.size(), d_hasher, thrust::equal_to<size_type>{}, stream.value());
+  auto const hasher = cudf::experimental::row::hash::row_hasher(preprocessed_input);
+  auto const d_hasher =
+    cudf::detail::experimental::compaction_hash(hasher.device_hasher(nullate::DYNAMIC{has_nulls}));
+
+  auto const comparator = cudf::experimental::row::equality::self_comparator(preprocessed_input);
+  // todo handle nans
+  auto const d_eqcomp = comparator.equal_to(nullate::DYNAMIC{has_nulls}, nulls_equal);
+
+  auto const kv_it =
+    cudf::detail::make_counting_transform_iterator(size_type{0}, [] __device__(size_type const i) {
+      return cuco::make_pair(lhs_index_type{i}, lhs_index_type{i});
+    });
+  map->insert(kv_it, kv_it + input.size(), d_hasher, d_eqcomp, stream.value());
   return map;
 }
 
@@ -70,25 +91,31 @@ std::unique_ptr<cudf::detail::hash_map_type> create_map(column_view const& input
  *        of the lhs column.
  */
 // todo: keys must be table with the first col is labels
-auto check_contains(std::unique_ptr<cudf::detail::hash_map_type> const& map,
+// todo handle nans
+auto check_contains(std::unique_ptr<hash_map> const& map,
                     column_view const& lhs,
                     column_view const& rhs,
+                    bool const lhs_has_nulls,
+                    bool const rhs_has_nulls,
+                    null_equality nulls_equal,
                     rmm::cuda_stream_view stream)
 {
   auto contained = rmm::device_uvector<bool>(rhs.size(), stream);
 
-  auto const lhs_tv   = table_view{{lhs}};
-  auto const rhs_tv   = table_view{{rhs}};
+  auto const lhs_tv = table_view{{lhs}};
+  auto const rhs_tv = table_view{{rhs}};
+
   auto const hasher   = cudf::experimental::row::hash::row_hasher(rhs_tv, stream);
-  auto const d_hasher = cudf::experimental::row::hash::index_normalized_hasher_adapter{
-    cudf::detail::experimental::compaction_hash(
-      hasher.device_hasher(nullate::DYNAMIC{rhs.has_nulls()}))};
+  auto const d_hasher = cudf::detail::experimental::compaction_hash(
+    hasher.device_hasher(nullate::DYNAMIC{rhs_has_nulls}));
+
   auto const comparator =
     cudf::experimental::row::equality::two_table_comparator(lhs_tv, rhs_tv, stream);
-  auto const d_eqcomp = cudf::experimental::row::equality::index_normalized_comparator_adapter{
-    comparator.equal_to(nullate::DYNAMIC{lhs.has_nulls() || rhs.has_nulls()})};
-  auto const rhs_it = thrust::make_reverse_iterator(thrust::make_counting_iterator(size_type{0}));
+  auto const d_eqcomp =
+    comparator.equal_to(nullate::DYNAMIC{lhs_has_nulls || rhs_has_nulls}, nulls_equal);
 
+  auto const rhs_it = cudf::detail::make_counting_transform_iterator(
+    size_type{0}, [] __device__(size_type const i) { return rhs_index_type{i}; });
   map->contains(rhs_it, rhs_it + rhs.size(), contained.begin(), d_hasher, d_eqcomp, stream.value());
   return contained;
 }
@@ -99,16 +126,18 @@ auto check_contains(std::unique_ptr<cudf::detail::hash_map_type> const& map,
  */
 auto generate_labels(lists_column_view const& input, rmm::cuda_stream_view stream)
 {
-  auto labels = rmm::device_uvector<size_type>(input.size(), stream);
+  auto labels = make_numeric_column(
+    data_type(type_to_id<size_type>()), input.size(), cudf::mask_state::UNALLOCATED, stream);
+  auto const labels_begin = labels->mutable_view().template begin<size_type>();
   cudf::detail::label_segments(
-    input.offsets_begin(), input.offsets_end(), labels.begin(), labels.end(), stream);
+    input.offsets_begin(), input.offsets_end(), labels_begin, labels_begin + input.size(), stream);
   return labels;
 }
 
 /**
  * @brief Reconstruct an offsets column from the input labels array.
  */
-auto reconstruct_offsets(rmm::device_uvector<size_type> const& labels,
+auto reconstruct_offsets(column_view const& labels,
                          size_type n_rows,
                          rmm::cuda_stream_view stream,
                          rmm::mr::device_memory_resource* mr)
@@ -116,50 +145,15 @@ auto reconstruct_offsets(rmm::device_uvector<size_type> const& labels,
 {
   auto out_offsets = make_numeric_column(
     data_type{type_to_id<offset_type>()}, n_rows + 1, mask_state::UNALLOCATED, stream, mr);
-  auto const offsets = out_offsets->mutable_view();
-  cudf::detail::labels_to_offsets(labels.begin(),
-                                  labels.end(),
-                                  offsets.template begin<size_type>(),
-                                  offsets.template end<size_type>(),
+
+  auto const labels_begin  = labels.template begin<size_type>();
+  auto const offsets_begin = out_offsets->mutable_view().template begin<size_type>();
+  cudf::detail::labels_to_offsets(labels_begin,
+                                  labels_begin + labels.size(),
+                                  offsets_begin,
+                                  offsets_begin + out_offsets->size(),
                                   stream);
   return out_offsets;
-}
-
-/**
- * @brief Extract rows from the input data column and labels array based on the boolean values in
- *        the input `condition` column.
- */
-auto extract_if(column_view const& data,
-                rmm::device_uvector<size_type> const& labels,
-                rmm::device_uvector<bool> const& condition,
-                rmm::cuda_stream_view stream,
-                rmm::mr::device_memory_resource* mr)
-
-{
-  CUDF_EXPECTS(static_cast<std::size_t>(data.size()) == labels.size(), "TBA");
-
-  auto gather_map = rmm::device_uvector<size_type>(data.size(), stream);
-  auto out_labels = rmm::device_uvector<size_type>(labels.size(), stream);
-
-  auto const input_it =
-    thrust::make_zip_iterator(thrust::make_counting_iterator<size_type>(0), labels.begin());
-  auto const output_it = thrust::make_zip_iterator(gather_map.begin(), out_labels.begin());
-  auto const copy_end  = thrust::copy_if(rmm::exec_policy(stream),
-                                        input_it,
-                                        input_it + data.size(),
-                                        condition.begin(),
-                                        output_it,
-                                        thrust::identity{});
-
-  auto const gather_map_size = thrust::distance(output_it, copy_end);
-  auto out_data              = cudf::detail::gather(table_view{{data}},
-                                       gather_map.begin(),
-                                       gather_map.begin() + gather_map_size,
-                                       out_of_bounds_policy::DONT_CHECK,
-                                       stream,
-                                       mr);
-
-  return std::pair(std::move(out_data->release().front()), std::move(out_labels));
 }
 
 }  // namespace
@@ -171,16 +165,24 @@ std::unique_ptr<column> set_overlap(lists_column_view const& lhs,
                                     rmm::cuda_stream_view stream,
                                     rmm::mr::device_memory_resource* mr)
 {
+  CUDF_EXPECTS(lhs.size() == rhs.size(), "TBA");
+
   // - Insert lhs child elements into map.
   // - Check contains for rhs child elements.
   // - Generate labels for rhs child elements.
   // - `reduce_by_key` with `logical_or` functor and keys are labels, values are contains.
 
-  auto const lhs_child = lhs.get_sliced_child(stream);
-  auto const rhs_child = rhs.get_sliced_child(stream);
-  auto const map       = create_map(lhs_child, stream);
-  auto const contained = check_contains(map, lhs_child, rhs_child, stream);
-  auto const labels    = generate_labels(rhs_child, stream);
+  auto const lhs_child           = lhs.get_sliced_child(stream);
+  auto const rhs_child           = rhs.get_sliced_child(stream);
+  auto const lhs_child_has_nulls = has_nested_nulls(lhs_child);
+  auto const rhs_child_has_nulls = has_nested_nulls(rhs_child);
+
+  auto const map = create_map(lhs_child, nulls_equal, stream);
+  // todo handle nans
+  auto const contained = check_contains(
+    map, lhs_child, rhs_child, lhs_child_has_nulls, rhs_child_has_nulls, nulls_equal, stream);
+  auto const labels       = generate_labels(rhs_child, stream);
+  auto const labels_begin = labels->view().template begin<size_type>();
 
   // This stores the unique label values, used as scatter map.
   auto list_indices = rmm::device_uvector<size_type>(lhs.size(), stream);
@@ -189,28 +191,30 @@ std::unique_ptr<column> set_overlap(lists_column_view const& lhs,
   auto overlap_result = rmm::device_uvector<bool>(lhs.size(), stream);
 
   auto const end                    = thrust::reduce_by_key(rmm::exec_policy(stream),
-                                         labels.begin(),
-                                         labels.end(),
-                                         contained.begin(),
-                                         list_indices.begin(),
-                                         overlap_result.begin(),
-                                         thrust::equal_to{},
-                                         thrust::logical_or{});
+                                         labels_begin,                  // keys
+                                         labels_begin + labels.size(),  // keys
+                                         contained.begin(),  // values to reduce
+                                         list_indices.begin(),    // out keys
+                                         overlap_result.begin(),  // out values
+                                         thrust::equal_to{},  // comp for keys
+                                         thrust::logical_or{});  // reduction op for values
   auto const num_non_empty_segments = thrust::distance(overlap_result.begin(), end.second);
 
   // todo fix null mask null count
-  auto result = make_numeric_column(data_type{type_to_id<bool>()},
+  auto result             = make_numeric_column(data_type{type_to_id<bool>()},
                                     lhs.size(),
                                     copy_bitmask(lhs.parent()),
                                     lhs.null_count(),
                                     stream,
                                     mr);
+  auto const result_begin = result->mutable_view().template begin<bool>();
+  thrust::uninitialized_fill(rmm::exec_policy(stream), result_begin, result_begin, false);
 
   thrust::scatter(rmm::exec_policy(stream),
                   overlap_result.begin(),
                   overlap_result.begin() + num_non_empty_segments,
                   list_indices.begin(),
-                  result->mutable_view().template begin<bool>());
+                  result_begin);
 
   return result;
 }
@@ -308,6 +312,7 @@ std::unique_ptr<column> set_overlap(lists_column_view const& lhs,
                                     nan_equality nans_equal,
                                     rmm::mr::device_memory_resource* mr)
 {
+  CUDF_FUNC_RANGE();
   return detail::set_overlap(lhs, rhs, nulls_equal, nans_equal, rmm::cuda_stream_default, mr);
 }
 
@@ -317,6 +322,7 @@ std::unique_ptr<column> set_intersect(lists_column_view const& lhs,
                                       nan_equality nans_equal,
                                       rmm::mr::device_memory_resource* mr)
 {
+  CUDF_FUNC_RANGE();
   return detail::set_intersect(lhs, rhs, nulls_equal, nans_equal, rmm::cuda_stream_default, mr);
 }
 
@@ -326,6 +332,7 @@ std::unique_ptr<column> set_union(lists_column_view const& lhs,
                                   nan_equality nans_equal,
                                   rmm::mr::device_memory_resource* mr)
 {
+  CUDF_FUNC_RANGE();
   return detail::set_union(lhs, rhs, nulls_equal, nans_equal, rmm::cuda_stream_default, mr);
 }
 
@@ -335,6 +342,7 @@ std::unique_ptr<column> set_difference(lists_column_view const& lhs,
                                        nan_equality nans_equal,
                                        rmm::mr::device_memory_resource* mr)
 {
+  CUDF_FUNC_RANGE();
   return detail::set_difference(lhs, rhs, nulls_equal, nans_equal, rmm::cuda_stream_default, mr);
 }
 
