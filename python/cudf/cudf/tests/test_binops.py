d38337c107f48b34766b2dcff75d90d17d36289a
Ashwin Srinath <shwina@users.noreply.github.com>
2022-06-13 17:23:37 -0400

    Don't special-case alignment for outer joins

--- python/cudf/cudf/tests/test_binops.py
+++ python/cudf/cudf/tests/test_binops.py
@@ -2975,7 +2975,7 @@ def test_binops_dot(df, other):
 def test_binop_series_with_repeated_index():
     # GH: #11094
     psr1 = pd.Series([1, 1], index=["a", "a"])
-    psr2 = pd.Series([1, 1], index=["a", "a"])
+    psr2 = pd.Series([1], index=["a"])
     gsr1 = cudf.from_pandas(psr1)
     gsr2 = cudf.from_pandas(psr2)
     expected = psr1 - psr2
