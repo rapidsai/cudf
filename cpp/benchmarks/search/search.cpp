7e51b86f0aee5142f2369b3a1ef3af3d75f7773a
Nghia Truong <nghiatruong.vn@gmail.com>
2022-06-13 07:24:06 -0700

    Update bench
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/benchmarks/search/search.cpp
+++ cpp/benchmarks/search/search.cpp
@@ -9,21 +9,19 @@
 #include <cudf/types.hpp>
 
 static constexpr cudf::size_type num_struct_members = 8;
-static constexpr cudf::size_type max_int            = 1000;
-static constexpr cudf::size_type max_str_length     = 2;
+static constexpr cudf::size_type max_str_length     = 10;
 
 static auto create_random_structs_column(cudf::size_type n_rows)
 {
   data_profile table_profile;
-  table_profile.set_distribution_params(
-    cudf::type_id::INT32, distribution_id::UNIFORM, 0, n_rows / 100);
+  table_profile.set_distribution_params(cudf::type_id::INT32, distribution_id::UNIFORM, 0, n_rows);
   table_profile.set_distribution_params(
     cudf::type_id::STRING, distribution_id::NORMAL, 0, max_str_length);
 
   // The first two struct members are int32 and string.
   // The first column is also used as keys in groupby.
   // The subsequent struct members are int32 and string again.
-  auto table = create_random_table(cycle_dtypes({cudf::type_id::INT32, cudf::type_id::INT32}, 2),
+  auto table = create_random_table(cycle_dtypes({cudf::type_id::INT32, cudf::type_id::STRING}, 8),
                                    row_count{n_rows},
                                    table_profile);
   return cudf::make_structs_column(n_rows, table->release(), 0, {});
@@ -33,9 +31,9 @@ void BM_contains(benchmark::State& state)
 {
   auto const size{static_cast<cudf::size_type>(state.range(0))};
 
-  //  constexpr cudf::size_type repeat_times = 4;  // <25% unique rows
-  //  constexpr cudf::size_type repeat_times = 2;  // <50% unique rows
-  constexpr int repeat_times = 1;  // <100% unique rows
+  constexpr cudf::size_type repeat_times = 10;  // <25% unique rows
+  //    constexpr cudf::size_type repeat_times = 2;  // <50% unique rows
+  //  constexpr int repeat_times = 1;  // <100% unique rows
 
   auto const needles = create_random_structs_column(size);
 
