9c2fb255861e1d0d3d6e8008d75e04f48ac4d80d
Nghia Truong <nghiatruong.vn@gmail.com>
2022-06-21 15:59:39 -0700

    Draft for doxygen
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/include/cudf/lists/set_operations.hpp
+++ cpp/include/cudf/lists/set_operations.hpp
@@ -28,23 +28,66 @@ namespace cudf::lists {
  */
 
 /**
- * @brief TBA
+ * @brief Check if lists at each row of the given lists columns overlap.
  *
+ * Given two input lists columns, each list row in one column is checked if it has any common
+ * elements with the corresponding row of the other column.
+ *
+ * A null input row in any of the input lists columns will result in a null output row.
+ *
+ * @throw cudf::logic_error if the input lists column have different sizes.
+ * @throw cudf::logic_error if children of the input lists column have different data types.
  *
  * Example:
  * @code{.pseudo}
+ * lhs = { {0, 1, 2}, {1, 2, 3}, null,         {4, null, 5} }
+ * rhs = { {1, 2, 3}, {4, 5},    {null, 7, 8}, {null, null} }
+ *
+ * result = { true, false, null, true }
  * @endcode
  *
- * @throws cudf::logic_error
- *
+ * @param lhs The input lists column for one side
+ * @param rhs The input lists column for the other side
+ * @param nulls_equal Flag to specify whether null elements should be considered as equal
+ * @param nans_equal Flag to specify whether floating-point NaNs should be considered as equal
+ * @param mr Device memory resource used to allocate the returned object
+ * @return A column of type BOOL containing the check result
  */
-std::unique_ptr<column> set_overlap(
+std::unique_ptr<column> list_overlap(
   lists_column_view const& lhs,
   lists_column_view const& rhs,
   null_equality nulls_equal           = null_equality::EQUAL,
   nan_equality nans_equal             = nan_equality::UNEQUAL,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 
+/**
+ * @brief Find the intersection without duplicate between lists at each row of the given lists
+ *        columns.
+ *
+ * Given two input lists columns, an output lists column is created in such a way that each of its
+ * row contains the common elements (without duplicates) of each pair of lists at the same row
+ * from the input columns.
+ *
+ * A null input row in any of the input lists columns will result in a null output row.
+ *
+ * @throw cudf::logic_error if the input lists column have different sizes.
+ * @throw cudf::logic_error if children of the input lists column have different data types.
+ *
+ * Example:
+ * @code{.pseudo}
+ * lhs = { {2, 1, 2}, {1, 2, 3}, null,         {4, null, 5} }
+ * rhs = { {1, 2, 3}, {4, 5},    {null, 7, 8}, {null, null} }
+ *
+ * result = { {1, 2}, {}, null, {null} }
+ * @endcode
+ *
+ * @param lhs The input lists column for one side
+ * @param rhs The input lists column for the other side
+ * @param nulls_equal Flag to specify whether null elements should be considered as equal
+ * @param nans_equal Flag to specify whether floating-point NaNs should be considered as equal
+ * @param mr Device memory resource used to allocate the returned object
+ * @return A column of type BOOL containing the check result
+ */
 std::unique_ptr<column> set_intersect(
   lists_column_view const& lhs,
   lists_column_view const& rhs,
@@ -52,6 +95,36 @@ std::unique_ptr<column> set_intersect(
   nan_equality nans_equal             = nan_equality::UNEQUAL,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 
+/**
+ * @brief Find the union (without duplicate) between lists at each row of the given lists
+ *        columns.
+ *
+ * Given two input lists columns, an output lists column is created by concatenating each pair of
+ * lists at the same row from the input columns then removing duplicates.
+ *
+ * each pair of lists at the same row is concatenated then the result
+ * is extracted without duplicates to the corresponding row in the output lists column.
+ *
+ * A null input row in any of the input lists columns will result in a null output row.
+ *
+ * @throw cudf::logic_error if the input lists column have different sizes.
+ * @throw cudf::logic_error if children of the input lists column have different data types.
+ *
+ * Example:
+ * @code{.pseudo}
+ * lhs = { {2, 1, 2}, {1, 2, 3}, null,         {4, null, 5} }
+ * rhs = { {1, 2, 3}, {4, 5},    {null, 7, 8}, {null, null} }
+ *
+ * result = { {1, 2, 3}, {1, 2, 3, 4, 5}, null, {4, null, 5} }
+ * @endcode
+ *
+ * @param lhs The input lists column for one side
+ * @param rhs The input lists column for the other side
+ * @param nulls_equal Flag to specify whether null elements should be considered as equal
+ * @param nans_equal Flag to specify whether floating-point NaNs should be considered as equal
+ * @param mr Device memory resource used to allocate the returned object
+ * @return A column of type BOOL containing the check result
+ */
 std::unique_ptr<column> set_union(
   lists_column_view const& lhs,
   lists_column_view const& rhs,
@@ -59,6 +132,34 @@ std::unique_ptr<column> set_union(
   nan_equality nans_equal             = nan_equality::UNEQUAL,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 
+/**
+ * @brief Find the elements (without duplicates) from each list of the left column that do not exist
+ *        in the corresponding list of the right column.
+ *
+ * Given two input lists columns, an output lists column is created by finding the elements (without
+ * duplicates) in each list of the left column such that they do not exist in the corresponding list
+ * of the right column.
+ *
+ * A null input row in any of the input lists columns will result in a null output row.
+ *
+ * @throw cudf::logic_error if the input lists column have different sizes.
+ * @throw cudf::logic_error if children of the input lists column have different data types.
+ *
+ * Example:
+ * @code{.pseudo}
+ * lhs = { {2, 1, 2}, {1, 2, 3}, null,         {4, null, 5} }
+ * rhs = { {1, 2, 3}, {4, 5},    {null, 7, 8}, {null, null} }
+ *
+ * result = { {}, {1, 2, 3}, null, {4, 5} }
+ * @endcode
+ *
+ * @param lhs The input lists column containing the searching elements
+ * @param rhs The input lists column for checking element existence
+ * @param nulls_equal Flag to specify whether null elements should be considered as equal
+ * @param nans_equal Flag to specify whether floating-point NaNs should be considered as equal
+ * @param mr Device memory resource used to allocate the returned object
+ * @return A column of type BOOL containing the check result
+ */
 std::unique_ptr<column> set_difference(
   lists_column_view const& lhs,
   lists_column_view const& rhs,
