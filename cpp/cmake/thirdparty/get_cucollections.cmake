fbfadc60330594d40ab3f9b45a106dcc9b021c20
Jason Lowe <jlowe@nvidia.com>
2022-06-24 11:10:59 -0500

    Ensure cuco export set is installed in cmake build

--- cpp/cmake/thirdparty/get_cucollections.cmake
+++ cpp/cmake/thirdparty/get_cucollections.cmake
@@ -15,7 +15,7 @@
 # This function finds cuCollections and performs any additional configuration.
 function(find_and_configure_cucollections)
   include(${rapids-cmake-dir}/cpm/cuco.cmake)
-  rapids_cpm_cuco(BUILD_EXPORT_SET cudf-exports)
+  rapids_cpm_cuco(BUILD_EXPORT_SET cudf-exports INSTALL_EXPORT_SET cudf-exports)
 
   # Have to manually export cuco for static builds.
   if(NOT BUILD_SHARED_LIBS)
