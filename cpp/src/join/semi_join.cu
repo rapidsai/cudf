1d5015e84688dbac384e47822cec5cf5df46ce4c
Nghia Truong <nghiatruong.vn@gmail.com>
2022-06-23 19:08:56 -0700

    Remove an extra kernel call

--- cpp/src/join/semi_join.cu
+++ cpp/src/join/semi_join.cu
@@ -60,34 +60,26 @@ std::unique_ptr<rmm::device_uvector<cudf::size_type>> left_semi_anti_join(
     return result;
   }
 
-  auto const flagged = [&] {
-    auto contained = contains(right_keys,
-                              left_keys,
-                              compare_nulls,
-                              nan_equality::ALL_EQUAL,
-                              stream,
-                              rmm::mr::get_current_device_resource());
-    if (kind == join_kind::LEFT_ANTI_JOIN) {
-      thrust::transform(rmm::exec_policy(stream),
-                        contained.begin(),
-                        contained.end(),
-                        contained.begin(),
-                        thrust::logical_not{});
-    }
-    return contained;
-  }();
+  auto const flagged = contains(right_keys,
+                                left_keys,
+                                compare_nulls,
+                                nan_equality::ALL_EQUAL,
+                                stream,
+                                rmm::mr::get_current_device_resource());
 
   auto const left_num_rows = left_keys.num_rows();
   auto gather_map =
     std::make_unique<rmm::device_uvector<cudf::size_type>>(left_num_rows, stream, mr);
 
   // gather_map_end will be the end of valid data in gather_map
-  auto gather_map_end = thrust::copy_if(
-    rmm::exec_policy(stream),
-    thrust::counting_iterator<size_type>(0),
-    thrust::counting_iterator<size_type>(left_num_rows),
-    gather_map->begin(),
-    [d_flagged = flagged.begin()] __device__(size_type const idx) { return *(d_flagged + idx); });
+  auto gather_map_end =
+    thrust::copy_if(rmm::exec_policy(stream),
+                    thrust::counting_iterator<size_type>(0),
+                    thrust::counting_iterator<size_type>(left_num_rows),
+                    gather_map->begin(),
+                    [kind, d_flagged = flagged.begin()] __device__(size_type const idx) {
+                      return *(d_flagged + idx) == (kind == join_kind::LEFT_SEMI_JOIN);
+                    });
 
   gather_map->resize(thrust::distance(gather_map->begin(), gather_map_end), stream);
   return gather_map;
