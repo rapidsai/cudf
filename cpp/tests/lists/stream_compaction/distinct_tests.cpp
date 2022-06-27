3845c9560fa974879169756ef17e67c0bcd8071d
Nghia Truong <nghiatruong.vn@gmail.com>
2022-06-27 15:43:11 -0700

    Implement string tests

--- cpp/tests/lists/stream_compaction/distinct_tests.cpp
+++ cpp/tests/lists/stream_compaction/distinct_tests.cpp
@@ -31,12 +31,14 @@ using namespace cudf::test::iterators;
 
 auto constexpr null{0};  // null at current level
 // auto constexpr XXX{0};   // null pushed down from parent level
-auto constexpr neg_NaN     = -std::numeric_limits<float_type>::quiet_NaN();
-auto constexpr neg_Inf     = -std::numeric_limits<float_type>::infinity();
-auto constexpr NaN         = std::numeric_limits<float_type>::quiet_NaN();
-auto constexpr Inf         = std::numeric_limits<float_type>::infinity();
-auto constexpr NAN_EQUAL   = cudf::nan_equality::ALL_EQUAL;
-auto constexpr NAN_UNEQUAL = cudf::nan_equality::UNEQUAL;
+auto constexpr neg_NaN      = -std::numeric_limits<float_type>::quiet_NaN();
+auto constexpr neg_Inf      = -std::numeric_limits<float_type>::infinity();
+auto constexpr NaN          = std::numeric_limits<float_type>::quiet_NaN();
+auto constexpr Inf          = std::numeric_limits<float_type>::infinity();
+auto constexpr NULL_EQUAL   = cudf::null_equality::EQUAL;
+auto constexpr NULL_UNEQUAL = cudf::null_equality::UNEQUAL;
+auto constexpr NAN_EQUAL    = cudf::nan_equality::ALL_EQUAL;
+auto constexpr NAN_UNEQUAL  = cudf::nan_equality::UNEQUAL;
 
 using bools_col = cudf::test::fixed_width_column_wrapper<bool>;
 // using int32s_col  = cudf::test::fixed_width_column_wrapper<int32_t>;
@@ -49,10 +51,11 @@ using lists_cv = cudf::lists_column_view;
 
 namespace {
 
-auto distinct_sorted(cudf::column_view const& input, cudf::nan_equality nans_equal = NAN_EQUAL)
+auto distinct_sorted(cudf::column_view const& input,
+                     cudf::null_equality nulls_equal = NULL_EQUAL,
+                     cudf::nan_equality nans_equal   = NAN_EQUAL)
 {
-  auto const results =
-    cudf::lists::distinct(lists_cv{input}, cudf::null_equality::EQUAL, nans_equal);
+  auto const results = cudf::lists::distinct(lists_cv{input}, nulls_equal, nans_equal);
 
   // The sorted result will have nulls first and NaNs last.
   // In addition, row equality comparisons in tests just ignore NaN sign thus the expected values
@@ -96,10 +99,10 @@ TEST_F(ListDistinctTest, TrivialTest)
 TEST_F(ListDistinctTest, FloatingPointTestsWithSignedZero)
 {
   // -0.0 and 0.0 should be considered equal.
-  auto const input  = floats_lists{0.0, 1, 2, -0.0, 1, 2, 0.0, 1, 2, -0.0, -0.0, 0.0, 0.0, 3};
-  auto const expect = floats_lists{0, 1, 2, 3};
+  auto const input    = floats_lists{0.0, 1, 2, -0.0, 1, 2, 0.0, 1, 2, -0.0, -0.0, 0.0, 0.0, 3};
+  auto const expected = floats_lists{0, 1, 2, 3};
   auto const results_sorted = distinct_sorted(input);
-  CUDF_TEST_EXPECT_COLUMNS_EQUAL(expect, *results_sorted);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
 }
 
 TEST_F(ListDistinctTest, FloatingPointTestsWithInf)
@@ -125,7 +128,114 @@ TEST_F(ListDistinctTest, FloatingPointTestsWithNaNs)
   // NaNs are unequal.
   {
     auto const expected       = floats_lists{-2, -1, 0, 1, 2, NaN, NaN, NaN, NaN, NaN, NaN, NaN};
-    auto const results_sorted = distinct_sorted(input, NAN_UNEQUAL);
+    auto const results_sorted = distinct_sorted(input, NULL_EQUAL, NAN_UNEQUAL);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
+  }
+}
+
+TEST_F(ListDistinctTest, StringTestsNonNull)
+{
+  // Trivial cases - empty input.
+  {
+    auto const input          = strings_lists{{}};
+    auto const expected       = strings_lists{{}};
+    auto const results_sorted = distinct_sorted(input);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
+  }
+
+  // No duplicate.
+  {
+    auto const input          = strings_lists{"this", "is", "a", "string"};
+    auto const expected       = strings_lists{"a", "is", "string", "this"};
+    auto const results_sorted = distinct_sorted(input);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
+  }
+
+  // One list column.
+  {
+    auto const input          = strings_lists{"this", "is", "is", "is", "a", "string", "string"};
+    auto const expected       = strings_lists{"a", "is", "string", "this"};
+    auto const results_sorted = distinct_sorted(input);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
+  }
+
+  // Multiple lists column.
+  {
+    auto const input = strings_lists{
+      strings_lists{"this", "is", "a", "no duplicate", "string"},
+      strings_lists{"this", "is", "is", "a", "one duplicate", "string"},
+      strings_lists{"this", "is", "is", "is", "a", "two duplicates", "string"},
+      strings_lists{"this", "is", "is", "is", "is", "a", "three duplicates", "string"}};
+    auto const expected =
+      strings_lists{strings_lists{"a", "is", "no duplicate", "string", "this"},
+                    strings_lists{"a", "is", "one duplicate", "string", "this"},
+                    strings_lists{"a", "is", "string", "this", "two duplicates"},
+                    strings_lists{"a", "is", "string", "this", "three duplicates"}};
+    auto const results_sorted = distinct_sorted(input);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
+  }
+}
+
+TEST_F(ListDistinctTest, StringTestsWithNullsEqual)
+{
+  auto const null = std::string("");
+
+  // One list column with null entries.
+  {
+    auto const input = strings_lists{
+      {"this", null, "is", "is", "is", "a", null, "string", null, "string"}, nulls_at({1, 6, 8})};
+    auto const expected       = strings_lists{{null, "a", "is", "string", "this"}, null_at(0)};
+    auto const results_sorted = distinct_sorted(input);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
+  }
+
+  // Multiple lists column with null lists and null entries.
+  {
+    auto const input = strings_lists{
+      {strings_lists{{"this", null, "is", null, "a", null, "no duplicate", null, "string"},
+                     nulls_at({1, 3, 5, 7})},
+       strings_lists{}, /* NULL */
+       strings_lists{"this", "is", "is", "a", "one duplicate", "string"}},
+      null_at(1)};
+    auto const expected =
+      strings_lists{{strings_lists{{null, "a", "is", "no duplicate", "string", "this"}, null_at(0)},
+                     strings_lists{}, /* NULL */
+                     strings_lists{"a", "is", "one duplicate", "string", "this"}},
+                    null_at(1)};
+    auto const results_sorted = distinct_sorted(input);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
+  }
+}
+
+TEST_F(ListDistinctTest, StringTestsWithNullsUnequal)
+{
+  auto const null = std::string("");
+
+  // One list column with null entries.
+  {
+    auto const input = strings_lists{
+      {"this", null, "is", "is", "is", "a", null, "string", null, "string"}, nulls_at({1, 6, 8})};
+    auto const expected =
+      strings_lists{{null, null, null, "a", "is", "string", "this"}, nulls_at({0, 1, 2})};
+    auto const results_sorted = distinct_sorted(input, NULL_UNEQUAL);
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
+  }
+
+  // Multiple lists column with null lists and null entries.
+  {
+    auto const input = strings_lists{
+      {strings_lists{{"this", null, "is", null, "a", null, "no duplicate", null, "string"},
+                     nulls_at({1, 3, 5, 7})},
+       strings_lists{}, /* NULL */
+       strings_lists{"this", "is", "is", "a", "one duplicate", "string"}},
+      null_at(1)};
+    auto const expected = strings_lists{
+      {strings_lists{{null, null, null, null, "a", "is", "no duplicate", "string", "this"},
+                     nulls_at({0, 1, 2, 3})},
+       strings_lists{}, /* NULL */
+       strings_lists{"a", "is", "one duplicate", "string", "this"}},
+      null_at(1)};
+    auto const results_sorted = distinct_sorted(input, NULL_UNEQUAL);
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results_sorted);
   }
 }
