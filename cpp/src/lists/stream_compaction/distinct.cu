b1231a27445409e9d73e91d342cf895d58be4cf1
Nghia Truong <nghiatruong.vn@gmail.com>
2022-06-30 10:04:07 -0700

    Update default stream

--- cpp/src/lists/stream_compaction/distinct.cu
+++ cpp/src/lists/stream_compaction/distinct.cu
@@ -75,7 +75,7 @@ std::unique_ptr<column> distinct(lists_column_view const& input,
                                  rmm::mr::device_memory_resource* mr)
 {
   CUDF_FUNC_RANGE();
-  return detail::distinct(input, nulls_equal, nans_equal, rmm::cuda_stream_default, mr);
+  return detail::distinct(input, nulls_equal, nans_equal, cudf::default_stream_value, mr);
 }
 
 }  // namespace cudf::lists
