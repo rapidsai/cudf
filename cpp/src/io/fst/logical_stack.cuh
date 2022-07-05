c833b721ac8a19c635094c37cea7f0d4c43f6aaa
Elias Stehle <3958403+elstehle@users.noreply.github.com>
2022-07-05 10:01:50 -0700

    removes debug print and uses c++17 namespace style

--- cpp/src/io/fst/logical_stack.cuh
+++ cpp/src/io/fst/logical_stack.cuh
@@ -430,14 +430,6 @@ void sparse_stack_op_to_top_of_stack(StackSymbolItT d_symbols,
                                                num_symbols_in,
                                                stream));
 
-  // Dump info on stack operations: (stack level change + symbol) -> (absolute stack level + symbol)
-  test::print::print_array(num_symbols_in,
-                           stream,
-                           make_stack_level_iterator(stack_symbols_in),
-                           make_value_iterator(stack_symbols_in),
-                           make_stack_level_iterator(d_kv_operations.Current()),
-                           make_value_iterator(d_kv_operations.Current()));
-
   // Stable radix sort, sorting by stack level of the operations
   d_kv_operations_unsigned = cub::DoubleBuffer<StackOpUnsignedT>{
     reinterpret_cast<StackOpUnsignedT*>(d_kv_operations.Current()),
@@ -466,15 +458,6 @@ void sparse_stack_op_to_top_of_stack(StackSymbolItT d_symbols,
     num_symbols_in,
     stream));
 
-  // Dump info on stack operations sorted by their stack level (i.e. stack level after applying
-  // operation)
-  test::print::print_array(num_symbols_in,
-                           stream,
-                           make_stack_level_iterator(kv_ops_scan_in),
-                           make_value_iterator(kv_ops_scan_in),
-                           make_stack_level_iterator(kv_ops_scan_out),
-                           make_value_iterator(kv_ops_scan_out));
-
   // Fill the output tape with read-symbol
   thrust::fill(rmm::exec_policy(stream),
                thrust::device_ptr<StackSymbolT>{d_top_of_stack},
@@ -492,11 +475,6 @@ void sparse_stack_op_to_top_of_stack(StackSymbolItT d_symbols,
                   kv_op_to_stack_sym_it + num_symbols_in,
                   d_symbol_positions_db.Current(),
                   d_top_of_stack);
-
-  // Dump the output tape that has many yet-to-be-filled spots (i.e., all spots that were not given
-  // in the sparse representation)
-  test::print::print_array(
-    std::min(num_symbols_in, static_cast<decltype(num_symbols_in)>(10000)), stream, d_top_of_stack);
 
   // We perform an exclusive scan in order to fill the items at the very left that may
   // be reading the empty stack before there's the first push occurrence in the sequence.
