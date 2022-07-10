24b7d0f40cb550078861df7ff735ec02a1434219
Nghia Truong <nghiatruong.vn@gmail.com>
2022-07-10 13:19:26 -0700

    Fix style

--- python/cudf/cudf/core/column/lists.py
+++ python/cudf/cudf/core/column/lists.py
@@ -603,9 +603,7 @@ class ListMethods(ColumnMethods):
             raise NotImplementedError("Nested lists unique is not supported.")
 
         return self._return_or_inplace(
-            distinct(
-                self._column, nulls_equal=True, nans_all_equal=True
-            )
+            distinct(self._column, nulls_equal=True, nans_all_equal=True)
         )
 
     def sort_values(
