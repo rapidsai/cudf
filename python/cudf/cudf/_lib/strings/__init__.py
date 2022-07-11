002cb1c45bb04ab249765a307ecbe17569292b7b
Srikar Vanavasam <srikar.vanavasam@gmail.com>
2022-07-11 16:46:00 -0500

    Expose get_json_object_options to Python (#11180)
    This PR exposes `get_json_object_options` to the Python API. Addresses https://github.com/rapidsai/cudf/issues/10196
    
    Authors:
      - Srikar Vanavasam (https://github.com/SrikarVanavasam)
    
    Approvers:
      - Ashwin Srinath (https://github.com/shwina)
      - Paul Taylor (https://github.com/trxcllnt)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/cudf/pull/11180

--- python/cudf/cudf/_lib/strings/__init__.py
+++ python/cudf/cudf/_lib/strings/__init__.py
@@ -62,7 +62,7 @@ from cudf._lib.strings.find import (
     startswith_multiple,
 )
 from cudf._lib.strings.findall import findall, findall_record
-from cudf._lib.strings.json import get_json_object
+from cudf._lib.strings.json import get_json_object, GetJsonObjectOptions
 from cudf._lib.strings.padding import PadSide, center, ljust, pad, rjust, zfill
 from cudf._lib.strings.repeat import repeat_scalar, repeat_sequence
 from cudf._lib.strings.replace import (
