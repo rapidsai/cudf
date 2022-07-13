eee0bbb7fc6140fde50b395ceee2b413b9944d23
Nghia Truong <nghiatruong.vn@gmail.com>
2022-07-13 11:08:34 -0700

    Rewrite `collect_set_tests`

--- cpp/tests/groupby/collect_set_tests.cpp
+++ cpp/tests/groupby/collect_set_tests.cpp
@@ -32,12 +32,10 @@ namespace {
 
 constexpr cudf::test::debug_output_level verbosity{cudf::test::debug_output_level::FIRST_ERROR};
 
-#define COL_K    cudf::test::fixed_width_column_wrapper<int32_t, int32_t>
-#define COL_V    cudf::test::fixed_width_column_wrapper<TypeParam, int32_t>
-#define COL_S    cudf::test::strings_column_wrapper
-#define LCL_V    cudf::test::lists_column_wrapper<TypeParam, int32_t>
-#define LCL_S    cudf::test::lists_column_wrapper<cudf::string_view>
-#define VALIDITY std::initializer_list<bool>
+using keys_col      = cudf::test::fixed_width_column_wrapper<int32_t, int32_t>;
+using strings_col   = cudf::test::strings_column_wrapper;
+using strings_lists = cudf::test::lists_column_wrapper<cudf::string_view>;
+using validity_col  = std::initializer_list<bool>;
 
 auto groupby_collect_set(cudf::column_view const& keys,
                          cudf::column_view const& values,
@@ -92,12 +90,15 @@ TYPED_TEST_SUITE(CollectSetTypedTest, FixedWidthTypesNotBool);
 
 TYPED_TEST(CollectSetTypedTest, TrivialInput)
 {
+  using vals_col  = cudf::test::fixed_width_column_wrapper<TypeParam, int32_t>;
+  using lists_col = cudf::test::lists_column_wrapper<TypeParam, int32_t>;
+
   // Empty input
   {
-    COL_K keys{};
-    COL_V vals{};
-    COL_K keys_expected{};
-    LCL_V vals_expected{};
+    keys_col keys{};
+    vals_col vals{};
+    keys_col keys_expected{};
+    lists_col vals_expected{};
 
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys, vals, CollectSetTest::collect_set());
@@ -107,10 +108,10 @@ TYPED_TEST(CollectSetTypedTest, TrivialInput)
 
   // Single key input
   {
-    COL_K keys{1};
-    COL_V vals{10};
-    COL_K keys_expected{1};
-    LCL_V vals_expected{LCL_V{10}};
+    keys_col keys{1};
+    vals_col vals{10};
+    keys_col keys_expected{1};
+    lists_col vals_expected{lists_col{10}};
 
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys, vals, CollectSetTest::collect_set());
@@ -120,10 +121,10 @@ TYPED_TEST(CollectSetTypedTest, TrivialInput)
 
   // Non-repeated keys
   {
-    COL_K keys{2, 1};
-    COL_V vals{20, 10};
-    COL_K keys_expected{1, 2};
-    LCL_V vals_expected{LCL_V{10}, LCL_V{20}};
+    keys_col keys{2, 1};
+    vals_col vals{20, 10};
+    keys_col keys_expected{1, 2};
+    lists_col vals_expected{lists_col{10}, lists_col{20}};
 
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys, vals, CollectSetTest::collect_set());
@@ -134,12 +135,15 @@ TYPED_TEST(CollectSetTypedTest, TrivialInput)
 
 TYPED_TEST(CollectSetTypedTest, TypicalInput)
 {
+  using vals_col  = cudf::test::fixed_width_column_wrapper<TypeParam, int32_t>;
+  using lists_col = cudf::test::lists_column_wrapper<TypeParam, int32_t>;
+
   // Pre-sorted keys
   {
-    COL_K keys{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
-    COL_V vals{10, 11, 10, 10, 20, 21, 21, 20, 30, 33, 32, 31};
-    COL_K keys_expected{1, 2, 3};
-    LCL_V vals_expected{{10, 11}, {20, 21}, {30, 31, 32, 33}};
+    keys_col keys{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+    vals_col vals{10, 11, 10, 10, 20, 21, 21, 20, 30, 33, 32, 31};
+    keys_col keys_expected{1, 2, 3};
+    lists_col vals_expected{{10, 11}, {20, 21}, {30, 31, 32, 33}};
 
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys, vals, CollectSetTest::collect_set());
@@ -149,10 +153,10 @@ TYPED_TEST(CollectSetTypedTest, TypicalInput)
 
   // Expect the result keys to be sorted by sort-based groupby
   {
-    COL_K keys{4, 1, 2, 4, 3, 3, 2, 1};
-    COL_V vals{40, 10, 20, 40, 30, 30, 20, 11};
-    COL_K keys_expected{1, 2, 3, 4};
-    LCL_V vals_expected{{10, 11}, {20}, {30}, {40}};
+    keys_col keys{4, 1, 2, 4, 3, 3, 2, 1};
+    vals_col vals{40, 10, 20, 40, 30, 30, 20, 11};
+    keys_col keys_expected{1, 2, 3, 4};
+    lists_col vals_expected{{10, 11}, {20}, {30}, {40}};
 
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys, vals, CollectSetTest::collect_set());
@@ -164,13 +168,16 @@ TYPED_TEST(CollectSetTypedTest, TypicalInput)
 // Keys and values columns are sliced columns
 TYPED_TEST(CollectSetTypedTest, SlicedColumnsInput)
 {
-  COL_K keys_original{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
-  COL_V vals_original{10, 11, 10, 10, 20, 21, 21, 20, 30, 33, 32, 31};
+  using vals_col  = cudf::test::fixed_width_column_wrapper<TypeParam, int32_t>;
+  using lists_col = cudf::test::lists_column_wrapper<TypeParam, int32_t>;
+
+  keys_col keys_original{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+  vals_col vals_original{10, 11, 10, 10, 20, 21, 21, 20, 30, 33, 32, 31};
   {
     auto const keys          = cudf::slice(keys_original, {0, 4})[0];  // { 1, 1, 1, 1 }
     auto const vals          = cudf::slice(vals_original, {0, 4})[0];  // { 10, 11, 10, 10 }
-    auto const keys_expected = COL_K{1};
-    auto const vals_expected = LCL_V{{10, 11}};
+    auto const keys_expected = keys_col{1};
+    auto const vals_expected = lists_col{{10, 11}};
 
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys, vals, CollectSetTest::collect_set());
@@ -180,8 +187,8 @@ TYPED_TEST(CollectSetTypedTest, SlicedColumnsInput)
   {
     auto const keys = cudf::slice(keys_original, {2, 10})[0];  // { 1, 1, 2, 2, 2, 2, 3, 3 }
     auto const vals = cudf::slice(vals_original, {2, 10})[0];  // { 10, 10, 20, 21, 21, 20, 30, 33 }
-    auto const keys_expected = COL_K{1, 2, 3};
-    auto const vals_expected = LCL_V{{10}, {20, 21}, {30, 33}};
+    auto const keys_expected = keys_col{1, 2, 3};
+    auto const vals_expected = lists_col{{10}, {20, 21}, {30, 33}};
 
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys, vals, CollectSetTest::collect_set());
@@ -192,8 +199,8 @@ TYPED_TEST(CollectSetTypedTest, SlicedColumnsInput)
 
 TEST_F(CollectSetTest, StringInput)
 {
-  COL_K keys{1, 2, 3, 3, 2, 1, 2, 1, 2, 1, 1, 1, 1};
-  COL_S vals{
+  keys_col keys{1, 2, 3, 3, 2, 1, 2, 1, 2, 1, 1, 1, 1};
+  strings_col vals{
     "String 1, first",
     "String 2, first",
     "String 3, first",
@@ -208,10 +215,10 @@ TEST_F(CollectSetTest, StringInput)
     "String 1, second",  // repeated
     "String 1, second"   // repeated
   };
-  COL_K keys_expected{1, 2, 3};
-  LCL_S vals_expected{{"String 1, first", "String 1, second"},
-                      {"String 2, first", "String 2, second"},
-                      {"String 3, first", "String 3, second"}};
+  keys_col keys_expected{1, 2, 3};
+  strings_lists vals_expected{{"String 1, first", "String 1, second"},
+                              {"String 2, first", "String 2, second"},
+                              {"String 3, first", "String 3, second"}};
 
   auto const [out_keys, out_lists] = groupby_collect_set(keys, vals, CollectSetTest::collect_set());
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(keys_expected, *out_keys, verbosity);
@@ -220,17 +227,17 @@ TEST_F(CollectSetTest, StringInput)
 
 TEST_F(CollectSetTest, FloatsWithNaN)
 {
-  COL_K keys{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+  keys_col keys{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
   cudf::test::fixed_width_column_wrapper<float> vals{
     {1.0f, 1.0f, -2.3e-5f, -2.3e-5f, 2.3e5f, 2.3e5f, -NAN, -NAN, NAN, NAN, 0.0f, 0.0f},
     {true, true, true, true, true, true, true, true, true, true, false, false}};
-  COL_K keys_expected{1};
+  keys_col keys_expected{1};
   cudf::test::lists_column_wrapper<float> vals_expected;
 
   // null equal with nan unequal
   {
     vals_expected = {{{-2.3e-5f, 1.0f, 2.3e5f, -NAN, -NAN, NAN, NAN, 0.0f},
-                      VALIDITY{true, true, true, true, true, true, true, false}}};
+                      validity_col{true, true, true, true, true, true, true, false}}};
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys, vals, CollectSetTest::collect_set());
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(keys_expected, *out_keys, verbosity);
@@ -240,7 +247,7 @@ TEST_F(CollectSetTest, FloatsWithNaN)
   // null unequal with nan unequal
   {
     vals_expected = {{{-2.3e-5f, 1.0f, 2.3e5f, -NAN, -NAN, NAN, NAN, 0.0f, 0.0f},
-                      VALIDITY{true, true, true, true, true, true, true, false, false}}};
+                      validity_col{true, true, true, true, true, true, true, false, false}}};
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys, vals, CollectSetTest::collect_set_null_unequal());
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(keys_expected, *out_keys, verbosity);
@@ -259,7 +266,7 @@ TEST_F(CollectSetTest, FloatsWithNaN)
   // null equal with nan equal
   {
     vals_expected = {
-      {{-2.3e-5f, 1.0f, 2.3e5f, NAN, 0.0f}, VALIDITY{true, true, true, true, false}}};
+      {{-2.3e-5f, 1.0f, 2.3e5f, NAN, 0.0f}, validity_col{true, true, true, true, false}}};
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys,
                           vals,
@@ -271,8 +278,8 @@ TEST_F(CollectSetTest, FloatsWithNaN)
 
   // null unequal with nan equal
   {
-    vals_expected = {
-      {{-2.3e-5f, 1.0f, 2.3e5f, -NAN, 0.0f, 0.0f}, VALIDITY{true, true, true, true, false, false}}};
+    vals_expected = {{{-2.3e-5f, 1.0f, 2.3e5f, -NAN, 0.0f, 0.0f},
+                      validity_col{true, true, true, true, false, false}}};
     auto const [out_keys, out_lists] =
       groupby_collect_set(keys,
                           vals,
@@ -285,23 +292,26 @@ TEST_F(CollectSetTest, FloatsWithNaN)
 
 TYPED_TEST(CollectSetTypedTest, CollectWithNulls)
 {
+  using vals_col  = cudf::test::fixed_width_column_wrapper<TypeParam, int32_t>;
+  using lists_col = cudf::test::lists_column_wrapper<TypeParam, int32_t>;
+
   // Just use an arbitrary value to store null entries
   // Using this alias variable will make the code look cleaner
   constexpr int32_t null = 0;
 
   // Pre-sorted keys
   {
-    COL_K keys{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
-    COL_V vals{{10, 10, null, null, 20, null, null, null, 30, 31, 30, 31},
-               {true, true, false, false, true, false, false, false, true, true, true, true}};
-    COL_K keys_expected{1, 2, 3};
-    LCL_V vals_expected;
+    keys_col keys{1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+    vals_col vals{{10, 10, null, null, 20, null, null, null, 30, 31, 30, 31},
+                  {true, true, false, false, true, false, false, false, true, true, true, true}};
+    keys_col keys_expected{1, 2, 3};
+    lists_col vals_expected;
 
     // By default, nulls are consider equals, thus only one null is kept per key
     {
-      vals_expected = {{{10, null}, VALIDITY{true, false}},
-                       {{20, null}, VALIDITY{true, false}},
-                       {{30, 31}, VALIDITY{true, true}}};
+      vals_expected = {{{10, null}, validity_col{true, false}},
+                       {{20, null}, validity_col{true, false}},
+                       {{30, 31}, validity_col{true, true}}};
       auto const [out_keys, out_lists] =
         groupby_collect_set(keys, vals, CollectSetTest::collect_set());
       CUDF_TEST_EXPECT_COLUMNS_EQUAL(keys_expected, *out_keys, verbosity);
@@ -310,9 +320,9 @@ TYPED_TEST(CollectSetTypedTest, CollectWithNulls)
 
     // All nulls per key are kept (nulls are put at the end of each list)
     {
-      vals_expected = LCL_V{{{10, null, null}, VALIDITY{true, false, false}},
-                            {{20, null, null, null}, VALIDITY{true, false, false, false}},
-                            {{30, 31}, VALIDITY{true, true}}};
+      vals_expected = lists_col{{{10, null, null}, validity_col{true, false, false}},
+                                {{20, null, null, null}, validity_col{true, false, false, false}},
+                                {{30, 31}, validity_col{true, true}}};
       auto const [out_keys, out_lists] =
         groupby_collect_set(keys, vals, CollectSetTest::collect_set_null_unequal());
       CUDF_TEST_EXPECT_COLUMNS_EQUAL(keys_expected, *out_keys, verbosity);
@@ -321,7 +331,7 @@ TYPED_TEST(CollectSetTypedTest, CollectWithNulls)
 
     // All nulls per key are excluded
     {
-      vals_expected = LCL_V{{10}, {20}, {30, 31}};
+      vals_expected = lists_col{{10}, {20}, {30, 31}};
       auto const [out_keys, out_lists] =
         groupby_collect_set(keys, vals, CollectSetTest::collect_set_null_exclude());
       CUDF_TEST_EXPECT_COLUMNS_EQUAL(keys_expected, *out_keys, verbosity);
@@ -331,18 +341,18 @@ TYPED_TEST(CollectSetTypedTest, CollectWithNulls)
 
   // Expect the result keys to be sorted by sort-based groupby
   {
-    COL_K keys{4, 1, 2, 4, 3, 3, 3, 3, 2, 1};
-    COL_V vals{{40, 10, 20, 40, null, null, null, null, 21, null},
-               {true, true, true, true, false, false, false, false, true, false}};
-    COL_K keys_expected{1, 2, 3, 4};
-    LCL_V vals_expected;
+    keys_col keys{4, 1, 2, 4, 3, 3, 3, 3, 2, 1};
+    vals_col vals{{40, 10, 20, 40, null, null, null, null, 21, null},
+                  {true, true, true, true, false, false, false, false, true, false}};
+    keys_col keys_expected{1, 2, 3, 4};
+    lists_col vals_expected;
 
     // By default, nulls are consider equals, thus only one null is kept per key
     {
-      vals_expected = {{{10, null}, VALIDITY{true, false}},
-                       {{20, 21}, VALIDITY{true, true}},
-                       {{null}, VALIDITY{false}},
-                       {{40}, VALIDITY{true}}};
+      vals_expected = {{{10, null}, validity_col{true, false}},
+                       {{20, 21}, validity_col{true, true}},
+                       {{null}, validity_col{false}},
+                       {{40}, validity_col{true}}};
       auto const [out_keys, out_lists] =
         groupby_collect_set(keys, vals, CollectSetTest::collect_set());
       CUDF_TEST_EXPECT_COLUMNS_EQUAL(keys_expected, *out_keys, verbosity);
@@ -351,10 +361,11 @@ TYPED_TEST(CollectSetTypedTest, CollectWithNulls)
 
     // All nulls per key are kept (nulls are put at the end of each list)
     {
-      vals_expected = LCL_V{{{10, null}, VALIDITY{true, false}},
-                            {{20, 21}, VALIDITY{true, true}},
-                            {{null, null, null, null}, VALIDITY{false, false, false, false}},
-                            {{40}, VALIDITY{true}}};
+      vals_expected =
+        lists_col{{{10, null}, validity_col{true, false}},
+                  {{20, 21}, validity_col{true, true}},
+                  {{null, null, null, null}, validity_col{false, false, false, false}},
+                  {{40}, validity_col{true}}};
       auto const [out_keys, out_lists] =
         groupby_collect_set(keys, vals, CollectSetTest::collect_set_null_unequal());
       CUDF_TEST_EXPECT_COLUMNS_EQUAL(keys_expected, *out_keys, verbosity);
@@ -363,7 +374,7 @@ TYPED_TEST(CollectSetTypedTest, CollectWithNulls)
 
     // All nulls per key are excluded
     {
-      vals_expected = LCL_V{{10}, {20, 21}, {}, {40}};
+      vals_expected = lists_col{{10}, {20, 21}, {}, {40}};
       auto const [out_keys, out_lists] =
         groupby_collect_set(keys, vals, CollectSetTest::collect_set_null_exclude());
       CUDF_TEST_EXPECT_COLUMNS_EQUAL(keys_expected, *out_keys, verbosity);
