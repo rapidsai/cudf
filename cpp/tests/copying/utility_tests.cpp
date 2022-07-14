5508fe203a992b47c20fdaae46d138fc7aea4325
Dave Baranec <dbaranec@nvidia.com>
2022-07-14 16:47:54 -0500

    Use a custom set of checks for verifying properties when using allocate_like() and empty_like().

--- cpp/tests/copying/utility_tests.cpp
+++ cpp/tests/copying/utility_tests.cpp
@@ -18,6 +18,7 @@
 #include <cudf/copying.hpp>
 #include <cudf/strings/detail/utilities.hpp>
 #include <cudf/table/table.hpp>
+#include <cudf/utilities/type_checks.hpp>
 #include <cudf/utilities/type_dispatcher.hpp>
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_utilities.hpp>
@@ -168,11 +169,26 @@ std::unique_ptr<cudf::table> create_table(cudf::size_type size, cudf::mask_state
   return std::make_unique<cudf::table>(std::move(columns));
 }
 
-void expect_tables_prop_equal(cudf::table_view lhs, cudf::table_view rhs)
+// The tests that call into here are expecting deliberately mismatched null masks to
+// not matter. However, cudf::expect_column_properties_equal() includes null count so
+// we do a less strenuous test here.
+//
+// NOTE: this doesn't handle nested types.
+//
+void expect_allocate_like_results_equal(cudf::column_view const& lhs, cudf::column_view const& rhs)
+{
+  EXPECT_EQ(cudf::column_types_equal(lhs, rhs), true);
+  EXPECT_EQ(lhs.size(), rhs.size());
+  EXPECT_EQ(lhs.nullable(), rhs.nullable());
+  EXPECT_EQ(lhs.num_children(), rhs.num_children());
+}
+
+void expect_allocate_like_results_equal(cudf::table_view const& lhs, cudf::table_view const& rhs)
 {
   EXPECT_EQ(lhs.num_columns(), rhs.num_columns());
-  for (cudf::size_type index = 0; index < lhs.num_columns(); index++)
-    CUDF_TEST_EXPECT_COLUMN_PROPERTIES_EQUAL(lhs.column(index), rhs.column(index));
+  for (cudf::size_type index = 0; index < lhs.num_columns(); index++) {
+    expect_allocate_like_results_equal(lhs.column(index), rhs.column(index));
+  }
 }
 
 struct EmptyLikeTableTest : public cudf::test::BaseFixture {
@@ -186,13 +202,12 @@ TEST_F(EmptyLikeTableTest, TableTest)
   auto expected          = create_table(0, cudf::mask_state::UNINITIALIZED);
   auto got               = cudf::empty_like(input->view());
 
-  expect_tables_prop_equal(got->view(), expected->view());
+  expect_allocate_like_results_equal(got->view(), expected->view());
 }
 
 template <typename T>
 struct AllocateLikeTest : public cudf::test::BaseFixture {
 };
-;
 
 TYPED_TEST_SUITE(AllocateLikeTest, numeric_types);
 
@@ -205,7 +220,7 @@ TYPED_TEST(AllocateLikeTest, ColumnNumericTestSameSize)
   auto expected = make_numeric_column(
     cudf::data_type{cudf::type_to_id<TypeParam>()}, size, cudf::mask_state::UNINITIALIZED);
   auto got = cudf::allocate_like(input->view());
-  CUDF_TEST_EXPECT_COLUMN_PROPERTIES_EQUAL(*expected, *got);
+  expect_allocate_like_results_equal(*expected, *got);
 }
 
 TYPED_TEST(AllocateLikeTest, ColumnNumericTestSpecifiedSize)
@@ -219,7 +234,7 @@ TYPED_TEST(AllocateLikeTest, ColumnNumericTestSpecifiedSize)
                                       specified_size,
                                       cudf::mask_state::UNINITIALIZED);
   auto got      = cudf::allocate_like(input->view(), specified_size);
-  CUDF_TEST_EXPECT_COLUMN_PROPERTIES_EQUAL(*expected, *got);
+  expect_allocate_like_results_equal(*expected, *got);
 }
 
 CUDF_TEST_PROGRAM_MAIN()
