f496f8da896c9e5193514d45e3a08bb1b433cde8
Jim Brennan <jimb@nvidia.com>
2022-07-21 17:08:05 -0500

    minor fixes from review

--- cpp/src/io/orc/reader_impl.cu
+++ cpp/src/io/orc/reader_impl.cu
@@ -309,7 +309,7 @@ rmm::device_buffer reader::impl::decompress_stripe_data(
   }
   compinfo.host_to_device(stream);
 
-  // Workaround for ZSTD.  It is possible to have compression ratios > 2048:1,
+  // Workaround for ZSTD. It is possible to have compression ratios > 2048:1,
   // so the heuristic in gpuParseCompressedStripeData() to estimate the size for
   // small blocks can be too low. Disable the estimation for ZSTD.
   auto allow_block_size_estimate = (decompressor.compression() != compression_type::ZSTD);
