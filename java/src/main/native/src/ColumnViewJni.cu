638e52c2a7f263288bd5c06f17ea6416521edd1c
Nghia Truong <nghiatruong.vn@gmail.com>
2022-07-26 13:21:37 -0700

    Fix typo

--- java/src/main/native/src/ColumnViewJni.cu
+++ java/src/main/native/src/ColumnViewJni.cu
@@ -169,51 +169,51 @@ void post_process_list_overlap(cudf::column_view const &lhs, cudf::column_view c
       overlap_result->set_null_mask(std::move(new_null_mask), new_null_count);
     }
   }
+}
 
-  std::unique_ptr<cudf::column> lists_distinct_by_key(cudf::lists_column_view const &input,
-                                                      rmm::cuda_stream_view stream) {
-    if (input.is_empty()) {
-      return empty_like(input.parent());
-    }
-
-    auto const child = input.get_sliced_child(stream);
-
-    // Genereate labels for the input list elements.
-    auto labels = rmm::device_uvector<cudf::size_type>(child.size(), stream);
-    cudf::detail::label_segments(input.offsets_begin(), input.offsets_end(), labels.begin(),
-                                 labels.end(), stream);
-
-    // Use `cudf::duplicate_keep_option::KEEP_LAST` so this will produce the desired behavior when
-    // being called in `create_map` in spark-rapids.
-    // Other options comparing nulls and NaNs are set as all-equal.
-    auto out_columns =
-        cudf::detail::stable_distinct(
-            table_view{{column_view{cudf::device_span<cudf::size_type const>{labels}},
-                        child.child(0), child.child(1)}}, // input table
-            std::vector<size_type>{0, 1},                 // key columns
-            cudf::duplicate_keep_option::KEEP_LAST, cudf::null_equality::EQUAL,
-            cudf::nan_equality::ALL_EQUAL, stream)
-            ->release();
-    auto const out_labels = out_columns.front()->view();
-
-    // Assemble a structs column of <out_keys, out_vals>.
-    auto out_structs_members = std::vector<std::unique_ptr<cudf::column>>();
-    out_structs_members.emplace_back(std::move(out_columns[1]));
-    out_structs_members.emplace_back(std::move(out_columns[2]));
-    auto out_structs =
-        cudf::make_structs_column(out_labels.size(), std::move(out_structs_members), 0, {});
-
-    // Assemble a lists column of structs<out_keys, out_vals>.
-    auto out_offsets = make_numeric_column(data_type{type_to_id<offset_type>()}, input.size() + 1,
-                                           mask_state::UNALLOCATED, stream);
-    auto const offsets_begin = out_offsets->mutable_view().template begin<offset_type>();
-    auto const labels_begin = out_labels.template begin<offset_type>();
-    cudf::detail::labels_to_offsets(labels_begin, labels_begin + out_labels.size(), offsets_begin,
-                                    offsets_begin + out_offsets->size(), stream);
-
-    return cudf::make_lists_column(input.size(), std::move(out_offsets), std::move(out_structs),
-                                   input.null_count(),
-                                   cudf::detail::copy_bitmask(input.parent(), stream), stream);
+std::unique_ptr<cudf::column> lists_distinct_by_key(cudf::lists_column_view const &input,
+                                                    rmm::cuda_stream_view stream) {
+  if (input.is_empty()) {
+    return empty_like(input.parent());
   }
+
+  auto const child = input.get_sliced_child(stream);
+
+  // Genereate labels for the input list elements.
+  auto labels = rmm::device_uvector<cudf::size_type>(child.size(), stream);
+  cudf::detail::label_segments(input.offsets_begin(), input.offsets_end(), labels.begin(),
+                               labels.end(), stream);
+
+  // Use `cudf::duplicate_keep_option::KEEP_LAST` so this will produce the desired behavior when
+  // being called in `create_map` in spark-rapids.
+  // Other options comparing nulls and NaNs are set as all-equal.
+  auto out_columns = cudf::detail::stable_distinct(
+                         table_view{{column_view{cudf::device_span<cudf::size_type const>{labels}},
+                                     child.child(0), child.child(1)}}, // input table
+                         std::vector<size_type>{0, 1},                 // key columns
+                         cudf::duplicate_keep_option::KEEP_LAST, cudf::null_equality::EQUAL,
+                         cudf::nan_equality::ALL_EQUAL, stream)
+                         ->release();
+  auto const out_labels = out_columns.front()->view();
+
+  // Assemble a structs column of <out_keys, out_vals>.
+  auto out_structs_members = std::vector<std::unique_ptr<cudf::column>>();
+  out_structs_members.emplace_back(std::move(out_columns[1]));
+  out_structs_members.emplace_back(std::move(out_columns[2]));
+  auto out_structs =
+      cudf::make_structs_column(out_labels.size(), std::move(out_structs_members), 0, {});
+
+  // Assemble a lists column of structs<out_keys, out_vals>.
+  auto out_offsets = make_numeric_column(data_type{type_to_id<offset_type>()}, input.size() + 1,
+                                         mask_state::UNALLOCATED, stream);
+  auto const offsets_begin = out_offsets->mutable_view().template begin<offset_type>();
+  auto const labels_begin = out_labels.template begin<offset_type>();
+  cudf::detail::labels_to_offsets(labels_begin, labels_begin + out_labels.size(), offsets_begin,
+                                  offsets_begin + out_offsets->size(), stream);
+
+  return cudf::make_lists_column(input.size(), std::move(out_offsets), std::move(out_structs),
+                                 input.null_count(),
+                                 cudf::detail::copy_bitmask(input.parent(), stream), stream);
+}
 
 } // namespace cudf::jni
