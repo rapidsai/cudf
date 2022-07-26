d6a8ce238935c1db7be46f59268f041d624d054a
Christopher Harris <xixonia@gmail.com>
2022-07-25 21:30:00 -0500

    use multiple small get_next_chunk calls instead of one large call

--- cpp/src/io/text/multibyte_split.cu
+++ cpp/src/io/text/multibyte_split.cu
@@ -418,12 +418,16 @@ std::unique_ptr<cudf::column> multibyte_split(cudf::io::text::data_chunk_source 
   auto reader = source.create_reader();
   reader->skip_bytes(relevant_offset_first);
 
-  auto relevant_bytes = reader->get_next_chunk(string_chars_size, stream);
+  for (int32_t i = 0; i < string_chars_size; i += ITEMS_PER_CHUNK)
+  {
+      auto read_size = std::min(static_cast<int64_t>(ITEMS_PER_CHUNK), string_chars_size - i);
+      auto relevant_bytes = reader->get_next_chunk(read_size, stream);
 
-  thrust::copy(rmm::exec_policy(stream),
-               relevant_bytes->data(),  //
-               relevant_bytes->data() + relevant_bytes->size(),
-               string_chars.begin());
+      thrust::copy(rmm::exec_policy(stream),
+          relevant_bytes->data(),  //
+          relevant_bytes->data() + relevant_bytes->size(),
+          string_chars.begin() + i);
+  }
 
   auto string_count = string_offsets_out.size() - 1;
 
