eeaed29d1196bebdbb1d36367cc212d7e3d2285b
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2022-07-27 16:28:43 +0530

    style fix clang-format

--- cpp/src/io/json/nested_json_gpu.cu
+++ cpp/src/io/json/nested_json_gpu.cu
@@ -223,7 +223,6 @@ const std::vector<std::vector<char>> pda_sgids{
   {0},  {1},  {2},  {3},  {4},  {5},  {6},  {7},  {8},  {9},  {10}, {11}, {12}, {13}, {14},
   {15}, {16}, {17}, {18}, {19}, {20}, {21}, {22}, {23}, {24}, {25}, {26}, {27}, {28}, {29}};
 
-
 // TODO: make these tables as static __constant__ or constexpr std::array
 /**
  * @brief Getting the transition table
