5c7c3d5b9bf8d1ffb3310bf4b792a293cd78a6d2
Michael Wang <isVoid@users.noreply.github.com>
2022-07-28 15:13:52 -0700

    Add mixed numpy ints as test case

--- python/cudf/cudf/tests/test_series.py
+++ python/cudf/cudf/tests/test_series.py
@@ -1911,7 +1911,14 @@ def test_series_between_with_null(data, left, right, inclusive):
     assert_eq(expected, actual.to_pandas(nullable=True))
 
 
-@pytest.mark.parametrize("data", [[0, 1, 2, 3, 4], range(5)])
+def test_default_construction():
+    s = cudf.Series([np.int8(8), np.int16(128)])
+    assert s.dtype == np.dtype("i2")
+
+
+@pytest.mark.parametrize(
+    "data", [[0, 1, 2, 3, 4], range(5), [np.int8(8), np.int16(128)]]
+)
 def test_default_integer_bitwidth_construction(default_integer_bitwidth, data):
     s = cudf.Series(data)
     assert s.dtype == np.dtype(f"i{default_integer_bitwidth//8}")
