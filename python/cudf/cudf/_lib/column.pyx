c11b780d6e09d6e6ad539c5a1c14d14393737c9d
Ashwin Srinath <3190405+shwina@users.noreply.github.com>
2022-07-28 08:34:25 -0400

    Fix constructing Column from column_view with expired mask (#11354)
    This PR fixes a subtle bug leading to a segfault when constructing a `Column` from a `column_view`.
    
    Closes #11349
    
    Authors:
      - Ashwin Srinath (https://github.com/shwina)
    
    Approvers:
      - https://github.com/brandon-b-miller
      - Vyas Ramasubramani (https://github.com/vyasr)
      - Matthew Roeschke (https://github.com/mroeschke)
      - Lawrence Mitchell (https://github.com/wence-)
    
    URL: https://github.com/rapidsai/cudf/pull/11354

--- python/cudf/cudf/_lib/column.pyx
+++ python/cudf/cudf/_lib/column.pyx
@@ -503,9 +503,12 @@ cdef class Column:
         data = None
         base_size = size + offset
         data_owner = owner
+
         if column_owner:
             data_owner = owner.base_data
+            mask_owner = mask_owner.base_mask
             base_size = owner.base_size
+
         if data_ptr:
             if data_owner is None:
                 data = Buffer(
@@ -523,18 +526,38 @@ cdef class Column:
                 rmm.DeviceBuffer(ptr=data_ptr, size=0)
             )
 
-        mask_ptr = <uintptr_t>(cv.null_mask())
         mask = None
+        mask_ptr = <uintptr_t>(cv.null_mask())
         if mask_ptr:
-            if column_owner:
-                mask_owner = mask_owner.base_mask
             if mask_owner is None:
-                mask = Buffer(
-                    rmm.DeviceBuffer(
-                        ptr=mask_ptr,
-                        size=bitmask_allocation_size_bytes(size+offset)
+                if column_owner:
+                    # if we reached here, it means `owner` is a `Column`
+                    # that does not have a null mask, but `cv` thinks it
+                    # should have a null mask. This can happen in the
+                    # following sequence of events:
+                    #
+                    # 1) `cv` is constructed as a view into a
+                    #    `cudf::column` that is nullable (i.e., it has
+                    #    a null mask), but contains no nulls.
+                    # 2) `owner`, a `Column`, is constructed from the
+                    #    same `cudf::column`. Because `cudf::column`
+                    #    is memory owning, `owner` takes ownership of
+                    #    the memory owned by the
+                    #    `cudf::column`. Because the column has a null
+                    #    count of 0, it may choose to discard the null
+                    #    mask.
+                    # 3) Now, `cv` points to a discarded null mask.
+                    #
+                    # TL;DR: we should not include a null mask in the
+                    # result:
+                    mask = None
+                else:
+                    mask = Buffer(
+                        rmm.DeviceBuffer(
+                            ptr=mask_ptr,
+                            size=bitmask_allocation_size_bytes(size+offset)
+                        )
                     )
-                )
             else:
                 mask = Buffer(
                     data=mask_ptr,
