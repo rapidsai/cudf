0f860ea11b35e5dc79fd0e0f6bb7bc526dce209a
Mike Wilson <hyperbolic2346@users.noreply.github.com>
2022-07-27 21:43:18 -0400

    Adding byte_array statistics (#11303)
    This PR is needed for PR #11160 and another PR for writing list<int8>'s as byte arrays as well.
    
    This PR will be the basis for the statistics written by #11160, but it should be noted that it will work due to an implementation detail. The union of the statistics types for strings and byte arrays is the same layout. This means the string statistics that are stored from the string column can be read as byte array statistics later when writing out the file and it will work simply because they can alias to each other.
    
    Alternatives considered included changing the incoming table type in the case of strings being written as a byte array to be a byte array, which would only work because of the implementation detail that the columns are identical data layouts. Also considered was templating out multiple paths in [column_statistics.cuh:107](https://github.com/rapidsai/cudf/blob/e98feab966f6d1b9eba83323e851c955a1691865/cpp/src/io/statistics/column_statistics.cuh#L107) that we could use to branch based on the data type.
    
    The latter seems more the correct way, but comes with a performance and complexity issue. The method used in the PR was mainly chosen due to simplicity and the existing reliance on the data formats being identical throughout the code. It seems unlikely these will ever decouple.
    
    Authors:
      - Mike Wilson (https://github.com/hyperbolic2346)
    
    Approvers:
      - Vukasin Milovanovic (https://github.com/vuule)
      - MithunR (https://github.com/mythrocks)
      - https://github.com/nvdbaranec
    
    URL: https://github.com/rapidsai/cudf/pull/11303

--- cpp/src/io/parquet/page_enc.cu
+++ cpp/src/io/parquet/page_enc.cu
@@ -117,6 +117,7 @@ __global__ void __launch_bounds__(block_size)
   frag_init_state_s* const s = &state_g;
   uint32_t t                 = threadIdx.x;
   int frag_y                 = blockIdx.y;
+  auto const physical_type   = col_desc[blockIdx.x].physical_type;
 
   if (t == 0) s->col = col_desc[blockIdx.x];
   __syncthreads();
@@ -135,9 +136,8 @@ __global__ void __launch_bounds__(block_size)
     s->frag.fragment_data_size = 0;
     s->frag.dict_data_size     = 0;
 
-    auto col                = *(s->col.parent_column);
-    s->frag.start_value_idx = row_to_value_idx(s->frag.start_row, col);
-    size_type end_value_idx = row_to_value_idx(s->frag.start_row + s->frag.num_rows, col);
+    s->frag.start_value_idx = row_to_value_idx(s->frag.start_row, s->col);
+    size_type end_value_idx = row_to_value_idx(s->frag.start_row + s->frag.num_rows, s->col);
     s->frag.num_leaf_values = end_value_idx - s->frag.start_value_idx;
 
     if (s->col.level_offsets != nullptr) {
@@ -151,8 +151,8 @@ __global__ void __launch_bounds__(block_size)
       s->frag.num_values = s->frag.num_rows;
     }
   }
-  auto const physical_type = s->col.physical_type;
-  auto const dtype_len     = physical_type_len(physical_type, s->col.leaf_column->type().id());
+  auto const leaf_type = s->col.leaf_column->type().id();
+  auto const dtype_len = physical_type_len(physical_type, leaf_type);
   __syncthreads();
 
   size_type nvals           = s->frag.num_leaf_values;
@@ -301,9 +301,14 @@ __global__ void __launch_bounds__(128)
       __syncwarp();
       if (num_rows < ck_g.num_rows) {
         if (t == 0) { frag_g = ck_g.fragments[fragments_in_chunk]; }
-        if (!t && ck_g.stats && col_g.stats_dtype == dtype_string) {
-          minmax_len = max(ck_g.stats[fragments_in_chunk].min_value.str_val.length,
-                           ck_g.stats[fragments_in_chunk].max_value.str_val.length);
+        if (!t && ck_g.stats) {
+          if (col_g.stats_dtype == dtype_string) {
+            minmax_len = max(ck_g.stats[fragments_in_chunk].min_value.str_val.length,
+                             ck_g.stats[fragments_in_chunk].max_value.str_val.length);
+          } else if (col_g.stats_dtype == dtype_byte_array) {
+            minmax_len = max(ck_g.stats[fragments_in_chunk].min_value.byte_val.length,
+                             ck_g.stats[fragments_in_chunk].max_value.byte_val.length);
+          }
         }
       } else if (!t) {
         frag_g.fragment_data_size = 0;
@@ -338,7 +343,7 @@ __global__ void __launch_bounds__(128)
           page_g.max_hdr_size  = 32;  // Max size excluding statistics
           if (ck_g.stats) {
             uint32_t stats_hdr_len = 16;
-            if (col_g.stats_dtype == dtype_string) {
+            if (col_g.stats_dtype == dtype_string || col_g.stats_dtype == dtype_byte_array) {
               stats_hdr_len += 5 * 3 + 2 * max_stats_len;
             } else {
               stats_hdr_len += ((col_g.stats_dtype >= dtype_int64) ? 10 : 5) * 3;
@@ -903,9 +908,8 @@ __global__ void __launch_bounds__(128, 8)
       dst[0]     = dict_bits;
       s->rle_out = dst + 1;
     }
-    auto col           = *(s->col.parent_column);
-    s->page_start_val  = row_to_value_idx(s->page.start_row, col);
-    s->chunk_start_val = row_to_value_idx(s->ck.start_row, col);
+    s->page_start_val  = row_to_value_idx(s->page.start_row, s->col);
+    s->chunk_start_val = row_to_value_idx(s->ck.start_row, s->col);
   }
   __syncthreads();
   for (uint32_t cur_val_idx = 0; cur_val_idx < s->page.num_leaf_values;) {
@@ -1327,12 +1331,16 @@ __device__ void get_extremum(const statistics_val* stats_val,
     case dtype_decimal64: dtype_len = 8; break;
     case dtype_decimal128: dtype_len = 16; break;
     case dtype_string:
+    case dtype_byte_array:
     default: dtype_len = 0; break;
   }
 
   if (dtype == dtype_string) {
     *len = stats_val->str_val.length;
     *val = stats_val->str_val.ptr;
+  } else if (dtype == dtype_byte_array) {
+    *len = stats_val->byte_val.length;
+    *val = stats_val->byte_val.ptr;
   } else {
     *len = dtype_len;
     if (dtype == dtype_float32) {  // Convert from double to float32
