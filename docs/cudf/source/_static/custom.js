0bef95678ee1941850613c9862574c8921c7d68c
galipremsagar <sagarprem75@gmail.com>
2022-07-29 07:46:03 -0700

    misc fixes

--- docs/cudf/source/_static/custom.js
+++ docs/cudf/source/_static/custom.js
@@ -1,22 +1,22 @@
 // Copyright (c) 2022, NVIDIA CORPORATION.
 
-
-function update_switch_theme_button(){
+function update_switch_theme_button() {
     current_theme = document.documentElement.dataset.mode;
     if (current_theme == "light") {
         document.getElementById("theme-switch").title = "Switch to auto theme";
-    } else if (current_theme == "auto"){
+    } else if (current_theme == "auto") {
         document.getElementById("theme-switch").title = "Switch to dark theme";
     } else {
         document.getElementById("theme-switch").title = "Switch to light theme";
     }
 }
 
-$( document ).ready(function() {
-
+$(document).ready(function() {
     var observer = new MutationObserver(function(mutations) {
         update_switch_theme_button();
-      })
-      observer.observe(document.documentElement, {attributes: true, attributeFilter: ['data-theme']});
+    })
+    observer.observe(document.documentElement, {
+        attributes: true,
+        attributeFilter: ['data-theme']
+    });
 });
-
