76d871adea3b764bfe74585abb2d0efc5d64351b
Robert Maynard <rmaynard@nvidia.com>
2022-08-02 15:28:38 -0400

    Use the custom version of rmm that supports thrust namespaces

--- cpp/cmake/thirdparty/get_rmm.cmake
+++ cpp/cmake/thirdparty/get_rmm.cmake
@@ -15,6 +15,21 @@
 # This function finds rmm and sets any additional necessary environment variables.
 function(find_and_configure_rmm)
   include(${rapids-cmake-dir}/cpm/rmm.cmake)
+  include(${rapids-cmake-dir}/cpm/package_override.cmake)
+
+  file(
+    WRITE "${CMAKE_CURRENT_BINARY_DIR}/rmm_override.json"
+    [=[
+{
+  "packages" : {
+    "rmm" : {
+      "git_url" : "https://github.com/robertmaynard/rmm.git",
+      "git_tag" : "support_custom_thrust_namespaces"
+    }
+  }
+}
+]=])
+  rapids_cpm_package_override("${CMAKE_CURRENT_BINARY_DIR}/rmm_override.json")
 
   # Find or install RMM
   rapids_cpm_rmm(BUILD_EXPORT_SET cudf-exports INSTALL_EXPORT_SET cudf-exports)
