cfda47b76f06bfdc4b4561923c576df91e864215
Gregory Kimball <gkimball@nvidia.com>
2022-08-16 19:48:40 -0700

    Adjust key and column names to match generator

--- python/cudf/benchmarks/API/bench_dataframe.py
+++ python/cudf/benchmarks/API/bench_dataframe.py
@@ -41,10 +41,10 @@ def bench_merge(benchmark, dataframe, num_key_cols):
 @pytest.mark.parametrize(
     "values",
     [
-        range(1000),
-        {f"key{i}": range(1000) for i in range(10)},
-        cudf.DataFrame({f"key{i}": range(1000) for i in range(10)}),
-        cudf.Series(range(1000)),
+        range(50),
+        {f"{string.ascii_lowercase[i]}": range(50) for i in range(10)},
+        cudf.DataFrame({f"{string.ascii_lowercase[i]}": range(50) for i in range(10)}),
+        cudf.Series(range(50)),
     ],
 )
 def bench_isin(benchmark, dataframe, values):
