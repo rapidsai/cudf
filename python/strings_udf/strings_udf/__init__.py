24d602212570a23da6ba4fda62138987898c4909
brandon-b-miller <brmiller@nvidia.com>
2022-08-17 13:13:42 -0700

    move PTX to conda prefix

--- python/strings_udf/strings_udf/__init__.py
+++ python/strings_udf/strings_udf/__init__.py
@@ -1,10 +1,7 @@
 # Copyright (c) 2022, NVIDIA CORPORATION.
 from ptxcompiler.patch import patch_needed
+import os
 
 ENABLED = False if patch_needed() else True
 
-from pathlib import Path
-
-here = str(Path(__file__).parent.absolute())
-relative = "/shim/shim.ptx"
-ptxpath = here + relative
+ptxpath = os.getenv("CONDA_PREFIX") + "/lib/shim.ptx"
