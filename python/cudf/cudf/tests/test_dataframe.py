3f740bc043d83d49421e76767a19c24f09283199
Vamsi Tallam <90267547+VamsiTallam95@users.noreply.github.com>
2022-08-18 10:39:44 -0700

    incorporating feedback

--- python/cudf/cudf/tests/test_dataframe.py
+++ python/cudf/cudf/tests/test_dataframe.py
@@ -289,7 +289,7 @@ def test_axes(data):
 
 
 def test_dataframe_truncate_axis_0():
-    cdf1 = cudf.DataFrame(
+    df = cudf.DataFrame(
         {
             "A": ["a", "b", "c", "d", "e"],
             "B": ["f", "g", "h", "i", "j"],
@@ -297,19 +297,19 @@ def test_dataframe_truncate_axis_0():
         },
         index=[1, 2, 3, 4, 5],
     )
-    pdf1 = cdf1.to_pandas()
+    pdf = df.to_pandas()
 
-    expected = pdf1.truncate(before=2, after=4, axis="index")
-    actual = cdf1.truncate(before=2, after=4, axis="index")
+    expected = pdf.truncate(before=2, after=4, axis="index")
+    actual = df.truncate(before=2, after=4, axis="index")
     assert_eq(actual, expected)
 
-    expected = pdf1.truncate(before=1, after=4, axis=0)
-    actual = cdf1.truncate(before=1, after=4, axis=0)
+    expected = pdf.truncate(before=1, after=4, axis=0)
+    actual = df.truncate(before=1, after=4, axis=0)
     assert_eq(expected, actual)
 
 
 def test_dataframe_truncate_axis_1():
-    cdf1 = cudf.DataFrame(
+    df = cudf.DataFrame(
         {
             "A": ["a", "b", "c", "d", "e"],
             "B": ["f", "g", "h", "i", "j"],
@@ -317,25 +317,26 @@ def test_dataframe_truncate_axis_1():
         },
         index=[1, 2, 3, 4, 5],
     )
-    pdf1 = cdf1.to_pandas()
+    pdf = df.to_pandas()
 
-    expected = pdf1.truncate(before="A", after="B", axis="columns")
-    actual = cdf1.truncate(before="A", after="B", axis="columns")
+    expected = pdf.truncate(before="A", after="B", axis="columns")
+    actual = df.truncate(before="A", after="B", axis="columns")
     assert_eq(actual, expected)
 
-    expected = pdf1.truncate(before="A", after="B", axis=1)
-    actual = cdf1.truncate(before="A", after="B", axis=1)
+    expected = pdf.truncate(before="A", after="B", axis=1)
+    actual = df.truncate(before="A", after="B", axis=1)
     assert_eq(actual, expected)
 
 
 def test_dataframe_truncate_datetimeindex():
-    dates = cudf.date_range("2021-01-01", "2021-01-02", freq="s")
-    cdf1 = cudf.DataFrame(data={"A": 1, "B": 2}, index=dates)
-    pdf1 = cdf1.to_pandas()
-    expected = pdf1.truncate(
+    dates = cudf.date_range(
+        '2021-01-01 23:45:00', '2021-01-02 23:46:00', freq="s")
+    df = cudf.DataFrame(data={"A": 1, "B": 2}, index=dates)
+    pdf = df.to_pandas()
+    expected = pdf.truncate(
         before="2021-01-01 23:45:18", after="2021-01-01 23:45:27"
     )
-    actual = cdf1.truncate(
+    actual = df.truncate(
         before="2021-01-01 23:45:18", after="2021-01-01 23:45:27"
     )
 
