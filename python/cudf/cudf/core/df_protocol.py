1e344205494472872a1c39c4a28b753503df04af
Bradley Dice <bdice@bradleydice.com>
2022-08-23 10:05:18 -0500

    Enable D406, D407.

--- python/cudf/cudf/core/df_protocol.py
+++ python/cudf/cudf/core/df_protocol.py
@@ -186,25 +186,25 @@ class _CuDFColumn:
                         Data Interface format.
         Endianness : current only native endianness (``=``) is supported
 
-        Notes:
-
-            - Kind specifiers are aligned with DLPack where possible
-             (hence the jump to 20, leave enough room for future extension)
-            - Masks must be specified as boolean with either bit width 1
-             (for bit masks) or 8 (for byte masks).
-            - Dtype width in bits was preferred over bytes
-            - Endianness isn't too useful, but included now in case
-              in the future we need to support non-native endianness
-            - Went with Apache Arrow format strings over NumPy format strings
-              because they're more complete from a dataframe perspective
-            - Format strings are mostly useful for datetime specification,
-              and for categoricals.
-            - For categoricals, the format string describes the type of the
-              categorical in the data buffer. In case of a separate encoding
-              of the categorical (e.g. an integer to string mapping),
-              this can be derived from ``self.describe_categorical``.
-            - Data types not included: complex, Arrow-style null,
-              binary, decimal, and nested (list, struct, map, union) dtypes.
+        Notes
+        -----
+        - Kind specifiers are aligned with DLPack where possible
+         (hence the jump to 20, leave enough room for future extension)
+        - Masks must be specified as boolean with either bit width 1
+         (for bit masks) or 8 (for byte masks).
+        - Dtype width in bits was preferred over bytes
+        - Endianness isn't too useful, but included now in case
+          in the future we need to support non-native endianness
+        - Went with Apache Arrow format strings over NumPy format strings
+          because they're more complete from a dataframe perspective
+        - Format strings are mostly useful for datetime specification,
+          and for categoricals.
+        - For categoricals, the format string describes the type of the
+          categorical in the data buffer. In case of a separate encoding
+          of the categorical (e.g. an integer to string mapping),
+          this can be derived from ``self.describe_categorical``.
+        - Data types not included: complex, Arrow-style null,
+          binary, decimal, and nested (list, struct, map, union) dtypes.
         """
         dtype = self._col.dtype
 
