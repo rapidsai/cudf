1509f1e0d622ffd6d4d30c8c03809bba49bfce40
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-08-25 15:40:15 -0500

    Update column.py

--- python/cudf/cudf/core/column/column.py
+++ python/cudf/cudf/core/column/column.py
@@ -1491,7 +1491,7 @@ def build_categorical_column(
         Null mask
     size : int, optional
     offset : int, optional
-    ordered : bool
+    ordered : bool, default False
         Indicates whether the categories are ordered
     """
     codes_dtype = min_unsigned_type(len(categories))
