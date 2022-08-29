ecf4662859782422f50046d3ed02ac4e47a9d500
David Wendt <45795991+davidwendt@users.noreply.github.com>
2022-08-29 16:48:16 -0400

    Move cudf::strings::findall_record to cudf::strings::findall (#11575)
    Replaces `cudf::strings::findall` with the implementation from `cudf::strings::findall_record`.
    As referenced in #11510, the column-based `findall` implementation is not used and unnecessary over `findall_record` which returns a lists result. For documentation and discoverability `findall_record` is renamed to `findall` and the current `findall` implementation is removed.
    
    Closes #11510
    
    Authors:
      - David Wendt (https://github.com/davidwendt)
    
    Approvers:
      - Robert Maynard (https://github.com/robertmaynard)
      - GALI PREM SAGAR (https://github.com/galipremsagar)
      - Bradley Dice (https://github.com/bdice)
      - Jason Lowe (https://github.com/jlowe)
    
    URL: https://github.com/rapidsai/cudf/pull/11575

--- python/cudf/cudf/_lib/strings/findall.pyx
+++ python/cudf/cudf/_lib/strings/findall.pyx
@@ -8,42 +8,12 @@ from libcpp.utility cimport move
 from cudf._lib.column cimport Column
 from cudf._lib.cpp.column.column cimport column
 from cudf._lib.cpp.column.column_view cimport column_view
-from cudf._lib.cpp.scalar.scalar cimport string_scalar
-from cudf._lib.cpp.strings.findall cimport (
-    findall as cpp_findall,
-    findall_record as cpp_findall_record,
-)
+from cudf._lib.cpp.strings.findall cimport findall as cpp_findall
 from cudf._lib.cpp.strings.regex_flags cimport regex_flags
-from cudf._lib.cpp.table.table cimport table
-from cudf._lib.scalar cimport DeviceScalar
 from cudf._lib.utils cimport data_from_unique_ptr
 
 
 def findall(Column source_strings, object pattern, uint32_t flags):
-    """
-    Returns data with all non-overlapping matches of `pattern`
-    in each string of `source_strings`.
-    """
-    cdef unique_ptr[table] c_result
-    cdef column_view source_view = source_strings.view()
-
-    cdef string pattern_string = <string>str(pattern).encode()
-    cdef regex_flags c_flags = <regex_flags>flags
-
-    with nogil:
-        c_result = move(cpp_findall(
-            source_view,
-            pattern_string,
-            c_flags
-        ))
-
-    return data_from_unique_ptr(
-        move(c_result),
-        column_names=range(0, c_result.get()[0].num_columns())
-    )
-
-
-def findall_record(Column source_strings, object pattern, uint32_t flags):
     """
     Returns data with all non-overlapping matches of `pattern`
     in each string of `source_strings` as a lists column.
@@ -55,7 +25,7 @@ def findall_record(Column source_strings, object pattern, uint32_t flags):
     cdef regex_flags c_flags = <regex_flags>flags
 
     with nogil:
-        c_result = move(cpp_findall_record(
+        c_result = move(cpp_findall(
             source_view,
             pattern_string,
             c_flags
