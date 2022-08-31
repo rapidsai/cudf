0460abc7389f34bc1e6b4a5aa4789a4d5817c569
brandon-b-miller <brmiller@nvidia.com>
2022-08-31 08:15:12 -0700

    build both 3.8 and 3.9 conda packages on CPU only, and add debugging warning

--- python/strings_udf/strings_udf/__init__.py
+++ python/strings_udf/strings_udf/__init__.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2022, NVIDIA CORPORATION.
 from ptxcompiler.patch import patch_needed
 import os
+import warnings
 
 ENABLED = False if patch_needed() else True
-
+warnings.warn(f"String UDFs are enabled: {ENABLED}")
 ptxpath = os.getenv("CONDA_PREFIX") + "/lib/shim.ptx"
 
 from . import _version
