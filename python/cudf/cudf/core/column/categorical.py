987aa263e57456fa288b85f2b019d2ada2a09560
brandon-b-miller <brmiller@nvidia.com>
2022-09-01 09:03:09 -0700

    cleanup

--- python/cudf/cudf/core/column/categorical.py
+++ python/cudf/cudf/core/column/categorical.py
@@ -1152,7 +1152,6 @@ class CategoricalColumn(column.ColumnBase):
                 fill_value = column.as_column(fill_value, nan_as_null=False)
                 if isinstance(fill_value, CategoricalColumn):
                     if self.dtype != fill_value.dtype:
-                        breakpoint()
                         raise TypeError(
                             "Cannot set a Categorical with another, "
                             "without identical categories"
