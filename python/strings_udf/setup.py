80b176ef8af0c9a5cf53a39127e8cc0069a9c728
brandon-b-miller <brmiller@nvidia.com>
2022-09-09 12:40:34 -0700

    prune setup.py

--- python/strings_udf/setup.py
+++ python/strings_udf/setup.py
@@ -11,30 +11,13 @@ from skbuild.command.build_ext import build_ext
 import versioneer
 
 install_requires = [
-    "cachetools",
-    "cuda-python>=11.5,<11.7.1",
-    "fsspec>=0.6.0",
     "numba>=0.53.1",
     "numpy",
-    "nvtx>=0.2.1",
-    "packaging",
-    "pandas>=1.0,<1.5.0dev0",
-    "protobuf>=3.20.1,<3.21.0a0",
-    "typing_extensions",
 ]
 
 extras_require = {
     "test": [
         "pytest",
-        "pytest-benchmark",
-        "pytest-xdist",
-        "hypothesis",
-        "mimesis",
-        "fastavro>=0.22.9",
-        "python-snappy>=0.6.0",
-        "pyorc",
-        "msgpack",
-        "transformers<=4.10.3",
     ]
 }
 
@@ -75,12 +58,6 @@ if not os.path.isdir(CUDA_HOME):
     raise OSError(f"Invalid CUDA_HOME: directory does not exist: {CUDA_HOME}")
 
 cuda_include_dir = os.path.join(CUDA_HOME, "include")
-install_requires.append(
-    "cupy-cuda"
-    + get_cuda_version_from_header(cuda_include_dir)
-    + ">=9.5.0,<11.0.0a0"
-)
-
 
 cmdclass = versioneer.get_cmdclass()
 cmdclass["build_ext"] = build_ext
