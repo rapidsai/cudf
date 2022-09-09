a64a745f8cf20b24c6c75ec8f1ee427e40f80e1b
brandon-b-miller <brmiller@nvidia.com>
2022-09-09 09:34:33 -0700

    dynamically generate attributes of MaskedType(string_view)

--- python/cudf/cudf/core/udf/strings_typing.py
+++ python/cudf/cudf/core/udf/strings_typing.py
@@ -69,6 +69,10 @@ class MaskedStringViewCmpOp(AbstractTemplate):
             )
 
 
+for op in comparison_ops:
+    cuda_decl_registry.register_global(op)(MaskedStringViewCmpOp)
+
+
 @cuda_decl_registry.register_global(len)
 class StringLiteralLength(AbstractTemplate):
     """
@@ -81,40 +85,26 @@ class StringLiteralLength(AbstractTemplate):
             return nb_signature(size_type, args[0])
 
 
-class MaskedStringViewStartsWith(AbstractTemplate):
-    key = "MaskedType.startswith"
+def create_masked_binary_attr(attrname, retty):
+    class MaskedStringViewBinaryAttr(AbstractTemplate):
+        key = attrname
 
-    def generic(self, args, kws):
-        return nb_signature(
-            MaskedType(types.boolean), MaskedType(string_view), recvr=self.this
-        )
+        def generic(self, args, kws):
+            return nb_signature(
+                MaskedType(retty), MaskedType(string_view), recvr=self.this
+            )
 
-
-class MaskedStringViewEndsWith(AbstractTemplate):
-    key = "MaskedType.endswith"
-
-    def generic(self, args, kws):
-        return nb_signature(
-            MaskedType(types.boolean), MaskedType(string_view), recvr=self.this
-        )
+    return MaskedStringViewBinaryAttr
 
 
-class MaskedStringViewFind(AbstractTemplate):
-    key = "MaskedType.find"
+def create_masked_identifier_attr(attrname):
+    class MaskedStringViewIdentifierAttr(AbstractTemplate):
+        key = attrname
 
-    def generic(self, args, kws):
-        return nb_signature(
-            MaskedType(size_type), MaskedType(string_view), recvr=self.this
-        )
+        def generic(self, args, kws):
+            return nb_signature(MaskedType(types.boolean), recvr=self.this)
 
-
-class MaskedStringViewRFind(AbstractTemplate):
-    key = "MaskedType.rfind"
-
-    def generic(self, args, kws):
-        return nb_signature(
-            MaskedType(size_type), MaskedType(string_view), recvr=self.this
-        )
+    return MaskedStringViewIdentifierAttr
 
 
 class MaskedStringViewCount(AbstractTemplate):
@@ -126,77 +116,32 @@ class MaskedStringViewCount(AbstractTemplate):
         )
 
 
-class MaskedStringViewIsAlnum(AbstractTemplate):
-    key = "MaskedType.isalnum"
-
-    def generic(self, args, kws):
-        return nb_signature(MaskedType(types.boolean), recvr=self.this)
-
-
-class MaskedStringViewIsAlpha(AbstractTemplate):
-    key = "MaskedType.isalpha"
-
-    def generic(self, args, kws):
-        return nb_signature(MaskedType(types.boolean), recvr=self.this)
-
-
-class MaskedStringViewIsDecimal(AbstractTemplate):
-    key = "MaskedType.isdecimal"
-
-    def generic(self, args, kws):
-        return nb_signature(MaskedType(types.boolean), recvr=self.this)
-
-
-class MaskedStringViewIsDigit(AbstractTemplate):
-    key = "MaskedType.isdigit"
-
-    def generic(self, args, kws):
-        return nb_signature(MaskedType(types.boolean), recvr=self.this)
-
-
-class MaskedStringViewIsLower(AbstractTemplate):
-    key = "MaskedType.islower"
-
-    def generic(self, args, kws):
-        return nb_signature(MaskedType(types.boolean), recvr=self.this)
-
-
-class MaskedStringViewIsUpper(AbstractTemplate):
-    key = "MaskedType.isupper"
-
-    def generic(self, args, kws):
-        return nb_signature(MaskedType(types.boolean), recvr=self.this)
-
-
-class MaskedStringViewIsSpace(AbstractTemplate):
-    key = "MaskedType.isspace"
-
-    def generic(self, args, kws):
-        return nb_signature(MaskedType(types.boolean), recvr=self.this)
-
-
 @cuda_decl_registry.register_attr
 class MaskedStringViewAttrs(AttributeTemplate):
     key = MaskedType(string_view)
 
     def resolve_startswith(self, mod):
         return types.BoundFunction(
-            MaskedStringViewStartsWith, MaskedType(string_view)
+            create_masked_binary_attr("MaskedType.startswith", types.boolean),
+            MaskedType(string_view),
         )
 
     def resolve_endswith(self, mod):
         return types.BoundFunction(
-            MaskedStringViewEndsWith, MaskedType(string_view)
+            create_masked_binary_attr("MaskedType.endswith", types.boolean),
+            MaskedType(string_view),
         )
 
     def resolve_find(self, mod):
         return types.BoundFunction(
-            MaskedStringViewFind, MaskedType(string_view)
+            create_masked_binary_attr("MaskedType.find", size_type),
+            MaskedType(string_view),
         )
 
     def resolve_rfind(self, mod):
         return types.BoundFunction(
-            MaskedStringViewRFind, MaskedType(string_view)
+            create_masked_binary_attr("MaskedType.rfind", size_type),
+            MaskedType(string_view),
         )
 
     def resolve_count(self, mod):
@@ -206,37 +151,44 @@ class MaskedStringViewAttrs(AttributeTemplate):
 
     def resolve_isalnum(self, mod):
         return types.BoundFunction(
-            MaskedStringViewIsAlnum, MaskedType(string_view)
+            create_masked_identifier_attr("MaskedType.isalnum"),
+            MaskedType(string_view),
         )
 
     def resolve_isalpha(self, mod):
         return types.BoundFunction(
-            MaskedStringViewIsAlpha, MaskedType(string_view)
+            create_masked_identifier_attr("MaskedType.isalpha"),
+            MaskedType(string_view),
         )
 
     def resolve_isdecimal(self, mod):
         return types.BoundFunction(
-            MaskedStringViewIsDecimal, MaskedType(string_view)
+            create_masked_identifier_attr("MaskedType.isdecimal"),
+            MaskedType(string_view),
         )
 
     def resolve_isdigit(self, mod):
         return types.BoundFunction(
-            MaskedStringViewIsDigit, MaskedType(string_view)
+            create_masked_identifier_attr("MaskedType.isdigit"),
+            MaskedType(string_view),
         )
 
     def resolve_islower(self, mod):
         return types.BoundFunction(
-            MaskedStringViewIsLower, MaskedType(string_view)
+            create_masked_identifier_attr("MaskedType.islower"),
+            MaskedType(string_view),
         )
 
     def resolve_isupper(self, mod):
         return types.BoundFunction(
-            MaskedStringViewIsUpper, MaskedType(string_view)
+            create_masked_identifier_attr("MaskedType.isupper"),
+            MaskedType(string_view),
         )
 
     def resolve_isspace(self, mod):
         return types.BoundFunction(
-            MaskedStringViewIsSpace, MaskedType(string_view)
+            create_masked_identifier_attr("MaskedType.isspace"),
+            MaskedType(string_view),
         )
 
     def resolve_value(self, mod):
@@ -244,7 +196,3 @@ class MaskedStringViewAttrs(AttributeTemplate):
 
     def resolve_valid(self, mod):
         return types.boolean
-
-
-for op in comparison_ops:
-    cuda_decl_registry.register_global(op)(MaskedStringViewCmpOp)
