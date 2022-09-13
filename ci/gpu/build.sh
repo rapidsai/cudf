0d4dae72cff696af7851fb04d2a94c64f92b5820
Mads R. B. Kristensen <madsbk@gmail.com>
2022-09-13 13:43:42 +0200

    Typo
    Co-authored-by: Vyas Ramasubramani <vyas.ramasubramani@gmail.com>

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -250,7 +250,7 @@ cd "$WORKSPACE/python/cudf/cudf"
 gpuci_logger "Python py.test for cuDF"
 py.test -n 8 --cache-clear --basetemp="$WORKSPACE/cudf-cuda-tmp" --ignore="$WORKSPACE/python/cudf/cudf/benchmarks" --junitxml="$WORKSPACE/junit-cudf.xml" -v --cov-config="$WORKSPACE/python/cudf/.coveragerc" --cov=cudf --cov-report=xml:"$WORKSPACE/python/cudf/cudf-coverage.xml" --cov-report term --dist=loadscope tests
 
-gpuci_logger "Python py.tests for cuDF with spelling (CUDF_SPILL_DEVICE_LIMIT=1)"
+gpuci_logger "Python py.tests for cuDF with spilling (CUDF_SPILL_DEVICE_LIMIT=1)"
 # Due to time concerns, we only run a limited set of tests
 CUDF_SPILL=on CUDF_SPILL_DEVICE_LIMIT=1 py.test -v -n 8 tests/test_binops.py tests/test_dataframe.py tests/test_buffer.py tests/test_onehot.py tests/test_reshape.py
 
