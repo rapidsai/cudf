267c5af143174917eebea58a15db74ce2443bfe6
Vyas Ramasubramani <vyasr@nvidia.com>
2022-09-13 13:29:20 -0700

    More copyright fixes.

--- python/strings_udf/strings_udf/_lib/cpp/strings_udf.pxd
+++ python/strings_udf/strings_udf/_lib/cpp/strings_udf.pxd
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2022, NVIDIA CORPORATION.
+# Copyright (c) 2022, NVIDIA CORPORATION.
 
 from libc.stdint cimport uint8_t
 from libcpp.memory cimport unique_ptr
--- python/strings_udf/strings_udf/_lib/cudf_jit_udf.pyx
+++ python/strings_udf/strings_udf/_lib/cudf_jit_udf.pyx
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2022, NVIDIA CORPORATION.
+# Copyright (c) 2022, NVIDIA CORPORATION.
 
 from libcpp.memory cimport unique_ptr
 from libcpp.utility cimport move
