7126c070d32da82e09c1983aa2eea080f79d2552
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2022-09-24 00:42:58 +0530

    remove default arg to stream

--- cpp/src/io/json/nested_json.hpp
+++ cpp/src/io/json/nested_json.hpp
@@ -274,7 +274,7 @@ std::tuple<rmm::device_uvector<NodeIndexT>, rmm::device_uvector<size_type>>
 records_orient_tree_traversal(
   device_span<SymbolT const> d_input,
   tree_meta_t& d_tree,
-  rmm::cuda_stream_view stream        = cudf::default_stream_value,
+  rmm::cuda_stream_view stream,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 /**
  * @brief Parses the given JSON string and generates table from the given input.
