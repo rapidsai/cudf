1ff6fd2a30afa56751784c21aec60b707dc1450c
galipremsagar <sagarprem75@gmail.com>
2022-09-26 12:55:51 -0700

    update tests

--- python/cudf/cudf/tests/test_setitem.py
+++ python/cudf/cudf/tests/test_setitem.py
@@ -245,13 +245,20 @@ def test_categorical_setitem_invalid():
 
 
 def test_series_slice_setitem_list():
-    actual = cudf.Series([[1, 2], [2, 3], [3, 4], [4, 5], [6, 7]])
-    actual[slice(0, 3, 1)] = [10, 11]
-    expected = cudf.Series([[10, 11], [10, 11], [10, 11], [4, 5], [6, 7]])
+    actual = cudf.Series([[[1, 2], [2, 3]], [[3, 4]], [[4, 5]], [[6, 7]]])
+    actual[slice(0, 3, 1)] = [[10, 11], [12, 23]]
+    expected = cudf.Series(
+        [
+            [[10, 11], [12, 23]],
+            [[10, 11], [12, 23]],
+            [[10, 11], [12, 23]],
+            [[6, 7]],
+        ]
+    )
     assert_eq(actual, expected)
 
-    actual = cudf.Series([[1, 2], [2, 3], [3, 4], [4, 5], [6, 7]])
-    actual[0:3] = cudf.Scalar([10, 11])
+    actual = cudf.Series([[[1, 2], [2, 3]], [[3, 4]], [[4, 5]], [[6, 7]]])
+    actual[0:3] = cudf.Scalar([[10, 11], [12, 23]])
 
     assert_eq(actual, expected)
 
@@ -259,19 +266,19 @@ def test_series_slice_setitem_list():
 def test_series_slice_setitem_struct():
     actual = cudf.Series(
         [
-            {"a": 10, "b": 11},
-            {"a": 100, "b": 5},
-            {"a": 50, "b": 2},
-            {"a": 1000, "b": 67},
-            {"a": 4000, "b": 1090},
+            {"a": {"b": 10}, "b": 11},
+            {"a": {"b": 100}, "b": 5},
+            {"a": {"b": 50}, "b": 2},
+            {"a": {"b": 1000}, "b": 67},
+            {"a": {"b": 4000}, "b": 1090},
         ]
     )
-    actual[slice(0, 3, 1)] = {"a": 5050, "b": 101}
+    actual[slice(0, 3, 1)] = {"a": {"b": 5050}, "b": 101}
     expected = cudf.Series(
         [
-            {"a": 5050, "b": 101},
-            {"a": 5050, "b": 101},
-            {"a": 5050, "b": 101},
+            {"a": {"b": 5050}, "b": 101},
+            {"a": {"b": 5050}, "b": 101},
+            {"a": {"b": 5050}, "b": 101},
             {"a": 1000, "b": 67},
             {"a": 4000, "b": 1090},
         ]
@@ -280,13 +287,13 @@ def test_series_slice_setitem_struct():
 
     actual = cudf.Series(
         [
-            {"a": 10, "b": 11},
-            {"a": 100, "b": 5},
-            {"a": 50, "b": 2},
-            {"a": 1000, "b": 67},
-            {"a": 4000, "b": 1090},
+            {"a": {"b": 10}, "b": 11},
+            {"a": {"b": 100}, "b": 5},
+            {"a": {"b": 50}, "b": 2},
+            {"a": {"b": 1000}, "b": 67},
+            {"a": {"b": 4000}, "b": 1090},
         ]
     )
-    actual[0:3] = cudf.Scalar({"a": 5050, "b": 101})
+    actual[0:3] = cudf.Scalar({"a": {"b": 5050}, "b": 101})
 
     assert_eq(actual, expected)
