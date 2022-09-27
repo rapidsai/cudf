610f0196d5d01a5cfd5ec8d8438d7cf5004d7206
Yunsong Wang <yunsongw@nvidia.com>
2022-09-27 14:37:55 -0400

    Update list tests

--- cpp/tests/groupby/lists_tests.cu
+++ cpp/tests/groupby/lists_tests.cu
@@ -185,8 +185,8 @@ TYPED_TEST(groupby_lists_test, lists_with_nulls)
   auto keys   = lcw<TypeParam> { {{1,1}, {2,2}, {3,3}, {1,1}, {2,2}}, nulls_at({1,2,4})};
   auto values = fwcw<int32_t>  {     0,     1,     2,     3,     4 };
 
-  auto expected_keys   = lcw<TypeParam> { {{1,1}, {null,null}}, null_at(1)};
-  auto expected_values = fwcw<R>        {     3,           7 };
+  auto expected_keys   = lcw<TypeParam> { {{null,null}, {1,1}}, null_at(0)};
+  auto expected_values = fwcw<R>        {           7,     3 };
   // clang-format on
 
   test_sum_agg(keys, values, expected_keys, expected_values);
@@ -203,8 +203,8 @@ TYPED_TEST(groupby_lists_test, lists_with_null_elements)
   auto values = fwcw<int32_t>{1, 2, 4, 5};
 
   auto expected_keys = lcw<TypeParam>{
-    {lcw<TypeParam>{{{1, 2, 3}, {}, {4, 5}, {}, {6, 0}}, nulls_at({1, 3})}, {}}, null_at(1)};
-  auto expected_values = fwcw<R>{3, 9};
+    {{}, lcw<TypeParam>{{{1, 2, 3}, {}, {4, 5}, {}, {6, 0}}, nulls_at({1, 3})}}, null_at(0)};
+  auto expected_values = fwcw<R>{9, 3};
 
   test_sum_agg(keys, values, expected_keys, expected_values);
 }
