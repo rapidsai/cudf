6f1ee1a6b372acdf792ea9d042b0d2697b37d794
Ashwin Srinath <shwina@users.noreply.github.com>
2022-09-27 12:50:16 -0700

    Revert "Comment out most of to_string_view_array"
    This reverts commit 1c5b3528c77228a2e9c694145af2639043f92dbb.

--- python/strings_udf/strings_udf/_lib/cudf_jit_udf.pyx
+++ python/strings_udf/strings_udf/_lib/cudf_jit_udf.pyx
@@ -7,7 +7,7 @@ from cudf.core.buffer import as_device_buffer_like
 
 from cudf._lib.column cimport Column
 from cudf._lib.cpp.column.column cimport column, column_view
-from rmm._cuda.stream import DEFAULT_STREAM, Stream
+from rmm._cuda.stream import DEFAULT_STREAM
 from rmm._lib.device_buffer cimport DeviceBuffer, device_buffer
 
 from strings_udf._lib.cpp.strings_udf cimport (
@@ -18,8 +18,8 @@ from strings_udf._lib.cpp.strings_udf cimport (
 def to_string_view_array(Column strings_col):
     cdef unique_ptr[device_buffer] c_buffer
     cdef column_view input_view = strings_col.view()
-    # with nogil:
-    #     c_buffer = move(cpp_to_string_view_array(input_view))
-    # device_buffer = DeviceBuffer.c_from_unique_ptr(move(c_buffer))
-    # device_buffer.stream.c_synchronize()
-    # return as_device_buffer_like(device_buffer, exposed=False)
+    with nogil:
+        c_buffer = move(cpp_to_string_view_array(input_view))
+    device_buffer = DeviceBuffer.c_from_unique_ptr(move(c_buffer))
+    DEFAULT_STREAM.c_synchronize()
+    return as_device_buffer_like(device_buffer, exposed=False)
