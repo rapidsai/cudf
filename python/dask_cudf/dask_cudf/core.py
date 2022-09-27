482b21a0061f09159622d10056f78c8c97e87c57
rjzamora <rzamora217@gmail.com>
2022-09-27 14:08:49 -0700

    remove stale code

--- python/dask_cudf/dask_cudf/core.py
+++ python/dask_cudf/dask_cudf/core.py
@@ -218,8 +218,7 @@ class DataFrame(_Frame, dd.core.DataFrame):
                 return df2.repartition(divisions=divisions)
             return df2
 
-        return _shuffle_context(
-            super().set_index,
+        return super().set_index(
             other,
             sorted=pre_sorted,
             shuffle=_get_shuffle_type(shuffle),
@@ -735,29 +734,6 @@ from_cudf.__doc__ = (
 @_dask_cudf_nvtx_annotate
 def from_dask_dataframe(df):
     return df.map_partitions(cudf.from_pandas)
-
-
-def _shuffle_context(func, *args, shuffle="tasks", **kwargs):
-    # Utility to check `shuffle` kwarg and use dask-config
-    # context for "explicit-comms" option, when necessary
-    if shuffle not in ("tasks", "explicit-comms"):
-        raise ValueError(
-            f"Dask-cudf only supports in-memory shuffling with "
-            f"'tasks' or 'explicit-comms'. Got shuffle={shuffle}"
-        )
-    if shuffle == "explicit-comms":
-        try:
-            import dask_cuda  # noqa: F401
-        except ImportError:
-            raise ValueError(
-                "shuffle='explicit-comms' requires dask_cuda. "
-                "Please install dask_cuda, or use shuffle='tasks'."
-            )
-        # Use explicit-comms shuffle
-        with dask.config.set({"explicit-comms": True}):
-            return func(*args, shuffle="tasks", **kwargs)
-
-    return func(*args, shuffle=shuffle, **kwargs)
 
 
 for name in [
