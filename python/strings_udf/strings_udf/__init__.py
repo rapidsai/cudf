c3c197377f888d7cbfaa4bd49dbeba33ef056e75
Brandon Miller <brmiller@nvidia.com>
2022-09-29 19:27:59 +0100

    Fix style issues

--- python/strings_udf/strings_udf/__init__.py
+++ python/strings_udf/strings_udf/__init__.py
@@ -20,23 +20,17 @@ if cp.returncode == 0:
 
     # Load the highest compute capability file available that is less than
     # the current device's.
-    files = glob.glob(
-        os.path.join(os.path.dirname(__file__), "shim_*.ptx")
-    )
+    files = glob.glob(os.path.join(os.path.dirname(__file__), "shim_*.ptx"))
     dev = cuda.get_current_device()
     cc = "".join(str(x) for x in dev.compute_capability)
-    files = glob.glob(
-        os.path.join(os.path.dirname(__file__), "shim_*.ptx")
-    )
+    files = glob.glob(os.path.join(os.path.dirname(__file__), "shim_*.ptx"))
     if len(files) == 0:
         raise RuntimeError(
             "This strings_udf installation is missing the necessary PTX "
             "files. Please file an issue reporting this error and how you "
             "installed cudf and strings_udf."
         )
-    sms = [
-        os.path.basename(f).rstrip(".ptx").lstrip("shim_") for f in files
-    ]
+    sms = [os.path.basename(f).rstrip(".ptx").lstrip("shim_") for f in files]
     selected_sm = max(sm for sm in sms if sm < cc)
     ptxpath = os.path.join(
         os.path.dirname(__file__), f"shim_{selected_sm}.ptx"
