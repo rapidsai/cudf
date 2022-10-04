72d44b177c100aae467e11deb8419fbfc4ff6e3b
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-10-04 18:31:01 -0500

    Update python/strings_udf/strings_udf/__init__.py

--- python/strings_udf/strings_udf/__init__.py
+++ python/strings_udf/strings_udf/__init__.py
@@ -81,7 +81,7 @@ if cp.returncode == 0:
         if virtual_sms:
             # First try to fetch ptx file from `shim_sm.ptx` files
             result = _get_appropriate_file(virtual_sms, cc)
-        if result is None:
+        if result is None and native_sms:
             # If the above fails, try to fetch ptx file from `shim_sm-real.ptx` files
             result = _get_appropriate_file(native_sms, cc)
 
