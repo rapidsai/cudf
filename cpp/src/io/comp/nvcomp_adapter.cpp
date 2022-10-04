914d728a4c5bbb4cfc34f0ed30af548d55319216
vuule <vmilovanovic@nvidia.com>
2022-10-03 18:02:10 -0700

    disable zstd decomp on nvcomp 2,4+ and compute 6.0

--- cpp/src/io/comp/nvcomp_adapter.cpp
+++ cpp/src/io/comp/nvcomp_adapter.cpp
@@ -152,6 +152,25 @@ size_t batched_decompress_temp_size(compression_type compression,
   return temp_size;
 }
 
+void check_is_zstd_enabled()
+{
+  CUDF_EXPECTS(NVCOMP_HAS_ZSTD_DECOMP, "nvCOMP 2.3 or newer is required for Zstandard compression");
+  CUDF_EXPECTS(NVCOMP_ZSTD_IS_STABLE or cudf::io::detail::nvcomp_integration::is_all_enabled(),
+               "Zstandard compression is experimental, you can enable it through "
+               "`LIBCUDF_NVCOMP_POLICY` environment variable.");
+
+#if NVCOMP_ZSTD_IS_DISABLED_ON_PASCAL
+  int device;
+  int cc_major;
+  int cc_minor;
+  CUDF_CUDA_TRY(cudaGetDevice(&device));
+  CUDF_CUDA_TRY(cudaDeviceGetAttribute(&cc_major, cudaDevAttrComputeCapabilityMajor, device));
+  CUDF_CUDA_TRY(cudaDeviceGetAttribute(&cc_minor, cudaDevAttrComputeCapabilityMinor, device));
+  CUDF_EXPECTS(cc_major != 6 or cc_minor != 0,
+               "Zstandard decompression is disabled on some Pascal GPUs");
+#endif
+}
+
 void batched_decompress(compression_type compression,
                         device_span<device_span<uint8_t const> const> inputs,
                         device_span<device_span<uint8_t> const> outputs,
@@ -160,18 +179,7 @@ void batched_decompress(compression_type compression,
                         size_t max_total_uncomp_size,
                         rmm::cuda_stream_view stream)
 {
-  // TODO Consolidate config use to a common location
-  if (compression == compression_type::ZSTD) {
-#if NVCOMP_HAS_ZSTD_DECOMP
-#if NVCOMP_ZSTD_IS_EXPERIMENTAL
-    CUDF_EXPECTS(cudf::io::detail::nvcomp_integration::is_all_enabled(),
-                 "Zstandard compression is experimental, you can enable it through "
-                 "`LIBCUDF_NVCOMP_POLICY` environment variable.");
-#endif
-#else
-    CUDF_FAIL("nvCOMP 2.3 or newer is required for Zstandard compression");
-#endif
-  }
+  if (compression == compression_type::ZSTD) { check_is_zstd_enabled(); }
 
   auto const num_chunks = inputs.size();
 
