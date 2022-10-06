09851dfb3b6d7e73d8f7984baa654fd02820020a
seidl <seidl2@llnl.gov>
2022-10-06 13:52:32 -0700

    remove leftover newline

--- cpp/src/io/parquet/page_enc.cu
+++ cpp/src/io/parquet/page_enc.cu
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #include "parquet_gpu.hpp"
 
 #include <io/utilities/block_utils.cuh>
