f596e93ee07872dce02393e2a07f949829210673
brandon-b-miller <brmiller@nvidia.com>
2022-10-10 07:08:52 -0700

    remove version guards

--- python/cudf/cudf/core/udf/__init__.py
+++ python/cudf/cudf/core/udf/__init__.py
@@ -26,34 +26,31 @@ _STRING_UDFS_ENABLED = False
 try:
     import strings_udf
 
-    if strings_udf.ENABLED:
-        from . import strings_typing  # isort: skip
-        from . import strings_lowering  # isort: skip
-        from strings_udf import ptxpath
-        from strings_udf._lib.cudf_jit_udf import to_string_view_array
-        from strings_udf._typing import str_view_arg_handler, string_view
+    from . import strings_typing  # isort: skip
+    from . import strings_lowering  # isort: skip
+    from strings_udf import ptxpath
+    from strings_udf._lib.cudf_jit_udf import to_string_view_array
+    from strings_udf._typing import str_view_arg_handler, string_view
 
-        # add an overload of MaskedType.__init__(string_view, bool)
-        cuda_lower(api.Masked, strings_typing.string_view, types.boolean)(
-            masked_lowering.masked_constructor
-        )
+    # add an overload of MaskedType.__init__(string_view, bool)
+    cuda_lower(api.Masked, strings_typing.string_view, types.boolean)(
+        masked_lowering.masked_constructor
+    )
 
-        # add an overload of pack_return(string_view)
-        cuda_lower(api.pack_return, strings_typing.string_view)(
-            masked_lowering.pack_return_scalar_impl
-        )
+    # add an overload of pack_return(string_view)
+    cuda_lower(api.pack_return, strings_typing.string_view)(
+        masked_lowering.pack_return_scalar_impl
+    )
 
-        _supported_masked_types |= {strings_typing.string_view}
-        utils.launch_arg_getters[dtype("O")] = to_string_view_array
-        utils.masked_array_types[dtype("O")] = string_view
-        utils.JIT_SUPPORTED_TYPES |= STRING_TYPES
-        utils.ptx_files.append(ptxpath)
-        utils.arg_handlers.append(str_view_arg_handler)
-        row_function.itemsizes[dtype("O")] = string_view.size_bytes
+    _supported_masked_types |= {strings_typing.string_view}
+    utils.launch_arg_getters[dtype("O")] = to_string_view_array
+    utils.masked_array_types[dtype("O")] = string_view
+    utils.JIT_SUPPORTED_TYPES |= STRING_TYPES
+    utils.ptx_files.append(ptxpath)
+    utils.arg_handlers.append(str_view_arg_handler)
+    row_function.itemsizes[dtype("O")] = string_view.size_bytes
 
-        _STRING_UDFS_ENABLED = True
-    else:
-        del strings_udf
+    _STRING_UDFS_ENABLED = True
 
 except ImportError as e:
     # allow cuDF to work without strings_udf
