9c8dbfb16951a27df9a0d8e666108c429ebc4be1
galipremsagar <sagarprem75@gmail.com>
2022-10-12 11:33:24 -0700

    use lambdas

--- cpp/src/datetime/datetime_ops.cu
+++ cpp/src/datetime/datetime_ops.cu
@@ -76,14 +76,30 @@ struct extract_component_operator {
 
     if (time_since_midnight.count() < 0) { time_since_midnight += days(1); }
 
-    auto const hrs_       = duration_cast<hours>(time_since_midnight);
-    auto const mins_      = duration_cast<minutes>(time_since_midnight - hrs_);
-    auto const secs_      = duration_cast<seconds>(time_since_midnight - hrs_ - mins_);
-    auto const millisecs_ = duration_cast<milliseconds>(time_since_midnight - hrs_ - mins_ - secs_);
-    auto const microsecs_ =
-      duration_cast<microseconds>(time_since_midnight - hrs_ - mins_ - secs_ - millisecs_);
-    auto const nanosecs_ = duration_cast<nanoseconds>(time_since_midnight - hrs_ - mins_ - secs_ -
-                                                      millisecs_ - microsecs_);
+    auto const hrs_  = [&] { return duration_cast<hours>(time_since_midnight); };
+    auto const mins_ = [&] { return duration_cast<minutes>(time_since_midnight) - hrs_(); };
+    auto const secs_ = [&] {
+      return duration_cast<seconds>(time_since_midnight) - hrs_();
+      -mins_();
+    };
+    auto const millisecs_ = [&] {
+      return duration_cast<milliseconds>(time_since_midnight) - hrs_();
+      -mins_();
+      -secs_();
+    };
+    auto const microsecs_ = [&] {
+      return duration_cast<microseconds>(time_since_midnight) - hrs_();
+      -mins_();
+      -secs_();
+      -millisecs_();
+    };
+    auto const nanosecs_ = [&] {
+      return duration_cast<nanoseconds>(time_since_midnight) - hrs_();
+      -mins_();
+      -secs_();
+      -millisecs_();
+      -microsecs_();
+    };
 
     switch (Component) {
       case datetime_component::YEAR:
@@ -94,12 +110,12 @@ struct extract_component_operator {
         return static_cast<unsigned>(year_month_day(days_since_epoch).day());
       case datetime_component::WEEKDAY:
         return year_month_weekday(days_since_epoch).weekday().iso_encoding();
-      case datetime_component::HOUR: return hrs_.count();
-      case datetime_component::MINUTE: return mins_.count();
-      case datetime_component::SECOND: return secs_.count();
-      case datetime_component::MILLISECOND: return millisecs_.count();
-      case datetime_component::MICROSECOND: return microsecs_.count();
-      case datetime_component::NANOSECOND: return nanosecs_.count();
+      case datetime_component::HOUR: return hrs_().count();
+      case datetime_component::MINUTE: return mins_().count();
+      case datetime_component::SECOND: return secs_().count();
+      case datetime_component::MILLISECOND: return millisecs_().count();
+      case datetime_component::MICROSECOND: return microsecs_().count();
+      case datetime_component::NANOSECOND: return nanosecs_().count();
       default: return 0;
     }
   }
