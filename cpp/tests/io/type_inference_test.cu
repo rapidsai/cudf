ed4f31d02593a7c619b4e3b8074f70b300649a39
Vyas Ramasubramani <vyasr@nvidia.com>
2022-10-14 10:08:04 -0700

    Fix tests that fail locally.

--- cpp/tests/io/type_inference_test.cu
+++ cpp/tests/io/type_inference_test.cu
@@ -17,6 +17,7 @@
 #include <io/utilities/trie.cuh>
 #include <io/utilities/type_inference.cuh>
 
+#include <cudf/detail/utilities/vector_factories.hpp>
 #include <cudf/scalar/scalar_factories.hpp>
 #include <cudf_test/base_fixture.hpp>
 
@@ -52,11 +53,13 @@ TEST_F(TypeInference, Basic)
 
   auto const string_offset = std::vector<int32_t>{1, 4, 7};
   auto const string_length = std::vector<std::size_t>{2, 2, 1};
-  rmm::device_vector<int32_t> d_string_offset{string_offset};
-  rmm::device_vector<std::size_t> d_string_length{string_length};
+  auto const d_string_offset =
+    cudf::detail::make_device_uvector_async(string_offset, cudf::get_default_stream());
+  auto const d_string_length =
+    cudf::detail::make_device_uvector_async(string_length, cudf::get_default_stream());
 
   auto d_col_strings =
-    thrust::make_zip_iterator(make_tuple(d_string_offset.begin(), d_string_length.begin()));
+    thrust::make_zip_iterator(thrust::make_tuple(d_string_offset.begin(), d_string_length.begin()));
 
   auto res_type =
     infer_data_type(options.json_view(),
@@ -83,11 +86,13 @@ TEST_F(TypeInference, Null)
 
   auto const string_offset = std::vector<int32_t>{1, 1, 4};
   auto const string_length = std::vector<std::size_t>{0, 2, 1};
-  rmm::device_vector<int32_t> d_string_offset{string_offset};
-  rmm::device_vector<std::size_t> d_string_length{string_length};
+  auto const d_string_offset =
+    cudf::detail::make_device_uvector_async(string_offset, cudf::get_default_stream());
+  auto const d_string_length =
+    cudf::detail::make_device_uvector_async(string_length, cudf::get_default_stream());
 
   auto d_col_strings =
-    thrust::make_zip_iterator(make_tuple(d_string_offset.begin(), d_string_length.begin()));
+    thrust::make_zip_iterator(thrust::make_tuple(d_string_offset.begin(), d_string_length.begin()));
 
   auto res_type =
     infer_data_type(options.json_view(),
@@ -115,11 +120,13 @@ TEST_F(TypeInference, AllNull)
 
   auto const string_offset = std::vector<int32_t>{1, 1, 1};
   auto const string_length = std::vector<std::size_t>{0, 0, 4};
-  rmm::device_vector<int32_t> d_string_offset{string_offset};
-  rmm::device_vector<std::size_t> d_string_length{string_length};
+  auto const d_string_offset =
+    cudf::detail::make_device_uvector_async(string_offset, cudf::get_default_stream());
+  auto const d_string_length =
+    cudf::detail::make_device_uvector_async(string_length, cudf::get_default_stream());
 
   auto d_col_strings =
-    thrust::make_zip_iterator(make_tuple(d_string_offset.begin(), d_string_length.begin()));
+    thrust::make_zip_iterator(thrust::make_tuple(d_string_offset.begin(), d_string_length.begin()));
 
   auto res_type =
     infer_data_type(options.json_view(),
@@ -146,11 +153,13 @@ TEST_F(TypeInference, String)
 
   auto const string_offset = std::vector<int32_t>{1, 8, 12};
   auto const string_length = std::vector<std::size_t>{6, 3, 4};
-  rmm::device_vector<int32_t> d_string_offset{string_offset};
-  rmm::device_vector<std::size_t> d_string_length{string_length};
+  auto const d_string_offset =
+    cudf::detail::make_device_uvector_async(string_offset, cudf::get_default_stream());
+  auto const d_string_length =
+    cudf::detail::make_device_uvector_async(string_length, cudf::get_default_stream());
 
   auto d_col_strings =
-    thrust::make_zip_iterator(make_tuple(d_string_offset.begin(), d_string_length.begin()));
+    thrust::make_zip_iterator(thrust::make_tuple(d_string_offset.begin(), d_string_length.begin()));
 
   auto res_type =
     infer_data_type(options.json_view(),
@@ -177,11 +186,13 @@ TEST_F(TypeInference, Bool)
 
   auto const string_offset = std::vector<int32_t>{1, 6, 12};
   auto const string_length = std::vector<std::size_t>{4, 5, 5};
-  rmm::device_vector<int32_t> d_string_offset{string_offset};
-  rmm::device_vector<std::size_t> d_string_length{string_length};
+  auto const d_string_offset =
+    cudf::detail::make_device_uvector_async(string_offset, cudf::get_default_stream());
+  auto const d_string_length =
+    cudf::detail::make_device_uvector_async(string_length, cudf::get_default_stream());
 
   auto d_col_strings =
-    thrust::make_zip_iterator(make_tuple(d_string_offset.begin(), d_string_length.begin()));
+    thrust::make_zip_iterator(thrust::make_tuple(d_string_offset.begin(), d_string_length.begin()));
 
   auto res_type =
     infer_data_type(options.json_view(),
@@ -208,11 +219,13 @@ TEST_F(TypeInference, Timestamp)
 
   auto const string_offset = std::vector<int32_t>{1, 10};
   auto const string_length = std::vector<std::size_t>{8, 9};
-  rmm::device_vector<int32_t> d_string_offset{string_offset};
-  rmm::device_vector<std::size_t> d_string_length{string_length};
+  auto const d_string_offset =
+    cudf::detail::make_device_uvector_async(string_offset, cudf::get_default_stream());
+  auto const d_string_length =
+    cudf::detail::make_device_uvector_async(string_length, cudf::get_default_stream());
 
   auto d_col_strings =
-    thrust::make_zip_iterator(make_tuple(d_string_offset.begin(), d_string_length.begin()));
+    thrust::make_zip_iterator(thrust::make_tuple(d_string_offset.begin(), d_string_length.begin()));
 
   auto res_type =
     infer_data_type(options.json_view(),
@@ -240,11 +253,13 @@ TEST_F(TypeInference, InvalidInput)
 
   auto const string_offset = std::vector<int32_t>{1, 3, 5, 7, 9};
   auto const string_length = std::vector<std::size_t>{1, 1, 1, 1, 1};
-  rmm::device_vector<int32_t> d_string_offset{string_offset};
-  rmm::device_vector<std::size_t> d_string_length{string_length};
+  auto const d_string_offset =
+    cudf::detail::make_device_uvector_async(string_offset, cudf::get_default_stream());
+  auto const d_string_length =
+    cudf::detail::make_device_uvector_async(string_length, cudf::get_default_stream());
 
   auto d_col_strings =
-    thrust::make_zip_iterator(make_tuple(d_string_offset.begin(), d_string_length.begin()));
+    thrust::make_zip_iterator(thrust::make_tuple(d_string_offset.begin(), d_string_length.begin()));
 
   auto res_type =
     infer_data_type(options.json_view(),
