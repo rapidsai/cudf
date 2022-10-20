28f404a186c2c1b46feb4c22d8d610c0d0a16958
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2022-10-20 16:37:19 -0700

    Apply suggestions from code review
    Co-authored-by: David Wendt <45795991+davidwendt@users.noreply.github.com>

--- cpp/include/cudf/detail/scan.hpp
+++ cpp/include/cudf/detail/scan.hpp
@@ -38,7 +38,7 @@ namespace detail {
  *                           `agg` is not Min or Max.
  *
  * @param input The input column view for the scan.
- * @param agg aggregation operator applied by the scan.
+ * @param agg Aggregation operator applied by the scan.
  * @param null_handling Exclude null values when computing the result if null_policy::EXCLUDE.
  *                      Include nulls if null_policy::INCLUDE. Any operation with a null results in
  *                      a null.
@@ -64,7 +64,7 @@ std::unique_ptr<column> scan_exclusive(column_view const& input,
  *                           but the `agg` is not Min or Max.
  *
  * @param input The input column view for the scan.
- * @param agg aggregation operator applied by the scan.
+ * @param agg Aggregation operator applied by the scan
  * @param null_handling Exclude null values when computing the result if null_policy::EXCLUDE.
  *                      Include nulls if null_policy::INCLUDE. Any operation with a null results in
  *                      a null.
