34c056c07c67398f554329cd78cbb7c0be6fbe30
vuule <vmilovanovic@nvidia.com>
2022-10-21 14:50:37 -0700

    C++ changes

--- cpp/include/cudf/io/csv.hpp
+++ cpp/include/cudf/io/csv.hpp
@@ -1339,7 +1339,7 @@ class csv_writer_options {
   // string to use for values == 0 in INT8 types (default 'false')
   std::string _false_value = std::string{"false"};
   // Optional associated metadata
-  table_metadata const* _metadata = nullptr;
+  std::vector<std::string> _names;
 
   /**
    * @brief Constructor from sink and table.
@@ -1391,7 +1391,7 @@ class csv_writer_options {
    *
    * @return Optional associated metadata
    */
-  [[nodiscard]] table_metadata const* get_metadata() const { return _metadata; }
+  [[nodiscard]] std::vector<std::string> const& get_names() const { return _names; }
 
   /**
    * @brief Returns string to used for null entries.
@@ -1444,11 +1444,11 @@ class csv_writer_options {
 
   // Setter
   /**
-   * @brief Sets optional associated metadata.
+   * @brief Sets optional associated column names.
    *
-   @param metadata Associated metadata
+   @param names Associated column names
    */
-  void set_metadata(table_metadata* metadata) { _metadata = metadata; }
+  void set_names(std::vector<std::string> names) { _names = std::move(names); }
 
   /**
    * @brief Sets string to used for null entries.
@@ -1526,14 +1526,14 @@ class csv_writer_options_builder {
   }
 
   /**
-   * @brief Sets optional associated metadata.
+   * @brief Sets optional column names.
    *
-   * @param metadata Associated metadata
+   * @param names Column names
    * @return this for chaining
    */
-  csv_writer_options_builder& metadata(table_metadata* metadata)
+  csv_writer_options_builder& names(std::vector<std::string> names)
   {
-    options._metadata = metadata;
+    options._names = names;
     return *this;
   }
 
