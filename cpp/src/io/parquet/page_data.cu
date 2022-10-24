44424a2f1e67e441c3df1061b57ffdb6159c2fab
Dave Baranec <dbaranec@nvidia.com>
2022-10-24 09:39:32 -0500

    Optimization:  don't do any decoding or page size computation for pages that have been trimmed.

--- cpp/src/io/parquet/page_data.cu
+++ cpp/src/io/parquet/page_data.cu
@@ -980,7 +980,7 @@ static __device__ bool setupLocalPageInfo(page_state_s* const s,
           if (s->col.max_level[level_type::REPETITION] == 0) {
             output_offset = page_start_row >= min_row ? page_start_row - min_row : 0;
           }
-          // for schemas with lists, we've already got the exactly value precomputed
+          // for schemas with lists, we've already got the exact value precomputed
           else {
             output_offset = pni->page_start_value;
           }
@@ -1556,11 +1556,13 @@ __global__ void __launch_bounds__(block_size)
   bool const has_repetition   = chunks[pp->chunk_idx].max_level[level_type::REPETITION] > 0;
   compute_string_sizes = compute_string_sizes && ((s->col.data_type & 7) == BYTE_ARRAY && s->dtype_len != 4);  
 
-  // if this is a flat hierarchy (no lists) and is not a string column, compute the size directly
-  // from the number of values.
-  if (!has_repetition && !compute_string_sizes) {
+  // reasons we might want to early out:
+  // - if this is a flat hierarchy (no lists) and is not a string column. in this case we don't need to do 
+  //   the expensive work of traversing the level data to determine sizes.  we can just compute it directly.
+  // - if this is the trim pass and we have no rows to output for this page.
+  if (!has_repetition && !compute_string_sizes) {    
     if (!t) {
-      // note: doing this for all nesting level because we can still have structs even if we don't
+      // note: doing this for all nesting levels because we can still have structs even if we don't
       // have lists.
       for (size_type idx = 0; idx < pp->num_nesting_levels; idx++) {
         pp->nesting[idx].size = pp->num_input_values;
@@ -1588,6 +1590,10 @@ __global__ void __launch_bounds__(block_size)
       s->num_rows              = INT_MAX;
       s->row_index_lower_bound = -1;
     }
+  }
+  // if we have no work to do for this page.
+  if(!compute_num_rows_pass && s->num_rows == 0){
+    return;
   }
   __syncthreads();
 
@@ -1669,6 +1675,11 @@ __global__ void __launch_bounds__(block_size) gpuDecodePageData(
   int out_thread0;
 
   if (!setupLocalPageInfo(s, &pages[page_idx], chunks, min_row, num_rows, true)) { return; }
+
+  // if we have no rows to do (eg, in a skip_rows/num_rows case)
+  if(s->num_rows == 0){
+    return;
+  }
 
   if (s->dict_base) {
     out_thread0 = (s->dict_bits > 0) ? 64 : 32;
