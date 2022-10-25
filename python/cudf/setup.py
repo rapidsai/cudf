81e60a813ec141d44db8dcfb02c2264be5872304
Vyas Ramasubramani <vyasr@nvidia.com>
2022-10-25 08:46:49 -0700

    Clean up setup.py.

--- python/cudf/setup.py
+++ python/cudf/setup.py
@@ -3,14 +3,10 @@
 import os
 import re
 import shutil
-import subprocess
-import sys
-from distutils.spawn import find_executable
 
 import versioneer
 from setuptools import find_packages
 from skbuild import setup
-from skbuild.command.build_ext import build_ext
 
 install_requires = [
     "cachetools",
