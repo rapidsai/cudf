f764578790e382f28d4d126defb56edb6a1ae54d
Vyas Ramasubramani <vyasr@nvidia.com>
2022-10-25 09:18:34 -0700

    Some cleanup and explicitly mark file as proto2 type.

--- python/cudf/cmake/Modules/ProtobufHelpers.cmake
+++ python/cudf/cmake/Modules/ProtobufHelpers.cmake
@@ -1,21 +1,21 @@
-#=============================================================================
+# =============================================================================
 # Copyright (c) 2022, NVIDIA CORPORATION.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#=============================================================================
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+# =============================================================================
 include_guard(GLOBAL)
 
+# Compile protobuf files to Python. All arguments are assumed to be .proto files.
 function(codegen_protoc)
+  # Allow user to provide path to protoc executable as an environment variable.
   if(DEFINED ENV{PROTOC})
     set(protoc_COMMAND $ENV{PROTOC})
   else()
@@ -27,19 +27,23 @@ function(codegen_protoc)
     set(pb2_py_path "${CMAKE_CURRENT_SOURCE_DIR}/${pb2_py_path}")
     execute_process(
       COMMAND ${protoc_COMMAND} --python_out=. "${_proto_path}"
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      ECHO_ERROR_VARIABLE
-      COMMAND_ECHO STDOUT
-      COMMAND_ERROR_IS_FATAL ANY
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} COMMAND_ERROR_IS_FATAL ANY
     )
+    # Mark entire file to skip formatting.
     file(READ "${pb2_py_path}" pb2_py)
-    file(WRITE "${pb2_py_path}" [=[
+    file(
+      WRITE "${pb2_py_path}"
+      [=[
 # flake8: noqa
 # fmt: off
-]=])
+]=]
+    )
     file(APPEND "${pb2_py_path}" "${pb2_py}")
-    file(APPEND "${pb2_py_path}" [=[
+    file(
+      APPEND "${pb2_py_path}"
+      [=[
 # fmt: on
-]=])
+]=]
+    )
   endforeach()
 endfunction()
