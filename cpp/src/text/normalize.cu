86e445bf30ff0ee0a90b6475ec65b8790d2214f4
David Wendt <dwendt@nvidia.com>
2022-10-26 17:53:17 -0400

    Remove default parameters for nvtext::detail functions

--- cpp/src/text/normalize.cu
+++ cpp/src/text/normalize.cu
@@ -170,10 +170,9 @@ struct codepoint_to_utf8_fn {
 }  // namespace
 
 // detail API
-std::unique_ptr<cudf::column> normalize_spaces(
-  cudf::strings_column_view const& strings,
-  rmm::cuda_stream_view stream,
-  rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource())
+std::unique_ptr<cudf::column> normalize_spaces(cudf::strings_column_view const& strings,
+                                               rmm::cuda_stream_view stream,
+                                               rmm::mr::device_memory_resource* mr)
 {
   if (strings.is_empty()) return cudf::make_empty_column(cudf::data_type{cudf::type_id::STRING});
 
