26f120fa9c3d1d7dcf53ec91317faa2a648a8bb4
AJ Schmidt <aschmidt@nvidia.com>
2022-10-27 12:12:37 -0400

    skip import tests for now

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -11,25 +11,30 @@ rapids-logger "Begin py build"
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
 rapids-mamba-retry mambabuild \
+  --no-test \
   --channel "${CPP_CHANNEL}" \
   conda/recipes/cudf
 
 rapids-mamba-retry mambabuild \
+  --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/dask-cudf
 
 rapids-mamba-retry mambabuild \
+  --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/cudf_kafka
 
 rapids-mamba-retry mambabuild \
+  --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/custreamz
 
 rapids-mamba-retry mambabuild \
+  --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/strings_udf
