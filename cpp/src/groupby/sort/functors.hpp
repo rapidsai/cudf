2cdcb50a36e52c12294e6e55b31d2a6642bf8fc5
Yunsong Wang <yunsongw@nvidia.com>
2022-10-27 15:22:48 -0400

    Add pragma once for functor header

--- cpp/src/groupby/sort/functors.hpp
+++ cpp/src/groupby/sort/functors.hpp
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#pragma once
+
 #include <cudf/column/column.hpp>
 #include <cudf/column/column_view.hpp>
 #include <cudf/detail/aggregation/result_cache.hpp>
