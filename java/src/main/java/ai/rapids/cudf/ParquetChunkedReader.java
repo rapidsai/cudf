5bb755ed82c9120ee756df94b232dec1cf63add7
Nghia Truong <nghiatruong.vn@gmail.com>
2022-10-31 10:35:44 -0700

    Update Javadoc
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- java/src/main/java/ai/rapids/cudf/ParquetChunkedReader.java
+++ java/src/main/java/ai/rapids/cudf/ParquetChunkedReader.java
@@ -21,37 +21,43 @@ package ai.rapids.cudf;
 import java.io.File;
 
 /**
- * TODO
+ * Provide an interface for reading a Parquet file in an iterative manner.
  */
 public class ParquetChunkedReader implements AutoCloseable {
   static {
     NativeDepsLoader.loadNativeDeps();
   }
 
-
-
   /**
-   * TODO
+   * Construct the reader instance from a read limit and a file path.
+   *
+   * @param chunkSizeByteLimit Limit on total number of bytes to be returned per read.
+   * @param filePath Full path of the input Parquet file to read.
    */
-  public ParquetChunkedReader(long chunkSizeByteLimit, File path) {
-    this(chunkSizeByteLimit, ParquetOptions.DEFAULT, path);
+  public ParquetChunkedReader(long chunkSizeByteLimit, File filePath) {
+    this(chunkSizeByteLimit, ParquetOptions.DEFAULT, filePath);
   }
 
   /**
-   * TODO
+   * Construct the reader instance from a read limit, a ParquetOptions object, and a file path.
+   *
+   * @param chunkSizeByteLimit Limit on total number of bytes to be returned per read.
+   * @param opts The options for Parquet reading.
+   * @param filePath Full path of the input Parquet file to read.
    */
-  public ParquetChunkedReader(long chunkSizeByteLimit, ParquetOptions opts, File path) {
+  public ParquetChunkedReader(long chunkSizeByteLimit, ParquetOptions opts, File filePath) {
     handle = create(chunkSizeByteLimit, opts.getIncludeColumnNames(), opts.getReadBinaryAsString(),
-        path.getAbsolutePath(), 0, 0, opts.timeUnit().typeId.getNativeId());
+        filePath.getAbsolutePath(), 0, 0, opts.timeUnit().typeId.getNativeId());
   }
 
   /**
-   * TODO
-   * @param chunkSizeByteLimit Byte limit (ex: 1MB=1048576)
-   * @param opts
-   * @param buffer
-   * @param offset
-   * @param len
+   * Construct the reader instance from a read limit and a file already read in a memory buffer.
+   *
+   * @param chunkSizeByteLimit Limit on total number of bytes to be returned per read.
+   * @param opts The options for Parquet reading.
+   * @param buffer Raw Parquet file content.
+   * @param offset The starting offset into buffer.
+   * @param len The number of bytes to parse the given buffer.
    */
   public ParquetChunkedReader(long chunkSizeByteLimit, ParquetOptions opts, HostMemoryBuffer buffer,
       long offset, long len) {
@@ -60,14 +66,20 @@ public class ParquetChunkedReader implements AutoCloseable {
   }
 
   /**
-   * TODO
+   * Check if the given file has anything left to read.
+   *
+   * @return A boolean value indicating if there is more data to read from file.
    */
   public boolean hasNext() {
     return hasNext(handle);
   }
 
   /**
-   * TODO
+   * Read a chunk of rows in the given Parquet file such that the returning data has total size
+   * does not exceed the given read limit. If the given file has no data, or all data has been read
+   * before by previous calls to this function, a null Table will be returned.
+   *
+   * @return A table of new rows reading from the given file.
    */
   public Table readChunk() {
     long[] columnPtrs = readChunk(handle);
@@ -90,14 +102,15 @@ public class ParquetChunkedReader implements AutoCloseable {
   private long handle;
 
   /**
-   * TODO
-   * @param chunkSizeByteLimit     TODO
-   * @param filterColumnNames  name of the columns to read, or an empty array if we want to read
-   * @param binaryToString     whether to convert this column to String if binary
-   * @param filePath           the path of the file to read, or null if no path should be read.
-   * @param bufferAddrs        the address of the buffer to read from or 0 if we should not.
-   * @param length             the length of the buffer to read from.
-   * @param timeUnit           return type of TimeStamp in units
+   * Create a native chunked Parquet reader object on heap and return its memory address.
+   *
+   * @param chunkSizeByteLimit Limit on total number of bytes to be returned per read.
+   * @param filterColumnNames Name of the columns to read, or an empty array if we want to read all.
+   * @param binaryToString Whether to convert the corresponding column to String if it is binary.
+   * @param filePath Full path of the file to read, or given as null if reading from a buffer.
+   * @param bufferAddrs The address of a buffer to read from, or 0 if we are not using that buffer.
+   * @param length The length of the buffer to read from.
+   * @param timeUnit Return type of time unit for timestamps.
    */
   private static native long create(long chunkSizeByteLimit, String[] filterColumnNames,
       boolean[] binaryToString, String filePath, long bufferAddrs, long length, int timeUnit);
