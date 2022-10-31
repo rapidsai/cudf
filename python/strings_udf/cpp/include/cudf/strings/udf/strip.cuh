e0526e6afa4774c179569089de19ab469a9489fe
David Wendt <dwendt@nvidia.com>
2022-10-31 19:16:29 -0400

    remove lstrip and rstrip

--- python/strings_udf/cpp/include/cudf/strings/udf/strip.cuh
+++ python/strings_udf/cpp/include/cudf/strings/udf/strip.cuh
@@ -41,6 +41,26 @@ namespace udf {
  * // result is "b" ('a' or ' ' removed from the ends)
  * @endcode
  *
+ * @code{.cpp}
+ * auto d_str = cudf::string_view{" aba ", 5};
+ * auto d_to_strip = cudf::string_view{}; // empty string
+ * auto result = strip(d_str, d_to_strip, side_type::LEFT);
+ * // result is "aba "
+ * d_to_strip = cudf::string_view{"a ", 2}; // 'a' and space
+ * result = strip(d_str, d_to_strip, side_type::LEFT);
+ * // result is "ba " ('a' or ' ' removed from the beginning)
+ * @endcode
+ *
+ * @code{.cpp}
+ * auto d_str = cudf::string_view{" aba ", 5};
+ * auto d_to_strip = cudf::string_view{}; // empty string
+ * auto result = strip(d_str, d_to_strip, side_type::RIGHT);
+ * // result is " aba"
+ * d_to_strip = cudf::string_view{" a", 2}; // space and 'a'
+ * result = rstrip(d_str, d_to_strip, side_type::RIGHT);
+ * // result is " ab" ('a' or ' ' removed from the end)
+ * @endcode
+ *
  * @param d_str String to strip characters from
  * @param d_to_strip Characters to remove
  * @param stype From where to strip the characters;
@@ -53,56 +73,6 @@ __device__ udf_string strip(cudf::string_view const d_str,
                             side_type stype = side_type::BOTH)
 {
   return udf_string{cudf::strings::detail::strip(d_str, d_to_strip, stype)};
-}
-
-/**
- * @brief Strip characters from the beginning of the given string
- *
- * The `d_to_strip` is interpreted as an array of characters to be removed.
- * If `d_to_strip` is an empty string, whitespace characters are stripped.
- *
- * @code{.cpp}
- * auto d_str = cudf::string_view{" aba ", 5};
- * auto d_to_strip = cudf::string_view{}; // empty string
- * auto result = lstrip(d_str, d_to_strip);
- * // result is "aba "
- * d_to_strip = cudf::string_view{"a ", 2}; // space and 'a'
- * result = lstrip(d_str, d_to_strip);
- * // result is "ba " ('a' or ' ' removed from the beginning)
- * @endcode
- *
- * @param d_str String to strip characters from
- * @param d_to_strip Characters to remove
- * @return New string with characters removed
- */
-__device__ udf_string lstrip(cudf::string_view const d_str, cudf::string_view d_to_strip)
-{
-  return strip(d_str, d_to_strip, side_type::LEFT);
-}
-
-/**
- * @brief Strip characters from the end of the given string
- *
- * The `d_to_strip` is interpreted as an array of characters to be removed.
- * If `d_to_strip` is an empty string, whitespace characters are stripped.
- *
- * @code{.cpp}
- * auto d_str = cudf::string_view{" aba ", 5};
- * auto d_to_strip = cudf::string_view{}; // empty string
- * auto result = rstrip(d_str, d_to_strip);
- * // result is " aba"
- * d_to_strip = cudf::string_view{" a", 2}; // space and 'a'
- * result = rstrip(d_str, d_to_strip);
- * // result is " ab" ('a' or ' ' removed from the end)
- * @endcode
- *
- * @param d_str String to strip characters from
- * @param d_to_strip Characters to remove
- * @return New string with characters removed
- */
-__device__ udf_string rstrip(cudf::string_view const d_str, cudf::string_view d_to_strip)
-{
-  return strip(d_str, d_to_strip, side_type::RIGHT);
 }
 
 }  // namespace udf
