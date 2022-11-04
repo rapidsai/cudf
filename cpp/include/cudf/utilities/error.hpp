2eb33430cc0d3855b98be50eb74d6b5d0c327e64
Vyas Ramasubramani <vyasr@nvidia.com>
2022-11-04 15:23:20 -0700

    Remove erroneous trailing semicolon.

--- cpp/include/cudf/utilities/error.hpp
+++ cpp/include/cudf/utilities/error.hpp
@@ -155,7 +155,7 @@ struct fatal_cuda_error : public cuda_error {
 
 #define CUDF_FAIL_2(_what, _exception_type)      \
   /*NOLINTNEXTLINE(bugprone-macro-parentheses)*/ \
-  throw _exception_type{"CUDF failure at:" __FILE__ ":" CUDF_STRINGIFY(__LINE__) ": " _what};
+  throw _exception_type { "CUDF failure at:" __FILE__ ":" CUDF_STRINGIFY(__LINE__) ": " _what }
 
 #define CUDF_FAIL_1(_what) CUDF_FAIL_2(_what, cudf::logic_error)
 
