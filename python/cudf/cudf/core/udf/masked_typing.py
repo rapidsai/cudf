7030108082cc2c7a54d1c722dab6a04c996280cb
brandon-b-miller <brmiller@nvidia.com>
2022-11-08 07:31:05 -0800

    plumb to maskedtype

--- python/cudf/cudf/core/udf/masked_typing.py
+++ python/cudf/cudf/core/udf/masked_typing.py
@@ -70,6 +70,7 @@ def _type_to_masked_type(t):
                 "attempting to use a column of unsupported dtype in a UDF. "
                 f"Supported dtypes are:\n{supported_type_str}"
             )
+            breakpoint()
             return types.Poison(err)
     else:
         return result
