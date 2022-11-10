0339b594f68074bf8189145540e17c013fd8bfc6
Vyas Ramasubramani <vyasr@nvidia.com>
2022-11-10 13:48:28 -0800

    Set cupy-cuda11x in dask-cudf.

--- python/dask_cudf/setup.py
+++ python/dask_cudf/setup.py
@@ -1,49 +1,9 @@
 # Copyright (c) 2019-2022, NVIDIA CORPORATION.
 
 import os
-import re
-import shutil
 
 import versioneer
 from setuptools import find_packages, setup
-
-CUDA_HOME = os.environ.get("CUDA_HOME", False)
-if not CUDA_HOME:
-    path_to_cuda_gdb = shutil.which("cuda-gdb")
-    if path_to_cuda_gdb is None:
-        raise OSError(
-            "Could not locate CUDA. "
-            "Please set the environment variable "
-            "CUDA_HOME to the path to the CUDA installation "
-            "and try again."
-        )
-    CUDA_HOME = os.path.dirname(os.path.dirname(path_to_cuda_gdb))
-
-if not os.path.isdir(CUDA_HOME):
-    raise OSError(f"Invalid CUDA_HOME: directory does not exist: {CUDA_HOME}")
-
-cuda_include_dir = os.path.join(CUDA_HOME, "include")
-
-
-def get_cuda_version_from_header(cuda_include_dir, delimeter=""):
-
-    cuda_version = None
-
-    with open(os.path.join(cuda_include_dir, "cuda.h"), encoding="utf-8") as f:
-        for line in f.readlines():
-            if re.search(r"#define CUDA_VERSION ", line) is not None:
-                cuda_version = line
-                break
-
-    if cuda_version is None:
-        raise TypeError("CUDA_VERSION not found in cuda.h")
-    cuda_version = int(cuda_version.split()[2])
-    return "%d%s%d" % (
-        cuda_version // 1000,
-        delimeter,
-        (cuda_version % 1000) // 10,
-    )
-
 
 install_requires = [
     "cudf",
@@ -53,10 +13,7 @@ install_requires = [
     "numpy",
     "pandas>=1.0,<1.6.0dev0",
     f"cudf{os.getenv('RAPIDS_PY_WHEEL_CUDA_SUFFIX', default='')}",
-    (
-        f"cupy-cuda{get_cuda_version_from_header(cuda_include_dir)}>=9.5.0,"
-        "<11.0.0a0; platform_machine=='x86_64'"
-    ),
+    "cupy-cuda11x",
 ]
 
 extras_require = {
