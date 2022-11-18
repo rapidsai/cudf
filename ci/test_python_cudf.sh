28a856cc10466af306eccb64d9e8974c4a4ef318
Bradley Dice <bdice@bradleydice.com>
2022-11-18 15:35:04 -0600

    Add benchmarks.

--- ci/test_python_cudf.sh
+++ ci/test_python_cudf.sh
@@ -38,4 +38,41 @@ if (( ${exitcode} != 0 )); then
 fi
 popd
 
+# Run benchmarks with both cudf and pandas to ensure compatibility is maintained.
+# Benchmarks are run in DEBUG_ONLY mode, meaning that only small data sizes are used.
+# Therefore, these runs only verify that benchmarks are valid.
+# They do not generate meaningful performance measurements.
+pushd python/cudf
+rapids-logger "pytest for cudf benchmarks"
+CUDF_BENCHMARKS_DEBUG_ONLY=ON \
+pytest \
+  --verbose \
+  --cache-clear \
+  --numprocesses=8 \
+  --dist=loadscope \
+  benchmarks
+exitcode=$?
+
+if (( ${exitcode} != 0 )); then
+    SUITEERROR=${exitcode}
+    echo "FAILED: 1 or more tests in cudf"
+fi
+
+rapids-logger "pytest for cudf benchmarks using pandas"
+CUDF_BENCHMARKS_USE_PANDAS=ON \
+CUDF_BENCHMARKS_DEBUG_ONLY=ON \
+pytest \
+  --verbose \
+  --cache-clear \
+  --numprocesses=8 \
+  --dist=loadscope \
+  benchmarks
+exitcode=$?
+
+if (( ${exitcode} != 0 )); then
+    SUITEERROR=${exitcode}
+    echo "FAILED: 1 or more tests in cudf"
+fi
+popd
+
 exit ${SUITEERROR}
