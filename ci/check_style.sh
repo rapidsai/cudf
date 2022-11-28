6c7f4c5196b9fd0c86aff92a11117053efcb32cc
AJ Schmidt <aschmidt@nvidia.com>
2022-11-28 14:08:38 -0500

    use `tee` for `check_style.sh` env

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -9,7 +9,7 @@ rapids-logger "Create checks conda environment"
 rapids-dependency-file-generator \
   --output conda \
   --file_key checks \
-  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" > env.yaml
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee env.yaml
 
 rapids-mamba-retry env create --force -f env.yaml -n checks
 conda activate checks
