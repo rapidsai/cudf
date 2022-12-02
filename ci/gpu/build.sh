b515fb07d3ea16f23c6a96728558af9e1a385747
AJ Schmidt <aschmidt@nvidia.com>
2022-12-02 17:14:20 -0500

    set `sccache` vars for jenkins builds

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -29,6 +29,9 @@ export CONDA_ARTIFACT_PATH="$WORKSPACE/ci/artifacts/cudf/cpu/.conda-bld/"
 # Workaround to keep Jenkins builds working
 # until we migrate fully to GitHub Actions
 export RAPIDS_CUDA_VERSION="${CUDA}"
+export SCCACHE_BUCKET=rapids-sccache
+export SCCACHE_REGION=us-west-2
+export SCCACHE_IDLE_TIMEOUT=32768
 
 # Make Jenkins use unique SCCACHE_S3_KEY_PREFIX compared to GH Actions
 sed -i '/SCCACHE_S3_KEY_PREFIX/ s| #|-jenkins #|g' conda/recipes/**/meta.yaml
