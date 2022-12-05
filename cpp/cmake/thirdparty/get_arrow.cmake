1bd1ea7dcd043fb1e78361b29e2bbe4ed6559d5f
galipremsagar <sagarprem75@gmail.com>
2022-12-04 21:18:05 -0800

    arrow 10.0.1

--- cpp/cmake/thirdparty/get_arrow.cmake
+++ cpp/cmake/thirdparty/get_arrow.cmake
@@ -365,7 +365,7 @@ endfunction()
 
 if(NOT DEFINED CUDF_VERSION_Arrow)
   set(CUDF_VERSION_Arrow
-      9.0.0
+      10.0.1
       CACHE STRING "The version of Arrow to find (or build)"
   )
 endif()
