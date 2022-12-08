347c3a5b79d9af6d840f697d7e39a68908a2aa7c
Nghia Truong <nghiatruong.vn@gmail.com>
2022-12-07 20:41:21 -0800

    Add tests for non-nested types
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/tests/lists/reverse_tests.cpp
+++ cpp/tests/lists/reverse_tests.cpp
@@ -17,23 +17,139 @@
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_utilities.hpp>
 #include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/iterator_utilities.hpp>
+#include <cudf_test/type_lists.hpp>
 
 #include <cudf/column/column.hpp>
 #include <cudf/copying.hpp>
 #include <cudf/lists/reverse.hpp>
 
-#include <vector>
+using namespace cudf::test::iterators;
+
+auto constexpr null{0};
 
 using ints_lists = cudf::test::lists_column_wrapper<int32_t>;
 
 struct ListsReverseTest : public cudf::test::BaseFixture {
 };
 
-TEST_F(ListsReverseTest, SimpleReverse)
-{
-  auto const input    = ints_lists{{}, {1, 2, 3}, {}, {4, 5}};
-  auto const expected = ints_lists{{}, {3, 2, 1}, {}, {5, 4}};
-  auto const results  = cudf::lists::reverse(cudf::lists_column_view(input));
+template <typename T>
+struct ListsReverseTypedTest : public cudf::test::BaseFixture {
+};
 
-  CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results);
+using TestTypes =
+  cudf::test::Concat<cudf::test::IntegralTypesNotBool, cudf::test::FloatingPointTypes>;
+
+TYPED_TEST_SUITE(ListsReverseTypedTest, TestTypes);
+
+TEST_F(ListsReverseTest, EmptyInput)
+{
+  // Empty input.
+  {
+    auto const input   = ints_lists{};
+    auto const results = cudf::lists::reverse(cudf::lists_column_view(input));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(input, *results);
+  }
+
+  // Empty lists.
+  {
+    auto const input   = ints_lists{ints_lists{}, ints_lists{}, ints_lists{}};
+    auto const results = cudf::lists::reverse(cudf::lists_column_view(input));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(input, *results);
+  }
+
+  // Empty nested lists.
+  {
+    auto const input   = ints_lists{ints_lists{ints_lists{}}, ints_lists{}, ints_lists{}};
+    auto const results = cudf::lists::reverse(cudf::lists_column_view(input));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(input, *results);
+  }
+}
+
+TYPED_TEST(ListsReverseTypedTest, InputNoNulls)
+{
+  using lists_col           = cudf::test::lists_column_wrapper<TypeParam>;
+  auto const input_original = lists_col{{}, {1, 2, 3}, {}, {4, 5}, {6, 7, 8}, {9}};
+
+  {
+    auto const expected = lists_col{{}, {3, 2, 1}, {}, {5, 4}, {8, 7, 6}, {9}};
+    auto const results  = cudf::lists::reverse(cudf::lists_column_view(input_original));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results);
+  }
+
+  {
+    auto const input    = cudf::slice(input_original, {1, 4})[0];
+    auto const expected = lists_col{{3, 2, 1}, {}, {5, 4}};
+    auto const results  = cudf::lists::reverse(cudf::lists_column_view(input));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results);
+  }
+
+  {
+    auto const input    = cudf::slice(input_original, {2, 3})[0];
+    auto const expected = lists_col{lists_col{}};
+    auto const results  = cudf::lists::reverse(cudf::lists_column_view(input));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results);
+  }
+
+  {
+    auto const input    = cudf::slice(input_original, {2, 4})[0];
+    auto const expected = lists_col{{}, {5, 4}};
+    auto const results  = cudf::lists::reverse(cudf::lists_column_view(input));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results);
+  }
+}
+
+TYPED_TEST(ListsReverseTypedTest, InputWithNulls)
+{
+  using lists_col           = cudf::test::lists_column_wrapper<TypeParam>;
+  auto const input_original = lists_col{{lists_col{},
+                                         lists_col{1, 2, 3},
+                                         lists_col{} /*null*/,
+                                         lists_col{{4, 5, null}, null_at(2)},
+                                         lists_col{6, 7, 8},
+                                         lists_col{9}},
+                                        null_at(2)};
+
+  {
+    auto const expected = lists_col{{lists_col{},
+                                     lists_col{3, 2, 1},
+                                     lists_col{} /*null*/,
+                                     lists_col{{null, 5, 4}, null_at(0)},
+                                     lists_col{8, 7, 6},
+                                     lists_col{9}},
+                                    null_at(2)};
+    auto const results  = cudf::lists::reverse(cudf::lists_column_view(input_original));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results);
+  }
+
+  {
+    auto const input    = cudf::slice(input_original, {1, 4})[0];
+    auto const expected = lists_col{
+      {lists_col{3, 2, 1}, lists_col{} /*null*/, lists_col{{null, 5, 4}, null_at(0)}}, null_at(1)};
+    auto const results = cudf::lists::reverse(cudf::lists_column_view(input));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results);
+  }
+
+  {
+    auto const input    = cudf::slice(input_original, {2, 3})[0];
+    auto const expected = lists_col{{lists_col{} /*null*/}, null_at(0)};
+    auto const results  = cudf::lists::reverse(cudf::lists_column_view(input));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results);
+  }
+
+  {
+    auto const input = cudf::slice(input_original, {2, 4})[0];
+    auto const expected =
+      lists_col{{lists_col{} /*null*/, lists_col{{null, 5, 4}, null_at(0)}}, null_at(0)};
+    auto const results = cudf::lists::reverse(cudf::lists_column_view(input));
+    CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *results);
+  }
+
+  {
+    auto const input    = cudf::slice(input_original, {4, 6})[0];
+    auto const expected = lists_col{{8, 7, 6}, {9}};
+    auto const results  = cudf::lists::reverse(cudf::lists_column_view(input));
+    // The result doesn't have nulls, but it is nullable.
+    CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(expected, *results);
+  }
 }
