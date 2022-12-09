d6e59fac77853c7e83e0589ead207b95455350d4
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2022-12-09 22:58:57 +0530

    use cudf::io::parse_options once to avoid repeated copying for each column

--- cpp/src/io/json/json_column.cu
+++ cpp/src/io/json/json_column.cu
@@ -560,7 +560,7 @@ cudf::io::parse_options parsing_options(cudf::io::json_reader_options const& opt
 std::pair<std::unique_ptr<column>, std::vector<column_name_info>> device_json_column_to_cudf_column(
   device_json_column& json_col,
   device_span<SymbolT const> d_input,
-  cudf::io::json_reader_options const& options,
+  cudf::io::parse_options const& options,
   std::optional<schema_element> schema,
   rmm::cuda_stream_view stream,
   rmm::mr::device_memory_resource* mr)
@@ -623,14 +623,14 @@ std::pair<std::unique_ptr<column>, std::vector<column_name_info>> device_json_co
       // Infer column type, if we don't have an explicit type for it
       else {
         target_type = cudf::io::detail::infer_data_type(
-          parsing_options(options).json_view(), d_input, string_ranges_it, col_size, stream);
+          options.json_view(), d_input, string_ranges_it, col_size, stream);
       }
       // Convert strings to the inferred data type
       auto col = experimental::detail::parse_data(string_spans_it,
                                                   col_size,
                                                   target_type,
                                                   make_validity(json_col).first,
-                                                  parsing_options(options).view(),
+                                                  options.view(),
                                                   stream,
                                                   mr);
 
@@ -762,6 +762,7 @@ table_with_metadata device_parse_nested_json(device_span<SymbolT const> d_input,
   // Initialize meta data to be populated while recursing through the tree of columns
   std::vector<std::unique_ptr<column>> out_columns;
   std::vector<column_name_info> out_column_names;
+  auto parse_opt = parsing_options(options);
 
   // Iterate over the struct's child columns and convert to cudf column
   size_type column_index = 0;
@@ -813,7 +814,7 @@ table_with_metadata device_parse_nested_json(device_span<SymbolT const> d_input,
 
     // Get this JSON column's cudf column and schema info, (modifies json_col)
     auto [cudf_col, col_name_info] = device_json_column_to_cudf_column(
-      json_col, d_input, options, child_schema_element, stream, mr);
+      json_col, d_input, parse_opt, child_schema_element, stream, mr);
 
     out_column_names.back().children = std::move(col_name_info);
     out_columns.emplace_back(std::move(cudf_col));
