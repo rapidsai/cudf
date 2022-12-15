96105065d37f2ed44674e1a1b1254f32fb351b90
Nghia Truong <nghiatruong.vn@gmail.com>
2022-12-15 11:41:01 -0800

    Rename `may_need_sanitize` to `may_contain_non_empty_nulls`
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/src/structs/utilities.cpp
+++ cpp/src/structs/utilities.cpp
@@ -216,31 +216,31 @@ flattened_table flatten_nested_columns(table_view const& input,
 namespace {
 
 /**
- * @brief Dispatcher to conservatively check if the input column needs to be sanitized (i.e., if it
- * may contain non-empty nulls).
+ * @brief Dispatcher to conservatively check if the input column may contain any non-empty nulls.
  *
  * For performance reason, no non-empty null will actually be checked. Instead, this will perform
  * conservative checking to see if the input column contains any lists or strings column that is
  * nulllable. As such, it may return false positive answer.
  */
-struct may_need_sanitize_dispatch {
+struct may_contain_non_empty_nulls_dispatch {
   template <typename T>
   bool operator()(column_view const& col) const
   {
     return std::any_of(col.child_begin(), col.child_end(), [](auto const& child) {
-      return type_dispatcher(child.type(), may_need_sanitize_dispatch{}, child);
+      return type_dispatcher(child.type(), may_contain_non_empty_nulls_dispatch{}, child);
     });
   }
 };
 
 template <>
-bool may_need_sanitize_dispatch::operator()<cudf::string_view>(column_view const& col) const
+bool may_contain_non_empty_nulls_dispatch::operator()<cudf::string_view>(
+  column_view const& col) const
 {
   return col.nullable();
 }
 
 template <>
-bool may_need_sanitize_dispatch::operator()<cudf::list_view>(column_view const& col) const
+bool may_contain_non_empty_nulls_dispatch::operator()<cudf::list_view>(column_view const& col) const
 {
   // A lists column may have empty data (i.e., having child column of type EMPTY).
   // Thus, it needs to be sanitized only if the child column is not empty.
@@ -249,19 +249,18 @@ bool may_need_sanitize_dispatch::operator()<cudf::list_view>(column_view const& 
 }
 
 /**
- * @brief Conservatively check if the input column needs to be sanitized (i.e., if it may contain
- * non-empty nulls).
+ * @brief Conservatively check if the input column may contain any non-empty nulls.
  *
- * This will just call `type_dispatcher` on the `may_need_sanitize_dispatch` functor.
+ * This will just call `type_dispatcher` on the `may_contain_non_empty_nulls_dispatch` functor.
  *
  * @param col The input column to check
  */
-bool may_need_sanitize(column_view const& col)
+bool may_contain_non_empty_nulls(column_view const& col)
 {
   // Type EMPTY is not handled by `type_dispatcher`.
   if (col.type().id() == type_id::EMPTY) { return false; }
 
-  return type_dispatcher(col.type(), may_need_sanitize_dispatch{}, col);
+  return type_dispatcher(col.type(), may_contain_non_empty_nulls_dispatch{}, col);
 }
 
 /**
@@ -427,7 +426,7 @@ std::unique_ptr<column> superimpose_nulls(bitmask_type const* null_mask,
 {
   input = superimpose_nulls_no_sanitize(null_mask, null_count, std::move(input), stream, mr);
 
-  if (auto const input_view = input->view(); may_need_sanitize(input_view)) {
+  if (auto const input_view = input->view(); may_contain_non_empty_nulls(input_view)) {
     // We can't call `purge_nonempty_nulls` for individual child column(s) that need to be
     // sanitized. Instead, we have to call it from the top level column.
     // This is to make sure all the columns (top level + all children) have consistent offsets.
@@ -445,7 +444,7 @@ std::pair<column_view, temporary_nullable_data> push_down_nulls(column_view cons
 {
   auto output = push_down_nulls_no_sanitize(input, stream, mr);
 
-  if (auto const output_view = output.first; may_need_sanitize(output_view)) {
+  if (auto const output_view = output.first; may_contain_non_empty_nulls(output_view)) {
     output.second.new_columns.emplace_back(
       cudf::detail::purge_nonempty_nulls(output_view, stream, mr));
     output.first = output.second.new_columns.back()->view();
