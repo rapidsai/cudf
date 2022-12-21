8fa84711d05f5eba3ba589edfaafd06b71f723ae
Vyas Ramasubramani <vyasr@nvidia.com>
2022-12-21 15:15:41 -0800

    Define a Python-accessible enum for working with data types.

--- python/cudf/cudf/_lib/pylibcudf/column_view.pyx
+++ python/cudf/cudf/_lib/pylibcudf/column_view.pyx
@@ -1,11 +1,48 @@
 # Copyright (c) 2022, NVIDIA CORPORATION.
 
-from libc.stdint cimport uintptr_t
+from enum import IntEnum
+
+from libc.stdint cimport int32_t, uintptr_t
 from libcpp.memory cimport unique_ptr
 from libcpp.vector cimport vector
 
 from cudf._lib.cpp.column.column_view cimport column_view
 from cudf._lib.cpp.types cimport bitmask_type, data_type, size_type, type_id
+
+
+# TODO: Unclear what the perf impacts of using pure Python enums here will be,
+# but I think it will be necessary because ultimately we need these to be
+# constructible from cudf (not just pylibcudf or custom Cython code using it).
+class TypeId(IntEnum):
+    EMPTY = type_id.EMPTY
+    INT8 = type_id.INT8
+    INT16 = type_id.INT16
+    INT32 = type_id.INT32
+    INT64 = type_id.INT64
+    UINT8 = type_id.UINT8
+    UINT16 = type_id.UINT16
+    UINT32 = type_id.UINT32
+    UINT64 = type_id.UINT64
+    FLOAT32 = type_id.FLOAT32
+    FLOAT64 = type_id.FLOAT64
+    BOOL8 = type_id.BOOL8
+    TIMESTAMP_DAYS = type_id.TIMESTAMP_DAYS
+    TIMESTAMP_SECONDS = type_id.TIMESTAMP_SECONDS
+    TIMESTAMP_MILLISECONDS = type_id.TIMESTAMP_MILLISECONDS
+    TIMESTAMP_MICROSECONDS = type_id.TIMESTAMP_MICROSECONDS
+    TIMESTAMP_NANOSECONDS = type_id.TIMESTAMP_NANOSECONDS
+    DICTIONARY32 = type_id.DICTIONARY32
+    STRING = type_id.STRING
+    LIST = type_id.LIST
+    STRUCT = type_id.STRUCT
+    NUM_TYPE_IDS = type_id.NUM_TYPE_IDS
+    DURATION_SECONDS = type_id.DURATION_SECONDS
+    DURATION_MILLISECONDS = type_id.DURATION_MILLISECONDS
+    DURATION_MICROSECONDS = type_id.DURATION_MICROSECONDS
+    DURATION_NANOSECONDS = type_id.DURATION_NANOSECONDS
+    DECIMAL32 = type_id.DECIMAL32
+    DECIMAL64 = type_id.DECIMAL64
+    DECIMAL128 = type_id.DECIMAL128
 
 
 # Helpers
@@ -15,21 +52,33 @@ cdef void * int_to_void_ptr(ptr):
     # intermediate cast to a uintptr_t is necessary
     return <void*><uintptr_t>(ptr)
 
+
 cdef bitmask_type * int_to_bitmask_ptr(ptr):
     # Cython will not cast a Python integer directly to a pointer, so the
     # intermediate cast to a uintptr_t is necessary
     return <bitmask_type*><uintptr_t>(ptr)
 
+
+# Cython doesn't support scoped enumerations. It assumes that enums correspond
+# to their underlying value types and will thus attempt operations that are
+# invalid. This code will ensure that these values are explicitly cast to the
+# underlying type before casting to the final type.
+ctypedef int32_t underlying_type_t_type_id
+cdef type_id py_type_to_c_type(py_type_id):
+    return <type_id> (<underlying_type_t_type_id> py_type_id)
+
+
 cdef class ColumnView:
     """Wrapper around column_view."""
     cdef unique_ptr[column_view] * c_obj
 
-    def __cinit__(self, object data_buf, object mask_buf):
-        # TODO: For now assuming data is a Buffer, but eventually need to
-        # define a new gpumemoryview type to handle this. For that object it
-        # should be possible to access all attributes via fast cdef functions
-        # (no Python overhead for querying size etc).
-        cdef data_type dtype = data_type(type_id.INT32)
+    # TODO: For now assuming data and mask are Buffers, but eventually need to
+    # define a new gpumemoryview type to handle this. For that object it should
+    # be possible to access all attributes via fast cdef functions (no Python
+    # overhead for querying size etc).
+    def __cinit__(self, py_type_id, object data_buf, object mask_buf):
+        cdef type_id c_type_id = py_type_to_c_type(py_type_id)
+        cdef data_type dtype = data_type(c_type_id)
         cdef size_type size = data_buf.size
         cdef const void * data = int_to_void_ptr(data_buf.ptr)
         cdef const bitmask_type * null_mask
