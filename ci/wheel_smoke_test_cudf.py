782b2afe0ce265ba853ee9c0881d3a7199703c51
Sevag Hanssian <shanssian@nvidia.com>
2022-12-21 15:04:00 -0500

    Move smoke test to its own file

---
+++ ci/wheel_smoke_test_cudf.py
@@ -0,0 +1,18 @@
+import cudf
+import pyarrow as pa
+
+if __name__ == '__main__':
+    n_legs = pa.array([2, 4, 5, 100])
+    animals = pa.array(["Flamingo", "Horse", "Brittle stars", "Centipede"])
+    names = ["n_legs", "animals"]
+
+    print(animals)
+
+    foo = pa.table([n_legs, animals], names=names)
+    print(foo)
+
+    df = cudf.DataFrame.from_arrow(foo)
+    print(df)
+    print(df.loc[df["animals"] == "Centipede"]["n_legs"])
+
+    assert df.loc[df["animals"] == "Centipede"]["n_legs"].iloc[0] == 100
