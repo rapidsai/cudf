e51e14951a53d2d6798253062ae830cee805d91e
Nghia Truong <nghiatruong.vn@gmail.com>
2022-12-27 11:18:42 -0800

    Fix compile issue
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/tests/utilities/column_utilities.cu
+++ cpp/tests/utilities/column_utilities.cu
@@ -1274,35 +1274,35 @@ std::vector<std::string> to_strings(cudf::column_view const& col)
 std::string to_string(cudf::type_id type)
 {
   switch (type) {
-    case EMPTY: return "EMPTY";
-    case INT8: return "INT8";
-    case INT16: return "INT16 ";
-    case INT32: return "INT32";
-    case INT64: return "INT32";
-    case UINT8: return "UINT8";
-    case UINT16: return "UINT16";
-    case UINT32: return "UINT32";
-    case UINT64: return "UINT64";
-    case FLOAT32: return "FLOAT32";
-    case FLOAT64: return "FLOAT64";
-    case BOOL8: return "BOOL8";
-    case TIMESTAMP_DAYS: return "TIMESTAMP_DAYS";
-    case TIMESTAMP_SECONDS: return "TIMESTAMP_SECONDS";
-    case TIMESTAMP_MILLISECONDS: return "TIMESTAMP_MILLISECONDS";
-    case TIMESTAMP_MICROSECONDS: return "TIMESTAMP_MICROSECONDS";
-    case TIMESTAMP_NANOSECONDS: return "TIMESTAMP_NANOSECONDS";
-    case DURATION_DAYS: return "DURATION_DAYS";
-    case DURATION_SECONDS: return "DURATION_SECONDS";
-    case DURATION_MILLISECONDS: return "DURATION_MILLISECONDS";
-    case DURATION_MICROSECONDS: return "DURATION_MICROSECONDS";
-    case DURATION_NANOSECONDS: return "DURATION_NANOSECONDS";
-    case DICTIONARY32: return "DICTIONARY32";
-    case STRING: return "STRING";
-    case LIST: return "LIST";
-    case DECIMAL32: return "DECIMAL32";
-    case DECIMAL64: return "DECIMAL64";
-    case DECIMAL128: return "DECIMAL128";
-    case STRUCT: return "STRUCT";
+    case cudf::type_id::EMPTY: return "EMPTY";
+    case cudf::type_id::INT8: return "INT8";
+    case cudf::type_id::INT16: return "INT16 ";
+    case cudf::type_id::INT32: return "INT32";
+    case cudf::type_id::INT64: return "INT32";
+    case cudf::type_id::UINT8: return "UINT8";
+    case cudf::type_id::UINT16: return "UINT16";
+    case cudf::type_id::UINT32: return "UINT32";
+    case cudf::type_id::UINT64: return "UINT64";
+    case cudf::type_id::FLOAT32: return "FLOAT32";
+    case cudf::type_id::FLOAT64: return "FLOAT64";
+    case cudf::type_id::BOOL8: return "BOOL8";
+    case cudf::type_id::TIMESTAMP_DAYS: return "TIMESTAMP_DAYS";
+    case cudf::type_id::TIMESTAMP_SECONDS: return "TIMESTAMP_SECONDS";
+    case cudf::type_id::TIMESTAMP_MILLISECONDS: return "TIMESTAMP_MILLISECONDS";
+    case cudf::type_id::TIMESTAMP_MICROSECONDS: return "TIMESTAMP_MICROSECONDS";
+    case cudf::type_id::TIMESTAMP_NANOSECONDS: return "TIMESTAMP_NANOSECONDS";
+    case cudf::type_id::DURATION_DAYS: return "DURATION_DAYS";
+    case cudf::type_id::DURATION_SECONDS: return "DURATION_SECONDS";
+    case cudf::type_id::DURATION_MILLISECONDS: return "DURATION_MILLISECONDS";
+    case cudf::type_id::DURATION_MICROSECONDS: return "DURATION_MICROSECONDS";
+    case cudf::type_id::DURATION_NANOSECONDS: return "DURATION_NANOSECONDS";
+    case cudf::type_id::DICTIONARY32: return "DICTIONARY32";
+    case cudf::type_id::STRING: return "STRING";
+    case cudf::type_id::LIST: return "LIST";
+    case cudf::type_id::DECIMAL32: return "DECIMAL32";
+    case cudf::type_id::DECIMAL64: return "DECIMAL64";
+    case cudf::type_id::DECIMAL128: return "DECIMAL128";
+    case cudf::type_id::STRUCT: return "STRUCT";
     default: return "INVALID_TYPE";
   }
 }
