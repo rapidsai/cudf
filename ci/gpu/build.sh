38fa6d24dfe519dfcf03183932646dde3d3aecff
divyegala <divyegala@gmail.com>
2023-01-04 12:26:02 -0800

    pre-commit hook

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2018-2022, NVIDIA CORPORATION.
+# Copyright (c) 2018-2023, NVIDIA CORPORATION.
 ##############################################
 # cuDF GPU build and test script for CI      #
 ##############################################
