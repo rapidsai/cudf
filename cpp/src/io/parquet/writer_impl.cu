699127e7999788b49c8c0fa35097cf4f6a9902f5
Ed Seidl <etseidl@users.noreply.github.com>
2023-01-04 17:51:38 -0800

    implement suggestion from review
    Co-authored-by: Vukasin Milovanovic <vukasin.milovanovic.87@gmail.com>

--- cpp/src/io/parquet/writer_impl.cu
+++ cpp/src/io/parquet/writer_impl.cu
@@ -1105,7 +1105,7 @@ build_chunk_dictionaries(hostdevice_2dvector<gpu::EncColumnChunk>& chunks,
       // don't use dictionary if it gets too large for the given compression codec
       if (dict_policy == dictionary_policy::ADAPTIVE) {
         auto const unique_size = static_cast<size_t>(ck.uniq_data_size);
-        if (unique_size > max_page_bytes(compression, std::min(unique_size, max_dict_size))) {
+        if (unique_size > max_page_bytes(compression, max_dict_size)) {
           return {false, 0};
         }
       }
