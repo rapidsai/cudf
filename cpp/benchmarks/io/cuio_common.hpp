b621be5c881041ec96fdd316d08a9b6be93cee7b
Yunsong Wang <yunsongw@nvidia.com>
2023-01-09 17:29:43 -0500

    Remove unused macro

--- cpp/benchmarks/io/cuio_common.hpp
+++ cpp/benchmarks/io/cuio_common.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2022, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,10 +23,6 @@
 #include <cudf/io/types.hpp>
 
 using cudf::io::io_type;
-
-#define RD_BENCHMARK_DEFINE_ALL_SOURCES(benchmark, name, type_or_group)                  \
-  benchmark(name##_file_input, type_or_group, static_cast<uint32_t>(io_type::FILEPATH)); \
-  benchmark(name##_buffer_input, type_or_group, static_cast<uint32_t>(io_type::HOST_BUFFER));
 
 #define WR_BENCHMARK_DEFINE_ALL_SINKS(benchmark, name, type_or_group)                          \
   benchmark(name##_file_output, type_or_group, static_cast<uint32_t>(io_type::FILEPATH));      \
