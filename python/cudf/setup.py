66b846a01d418c51fb01126218d6b5e3fd83d405
Sevag H <shanssian@nvidia.com>
2023-01-10 11:49:31 -0500

    Build wheels alongside conda CI (#12427)
    This PR adds pip wheel CI to the Conda CI, instead of having them work separately.
    
    Authors:
      - Sevag H (https://github.com/sevagh)
      - AJ Schmidt (https://github.com/ajschmidt8)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - AJ Schmidt (https://github.com/ajschmidt8)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/cudf/pull/12427

--- python/cudf/setup.py
+++ python/cudf/setup.py
@@ -19,7 +19,7 @@ install_requires = [
     "pandas>=1.0,<1.6.0dev0",
     "protobuf==4.21",
     "typing_extensions",
-    "pyarrow==10",
+    "pyarrow==10.0.1",
     f"rmm{cuda_suffix}",
     f"ptxcompiler{cuda_suffix}",
     f"cubinlinker{cuda_suffix}",
