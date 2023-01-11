e514e4eabb5d06b6f430a86997ed1fb45a30038a
MithunR <mythrocks@gmail.com>
2023-01-11 15:39:59 -0800

    Test for custom field delimiter.

--- cpp/tests/io/csv_test.cpp
+++ cpp/tests/io/csv_test.cpp
@@ -453,10 +453,13 @@ TYPED_TEST(CsvFixedPointWriterTest, SingleColumnPositiveScale)
   EXPECT_EQ(result_strings, reference_strings);
 }
 
-TEST_F(CsvWriterTest, QuotingDisabled)
+void test_quoting_disabled_with_delimiter(char delimiter_char)
 {
+  auto const delimiter = std::string{delimiter_char};
   auto const input_strings = cudf::test::strings_column_wrapper{
-    "All,the,leaves", "are\"brown", "and\nthe\nsky\nis\ngrey"
+    std::string{"All"} + delimiter + "the" + delimiter + "leaves",
+    "are\"brown", 
+    "and\nthe\nsky\nis\ngrey"
   };
   auto const input_table = table_view{{input_strings}};
 
@@ -464,11 +467,13 @@ TEST_F(CsvWriterTest, QuotingDisabled)
   auto w_options = cudf::io::csv_writer_options::builder(cudf::io::sink_info{filepath},
                                                          input_table)
                                                 .include_header(false)
+                                                .inter_column_delimiter(delimiter_char)
                                                 .enable_quote_strings(false);
   cudf::io::write_csv(w_options.build());
 
   auto r_options = cudf::io::csv_reader_options::builder(cudf::io::source_info{filepath})
                                                 .header(-1)
+                                                .delimiter(delimiter_char)
                                                 .quoting(cudf::io::quote_style::NONE);
   auto r_table = cudf::io::read_csv(r_options.build());
 
@@ -476,6 +481,12 @@ TEST_F(CsvWriterTest, QuotingDisabled)
     "All", "are\"brown", "and", "the", "sky", "is", "grey"
   };
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(r_table.tbl->view().column(0), expected);
+}
+
+TEST_F(CsvWriterTest, QuotingDisabled)
+{
+  test_quoting_disabled_with_delimiter(',');
+  test_quoting_disabled_with_delimiter('\u0001');
 }
 
 TEST_F(CsvReaderTest, MultiColumn)
