8fad03bf9fd6b53e539c064217ec6fa3a104dc9a
MithunR <mythrocks@gmail.com>
2023-01-11 15:46:58 -0800

    Formatting.

--- cpp/include/cudf/io/csv.hpp
+++ cpp/include/cudf/io/csv.hpp
@@ -1447,9 +1447,9 @@ class csv_writer_options {
   /**
    * @brief Whether or not string rows containing the delimiter, quote, or newline
    * are to be quoted.
-   * 
+   *
    * @return true If string rows should be quoted
-   * @return false If string rows should not be quoted, even if they have special 
+   * @return false If string rows should not be quoted, even if they have special
    *         characters.
    */
   [[nodiscard]] bool is_enabled_quote_strings() const { return _quote_strings; }
@@ -1521,7 +1521,7 @@ class csv_writer_options {
   /**
    * @brief Whether string rows containing the delimiter, quote, or newline
    * should be quoted
-   * 
+   *
    * @param is_enabled_quoting Set to `true` to enable quoting, `false` otherwise
    */
   void enable_quote_strings(bool is_enabled_quoting) { _quote_strings = is_enabled_quoting; }
@@ -1651,7 +1651,7 @@ class csv_writer_options_builder {
   /**
    * @brief Whether string rows containing the delimiter, quote, or newline
    * should be quoted
-   * 
+   *
    * @param is_enabled_quoting Set to `true` to enable quoting, `false` otherwise
    */
   csv_writer_options_builder& enable_quote_strings(bool is_enabled_quoting)
