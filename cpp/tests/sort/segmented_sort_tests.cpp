869f38c684865e9cfed1c6515e9dea8c8534e7b9
David Wendt <dwendt@nvidia.com>
2023-01-25 14:57:53 -0500

    Add comment about CUB patch for SegmentedSortInt.Bool gtest

--- cpp/tests/sort/segmented_sort_tests.cpp
+++ cpp/tests/sort/segmented_sort_tests.cpp
@@ -318,6 +318,8 @@ TEST_F(SegmentedSortInt, ErrorsMismatchArgSizes)
   CUDF_EXPECT_NO_THROW(cudf::stable_segmented_sort_by_key(input1, input1, segments));
 }
 
+// Test specifically verifies the patch added in https://github.com/rapidsai/cudf/pull/12234
+// This test will fail if CUB bug fix is not available or the patch has not been applied.
 TEST_F(SegmentedSortInt, Bool)
 {
   cudf::test::fixed_width_column_wrapper<bool> col1{
