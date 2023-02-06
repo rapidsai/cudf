534b6c5197e3b33aa6feb45606dc8ce284d2ff19
Jake Awe <jawe@nvidia.com>
2023-02-06 13:20:13 -0600

    rm -W flag

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -37,8 +37,8 @@ popd
 # Build Python docs
 rapids-logger "Build Sphinx docs"
 pushd docs/cudf
-sphinx-build -b dirhtml source _html -W
-sphinx-build -b text source _text -W
+sphinx-build -b dirhtml source _html
+sphinx-build -b text source _text
 popd
 
 
