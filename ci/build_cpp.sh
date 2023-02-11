471a9f2fa7e26727261bcb4c877ed00d23f6e34f
galipremsagar <sagarprem75@gmail.com>
2023-02-10 18:19:37 -0800

    temp

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
 set -euo pipefail
 
 source rapids-env-update
 
 export CMAKE_GENERATOR=Ninja
+
+gpuci_conda_retry remove --force pyarrow arrow-cpp openssl librdkafka
+rapids-mamba-retry install -y "pyarrow=11" "libarrow=11" "librdkafka=1.7.0"
 
 rapids-print-env
 
