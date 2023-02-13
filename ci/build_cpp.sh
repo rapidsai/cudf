6f3611bb109e81604497d813720d8a9ddbacfda4
David Wendt <dwendt@nvidia.com>
2023-02-13 08:25:41 -0500

    check env vars in build_cpp.sh

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -14,3 +14,11 @@ rapids-logger "Begin cpp build"
 rapids-mamba-retry mambabuild conda/recipes/libcudf
 
 rapids-upload-conda-to-s3 cpp
+
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+printenv
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+
+#UPLOAD_NAME=cpp_cuda${RAPIDS_CUDA_VERSION%%.*}_$(arch).ninja_log
+#FILE=$(echo /opt/conda/conda-bld/*libraft-split*/cpp/build/.ninja_log)
+#rapids-upload-to-s3 "${UPLOAD_NAME}" "${FILE}"
