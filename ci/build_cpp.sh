3ed6de25b8df6ab804263559773fc7190cb2d514
David Wendt <dwendt@nvidia.com>
2023-02-15 18:46:43 -0500

    output text per link

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -16,20 +16,25 @@ rapids-mamba-retry mambabuild conda/recipes/libcudf
 rapids-upload-conda-to-s3 cpp
 
 echo "++++++++++++++++++++++++++++++++++++++++++++"
-#printenv | sort
 
 if [[ -d $RAPIDS_ARTIFACTS_DIR ]]; then
   ls -l ${RAPIDS_ARTIFACTS_DIR}
 fi
 
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+
 FILE=${RAPIDS_ARTIFACTS_DIR}/ninja.log
 if [[ -f $FILE ]]; then
+  echo "Ninja log for this build available at the following link"
   UPLOAD_NAME=cpp_cuda${RAPIDS_CUDA_VERSION%%.*}_$(arch).ninja.log
   rapids-upload-to-s3 "${UPLOAD_NAME}" "${FILE}"
 fi
 
+echo "++++++++++++++++++++++++++++++++++++++++++++"
+
 FILE=${RAPIDS_ARTIFACTS_DIR}/ninja_log.html
 if [[ -f $FILE ]]; then
+  echo "Build Metrics Report for this build available at the following link"
   UPLOAD_NAME=cpp_cuda${RAPIDS_CUDA_VERSION%%.*}_$(arch).BuildMetricsReport.html
   rapids-upload-to-s3 "${UPLOAD_NAME}" "${FILE}"
 fi
