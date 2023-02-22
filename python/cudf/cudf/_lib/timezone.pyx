fbd8a3f8ba75c3fcbfae632da20312d7b25ee42f
Ashwin Srinath <shwina@users.noreply.github.com>
2023-02-22 14:30:45 -0500

    Add get_tz_data

--- python/cudf/cudf/_lib/timezone.pyx
+++ python/cudf/cudf/_lib/timezone.pyx
@@ -1,4 +1,5 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.
+import os
 
 from libcpp.memory cimport unique_ptr
 from libcpp.string cimport string
@@ -6,24 +7,26 @@ from libcpp.utility cimport move
 
 from cudf._lib.cpp.io.timezone cimport (
     build_timezone_transition_table as cpp_build_timezone_transition_table,
-    nullopt,
-    optional,
+    make_optional,
 )
 from cudf._lib.cpp.table.table cimport table
 from cudf._lib.utils cimport columns_from_unique_ptr
 
 
-def build_timezone_transition_table(timezone_name, tzif_dir):
+def build_timezone_transition_table(tzdir, tzname):
+    # TODO: libcudf needs the path to end with a '/' separator (but
+    # shouldn't).  Remove this if/when that no longer a requirement:
+    tzdir = os.path.join(tzdir, "")
 
     cdef unique_ptr[table] c_result
-    cdef optional[string] c_tzif_dir = nullopt
-    cdef string c_timezone_name = timezone_name.encode()
+    cdef string c_tzdir = tzdir.encode()
+    cdef string c_tzname = tzname.encode()
 
     with nogil:
         c_result = move(
             cpp_build_timezone_transition_table(
-                c_tzif_dir,
-                c_timezone_name
+                make_optional[string](c_tzdir),
+                c_tzname
             )
         )
 
