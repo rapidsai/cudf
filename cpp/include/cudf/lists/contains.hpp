bb9d5b32c939a6ae3559a12f45582d59b1047048
Vyas Ramasubramani <vyasr@nvidia.com>
2023-02-22 17:01:47 -0800

    Address remaining C++ reviews.

--- cpp/include/cudf/lists/contains.hpp
+++ cpp/include/cudf/lists/contains.hpp
@@ -129,6 +129,7 @@ enum class duplicate_find_option : int32_t {
  * @throw cudf::logic_error If `search_key` type does not match the element type in `lists`
  * @throw cudf::logic_error If `search_key` is of a nested type, or `lists` contains nested
  * elements (LIST, STRUCT)
+ * @throw cudf::data_type_error If `search_keys` type does not match the element type in `lists`
  */
 std::unique_ptr<column> index_of(
   cudf::lists_column_view const& lists,
@@ -163,8 +164,8 @@ std::unique_ptr<column> index_of(
  * @return std::unique_ptr<column> INT32 column of `n` rows with the location of the `search_key`
  *
  * @throw cudf::logic_error If `search_keys` does not match `lists` in its number of rows
- * @throw cudf::data_type_error If `search_keys` type does not match the element type in `lists`
  * @throw cudf::logic_error If `lists` or `search_keys` contains nested elements (LIST, STRUCT)
+ * @throw cudf::data_type_error If `search_keys` type does not match the element type in `lists`
  */
 std::unique_ptr<column> index_of(
   cudf::lists_column_view const& lists,
