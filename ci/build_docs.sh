6072cc4b1b39d19f5d3d1889fd647dbe5fa00d0f
Jake Awe <jawe@nvidia.com>
2023-02-23 15:01:30 -0600

    add logger stmt

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -41,6 +41,7 @@ popd
 
 
 if [[ ${RAPIDS_BUILD_TYPE} == "branch" ]]; then
+  rapids-logger "Upload Docs to S3"
   aws s3 sync --no-progress --delete cpp/doxygen/html "s3://rapidsai-docs/libcudf/${VERSION_NUMBER}/html"
   aws s3 sync --no-progress --delete docs/cudf/_html "s3://rapidsai-docs/cudf/${VERSION_NUMBER}/html"
   aws s3 sync --no-progress --delete docs/cudf/_text "s3://rapidsai-docs/cudf/${VERSION_NUMBER}/txt"
