c685734adb161132ac060fbd56df71441db0cbcf
Vyas Ramasubramani <vyasr@nvidia.com>
2023-02-24 12:15:56 -0800

    Move entry point back to setup.py as per scikit-build requirements.

--- python/dask_cudf/setup.py
+++ python/dask_cudf/setup.py
@@ -5,4 +5,9 @@ from setuptools import find_packages, setup
 setup(
     include_package_data=True,
     packages=find_packages(exclude=["tests", "tests.*"]),
+    entry_points={
+        "dask.dataframe.backends": [
+            "cudf = dask_cudf.backends:CudfBackendEntrypoint",
+        ]
+    },
 )
