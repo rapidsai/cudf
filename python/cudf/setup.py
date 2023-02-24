84d698b0536489dd8544a5869a77f07f690d529b
Vyas Ramasubramani <vyasr@nvidia.com>
2023-02-24 14:34:38 -0800

    Use MANIFEST for cudf.

--- python/cudf/setup.py
+++ python/cudf/setup.py
@@ -6,8 +6,5 @@ from skbuild import setup
 setup(
     include_package_data=True,
     packages=find_packages(include=["cudf", "cudf.*"]),
-    package_data={
-        key: ["*.pxd"] for key in find_packages(include=["cudf._lib*"])
-    },
     zip_safe=False,
 )
