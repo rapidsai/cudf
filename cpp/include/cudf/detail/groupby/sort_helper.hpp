4e0cf5b0b80170af25db40dfd00a649a41f81c11
Nghia Truong <nghiatruong.vn@gmail.com>
2023-03-08 13:12:48 -0800

    Cleanup more header

--- cpp/include/cudf/detail/groupby/sort_helper.hpp
+++ cpp/include/cudf/detail/groupby/sort_helper.hpp
@@ -18,7 +18,6 @@
 
 #include <cudf/column/column.hpp>
 #include <cudf/column/column_view.hpp>
-#include <cudf/detail/structs/utilities.hpp>
 #include <cudf/table/table_view.hpp>
 #include <cudf/types.hpp>
 
