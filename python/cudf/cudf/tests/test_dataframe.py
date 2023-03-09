ae1f7cc98f33c1a311a4d389deed6bee4629b046
galipremsagar <sagarprem75@gmail.com>
2023-03-09 10:03:53 -0800

    isort

--- python/cudf/cudf/tests/test_dataframe.py
+++ python/cudf/cudf/tests/test_dataframe.py
@@ -27,8 +27,8 @@ from cudf.core._compat import (
     PANDAS_GE_120,
     PANDAS_GE_134,
     PANDAS_GE_150,
-    PANDAS_LT_140,
     PANDAS_GE_200,
+    PANDAS_LT_140,
 )
 from cudf.core.buffer.spill_manager import get_global_manager
 from cudf.core.column import column
