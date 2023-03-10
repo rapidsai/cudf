95f8fac1c69832dcf039a41dce5fc0d5295ef694
Vyas Ramasubramani <vyasr@nvidia.com>
2023-03-10 11:14:34 -0800

    Document purpose of cudf::test::get_default_stream.

--- cpp/include/cudf_test/default_stream.hpp
+++ cpp/include/cudf_test/default_stream.hpp
@@ -24,6 +24,15 @@ namespace test {
 /**
  * @brief Get the default stream to use for tests.
  *
+ * The standard behavior of this function is to return cudf's default stream
+ * (cudf::get_default_stream). This function is primarily provided as an
+ * overload target for preload libraries (via LD_PRELOAD) so that the default
+ * stream used for tests may be modified for tracking purposes. All tests of
+ * public APIs that accept streams should pass `cudf::test::get_default_stream`
+ * as the stream argument so that a preload library changing the behavior of
+ * this function will trigger those tests to run on a different stream than
+ * `cudf::get_default_stream`.
+ *
  * @return The default stream to use for tests.
  */
 rmm::cuda_stream_view const get_default_stream();
