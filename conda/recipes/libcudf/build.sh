38591f23c0546fcd270abb02f17035435f69a938
Bradley Dice <bdice@bradleydice.com>
2023-03-10 00:43:41 -0600

    Update libcudf recipe.

--- conda/recipes/libcudf/build.sh
+++ conda/recipes/libcudf/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright (c) 2018-2022, NVIDIA CORPORATION.
+# Copyright (c) 2018-2023, NVIDIA CORPORATION.
 
+export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
 export cudf_ROOT="$(realpath ./cpp/build)"
 ./build.sh -n -v libcudf libcudf_kafka benchmarks tests --build_metrics --incl_cache_stats --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib -DCUDF_ENABLE_ARROW_S3=ON\"
