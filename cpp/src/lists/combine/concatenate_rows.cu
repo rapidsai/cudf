efa2d0a35546a57d658678679264f5e0c98299ed
Vyas Ramasubramani <vyasr@nvidia.com>
2023-03-15 11:20:11 -0700

    valid_if

--- cpp/src/lists/combine/concatenate_rows.cu
+++ cpp/src/lists/combine/concatenate_rows.cu
@@ -246,7 +246,8 @@ std::unique_ptr<column> concatenate_rows(table_view const& input,
             auto const row_index = i % num_rows;
             return row_null_counts[row_index] != num_columns;
           },
-          stream);
+          stream,
+          rmm::mr::get_current_device_resource());
       }
       // NULLIFY_OUTPUT_ROW.  Output row is nullfied if any input row is null
       return cudf::detail::valid_if(
@@ -257,7 +258,8 @@ std::unique_ptr<column> concatenate_rows(table_view const& input,
           auto const row_index = i % num_rows;
           return row_null_counts[row_index] == 0;
         },
-        stream);
+        stream,
+        rmm::mr::get_current_device_resource());
     }();
     concat->set_null_mask(std::move(null_mask), null_count);
   }
