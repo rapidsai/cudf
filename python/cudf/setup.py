02dea12b313cc90b16d7b676ec5205c23fc465a4
Vyas Ramasubramani <vyasr@nvidia.com>
2023-03-16 14:00:39 -0700

    Fix typo.

--- python/cudf/setup.py
+++ python/cudf/setup.py
@@ -6,6 +6,6 @@ from skbuild import setup
 packages = find_packages(include=["cudf*", "udf_cpp*"])
 setup(
     packages=packages,
-    package_data={key: ["*.pxd", ".hpp", ".cuh"] for key in packages},
+    package_data={key: ["*.pxd", "*.hpp", "*.cuh"] for key in packages},
     zip_safe=False,
 )
