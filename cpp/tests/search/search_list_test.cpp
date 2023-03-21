e8189223045cdb890845796b2baf5e5a90ac709f
Nghia Truong <nghiatruong.vn@gmail.com>
2023-03-20 19:57:42 -0700

    Add a test with equal structs
    Signed-off-by: Nghia Truong <nghiatruong.vn@gmail.com>

--- cpp/tests/search/search_list_test.cpp
+++ cpp/tests/search/search_list_test.cpp
@@ -456,7 +456,7 @@ TEST_F(ListBinarySearch, ListsOfStructs)
   }();
 
   auto const needles = [] {
-    auto offsets = int32s_col{0, 3, 4, 6, 8, 10, 13, 14, 15, 17};
+    auto offsets = int32s_col{0, 3, 4, 6, 8, 10, 13, 14, 15};
     // clang-format off
     auto data1 = tdata_col{1, 2, 1,
                            3,
@@ -465,8 +465,7 @@ TEST_F(ListBinarySearch, ListsOfStructs)
                            1, 0,
                            1, 3, 5,
                            3,
-                           3,
-                           0, 0
+                           3
     };
     auto data2 = tdata_col{1, 3, 0,
                            3,
@@ -475,18 +474,89 @@ TEST_F(ListBinarySearch, ListsOfStructs)
                            1, 2,
                            0, 2, 2,
                            3,
+                           3
+    };
+    // clang-format on
+    auto child = structs_col{{data1, data2}};
+    return cudf::make_lists_column(8, offsets.release(), child.release(), 0, {});
+  }();
+
+  auto const results = search_bounds(
+    cudf::table_view{{*haystack}}, cudf::table_view{{*needles}}, {cudf::order::ASCENDING});
+  auto const expected_lower_bound = int32s_col{1, 1, 4, 0, 0, 0, 1, 1};
+  auto const expected_upper_bound = int32s_col{1, 4, 4, 0, 0, 0, 4, 4};
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected_lower_bound, results.first->view(), verbosity);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected_upper_bound, results.second->view(), verbosity);
+}
+
+TEST_F(ListBinarySearch, ListsOfEqualStructsInTwoTables)
+{
+  using tdata_col = cudf::test::fixed_width_column_wrapper<int32_t>;
+
+  // Haystack must be pre-sorted.
+  auto const haystack = [] {
+    auto offsets = int32s_col{0, 2, 3, 4, 5, 7, 10, 13, 16, 18};
+    // clang-format off
+    auto data1 = tdata_col{1, 2,
                            3,
-                           1, 3
+                           3,
+                           3,
+                           4, 5,
+                           4, 5, 4,
+                           4, 5, 4,
+                           4, 5, 4,
+                           4, 6
+    };
+    auto data2 = tdata_col{1, 2,
+                           3,
+                           3,
+                           3,
+                           4, 5,
+                           4, 5, 4,
+                           4, 5, 4,
+                           4, 5, 4,
+                           5, 1
     };
     // clang-format on
     auto child = structs_col{{data1, data2}};
     return cudf::make_lists_column(9, offsets.release(), child.release(), 0, {});
   }();
 
+  auto const needles = [] {
+    auto offsets = int32s_col{0, 2, 3, 4, 5, 7, 10, 13, 15, 17};
+    // clang-format off
+    auto data1 = tdata_col{1, 2,
+                           3,
+                           4,
+                           5,
+                           4, 5,
+                           5, 5, 4,
+                           4, 5, 4,
+                           4, 4,
+                           4, 6
+    };
+    auto data2 = tdata_col{1, 2,
+                           3,
+                           4,
+                           5,
+                           4, 5,
+                           5, 5, 4,
+                           4, 5, 4,
+                           4, 4,
+                           5, 1
+    };
+    // clang-format on
+    auto child = structs_col{{data1, data2}};
+    return cudf::make_lists_column(9, offsets.release(), child.release(), 0, {});
+  }();
+
+  // In this search, the two table have many equal structs.
+  // This help to verify the internal implementation of two-table lex comparator in which the
+  // structs column of two input tables are concatenated, ranked, then split.
   auto const results = search_bounds(
     cudf::table_view{{*haystack}}, cudf::table_view{{*needles}}, {cudf::order::ASCENDING});
-  auto const expected_lower_bound = int32s_col{1, 1, 4, 0, 0, 0, 1, 1, 0};
-  auto const expected_upper_bound = int32s_col{1, 4, 4, 0, 0, 0, 4, 4, 0};
+  auto const expected_lower_bound = int32s_col{0, 1, 4, 9, 4, 9, 5, 4, 8};
+  auto const expected_upper_bound = int32s_col{1, 4, 4, 9, 5, 9, 8, 4, 9};
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected_lower_bound, results.first->view(), verbosity);
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected_upper_bound, results.second->view(), verbosity);
 }
