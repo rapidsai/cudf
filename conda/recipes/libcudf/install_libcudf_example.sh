1aa98e323b11832946ad07cfae891900f7e9587f
Bradley Dice <bdice@bradleydice.com>
2023-03-22 17:38:17 -0500

    Prepend flags for libcudf example.

--- conda/recipes/libcudf/install_libcudf_example.sh
+++ conda/recipes/libcudf/install_libcudf_example.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2018-2022, NVIDIA CORPORATION.
+# Copyright (c) 2018-2023, NVIDIA CORPORATION.
 
+export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
 ./cpp/examples/build.sh
