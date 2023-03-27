b7df54b9677cd013660ebcd659384290938145e7
Bradley Dice <bdice@bradleydice.com>
2023-03-27 14:20:00 -0500

    Use upstream rmm packages.

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -7,10 +7,16 @@ source rapids-env-update
 
 export CMAKE_GENERATOR=Ninja
 
+# TODO: Should we define the major version upstream?
+RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
+LIBRMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1223/c3cf48a/rmm_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
+
 rapids-print-env
 
 rapids-logger "Begin cpp build"
 
-rapids-mamba-retry mambabuild conda/recipes/libcudf
+rapids-mamba-retry mambabuild \
+    --channel "${LIBRMM_CHANNEL}" \
+    conda/recipes/libcudf
 
 rapids-upload-conda-to-s3 cpp
