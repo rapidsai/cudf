b12bc7b988630b071bed1296de21a8f329af91ee
brandon-b-miller <brmiller@nvidia.com>
2023-04-10 07:54:15 -0700

    add managed_udf_string to c++ and introduce matching API variants

--- python/cudf/udf_cpp/strings/include/cudf/strings/udf/udf_apis.hpp
+++ python/cudf/udf_cpp/strings/include/cudf/strings/udf/udf_apis.hpp
@@ -28,6 +28,7 @@ namespace strings {
 namespace udf {
 
 class udf_string;
+class managed_udf_string;
 
 /**
  * @brief Return a cudf::string_view array for the given strings column
@@ -58,6 +59,21 @@ std::unique_ptr<cudf::column> column_from_udf_string_array(udf_string* d_strings
                                                            cudf::size_type size);
 
 /**
+ * @brief Return a STRINGS column given an array of managed_udf_string objects
+ *
+ * This will make a copy of the strings in managed_strings in order to build
+ * the output column.
+ * The individual managed_udf_strings are also cleared freeing each of their internal
+ * device memory buffers.
+ *
+ * @param managed_strings Pointer to device memory of managed_udf_string objects
+ * @param size The number of elements in the managed_strings array
+ * @return A strings column copy of the managed_udf_string objects
+ */
+std::unique_ptr<cudf::column> column_from_managed_udf_string_array(
+  managed_udf_string* managed_strings, cudf::size_type size);
+
+/**
  * @brief Frees a vector of udf_string objects
  *
  * The individual udf_strings are cleared freeing each of their internal
@@ -67,6 +83,18 @@ std::unique_ptr<cudf::column> column_from_udf_string_array(udf_string* d_strings
  * @param size The number of elements in the d_strings array
  */
 void free_udf_string_array(udf_string* d_strings, cudf::size_type size);
+
+/**
+ * @brief Frees a vector of managed_udf_string objects
+ *
+ * The individual managed_udf_strings are cleared freeing each of their internal
+ * device memory buffers. In addition, the meminfo object allocated by numba using
+ * malloc is also freed.
+ *
+ * @param managed_strings Pointer to device memory of udf_string objects
+ * @param size The number of elements in the d_strings array
+ */
+void free_managed_udf_string_array(managed_udf_string* managed_strings, cudf::size_type size);
 
 }  // namespace udf
 }  // namespace strings
