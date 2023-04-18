a50c888fdee968ff7eef36ab3bfc07bd3c044c32
Raza Jafri <rjafri@nvidia.com>
2023-04-18 11:17:26 -0700

    clarify the code

--- java/src/main/java/ai/rapids/cudf/AssertEmptyNulls.java
+++ java/src/main/java/ai/rapids/cudf/AssertEmptyNulls.java
@@ -21,17 +21,13 @@ package ai.rapids.cudf;
 public class AssertEmptyNulls {
   // This is the recommended way to check at runtime that assertions are enabled.
   // https://docs.oracle.com/javase/8/docs/technotes/guides/language/assert.html
-  private static boolean ALLOW_NON_EMPTY_NULLS = true;
+  private static boolean assertNullsAreEmpty = false;
   static {
-    try {
-      assert (false); // Intentional side effect
-    } catch (AssertionError ae) {
-      ALLOW_NON_EMPTY_NULLS = false;
-    }
+      assert assertNullsAreEmpty = true ; // Intentional side effect
   }
 
   public static void assertNullsAreEmpty(ColumnView cv) {
-    if (cv.type.isNestedType() || cv.type.hasOffsets() && !ALLOW_NON_EMPTY_NULLS) {
+    if (cv.type.isNestedType() || cv.type.hasOffsets() && assertNullsAreEmpty) {
       assert !cv.hasNonEmptyNulls() : "Column has non-empty nulls";
     }
   }
