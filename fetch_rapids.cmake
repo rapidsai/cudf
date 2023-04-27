691c26d364b1f04929c5d2d5e1620a2ee1cc5bcc
Robert Maynard <rmaynard@nvidia.com>
2023-04-27 09:54:20 -0400

    Verify with rapids-cmake proposed changes

--- fetch_rapids.cmake
+++ fetch_rapids.cmake
@@ -12,6 +12,8 @@
 # the License.
 # =============================================================================
 if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake)
+  set(rapids-cmake-repo robertmaynard)
+  set(rapids-cmake-branch bug/install_cccl_cmake_modules_in_namespace)
   file(DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-23.06/RAPIDS.cmake
        ${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake
   )
