fa3136080901e32dd9e38dfc73c2d445f0e7393a
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2023-05-03 16:53:06 -0700

    Update python/cudf/cudf/core/dataframe.py

--- python/cudf/cudf/core/dataframe.py
+++ python/cudf/cudf/core/dataframe.py
@@ -7057,8 +7057,8 @@ class DataFrame(IndexedFrame, Serializable, GetAttrGetItemMixin):
               Specifically, `&` must be used for bitwise operators on integers,
               not `and`, which is specifically for the logical and between
               booleans.
-            * Only numerical types are currently supported on all operators.
-            * String types are supported only on comparison operators.
+            * Only numerical types currently support all operators.
+            * String types currently support comparison operators.
             * Operators generally will not cast automatically. Users are
               responsible for casting columns to suitable types before
               evaluating a function.
