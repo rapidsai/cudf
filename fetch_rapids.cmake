205e4a9ca76b418aa029bc2ac6fffca72bb029e4
Bradley Dice <bdice@bradleydice.com>
2023-05-04 11:44:51 -0700

    Set variables even if CUDF_RAPIDS.cmake already exists.

--- fetch_rapids.cmake
+++ fetch_rapids.cmake
@@ -11,9 +11,9 @@
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
 # =============================================================================
+set(rapids-cmake-repo bdice/rapids-cmake)
+set(rapids-cmake-branch cccl-update-2.1.0)
 if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake)
-  set(rapids-cmake-repo bdice/rapids-cmake)
-  set(rapids-cmake-branch cccl-update-2.1.0)
   file(DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-23.06/RAPIDS.cmake
        ${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake
   )
