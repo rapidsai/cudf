e9021027939743c2354b203c3ead97b9c1594138
Yunsong Wang <yunsongw@nvidia.com>
2023-05-09 12:20:20 -0700

    Revert unrelated changes

--- cpp/cmake/thirdparty/get_cucollections.cmake
+++ cpp/cmake/thirdparty/get_cucollections.cmake
@@ -14,14 +14,12 @@
 
 # This function finds cuCollections and performs any additional configuration.
 function(find_and_configure_cucollections)
-  rapids_cpm_find(
-    cuco 0.0.1
-    GLOBAL_TARGETS cuco::cuco
-    CPM_ARGS
-    GIT_REPOSITORY https://github.com/NVIDIA/cuCollections.git
-    GIT_TAG 1a841121d1a32b83c6de0ccccbcdf037790d61ad
-    OPTIONS "BUILD_TESTS OFF" ￼ "BUILD_BENCHMARKS OFF" ￼ "BUILD_EXAMPLES OFF"
-  )
+  include(${rapids-cmake-dir}/cpm/cuco.cmake)
+  if(BUILD_SHARED_LIBS)
+    rapids_cpm_cuco(BUILD_EXPORT_SET cudf-exports)
+  else()
+    rapids_cpm_cuco(BUILD_EXPORT_SET cudf-exports INSTALL_EXPORT_SET cudf-exports)
+  endif()
 endfunction()
 
 find_and_configure_cucollections()
