773e3206d5743135c11f495cae62fae5cc2fb1a1
Bradley Dice <bdice@bradleydice.com>
2023-05-10 13:18:07 -0700

    Proclaim return types needed in sort_helper.cu.

--- cpp/src/groupby/sort/sort_helper.cu
+++ cpp/src/groupby/sort/sort_helper.cu
@@ -43,6 +43,8 @@
 #include <thrust/sequence.h>
 #include <thrust/unique.h>
 
+#include <cuda/functional>
+
 #include <algorithm>
 #include <numeric>
 #include <tuple>
@@ -294,8 +296,10 @@ std::unique_ptr<table> sort_groupby_helper::unique_keys(rmm::cuda_stream_view st
 {
   auto idx_data = key_sort_order(stream).data<size_type>();
 
-  auto gather_map_it = thrust::make_transform_iterator(
-    group_offsets(stream).begin(), [idx_data] __device__(size_type i) { return idx_data[i]; });
+  auto gather_map_it =
+    thrust::make_transform_iterator(group_offsets(stream).begin(),
+                                    cuda::proclaim_return_type<size_type>(
+                                      [idx_data] __device__(size_type i) { return idx_data[i]; }));
 
   return cudf::detail::gather(_keys,
                               gather_map_it,
