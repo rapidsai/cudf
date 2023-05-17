62106e0199e5acbcdacf3795fbceb4588d427755
Vyas Ramasubramani <vyasr@nvidia.com>
2023-05-16 17:11:13 -0700

    Add ability to convert to libcudf type

--- python/cudf/cudf/_lib/pylibcudf/column.pxd
+++ python/cudf/cudf/_lib/pylibcudf/column.pxd
@@ -2,17 +2,25 @@
 
 from cudf._lib.cpp.types cimport offset_type, size_type
 
+from . cimport libcudf_types
 from .gpumemoryview cimport gpumemoryview
 from .types cimport DataType
 
 
 cdef class Column:
-    cdef DataType data_type
-    cdef size_type size
-    cdef gpumemoryview data
-    cdef gpumemoryview mask
-    cdef size_type null_count
-    cdef offset_type offset
-    # TODO: Is there a more efficient container I could use than a list here?
-    # For now defaulting to List[gpumemoryview]
-    cdef object children
+    cdef:
+        # Core data
+        DataType data_type
+        size_type size
+        gpumemoryview data
+        gpumemoryview mask
+        size_type null_count
+        offset_type offset
+        # TODO: Is there a more efficient container I could use than a list
+        # here? For now defaulting to List[gpumemoryview]
+        object children
+
+        # Internals
+        libcudf_types.ColumnView _underlying
+
+    cpdef libcudf_types.ColumnView get_underlying(self)
--- python/cudf/cudf/_lib/pylibcudf/column.pyx
+++ python/cudf/cudf/_lib/pylibcudf/column.pyx
@@ -2,6 +2,7 @@
 
 from cudf._lib.cpp.types cimport offset_type, size_type
 
+from . cimport libcudf_types
 from .gpumemoryview cimport gpumemoryview
 from .types cimport DataType
 
@@ -9,16 +10,31 @@ from .types cimport DataType
 cdef class Column:
     """A container of nullable device data as a column of elements."""
     def __init__(
-        self, DataType dtype not None, size_type size, gpumemoryview data,
+        self, DataType data_type not None, size_type size, gpumemoryview data,
         gpumemoryview mask, size_type null_count, offset_type offset,
         # TODO: Not sure what the best input is for children, for now just
         # using a List[ColumnView]
         object children
     ):
-        self.data_type = dtype
+        self.data_type = data_type
         self.size = size
         self.data = data
         self.mask = mask
         self.null_count = null_count
         self.offset = offset
         self.children = children
+
+        self._underlying = None
+
+    cpdef libcudf_types.ColumnView get_underlying(self):
+        if self._underlying is None:
+            return libcudf_types.ColumnView(
+                self.data_type,
+                self.size,
+                self.data,
+                self.mask,
+                self.null_count,
+                self.offset,
+                self.children,
+            )
+        return self._underlying
