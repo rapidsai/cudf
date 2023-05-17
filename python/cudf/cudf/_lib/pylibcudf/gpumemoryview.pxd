47f84953841a4cca52546662a027edb123fe4290
Vyas Ramasubramani <vyasr@nvidia.com>
2023-05-17 15:44:40 -0700

    Enable creating gpumemoryview that refers to DeviceBuffer

--- python/cudf/cudf/_lib/pylibcudf/gpumemoryview.pxd
+++ python/cudf/cudf/_lib/pylibcudf/gpumemoryview.pxd
@@ -1,5 +1,11 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.
 
+from rmm._lib.device_buffer cimport DeviceBuffer
+
 
 cdef class gpumemoryview:
     cdef readonly Py_ssize_t ptr
+    cdef object base
+
+
+cpdef gpumemoryview_from_device_buffer(DeviceBuffer buf)
--- python/cudf/cudf/_lib/pylibcudf/gpumemoryview.pyx
+++ python/cudf/cudf/_lib/pylibcudf/gpumemoryview.pyx
@@ -1,7 +1,19 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.
 
+from rmm._lib.device_buffer cimport DeviceBuffer
+
 
 cdef class gpumemoryview:
     """Minimal representation of a memory buffer."""
     def __init__(self, Py_ssize_t ptr):
         self.ptr = ptr
+        self.base = None
+
+
+# TODO: Eventually cpdef classmethod
+# TODO: Support arbitrary CAI objects
+cpdef gpumemoryview_from_device_buffer(DeviceBuffer buf):
+    cdef gpumemoryview ret = gpumemoryview.__new__(gpumemoryview)
+    ret.base = buf
+    ret.ptr = buf.ptr
+    return ret
