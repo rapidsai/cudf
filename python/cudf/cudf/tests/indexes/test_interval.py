0632bc5500bd69931cbea4ae10b02db9c545373d
Ashwin Srinath <shwina@users.noreply.github.com>
2023-05-19 14:51:37 -0400

    Add a test for converting interval indexes to arrow

--- python/cudf/cudf/tests/indexes/test_interval.py
+++ python/cudf/cudf/tests/indexes/test_interval.py
@@ -1,1 +1,12 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.
+import pandas as pd
+import pyarrow as pa
+
+import cudf
+from cudf.testing._utils import assert_eq
+
+
+def test_interval_to_arrow():
+    expect = pa.Array.from_pandas(pd.IntervalIndex([pd.Interval(0, 1)]))
+    got = cudf.IntervalIndex([pd.Interval(0, 1)]).to_arrow()
+    assert_eq(expect, got)
