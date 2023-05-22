c3ae555934d6522f60e7914dbf2a00578692a40f
Vyas Ramasubramani <vyasr@nvidia.com>
2023-05-22 16:05:48 -0700

    Remove groupby and aggregation code

--- python/cudf/cudf/_lib/pylibcudf/__init__.py
+++ python/cudf/cudf/_lib/pylibcudf/__init__.py
@@ -1,10 +1,8 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.
 
 from . import copying, libcudf_classes
-from .aggregation import GroupbyAggregation
 from .column import Column
 from .gpumemoryview import gpumemoryview
-from .groupby import AggregationRequest, GroupBy
 from .table import Table
 from .types import DataType, TypeId
 
@@ -16,7 +14,4 @@ __all__ = [
     "copying",
     "gpumemoryview",
     "libcudf_classes",
-    "GroupBy",
-    "AggregationRequest",
-    "GroupbyAggregation",
 ]
