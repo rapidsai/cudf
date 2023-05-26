81fb10903405a0357e4862d74fecdaf76bea39bf
Bradley Dice <bdice@bradleydice.com>
2023-05-26 11:33:18 -0500

    Revert install_libcudf_example changes.

--- conda/recipes/libcudf/install_libcudf_example.sh
+++ conda/recipes/libcudf/install_libcudf_example.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Copyright (c) 2018-2023, NVIDIA CORPORATION.
+# Copyright (c) 2018-2022, NVIDIA CORPORATION.
 
-export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
 ./cpp/examples/build.sh
