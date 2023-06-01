a64ae77b6f0a1153d279aafdb31bca081b19bcdb
David Wendt <dwendt@nvidia.com>
2023-06-01 14:34:43 -0400

    refactor to remove subtraction

--- cpp/include/cudf/detail/null_mask.cuh
+++ cpp/include/cudf/detail/null_mask.cuh
@@ -69,9 +69,8 @@ __global__ void offset_bitmask_binop(Binop op,
   constexpr auto const word_size{detail::size_in_bits<bitmask_type>()};
   auto const tid = threadIdx.x + blockIdx.x * blockDim.x;
 
-  size_type const last_bit_index  = source_size_bits - 1;
-  size_type const num_slack_bits  = word_size - (last_bit_index % word_size) - 1;
-  size_type const last_word_index = cudf::word_index(last_bit_index);
+  auto const last_bit_index  = source_size_bits - 1;
+  auto const last_word_index = cudf::word_index(last_bit_index);
 
   size_type thread_count = 0;
 
@@ -90,11 +89,14 @@ __global__ void offset_bitmask_binop(Binop op,
                                                          source_begin_bits[i] + source_size_bits));
     }
 
+    if (destination_word_index == last_word_index) {
+      // mask out any bits not part of this word
+      auto const num_bits_in_last_word = word_size - intra_word_index(last_bit_index) - 1;
+      destination_word &= set_least_significant_bits(word_size - num_bits_in_last_word);
+    }
+
     destination[destination_word_index] = destination_word;
     thread_count += __popc(destination_word);
-    if (destination_word_index == last_word_index) {
-      thread_count -= __popc(destination_word & set_most_significant_bits(num_slack_bits));
-    }
   }
 
   using BlockReduce = cub::BlockReduce<size_type, block_size>;
