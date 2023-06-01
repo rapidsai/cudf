e1f33ef323f6a764d99f17e0d0846e85eb6da33f
Bradley Dice <bdice@bradleydice.com>
2023-06-01 14:17:46 -0500

    Try altering CMAKE_PREFIX_PATH to help find CUDA driver stubs.

--- conda/recipes/libcudf/build.sh
+++ conda/recipes/libcudf/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Copyright (c) 2018-2022, NVIDIA CORPORATION.
+# Copyright (c) 2018-2023, NVIDIA CORPORATION.
 
 export cudf_ROOT="$(realpath ./cpp/build)"
+
+[[ $(arch) == "x86_64" ]] && targetsDir="targets/x86_64-linux"
+[[ $(arch) == "aarch64" ]] && targetsDir="targets/sbsa-linux"
+export CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}:${BUILD_PREFIX}/${targetsDir}/lib:${BUILD_PREFIX}/${targetsDir}/lib/stubs"
+
 ./build.sh -n -v libcudf libcudf_kafka benchmarks tests --build_metrics --incl_cache_stats --cmake-args=\"-DCMAKE_INSTALL_LIBDIR=lib -DCUDF_ENABLE_ARROW_S3=ON\"
