36c7db1ee5c2b975bdc3bb2677a0a50ad7505198
Nghia Truong <nghiatruong.vn@gmail.com>
2023-06-08 14:18:27 -0700

    Remove unused headers

--- cpp/src/io/orc/reader_impl.cu
+++ cpp/src/io/orc/reader_impl.cu
@@ -45,13 +45,12 @@
 
 #include <thrust/copy.h>
 #include <thrust/execution_policy.h>
+#include <thrust/fill.h>
 #include <thrust/for_each.h>
 #include <thrust/iterator/counting_iterator.h>
-#include <thrust/iterator/zip_iterator.h>
 #include <thrust/pair.h>
 #include <thrust/scan.h>
 #include <thrust/transform.h>
-#include <thrust/tuple.h>
 
 #include <algorithm>
 #include <iterator>
