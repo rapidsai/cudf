cd85421d343443130c73420c2df6b144c8cf2d19
Nghia Truong <nghiatruong.vn@gmail.com>
2023-06-09 15:43:26 -0700

    Add Java test

--- java/src/test/java/ai/rapids/cudf/ColumnVectorTest.java
+++ java/src/test/java/ai/rapids/cudf/ColumnVectorTest.java
@@ -2868,6 +2868,23 @@ public class ColumnVectorTest extends CudfTestBase {
   }
 
   @Test
+  void testFlattenLists() {
+    HostColumnVector.ListType listType = new HostColumnVector.ListType(true,
+        new HostColumnVector.BasicType(true, DType.INT32));
+    HostColumnVector.ListType listOfListsType = new HostColumnVector.ListType(true, listType);
+
+    try (ColumnVector input = ColumnVector.fromLists(listOfListsType,
+             Arrays.asList(Arrays.asList(1, 2), Arrays.asList(3), Arrays.asList(4, 5, 6)),
+             Arrays.asList(Arrays.asList(7, 8, 9), Arrays.asList(10, 11, 12, 13, 14, 15)));
+         ColumnVector result = ColumnVector.flattenLists(input);
+         ColumnVector expected = ColumnVector.fromLists(listType,
+             Arrays.asList(1, 2, 3, 4, 5, 6),
+             Arrays.asList(7, 8, 9, 10, 11, 12, 13, 14, 15))) {
+      assertColumnsAreEqual(expected, result);
+    }
+  }
+
+  @Test
   void testPrefixSum() {
     try (ColumnVector v1 = ColumnVector.fromLongs(1, 2, 3, 5, 8, 10);
          ColumnVector summed = v1.prefixSum();
