29a64685f82ad2a63168046f94bb647cf4b72297
Nghia Truong <nghiatruong.vn@gmail.com>
2023-06-12 11:42:50 -0700

    Functions should not be static

--- java/src/test/java/ai/rapids/cudf/ColumnVectorTest.java
+++ java/src/test/java/ai/rapids/cudf/ColumnVectorTest.java
@@ -2876,7 +2876,7 @@ public class ColumnVectorTest extends CudfTestBase {
     try (ColumnVector input = ColumnVector.fromLists(listOfListsType,
              Arrays.asList(Arrays.asList(1, 2), Arrays.asList(3), Arrays.asList(4, 5, 6)),
              Arrays.asList(Arrays.asList(7, 8, 9), Arrays.asList(10, 11, 12, 13, 14, 15)));
-         ColumnVector result = ColumnVector.flattenLists(input);
+         ColumnVector result = input.flattenLists();
          ColumnVector expected = ColumnVector.fromLists(listType,
              Arrays.asList(1, 2, 3, 4, 5, 6),
              Arrays.asList(7, 8, 9, 10, 11, 12, 13, 14, 15))) {
