6e81e2eb984aaa50cfc1e6fa88c263b5463ef611
Bradley Dice <bdice@bradleydice.com>
2023-06-22 11:39:44 -0500

    Remove CMAKE_PREFIX_PATH change.

--- conda/recipes/libcudf/build.sh
+++ conda/recipes/libcudf/build.sh
@@ -3,10 +3,6 @@
 
 export cudf_ROOT="$(realpath ./cpp/build)"
 
-[[ $(arch) == "x86_64" ]] && targetsDir="targets/x86_64-linux"
-[[ $(arch) == "aarch64" ]] && targetsDir="targets/sbsa-linux"
-export CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH}:${BUILD_PREFIX}/${targetsDir}/lib:${BUILD_PREFIX}/${targetsDir}/lib/stubs"
-
 ./build.sh -n -v \
     libcudf libcudf_kafka benchmarks tests \
     --build_metrics --incl_cache_stats \
