bc44c3f87e7d8a6dba29407a2183da5801b98319
Lawrence Mitchell <lmitchell@nvidia.com>
2023-06-23 10:46:06 +0100

    Update benchmark usage of _apply_boolean_mask

--- python/cudf/benchmarks/internal/bench_dataframe_internal.py
+++ python/cudf/benchmarks/internal/bench_dataframe_internal.py
@@ -1,11 +1,15 @@
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
 """Benchmarks of internal DataFrame methods."""
 
 from utils import benchmark_with_object, make_boolean_mask_column
 
+from cudf.core.copy_types import as_boolean_mask
+
 
 @benchmark_with_object(cls="dataframe", dtype="int")
 def bench_apply_boolean_mask(benchmark, dataframe):
     mask = make_boolean_mask_column(len(dataframe))
-    benchmark(dataframe._apply_boolean_mask, mask)
+    benchmark(
+        dataframe._apply_boolean_mask, as_boolean_mask(mask, len(dataframe))
+    )
