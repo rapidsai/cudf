ebc48bd9743305afe5433f0428386192b3746212
Nghia Truong <nghiatruong.vn@gmail.com>
2023-07-10 18:07:02 -0700

    Add reduction tests

--- cpp/tests/reductions/reduction_tests.cpp
+++ cpp/tests/reductions/reduction_tests.cpp
@@ -2548,6 +2548,98 @@ TEST_F(ListReductionTest, NonValidListReductionNthElement)
     *cudf::make_nth_element_aggregation<reduce_aggregation>(0, cudf::null_policy::INCLUDE));
 }
 
+TEST_F(ListReductionTest, ReductionMinMaxNoNull)
+{
+  using INTS_CW          = cudf::test::fixed_width_column_wrapper<int>;
+  using LISTS_CW         = cudf::test::lists_column_wrapper<int>;
+  using STRINGS_CW       = cudf::test::strings_column_wrapper;
+  using LISTS_STRINGS_CW = cudf::test::lists_column_wrapper<cudf::string_view>;
+
+  {
+    auto const input = LISTS_CW{{3, 4}, {1, 2}, {5, 6, 7}, {0, 8}, {9, 10}, {1, 0}};
+    this->reduction_test(
+      input, INTS_CW{0, 8}, true, true, *cudf::make_min_aggregation<reduce_aggregation>());
+    this->reduction_test(
+      input, INTS_CW{9, 10}, true, true, *cudf::make_max_aggregation<reduce_aggregation>());
+  }
+  {
+    auto const input = LISTS_STRINGS_CW{
+      {"34", "43"}, {"12", "21"}, {"567", "6", "765"}, {"08", "8"}, {"109", "10"}, {"10", "00"}};
+    this->reduction_test(
+      input, STRINGS_CW{"08", "8"}, true, true, *cudf::make_min_aggregation<reduce_aggregation>());
+    this->reduction_test(input,
+                         STRINGS_CW{"567", "6", "765"},
+                         true,
+                         true,
+                         *cudf::make_max_aggregation<reduce_aggregation>());
+  }
+}
+
+TEST_F(ListReductionTest, ReductionMinMaxSlicedInput)
+{
+  using INTS_CW          = cudf::test::fixed_width_column_wrapper<int>;
+  using LISTS_CW         = cudf::test::lists_column_wrapper<int>;
+  using STRINGS_CW       = cudf::test::strings_column_wrapper;
+  using LISTS_STRINGS_CW = cudf::test::lists_column_wrapper<cudf::string_view>;
+
+  {
+    auto const input_original = LISTS_CW{{9, 9} /*dont care*/,
+                                         {0, 0} /*dont care*/,
+                                         {3, 4},
+                                         {1, 2},
+                                         {5, 6, 7},
+                                         {0, 8},
+                                         {9, 10},
+                                         {1, 0},
+                                         {0, 7} /*dont care*/};
+    auto const input          = cudf::slice(input_original, {2, 8})[0];
+    this->reduction_test(
+      input, INTS_CW{0, 8}, true, true, *cudf::make_min_aggregation<reduce_aggregation>());
+    this->reduction_test(
+      input, INTS_CW{9, 10}, true, true, *cudf::make_max_aggregation<reduce_aggregation>());
+  }
+  {
+    auto const input_original = LISTS_STRINGS_CW{{"08", "8"} /*dont care*/,
+                                                 {"999", "8"} /*dont care*/,
+                                                 {"34", "43"},
+                                                 {"12", "21"},
+                                                 {"567", "6", "765"},
+                                                 {"08", "8"},
+                                                 {"109", "10"},
+                                                 {"10", "00"}};
+    auto const input          = cudf::slice(input_original, {2, 8})[0];
+    this->reduction_test(
+      input, STRINGS_CW{"08", "8"}, true, true, *cudf::make_min_aggregation<reduce_aggregation>());
+    this->reduction_test(input,
+                         STRINGS_CW{"567", "6", "765"},
+                         true,
+                         true,
+                         *cudf::make_max_aggregation<reduce_aggregation>());
+  }
+}
+
+TEST_F(ListReductionTest, ReductionMinMaxWithNulls)
+{
+  using INTS_CW  = cudf::test::fixed_width_column_wrapper<int>;
+  using LISTS_CW = cudf::test::lists_column_wrapper<int>;
+
+  auto const input = LISTS_CW{{LISTS_CW{3, 4},
+                               LISTS_CW{1, 2},
+                               LISTS_CW{} /*null*/,
+                               LISTS_CW{5, 6, 7},
+                               LISTS_CW{0, 8},
+                               LISTS_CW{{9, 0}, cudf::test::iterators::null_at(1)},
+                               LISTS_CW{} /*null*/},
+                              cudf::test::iterators::nulls_at({2, 6})};
+  this->reduction_test(
+    input, INTS_CW{} /*null*/, true, false, *cudf::make_min_aggregation<reduce_aggregation>());
+  this->reduction_test(input,
+                       INTS_CW{{9, 0}, cudf::test::iterators::null_at(1)},
+                       true,
+                       true,
+                       *cudf::make_max_aggregation<reduce_aggregation>());
+}
+
 struct StructReductionTest : public cudf::test::BaseFixture {
   using SCW = cudf::test::structs_column_wrapper;
 
