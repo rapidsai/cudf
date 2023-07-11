99579d0b3c548341420b4f693eb1ec88e70f27b1
galipremsagar <sagarprem75@gmail.com>
2023-07-11 12:34:12 -0700

    fixes

--- python/cudf/cudf/tests/test_dataframe.py
+++ python/cudf/cudf/tests/test_dataframe.py
@@ -26,6 +26,7 @@ from cudf.core._compat import (
     PANDAS_GE_150,
     PANDAS_GE_200,
     PANDAS_LT_140,
+    PANDAS_LT_203,
 )
 from cudf.core.buffer.spill_manager import get_global_manager
 from cudf.core.column import column
@@ -8411,6 +8412,7 @@ def test_dataframe_mode(request, df, numeric_only, dropna):
     request.applymarker(
         pytest.mark.xfail(
             condition=PANDAS_GE_200
+            and PANDAS_LT_203
             and numeric_only is False
             and "b" in df.columns
             and df["b"].dtype == np.dtype("timedelta64[s]"),
