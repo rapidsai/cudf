e6f04d9dc20c1df3ea301307cbf46cbdbec89dcb
GALI PREM SAGAR <sagarprem75@gmail.com>
2023-07-14 12:19:38 -0500

    Update test_python_common.sh

--- ci/test_python_common.sh
+++ ci/test_python_common.sh
@@ -39,6 +39,7 @@ COMMIT="99579d0"
 LIBCUDF_CHANNEL_20=$(rapids-get-artifact ci/cudf/pull-request/13599/${COMMIT}/cudf_conda_cpp_cuda11_$(arch).tar.gz)
 CUDF_CHANNEL_20=$(rapids-get-artifact ci/cudf/pull-request/13599/${COMMIT}/cudf_conda_python_cuda11_${RAPIDS_PY_VERSION//.}_$(arch).tar.gz)
 PYTHON_CHANNEL=$(rapids-get-artifact ci/cudf/pull-request/13599/${COMMIT}/cudf_conda_python_cuda11_${RAPIDS_PY_VERSION//.}_$(arch).tar.gz)
+CPP_CHANNEL=$(rapids-get-artifact ci/cudf/pull-request/13599/${COMMIT}/cudf_conda_cpp_cuda11_$(arch).tar.gz)
 
 rapids-logger $LIBCUDF_CHANNEL_20
 rapids-logger $CUDF_CHANNEL_20
