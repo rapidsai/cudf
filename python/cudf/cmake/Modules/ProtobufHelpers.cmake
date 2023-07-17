89735d8efd18990478e46cbb9f2586255a22639c
Vyas Ramasubramani <vyasr@nvidia.com>
2023-07-17 09:41:54 -0700

    Update cython-lint and replace flake8 with ruff (#13699)
    This PR makes three changes to linting
    - Updates the cython-lint version and fixes associated errors
    - Adds the [ruff](https://beta.ruff.rs/docs/) linter
    - Removes flake8
    
    [cython-lint has achieved parity with flake8 for Cython files](https://github.com/MarcoGorelli/cython-lint/issues/45), so we no longer need flake8 for that. Meanwhile [ruff advertises itself as a suitable replacement for flake8 under three conditions](https://beta.ruff.rs/docs/faq/#how-does-ruff-compare-to-flake8): 1) few plugins, 2) using with black, and 3) Python 3 only. All of those apply to cudf code. From a quick analysis, there are a small handful of error codes supported by flake8 (via pyflakes or pycodestyle) that are not yet supported by ruff (pyflakes: F723, F842; pycodstyle: W391,W503, W504, W505, W606). On the flip side, ruff autofixes a range of issues that flake8 can't, it runs more than an order of magnitude faster, it supports more flexible configuration via pyproject.toml, and it promises to replace multiple tools over time. Therefore, I think this is a good switch for us to make.
    
    Note that ruff could in theory also replace isort for Python files, but it does not support Cython yet, so I have left our isort hook in place. We could revisit in the future if that changes.
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - AJ Schmidt (https://github.com/ajschmidt8)
    
    URL: https://github.com/rapidsai/cudf/pull/13699

--- python/cudf/cmake/Modules/ProtobufHelpers.cmake
+++ python/cudf/cmake/Modules/ProtobufHelpers.cmake
@@ -1,5 +1,5 @@
 # =============================================================================
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -36,7 +36,6 @@ function(codegen_protoc)
     file(
       WRITE "${pb2_py_path}"
       [=[
-# flake8: noqa
 # fmt: off
 ]=]
     )
