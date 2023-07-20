9baf847863a3f6bc372d21c4946333abf08c44c4
Vyas Ramasubramani <vyasr@nvidia.com>
2023-07-20 16:22:32 -0700

    Rename var

--- ci/build_wheel_cudf.sh
+++ ci/build_wheel_cudf.sh
@@ -11,7 +11,7 @@ source /tmp/gha-tools/tools/rapids-configure-sccache
 
 # Use gha-tools rapids-pip-wheel-version to generate wheel version then
 # update the necessary files
-version_override="$(rapids-pip-wheel-version ${RAPIDS_EPOCH_TIMESTAMP})"
+version_override="$(rapids-pip-wheel-version ${RAPIDS_DATE_STRING})"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
