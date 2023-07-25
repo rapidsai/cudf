cb5ed0773e52f3291624bf53acaaf7b87aeabee4
Vyas Ramasubramani <vyasr@nvidia.com>
2023-07-25 10:51:56 -0700

    Try removing the mkdir

--- ci/test_wheel_dask_cudf.sh
+++ ci/test_wheel_dask_cudf.sh
@@ -3,7 +3,6 @@
 
 set -eou pipefail
 
-mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="dask_cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
