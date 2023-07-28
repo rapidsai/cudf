0b576720598371ce57e6a7a0a779c1f840beef1a
GALI PREM SAGAR <sagarprem75@gmail.com>
2023-07-28 13:03:56 -0500

    Update dataframe.py

--- python/cudf/cudf/core/dataframe.py
+++ python/cudf/cudf/core/dataframe.py
@@ -1856,12 +1856,6 @@ class DataFrame(IndexedFrame, Serializable, GetAttrGetItemMixin):
                     can_use_self_column_name = other.index.to_pandas().equals(
                         self._data.to_pandas_index()
                     )
-            equal_columns = other.index.to_pandas().equals(
-                self._data.to_pandas_index()
-            )
-            can_use_self_column_name = equal_columns or (
-                list(other._index._data.names) == self._data._level_names
-            )
         elif isinstance(other, DataFrame):
             if (
                 not can_reindex
