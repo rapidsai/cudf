196f4903dc6eacbff941b71ccbc48c888f10ea7a
vuule <vmilovanovic@nvidia.com>
2023-08-02 16:48:41 -0700

    omg

--- cpp/src/io/utilities/datasource.cpp
+++ cpp/src/io/utilities/datasource.cpp
@@ -123,11 +123,7 @@ void register_mmaped_buffer(void const* ptr, size_t size)
   }
 
   auto const result = cudaHostRegister(const_cast<void*>(ptr), size, cudaHostRegisterDefault);
-  if (result != cudaSuccess) {
-    CUDF_LOG_WARN("Register mmap buffer: success");
-  } else {
-    CUDF_LOG_WARN("Register mmap buffer: cudaHostRegister failed with error {}", result);
-  }
+  CUDF_LOG_WARN("Register mmap buffer: cudaHostRegister returned {}", result);
 }
 
 /**
