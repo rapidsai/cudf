893fac3bd04e6eec96969182021c4c0654d43af3
brandon-b-miller <brmiller@nvidia.com>
2023-08-08 07:58:26 -0700

    xfail all nan case

--- python/cudf/cudf/tests/test_groupby.py
+++ python/cudf/cudf/tests/test_groupby.py
@@ -475,7 +475,19 @@ def test_groupby_apply_jit_reductions_special_vals(
 
 @pytest.mark.parametrize("dtype", ["float64"])
 @pytest.mark.parametrize("func", ["idxmax", "idxmin"])
-@pytest.mark.parametrize("special_val", [np.nan, np.inf, -np.inf])
+@pytest.mark.parametrize(
+    "special_val",
+    [
+        pytest.param(
+            np.nan,
+            marks=pytest.mark.xfail(
+                reason="https://github.com/rapidsai/cudf/issues/13832"
+            ),
+        ),
+        np.inf,
+        -np.inf,
+    ],
+)
 def test_groupby_apply_jit_idx_reductions_special_vals(
     func, groupby_jit_data, dtype, special_val
 ):
@@ -497,16 +509,6 @@ def test_groupby_apply_jit_idx_reductions_special_vals(
     expect = groupby_jit_data.to_pandas().groupby("key1").apply(func)
     got = groupby_jit_data.groupby("key1").apply(func, engine="jit")
 
-    if special_val is np.nan:
-        # we return the first occurence of nan whereas
-        # pandas returns nan itself when the group is all nans
-        expect[:] = (
-            groupby_jit_data.groupby("key1")
-            ._grouped()[3]
-            .to_pandas()["key1"]
-            .drop_duplicates()
-            .index
-        )
     assert_eq(expect, got, check_dtype=False)
 
 
