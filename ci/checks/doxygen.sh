0ec51ea5cd786f6f0d321c7163478e7051935c24
Vyas Ramasubramani <vyasr@nvidia.com>
2023-08-09 18:15:50 -0700

    Temporarily disable CI doxygen check

--- ci/checks/doxygen.sh
+++ ci/checks/doxygen.sh
@@ -10,6 +10,9 @@ if ! [ -x "$(command -v doxygen)" ]; then
   exit 0
 fi
 
+echo -e "Temporarily disabling check to allow CI to complete"
+exit 0
+
 # Utility to return version as number for comparison
 function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
