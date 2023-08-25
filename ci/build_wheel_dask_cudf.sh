3cac1743941fa390a1c23d8c5f24dea53cfe8bf2
Vyas Ramasubramani <vyasr@nvidia.com>
2023-08-25 08:57:28 -0700

    Centralize logic

--- ci/build_wheel_dask_cudf.sh
+++ ci/build_wheel_dask_cudf.sh
@@ -3,22 +3,8 @@
 
 set -euo pipefail
 
-source rapids-configure-sccache
-source rapids-date-string
+export SKBUILD_CONFIGURE_OPTIONS="-DCUDF_BUILD_WHEELS=ON -DDETECT_CONDA_ENV=OFF"
 
-# Use gha-tools rapids-pip-wheel-version to generate wheel version then
-# update the necessary files
-version_override="$(rapids-pip-wheel-version ${RAPIDS_DATE_STRING})"
+./ci/build_wheel.sh dask_cudf python/dask_cudf
 
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-
-ci/release/apply_wheel_modifications.sh ${version_override} "-${RAPIDS_PY_CUDA_SUFFIX}"
-echo "The package name and/or version was modified in the package source. The git diff is:"
-git diff
-
-cd python/dask_cudf
-
-# Hardcode the output dir
-python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
-
-RAPIDS_PY_WHEEL_NAME="dask_cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 dist
+RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 ${package_dir}/dist
