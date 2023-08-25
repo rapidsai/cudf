4c350212bc77db3cbe83b99ebbe87276dc2c9a34
Vyas Ramasubramani <vyasr@nvidia.com>
2023-08-25 10:29:29 -0700

    Make sure suffix exists in outer script

--- ci/build_wheel_dask_cudf.sh
+++ ci/build_wheel_dask_cudf.sh
@@ -3,8 +3,7 @@
 
 set -euo pipefail
 
-export SKBUILD_CONFIGURE_OPTIONS="-DCUDF_BUILD_WHEELS=ON -DDETECT_CONDA_ENV=OFF"
-
 ./ci/build_wheel.sh dask_cudf python/dask_cudf
 
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 ${package_dir}/dist
