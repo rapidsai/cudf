3aa461f0c80606cef29e52858a2a733bdf131ed7
Vyas Ramasubramani <vyasr@nvidia.com>
2023-08-30 20:31:59 -0700

    Initial version of scalar

---
+++ python/cudf/cudf/_lib/pylibcudf/scalar.pyx
@@ -0,0 +1,26 @@
+# Copyright (c) 2023, NVIDIA CORPORATION.
+
+from rmm._lib.memory_resource cimport get_current_device_resource
+
+from .types cimport DataType
+
+
+# The DeviceMemoryResource attribute could be released prematurely
+# by the gc if the DeviceScalar is in a reference cycle. Removing
+# the tp_clear function with the no_gc_clear decoration prevents that.
+# See https://github.com/rapidsai/rmm/pull/931 for details.
+cdef class Scalar:
+
+    def __cinit__(self, *args, **kwargs):
+        self.mr = get_current_device_resource()
+
+    cdef const scalar* get(self) except *:
+        return self.c_obj.get()
+
+    cpdef DataType type(self):
+        """The type of data in the column."""
+        return self._data_type
+
+    cpdef bool is_valid(self):
+        """True if the scalar is valid, false if not"""
+        return self.get().is_valid()
