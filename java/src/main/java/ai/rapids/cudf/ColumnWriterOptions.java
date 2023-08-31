fb29377c66686b9587b3457639c38ed5fea75ffe
Chong Gao <res_life@163.com>
2023-08-31 10:08:25 +0800

    Refactor

--- java/src/main/java/ai/rapids/cudf/ColumnWriterOptions.java
+++ java/src/main/java/ai/rapids/cudf/ColumnWriterOptions.java
@@ -546,16 +546,18 @@ public class ColumnWriterOptions {
    * Maps are List columns with a Struct named 'key_value' with a child named 'key' and a child
    * named 'value'. The caller of this method doesn't need to worry about this as this method will
    * take care of this without the knowledge of the caller.
-   * @param isNullable is the returned map nullable.
+   *
    * Note: If this map column is a key of another map, should pass isNullable = false.
    * e.g.: map1(map2(int, int), int) the map2 should be non-nullable.
+   *
+   * @param isNullable is the returned map nullable.
    */
   public static ColumnWriterOptions mapColumn(String name, ColumnWriterOptions key,
                                               ColumnWriterOptions value, Boolean isNullable) {
-    StructColumnWriterOptions struct = structBuilder("key_value").build();
     if (key.isNullable) {
       throw new IllegalArgumentException("key column can not be nullable");
     }
+    StructColumnWriterOptions struct = structBuilder("key_value").build();
     struct.childColumnOptions = new ColumnWriterOptions[]{key, value};
     ColumnWriterOptions opt = listBuilder(name, isNullable)
         .withStructColumn(struct)
