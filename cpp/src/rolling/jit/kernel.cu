6ea6de4fa2d19fc89dcc485a82574016d1aae008
Bradley Dice <bdice@bradleydice.com>
2023-08-31 22:09:30 -0500

    Revert cpp/src/rolling/jit/kernel.cu

--- cpp/src/rolling/jit/kernel.cu
+++ cpp/src/rolling/jit/kernel.cu
@@ -51,8 +51,8 @@ __global__ void gpu_rolling_new(cudf::size_type nrows,
                                 FollowingWindowType following_window_begin,
                                 cudf::size_type min_periods)
 {
-  auto i            = cudf::detail::grid_1d::global_thread_id();
-  auto const stride = cudf::detail::grid_1d::grid_stride();
+  cudf::thread_index_type i = blockIdx.x * blockDim.x + threadIdx.x;
+  cudf::thread_index_type const stride = blockDim.x * gridDim.x;
 
   cudf::size_type warp_valid_count{0};
 
