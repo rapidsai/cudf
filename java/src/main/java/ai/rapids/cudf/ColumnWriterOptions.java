6c402ff61ca6bd038d55e125acd24d4a29e8e757
Nghia Truong <7416935+ttnghia@users.noreply.github.com>
2023-09-04 11:52:51 -0600

    Update java/src/main/java/ai/rapids/cudf/ColumnWriterOptions.java

--- java/src/main/java/ai/rapids/cudf/ColumnWriterOptions.java
+++ java/src/main/java/ai/rapids/cudf/ColumnWriterOptions.java
@@ -523,7 +523,7 @@ public class ColumnWriterOptions {
    * named 'value'. The caller of this method doesn't need to worry about this as this method will
    * take care of this without the knowledge of the caller.
    *
-   * Note: this method always returns a nullabe column, can not return non-nullable column.
+   * Note: This method always returns a nullabe column, cannot return non-nullable column.
    * Do not use this, use the next function with the parameter `isNullable`.
    */
   @Deprecated
