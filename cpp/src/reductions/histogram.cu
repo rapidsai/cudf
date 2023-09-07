1b04436990377028c8f78bbd25742bc02808d757
Nghia Truong <nghiat@nvidia.com>
2023-09-07 10:33:12 -0700

    Add new files

---
+++ cpp/src/reductions/histogram.cu
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2023, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <reductions/histogram.cuh>
+
+#include <cudf/column/column.hpp>
+
+
+#include <rmm/cuda_stream_view.hpp>
+#include <rmm/exec_policy.hpp>
+
+namespace cudf::reduction::detail {
+
+std::unique_ptr<cudf::column> histogram(column_view const& col,
+                                          rmm::cuda_stream_view stream,
+                                          rmm::mr::device_memory_resource* mr)
+{
+  return nullptr;
+}
+
+}  // namespace cudf::reduction::detail
