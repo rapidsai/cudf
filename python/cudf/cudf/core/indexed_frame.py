81194fb809159103768a914b5c23072d44180f5a
GALI PREM SAGAR <sagarprem75@gmail.com>
2023-09-12 20:10:12 -0500

    add comment

--- python/cudf/cudf/core/indexed_frame.py
+++ python/cudf/cudf/core/indexed_frame.py
@@ -359,6 +359,9 @@ class IndexedFrame(Frame):
         )
 
     def __round__(self, digits=0):
+        # Shouldn't be added to BinaryOperand
+        # because pandas Index doesn't implement
+        # this method.
         return self.round(decimals=digits)
 
     def _mimic_inplace(
