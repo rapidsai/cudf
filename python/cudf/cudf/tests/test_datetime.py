1bfeee7575e137bc75741cb2caf015e55ecab2cd
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-09-13 14:23:14 -1000

    Raise NotImplementedError for datetime strings with UTC offset (#14070)
    Avoids e.g. DatetimeIndex(["2022-07-22 00:00:00+02:00"]) from dropping the +02:00 since timezones are not supported
    
    Authors:
      - Matthew Roeschke (https://github.com/mroeschke)
    
    Approvers:
      - Lawrence Mitchell (https://github.com/wence-)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/cudf/pull/14070

--- python/cudf/cudf/tests/test_datetime.py
+++ python/cudf/cudf/tests/test_datetime.py
@@ -2141,6 +2141,12 @@ def test_daterange_pandas_compatibility():
     assert_eq(expected, actual)
 
 
+def test_strings_with_utc_offset_not_implemented():
+    with pytest.warns(DeprecationWarning, match="parsing timezone"):  # cupy
+        with pytest.raises(NotImplementedError):
+            DatetimeIndex(["2022-07-22 00:00:00+02:00"])
+
+
 @pytest.mark.parametrize("code", ["z", "Z"])
 def test_format_timezone_not_implemented(code):
     with pytest.raises(NotImplementedError):
