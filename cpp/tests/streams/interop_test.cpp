cf30fef29071d3674135d114968dd7db6e2266e4
Vyas Ramasubramani <vyasr@nvidia.com>
2023-09-14 17:07:12 -0700

    Also add stream tests for the table APIs

--- cpp/tests/streams/interop_test.cpp
+++ cpp/tests/streams/interop_test.cpp
@@ -19,11 +19,38 @@
 #include <cudf/scalar/scalar_factories.hpp>
 
 #include <cudf_test/base_fixture.hpp>
+#include <cudf_test/column_wrapper.hpp>
 #include <cudf_test/default_stream.hpp>
 
 struct ArrowTest : public cudf::test::BaseFixture {};
 
 TEST_F(ArrowTest, ToArrow)
+{
+  int32_t const value{42};
+  auto col = cudf::test::fixed_width_column_wrapper<int32_t>{{value}};
+  cudf::table_view tbl{{col}};
+
+  std::vector<cudf::column_metadata> metadata{{""}};
+  auto arrow_scalar = cudf::to_arrow(tbl, metadata, cudf::test::get_default_stream());
+}
+
+TEST_F(ArrowTest, FromArrow)
+{
+  std::vector<int64_t> host_values = {1, 2, 3, 5, 6, 7, 8};
+  std::vector<bool> host_validity  = {true, true, true, false, true, true, true};
+
+  arrow::Int64Builder builder;
+  auto status      = builder.AppendValues(host_values, host_validity);
+  auto maybe_array = builder.Finish();
+  auto array       = *maybe_array;
+
+  auto field  = arrow::field("", arrow::int32());
+  auto schema = arrow::schema({field});
+  auto table  = arrow::Table::Make(schema, {array});
+  cudf::from_arrow(*table, cudf::test::get_default_stream());
+}
+
+TEST_F(ArrowTest, ToArrowScalar)
 {
   int32_t const value{42};
   auto cudf_scalar =
@@ -33,7 +60,7 @@ TEST_F(ArrowTest, ToArrow)
   auto arrow_scalar = cudf::to_arrow(*cudf_scalar, metadata, cudf::test::get_default_stream());
 }
 
-TEST_F(ArrowTest, FromArrow)
+TEST_F(ArrowTest, FromArrowScalar)
 {
   int32_t const value{42};
   auto arrow_scalar = arrow::MakeScalar(value);
