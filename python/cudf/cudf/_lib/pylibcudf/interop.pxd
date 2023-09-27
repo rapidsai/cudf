a0aec16614f4a2c98016b05c42486730b469b111
Vyas Ramasubramani <vyasr@nvidia.com>
2023-09-27 16:55:09 -0700

    Also implement from/to_arrow for Table

--- python/cudf/cudf/_lib/pylibcudf/interop.pxd
+++ python/cudf/cudf/_lib/pylibcudf/interop.pxd
@@ -1,20 +1,9 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.
 
-from pyarrow cimport lib as pa
-
 from cudf._lib.cpp.interop cimport column_metadata
-
-from .scalar cimport Scalar
-from .table cimport Table
 
 
 cdef class ColumnMetadata:
     cdef public object name
     cdef public object children_meta
     cdef column_metadata to_libcudf(self)
-
-cpdef Table from_arrow(
-    pa.Table pyarrow_table,
-)
-
-cpdef pa.Table to_arrow(Table tbl, list metadata)
--- python/cudf/cudf/_lib/pylibcudf/interop.pyx
+++ python/cudf/cudf/_lib/pylibcudf/interop.pyx
@@ -1,18 +1,6 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.
 
-from libcpp.memory cimport shared_ptr, unique_ptr
-from libcpp.utility cimport move
-from libcpp.vector cimport vector
-from pyarrow cimport lib as pa
-
-from cudf._lib.cpp.interop cimport (
-    column_metadata,
-    from_arrow as cpp_from_arrow,
-    to_arrow as cpp_to_arrow,
-)
-from cudf._lib.cpp.table.table cimport table
-
-from .table cimport Table
+from cudf._lib.cpp.interop cimport column_metadata
 
 
 cdef class ColumnMetadata:
@@ -33,30 +21,3 @@ cdef class ColumnMetadata:
         for child_meta in self.children_meta:
             c_metadata.children_meta.push_back(child_meta.to_libcudf())
         return c_metadata
-
-
-cpdef Table from_arrow(
-    pa.Table pyarrow_table,
-):
-    cdef shared_ptr[pa.CTable] ctable = (
-        pa.pyarrow_unwrap_table(pyarrow_table)
-    )
-    cdef unique_ptr[table] c_result
-
-    with nogil:
-        c_result = move(cpp_from_arrow(ctable.get()[0]))
-
-    return Table.from_libcudf(move(c_result))
-
-
-cpdef pa.Table to_arrow(Table tbl, list metadata):
-    cdef shared_ptr[pa.CTable] c_result
-    cdef vector[column_metadata] c_metadata
-    cdef ColumnMetadata meta
-    for meta in metadata:
-        c_metadata.push_back(meta.to_libcudf())
-
-    with nogil:
-        c_result = move(cpp_to_arrow(tbl.view(), c_metadata))
-
-    return pa.pyarrow_wrap_table(c_result)
