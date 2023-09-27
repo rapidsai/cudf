f6419b48636f8bf58424bfc1eeb74438cecf6ad1
David Wendt <dwendt@nvidia.com>
2023-09-27 18:41:39 -0400

    return ref experiment

--- cpp/include/cudf/detail/normalizing_iterator.cuh
+++ cpp/include/cudf/detail/normalizing_iterator.cuh
@@ -331,7 +331,7 @@ struct output_normalator : base_normalator<output_normalator<Integer>, Integer> 
    * @brief Indirection operator returns this iterator instance in order
    * to capture the `operator=(Integer)` calls.
    */
-  __device__ inline output_normalator const operator*() const { return *this; }
+  __device__ inline output_normalator const& operator*() const { return *this; }
 
   /**
    * @brief Array subscript operator returns an iterator instance at the specified `idx` position.
