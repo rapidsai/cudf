317a4a49e9aef5e7f2498143a8ac0fe3313bdcfe
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-10-06 16:08:44 -0700

    Correct and test cases with DataFrame(Series(name=), columns=)

--- python/cudf/cudf/tests/test_dataframe.py
+++ python/cudf/cudf/tests/test_dataframe.py
@@ -10413,10 +10413,35 @@ def test_dataframe_nlargest_nsmallest_str_error(attr):
     )
 
 
-def test_dictlike_data_column_length_mismatch():
-    ser = cudf.Series(range(5))
+def test_series_data_no_name_with_columns():
+    gdf = cudf.DataFrame(cudf.Series([1]), columns=[1])
+    pdf = pd.DataFrame(pd.Series([1]), columns=[1])
+    assert_eq(gdf, pdf)
+
+
+def test_series_data_no_name_with_columns_more_than_one_raises():
     with pytest.raises(ValueError):
-        cudf.DataFrame(ser, columns=[1, 2])
+        cudf.DataFrame(cudf.Series([1]), columns=[1, 2])
+    with pytest.raises(ValueError):
+        pd.DataFrame(pd.Series([1]), columns=[1, 2])
+
+
+def test_series_data_with_name_with_columns_matching():
+    gdf = cudf.DataFrame(cudf.Series([1], name=1), columns=[1])
+    pdf = pd.DataFrame(pd.Series([1], name=1), columns=[1])
+    assert_eq(gdf, pdf)
+
+
+def test_series_data_with_name_with_columns_not_matching():
+    gdf = cudf.DataFrame(cudf.Series([1], name=2), columns=[1])
+    pdf = pd.DataFrame(pd.Series([1], name=2), columns=[1])
+    assert_eq(gdf, pdf)
+
+
+def test_series_data_with_name_with_columns_matching_align():
+    gdf = cudf.DataFrame(cudf.Series([1], name=2), columns=[1, 2])
+    pdf = pd.DataFrame(pd.Series([1], name=2), columns=[1, 2])
+    assert_eq(gdf, pdf)
 
 
 @pytest.mark.parametrize("digits", [0, 1, 3, 4, 10])
