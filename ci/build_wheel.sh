8dfe66e3a0945a160acb9c17019d4d433313f6f2
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-17 14:31:44 -0700

    Fix one more path

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -20,7 +20,7 @@ PACKAGE_CUDA_SUFFIX="-${RAPIDS_PY_CUDA_SUFFIX}"
 
 # Patch project metadata files to include the CUDA version suffix and version override.
 pyproject_file="${package_dir}/pyproject.toml"
-version_file="${package_dir}/${package_name}/_version.py"
+version_file="${package_dir}/${package_name}/${package_name}/_version.py"
 
 sed -i "s/name = \"${package_name}\"/name = \"${package_name}${PACKAGE_CUDA_SUFFIX}\"/g" ${pyproject_file}
 sed -i "/^__version__/ s/= .*/= ${version}/g" ${version_file}
