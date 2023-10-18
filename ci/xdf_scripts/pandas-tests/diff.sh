1ee0a8ddff07df840882807993656bab1a654355
Lawrence Mitchell <lmitchell@nvidia.com>
2023-10-18 17:51:26 +0100

    Show trunk stats (total passed and total tests run) in job summary

--- ci/xdf_scripts/pandas-tests/diff.sh
+++ ci/xdf_scripts/pandas-tests/diff.sh
@@ -23,7 +23,7 @@ aws s3 cp $PR_ARTIFACT pr-results.json
 python -m pip install pandas tabulate
 python ci/xdf_scripts/pandas-tests/job-summary.py main-results.json pr-results.json | tee summary.txt >> "$GITHUB_STEP_SUMMARY"
 
-COMMENT=$(head -1 summary.txt)
+COMMENT=$(head -2 summary.txt)
 
 echo "$COMMENT"
 
--- ci/xdf_scripts/pandas-tests/job-summary.py
+++ ci/xdf_scripts/pandas-tests/job-summary.py
@@ -48,7 +48,8 @@ comment = (
     f"{pr_passed}/{pr_total} ({passing_percentage:.2f}%) "
     "Pandas tests passing, "
     f"{rate_change_type} in the test pass rate by "
-    f"{pass_rate_change:.2f}%."
+    f"{pass_rate_change:.2f}%.\n"
+    f"Trunk stats: {main_passed}/{main_total}."
 )
 
 
