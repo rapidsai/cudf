51940f5fd6ac268b445f8bf23b3ec3be58f638cb
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-10-18 10:44:09 -0700

    generalize construction

--- python/cudf/cudf/pandas/fast_slow_proxy.py
+++ python/cudf/cudf/pandas/fast_slow_proxy.py
@@ -911,13 +911,9 @@ def _transform_arg(
         transformed = [
             _transform_arg(a, attribute_name, seen) for a in arg.flat
         ]
-        if arg.ndim == 1 and len(arg) and isinstance(arg[0], tuple):
-            # Don't unpack tuple elements
-            result = np.empty(len(arg), dtype=object)
-            result[...] = transformed
-            return result
-        else:
-            return np.asarray(transformed, dtype=object).reshape(arg.shape)
+        result = np.empty(np.prod(arg.shape), dtype=object)
+        result[...] = transformed
+        return result.reshape(arg.shape)
     elif isinstance(arg, Iterator) and attribute_name == "_xdf_fast":
         # this may include consumable objects like generators or
         # IOBase objects, which we don't want unavailable to the slow
