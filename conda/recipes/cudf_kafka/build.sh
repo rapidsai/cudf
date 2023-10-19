fb5e9217931b06a262bb8366aac3b2c0a4ddaf19
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-18 18:13:21 -0700

    Make sure cuda.h is installed and patch path during conda build

--- conda/recipes/cudf_kafka/build.sh
+++ conda/recipes/cudf_kafka/build.sh
@@ -1,4 +1,13 @@
 # Copyright (c) 2020-2022, NVIDIA CORPORATION.
 
 # This assumes the script is executed from the root of the repo directory
+# Need to set CUDA_HOME inside conda environments because the hacked together
+# setup.py for cudf-kafka searches that way.
+# TODO: Remove after https://github.com/rapidsai/cudf/pull/14292 updates
+# cudf_kafka to use scikit-build
+target_name="x86_64-linux"
+if [[ ! $(arch) == "x86_64" ]]; then
+  target_name="sbsa-linux"
+fi
+export CUDA_HOME="${PREFIX}/targets/${target_name}/"
 ./build.sh -v cudf_kafka
