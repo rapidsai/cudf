2b752119363eea3e8d9aae05101afc8d19aaa2eb
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-18 17:27:20 -0700

    Add some CPU code to the profile logic

--- python/cudf/xdf_tests/test_profiler.py
+++ python/cudf/xdf_tests/test_profiler.py
@@ -30,16 +30,24 @@ def test_profiler():
         sums = df.groupby("idx").sum()
         total = df.sum()["data"]
         assert np.isclose(total, sums.sum()["data"])
+        _ = pd.Timestamp(2020, 1, 1) + pd.Timedelta(1)
 
     per_function_stats = profiler.per_function_stats
     assert set(per_function_stats) == {
+        "Timestamp",
+        "Timedelta",
         "DataFrame",
         "DataFrame.groupby",
         "DataFrameGroupBy.sum",
         "DataFrame.sum",
         "Series.__getitem__",
     }
-    assert all(len(func["cpu"]) == 0 for func in per_function_stats.values())
+    for name, func in per_function_stats.items():
+        assert (
+            len(func["cpu"]) == 0
+            if "Time" not in name
+            else len(func["gpu"]) == 0
+        )
 
     per_line_stats = profiler.per_line_stats
     calls = [
@@ -50,9 +58,10 @@ def test_profiler():
         'df.groupby("idx").sum',
         'df.sum()["data"]',
         "np.isclose",
+        "pd.Timestamp",
     ]
     for line_stats, call in zip(per_line_stats, calls):
         # Check that the expected function calls were recorded.
         assert call in line_stats[1]
         # No CPU time
-        assert line_stats[3] == 0
+        assert line_stats[3] == 0 if "Time" not in call else line_stats[2] == 0
