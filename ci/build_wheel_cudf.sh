d92161ec23012d7ee71b92ceae0c0e22d5a904d8
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-28 10:42:27 -0700

    Manually overwrite the manylinux policy for rocky

--- ci/build_wheel_cudf.sh
+++ ci/build_wheel_cudf.sh
@@ -10,6 +10,13 @@ export SKBUILD_CONFIGURE_OPTIONS="-DCUDF_BUILD_WHEELS=ON -DDETECT_CONDA_ENV=OFF"
 ./ci/build_wheel.sh cudf ${package_dir}
 
 mkdir -p ${package_dir}/final_dist
+if command -v dnf >/dev/null 2>&1 ; then
+    export POLICY="manylinux_2_28"
+    export REAL_ARCH=$(arch)
+    export AUDITWHEEL_POLICY=${POLICY}
+    export AUDITWHEEL_ARCH=${REAL_ARCH}
+    export AUDITWHEEL_PLAT=${POLICY}_${REAL_ARCH}
+fi
 python -m auditwheel repair -w ${package_dir}/final_dist ${package_dir}/dist/*
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
