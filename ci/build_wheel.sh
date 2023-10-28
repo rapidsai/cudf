c6d2054b07e6ea9fe228790245687f3f4b55d0fc
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-28 09:02:38 -0700

    Only install on rocky

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -6,7 +6,9 @@ set -euo pipefail
 package_name=$1
 package_dir=$2
 
-dnf install -y jq
+if command -v dnf >/dev/null 2>&1 ; then
+    dnf install -y jq
+fi
 source rapids-configure-sccache
 source rapids-date-string
 
