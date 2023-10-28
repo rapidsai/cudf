626df446eeace712faec1f776a99b7d67917b0a3
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-28 13:24:53 -0700

    Encode the manylinux version in the built wheels and pull appropriately

--- ci/build_wheel_cudf.sh
+++ ci/build_wheel_cudf.sh
@@ -9,6 +9,7 @@ export SKBUILD_CONFIGURE_OPTIONS="-DCUDF_BUILD_WHEELS=ON -DDETECT_CONDA_ENV=OFF"
 
 ./ci/build_wheel.sh cudf ${package_dir}
 
+manylinux="manylinux_2_17"
 mkdir -p ${package_dir}/final_dist
 if command -v dnf >/dev/null 2>&1 ; then
     export POLICY="manylinux_2_28"
@@ -16,8 +17,9 @@ if command -v dnf >/dev/null 2>&1 ; then
     export AUDITWHEEL_POLICY=${POLICY}
     export AUDITWHEEL_ARCH=${REAL_ARCH}
     export AUDITWHEEL_PLAT=${POLICY}_${REAL_ARCH}
+    manylinux="manylinux_2_28"
 fi
 python -m auditwheel repair -w ${package_dir}/final_dist ${package_dir}/dist/*
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 ${package_dir}/final_dist
+RAPIDS_PY_WHEEL_NAME="cudf_${manylinux}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 ${package_dir}/final_dist
--- ci/test_wheel_cudf.sh
+++ ci/test_wheel_cudf.sh
@@ -3,8 +3,18 @@
 
 set -eou pipefail
 
+# Set the manylinux version to pull the newer ABI wheels when testing on Ubuntu 20.04 or later.
+manylinux="manylinux_2_17"
+if command -v lsb_release >/dev/null 2>&1 ; then
+    release_info=$(lsb_release -r)
+    major_version=$(echo "$release_info" | awk '{print $2}' | cut -d. -f1)
+    if [[ ${major_version} -ge 20 ]]; then
+        manylinux="manylinux_2_28"
+    fi
+fi
+
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
+RAPIDS_PY_WHEEL_NAME="cudf_${manylinux}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/cudf*.whl)[test]
