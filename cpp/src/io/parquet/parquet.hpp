244c36939e36196c34145a982faf7ac15f9005b4
Ed Seidl <etseidl@live.com>
2023-10-28 13:11:07 -0700

    more clean up

--- cpp/src/io/parquet/parquet.hpp
+++ cpp/src/io/parquet/parquet.hpp
@@ -331,7 +331,6 @@ struct ColumnChunkMetaData {
     0;  // total byte size of all uncompressed pages in this column chunk (including the headers)
   int64_t total_compressed_size =
     0;  // total byte size of all compressed pages in this column chunk (including the headers)
-  std::vector<KeyValue> key_value_metadata;  // per chunk metadata
   int64_t data_page_offset  = 0;  // Byte offset from beginning of file to first data page
   int64_t index_page_offset = 0;  // Byte offset from beginning of file to root index page
   int64_t dictionary_page_offset =
