a55b3afff91d71ce44c8a055e6e59902437d1c34
Shruti Shivakumar <shruti.shivakumar@gmail.com>
2023-11-03 21:47:27 +0000

    modified tests to take constant columns; stream pool error fix

--- cpp/tests/streams/io/orc_test.cpp
+++ cpp/tests/streams/io/orc_test.cpp
@@ -38,78 +38,35 @@ auto const temp_env = static_cast<cudf::test::TempDirTestEnvironment*>(
 
 class ORCTest : public cudf::test::BaseFixture {};
 
-template <typename T>
-inline auto random_values(size_t size)
-{
-  std::vector<T> values(size);
-
-  using T1 = T;
-  using uniform_distribution =
-    typename std::conditional_t<std::is_same_v<T1, bool>,
-                                std::bernoulli_distribution,
-                                std::conditional_t<std::is_floating_point_v<T1>,
-                                                   std::uniform_real_distribution<T1>,
-                                                   std::uniform_int_distribution<T1>>>;
-
-  static constexpr auto seed = 0xf00d;
-  static std::mt19937 engine{seed};
-  static uniform_distribution dist{};
-  std::generate_n(values.begin(), size, [&]() { return T{dist(engine)}; });
-
-  return values;
-}
-
-inline auto random_strings(size_t size)
-{
-  std::vector<std::string> values(size);
-  std::vector<int> pos(size);
-  static constexpr auto seed = 0xf00d;
-  static std::mt19937 engine{seed};
-  static std::uniform_int_distribution<> dist(0, 25);
-  std::transform(values.begin(), values.end(), values.begin(), [&pos](std::string s) {
-    std::generate(pos.begin(), pos.end(), [&]() { return dist(engine); });
-    std::transform(
-      pos.begin(), pos.end(), std::back_inserter(s), [](const int& p) { return p + 'a'; });
-    return s;
-  });
-  return values;
-}
-
 TEST_F(ORCTest, ORCWriter)
 {
   constexpr auto num_rows = 10;
 
-  auto col0_data = random_values<bool>(num_rows);
-  auto col1_data = random_values<int8_t>(num_rows);
-  auto col2_data = random_values<int16_t>(num_rows);
-  auto col3_data = random_values<int32_t>(num_rows);
-  auto col4_data = random_values<float>(num_rows);
-  auto col5_data = random_values<double>(num_rows);
-  auto col6_vals = random_values<int64_t>(num_rows);
+  std::vector<size_t> zeros(num_rows, 0);
+  std::vector<size_t> ones(num_rows, 1);
   auto col6_data = cudf::detail::make_counting_transform_iterator(0, [&](auto i) {
-    return numeric::decimal128{col6_vals[i], numeric::scale_type{12}};
+    return numeric::decimal128{ones[i], numeric::scale_type{12}};
   });
   auto col7_data = cudf::detail::make_counting_transform_iterator(0, [&](auto i) {
-    return numeric::decimal128{col6_vals[i], numeric::scale_type{-12}};
+    return numeric::decimal128{ones[i], numeric::scale_type{-12}};
   });
 
-  cudf::test::fixed_width_column_wrapper<bool> col0(col0_data.begin(), col0_data.end());
-  cudf::test::fixed_width_column_wrapper<int8_t> col1(col1_data.begin(), col1_data.end());
-  cudf::test::fixed_width_column_wrapper<int16_t> col2(col2_data.begin(), col2_data.end());
-  cudf::test::fixed_width_column_wrapper<int32_t> col3(col3_data.begin(), col3_data.end());
-  cudf::test::fixed_width_column_wrapper<float> col4(col4_data.begin(), col4_data.end());
-  cudf::test::fixed_width_column_wrapper<double> col5(col5_data.begin(), col5_data.end());
+  cudf::test::fixed_width_column_wrapper<bool> col0(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<int8_t> col1(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<int16_t> col2(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<int32_t> col3(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<float> col4(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<double> col5(zeros.begin(), zeros.end());
   cudf::test::fixed_width_column_wrapper<numeric::decimal128> col6(col6_data, col6_data + num_rows);
   cudf::test::fixed_width_column_wrapper<numeric::decimal128> col7(col7_data, col7_data + num_rows);
 
   cudf::test::lists_column_wrapper<int64_t> col8{
-    {9, 8}, {7, 6, 5}, {}, {4}, {3, 2, 1, 0}, {20, 21, 22, 23, 24}, {}, {66, 666}, {}, {-1, -2}};
+    {1, 1}, {1, 1, 1}, {}, {1}, {1, 1, 1, 1}, {1, 1, 1, 1, 1}, {}, {1, -1}, {}, {-1, -1}};
 
-  cudf::test::fixed_width_column_wrapper<int32_t> child_col{
-    48, 27, 25, 31, 351, 351, 29, 15, -1, -99};
+  cudf::test::fixed_width_column_wrapper<int32_t> child_col(ones.begin(), ones.end());
   cudf::test::structs_column_wrapper col9{child_col};
 
-  auto col10_data = random_strings(num_rows);
+  std::vector<std::string> col10_data(num_rows, "rapids");
   cudf::test::strings_column_wrapper col10(col10_data.begin(), col10_data.end());
 
   cudf::table_view tab({col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10});
@@ -138,37 +95,31 @@ TEST_F(ORCTest, ORCReader)
 {
   constexpr auto num_rows = 10;
 
-  auto col0_data = random_values<bool>(num_rows);
-  auto col1_data = random_values<int8_t>(num_rows);
-  auto col2_data = random_values<int16_t>(num_rows);
-  auto col3_data = random_values<int32_t>(num_rows);
-  auto col4_data = random_values<float>(num_rows);
-  auto col5_data = random_values<double>(num_rows);
-  auto col6_vals = random_values<int64_t>(num_rows);
+  std::vector<size_t> zeros(num_rows, 0);
+  std::vector<size_t> ones(num_rows, 1);
   auto col6_data = cudf::detail::make_counting_transform_iterator(0, [&](auto i) {
-    return numeric::decimal128{col6_vals[i], numeric::scale_type{12}};
+    return numeric::decimal128{ones[i], numeric::scale_type{12}};
   });
   auto col7_data = cudf::detail::make_counting_transform_iterator(0, [&](auto i) {
-    return numeric::decimal128{col6_vals[i], numeric::scale_type{-12}};
+    return numeric::decimal128{ones[i], numeric::scale_type{-12}};
   });
 
-  cudf::test::fixed_width_column_wrapper<bool> col0(col0_data.begin(), col0_data.end());
-  cudf::test::fixed_width_column_wrapper<int8_t> col1(col1_data.begin(), col1_data.end());
-  cudf::test::fixed_width_column_wrapper<int16_t> col2(col2_data.begin(), col2_data.end());
-  cudf::test::fixed_width_column_wrapper<int32_t> col3(col3_data.begin(), col3_data.end());
-  cudf::test::fixed_width_column_wrapper<float> col4(col4_data.begin(), col4_data.end());
-  cudf::test::fixed_width_column_wrapper<double> col5(col5_data.begin(), col5_data.end());
+  cudf::test::fixed_width_column_wrapper<bool> col0(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<int8_t> col1(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<int16_t> col2(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<int32_t> col3(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<float> col4(zeros.begin(), zeros.end());
+  cudf::test::fixed_width_column_wrapper<double> col5(zeros.begin(), zeros.end());
   cudf::test::fixed_width_column_wrapper<numeric::decimal128> col6(col6_data, col6_data + num_rows);
   cudf::test::fixed_width_column_wrapper<numeric::decimal128> col7(col7_data, col7_data + num_rows);
 
   cudf::test::lists_column_wrapper<int64_t> col8{
-    {9, 8}, {7, 6, 5}, {}, {4}, {3, 2, 1, 0}, {20, 21, 22, 23, 24}, {}, {66, 666}, {}, {-1, -2}};
+    {1, 1}, {1, 1, 1}, {}, {1}, {1, 1, 1, 1}, {1, 1, 1, 1, 1}, {}, {1, -1}, {}, {-1, -1}};
 
-  cudf::test::fixed_width_column_wrapper<int32_t> child_col{
-    48, 27, 25, 31, 351, 351, 29, 15, -1, -99};
+  cudf::test::fixed_width_column_wrapper<int32_t> child_col(ones.begin(), ones.end());
   cudf::test::structs_column_wrapper col9{child_col};
 
-  auto col10_data = random_strings(num_rows);
+  std::vector<std::string> col10_data(num_rows, "rapids");
   cudf::test::strings_column_wrapper col10(col10_data.begin(), col10_data.end());
 
   cudf::table_view tab({col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10});
