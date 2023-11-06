1667263260cc04684bd1768b10e4d2344c4fe12b
Mark Harris <783069+harrism@users.noreply.github.com>
2023-11-06 23:26:43 +0000

    Modify CI to test cuDF with rapidsai/rmm#1095

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -7,6 +7,12 @@ source rapids-env-update
 
 export CMAKE_GENERATOR=Ninja
 
+REPO="rmm"
+PR_NUMBER="1095"
+COMMIT=$(git ls-remote https://github.com/rapidsai/${REPO}.git refs/heads/pull-request/${PR_NUMBER} | cut -c1-7)
+RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
+LIBRMM_CHANNEL=$(rapids-get-artifact ci/${REPO}/pull-request/${PR_NUMBER}/${COMMIT}/rmm_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
+
 rapids-print-env
 
 version=$(rapids-generate-version)
@@ -15,6 +21,7 @@ rapids-logger "Begin cpp build"
 
 # With boa installed conda build forward to boa
 RAPIDS_PACKAGE_VERSION=${version} rapids-conda-retry mambabuild \
+    --channel "${LIBRMM_CHANNEL}" \
     conda/recipes/libcudf
 
 rapids-upload-conda-to-s3 cpp
--- ci/test_cpp_common.sh
+++ ci/test_cpp_common.sh
@@ -19,6 +19,15 @@ conda activate test
 set -u
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+
+REPO="rmm"
+PR_NUMBER="1095"
+COMMIT=$(git ls-remote https://github.com/rapidsai/${REPO}.git refs/heads/pull-request/${PR_NUMBER} | cut -c1-7)
+RAPIDS_CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
+PYTHON_MINOR_VERSION=$(python --version | sed -E 's/Python [0-9]+\.([0-9]+)\.[0-9]+/\1/g')
+LIBRMM_CHANNEL=$(rapids-get-artifact ci/${REPO}/pull-request/${PR_NUMBER}/${COMMIT}/rmm_conda_cpp_cuda${RAPIDS_CUDA_MAJOR}_$(arch).tar.gz)
+RMM_CHANNEL=$(rapids-get-artifact ci/${REPO}/pull-request/${PR_NUMBER}/${COMMIT}/rmm_conda_python_cuda${RAPIDS_CUDA_MAJOR}_3${PYTHON_MINOR_VERSION}_$(arch).tar.gz)
+
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}/
 mkdir -p "${RAPIDS_TESTS_DIR}"
 
@@ -26,6 +35,8 @@ rapids-print-env
 
 rapids-mamba-retry install \
   --channel "${CPP_CHANNEL}" \
+  --channel "${RMM_CHANNEL}" \
+  --channel "${LIBRMM_CHANNEL}" \
   libcudf libcudf_kafka libcudf-tests
 
 rapids-logger "Check GPU usage"
