a5551e0f4923ed1a5d59443c2ae86d1ffc629e13
Alexander <aocsa.cs@gmail.com>
2023-11-21 21:38:04 +0000

    minor update

--- cpp/tests/ast/transform_tests.cpp
+++ cpp/tests/ast/transform_tests.cpp
@@ -346,25 +346,24 @@ TEST_F(TransformTest, ImbalancedTreeArithmeticDeep)
 TEST_F(TransformTest, DeeplyNestedArithmeticLogicalExpression)
 {
   // Test logic for deeply nested arithmetic and logical expressions.
-  constexpr int64_t kLeftDepthLevel  = 100;
-  constexpr int64_t kRightDepthLevel = 75;
+  constexpr int64_t left_depth_level  = 100;
+  constexpr int64_t right_depth_level = 75;
 
   auto generate_ast_expr = [](int64_t depth_level,
                               cudf::ast::column_reference col_ref,
                               cudf::ast::ast_operator root_operator,
+                              cudf::ast::ast_operator arithmetic_operator,
                               bool nested_left_tree) {
-    auto expressions                                          = std::list<cudf::ast::operation>();
-    std::vector<cudf::ast::ast_operator> arithmetic_operators = {cudf::ast::ast_operator::ADD,
-                                                                 cudf::ast::ast_operator::SUB};
+    std::vector<cudf::ast::operation> expressions;
 
-    auto op = arithmetic_operators[rand() % arithmetic_operators.size()];
+    auto op = arithmetic_operator;
     expressions.push_back(cudf::ast::operation(op, col_ref, col_ref));
 
     for (int64_t i = 0; i < depth_level - 1; i++) {
       if (i == depth_level - 2) {
         op = root_operator;
       } else {
-        op = arithmetic_operators[rand() % arithmetic_operators.size()];
+        op = arithmetic_operator;
       }
       if (nested_left_tree) {
         expressions.push_back(cudf::ast::operation(op, expressions.back(), col_ref));
@@ -379,28 +378,26 @@ TEST_F(TransformTest, DeeplyNestedArithmeticLogicalExpression)
   auto c_1   = column_wrapper<int32_t>{0, 0, 0};
   auto table = cudf::table_view{{c_0, c_1}};
 
-  auto expressions = std::list<cudf::ast::operation>();
-
   auto col_ref_0 = cudf::ast::column_reference(0);
   auto col_ref_1 = cudf::ast::column_reference(1);
 
   auto left_expression =
-    generate_ast_expr(kLeftDepthLevel, col_ref_0, cudf::ast::ast_operator::LESS, false);
+    generate_ast_expr(left_depth_level, col_ref_0, cudf::ast::ast_operator::LESS, cudf::ast::ast_operator::ADD, false);
   auto right_expression =
-    generate_ast_expr(kRightDepthLevel, col_ref_1, cudf::ast::ast_operator::EQUAL, true);
+    generate_ast_expr(right_depth_level, col_ref_1, cudf::ast::ast_operator::EQUAL, cudf::ast::ast_operator::SUB, true);
 
   auto expression_tree = cudf::ast::operation(
     cudf::ast::ast_operator::LOGICAL_OR, left_expression.back(), right_expression.back());
 
   // Expression:
-  // OR(<(-(-(+(-($0, $0), $0), $0), $0), $0), ==($1, -($1, +($1, +($1, -($1, $1))))))
+  // OR(<(+(+(+(+($0, $0), $0), $0), $0), $0), ==($1, -($1, -($1, -($1, -($1, $1))))))
   // ...
   // OR(<($L, $0), ==($1, $R))
   // true
   //
   // Breakdown:
-  // - Left Operand ($L): (-(-(+(-($0, $0), $0), $0), $0), $0)
-  // - Right Operand ($R): -($1, +($1, +($1, -($1, $1))))
+  // - Left Operand ($L): (+(+(+(+($0, $0), $0), $0), $0), $0)
+  // - Right Operand ($R): -($1, -($1, -($1, -($1, $1))))
   // Explanation:
   // If all $1 values and $R values are zeros, the result is true because of the equality check
   // combined with the OR operator in OR(<($L, $0), ==($1, $R)).
