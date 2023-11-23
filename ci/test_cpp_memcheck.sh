e3bd947e7dbee660a3ce4735414e7dd867ede52b
Bradley Dice <bdice@bradleydice.com>
2023-11-22 18:09:10 -0600

    Update ci/test_cpp_memcheck.sh
    Co-authored-by: David Wendt <45795991+davidwendt@users.noreply.github.com>

--- ci/test_cpp_memcheck.sh
+++ ci/test_cpp_memcheck.sh
@@ -9,7 +9,7 @@ set +e
 
 # Run gtests with compute-sanitizer
 compute-sanitizer --tool memcheck "$CONDA_PREFIX"/bin/gtests/libcudf/PARQUET_TEST --gtest_filter=ParquetWriterNumericTypeTest/3.SingleColumnWithNulls --rmm_mode=cuda
-compute-sanitizer --tool memcheck "$CONDA_PREFIX"/bin/gtests/ORC_TEST --gtest_filter=OrcWriterNumericTypeTest/6.SingleColumn --rmm_mode=cuda
+compute-sanitizer --tool memcheck "$CONDA_PREFIX"/bin/gtests/libcudf/ORC_TEST --gtest_filter=OrcWriterNumericTypeTest/6.SingleColumn --rmm_mode=cuda
 
 rapids-logger "Test script exiting with value: $EXITCODE"
 exit ${EXITCODE}
