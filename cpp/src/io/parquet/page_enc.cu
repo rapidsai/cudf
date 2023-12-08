41b2ea507ad033508bf6c624c0ffbeff8d1c0c7a
seidl <seidl2@llnl.gov>
2023-12-08 10:51:37 -0800

    remove unnecessary syncthreads

--- cpp/src/io/parquet/page_enc.cu
+++ cpp/src/io/parquet/page_enc.cu
@@ -205,7 +205,7 @@ void __device__ calculate_frag_size(frag_init_state_s* const s, int t)
       }
     }
   }
-  __syncthreads();
+
   auto const total_len = block_reduce(reduce_storage).Sum(len);
   __syncthreads();
   auto const total_valid = block_reduce(reduce_storage).Sum(num_valid);
