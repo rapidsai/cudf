681721761b17e97f69affa8bd9874d5bf1484319
Vyas Ramasubramani <vyasr@nvidia.com>
2023-12-15 00:33:14 +0000

    Parse more precisely and remove potential SFINAE duplicates

--- cpp/doxygen/parse_xml.py
+++ cpp/doxygen/parse_xml.py
@@ -4,20 +4,36 @@ import xml.etree.ElementTree as ET
 import glob
 
 
-types_to_remove = ("CUDF_ENABLE_IF", "std::enable_if")
-defvals_to_remove = ("nullptr")
 def remove_param_with_cudf_enable_if(root):
-    for parent in root.findall(".//param/.."):
-        for param in parent.findall(".//param"):
-            for type_ in param.findall("./type"):
-                if any(r in ET.tostring(type_).decode() for r in types_to_remove):
-                    parent.remove(param)
-                    break
-        for param in parent.findall(".//param"):
-            for type_ in param.findall("./defval"):
-                if any(r in ET.tostring(type_).decode() for r in defvals_to_remove):
-                    parent.remove(param)
-                    break
+    types_to_remove = ("CUDF_ENABLE_IF", "std::enable_if")
+    defvals_to_remove = ("nullptr")
+
+
+    seen_ids = set()
+    for sectiondef in root.findall(".//sectiondef"):
+        for memberdef in sectiondef.findall("./memberdef"):
+            for parent in memberdef.findall("./templateparamlist"):
+                for param in parent.findall("./param"):
+                    for type_ in param.findall("./type"):
+                        if any(r in ET.tostring(type_).decode() for r in types_to_remove):
+                            id_ = memberdef.get("id")
+                            assert id_ is not None
+                            # If this is the first time we're seeing this function, just
+                            # remove the template parameter. Otherwise, remove the
+                            # overload altogether and just rely on documenting one of
+                            # the SFINAE overloads.
+                            if id_ in seen_ids:
+                                sectiondef.remove(memberdef)
+                            else:
+                                seen_ids.add(id_)
+                                parent.remove(param)
+                            break
+            for parent in memberdef.findall("./templateparamlist"):
+                for param in parent.findall("./param"):
+                    for type_ in param.findall("./defval"):
+                        if any(r in ET.tostring(type_).decode() for r in defvals_to_remove):
+                            parent.remove(param)
+                            break
 
     strings_to_remove = ("__forceinline__", "CUDF_HOST_DEVICE", "decltype(auto)")
     for field in (".//type", ".//definition"):
