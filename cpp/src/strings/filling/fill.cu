10cc3aa6d368faf125269098a5271ea72646f9a7
David Wendt <dwendt@nvidia.com>
2024-01-02 22:38:47 -0500

    handle fill-all edge case

--- cpp/src/strings/filling/fill.cu
+++ cpp/src/strings/filling/fill.cu
@@ -16,7 +16,6 @@
 
 #include <cudf/column/column_device_view.cuh>
 #include <cudf/detail/valid_if.cuh>
-#include <cudf/scalar/scalar_device_view.cuh>
 #include <cudf/strings/detail/fill.hpp>
 #include <cudf/strings/detail/strings_children.cuh>
 #include <cudf/strings/detail/utilities.cuh>
@@ -40,56 +39,61 @@ struct fill_fn {
   size_type* d_offsets{};
   char* d_chars{};
 
-  __device__ void operator()(size_type idx)
+  __device__ string_view resolve_string_at(size_type idx) const
   {
-    auto const d_str = d_strings.is_null(idx) ? string_view{} : d_strings.element<string_view>(idx);
-    auto const d_output = ((begin <= idx) && (idx < end)) ? d_value : d_str;
+    if ((begin <= idx) && (idx < end)) { return d_value; }
+    return d_strings.is_valid(idx) ? d_strings.element<string_view>(idx) : string_view{};
+  }
+
+  __device__ void operator()(size_type idx) const
+  {
+    auto const d_str = resolve_string_at(idx);
     if (!d_chars) {
-      d_offsets[idx] = d_output.size_bytes();
+      d_offsets[idx] = d_str.size_bytes();
     } else {
-      copy_string(d_chars + d_offsets[idx], d_output);
+      copy_string(d_chars + d_offsets[idx], d_str);
     }
   }
 };
 }  // namespace
 
-std::unique_ptr<column> fill(strings_column_view const& strings,
+std::unique_ptr<column> fill(strings_column_view const& input,
                              size_type begin,
                              size_type end,
                              string_scalar const& value,
                              rmm::cuda_stream_view stream,
                              rmm::mr::device_memory_resource* mr)
 {
-  auto strings_count = strings.size();
-  if (strings_count == 0) return make_empty_column(type_id::STRING);
+  auto const strings_count = input.size();
+  if (strings_count == 0) { return make_empty_column(type_id::STRING); }
   CUDF_EXPECTS((begin >= 0) && (end <= strings_count),
                "Parameters [begin,end) are outside the range of the provided strings column");
   CUDF_EXPECTS(begin <= end, "Parameters [begin,end) have invalid range values");
-  if (begin == end)  // return a copy
-    return std::make_unique<column>(strings.parent(), stream, mr);
+  if (begin == end) { return std::make_unique<column>(input.parent(), stream, mr); }
 
-  auto strings_column = column_device_view::create(strings.parent(), stream);
-  auto d_strings      = *strings_column;
+  auto strings_column  = column_device_view::create(input.parent(), stream);
+  auto const d_strings = *strings_column;
+  auto const is_valid  = value.is_valid(stream);
 
   // create resulting null mask
-  auto [null_mask, null_count] = [begin, end, &value, d_strings, stream, mr] {
-    if (begin == 0 and end == d_strings.size() and value.is_valid(stream)) {
+  auto [null_mask, null_count] = [begin, end, is_valid, d_strings, stream, mr] {
+    if (begin == 0 and end == d_strings.size() and is_valid) {
       return std::pair(rmm::device_buffer{}, 0);
     }
-    auto d_value = get_scalar_device_view(const_cast<string_scalar&>(value));
     return cudf::detail::valid_if(
       thrust::make_counting_iterator<size_type>(0),
       thrust::make_counting_iterator<size_type>(d_strings.size()),
-      [d_strings, begin, end, d_value] __device__(size_type idx) {
-        return ((begin <= idx) && (idx < end)) ? d_value.is_valid() : d_strings.is_valid(idx);
+      [d_strings, begin, end, is_valid] __device__(size_type idx) {
+        return ((begin <= idx) && (idx < end)) ? is_valid : d_strings.is_valid(idx);
       },
       stream,
       mr);
   }();
 
-  auto d_value     = const_cast<string_scalar&>(value);
-  auto const d_str = d_value.is_valid(stream) ? d_value.value(stream) : string_view{};
-  auto fn          = fill_fn{d_strings, begin, end, d_str};
+  auto const d_value = const_cast<string_scalar&>(value);
+  auto const d_str   = is_valid ? d_value.value(stream) : string_view{};
+  auto fn            = fill_fn{d_strings, begin, end, d_str};
+
   auto [offsets_column, chars_column] = make_strings_children(fn, strings_count, stream, mr);
 
   return make_strings_column(strings_count,
