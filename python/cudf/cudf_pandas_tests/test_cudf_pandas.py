be819e287c53550404e208a5df5d270a21e2a124
brandon-b-miller <brmiller@nvidia.com>
2024-01-04 07:38:02 -0800

    fix style

--- python/cudf/cudf_pandas_tests/test_cudf_pandas.py
+++ python/cudf/cudf_pandas_tests/test_cudf_pandas.py
@@ -668,7 +668,9 @@ def test_rolling_win_type():
     tm.assert_equal(result, expected)
 
 
-@pytest.mark.skip(reason="Requires Numba 0.59 to fix segfaults on ARM. See https://github.com/numba/llvmlite/pull/1009")
+@pytest.mark.skip(
+    reason="Requires Numba 0.59 to fix segfaults on ARM. See https://github.com/numba/llvmlite/pull/1009"
+)
 def test_rolling_apply_numba_engine():
     def weighted_mean(x):
         arr = np.ones((1, x.shape[1]))
