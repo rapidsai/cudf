23427a1ec3ec6b194a8305446590fd2ad04c57d8
Shruti Shivakumar <shruti.shivakumar@gmail.com>
2024-01-05 18:31:58 +0000

    removed relaxation to test stream relaxation; test cleanup

--- cpp/tests/streams/io/parquet_test.cpp
+++ cpp/tests/streams/io/parquet_test.cpp
@@ -34,8 +34,14 @@ auto const temp_env = static_cast<cudf::test::TempDirTestEnvironment*>(
 
 class ParquetTest : public cudf::test::BaseFixture {};
 
-TEST_F(ParquetTest, ParquetWriter)
-{
+template<typename ... UniqPtrs>
+std::vector<std::unique_ptr<cudf::column>> make_uniqueptrs_vector(UniqPtrs&& ... uniqptrs) {
+  std::vector<std::unique_ptr<cudf::column>> ptrsvec;
+  (ptrsvec.push_back(std::forward<UniqPtrs>(uniqptrs)), ...);
+  return ptrsvec;
+}
+
+cudf::table construct_table() {
   constexpr auto num_rows = 10;
 
   std::vector<size_t> zeros(num_rows, 0);
@@ -65,78 +71,25 @@ TEST_F(ParquetTest, ParquetWriter)
   std::vector<std::string> col10_data(num_rows, "rapids");
   cudf::test::strings_column_wrapper col10(col10_data.begin(), col10_data.end());
 
-  cudf::table_view tab({col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10});
+  auto colsptr = make_uniqueptrs_vector(col0.release(), col1.release(), col2.release(), col3.release(), col4.release(), col5.release(), col6.release(), col7.release(), col8.release(), col9.release(), col10.release());
+  return cudf::table(std::move(colsptr));
+}
 
-  cudf::io::table_input_metadata tab_metadata(tab);
-  tab_metadata.column_metadata[0].set_name("bools");
-  tab_metadata.column_metadata[1].set_name("int8s");
-  tab_metadata.column_metadata[2].set_name("int16s");
-  tab_metadata.column_metadata[3].set_name("int32s");
-  tab_metadata.column_metadata[4].set_name("floats");
-  tab_metadata.column_metadata[5].set_name("doubles");
-  tab_metadata.column_metadata[6].set_name("decimal_pos_scale");
-  tab_metadata.column_metadata[7].set_name("decimal_neg_scale");
-  tab_metadata.column_metadata[8].set_name("lists");
-  tab_metadata.column_metadata[9].set_name("structs");
-  tab_metadata.column_metadata[10].set_name("strings");
-
+TEST_F(ParquetTest, ParquetWriter)
+{
+  auto tab = construct_table();
   auto filepath = temp_env->get_temp_filepath("MultiColumn.parquet");
   cudf::io::parquet_writer_options out_opts =
-    cudf::io::parquet_writer_options::builder(cudf::io::sink_info{filepath}, tab)
-      .metadata(tab_metadata);
+    cudf::io::parquet_writer_options::builder(cudf::io::sink_info{filepath}, tab);
   cudf::io::write_parquet(out_opts, cudf::test::get_default_stream());
 }
 
 TEST_F(ParquetTest, ParquetReader)
 {
-  constexpr auto num_rows = 10;
-
-  std::vector<size_t> zeros(num_rows, 0);
-  std::vector<size_t> ones(num_rows, 1);
-  auto col6_data = cudf::detail::make_counting_transform_iterator(0, [&](auto i) {
-    return numeric::decimal128{ones[i], numeric::scale_type{12}};
-  });
-  auto col7_data = cudf::detail::make_counting_transform_iterator(0, [&](auto i) {
-    return numeric::decimal128{ones[i], numeric::scale_type{-12}};
-  });
-
-  cudf::test::fixed_width_column_wrapper<bool> col0(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<int8_t> col1(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<int16_t> col2(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<int32_t> col3(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<float> col4(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<double> col5(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<numeric::decimal128> col6(col6_data, col6_data + num_rows);
-  cudf::test::fixed_width_column_wrapper<numeric::decimal128> col7(col7_data, col7_data + num_rows);
-
-  cudf::test::lists_column_wrapper<int64_t> col8{
-    {1, 1}, {1, 1, 1}, {}, {1}, {1, 1, 1, 1}, {1, 1, 1, 1, 1}, {}, {1, -1}, {}, {-1, -1}};
-
-  cudf::test::fixed_width_column_wrapper<int32_t> child_col(ones.begin(), ones.end());
-  cudf::test::structs_column_wrapper col9{child_col};
-
-  std::vector<std::string> col10_data(num_rows, "rapids");
-  cudf::test::strings_column_wrapper col10(col10_data.begin(), col10_data.end());
-
-  cudf::table_view tab({col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10});
-
-  cudf::io::table_input_metadata tab_metadata(tab);
-  tab_metadata.column_metadata[0].set_name("bools");
-  tab_metadata.column_metadata[1].set_name("int8s");
-  tab_metadata.column_metadata[2].set_name("int16s");
-  tab_metadata.column_metadata[3].set_name("int32s");
-  tab_metadata.column_metadata[4].set_name("floats");
-  tab_metadata.column_metadata[5].set_name("doubles");
-  tab_metadata.column_metadata[6].set_name("decimal_pos_scale");
-  tab_metadata.column_metadata[7].set_name("decimal_neg_scale");
-  tab_metadata.column_metadata[8].set_name("lists");
-  tab_metadata.column_metadata[9].set_name("structs");
-  tab_metadata.column_metadata[10].set_name("strings");
-
+  auto tab = construct_table();
   auto filepath = temp_env->get_temp_filepath("MultiColumn.parquet");
   cudf::io::parquet_writer_options out_opts =
-    cudf::io::parquet_writer_options::builder(cudf::io::sink_info{filepath}, tab)
-      .metadata(tab_metadata);
+    cudf::io::parquet_writer_options::builder(cudf::io::sink_info{filepath}, tab);
   cudf::io::write_parquet(out_opts, cudf::test::get_default_stream());
 
   cudf::io::parquet_reader_options in_opts =
@@ -147,54 +100,10 @@ TEST_F(ParquetTest, ParquetReader)
 
 TEST_F(ParquetTest, ChunkedOperations)
 {
-  constexpr auto num_rows = 10;
-
-  std::vector<size_t> zeros(num_rows, 0);
-  std::vector<size_t> ones(num_rows, 1);
-  auto col6_data = cudf::detail::make_counting_transform_iterator(0, [&](auto i) {
-    return numeric::decimal128{ones[i], numeric::scale_type{12}};
-  });
-  auto col7_data = cudf::detail::make_counting_transform_iterator(0, [&](auto i) {
-    return numeric::decimal128{ones[i], numeric::scale_type{-12}};
-  });
-
-  cudf::test::fixed_width_column_wrapper<bool> col0(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<int8_t> col1(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<int16_t> col2(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<int32_t> col3(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<float> col4(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<double> col5(zeros.begin(), zeros.end());
-  cudf::test::fixed_width_column_wrapper<numeric::decimal128> col6(col6_data, col6_data + num_rows);
-  cudf::test::fixed_width_column_wrapper<numeric::decimal128> col7(col7_data, col7_data + num_rows);
-
-  cudf::test::lists_column_wrapper<int64_t> col8{
-    {1, 1}, {1, 1, 1}, {}, {1}, {1, 1, 1, 1}, {1, 1, 1, 1, 1}, {}, {1, -1}, {}, {-1, -1}};
-
-  cudf::test::fixed_width_column_wrapper<int32_t> child_col(ones.begin(), ones.end());
-  cudf::test::structs_column_wrapper col9{child_col};
-
-  std::vector<std::string> col10_data(num_rows, "rapids");
-  cudf::test::strings_column_wrapper col10(col10_data.begin(), col10_data.end());
-
-  cudf::table_view tab({col0, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10});
-
-  cudf::io::table_input_metadata tab_metadata(tab);
-  tab_metadata.column_metadata[0].set_name("bools");
-  tab_metadata.column_metadata[1].set_name("int8s");
-  tab_metadata.column_metadata[2].set_name("int16s");
-  tab_metadata.column_metadata[3].set_name("int32s");
-  tab_metadata.column_metadata[4].set_name("floats");
-  tab_metadata.column_metadata[5].set_name("doubles");
-  tab_metadata.column_metadata[6].set_name("decimal_pos_scale");
-  tab_metadata.column_metadata[7].set_name("decimal_neg_scale");
-  tab_metadata.column_metadata[8].set_name("lists");
-  tab_metadata.column_metadata[9].set_name("structs");
-  tab_metadata.column_metadata[10].set_name("strings");
-
+  auto tab = construct_table();
   auto filepath = temp_env->get_temp_filepath("MultiColumn.parquet");
   cudf::io::chunked_parquet_writer_options out_opts =
-    cudf::io::chunked_parquet_writer_options::builder(cudf::io::sink_info{filepath})
-      .metadata(tab_metadata);
+    cudf::io::chunked_parquet_writer_options::builder(cudf::io::sink_info{filepath});
   cudf::io::parquet_chunked_writer(out_opts, cudf::test::get_default_stream()).write(tab);
 
   auto reader = cudf::io::chunked_parquet_reader(
--- cpp/tests/utilities/identify_stream_usage.cpp
+++ cpp/tests/utilities/identify_stream_usage.cpp
@@ -105,13 +105,7 @@ bool stream_is_invalid(cudaStream_t stream)
 {
 #ifdef STREAM_MODE_TESTING
   // In this mode the _only_ valid stream is the one returned by cudf::test::get_default_stream.
-#if defined(CUDF_USE_PER_THREAD_DEFAULT_STREAM)
-  rmm::cuda_stream_view default_stream_value{rmm::cuda_stream_per_thread};
-#else
-  rmm::cuda_stream_view default_stream_value{};
-#endif
-  return (stream == default_stream_value.value() || stream == cudaStreamDefault ||
-          stream == cudaStreamLegacy || stream == cudaStreamPerThread);
+  return (stream != cudf::test::get_default_stream().value());
 #else
   // We explicitly list the possibilities rather than using
   // `cudf::get_default_stream().value()` because there is no guarantee that
