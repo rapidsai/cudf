bbe15b69a529d26dbf4d9a0cd7aeb58d427d31e6
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-01-08 15:21:00 -0800

    Ensure column.fillna signatures are consistent

--- python/cudf/cudf/core/column/datetime.py
+++ python/cudf/cudf/core/column/datetime.py
@@ -11,6 +11,7 @@ from typing import Any, Mapping, Optional, Sequence, cast
 import numpy as np
 import pandas as pd
 import pyarrow as pa
+from typing_extensions import Self
 
 import cudf
 from cudf import _lib as libcudf
@@ -598,8 +599,7 @@ class DatetimeColumn(column.ColumnBase):
         self,
         fill_value: Any = None,
         method: Optional[str] = None,
-        dtype: Optional[Dtype] = None,
-    ) -> DatetimeColumn:
+    ) -> Self:
         if fill_value is not None:
             if cudf.utils.utils._isnat(fill_value):
                 return self.copy(deep=True)
