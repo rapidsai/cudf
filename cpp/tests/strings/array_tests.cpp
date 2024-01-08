40553fec8335a0828c30cfe5219b986cfbe21879
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2024-01-08 16:09:24 +0530

    StringsColumnTest.GatherTooBig test column_view fix children

--- cpp/tests/strings/array_tests.cpp
+++ cpp/tests/strings/array_tests.cpp
@@ -154,9 +154,14 @@ TEST_F(StringsColumnTest, GatherTooBig)
   std::vector<int8_t> h_chars(3000000);
   cudf::test::fixed_width_column_wrapper<int8_t> chars(h_chars.begin(), h_chars.end());
   cudf::test::fixed_width_column_wrapper<cudf::size_type> offsets({0, 3000000});
-  auto input = cudf::column_view(
-    cudf::data_type{cudf::type_id::STRING}, 1, nullptr, nullptr, 0, 0, {offsets, chars});
-  auto map = thrust::constant_iterator<int8_t>(0);
+  auto input = cudf::column_view(cudf::data_type{cudf::type_id::STRING},
+                                 1,
+                                 cudf::column_view(chars).begin<int8_t>(),
+                                 nullptr,
+                                 0,
+                                 0,
+                                 {offsets});
+  auto map   = thrust::constant_iterator<int8_t>(0);
   cudf::test::fixed_width_column_wrapper<int8_t> gather_map(map, map + 1000);
   EXPECT_THROW(cudf::gather(cudf::table_view{{input}}, gather_map), std::overflow_error);
 }
