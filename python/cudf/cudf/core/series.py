4c7e3a52de60759a36c78efa202429dc68e1793f
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-01-11 17:41:34 -0800

    Use _from_data

--- python/cudf/cudf/core/series.py
+++ python/cudf/cudf/core/series.py
@@ -1702,7 +1702,7 @@ class Series(SingleColumnFrame, IndexedFrame, Serializable):
         if len(objs):
             col = col._with_type_metadata(objs[0].dtype)
 
-        return cls(data=col, index=index, name=name, nan_as_null=False)
+        return cls._from_data({name: col}, index=index)
 
     @property  # type: ignore
     @_cudf_nvtx_annotate
