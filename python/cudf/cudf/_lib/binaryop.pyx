65a2810e5b0878fce923e5b67fe4fd28bdfbb341
Vyas Ramasubramani <vyasr@nvidia.com>
2024-01-17 20:52:21 +0000

    Remove now unused jit binaryop code path

--- python/cudf/cudf/_lib/binaryop.pyx
+++ python/cudf/cudf/_lib/binaryop.pyx
@@ -1,29 +1,14 @@
 # Copyright (c) 2020-2024, NVIDIA CORPORATION.
 
-from libcpp.memory cimport unique_ptr
-from libcpp.string cimport string
-from libcpp.utility cimport move
-
 from cudf._lib.column cimport Column
-
-from cudf._lib.scalar import as_device_scalar
-
 from cudf._lib.scalar cimport DeviceScalar
+from cudf._lib.types cimport dtype_to_pylibcudf_type
 
 from cudf._lib import pylibcudf
-from cudf._lib.types import SUPPORTED_NUMPY_TO_LIBCUDF_TYPES
-
-from cudf._lib.cpp.column.column cimport column
-from cudf._lib.cpp.column.column_view cimport column_view
-from cudf._lib.cpp.types cimport data_type, type_id
-from cudf._lib.types cimport dtype_to_pylibcudf_type, underlying_type_t_type_id
-
+from cudf._lib.scalar import as_device_scalar
 from cudf.core.buffer import acquire_spill_lock
 
-cimport cudf._lib.cpp.binaryop as cpp_binaryop
-
-import cudf
-
+# Map pandas operation names to pylibcudf operation names.
 _op_map = {
     "TRUEDIV": "TRUE_DIV",
     "FLOORDIV": "FLOOR_DIV",
@@ -73,40 +58,3 @@ def binaryop(lhs, rhs, op, dtype):
             dtype_to_pylibcudf_type(dtype),
         )
     )
-
-
-@acquire_spill_lock()
-def binaryop_udf(Column lhs, Column rhs, udf_ptx, dtype):
-    """
-    Apply a user-defined binary operator (a UDF) defined in `udf_ptx` on
-    the two input columns `lhs` and `rhs`. The output type of the UDF
-    has to be specified in `dtype`, a numpy data type.
-    Currently ONLY int32, int64, float32 and float64 are supported.
-    """
-    cdef column_view c_lhs = lhs.view()
-    cdef column_view c_rhs = rhs.view()
-
-    cdef type_id tid = (
-        <type_id> (
-            <underlying_type_t_type_id> (
-                SUPPORTED_NUMPY_TO_LIBCUDF_TYPES[cudf.dtype(dtype)]
-            )
-        )
-    )
-    cdef data_type c_dtype = data_type(tid)
-
-    cdef string cpp_str = udf_ptx.encode("UTF-8")
-
-    cdef unique_ptr[column] c_result
-
-    with nogil:
-        c_result = move(
-            cpp_binaryop.binary_operation(
-                c_lhs,
-                c_rhs,
-                cpp_str,
-                c_dtype
-            )
-        )
-
-    return Column.from_unique_ptr(move(c_result))
