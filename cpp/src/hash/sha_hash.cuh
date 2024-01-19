335e99139afb949f7295438efe2918e530e818fb
Bradley Dice <bdice@bradleydice.com>
2024-01-19 12:49:10 -0600

    Fix calls to deprecated strings factory API.

--- cpp/src/hash/sha_hash.cuh
+++ cpp/src/hash/sha_hash.cuh
@@ -506,9 +506,8 @@ std::unique_ptr<column> sha_hash(table_view const& input,
   auto [offsets_column, bytes] =
     cudf::detail::make_offsets_child_column(begin, begin + input.num_rows(), stream, mr);
 
-  auto chars_column = strings::detail::create_chars_child_column(bytes, stream, mr);
-  auto chars_view   = chars_column->mutable_view();
-  auto d_chars      = chars_view.data<char>();
+  auto chars   = rmm::device_uvector<char>(bytes, stream, mr);
+  auto d_chars = chars.data();
 
   auto const device_input = table_device_view::create(input, stream);
 
@@ -527,8 +526,7 @@ std::unique_ptr<column> sha_hash(table_view const& input,
                      hasher.finalize();
                    });
 
-  return make_strings_column(
-    input.num_rows(), std::move(offsets_column), std::move(chars_column), 0, {});
+  return make_strings_column(input.num_rows(), std::move(offsets_column), chars.release(), 0, {});
 }
 
 }  // namespace detail
