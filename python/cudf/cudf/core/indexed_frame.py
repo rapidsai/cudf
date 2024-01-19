51ecef3b8616eb851e23814f21db7b705cf75504
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-01-19 09:40:51 -1000

    Remove **kwargs from astype (#14765)
    The aligns with pandas's behavior of `astype` which doesn't accept kwargs. AFAICT, `**kwargs` was never really respected in the first place
    
    Authors:
      - Matthew Roeschke (https://github.com/mroeschke)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Michael Wang (https://github.com/isVoid)
    
    URL: https://github.com/rapidsai/cudf/pull/14765

--- python/cudf/cudf/core/indexed_frame.py
+++ python/cudf/cudf/core/indexed_frame.py
@@ -14,6 +14,7 @@ from typing import (
     Callable,
     Dict,
     List,
+    Literal,
     MutableMapping,
     Optional,
     Tuple,
@@ -3736,7 +3737,12 @@ class IndexedFrame(Frame):
 
         return cudf.concat(to_concat, ignore_index=ignore_index, sort=sort)
 
-    def astype(self, dtype, copy=False, errors="raise", **kwargs):
+    def astype(
+        self,
+        dtype,
+        copy: bool = False,
+        errors: Literal["raise", "ignore"] = "raise",
+    ):
         """Cast the object to the given dtype.
 
         Parameters
@@ -3757,7 +3763,6 @@ class IndexedFrame(Frame):
             -   ``raise`` : allow exceptions to be raised
             -   ``ignore`` : suppress exceptions. On error return original
                 object.
-        **kwargs : extra arguments to pass on to the constructor
 
         Returns
         -------
@@ -3848,7 +3853,7 @@ class IndexedFrame(Frame):
             raise ValueError("invalid error value specified")
 
         try:
-            data = super().astype(dtype, copy, **kwargs)
+            data = super().astype(dtype, copy)
         except Exception as e:
             if errors == "raise":
                 raise e
