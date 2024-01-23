c9ec9c247f90010b27f8f7aa057406fd46581b8e
Ray Douglass <3107146+raydouglass@users.noreply.github.com>
2024-01-22 21:43:03 -0500

    Fix 24.04 versions (#14825)
    Fix `update-version.sh` to include pip `cu` suffixed dependencies
    
    Authors:
      - Ray Douglass (https://github.com/raydouglass)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - AJ Schmidt (https://github.com/ajschmidt8)
    
    URL: https://github.com/rapidsai/cudf/pull/14825

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2020-2023, NVIDIA CORPORATION.
+# Copyright (c) 2020-2024, NVIDIA CORPORATION.
 ########################
 # cuDF Version Updater #
 ########################
@@ -86,7 +86,7 @@ DEPENDENCIES=(
 )
 for DEP in "${DEPENDENCIES[@]}"; do
   for FILE in dependencies.yaml conda/environments/*.yaml; do
-    sed_runner "/-.* ${DEP}==/ s/==.*/==${NEXT_SHORT_TAG_PEP440}.*/g" ${FILE}
+    sed_runner "/-.* ${DEP}\(-cu[[:digit:]]\{2\}\)\{0,1\}==/ s/==.*/==${NEXT_SHORT_TAG_PEP440}.*/g" "${FILE}"
   done
   for FILE in python/*/pyproject.toml; do
     sed_runner "/\"${DEP}==/ s/==.*\"/==${NEXT_SHORT_TAG_PEP440}.*\"/g" ${FILE}
