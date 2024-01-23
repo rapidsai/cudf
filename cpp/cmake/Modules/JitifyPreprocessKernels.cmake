f65c8dd60ce056cdb400ec9ded0cb494bf4c782c
Vyas Ramasubramani <vyasr@nvidia.com>
2024-01-23 00:30:39 +0000

    Remove conditional

--- cpp/cmake/Modules/JitifyPreprocessKernels.cmake
+++ cpp/cmake/Modules/JitifyPreprocessKernels.cmake
@@ -17,9 +17,7 @@ add_executable(jitify_preprocess "${JITIFY_INCLUDE_DIR}/jitify2_preprocess.cpp")
 
 target_compile_definitions(jitify_preprocess PRIVATE "_FILE_OFFSET_BITS=64")
 rapids_cuda_set_runtime(jitify_preprocess USE_STATIC ${CUDA_STATIC_RUNTIME})
-if(CMAKE_DL_LIBS)
-  target_link_libraries(jitify_preprocess PUBLIC ${CMAKE_DL_LIBS})
-endif()
+target_link_libraries(jitify_preprocess PUBLIC ${CMAKE_DL_LIBS})
 
 # Take a list of files to JIT-compile and run them through jitify_preprocess.
 function(jit_preprocess_files)
