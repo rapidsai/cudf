7b984b22774f18c01f628aef525f0eb37477f63f
rjzamora <rzamora217@gmail.com>
2024-01-23 12:56:25 -0800

    Avoid unnecessary caching

--- python/dask_cudf/dask_cudf/expr/_expr.py
+++ python/dask_cudf/dask_cudf/expr/_expr.py
@@ -1,38 +1,34 @@
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
-import functools
-
 from dask_expr._cumulative import CumulativeBlockwise, TakeLast
 
 ##
-## Custom expression classes
+## Custom expression patching
 ##
 
 
-class CumulativeBlockwiseCudf(CumulativeBlockwise):
-    @functools.cached_property
+class PatchCumulativeBlockwise(CumulativeBlockwise):
+    @property
     def _args(self) -> list:
         return self.operands[:1]
 
-    @functools.cached_property
+    @property
     def _kwargs(self) -> dict:
         # Must pass axis and skipna as kwargs in cudf
         return {"axis": self.axis, "skipna": self.skipna}
 
 
-CumulativeBlockwise._args = CumulativeBlockwiseCudf._args
-CumulativeBlockwise._kwargs = CumulativeBlockwiseCudf._kwargs
+CumulativeBlockwise._args = PatchCumulativeBlockwise._args
+CumulativeBlockwise._kwargs = PatchCumulativeBlockwise._kwargs
 
 
-class TakeLastCudf(TakeLast):
-    @staticmethod
-    def operation(a, skipna=True):
-        if not len(a):
-            return a
-        if skipna:
-            a = a.bfill()
-        # Cannot use `squeeze` with cudf
-        return a.tail(n=1).iloc[0]
+def _takelast(a, skipna=True):
+    if not len(a):
+        return a
+    if skipna:
+        a = a.bfill()
+    # Cannot use `squeeze` with cudf
+    return a.tail(n=1).iloc[0]
 
 
-TakeLast.operation = staticmethod(TakeLastCudf.operation)
+TakeLast.operation = staticmethod(_takelast)
