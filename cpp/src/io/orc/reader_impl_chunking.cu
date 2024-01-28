5c2c5ea22b5859a18fbc076b1df9dc5c9885a3d0
Nghia Truong <nghiat@nvidia.com>
2024-01-27 23:19:27 -0800

    Read without async
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- cpp/src/io/orc/reader_impl_chunking.cu
+++ cpp/src/io/orc/reader_impl_chunking.cu
@@ -233,6 +233,15 @@ void reader::impl::query_stripe_compression_info()
                   read.length));
 
     } else {
+      auto const buffer =
+        _metadata.per_file_metadata[read.source_idx].source->host_read(read.offset, read.length);
+      CUDF_EXPECTS(buffer->size() == read.length, "Unexpected discrepancy in bytes read.");
+      CUDF_CUDA_TRY(cudaMemcpyAsync(
+        dst_base + read.dst_pos, buffer->data(), read.length, cudaMemcpyDefault, _stream.value()));
+      _stream.synchronize();
+
+#if 0
+     // This in theory should be faster, but in practice it's slower. Why?
       read_tasks.push_back(
         std::pair(std::async(std::launch::async,
                              [&, read = read, dst_base = dst_base] {
@@ -250,6 +259,7 @@ void reader::impl::query_stripe_compression_info()
                                return read.length;
                              }),
                   read.length));
+#endif
     }
   }
   for (auto& task : read_tasks) {
