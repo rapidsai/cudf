83c3d2f87a5c69cd94e91f3c6dbf8ced5f153823
Yunsong Wang <yunsongw@nvidia.com>
2024-02-07 17:54:30 -0800

    Update tests

--- cpp/tests/join/unique_join_tests.cpp
+++ cpp/tests/join/unique_join_tests.cpp
@@ -13,10 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #include <cudf/column/column.hpp>
 #include <cudf/filling.hpp>
 #include <cudf/join.hpp>
+#include <cudf/sorting.hpp>
 #include <cudf/table/table.hpp>
 #include <cudf/table/table_view.hpp>
 #include <cudf/types.hpp>
@@ -44,16 +44,19 @@ TEST_F(UniqueJoinTest, IntegerInnerJoin)
 {
   auto constexpr num = 2024;
 
-  cudf::numeric_scalar<int32_t> init(0);
-  cudf::numeric_scalar<int32_t> step(1);
+  auto const init = cudf::numeric_scalar<int32_t>{0};
 
-  auto col = cudf::sequence(num, init, step);
+  auto build = cudf::sequence(num, init, cudf::numeric_scalar<int32_t>{1});
+  auto probe = cudf::sequence(num, init, cudf::numeric_scalar<int32_t>{2});
 
   auto unique_join = cudf::unique_hash_join<cudf::has_nested::NO>{
-    cudf::table_view{{col->view()}}, cudf::table_view{{col->view()}}, cudf::nullable_join::NO};
-  auto const result = unique_join.inner_join();
+    cudf::table_view{{build->view()}}, cudf::table_view{{probe->view()}}, cudf::nullable_join::NO};
 
-  EXPECT_EQ(result.first->size(), num);
+  auto result = unique_join.inner_join();
+
+  auto constexpr gold_size = num / 2;
+  EXPECT_EQ(result.first->size(), gold_size);
+  EXPECT_EQ(result.second->size(), gold_size);
 }
 
 TEST_F(UniqueJoinTest, InnerJoinNestedNoNulls)
@@ -77,22 +80,39 @@ TEST_F(UniqueJoinTest, InnerJoinNestedNoNulls)
   Table build(std::move(cols0));
   Table probe(std::move(cols1));
 
-  auto unique_join  = cudf::unique_hash_join<cudf::has_nested::YES>{build.view(), probe.view()};
-  auto const result = unique_join.inner_join();
+  auto unique_join = cudf::unique_hash_join<cudf::has_nested::YES>{build.view(), probe.view()};
+  auto [left_join_indices, right_join_indices] = unique_join.inner_join();
 
   auto constexpr gold_size = 2;
-  EXPECT_EQ(result.first->size(), gold_size);
 
-  /*
-  auto result_sort_order = cudf::sorted_order(result->view());
-  auto sorted_result     = cudf::gather(result->view(), *result_sort_order);
+  EXPECT_EQ(left_join_indices->size(), gold_size);
+  EXPECT_EQ(right_join_indices->size(), gold_size);
+
+  auto left_indices_span  = cudf::device_span<cudf::size_type const>{*left_join_indices};
+  auto right_indices_span = cudf::device_span<cudf::size_type const>{*right_join_indices};
+
+  auto left_indices_col  = cudf::column_view{left_indices_span};
+  auto right_indices_col = cudf::column_view{right_indices_span};
+
+  auto constexpr oob_policy = cudf::out_of_bounds_policy::DONT_CHECK;
+  auto left_result          = cudf::gather(build.view(), left_indices_col, oob_policy);
+  auto right_result         = cudf::gather(probe.view(), right_indices_col, oob_policy);
+
+  auto joined_cols = left_result->release();
+  auto right_cols  = right_result->release();
+  joined_cols.insert(joined_cols.end(),
+                     std::make_move_iterator(right_cols.begin()),
+                     std::make_move_iterator(right_cols.end()));
+  auto joined_table        = std::make_unique<cudf::table>(std::move(joined_cols));
+  auto result_sort_order   = cudf::sorted_order(joined_table->view());
+  auto sorted_joined_table = cudf::gather(joined_table->view(), *result_sort_order);
 
   column_wrapper<int32_t> col_gold_0{{1, 2}};
   strcol_wrapper col_gold_1({"s0", "s0"});
   column_wrapper<int32_t> col_gold_2{{9, 9}};
   column_wrapper<int32_t> col_gold_3{{1, 2}};
   strcol_wrapper col_gold_4({"s0", "s0"});
-  column_wrapper<int32_t> col_gold_5{{1, 0, 0}};
+  column_wrapper<int32_t> col_gold_5{{9, 9}};
   CVector cols_gold;
   cols_gold.push_back(col_gold_0.release());
   cols_gold.push_back(col_gold_1.release());
@@ -104,6 +124,5 @@ TEST_F(UniqueJoinTest, InnerJoinNestedNoNulls)
 
   auto gold_sort_order = cudf::sorted_order(gold.view());
   auto sorted_gold     = cudf::gather(gold.view(), *gold_sort_order);
-  CUDF_TEST_EXPECT_TABLES_EQUIVALENT(*sorted_gold, *sorted_result);
-  */
+  CUDF_TEST_EXPECT_TABLES_EQUIVALENT(*sorted_gold, *sorted_joined_table);
 }
