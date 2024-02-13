579d34444299b61540f26f651b5c9099cb2a569d
Vyas Ramasubramani <vyasr@nvidia.com>
2024-02-13 00:50:42 +0000

    Use pylibcudf for concat

--- python/cudf/cudf/_lib/concat.pyx
+++ python/cudf/cudf/_lib/concat.pyx
@@ -1,47 +1,34 @@
 # Copyright (c) 2020-2024, NVIDIA CORPORATION.
 
 from libcpp cimport bool
-from libcpp.memory cimport unique_ptr
-from libcpp.utility cimport move
-from libcpp.vector cimport vector
 
 from cudf._lib.column cimport Column
-from cudf._lib.cpp.column.column cimport column, column_view
-from cudf._lib.cpp.concatenate cimport (
-    concatenate_columns as libcudf_concatenate_columns,
-    concatenate_tables as libcudf_concatenate_tables,
-)
-from cudf._lib.cpp.table.table cimport table, table_view
-from cudf._lib.utils cimport (
-    data_from_unique_ptr,
-    make_column_views,
-    table_view_from_table,
-)
+from cudf._lib.utils cimport data_from_pylibcudf_table
 
+from cudf._lib import pylibcudf
 from cudf.core.buffer import acquire_spill_lock
 
 
 @acquire_spill_lock()
 def concat_columns(object columns):
-    cdef unique_ptr[column] c_result
-    cdef vector[column_view] c_views = make_column_views(columns)
-    with nogil:
-        c_result = move(libcudf_concatenate_columns(c_views))
-    return Column.from_unique_ptr(move(c_result))
+    return Column.from_pylibcudf(
+        pylibcudf.concatenate.concatenate(
+            [col.to_pylibcudf(mode="read") for col in columns]
+        )
+    )
 
 
 @acquire_spill_lock()
 def concat_tables(object tables, bool ignore_index=False):
-    cdef unique_ptr[table] c_result
-    cdef vector[table_view] c_views
-    c_views.reserve(len(tables))
-    for tbl in tables:
-        c_views.push_back(table_view_from_table(tbl, ignore_index))
-    with nogil:
-        c_result = move(libcudf_concatenate_tables(c_views))
+    plc_tables = []
+    for table in tables:
+        cols = table._data.columns
+        if not ignore_index:
+            cols = table._index._data.columns + cols
+        plc_tables.append(pylibcudf.Table([c.to_pylibcudf(mode="read") for c in cols]))
 
-    return data_from_unique_ptr(
-        move(c_result),
+    return data_from_pylibcudf_table(
+        pylibcudf.concatenate.concatenate(plc_tables),
         column_names=tables[0]._column_names,
         index_names=None if ignore_index else tables[0]._index_names
     )
