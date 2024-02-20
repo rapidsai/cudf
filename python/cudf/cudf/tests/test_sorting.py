ada376e291c224f394974d00454df85a022838a5
GALI PREM SAGAR <sagarprem75@gmail.com>
2024-02-20 13:41:06 -0600

    Update python/cudf/cudf/tests/test_sorting.py

--- python/cudf/cudf/tests/test_sorting.py
+++ python/cudf/cudf/tests/test_sorting.py
@@ -8,7 +8,7 @@ import pandas as pd
 import pytest
 
 from cudf import DataFrame, Series
-from cudf.core._compat import PANDAS_GE_200
+from cudf.core._compat import PANDAS_GE_220
 from cudf.core.column import NumericalColumn
 from cudf.testing._utils import (
     DATETIME_TYPES,
