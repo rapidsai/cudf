8e68b37684ee8780f39b43609b3192a982aa9a5f
David Wendt <45795991+davidwendt@users.noreply.github.com>
2024-02-21 09:20:09 -0500

    Fix deprecation warnings for deprecated hash() calls (#15095)
    Merged #15074 too soon and missed fixing these now deprecated call warnings.
    
    Authors:
      - David Wendt (https://github.com/davidwendt)
    
    Approvers:
      - Nghia Truong (https://github.com/ttnghia)
      - Mark Harris (https://github.com/harrism)
    
    URL: https://github.com/rapidsai/cudf/pull/15095

--- cpp/tests/streams/hash_test.cpp
+++ cpp/tests/streams/hash_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, NVIDIA CORPORATION.
+ * Copyright (c) 2023-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,6 +49,6 @@ TEST_F(HashTest, MultiValue)
 
   auto const input1 = cudf::table_view({strings_col, ints_col, bools_col1, secs_col});
 
-  auto const output1 = cudf::hash(
-    input1, cudf::hash_id::HASH_MURMUR3, cudf::DEFAULT_HASH_SEED, cudf::test::get_default_stream());
+  auto const output1 = cudf::hashing::murmurhash3_x86_32(
+    input1, cudf::DEFAULT_HASH_SEED, cudf::test::get_default_stream());
 }
