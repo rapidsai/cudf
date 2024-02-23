505a6abc2e0e70a7cac54800481079e64e592f1a
Yunsong Wang <yunsongw@nvidia.com>
2024-02-23 09:12:22 -0800

    Update cpp/tests/join/distinct_join_tests.cpp
    Co-authored-by: Nghia Truong <7416935+ttnghia@users.noreply.github.com>

--- cpp/tests/join/distinct_join_tests.cpp
+++ cpp/tests/join/distinct_join_tests.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_utilities.hpp>
 #include <cudf_test/column_wrapper.hpp>
