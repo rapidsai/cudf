3efb4f8269d9834e1bbe695480d661fcbd39ba9b
galipremsagar <sagarprem75@gmail.com>
2024-02-23 17:09:55 +0000

    fix script

--- python/cudf/cudf/pandas/scripts/run-pandas-tests.sh
+++ python/cudf/cudf/pandas/scripts/run-pandas-tests.sh
@@ -184,7 +184,7 @@ and not test_numpy_ufuncs_basic[nullable_float-rad2deg]"
 
 PANDAS_CI="1" python -m pytest -p cudf.pandas \
     -v -m "not single_cpu and not db" \
-    -k "not test_overwrite_warns and not test_complex_series_frame_alignment and not test_to_parquet_gcs_new_file and not test_qcut_nat not test_add and $TEST_NUMPY_UFUNCS_BASIC_FLAKY" \
+    -k "not test_overwrite_warns and not test_complex_series_frame_alignment and not test_to_parquet_gcs_new_file and not test_qcut_nat and not test_add and $TEST_NUMPY_UFUNCS_BASIC_FLAKY" \
     --durations=50 \
     --import-mode=importlib \
     -o xfail_strict=True \
