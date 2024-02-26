5a6b1c29bed9d33473efe1fe6cf476d97e28f3d2
rjzamora <rzamora217@gmail.com>
2024-02-26 06:47:44 -0800

    add conda testing for dask-expr

--- ci/test_wheel_dask_cudf.sh
+++ ci/test_wheel_dask_cudf.sh
@@ -40,7 +40,7 @@ python -m pytest \
 popd
 
 # Run tests in dask_cudf/tests and dask_cudf/io/tests with dask-expr
-rapids-logger "pytest dask_cudf + dask-expr"
+rapids-logger "pytest dask_cudf + dask_expr"
 pushd python/dask_cudf/dask_cudf
 DASK_DATAFRAME__QUERY_PLANNING=True python -m pytest \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-dask-cudf-expr.xml" \
