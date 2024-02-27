8f541b8efd9a779b19f29593e93e9af6a543c7f0
David Wendt <dwendt@nvidia.com>
2024-02-27 13:45:39 -0500

    Remove unneeded script parameters in test_cpp_memcheck.sh

--- ci/test_cpp_memcheck.sh
+++ ci/test_cpp_memcheck.sh
@@ -9,7 +9,6 @@ source ./ci/test_cpp_common.sh
 rapids-logger "Memcheck gtests with rmm_mode=cuda"
 
 ./ci/run_cudf_memcheck_ctests.sh \
-    --gtest_output=xml:"${RAPIDS_TESTS_DIR}${test_name}.xml" \
  && EXITCODE=$? || EXITCODE=$?;
 
 rapids-logger "Test script exiting with value: $EXITCODE"
