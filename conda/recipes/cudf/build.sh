9fdbfc7408b6d0379ce5f87a01aff4a657164a4e
jakirkham <jakirkham@gmail.com>
2024-02-27 19:59:02 -0800

    Increase `pip` verbosity in cuDF Conda build

--- conda/recipes/cudf/build.sh
+++ conda/recipes/cudf/build.sh
@@ -1,4 +1,8 @@
-# Copyright (c) 2018-2022, NVIDIA CORPORATION.
+# Copyright (c) 2018-2024, NVIDIA CORPORATION.
+
+export PIP_VERBOSE=2
 
 # This assumes the script is executed from the root of the repo directory
 ./build.sh cudf
+
+unset PIP_VERBOSE
