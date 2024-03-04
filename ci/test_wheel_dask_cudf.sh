eb5b6c5896194797e64e7258ef2081555d319f84
Bradley Dice <bdice@bradleydice.com>
2024-03-04 12:36:32 -0600

    Mark dask-cudf as a pure wheel.

--- ci/test_wheel_dask_cudf.sh
+++ ci/test_wheel_dask_cudf.sh
@@ -9,7 +9,7 @@ unzip wheel-python-pure.zip
 cp gha-tools-wheel-python-pure/tools/* /usr/local/bin
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="dask_cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
+RAPIDS_PY_WHEEL_NAME="dask_cudf_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-s3 ./dist
 
 # Download the cudf built in the previous step
 # Set the manylinux version used for downloading the wheels so that we test the
