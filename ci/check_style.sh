a4966109b4d3464b7bebdb455c8b65eca6efb582
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-06 23:47:51 +0000

    Skip checks altogether

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -8,18 +8,18 @@ rapids-logger "Create checks conda environment"
 
 ENV_YAML_DIR="$(mktemp -d)"
 
-rapids-dependency-file-generator \
-  --output conda \
-  --file_key checks \
-  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee "${ENV_YAML_DIR}/env.yaml"
-
-rapids-mamba-retry env create --force -f "${ENV_YAML_DIR}/env.yaml" -n checks
-conda activate checks
-
-FORMAT_FILE_URL=https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-24.04/cmake-format-rapids-cmake.json
-export RAPIDS_CMAKE_FORMAT_FILE=/tmp/rapids_cmake_ci/cmake-formats-rapids-cmake.json
-mkdir -p $(dirname ${RAPIDS_CMAKE_FORMAT_FILE})
-wget -O ${RAPIDS_CMAKE_FORMAT_FILE} ${FORMAT_FILE_URL}
-
-# Run pre-commit checks
-pre-commit run --all-files --show-diff-on-failure
+#rapids-dependency-file-generator \
+#  --output conda \
+#  --file_key checks \
+#  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee "${ENV_YAML_DIR}/env.yaml"
+#
+#rapids-mamba-retry env create --force -f "${ENV_YAML_DIR}/env.yaml" -n checks
+#conda activate checks
+#
+#FORMAT_FILE_URL=https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-24.04/cmake-format-rapids-cmake.json
+#export RAPIDS_CMAKE_FORMAT_FILE=/tmp/rapids_cmake_ci/cmake-formats-rapids-cmake.json
+#mkdir -p $(dirname ${RAPIDS_CMAKE_FORMAT_FILE})
+#wget -O ${RAPIDS_CMAKE_FORMAT_FILE} ${FORMAT_FILE_URL}
+#
+## Run pre-commit checks
+#pre-commit run --all-files --show-diff-on-failure
