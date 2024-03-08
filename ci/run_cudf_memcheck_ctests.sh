7779850fddad0d2132a6c6aa57ad8c64c9188f1a
David Wendt <dwendt@nvidia.com>
2024-03-08 16:43:19 -0500

    Workaround compute-sanitizer memcheck bug

--- ci/run_cudf_memcheck_ctests.sh
+++ ci/run_cudf_memcheck_ctests.sh
@@ -10,6 +10,8 @@ trap "EXITCODE=1" ERR
 cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/gtests/libcudf/";
 
 export GTEST_CUDF_RMM_MODE=cuda
+# compute-sanitizer bug 4553815
+export LIBCUDF_MEMCHECK_ENABLED=1
 for gt in ./*_TEST ; do
   test_name=$(basename ${gt})
   # Run gtests with compute-sanitizer
@@ -17,8 +19,9 @@ for gt in ./*_TEST ; do
     continue
   fi
   echo "Running compute-sanitizer on $test_name"
-  compute-sanitizer --tool memcheck ${gt} "$@"
+  compute-sanitizer --tool memcheck ${extra_param} ${gt} "$@"
 done
 unset GTEST_CUDF_RMM_MODE
+unset LIBCUDF_MEMCHECK_ENABLED
 
 exit ${EXITCODE}
