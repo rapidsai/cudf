c43d7b6a58c7540079fb6d3cc27ea86ed78bf406
GALI PREM SAGAR <sagarprem75@gmail.com>
2024-03-12 10:35:33 -0500

    Update run.sh

--- ci/cudf_pandas_scripts/pandas-tests/run.sh
+++ ci/cudf_pandas_scripts/pandas-tests/run.sh
@@ -33,7 +33,7 @@ mkdir -p "${RAPIDS_TESTS_DIR}"
 
 bash python/cudf/cudf/pandas/scripts/run-pandas-tests.sh \
   -n 10 \
-  --tb=line \
+  --tb=no \
   -m "not slow" \
   --max-worker-restart=3 \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cudf-pandas.xml" \
