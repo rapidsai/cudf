344fffa5de870f7da836b06bf5bc990a48c1182a
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-14 01:30:35 +0000

    Add test of gather and fix a number of minor issues

--- python/cudf/cudf/tests/pylibcudf/common/utils.py
+++ python/cudf/cudf/tests/pylibcudf/common/utils.py
@@ -5,21 +5,33 @@ import pyarrow as pa
 from cudf._lib import pylibcudf as plc
 
 
+# TODO: The two functions below should probably be a function in interop. We should move
+# the class method on Table into a function in interop, and then use that to implement
+# this function in interop as well.
+def column_from_arrow(pa_array):
+    """Create a pylibcudf column from a PyArrow array."""
+    pa_table = pa.table([pa_array], [""])
+    table = plc.Table.from_arrow(pa_table)
+    return table.columns()[0]
+
+
+def column_to_arrow(plc_column):
+    """Create a PyArrow array from a pylibcudf column."""
+    return plc.Table([plc_column]).to_arrow([plc.interop.ColumnMetadata("")])[
+        0
+    ]
+
+
 def assert_array_eq(plc_column, pa_array):
     """Verify that the pylibcudf array and PyArrow array are equal."""
-    plc_as_pa = plc.Table([plc_column]).to_arrow(
-        [plc.interop.ColumnMetadata("tmp")]
-    )[0]
-    pa_equal = pa.compute.equal(plc_as_pa, pa_array)
+    pa_equal = pa.compute.equal(column_to_arrow(plc_column), pa_array)
     assert pa.compute.all(pa_equal).as_py()
 
 
-def column_from_arrow(pa_array):
-    """Create a pylibcudf column from a PyArrow array."""
-    # TODO: This should probably be a function in interop. We should move the class
-    # method on Table into a function in interop, and then use that to implement this
-    # function in inteorp as well.
+def assert_table_eq(plc_table, pa_table):
+    """Verify that the pylibcudf array and PyArrow array are equal."""
+    plc_shape = (plc_table.num_rows(), plc_table.num_columns())
+    assert plc_shape == pa_table.shape
 
-    pa_tbl = pa.table([pa_array], ["tmp"])
-    tbl = plc.Table.from_arrow(pa_tbl)
-    return tbl.columns()[0]
+    for plc_col, pa_col in zip(plc_table.columns(), pa_table.columns):
+        assert_array_eq(plc_col, pa_col)
--- python/cudf/cudf/tests/pylibcudf/test_copying.py
+++ python/cudf/cudf/tests/pylibcudf/test_copying.py
@@ -2,7 +2,12 @@
 
 import pyarrow as pa
 import pytest
-from utils import assert_array_eq, column_from_arrow
+from utils import (
+    assert_array_eq,
+    assert_table_eq,
+    column_from_arrow,
+    column_to_arrow,
+)
 
 from cudf._lib import pylibcudf as plc
 
@@ -20,6 +25,24 @@ def target_column():
 @pytest.fixture
 def mutable_target_column(target_column):
     return target_column.copy()
+
+
+@pytest.fixture(scope="module")
+def source_table(target_column):
+    return plc.Table([target_column] * 3)
+
+
+def test_gather(source_table, input_column):
+    result = plc.copying.gather(
+        source_table,
+        input_column,
+        plc.copying.OutOfBoundsPolicy.DONT_CHECK,
+    )
+    pa_table = source_table.to_arrow(
+        [plc.interop.ColumnMetadata("")] * source_table.num_columns()
+    )
+    expected = pa_table.take(column_to_arrow(input_column))
+    assert_table_eq(result, expected)
 
 
 def test_copy_range(input_column, target_column):
