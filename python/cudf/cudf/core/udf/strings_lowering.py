90a570bbfc34aec9647393f6f95fe4b6759f4858
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-03-14 15:22:33 -0700

    Probably errant comma

--- python/cudf/cudf/core/udf/strings_lowering.py
+++ python/cudf/cudf/core/udf/strings_lowering.py
@@ -249,7 +249,7 @@ def replace_impl(context, builder, sig, args):
     replacement_ptr = builder.alloca(args[2].type)
 
     builder.store(args[0], src_ptr)
-    (builder.store(args[1], to_replace_ptr),)
+    builder.store(args[1], to_replace_ptr)
     builder.store(args[2], replacement_ptr)
 
     udf_str_ptr = builder.alloca(default_manager[udf_string].get_value_type())
