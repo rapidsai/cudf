639b48624ab19b3f8a55a22671049428d8584562
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-15 01:01:38 +0000

    Add tests of the remaining error cases

--- python/cudf/cudf/pylibcudf_tests/test_copying.py
+++ python/cudf/cudf/pylibcudf_tests/test_copying.py
@@ -130,6 +130,33 @@ def test_scatter_table_type_mismatch(source_table, input_column, target_table):
         )
 
 
+def test_scatter_scalars(source_scalar, input_column, target_table):
+    result = plc.copying.scatter(
+        [source_scalar] * target_table.num_columns(),
+        input_column,
+        target_table,
+    )
+    # TODO: There's no reason to require column metadata in this API. We can construct a
+    # placeholder internally and discard it after the conversion if necessary.
+    host_scalar = scalar_to_arrow(source_scalar).as_py()
+    pa_input_column = column_to_arrow(input_column)
+    pa_target_table = table_to_arrow(target_table)
+
+    # TODO: Is there a cleaner vectorized way to do this in pyarrow?
+    arrays = []
+    for i in range(target_table.num_columns()):
+        values = []
+        for j in range(target_table.num_rows()):
+            if pa.compute.is_in(j, pa_input_column).as_py():
+                values.append(host_scalar)
+            else:
+                values.append(pa_target_table.column(i)[j].as_py())
+        arrays.append(pa.array(values))
+
+    expected = pa.table(arrays, [""] * target_table.num_columns())
+    assert_table_eq(result, expected)
+
+
 def test_scatter_scalars_num_scalars_mismatch(
     source_scalar, input_column, target_table
 ):
@@ -160,33 +187,6 @@ def test_scatter_scalars_type_mismatch(input_column, target_table):
         )
 
 
-def test_scatter_scalars(source_scalar, input_column, target_table):
-    result = plc.copying.scatter(
-        [source_scalar] * target_table.num_columns(),
-        input_column,
-        target_table,
-    )
-    # TODO: There's no reason to require column metadata in this API. We can construct a
-    # placeholder internally and discard it after the conversion if necessary.
-    host_scalar = scalar_to_arrow(source_scalar).as_py()
-    pa_input_column = column_to_arrow(input_column)
-    pa_target_table = table_to_arrow(target_table)
-
-    # TODO: Is there a cleaner vectorized way to do this in pyarrow?
-    arrays = []
-    for i in range(target_table.num_columns()):
-        values = []
-        for j in range(target_table.num_rows()):
-            if pa.compute.is_in(j, pa_input_column).as_py():
-                values.append(host_scalar)
-            else:
-                values.append(pa_target_table.column(i)[j].as_py())
-        arrays.append(pa.array(values))
-
-    expected = pa.table(arrays, [""] * target_table.num_columns())
-    assert_table_eq(result, expected)
-
-
 def test_empty_like_column(input_column):
     result = plc.copying.empty_like(input_column)
     assert result.type() == input_column.type()
@@ -209,18 +209,6 @@ def test_allocate_like(input_column, size):
     assert result.size() == (input_column.size() if size is None else size)
 
 
-def test_copy_range(input_column, target_column):
-    result = plc.copying.copy_range(
-        input_column,
-        target_column,
-        0,
-        input_column.size(),
-        0,
-    )
-    expected = pa.array([1, 2, 3, 7, 8, 9])
-    assert_array_eq(result, expected)
-
-
 def test_copy_range_in_place(input_column, mutable_target_column):
     plc.copying.copy_range_in_place(
         input_column,
@@ -233,28 +221,93 @@ def test_copy_range_in_place(input_column, mutable_target_column):
     assert_array_eq(mutable_target_column, expected)
 
 
+# TODO: Test error case with non-fixed width types (currently this module only tests
+# everything on ints, so holding off there for now).
+def test_copy_range_in_place_out_of_bounds(
+    input_column, mutable_target_column
+):
+    with pytest.raises(IndexError):
+        plc.copying.copy_range_in_place(
+            input_column,
+            mutable_target_column,
+            5,
+            5 + input_column.size(),
+            0,
+        )
+
+
+def test_copy_range_in_place_different_types(
+    input_column, mutable_target_column
+):
+    with pytest.raises(TypeError):
+        plc.copying.copy_range_in_place(
+            column_from_arrow(pa.array([1.0, 2.0, 3.0])),
+            mutable_target_column,
+            0,
+            input_column.size(),
+            0,
+        )
+
+
+def test_copy_range_in_place_null_mismatch(
+    input_column, mutable_target_column
+):
+    with pytest.raises(ValueError):
+        plc.copying.copy_range_in_place(
+            column_from_arrow(pa.array([1, 2, None])),
+            mutable_target_column,
+            0,
+            input_column.size(),
+            0,
+        )
+
+
+def test_copy_range(input_column, target_column):
+    result = plc.copying.copy_range(
+        input_column,
+        target_column,
+        0,
+        input_column.size(),
+        0,
+    )
+    expected = pa.array([1, 2, 3, 7, 8, 9])
+    assert_array_eq(result, expected)
+
+
+def test_copy_range_out_of_bounds(input_column, target_column):
+    with pytest.raises(IndexError):
+        plc.copying.copy_range(
+            input_column,
+            target_column,
+            5,
+            5 + input_column.size(),
+            0,
+        )
+
+
+def test_copy_range_different_types(input_column, target_column):
+    with pytest.raises(TypeError):
+        plc.copying.copy_range(
+            column_from_arrow(pa.array([1.0, 2.0, 3.0])),
+            target_column,
+            0,
+            input_column.size(),
+            0,
+        )
+
+
 def test_shift(target_column, source_scalar):
     result = plc.copying.shift(target_column, 2, source_scalar)
     expected = pa.array([1, 1, 4, 5, 6, 7])
     assert_array_eq(result, expected)
 
 
-def test_split_column(target_column):
-    upper_bounds = [1, 3, 5]
-    lower_bounds = [0] + upper_bounds[:-1]
-    result = plc.copying.split(target_column, upper_bounds)
-    pa_target_column = column_to_arrow(target_column)
-    for lb, ub, split in zip(lower_bounds, upper_bounds, result):
-        assert_array_eq(split, pa_target_column[lb:ub])
-
-
-def test_split_table(target_table):
-    upper_bounds = [1, 3, 5]
-    lower_bounds = [0] + upper_bounds[:-1]
-    result = plc.copying.split(target_table, upper_bounds)
-    pa_target_table = table_to_arrow(target_table)
-    for lb, ub, split in zip(lower_bounds, upper_bounds, result):
-        assert_table_eq(split, pa_target_table[lb:ub])
+# TODO: Test error case for non-fixed width types.
+def test_shift_type_mismatch(target_column):
+    with pytest.raises(TypeError):
+        plc.copying.shift(
+            target_column, 2, plc.Scalar.from_arrow(pa.scalar(1.0))
+        )
 
 
 def test_slice_column(target_column):
@@ -267,6 +320,21 @@ def test_slice_column(target_column):
         assert_array_eq(slice_, pa_target_column[lb:ub])
 
 
+def test_slice_column_wrong_length(target_column):
+    with pytest.raises(ValueError):
+        plc.copying.slice(target_column, list(range(5)))
+
+
+def test_slice_column_decreasing(target_column):
+    with pytest.raises(ValueError):
+        plc.copying.slice(target_column, list(range(5, -1, -1)))
+
+
+def test_slice_column_out_of_bounds(target_column):
+    with pytest.raises(IndexError):
+        plc.copying.slice(target_column, list(range(2, 8)))
+
+
 def test_slice_table(target_table):
     bounds = list(range(6))
     upper_bounds = bounds[1::2]
@@ -275,6 +343,34 @@ def test_slice_table(target_table):
     pa_target_table = table_to_arrow(target_table)
     for lb, ub, slice_ in zip(lower_bounds, upper_bounds, result):
         assert_table_eq(slice_, pa_target_table[lb:ub])
+
+
+def test_split_column(target_column):
+    upper_bounds = [1, 3, 5]
+    lower_bounds = [0] + upper_bounds[:-1]
+    result = plc.copying.split(target_column, upper_bounds)
+    pa_target_column = column_to_arrow(target_column)
+    for lb, ub, split in zip(lower_bounds, upper_bounds, result):
+        assert_array_eq(split, pa_target_column[lb:ub])
+
+
+def test_split_column_decreasing(target_column):
+    with pytest.raises(ValueError):
+        plc.copying.split(target_column, list(range(5, -1, -1)))
+
+
+def test_split_column_out_of_bounds(target_column):
+    with pytest.raises(IndexError):
+        plc.copying.split(target_column, list(range(5, 8)))
+
+
+def test_split_table(target_table):
+    upper_bounds = [1, 3, 5]
+    lower_bounds = [0] + upper_bounds[:-1]
+    result = plc.copying.split(target_table, upper_bounds)
+    pa_target_table = table_to_arrow(target_table)
+    for lb, ub, split in zip(lower_bounds, upper_bounds, result):
+        assert_table_eq(split, pa_target_table[lb:ub])
 
 
 def test_copy_if_else_column_column(target_column):
@@ -297,6 +393,48 @@ def test_copy_if_else_column_column(target_column):
         pa_other_column,
     )
     assert_array_eq(result, expected)
+
+
+def test_copy_if_else_wrong_type(target_column):
+    with pytest.raises(TypeError):
+        plc.copying.copy_if_else(
+            column_from_arrow(pa.array([1.0] * target_column.size())),
+            target_column,
+            column_from_arrow(
+                pa.array([True, False] * (target_column.size() // 2))
+            ),
+        )
+
+
+def test_copy_if_else_wrong_type_mask(target_column):
+    with pytest.raises(TypeError):
+        plc.copying.copy_if_else(
+            target_column,
+            target_column,
+            column_from_arrow(
+                pa.array([1.0, 2.0] * (target_column.size() // 2))
+            ),
+        )
+
+
+def test_copy_if_else_wrong_size(target_column):
+    with pytest.raises(ValueError):
+        plc.copying.copy_if_else(
+            column_from_arrow(pa.array([1])),
+            target_column,
+            column_from_arrow(
+                pa.array([True, False] * (target_column.size() // 2))
+            ),
+        )
+
+
+def test_copy_if_else_wrong_size_mask(target_column):
+    with pytest.raises(ValueError):
+        plc.copying.copy_if_else(
+            target_column,
+            target_column,
+            column_from_arrow(pa.array([True])),
+        )
 
 
 @pytest.mark.parametrize("array_left", [True, False])
@@ -355,6 +493,51 @@ def test_boolean_mask_scatter_from_table(source_table, target_table):
     assert_table_eq(result, expected)
 
 
+def test_boolean_mask_scatter_from_wrong_num_cols(source_table, target_table):
+    with pytest.raises(ValueError):
+        plc.copying.boolean_mask_scatter(
+            plc.Table(source_table.columns()[:2]),
+            target_table,
+            column_from_arrow(pa.array([True, False] * 3)),
+        )
+
+
+def test_boolean_mask_scatter_from_wrong_mask_size(source_table, target_table):
+    with pytest.raises(ValueError):
+        plc.copying.boolean_mask_scatter(
+            source_table,
+            target_table,
+            column_from_arrow(pa.array([True, False] * 2)),
+        )
+
+
+def test_boolean_mask_scatter_from_wrong_num_true(source_table, target_table):
+    with pytest.raises(ValueError):
+        plc.copying.boolean_mask_scatter(
+            plc.Table(source_table.columns()[:2]),
+            target_table,
+            column_from_arrow(pa.array([True, False] * 2 + [False, False])),
+        )
+
+
+def test_boolean_mask_scatter_from_wrong_col_type(target_table):
+    with pytest.raises(TypeError):
+        plc.copying.boolean_mask_scatter(
+            plc.Table([column_from_arrow(pa.array([1.0, 2.0, 3.0]))] * 3),
+            target_table,
+            column_from_arrow(pa.array([True, False] * 3)),
+        )
+
+
+def test_boolean_mask_scatter_from_wrong_mask_type(source_table, target_table):
+    with pytest.raises(TypeError):
+        plc.copying.boolean_mask_scatter(
+            source_table,
+            target_table,
+            column_from_arrow(pa.array([1.0, 2.0] * 3)),
+        )
+
+
 def test_boolean_mask_scatter_from_scalars(source_scalar, target_table):
     py_mask = [False] * target_table.num_rows()
     py_mask[2:5] = [True, True, True]
@@ -389,3 +572,8 @@ def test_get_element(input_column):
     result = plc.copying.get_element(input_column, index)
     pa_input_column = column_to_arrow(input_column)
     assert scalar_to_arrow(result).as_py() == pa_input_column[index].as_py()
+
+
+def test_get_element_out_of_bounds(input_column):
+    with pytest.raises(IndexError):
+        plc.copying.get_element(input_column, 100)
