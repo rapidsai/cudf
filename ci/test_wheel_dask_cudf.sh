296521606db25c50b46db0f8d670497e6a10c828
Bradley Dice <bdice@bradleydice.com>
2024-03-16 09:07:33 -0500

    Remove manylinux_2_17 compatibility logic.

--- ci/test_wheel_dask_cudf.sh
+++ ci/test_wheel_dask_cudf.sh
@@ -7,20 +7,7 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="dask_cudf_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-s3 ./dist
 
 # Download the cudf built in the previous step
-# Set the manylinux version used for downloading the wheels so that we test the
-# newer ABI wheels on the newer images that support their installation.
-# Need to disable pipefail for the head not to fail, see
-# https://stackoverflow.com/questions/19120263/why-exit-code-141-with-grep-q
-set +o pipefail
-glibc_minor_version=$(ldd --version | head -1 | grep -o "[0-9]\.[0-9]\+" | tail -1 | cut -d '.' -f2)
-set -o pipefail
-manylinux_version="2_17"
-if [[ ${glibc_minor_version} -ge 28 ]]; then
-    manylinux_version="2_28"
-fi
-manylinux="manylinux_${manylinux_version}"
-
-RAPIDS_PY_WHEEL_NAME="cudf_${manylinux}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-cudf-dep
+RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-cudf-dep
 python -m pip install --no-deps ./local-cudf-dep/cudf*.whl
 
 # echo to expand wildcard before adding `[extra]` requires for pip
