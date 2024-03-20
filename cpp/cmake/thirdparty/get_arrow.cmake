a382338e152c0c30b3f55308d3fcb63176f21846
galipremsagar <sagarprem75@gmail.com>
2024-03-20 17:34:46 +0000

    upgrade arrow

--- cpp/cmake/thirdparty/get_arrow.cmake
+++ cpp/cmake/thirdparty/get_arrow.cmake
@@ -441,7 +441,7 @@ if(NOT DEFINED CUDF_VERSION_Arrow)
   set(CUDF_VERSION_Arrow
       # This version must be kept in sync with the libarrow version pinned for builds in
       # dependencies.yaml.
-      14.0.2
+      15.0.2
       CACHE STRING "The version of Arrow to find (or build)"
   )
 endif()
