1174d09a2154439ed37bacc3d16d2d720c347543
Vyas Ramasubramani <vyasr@nvidia.com>
2024-03-20 00:59:57 +0000

    Switch testing utilities to use the new interop APIs and make sure they support chunked arrays

--- python/cudf/cudf/pylibcudf_tests/common/utils.py
+++ python/cudf/cudf/pylibcudf_tests/common/utils.py
@@ -5,39 +5,9 @@ import pyarrow as pa
 from cudf._lib import pylibcudf as plc
 
 
-# TODO: The two functions below should probably be a function in interop. We should move
-# the class method on Table into a function in interop, and then use that to implement
-# this function in interop as well.
-def column_from_arrow(pa_array):
-    """Create a pylibcudf column from a PyArrow array."""
-    pa_table = pa.table([pa_array], [""])
-    table = plc.Table.from_arrow(pa_table)
-    return table.columns()[0]
-
-
-def column_to_arrow(plc_column):
-    """Create a PyArrow array from a pylibcudf column."""
-    return plc.Table([plc_column]).to_arrow([plc.interop.ColumnMetadata("")])[
-        0
-    ]
-
-
-# This function is just a convenient wrapper for the metadata handling. We should update
-# the API to make naming more convenient when we don't need metadata objects.
-def table_to_arrow(plc_table):
-    """Create a PyArrow table from a pylibcudf Table."""
-    return plc_table.to_arrow(
-        [plc.interop.ColumnMetadata("")] * plc_table.num_columns()
-    )
-
-
-def scalar_to_arrow(plc_scalar):
-    return plc_scalar.to_arrow(plc.interop.ColumnMetadata(""))
-
-
 def assert_array_eq(plc_column, pa_array):
     """Verify that the pylibcudf array and PyArrow array are equal."""
-    pa_equal = pa.compute.equal(column_to_arrow(plc_column), pa_array)
+    pa_equal = pa.compute.equal(plc.interop.to_arrow(plc_column), pa_array)
     assert pa.compute.all(pa_equal).as_py()
 
 
--- python/cudf/cudf/pylibcudf_tests/test_copying.py
+++ python/cudf/cudf/pylibcudf_tests/test_copying.py
@@ -2,26 +2,19 @@
 
 import pyarrow as pa
 import pytest
-from utils import (
-    assert_array_eq,
-    assert_table_eq,
-    column_from_arrow,
-    column_to_arrow,
-    scalar_to_arrow,
-    table_to_arrow,
-)
+from utils import assert_array_eq, assert_table_eq
 
 from cudf._lib import pylibcudf as plc
 
 
 @pytest.fixture(scope="module")
 def input_column():
-    return column_from_arrow(pa.array([1, 2, 3]))
+    return plc.interop.from_arrow(pa.array([1, 2, 3]))
 
 
 @pytest.fixture(scope="module")
 def target_column():
-    return column_from_arrow(pa.array([4, 5, 6, 7, 8, 9]))
+    return plc.interop.from_arrow(pa.array([4, 5, 6, 7, 8, 9]))
 
 
 @pytest.fixture
@@ -41,7 +34,7 @@ def target_table(target_column):
 
 @pytest.fixture(scope="module")
 def source_scalar():
-    return plc.Scalar.from_arrow(pa.scalar(1))
+    return plc.interop.from_arrow(pa.scalar(1))
 
 
 def test_gather(target_table, input_column):
@@ -50,13 +43,13 @@ def test_gather(target_table, input_column):
         input_column,
         plc.copying.OutOfBoundsPolicy.DONT_CHECK,
     )
-    pa_table = table_to_arrow(target_table)
-    expected = pa_table.take(column_to_arrow(input_column))
+    pa_table = plc.interop.to_arrow(target_table)
+    expected = pa_table.take(plc.interop.to_arrow(input_column))
     assert_table_eq(result, expected)
 
 
 def test_gather_map_has_nulls(target_table):
-    gather_map = column_from_arrow(pa.array([0, 1, None]))
+    gather_map = plc.interop.from_arrow(pa.array([0, 1, None]))
     with pytest.raises(ValueError):
         plc.copying.gather(
             target_table,
@@ -71,9 +64,9 @@ def test_scatter_table(source_table, input_column, target_table):
         input_column,
         target_table,
     )
-    pa_source_table = table_to_arrow(source_table)
-    pa_input_column = column_to_arrow(input_column)
-    pa_target_table = table_to_arrow(target_table)
+    pa_source_table = plc.interop.to_arrow(source_table)
+    pa_input_column = plc.interop.to_arrow(input_column)
+    pa_target_table = plc.interop.to_arrow(target_table)
 
     # TODO: Is there a cleaner vectorized way to do this in pyarrow?
     output_rows = []
@@ -104,7 +97,9 @@ def test_scatter_table_num_row_mismatch(source_table, target_table):
     with pytest.raises(ValueError):
         plc.copying.scatter(
             source_table,
-            column_from_arrow(pa.array(range(source_table.num_rows() * 2))),
+            plc.interop.from_arrow(
+                pa.array(range(source_table.num_rows() * 2))
+            ),
             target_table,
         )
 
@@ -113,7 +108,7 @@ def test_scatter_table_map_has_nulls(source_table, target_table):
     with pytest.raises(ValueError):
         plc.copying.scatter(
             source_table,
-            column_from_arrow(pa.array([None] * source_table.num_rows())),
+            plc.interop.from_arrow(pa.array([None] * source_table.num_rows())),
             target_table,
         )
 
@@ -124,7 +119,7 @@ def test_scatter_table_type_mismatch(source_table, input_column, target_table):
         ncol = source_table.num_columns()
         pa_table = pa.table([pa_array] * ncol, [""] * ncol)
         plc.copying.scatter(
-            plc.Table.from_arrow(pa_table),
+            plc.interop.from_arrow(pa_table),
             input_column,
             target_table,
         )
@@ -138,9 +133,9 @@ def test_scatter_scalars(source_scalar, input_column, target_table):
     )
     # TODO: There's no reason to require column metadata in this API. We can construct a
     # placeholder internally and discard it after the conversion if necessary.
-    host_scalar = scalar_to_arrow(source_scalar).as_py()
-    pa_input_column = column_to_arrow(input_column)
-    pa_target_table = table_to_arrow(target_table)
+    host_scalar = plc.interop.to_arrow(source_scalar).as_py()
+    pa_input_column = plc.interop.to_arrow(input_column)
+    pa_target_table = plc.interop.to_arrow(target_table)
 
     # TODO: Is there a cleaner vectorized way to do this in pyarrow?
     arrays = []
@@ -172,7 +167,7 @@ def test_scatter_scalars_map_has_nulls(source_scalar, target_table):
     with pytest.raises(ValueError):
         plc.copying.scatter(
             [source_scalar] * target_table.num_columns(),
-            column_from_arrow(pa.array([None, None])),
+            plc.interop.from_arrow(pa.array([None, None])),
             target_table,
         )
 
@@ -180,7 +175,7 @@ def test_scatter_scalars_map_has_nulls(source_scalar, target_table):
 def test_scatter_scalars_type_mismatch(input_column, target_table):
     with pytest.raises(TypeError):
         plc.copying.scatter(
-            [plc.Scalar.from_arrow(pa.scalar(True))]
+            [plc.interop.from_arrow(pa.scalar(True))]
             * target_table.num_columns(),
             input_column,
             target_table,
@@ -241,7 +236,7 @@ def test_copy_range_in_place_different_types(
 ):
     with pytest.raises(TypeError):
         plc.copying.copy_range_in_place(
-            column_from_arrow(pa.array([1.0, 2.0, 3.0])),
+            plc.interop.from_arrow(pa.array([1.0, 2.0, 3.0])),
             mutable_target_column,
             0,
             input_column.size(),
@@ -254,7 +249,7 @@ def test_copy_range_in_place_null_mismatch(
 ):
     with pytest.raises(ValueError):
         plc.copying.copy_range_in_place(
-            column_from_arrow(pa.array([1, 2, None])),
+            plc.interop.from_arrow(pa.array([1, 2, None])),
             mutable_target_column,
             0,
             input_column.size(),
@@ -288,7 +283,7 @@ def test_copy_range_out_of_bounds(input_column, target_column):
 def test_copy_range_different_types(input_column, target_column):
     with pytest.raises(TypeError):
         plc.copying.copy_range(
-            column_from_arrow(pa.array([1.0, 2.0, 3.0])),
+            plc.interop.from_arrow(pa.array([1.0, 2.0, 3.0])),
             target_column,
             0,
             input_column.size(),
@@ -306,7 +301,7 @@ def test_shift(target_column, source_scalar):
 def test_shift_type_mismatch(target_column):
     with pytest.raises(TypeError):
         plc.copying.shift(
-            target_column, 2, plc.Scalar.from_arrow(pa.scalar(1.0))
+            target_column, 2, plc.interop.from_arrow(pa.scalar(1.0))
         )
 
 
@@ -315,7 +310,7 @@ def test_slice_column(target_column):
     upper_bounds = bounds[1::2]
     lower_bounds = bounds[::2]
     result = plc.copying.slice(target_column, bounds)
-    pa_target_column = column_to_arrow(target_column)
+    pa_target_column = plc.interop.to_arrow(target_column)
     for lb, ub, slice_ in zip(lower_bounds, upper_bounds, result):
         assert_array_eq(slice_, pa_target_column[lb:ub])
 
@@ -340,7 +335,7 @@ def test_slice_table(target_table):
     upper_bounds = bounds[1::2]
     lower_bounds = bounds[::2]
     result = plc.copying.slice(target_table, bounds)
-    pa_target_table = table_to_arrow(target_table)
+    pa_target_table = plc.interop.to_arrow(target_table)
     for lb, ub, slice_ in zip(lower_bounds, upper_bounds, result):
         assert_table_eq(slice_, pa_target_table[lb:ub])
 
@@ -349,7 +344,7 @@ def test_split_column(target_column):
     upper_bounds = [1, 3, 5]
     lower_bounds = [0] + upper_bounds[:-1]
     result = plc.copying.split(target_column, upper_bounds)
-    pa_target_column = column_to_arrow(target_column)
+    pa_target_column = plc.interop.to_arrow(target_column)
     for lb, ub, split in zip(lower_bounds, upper_bounds, result):
         assert_array_eq(split, pa_target_column[lb:ub])
 
@@ -368,25 +363,25 @@ def test_split_table(target_table):
     upper_bounds = [1, 3, 5]
     lower_bounds = [0] + upper_bounds[:-1]
     result = plc.copying.split(target_table, upper_bounds)
-    pa_target_table = table_to_arrow(target_table)
+    pa_target_table = plc.interop.to_arrow(target_table)
     for lb, ub, split in zip(lower_bounds, upper_bounds, result):
         assert_table_eq(split, pa_target_table[lb:ub])
 
 
 def test_copy_if_else_column_column(target_column):
-    pa_other_column = column_to_arrow(target_column)
+    pa_other_column = plc.interop.to_arrow(target_column)
     pa_other_column = pa.compute.add(pa_other_column, 4)
-    other_column = column_from_arrow(pa_other_column)
+    other_column = plc.interop.from_arrow(pa_other_column)
 
     pa_mask = pa.array([True, False] * (target_column.size() // 2))
-    mask = column_from_arrow(pa_mask)
+    mask = plc.interop.from_arrow(pa_mask)
     result = plc.copying.copy_if_else(
         target_column,
         other_column,
         mask,
     )
 
-    pa_target_column = column_to_arrow(target_column)
+    pa_target_column = plc.interop.to_arrow(target_column)
     expected = pa.compute.if_else(
         pa_mask,
         pa_target_column,
@@ -398,9 +393,9 @@ def test_copy_if_else_column_column(target_column):
 def test_copy_if_else_wrong_type(target_column):
     with pytest.raises(TypeError):
         plc.copying.copy_if_else(
-            column_from_arrow(pa.array([1.0] * target_column.size())),
+            plc.interop.from_arrow(pa.array([1.0] * target_column.size())),
             target_column,
-            column_from_arrow(
+            plc.interop.from_arrow(
                 pa.array([True, False] * (target_column.size() // 2))
             ),
         )
@@ -411,7 +406,7 @@ def test_copy_if_else_wrong_type_mask(target_column):
         plc.copying.copy_if_else(
             target_column,
             target_column,
-            column_from_arrow(
+            plc.interop.from_arrow(
                 pa.array([1.0, 2.0] * (target_column.size() // 2))
             ),
         )
@@ -420,9 +415,9 @@ def test_copy_if_else_wrong_type_mask(target_column):
 def test_copy_if_else_wrong_size(target_column):
     with pytest.raises(ValueError):
         plc.copying.copy_if_else(
-            column_from_arrow(pa.array([1])),
+            plc.interop.from_arrow(pa.array([1])),
             target_column,
-            column_from_arrow(
+            plc.interop.from_arrow(
                 pa.array([True, False] * (target_column.size() // 2))
             ),
         )
@@ -433,14 +428,14 @@ def test_copy_if_else_wrong_size_mask(target_column):
         plc.copying.copy_if_else(
             target_column,
             target_column,
-            column_from_arrow(pa.array([True])),
+            plc.interop.from_arrow(pa.array([True])),
         )
 
 
 @pytest.mark.parametrize("array_left", [True, False])
 def test_copy_if_else_column_scalar(target_column, source_scalar, array_left):
     pa_mask = pa.array([True, False] * (target_column.size() // 2))
-    mask = column_from_arrow(pa_mask)
+    mask = plc.interop.from_arrow(pa_mask)
     args = (
         (target_column, source_scalar)
         if array_left
@@ -451,8 +446,10 @@ def test_copy_if_else_column_scalar(target_column, source_scalar, array_left):
         mask,
     )
 
-    pa_scalar = source_scalar.to_arrow(plc.interop.ColumnMetadata(""))
-    pa_target_column = column_to_arrow(target_column)
+    pa_scalar = plc.interop.to_arrow(
+        source_scalar, plc.interop.ColumnMetadata("")
+    )
+    pa_target_column = plc.interop.to_arrow(target_column)
     pa_args = (
         (pa_target_column, pa_scalar)
         if array_left
@@ -470,7 +467,7 @@ def test_boolean_mask_scatter_from_table(source_table, target_table):
     py_mask[2:5] = [True, True, True]
     pa_mask = pa.array(py_mask)
 
-    mask = column_from_arrow(pa_mask)
+    mask = plc.interop.from_arrow(pa_mask)
     result = plc.copying.boolean_mask_scatter(
         source_table,
         target_table,
@@ -478,8 +475,8 @@ def test_boolean_mask_scatter_from_table(source_table, target_table):
     )
 
     # TODO: Is there a cleaner vectorized way to do this in pyarrow?
-    pa_source_table = table_to_arrow(source_table)
-    pa_target_table = table_to_arrow(target_table)
+    pa_source_table = plc.interop.to_arrow(source_table)
+    pa_target_table = plc.interop.to_arrow(target_table)
     output_rows = []
     source_index = 0
     for target_index, mask_val in enumerate(pa_mask.to_pylist()):
@@ -498,7 +495,7 @@ def test_boolean_mask_scatter_from_wrong_num_cols(source_table, target_table):
         plc.copying.boolean_mask_scatter(
             plc.Table(source_table.columns()[:2]),
             target_table,
-            column_from_arrow(pa.array([True, False] * 3)),
+            plc.interop.from_arrow(pa.array([True, False] * 3)),
         )
 
 
@@ -507,7 +504,7 @@ def test_boolean_mask_scatter_from_wrong_mask_size(source_table, target_table):
         plc.copying.boolean_mask_scatter(
             source_table,
             target_table,
-            column_from_arrow(pa.array([True, False] * 2)),
+            plc.interop.from_arrow(pa.array([True, False] * 2)),
         )
 
 
@@ -516,16 +513,18 @@ def test_boolean_mask_scatter_from_wrong_num_true(source_table, target_table):
         plc.copying.boolean_mask_scatter(
             plc.Table(source_table.columns()[:2]),
             target_table,
-            column_from_arrow(pa.array([True, False] * 2 + [False, False])),
+            plc.interop.from_arrow(
+                pa.array([True, False] * 2 + [False, False])
+            ),
         )
 
 
 def test_boolean_mask_scatter_from_wrong_col_type(target_table):
     with pytest.raises(TypeError):
         plc.copying.boolean_mask_scatter(
-            plc.Table([column_from_arrow(pa.array([1.0, 2.0, 3.0]))] * 3),
+            plc.Table([plc.interop.from_arrow(pa.array([1.0, 2.0, 3.0]))] * 3),
             target_table,
-            column_from_arrow(pa.array([True, False] * 3)),
+            plc.interop.from_arrow(pa.array([True, False] * 3)),
         )
 
 
@@ -534,7 +533,7 @@ def test_boolean_mask_scatter_from_wrong_mask_type(source_table, target_table):
         plc.copying.boolean_mask_scatter(
             source_table,
             target_table,
-            column_from_arrow(pa.array([1.0, 2.0] * 3)),
+            plc.interop.from_arrow(pa.array([1.0, 2.0] * 3)),
         )
 
 
@@ -543,7 +542,7 @@ def test_boolean_mask_scatter_from_scalars(source_scalar, target_table):
     py_mask[2:5] = [True, True, True]
     pa_mask = pa.array(py_mask)
 
-    mask = column_from_arrow(pa_mask)
+    mask = plc.interop.from_arrow(pa_mask)
     result = plc.copying.boolean_mask_scatter(
         [source_scalar] * 3,
         target_table,
@@ -551,8 +550,8 @@ def test_boolean_mask_scatter_from_scalars(source_scalar, target_table):
     )
 
     # TODO: Is there a cleaner vectorized way to do this in pyarrow?
-    host_scalar = scalar_to_arrow(source_scalar).as_py()
-    pa_target_table = table_to_arrow(target_table)
+    host_scalar = plc.interop.to_arrow(source_scalar).as_py()
+    pa_target_table = plc.interop.to_arrow(target_table)
     arrays = []
     for i in range(target_table.num_columns()):
         values = []
@@ -570,8 +569,10 @@ def test_boolean_mask_scatter_from_scalars(source_scalar, target_table):
 def test_get_element(input_column):
     index = 1
     result = plc.copying.get_element(input_column, index)
-    pa_input_column = column_to_arrow(input_column)
-    assert scalar_to_arrow(result).as_py() == pa_input_column[index].as_py()
+    pa_input_column = plc.interop.to_arrow(input_column)
+    assert (
+        plc.interop.to_arrow(result).as_py() == pa_input_column[index].as_py()
+    )
 
 
 def test_get_element_out_of_bounds(input_column):
