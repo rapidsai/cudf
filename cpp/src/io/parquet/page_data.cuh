9d1f23b6a90e25891365cf2427bb387f39d4d939
seidl <seidl2@llnl.gov>
2024-03-21 16:16:09 +0000

    remove unnecessary comment

--- cpp/src/io/parquet/page_data.cuh
+++ cpp/src/io/parquet/page_data.cuh
@@ -34,7 +34,6 @@ template <typename state_buf>
 inline __device__ void gpuOutputString(page_state_s* s, state_buf* sb, int src_pos, void* dstv)
 {
   auto [ptr, len] = gpuGetStringData(s, sb, src_pos);
-  // make sure to only hash `BYTE_ARRAY` when specified with the output type size
   if (s->col.is_strings_to_cat and s->col.physical_type == BYTE_ARRAY) {
     // Output hash. This hash value is used if the option to convert strings to
     // categoricals is enabled. The seed value is chosen arbitrarily.
