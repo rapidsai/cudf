ea84f0530b2342b74eacc4f6830df8ed98bb9dfb
galipremsagar <sagarprem75@gmail.com>
2024-03-26 15:53:59 +0000

    test

--- ci/cudf_pandas_scripts/pandas-tests/run.sh
+++ ci/cudf_pandas_scripts/pandas-tests/run.sh
@@ -10,8 +10,8 @@ PANDAS_TESTS_BRANCH=${1}
 rapids-logger "Running Pandas tests using $PANDAS_TESTS_BRANCH branch"
 rapids-logger "PR number: ${RAPIDS_REF_NAME:-"unknown"}"
 
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-cudf-dep
+# RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+# RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-cudf-dep
 # python -m pip install $(ls ./local-cudf-dep/cudf*.whl)[test,pandas-tests]
 
 # RESULTS_DIR=${RAPIDS_TESTS_DIR:-"$(mktemp -d)"}
