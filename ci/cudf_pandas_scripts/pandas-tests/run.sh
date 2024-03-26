07432ac1e662ad4dbff64865f03340615c812abc
galipremsagar <sagarprem75@gmail.com>
2024-03-26 16:01:02 +0000

    test

--- ci/cudf_pandas_scripts/pandas-tests/run.sh
+++ ci/cudf_pandas_scripts/pandas-tests/run.sh
@@ -17,11 +17,11 @@ rapids-logger "PR number: ${RAPIDS_REF_NAME:-"unknown"}"
 # RESULTS_DIR=${RAPIDS_TESTS_DIR:-"$(mktemp -d)"}
 # RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${RESULTS_DIR}/test-results"}/
 # mkdir -p "${RAPIDS_TESTS_DIR}"
-sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& apt update \
+&& apt install gh -y
 gh pr comment "15369" --body "Thank you for your contribution!" --repo "rapidsai/cudf"
 bash python/cudf/cudf/pandas/scripts/run-pandas-tests.sh \
   -n 10 \
