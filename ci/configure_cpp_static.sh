bc37d5f671acbc94307db719cdaaab7e65f0f9e4
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-02 16:54:28 +0000

    Rehash the pyenv env

--- ci/configure_cpp_static.sh
+++ ci/configure_cpp_static.sh
@@ -10,4 +10,5 @@ source rapids-date-string
 rapids-logger "Configure static cpp build"
 
 python -m pip install cmake ninja
+pyenv rehash
 cmake -S cpp -B build_static -GNinja -DBUILD_SHARED_LIBS=OFF -DBUILD_STATIC=OFF
