4401689e406f3d9988ec8cbe58fd1a64c5ba95fe
galipremsagar <sagarprem75@gmail.com>
2024-04-02 20:47:26 +0000

    cleanup

--- ci/cudf_pandas_scripts/pandas-tests/diff.sh
+++ ci/cudf_pandas_scripts/pandas-tests/diff.sh
@@ -10,17 +10,12 @@
 MAIN_ARTIFACT=$(rapids-s3-path)cuda12_$(arch)_py310.main-results.json
 PR_ARTIFACT=$(rapids-s3-path)cuda12_$(arch)_py39.pr-results.json
 
-rapids-logger "abc"
-rapids-logger "abc-1"
+rapids-logger "Fetching latest available results from nightly"
 aws s3api list-objects-v2 --bucket rapids-downloads --prefix "nightly/" --query "sort_by(Contents[?ends_with(Key, '.main-results.json')], &LastModified)[::-1].[Key]" --output text > s3_output.txt
-# aws s3api list-objects-v2 --bucket rapids-downloads --prefix "nightly/" --query 'sort_by(Contents, &LastModified)[*].{Key: Key, LastModified: LastModified}' --output text > s3_output.txt
-# aws s3 ls s3://rapids-downloads/nightly/cudf/ --recursive --output text > s3_output.txt
-# grep "-results.json" s3_output.txt
 cat s3_output.txt
 read -r COMPARE_ENV < s3_output.txt
 export COMPARE_ENV
-rapids-logger "Got ENV: ${COMPARE_ENV}"
-rapids-logger "abc-exit"
+rapids-logger "Latest available results from nightly: ${COMPARE_ENV}"
 
 aws s3 cp "s3://rapids-downloads/${COMPARE_ENV}" main-results.json
 aws s3 cp $PR_ARTIFACT pr-results.json
@@ -29,27 +24,6 @@ aws s3 cp $PR_ARTIFACT pr-results.json
 python -m pip install pandas tabulate
 python ci/cudf_pandas_scripts/pandas-tests/job-summary.py main-results.json pr-results.json | tee summary.txt >> "$GITHUB_STEP_SUMMARY"
 
-# COMMENT=$(head -n 10 summary.txt)
-# COMMENT=$(cat summary.txt)
-# read -r COMMENT < summary.txt
-# export COMMENT
-
-# echo "$COMMENT"
-# rapids-logger "comment: ${COMMENT}"
-cat summary.txt
-# rapids-logger "EOF"
-
-# Magic name that the custom-job.yaml workflow reads and re-exports
-# echo "job_output=${COMMENT}" >> $GITHUB_OUTPUT
-# echo "EOF" >> $GITHUB_ENV
-RAPIDS_ARTIFACTS_DIR=${RAPIDS_ARTIFACTS_DIR:-"${PWD}/artifacts"}
-mkdir -p "${RAPIDS_ARTIFACTS_DIR}"
-mv summary.txt ${RAPIDS_ARTIFACTS_DIR}/
-rapids-upload-to-s3 ${RAPIDS_ARTIFACTS_DIR}/summary.txt "${RAPIDS_ARTIFACTS_DIR}"
-ART_URL="$(rapids-s3-path)${RAPIDS_ARTIFACTS_DIR}/summary.txt"
-echo "job_output=${ART_URL}" >> $GITHUB_OUTPUT
-
-
-# https://downloads.rapids.ai/ci/cudf/pull-request/15369/6f62013//__w/cudf/cudf/artifacts/summary.txt
-
-# s3://rapids-downloads/ci/cudf/pull-request/15369/6f62013//__w/cudf/cudf/artifacts/summary.txt
+COMMENT=$(head -1 summary.txt)
+echo "$COMMENT"
+echo "$COMMENT" > gh-status.json
