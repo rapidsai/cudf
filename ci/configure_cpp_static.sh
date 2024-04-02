cb47003cef157f9cece1494457e71ce9a78d22d0
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-02 17:35:37 +0000

    Use dfg to centralize the requirement

--- ci/configure_cpp_static.sh
+++ ci/configure_cpp_static.sh
@@ -9,6 +9,14 @@ source rapids-date-string
 
 rapids-logger "Configure static cpp build"
 
-python -m pip install cmake ninja
+ENV_YAML_DIR="$(mktemp -d)"
+REQUIREMENTS_FILE="${ENV_YAML_DIR}/requirements.txt"
+
+rapids-dependency-file-generator \
+  --output requirements \
+  --file_key build_base | tee "${REQUIREMENTS_FILE}"
+
+python -m pip install -r "${REQUIREMENTS_FILE}"
 pyenv rehash
+
 cmake -S cpp -B build_static -GNinja -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF
