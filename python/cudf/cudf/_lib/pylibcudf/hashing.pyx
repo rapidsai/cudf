8ab4afafe722e43106cd4fb8a1182617f1448e2e
brandon-b-miller <brmiller@nvidia.com>
2024-04-05 10:12:13 -0500

    docs

--- python/cudf/cudf/_lib/pylibcudf/hashing.pyx
+++ python/cudf/cudf/_lib/pylibcudf/hashing.pyx
@@ -25,6 +25,24 @@ cpdef Column murmurhash3_x86_32(
     Table input,
     uint32_t seed
 ):
+    """Computes the MurmurHash3 32-bit hash value of each row in the given table.
+
+    For details, see :cpp:func:murmurhash3_x86_32.
+
+    Parameters
+    ----------
+
+    input : Table
+        The table of columns to hash
+    seed : uint32_t
+        Optional seed value to use for the hash function
+
+    Returns
+    -------
+
+    pylibcudf.Column
+        A column where each row is the hash of a row from the input
+    """
     cdef unique_ptr[column] c_result
     with nogil:
         c_result = move(
@@ -40,6 +58,24 @@ cpdef Table murmurhash3_x64_128(
     Table input,
     uint64_t seed
 ):
+    """Computes the MurmurHash3 64-bit hash value of each row in the given table.
+
+    For details, see :cpp:func:murmurhash3_x64_128.
+
+    Parameters
+    ----------
+
+    input : Table
+        The table of columns to hash
+    seed : uint32_t
+        Optional seed value to use for the hash function
+
+    Returns
+    -------
+
+    pylibcudf.Table
+        A table of two UINT64 columns
+    """
     cdef unique_ptr[table] c_result
     with nogil:
         c_result = move(
@@ -75,30 +111,106 @@ cpdef Column md5(Table input):
     return Column.from_libcudf(move(c_result))
 
 cpdef Column sha1(Table input):
+    """Computes the SHA-1 hash value of each row in the given table.
+
+    For details, see :cpp:func:sha1.
+
+    Parameters
+    ----------
+
+    input : Table
+        The table of columns to hash
+
+    Returns
+    -------
+
+    pylibcudf.Column
+        A column where each row is the hash of a row from the input
+    """
     cdef unique_ptr[column] c_result
     with nogil:
         c_result = move(cpp_sha1(input.view()))
     return Column.from_libcudf(move(c_result))
 
 cpdef Column sha224(Table input):
+    """Computes the SHA-224 hash value of each row in the given table.
+
+    For details, see :cpp:func:sha224.
+
+    Parameters
+    ----------
+
+    input : Table
+        The table of columns to hash
+
+    Returns
+    -------
+    pylibcudf.Column
+        A column where each row is the hash of a row from the input
+    """
     cdef unique_ptr[column] c_result
     with nogil:
         c_result = move(cpp_sha224(input.view()))
     return Column.from_libcudf(move(c_result))
 
 cpdef Column sha256(Table input):
+    """Computes the SHA-256 hash value of each row in the given table.
+
+    For details, see :cpp:func:256.
+
+    Parameters
+    ----------
+
+    input : Table
+        The table of columns to hash
+
+    Returns
+    -------
+    pylibcudf.Column
+        A column where each row is the hash of a row from the input
+    """
     cdef unique_ptr[column] c_result
     with nogil:
         c_result = move(cpp_sha256(input.view()))
     return Column.from_libcudf(move(c_result))
 
 cpdef Column sha384(Table input):
+    """Computes the SHA-384 hash value of each row in the given table.
+
+    For details, see :cpp:func:sha384.
+
+    Parameters
+    ----------
+
+    input : Table
+        The table of columns to hash
+
+    Returns
+    -------
+    pylibcudf.Column
+        A column where each row is the hash of a row from the input
+    """
     cdef unique_ptr[column] c_result
     with nogil:
         c_result = move(cpp_sha384(input.view()))
     return Column.from_libcudf(move(c_result))
 
 cpdef Column sha512(Table input):
+    """Computes the SHA-512 hash value of each row in the given table.
+
+    For details, see :cpp:func:sha512.
+
+    Parameters
+    ----------
+
+    input : Table
+        The table of columns to hash
+
+    Returns
+    -------
+    pylibcudf.Column
+        A column where each row is the hash of a row from the input
+    """
     cdef unique_ptr[column] c_result
     with nogil:
         c_result = move(cpp_sha512(input.view()))
