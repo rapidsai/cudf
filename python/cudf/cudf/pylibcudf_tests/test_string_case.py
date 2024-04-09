d343a62660a8eb7e4515733aece32593704cd4d6
brandon-b-miller <brmiller@nvidia.com>
2024-04-09 07:42:50 -0500

    fix up tests

--- python/cudf/cudf/pylibcudf_tests/test_string_case.py
+++ python/cudf/cudf/pylibcudf_tests/test_string_case.py
@@ -10,31 +10,51 @@ from cudf._lib.pylibcudf.strings import case
 
 @pytest.fixture(scope="module")
 def string_col():
-    return pa.array(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"])
+    return pa.array(
+        ["AbC", "de", "FGHI", "j", "kLm", "nOPq", None, "RsT", None, "uVw"]
+    )
+
+
+def wrap_nulls(func):
+    def wrapper(x):
+        if x is None:
+            return None
+        return func(x)
+
+    return wrapper
 
 
 def test_to_upper(string_col):
     plc_col = plc.interop.from_arrow(string_col)
     got = case.to_upper(plc_col)
-    expected = pa.Array.from_pandas(
-        string_col.to_pandas().apply(lambda x: x.upper())
-    )
+
+    @wrap_nulls
+    def to_upper(x):
+        return x.upper()
+
+    expected = pa.Array.from_pandas(string_col.to_pandas().apply(to_upper))
     assert_column_eq(got, expected)
 
 
 def test_to_lower(string_col):
     plc_col = plc.interop.from_arrow(string_col)
     got = case.to_lower(plc_col)
-    expected = pa.Array.from_pandas(
-        string_col.to_pandas().apply(lambda x: x.lower())
-    )
+
+    @wrap_nulls
+    def to_lower(x):
+        return x.lower()
+
+    expected = pa.Array.from_pandas(string_col.to_pandas().apply(to_lower))
     assert_column_eq(got, expected)
 
 
 def test_swapcase(string_col):
     plc_col = plc.interop.from_arrow(string_col)
     got = case.swapcase(plc_col)
-    expected = pa.Array.from_pandas(
-        string_col.to_pandas().apply(lambda x: x.swapcase())
-    )
+
+    @wrap_nulls
+    def swapcase(x):
+        return x.swapcase()
+
+    expected = pa.Array.from_pandas(string_col.to_pandas().apply(swapcase))
     assert_column_eq(got, expected)
