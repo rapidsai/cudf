0d648b4ff3f60eec5f7eb51c36cfc6491d9f406a
David Wendt <dwendt@nvidia.com>
2024-04-10 14:46:56 -0400

    remove unneeded headers

--- cpp/src/strings/merge/merge.cu
+++ cpp/src/strings/merge/merge.cu
@@ -24,10 +24,8 @@
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/exec_policy.hpp>
 
-#include <cuda/functional>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/transform.h>
-#include <thrust/tuple.h>
 
 namespace cudf {
 namespace strings {
