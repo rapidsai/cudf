f0ad27d44d2470cca3b2d40a2731b0f040fe789b
brandon-b-miller <brmiller@nvidia.com>
2024-04-10 07:05:21 -0500

    address reviews

--- python/cudf/cudf/pylibcudf_tests/test_string_case.py
+++ python/cudf/cudf/pylibcudf_tests/test_string_case.py
@@ -26,34 +26,19 @@ def wrap_nulls(func):
 def test_to_upper(string_col):
     plc_col = plc.interop.from_arrow(string_col)
     got = plc.strings.case.to_upper(plc_col)
-
-    @wrap_nulls
-    def to_upper(x):
-        return x.upper()
-
-    expected = pa.Array.from_pandas(string_col.to_pandas().apply(to_upper))
+    expected = pa.compute.utf8_upper(string_col)
     assert_column_eq(got, expected)
 
 
 def test_to_lower(string_col):
     plc_col = plc.interop.from_arrow(string_col)
     got = plc.strings.case.to_lower(plc_col)
-
-    @wrap_nulls
-    def to_lower(x):
-        return x.lower()
-
-    expected = pa.Array.from_pandas(string_col.to_pandas().apply(to_lower))
+    expected = pa.compute.utf8_lower(string_col)
     assert_column_eq(got, expected)
 
 
 def test_swapcase(string_col):
     plc_col = plc.interop.from_arrow(string_col)
     got = plc.strings.case.swapcase(plc_col)
-
-    @wrap_nulls
-    def swapcase(x):
-        return x.swapcase()
-
-    expected = pa.Array.from_pandas(string_col.to_pandas().apply(swapcase))
+    expected = pa.compute.utf8_swapcase(string_col)
     assert_column_eq(got, expected)
