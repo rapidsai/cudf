b8d841abec78140c8821c99346ad3f78d504e5c7
galipremsagar <sagarprem75@gmail.com>
2024-04-11 14:39:53 +0000

    fix version

--- ci/cudf_pandas_scripts/pandas-tests/run.sh
+++ ci/cudf_pandas_scripts/pandas-tests/run.sh
@@ -10,11 +10,14 @@ PANDAS_TESTS_BRANCH=${1}
 rapids-logger "Running Pandas tests using $PANDAS_TESTS_BRANCH branch"
 rapids-logger "PR number: ${RAPIDS_REF_NAME:-"unknown"}"
 
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+
 if [[ "${PANDAS_TESTS_BRANCH}" == "main" ]]; then
     git checkout branch-24.06
-    pip install cudf_cu${RAPIDS_CUDA_VERSION%%.*}
+    # pip install cudf_cu${RAPIDS_CUDA_VERSION%%.*}
+    pip install cudf_${RAPIDS_PY_CUDA_SUFFIX}-24.6.*
 else
-    RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+    
     RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-cudf-dep
     python -m pip install $(ls ./local-cudf-dep/cudf*.whl)[test,pandas-tests]
 fi
