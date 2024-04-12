5afc5e20970d8bd3321bdc2158d4c8f85035ac70
Bradley Dice <bdice@bradleydice.com>
2024-04-12 16:41:05 -0700

    Use std::equal.

--- cpp/src/groupby/groupby.cu
+++ cpp/src/groupby/groupby.cu
@@ -312,14 +312,14 @@ std::pair<std::unique_ptr<table>, std::unique_ptr<table>> groupby::shift(
   CUDF_FUNC_RANGE();
   CUDF_EXPECTS(values.num_columns() == static_cast<size_type>(fill_values.size()),
                "Mismatch number of fill_values and columns.");
-  CUDF_EXPECTS(std::all_of(thrust::make_counting_iterator(0),
-                           thrust::make_counting_iterator(values.num_columns()),
-                           [&](auto i) {
-                             return cudf::have_same_types(values.column(i), fill_values[i].get());
-                           }),
+  CUDF_EXPECTS(std::equal(values.begin(),
+                          values.end(),
+                          fill_values.begin(),
+                          [](auto const& col, auto const& scalar) {
+                            return cudf::have_same_types(col, scalar.get());
+                          }),
                "values and fill_value should have the same type.",
                cudf::data_type_error);
-
   auto stream = cudf::get_default_stream();
   std::vector<std::unique_ptr<column>> results;
   auto const& group_offsets = helper().group_offsets(stream);
