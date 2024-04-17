b8824433027841f733f2381d6d0fda3286faeb36
brandon-b-miller <brmiller@nvidia.com>
2024-04-17 09:38:47 -0500

    minor cleanup

--- python/cudf/udf_cpp/nrt/numba_cuda_runtime.cuh
+++ python/cudf/udf_cpp/nrt/numba_cuda_runtime.cuh
@@ -1,21 +1,20 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef _NRT_H
 #define _NRT_H
-
-#define CUDA_CHECK(fn)                                                               \
-  do {                                                                               \
-    CUresult status = (fn);                                                          \
-    if (CUDA_SUCCESS != status) {                                                    \
-      const char* errstr;                                                            \
-      cuGetErrorString(status, &errstr);                                             \
-      printf("CUDA Driver Failure (line %d of file %s):\n\t%s returned 0x%x (%s)\n", \
-             __LINE__,                                                               \
-             __FILE__,                                                               \
-             #fn,                                                                    \
-             status,                                                                 \
-             errstr);                                                                \
-      exit(EXIT_FAILURE);                                                            \
-    }                                                                                \
-  } while (0)
 
 #include <cuda/atomic>
 
