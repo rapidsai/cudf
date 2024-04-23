7a9c43671c29d6683287c90028afff6af42a1cdd
Nghia Truong <nghiat@nvidia.com>
2024-04-23 13:53:02 -0700

    Remove `reader.cu`
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- cpp/src/io/orc/reader_impl.cu
+++ cpp/src/io/orc/reader_impl.cu
@@ -201,4 +201,50 @@ table_with_metadata reader_impl::read_chunk()
   return make_output_chunk();
 }
 
+chunked_reader::chunked_reader(std::size_t output_size_limit,
+                               std::size_t data_read_limit,
+                               std::vector<std::unique_ptr<datasource>>&& sources,
+                               orc_reader_options const& options,
+                               rmm::cuda_stream_view stream,
+                               rmm::device_async_resource_ref mr)
+  : _impl{std::make_unique<reader_impl>(
+      output_size_limit, data_read_limit, std::move(sources), options, stream, mr)}
+{
+}
+
+chunked_reader::chunked_reader(std::size_t output_size_limit,
+                               std::size_t data_read_limit,
+                               size_type output_row_granularity,
+                               std::vector<std::unique_ptr<datasource>>&& sources,
+                               orc_reader_options const& options,
+                               rmm::cuda_stream_view stream,
+                               rmm::device_async_resource_ref mr)
+  : _impl{std::make_unique<reader_impl>(output_size_limit,
+                                        data_read_limit,
+                                        output_row_granularity,
+                                        std::move(sources),
+                                        options,
+                                        stream,
+                                        mr)}
+{
+}
+
+chunked_reader::~chunked_reader() = default;
+
+bool chunked_reader::has_next() const { return _impl->has_next(); }
+
+table_with_metadata chunked_reader::read_chunk() const { return _impl->read_chunk(); }
+
+reader::reader(std::vector<std::unique_ptr<cudf::io::datasource>>&& sources,
+               orc_reader_options const& options,
+               rmm::cuda_stream_view stream,
+               rmm::device_async_resource_ref mr)
+  : _impl{std::make_unique<reader_impl>(std::move(sources), options, stream, mr)}
+{
+}
+
+reader::~reader() = default;
+
+table_with_metadata reader::read() { return _impl->read(); }
+
 }  // namespace cudf::io::orc::detail
