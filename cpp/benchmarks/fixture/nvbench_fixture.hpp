a1a6d499d1f544d1f3d97850fcb8f6dddbc231f7
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2024-04-24 13:24:34 -0700

    diff pool options in the benchmarks

--- cpp/benchmarks/fixture/nvbench_fixture.hpp
+++ cpp/benchmarks/fixture/nvbench_fixture.hpp
@@ -26,6 +26,8 @@
 #include <rmm/mr/device/owning_wrapper.hpp>
 #include <rmm/mr/device/per_device_resource.hpp>
 #include <rmm/mr/device/pool_memory_resource.hpp>
+#include <rmm/mr/host/new_delete_resource.hpp>
+#include <rmm/mr/host/pinned_memory_resource.hpp>
 #include <rmm/mr/pinned_host_memory_resource.hpp>
 #include <rmm/resource_ref.hpp>
 
@@ -38,6 +40,107 @@ static std::string rmm_mode_param{"--rmm_mode"};  ///< RMM mode command-line par
 static std::string cuio_host_mem_param{
   "--cuio_host_mem"};  ///< cuio host memory mode parameter name
 }  // namespace detail
+
+using rmm::mr::device_memory_resource;
+using rmm_pinned_pool_t = rmm::mr::pool_memory_resource<rmm::mr::pinned_host_memory_resource>;
+
+inline rmm::mr::host_memory_resource* fallback_mr(bool fall_to_pinned)
+{
+  static rmm::mr::pinned_memory_resource pinned_mr{};
+  static rmm::mr::new_delete_resource new_delete_mr{};
+
+  if (fall_to_pinned) {
+    return &pinned_mr;
+  } else {
+    return &new_delete_mr;
+  }
+}
+
+class pinned_fallback_host_memory_resource {
+ private:
+  rmm_pinned_pool_t* _pool;
+  void* pool_begin_{};
+  void* pool_end_{};
+  size_t pool_size_{};
+  rmm::mr::host_memory_resource* fallback_mr_;
+
+ public:
+  pinned_fallback_host_memory_resource(rmm_pinned_pool_t* pool, bool is_fall_back_pinned)
+    : _pool(pool), pool_size_{pool->pool_size()}, fallback_mr_(fallback_mr(is_fall_back_pinned))
+  {
+    // allocate from the pinned pool the full size to figure out
+    // our beginning and end address.
+    if (pool_size_ != 0) {
+      pool_begin_ = pool->allocate(pool_size_);
+      pool_end_   = static_cast<void*>(static_cast<uint8_t*>(pool_begin_) + pool_size_);
+      pool->deallocate(pool_begin_, pool_size_);
+    }
+  }
+
+  void* allocate(std::size_t bytes,
+                 [[maybe_unused]] std::size_t alignment = rmm::RMM_DEFAULT_HOST_ALIGNMENT)
+  {
+    if (bytes <= pool_size_) {
+      try {
+        return _pool->allocate(bytes, alignment);
+      } catch (const std::exception& unused) {
+      }
+    }
+
+    // std::cout << "Falling back!\n";
+    return fallback_mr_->allocate(bytes, alignment);
+  }
+  void deallocate(void* ptr,
+                  std::size_t bytes,
+                  std::size_t alignment = rmm::RMM_DEFAULT_HOST_ALIGNMENT) noexcept
+  {
+    if (bytes <= pool_size_ && ptr >= pool_begin_ && ptr <= pool_end_) {
+      _pool->deallocate(ptr, bytes, alignment);
+    } else {
+      fallback_mr_->deallocate(ptr, bytes, alignment);
+    }
+  }
+
+  void* allocate_async(std::size_t bytes, [[maybe_unused]] cuda::stream_ref stream)
+  {
+    return allocate(bytes);
+  }
+
+  void* allocate_async(std::size_t bytes,
+                       std::size_t alignment,
+                       [[maybe_unused]] cuda::stream_ref stream)
+  {
+    return allocate(bytes, alignment);
+  }
+
+  void deallocate_async(void* ptr,
+                        std::size_t bytes,
+                        [[maybe_unused]] cuda::stream_ref stream) noexcept
+  {
+    return deallocate(ptr, bytes);
+  }
+
+  void deallocate_async(void* ptr,
+                        std::size_t bytes,
+                        std::size_t alignment,
+                        [[maybe_unused]] cuda::stream_ref stream) noexcept
+  {
+    return deallocate(ptr, bytes, alignment);
+  }
+  bool operator==(const pinned_fallback_host_memory_resource&) const { return true; }
+
+  bool operator!=(const pinned_fallback_host_memory_resource&) const { return false; }
+
+  friend void get_property(pinned_fallback_host_memory_resource const&,
+                           cuda::mr::device_accessible) noexcept
+  {
+  }
+
+  friend void get_property(pinned_fallback_host_memory_resource const&,
+                           cuda::mr::host_accessible) noexcept
+  {
+  }
+};
 
 /**
  * Base fixture for cudf benchmarks using nvbench.
@@ -90,7 +193,7 @@ struct nvbench_base_fixture {
     return *mr;
   }
 
-  inline rmm::host_async_resource_ref make_cuio_host_pinned_pool()
+  inline rmm::host_async_resource_ref make_cuio_host_pinned_pool_expandable()
   {
     if (!this->host_pooled_mr) {
       // Don't store in static, as the CUDA context may be destroyed before static destruction
@@ -102,11 +205,38 @@ struct nvbench_base_fixture {
     return *this->host_pooled_mr;
   }
 
+  inline rmm::host_async_resource_ref make_cuio_host_pinned_pool_pinned_fallback(
+    std::optional<size_t> size, bool fall_to_pinned)
+  {
+    using host_pooled_mr = rmm::mr::pool_memory_resource<rmm::mr::pinned_host_memory_resource>;
+
+    if (not size.has_value()) {
+      size = getenv("CUIO_PINNED_POOL_SIZE") ? atoi(getenv("CUIO_PINNED_POOL_SIZE"))
+                                             : size_t{1} * 1024 * 1024 * 1024;
+    }
+
+    auto pool_size = (size.value() + 255) & ~255;
+    std::cout << "Pinned host pool size = " << pool_size << "\n";
+
+    static std::shared_ptr<host_pooled_mr> pool_mr = std::make_shared<host_pooled_mr>(
+      std::make_shared<rmm::mr::pinned_host_memory_resource>().get(), pool_size, pool_size);
+
+    static std::shared_ptr<pinned_fallback_host_memory_resource> mr =
+      std::make_shared<pinned_fallback_host_memory_resource>(pool_mr.get(), fall_to_pinned);
+
+    return *mr;
+  }
+
   inline rmm::host_async_resource_ref create_cuio_host_memory_resource(std::string const& mode)
   {
-    if (mode == "pinned") return make_cuio_host_pinned();
-    if (mode == "pinned_pool") return make_cuio_host_pinned_pool();
-    CUDF_FAIL("Unknown cuio_host_mem parameter: " + mode + "\nExpecting: pinned or pinned_pool");
+    if (mode == "pageable") return make_cuio_host_pinned_pool_pinned_fallback(0, false);
+    if (mode == "pinned") return make_cuio_host_pinned_pool_pinned_fallback(0, true);
+    // if (mode == "pinned_pool_expand") return make_cuio_host_pinned_pool_expandable();
+    if (mode == "pinned_pool_fall_to_pageable")
+      return make_cuio_host_pinned_pool_pinned_fallback(std::nullopt, false);
+    if (mode == "pinned_pool_fall_to_pinned")
+      return make_cuio_host_pinned_pool_pinned_fallback(std::nullopt, true);
+    CUDF_FAIL("Unknown cuio_host_mem parameter: " + mode + "\n");
   }
 
   nvbench_base_fixture(int argc, char const* const* argv)
