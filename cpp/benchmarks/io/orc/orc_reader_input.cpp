ca15afce96eb91ef92db1ca4459dacfea5cffad6
Nghia Truong <nghiat@nvidia.com>
2024-04-25 20:35:56 -0700

    Change initialization style
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- cpp/benchmarks/io/orc/orc_reader_input.cpp
+++ cpp/benchmarks/io/orc/orc_reader_input.cpp
@@ -30,7 +30,7 @@ namespace {
 // run on most GPUs, but large enough to allow highest throughput
 constexpr int64_t data_size        = 512 << 20;
 constexpr cudf::size_type num_cols = 64;
-constexpr std::size_t Mbytes{1024 * 1024};
+constexpr std::size_t Mbytes       = 1024 * 1024;
 
 template <bool is_chunked_read>
 void orc_read_common(cudf::size_type num_rows_to_read,
