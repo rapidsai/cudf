b43ad02dbda743ed0ece0d7ccb28e392adcd625d
Michael Sarahan <msarahan@nvidia.com>
2024-04-26 11:34:43 -0500

    add auditwheel to libcudf wheels

--- ci/build_wheel_libcudf.sh
+++ ci/build_wheel_libcudf.sh
@@ -8,4 +8,9 @@ package_dir="python/libcudf"
 ./ci/build_wheel.sh libcudf ${package_dir}
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="libcudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 cpp ${package_dir}/dist
+
+cd ${package_dir}
+mkdir -p final_dist
+python -m auditwheel repair --exclude libarrow.so.1400 -w final_dist dist/*
+
+RAPIDS_PY_WHEEL_NAME="libcudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 cpp ${package_dir}/final_dist
