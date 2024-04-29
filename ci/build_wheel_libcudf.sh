d7dc523e9b7b703b01e872640a6fb96178a7a1d2
Michael Sarahan <msarahan@nvidia.com>
2024-04-29 13:44:21 -0500

    check vyas' nvcomp rapids-cmake fix

--- ci/build_wheel_libcudf.sh
+++ ci/build_wheel_libcudf.sh
@@ -9,8 +9,7 @@ package_dir="python/libcudf"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
-cd ${package_dir}
-mkdir -p final_dist
-python -m auditwheel repair --exclude libarrow.so.1400 -w final_dist dist/*
+mkdir -p ${package_dir}/final_dist
+python -m auditwheel repair --exclude libarrow.so.1400 -w ${package_dir}/final_dist ${package_dir}/dist/*
 
 RAPIDS_PY_WHEEL_NAME="libcudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 cpp ${package_dir}/final_dist
