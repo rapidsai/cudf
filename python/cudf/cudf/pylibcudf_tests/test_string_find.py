b27944a924b060b1fca0d67589c0c0e9124b2b22
brandon-b-miller <brmiller@nvidia.com>
2024-04-30 14:09:45 -0500

    fixturize the plc col instead

--- python/cudf/cudf/pylibcudf_tests/test_string_find.py
+++ python/cudf/cudf/pylibcudf_tests/test_string_find.py
@@ -10,26 +10,26 @@ from cudf._lib.scalar import DeviceScalar
 
 
 @pytest.fixture(scope="module")
-def string_col():
-    return pa.array(
+def plc_col():
+    data = pa.array(
         ["AbC", "de", "FGHI", "j", "kLm", "nOPq", None, "RsT", None, "uVw"]
     )
+    return plc.interop.from_arrow(data)
 
 
 @pytest.mark.parametrize("target", ["a", ""])
-def test_find(string_col, target):
-    plc_col = plc.interop.from_arrow(string_col)
+def test_find(plc_col, target):
     got = plc.strings.find.find(
         plc_col, DeviceScalar(target, dtype=np.dtype("object")).c_value, 0, -1
     )
     expected = pa.Array.from_pandas(
-        string_col.to_pandas().str.find(target), type=pa.int32()
+        plc.interop.to_arrow(plc_col).to_pandas().str.find(target),
+        type=pa.int32(),
     )
     assert_column_eq(got, expected)
 
 
-def test_find_column(string_col):
-    plc_col = plc.interop.from_arrow(string_col)
+def test_find_column(plc_col):
     target_col = plc.interop.from_arrow(
         pa.array(["A", "d", "F", "j", "k", "n", None, "R", None, "u"])
     )
@@ -39,31 +39,29 @@ def test_find_column(string_col):
 
 
 @pytest.mark.parametrize("target", ["a", ""])
-def test_rfind(string_col, target):
-    plc_col = plc.interop.from_arrow(string_col)
+def test_rfind(plc_col, target):
     got = plc.strings.find.rfind(
         plc_col, DeviceScalar(target, dtype=np.dtype("object")).c_value, 0, -1
     )
     expected = pa.Array.from_pandas(
-        string_col.to_pandas().str.rfind(target), type=pa.int32()
+        plc.interop.to_arrow(plc_col).to_pandas().str.rfind(target),
+        type=pa.int32(),
     )
     assert_column_eq(got, expected)
 
 
 @pytest.mark.parametrize("target", ["a", "aB", "Ab", ""])
-def test_contains(string_col, target):
-    plc_col = plc.interop.from_arrow(string_col)
+def test_contains(plc_col, target):
     got = plc.strings.find.contains(
         plc_col, DeviceScalar(target, dtype=np.dtype("object")).c_value
     )
     expected = pa.Array.from_pandas(
-        string_col.to_pandas().str.contains(target)
+        plc.interop.to_arrow(plc_col).to_pandas().str.contains(target)
     )
     assert_column_eq(got, expected)
 
 
-def test_contains_column(string_col):
-    plc_col = plc.interop.from_arrow(string_col)
+def test_contains_column(plc_col):
     target_col = plc.interop.from_arrow(
         pa.array(["a", "d", "F", "j", "m", "q", None, "R", None, "w"])
     )
@@ -75,17 +73,15 @@ def test_contains_column(string_col):
 
 
 @pytest.mark.parametrize("target", ["A", "", "Ab"])
-def test_starts_with(string_col, target):
-    plc_col = plc.interop.from_arrow(string_col)
+def test_starts_with(plc_col, target):
     got = plc.strings.find.starts_with(
         plc_col, DeviceScalar(target, dtype=np.dtype("object")).c_value
     )
-    expected = pa.compute.starts_with(string_col, target)
+    expected = pa.compute.starts_with(plc.interop.to_arrow(plc_col), target)
     assert_column_eq(got, expected)
 
 
-def test_starts_with_column(string_col):
-    plc_col = plc.interop.from_arrow(string_col)
+def test_starts_with_column(plc_col):
     target_col = plc.interop.from_arrow(
         pa.array(["A", "d", "F", "j", "k", "n", None, "R", None, "u"])
     )
@@ -97,17 +93,15 @@ def test_starts_with_column(string_col):
 
 
 @pytest.mark.parametrize("target", ["C", "bC", "BC", ""])
-def test_ends_with(string_col, target):
-    plc_col = plc.interop.from_arrow(string_col)
+def test_ends_with(plc_col, target):
     got = plc.strings.find.ends_with(
         plc_col, DeviceScalar(target, dtype=np.dtype("object")).c_value
     )
-    expected = pa.compute.ends_with(string_col, target)
+    expected = pa.compute.ends_with(plc.interop.to_arrow(plc_col), target)
     assert_column_eq(got, expected)
 
 
-def test_ends_with_column(string_col):
-    plc_col = plc.interop.from_arrow(string_col)
+def test_ends_with_column(plc_col):
     target_col = plc.interop.from_arrow(
         pa.array(["C", "e", "I", "j", "m", "q", None, "T", None, "w"])
     )
