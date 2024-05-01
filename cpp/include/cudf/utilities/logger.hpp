ff0ac5a54647e108aa6191df736e061bb17568ae
Vyas Ramasubramani <vyasr@nvidia.com>
2024-05-01 20:21:52 +0000

    Style

--- cpp/include/cudf/utilities/logger.hpp
+++ cpp/include/cudf/utilities/logger.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, NVIDIA CORPORATION.
+ * Copyright (c) 2023-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,6 @@ namespace cudf {
  *
  * @return spdlog::logger& The logger.
  */
-__attribute__((visibility("default")))
-spdlog::logger& logger();
+__attribute__((visibility("default"))) spdlog::logger& logger();
 
 }  // namespace cudf
