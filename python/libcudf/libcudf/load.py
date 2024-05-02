b8ae5c3d7a5f5ef2693bf952b9aadf10547f30ae
Vyas Ramasubramani <vyasr@nvidia.com>
2024-05-02 23:56:34 +0000

    Update library loading logic for the new nvcomp layout and make build compatible with devcontainers

--- python/libcudf/libcudf/load.py
+++ python/libcudf/libcudf/load.py
@@ -18,34 +18,34 @@ import os
 
 
 def load_library():
-    # this is loading the libarrow shared library
+    # This is loading the libarrow shared library in situations where it comes from the
+    # pyarrow package (i.e. when installed as a wheel).
     import pyarrow  # noqa: F401
 
     # Dynamically load libcudf.so. Prefer a system library if one is present to
     # avoid clobbering symbols that other packages might expect, but if no
     # other library is present use the one in the wheel.
+    libcudf_lib = None
     try:
         libcudf_lib = ctypes.CDLL("libcudf.so", ctypes.RTLD_GLOBAL)
     except OSError:
-        # load the nvcomp libraries from lib first, so that libcudf has them when we load it from lib64
-        # TODO: if we put both of these in the same folder, I don't think we'd need this.
-        for nvcomp_lib in (
-            "libnvcomp.so",
-            "libnvcomp_gdeflate.so",
-            "libnvcomp_bitcomp.so",
-        ):
-            _loaded_lib = ctypes.CDLL(
-                # TODO: Do we always know it will be lib64? Should we consider
-                # finding a way for CMake to export the path for us to find here?
-                os.path.join(os.path.dirname(__file__), "lib64", nvcomp_lib),
+        this_dir = os.path.dirname(__file__)
+        lib_dir = os.path.join(this_dir, "lib")
+        lib64_dir = os.path.join(this_dir, "lib64")
+        for real_lib_dir in (lib_dir, lib64_dir):
+            if os.path.isdir(real_lib_dir):
+                break
+        else:
+            real_lib_dir = None
+
+        # If neither directory was found in the wheel, we assume we are in an
+        # environment where the C++ library is already installed somewhere else and the
+        # CMake build of the libcudf Python package was a no-op.
+        if real_lib_dir is not None:
+            libcudf_lib = ctypes.CDLL(
+                os.path.join(real_lib_dir, "libcudf.so"),
                 ctypes.RTLD_GLOBAL,
             )
-        libcudf_lib = ctypes.CDLL(
-            # TODO: Do we always know it will be lib64? Should we consider
-            # finding a way for CMake to export the path for us to find here?
-            os.path.join(os.path.dirname(__file__), "lib64", "libcudf.so"),
-            ctypes.RTLD_GLOBAL,
-        )
 
     # The caller almost never needs to do anything with this library, but no
     # harm in offering the option since this object at least provides a handle
