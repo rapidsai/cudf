7c962ec752c94a1c1c8a0198a155c95b055e7fa7
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-05-02 17:32:02 -0700

    address read_json warning

--- python/dask_cudf/dask_cudf/io/tests/test_json.py
+++ python/dask_cudf/dask_cudf/io/tests/test_json.py
@@ -84,9 +84,9 @@ def test_read_json_nested(tmp_path):
         }
     )
     kwargs = dict(orient="records", lines=True)
-    with tmp_path / "data.json" as f, dask.config.set(
-        {"dataframe.convert-string": False}
-    ):
+    f = tmp_path / "data.json"
+    f.touch()
+    with dask.config.set({"dataframe.convert-string": False}):
         df.to_json(f, **kwargs)
         # Ensure engine='cudf' is tested.
         actual = dask_cudf.read_json(f, engine="cudf", **kwargs)
