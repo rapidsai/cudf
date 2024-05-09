dffb148580d4b336eaca8eba2cb83f851e0a8049
James Lamb <jlamb@nvidia.com>
2024-05-09 06:58:15 -0700

    remove cuda-python rewriting from scripts

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -26,16 +26,6 @@ source rapids-date-string
 version=$(rapids-generate-version)
 echo "${version}" > VERSION
 
-if rapids-is-release-build; then
-    export RAPIDS_ONLY_RELEASE_DEPS=1
-fi
-
-# Need to manually patch the cuda-python version for CUDA 12.
-ctk_major=$(echo "${RAPIDS_CUDA_VERSION}" | cut -d'.' -f1)
-if [[ ${ctk_major} == "12" ]]; then
-    sed -i "s/cuda-python[<=>\.,0-9a]*/cuda-python>=12.0,<13.0a0/g" ${package_dir}/pyproject.toml
-fi
-
 cd "${package_dir}"
 
 python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
