ffaafb94d8083543d233142e72d1ab8d112380be
sperlingxx <lovedreamf@gmail.com>
2024-05-10 09:46:39 +0800

    fix typo

--- java/src/test/java/ai/rapids/cudf/CudaTest.java
+++ java/src/test/java/ai/rapids/cudf/CudaTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2021-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 
 package ai.rapids.cudf;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
