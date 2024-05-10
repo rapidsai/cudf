3900120d4306b17971b783e801fe55e1a72a3f67
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2024-05-10 15:22:45 -0700

    Update comment for consistency
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- python/cudf/cudf/utils/ioutils.py
+++ python/cudf/cudf/utils/ioutils.py
@@ -277,7 +277,7 @@ max_page_size_bytes: integer or None, default None
 max_page_size_rows: integer or None, default None
     Maximum number of rows of each page of the output.
     If None, 20000 will be used.
-max_dictionary_size: integer or None. default None
+max_dictionary_size: integer or None, default None
     Maximum size of the dictionary page for each output column chunk. Dictionary
     encoding for column chunks that exceeds this limit will be disabled.
     If None, 1048576 (1MB) will be used.
