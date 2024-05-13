d4bd761438b7c4f6cef1219015580d1591bfd47a
James Lamb <jlamb@nvidia.com>
2024-05-13 09:46:58 -0700

    cudf_polars exists now

--- python/cudf_polars/cudf_polars/_version.py
+++ python/cudf_polars/cudf_polars/_version.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023-2024, NVIDIA CORPORATION.
+# Copyright (c) 2024, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
