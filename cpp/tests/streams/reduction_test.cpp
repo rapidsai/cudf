fe00ccf6e583fe9ee0dce81d8938c3782d9b840b
srinivasyadav18 <ssinga5@lsu.edu>
2024-05-14 19:03:51 +0000

    Add missing stream argument in tests

--- cpp/tests/streams/reduction_test.cpp
+++ cpp/tests/streams/reduction_test.cpp
@@ -35,10 +35,10 @@ TEST_F(ReductionTest, ReductionSum)
                cudf::test::get_default_stream());
 }
 
-TEST_F(ReductionTest, ReductionSumInit)
+TEST_F(ReductionTest, ReductionSumScalarInit)
 {
   cudf::test::fixed_width_column_wrapper<int> input({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-  auto const init_scalar = cudf::make_fixed_width_scalar<int>(3);
+  auto const init_scalar = cudf::make_fixed_width_scalar<int>(3, cudf::test::get_default_stream());
   cudf::reduce(input,
                *cudf::make_sum_aggregation<cudf::reduce_aggregation>(),
                cudf::data_type(cudf::type_id::INT32),
@@ -70,7 +70,7 @@ TEST_F(ReductionTest, SegmentedReductionSumScalarInit)
   auto const offsets   = std::vector<cudf::size_type>{0, 3, 6, 7, 8, 10, 10};
   auto const d_offsets = cudf::detail::make_device_uvector_async(
     offsets, cudf::test::get_default_stream(), rmm::mr::get_current_device_resource());
-  auto const init_scalar = cudf::make_fixed_width_scalar<int>(3);
+  auto const init_scalar = cudf::make_fixed_width_scalar<int>(3, cudf::test::get_default_stream());
   auto res =
     cudf::segmented_reduce(input,
                            d_offsets,
