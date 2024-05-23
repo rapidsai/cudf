456d1f9cbe7ab515dc4065a15819587912ac24a8
Robert Maynard <rmaynard@nvidia.com>
2024-05-23 12:59:16 -0400

    Use rapids_cpm_nvtx3 to get same nvtx3 target state as rmm

--- cpp/cmake/thirdparty/get_nvtx.cmake
+++ cpp/cmake/thirdparty/get_nvtx.cmake
@@ -12,16 +12,12 @@
 # the License.
 # =============================================================================
 
-# This function finds NVTX and sets any additional necessary environment variables.
 function(find_and_configure_nvtx)
-  rapids_cpm_find(
-    NVTX3 3.1.0
-    GLOBAL_TARGETS nvtx3-c nvtx3-cpp
-    CPM_ARGS
-    GIT_REPOSITORY https://github.com/NVIDIA/NVTX.git
-    GIT_TAG v3.1.0
-    GIT_SHALLOW TRUE SOURCE_SUBDIR c
-  )
+  include(${rapids-cmake-dir}/cpm/nvtx3.cmake)
+
+  # Find or install nvtx3
+  rapids_cpm_nvtx3(BUILD_EXPORT_SET cudf-exports INSTALL_EXPORT_SET cudf-exports)
+
 endfunction()
 
 find_and_configure_nvtx()
