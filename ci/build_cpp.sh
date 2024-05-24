d6114c886d256f309562bdb1ad1f8c8ce4bdab7a
James Lamb <jlamb@nvidia.com>
2024-05-24 07:04:37 -0700

    standardize rapids-build-backend constraint, revert build_cpp.sh changes

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -13,12 +13,12 @@ export CMAKE_GENERATOR=Ninja
 
 rapids-print-env
 
-rapids-generate-version > ./VERSION
+version=$(rapids-generate-version)
 
 rapids-logger "Begin cpp build"
 
 # With boa installed conda build forward to boa
-RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry mambabuild \
+RAPIDS_PACKAGE_VERSION=${version} rapids-conda-retry mambabuild \
     conda/recipes/libcudf
 
 rapids-upload-conda-to-s3 cpp
