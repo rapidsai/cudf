6a671335feab6dee0df7e04704c9a622ad4a5925
Thomas Li <thomasli1234567890@gmail.com>
2024-05-29 10:27:23 -0700

    update after feedback

--- python/cudf/cudf/_lib/round.pyx
+++ python/cudf/cudf/_lib/round.pyx
@@ -3,9 +3,9 @@
 from cudf.core.buffer import acquire_spill_lock
 
 from cudf._lib.column cimport Column
-from cudf._lib.pylibcudf.round cimport rounding_method
 
 import cudf._lib.pylibcudf as plc
+from cudf._lib.pylibcudf.round import RoundingMethod
 
 
 @acquire_spill_lock()
@@ -25,15 +25,15 @@ def round(Column input_col, int decimal_places=0, how="half_even"):
     if how not in {"half_even", "half_up"}:
         raise ValueError("'how' must be either 'half_even' or 'half_up'")
 
-    cdef rounding_method c_how = (
-        rounding_method.HALF_EVEN if how == "half_even"
-        else rounding_method.HALF_UP
+    how = (
+        RoundingMethod.HALF_EVEN if how == "half_even"
+        else RoundingMethod.HALF_UP
     )
 
     return Column.from_pylibcudf(
         plc.round.round(
             input_col.to_pylibcudf(mode="read"),
             decimal_places,
-            c_how
+            how
         )
     )
