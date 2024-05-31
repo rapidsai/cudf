5e6b22410c42e29aaa9c4214e96a3d72ab07d0be
David Wendt <dwendt@nvidia.com>
2024-05-31 10:40:30 -0400

    fix jni call to multi-replace

--- cpp/include/cudf/strings/replace.hpp
+++ cpp/include/cudf/strings/replace.hpp
@@ -160,6 +160,19 @@ std::unique_ptr<column> replace_multiple(
   rmm::cuda_stream_view stream      = cudf::get_default_stream(),
   rmm::device_async_resource_ref mr = rmm::mr::get_current_device_resource());
 
+/**
+ * @copydoc cudf::strings::replace(strings_column_view const&, strings_column_view const&,
+ * strings_column_view const&, rmm::cuda_stream_view, rmm::device_async_resource_ref)
+ *
+ * @deprecated since 24.08
+ */
+[[deprecated]] std::unique_ptr<column> replace(
+  strings_column_view const& input,
+  strings_column_view const& targets,
+  strings_column_view const& repls,
+  rmm::cuda_stream_view stream      = cudf::get_default_stream(),
+  rmm::device_async_resource_ref mr = rmm::mr::get_current_device_resource());
+
 /** @} */  // end of doxygen group
 }  // namespace strings
 }  // namespace cudf
