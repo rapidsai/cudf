c3a0bbff6ddaa03bce138f970ca3b30582f08b8c
Sebastian Berg <sebastianb@nvidia.com>
2024-05-31 14:02:49 +0200

    MAINT: Adapt to e.g. uint8(-1) failing now.  Mostly in where.
    Note that pandas `where` seems to promote the Series based on the
    value even with NumPy 2.  This was never copied by cudf (i.e. an
    outstanding issue)

--- python/cudf/cudf/core/_internals/where.py
+++ python/cudf/cudf/core/_internals/where.py
@@ -54,13 +54,17 @@ def _check_and_cast_columns_with_other(
 
     other_is_scalar = is_scalar(other)
     if other_is_scalar:
-        if (isinstance(other, float) and not np.isnan(other)) and (
-            source_dtype.type(other) != other
-        ):
-            raise TypeError(
-                f"Cannot safely cast non-equivalent "
-                f"{type(other).__name__} to {source_dtype.name}"
-            )
+        if isinstance(other, float) and not np.isnan(other):
+            try:
+                is_safe = source_dtype.type(other) == other
+            except OverflowError:
+                is_safe = False
+
+            if not is_safe:
+                raise TypeError(
+                    f"Cannot safely cast non-equivalent "
+                    f"{type(other).__name__} to {source_dtype.name}"
+                )
 
         if cudf.utils.utils.is_na_like(other):
             return _normalize_categorical(
