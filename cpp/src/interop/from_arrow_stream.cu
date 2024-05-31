04666b0e13a06bb4ebb2529bba9285e6b718e283
Vyas Ramasubramani <vyasr@nvidia.com>
2024-05-31 00:30:12 +0000

    Add basic handling for empty stream

--- cpp/src/interop/from_arrow_stream.cu
+++ cpp/src/interop/from_arrow_stream.cu
@@ -14,10 +14,13 @@
  * limitations under the License.
  */
 
-#include <cudf/interop.hpp>
-#include <cudf/table/table.hpp>
+#include "arrow_utilities.hpp"
+
+#include <cudf/column/column_factories.hpp>
 #include <cudf/concatenate.hpp>
 #include <cudf/detail/nvtx/ranges.hpp>
+#include <cudf/interop.hpp>
+#include <cudf/table/table.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/mr/device/device_memory_resource.hpp>
@@ -26,7 +29,6 @@
 #include <nanoarrow/nanoarrow.h>
 #include <nanoarrow/nanoarrow.hpp>
 
-#include <algorithm>
 #include <memory>
 #include <stdexcept>
 #include <utility>
@@ -35,15 +37,39 @@
 namespace cudf {
 namespace detail {
 
+namespace {
 
-std::unique_ptr<table> from_arrow_stream(
-  ArrowArrayStream* input,
-  rmm::cuda_stream_view stream,
-  rmm::mr::device_memory_resource* mr)
+std::unique_ptr<table> make_empty_table(ArrowSchema const& schema,
+                                        rmm::cuda_stream_view stream,
+                                        rmm::mr::device_memory_resource* mr)
 {
-  CUDF_EXPECTS(input != nullptr,
-               "input ArrowArrayStream must not be NULL",
-               std::invalid_argument);
+  if (schema.n_children == 0) {
+    // If there are no chunks but the schema has children, we need to construct a suitable empty
+    // table.
+    return std::make_unique<cudf::table>();
+  }
+
+  std::vector<std::unique_ptr<cudf::column>> columns;
+  for (int i = 0; i < schema.n_children; i++) {
+    ArrowSchema* child = schema.children[i];
+    CUDF_EXPECTS(child->n_children == 0,
+                 "Nested types in empty columns not yet supported",
+                 std::invalid_argument);
+    // If the child has children, we need to construct a suitable empty table.
+    ArrowSchemaView schema_view;
+    NANOARROW_THROW_NOT_OK(ArrowSchemaViewInit(&schema_view, child, nullptr));
+    columns.push_back(cudf::make_empty_column(arrow_to_cudf_type(&schema_view)));
+  }
+  return std::make_unique<cudf::table>(std::move(columns));
+}
+
+}  // namespace
+
+std::unique_ptr<table> from_arrow_stream(ArrowArrayStream* input,
+                                         rmm::cuda_stream_view stream,
+                                         rmm::mr::device_memory_resource* mr)
+{
+  CUDF_EXPECTS(input != nullptr, "input ArrowArrayStream must not be NULL", std::invalid_argument);
 
   // Potential future optimization: Since the from_arrow API accepts an
   // ArrowSchema we're allocating one here instead of using a view, which we
@@ -51,35 +77,31 @@ std::unique_ptr<table> from_arrow_stream(
   ArrowSchema schema;
   NANOARROW_THROW_NOT_OK(ArrowArrayStreamGetSchema(input, &schema, nullptr));
 
-  // Assume that each chunk is a column.
-  // TODO: Is that restrictive? If we need to support actual chunking of a
-  // single array, how do we differentiate? Do we need a parameter in the API,
-  // or a different overload?
-  ArrowArray chunk;
-
   std::vector<std::unique_ptr<cudf::table>> chunks;
+  ArrowArray chunk;
   while (true) {
     NANOARROW_THROW_NOT_OK(ArrowArrayStreamGetNext(input, &chunk, nullptr));
-    if (chunk.release == nullptr) {
-        break;
-    }
+    if (chunk.release == nullptr) { break; }
     chunks.push_back(from_arrow(&schema, &chunk, stream, mr));
   }
-
   input->release(input);
+
+  if (chunks.empty()) { return make_empty_table(schema, stream, mr); }
+
   auto chunk_views = std::vector<table_view>{};
   chunk_views.reserve(chunks.size());
-  std::transform(chunks.begin(), chunks.end(), std::back_inserter(chunk_views), [](auto const& chunk) { return chunk->view(); });
+  std::transform(
+    chunks.begin(), chunks.end(), std::back_inserter(chunk_views), [](auto const& chunk) {
+      return chunk->view();
+    });
   return cudf::concatenate(chunk_views, stream, mr);
 }
 
 }  // namespace detail
 
-
-std::unique_ptr<table> from_arrow_stream(
-  ArrowArrayStream* input,
-  rmm::cuda_stream_view stream,
-  rmm::mr::device_memory_resource* mr)
+std::unique_ptr<table> from_arrow_stream(ArrowArrayStream* input,
+                                         rmm::cuda_stream_view stream,
+                                         rmm::mr::device_memory_resource* mr)
 {
   CUDF_FUNC_RANGE();
 
