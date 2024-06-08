093ef5a51bd5919646d4b92ec4011e85fe14eec6
Thomas Li <thomasli1234567890@gmail.com>
2024-06-08 00:30:51 +0000

    Condense pylibcudf data fixtures

--- python/cudf/cudf/pylibcudf_tests/test_reshape.py
+++ python/cudf/cudf/pylibcudf_tests/test_reshape.py
@@ -10,20 +10,15 @@ from cudf._lib import pylibcudf as plc
 @pytest.fixture(scope="module")
 def reshape_data():
     data = [[1, 2, 3], [4, 5, 6]]
-    return data
+    arrow_tbl = pa.Table.from_arrays(data, names=["a", "b"])
+    return data, plc.interop.from_arrow(arrow_tbl)
 
 
-@pytest.fixture(scope="module")
-def reshape_plc_tbl(reshape_data):
-    arrow_tbl = pa.Table.from_arrays(reshape_data, names=["a", "b"])
-    plc_tbl = plc.interop.from_arrow(arrow_tbl)
-    return plc_tbl
-
-
-def test_interleave_columns(reshape_data, reshape_plc_tbl):
+def test_interleave_columns(reshape_data):
+    raw_data, reshape_plc_tbl = reshape_data
     res = plc.reshape.interleave_columns(reshape_plc_tbl)
 
-    interleaved_data = [pa.array(pair) for pair in zip(*reshape_data)]
+    interleaved_data = [pa.array(pair) for pair in zip(*raw_data)]
 
     expect = pa.concat_arrays(interleaved_data)
 
@@ -31,10 +26,11 @@ def test_interleave_columns(reshape_data, reshape_plc_tbl):
 
 
 @pytest.mark.parametrize("cnt", [0, 1, 3])
-def test_tile(reshape_data, reshape_plc_tbl, cnt):
+def test_tile(reshape_data, cnt):
+    raw_data, reshape_plc_tbl = reshape_data
     res = plc.reshape.tile(reshape_plc_tbl, cnt)
 
-    tiled_data = [pa.array(col * cnt) for col in reshape_data]
+    tiled_data = [pa.array(col * cnt) for col in raw_data]
 
     expect = pa.Table.from_arrays(
         tiled_data, schema=plc.interop.to_arrow(reshape_plc_tbl).schema
