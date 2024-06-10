c9a82d010a997d4c1f4afad94b36709e859d98fe
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2024-06-10 16:58:14 -0700

    Revert "docs; prefixes"
    This reverts commit 164fce20ad07632b5a9899668d9da7d23ced6b97.

--- cpp/src/utilities/pinned_memory.cpp
+++ cpp/src/utilities/pinned_memory.cpp
@@ -28,39 +28,34 @@
 namespace cudf {
 
 namespace {
-
-// Asynchronous memory resource that allocates a fixed-size pool of pinned memory and falls back to
-// additional pinned allocations if the pool is exhausted.
 class fixed_pinned_pool_memory_resource {
   using upstream_mr    = rmm::mr::pinned_host_memory_resource;
   using host_pooled_mr = rmm::mr::pool_memory_resource<upstream_mr>;
 
  private:
-  upstream_mr _upstream_mr{};
-  size_t _pool_size{0};
+  upstream_mr upstream_mr_{};
+  size_t pool_size_{0};
   // Raw pointer to avoid a segfault when the pool is destroyed on exit
   host_pooled_mr* pool_{nullptr};
-  // The beginning and end of the pool memory range; pool is never reallocated so these are constant
-  // and can be used to determine if a pointer is within the pool
-  void* _pool_begin{nullptr};
-  void* _pool_end{nullptr};
-  cuda::stream_ref _stream{cudf::detail::global_cuda_stream_pool().get_stream().value()};
+  void* pool_begin_{nullptr};
+  void* pool_end_{nullptr};
+  cuda::stream_ref stream_{cudf::detail::global_cuda_stream_pool().get_stream().value()};
 
  public:
   fixed_pinned_pool_memory_resource(size_t size)
-    : _pool_size{size}, pool_{new host_pooled_mr(_upstream_mr, size, size)}
+    : pool_size_{size}, pool_{new host_pooled_mr(upstream_mr_, size, size)}
   {
-    if (_pool_size == 0) { return; }
+    if (pool_size_ == 0) { return; }
 
     // Allocate full size from the pinned pool to figure out the beginning and end address
-    _pool_begin = pool_->allocate_async(_pool_size, _stream);
-    _pool_end   = static_cast<void*>(static_cast<uint8_t*>(_pool_begin) + _pool_size);
-    pool_->deallocate_async(_pool_begin, _pool_size, _stream);
+    pool_begin_ = pool_->allocate_async(pool_size_, stream_);
+    pool_end_   = static_cast<void*>(static_cast<uint8_t*>(pool_begin_) + pool_size_);
+    pool_->deallocate_async(pool_begin_, pool_size_, stream_);
   }
 
   void* allocate_async(std::size_t bytes, std::size_t alignment, cuda::stream_ref stream)
   {
-    if (bytes <= _pool_size) {
+    if (bytes <= pool_size_) {
       try {
         return pool_->allocate_async(bytes, alignment, stream);
       } catch (...) {
@@ -68,7 +63,7 @@ class fixed_pinned_pool_memory_resource {
       }
     }
 
-    return _upstream_mr.allocate_async(bytes, alignment, stream);
+    return upstream_mr_.allocate_async(bytes, alignment, stream);
   }
 
   void* allocate_async(std::size_t bytes, cuda::stream_ref stream)
@@ -78,8 +73,8 @@ class fixed_pinned_pool_memory_resource {
 
   void* allocate(std::size_t bytes, std::size_t alignment = rmm::RMM_DEFAULT_HOST_ALIGNMENT)
   {
-    auto const result = allocate_async(bytes, alignment, _stream);
-    _stream.wait();
+    auto const result = allocate_async(bytes, alignment, stream_);
+    stream_.wait();
     return result;
   }
 
@@ -88,10 +83,10 @@ class fixed_pinned_pool_memory_resource {
                         std::size_t alignment,
                         cuda::stream_ref stream) noexcept
   {
-    if (bytes <= _pool_size && ptr >= _pool_begin && ptr < _pool_end) {
+    if (bytes <= pool_size_ && ptr >= pool_begin_ && ptr < pool_end_) {
       pool_->deallocate_async(ptr, bytes, alignment, stream);
     } else {
-      _upstream_mr.deallocate_async(ptr, bytes, alignment, stream);
+      upstream_mr_.deallocate_async(ptr, bytes, alignment, stream);
     }
   }
 
@@ -104,13 +99,13 @@ class fixed_pinned_pool_memory_resource {
                   std::size_t bytes,
                   std::size_t alignment = rmm::RMM_DEFAULT_HOST_ALIGNMENT) noexcept
   {
-    deallocate_async(ptr, bytes, alignment, _stream);
-    _stream.wait();
+    deallocate_async(ptr, bytes, alignment, stream_);
+    stream_.wait();
   }
 
   bool operator==(fixed_pinned_pool_memory_resource const& other) const
   {
-    return pool_ == other.pool_ and _stream == other._stream;
+    return pool_ == other.pool_ and stream_ == other.stream_;
   }
 
   bool operator!=(fixed_pinned_pool_memory_resource const& other) const
