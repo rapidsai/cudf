bf13c9dbf7801290d1c09f73f6baa900d2088ec6
galipremsagar <sagarprem75@gmail.com>
2024-06-10 20:10:18 +0000

    revert pytest

--- python/cudf/cudf/tests/test_column.py
+++ python/cudf/cudf/tests/test_column.py
@@ -521,10 +521,8 @@ def test_concatenate_large_column_strings():
     s_1 = cudf.Series(["very long string " * string_scale_f] * num_strings)
     s_2 = cudf.Series(["very long string " * string_scale_f] * num_strings)
 
-    actual = cudf.concat([s_1, s_2])
-    expected = pd.concat([s_1.to_pandas(), s_2.to_pandas()])
-
-    assert_eq(actual, expected)
+    with pytest.raises(OverflowError):
+        cudf.concat([s_1, s_2])
 
 
 @pytest.mark.parametrize(
