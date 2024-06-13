52385d3211d14d198df2b449b32d06d506ca222d
Yunsong Wang <yunsongw@nvidia.com>
2024-06-13 14:30:21 -0700

    Fix the pool size alignment issue

--- cpp/src/utilities/pinned_memory.cpp
+++ cpp/src/utilities/pinned_memory.cpp
@@ -142,7 +142,7 @@ CUDF_EXPORT rmm::host_device_async_resource_ref& make_default_pinned_mr(
 
       auto const total = rmm::available_device_memory().second;
       // 0.5% of the total device memory, capped at 100MB
-      return std::min(total / 200, size_t{100} * 1024 * 1024);
+      return std::min(rmm::align_up(total / 200, rmm::CUDA_ALLOCATION_ALIGNMENT), size_t{100} * 1024 * 1024);
     }();
 
     // rmm requires the pool size to be a multiple of 256 bytes
