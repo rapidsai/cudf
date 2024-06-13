31b33b90430a4f2496fcf1a42778bcd8e070c87c
Lawrence Mitchell <lmitchell@nvidia.com>
2024-06-13 08:58:02 +0100

    Add tests of implemented StringFunctions (#16007)
    Additionally, assert that we raise during translation for an unhandled function.
    
    Authors:
      - Lawrence Mitchell (https://github.com/wence-)
    
    Approvers:
      - https://github.com/brandon-b-miller
    
    URL: https://github.com/rapidsai/cudf/pull/16007

--- python/cudf_polars/cudf_polars/dsl/expr.py
+++ python/cudf_polars/cudf_polars/dsl/expr.py
@@ -691,7 +691,9 @@ class StringFunction(Expr):
                 )
             )
         else:
-            raise NotImplementedError(f"StringFunction {self.name}")
+            raise NotImplementedError(
+                f"StringFunction {self.name}"
+            )  # pragma: no cover; handled by init raising
 
 
 class Sort(Expr):
