e19587d88e6f4042a2cd9dda4f6e3fa38d1a2f2f
Robert Maynard <rmaynard@nvidia.com>
2024-07-01 09:01:48 -0400

    Correct issues found by CI

--- cpp/include/cudf/table/table_view.hpp
+++ cpp/include/cudf/table/table_view.hpp
@@ -182,7 +182,10 @@ class table_view_base {
 };
 
 /**
- * @copydoc bool has_nested_columns(table_view const& table)
+ * @brief Determine if any nested columns exist in a given table.
+ *
+ * @param table The input table
+ * @return Whether nested columns exist in the input table
  */
 bool has_nested_columns(table_view const& table);
 
