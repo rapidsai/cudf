bbf40e247bbf4e40d37728f79e883ee11db6e04e
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-02 14:53:33 -0700

    Pass dataset path as an argument to tpch/run.sh

--- cpp/examples/tpch/run.sh
+++ cpp/examples/tpch/run.sh
@@ -2,6 +2,7 @@
 set -e
 
 query_no=$1
+dataset_path=$2
 
 if [ -z "$query_no" ]; then
   echo "Usage: $0 <query_no>"
@@ -12,4 +13,4 @@ fi
 export KVIKIO_COMPAT_MODE="on"
 export LIBCUDF_CUFILE_POLICY="KVIKIO"
 
-./tpch/build/tpch_q${query_no}
+./tpch/build/tpch_q${query_no} ${dataset_path}
