c0be3198c67ba26b96b1a60343ebc85899e6f50e
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-02 13:53:34 -0700

    Add script to run the benchmarks

---
+++ cpp/examples/tpch/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+query_no=$1
+
+if [ -z "$query_no" ]; then
+  echo "Usage: $0 <query_no>"
+  exit 1
+fi
+
+# Set up environment
+export KVIKIO_COMPAT_MODE="on"
+export LIBCUDF_CUFILE_POLICY="KVIKIO"
+
+./tpch/build/tpch_q${query_no}
