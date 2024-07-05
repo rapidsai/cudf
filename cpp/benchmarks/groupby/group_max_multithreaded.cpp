32181eb053404ed363639f399acba786f341171b
Srinivas Yadav Singanaboina <ssinganaboin@nvidia.com>
2024-07-05 17:44:59 +0000

    Add num_aggregations axis and remove multistream benchmark

--- cpp/benchmarks/groupby/group_max_multithreaded.cpp
+++ cpp/benchmarks/groupby/group_max_multithreaded.cpp
@@ -31,6 +31,7 @@ void bench_groupby_max_multithreaded(nvbench::state& state, nvbench::type_list<T
   auto const num_rows         = static_cast<cudf::size_type>(state.get_int64("num_rows"));
   auto const null_probability = state.get_float64("null_probability");
   auto const num_threads      = state.get_int64("num_threads");
+  auto const num_aggregations = state.get_int64("num_aggregations");
 
   auto const keys = [&] {
     data_profile const profile =
@@ -61,28 +62,31 @@ void bench_groupby_max_multithreaded(nvbench::state& state, nvbench::type_list<T
 
   std::vector<std::vector<cudf::groupby::aggregation_request>> requests(num_threads);
   for (int64_t i = 0; i < num_threads; i++) {
-    requests[i].emplace_back(cudf::groupby::aggregation_request());
-    requests[i][0].values = vals->view();
-    requests[i][0].aggregations.push_back(cudf::make_max_aggregation<cudf::groupby_aggregation>());
+    for (int64_t j = 0; j < num_aggregations; j++) {
+      requests[i].emplace_back(cudf::groupby::aggregation_request());
+      requests[i][j].values = vals->view();
+      requests[i][j].aggregations.push_back(
+        cudf::make_max_aggregation<cudf::groupby_aggregation>());
+    }
   }
 
   auto const mem_stats_logger = cudf::memory_stats_logger();
   state.exec(
     nvbench::exec_tag::sync | nvbench::exec_tag::timer, [&](nvbench::launch& launch, auto& timer) {
       auto perform_agg = [&](int64_t index) { gb_obj.aggregate(requests[index], streams[index]); };
-      threads.paused   = true;
       for (int64_t i = 0; i < num_threads; ++i) {
         threads.submit(perform_agg, i);
       }
       timer.start();
-      threads.paused = false;
       threads.wait_for_tasks();
       cudf::detail::join_streams(streams, cudf::get_default_stream());
       timer.stop();
     });
 
   auto const elapsed_time = state.get_summary("nv/cold/time/gpu/mean").get_float64("value");
-  state.add_element_count(static_cast<double>(num_rows) / elapsed_time / 1'000'000., "Mrows/s");
+  state.add_element_count(
+    static_cast<double>(num_rows * num_threads * num_aggregations) / elapsed_time / 1'000'000.,
+    "Mrows/s");
   state.add_buffer_size(
     mem_stats_logger.peak_memory_usage(), "peak_memory_usage", "peak_memory_usage");
 }
@@ -93,4 +97,5 @@ NVBENCH_BENCH_TYPES(bench_groupby_max_multithreaded,
   .add_int64_axis("cardinality", {0})
   .add_int64_power_of_two_axis("num_rows", {12, 18})
   .add_float64_axis("null_probability", {0, 0.1, 0.9})
+  .add_int64_axis("num_aggregations", {1, 2, 4, 8, 16, 32})
   .add_int64_axis("num_threads", {1, 2, 4, 8});
