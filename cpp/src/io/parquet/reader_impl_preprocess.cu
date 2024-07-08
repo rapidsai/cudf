1b9a47d346f522b957296fa6b99c0c4c2db389b3
Rahul Prabhu <raprabhu@nvidia.com>
2024-07-08 14:22:43 -0700

    Update branch

--- cpp/src/io/parquet/reader_impl_preprocess.cu
+++ cpp/src/io/parquet/reader_impl_preprocess.cu
@@ -1520,7 +1520,7 @@ void reader::impl::allocate_columns(read_mode mode, size_t skip_rows, size_t num
         // we're going to start null mask as all valid and then turn bits off if necessary
         out_buf.create_with_mask(
           out_buf.type.id() == type_id::LIST && l_idx < max_depth ? num_rows + 1 : num_rows,
-          cudf::mask_state::UNINITIALIZED,
+          cudf::mask_state::ALL_VALID,
           _stream,
           _mr);
         memset_bufs.push_back(cudf::device_span<uint8_t>((uint8_t *)(out_buf.data()), out_buf.data_size()));
@@ -1600,7 +1600,7 @@ void reader::impl::allocate_columns(read_mode mode, size_t skip_rows, size_t num
 
           // allocate
           // we're going to start null mask as all valid and then turn bits off if necessary
-          out_buf.create_with_mask(size, cudf::mask_state::UNINITIALIZED, _stream, _mr);
+          out_buf.create_with_mask(size, cudf::mask_state::ALL_VALID, _stream, _mr);
           memset_bufs.push_back(cudf::device_span<uint8_t>(static_cast<uint8_t *>(out_buf.data()), out_buf.data_size()));
           nullmask_bufs.push_back(cudf::device_span<uint8_t>((uint8_t *)(out_buf.null_mask()), out_buf.null_mask_size()));
 
@@ -1611,8 +1611,8 @@ void reader::impl::allocate_columns(read_mode mode, size_t skip_rows, size_t num
   // for (size_t i = 0; i < memset_bufs.size(); i++) {
   //   fprintf(stderr, "Buf %lu size: %lu \n", i, memset_bufs[i].size());
   // }
-  multibuffer_memset(memset_bufs, 0, _stream, _mr);
-  multibuffer_memset(nullmask_bufs, 0xFF, _stream, _mr);
+  //multibuffer_memset(memset_bufs, 0, _stream, _mr);
+  //multibuffer_memset(nullmask_bufs, 0xFF, _stream, _mr);
 }
 
 std::vector<size_t> reader::impl::calculate_page_string_offsets()
--- cpp/tests/utilities_tests/multibuffer_memset_tests.cpp
+++ cpp/tests/utilities_tests/multibuffer_memset_tests.cpp
@@ -9,21 +9,34 @@
 template <typename T>
 struct MultiBufferTestIntegral : public cudf::test::BaseFixture {};
 
-void memset_test()
-{
-  std::vector<std::string> filenames;
-  filenames.push_back({"/home/raprabhu/Downloads/part-09000-f1533775-48e7-498f-b06c-b3bb7baa2b7d-c000.zstd.parquet"});
+// void memset_test()
+// {
+// //   std::vector<std::string> filenames;
+// //   filenames.push_back({"/home/raprabhu/Downloads/part-09000-f1533775-48e7-498f-b06c-b3bb7baa2b7d-c000.zstd.parquet"});
 
-  //auto result = load_parquet_files(filenames);
-  auto in_opts =
-    cudf::io::parquet_reader_options::builder(cudf::io::source_info{filenames}).build();
-  auto result = cudf::io::read_parquet(in_opts);
-}
+// //   //auto result = load_parquet_files(filenames);
+// //   auto in_opts =
+// //     cudf::io::parquet_reader_options::builder(cudf::io::source_info{filenames}).build();
+// //   auto result = cudf::io::read_parquet(in_opts);
+//     srand(31337);
+//     auto expected = create_random_fixed_table<int>(2000, 500000, true);
 
-TEST(MultiBufferTestIntegral, Baseline) 
-{
-    memset_test();
-}
+//     auto filepath = temp_env->get_temp_filepath("memset.parquet");
+//     cudf::io::parquet_writer_options args =
+//       cudf::io::parquet_writer_options::builder(cudf::io::sink_info{filepath}, *expected);
+//     cudf::io::write_parquet(args);
+
+//     // attempt to read more rows than there actually are
+//     cudf::io::parquet_reader_options read_opts =
+//       cudf::io::parquet_reader_options::builder(cudf::io::source_info{filepath}).build();
+//     auto result = cudf::io::read_parquet(read_opts);
+
+// }
+
+// TEST(MultiBufferTestIntegral, Baseline) 
+// {
+//     memset_test();
+// }
 
 TEST(MultiBufferTestIntegral, BasicTest)
 {
