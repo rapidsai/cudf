3eeb95a28d5e6374c3a505ce193a3c03a3fdd7dd
Thomas Li <thomasli1234567890@gmail.com>
2024-07-08 21:37:09 +0000

    fix docs

--- python/cudf/cudf/utils/ioutils.py
+++ python/cudf/cudf/utils/ioutils.py
@@ -193,13 +193,13 @@ bytes_per_thread : int, default None
 skiprows : int, default None
     If not None, the number of rows to skip from the start of the file.
 
-    .. pandas-compat::
-    This option is not supported when pandas compatibility mode is on.
+    .. note::
+       This option is not supported when pandas compatibility mode is on.
 nrows : int, default None
     If not None, the total number of rows to read.
 
-    .. pandas-compat::
-    This option is not supported when pandas compatibility mode is on.
+    .. note:
+       This option is not supported when pandas compatibility mode is on.
 
 Returns
 -------
