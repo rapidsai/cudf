ecbb895a46f278ea22683c431b7107b2adc0a2eb
brandon-b-miller <brmiller@nvidia.com>
2024-07-10 06:21:21 -0700

    refactor again

--- python/cudf/cudf/pylibcudf_tests/test_binaryops.py
+++ python/cudf/cudf/pylibcudf_tests/test_binaryops.py
@@ -1,8 +1,6 @@
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
 
-from decimal import Decimal
-
 import numpy as np
 import pyarrow as pa
 import pytest
@@ -11,196 +9,239 @@ from utils import assert_column_eq
 from cudf._lib import pylibcudf as plc
 
 
-@pytest.fixture(params=[True, False])
+def idfn(param):
+    ltype, rtype, outtype = param
+    return f"{ltype}-{rtype}-{outtype}"
+
+
+@pytest.fixture(params=[True, False], ids=["nulls", "no_nulls"])
 def nulls(request):
     return request.param
 
 
 @pytest.fixture
-def pa_int_col(nulls):
-    return pa.array([1, 2, 3 if not nulls else None, 4, 5], type=pa.int32())
+def pa_data(request, nulls):
+    ltype, rtype, outtype = request.param
+    values = make_col(ltype, nulls), make_col(rtype, nulls), outtype
+    return values
 
 
 @pytest.fixture
-def plc_int_col(pa_int_col):
-    return plc.interop.from_arrow(pa_int_col)
-
-
-@pytest.fixture
-def pa_uint_col(nulls):
-    return pa.array([1, 2, 3 if not nulls else None, 4, 5], type=pa.uint32())
-
-
-@pytest.fixture
-def plc_uint_col(pa_uint_col):
-    return plc.interop.from_arrow(pa_uint_col)
-
-
-@pytest.fixture
-def pa_float_col(nulls):
-    return pa.array(
-        [1.0, 2.0, 3.0 if not nulls else None, 4.0, 5.0], type=pa.float32()
+def plc_data(pa_data):
+    lhs, rhs, outtype = pa_data
+    return (
+        plc.interop.from_arrow(lhs),
+        plc.interop.from_arrow(rhs),
+        plc.interop.from_arrow(pa.from_numpy_dtype(np.dtype(outtype))),
     )
 
 
-@pytest.fixture
-def plc_float_col(pa_float_col):
-    return plc.interop.from_arrow(pa_float_col)
-
-
-@pytest.fixture
-def pa_bool_col(nulls):
-    return pa.array(
-        [True, False, True if not nulls else None, False, True],
-        type=pa.bool_(),
-    )
-
-
-@pytest.fixture
-def plc_bool_col(pa_bool_col):
-    return plc.interop.from_arrow(pa_bool_col)
-
-
-@pytest.fixture
-def pa_timestamp_col(nulls):
-    return pa.array(
-        [
+def make_col(dtype, nulls):
+    if dtype == "int64":
+        data = [1, 2, 3, 4, 5]
+        pa_type = pa.int32()
+    elif dtype == "uint64":
+        data = [1, 2, 3, 4, 5]
+        pa_type = pa.uint32()
+    elif dtype == "float64":
+        data = [1.0, 2.0, 3.0, 4.0, 5.0]
+        pa_type = pa.float32()
+    elif dtype == "bool":
+        data = [True, False, True, False, True]
+        pa_type = pa.bool_()
+    elif dtype == "timestamp64[ns]":
+        data = [
             np.datetime64("2022-01-01"),
             np.datetime64("2022-01-02"),
-            np.datetime64("2022-01-03") if not nulls else None,
+            np.datetime64("2022-01-03"),
             np.datetime64("2022-01-04"),
             np.datetime64("2022-01-05"),
-        ],
-        type=pa.timestamp("ns"),
-    )
-
-
-@pytest.fixture
-def plc_timestamp_col(pa_timestamp_col):
-    return plc.interop.from_arrow(pa_timestamp_col)
-
-
-@pytest.fixture
-def pa_duration_col(nulls):
-    return pa.array(
-        [
+        ]
+        pa_type = pa.timestamp("ns")
+    elif dtype == "timedelta64[ns]":
+        data = [
             np.timedelta64(1, "ns"),
             np.timedelta64(2, "ns"),
-            np.timedelta64(3, "ns") if not nulls else None,
+            np.timedelta64(3, "ns"),
             np.timedelta64(4, "ns"),
             np.timedelta64(5, "ns"),
-        ],
-        type=pa.duration("ns"),
-    )
+        ]
+        pa_type = pa.duration("ns")
+    else:
+        raise ValueError("Unsupported dtype")
+
+    if nulls:
+        data[3] = None
+
+    return pa.array(data, type=pa_type)
 
 
-@pytest.fixture
-def plc_duration_col(pa_duration_col):
-    return plc.interop.from_arrow(pa_duration_col)
+def _test_binaryop_inner(pa_data, plc_data, pyop, plc_op):
+    lhs_py, rhs_py, outty_py = pa_data
+    lhs_plc, rhs_plc, outty_plc = plc_data
+
+    def get_result():
+        return plc.binaryop.binary_operation(
+            lhs_plc,
+            rhs_plc,
+            plc_op,
+            outty_plc,
+        )
+
+    if not plc.binaryop.is_supported_operation(
+        outty_plc, lhs_plc.type(), rhs_plc.type(), plc_op
+    ):
+        with pytest.raises(TypeError):
+            get_result()
+        return
+
+    expect = [
+        pyop(x, y) for x, y in zip(lhs_py.to_pylist(), rhs_py.to_pylist())
+    ]
+    expect = pa.array(expect, type=outty_py)
+    got = get_result()
+    assert_column_eq(expect, got)
 
 
-@pytest.fixture
-def pa_decimal_col(nulls):
-    return pa.array(
-        [
-            Decimal("1.23"),
-            Decimal("4.56"),
-            Decimal("7.89") if not nulls else None,
-            Decimal("0.12"),
-            Decimal("3.45"),
-        ],
-        type=pa.decimal128(9, 2),
-    )
+@pytest.mark.parametrize(
+    "pa_data",
+    [
+        ("int64", "int64", "int64"),
+        ("int64", "float64", "float64"),
+        ("int64", "int64", "datetime64[ns]"),
+    ],
+    indirect=True,
+    ids=idfn,
+)
+def test_add(pa_data, plc_data):
+    def add(x, y):
+        if x is None or y is None:
+            return None
+        return x + y
 
-
-@pytest.fixture
-def plc_decimal_col(pa_decimal_col):
-    return plc.interop.from_arrow(pa_decimal_col)
-
-
-def test_add(
-    pa_int_col, pa_float_col, plc_int_col, plc_float_col, plc_duration_col
-):
-    expect = pa.compute.add(pa_int_col, pa_float_col).cast(pa.int32())
-    got = plc.binaryop.binary_operation(
-        plc_int_col,
-        plc_float_col,
+    _test_binaryop_inner(
+        pa_data,
+        plc_data,
+        add,
         plc.binaryop.BinaryOperator.ADD,
-        plc.DataType(plc.TypeId.INT32),
     )
 
-    assert_column_eq(expect, got)
 
-    with pytest.raises(TypeError):
-        plc.binaryop.binary_operation(
-            plc_duration_col,
-            plc_duration_col,
-            plc.binaryop.BinaryOperator.ADD,
-            plc.DataType(plc.TypeId.INT32),
-        )
+@pytest.mark.parametrize(
+    "pa_data",
+    [("int64", "int64", "int64"), ("int64", "float64", "float64")],
+    indirect=True,
+    ids=idfn,
+)
+def test_sub(pa_data, plc_data):
+    def sub(x, y):
+        if x is None or y is None:
+            return None
+        return x - y
 
-
-def test_sub(
-    pa_int_col, pa_float_col, plc_int_col, plc_float_col, plc_duration_col
-):
-    expect = pa.compute.subtract(pa_int_col, pa_float_col).cast(pa.int32())
-    got = plc.binaryop.binary_operation(
-        plc_int_col,
-        plc_float_col,
+    _test_binaryop_inner(
+        pa_data,
+        plc_data,
+        sub,
         plc.binaryop.BinaryOperator.SUB,
-        plc.DataType(plc.TypeId.INT32),
     )
 
-    assert_column_eq(expect, got)
 
-    with pytest.raises(TypeError):
-        plc.binaryop.binary_operation(
-            plc_duration_col,
-            plc_duration_col,
-            plc.binaryop.BinaryOperator.SUB,
-            plc.DataType(plc.TypeId.INT32),
-        )
+@pytest.mark.parametrize(
+    "pa_data",
+    [("int64", "int64", "int64"), ("int64", "float64", "float64")],
+    indirect=True,
+    ids=idfn,
+)
+def test_mul(pa_data, plc_data):
+    def mul(x, y):
+        if x is None or y is None:
+            return None
+        return x * y
 
-
-def test_mul(
-    pa_int_col, pa_float_col, plc_int_col, plc_float_col, plc_duration_col
-):
-    expect = pa.compute.multiply(pa_int_col, pa_float_col).cast(pa.int32())
-    got = plc.binaryop.binary_operation(
-        plc_int_col,
-        plc_float_col,
+    _test_binaryop_inner(
+        pa_data,
+        plc_data,
+        mul,
         plc.binaryop.BinaryOperator.MUL,
-        plc.DataType(plc.TypeId.INT32),
     )
 
-    assert_column_eq(expect, got)
 
-    with pytest.raises(TypeError):
-        plc.binaryop.binary_operation(
-            plc_duration_col,
-            plc_duration_col,
-            plc.binaryop.BinaryOperator.MUL,
-            plc.DataType(plc.TypeId.INT32),
-        )
+@pytest.mark.parametrize(
+    "pa_data",
+    [("int64", "int64", "int64"), ("int64", "float64", "float64")],
+    indirect=True,
+    ids=idfn,
+)
+def test_div(pa_data, plc_data):
+    def div(x, y):
+        if x is None or y is None:
+            return None
+        return x / y
 
-
-def test_div(
-    pa_int_col, pa_float_col, plc_int_col, plc_float_col, plc_duration_col
-):
-    expect = pa.compute.divide(pa_int_col, pa_float_col).cast(pa.int32())
-    got = plc.binaryop.binary_operation(
-        plc_int_col,
-        plc_float_col,
+    _test_binaryop_inner(
+        pa_data,
+        plc_data,
+        div,
         plc.binaryop.BinaryOperator.DIV,
-        plc.DataType(plc.TypeId.INT32),
     )
 
-    assert_column_eq(expect, got)
 
-    with pytest.raises(TypeError):
-        plc.binaryop.binary_operation(
-            plc_float_col,
-            plc_duration_col,
-            plc.binaryop.BinaryOperator.DIV,
-            plc.DataType(plc.TypeId.INT32),
-        )
+@pytest.mark.parametrize(
+    "pa_data",
+    [("int64", "int64", "int64"), ("int64", "float64", "float64")],
+    indirect=True,
+    ids=idfn,
+)
+def test_floordiv(pa_data, plc_data):
+    def floordiv(x, y):
+        if x is None or y is None:
+            return None
+        return x // y
+
+    _test_binaryop_inner(
+        pa_data,
+        plc_data,
+        floordiv,
+        plc.binaryop.BinaryOperator.FLOOR_DIV,
+    )
+
+
+@pytest.mark.parametrize(
+    "pa_data",
+    [("int64", "int64", "int64"), ("int64", "float64", "float64")],
+    indirect=True,
+    ids=idfn,
+)
+def test_truediv(pa_data, plc_data):
+    def truediv(x, y):
+        if x is None or y is None:
+            return None
+        return x / y
+
+    _test_binaryop_inner(
+        pa_data,
+        plc_data,
+        truediv,
+        plc.binaryop.BinaryOperator.TRUE_DIV,
+    )
+
+
+@pytest.mark.parametrize(
+    "pa_data",
+    [("int64", "int64", "int64"), ("int64", "float64", "float64")],
+    indirect=True,
+    ids=idfn,
+)
+def test_mod(pa_data, plc_data):
+    def mod(x, y):
+        if x is None or y is None:
+            return None
+        return x % y
+
+    _test_binaryop_inner(
+        pa_data,
+        plc_data,
+        mod,
+        plc.binaryop.BinaryOperator.MOD,
+    )
