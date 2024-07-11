914b459509126ef96a17814b042b44e446d518c4
raybellwaves <rayjohnbell0@gmail.com>
2024-07-11 19:55:03 -0400

    add emw

--- python/cudf/cudf/core/dataframe.py
+++ python/cudf/cudf/core/dataframe.py
@@ -5451,7 +5451,7 @@ class DataFrame(IndexedFrame, Serializable, GetAttrGetItemMixin):
         .. pandas-compat::
             `pandas.DataFrame.from_arrow`
 
-            This method does not exit in pandas but it similar to
+            This method does not exit in pandas but it is simlar to
             how :meth:`pyarrow.Table.to_pandas` works for PyArrow Tables i.e.
             it does not support automatically setting index column(s).
         """
