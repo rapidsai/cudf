c7b6d564c58ea9bf017a73ffa7a6ad7010529fa9
galipremsagar <sagarprem75@gmail.com>
2024-07-23 21:19:18 +0000

    move enable prefecting order to after module is run

--- python/cudf/cudf/pandas/__init__.py
+++ python/cudf/cudf/pandas/__init__.py
@@ -7,8 +7,6 @@ import warnings
 
 import rmm.mr
 
-from cudf._lib import pylibcudf
-
 from .fast_slow_proxy import is_proxy_object
 from .magics import load_ipython_extension
 from .profiler import Profiler
