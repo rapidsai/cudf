f345e6176a7680fc29954d550026ba66d91d59b1
Rahul Prabhu <raprabhu@nvidia.com>
2024-07-23 11:18:54 -0700

    Added PR Fixes

--- cpp/tests/utilities_tests/multibuffer_memset_tests.cpp
+++ cpp/tests/utilities_tests/multibuffer_memset_tests.cpp
@@ -22,10 +22,10 @@
 
 #include <rmm/device_uvector.hpp>
 
+#include <thrust/iterator/counting_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
 
 #include <src/io/utilities/multibuffer_memset.hpp>
-#include <thrust/iterator/counting_iterator.h>
 
 #include <type_traits>
 
@@ -42,46 +42,41 @@ TEST(MultiBufferTestIntegral, BasicTest1)
   auto _mr    = rmm::mr::get_current_device_resource();
 
   // Init Host buffers to 0xEE and 0xFF as padding around it
-  std::vector<uint64_t *> host_bufs(NUM_BUFS, nullptr);
+  std::vector<uint64_t*> host_bufs(NUM_BUFS, nullptr);
   for (int i = 0; i < NUM_BUFS; i++) {
     CUDF_CUDA_TRY(cudaMalloc(&host_bufs[i], (BUF_SIZES[i] + 2000) * 8));
     CUDF_CUDA_TRY(cudaMemsetAsync(host_bufs[i], 0xFF, (BUF_SIZES[i] + 2000) * 8, stream.value()));
     CUDF_CUDA_TRY(cudaMemsetAsync(host_bufs[i] + 1000, 0xEE, BUF_SIZES[i] * 8, stream.value()));
   }
 
-
   // Init Device buffers
   std::vector<cudf::device_span<uint64_t>> device_bufs(NUM_BUFS);
-  std::transform(
-    thrust::make_counting_iterator(0L), 
-    thrust::make_counting_iterator(NUM_BUFS), 
-    device_bufs.begin(), 
-    [BUF_SIZES, host_bufs](auto i){
-        return cudf::device_span<uint64_t>(host_bufs[i] + 1000, BUF_SIZES[i]);
-    }
-  );
+  std::transform(thrust::make_counting_iterator(0L),
+                 thrust::make_counting_iterator(NUM_BUFS),
+                 device_bufs.begin(),
+                 [BUF_SIZES, host_bufs](auto i) {
+                   return cudf::device_span<uint64_t>(host_bufs[i] + 1000, BUF_SIZES[i]);
+                 });
 
   // Function Call
   multibuffer_memset(device_bufs, 0UL, stream, _mr);
 
   // Compare to see that only given buffers are zeroed out
-  std::for_each(
-    thrust::make_counting_iterator(0L), 
-    thrust::make_counting_iterator(NUM_BUFS), 
-    [BUF_SIZES, host_bufs, device_bufs](auto i){
-        std::vector<uint64_t> temp(BUF_SIZES[i] + 2000);
-        cudf::host_span<uint64_t> host(temp);
-        CUDF_CUDA_TRY(cudaMemcpy(host.data(), host_bufs[i], (BUF_SIZES[i] + 2000) * 8, cudaMemcpyDefault));
-        for (int j = 0; j < BUF_SIZES[i] + 2000; j++) {
-            if (j < 1000 || j >= BUF_SIZES[i] + 1000) {
-                EXPECT_EQ(host[j], 0xFFFFFFFFFFFFFFFF);
-            }
-            else {
-                EXPECT_EQ(host[j], 0UL);
-            }
-        }
-    }
-  );
+  std::for_each(thrust::make_counting_iterator(0L),
+                thrust::make_counting_iterator(NUM_BUFS),
+                [BUF_SIZES, host_bufs, device_bufs](auto i) {
+                  std::vector<uint64_t> temp(BUF_SIZES[i] + 2000);
+                  cudf::host_span<uint64_t> host(temp);
+                  CUDF_CUDA_TRY(cudaMemcpy(
+                    host.data(), host_bufs[i], (BUF_SIZES[i] + 2000) * 8, cudaMemcpyDefault));
+                  for (int j = 0; j < BUF_SIZES[i] + 2000; j++) {
+                    if (j < 1000 || j >= BUF_SIZES[i] + 1000) {
+                      EXPECT_EQ(host[j], 0xFFFFFFFFFFFFFFFF);
+                    } else {
+                      EXPECT_EQ(host[j], 0UL);
+                    }
+                  }
+                });
 }
 
 TEST(MultiBufferTestIntegral, BasicTest2)
@@ -94,46 +89,41 @@ TEST(MultiBufferTestIntegral, BasicTest2)
   auto _mr    = rmm::mr::get_current_device_resource();
 
   // Init Host buffers to 0xEE and 0xFF as padding around it
-  std::vector<uint64_t *> host_bufs(NUM_BUFS, nullptr);
+  std::vector<uint64_t*> host_bufs(NUM_BUFS, nullptr);
   for (int i = 0; i < NUM_BUFS; i++) {
     CUDF_CUDA_TRY(cudaMalloc(&host_bufs[i], (BUF_SIZES[i] + 2000) * 8));
     CUDF_CUDA_TRY(cudaMemsetAsync(host_bufs[i], 0xFF, (BUF_SIZES[i] + 2000) * 8, stream.value()));
     CUDF_CUDA_TRY(cudaMemsetAsync(host_bufs[i] + 1000, 0xEE, BUF_SIZES[i] * 8, stream.value()));
   }
 
-
   // Init Device buffers
   std::vector<cudf::device_span<uint64_t>> device_bufs(NUM_BUFS);
-  std::transform(
-    thrust::make_counting_iterator(0L), 
-    thrust::make_counting_iterator(NUM_BUFS), 
-    device_bufs.begin(), 
-    [BUF_SIZES, host_bufs](auto i){
-        return cudf::device_span<uint64_t>(host_bufs[i] + 1000, BUF_SIZES[i]);
-    }
-  );
+  std::transform(thrust::make_counting_iterator(0L),
+                 thrust::make_counting_iterator(NUM_BUFS),
+                 device_bufs.begin(),
+                 [BUF_SIZES, host_bufs](auto i) {
+                   return cudf::device_span<uint64_t>(host_bufs[i] + 1000, BUF_SIZES[i]);
+                 });
 
   // Function Call
   multibuffer_memset(device_bufs, 0UL, stream, _mr);
 
   // Compare to see that only given buffers are zeroed out
-  std::for_each(
-    thrust::make_counting_iterator(0L), 
-    thrust::make_counting_iterator(NUM_BUFS), 
-    [BUF_SIZES, host_bufs, device_bufs](auto i){
-        std::vector<uint64_t> temp(BUF_SIZES[i] + 2000);
-        cudf::host_span<uint64_t> host(temp);
-        CUDF_CUDA_TRY(cudaMemcpy(host.data(), host_bufs[i], (BUF_SIZES[i] + 2000) * 8, cudaMemcpyDefault));
-        for (int j = 0; j < BUF_SIZES[i] + 2000; j++) {
-            if (j < 1000 || j >= BUF_SIZES[i] + 1000) {
-                EXPECT_EQ(host[j], 0xFFFFFFFFFFFFFFFF);
-            }
-            else {
-                EXPECT_EQ(host[j], 0UL);
-            }
-        }
-    }
-  );
+  std::for_each(thrust::make_counting_iterator(0L),
+                thrust::make_counting_iterator(NUM_BUFS),
+                [BUF_SIZES, host_bufs, device_bufs](auto i) {
+                  std::vector<uint64_t> temp(BUF_SIZES[i] + 2000);
+                  cudf::host_span<uint64_t> host(temp);
+                  CUDF_CUDA_TRY(cudaMemcpy(
+                    host.data(), host_bufs[i], (BUF_SIZES[i] + 2000) * 8, cudaMemcpyDefault));
+                  for (int j = 0; j < BUF_SIZES[i] + 2000; j++) {
+                    if (j < 1000 || j >= BUF_SIZES[i] + 1000) {
+                      EXPECT_EQ(host[j], 0xFFFFFFFFFFFFFFFF);
+                    } else {
+                      EXPECT_EQ(host[j], 0UL);
+                    }
+                  }
+                });
 }
 
 TEST(MultiBufferTestIntegral, BasicTest3)
@@ -146,44 +136,39 @@ TEST(MultiBufferTestIntegral, BasicTest3)
   auto _mr    = rmm::mr::get_current_device_resource();
 
   // Init Host buffers to 0xEE and 0xFF as padding around it
-  std::vector<uint64_t *> host_bufs(NUM_BUFS, nullptr);
+  std::vector<uint64_t*> host_bufs(NUM_BUFS, nullptr);
   for (int i = 0; i < NUM_BUFS; i++) {
     CUDF_CUDA_TRY(cudaMalloc(&host_bufs[i], (BUF_SIZES[i] + 2000) * 8));
     CUDF_CUDA_TRY(cudaMemsetAsync(host_bufs[i], 0xFF, (BUF_SIZES[i] + 2000) * 8, stream.value()));
     CUDF_CUDA_TRY(cudaMemsetAsync(host_bufs[i] + 1000, 0xEE, BUF_SIZES[i] * 8, stream.value()));
   }
 
-
   // Init Device buffers
   std::vector<cudf::device_span<uint64_t>> device_bufs(NUM_BUFS);
-  std::transform(
-    thrust::make_counting_iterator(0L), 
-    thrust::make_counting_iterator(NUM_BUFS), 
-    device_bufs.begin(), 
-    [BUF_SIZES, host_bufs](auto i){
-        return cudf::device_span<uint64_t>(host_bufs[i] + 1000, BUF_SIZES[i]);
-    }
-  );
+  std::transform(thrust::make_counting_iterator(0L),
+                 thrust::make_counting_iterator(NUM_BUFS),
+                 device_bufs.begin(),
+                 [BUF_SIZES, host_bufs](auto i) {
+                   return cudf::device_span<uint64_t>(host_bufs[i] + 1000, BUF_SIZES[i]);
+                 });
 
   // Function Call
   multibuffer_memset(device_bufs, 0UL, stream, _mr);
 
   // Compare to see that only given buffers are zeroed out
-  std::for_each(
-    thrust::make_counting_iterator(0L), 
-    thrust::make_counting_iterator(NUM_BUFS), 
-    [BUF_SIZES, host_bufs, device_bufs](auto i){
-        std::vector<uint64_t> temp(BUF_SIZES[i] + 2000);
-        cudf::host_span<uint64_t> host(temp);
-        CUDF_CUDA_TRY(cudaMemcpy(host.data(), host_bufs[i], (BUF_SIZES[i] + 2000) * 8, cudaMemcpyDefault));
-        for (int j = 0; j < BUF_SIZES[i] + 2000; j++) {
-            if (j < 1000 || j >= BUF_SIZES[i] + 1000) {
-                EXPECT_EQ(host[j], 0xFFFFFFFFFFFFFFFF);
-            }
-            else {
-                EXPECT_EQ(host[j], 0UL);
-            }
-        }
-    }
-  );
+  std::for_each(thrust::make_counting_iterator(0L),
+                thrust::make_counting_iterator(NUM_BUFS),
+                [BUF_SIZES, host_bufs, device_bufs](auto i) {
+                  std::vector<uint64_t> temp(BUF_SIZES[i] + 2000);
+                  cudf::host_span<uint64_t> host(temp);
+                  CUDF_CUDA_TRY(cudaMemcpy(
+                    host.data(), host_bufs[i], (BUF_SIZES[i] + 2000) * 8, cudaMemcpyDefault));
+                  for (int j = 0; j < BUF_SIZES[i] + 2000; j++) {
+                    if (j < 1000 || j >= BUF_SIZES[i] + 1000) {
+                      EXPECT_EQ(host[j], 0xFFFFFFFFFFFFFFFF);
+                    } else {
+                      EXPECT_EQ(host[j], 0UL);
+                    }
+                  }
+                });
 }
