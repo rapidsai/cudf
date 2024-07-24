cad780c4158cd9540d76ec595b580274aa669359
galipremsagar <sagarprem75@gmail.com>
2024-07-24 21:29:46 +0000

    reduce parallelism

--- python/cudf/cudf/pandas/scripts/run-pandas-tests.sh
+++ python/cudf/cudf/pandas/scripts/run-pandas-tests.sh
@@ -137,7 +137,7 @@ and not test_eof_states \
 and not test_array_tz"
 
 # TODO: Remove "not db" once a postgres & mysql container is set up on the CI
-PANDAS_CI="1" timeout 30m python -m pytest -p cudf.pandas \
+PANDAS_CI="1" timeout 300m python -m pytest -p cudf.pandas \
     -v -m "not single_cpu and not db" \
     -k "$TEST_THAT_NEED_MOTO_SERVER and $TEST_THAT_CRASH_PYTEST_WORKERS and not test_groupby_raises_category_on_category and not test_constructor_no_pandas_array and not test_is_monotonic_na and not test_index_contains and not test_index_contains and not test_frame_op_subclass_nonclass_constructor and not test_round_trip_current" \
     --import-mode=importlib \
