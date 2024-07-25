0f7a03ee3e3029ce972bb7636d3b8995453d52b1
JayjeetAtGithub <JayjeetAtGithub@users.noreply.github.com>
2024-07-25 07:54:27 -0700

    Generate a mask based on predicate

--- cpp/benchmarks/common/cudf_datagen/utils.hpp
+++ cpp/benchmarks/common/cudf_datagen/utils.hpp
@@ -20,6 +20,7 @@
 #include <cudf/join.hpp>
 #include <cudf/table/table.hpp>
 
+#include <ctime>
 #include <vector>
 
 void write_parquet(cudf::table_view tbl,
@@ -68,4 +69,37 @@ std::unique_ptr<cudf::table> perform_left_join(cudf::table_view const& left_inpu
                      std::make_move_iterator(right_cols.begin()),
                      std::make_move_iterator(right_cols.end()));
   return std::make_unique<cudf::table>(std::move(joined_cols));
+}
+
+/**
+ * @brief Generate the `std::tm` structure from year, month, and day
+ *
+ * @param year The year
+ * @param month The month
+ * @param day The day
+ */
+std::tm make_tm(int year, int month, int day)
+{
+  std::tm tm{};
+  tm.tm_year = year - 1900;
+  tm.tm_mon  = month - 1;
+  tm.tm_mday = day;
+  return tm;
+}
+
+/**
+ * @brief Calculate the number of days since the UNIX epoch
+ *
+ * @param year The year
+ * @param month The month
+ * @param day The day
+ */
+int32_t days_since_epoch(int year, int month, int day)
+{
+  std::tm tm             = make_tm(year, month, day);
+  std::tm epoch          = make_tm(1970, 1, 1);
+  std::time_t time       = std::mktime(&tm);
+  std::time_t epoch_time = std::mktime(&epoch);
+  double diff            = std::difftime(time, epoch_time) / (60 * 60 * 24);
+  return static_cast<int32_t>(diff);
 }
