adf9c8ac0899f2c682a433f177b49e12f9e192b7
Muhammad Haseeb <mhaseeb@nvidia.com>
2024-07-26 20:24:28 +0000

    Add const to child_idx

--- cpp/src/io/parquet/reader_impl_helpers.cpp
+++ cpp/src/io/parquet/reader_impl_helpers.cpp
@@ -1224,7 +1224,7 @@ aggregate_reader_metadata::select_columns(
         std::for_each(
           thrust::make_counting_iterator(0),
           thrust::make_counting_iterator(src_schema_elem.num_children),
-          [&](auto child_idx) {
+          [&](auto const child_idx) {
             // Check the number of children to be equal here.
             CUDF_EXPECTS(
               src_schema_elem.num_children == dst_schema_elem.num_children,
