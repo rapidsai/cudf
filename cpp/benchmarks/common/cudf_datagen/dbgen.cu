345a8270d8403650dba1045f0fa7a21ea98a3d0d
JayjeetAtGithub <JayjeetAtGithub@users.noreply.github.com>
2024-07-26 18:34:41 -0700

    Extracted table helper functions into table_helpers.hpp

--- cpp/benchmarks/common/cudf_datagen/dbgen.cu
+++ cpp/benchmarks/common/cudf_datagen/dbgen.cu
@@ -13,109 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "schema.hpp"
+#include "table_helpers.hpp"
 #include "utils.hpp"
 #include "vocab.hpp"
 
-std::unique_ptr<cudf::column> l_calc_suppkey(cudf::column_view const& l_partkey,
-                                             int64_t const& scale_factor,
-                                             int64_t const& num_rows)
-{
-  // Generating the `s` col
-  auto s_empty = cudf::make_numeric_column(cudf::data_type{cudf::type_id::INT64},
-                                           num_rows,
-                                           cudf::mask_state::UNALLOCATED,
-                                           cudf::get_default_stream());
-
-  auto s =
-    cudf::fill(s_empty->view(), 0, num_rows, cudf::numeric_scalar<int64_t>(10000 * scale_factor));
-
-  // Generating the `i` col
-  auto i = gen_rep_seq_col(4, num_rows);
-
-  // Create a table view out of `l_partkey`, `s`, and `i`
-  auto table = cudf::table_view({l_partkey, s->view(), i->view()});
-
-  // Create the AST expression
-  auto scalar_1  = cudf::numeric_scalar<int64_t>(1);
-  auto scalar_4  = cudf::numeric_scalar<int64_t>(4);
-  auto literal_1 = cudf::ast::literal(scalar_1);
-  auto literal_4 = cudf::ast::literal(scalar_4);
-
-  auto l_partkey_col_ref = cudf::ast::column_reference(0);
-  auto s_col_ref         = cudf::ast::column_reference(1);
-  auto i_col_ref         = cudf::ast::column_reference(2);
-
-  // (int)(l_partkey - 1)/s
-  auto expr_a = cudf::ast::operation(cudf::ast::ast_operator::SUB, l_partkey_col_ref, literal_1);
-  auto expr_b = cudf::ast::operation(cudf::ast::ast_operator::DIV, expr_a, s_col_ref);
-  auto expr_b_casted = cudf::ast::operation(cudf::ast::ast_operator::CAST_TO_INT64, expr_b);
-
-  // s/4
-  auto expr_c = cudf::ast::operation(cudf::ast::ast_operator::DIV, s_col_ref, literal_4);
-
-  // (s/4 + (int)(l_partkey - 1)/s)
-  auto expr_d = cudf::ast::operation(cudf::ast::ast_operator::ADD, expr_c, expr_b_casted);
-
-  // (i * (s/4 + (int)(l_partkey - 1)/s))
-  auto expr_e = cudf::ast::operation(cudf::ast::ast_operator::MUL, i_col_ref, expr_d);
-
-  // (l_partkey + (i * (s/4 + (int)(l_partkey - 1)/s)))
-  auto expr_f = cudf::ast::operation(cudf::ast::ast_operator::ADD, l_partkey_col_ref, expr_e);
-
-  // (l_partkey + (i * (s/4 + (int)(l_partkey - 1)/s))) % s
-  auto expr_g = cudf::ast::operation(cudf::ast::ast_operator::MOD, expr_f, s_col_ref);
-
-  // (l_partkey + (i * (s/4 + (int)(l_partkey - 1)/s))) % s + 1
-  auto final_expr = cudf::ast::operation(cudf::ast::ast_operator::ADD, expr_g, literal_1);
-
-  // Execute the AST expression
-  auto l_suppkey = cudf::compute_column(table, final_expr);
-  return l_suppkey;
-}
-
-int64_t l_calc_cardinality(cudf::column_view const& o_orderkey_repeat_freqs)
-{
-  auto const sum_agg = cudf::make_sum_aggregation<cudf::reduce_aggregation>();
-  auto const l_num_rows_scalar =
-    cudf::reduce(o_orderkey_repeat_freqs, *sum_agg, cudf::data_type{cudf::type_id::INT64});
-  return reinterpret_cast<cudf::numeric_scalar<int64_t>*>(l_num_rows_scalar.get())->value();
-}
-
-/**
- * @brief Calculate the charge column
- *
- * @param tax The tax column
- * @param disc_price The discount price column
- * @param stream The CUDA stream used for device memory operations and kernel launches.
- * @param mr Device memory resource used to allocate the returned column's device memory.
- */
-[[nodiscard]] std::unique_ptr<cudf::column> l_calc_charge(
-  cudf::column_view const& extendedprice,
-  cudf::column_view const& tax,
-  cudf::column_view const& discount,
-  rmm::cuda_stream_view stream      = cudf::get_default_stream(),
-  rmm::device_async_resource_ref mr = rmm::mr::get_current_device_resource())
-{
-  auto const one = cudf::numeric_scalar<double>(1);
-  auto const one_minus_discount =
-    cudf::binary_operation(one, discount, cudf::binary_operator::SUB, discount.type(), stream, mr);
-  auto const disc_price_type = cudf::data_type{cudf::type_id::FLOAT64};
-  auto disc_price            = cudf::binary_operation(extendedprice,
-                                           one_minus_discount->view(),
-                                           cudf::binary_operator::MUL,
-                                           disc_price_type,
-                                           stream,
-                                           mr);
-  auto const one_plus_tax =
-    cudf::binary_operation(one, tax, cudf::binary_operator::ADD, tax.type(), stream, mr);
-  auto const charge_type = cudf::data_type{cudf::type_id::FLOAT64};
-  auto charge            = cudf::binary_operation(
-    disc_price->view(), one_plus_tax->view(), cudf::binary_operator::MUL, charge_type, stream, mr);
-  return charge;
-}
-
-// NOTE: Incomplete table
 void generate_lineitem_and_orders(
   int64_t scale_factor,
   rmm::cuda_stream_view stream      = cudf::get_default_stream(),
@@ -200,7 +103,7 @@ void generate_lineitem_and_orders(
   // Sum up the `o_orderkey_repeat_freqs` to get the number of rows in the
   // `lineitem` table. This is required to generate the independent columns
   // in the `lineitem` table
-  auto const l_num_rows = l_calc_cardinality(o_orderkey_repeat_freqs->view());
+  auto const l_num_rows = calc_l_cardinality(o_orderkey_repeat_freqs->view());
 
   // We create a column, `l_pkey` which will contain the repeated primary keys,
   // `_o_pkey` of the `orders` table as per the frequencies in `o_orderkey_repeat_freqs`
@@ -225,7 +128,7 @@ void generate_lineitem_and_orders(
   auto const l_partkey = gen_rand_num_col<int64_t>(1, 200'000 * scale_factor, l_num_rows);
 
   // Generate the `l_suppkey` column
-  auto const l_suppkey = l_calc_suppkey(l_partkey->view(), scale_factor, l_num_rows);
+  auto const l_suppkey = calc_l_suppkey(l_partkey->view(), scale_factor, l_num_rows);
 
   // Generate the `l_linenumber` column
   auto l_linenumber = gen_rep_seq_col(7, l_num_rows);
@@ -427,63 +330,6 @@ void generate_lineitem_and_orders(
   write_parquet(lineitem, "lineitem.parquet", schema_lineitem);
 }
 
-std::unique_ptr<cudf::column> ps_calc_suppkey(cudf::column_view const& ps_partkey,
-                                              int64_t const& scale_factor,
-                                              int64_t const& num_rows)
-{
-  // Generating the `s` col
-  auto s_empty = cudf::make_numeric_column(cudf::data_type{cudf::type_id::INT64},
-                                           num_rows,
-                                           cudf::mask_state::UNALLOCATED,
-                                           cudf::get_default_stream());
-
-  auto s =
-    cudf::fill(s_empty->view(), 0, num_rows, cudf::numeric_scalar<int64_t>(10000 * scale_factor));
-
-  // Generating the `i` col
-  auto i = gen_rep_seq_col(4, num_rows);
-
-  // Create a table view out of `p_partkey`, `s`, and `i`
-  auto table = cudf::table_view({ps_partkey, s->view(), i->view()});
-
-  // Create the AST expression
-  auto scalar_1  = cudf::numeric_scalar<int64_t>(1);
-  auto scalar_4  = cudf::numeric_scalar<int64_t>(4);
-  auto literal_1 = cudf::ast::literal(scalar_1);
-  auto literal_4 = cudf::ast::literal(scalar_4);
-
-  auto ps_partkey_col_ref = cudf::ast::column_reference(0);
-  auto s_col_ref          = cudf::ast::column_reference(1);
-  auto i_col_ref          = cudf::ast::column_reference(2);
-
-  // (int)(ps_partkey - 1)/s
-  auto expr_a = cudf::ast::operation(cudf::ast::ast_operator::SUB, ps_partkey_col_ref, literal_1);
-  auto expr_b = cudf::ast::operation(cudf::ast::ast_operator::DIV, expr_a, s_col_ref);
-  auto expr_b_casted = cudf::ast::operation(cudf::ast::ast_operator::CAST_TO_INT64, expr_b);
-
-  // s/4
-  auto expr_c = cudf::ast::operation(cudf::ast::ast_operator::DIV, s_col_ref, literal_4);
-
-  // (s/4 + (int)(ps_partkey - 1)/s)
-  auto expr_d = cudf::ast::operation(cudf::ast::ast_operator::ADD, expr_c, expr_b_casted);
-
-  // (i * (s/4 + (int)(ps_partkey - 1)/s))
-  auto expr_e = cudf::ast::operation(cudf::ast::ast_operator::MUL, i_col_ref, expr_d);
-
-  // (ps_partkey + (i * (s/4 + (int)(ps_partkey - 1)/s)))
-  auto expr_f = cudf::ast::operation(cudf::ast::ast_operator::ADD, ps_partkey_col_ref, expr_e);
-
-  // (ps_partkey + (i * (s/4 + (int)(ps_partkey - 1)/s))) % s
-  auto expr_g = cudf::ast::operation(cudf::ast::ast_operator::MOD, expr_f, s_col_ref);
-
-  // (ps_partkey + (i * (s/4 + (int)(ps_partkey - 1)/s))) % s + 1
-  auto final_expr = cudf::ast::operation(cudf::ast::ast_operator::ADD, expr_g, literal_1);
-
-  // Execute the AST expression
-  auto ps_suppkey = cudf::compute_column(table, final_expr);
-  return ps_suppkey;
-}
-
 /**
  * @brief Generate the `partsupp` table
  *
@@ -509,7 +355,7 @@ void generate_partsupp(int64_t const& scale_factor,
   auto const ps_partkey = rep_table->get_column(0);
 
   // Generate the `ps_suppkey` column
-  auto const ps_suppkey = ps_calc_suppkey(ps_partkey.view(), scale_factor, num_rows);
+  auto const ps_suppkey = calc_ps_suppkey(ps_partkey.view(), scale_factor, num_rows, stream, mr);
 
   // Generate the `p_availqty` column
   auto const ps_availqty = gen_rand_num_col<int64_t>(1, 9999, num_rows, stream, mr);
