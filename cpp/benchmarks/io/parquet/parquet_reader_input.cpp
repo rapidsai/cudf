9f6c5bf5e62514693a23976afec55b69e4f7957c
Rahul Prabhu <raprabhu@nvidia.com>
2024-07-31 10:02:46 -0700

    Initial Commit

--- cpp/benchmarks/io/parquet/parquet_reader_input.cpp
+++ cpp/benchmarks/io/parquet/parquet_reader_input.cpp
@@ -182,8 +182,9 @@ void BM_parquet_read_chunks(nvbench::state& state, nvbench::type_list<nvbench::e
   auto const d_type      = get_type_or_group(static_cast<int32_t>(DataType));
   auto const cardinality = static_cast<cudf::size_type>(state.get_int64("cardinality"));
   auto const run_length  = static_cast<cudf::size_type>(state.get_int64("run_length"));
-  auto const byte_limit  = static_cast<cudf::size_type>(state.get_int64("byte_limit"));
+  auto const chunk_read_limit  = static_cast<cudf::size_type>(state.get_int64("byte_limit"));
   auto const source_type = retrieve_io_type_enum(state.get_string("io_type"));
+  auto const data_size = static_cast<size_t>(state.get_int64("data_size"));
   auto const compression = cudf::io::compression_type::SNAPPY;
   cuio_source_sink_pair source_sink(source_type);
 
@@ -212,7 +213,64 @@ void BM_parquet_read_chunks(nvbench::state& state, nvbench::type_list<nvbench::e
       try_drop_l3_cache();
 
       timer.start();
-      auto reader                   = cudf::io::chunked_parquet_reader(byte_limit, read_opts);
+      auto reader                   = cudf::io::chunked_parquet_reader(chunk_read_limit, read_opts);
+      cudf::size_type num_rows_read = 0;
+      do {
+        auto const result = reader.read_chunk();
+        num_rows_read += result.tbl->num_rows();
+        CUDF_EXPECTS(result.tbl->num_columns() == num_cols, "Unexpected number of columns");
+      } while (reader.has_next());
+      timer.stop();
+
+      CUDF_EXPECTS(num_rows_read == num_rows_written, "Benchmark did not read the entire table");
+    });
+
+  auto const time = state.get_summary("nv/cold/time/gpu/mean").get_float64("value");
+  state.add_element_count(static_cast<double>(data_size) / time, "bytes_per_second");
+  state.add_buffer_size(
+    mem_stats_logger.peak_memory_usage(), "peak_memory_usage", "peak_memory_usage");
+  state.add_buffer_size(source_sink.size(), "encoded_file_size", "encoded_file_size");
+}
+
+template <data_type DataType>
+void BM_parquet_read_subrowgroup_chunks(nvbench::state& state, nvbench::type_list<nvbench::enum_type<DataType>>)
+{
+  auto const d_type      = get_type_or_group(static_cast<int32_t>(DataType));
+  auto const cardinality = static_cast<cudf::size_type>(state.get_int64("cardinality"));
+  auto const run_length  = static_cast<cudf::size_type>(state.get_int64("run_length"));
+  auto const chunk_read_limit  = static_cast<cudf::size_type>(state.get_int64("chunk_read_limit"));
+  auto const pass_read_limit  = static_cast<cudf::size_type>(state.get_int64("pass_read_limit"));
+  auto const source_type = retrieve_io_type_enum(state.get_string("io_type"));
+  auto const data_size = static_cast<size_t>(state.get_int64("data_size"));
+  auto const compression = cudf::io::compression_type::SNAPPY;
+  cuio_source_sink_pair source_sink(source_type);
+
+  auto const num_rows_written = [&]() {
+    auto const tbl = create_random_table(
+      cycle_dtypes(d_type, num_cols),
+      table_size_bytes{data_size},
+      data_profile_builder().cardinality(cardinality).avg_run_length(run_length));
+    auto const view = tbl->view();
+
+    cudf::io::parquet_writer_options write_opts =
+      cudf::io::parquet_writer_options::builder(source_sink.make_sink_info(), view)
+        .compression(compression);
+
+    cudf::io::write_parquet(write_opts);
+    return view.num_rows();
+  }();
+
+  cudf::io::parquet_reader_options read_opts =
+    cudf::io::parquet_reader_options::builder(source_sink.make_source_info());
+
+  auto mem_stats_logger = cudf::memory_stats_logger();
+  state.set_cuda_stream(nvbench::make_cuda_stream_view(cudf::get_default_stream().value()));
+  state.exec(
+    nvbench::exec_tag::sync | nvbench::exec_tag::timer, [&](nvbench::launch& launch, auto& timer) {
+      try_drop_l3_cache();
+
+      timer.start();
+      auto reader                   = cudf::io::chunked_parquet_reader(chunk_read_limit, pass_read_limit, read_opts);
       cudf::size_type num_rows_read = 0;
       do {
         auto const result = reader.read_chunk();
@@ -262,7 +320,18 @@ NVBENCH_BENCH_TYPES(BM_parquet_read_chunks, NVBENCH_TYPE_AXES(d_type_list))
   .set_min_samples(4)
   .add_int64_axis("cardinality", {0, 1000})
   .add_int64_axis("run_length", {1, 32})
-  .add_int64_axis("byte_limit", {0, 500'000});
+  .add_int64_axis("chunk_read_limit", {0, 500'000})
+  .add_int64_axis("data_size", {512 << 20});
+
+NVBENCH_BENCH_TYPES(BM_parquet_read_subrowgroup_chunks, NVBENCH_TYPE_AXES(d_type_list))
+  .set_name("parquet_read_chunks")
+  .add_string_axis("io_type", {"DEVICE_BUFFER"})
+  .set_min_samples(4)
+  .add_int64_axis("cardinality", {0, 1000})
+  .add_int64_axis("run_length", {1, 32})
+  .add_int64_axis("chunk_read_limit", {0, 500'000})
+  .add_int64_axis("pass_read_limit", {0, 500'000})
+  .add_int64_axis("data_size", {512 << 20});
 
 NVBENCH_BENCH(BM_parquet_read_io_small_mixed)
   .set_name("parquet_read_io_small_mixed")
