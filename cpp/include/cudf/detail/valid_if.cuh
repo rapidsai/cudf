5549de69f553c16903a82ca962578b3db47f3b9d
Nghia Truong <nghiat@nvidia.com>
2024-08-02 16:27:37 -0700

    Implement `valid_if_async`
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- cpp/include/cudf/detail/valid_if.cuh
+++ cpp/include/cudf/detail/valid_if.cuh
@@ -42,11 +42,11 @@ namespace detail {
  * @param[in] begin The beginning of the sequence of elements
  * @param[in] size The number of elements
  * @param[in] p The predicate to apply to each element
- * @param[out] valid_count The count of set bits in the output bitmask
+ * @param[out] null_count The count of unset bits in the output bitmask
  */
 template <size_type block_size, typename InputIterator, typename Predicate>
 CUDF_KERNEL void valid_if_kernel(
-  bitmask_type* output, InputIterator begin, size_type size, Predicate p, size_type* valid_count)
+  bitmask_type* output, InputIterator begin, size_type size, Predicate p, size_type* null_count)
 {
   constexpr size_type leader_lane{0};
   auto const lane_id{threadIdx.x % warp_size};
@@ -65,8 +65,51 @@ CUDF_KERNEL void valid_if_kernel(
     active_mask = __ballot_sync(active_mask, i < size);
   }
 
-  size_type block_count = single_lane_block_sum_reduce<block_size, leader_lane>(warp_valid_count);
-  if (threadIdx.x == 0) { atomicAdd(valid_count, block_count); }
+  size_type block_valid_count =
+    single_lane_block_sum_reduce<block_size, leader_lane>(warp_valid_count);
+  if (threadIdx.x == 0) { atomicSub(null_count, block_valid_count); }
+}
+
+/**
+ * @brief Generate a bitmask where every bit is set for which a predicate is
+ * `true` over the elements in `[begin,end)`.
+ *
+ * Bit `i` in the output mask will be set if `p(*(begin+i)) == true`.
+ *
+ * If `distance(begin,end) == 0`, returns an empty `rmm::device_buffer`.
+ *
+ * @throws cudf::logic_error if `(begin > end)`
+ *
+ * @param begin The beginning of the sequence
+ * @param end The end of the sequence
+ * @param p The predicate
+ * @param stream CUDA stream used for device memory operations and kernel launches
+ * @return A pair containing a `device_buffer` with the new bitmask and a `device_scalar`
+ *         storing null count
+ */
+template <typename InputIterator, typename Predicate>
+std::pair<rmm::device_buffer, rmm::device_scalar<size_type>> valid_if_async(
+  InputIterator begin,
+  InputIterator end,
+  Predicate p,
+  rmm::cuda_stream_view stream,
+  rmm::device_async_resource_ref mr)
+{
+  CUDF_EXPECTS(begin <= end, "Invalid range.");
+
+  size_type size = thrust::distance(begin, end);
+
+  auto null_mask  = cudf::create_null_mask(size, mask_state::UNINITIALIZED, stream, mr);
+  auto null_count = rmm::device_scalar<size_type>{size, stream};
+
+  if (size > 0) {
+    constexpr size_type block_size{256};
+    grid_1d grid{size, block_size};
+
+    valid_if_kernel<block_size><<<grid.num_blocks, grid.num_threads_per_block, 0, stream.value()>>>(
+      static_cast<bitmask_type*>(null_mask.data()), begin, size, p, null_count.data());
+  }
+  return std::pair(std::move(null_mask), std::move(null_count));
 }
 
 /**
@@ -93,25 +136,8 @@ std::pair<rmm::device_buffer, size_type> valid_if(InputIterator begin,
                                                   rmm::cuda_stream_view stream,
                                                   rmm::device_async_resource_ref mr)
 {
-  CUDF_EXPECTS(begin <= end, "Invalid range.");
-
-  size_type size = thrust::distance(begin, end);
-
-  auto null_mask = cudf::create_null_mask(size, mask_state::UNINITIALIZED, stream, mr);
-
-  size_type null_count{0};
-  if (size > 0) {
-    rmm::device_scalar<size_type> valid_count{0, stream};
-
-    constexpr size_type block_size{256};
-    grid_1d grid{size, block_size};
-
-    valid_if_kernel<block_size><<<grid.num_blocks, grid.num_threads_per_block, 0, stream.value()>>>(
-      static_cast<bitmask_type*>(null_mask.data()), begin, size, p, valid_count.data());
-
-    null_count = size - valid_count.value(stream);
-  }
-  return std::pair(std::move(null_mask), null_count);
+  auto [null_mask, null_count] = valid_if_async(begin, end, p, stream, mr);
+  return std::pair(std::move(null_mask), null_count.value(stream));
 }
 
 /**
