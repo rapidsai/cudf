2a7a0ef9b3baffab40a86e28b56325fff69f34aa
JayjeetAtGithub <JayjeetAtGithub@users.noreply.github.com>
2024-08-02 11:16:49 -0700

    Add transform stream tests

--- cpp/src/interop/to_arrow_host.cu
+++ cpp/src/interop/to_arrow_host.cu
@@ -147,7 +147,7 @@ int dispatch_to_arrow_host::operator()<bool>(ArrowArray* out) const
   NANOARROW_RETURN_NOT_OK(initialize_array(tmp.get(), NANOARROW_TYPE_BOOL, column));
 
   NANOARROW_RETURN_NOT_OK(populate_validity_bitmap(ArrowArrayValidityBitmap(tmp.get())));
-  auto bitmask = bools_to_mask(column, stream, mr);
+  auto bitmask = detail::bools_to_mask(column, stream, mr);
   NANOARROW_RETURN_NOT_OK(populate_data_buffer(
     device_span<uint8_t const>(reinterpret_cast<const uint8_t*>(bitmask.first->data()),
                                bitmask.first->size()),
--- cpp/tests/streams/transform_test.cpp
+++ cpp/tests/streams/transform_test.cpp
@@ -15,13 +15,65 @@
  */
 
 #include <cudf_test/base_fixture.hpp>
+#include <cudf_test/column_utilities.hpp>
 #include <cudf_test/column_wrapper.hpp>
 #include <cudf_test/default_stream.hpp>
+#include <cudf_test/type_lists.hpp>
 
 #include <cudf/ast/expressions.hpp>
+#include <cudf/column/column.hpp>
+#include <cudf/column/column_factories.hpp>
+#include <cudf/column/column_view.hpp>
+#include <cudf/detail/utilities/integer_utils.hpp>
 #include <cudf/transform.hpp>
+#include <cudf/types.hpp>
+
+#include <rmm/exec_policy.hpp>
+
+#include <cuda/functional>
+#include <thrust/fill.h>
+#include <thrust/functional.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/reduce.h>
+#include <thrust/tabulate.h>
+#include <thrust/transform.h>
+
+#include <numeric>
 
 class TransformTest : public cudf::test::BaseFixture {};
+
+// Compute row bit count, then sum up sizes for each segment of rows.
+std::pair<std::unique_ptr<cudf::column>, std::unique_ptr<cudf::column>>
+compute_segmented_row_bit_count(cudf::table_view const& input, cudf::size_type segment_length)
+{
+  // The expected values are computed with the assumption that
+  // the outputs of `cudf::row_bit_count` are correct.
+  // This should be fine as they are verified by their own unit tests in `row_bit_count_test.cu`.
+  auto const row_sizes    = cudf::row_bit_count(input);
+  auto const num_segments = cudf::util::div_rounding_up_safe(row_sizes->size(), segment_length);
+  auto expected =
+    cudf::make_fixed_width_column(cudf::data_type{cudf::type_id::INT32}, num_segments);
+
+  thrust::transform(
+    rmm::exec_policy(cudf::get_default_stream()),
+    thrust::make_counting_iterator(0),
+    thrust::make_counting_iterator(num_segments),
+    expected->mutable_view().begin<cudf::size_type>(),
+    cuda::proclaim_return_type<cudf::size_type>(
+      [segment_length,
+       num_segments,
+       num_rows = row_sizes->size(),
+       d_sizes  = row_sizes->view().begin<cudf::size_type>()] __device__(auto const segment_idx) {
+        // Since the number of rows may not divisible by segment_length,
+        // the last segment may be shorter than the others.
+        auto const size_begin = d_sizes + segment_idx * segment_length;
+        auto const size_end   = std::min(size_begin + segment_length, d_sizes + num_rows);
+        return thrust::reduce(thrust::seq, size_begin, size_end);
+      }));
+
+  auto actual = cudf::segmented_row_bit_count(input, segment_length);
+  return {std::move(expected), std::move(actual)};
+}
 
 TEST_F(TransformTest, ComputeColumn)
 {
@@ -37,4 +89,113 @@ TEST_F(TransformTest, ComputeColumn)
   auto result   = cudf::compute_column(table, expression, cudf::test::get_default_stream());
 
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, result->view());
+}
+
+TEST_F(TransformTest, BoolsToMask)
+{
+  std::vector<bool> input({1, 0, 1, 0, 1, 0, 1, 0});
+  std::vector<bool> val({1, 1, 1, 1, 1, 1, 0, 1});
+
+  cudf::test::fixed_width_column_wrapper<bool> input_column(input.begin(), input.end());
+
+  auto sample = cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i; });
+  cudf::test::fixed_width_column_wrapper<int32_t> expected(
+    sample, sample + input.size(), input.begin());
+
+  auto [null_mask, null_count] = cudf::bools_to_mask(input_column, cudf::get_default_stream());
+  cudf::column got_column(expected);
+  got_column.set_null_mask(std::move(*null_mask), null_count);
+
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, got_column.view());
+}
+
+TEST_F(TransformTest, OneHotEncode)
+{
+  auto input    = cudf::test::fixed_width_column_wrapper<int32_t>{8, 8, 8, 9, 9};
+  auto category = cudf::test::fixed_width_column_wrapper<int32_t>{8, 9};
+
+  auto col0 = cudf::test::fixed_width_column_wrapper<bool>{1, 1, 1, 0, 0};
+  auto col1 = cudf::test::fixed_width_column_wrapper<bool>{0, 0, 0, 1, 1};
+
+  auto expected = cudf::table_view{{col0, col1}};
+
+  [[maybe_unused]] auto [res_ptr, got] =
+    cudf::one_hot_encode(input, category, cudf::test::get_default_stream());
+
+  CUDF_TEST_EXPECT_TABLES_EQUAL(expected, got);
+}
+
+TEST_F(TransformTest, NaNsToNulls)
+{
+  std::vector<int32_t> input = {1, 2, 3, 4, 5};
+
+  auto input_column = cudf::test::fixed_width_column_wrapper<int32_t>(input.begin(), input.end());
+  auto expected_column = [&]() {
+    std::vector<int32_t> expected(input);
+    std::vector<bool> expected_mask;
+
+    if (mask.size() > 0) {
+      std::transform(input.begin(),
+                     input.end(),
+                     mask.begin(),
+                     std::back_inserter(expected_mask),
+                     [](int32_t val, bool validity) { return validity and not std::isnan(val); });
+    } else {
+      std::transform(
+        input.begin(), input.end(), std::back_inserter(expected_mask), [](int32_t val) {
+          return not std::isnan(val);
+        });
+    }
+
+    return cudf::test::fixed_width_column_wrapper<int32_t>(
+             expected.begin(), expected.end(), expected_mask.begin())
+      .release();
+  }();
+
+  auto [null_mask, null_count] =
+    cudf::nans_to_nulls(input_column, cudf::test::get_default_stream());
+  cudf::column got(input_column);
+  got.set_null_mask(std::move(*null_mask), null_count);
+
+  EXPECT_EQ(expected_column.null_count(), null_count);
+
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected_column, got.view());
+}
+
+TEST_F(TransformTest, RowBitCount)
+{
+  std::vector<std::string> strings{"abc", "ï", "", "z", "bananas", "warp", "", "zing"};
+
+  cudf::test::strings_column_wrapper col(strings.begin(), strings.end());
+
+  cudf::table_view t({col});
+  auto result = cudf::row_bit_count(t, cudf::test::get_default_stream());
+
+  auto size_iter = cudf::detail::make_counting_transform_iterator(0, [&strings](int i) {
+    return (static_cast<cudf::size_type>(strings[i].size()) + sizeof(cudf::size_type)) * CHAR_BIT;
+  });
+  cudf::test::fixed_width_column_wrapper<cudf::size_type> expected(size_iter,
+                                                                   size_iter + strings.size());
+
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *result);
+}
+
+TEST_F(TransformTest, SegmentedRowBitCount)
+{
+  // clang-format off
+  std::vector<std::string> const strings { "daïs", "def", "", "z", "bananas", "warp", "", "zing" };
+  std::vector<bool>        const valids  {  1,      0,    0,  1,   0,          1,      1,  1 };
+  // clang-format on
+  cudf::test::strings_column_wrapper const col(strings.begin(), strings.end(), valids.begin());
+  auto const input = cudf::table_view({col});
+
+  auto constexpr segment_length = 2;
+  auto const [expected, actual] = compute_segmented_row_bit_count(input, segment_length);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(*expected, *actual);
+}
+
+TEST_F(TransformTest, Unary)
+{
+  cudf::test::fixed_width_column_wrapper<int32_t> const column{10, 20, 30, 40, 50};
+  cudf::cast(column, cudf::data_type{cudf::type_id::INT64}, cudf::test::get_default_stream());
 }
