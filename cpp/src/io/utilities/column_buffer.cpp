4d0497bfabfbdc72bb9a2e193ac4efce0e2a793a
Rahul Prabhu <raprabhu@nvidia.com>
2024-08-05 08:55:37 -0700

    Small Type Fixes

--- cpp/src/io/utilities/column_buffer.cpp
+++ cpp/src/io/utilities/column_buffer.cpp
@@ -61,7 +61,7 @@ void cudf::io::detail::inline_column_buffer::allocate_strings_data(bool memset_d
 {
   CUDF_EXPECTS(type.id() == type_id::STRING, "allocate_strings_data called for non-string column");
   // size + 1 for final offset. _string_data will be initialized later.
-  _data = create_data(data_type{type_id::INT32}, size + 1, memset_data, stream, _mr);
+  _data = create_data(data_type{type_to_id<size_type>()}, size + 1, memset_data, stream, _mr);
 }
 
 void cudf::io::detail::inline_column_buffer::create_string_data(size_t num_bytes,
@@ -109,7 +109,7 @@ void column_buffer_base<string_policy>::create_with_mask(size_type _size,
     // list columns store a buffer of int32's as offsets to represent
     // their individual rows
     case type_id::LIST:
-      _data = create_data(data_type{type_id::INT32}, size, memset_data, stream, _mr);
+      _data = create_data(data_type{type_to_id<size_type>()}, size, memset_data, stream, _mr);
       break;
 
     // struct columns store no data themselves.  just validity and children.
