ba36f6c42f669fbeafec8f9ab9a5536947329cdf
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-08-08 15:01:52 -0700

    Fix date_range(start, end, freq) when end-start is divisible by freq

--- python/cudf/cudf/core/tools/datetimes.py
+++ python/cudf/cudf/core/tools/datetimes.py
@@ -951,7 +951,7 @@ def date_range(
         end = cudf.Scalar(end, dtype=dtype)
         _is_increment_sequence = end >= start
 
-        periods = math.ceil(
+        periods = math.floor(
             int(end - start) / _offset_to_nanoseconds_lower_bound(offset)
         )
 
@@ -962,6 +962,9 @@ def date_range(
         elif periods == 0:
             # end == start, return exactly 1 timestamp (start)
             periods = 1
+        else:
+            # Since closed="both", ensure end point is included
+            periods += 1
 
     # We compute `end_estim` (the estimated upper bound of the date
     # range) below, but don't always use it.  We do this to ensure
