b43249523cc987f8e6565e39dabb49324970fe44
brandon-b-miller <brmiller@nvidia.com>
2024-08-11 14:37:47 -0700

    broken to_duration tests

--- python/cudf/cudf/pylibcudf_tests/test_string_convert.py
+++ python/cudf/cudf/pylibcudf_tests/test_string_convert.py
@@ -29,24 +29,49 @@ def timestamp_type(request):
         pa.duration("s"),
     ],
 )
-def timedelta_type(request):
+def duration_type(request):
     return request.param
 
 
 @pytest.fixture(scope="module")
-def pa_col():
+def pa_timestamp_col():
     return pa.array(["2011-01-01", "2011-01-02", "2011-01-03"])
 
 
 @pytest.fixture(scope="module")
-def plc_col(pa_col):
-    return plc.interop.from_arrow(pa_col)
+def pa_duration_col():
+    return pa.array(["17975 days 12:34:56"])
+
+
+@pytest.fixture(scope="module")
+def plc_timestamp_col(pa_timestamp_col):
+    return plc.interop.from_arrow(pa_timestamp_col)
+
+
+@pytest.fixture(scope="module")
+def plc_duration_col(pa_duration_col):
+    return plc.interop.from_arrow(pa_duration_col)
 
 
 @pytest.mark.parametrize("format", ["%Y-%m-%d"])
-def test_to_datetime(pa_col, plc_col, timestamp_type, format):
-    expect = pa.compute.strptime(pa_col, format, timestamp_type.unit)
+def test_to_datetime(
+    pa_timestamp_col, plc_timestamp_col, timestamp_type, format
+):
+    expect = pa.compute.strptime(pa_timestamp_col, format, timestamp_type.unit)
     got = plc.strings.convert.convert_datetime.to_timestamps(
-        plc_col, plc.interop.from_arrow(timestamp_type), format.encode()
+        plc_timestamp_col,
+        plc.interop.from_arrow(timestamp_type),
+        format.encode(),
+    )
+    assert_column_eq(expect, got)
+
+
+@pytest.mark.parametrize("format", ["%D days %H:%M:%S"])
+def test_to_duration(pa_duration_col, plc_duration_col, duration_type, format):
+    expect = pa.compute.strptime(pa_duration_col, format, duration_type.unit)
+    got = plc.strings.convert.convert_datetime.to_durations(
+        plc_duration_col,
+        plc.interop.from_arrow(duration_type),
+        format.encode(),
     )
     assert_column_eq(expect, got)
