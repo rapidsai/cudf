5440fe2d2436a399c9c14e92b347fdce1aef012a
David Wendt <dwendt@nvidia.com>
2024-08-12 09:49:11 -0400

    Remove invalid column_view usage in string-scalar-to-column

--- cpp/src/column/column_factories.cu
+++ cpp/src/column/column_factories.cu
@@ -20,10 +20,11 @@
 #include <cudf/dictionary/dictionary_factories.hpp>
 #include <cudf/lists/detail/lists_column_factories.hpp>
 #include <cudf/scalar/scalar.hpp>
-#include <cudf/strings/detail/fill.hpp>
+#include <cudf/strings/detail/strings_column_factories.cuh>
 
 #include <rmm/resource_ref.hpp>
 
+#include <thrust/generate.h>
 #include <thrust/iterator/constant_iterator.h>
 
 namespace cudf {
@@ -57,15 +58,28 @@ std::unique_ptr<cudf::column> column_from_scalar_dispatch::operator()<cudf::stri
 {
   if (size == 0) return make_empty_column(value.type());
 
-  // Since we are setting every row to the scalar, the fill() never needs to access
-  // any of the children in the strings column which would otherwise cause an exception.
-  column_view sc{value.type(), size, nullptr, nullptr, 0};
-  auto& sv = static_cast<scalar_type_t<cudf::string_view> const&>(value);
+  if (!value.is_valid(stream)) {
+    return make_strings_column(
+      size,
+      make_column_from_scalar(numeric_scalar<int32_t>(0), size + 1, stream, mr),
+      rmm::device_buffer{},
+      size,
+      cudf::detail::create_null_mask(size, mask_state::ALL_NULL, stream, mr));
+  }
+
+  auto& ss       = static_cast<scalar_type_t<cudf::string_view> const&>(value);
+  auto const sdv = cudf::get_scalar_device_view(const_cast<string_scalar&>(ss));
 
   // fill the column with the scalar
-  auto output = strings::detail::fill(strings_column_view(sc), 0, size, sv, stream, mr);
-
-  return output;
+  using sip_t = cudf::strings::detail::string_index_pair;
+  rmm::device_uvector<sip_t> indices(size, stream);
+  thrust::generate(
+    rmm::exec_policy_nosync(stream), indices.begin(), indices.end(), [sdv] __device__() {
+      auto const d_str = sdv.value();
+      // special handling for empty string -- from an all-empty strings column
+      return d_str.empty() ? sip_t{"", 0} : sip_t{d_str.data(), d_str.size_bytes()};
+    });
+  return cudf::strings::detail::make_strings_column(indices.begin(), indices.end(), stream, mr);
 }
 
 template <>
