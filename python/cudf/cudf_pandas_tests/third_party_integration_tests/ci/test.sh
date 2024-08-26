aba7509c13996b4cee75889bea58df39674d2ddf
Matthew Murray <matthewmurray711@gmail.com>
2024-08-26 09:52:14 -0700

    Change --config arg in rdfg

--- python/cudf/cudf_pandas_tests/third_party_integration_tests/ci/test.sh
+++ python/cudf/cudf_pandas_tests/third_party_integration_tests/ci/test.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 
 rapids-logger "Generate Python testing dependencies"
 rapids-dependency-file-generator \
-  --config "../dependencies.yaml"
+  --config "python/cudf/cudf_pandas_tests/third_party_integration_tests/dependencies.yaml"
   --output conda \
   --file-key test_${LIB} \
   --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee env.yaml
