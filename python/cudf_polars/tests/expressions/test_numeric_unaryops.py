a37260306e23b4088f9f29e5362afec8d3493ad0
Lawrence Mitchell <lmitchell@nvidia.com>
2024-08-27 09:49:38 +0000

    Add tests of pow and more exceptional values in dataframe

--- python/cudf_polars/tests/expressions/test_numeric_unaryops.py
+++ python/cudf_polars/tests/expressions/test_numeric_unaryops.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 from __future__ import annotations
 
+import numpy as np
 import pytest
 
 import polars as pl
@@ -42,11 +43,38 @@ def dtype(request):
 
 
 @pytest.fixture
-def ldf(dtype):
-    return pl.DataFrame({"a": pl.Series([1, 2, None, 4, 5], dtype=dtype)}).lazy()
+def ldf(with_nulls, dtype):
+    values = [1, 2, 4, 5, -2, -4, 0]
+    if with_nulls:
+        values.append(None)
+    if dtype == pl.Float32:
+        values.append(-float("inf"))
+        values.append(float("nan"))
+        values.append(float("inf"))
+    elif dtype == pl.Int32:
+        iinfo = np.iinfo("int32")
+        values.append(iinfo.min)
+        values.append(iinfo.max)
+    return pl.LazyFrame(
+        {
+            "a": pl.Series(values, dtype=dtype),
+            "b": pl.Series([i - 4 for i in range(len(values))], dtype=pl.Float32),
+        }
+    )
 
 
 def test_unary(ldf, op):
     expr = getattr(pl.col("a"), op)()
     q = ldf.select(expr)
+    assert_gpu_result_equal(q, check_exact=False)
+
+
+@pytest.mark.parametrize("base_literal", [False, True])
+@pytest.mark.parametrize("exponent_literal", [False, True])
+def test_pow(ldf, base_literal, exponent_literal):
+    base = pl.lit(2) if base_literal else pl.col("a")
+    exponent = pl.lit(-3, dtype=pl.Float32) if exponent_literal else pl.col("b")
+
+    q = ldf.select(base.pow(exponent))
+
     assert_gpu_result_equal(q, check_exact=False)
