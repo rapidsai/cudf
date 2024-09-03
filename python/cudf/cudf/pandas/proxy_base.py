78bc30a1c1d1accaa584e30aa9b9c15cf06ade22
Matthew Murray <matthewmurray711@gmail.com>
2024-09-03 13:38:17 -0700

    cleanup

--- python/cudf/cudf/pandas/proxy_base.py
+++ python/cudf/cudf/pandas/proxy_base.py
@@ -9,10 +9,10 @@ import numpy as np
 class ProxyNDarrayBase(np.ndarray):
     def __new__(cls, arr):
         if isinstance(arr, cp.ndarray):
-            obj = np.asarray(arr.get(), dtype=arr.dtype).view(cls)
+            obj = arr.get().view(cls)
             return obj
         elif isinstance(arr, np.ndarray):
-            obj = np.asarray(arr, dtype=arr.dtype).view(cls)
+            obj = arr.view(cls)
             return obj
         else:
             raise TypeError(
@@ -22,4 +22,4 @@ class ProxyNDarrayBase(np.ndarray):
     def __array_finalize__(self, obj):
         if obj is None:
             return
-        self._fsproxy_wrapped = obj
+        self._fsproxy_wrapped = getattr(obj, "_fsproxy_wrapped", obj)
