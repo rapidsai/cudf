cdc417e7bb457e5fa4e4af1c566c655b92903e0a
Karthikeyan <6488848+karthikeyann@users.noreply.github.com>
2024-09-05 02:09:03 -0500

    Update doc mixed type as string in json.hpp

--- cpp/include/cudf/io/json.hpp
+++ cpp/include/cudf/io/json.hpp
@@ -376,6 +376,7 @@ class json_reader_options {
   /**
    * @brief Set whether to parse mixed types as a string column.
    * Also enables forcing to read a struct as string column using schema.
+   * If enable, mixed types are parsed a string column regardless of schema.
    *
    * @param val Boolean value to enable/disable parsing mixed types as a string column
    */
