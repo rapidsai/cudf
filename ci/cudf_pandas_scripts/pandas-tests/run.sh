39f7ea603b18c69c1d40abb5a4103730a2f33952
galipremsagar <sagarprem75@gmail.com>
2024-09-06 01:33:29 +0000

    test

--- ci/cudf_pandas_scripts/pandas-tests/run.sh
+++ ci/cudf_pandas_scripts/pandas-tests/run.sh
@@ -11,7 +11,7 @@ RAPIDS_FULL_VERSION=$(<./VERSION)
 rapids-logger "Running Pandas tests using $PANDAS_TESTS_BRANCH branch and rapids-version $RAPIDS_FULL_VERSION"
 rapids-logger "PR number: ${RAPIDS_REF_NAME:-"unknown"}"
 
-
+apt-get update && apt-get install git-lfs
 git lfs install
 git lfs fetch --all
 git lfs checkout
