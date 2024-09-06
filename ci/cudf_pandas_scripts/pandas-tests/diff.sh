c3e257de5b28c47e12084d764b9ec7725d7e7d20
galipremsagar <sagarprem75@gmail.com>
2024-09-06 16:16:11 +0000

    test

--- ci/cudf_pandas_scripts/pandas-tests/diff.sh
+++ ci/cudf_pandas_scripts/pandas-tests/diff.sh
@@ -13,7 +13,7 @@ rapids-logger "Github job name: ${GH_JOB_NAME}"
 rapids-logger "Rapids version: ${RAPIDS_FULL_VERSION}"
 
 PY_VER="310"
-aws s3 cp $(rapids-s3-path) . --recursive --exclude "*" --include "*-results.json"
+# aws s3 cp $(rapids-s3-path) . --recursive --exclude "*" --include "*-results.json"
 MAIN_ARTIFACT=$(rapids-s3-path)cuda12_$(arch)_py${PY_VER}.main-${RAPIDS_FULL_VERSION}-results.json
 PR_ARTIFACT=$(rapids-s3-path)cuda12_$(arch)_py${PY_VER}.pr-${RAPIDS_FULL_VERSION}-*-results.json
 
@@ -24,7 +24,8 @@ rapids-logger "Latest available results from nightly: ${COMPARE_ENV}"
 
 aws s3 cp "s3://rapids-downloads/${COMPARE_ENV}" main-results.json
 aws s3 cp $(rapids-s3-path) . --recursive --exclude "*" --include "*-results.json"
-$PR_ARTIFACT pr-results.json
+ls -al
+# $PR_ARTIFACT pr-results.json
 
 # Compute the diff and prepare job summary:
 python -m pip install pandas tabulate
