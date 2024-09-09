6f80d07ae2e62de8c0e5b2f4a684abda3afaf36a
Matthew Murray <matthewmurray711@gmail.com>
2024-09-09 07:43:30 -0700

    add repl test

--- python/cudf/cudf_pandas_tests/test__main__.py
+++ python/cudf/cudf_pandas_tests/test__main__.py
@@ -30,7 +30,7 @@ def test_run_cudf_pandas_with_script():
     assert res.stdout == expect.stdout
 
 
-def test_run_cudf_pandas_with_cli_with_cmd_args():
+def test_run_cudf_pandas_with_script_with_cmd_args():
     input_args_and_code = '''-c "import pandas as pd; df = pd.DataFrame({'a': [1, 2, 3]}); print(df['a'].sum())"'''
     res = subprocess.run(
         "python -m cudf.pandas " + input_args_and_code,
@@ -44,5 +44,34 @@ def test_run_cudf_pandas_with_cli_with_cmd_args():
         capture_output=True,
         text=True,
     )
-    print(res.stdout)
     assert res.stdout == expect.stdout
+
+
+def test_cudf_pandas_script_repl():
+    def start_repl_process(cmd):
+        process = subprocess.Popen(
+            cmd.split(),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            text=True,
+        )
+        return process
+
+    p1 = start_repl_process("python -m cudf.pandas")
+    p2 = start_repl_process("python")
+    commands = [
+        "import pandas as pd\n",
+        "print(pd.Series(range(2)).sum())\n",
+        "print(pd.Series(range(5)).sum())\n",
+    ]
+
+    for c in commands:
+        p1.stdin.write(c)
+        p2.stdin.write(c)
+        p1.stdin.flush()
+        p2.stdin.flush()
+    res = p1.communicate()[0]
+    expect = p1.communicate()[0]
+    assert res == expect
+    p1.kill()
+    p2.kill()
