2bab2d9d509229af82b0eaa24d30c0b2d20e5f9b
galipremsagar <sagarprem75@gmail.com>
2024-09-09 00:07:42 +0000

    test

--- ci/cudf_pandas_scripts/pandas-tests/diff.sh
+++ ci/cudf_pandas_scripts/pandas-tests/diff.sh
@@ -26,9 +26,9 @@ aws s3 cp "s3://rapids-downloads/${COMPARE_ENV}" main-results.json
 aws s3 cp $(rapids-s3-path) . --recursive --exclude "*" --include "*-results.json"
 ls -al
 # $PR_ARTIFACT pr-results.json
-python python/cudf/cudf/pandas/scripts/merge-json-files.py -d python/cudf/cudf/pandas/scripts/ -o pr-results.json
+python python/cudf/cudf/pandas/scripts/merge-json-files.py -d python/cudf/cudf/pandas/scripts/ -o python/cudf/cudf/pandas/scripts/pr-results.json
 ls -al
-cat pr-results.json
+cat python/cudf/cudf/pandas/scripts/pr-results.json
 
 # Compute the diff and prepare job summary:
 python -m pip install pandas tabulate
