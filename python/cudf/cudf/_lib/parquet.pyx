e95b96c683cbcb0005ebe522cd270238d87791e6
Muhammad Haseeb <mhaseeb@nvidia.com>
2024-09-11 19:42:49 +0000

    Update docstring

--- python/cudf/cudf/_lib/parquet.pyx
+++ python/cudf/cudf/_lib/parquet.pyx
@@ -616,9 +616,9 @@ cdef class ParquetWriter:
         Name of the compression to use. Use ``None`` for no compression.
     statistics : {'ROWGROUP', 'PAGE', 'COLUMN', 'NONE'}, default 'ROWGROUP'
         Level at which column statistics should be included in file.
-    row_group_size_bytes: int, default None
+    row_group_size_bytes: int, default infinite (``uint64 max``)
         Maximum size of each stripe of the output.
-        By default, no limit on row group stripe size will be used.
+        By default, an infinite size equal to ``uint64 max`` will be used.
     row_group_size_rows: int, default 1000000
         Maximum number of rows of each stripe of the output.
         By default, 1000000 (10^6 rows) will be used.
