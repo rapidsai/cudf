1fba1e12a7f61de1c87c11e76cea2c897acfcd9b
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-09-18 11:27:38 -0700

    Add comma

--- python/cudf/cudf/_lib/strings/find_multiple.pyx
+++ python/cudf/cudf/_lib/strings/find_multiple.pyx
@@ -14,7 +14,7 @@ def find_multiple(Column source_strings, Column target_strings):
     of the `target_strings` are found in each string of `source_strings`.
     """
     plc_result = plc.strings.find_multiple.find_multiple(
-        source_strings.to_pylibcudf(mode="read")
+        source_strings.to_pylibcudf(mode="read"),
         target_strings.to_pylibcudf(mode="read")
     )
     return Column.from_pylibcudf(plc_result)
