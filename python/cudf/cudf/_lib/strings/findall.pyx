99651c1e29918aa7e8eb73b9cf1e571fdc879483
David Wendt <dwendt@nvidia.com>
2024-09-25 13:31:23 -0400

    fix imports

--- python/cudf/cudf/_lib/strings/findall.pyx
+++ python/cudf/cudf/_lib/strings/findall.pyx
@@ -4,18 +4,6 @@ from libc.stdint cimport uint32_t
 
 from cudf.core.buffer import acquire_spill_lock
 
-<<<<<<< HEAD
-from pylibcudf.libcudf.column.column cimport column
-from pylibcudf.libcudf.column.column_view cimport column_view
-from pylibcudf.libcudf.strings.findall cimport (
-    find_re as cpp_find_re,
-    findall as cpp_findall,
-)
-from pylibcudf.libcudf.strings.regex_flags cimport regex_flags
-from pylibcudf.libcudf.strings.regex_program cimport regex_program
-
-=======
->>>>>>> branch-24.12
 from cudf._lib.column cimport Column
 
 import pylibcudf as plc
