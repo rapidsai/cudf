36ae931bae9d74931ba0e8b6ad8b7be8f5f0a027
Matthew Murray <matthewmurray711@gmail.com>
2024-09-25 09:40:25 -0700

    address review

--- python/pylibcudf/pylibcudf/tests/test_partitioning.py
+++ python/pylibcudf/pylibcudf/tests/test_partitioning.py
@@ -3,49 +3,52 @@
 import pyarrow as pa
 import pylibcudf as plc
 import pytest
+from utils import assert_table_eq
 
 
-@pytest.fixture
-def pa_table():
-    return pa.table({"a": [1, 2, 3], "b": [1, 2, 5], "c": [1, 2, 10]})
+@pytest.fixture(scope="module")
+def partitioning_data():
+    data = {"a": [1, 2, 3], "b": [1, 2, 5], "c": [1, 2, 10]}
+    pa_table = pa.table(data)
+    return data, pa_table
 
 
-def test_partition(pa_table):
-    plc_result, result_offsets = plc.partitioning.partition(
+def test_partition(partitioning_data):
+    raw_data, pa_table = partitioning_data
+    result, result_offsets = plc.partitioning.partition(
         plc.interop.from_arrow(pa_table),
         plc.interop.from_arrow(pa.array([0, 0, 0])),
         1,
     )
-    pa_result = plc.interop.to_arrow(plc_result)
-    pa_expected = pa.table(
-        [[1, 2, 3], [1, 2, 5], [1, 2, 10]],
+    expected = pa.table(
+        list(raw_data.values()),
         schema=pa.schema([pa.field("", pa.int64(), nullable=False)] * 3),
     )
-    assert pa_result.equals(pa_expected)
+    assert_table_eq(expected, result)
     assert result_offsets == [0, 3]
 
 
-def test_hash_partition(pa_table):
-    plc_result, result_offsets = plc.partitioning.hash_partition(
+def test_hash_partition(partitioning_data):
+    raw_data, pa_table = partitioning_data
+    result, result_offsets = plc.partitioning.hash_partition(
         plc.interop.from_arrow(pa_table), [0, 1], 1
     )
-    pa_result = plc.interop.to_arrow(plc_result)
-    pa_expected = pa.table(
-        [[1, 2, 3], [1, 2, 5], [1, 2, 10]],
+    expected = pa.table(
+        list(raw_data.values()),
         schema=pa.schema([pa.field("", pa.int64(), nullable=False)] * 3),
     )
-    assert pa_result.equals(pa_expected)
+    assert_table_eq(expected, result)
     assert result_offsets == [0]
 
 
-def test_round_robin_partition(pa_table):
-    plc_result, result_offsets = plc.partitioning.round_robin_partition(
+def test_round_robin_partition(partitioning_data):
+    raw_data, pa_table = partitioning_data
+    result, result_offsets = plc.partitioning.round_robin_partition(
         plc.interop.from_arrow(pa_table), 1, 0
     )
-    pa_result = plc.interop.to_arrow(plc_result)
-    pa_expected = pa.table(
-        [[1, 2, 3], [1, 2, 5], [1, 2, 10]],
+    expected = pa.table(
+        list(raw_data.values()),
         schema=pa.schema([pa.field("", pa.int64(), nullable=False)] * 3),
     )
-    assert pa_result.equals(pa_expected)
+    assert_table_eq(expected, result)
     assert result_offsets == [0]
