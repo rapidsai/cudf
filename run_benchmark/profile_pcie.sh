73725dde55b15acd0421dc1c3a6b3f09c91e106b
Tianyu Liu <kingcrimsontianyu@gmail.com>
2024-09-25 01:38:13 -0400

    Update scripts. Improve nvtx

--- run_benchmark/profile_pcie.sh
+++ run_benchmark/profile_pcie.sh
@@ -11,12 +11,11 @@ num_threads_arr=(8 1)
 color_reset='\e[m'
 color_green='\e[1;32m'
 
-# LIBCUDF_CUFILE_POLICY: Disable GDS
 for num_threads in ${num_threads_arr[@]}; do
     echo -e "$color_green--> Thread count: $num_threads$color_reset"
 
     $nsys_bin profile \
-    -o $num_threads \
+    -o /mnt/profile/$num_threads \
     -t nvtx,cuda,osrt \
     -f true \
     --backtrace=none \
@@ -25,5 +24,5 @@ for num_threads in ${num_threads_arr[@]}; do
     --cuda-memory-usage=true \
     --env-var KVIKIO_NTHREADS=$num_threads,LIBCUDF_CUFILE_POLICY=OFF \
     ${parquet_reader_bench_bin} -d 0 -b $parquet_benchmark_name \
-    -a compression_type=SNAPPY -a io_type=FILEPATH -a cardinality=0 -a run_length=1
+    -a compression_type=SNAPPY -a io_type=FILEPATH -a cardinality=0 -a run_length=1 --timeout 1 --run-once
 done
