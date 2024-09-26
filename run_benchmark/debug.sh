a98cb93962e00555fe3b9dd165607b9a6dc617c9
Tianyu Liu <kingcrimsontianyu@gmail.com>
2024-09-26 18:16:43 +0000

    Update scripts

--- run_benchmark/debug.sh
+++ run_benchmark/debug.sh
@@ -9,20 +9,25 @@ color_green='\e[1;32m'
 # Force all CUDA calls to be synchronous
 
 # Only allow GDS IO. Disallow POSIX fallback.
-export CUFILE_ALLOW_COMPAT_MODE=false
+# export CUFILE_ALLOW_COMPAT_MODE=false
 
 export KVIKIO_NTHREADS=1
 
 export LIBCUDF_CUFILE_POLICY="GDS"
 
+# export KVIKIO_COMPAT_MODE=on
+
 export LIBCUDF_CUFILE_THREAD_COUNT=1
 
 export TMPDIR=/home/coder/cudf/run_benchmark
+
+CUFILE_LOGGING_LEVEL=TRACE
 
 gdb -ex start --args ${parquet_reader_bench_bin} -d 0 -b $parquet_benchmark_name \
 -a compression_type=NONE -a io_type=FILEPATH -a cardinality=0 -a run_length=1 \
 --run-once
 
-# valgrind --track-origins=yes ${parquet_reader_bench_bin} -d 0 -b $parquet_benchmark_name \
+# ${parquet_reader_bench_bin} -d 0 -b $parquet_benchmark_name \
 # -a compression_type=NONE -a io_type=FILEPATH -a cardinality=0 -a run_length=1 \
 # --run-once
+
