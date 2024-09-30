407c7e836ffb72522ddbd96f4f6735ebce0bd360
Tianyu Liu <kingcrimsontianyu@gmail.com>
2024-09-30 04:12:32 +0000

    Update scripts

--- run_benchmark/run_test.sh
+++ run_benchmark/run_test.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-export LIBCUDF_CUFILE_POLICY="GDS"
-export CUFILE_ALLOW_COMPAT_MODE=false
+export KVIKIO_COMPAT_MODE=off
 
-# export CUFILE_ALLOW_COMPAT_MODE=false
-
+export LIBCUDF_CUFILE_POLICY="KVIKIO"
 export LIBCUDF_LOGGING_LEVEL=INFO
+
+export TMPDIR=/home/coder/cudf/run_benchmark
+
+export CUFILE_ALLOW_COMPAT_MODE=false
+export CUFILE_LOGGING_LEVEL=WARN
 
 ctest --test-dir ~/cudf/cpp/build/latest -V -R PARQUET_TEST
