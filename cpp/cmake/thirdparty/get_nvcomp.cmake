dcf0b50bee0eca0b5f2364c35891c2cd7105e461
Kyle Edwards <kyedwards@nvidia.com>
2024-09-30 11:16:57 -0400

    Style

--- cpp/cmake/thirdparty/get_nvcomp.cmake
+++ cpp/cmake/thirdparty/get_nvcomp.cmake
@@ -16,9 +16,7 @@
 function(find_and_configure_nvcomp)
 
   include(${rapids-cmake-dir}/cpm/nvcomp.cmake)
-  rapids_cpm_nvcomp(
-    USE_PROPRIETARY_BINARY ${CUDF_USE_PROPRIETARY_NVCOMP}
-  )
+  rapids_cpm_nvcomp(USE_PROPRIETARY_BINARY ${CUDF_USE_PROPRIETARY_NVCOMP})
 
   # Per-thread default stream
   if(TARGET nvcomp AND CUDF_USE_PER_THREAD_DEFAULT_STREAM)
