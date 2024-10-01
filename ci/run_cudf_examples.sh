1cc4e1f193b1c875e6a03fa3ab9658947b53f69b
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2024-09-30 18:50:09 -0700

    Revert erroneous commit

--- ci/run_cudf_examples.sh
+++ ci/run_cudf_examples.sh
@@ -26,7 +26,4 @@ compute-sanitizer --tool memcheck custom_with_malloc names.csv
 compute-sanitizer --tool memcheck parquet_io
 compute-sanitizer --tool memcheck parquet_io example.parquet output.parquet DELTA_BINARY_PACKED ZSTD TRUE
 
-compute-sanitizer --tool memcheck parquet_io_multithreaded
-compute-sanitizer --tool memcheck parquet_io_multithreaded example.parquet 4 PINNED_BUFFER 2 2
-
 exit ${EXITCODE}
