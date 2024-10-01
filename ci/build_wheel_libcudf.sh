1d875c3bd4e75f0c3ac475110953214f93e3ba7b
Kyle Edwards <kyedwards@nvidia.com>
2024-10-01 09:46:10 -0400

    Review feedback

--- ci/build_wheel_libcudf.sh
+++ ci/build_wheel_libcudf.sh
@@ -11,6 +11,9 @@ export SKBUILD_CMAKE_ARGS="-DUSE_CUDA_NVCOMP_WHEEL=ON"
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
 mkdir -p ${package_dir}/final_dist
-python -m auditwheel repair -w ${package_dir}/final_dist --exclude libnvcomp.so.4 ${package_dir}/dist/*
+python -m auditwheel repair \
+    --exclude libnvcomp.so.4 \
+    -w ${package_dir}/final_dist \
+    ${package_dir}/dist/*
 
 RAPIDS_PY_WHEEL_NAME="libcudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 cpp ${package_dir}/final_dist
