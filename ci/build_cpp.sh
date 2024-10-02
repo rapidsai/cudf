26463f1e5bdfde6c0d4200275078d9c46efeebd6
Michael Sarahan <msarahan@nvidia.com>
2024-10-02 17:44:00 -0500

    telemetry setup

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -18,7 +18,7 @@ rapids-logger "Begin cpp build"
 sccache --zero-stats
 
 # With boa installed conda build forward to boa
-RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rapids-conda-retry mambabuild \
+RAPIDS_PACKAGE_VERSION=$(rapids-generate-version) rapids-conda-retry build \
     conda/recipes/libcudf
 
 sccache --show-adv-stats
