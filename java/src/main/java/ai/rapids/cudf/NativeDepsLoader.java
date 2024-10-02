f028292986fff5fb9ad541b6090d388130ed3a19
Gera Shegalov <gera@apache.org>
2024-10-01 17:17:17 -0700

    Revert "Upgrade to nvcomp 4.0.1 (#16076)"
    This reverts commit 0cc059fb2b81adbdc9593052292838995dc78b10.

--- java/src/main/java/ai/rapids/cudf/NativeDepsLoader.java
+++ java/src/main/java/ai/rapids/cudf/NativeDepsLoader.java
@@ -55,6 +55,9 @@ public class NativeDepsLoader {
    */
   private static final String[][] loadOrder = new String[][]{
       new String[]{
+          "nvcomp_bitcomp", "nvcomp_gdeflate"
+      },
+      new String[]{
           "nvcomp"
       },
       new String[]{
