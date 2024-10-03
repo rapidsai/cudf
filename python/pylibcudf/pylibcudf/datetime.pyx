af1397c3a0478ecd7d03776d97f4725021eed671
brandon-b-miller <53796099+brandon-b-miller@users.noreply.github.com>
2024-10-03 12:24:40 -0500

    Update python/pylibcudf/pylibcudf/datetime.pyx

--- python/pylibcudf/pylibcudf/datetime.pyx
+++ python/pylibcudf/pylibcudf/datetime.pyx
@@ -43,7 +43,7 @@ cpdef Column extract_datetime_component(
     """
     Extract a datetime component from a datetime column.
 
-    For details, see :cpp:func:`cudf::datetime::extract_datetime_component`.
+    For details, see :cpp:func:`cudf::extract_datetime_component`.
 
     Parameters
     ----------
