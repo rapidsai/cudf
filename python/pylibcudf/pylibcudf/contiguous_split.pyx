1b98db842f47ed00769dddf3fb84c33cc2ce87c9
Mads R. B. Kristensen <madsbk@gmail.com>
2024-10-08 13:56:48 +0200

    doc

--- python/pylibcudf/pylibcudf/contiguous_split.pyx
+++ python/pylibcudf/pylibcudf/contiguous_split.pyx
@@ -15,6 +15,14 @@ from .table cimport Table
 
 
 cdef class PackedColumns:
+    """Column data in a serialized format.
+
+    Contains data from an array of columns in two contiguous buffers:
+    one on host, which contains table metadata and one on device which
+    contains the table data.
+
+    For details, see :cpp:class:`cudf::packed_columns`.
+    """
     def __init__(self):
         raise ValueError(
             "PackedColumns should not be constructed directly. "
@@ -29,12 +37,44 @@ cdef class PackedColumns:
         return out
 
 cpdef PackedColumns pack(Table input):
+    """Deep-copy a table into a serialized contiguous memory format.
+
+    For details, see :cpp:func:`cudf::pack`.
+
+    Parameters
+    ----------
+    input : Table
+        Table to pack.
+
+    Returns
+    -------
+    PackedColumns
+        The packed columns.
+    """
     return PackedColumns.from_libcudf(
         make_unique[packed_columns](cpp_pack(input.view()))
     )
 
 
 cpdef Table unpack(PackedColumns input):
+    """Deserialize the result of `pack`.
+
+    Copies the result of a serialized table into a table.
+    Contrary to the libcudf C++ function, the returned table is a copy
+    of the serialized data.
+
+    For details, see :cpp:func:`cudf::unpack`.
+
+    Parameters
+    ----------
+    input : PackedColumns
+        The packed columns to unpack.
+
+    Returns
+    -------
+    Table
+        Copy of the packed columns.
+    """
     cdef table_view v = cpp_unpack(dereference(input.c_obj))
     cdef unique_ptr[table] t = make_unique[table](v)
     return Table.from_libcudf(move(t))
