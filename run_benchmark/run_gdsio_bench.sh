d5db45611e628a0a1c4e525ce9b07e1335ba743e
Tianyu Liu <kingcrimsontianyu@gmail.com>
2024-10-10 19:37:55 +0000

    Update

--- run_benchmark/run_gdsio_bench.sh
+++ run_benchmark/run_gdsio_bench.sh
@@ -11,27 +11,27 @@ file_size=4G
 io_size=128K
 test_duration=60 # sec
 device_idx=0
-num_threads_per_file=4
+num_threads_per_job=4
 
 echo -e "$color_green--> Sequential write. GDS.$color_reset"
 # Sequential write. (-I 1)
 # Path: GDS: storage <-> GPU (-x 0).
-$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_file -s $file_size -i $io_size -x 0 -I 1 -T $test_duration
+$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_job -s $file_size -i $io_size -x 0 -I 1 -T $test_duration
 
 echo -e "$color_green--> Sequential read. GDS.$color_reset"
 # Sequential read. (-I 0)
 # Path: GDS: storage <-> GPU (-x 0).
-$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_file -s $file_size -i $io_size -x 0 -I 0 -T $test_duration
+$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_job -s $file_size -i $io_size -x 0 -I 0 -T $test_duration
 
 echo -e "$color_green--> Sequential write. Non-GDS.$color_reset"
 # Sequential write. (-I 1)
 # Path: Storage <-> CPU <-> GPU (-x 2)
-$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_file -s $file_size -i $io_size -x 2 -I 1 -T $test_duration
+$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_job -s $file_size -i $io_size -x 2 -I 1 -T $test_duration
 
 echo -e "$color_green--> Sequential read. Non-GDS.$color_reset"
 # Sequential read. (-I 0)
 # Path: Storage <-> CPU <-> GPU (-x 2)
-$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_file -s $file_size -i $io_size -x 2 -I 0 -T $test_duration
+$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_job -s $file_size -i $io_size -x 2 -I 0 -T $test_duration
 
 
 
@@ -41,24 +41,24 @@ file_size=4G
 io_size=4K
 test_duration=60 # sec
 device_idx=0
-num_threads_per_file=4
+num_threads_per_job=4
 
 echo -e "$color_green--> Random write. GDS.$color_reset"
 # Random write. (-I 3)
 # Path: GDS: storage <-> GPU (-x 0).
-$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_file -s $file_size -i $io_size -x 0 -I 3 -T $test_duration
+$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_job -s $file_size -i $io_size -x 0 -I 3 -T $test_duration
 
 echo -e "$color_green--> Random read. GDS.$color_reset"
 # Random read. (-I 2)
 # Path: GDS: storage <-> GPU (-x 0).
-$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_file -s $file_size -i $io_size -x 0 -I 2 -T $test_duration
+$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_job -s $file_size -i $io_size -x 0 -I 2 -T $test_duration
 
 echo -e "$color_green--> Random write. Non-GDS.$color_reset"
 # Random write. (-I 3)
 # Path: Storage <-> CPU <-> GPU (-x 2)
-$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_file -s $file_size -i $io_size -x 2 -I 3 -T $test_duration
+$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_job -s $file_size -i $io_size -x 2 -I 3 -T $test_duration
 
 echo -e "$color_green--> Random read. Non-GDS.$color_reset"
 # Random read. (-I 2)
 # Path: Storage <-> CPU <-> GPU (-x 2)
-$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_file -s $file_size -i $io_size -x 2 -I 2 -T $test_duration
+$gdsio_bin -D $nvme_dir -d $device_idx -w $num_threads_per_job -s $file_size -i $io_size -x 2 -I 2 -T $test_duration
