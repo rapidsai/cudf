6d210c037a36a74724a501a2d95b01a6bccee5ef
Tianyu Liu <kingcrimsontianyu@gmail.com>
2024-10-16 14:09:02 -0400

    Improve interface

--- cpp/include/cudf/io/datasource.hpp
+++ cpp/include/cudf/io/datasource.hpp
@@ -336,25 +336,37 @@ class datasource {
   class owning_buffer : public buffer {
    public:
     /**
+     * @brief Disallow callers to pass an lvalue Container.
+     */
+    owning_buffer(Container&) = delete;
+
+    /**
      * @brief Moves the input container into the newly created object.
      *
-     * @param data_owner The container to construct the buffer from (ownership is transferred)
+     * @param moved_data_owner The container to construct the buffer from. Callers should explicitly
+     * pass std::move(data_owner_from) to this function to transfer the ownership.
      */
-    owning_buffer(Container&& data_owner)
-      : _data(std::move(data_owner)), _data_ptr(_data.data()), _size(_data.size())
+    owning_buffer(Container&& moved_data_owner)
+      : _data(std::move(moved_data_owner)), _data_ptr(_data.data()), _size(_data.size())
     {
     }
+
+    /**
+     * @brief Disallow callers to pass an lvalue Container.
+     */
+    owning_buffer(Container&, uint8_t const*, size_t) = delete;
 
     /**
      * @brief Moves the input container into the newly created object, and exposes a subspan of the
      * buffer.
      *
-     * @param data_owner The container to construct the buffer from (ownership is transferred)
+     * @param moved_data_owner The container to construct the buffer from. Callers should explicitly
+     * pass std::move(data_owner_from) to this function to transfer the ownership.
      * @param data_ptr Pointer to the start of the subspan
      * @param size The size of the subspan
      */
-    owning_buffer(Container&& data_owner, uint8_t const* data_ptr, size_t size)
-      : _data(std::move(data_owner)), _data_ptr(data_ptr), _size(size)
+    owning_buffer(Container&& moved_data_owner, uint8_t const* data_ptr, size_t size)
+      : _data(std::move(moved_data_owner)), _data_ptr(data_ptr), _size(size)
     {
     }
 
