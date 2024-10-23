54633c1741252e8632c3334d6146a349413f9479
Matthew Murray <matthewmurray711@gmail.com>
2024-10-23 16:13:33 -0700

    clean up tests

--- python/pylibcudf/pylibcudf/tests/test_datetime.py
+++ python/pylibcudf/pylibcudf/tests/test_datetime.py
@@ -48,13 +48,28 @@ def component(request):
 
 @pytest.fixture(
     params=[
-        pa.scalar(-3, pa.int32()),
-        pa.scalar(1, pa.int16()),
-        pa.array([1, -3, 2, 4, -1, 5], pa.int32()),
+        ("day", plc.datetime.RoundingFrequency.DAY),
+        ("hour", plc.datetime.RoundingFrequency.HOUR),
+        ("minute", plc.datetime.RoundingFrequency.MINUTE),
+        ("second", plc.datetime.RoundingFrequency.SECOND),
+        ("millisecond", plc.datetime.RoundingFrequency.MILLISECOND),
+        ("microsecond", plc.datetime.RoundingFrequency.MICROSECOND),
+        ("nanosecond", plc.datetime.RoundingFrequency.NANOSECOND),
     ]
 )
-def months(request):
+def rounding_frequency(request):
     return request.param
+
+
+# @pytest.fixture(
+#     params=[
+#         pa.scalar(-3, pa.int32()),
+#         pa.scalar(1, pa.int16()),
+#         pa.array([1, -3, 2, 4, -1, 5], pa.int32()),
+#     ]
+# )
+# def months_offset(request):
+#     return request.param
 
 
 def test_extract_datetime_component(datetime_column, component):
@@ -107,56 +122,51 @@ def test_datetime_extracting_functions(datetime_column, datetime_func):
         ("round_temporal", "round_datetimes"),
     ],
 )
-@pytest.mark.parametrize(
-    "freq",
-    [
-        ("day", plc.datetime.RoundingFrequency.DAY),
-        ("hour", plc.datetime.RoundingFrequency.HOUR),
-        ("minute", plc.datetime.RoundingFrequency.MINUTE),
-        ("second", plc.datetime.RoundingFrequency.SECOND),
-        ("millisecond", plc.datetime.RoundingFrequency.MILLISECOND),
-        ("microsecond", plc.datetime.RoundingFrequency.MICROSECOND),
-        ("nanosecond", plc.datetime.RoundingFrequency.NANOSECOND),
-    ],
-)
-def test_rounding_operations(datetime_column, op, freq):
-    got = getattr(plc.datetime, op[1])(datetime_column, freq[1])
+def test_rounding_operations(datetime_column, op, rounding_frequency):
+    got = getattr(plc.datetime, op[1])(datetime_column, rounding_frequency[1])
     pa_col = plc.interop.to_arrow(datetime_column)
     pa_got = plc.interop.to_arrow(got)
     expect = getattr(pc, op[0])(
         pa_col,
-        unit=freq[0],
+        unit=rounding_frequency[0],
     ).cast(pa_got.type)
     assert_column_eq(expect, got)
 
 
+@pytest.mark.parametrize(
+    "months",
+    [
+        pa.scalar(-3, pa.int32()),
+        pa.scalar(1, pa.int16()),
+        pa.array([1, -3, 2, 4, -1, 5], pa.int32()),
+    ],
+)
 def test_calendrical_months(datetime_column, months):
+    def add_calendrical_months(timestamps, months):
+        result = []
+        if isinstance(months, pa.Array):
+            months_list = months.to_pylist()
+        else:
+            months_list = [months.as_py()] * len(timestamps)
+        for i, dt in enumerate(timestamps):
+            if dt.as_py() is not None:
+                year, month = dt.as_py().year, dt.as_py().month
+                new_month = month + months_list[i]
+                new_year = year + (new_month - 1) // 12
+                result.append(
+                    dt.as_py().replace(
+                        year=new_year, month=(new_month - 1) % 12 + 1
+                    )
+                )
+            else:
+                result.append(None)
+        return pa.array(result)
+
     pa_col = plc.interop.to_arrow(datetime_column)
     got = plc.datetime.add_calendrical_months(
         datetime_column, plc.interop.from_arrow(months)
     )
     pa_got = plc.interop.to_arrow(got)
-
-    def add_calendrical_months(timestamps, months):
-        result = []
-        if isinstance(months, (pa.Int32Scalar, pa.Int16Scalar)):
-            months = months.as_py()
-            months = [months] * len(timestamps)
-        else:
-            months = months.to_pylist()
-        for i, d in enumerate(timestamps):
-            dt = d.as_py()
-            if dt is not None:
-                year, month = dt.year, dt.month
-                new_month = month + months[i]
-                new_year = year + (new_month - 1) // 12
-                new_month = (new_month - 1) % 12 + 1
-                dt = dt.replace(year=new_year, month=new_month)
-                result.append(dt)
-            else:
-                result.append(dt)
-        return pa.array(result)
-
     expect = add_calendrical_months(pa_col, months).cast(pa_got.type)
     assert_column_eq(expect, got)
 
@@ -170,7 +180,7 @@ def test_day_of_year(datetime_column):
             d.as_py().timetuple().tm_yday if d.as_py() is not None else None
             for d in pa_col
         ],
-        pa_got.type,
+        type=pa_got.type,
     )
     assert_column_eq(expect, got)
 
@@ -184,21 +194,17 @@ def test_is_leap_year(datetime_column):
 
 def test_last_day_of_month(datetime_column):
     def last_day_of_month(dates):
-        last_days = []
-        for d in dates:
-            py_d = d.as_py()
-            if py_d is not None:
-                last_day = calendar.monthrange(py_d.year, py_d.month)[1]
-                py_d = py_d.replace(day=last_day)
-                last_days.append(py_d)
-            else:
-                last_days.append(py_d)
-        return last_days
+        return [
+            d.replace(day=calendar.monthrange(d.year, d.month)[1])
+            if d is not None
+            else d
+            for d in dates.to_pylist()
+        ]
 
     got = plc.datetime.last_day_of_month(datetime_column)
     pa_got = plc.interop.to_arrow(got)
     pa_col = plc.interop.to_arrow(datetime_column)
-    expect = pa.array(last_day_of_month(pa_col), pa_got.type)
+    expect = pa.array(last_day_of_month(pa_col), type=pa_got.type)
     assert_column_eq(expect, got)
 
 
@@ -211,27 +217,14 @@ def test_extract_quarter(datetime_column):
 
 
 def test_days_in_month(datetime_column):
-    def first_day(dates):
-        days = []
-        for d in dates:
-            py_d = d.as_py()
-            if py_d is not None:
-                py_d = py_d.replace(day=1)
-                days.append(py_d)
-            else:
-                days.append(None)
-        return days
+    def days_in_month(dates):
+        return [
+            calendar.monthrange(d.year, d.month)[1] if d is not None else None
+            for d in dates.to_pylist()
+        ]
 
     got = plc.datetime.days_in_month(datetime_column)
     pa_col = plc.interop.to_arrow(datetime_column)
     pa_got = plc.interop.to_arrow(got)
-    expect = pa.array(
-        [
-            calendar.monthrange(d.as_py().year, d.as_py().month)[1]
-            if d.as_py() is not None
-            else None
-            for d in pa_col
-        ],
-        pa_got.type,
-    )
+    expect = pa.array(days_in_month(pa_col), type=pa_got.type)
     assert_column_eq(expect, got)
