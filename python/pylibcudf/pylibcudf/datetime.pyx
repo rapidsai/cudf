536529d9e7929ad346018afb879db1d71457bfa9
Matthew Murray <matthewmurray711@gmail.com>
2024-10-23 09:47:43 -0700

    add remaining datetime functions

--- python/pylibcudf/pylibcudf/datetime.pyx
+++ python/pylibcudf/pylibcudf/datetime.pyx
@@ -3,7 +3,11 @@ from libcpp.memory cimport unique_ptr
 from libcpp.utility cimport move
 from pylibcudf.libcudf.column.column cimport column
 from pylibcudf.libcudf.datetime cimport (
+    add_calendrical_months as cpp_add_calendrical_months,
+    ceil_datetimes as cpp_ceil_datetimes,
     datetime_component,
+    day_of_year as cpp_day_of_year,
+    days_in_month as cpp_days_in_month,
     extract_datetime_component as cpp_extract_datetime_component,
     extract_day as cpp_extract_day,
     extract_hour as cpp_extract_hour,
@@ -12,19 +16,29 @@ from pylibcudf.libcudf.datetime cimport (
     extract_minute as cpp_extract_minute,
     extract_month as cpp_extract_month,
     extract_nanosecond_fraction as cpp_extract_nanosecond_fraction,
+    extract_quarter as cpp_extract_quarter,
     extract_second as cpp_extract_second,
     extract_weekday as cpp_extract_weekday,
     extract_year as cpp_extract_year,
+    floor_datetimes as cpp_floor_datetimes,
+    is_leap_year as cpp_is_leap_year,
+    last_day_of_month as cpp_last_day_of_month,
+    round_datetimes as cpp_round_datetimes,
+    rounding_frequency,
 )
 
 from pylibcudf.libcudf.datetime import \
     datetime_component as DatetimeComponent  # no-cython-lint
+from pylibcudf.libcudf.datetime import \
+    rounding_frequency as RoundingFrequency  # no-cython-lint
+
+from cython.operator cimport dereference
 
 from .column cimport Column
 
 
 cpdef Column extract_year(
-    Column values
+    Column input
 ):
     """
     Extract the year from a datetime column.
@@ -33,7 +47,7 @@ cpdef Column extract_year(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the year from.
 
     Returns
@@ -44,11 +58,11 @@ cpdef Column extract_year(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_year(values.view())
+        result = cpp_extract_year(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_month(
-    Column values
+    Column input
 ):
     """
     Extract the month from a datetime column.
@@ -57,7 +71,7 @@ cpdef Column extract_month(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the month from.
 
     Returns
@@ -68,11 +82,11 @@ cpdef Column extract_month(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_month(values.view())
+        result = cpp_extract_month(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_day(
-    Column values
+    Column input
 ):
     """
     Extract the day from a datetime column.
@@ -81,7 +95,7 @@ cpdef Column extract_day(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the day from.
 
     Returns
@@ -92,11 +106,11 @@ cpdef Column extract_day(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_day(values.view())
+        result = cpp_extract_day(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_weekday(
-    Column values
+    Column input
 ):
     """
     Extract the weekday from a datetime column.
@@ -105,7 +119,7 @@ cpdef Column extract_weekday(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the weekday from.
 
     Returns
@@ -116,11 +130,11 @@ cpdef Column extract_weekday(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_weekday(values.view())
+        result = cpp_extract_weekday(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_hour(
-    Column values
+    Column input
 ):
     """
     Extract the hour from a datetime column.
@@ -129,7 +143,7 @@ cpdef Column extract_hour(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the hour from.
 
     Returns
@@ -140,11 +154,11 @@ cpdef Column extract_hour(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_hour(values.view())
+        result = cpp_extract_hour(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_minute(
-    Column values
+    Column input
 ):
     """
     Extract the minute from a datetime column.
@@ -153,7 +167,7 @@ cpdef Column extract_minute(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the minute from.
 
     Returns
@@ -164,11 +178,11 @@ cpdef Column extract_minute(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_minute(values.view())
+        result = cpp_extract_minute(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_second(
-    Column values
+    Column input
 ):
     """
     Extract the second from a datetime column.
@@ -177,7 +191,7 @@ cpdef Column extract_second(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the second from.
 
     Returns
@@ -188,11 +202,11 @@ cpdef Column extract_second(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_second(values.view())
+        result = cpp_extract_second(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_millisecond_fraction(
-    Column values
+    Column input
 ):
     """
     Extract the millisecond from a datetime column.
@@ -201,7 +215,7 @@ cpdef Column extract_millisecond_fraction(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the millisecond from.
 
     Returns
@@ -212,11 +226,11 @@ cpdef Column extract_millisecond_fraction(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_millisecond_fraction(values.view())
+        result = cpp_extract_millisecond_fraction(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_microsecond_fraction(
-    Column values
+    Column input
 ):
     """
     Extract the microsecond fraction from a datetime column.
@@ -225,7 +239,7 @@ cpdef Column extract_microsecond_fraction(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the microsecond fraction from.
 
     Returns
@@ -236,11 +250,11 @@ cpdef Column extract_microsecond_fraction(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_microsecond_fraction(values.view())
+        result = cpp_extract_microsecond_fraction(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_nanosecond_fraction(
-    Column values
+    Column input
 ):
     """
     Extract the nanosecond fraction from a datetime column.
@@ -249,7 +263,7 @@ cpdef Column extract_nanosecond_fraction(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the nanosecond fraction from.
 
     Returns
@@ -260,11 +274,11 @@ cpdef Column extract_nanosecond_fraction(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_nanosecond_fraction(values.view())
+        result = cpp_extract_nanosecond_fraction(input.view())
     return Column.from_libcudf(move(result))
 
 cpdef Column extract_datetime_component(
-    Column values,
+    Column input,
     datetime_component component
 ):
     """
@@ -274,7 +288,7 @@ cpdef Column extract_datetime_component(
 
     Parameters
     ----------
-    values : Column
+    input : Column
         The column to extract the component from.
     component : DatetimeComponent
         The datetime component to extract.
@@ -287,5 +301,237 @@ cpdef Column extract_datetime_component(
     cdef unique_ptr[column] result
 
     with nogil:
-        result = cpp_extract_datetime_component(values.view(), component)
+        result = cpp_extract_datetime_component(input.view(), component)
+    return Column.from_libcudf(move(result))
+
+cpdef Column ceil_datetimes(
+    Column input,
+    rounding_frequency freq
+):
+    """
+    Round datetimes up to the nearest multiple of the given frequency.
+
+    For details, see :cpp:func:`ceil_datetimes`.
+
+    Parameters
+    ----------
+    input : Column
+        The column of input datetime values.
+    freq : rounding_frequency
+        The frequency to round up to.
+
+    Returns
+    -------
+    Column
+        Column of the same datetime resolution as the input column.
+    """
+    cdef unique_ptr[column] result
+
+    with nogil:
+        result = cpp_ceil_datetimes(input.view(), freq)
+    return Column.from_libcudf(move(result))
+
+cpdef Column floor_datetimes(
+    Column input,
+    rounding_frequency freq
+):
+    """
+    Round datetimes down to the nearest multiple of the given frequency.
+
+    For details, see :cpp:func:`floor_datetimes`.
+
+    Parameters
+    ----------
+    input : Column
+        The column of input datetime values.
+    freq : rounding_frequency
+        The frequency to round down to.
+
+    Returns
+    -------
+    Column
+        Column of the same datetime resolution as the input column.
+    """
+    cdef unique_ptr[column] result
+
+    with nogil:
+        result = cpp_floor_datetimes(input.view(), freq)
+    return Column.from_libcudf(move(result))
+
+cpdef Column round_datetimes(
+    Column input,
+    rounding_frequency freq
+):
+    """
+    Round datetimes to the nearest multiple of the given frequency.
+
+    For details, see :cpp:func:`round_datetimes`.
+
+    Parameters
+    ----------
+    input : Column
+        The column of input datetime values.
+    freq : rounding_frequency
+        The frequency to round to.
+
+    Returns
+    -------
+    Column
+        Column of the same datetime resolution as the input column.
+    """
+    cdef unique_ptr[column] result
+
+    with nogil:
+        result = cpp_round_datetimes(input.view(), freq)
+    return Column.from_libcudf(move(result))
+
+cpdef Column add_calendrical_months(
+    Column input,
+    ColumnOrScalar months,
+):
+    """
+    Adds or subtracts a number of months from the datetime
+    type and returns a timestamp column that is of the same
+    type as the input timestamps column.
+
+    For details, see :cpp:func:`add_calendrical_months`.
+
+    Parameters
+    ----------
+    input : Column
+        The column of input timestamp values.
+    months : ColumnOrScalar
+        The number of months to add.
+
+    Returns
+    -------
+    Column
+        Column of computed timestamps.
+    """
+    if not isinstance(months, (Column, Scalar)):
+        raise TypeError("Must pass a Column or Scalar")
+
+    cdef unique_ptr[column] result
+
+    with nogil:
+        result = cpp_add_calendrical_months(
+            input.view(),
+            months.view() if ColumnOrScalar is Column else
+            dereference(months.get())
+        )
+    return Column.from_libcudf(move(result))
+
+cpdef Column day_of_year(Column input):
+    """
+    Computes the day number since the start of
+    the year from the datetime. The value is between
+    [1, {365-366}].
+
+    For details, see :cpp:func:`day_of_year`.
+
+    Parameters
+    ----------
+    input : Column
+        The column of input datetime values.
+
+    Returns
+    -------
+    Column
+        Column of day numbers.
+    """
+    cdef unique_ptr[column] result
+
+    with nogil:
+        result = cpp_day_of_year(input.view())
+    return Column.from_libcudf(move(result))
+
+cpdef Column is_leap_year(Column input):
+    """
+    Check if the year of the given date is a leap year.
+
+    For details, see :cpp:func:`is_leap_year`.
+
+    Parameters
+    ----------
+    input : Column
+        The column of input datetime values.
+
+    Returns
+    -------
+    Column
+        Column of bools indicating whether the given year
+        is a leap year.
+    """
+    cdef unique_ptr[column] result
+
+    with nogil:
+        result = cpp_is_leap_year(input.view())
+    return Column.from_libcudf(move(result))
+
+cpdef Column last_day_of_month(Column input):
+    """
+    Computes the last day of the month.
+
+    For details, see :cpp:func:`last_day_of_month`.
+
+    Parameters
+    ----------
+    input : Column
+        The column of input datetime values.
+
+    Returns
+    -------
+    Column
+        Column of ``TIMESTAMP_DAYS`` representing the last day
+        of the month.
+    """
+    cdef unique_ptr[column] result
+
+    with nogil:
+        result = cpp_last_day_of_month(input.view())
+    return Column.from_libcudf(move(result))
+
+cpdef Column extract_quarter(Column input):
+    """
+    Returns the quarter (ie. a value from {1, 2, 3, 4})
+    that the date is in.
+
+    For details, see :cpp:func:`extract_quarter`.
+
+    Parameters
+    ----------
+    input : Column
+        The column of input datetime values.
+
+    Returns
+    -------
+    Column
+        Column indicating which quarter the date is in.
+    """
+    cdef unique_ptr[column] result
+
+    with nogil:
+        result = cpp_extract_quarter(input.view())
+    return Column.from_libcudf(move(result))
+
+cpdef Column days_in_month(Column input):
+    """
+    Extract the number of days in the month.
+
+    For details, see :cpp:func:`days_in_month`.
+
+    Parameters
+    ----------
+    input : Column
+        The column of input datetime values.
+
+    Returns
+    -------
+    Column
+        Column of the number of days in the given month.
+    """
+    cdef unique_ptr[column] result
+
+    with nogil:
+        result = cpp_days_in_month(input.view())
     return Column.from_libcudf(move(result))
--- python/pylibcudf/pylibcudf/tests/test_datetime.py
+++ python/pylibcudf/pylibcudf/tests/test_datetime.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2024, NVIDIA CORPORATION.
 
+import calendar
 import datetime
 
 import pyarrow as pa
@@ -45,6 +46,17 @@ def component(request):
     return request.param
 
 
+@pytest.fixture(
+    params=[
+        pa.scalar(-3, pa.int32()),
+        pa.scalar(1, pa.int16()),
+        pa.array([1, -3, 2, 4, -1, 5], pa.int32()),
+    ]
+)
+def months(request):
+    return request.param
+
+
 def test_extract_datetime_component(datetime_column, component):
     attr, component = component
     kwargs = {}
@@ -84,4 +96,142 @@ def test_datetime_extracting_functions(datetime_column, datetime_func):
         kwargs = {"count_from_zero": False}
         attr = "day_of_week"
     expect = getattr(pc, attr)(pa_col, **kwargs).cast(pa.int16())
+    assert_column_eq(expect, got)
+
+
+@pytest.mark.parametrize(
+    "op",
+    [
+        ("ceil_temporal", "ceil_datetimes"),
+        ("floor_temporal", "floor_datetimes"),
+        ("round_temporal", "round_datetimes"),
+    ],
+)
+@pytest.mark.parametrize(
+    "freq",
+    [
+        ("day", plc.datetime.RoundingFrequency.DAY),
+        ("hour", plc.datetime.RoundingFrequency.HOUR),
+        ("minute", plc.datetime.RoundingFrequency.MINUTE),
+        ("second", plc.datetime.RoundingFrequency.SECOND),
+        ("millisecond", plc.datetime.RoundingFrequency.MILLISECOND),
+        ("microsecond", plc.datetime.RoundingFrequency.MICROSECOND),
+        ("nanosecond", plc.datetime.RoundingFrequency.NANOSECOND),
+    ],
+)
+def test_rounding_operations(datetime_column, op, freq):
+    got = getattr(plc.datetime, op[1])(datetime_column, freq[1])
+    pa_col = plc.interop.to_arrow(datetime_column)
+    pa_got = plc.interop.to_arrow(got)
+    expect = getattr(pc, op[0])(
+        pa_col,
+        unit=freq[0],
+    ).cast(pa_got.type)
+    assert_column_eq(expect, got)
+
+
+def test_calendrical_months(datetime_column, months):
+    pa_col = plc.interop.to_arrow(datetime_column)
+    got = plc.datetime.add_calendrical_months(
+        datetime_column, plc.interop.from_arrow(months)
+    )
+    pa_got = plc.interop.to_arrow(got)
+
+    def add_calendrical_months(timestamps, months):
+        result = []
+        if isinstance(months, (pa.Int32Scalar, pa.Int16Scalar)):
+            months = months.as_py()
+            months = [months] * len(timestamps)
+        else:
+            months = months.to_pylist()
+        for i, d in enumerate(timestamps):
+            dt = d.as_py()
+            if dt is not None:
+                year, month = dt.year, dt.month
+                new_month = month + months[i]
+                new_year = year + (new_month - 1) // 12
+                new_month = (new_month - 1) % 12 + 1
+                dt = dt.replace(year=new_year, month=new_month)
+                result.append(dt)
+            else:
+                result.append(dt)
+        return pa.array(result)
+
+    expect = add_calendrical_months(pa_col, months).cast(pa_got.type)
+    assert_column_eq(expect, got)
+
+
+def test_day_of_year(datetime_column):
+    got = plc.datetime.day_of_year(datetime_column)
+    pa_got = plc.interop.to_arrow(got)
+    pa_col = plc.interop.to_arrow(datetime_column)
+    expect = pa.array(
+        [
+            d.as_py().timetuple().tm_yday if d.as_py() is not None else None
+            for d in pa_col
+        ],
+        pa_got.type,
+    )
+    assert_column_eq(expect, got)
+
+
+def test_is_leap_year(datetime_column):
+    got = plc.datetime.is_leap_year(datetime_column)
+    pa_col = plc.interop.to_arrow(datetime_column)
+    expect = pc.is_leap_year(pa_col)
+    assert_column_eq(expect, got)
+
+
+def test_last_day_of_month(datetime_column):
+    def last_day_of_month(dates):
+        last_days = []
+        for d in dates:
+            py_d = d.as_py()
+            if py_d is not None:
+                last_day = calendar.monthrange(py_d.year, py_d.month)[1]
+                py_d = py_d.replace(day=last_day)
+                last_days.append(py_d)
+            else:
+                last_days.append(py_d)
+        return last_days
+
+    got = plc.datetime.last_day_of_month(datetime_column)
+    pa_got = plc.interop.to_arrow(got)
+    pa_col = plc.interop.to_arrow(datetime_column)
+    expect = pa.array(last_day_of_month(pa_col), pa_got.type)
+    assert_column_eq(expect, got)
+
+
+def test_extract_quarter(datetime_column):
+    got = plc.datetime.extract_quarter(datetime_column)
+    pa_col = plc.interop.to_arrow(datetime_column)
+    pa_got = plc.interop.to_arrow(got)
+    expect = pc.quarter(pa_col).cast(pa_got.type)
+    assert_column_eq(expect, got)
+
+
+def test_days_in_month(datetime_column):
+    def first_day(dates):
+        days = []
+        for d in dates:
+            py_d = d.as_py()
+            if py_d is not None:
+                py_d = py_d.replace(day=1)
+                days.append(py_d)
+            else:
+                days.append(None)
+        return days
+
+    got = plc.datetime.days_in_month(datetime_column)
+    pa_col = plc.interop.to_arrow(datetime_column)
+    pa_got = plc.interop.to_arrow(got)
+    expect = pa.array(
+        [
+            calendar.monthrange(d.as_py().year, d.as_py().month)[1]
+            if d.as_py() is not None
+            else None
+            for d in pa_col
+        ],
+        pa_got.type,
+    )
     assert_column_eq(expect, got)
