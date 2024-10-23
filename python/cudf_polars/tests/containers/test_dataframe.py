14691a6969d6501b8e5830d6954997419bd27d6f
Mads R. B. Kristensen <madsbk@gmail.com>
2024-10-23 14:27:24 +0200

    test/experimental

--- python/cudf_polars/tests/containers/test_dataframe.py
+++ python/cudf_polars/tests/containers/test_dataframe.py
@@ -6,7 +6,6 @@ from __future__ import annotations
 import pyarrow as pa
 import pylibcudf as plc
 import pytest
-from distributed.protocol import deserialize, serialize
 
 import polars as pl
 
@@ -181,30 +180,5 @@ def test_serialize(arrow_tbl):
 
     header, frames = df.serialize()
     res = DataFrame.deserialize(header, frames)
-
-    pl.testing.asserts.assert_frame_equal(df.to_polars(), res.to_polars())
-
-
-@pytest.mark.parametrize(
-    "arrow_tbl",
-    [
-        pa.table([]),
-        pa.table({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]}),
-        pa.table({"a": [1, 2, 3]}),
-        pa.table({"a": [1], "b": [2], "c": [3]}),
-        pa.table({"a": ["a", "bb", "ccc"]}),
-        pa.table({"a": [1, 2, None], "b": [None, 3, 4]}),
-    ],
-)
-@pytest.mark.parametrize("protocol", ["cuda", "dask"])
-def test_dask_serialize(arrow_tbl, protocol):
-    # To register dask serializers, we need to import dask_serialize
-    import cudf_polars.experimental.dask_serialize  # noqa: F401
-
-    plc_tbl = plc.interop.from_arrow(arrow_tbl)
-    df = DataFrame.from_table(plc_tbl, names=arrow_tbl.column_names)
-
-    header, frames = serialize(df, on_error="raise", serializers=[protocol])
-    res = deserialize(header, frames, deserializers=[protocol])
 
     pl.testing.asserts.assert_frame_equal(df.to_polars(), res.to_polars())
