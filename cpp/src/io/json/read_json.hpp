c3b6cb31abb8f7b7726032e36a4d754b0831d478
Shruti Shivakumar <shruti.shivakumar@gmail.com>
2024-10-24 18:19:24 +0000

    cleanup

--- cpp/src/io/json/read_json.hpp
+++ cpp/src/io/json/read_json.hpp
@@ -18,7 +18,6 @@
 
 #include "../comp/io_uncomp.hpp"
 
-#include <cudf/io/datasource.hpp>
 #include <cudf/io/json.hpp>
 #include <cudf/types.hpp>
 #include <cudf/utilities/export.hpp>
@@ -74,56 +73,6 @@ table_with_metadata read_json(host_span<std::unique_ptr<datasource>> sources,
                               json_reader_options const& reader_opts,
                               rmm::cuda_stream_view stream,
                               rmm::device_async_resource_ref mr);
-
-class compressed_host_buffer_source final : public datasource {
- public:
-  explicit compressed_host_buffer_source(cudf::host_span<std::uint8_t const> ch_buffer,
-                                         compression_type comptype)
-    : _ch_buffer{ch_buffer}, _comptype{comptype}
-  {
-    if (comptype == compression_type::GZIP || comptype == compression_type::ZIP ||
-        comptype == compression_type::SNAPPY) {
-      _decompressed_ch_buffer_size = estimate_uncompressed_size(_comptype, _ch_buffer);
-      _decompressed_buffer.resize(0);
-    } else {
-      _decompressed_buffer         = decompress(_comptype, _ch_buffer);
-      _decompressed_ch_buffer_size = _decompressed_buffer.size();
-    }
-  }
-
-  size_t host_read(size_t offset, size_t size, uint8_t* dst) override
-  {
-    auto decompressed_hbuf = decompress(_comptype, _ch_buffer);
-    auto const count       = std::min(size, decompressed_hbuf.size() - offset);
-    std::memcpy(dst, decompressed_hbuf.data() + offset, count);
-    return count;
-  }
-
-  std::unique_ptr<buffer> host_read(size_t offset, size_t size) override
-  {
-    if (_decompressed_buffer.empty()) {
-      auto decompressed_hbuf = decompress(_comptype, _ch_buffer);
-      auto const count       = std::min(size, decompressed_hbuf.size() - offset);
-      bool partial_read      = offset + count < decompressed_hbuf.size();
-      if (!partial_read)
-        return std::make_unique<owning_buffer<std::vector<uint8_t>>>(
-          std::move(decompressed_hbuf), decompressed_hbuf.data() + offset, count);
-      _decompressed_buffer = std::move(decompressed_hbuf);
-    }
-    auto const count = std::min(size, _decompressed_buffer.size() - offset);
-    return std::make_unique<non_owning_buffer>(_decompressed_buffer.data() + offset, count);
-  }
-
-  [[nodiscard]] bool supports_device_read() const override { return false; }
-
-  [[nodiscard]] size_t size() const override { return _decompressed_ch_buffer_size; }
-
- private:
-  cudf::host_span<std::uint8_t const> _ch_buffer;  ///< A non-owning view of the existing host data
-  compression_type _comptype;
-  size_t _decompressed_ch_buffer_size;
-  std::vector<std::uint8_t> _decompressed_buffer;
-};
 
 }  // namespace io::json::detail
 }  // namespace CUDF_EXPORT cudf
