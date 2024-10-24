4576f8986635e77bacf7f5ae1dc7b69b8357313c
Paul Mattione <pmattione@nvidia.com>
2024-10-24 13:29:57 -0400

    style fixes

--- cpp/src/io/parquet/page_hdr.cu
+++ cpp/src/io/parquet/page_hdr.cu
@@ -182,8 +182,9 @@ __device__ decode_kernel_mask kernel_mask_for_page(PageInfo const& page,
     // check for string before byte_stream_split so FLBA will go to the right kernel
     return decode_kernel_mask::STRING;
   } else if (is_boolean(chunk)) {
-    return is_list(chunk) ? decode_kernel_mask::BOOLEAN_LIST
-           : is_nested(chunk) ? decode_kernel_mask::BOOLEAN_NESTED : decode_kernel_mask::BOOLEAN;
+    return is_list(chunk)     ? decode_kernel_mask::BOOLEAN_LIST
+           : is_nested(chunk) ? decode_kernel_mask::BOOLEAN_NESTED
+           : decode_kernel_mask::BOOLEAN;
   }
 
   if (!is_byte_array(chunk)) {
