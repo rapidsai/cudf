80cbb65faaebd38f39f14d6075240cc7443d524b
Mads R. B. Kristensen <madsbk@gmail.com>
2024-10-28 12:48:17 +0100

    ^

--- python/pylibcudf/pylibcudf/io/types.pyx
+++ python/pylibcudf/pylibcudf/io/types.pyx
@@ -145,7 +145,7 @@ cdef class SourceInfo:
         Mixing different types of sources will raise a `ValueError`.
     """
     # Regular expression that match remote file paths supported by libcudf
-    _is_remote_file_pattern = re.compile(r"s3://", re.IGNORECASE)
+    _is_remote_file_pattern = re.compile(r"^s3://", re.IGNORECASE)
 
     def __init__(self, list sources):
         if not sources:
