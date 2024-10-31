3dfeada4090c14aea1b0e118d1a8865fc790396d
Shruti Shivakumar <shruti.shivakumar@gmail.com>
2024-10-31 03:43:01 +0000

    addressing pr comments

--- cpp/tests/streams/text/subword_tokenize_test.cpp
+++ cpp/tests/streams/text/subword_tokenize_test.cpp
@@ -39,34 +39,37 @@ struct TextSubwordTest : public cudf::test::BaseFixture {};
 // The period '.' character also has a token id.
 void create_hashed_vocab(std::string const& hash_file)
 {
-  std::vector<std::pair<int, int>> coefficients(23, {65559, 0});
+  constexpr size_t coefsize = 23;
+  std::vector<std::pair<int, int>> coefficients(coefsize, {65559, 0});
   std::ofstream outfile(hash_file, std::ofstream::out);
   outfile << "1\n0\n" << coefficients.size() << "\n";
-  for (auto c : coefficients)
+  for (auto c : coefficients) {
     outfile << c.first << " " << c.second << "\n";
-  std::vector<uint64_t> hash_table(23, 0);
+  }
+  std::vector<uint64_t> hash_table(coefsize, 0);
   outfile << hash_table.size() << "\n";
   hash_table[0]  = 3015668L;              // based on values
   hash_table[1]  = 6205475701751155871L;  // from the
   hash_table[5]  = 6358029;               // bert_hash_table.txt
   hash_table[16] = 451412625363L;         // file for the test
   hash_table[20] = 6206321707968235495L;  // words above
-  for (auto h : hash_table)
+  for (auto h : hash_table) {
     outfile << h << "\n";
+  }
   outfile << "100\n101\n102\n\n";
 }
 
 TEST(TextSubwordTest, Tokenize)
 {
-  uint32_t nrows = 100;
+  uint32_t const nrows = 100;
   std::vector<char const*> h_strings(nrows, "This is a test. A test this is.");
-  cudf::test::strings_column_wrapper strings(h_strings.begin(), h_strings.end());
-  std::string hash_file = temp_env->get_temp_filepath("hashed_vocab.txt");
+  cudf::test::strings_column_wrapper strings(h_strings.cbegin(), h_strings.cend());
+  std::string const hash_file = temp_env->get_temp_filepath("hashed_vocab.txt");
   create_hashed_vocab(hash_file);
   auto vocab = nvtext::load_vocabulary_file(hash_file, cudf::test::get_default_stream());
 
-  uint32_t max_sequence_length = 16;
-  uint32_t stride              = 16;
+  uint32_t const max_sequence_length = 16;
+  uint32_t const stride              = 16;
 
   auto result = nvtext::subword_tokenize(cudf::strings_column_view{strings},
                                          *vocab,
