789f5ed76f811258bbef54bdad0c33889ef8490e
galipremsagar <sagarprem75@gmail.com>
2024-11-04 17:44:54 +0000

    fix pytest

--- python/cudf/cudf_pandas_tests/test_cudf_pandas.py
+++ python/cudf/cudf_pandas_tests/test_cudf_pandas.py
@@ -1773,4 +1773,7 @@ def test_fallback_raises_error(monkeypatch):
     ],
 )
 def test_cudf_pandas_util_version(attrs):
-    assert hasattr(pd.util, attrs)
+    if PANDAS_GE_220 and attrs == "capitalize_first_letter":
+        assert not hasattr(pd.util, attrs)
+    else:
+        assert hasattr(pd.util, attrs)
