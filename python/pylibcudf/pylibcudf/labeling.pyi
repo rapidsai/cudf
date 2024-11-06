5eb87fb8771993c827117d6433da513d3b3b3798
Lawrence Mitchell <lmitchell@nvidia.com>
2024-11-06 18:14:49 +0000

    Use typed enum for inclusive parameter in label_bins

--- python/pylibcudf/pylibcudf/labeling.pyi
+++ python/pylibcudf/pylibcudf/labeling.pyi
@@ -1,10 +1,17 @@
 # Copyright (c) 2024, NVIDIA CORPORATION.
+
+from enum import IntEnum, auto
+
 from pylibcudf.column import Column
+
+class Inclusive(IntEnum):
+    YES = auto()
+    NO = auto()
 
 def label_bins(
     input: Column,
     left_edges: Column,
-    left_inclusive: bool,
+    left_inclusive: Inclusive,
     right_edges: Column,
-    right_inclusive: bool,
+    right_inclusive: Inclusive,
 ) -> Column: ...
