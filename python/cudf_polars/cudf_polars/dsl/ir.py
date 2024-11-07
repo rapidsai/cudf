4aee59fd625a0aa854e9a02abdba0fcc9dcf6f91
brandon-b-miller <brmiller@nvidia.com>
2024-11-07 07:18:52 -0800

    Revert "a trial commit to test a different concatenation strategy"
    This reverts commit 53b0b2a08e719130b232df1f6a083a3f05973482.

--- python/cudf_polars/cudf_polars/dsl/ir.py
+++ python/cudf_polars/cudf_polars/dsl/ir.py
@@ -503,17 +503,24 @@ class Scan(IR):
                         "pass_read_limit", cls.PARQUET_DEFAULT_PASS_LIMIT
                     ),
                 )
-
-                chunks = []  # type: ignore
                 chk = reader.read_chunk()
                 tbl = chk.tbl
-                chunks.append(tbl)
                 names = chk.column_names()
+                concatenated_columns = tbl.columns()
                 while reader.has_next():
-                    chunks.append(reader.read_chunk().tbl)
+                    tbl = reader.read_chunk().tbl
 
-                chunks = plc.concatenate.concatenate(chunks)
-                df = DataFrame.from_table(chunks, names=names)
+                    for i in range(tbl.num_columns()):
+                        concatenated_columns[i] = plc.concatenate.concatenate(
+                            [concatenated_columns[i], tbl._columns[i]]
+                        )
+                        # Drop residual columns to save memory
+                        tbl._columns[i] = None
+
+                df = DataFrame.from_table(
+                    plc.Table(concatenated_columns),
+                    names=names,
+                )
             else:
                 filters = None
                 if predicate is not None and row_index is None:
