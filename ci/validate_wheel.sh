89f38a258679b7dadb40a7e56c1f26095ac148c1
James Lamb <jlamb@nvidia.com>
2024-11-08 11:37:56 -0600

    get more logs

--- ci/validate_wheel.sh
+++ ci/validate_wheel.sh
@@ -6,8 +6,10 @@ set -euo pipefail
 package_dir=$1
 wheel_dir_relative_path=$2
 
-# TODO: move this into ci-imgs
+# TODO: pre-install these in ci-imgs
+rapids-logger "installing validation tools"
 python -m pip install \
+    -v \
     'pydistcheck==0.8.0' \
     'twine>=5.0.0'
 
