4aa1d3c07e4492f02ff34b1365c7d2955e959df6
James Lamb <jlamb@nvidia.com>
2024-11-12 14:22:08 -0600

    get validation tools from CI images

--- ci/validate_wheel.sh
+++ ci/validate_wheel.sh
@@ -6,15 +6,6 @@ set -euo pipefail
 package_dir=$1
 wheel_dir_relative_path=$2
 
-# TODO: pre-install these in ci-imgs
-rapids-logger "installing validation tools"
-python -m pip install \
-    -v \
-    'pydistcheck==0.8.0' \
-    'twine>=5.0.0'
-
-pyenv rehash
-
 cd "${package_dir}"
 
 rapids-logger "validate packages with 'pydistcheck'"
