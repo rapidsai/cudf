5dd9718eb82862f44ce790fb46f349416630e17a
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-12 23:11:33 +0000

    Stop building include-what-you-use in CI

--- ci/cpp_linters.sh
+++ ci/cpp_linters.sh
@@ -24,19 +24,6 @@ RAPIDS_VERSION_MAJOR_MINOR="$(rapids-version-major-minor)"
 
 source rapids-configure-sccache
 
-# TODO: For testing purposes, clone and build IWYU. We can switch to a release
-# once a clang 19-compatible version is available, which should be soon
-# (https://github.com/include-what-you-use/include-what-you-use/issues/1641).
-git clone --depth 1 https://github.com/include-what-you-use/include-what-you-use.git
-pushd include-what-you-use
-# IWYU's CMake build uses some Python scripts that assume that the cwd is
-# importable, so support that legacy behavior.
-export PYTHONPATH=${PWD}:${PYTHONPATH:-}
-cmake -S . -B build -GNinja --install-prefix=${CONDA_PREFIX}
-cmake --build build
-cmake --install build
-popd
-
 # Run the build via CMake, which will run clang-tidy when CUDF_STATIC_LINTERS is enabled.
 cmake -S cpp -B cpp/build -DCMAKE_BUILD_TYPE=Release -DCUDF_STATIC_LINTERS=ON -GNinja
 cmake --build cpp/build 2>&1 | python cpp/scripts/parse_iwyu_output.py
