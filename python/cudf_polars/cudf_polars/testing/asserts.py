9c4c1bfea4e26152221a01f9c64be2398bae9414
brandon-b-miller <brmiller@nvidia.com>
2024-11-12 17:47:15 -0800

    raise on fail true for default testing engine

--- python/cudf_polars/cudf_polars/testing/asserts.py
+++ python/cudf_polars/cudf_polars/testing/asserts.py
@@ -19,7 +19,7 @@ if TYPE_CHECKING:
 
 __all__: list[str] = ["assert_gpu_result_equal", "assert_ir_translation_raises"]
 
-DEFAULT_GPU_ENGINE: GPUEngine = GPUEngine()
+DEFAULT_GPU_ENGINE: GPUEngine = GPUEngine(raise_on_fail=True)
 
 
 def assert_gpu_result_equal(
