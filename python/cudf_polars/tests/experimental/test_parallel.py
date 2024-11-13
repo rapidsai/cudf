8079ac02c1dec693e509fa8fa229eb0c1f77195e
Peter Andreas Entschev <peter@entschev.com>
2024-11-13 06:55:40 -0800

    Add couple of Dask executor tests

--- python/cudf_polars/tests/experimental/test_parallel.py
+++ python/cudf_polars/tests/experimental/test_parallel.py
@@ -8,6 +8,7 @@ from polars.testing import assert_frame_equal
 
 from cudf_polars import Translator
 from cudf_polars.experimental.parallel import evaluate_dask
+from cudf_polars.testing.asserts import assert_gpu_result_equal
 
 
 def test_evaluate_dask():
@@ -22,3 +23,31 @@ def test_evaluate_dask():
     got = evaluate_dask(qir).to_polars()
 
     assert_frame_equal(expected, got)
+
+
+def test_can_convert_lists():
+    df = pl.LazyFrame(
+        {
+            "a": pl.Series([[1, 2], [3]], dtype=pl.List(pl.Int8())),
+            "b": pl.Series([[1], [2]], dtype=pl.List(pl.UInt16())),
+            "c": pl.Series(
+                [
+                    [["1", "2", "3"], ["4", "567"]],
+                    [["8", "9"], []],
+                ],
+                dtype=pl.List(pl.List(pl.String())),
+            ),
+            "d": pl.Series([[[1, 2]], []], dtype=pl.List(pl.List(pl.UInt16()))),
+        }
+    )
+
+    assert_gpu_result_equal(df, executor="dask")
+
+
+def test_scan_csv_comment_char(tmp_path):
+    with (tmp_path / "test.csv").open("w") as f:
+        f.write("""foo,bar,baz\n# 1,2,3\n3,4,5""")
+
+    q = pl.scan_csv(tmp_path / "test.csv", comment_prefix="#")
+
+    assert_gpu_result_equal(q, executor="dask")
