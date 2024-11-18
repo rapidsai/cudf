ffe895e61c6ae2badc52dc907c4e2f43d124ebc0
Matthew Murray <matthewmurray711@gmail.com>
2024-11-18 13:24:49 -0800

    add to all

--- python/pylibcudf/pylibcudf/io/csv.pyx
+++ python/pylibcudf/pylibcudf/io/csv.pyx
@@ -23,7 +23,12 @@ from pylibcudf.libcudf.types cimport data_type, size_type
 from pylibcudf.types cimport DataType
 from pylibcudf.table cimport Table
 
-__all__ = ["read_csv", "write_csv"]
+__all__ = [
+    "read_csv",
+    "write_csv",
+    "CsvWriterOptions",
+    "CsvWriterOptionsBuilder",
+]
 
 cdef tuple _process_parse_dates_hex(list cols):
     cdef vector[string] str_cols
