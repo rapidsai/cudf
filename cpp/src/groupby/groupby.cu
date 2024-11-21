227016bec9d95ea942313979a9a26135239e5953
Nghia Truong <nghiat@nvidia.com>
2024-11-21 15:13:46 -0800

    Construct empty output when the input is empty
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- cpp/src/groupby/groupby.cu
+++ cpp/src/groupby/groupby.cu
@@ -140,6 +140,11 @@ struct empty_column_constructor {
       return empty_like(values);
     }
 
+    if constexpr (k == aggregation::Kind::HOST_UDF) {
+      auto const& udf_ptr = dynamic_cast<cudf::detail::host_udf_aggregation const&>(agg).udf_ptr;
+      return udf_ptr->get_empty_output();
+    }
+
     return make_empty_column(target_type(values.type(), k));
   }
 };
