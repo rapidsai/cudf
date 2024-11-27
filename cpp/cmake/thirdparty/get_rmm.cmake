1a3d812278c2c149091c2ab9749ed055584996ad
Vyas Ramasubramani <vyasr@nvidia.com>
2024-11-27 14:47:29 +0000

    Revert "Use an override to test in static compilation settings"
    This reverts commit 0ce74dac8ab4ec2378630202747edc834843d6b9.

--- cpp/cmake/thirdparty/get_rmm.cmake
+++ cpp/cmake/thirdparty/get_rmm.cmake
@@ -1,5 +1,5 @@
 # =============================================================================
-# Copyright (c) 2020-2024, NVIDIA CORPORATION.
+# Copyright (c) 2020-2021, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -15,10 +15,6 @@
 # This function finds rmm and sets any additional necessary environment variables.
 function(find_and_configure_rmm)
   include(${rapids-cmake-dir}/cpm/rmm.cmake)
-  include(${rapids-cmake-dir}/cpm/package_override.cmake)
-
-  set(cudf_patch_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/patches")
-  rapids_cpm_package_override("${cudf_patch_dir}/rmm_override.json")
 
   # Find or install RMM
   rapids_cpm_rmm(BUILD_EXPORT_SET cudf-exports INSTALL_EXPORT_SET cudf-exports)
