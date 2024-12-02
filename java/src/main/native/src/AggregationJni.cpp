bbdc699a8922ce11592850f3ee3778e7862c2e14
Nghia Truong <nghiat@nvidia.com>
2024-12-02 10:46:00 -0800

    Fix out of sync enums
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- java/src/main/native/src/AggregationJni.cpp
+++ java/src/main/native/src/AggregationJni.cpp
@@ -163,10 +163,10 @@ JNIEXPORT jlong JNICALL Java_ai_rapids_cudf_Aggregation_createTDigestAgg(JNIEnv*
     std::unique_ptr<cudf::aggregation> ret;
     // These numbers come from Aggregation.java and must stay in sync
     switch (kind) {
-      case 31:  // TDIGEST
+      case 32:  // TDIGEST
         ret = cudf::make_tdigest_aggregation<cudf::groupby_aggregation>(delta);
         break;
-      case 32:  // MERGE_TDIGEST
+      case 33:  // MERGE_TDIGEST
         ret = cudf::make_merge_tdigest_aggregation<cudf::groupby_aggregation>(delta);
         break;
       default: throw std::logic_error("Unsupported TDigest Aggregation Operation");
