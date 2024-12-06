385f03af6ae7acb5454c8d15501a002a049eb58e
rjzamora <rzamora217@gmail.com>
2024-12-05 20:19:06 -0800

    simplify lower

--- python/cudf_polars/cudf_polars/experimental/groupby.py
+++ python/cudf_polars/cudf_polars/experimental/groupby.py
@@ -4,8 +4,6 @@
 
 from __future__ import annotations
 
-import operator
-from functools import reduce
 from typing import TYPE_CHECKING, Any
 
 import pylibcudf as plc
@@ -28,38 +26,37 @@ _GB_AGG_SUPPORTED = ("sum", "count", "mean")
 
 def _single_fallback(
     ir: IR,
-    children: tuple[IR],
+    child: IR,
     partition_info: MutableMapping[IR, PartitionInfo],
     unsupported_agg: Expr | None = None,
 ):
-    if any(partition_info[child].count > 1 for child in children):  # pragma: no cover
+    if partition_info[child].count > 1:
         msg = f"Class {type(ir)} does not support multiple partitions."
         if unsupported_agg:
             msg = msg[:-1] + f" with {unsupported_agg} expression."
         raise NotImplementedError(msg)
-
-    new_node = ir.reconstruct(children)
-    partition_info[new_node] = PartitionInfo(count=1)
-    return new_node, partition_info
+    else:  # pragma: no cover
+        new_node = ir.reconstruct([child])
+        partition_info[new_node] = PartitionInfo(count=1)
+        return new_node, partition_info
 
 
 @lower_ir_node.register(GroupBy)
 def _(
     ir: GroupBy, rec: LowerIRTransformer
 ) -> tuple[IR, MutableMapping[IR, PartitionInfo]]:
-    # Lower children
-    children, _partition_info = zip(*(rec(c) for c in ir.children), strict=True)
-    partition_info = reduce(operator.or_, _partition_info)
+    (child,) = ir.children
+    child, partition_info = rec(child)
 
-    if partition_info[children[0]].count == 1:
+    if partition_info[child].count == 1:
         # Single partition
-        return _single_fallback(ir, children, partition_info)
+        return _single_fallback(ir, child, partition_info)
 
     # Check that we are grouping on element-wise
     # keys (is this already guaranteed?)
     for ne in ir.keys:
         if not isinstance(ne.value, Col):  # pragma: no cover
-            return _single_fallback(ir, children, partition_info)
+            return _single_fallback(ir, child, partition_info)
 
     name_map: MutableMapping[str, Any] = {}
     agg_tree: Cast | Agg | None = None
@@ -84,7 +81,7 @@ def _(
             )
         elif isinstance(agg, Agg):
             if agg.name not in _GB_AGG_SUPPORTED:
-                return _single_fallback(ir, children, partition_info, agg)
+                return _single_fallback(ir, child, partition_info, agg)
 
             if agg.name in ("sum", "count"):
                 agg_requests_pwise.append(ne)
@@ -106,14 +103,11 @@ def _(
                     agg_pwise = Agg(dtype, sub, agg.options, *agg.children)
                     agg_requests_pwise.append(NamedExpr(tmp_name, agg_pwise))
                     # Tree
-                    child = Col(dtype, tmp_name)
-                    agg_tree = Agg(dtype, "sum", agg.options, child)
+                    agg_tree = Agg(dtype, "sum", agg.options, Col(dtype, tmp_name))
                     agg_requests_tree.append(NamedExpr(tmp_name, agg_tree))
         else:
             # Unsupported
-            return _single_fallback(
-                ir, children, partition_info, agg
-            )  # pragma: no cover
+            return _single_fallback(ir, child, partition_info, agg)  # pragma: no cover
 
     gb_pwise = GroupBy(
         ir.schema,
@@ -121,9 +115,9 @@ def _(
         agg_requests_pwise,
         ir.maintain_order,
         ir.options,
-        *children,
+        child,
     )
-    child_count = partition_info[children[0]].count
+    child_count = partition_info[child].count
     partition_info[gb_pwise] = PartitionInfo(count=child_count)
 
     gb_tree = GroupBy(
@@ -174,7 +168,7 @@ def _tree_node(do_evaluate, batch, *args):
 def _(
     ir: GroupBy, partition_info: MutableMapping[IR, PartitionInfo]
 ) -> MutableMapping[Any, Any]:
-    child = ir.children[0]
+    (child,) = ir.children
     child_count = partition_info[child].count
     child_name = get_key_name(child)
     output_count = partition_info[ir].count
