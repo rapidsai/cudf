688d60aa7c793744142602f845304d71a75730b0
Matthew Murray <matthewmurray711@gmail.com>
2024-12-10 15:09:31 -0800

    specialized template for DeviceNegate

--- cpp/include/cudf/unary.hpp
+++ cpp/include/cudf/unary.hpp
@@ -139,7 +139,7 @@ enum class unary_operator : int32_t {
   RINT,        ///< Rounds the floating-point argument arg to an integer value
   BIT_INVERT,  ///< Bitwise Not (~)
   NOT,         ///< Logical Not (!)
-  NEGATE,      ///< negate (-)
+  NEGATE,      ///< Unary negation (-), only for signed numeric types.
 };
 
 /**
