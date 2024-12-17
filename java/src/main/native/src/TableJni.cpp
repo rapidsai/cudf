a7ee992b297972c4f34f376ebfe0b8fbde2571ec
Yunsong Wang <yunsongw@nvidia.com>
2024-12-17 12:52:14 -0800

    Update java bindings

--- java/src/main/native/src/TableJni.cpp
+++ java/src/main/native/src/TableJni.cpp
@@ -2901,15 +2901,11 @@ JNIEXPORT jlongArray JNICALL Java_ai_rapids_cudf_Table_leftDistinctJoinGatherMap
     j_right_keys,
     compare_nulls_equal,
     [](cudf::table_view const& left, cudf::table_view const& right, cudf::null_equality nulleq) {
-      auto has_nulls = cudf::has_nested_nulls(left) || cudf::has_nested_nulls(right)
-                         ? cudf::nullable_join::YES
-                         : cudf::nullable_join::NO;
-      if (cudf::has_nested_columns(right)) {
-        cudf::distinct_hash_join<cudf::has_nested::YES> hash(right, left, has_nulls, nulleq);
-        return hash.left_join();
-      } else {
-        cudf::distinct_hash_join<cudf::has_nested::NO> hash(right, left, has_nulls, nulleq);
-        return hash.left_join();
+  auto has_nulls = cudf::has_nested_nulls(left) || cudf::has_nested_nulls(right)
+                     ? cudf::nullable_join::YES
+                     : cudf::nullable_join::NO;
+  cudf::distinct_hash_join hash(right, has_nulls, nulleq);
+  return hash.left_join(left);
       }
     });
 }
@@ -3125,13 +3121,8 @@ JNIEXPORT jlongArray JNICALL Java_ai_rapids_cudf_Table_innerDistinctJoinGatherMa
       std::pair<std::unique_ptr<rmm::device_uvector<cudf::size_type>>,
                 std::unique_ptr<rmm::device_uvector<cudf::size_type>>>
         maps;
-      if (cudf::has_nested_columns(right)) {
-        cudf::distinct_hash_join<cudf::has_nested::YES> hash(right, left, has_nulls, nulleq);
-        maps = hash.inner_join();
-      } else {
-        cudf::distinct_hash_join<cudf::has_nested::NO> hash(right, left, has_nulls, nulleq);
-        maps = hash.inner_join();
-      }
+      cudf::distinct_hash_join hash(right, has_nulls, nulleq);
+      maps = hash.inner_join(left);
       // Unique join returns {right map, left map} but all the other joins
       // return {left map, right map}. Swap here to make it consistent.
       return std::make_pair(std::move(maps.second), std::move(maps.first));
