3d6bb4cf5518cf1de7fddb4f86d3699979742b66
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-12-19 11:45:41 -0800

    Revert to old usage

--- python/pylibcudf/pylibcudf/interop.pyx
+++ python/pylibcudf/pylibcudf/interop.pyx
@@ -285,12 +285,7 @@ def _table_to_schema(Table tbl, metadata):
     if metadata is None:
         metadata = [_maybe_create_nested_column_metadata(col) for col in tbl.columns()]
     else:
-        metadata = [
-            _maybe_create_nested_column_metadata(m)
-            if isinstance(m, str)
-            else m
-            for m in metadata
-        ]
+        metadata = [ColumnMetadata(m) if isinstance(m, str) else m for m in metadata]
 
     cdef vector[column_metadata] c_metadata
     c_metadata.reserve(len(metadata))
