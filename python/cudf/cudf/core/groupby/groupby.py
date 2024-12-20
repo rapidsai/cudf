6fd6eabc1cfe675f79177c171efc29a81caae906
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-12-20 10:16:23 -0800

    Use less grouping.keys

--- python/cudf/cudf/core/groupby/groupby.py
+++ python/cudf/cudf/core/groupby/groupby.py
@@ -30,7 +30,7 @@ from cudf.core._compat import PANDAS_LT_300
 from cudf.core._internals import aggregation, sorting
 from cudf.core.abc import Serializable
 from cudf.core.buffer import acquire_spill_lock
-from cudf.core.column.column import ColumnBase, as_column
+from cudf.core.column.column import ColumnBase, as_column, column_empty
 from cudf.core.column_accessor import ColumnAccessor
 from cudf.core.copy_types import GatherMap
 from cudf.core.dtypes import (
@@ -745,7 +745,7 @@ class GroupBy(Serializable, Reducible, Scannable):
                 plc.Table(
                     [
                         col.to_pylibcudf(mode="read")
-                        for col in self.grouping.keys._columns
+                        for col in self.grouping._key_columns
                     ]
                 ),
                 plc.types.NullPolicy.EXCLUDE
@@ -1047,8 +1047,8 @@ class GroupBy(Serializable, Reducible, Scannable):
             ) and not _is_all_scan_aggregate(normalized_aggs):
                 # Even with `sort=False`, pandas guarantees that
                 # groupby preserves the order of rows within each group.
-                left_cols = list(self.grouping.keys.drop_duplicates()._columns)
-                right_cols = list(result_index._columns)
+                left_cols = self.grouping.keys.drop_duplicates()._columns
+                right_cols = result_index._columns
                 join_keys = [
                     _match_join_keys(lcol, rcol, "inner")
                     for lcol, rcol in zip(left_cols, right_cols)
@@ -2480,7 +2480,7 @@ class GroupBy(Serializable, Reducible, Scannable):
 
         column_pair_groupby = cudf.DataFrame._from_data(
             column_pair_structs
-        ).groupby(by=self.grouping.keys)
+        ).groupby(by=self.grouping)
 
         try:
             gb_cov_corr = column_pair_groupby.agg(func)
@@ -3070,7 +3070,7 @@ class DataFrameGroupBy(GroupBy, GetAttrGetItemMixin):
 
     def __getitem__(self, key):
         return self.obj[key].groupby(
-            by=self.grouping.keys,
+            by=self.grouping,
             dropna=self._dropna,
             sort=self._sort,
             group_keys=self._group_keys,
@@ -3504,7 +3504,9 @@ class _Grouping(Serializable):
                 self._handle_level(level)
         else:
             by_list = by if isinstance(by, list) else [by]
-
+            if not len(self._obj) and not len(by_list):
+                # We pretend to groupby an empty column
+                by_list = [cudf.Index._from_column(column_empty(0))]
             for by in by_list:
                 if callable(by):
                     self._handle_callable(by)
@@ -3529,13 +3531,9 @@ class _Grouping(Serializable):
     @property
     def keys(self):
         """Return grouping key columns as index"""
-        nkeys = len(self._key_columns)
-
-        if nkeys == 0:
-            return cudf.Index([], name=None)
-        elif nkeys > 1:
+        if len(self._key_columns) > 1:
             return cudf.MultiIndex._from_data(
-                dict(zip(range(nkeys), self._key_columns))
+                dict(enumerate(self._key_columns))
             )._set_names(self.names)
         else:
             return cudf.Index._from_column(
