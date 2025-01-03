022e2eb92894d156c6c7397ad6427af30c51fde1
Vyas Ramasubramani <vyasr@nvidia.com>
2025-01-03 03:22:16 +0000

    Make the cuda suffix available early.

--- ci/build_wheel_libcudf.sh
+++ ci/build_wheel_libcudf.sh
@@ -6,6 +6,8 @@ set -euo pipefail
 package_name="libcudf"
 package_dir="python/libcudf"
 
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+
 rapids-logger "Generating build requirements"
 
 rapids-dependency-file-generator \
@@ -34,8 +36,6 @@ export PIP_NO_BUILD_ISOLATION=0
 
 export SKBUILD_CMAKE_ARGS="-DUSE_NVCOMP_RUNTIME_WHEEL=ON"
 ./ci/build_wheel.sh "${package_name}" "${package_dir}"
-
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
 mkdir -p ${package_dir}/final_dist
 python -m auditwheel repair \
