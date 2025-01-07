2b578f572ef3f7a48c09cf82520d332c0c074e20
Shruti Shivakumar <shruti.shivakumar@gmail.com>
2025-01-07 22:28:06 +0000

    cleanup

--- cpp/src/io/json/read_json.cu
+++ cpp/src/io/json/read_json.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2025, NVIDIA CORPORATION.
+ * Copyright (c) 2022-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -310,7 +310,6 @@ table_with_metadata read_batch(host_span<std::unique_ptr<datasource>> sources,
   auto buffer =
     cudf::device_span<char const>(reinterpret_cast<char const*>(bufview.data()), bufview.size());
   stream.synchronize();
-
   return device_parse_nested_json(buffer, reader_opts, stream, mr);
 }
 
