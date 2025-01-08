6efc76d498a8b6e33f543845be985c59c5654a09
Lawrence Mitchell <lmitchell@nvidia.com>
2025-01-08 10:59:02 +0000

    Remove unnecessary includes

--- cpp/benchmarks/rolling/grouped_rolling_sum.cpp
+++ cpp/benchmarks/rolling/grouped_rolling_sum.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, NVIDIA CORPORATION.
+ * Copyright (c) 2024-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,8 +18,6 @@
 #include <benchmarks/fixture/benchmark_fixture.hpp>
 
 #include <cudf/aggregation.hpp>
-#include <cudf/copying.hpp>
-#include <cudf/detail/aggregation/aggregation.hpp>
 #include <cudf/rolling.hpp>
 #include <cudf/sorting.hpp>
 #include <cudf/utilities/default_stream.hpp>
