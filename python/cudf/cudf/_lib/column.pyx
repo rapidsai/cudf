171ebe3c1818707f0371c6326ad5d0ae9e7ccd80
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-01-10 10:50:31 -0800

    Add else:

--- python/cudf/cudf/_lib/column.pyx
+++ python/cudf/cudf/_lib/column.pyx
@@ -347,12 +347,13 @@ cdef class Column:
         if self._null_count is None:
             if not self.nullable or self.size == 0:
                 self._null_count = 0
-            with acquire_spill_lock():
-                self._null_count = pylibcudf.null_mask.null_count(
-                    self.base_mask.get_ptr(mode="read"),
-                    self.offset,
-                    self.offset + self.size
-                )
+            else:
+                with acquire_spill_lock():
+                    self._null_count = pylibcudf.null_mask.null_count(
+                        self.base_mask.get_ptr(mode="read"),
+                        self.offset,
+                        self.offset + self.size
+                    )
         return self._null_count
 
     @property
