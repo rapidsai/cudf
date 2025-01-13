1087402635cf98735a04c473499c7c7831b2a0a2
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-01-13 14:18:51 -0800

    Remomve breakpoint

--- python/cudf/cudf/core/multiindex.py
+++ python/cudf/cudf/core/multiindex.py
@@ -1795,7 +1795,6 @@ class MultiIndex(Frame, BaseIndex, NotIterable):
     @_performance_tracking
     def memory_usage(self, deep: bool = False) -> int:
         usage = sum(col.memory_usage for col in self._columns)
-        breakpoint()
         if self._levels is not None:
             usage += sum(
                 level.memory_usage(deep=deep) for level in self._levels
