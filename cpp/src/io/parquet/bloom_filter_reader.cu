96c600e02220a08f32562e0f6c629a70469a180f
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2025-01-15 23:26:39 +0000

    Put the literal type check back in query function

--- cpp/src/io/parquet/bloom_filter_reader.cu
+++ cpp/src/io/parquet/bloom_filter_reader.cu
@@ -70,6 +70,14 @@ struct bloom_filter_caster {
     using policy_type = cuco::arrow_filter_policy<key_type, cudf::hashing::detail::XXHash_64>;
     using word_type   = typename policy_type::word_type;
 
+    // Check if the literal has the same type as the predicate column
+    CUDF_EXPECTS(
+      dtype == literal->get_data_type() and
+        cudf::have_same_types(
+          cudf::column_view{dtype, 0, {}, {}, 0, 0, {}},
+          cudf::scalar_type_t<T>(T{}, false, stream, cudf::get_current_device_resource_ref())),
+      "Mismatched predicate column and literal types");
+
     // Filter properties
     auto constexpr bytes_per_block = sizeof(word_type) * policy_type::words_per_block;
 
@@ -139,13 +147,6 @@ struct bloom_filter_caster {
                   (cudf::is_compound<T>() and not std::is_same_v<T, string_view>)) {
       CUDF_FAIL("Bloom filters do not support boolean or compound types");
     } else {
-      // Check if the literal has the same type as the predicate column
-      CUDF_EXPECTS(
-        dtype == literal->get_data_type() and
-          cudf::have_same_types(
-            cudf::column_view{dtype, 0, {}, {}, 0, 0, {}},
-            cudf::scalar_type_t<T>(T{}, false, stream, cudf::get_current_device_resource_ref())),
-        "Mismatched predicate column and literal types");
       // For INT96 timestamps, use cudf::string_view type and set is_int96_timestamp to YES
       if constexpr (cudf::is_timestamp<T>()) {
         if (parquet_types[equality_col_idx] == Type::INT96) {
