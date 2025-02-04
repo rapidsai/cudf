6e3dd373c82239e45e4af7318cb2bec9ecba3ad7
Bradley Dice <bdice@bradleydice.com>
2025-02-03 19:29:14 -0600

    Add polars.

--- ci/test_narwhals.sh
+++ ci/test_narwhals.sh
@@ -5,7 +5,7 @@
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../ || exit
 
 # Common setup steps shared by Python test jobs
-source ./ci/test_python_common.sh test_python_cudf
+source ./ci/test_python_common.sh test_python_narwhals
 
 rapids-logger "Check GPU usage"
 nvidia-smi
@@ -18,13 +18,26 @@ rapids-logger "pytest narwhals"
 git clone https://github.com/narwhals-dev/narwhals --depth=1
 pushd narwhals || exit
 pip install -U -e ".[dev]"
+
+rapids-logger "Check narwhals versions"
 python -c "import narwhals; print(narwhals.show_versions())"
+
+rapids-logger "Run narwhals tests for cuDF"
 python -m pytest \
     --cache-clear \
     --junitxml="${RAPIDS_TESTS_DIR}/junit-cudf-narwhals.xml" \
     --numprocesses=8 \
     --dist=worksteal \
     --constructors=cudf
+
+rapids-logger "Run narwhals tests for cuDF Polars"
+NARWHALS_POLARS_GPU=1 python -m pytest \
+    --cache-clear \
+    --junitxml="${RAPIDS_TESTS_DIR}/junit-cudf-polars-narwhals.xml" \
+    --numprocesses=8 \
+    --dist=worksteal \
+    --constructors=polars[lazy]
+
 popd || exit
 
 rapids-logger "Test script exiting with value: $EXITCODE"
