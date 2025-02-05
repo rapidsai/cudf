169d9bbb349f1f26f9d72077a28fa862e16d9666
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-02-04 22:06:48 -0500

    Quick fix

--- cpp/src/io/utilities/datasource.cpp
+++ cpp/src/io/utilities/datasource.cpp
@@ -52,7 +52,7 @@ class file_source : public datasource {
   {
     kvikio_integration::set_up_kvikio();
     _kvikio_file = kvikio::FileHandle(filepath, "r");
-    CUDF_EXPECTS(_kvikio_file.closed(), "KvikIO did not open the file successfully.");
+    CUDF_EXPECTS(!_kvikio_file.closed(), "KvikIO did not open the file successfully.");
     CUDF_LOG_INFO("Reading a file using kvikIO, with compatibility mode %s.",
                   _kvikio_file.is_compat_mode_preferred() ? "on" : "off");
   }
