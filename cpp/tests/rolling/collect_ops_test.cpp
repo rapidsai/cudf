2410d4444092cde0a7b390e74094b8ef0a07b5b5
Yunsong Wang <yunsongw@nvidia.com>
2025-02-06 15:26:52 -0800

    Add cast_fn for type conversions

--- cpp/tests/rolling/collect_ops_test.cpp
+++ cpp/tests/rolling/collect_ops_test.cpp
@@ -455,7 +455,7 @@ TEST_F(CollectListTest, RollingWindowHonoursMinPeriodsWithDecimal)
 {
   // Test that when the number of observations is fewer than min_periods,
   // the result is null.
-  auto const input_iter = cudf::detail::make_counting_transform_iterator(0, cuda::std::identity{});
+  auto const input_iter   = thrust::counting_iterator{0};
   auto const input_column = cudf::test::fixed_point_column_wrapper<int32_t>{
     input_iter, input_iter + 6, numeric::scale_type{0}};
 
