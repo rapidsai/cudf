2a8d8ced6a0d00fd8beacb4888af34f955359dfd
David Wendt <dwendt@nvidia.com>
2025-02-06 11:59:27 -0500

    Add nvtext::tokenized_to_tensor API

--- cpp/include/nvtext/subword_tokenize.hpp
+++ cpp/include/nvtext/subword_tokenize.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2024, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 
 #include <cudf/column/column.hpp>
 #include <cudf/column/column_view.hpp>
+#include <cudf/lists/lists_column_view.hpp>
 #include <cudf/strings/strings_column_view.hpp>
 #include <cudf/utilities/export.hpp>
 #include <cudf/utilities/memory_resource.hpp>
@@ -159,6 +160,44 @@ tokenizer_result subword_tokenize(
   uint32_t max_sequence_length,
   uint32_t stride,
   bool do_lower_case,
+  bool do_truncate,
+  rmm::cuda_stream_view stream      = cudf::get_default_stream(),
+  rmm::device_async_resource_ref mr = cudf::get_current_device_resource_ref());
+
+/**
+ * @brief Converts the output of the wordpiece tokenizer to tensor data
+ * and metadata format
+ *
+ * For `do_truncate==true`:
+ * ```
+ * size of tensor_token_ids = max_sequence_length * input.size()
+ * size of tensor_attention_mask = max_sequence_length * input.size()
+ * size of tensor_metadata = 3 * input.size()
+ * ```
+ *
+ * For `do_truncate==false` the number of output rows depends on the
+ * number of tokens resolved and the `stride` value which may repeat tokens
+ * in subsequent overflow rows.
+ *
+ * @throw cudf::logic_error if `stride > max_sequence_length`
+ * @throw std::overflow_error if `max_sequence_length * max_rows_tensor`
+ *        exceeds the column size limit
+ *
+ * @param input The input tokens from wordpiece tokenizer or similar algorithm
+ * @param max_sequence_length Limit of the number of token-ids per row in the output
+ * @param stride Each output row will replicate the `max_sequence_length - stride`
+ *        token-ids from the previous row, unless it is the first row.
+ * @param do_truncate If true, the tokenizer will discard all the token-ids after
+ *        `max_sequence_length` for each input string. If false, it will use a new row
+ *        in the output token-ids to continue generating the output.
+ * @param stream CUDA stream used for device memory operations and kernel launches
+ * @param mr Memory resource to allocate any returned objects.
+ * @return token-ids, attention-mask, and metadata
+ */
+tokenizer_result tokenized_to_tensor(
+  cudf::lists_column_view const& input,
+  cudf::size_type max_sequence_length,
+  cudf::size_type stride,
   bool do_truncate,
   rmm::cuda_stream_view stream      = cudf::get_default_stream(),
   rmm::device_async_resource_ref mr = cudf::get_current_device_resource_ref());
