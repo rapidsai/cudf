1c2d408c3573affd65c5aa68e1b9e5c0be25a9e0
David Wendt <dwendt@nvidia.com>
2025-02-11 17:08:02 -0500

    fix doxygen, add more gtests

--- cpp/tests/text/minhash_tests.cpp
+++ cpp/tests/text/minhash_tests.cpp
@@ -17,6 +17,7 @@
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_utilities.hpp>
 #include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/debug_utilities.hpp>
 #include <cudf_test/iterator_utilities.hpp>
 
 #include <cudf/column/column.hpp>
@@ -82,41 +83,6 @@ TEST_F(MinHashTest, Permuted)
     LCW64{ 121498891461700668ul,  171065800427907402ul,  97466271004074331ul},
     LCW64{  54617739511834072ul,  231454301607238929ul,  97466271004074331ul},
     LCW64{ 576418665851990314ul,  231454301607238929ul,  97466271004074331ul}
-  });
-  // clang-format on
-  CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results64, expected64);
-}
-
-TEST_F(MinHashTest, Ngrams)
-{
-  using LCWS = cudf::test::lists_column_wrapper<cudf::string_view>;
-  auto input =
-    LCWS({LCWS{"The", "quick", "brown", "fox", "jumpéd", "over", "the", "lazy", "brown", "dog."},
-          LCWS{"The", "quick", "brown", "fox", "jumpéd", "over", "the", "lazy", "brown", "dog."}});
-
-  auto view = cudf::lists_column_view(input);
-
-  auto first  = thrust::counting_iterator<uint32_t>(10);
-  auto params = cudf::test::fixed_width_column_wrapper<uint32_t>(first, first + 3);
-  auto results =
-    nvtext::minhash_ngrams(view, 4, 0, cudf::column_view(params), cudf::column_view(params));
-  using LCW32 = cudf::test::lists_column_wrapper<uint32_t>;
-  // clang-format off
-  LCW32 expected({
-    LCW32{230924604u, 55492793u, 963436400u},
-    LCW32{230924604u, 55492793u, 963436400u}
-  });
-  // clang-format on
-  CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected);
-
-  auto params64 = cudf::test::fixed_width_column_wrapper<uint64_t, uint32_t>(first, first + 3);
-  auto results64 =
-    nvtext::minhash64_ngrams(view, 4, 0, cudf::column_view(params64), cudf::column_view(params64));
-  using LCW64 = cudf::test::lists_column_wrapper<uint64_t>;
-  // clang-format off
-  LCW64 expected64({
-    LCW64{ 208926840193078200ul, 576399628675212695ul, 312927673584437419ul},
-    LCW64{ 208926840193078200ul, 576399628675212695ul, 312927673584437419ul}
   });
   // clang-format on
   CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results64, expected64);
@@ -259,4 +225,79 @@ TEST_F(MinHashTest, ErrorsTest)
   EXPECT_THROW(
     nvtext::minhash64(view, 0, cudf::column_view(params64), cudf::column_view(empty64), 4),
     std::invalid_argument);
+}
+
+TEST_F(MinHashTest, Ngrams)
+{
+  using LCWS = cudf::test::lists_column_wrapper<cudf::string_view>;
+  auto input =
+    LCWS({LCWS{"The", "quick", "brown", "fox", "jumpéd", "over", "the", "lazy", "brown", "dog."},
+          LCWS{"The", "quick", "brown", "fox", "jumpéd", "over", "the", "lazy", "", "dog."},
+          LCWS{"short", "row"}});
+
+  auto view = cudf::lists_column_view(input);
+
+  auto first  = thrust::counting_iterator<uint32_t>(10);
+  auto params = cudf::test::fixed_width_column_wrapper<uint32_t>(first, first + 3);
+  auto results =
+    nvtext::minhash_ngrams(view, 4, 0, cudf::column_view(params), cudf::column_view(params));
+  using LCW32 = cudf::test::lists_column_wrapper<uint32_t>;
+  // clang-format off
+  LCW32 expected({
+    LCW32{ 230924604u,   55492793u, 963436400u},
+    LCW32{ 230924604u,  367515795u, 963436400u},
+    LCW32{2380648568u, 1330223236u, 279797904u}
+  });
+  // clang-format on
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected);
+
+  auto params64 = cudf::test::fixed_width_column_wrapper<uint64_t, uint32_t>(first, first + 3);
+  auto results64 =
+    nvtext::minhash64_ngrams(view, 4, 0, cudf::column_view(params64), cudf::column_view(params64));
+  using LCW64 = cudf::test::lists_column_wrapper<uint64_t>;
+  // clang-format off
+  LCW64 expected64({
+    LCW64{ 208926840193078200ul, 576399628675212695ul,  312927673584437419ul},
+    LCW64{ 677038498284219393ul, 326338087730412201ul,  298455901014050223ul},
+    LCW64{1493265692486268500ul, 720255058049417768ul, 2253087432826260995ul}
+  });
+  // clang-format on
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results64, expected64);
+}
+
+TEST_F(MinHashTest, NgramsWide)
+{
+  // using LCWS    = cudf::test::lists_column_wrapper<cudf::string_view>;
+  auto many     = std::vector<char const*>(1024, "hello");
+  auto str_data = cudf::test::strings_column_wrapper(many.begin(), many.end());
+  auto offsets =
+    cudf::test::fixed_width_column_wrapper<int32_t, uint64_t>({0ul, many.size() / 2, many.size()});
+  auto input = cudf::make_lists_column(2, offsets.release(), str_data.release(), 0, {});
+
+  auto view = cudf::lists_column_view(input->view());
+
+  auto first  = thrust::counting_iterator<uint32_t>(10);
+  auto params = cudf::test::fixed_width_column_wrapper<uint32_t>(first, first + 3);
+  auto results =
+    nvtext::minhash_ngrams(view, 4, 0, cudf::column_view(params), cudf::column_view(params));
+  using LCW32 = cudf::test::lists_column_wrapper<uint32_t>;
+  // clang-format off
+  LCW32 expected({
+    LCW32{ 571536396u, 2346676954u, 4121817512u},
+    LCW32{ 571536396u, 2346676954u, 4121817512u}
+  });
+  // clang-format on
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected);
+
+  auto params64 = cudf::test::fixed_width_column_wrapper<uint64_t, uint32_t>(first, first + 3);
+  auto results64 =
+    nvtext::minhash64_ngrams(view, 4, 0, cudf::column_view(params64), cudf::column_view(params64));
+  using LCW64 = cudf::test::lists_column_wrapper<uint64_t>;
+  // clang-format off
+  LCW64 expected64({
+    LCW64{ 1947142336021414174ul, 1219519365938078011ul, 491896395854741840ul},
+    LCW64{ 1947142336021414174ul, 1219519365938078011ul, 491896395854741840ul}
+  });
+  // clang-format on
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results64, expected64);
 }
