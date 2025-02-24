bbd0427e5588e0ff00b92a784f0b4bac67d37847
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2025-02-24 12:50:32 -0800

    cap default pool size

--- cpp/src/utilities/host_worker_pool.cpp
+++ cpp/src/utilities/host_worker_pool.cpp
@@ -22,7 +22,8 @@ namespace cudf::detail {
 
 BS::thread_pool& host_worker_pool()
 {
-  static const std::size_t default_pool_size = std::min(32u, std::thread::hardware_concurrency());
+  static const std::size_t default_pool_size =
+    std::min(32u, std::thread::hardware_concurrency() / 2);
   static const std::size_t pool_size = getenv_or("LIBCUDF_NUM_HOST_WORKERS", default_pool_size);
   static BS::thread_pool pool(pool_size);
   return pool;
