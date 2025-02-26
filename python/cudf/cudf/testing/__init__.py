8ce8718b771e0e019864e327bc0a9ac74ed2f1f0
Matt711 <matthewmurray711@gmail.com>
2025-02-26 09:47:59 -0500

    lazy import narwhals_tests_plugin

--- python/cudf/cudf/testing/__init__.py
+++ python/cudf/cudf/testing/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2020-2025, NVIDIA CORPORATION.
 
-from cudf.testing import narwhals_tests_plugin
+import importlib
+
 from cudf.testing.testing import (
     assert_eq,
     assert_frame_equal,
@@ -8,3 +9,10 @@ from cudf.testing.testing import (
     assert_neq,
     assert_series_equal,
 )
+
+
+def _import_narwhals_plugin():
+    importlib.import_module("cudf.testing.narwhals_tests_plugin")
+
+
+_import_narwhals_plugin()
