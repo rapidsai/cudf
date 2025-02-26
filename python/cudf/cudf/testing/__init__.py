18543268cef9fcc4c931bfc485862fef1549e58b
Vyas Ramasubramani <vyasr@nvidia.com>
2025-02-26 11:15:29 -0800

    Rename module so that it is installed into the wheel

--- python/cudf/cudf/testing/__init__.py
+++ python/cudf/cudf/testing/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2020-2025, NVIDIA CORPORATION.
 
-import cudf.testing.narwhals_tests_plugin as narwhals_tests_plugin
+from cudf.testing import narwhals_test_plugin
 from cudf.testing.testing import (
     assert_eq,
     assert_frame_equal,
