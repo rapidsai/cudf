ec82b2e0dd6fe250deb38e3fc6cfc67291cac54a
Vyas Ramasubramani <vyasr@nvidia.com>
2025-02-26 11:07:50 -0800

    Revert "lazy import narwhals_tests_plugin"
    This reverts commit 8ce8718b771e0e019864e327bc0a9ac74ed2f1f0.

--- python/cudf/cudf/testing/__init__.py
+++ python/cudf/cudf/testing/__init__.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2020-2025, NVIDIA CORPORATION.
 
-import importlib
-
+from cudf.testing import narwhals_tests_plugin
 from cudf.testing.testing import (
     assert_eq,
     assert_frame_equal,
@@ -9,10 +8,3 @@ from cudf.testing.testing import (
     assert_neq,
     assert_series_equal,
 )
-
-
-def _import_narwhals_plugin():
-    importlib.import_module("cudf.testing.narwhals_tests_plugin")
-
-
-_import_narwhals_plugin()
