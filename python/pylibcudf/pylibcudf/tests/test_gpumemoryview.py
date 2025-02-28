33bf65c55ced04d54f15bfcc30bc96e7ea537c78
Peter Andreas Entschev <peter@entschev.com>
2025-02-28 06:24:08 -0800

    Add gpumemoryview tests for `len()`/`nbytes`

---
+++ python/pylibcudf/pylibcudf/tests/test_gpumemoryview.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.
+
+import itertools
+
+import numpy as np
+import pytest
+
+import rmm
+
+import pylibcudf as plc
+
+DTYPES = [
+    "u1",
+    "i2",
+    "f4",
+    "f8",
+    "f16",
+]
+SIZES = [
+    0,
+    1,
+    1000,
+    1024,
+    10000,
+]
+
+
+@pytest.fixture(params=tuple(itertools.product(SIZES, DTYPES)), ids=repr)
+def np_array(request):
+    size, dtype = request.param
+    return np.empty((size,), dtype=dtype)
+
+
+def test_len_nbytes(np_array):
+    buf = rmm.DeviceBuffer(
+        ptr=np_array.__array_interface__["data"][0], size=np_array.nbytes
+    )
+    gpumemview = plc.gpumemoryview(buf)
+
+    np_array_view = np_array.view("u1")
+
+    assert len(gpumemview) == len(np_array_view)
+    assert gpumemview.nbytes == np_array.nbytes
