bee1f080bfd78fbc646706d6184eb633397f5cb5
Matt711 <matthewmurray711@gmail.com>
2025-03-04 09:53:37 -0500

    xfail Narwhals over tests

--- python/cudf/cudf/testing/narwhals_test_plugin.py
+++ python/cudf/cudf/testing/narwhals_test_plugin.py
@@ -12,6 +12,24 @@ if TYPE_CHECKING:
 
 EXPECTED_FAILURES: Mapping[str, str] = {
     "tests/frame/select_test.py::test_select_duplicates[cudf]": "cuDF doesn't support having multiple columns with same names",
+    "tests/expr_and_series/over_test.py::test_over_cummin[cudf]": "NotImplementedError: Passing kwargs to func is currently not supported",
+    "tests/expr_and_series/over_test.py::test_over_anonymous_cumulative[cudf]": "NotImplementedError: Passing kwargs to func is currently not supported",
+    "tests/expr_and_series/over_test.py::test_over_cummax[cudf]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/over_test.py::test_over_cumprod[cudf]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/over_test.py::test_over_cum_reverse[cudf-cum_max-expected_b0]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/over_test.py::test_over_shift[cudf]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/over_test.py::test_over_cumcount[cudf]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/over_test.py::test_over_cum_reverse[cudf-cum_prod-expected_b4]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/over_test.py::test_over_diff[cudf]": "AttributeError: type object 'Aggregation' has no attribute 'diff'",
+    "tests/expr_and_series/over_test.py::test_over_cum_reverse[cudf-cum_count-expected_b3]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/over_test.py::test_over_cum_reverse[cudf-cum_sum-expected_b2]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/over_test.py::test_over_cum_reverse[cudf-cum_min-expected_b1]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/over_test.py::test_over_cumsum[cudf]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/rank_test.py::test_rank_expr_in_over_context[cudf-ordinal]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/rank_test.py::test_rank_expr_in_over_context[cudf-dense]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/rank_test.py::test_rank_expr_in_over_context[cudf-min]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/rank_test.py::test_rank_expr_in_over_context[cudf-average]": "NotImplementedError: Passing kwargs to func is currently not supported.",
+    "tests/expr_and_series/rank_test.py::test_rank_expr_in_over_context[cudf-max]": "NotImplementedError: Passing kwargs to func is currently not supported.",
 }
 
 
