fe4faa843723a2e1bffe38e3072a477da4167361
Shruti Shivakumar <shruti.shivakumar@gmail.com>
2025-03-06 00:08:46 +0000

    static libzstd link

---
+++ cpp/cmake/thirdparty/get_zstd.cmake
@@ -0,0 +1,39 @@
+# =============================================================================
+# Copyright (c) 2025, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+# =============================================================================
+
+# Use CPM to find or clone libzstd
+function(find_and_configure_zstd)
+
+  set(CPM_DOWNLOAD_zstd ON)
+  rapids_cpm_find(
+    zstd 1.5.7
+    GLOBAL_TARGETS libzstd_static
+    CPM_ARGS
+    GIT_REPOSITORY https://github.com/facebook/zstd.git
+    GIT_TAG v1.5.7
+    GIT_SHALLOW FALSE SOURCE_SUBDIR build/cmake
+    OPTIONS "ZSTD_BUILD_STATIC ON" "ZSTD_BUILD_SHARED OFF" "ZSTD_BUILD_TESTS OFF"
+            "ZSTD_BUILD_PROGRAMS OFF"
+  )
+
+  if(DEFINED zstd_SOURCE_DIR)
+    set(ZSTD_INCLUDE_DIR
+        "${zstd_SOURCE_DIR}/lib"
+        PARENT_SCOPE
+    )
+  endif()
+
+endfunction()
+
+find_and_configure_zstd()
