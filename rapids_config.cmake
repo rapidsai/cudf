2581994643705a21cda73e55063790d4ceb854c4
Vyas Ramasubramani <vyasr@nvidia.com>
2025-03-06 21:40:30 +0000

    Revert rapids-cmake customization

--- rapids_config.cmake
+++ rapids_config.cmake
@@ -1,5 +1,5 @@
 # =============================================================================
-# Copyright (c) 2018-2025, NVIDIA CORPORATION.
+# Copyright (c) 2018-2024, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -26,8 +26,6 @@ else()
   )
 endif()
 
-set(rapids-cmake-repo "vyasr/rapids-cmake")
-set(rapids-cmake-branch "feat/nvbench_static_fmt")
 if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake")
   file(
     DOWNLOAD
