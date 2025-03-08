799a4a135fb1fdb36ec05927f6dad734e885f246
Bradley Dice <bdice@bradleydice.com>
2025-03-08 00:01:36 -0600

    Use testing branch of cccl.

--- rapids_config.cmake
+++ rapids_config.cmake
@@ -13,7 +13,7 @@
 # =============================================================================
 
 set(rapids-cmake-repo bdice/rapids-cmake)
-set(rapids-cmake-branch cccl-2.8.0)
+set(rapids-cmake-branch cccl-2.8.x-testing)
 
 file(READ "${CMAKE_CURRENT_LIST_DIR}/VERSION" _rapids_version)
 if(_rapids_version MATCHES [[^([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9])]])
