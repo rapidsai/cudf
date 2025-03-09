e5a9d7ee74f46487e0fc07e1327f1ccb1352d4e1
David Wendt <dwendt@nvidia.com>
2025-03-09 15:36:44 -0400

    change test; pare pr.yaml

--- ci/run_cudf_ctests.sh
+++ ci/run_cudf_ctests.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/gtests/libcudf/";
 
 #ctest --output-on-failure --no-tests=error "$@"
-compute-sanitizer ./TEXT_TEST --gtest_filter=TextBytePairEncoding.BytePairEncoding --rmm_mode=cuda
+compute-sanitizer ./TEXT_TEST --gtest_filter=TextSubwordTest.Tokenize --rmm_mode=cuda
