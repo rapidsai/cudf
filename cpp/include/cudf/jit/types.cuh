18114dfcc9f89e1cf8b0508df2603c01361dcc37
Basit Ayantunde <rlamarrr@gmail.com>
2025-03-10 19:03:10 +0000

    updated code docs

--- cpp/include/cudf/jit/types.cuh
+++ cpp/include/cudf/jit/types.cuh
@@ -23,31 +23,57 @@
 namespace cudf {
 namespace jit {
 
-/// @brief This is a minified version of `cudf::mutable_column_device_view` for use in JIT kernels.
+/// @brief This is a minified version of `cudf::column_device_view` for use in JIT kernels.
 struct mutable_column_device_view {
  protected:
-  /// @copydoc cudf::column_device_view::_type
+  /**
+   * @copydoc cudf::column_device_view::_type
+   */
   data_type _type = {};
 
-  /// @copydoc cudf::column_device_view::_size
+  /**
+   * @copydoc cudf::column_device_view::_size
+   */
   size_type _size = 0;
 
-  /// @copydoc cudf::column_device_view::_data
+  /**
+   * @copydoc cudf::column_device_view::_data
+   */
   void* _data = nullptr;
 
-  /// @copydoc cudf::column_device_view::_null_mask
+  /**
+   * @copydoc cudf::column_device_view::_null_mask
+   */
   bitmask_type const* _null_mask = nullptr;
 
-  /// @copydoc cudf::column_device_view::_offset
+  /**
+   * @copydoc cudf::column_device_view::_offset
+   */
   size_type _offset = 0;
 
-  /// @copydoc cudf::column_device_view::d_children
+  /**
+   * @copydoc cudf::column_device_view::d_children
+   */
   [[maybe_unused]] void* _children = nullptr;
 
-  /// @copydoc cudf::column_device_view::_num_children
+  /**
+   * @copydoc cudf::column_device_view::_num_children
+   */
   [[maybe_unused]] size_type _num_children = 0;
 
  public:
+  /**
+   * @brief Creates an instance of this class using pre-existing device memory pointers to data,
+   * nullmask, and offset.
+   *
+   * @param type The type of the column
+   * @param size The number of elements in the column
+   * @param data Pointer to the device memory containing the data
+   * @param null_mask Pointer to the device memory containing the null bitmask
+   * @param offset The index of the first element in the column
+   * @param children Pointer to the device memory containing child data
+   * @param num_children The number of child columns
+   */
   mutable_column_device_view(data_type type,
                              size_type size,
                              void* data,
@@ -64,6 +90,9 @@ struct mutable_column_device_view {
   {
   }
 
+  /**
+   * @copydoc cudf::column_device_view::head
+   */
   template <typename T = void,
             CUDF_ENABLE_IF(cuda::std::is_same_v<T, void> or is_rep_layout_compatible<T>())>
   [[nodiscard]] CUDF_HOST_DEVICE T* head() const noexcept
@@ -71,56 +100,95 @@ struct mutable_column_device_view {
     return static_cast<T*>(_data);
   }
 
+  /**
+   * @copydoc cudf::column_device_view::data
+   */
   template <typename T>
   CUDF_HOST_DEVICE T* data() const
   {
     return head<T>() + _offset;
   }
 
+  /**
+   * @copydoc cudf::column_device_view::size
+   */
   [[nodiscard]] CUDF_HOST_DEVICE size_type size() const noexcept { return _size; }
 
+  /**
+   * @copydoc cudf::column_device_view::type
+   */
   [[nodiscard]] CUDF_HOST_DEVICE data_type type() const noexcept { return _type; }
 
+  /**
+   * @copydoc cudf::column_device_view::nullable
+   */
   [[nodiscard]] CUDF_HOST_DEVICE bool nullable() const noexcept { return nullptr != _null_mask; }
 
+  /**
+   * @copydoc cudf::column_device_view::null_mask
+   */
   [[nodiscard]] CUDF_HOST_DEVICE bitmask_type const* null_mask() const noexcept
   {
     return _null_mask;
   }
 
+  /**
+   * @copydoc cudf::column_device_view::offset
+   */
   [[nodiscard]] CUDF_HOST_DEVICE size_type offset() const noexcept { return _offset; }
 
+  /**
+   * @copydoc cudf::column_device_view::is_valid
+   */
   [[nodiscard]] __device__ bool is_valid(size_type element_index) const noexcept
   {
     return not nullable() or is_valid_nocheck(element_index);
   }
 
+  /**
+   * @copydoc cudf::column_device_view::is_valid_nocheck
+   */
   [[nodiscard]] __device__ bool is_valid_nocheck(size_type element_index) const noexcept
   {
     return bit_is_set(_null_mask, offset() + element_index);
   }
 
+  /**
+   * @copydoc cudf::column_device_view::is_null
+   */
   [[nodiscard]] __device__ bool is_null(size_type element_index) const noexcept
   {
     return not is_valid(element_index);
   }
 
+  /**
+   * @copydoc cudf::column_device_view::is_null_nocheck
+   */
   [[nodiscard]] __device__ bool is_null_nocheck(size_type element_index) const noexcept
   {
     return not is_valid_nocheck(element_index);
   }
 
+  /**
+   * @copydoc cudf::column_device_view::get_mask_word
+   */
   [[nodiscard]] __device__ bitmask_type get_mask_word(size_type word_index) const noexcept
   {
     return null_mask()[word_index];
   }
 
+  /**
+   * @copydoc cudf::column_device_view::element
+   */
   template <typename T, CUDF_ENABLE_IF(is_rep_layout_compatible<T>())>
   [[nodiscard]] __device__ T& element(size_type element_index) const noexcept
   {
     return data<T>()[element_index];
   }
 
+  /**
+   * @copydoc cudf::column_device_view::element
+   */
   template <typename T, CUDF_ENABLE_IF(is_fixed_point<T>())>
   [[nodiscard]] __device__ T element(size_type element_index) const noexcept
   {
@@ -130,12 +198,27 @@ struct mutable_column_device_view {
     return T{scaled_integer<rep>{data<rep>()[element_index], scale}};
   }
 
+  /**
+   * @brief Assigns `value` to the element at `element_index`
+   *
+   * @tparam T The element type
+   * @param element_index Position of the desired element
+   * @param value The value to assign
+   */
   template <typename T, CUDF_ENABLE_IF(is_rep_layout_compatible<T>())>
   __device__ void assign(size_type element_index, T value) const noexcept
   {
     data<T>()[element_index] = value;
   }
 
+  /**
+   * @brief Assigns `value` to the element at `element_index`.
+   * @warning Expects that `value` has been scaled to the column's scale
+   *
+   * @tparam T The element type
+   * @param element_index Position of the desired element
+   * @param value The value to assign
+   */
   template <typename T, CUDF_ENABLE_IF(is_fixed_point<T>())>
   __device__ void assign(size_type element_index, T value) const noexcept
   {
