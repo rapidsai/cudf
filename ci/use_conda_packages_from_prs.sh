a7cdfcb869830e82d985ebf51b7c4a8d8e23e998
jakirkham <jakirkham@gmail.com>
2025-03-12 22:19:42 -0700

    Add `file://` before local channels

--- ci/use_conda_packages_from_prs.sh
+++ ci/use_conda_packages_from_prs.sh
@@ -8,4 +8,4 @@ RMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1858 python)
 # make sure they can be found locally
 conda config --system --add channels "${LIBRMM_CHANNEL}"
 conda config --system --add channels "${RMM_CHANNEL}"
-RATTLER_CHANNELS=("-c ${RMM_CHANNEL}" "-c ${LIBRMM_CHANNEL}" "${RATTLER_CHANNELS[@]}")
+RATTLER_CHANNELS=("-c file://${RMM_CHANNEL}" "-c file://${LIBRMM_CHANNEL}" "${RATTLER_CHANNELS[@]}")
