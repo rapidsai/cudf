a3c2fc4120f0d61836954471f803468202e3fe29
Lawrence Mitchell <lmitchell@nvidia.com>
2025-03-18 18:29:33 +0000

    Expose grouped_range_rolling_window in pylibcudf

--- python/pylibcudf/pylibcudf/aggregation.pxd
+++ python/pylibcudf/pylibcudf/aggregation.pxd
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
 from libcpp.memory cimport unique_ptr
 from pylibcudf.libcudf.aggregation cimport (
@@ -45,6 +45,7 @@ cdef class Aggregation:
     cdef const reduce_aggregation* view_underlying_as_reduce(self) except *
     cdef const scan_aggregation* view_underlying_as_scan(self) except *
     cdef const rolling_aggregation* view_underlying_as_rolling(self) except *
+    cdef unique_ptr[rolling_aggregation] clone_underlying_as_rolling(self) except *
 
     @staticmethod
     cdef Aggregation from_libcudf(unique_ptr[aggregation] agg)
--- python/pylibcudf/pylibcudf/rolling.pyi
+++ python/pylibcudf/pylibcudf/rolling.pyi
@@ -1,8 +1,35 @@
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
 from pylibcudf.aggregation import Aggregation
 from pylibcudf.column import Column
+from pylibcudf.scalar import Scalar
+from pylibcudf.table import Table
+from pylibcudf.types import NullOrder, Order
 
+class Unbounded: ...
+class CurrentRow: ...
+
+class BoundedClosed:
+    def __init__(self, delta: Scalar) -> None: ...
+
+class BoundedOpen:
+    def __init__(self, delta: Scalar) -> None: ...
+
+class RollingRequest:
+    def __init__(self, values: Column, aggregation: Aggregation) -> None: ...
+
+RangeWindowType = BoundedClosed | BoundedOpen | CurrentRow | Unbounded
+
+def grouped_range_rolling_window(
+    group_keys: Table,
+    orderby: Column,
+    order: Order,
+    null_order: NullOrder,
+    preceding: RangeWindowType,
+    following: RangeWindowType,
+    min_periods: int,
+    requests: list[RollingRequest],
+) -> Table: ...
 def rolling_window[WindowType: (Column, int)](
     source: Column,
     preceding_window: WindowType,
