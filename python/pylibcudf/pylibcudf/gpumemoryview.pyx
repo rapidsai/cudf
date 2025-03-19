8a588ab902346f1392c6314a0e6cc00519bbbe83
Vyas Ramasubramani <vyasr@nvidia.com>
2025-03-19 13:33:28 -0700

    Support constructing pylibcudf Columns and Tables from views into arbitrary objects (#18314)
    The new constructors are necessary when we need to produce views into data that is not owned by one of the standard libcudf types (column/table). The original motivating example is [contiguous_split](https://github.com/rapidsai/cudf/pull/17012#discussion_r1791839645), so that API is modified in this PR for testing.
    
    Closes #17040
    Supersedes #17543
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - Matthew Murray (https://github.com/Matt711)
    
    URL: https://github.com/rapidsai/cudf/pull/18314

--- python/pylibcudf/pylibcudf/gpumemoryview.pyx
+++ python/pylibcudf/pylibcudf/gpumemoryview.pyx
@@ -26,6 +26,26 @@ cdef class gpumemoryview:
         # TODO: Need to respect readonly
         self.ptr = cai["data"][0]
 
+    @staticmethod
+    cdef gpumemoryview from_pointer(Py_ssize_t ptr, object owner):
+        """Create a gpumemoryview from a pointer and an owning object.
+
+        Parameters
+        ----------
+        ptr : Py_ssize_t
+            The pointer to the memory.
+        owner : object
+            The object that owns the data the pointer points to.
+
+        Returns
+        -------
+        gpumemoryview
+        """
+        cdef gpumemoryview out = gpumemoryview.__new__(gpumemoryview)
+        out.obj = owner
+        out.ptr = ptr
+        return out
+
     @property
     def __cuda_array_interface__(self):
         return self.obj.__cuda_array_interface__
