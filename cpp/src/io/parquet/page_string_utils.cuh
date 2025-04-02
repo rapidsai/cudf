93cf86fd7b138a4af6b74de38940004a0a614bd0
Muhammad Haseeb <mhaseeb@nvidia.com>
2025-04-02 22:27:01 +0000

    minor improvements

--- cpp/src/io/parquet/page_string_utils.cuh
+++ cpp/src/io/parquet/page_string_utils.cuh
@@ -175,7 +175,7 @@ __device__ void update_string_offsets_for_pruned_pages(
   // column construction. Otherwise, convert string sizes to final offsets
   if (state->col.is_large_string_col) {
     // Write zero string sizes
-    for (int idx = tid; idx < value_count; idx += blockDim.x) {
+    for (int idx = tid; idx < value_count; idx += block_size) {
       offptr[idx] = 0;
     }
     // page.chunk_idx are ordered by input_col_idx and row_group_idx respectively
@@ -188,7 +188,7 @@ __device__ void update_string_offsets_for_pruned_pages(
     if (not has_lists) { value_count -= state->first_row; }
 
     // Write the initial offset at all positions to indicate zero sized strings
-    for (int idx = tid; idx < value_count; idx += blockDim.x) {
+    for (int idx = tid; idx < value_count; idx += block_size) {
       offptr[idx] = initial_value;
     }
   }
