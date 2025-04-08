4754c7e2d6a2c6333ad48ab8b85c6792f8eac557
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2025-04-08 20:53:52 +0000

    Minor code cleanup

--- cpp/src/io/parquet/decode_fixed.cu
+++ cpp/src/io/parquet/decode_fixed.cu
@@ -581,26 +581,27 @@ static __device__ int gpuUpdateValidityAndRowIndicesNonNullable(int32_t target_v
  * @param s Pointer to page state containing column and nesting information.
  */
 template <int decode_block_size>
-static __device__ void update_list_offsets_for_pruned_pages(page_state_s* s)
+static __device__ void update_list_offsets_for_pruned_pages(page_state_s* state)
 {
   namespace cg = cooperative_groups;
 
-  int constexpr start_depth   = 0;
-  int const max_depth         = s->col.max_nesting_depth - 1;
-  int const in_nesting_bounds = (0 >= start_depth && 0 <= max_depth);
-  auto const t                = cg::this_thread_block().thread_rank();
+  int constexpr start_depth    = 0;
+  int const max_depth          = state->col.max_nesting_depth - 1;
+  bool const in_nesting_bounds = (0 >= start_depth and 0 <= max_depth);
+  auto const tid               = cg::this_thread_block().thread_rank();
 
   // Iterate by depth and store offset(s) to the list location(s)
-  for (int d_idx = t; d_idx <= max_depth; d_idx += decode_block_size) {
-    auto& ni = s->nesting_info[d_idx];
+  for (int depth = tid; depth <= max_depth; depth += decode_block_size) {
+    auto& nesting_info = state->nesting_info[depth];
     // If we're -not- at a leaf column and we're within nesting/row bounds and we have a valid
     // data_out pointer, it implies this is a list column, so emit an offset for the current nesting
     // level equal to current length of the next nesting level
-    if (in_nesting_bounds && ni.data_out != nullptr) {
-      auto const& next_ni          = s->nesting_info[d_idx + 1];
-      int const idx                = ni.value_count;
-      cudf::size_type const offset = next_ni.value_count + next_ni.page_start_value;
-      (reinterpret_cast<cudf::size_type*>(ni.data_out))[idx] = offset;
+    if (in_nesting_bounds and nesting_info.data_out != nullptr) {
+      auto const& next_nesting_info = state->nesting_info[depth + 1];
+      int const idx                 = nesting_info.value_count;
+      cudf::size_type const offset =
+        next_nesting_info.value_count + next_nesting_info.page_start_value;
+      (reinterpret_cast<cudf::size_type*>(nesting_info.data_out))[idx] = offset;
     }
   }
 }
