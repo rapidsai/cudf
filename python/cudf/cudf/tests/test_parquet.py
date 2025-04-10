1febc521d7444ef29cbe0fe80222989bd9f1bf99
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2025-04-10 16:11:03 -0700

    style pls

--- python/cudf/cudf/tests/test_parquet.py
+++ python/cudf/cudf/tests/test_parquet.py
@@ -4458,4 +4458,4 @@ def test_parquet_reader_empty_compressed_page(datadir):
     fname = datadir / "empty_datapage_v2.parquet"
 
     df = cudf.DataFrame({"value": cudf.Series([None], dtype="float32")})
-    assert_eq(cudf.read_parquet(fname),df)
+    assert_eq(cudf.read_parquet(fname), df)
