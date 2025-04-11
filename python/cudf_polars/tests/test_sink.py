0fc665e5738688f143a7807efdf8851867b35383
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-04-11 01:23:37 +0000

    pre-commit

--- python/cudf_polars/tests/test_sink.py
+++ python/cudf_polars/tests/test_sink.py
@@ -9,6 +9,8 @@ import pytest
 import polars as pl
 from polars.testing.asserts import assert_frame_equal
 
+from cudf_polars.testing.asserts import assert_collect_raises
+
 
 @pytest.fixture(scope="module")
 def df():
@@ -24,14 +26,7 @@ def df():
 @pytest.mark.parametrize("null_value", [None, "NA"])
 @pytest.mark.parametrize("line_terminator", ["\n", "\n\n"])
 @pytest.mark.parametrize("separator", [",", "|"])
-def test_sink_csv(
-    df,
-    tmp_path,
-    include_header,
-    null_value,
-    line_terminator,
-    separator
-):
+def test_sink_csv(df, tmp_path, include_header, null_value, line_terminator, separator):
     path_polars = tmp_path / "polars.csv"
     path_cudf = tmp_path / "cudf.csv"
 
@@ -40,7 +35,7 @@ def test_sink_csv(
         include_header=include_header,
         null_value=null_value,
         line_terminator=line_terminator,
-        separator=separator
+        separator=separator,
     )
     df.sink_csv(
         str(path_cudf),
@@ -48,29 +43,31 @@ def test_sink_csv(
         include_header=include_header,
         null_value=null_value,
         line_terminator=line_terminator,
-        separator=separator
+        separator=separator,
     )
-    
+
     expected = path_polars.read_text()
     result = path_cudf.read_text()
     assert result == expected
 
 
-@pytest.mark.parametrize("kwargs, value", [
-    ("include_bom", True),
-    ("date_format", "%Y-%m-%d"),
-    ("time_format", "%H:%M:%S"),
-    ("datetime_format", "%Y-%m-%dT%H:%M:%S"),
-    ("float_scientific", True),
-    ("float_precision", 10),
-    ("quote_style", "non_numeric"),
-    ("quote_char", "`"),
-])
+@pytest.mark.parametrize(
+    "kwargs, value",
+    [
+        ("include_bom", True),
+        ("date_format", "%Y-%m-%d"),
+        ("time_format", "%H:%M:%S"),
+        ("datetime_format", "%Y-%m-%dT%H:%M:%S"),
+        ("float_scientific", True),
+        ("float_precision", 10),
+        ("quote_style", "non_numeric"),
+        ("quote_char", "`"),
+    ],
+)
 def test_sink_csv_unsupported_kwargs(df, tmp_path, kwarg, value):
     path = tmp_path / "unsupported.csv"
     q = df.sink_csv(str(path), **{kwarg: value})
     assert_collect_raises(q, cudf_except=NotImplementedError)
-
 
 
 def test_sink_ndjson(df, tmp_path):
@@ -99,10 +96,9 @@ def test_sink_parquet(df, tmp_path):
     assert_frame_equal(result, expected)
 
 
-@pytest.mark.parametrize("kwarg, value", [
-    ("compression_level", 10),
-    ("compression", "brotli")
-])
+@pytest.mark.parametrize(
+    "kwarg, value", [("compression_level", 10), ("compression", "brotli")]
+)
 def test_sink_parquet_unsupported_kwargs(df, tmp_path, kwarg, value):
     path = tmp_path / "unsupported.pq"
     q = df.sink_parquet(str(path), **{kwarg: value})
