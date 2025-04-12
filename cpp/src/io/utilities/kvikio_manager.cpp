d712cd95bea16432662b6baae5e8317e7a4c6761
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-04-12 02:52:32 -0400

    Improve implementation

--- cpp/src/io/utilities/kvikio_manager.cpp
+++ cpp/src/io/utilities/kvikio_manager.cpp
@@ -17,6 +17,7 @@
 #include "getenv_or.hpp"
 
 #include <cudf/io/kvikio_manager.hpp>
+#include <cudf/logger.hpp>
 #include <cudf/utilities/error.hpp>
 
 #include <kvikio/defaults.hpp>
@@ -33,14 +34,33 @@ kvikio_manager::kvikio_manager()
   // initialized.
   cudaFree(nullptr);
 
-  auto env_var_setting = kvikio::defaults::thread_pool_nthreads();
-  auto default_setting = get_default_num_io_threads();
-  if (env_var_setting != default_setting) {
-    kvikio::defaults::set_thread_pool_nthreads(default_setting);
-  }
-
   auto const compat_mode = kvikio::getenv_or("KVIKIO_COMPAT_MODE", kvikio::CompatMode::ON);
   kvikio::defaults::set_compat_mode(compat_mode);
+
+  std::string const env_var_name{"KVIKIO_NTHREADS"};
+  auto const env_val = std::getenv(env_var_name.c_str());
+  if (env_val != nullptr) {
+    // If the env var KVIKIO_NTHREADS is set, KvikIO will create the thread pool with
+    // KVIKIO_NTHREADS when the singleton class kvikio::defaults is first instantiated via the above
+    // kvikio::defaults::set_compat_mode() call. In this case, cuDF's default value is not derived
+    // here so as to avoid the overhead of NVML initialization.
+    CUDF_LOG_INFO("Environment variable %.*s read as %s",
+                  static_cast<int>(env_var_name.length()),
+                  env_var_name.data(),
+                  env_val);
+  } else {
+    // If the env var KVIKIO_NTHREADS is not set, KvikIO will create the thread pool with its own
+    // default value M. Here if cuDF's derived default value N is not equal to M, the existing
+    // thread pool will be destroyed and a new one with N threads created.
+    auto default_val = get_default_num_io_threads();
+    std::stringstream ss;
+    ss << default_val;
+    CUDF_LOG_INFO("Environment variable %.*s is not set, using default value %s",
+                  static_cast<int>(env_var_name.length()),
+                  env_var_name.data(),
+                  ss.str());
+    set_num_io_threads(default_val);
+  }
 }
 
 kvikio_manager& kvikio_manager::instance()
