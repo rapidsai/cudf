11dad262b8b0ed2c3effe6fe91e842b00f6765c6
Bradley Dice <bdice@bradleydice.com>
2025-04-15 11:05:31 -0500

    Remove comments.

--- cpp/examples/set_cuda_architecture.cmake
+++ cpp/examples/set_cuda_architecture.cmake
@@ -14,10 +14,6 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/versions.cmake)
 
-# if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/libcudf_cpp_examples_RAPIDS.cmake) file(DOWNLOAD
-# https://raw.githubusercontent.com/rapidsai/rapids-cmake/${CUDF_TAG}/RAPIDS.cmake
-# ${CMAKE_CURRENT_BINARY_DIR}/libcudf_cpp_examples_RAPIDS.cmake ) endif()
-# include(${CMAKE_CURRENT_BINARY_DIR}/libcudf_cpp_examples_RAPIDS.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../../cmake/rapids_config.cmake)
 
 include(rapids-cmake)
