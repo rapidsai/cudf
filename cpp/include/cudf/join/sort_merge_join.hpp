8444cbb8f6def8b4f2a380ec15d05115151771e4
Shruti Shivakumar <shruti.shivakumar@gmail.com>
2025-04-15 22:57:14 +0000

    passed formatting

--- cpp/include/cudf/join/sort_merge_join.hpp
+++ cpp/include/cudf/join/sort_merge_join.hpp
@@ -50,18 +50,21 @@ namespace CUDF_EXPORT cudf {
  * @file
  */
 
+/**
+ * @brief Class that implements sort-merge algorithm for table joins
+ */
 class sort_merge_join {
  public:
   /**
-   * @brief Construct a sort-merge join object 
+   * @brief Construct a sort-merge join object
    *
-   * @note The `sort_merge_join` object must not outlive the tables viewed by `left` and `right`, else behavior is
-   * undefined.
+   * @note The `sort_merge_join` object must not outlive the tables viewed by `left` and `right`,
+   * else behavior is undefined.
    *
    * @param left The left table
    * @param is_left_sorted Boolean to indicate if left table is pre-sorted
    * @param right The right table
-   * @param is_right_table Boolean to indicate if right table is pre-sorted
+   * @param is_right_sorted Boolean to indicate if right table is pre-sorted
    * @param compare_nulls Controls whether null join-key values should match or not
    * @param stream CUDA stream used for device memory operations and kernel launches
    * @param mr Device memory resource used to allocate the table and columns' device
@@ -77,14 +80,14 @@ class sort_merge_join {
 
   /**
    * Returns the row indices that can be used to construct the result of performing
-   * an inner join between two tables. @see cudf::inner_join(). 
+   * an inner join between two tables. @see cudf::inner_join().
    *
    * @param stream CUDA stream used for device memory operations and kernel launches
    * @param mr Device memory resource used to allocate the table and columns' device
    * memory.
    *
    * @return A pair of columns [`left_indices`, `right_indices`] that can be used to construct
-   * the result of performing an inner join between two tables 
+   * the result of performing an inner join between two tables
    */
   std::pair<std::unique_ptr<rmm::device_uvector<size_type>>,
             std::unique_ptr<rmm::device_uvector<size_type>>>
@@ -94,18 +97,25 @@ class sort_merge_join {
    * @brief Helper struct to pre-process the tables before join operations can be performed
    */
   struct preprocessed_table {
-    table_view raw_tbl_view;
-    table_view tbl_view;
-    // filters for null_equality::UNEQUAL
-    std::optional<rmm::device_buffer> raw_validity_mask = std::nullopt;
-    std::optional<size_type> raw_num_nulls              = std::nullopt;
-    std::optional<std::unique_ptr<table>> tbl           = std::nullopt;
-    // optional reordering if we are given pre-sorted tables
-    std::optional<std::unique_ptr<column>> tbl_sorted_order = std::nullopt;
+    table_view raw_tbl_view;  ///< raw table view before pre processing
+
+    table_view tbl_view;  ///< processed table view which is the null-free subset of the rows of the
+                          ///< raw table view if null equality is set to false, and is equal to the
+                          ///< raw table view otherwise
+
+    std::optional<rmm::device_buffer> raw_validity_mask =
+      std::nullopt;  ///< optional filters for null_equality::UNEQUAL
+    std::optional<size_type> raw_num_nulls =
+      std::nullopt;  ///< optional filters for null_equality::UNEQUAL
+    std::optional<std::unique_ptr<table>> tbl =
+      std::nullopt;  ///< optional filters for null_equality::UNEQUAL
+
+    std::optional<std::unique_ptr<column>> tbl_sorted_order =
+      std::nullopt;  ///< optional reordering if we are given pre-sorted tables
 
     /**
      * @brief Mark rows in raw table with nulls at root or child levels by populating the
-     * raw_validity_mask 
+     * raw_validity_mask
      *
      * @param stream CUDA stream used for device memory operations and kernel launches
      * @param mr Device memory resource used to allocate the table and columns' device
@@ -113,7 +123,7 @@ class sort_merge_join {
      */
     void populate_nonnull_filter(rmm::cuda_stream_view stream, rmm::device_async_resource_ref mr);
     /**
-     * @brief Apply raw_validity_mask on the raw_tbl_view to create a null-free table 
+     * @brief Apply raw_validity_mask on the raw_tbl_view to create a null-free table
      *
      * @param stream CUDA stream used for device memory operations and kernel launches
      * @param mr Device memory resource used to allocate the table and columns' device
@@ -142,6 +152,7 @@ class sort_merge_join {
      * @param stream CUDA stream used for device memory operations and kernel launches
      * @param mr Device memory resource used to allocate the table and columns' device
      * memory.
+     * @return A device vector indicating the mapping between pre-processed table and raw table
      */
     rmm::device_uvector<size_type> map_tbl_to_raw(rmm::cuda_stream_view stream,
                                                   rmm::device_async_resource_ref mr);
@@ -157,7 +168,6 @@ class sort_merge_join {
    *
    * @param left The left table
    * @param right The right table
-   * @param compare_nulls Controls whether null join-key values should match or not
    * @param stream CUDA stream used for device memory operations and kernel launches
    * @param mr Device memory resource used to allocate the table and columns' device
    * memory.
@@ -169,13 +179,14 @@ class sort_merge_join {
   /**
    * @brief Post-process left and right tables after the merge operation
    *
-   * @param smaller_indices Indices for the smaller processed table used to construct the join result 
-   * @param larger_indices Indices for the larger processed table used to construct the join result 
+   * @param smaller_indices Indices for the smaller processed table used to construct the join
+   * result
+   * @param larger_indices Indices for the larger processed table used to construct the join result
    * @param stream CUDA stream used for device memory operations and kernel launches
    * @param mr Device memory resource used to allocate the table and columns' device
    * memory.
-   * @return A pair of device vectors [`left_indices`, `right_indices`] that can be used to construct
-   * the result of performing an inner join between two pre-processed tables 
+   * @return A pair of device vectors [`left_indices`, `right_indices`] that can be used to
+   * construct the result of performing an inner join between two pre-processed tables
    */
   std::pair<std::unique_ptr<rmm::device_uvector<size_type>>,
             std::unique_ptr<rmm::device_uvector<size_type>>>
