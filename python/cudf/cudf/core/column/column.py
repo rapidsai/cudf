a29575198a80579167d8280dbbbe71865f8412bd
GALI PREM SAGAR <sagarprem75@gmail.com>
2025-04-20 13:43:05 -0500

    Fix copy-on-write buffer separation and cleanup (#18530)
    Fixes: #18529 
    
    This PR fixes an issues where stale data was being held back in copy-on-write. This is achieved by resetting the `base_data` once we make a copy of `data`.
    
    Authors:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
      - https://github.com/apps/pre-commit-ci
    
    Approvers:
      - Matthew Roeschke (https://github.com/mroeschke)
    
    URL: https://github.com/rapidsai/cudf/pull/18530

--- python/cudf/cudf/core/column/column.py
+++ python/cudf/cudf/core/column/column.py
@@ -173,7 +173,20 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
         if self.data is None:
             return 0
         else:
-            return self.data.get_ptr(mode="write")
+            # Save the original ptr
+            original_ptr = self.data.get_ptr(mode="read")
+
+            # Get the pointer which may trigger a copy due to copy-on-write
+            ptr = self.data.get_ptr(mode="write")
+
+            # Check if a new buffer was created or if the underlying data was modified
+            # This happens both when the buffer object is replaced and when
+            # ExposureTrackedBuffer.make_single_owner_inplace() is called
+            if cudf.get_option("copy_on_write") and (ptr != original_ptr):
+                # Update base_data to match the new data buffer
+                self.set_base_data(self.data)
+
+            return ptr
 
     def set_base_data(self, value: None | Buffer) -> None:
         if value is not None and not isinstance(value, Buffer):
@@ -215,7 +228,20 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
         if self.mask is None:
             return 0
         else:
-            return self.mask.get_ptr(mode="write")
+            # Save the original ptr
+            original_ptr = self.mask.get_ptr(mode="read")
+
+            # Get the pointer which may trigger a copy due to copy-on-write
+            ptr = self.mask.get_ptr(mode="write")
+
+            # Check if a new buffer was created or if the underlying data was modified
+            # This happens both when the buffer object is replaced and when
+            # ExposureTrackedBuffer.make_single_owner_inplace() is called
+            if cudf.get_option("copy_on_write") and (ptr != original_ptr):
+                # Update base_data to match the new data buffer
+                self.set_base_mask(self.mask)
+
+            return ptr
 
     def set_base_mask(self, value: None | Buffer) -> None:
         """
