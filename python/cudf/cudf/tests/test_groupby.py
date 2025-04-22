0ea19010b7a13b6b664f5f9424c0664b9cc0fb07
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-04-22 12:12:48 -0700

    Add by instead

--- python/cudf/cudf/tests/test_groupby.py
+++ python/cudf/cudf/tests/test_groupby.py
@@ -4096,7 +4096,7 @@ def test_size_as_index_false():
     df = pd.DataFrame({"a": [1, 2, 1], "b": [1, 2, 3]}, columns=["a", "b"])
     expected = df.groupby("a", as_index=False).size()
     result = cudf.from_pandas(df).groupby("a", as_index=False).size()
-    assert_groupby_results_equal(result, expected)
+    assert_groupby_results_equal(result, expected, as_index=False, by="a")
 
 
 def test_size_series_with_name():
