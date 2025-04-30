97faca6adb6222f6351b8d6c531ea577c6d48a69
Gil Forsyth <gforsyth@nvidia.com>
2025-04-30 11:13:00 -0400

    try to fix install paths

--- ci/test_wheel_dask_cudf.sh
+++ ci/test_wheel_dask_cudf.sh
@@ -17,6 +17,10 @@ rapids-logger "Install dask_cudf, cudf, pylibcudf, and test requirements"
 rapids-generate-pip-constraints py_test_dask_cudf ./constraints.txt
 
 set -x
+AUDITWHEEL_ARCH="$(arch)"
+export AUDITWHEEL_ARCH
+AUDITWHEEL_PLAT="manylinux_2_28_$(arch)"
+export AUDITWHEEL_PLAT
 LIBKVIKIO_WHL="libkvikio_${RAPIDS_PY_CUDA_SUFFIX}-25.6.0a32-py3-none-${AUDITWHEEL_PLAT}.whl"
 LIBKVIKIO_TARBALL="kvikio_wheel_cpp_libkvikio_${RAPIDS_PY_CUDA_SUFFIX}_${AUDITWHEEL_ARCH}.tar.gz"
 LIBKVIKIO_DIR=$(rapids-get-artifact "ci/kvikio/pull-request/702/7f957eb/${LIBKVIKIO_TARBALL}")
