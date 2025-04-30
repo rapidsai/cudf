b9eb6893fd003cfab43012eb1f0ea79df95bdbac
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-04-30 09:39:04 -0700

    Add sphinx ignore for pa.ListType

--- docs/cudf/source/conf.py
+++ docs/cudf/source/conf.py
@@ -596,6 +596,7 @@ nitpick_ignore = [
     ("py:class", "pd.DataFrame"),
     ("py:class", "pandas.core.indexes.frozen.FrozenList"),
     ("py:class", "pa.Array"),
+    ("py:class", "pa.ListType"),
     ("py:class", "pa.Decimal128Type"),
     ("py:class", "ScalarLike"),
     ("py:class", "ParentType"),
