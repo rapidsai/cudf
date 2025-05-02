29843ffafb7bf902f9fa5e54325d4dd500afcf28
Matthew Murray <matthewmurray711@gmail.com>
2025-05-01 20:33:50 -0400

    xfail tests

--- python/cudf_polars/cudf_polars/dsl/expressions/unary.py
+++ python/cudf_polars/cudf_polars/dsl/expressions/unary.py
@@ -233,6 +233,13 @@ class UnaryFunction(Expr):
             else:
                 evaluated = self.children[1].evaluate(df, context=context)
                 arg = evaluated.obj_scalar if evaluated.is_scalar else evaluated.obj
+            if isinstance(arg, plc.Scalar) and dtypes.can_cast(
+                column.obj.type(), arg.type()
+            ):
+                # TODO: Do the cast without arrow
+                arg = plc.Scalar.from_py(
+                    plc.interop.to_arrow(arg).as_py(), column.obj.type()
+                )
             return Column(plc.replace.replace_nulls(column.obj, arg))
         elif self.name in self._OP_MAPPING:
             column = self.children[0].evaluate(df, context=context)
