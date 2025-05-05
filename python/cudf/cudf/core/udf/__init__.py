e00cfe621e7736e90917bd8136f12c84bbee445b
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-05 13:19:41 -0700

    Don't import modules in udf/__init__.py

--- python/cudf/cudf/core/udf/__init__.py
+++ python/cudf/cudf/core/udf/__init__.py
@@ -1,9 +1,1 @@
-# Copyright (c) 2022-2023, NVIDIA CORPORATION.
-from . import (
-    groupby_lowering,
-    groupby_typing,
-    masked_lowering,
-    masked_typing,
-    strings_lowering,
-    strings_typing,
-)
+# Copyright (c) 2022-2025, NVIDIA CORPORATION.
