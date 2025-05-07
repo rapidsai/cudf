ac0e7eafe7f4be38dc3d34ad61beb84b3c84e578
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-07 10:53:00 -0700

    Add headers overloads of arrow_column, from_arrow_stream_column

--- cpp/include/cudf/interop.hpp
+++ cpp/include/cudf/interop.hpp
@@ -761,6 +761,24 @@ std::unique_ptr<table> from_arrow_stream(
   rmm::device_async_resource_ref mr = cudf::get_current_device_resource_ref());
 
 /**
+ * @brief Create `cudf::column` from given ArrowArrayStream input
+ *
+ * @throws std::invalid_argument if input is NULL
+ *
+ * The conversion WILL release the input ArrayArrayStream and its constituent
+ * arrays or schema since Arrow streams are not suitable for multiple reads.
+ *
+ * @param input `ArrowArrayStream` pointer to object that will produce ArrowArray data
+ * @param stream CUDA stream used for device memory operations and kernel launches
+ * @param mr Device memory resource used to perform cuda allocation
+ * @return cudf column generated from the given Arrow data
+ */
+std::unique_ptr<column> from_arrow_stream_column(
+  ArrowArrayStream* input,
+  rmm::cuda_stream_view stream      = cudf::get_default_stream(),
+  rmm::device_async_resource_ref mr = cudf::get_current_device_resource_ref());
+
+/**
  * @brief Create `cudf::column` from given ArrowDeviceArray input
  *
  * @throws std::invalid_argument if either schema or input are NULL
