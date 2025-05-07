7e555e0a265fef4cdcbd0b3a275c7ea18be3e8c0
Matthew Murray <41342305+Matt711@users.noreply.github.com>
2025-05-07 10:49:38 -0400

    Add a public API for copying a table_view to device array (#18450)
    Contributes to #16483.
    
    This PR adds a new libcudf API: `cudf::table_to_array`, which copies data from a table_view into a preallocated column-major device array using `cub::DeviceMemcpy::Batched`. 
    
    The primary use case for this API is to accelerate the conversion of a cudf.DataFrame to a CuPy array when users access `DataFrame.values` in Python.
    
    In a follow-up PR, I'll integrate this API into the cudf Python layer. 
    
    - [x] I am familiar with the [Contributing Guidelines](https://github.com/rapidsai/cudf/blob/HEAD/CONTRIBUTING.md).
    - [x] New or existing tests cover these changes.
    - [x] The documentation is up to date with these changes.
    
    Authors:
      - Matthew Murray (https://github.com/Matt711)
    
    Approvers:
      - David Wendt (https://github.com/davidwendt)
      - Vukasin Milovanovic (https://github.com/vuule)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/cudf/pull/18450

--- cpp/include/cudf/reshape.hpp
+++ cpp/include/cudf/reshape.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2024, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,9 @@
 #include <cudf/types.hpp>
 #include <cudf/utilities/export.hpp>
 #include <cudf/utilities/memory_resource.hpp>
+#include <cudf/utilities/span.hpp>
+
+#include <cuda/functional>
 
 #include <memory>
 
@@ -106,6 +109,28 @@ std::unique_ptr<column> byte_cast(
   flip_endianness endian_configuration,
   rmm::cuda_stream_view stream      = cudf::get_default_stream(),
   rmm::device_async_resource_ref mr = cudf::get_current_device_resource_ref());
+
+/**
+ * @brief Copies a table into a contiguous column-major device array.
+ *
+ * This function copies a `table_view` with columns of the same fixed-width type
+ * into a 2D device array stored in column-major order.
+ *
+ * The output buffer must be preallocated and passed as a `device_span` using
+ * a `device_span<cuda::std::byte>`. It must be large enough to hold
+ * `num_rows * num_columns * sizeof(dtype)` bytes.
+ *
+ * @throws cudf::logic_error if columns do not all have the same type
+ * @throws cudf::logic_error if the dtype of the columns is not a fixed-width type
+ * @throws std::invalid_argument if the output span is too small
+ *
+ * @param input A table with fixed-width, non-nullable columns of the same type
+ * @param output A span representing preallocated device memory for the output
+ * @param stream CUDA stream used for memory operations
+ */
+void table_to_array(table_view const& input,
+                    device_span<cuda::std::byte> output,
+                    rmm::cuda_stream_view stream = cudf::get_default_stream());
 
 /** @} */  // end of group
 
