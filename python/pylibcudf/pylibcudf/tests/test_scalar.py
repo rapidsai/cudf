4cde11ba0c6e9ad0885457b56e4d6cc911917c32
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-12 11:18:46 -0700

    Make PY_SCALAR a fixture

--- python/pylibcudf/pylibcudf/tests/test_scalar.py
+++ python/pylibcudf/pylibcudf/tests/test_scalar.py
@@ -13,27 +13,31 @@ def np():
     return pytest.importorskip("numpy")
 
 
-PY_SCALARS = [
-    True,
-    False,
-    -1,
-    0,
-    1 - 1.0,
-    0.0,
-    1.52,
-    "",
-    "a1!",
-    datetime.datetime(2020, 1, 1),
-    datetime.datetime(2020, 1, 1, microsecond=1),
-    datetime.timedelta(1),
-    datetime.timedelta(days=1, microseconds=1),
-]
+@pytest.fixture(
+    params=[
+        True,
+        False,
+        -1,
+        0,
+        1 - 1.0,
+        0.0,
+        1.52,
+        "",
+        "a1!",
+        datetime.datetime(2020, 1, 1),
+        datetime.datetime(2020, 1, 1, microsecond=1),
+        datetime.timedelta(1),
+        datetime.timedelta(days=1, microseconds=1),
+    ],
+    ids=repr,
+)
+def py_scalar(request):
+    return request.param
 
 
-@pytest.mark.parametrize("val", PY_SCALARS)
-def test_from_py(val):
-    result = plc.Scalar.from_py(val)
-    expected = pa.scalar(val)
+def test_from_py(py_scalar):
+    result = plc.Scalar.from_py(py_scalar)
+    expected = pa.scalar(py_scalar)
     assert plc.interop.to_arrow(result).equals(expected)
 
 
@@ -227,10 +231,11 @@ def test_from_numpy_typeerror(np):
         plc.Scalar.from_numpy(np.void(5))
 
 
-@pytest.mark.parametrize("val", PY_SCALARS)
-def test_round_trip_scalar_through_column(val):
-    result = plc.Column.from_scalar(plc.Scalar.from_py(val), 1).to_scalar()
-    expected = pa.scalar(val)
+def test_round_trip_scalar_through_column(py_scalar):
+    result = plc.Column.from_scalar(
+        plc.Scalar.from_py(py_scalar), 1
+    ).to_scalar()
+    expected = pa.scalar(py_scalar)
     assert plc.interop.to_arrow(result).equals(expected)
 
 
