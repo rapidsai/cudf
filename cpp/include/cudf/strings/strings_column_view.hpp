ea10cb34420ea69e3f7e38db02826473d30d1ddd
Lawrence Mitchell <lmitchell@nvidia.com>
2025-05-12 16:18:06 +0000

    Use cpp_locals=True to avoid need for strings_column_view() ctor

--- cpp/include/cudf/strings/strings_column_view.hpp
+++ cpp/include/cudf/strings/strings_column_view.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2025, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2024, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,8 +43,6 @@ class strings_column_view : private column_view {
    * @param strings_column The column view to wrap.
    */
   strings_column_view(column_view strings_column);
-  // So we can use this from cython.
-  strings_column_view()                           = default;
   strings_column_view(strings_column_view&&)      = default;  ///< Move constructor
   strings_column_view(strings_column_view const&) = default;  ///< Copy constructor
   ~strings_column_view() override                 = default;
--- python/pylibcudf/pylibcudf/column.pyx
+++ python/pylibcudf/pylibcudf/column.pyx
@@ -1,4 +1,5 @@
 # Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# cython: cpp_locals=True
 
 from cython.operator cimport dereference
 
