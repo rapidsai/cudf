5b45270b8e4367041e677351d9218f7a9551f6e1
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-12 11:16:18 -0700

    Directly convert int with duration type instead of going through timedelta

--- python/pylibcudf/pylibcudf/scalar.pyx
+++ python/pylibcudf/pylibcudf/scalar.pyx
@@ -227,6 +227,9 @@ def _(py_val: int, dtype: DataType | None):
     cdef unique_ptr[scalar] c_obj
     cdef DataType c_dtype
     cdef duration_ns c_duration_ns
+    cdef duration_us c_duration_us
+    cdef duration_ms c_duration_ms
+    cdef duration_s c_duration_s
     if dtype is None:
         c_dtype = DataType(type_id.INT64)
     elif is_floating_point(dtype):
@@ -309,13 +312,31 @@ def _(py_val: int, dtype: DataType | None):
         (<duration_scalar[duration_ns]*>c_obj.get()).set_value(c_duration_ns)
 
     elif tid == type_id.DURATION_MICROSECONDS:
-        return _from_py(datetime.timedelta(microseconds=py_val), dtype)
+        if py_val > numeric_limits[int64_t].max():
+            raise OverflowError(
+                f"{py_val} microseconds out of range for INT64 limit."
+            )
+        c_obj = make_duration_scalar(c_dtype.c_obj)
+        c_duration_us = duration_us(<int64_t>py_val)
+        (<duration_scalar[duration_us]*>c_obj.get()).set_value(c_duration_us)
 
     elif tid == type_id.DURATION_MILLISECONDS:
-        return _from_py(datetime.timedelta(milliseconds=py_val), dtype)
+        if py_val > numeric_limits[int64_t].max():
+            raise OverflowError(
+                f"{py_val} milliseconds out of range for INT64 limit."
+            )
+        c_obj = make_duration_scalar(c_dtype.c_obj)
+        c_duration_ms = duration_ms(<int64_t>py_val)
+        (<duration_scalar[duration_ms]*>c_obj.get()).set_value(c_duration_ms)
 
     elif tid == type_id.DURATION_SECONDS:
-        return _from_py(datetime.timedelta(seconds=py_val), dtype)
+        if py_val > numeric_limits[int64_t].max():
+            raise OverflowError(
+                f"{py_val} seconds out of range for INT64 limit."
+            )
+        c_obj = make_duration_scalar(c_dtype.c_obj)
+        c_duration_s = duration_s(<int64_t>py_val)
+        (<duration_scalar[duration_s]*>c_obj.get()).set_value(c_duration_s)
 
     else:
         typ = c_dtype.id()
