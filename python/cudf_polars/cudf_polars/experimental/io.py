ccb909f372441fbb544acebf7ea6e4ba5c2b38a9
rjzamora <rzamora217@gmail.com>
2025-05-14 06:02:47 -0700

    avoid random in metadata sampling

--- python/cudf_polars/cudf_polars/experimental/io.py
+++ python/cudf_polars/cudf_polars/experimental/io.py
@@ -7,7 +7,6 @@ from __future__ import annotations
 import dataclasses
 import enum
 import math
-import random
 from enum import IntEnum
 from typing import TYPE_CHECKING, Any, TypeVar
 
@@ -263,10 +262,9 @@ def _sample_pq_statistics(ir: Scan) -> dict[str, np.floating[T]]:
     import numpy as np
 
     # Use average total_uncompressed_size of three files
-    n_sample = min(5, len(ir.paths))
-    random.seed(42)  # Deterministic partitioning
     metadata = plc.io.parquet_metadata.read_parquet_metadata(
-        plc.io.SourceInfo(random.sample(ir.paths, n_sample))
+        # TODO: Add configuration for random sampling
+        plc.io.SourceInfo(ir.paths[:5])
     )
     column_sizes = {}
     rowgroup_offsets_per_file = np.insert(
