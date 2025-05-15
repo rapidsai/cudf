41b98851d4e7a0bcb0c3fb376892372acf6501ab
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-14 17:43:58 -0700

    Move implemented BaseIndex methods over to Index (#18807)
    Towards https://github.com/rapidsai/cudf/issues/10068. Broken off from https://github.com/rapidsai/cudf/pull/18751. 
    
    This PR moves implemented `BaseIndex` methods over to `Index` for preparation of removal. There will be a short period where the same implementation exists on `BaseIndex` as `Index`.
    
    Additionally, converts `._values` calls (a `BaseIndex` attribute) to `._column` calls (an `Index` attribute)
    
    Authors:
      - Matthew Roeschke (https://github.com/mroeschke)
    
    Approvers:
      - Matthew Murray (https://github.com/Matt711)
    
    URL: https://github.com/rapidsai/cudf/pull/18807

--- python/cudf/cudf/core/single_column_frame.py
+++ python/cudf/cudf/core/single_column_frame.py
@@ -210,6 +210,28 @@ class SingleColumnFrame(Frame, NotIterable):
         """
         return self._column.to_arrow()
 
+    def tolist(self) -> None:
+        """Conversion to host memory lists is currently unsupported
+
+        Raises
+        ------
+        TypeError
+            If this method is called
+
+        Notes
+        -----
+        cuDF currently does not support implicit conversion from GPU stored series to
+        host stored lists. A `TypeError` is raised when this method is called.
+        Consider calling `.to_arrow().to_pylist()` to construct a Python list.
+        """
+        raise TypeError(
+            "cuDF does not support conversion to host memory "
+            "via the `tolist()` method. Consider using "
+            "`.to_arrow().to_pylist()` to construct a Python list."
+        )
+
+    to_list = tolist
+
     def _to_frame(
         self, name: Hashable, index: cudf.Index | None
     ) -> cudf.DataFrame:
