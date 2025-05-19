330362e4b768adda21a7eeb7835a084b65adb9bb
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-19 09:40:46 -0700

    Ignore cp.ndarray

--- docs/cudf/source/conf.py
+++ docs/cudf/source/conf.py
@@ -594,6 +594,7 @@ nitpick_ignore = [
     ("py:obj", "cudf.MultiIndex.to_flat_index"),
     ("py:meth", "pyarrow.Table.to_pandas"),
     ("py:class", "abc.Hashable"),
+    ("py:class", "cp.ndarray"),
     ("py:class", "pd.DataFrame"),
     ("py:class", "pandas.core.indexes.frozen.FrozenList"),
     ("py:class", "pa.Array"),
