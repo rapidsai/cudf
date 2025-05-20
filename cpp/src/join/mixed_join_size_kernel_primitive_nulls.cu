6d9db22fa1a31636de223e41b9fc2aa8e204ed2c
Yunsong Wang <yunsongw@nvidia.com>
2025-05-20 15:19:41 -0700

    Add more explicit instantiations

---
+++ cpp/src/join/mixed_join_size_kernel_primitive_nulls.cu
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mixed_join_size_kernel.cuh"
+#include "mixed_join_size_kernel.hpp"
+
+#include <cudf/table/primitive_row_operators.cuh>
+
+namespace cudf::detail {
+template std::size_t launch_compute_mixed_join_output_size<
+  true,
+  cudf::row::primitive::row_hasher<cudf::hashing::detail::default_hash>,
+  cudf::row::primitive::row_equality_comparator>(
+  table_device_view left_table,
+  table_device_view right_table,
+  table_device_view probe,
+  table_device_view build,
+  cudf::row::primitive::row_hasher<cudf::hashing::detail::default_hash> hash_probe,
+  cudf::row::primitive::row_equality_comparator equality_probe,
+  join_kind const join_type,
+  cudf::detail::mixed_multimap_type::device_view hash_table_view,
+  ast::detail::expression_device_view device_expression_data,
+  bool const swap_tables,
+  cudf::device_span<cudf::size_type> matches_per_row,
+  detail::grid_1d const config,
+  int64_t shmem_size_per_block,
+  rmm::cuda_stream_view stream,
+  rmm::device_async_resource_ref mr);
+}  // namespace cudf::detail
