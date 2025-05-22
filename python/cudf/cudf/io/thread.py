3ad2c7116325fb1e638afa410e8836d1fdf4fdc9
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-05-21 22:59:03 -0400

    Update

--- python/cudf/cudf/io/thread.py
+++ python/cudf/cudf/io/thread.py
@@ -3,9 +3,25 @@
 import pylibcudf as plc
 
 
-def set_num_io_threads(num_io_threads):
+def set_num_io_threads(num_io_threads: int) -> None:
+    """
+    Set the number of IO threads used by KvikIO.
+
+    Parameters
+    ----------
+    num_io_threads: int
+        The number of IO threads to be used.
+    """
     plc.io.set_num_io_threads(num_io_threads)
 
 
-def num_io_threads():
+def num_io_threads() -> int:
+    """
+    Get the number of IO threads used by KvikIO.
+
+    Returns
+    -------
+    int
+        The number of IO threads used by KvikIO.
+    """
     return plc.io.num_io_threads()
