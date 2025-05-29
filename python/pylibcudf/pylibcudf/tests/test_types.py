a40e06ea5471f63f89cced65dd46662429024884
Matthew Murray <matthewmurray711@gmail.com>
2025-05-29 19:59:30 -0400

    address review

---
+++ python/pylibcudf/pylibcudf/tests/test_types.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.
+
+import pyarrow as pa
+from packaging.version import parse
+
+import pylibcudf as plc
+
+
+def test_dtype_from_arrow():
+    assert (
+        plc.DataType.from_arrow(pa.decimal128(10, 2)).id()
+        == plc.TypeId.DECIMAL128
+    )
+
+    assert plc.DataType.from_arrow(pa.struct([])).id() == plc.TypeId.STRUCT
+
+    assert (
+        plc.DataType.from_arrow(pa.list_(pa.int32())).id() == plc.TypeId.LIST
+    )
+
+    if parse(pa.__version__) > parse("19.0.0"):
+        assert (
+            plc.DataType.from_arrow(pa.decimal32(3)).id()
+            == plc.TypeId.DECIMAL32
+        )
+
+        assert (
+            plc.DataType.from_arrow(pa.decimal64(5)).id()
+            == plc.TypeId.DECIMAL64
+        )
+
+    assert plc.DataType.from_arrow(pa.int32()).id() == plc.TypeId.INT32
