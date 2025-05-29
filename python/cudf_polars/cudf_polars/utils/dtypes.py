495367590f46a3eb414af3fa146100cdc37435c6
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-29 12:16:05 -0700

    Remove downcast_arrow_lists

--- python/cudf_polars/cudf_polars/utils/dtypes.py
+++ python/cudf_polars/cudf_polars/utils/dtypes.py
@@ -5,8 +5,6 @@
 
 from __future__ import annotations
 
-import pyarrow as pa
-
 import pylibcudf as plc
 from pylibcudf.traits import (
     is_floating_point,
@@ -16,38 +14,8 @@ from pylibcudf.traits import (
 
 __all__ = [
     "can_cast",
-    "downcast_arrow_lists",
     "is_order_preserving_cast",
 ]
-
-
-def downcast_arrow_lists(typ: pa.DataType) -> pa.DataType:
-    """
-    Sanitize an arrow datatype from polars.
-
-    Parameters
-    ----------
-    typ
-        Arrow type to sanitize
-
-    Returns
-    -------
-    Sanitized arrow type
-
-    Notes
-    -----
-    As well as arrow ``ListType``s, polars can produce
-    ``LargeListType``s and ``FixedSizeListType``s, these are not
-    currently handled by libcudf, so we attempt to cast them all into
-    normal ``ListType``s on the arrow side before consuming the arrow
-    data.
-    """
-    if isinstance(typ, pa.LargeListType):
-        return pa.list_(downcast_arrow_lists(typ.value_type))
-    # We don't have to worry about diving into struct types for now
-    # since those are always NotImplemented before we get here.
-    assert not isinstance(typ, pa.StructType)
-    return typ
 
 
 def can_cast(from_: plc.DataType, to: plc.DataType) -> bool:
