ee3d34fea9bc74e040c8af1ee134ee515118e396
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-30 16:20:32 -0700

    Add test for unnest

--- python/cudf_polars/tests/expressions/test_struct.py
+++ python/cudf_polars/tests/expressions/test_struct.py
@@ -2,12 +2,23 @@
 # SPDX-License-Identifier: Apache-2.0
 from __future__ import annotations
 
+import pytest
+
 import polars as pl
 
 from cudf_polars.testing.asserts import assert_gpu_result_equal
 
 
-def test_struct_field():
-    ldf = pl.LazyFrame({"a": [{"b": "c", "d": "e"}]})
+@pytest.fixture
+def ldf():
+    return pl.LazyFrame({"a": [{"b": "c", "d": "e"}]})
+
+
+def test_field(ldf):
     query = ldf.select(pl.col("a").struct.field("b"))
+    assert_gpu_result_equal(query)
+
+
+def test_unnest(ldf):
+    query = ldf.select(pl.col("a").struct.unnest())
     assert_gpu_result_equal(query)
