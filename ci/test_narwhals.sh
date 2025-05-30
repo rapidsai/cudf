bd001a98e464084f7781e520d3fce2ac1bf63473
Matthew Murray <41342305+Matt711@users.noreply.github.com>
2025-05-30 06:38:49 -0400

    Update ci/test_narwhals.sh

--- ci/test_narwhals.sh
+++ ci/test_narwhals.sh
@@ -20,7 +20,7 @@ rapids-logger "pytest narwhals"
 NARWHALS_VERSION=$(python -c "import narwhals; print(narwhals.__version__)")
 git clone https://github.com/narwhals-dev/narwhals.git --depth=1 -b "v${NARWHALS_VERSION}" narwhals
 pushd narwhals || exit 1
-rapids-pip-retry install -U -e . pytest-env "hypothesis>=6.131.7" sqlframe ibis-framework[duckdb]
+rapids-pip-retry install -U -e .
 
 rapids-logger "Check narwhals versions"
 python -c "import narwhals; print(narwhals.show_versions())"
