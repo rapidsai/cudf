121d0d7bed3677825aed0f0a6d01cab66414a2b9
Tom Augspurger <toaugspurger@nvidia.com>
2025-06-05 13:20:17 -0700

    Fix cudf_polars spilling
    This fixes a bug where cudf_polars failed when using spilling.
    
    https://github.com/rapidsai/cudf/pull/18970 made some assumptions about
    the callables that we put in our task graph: namely that the first
    argument is always some `IR.do_evaluate`. This is true, *except* for
    when we post-process the task graph to do our spillable wrapping.
    
    I'm hopeful that there's a better way to do this by integrating
    spilling more into the IR, but this should be an OK fix in the meantime.
    
    Closes https://github.com/rapidsai/cudf/issues/19100

--- python/cudf_polars/cudf_polars/experimental/spilling.py
+++ python/cudf_polars/cudf_polars/experimental/spilling.py
@@ -4,8 +4,10 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+import functools
+from typing import TYPE_CHECKING, Any, Literal, TypeVar, overload
 
+import nvtx
 from dask.sizeof import sizeof
 from distributed import get_worker
 from rapidsmpf.buffer.buffer import MemoryType
@@ -13,15 +15,28 @@ from rapidsmpf.integrations.dask.core import get_worker_context
 from rapidsmpf.integrations.dask.spilling import SpillableWrapper
 
 from cudf_polars.containers import DataFrame
+from cudf_polars.dsl.tracing import CUDF_POLARS_NVTX_DOMAIN, do_evaluate_with_tracing
 
 if TYPE_CHECKING:
     from collections.abc import Callable, MutableMapping
     from typing import Any
 
+    from cudf_polars.dsl.ir import IR
     from cudf_polars.utils.config import ConfigOptions
 
 
-def wrap_arg(obj: Any) -> Any:
+T = TypeVar("T")
+
+
+@overload
+def wrap_arg(obj: DataFrame) -> SpillableWrapper: ...
+
+
+@overload
+def wrap_arg(obj: T) -> T: ...
+
+
+def wrap_arg(obj: T) -> T | SpillableWrapper:
     """
     Make `obj` spillable if it is a DataFrame.
 
@@ -39,6 +54,9 @@ def wrap_arg(obj: Any) -> Any:
     return obj
 
 
+# We can't really express this type correctly. We know that
+# SpillableWrapper will always return a DataFrame, and all
+# other types will be unchanged.
 def unwrap_arg(obj: Any) -> Any:
     """
     Unwraps a SpillableWrapper to retrieve the original object.
@@ -57,31 +75,57 @@ def unwrap_arg(obj: Any) -> Any:
     return obj
 
 
-def wrap_func_spillable(
-    func: Callable,
-    *,
+@overload
+def do_evaluate_with_tracing_and_spilling(
+    cls: type[IR],
+    *args: Any,
+    make_func_output_spillable: Literal[False],
+    target_partition_size: int,
+) -> DataFrame: ...
+
+
+@overload
+def do_evaluate_with_tracing_and_spilling(
+    cls: type[IR],
+    *args: Any,
+    make_func_output_spillable: Literal[True],
+    target_partition_size: int,
+) -> SpillableWrapper: ...
+
+
+def do_evaluate_with_tracing_and_spilling(
+    cls: type[IR],
+    *args: Any,
     make_func_output_spillable: bool,
     target_partition_size: int,
-) -> Callable:
+) -> DataFrame | SpillableWrapper:
     """
-    Wraps a function to handle spillable DataFrames.
+    Evaluate an IR node with tracing and spilling.
 
     Parameters
     ----------
-    func
-        The function to be wrapped.
+    cls
+        The type of the IR node to evaluate.
+    args
+        The arguments to pass to ``cls.do_evaluate``.
     make_func_output_spillable
-        Whether to wrap the function's output in a SpillableWrapper.
+        Whether to make the output of the function spillable.
     target_partition_size
-        Target byte size for IO tasks.
+        The target partition size.
 
     Returns
     -------
-    A wrapped function that processes spillable DataFrames.
+    output
+        The result of calling ``cls.do_evaluate`` with the unwrapped arguments.
+        If ``make_func_output_spillable`` is True, the output will be wrapped in
+        a SpillableWrapper.
     """
+    func = cls.do_evaluate
 
-    def wrapper(*args: Any) -> Any:
-        # Make headroom before executing the task
+    with nvtx.annotate(
+        message=cls.__name__,
+        domain=CUDF_POLARS_NVTX_DOMAIN,
+    ):
         headroom = 0
         probable_io_task = True
         for arg in args:
@@ -100,9 +144,26 @@ def wrap_func_spillable(
         ret: Any = func(*(unwrap_arg(arg) for arg in args))
         if make_func_output_spillable:
             ret = wrap_arg(ret)
-        return ret
 
-    return wrapper
+    return ret
+
+
+def unwrap_and_apply(func: Callable, *args: Any) -> Any:
+    """
+    Unwrap and SpillableWrapper arguments before calling ``func``.
+
+    Parameters
+    ----------
+    func
+        The function to be called.
+    args
+        The arguments to be passed to the function.
+
+    Returns
+    -------
+    The result of calling ``func`` with the unwrapped arguments.
+    """
+    return func(*(unwrap_arg(arg) for arg in args))
 
 
 def wrap_dataframe_in_spillable(
@@ -136,16 +197,33 @@ def wrap_dataframe_in_spillable(
     target_partition_size = config_options.executor.target_partition_size
 
     ret = {}
+
+    apply_and_spill = functools.partial(
+        do_evaluate_with_tracing_and_spilling,
+        make_func_output_spillable=True,
+        target_partition_size=target_partition_size,
+    )
+    apply_no_spill = functools.partial(
+        do_evaluate_with_tracing_and_spilling,
+        make_func_output_spillable=False,
+        target_partition_size=target_partition_size,
+    )
+
     for key, task in graph.items():
         assert isinstance(task, tuple)
-        ret[key] = tuple(
-            wrap_func_spillable(
-                a,
-                make_func_output_spillable=key != ignore_key,
-                target_partition_size=target_partition_size,
-            )
-            if callable(a)
-            else a
-            for a in task
-        )
+
+        if task and task[0] is do_evaluate_with_tracing:
+            if key == ignore_key:
+                new = apply_no_spill
+            else:
+                new = apply_and_spill
+
+            ret[key] = (new, *task[1:])
+
+        elif task and callable(task[0]):
+            f, *args = task
+            ret[key] = (unwrap_and_apply, f, *args)
+        else:
+            ret[key] = task
+
     return ret
