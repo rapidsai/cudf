44ee9e5d449fdebd5afb8c32dff118460d8fd968
Tom Augspurger <toaugspurger@nvidia.com>
2025-06-06 13:25:01 -0500

    Fix cudf_polars spilling (#19101)
    This fixes a bug where cudf_polars failed when using spilling.
    
    https://github.com/rapidsai/cudf/pull/18970 made some assumptions about the callables that we put in our task graph: namely that the first argument is always some `IR.do_evaluate`. This is true, *except* for when we post-process the task graph to do our spillable wrapping.
    
    We're able to simplify the implementation to just decorating `{IrNode}.do_evaluate` because we don't have to deal with `Expr` types and we aren't (currently) inspecting the `*args` and the output for things like table sizes.
    
    Closes https://github.com/rapidsai/cudf/issues/19100
    
    Authors:
      - Tom Augspurger (https://github.com/TomAugspurger)
    
    Approvers:
      - Richard (Rick) Zamora (https://github.com/rjzamora)
    
    URL: https://github.com/rapidsai/cudf/pull/19101

--- python/cudf_polars/cudf_polars/dsl/tracing.py
+++ python/cudf_polars/cudf_polars/dsl/tracing.py
@@ -1,45 +1,16 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
 
-"""Tracing."""
+"""Utilities for tracing and monitoring IR execution."""
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+import functools
 
 import nvtx
 
-if TYPE_CHECKING:
-    import cudf_polars.containers.dataframe
-    from cudf_polars.dsl.ir import IR
-
-
 CUDF_POLARS_NVTX_DOMAIN = "cudf_polars"
 
-
-def do_evaluate_with_tracing(
-    cls: type[IR],
-    *args: Any,
-) -> cudf_polars.containers.dataframe.DataFrame:
-    """
-    Wrapper for IR.do_evaluate.
-
-    This adds an nvtx annotation in the cudf_polars domain.
-
-    Parameters
-    ----------
-    cls
-        The type of the IR node to evaluate. Its ``do_evaluate``
-        method will be called inside an nvtx annotation.
-    args
-        The arguments to pass to ``cls.do_evaluate``.
-
-    Returns
-    -------
-    The result of the do_evaluate method.
-    """
-    with nvtx.annotate(
-        message=cls.__name__,
-        domain=CUDF_POLARS_NVTX_DOMAIN,
-    ):
-        return cls.do_evaluate(*args)
+nvtx_annotate_cudf_polars = functools.partial(
+    nvtx.annotate, domain=CUDF_POLARS_NVTX_DOMAIN
+)
