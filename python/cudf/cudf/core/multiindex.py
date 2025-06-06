2a50b77da98814fd6cc422d7ae497e1cc9934abf
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-06-06 15:52:59 -0700

    Share Index.values with base implementaiton

--- python/cudf/cudf/core/multiindex.py
+++ python/cudf/cudf/core/multiindex.py
@@ -1245,7 +1245,7 @@ class MultiIndex(Index):
         # TODO: Could implement as Index of ListDtype?
         raise NotImplementedError("to_flat_index is not currently supported.")
 
-    @property  # type: ignore
+    @property
     @_performance_tracking
     def values_host(self) -> np.ndarray:
         """
@@ -1273,7 +1273,7 @@ class MultiIndex(Index):
         """
         return self.to_pandas().values
 
-    @property  # type: ignore
+    @property
     @_performance_tracking
     def values(self) -> cp.ndarray:
         """
@@ -1307,7 +1307,7 @@ class MultiIndex(Index):
             raise NotImplementedError(
                 "Unable to create a cupy array with tuples."
             )
-        return self.to_frame(index=False).values
+        return Frame.to_cupy(self)
 
     @classmethod
     @_performance_tracking
