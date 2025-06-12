7a40e011f089356782b74199a891dbdfade54b4e
Gil Forsyth <gforsyth@nvidia.com>
2025-06-12 09:57:41 -0400

    test(pylibcudf): update coveragerc relative path

--- ci/test_python_cudf.sh
+++ ci/test_python_cudf.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2022-2025, NVIDIA CORPORATION.
 
 # Support invoking test_python_cudf.sh outside the script directory
-cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../;
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../ || exit 1
 
 # Common setup steps shared by Python test jobs
 source ./ci/test_python_common.sh test_python_pylibcudf test_python_cudf
@@ -19,7 +19,7 @@ rapids-logger "pytest pylibcudf"
   --junitxml="${RAPIDS_TESTS_DIR}/junit-pylibcudf.xml" \
   --numprocesses=8 \
   --dist=worksteal \
-  --cov-config=../.coveragerc \
+  --cov-config=.coveragerc \
   --cov=pylibcudf \
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/pylibcudf-coverage.xml" \
   --cov-report=term
