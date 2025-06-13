8af7e2ff0925d6d336a11cefaffdc967bb07fa01
rjzamora <rzamora217@gmail.com>
2025-06-13 10:41:40 -0700

    replace child with Empty when selection doesn't depend on real columns

--- python/cudf_polars/cudf_polars/experimental/select.py
+++ python/cudf_polars/cudf_polars/experimental/select.py
@@ -4,17 +4,18 @@
 
 from __future__ import annotations
 
+from itertools import chain
 from typing import TYPE_CHECKING
 
 import pylibcudf as plc
 
 from cudf_polars.dsl import expr
-from cudf_polars.dsl.ir import HConcat, Scan, Select, Union
+from cudf_polars.dsl.ir import Empty, HConcat, Scan, Select, Union
 from cudf_polars.dsl.traversal import traversal
 from cudf_polars.experimental.base import PartitionInfo
 from cudf_polars.experimental.dispatch import lower_ir_node
 from cudf_polars.experimental.expressions import decompose_expr_graph
-from cudf_polars.experimental.utils import _lower_ir_fallback
+from cudf_polars.experimental.utils import _leaf_column_names, _lower_ir_fallback
 from cudf_polars.utils.versions import POLARS_VERSION_LT_128
 
 if TYPE_CHECKING:
@@ -116,6 +117,7 @@ def _(
         and isinstance(child.children[0], Scan)
         and child.children[0].predicate is None
     ):
+        # Special Case: Fast count.
         scan = child.children[0]
         count = scan.fast_count()
         dtype = ir.exprs[0].value.dtype
@@ -135,9 +137,18 @@ def _(
         partition_info[new_node] = PartitionInfo(count=1)
         return new_node, partition_info
 
+    if not tuple(
+        chain.from_iterable(_leaf_column_names(expr.value) for expr in ir.exprs)
+    ):
+        # Special Case: Selection does not depend on any columns.
+        new_node = ir.reconstruct([input_ir := Empty()])
+        partition_info[new_node] = partition_info[input_ir] = PartitionInfo(count=1)
+        return new_node, partition_info
+
     if pi.count > 1 and not all(
         expr.is_pointwise for expr in traversal([e.value for e in ir.exprs])
     ):
+        # Special Case: Multiple partitions with 1+ non-pointwise expressions.
         try:
             # Try decomposing the underlying expressions
             return decompose_select(
