386603b94a15628f5e9c16267f77c0a21687fd73
Nghia Truong <7416935+ttnghia@users.noreply.github.com>
2025-06-13 20:43:06 -0700

    Add `cudf/io/codec.hpp` to expose compression/decompression APIs (#19113)
    This pull request makes some changes to the IO framework, moving the declaration of compression/decompression-related stuffs into a detail header. The new header is `codec.hpp` which includes compression and decompression data structure and functions.
    
    Beyond that, it also performs some other additional refactors, renaming variables to improve clarity and maintainability. In particular, renaming `compression_status` into `codec_status` and `compression_result` into `codec_exec_result`.
    
    No new code or functionality is added in this work. Only renaming variables and moving code around.
    
    Authors:
      - Nghia Truong (https://github.com/ttnghia)
    
    Approvers:
      - Muhammad Haseeb (https://github.com/mhaseeb123)
      - Vukasin Milovanovic (https://github.com/vuule)
    
    URL: https://github.com/rapidsai/cudf/pull/19113

--- cpp/src/io/comp/compression.hpp
+++ cpp/src/io/comp/compression.hpp
@@ -16,33 +16,12 @@
 
 #pragma once
 
-#include "common.hpp"
-
+#include <cudf/io/detail/codec.hpp>
 #include <cudf/io/types.hpp>
 #include <cudf/utilities/span.hpp>
 
-#include <vector>
-
 namespace CUDF_EXPORT cudf {
 namespace io::detail {
-
-/**
- * @brief Check if compression is supported for the given compression type.
- *
- * @param compression Compression type
- * @return Boolean indicating if the compression type is supported
- */
-[[nodiscard]] bool is_compression_supported(compression_type compression);
-
-/**
- * @brief Compresses a system memory buffer.
- *
- * @param compression Type of compression of the input data
- * @param src         Decompressed host buffer
- *
- * @return Vector containing the Compressed output
- */
-std::vector<uint8_t> compress(compression_type compression, host_span<uint8_t const> src);
 
 /**
  * @brief Maximum size of uncompressed chunks that can be compressed.
@@ -69,21 +48,6 @@ std::vector<uint8_t> compress(compression_type compression, host_span<uint8_t co
 [[nodiscard]] size_t max_compressed_size(compression_type compression, uint32_t uncompressed_size);
 
 /**
- * @brief Compresses device memory buffers.
- *
- * @param compression Type of compression of the input data
- * @param inputs      Device memory buffers to compress
- * @param outputs     Device memory buffers to store the compressed output
- * @param results     Compression results
- * @param stream      CUDA stream used for device memory operations and kernel launches
- */
-void compress(compression_type compression,
-              device_span<device_span<uint8_t const> const> inputs,
-              device_span<device_span<uint8_t> const> outputs,
-              device_span<compression_result> results,
-              rmm::cuda_stream_view stream);
-
-/**
  * @brief Aggregate results of compression into a single statistics object.
  *
  * @param inputs List of uncompressed input buffers
@@ -93,7 +57,7 @@ void compress(compression_type compression,
  */
 [[nodiscard]] writer_compression_statistics collect_compression_statistics(
   device_span<device_span<uint8_t const> const> inputs,
-  device_span<compression_result const> results,
+  device_span<codec_exec_result const> results,
   rmm::cuda_stream_view stream);
 
 }  // namespace io::detail
