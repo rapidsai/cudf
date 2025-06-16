a72ac4805927cd9f047c218ed933be72ae5c2601
rjzamora <rzamora217@gmail.com>
2025-06-16 11:35:27 -0700

    fix 19152

--- python/cudf_polars/cudf_polars/experimental/groupby.py
+++ python/cudf_polars/cudf_polars/experimental/groupby.py
@@ -231,10 +231,11 @@ def _(
     )
     child_count = partition_info[child].count
     partition_info[gb_pwise] = PartitionInfo(count=child_count)
+    grouped_keys = tuple(NamedExpr(k.name, Col(k.value.dtype, k.name)) for k in ir.keys)
 
     # Reduction
     gb_inter: GroupBy | Repartition | Shuffle
-    reduction_schema = {k.name: k.value.dtype for k in ir.keys} | {
+    reduction_schema = {k.name: k.value.dtype for k in grouped_keys} | {
         k.name: k.value.dtype for k in reduction_exprs
     }
     if not shuffled and post_aggregation_count > 1:
@@ -248,7 +249,7 @@ def _(
 
         gb_inter = Shuffle(
             gb_pwise.schema,
-            ir.keys,
+            grouped_keys,
             ir.config_options,
             gb_pwise,
         )
@@ -269,7 +270,7 @@ def _(
             if count > post_aggregation_count:
                 gb_inter = GroupBy(
                     reduction_schema,
-                    ir.keys,
+                    grouped_keys,
                     reduction_exprs,
                     ir.maintain_order,
                     None,
@@ -281,7 +282,7 @@ def _(
     # Final aggregation
     gb_reduce = GroupBy(
         reduction_schema,
-        ir.keys,
+        grouped_keys,
         reduction_exprs,
         ir.maintain_order,
         ir.zlice,
@@ -294,7 +295,7 @@ def _(
     new_node = Select(
         ir.schema,
         [
-            *(NamedExpr(k.name, Col(k.value.dtype, k.name)) for k in ir.keys),
+            *(NamedExpr(k.name, Col(k.value.dtype, k.name)) for k in grouped_keys),
             *selection_exprs,
         ],
         False,  # noqa: FBT003
@@ -302,6 +303,6 @@ def _(
     )
     partition_info[new_node] = PartitionInfo(
         count=post_aggregation_count,
-        partitioned_on=ir.keys,
+        partitioned_on=grouped_keys,
     )
     return new_node, partition_info
