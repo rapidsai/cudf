0684f654b5f55ea6d454b1fbfe9919af2e8432af
David Wendt <dwendt@nvidia.com>
2025-06-26 11:26:53 -0400

    Use cub radix sort directly for all fixed-width-types in cudf::sorted_order

--- cpp/src/sort/sorted_order_radix.cu
+++ cpp/src/sort/sorted_order_radix.cu
@@ -14,26 +14,29 @@
  * limitations under the License.
  */
 
-#pragma once
-
-#include "common_sort_impl.cuh"
-
-#include <cudf/column/column_device_view.cuh>
+#include <cudf/column/column.hpp>
+#include <cudf/column/column_view.hpp>
 #include <cudf/utilities/error.hpp>
 #include <cudf/utilities/traits.hpp>
+#include <cudf/utilities/type_dispatcher.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
+#include <rmm/device_uvector.hpp>
 #include <rmm/exec_policy.hpp>
 
-#include <thrust/sort.h>
+#include <cub/device/device_radix_sort.cuh>
+#include <thrust/iterator/zip_iterator.h>
+#include <thrust/sequence.h>
+#include <thrust/transform.h>
 
 namespace cudf {
 namespace detail {
+namespace {
 
 template <typename F>
 struct float_pair {
-  size_type s;
-  F f;
+  size_type s;  // index bias for sorting nan
+  F f;          // actual float value to sort
 };
 
 template <typename F>
@@ -46,8 +49,8 @@ struct float_decomposer {
 
 template <typename F>
 struct float_to_pair_and_seq {
-  F* fs;
-  __device__ cuda::std::pair<float_pair<F>, size_type> operator()(cudf::size_type idx)
+  F const* fs;
+  __device__ cuda::std::pair<float_pair<F>, size_type> operator()(cudf::size_type idx) const
   {
     auto const f = fs[idx];
     auto const s = (isnan(f) * (idx + 1));  // multiplier helps keep the sort stable for NaNs
@@ -56,75 +59,50 @@ struct float_to_pair_and_seq {
 };
 
 /**
- * @brief Sort indices of a single column.
+ * @brief Sorts fixed-width columns using faster thrust sort
  *
- * This API offers fast sorting for most primitive types.
- *
- * @tparam method Whether to use stable sort
- * @param input Column to sort. The column data is not modified.
- * @param indices The result of the sort
- * @param ascending Sort order
- * @param stream CUDA stream used for device memory operations and kernel launches
+ * Should not be called if `input.has_nulls()==true`
  */
-template <sort_method method>
-void faster_sorted_order(column_view const& input,
-                         mutable_column_view& indices,
-                         bool ascending,
-                         rmm::cuda_stream_view stream);
+struct sorted_order_radix_fn {
+  column_view const& input;      // keys to sort
+  mutable_column_view& indices;  // output of sort
+  bool ascending;                // true for ascending sort
+  rmm::cuda_stream_view stream;  // for allocation and kernel launches
 
-template <sort_method method>
-struct faster_sorted_order_fn {
-  /**
-   * @brief Sorts fixed-width columns using faster thrust sort
-   *
-   * Should not be called if `input.has_nulls()==true`
-   *
-   * @param input Column to sort
-   * @param indices Output sorted indices
-   * @param ascending True if sort order is ascending
-   * @param stream CUDA stream used for device memory operations and kernel launches
-   */
   template <typename T>
-  void faster_sort(mutable_column_view& input,
-                   mutable_column_view& indices,
-                   bool ascending,
-                   rmm::cuda_stream_view stream)
+  void radix_sort()
   {
-    // A thrust sort on a column of most primitive types will use a radix sort.
-    // For other fixed-width types, thrust may use merge-sort.
-    // The API sorts inplace so it requires making a copy of the input data
-    // and creating the input indices sequence.
-    thrust::sequence(
-      rmm::exec_policy_nosync(stream), indices.begin<size_type>(), indices.end<size_type>(), 0);
+    auto d_in   = input.begin<T>();
+    auto output = rmm::device_uvector<T>(input.size(), stream);
+    auto d_out  = output.begin();  // not returned
+    auto seqs   = rmm::device_uvector<cudf::size_type>(input.size(), stream);
+    thrust::sequence(rmm::exec_policy_nosync(stream), seqs.begin(), seqs.end(), 0);
+    auto dv_in  = seqs.begin();
+    auto dv_out = indices.begin<cudf::size_type>();
 
-    auto const do_sort = [&](auto const comp) {
-      if constexpr (method == sort_method::STABLE) {
-        thrust::stable_sort_by_key(rmm::exec_policy_nosync(stream),
-                                   input.begin<T>(),
-                                   input.end<T>(),
-                                   indices.begin<size_type>(),
-                                   comp);
-      } else {
-        thrust::sort_by_key(rmm::exec_policy_nosync(stream),
-                            input.begin<T>(),
-                            input.end<T>(),
-                            indices.begin<size_type>(),
-                            comp);
-      }
-    };
+    auto const n       = input.size();
+    auto const sv      = stream.value();
+    auto const end_bit = sizeof(T) * 8;
 
+    // cub radix sort implementation is always stable
+    std::size_t tmp_bytes = 0;
     if (ascending) {
-      do_sort(cuda::std::less<T>{});
+      cub::DeviceRadixSort::SortPairs(
+        nullptr, tmp_bytes, d_in, d_out, dv_in, dv_out, n, 0, end_bit, sv);
+      auto tmp_stg = rmm::device_buffer(tmp_bytes, stream);
+      cub::DeviceRadixSort::SortPairs(
+        tmp_stg.data(), tmp_bytes, d_in, d_out, dv_in, dv_out, n, 0, end_bit, sv);
     } else {
-      do_sort(cuda::std::greater<T>{});
+      cub::DeviceRadixSort::SortPairsDescending(
+        nullptr, tmp_bytes, d_in, d_out, dv_in, dv_out, n, 0, end_bit, sv);
+      auto tmp_stg = rmm::device_buffer(tmp_bytes, stream);
+      cub::DeviceRadixSort::SortPairsDescending(
+        tmp_stg.data(), tmp_bytes, d_in, d_out, dv_in, dv_out, n, 0, end_bit, sv);
     }
   }
 
   template <typename T>
-  void operator()(mutable_column_view& input,
-                  mutable_column_view& indices,
-                  bool ascending,
-                  rmm::cuda_stream_view stream)
+  void operator()()
     requires(cudf::is_floating_point<T>())
   {
     auto pair_in = rmm::device_uvector<float_pair<T>>(input.size(), stream);
@@ -165,33 +143,51 @@ struct faster_sorted_order_fn {
   }
 
   template <typename T>
-  void operator()(mutable_column_view& input,
-                  mutable_column_view& indices,
-                  bool ascending,
-                  rmm::cuda_stream_view stream)
+  void operator()()
     requires(cudf::is_chrono<T>())
   {
     using rep_type = typename T::rep;
-    faster_sort<rep_type>(input, indices, ascending, stream);
+    radix_sort<rep_type>();
   }
 
   template <typename T>
-  void operator()(mutable_column_view& input,
-                  mutable_column_view& indices,
-                  bool ascending,
-                  rmm::cuda_stream_view stream)
+  void operator()()
     requires(cudf::is_fixed_width<T>() and !cudf::is_chrono<T>() and !cudf::is_floating_point<T>())
   {
-    faster_sort<T>(input, indices, ascending, stream);
+    radix_sort<T>();
   }
 
   template <typename T>
-  void operator()(mutable_column_view&, mutable_column_view&, bool, rmm::cuda_stream_view)
+  void operator()()
     requires(not cudf::is_fixed_width<T>())
   {
-    CUDF_UNREACHABLE("invalid type for faster sort");
+    CUDF_UNREACHABLE("invalid type for radix sort");
   }
 };
+}  // namespace
 
+bool is_radix_sortable(column_view const& column)
+{
+  return !column.has_nulls() && cudf::is_fixed_width(column.type());
+}
+
+/**
+ * @brief Sort indices of a single column.
+ *
+ * This API offers fast sorting for most primitive types.
+ *
+ * @param input Column to sort
+ * @param indices The result of the sort
+ * @param ascending Sort order
+ * @param stream CUDA stream used for device memory operations and kernel launches
+ */
+void sorted_order_radix(column_view const& input,
+                        mutable_column_view& indices,
+                        bool ascending,
+                        rmm::cuda_stream_view stream)
+{
+  cudf::type_dispatcher<dispatch_storage_type>(
+    input.type(), sorted_order_radix_fn{input, indices, ascending, stream});
+}
 }  // namespace detail
 }  // namespace cudf
