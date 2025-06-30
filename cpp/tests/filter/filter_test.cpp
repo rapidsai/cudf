2e8a66dfe571136ec550f01af9c1022d14aa3d50
Basit Ayantunde <rlamarrr@gmail.com>
2025-06-30 18:11:22 +0100

    Update cpp/tests/filter/filter_test.cpp
    Co-authored-by: David Wendt <45795991+davidwendt@users.noreply.github.com>

--- cpp/tests/filter/filter_test.cpp
+++ cpp/tests/filter/filter_test.cpp
@@ -1,8 +1,6 @@
 /*
- * Copyright (c) 2019-2025, NVIDIA CORPORATION.
- *
- * Copyright 2018-2019 BlazingDB, Inc.
- *     Copyright 2018 Christian Noboa Mardini <christian@blazingdb.com>
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
