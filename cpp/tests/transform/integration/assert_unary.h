d894d8ceb7957c61627505ca643d0cab93d6c2a8
Vyas Ramasubramani <vyasr@nvidia.com>
2025-06-30 16:44:46 -0700

    Fix std::equal usage in tests

--- cpp/tests/transform/integration/assert_unary.h
+++ cpp/tests/transform/integration/assert_unary.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2023, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,32 +20,55 @@
 #include <cudf_test/column_wrapper.hpp>
 #include <cudf_test/cudf_gtest.hpp>
 
+#include <cuda/std/tuple.h>
+#include <thrust/iterator/zip_iterator.h>
+
+#include <algorithm>
+
 namespace transformation {
 template <typename TypeOut, typename TypeIn, typename TypeOpe>
 void ASSERT_UNARY(cudf::column_view const& out, cudf::column_view const& in, TypeOpe&& ope)
 {
-  auto in_h     = cudf::test::to_host<TypeIn>(in);
-  auto in_data  = in_h.first;
-  auto out_h    = cudf::test::to_host<TypeOut>(out);
-  auto out_data = out_h.first;
+  auto in_h    = cudf::test::to_host<TypeIn>(in);
+  auto in_data = in_h.first;
 
-  ASSERT_TRUE(out_data.size() == in_data.size());
+  auto begin = thrust::make_zip_iterator(cuda::std::make_tuple(in_data.begin(), out_data.begin()));
+  auto end   = thrust::make_zip_iterator(cuda::std::make_tuple(in_data.end(), out_data.end()));
 
-  auto data_comparator = [ope](TypeIn const& in, TypeOut const& out) {
-    EXPECT_EQ(out, static_cast<TypeOut>(ope(in)));
-    return true;
-  };
-  std::equal(in_data.begin(), in_data.end(), out_data.begin(), data_comparator);
+  std::for_each(begin, end, [ope](auto const& zipped) {
+    auto [in_val, out_val] = zipped;
+    EXPECT_EQ(out_val, static_cast<TypeOut>(ope(in_val)));
+  });
 
   auto in_valid  = in_h.second;
   auto out_valid = out_h.second;
 
   ASSERT_TRUE(out_valid.size() == in_valid.size());
-  auto valid_comparator = [](bool const& in, bool const& out) {
-    EXPECT_EQ(out, in);
-    return true;
-  };
-  std::equal(in_valid.begin(), in_valid.end(), out_valid.begin(), valid_comparator);
+
+  auto valid_begin =
+    thrust::make_zip_iterator(cuda::std::make_tuple(in_valid.begin(), out_valid.begin()));
+  auto valid_end =
+    thrust::make_zip_iterator(cuda::std::make_tuple(in_valid.end(), out_valid.end()));
+
+  std::for_each(valid_begin, valid_end, [](auto const& zipped) {
+    auto [in_flag, out_flag] = zipped;
+    EXPECT_EQ(out_flag, in_flag);
+  });
+
+  auto in_valid  = in_h.second;
+  auto out_valid = out_h.second;
+
+  ASSERT_TRUE(out_valid.size() == in_valid.size());
+
+  auto valid_begin =
+    thrust::make_zip_iterator(thrust::make_tuple(in_valid.begin(), out_valid.begin()));
+  auto valid_end = thrust::make_zip_iterator(thrust::make_tuple(in_valid.end(), out_valid.end()));
+
+  std::for_each(valid_begin, valid_end, [](thrust::tuple<bool, bool> const& t) {
+    bool const& in_flag  = thrust::get<0>(t);
+    bool const& out_flag = thrust::get<1>(t);
+    EXPECT_EQ(out_flag, in_flag);
+  });
 }
 
 }  // namespace transformation
