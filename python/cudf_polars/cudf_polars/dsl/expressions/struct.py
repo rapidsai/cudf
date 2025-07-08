7c73a87b926d73844e55239f13f384045c62e18b
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-07-08 10:34:39 -0700

    Add note about D101 noqa

--- python/cudf_polars/cudf_polars/dsl/expressions/struct.py
+++ python/cudf_polars/cudf_polars/dsl/expressions/struct.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
-# TODO: remove need for this
+# TODO: Document StructFunction to remove noqa
 # ruff: noqa: D101
 """Struct DSL nodes."""
 
