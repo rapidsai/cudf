76001c022ca068417de0df2ac1bd950ee42f4318
David Wendt <dwendt@nvidia.com>
2025-07-14 08:53:47 -0400

    Replace sprintf with std::format in libcudf parquet tests

--- cpp/tests/io/parquet_experimental_reader_test.cpp
+++ cpp/tests/io/parquet_experimental_reader_test.cpp
@@ -134,12 +134,8 @@ cudf::test::strings_column_wrapper constant_strings(cudf::size_type value)
 {
   CUDF_EXPECTS(value >= 0 && value <= 9999, "String value must be between 0000 and 9999");
 
-  std::array<char, 5> buf;
-  auto elements =
-    thrust::make_transform_iterator(thrust::make_constant_iterator(value), [&buf](auto i) {
-      sprintf(buf.data(), "%04d", i);
-      return std::string(buf.data());
-    });
+  auto elements = thrust::make_transform_iterator(thrust::make_constant_iterator(value),
+                                                  [](auto i) { return std::format("{:04d}", i); });
   return cudf::test::strings_column_wrapper(elements, elements + num_ordered_rows);
 }
 
