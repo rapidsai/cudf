f28d8bae1d1243855ba06852d745c576ce2af81c
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-07-25 13:22:08 -0700

    Move constructor tests

--- python/cudf/cudf/tests/test_datetime.py
+++ python/cudf/cudf/tests/test_datetime.py
@@ -1530,34 +1530,6 @@ def test_last(idx, offset):
     assert_eq(expect, got)
 
 
-@pytest.mark.parametrize(
-    "data",
-    [
-        [
-            "2020-01-31",
-            "2020-02-15",
-            "2020-02-29",
-            "2020-03-15",
-            "2020-03-31",
-            "2020-04-15",
-            "2020-04-30",
-        ],
-        [43534, 43543, 37897, 2000],
-    ],
-)
-@pytest.mark.parametrize("dtype", [None, "datetime64[ns]"])
-def test_datetime_constructor(data, dtype):
-    expected = pd.DatetimeIndex(data=data, dtype=dtype)
-    actual = cudf.DatetimeIndex(data=data, dtype=dtype)
-
-    assert_eq(expected, actual)
-
-    expected = pd.DatetimeIndex(data=pd.Series(data), dtype=dtype)
-    actual = cudf.DatetimeIndex(data=cudf.Series(data), dtype=dtype)
-
-    assert_eq(expected, actual)
-
-
 def test_datetime_binop_tz_timestamp(op):
     s = cudf.Series([1, 2, 3], dtype="datetime64[ns]")
     pd_tz_timestamp = pd.Timestamp("1970-01-01 00:00:00.000000001", tz="utc")
@@ -1588,45 +1560,6 @@ def test_datetime_series_cmpops_pandas_compatibility(op):
 def test_datetime_getitem_na():
     s = cudf.Series([1, 2, None, 3], dtype="datetime64[ns]")
     assert s[2] is cudf.NaT
-
-
-@pytest.mark.parametrize(
-    "data,dtype,freq",
-    [
-        ([10], "datetime64[ns]", "2ns"),
-        ([10, 12, 14, 16], "datetime64[ns]", "2ns"),
-        ([10, 11, 12, 13], "datetime64[ns]", "1ns"),
-        ([100, 200, 300, 400], "datetime64[s]", "100s"),
-        ([101, 201, 301, 401], "datetime64[ms]", "100ms"),
-    ],
-)
-def test_datetime_index_with_freq(data, dtype, freq):
-    actual = cudf.DatetimeIndex(data, dtype=dtype, freq=freq)
-    expected = pd.DatetimeIndex(data, dtype=dtype, freq=freq)
-    assert_eq(actual, expected)
-
-
-@pytest.mark.parametrize(
-    "data,dtype,freq",
-    [
-        ([10, 1232, 13244, 13426], "datetime64[ns]", "2ns"),
-        ([10, 11, 12, 13], "datetime64[ns]", "1s"),
-        ([10000, 200, 300, 400], "datetime64[s]", "100s"),
-        ([107871, 201, 301, 401], "datetime64[ms]", "100ns"),
-    ],
-)
-def test_datetime_index_freq_error(data, dtype, freq):
-    assert_exceptions_equal(
-        pd.DatetimeIndex,
-        cudf.DatetimeIndex,
-        ([data], {"dtype": dtype, "freq": freq}),
-        ([data], {"dtype": dtype, "freq": freq}),
-    )
-
-
-def test_strings_with_utc_offset_not_implemented():
-    with pytest.raises(NotImplementedError):
-        DatetimeIndex(["2022-07-22 00:00:00+02:00"])
 
 
 @pytest.mark.parametrize(
@@ -1763,14 +1696,6 @@ def test_datetime_string_to_datetime_resolution_loss_raises():
         cudf.Series(data, dtype=dtype)
     with pytest.raises(ValueError):
         pd.Series(data, dtype=dtype)
-
-
-def test_dateimeindex_from_noniso_string():
-    data = ["20160920", "20160925"]
-    gdti = cudf.DatetimeIndex(data)
-    pdti = pd.DatetimeIndex(data)
-
-    assert_eq(gdti, pdti)
 
 
 @pytest.mark.parametrize(
