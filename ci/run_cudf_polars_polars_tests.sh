a49cd3c3a670f1ccdd6d01abc1ebacc529d43f34
Matthew Murray <matthewmurray711@gmail.com>
2025-08-06 18:20:34 -0400

    remove skip test module

--- ci/run_cudf_polars_polars_tests.sh
+++ ci/run_cudf_polars_polars_tests.sh
@@ -15,7 +15,6 @@ DESELECTED_TESTS=(
     "tests/unit/test_polars_import.py::test_fork_safety" # test started to hang in polars-1.14
     "tests/unit/operations/test_join.py::test_join_4_columns_with_validity" # fails in some systems, see https://github.com/pola-rs/polars/issues/19870
     "tests/unit/io/test_csv.py::test_read_web_file" # fails in rockylinux8 due to SSL CA issues
-    "tests/unit/io/cloud/test_credential_provider.py" # Test module has a bad import in Polars 1.32
 )
 
 if [[ $(arch) == "aarch64" ]]; then
