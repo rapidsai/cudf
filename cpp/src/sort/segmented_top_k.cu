de3d28171be1b6d94140c0bc6870c04d42443dac
David Wendt <dwendt@nvidia.com>
2025-08-11 17:42:37 -0400

    update tests

---
+++ cpp/src/sort/segmented_top_k.cu
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "sort_column_impl.cuh"
+
+#include <cudf/column/column.hpp>
+#include <cudf/column/column_factories.hpp>
+#include <cudf/detail/copy.hpp>
+#include <cudf/detail/gather.hpp>
+#include <cudf/detail/nvtx/ranges.hpp>
+#include <cudf/detail/sizes_to_offsets_iterator.cuh>
+#include <cudf/detail/sorting.hpp>
+#include <cudf/detail/utilities/grid_1d.cuh>
+#include <cudf/sorting.hpp>
+#include <cudf/table/table_view.hpp>
+#include <cudf/utilities/default_stream.hpp>
+#include <cudf/utilities/memory_resource.hpp>
+
+#include <rmm/cuda_stream_view.hpp>
+
+#include <thrust/binary_search.h>
+#include <thrust/execution_policy.h>
+#include <thrust/remove.h>
+
+namespace cudf {
+namespace detail {
+namespace {
+
+CUDF_KERNEL void resolve_segment_indices(device_span<size_type const> d_offsets,
+                                         size_type k,
+                                         device_span<size_type> d_indices,
+                                         device_span<size_type> d_segment_sizes)
+{
+  auto const tid = cudf::detail::grid_1d::global_thread_id();
+  if (tid >= d_indices.size()) { return; }
+
+  auto const sitr = thrust::upper_bound(thrust::seq, d_offsets.begin(), d_offsets.end(), tid);
+  auto const segment_start = *(sitr - 1);
+  auto const segment_end   = *sitr;
+  auto const index         = tid - segment_start;
+  if (index >= k) { d_indices[tid] = -1; }  // mark values outside of top k
+
+  if (index == 0) {
+    auto const segment_size  = segment_end - segment_start;
+    auto const segment_index = thrust::distance(d_offsets.begin(), sitr) - 1;
+    // segment is k or less elements
+    d_segment_sizes[segment_index] = cuda::std::min(k, segment_size);
+  }
+}
+}  // namespace
+
+std::unique_ptr<column> top_k_segmented_order(column_view const& col,
+                                              column_view const& segment_offsets,
+                                              size_type k,
+                                              order sort_order,
+                                              rmm::cuda_stream_view stream,
+                                              rmm::device_async_resource_ref mr)
+{
+  CUDF_EXPECTS(k > 0, "k must be greater than 0", std::invalid_argument);
+  CUDF_EXPECTS(segment_offsets.size() > 0,
+               "segment_offsets must have at least one element",
+               std::invalid_argument);
+  auto const size_data_type = data_type{type_to_id<size_type>()};
+  CUDF_EXPECTS(segment_offsets.type() == size_data_type,
+               "segment_offsets must be of type INT32",
+               std::invalid_argument);
+  CUDF_EXPECTS(segment_offsets.null_count() == 0,
+               "segment_offsets must not have nulls",
+               std::invalid_argument);
+
+  auto const nulls   = sort_order == order::ASCENDING ? null_order::AFTER : null_order::BEFORE;
+  auto const temp_mr = cudf::get_current_device_resource_ref();
+  auto const indices = cudf::detail::segmented_sorted_order(
+    cudf::table_view({col}), segment_offsets, {sort_order}, {nulls}, stream, temp_mr);
+  if (indices->view().is_empty()) {
+    return cudf::make_empty_lists_column(size_data_type, stream, mr);
+  }
+
+  auto const d_indices = indices->mutable_view().begin<size_type>();
+  auto segment_sizes   = rmm::device_uvector<size_type>(segment_offsets.size() - 1, stream);
+  auto span_indices = device_span<size_type>{d_indices, static_cast<std::size_t>(indices->size())};
+  auto const grid   = cudf::detail::grid_1d(indices->size(), 256);
+  resolve_segment_indices<<<grid.num_blocks, grid.num_threads_per_block, 0, stream>>>(
+    segment_offsets, k, span_indices, segment_sizes);
+  auto [offsets, total] =
+    cudf::detail::make_offsets_child_column(segment_sizes.begin(), segment_sizes.end(), stream, mr);
+  auto result =
+    cudf::make_fixed_width_column(size_data_type, total, mask_state::UNALLOCATED, stream, mr);
+  auto d_result = result->mutable_view().begin<size_type>();
+  thrust::remove_copy(
+    rmm::exec_policy(stream), d_indices, d_indices + indices->size(), d_result, -1);
+  auto const num_rows = static_cast<size_type>(offsets->size() - 1);
+  return make_lists_column(
+    num_rows, std::move(offsets), std::move(result), 0, rmm::device_buffer{}, stream, mr);
+}
+
+std::unique_ptr<column> top_k_segmented(column_view const& col,
+                                        column_view const& segment_offsets,
+                                        size_type k,
+                                        order sort_order,
+                                        rmm::cuda_stream_view stream,
+                                        rmm::device_async_resource_ref mr)
+{
+  if (col.is_empty()) { return cudf::make_empty_column(col.type()); }
+
+  auto ordered =
+    cudf::detail::top_k_segmented_order(col, segment_offsets, k, sort_order, stream, mr);
+  auto lv = cudf::lists_column_view(ordered->view());
+  if (lv.is_empty()) { return cudf::make_empty_lists_column(col.type(), stream, mr); }
+
+  auto result         = cudf::detail::gather(cudf::table_view({col}),
+                                     lv.child(),
+                                     out_of_bounds_policy::DONT_CHECK,
+                                     negative_index_policy::NOT_ALLOWED,
+                                     stream,
+                                     mr);
+  auto offsets        = std::move(ordered->release().children.front());
+  auto const num_rows = static_cast<size_type>(offsets->size() - 1);
+  return make_lists_column(num_rows,
+                           std::move(offsets),
+                           std::move(result->release().front()),
+                           0,
+                           rmm::device_buffer{},
+                           stream,
+                           mr);
+}
+
+}  // namespace detail
+
+std::unique_ptr<column> top_k_segmented(column_view const& col,
+                                        column_view const& segment_offsets,
+                                        size_type k,
+                                        order sort_order,
+                                        rmm::cuda_stream_view stream,
+                                        rmm::device_async_resource_ref mr)
+{
+  CUDF_FUNC_RANGE();
+  return detail::top_k_segmented(col, segment_offsets, k, sort_order, stream, mr);
+}
+
+std::unique_ptr<column> top_k_segmented_order(column_view const& col,
+                                              column_view const& segment_offsets,
+                                              size_type k,
+                                              order sort_order,
+                                              rmm::cuda_stream_view stream,
+                                              rmm::device_async_resource_ref mr)
+{
+  CUDF_FUNC_RANGE();
+  return detail::top_k_segmented_order(col, segment_offsets, k, sort_order, stream, mr);
+}
+}  // namespace cudf
--- cpp/tests/sort/top_k_tests.cpp
+++ cpp/tests/sort/top_k_tests.cpp
@@ -74,14 +74,14 @@ TYPED_TEST(TopK, TopKSegmented)
   auto input = cudf::test::fixed_width_column_wrapper<T, int32_t>(
     itr, itr + 100, cudf::test::iterators::null_at(4));
   auto offsets =
-    cudf::test::fixed_width_column_wrapper<int32_t>({0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100});
+    cudf::test::fixed_width_column_wrapper<int32_t>({0, 15, 20, 23, 40, 42, 60, 70, 80, 90, 100});
   {
     // clang-format off
     LCW expected({
-      { 9,  8,  7}, {19, 18, 17}, {29, 28, 27}, {39, 38, 37}, {49, 48, 47},
+      {14, 13, 12}, {19, 18, 17}, {22, 21, 20}, {39, 38, 37}, {41, 40},
       {59, 58, 57}, {69, 68, 67}, {79, 78, 77}, {89, 88, 87}, {99, 98, 97}});
     LCWO expected_order({
-      { 9,  8,  7}, {19, 18, 17}, {29, 28, 27}, {39, 38, 37}, {49, 48, 47},
+      {14, 13, 12}, {19, 18, 17}, {22, 21, 20}, {39, 38, 37}, {41, 40},
       {59, 58, 57}, {69, 68, 67}, {79, 78, 77}, {89, 88, 87}, {99, 98, 97}});
     // clang-format on
     auto result = cudf::top_k_segmented(input, offsets, 3);
@@ -93,11 +93,11 @@ TYPED_TEST(TopK, TopKSegmented)
   {
     // clang-format off
     LCW expected({
-      {0,  1,  2},  {10, 11, 12}, {20, 21, 22}, {30, 31, 32}, {40, 41, 42},
-      {50, 51, 52}, {60, 61, 62}, {70, 71, 72}, {80, 81, 82}, {90, 91, 92}});
+      {0,  1,  2},  {15, 16, 17}, {20, 21, 22}, {23, 24, 25}, {40, 41},
+      {42, 43, 44}, {60, 61, 62}, {70, 71, 72}, {80, 81, 82}, {90, 91, 92}});
      LCWO expected_order({
-      {0,  1,  2},  {10, 11, 12}, {20, 21, 22}, {30, 31, 32}, {40, 41, 42},
-      {50, 51, 52}, {60, 61, 62}, {70, 71, 72}, {80, 81, 82}, {90, 91, 92}});
+      {0,  1,  2},  {15, 16, 17}, {20, 21, 22}, {23, 24, 25}, {40, 41},
+      {42, 43, 44}, {60, 61, 62}, {70, 71, 72}, {80, 81, 82}, {90, 91, 92}});
     // clang-format on
     auto result = cudf::top_k_segmented(input, offsets, 3, cudf::order::ASCENDING);
     CUDF_TEST_EXPECT_COLUMNS_EQUIVALENT(expected, result->view());
