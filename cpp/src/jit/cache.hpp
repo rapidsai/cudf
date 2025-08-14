746677b0000655e98a841a582ceee268dd5a9e98
Basit Ayantunde <rlamarrr@gmail.com>
2025-08-14 20:10:03 +0000

    added WAR for gcc warning

--- cpp/src/jit/cache.hpp
+++ cpp/src/jit/cache.hpp
@@ -15,7 +15,9 @@
  */
 
 #pragma once
-#pragma GCC diagnostic ignored "-Werror=ignored-attributes"  // Work-around for JITIFY2's false-positive warnings when compiled with GCC13
+#pragma GCC diagnostic ignored \
+  "-Werror=ignored-attributes"  // Work-around for JITIFY2's false-positive warnings when compiled
+                                // with GCC13
 
 #include <cudf/utilities/export.hpp>
 
