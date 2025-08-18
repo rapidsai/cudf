4e805218755474b06f16320cf9100f80fa22d3d7
Matthew Murray <matthewmurray711@gmail.com>
2025-08-18 13:10:36 -0400

    dont set numprocesses in conda python tests job for cudf-polars

--- ci/test_python_other.sh
+++ ci/test_python_other.sh
@@ -40,14 +40,9 @@ rapids-logger "pytest custreamz"
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/custreamz-coverage.xml" \
   --cov-report=term
 
-# Note that cudf-polars uses rmm.mr.CudaAsyncMemoryResource() which allocates
-# half the available memory. This doesn't play well with multiple workers, so
-# we keep --numprocesses=1 for now. This should be resolved by
-# https://github.com/rapidsai/cudf/issues/16723.
 rapids-logger "pytest cudf-polars"
 ./ci/run_cudf_polars_pytests.sh \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cudf-polars.xml" \
-  --numprocesses=1 \
   --dist=worksteal \
   --cov-config=./pyproject.toml \
   --cov=cudf_polars \
