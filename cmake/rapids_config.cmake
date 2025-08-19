050de17d318bc00ee0616d2fd7463d20f1417f42
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2025-08-19 16:49:04 -0700

    remove cmake hack

--- cmake/rapids_config.cmake
+++ cmake/rapids_config.cmake
@@ -35,9 +35,6 @@ if(NOT _rapids_branch)
   )
 endif()
 
-set(rapids-cmake-repo vuule/rapids-cmake)
-set(rapids-cmake-branch fea-nvcomp-5)
-
 if(NOT rapids-cmake-version)
   set(rapids-cmake-version "${RAPIDS_VERSION_MAJOR_MINOR}")
 endif()
