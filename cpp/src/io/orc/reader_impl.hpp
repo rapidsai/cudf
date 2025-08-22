b3524491ae8e1b682f29ceae015f956b31a5614a
Chong Gao <res_life@163.com>
2025-08-22 15:21:40 +0800

    Add an option to support reading ORC timestamp column as UTC time.
    Signed-off-by: Chong Gao <res_life@163.com>

--- cpp/src/io/orc/reader_impl.hpp
+++ cpp/src/io/orc/reader_impl.hpp
@@ -173,6 +173,7 @@ class reader_impl {
     bool use_index;            // enable or disable attempt to use row index for parsing
     bool use_np_dtypes;        // enable or disable the conversion to numpy-compatible dtypes
     std::vector<std::string> decimal128_columns;  // control decimals conversion
+    bool ignore_timezone_in_stripe_footer;        // ignore writer timezone in strip footer
 
     // User specified reading rows/stripes selection.
     int64_t const skip_rows;
