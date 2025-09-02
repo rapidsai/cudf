6d415af7c9cbb204e5e1f6597c056808bf0a2e16
GALI PREM SAGAR <sagarprem75@gmail.com>
2025-09-02 17:55:22 -0500

    Update test_groupby.py

--- python/dask_cudf/dask_cudf/tests/test_groupby.py
+++ python/dask_cudf/dask_cudf/tests/test_groupby.py
@@ -11,10 +11,7 @@ from dask import dataframe as dd
 from dask.utils_test import hlg_layer
 
 import cudf
-from cudf.core._compat import (
-    PANDAS_CURRENT_SUPPORTED_VERSION,
-    PANDAS_VERSION,
-)
+from cudf.core._compat import PANDAS_CURRENT_SUPPORTED_VERSION, PANDAS_VERSION
 
 import dask_cudf
 from dask_cudf._expr.groupby import OPTIMIZED_AGGS, _aggs_optimized
