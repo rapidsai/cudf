832d96475b1f1d116c9871fdfc27ee950f4ae0e2
Nghia Truong <nghiat@nvidia.com>
2025-09-03 18:38:10 -0700

    Handle empty aggregation requests
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- cpp/src/groupby/groupby.cu
+++ cpp/src/groupby/groupby.cu
@@ -223,6 +223,9 @@ std::pair<std::unique_ptr<table>, std::vector<aggregation_result>> groupby::aggr
   rmm::device_async_resource_ref mr)
 {
   CUDF_FUNC_RANGE();
+
+  if (requests.size() == 0) { return {empty_like(_keys), std::vector<aggregation_result>{}}; }
+
   CUDF_EXPECTS(
     std::all_of(requests.begin(),
                 requests.end(),
@@ -243,6 +246,9 @@ std::pair<std::unique_ptr<table>, std::vector<aggregation_result>> groupby::scan
   rmm::device_async_resource_ref mr)
 {
   CUDF_FUNC_RANGE();
+
+  if (requests.size() == 0) { return {empty_like(_keys), std::vector<aggregation_result>{}}; }
+
   CUDF_EXPECTS(
     std::all_of(requests.begin(),
                 requests.end(),
