f3106f072e9844a54da468cb60d1d278caeab9b1
Nghia Truong <7416935+ttnghia@users.noreply.github.com>
2025-09-05 12:54:58 -0700

    Update cpp/src/io/json/json_column.cu
    Co-authored-by: Vyas Ramasubramani <vyas.ramasubramani@gmail.com>

--- cpp/src/io/json/json_column.cu
+++ cpp/src/io/json/json_column.cu
@@ -504,7 +504,7 @@ std::pair<std::unique_ptr<column>, std::vector<column_name_info>> device_json_co
         null_count == 0 ? rmm::device_buffer{0, stream, mr} : std::move(result_bitmask),
         stream,
         mr);
-      // Since some rows in child column may need to be nullified due to mixed types, we can not
+      // Since some rows in child column may need to be nullified due to mixed types, we cannot
       // skip the purge_nonempty_nulls call.
       if (auto const output_cv = ret_col->view();
           cudf::detail::has_nonempty_nulls(output_cv, stream)) {
