2325d0bfc469ec8a50b1c124592c39d679c54b80
Krishi Chawda <krishichawda@Krishis-MacBook-Air.local>
2025-09-08 14:17:15 -0700

    Fix test_bit compilation error in host_tree_algorithms.cu

--- cpp/src/io/json/host_tree_algorithms.cu
+++ cpp/src/io/json/host_tree_algorithms.cu
@@ -972,8 +972,8 @@ void scatter_offsets(tree_meta_t const& tree,
         // This avoids creating non-empty nulls and optimizes device column construction
         if (d_columns_data[col_ids[parent_node_id]].validity != nullptr) {
           // Copy parent's null state to child's null state
-          auto const parent_is_null = !test_bit(d_columns_data[col_ids[parent_node_id]].validity,
-                                                row_offsets[parent_node_id]);
+          auto const parent_is_null = !cudf::bit_is_set(
+            d_columns_data[col_ids[parent_node_id]].validity, row_offsets[parent_node_id]);
           if (parent_is_null) {
             clear_bit(d_columns_data[col_ids[node_id]].validity, row_offsets[node_id]);
           }
