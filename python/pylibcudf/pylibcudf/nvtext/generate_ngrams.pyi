dd31003530d70e803ef33d1633f6a262fbdef74d
Vyas Ramasubramani <vyasr@nvidia.com>
2025-09-08 08:35:01 -0700

    Add stream support to all nvtext modules (#19911)
    Contributes to #15163
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Matthew Roeschke (https://github.com/mroeschke)
    
    URL: https://github.com/rapidsai/cudf/pull/19911

--- python/pylibcudf/pylibcudf/nvtext/generate_ngrams.pyi
+++ python/pylibcudf/pylibcudf/nvtext/generate_ngrams.pyi
@@ -1,10 +1,16 @@
 # Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
+from rmm.pylibrmm.stream import Stream
+
 from pylibcudf.column import Column
 from pylibcudf.scalar import Scalar
 
 def generate_ngrams(
-    input: Column, ngrams: int, separator: Scalar
+    input: Column, ngrams: int, separator: Scalar, stream: Stream | None = None
 ) -> Column: ...
-def generate_character_ngrams(input: Column, ngrams: int = 2) -> Column: ...
-def hash_character_ngrams(input: Column, ngrams: int, seed: int) -> Column: ...
+def generate_character_ngrams(
+    input: Column, ngrams: int = 2, stream: Stream | None = None
+) -> Column: ...
+def hash_character_ngrams(
+    input: Column, ngrams: int, seed: int, stream: Stream | None = None
+) -> Column: ...
