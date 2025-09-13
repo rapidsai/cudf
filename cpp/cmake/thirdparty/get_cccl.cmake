c080de041c9b0acb72d836439d43b3aefa185426
Bradley Dice <bdice@bradleydice.com>
2025-09-13 18:13:07 -0500

    Revert CCCL testing changes in favor of rapids-cmake branch

--- cpp/cmake/thirdparty/get_cccl.cmake
+++ cpp/cmake/thirdparty/get_cccl.cmake
@@ -15,8 +15,6 @@
 # Use CPM to find or clone CCCL
 function(find_and_configure_cccl)
   include(${rapids-cmake-dir}/cpm/cccl.cmake)
-  include(${rapids-cmake-dir}/cpm/package_override.cmake)
-  rapids_cpm_package_override("${CMAKE_CURRENT_FUNCTION_LIST_DIR}/cccl_override.json")
   rapids_cpm_cccl(BUILD_EXPORT_SET cudf-exports INSTALL_EXPORT_SET cudf-exports)
 endfunction()
 
