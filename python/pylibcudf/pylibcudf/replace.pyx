528b78e8c9956fa91d00c2b787b580fa7f770b6d
Vyas Ramasubramani <vyasr@nvidia.com>
2025-09-15 19:26:08 +0000

    Add test comment

--- python/pylibcudf/pylibcudf/replace.pyx
+++ python/pylibcudf/pylibcudf/replace.pyx
@@ -253,4 +253,5 @@ cpdef Column normalize_nans_and_zeros(
     if not inplace:
         return Column.from_libcudf(move(c_result), stream)
 
+# Test comment
 ReplacePolicy.__str__ = ReplacePolicy.__repr__
