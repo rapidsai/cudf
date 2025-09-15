ba51153c0c904c7875177a06d2f06824c0e25b15
Vyas Ramasubramani <vyasr@nvidia.com>
2025-09-15 19:27:26 +0000

    Another test comment

--- python/cudf/cudf/pandas/magics.py
+++ python/cudf/cudf/pandas/magics.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION & AFFILIATES.
 # All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
@@ -30,4 +30,5 @@ try:
 except ImportError:
 
     def load_ipython_extension(ip):
+        # A different test comment
         pass
