b49316cd0996b0e8256d08e84c8331e454a26662
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-09-17 16:36:00 -0700

    Missed cudf.DataFrame.from_pandas usage

--- python/dask_cudf/dask_cudf/tests/test_distributed.py
+++ python/dask_cudf/dask_cudf/tests/test_distributed.py
@@ -26,7 +26,7 @@ def test_basic(loop, delayed):  # noqa: F811
     with dask_cuda.LocalCUDACluster(loop=loop) as cluster:
         with Client(cluster):
             pdf = dask.datasets.timeseries(dtypes={"x": int}).reset_index()
-            gdf = pdf.map_partitions(cudf.DataFrame.from_pandas)
+            gdf = pdf.map_partitions(cudf.DataFrame)
             if delayed:
                 gdf = dd.from_delayed(gdf.to_delayed())
             assert_eq(pdf.head(), gdf.head())
