00cd9fd7fbe6629f5007a75be12c5bcf9cb5549d
rjzamora <rzamora217@gmail.com>
2025-09-23 11:02:15 -0700

    enable reduction planning by default

--- python/cudf_polars/cudf_polars/experimental/benchmarks/utils.py
+++ python/cudf_polars/cudf_polars/experimental/benchmarks/utils.py
@@ -350,22 +350,29 @@ def get_executor_options(
         executor_options["rapidsmpf_spill"] = run_config.rapidsmpf_spill
     if run_config.scheduler == "distributed":
         executor_options["scheduler"] = "distributed"
-    if run_config.stats_planning:
-        executor_options["stats_planning"] = {"use_reduction_planning": True}
+    executor_options["stats_planning"] = {
+        "use_reduction_planning": run_config.stats_planning
+    }
 
     if (
         benchmark
         and benchmark.__name__ == "PDSHQueries"
         and run_config.executor == "streaming"
-        # Only use the unique_fraction config if stats_planning is disabled
-        and not run_config.stats_planning
     ):
-        executor_options["unique_fraction"] = {
-            "c_custkey": 0.05,
-            "l_orderkey": 1.0,
-            "l_partkey": 0.1,
-            "o_custkey": 0.25,
-        }
+        if run_config.stats_planning:
+            executor_options["unique_fraction"] = {
+                "l_suppkey": 0.001,  # Query 15
+                "l_partkey": 0.1,  # Query 20
+                "ps_suppkey": 0.05,  # Query 16 & 20
+                "p_partkey": 0.005,  # Query 2, 17 & 20
+            }
+        else:
+            executor_options["unique_fraction"] = {
+                "c_custkey": 0.05,  # Query 10 & 13
+                "l_orderkey": 1.0,  # Query 18 & 21
+                "l_partkey": 0.1,  # Query 20
+                "o_custkey": 0.25,  # Query 18 & 22
+            }
 
     return executor_options
 
