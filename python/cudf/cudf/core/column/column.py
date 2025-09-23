Fix casting between numerical and temporal columns, codes placement. All dask serialize test pass

python/cudf/cudf/core/column/column.py
@@ -549,55 +549,56 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
         pylibcudf.Column
             A new pylibcudf.Column referencing the same data.
         """
+        return self.plc_column
 
-        # TODO: Categoricals will need to be treated differently eventually.
-        # There is no 1-1 correspondence between cudf and libcudf for
-        # categoricals because cudf supports ordered and unordered categoricals
-        # while libcudf supports only unordered categoricals (see
-        # https://github.com/rapidsai/cudf/pull/8567).
-        if isinstance(self.dtype, cudf.CategoricalDtype):
-            col = self.base_children[0]
-        else:
-            col = self
+        # # TODO: Categoricals will need to be treated differently eventually.
+        # # There is no 1-1 correspondence between cudf and libcudf for
+        # # categoricals because cudf supports ordered and unordered categoricals
+        # # while libcudf supports only unordered categoricals (see
+        # # https://github.com/rapidsai/cudf/pull/8567).
+        # if isinstance(self.dtype, cudf.CategoricalDtype):
+        #     col = self.base_children[0]
+        # else:
+        #     col = self
 
-        dtype = dtype_to_pylibcudf_type(col.dtype)
+        # dtype = dtype_to_pylibcudf_type(col.dtype)
 
-        data = None
-        if col.base_data is not None:
-            cai = cuda_array_interface_wrapper(
-                ptr=col.base_data.get_ptr(mode=mode),
-                size=col.base_data.size,
-                owner=col.base_data,
-            )
-            data = plc.gpumemoryview(cai)
+        # data = None
+        # if col.base_data is not None:
+        #     cai = cuda_array_interface_wrapper(
+        #         ptr=col.base_data.get_ptr(mode=mode),
+        #         size=col.base_data.size,
+        #         owner=col.base_data,
+        #     )
+        #     data = plc.gpumemoryview(cai)
 
-        mask = None
-        if self.nullable:
-            # TODO: Are we intentionally use self's mask instead of col's?
-            # Where is the mask stored for categoricals?
-            cai = cuda_array_interface_wrapper(
-                ptr=self.base_mask.get_ptr(mode=mode),  # type: ignore[union-attr]
-                size=self.base_mask.size,  # type: ignore[union-attr]
-                owner=self.base_mask,
-            )
-            mask = plc.gpumemoryview(cai)
+        # mask = None
+        # if self.nullable:
+        #     # TODO: Are we intentionally use self's mask instead of col's?
+        #     # Where is the mask stored for categoricals?
+        #     cai = cuda_array_interface_wrapper(
+        #         ptr=self.base_mask.get_ptr(mode=mode),  # type: ignore[union-attr]
+        #         size=self.base_mask.size,  # type: ignore[union-attr]
+        #         owner=self.base_mask,
+        #     )
+        #     mask = plc.gpumemoryview(cai)
 
-        children = []
-        if col.base_children:
-            children = [
-                child_column.to_pylibcudf(mode=mode)
-                for child_column in col.base_children
-            ]
+        # children = []
+        # if col.base_children:
+        #     children = [
+        #         child_column.to_pylibcudf(mode=mode)
+        #         for child_column in col.base_children
+        #     ]
 
-        return plc.Column(
-            dtype,
-            self.size,
-            data,
-            mask,
-            self.null_count,
-            self.offset,
-            children,
-        )
+        # return plc.Column(
+        #     dtype,
+        #     self.size,
+        #     data,
+        #     mask,
+        #     self.null_count,
+        #     self.offset,
+        #     children,
+        # )
 
     @classmethod
     def from_pylibcudf(
@@ -2045,6 +2046,16 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
             codes_dtype = np.dtype(header["codes_dtype"])
         else:
             codes_dtype = None
+        if "null_count" not in header:
+            # test_deserialize_cudf_23_12
+            if mask is None:
+                null_count = 0
+            else:
+                null_count = plc.null_mask.null_count(
+                    plc.gpumemoryview(mask), 0, header["size"]
+                )
+        else:
+            null_count = header["null_count"]
         plc_column = plc.Column(
             dtype_to_pylibcudf_type(
                 codes_dtype if isinstance(dtype, CategoricalDtype) else dtype
@@ -2052,8 +2063,8 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
             header["size"],
             plc.gpumemoryview(data) if data is not None else None,
             plc.gpumemoryview(mask) if mask is not None else None,
-            header["null_count"],
-            header["offset"],
+            null_count,
+            header.get("offset", 0),
             [child.to_pylibcudf(mode="read") for child in children],
         )
         return build_column(

python/cudf/cudf/core/column/numerical.py
@@ -519,8 +519,28 @@ class NumericalColumn(NumericalBaseColumn):
                 ._with_type_metadata(dtype)
             )
 
+    def _as_temporal_column(self, dtype: np.dtype) -> plc.Column:
+        return plc.Column(
+            data_type=dtype_to_pylibcudf_type(dtype),
+            size=self.size,
+            data=plc.gpumemoryview(self.astype(np.dtype(np.int64)).base_data),
+            mask=plc.gpemoryview(self.base_mask)
+            if self.base_mask is not None
+            else None,
+            null_count=self.null_count,
+            offset=self.offset,
+            children=[],
+        )
+
     def as_datetime_column(self, dtype: np.dtype) -> DatetimeColumn:
-        return self.cast(dtype=dtype)  # type: ignore[return-value]
+        new_plc_column = self._as_temporal_column(dtype)
+        return cudf.core.column.datetime.DatetimeColumn(
+            plc_column=new_plc_column,
+            size=new_plc_column.size(),
+            dtype=dtype,
+            offset=new_plc_column.offset(),
+            null_count=new_plc_column.null_count(),
+        )
         # return cudf.core.column.DatetimeColumn(
         #     data=self.astype(np.dtype(np.int64)).base_data,  # type: ignore[arg-type]
         #     dtype=dtype,
@@ -530,7 +550,14 @@ class NumericalColumn(NumericalBaseColumn):
         # )
 
     def as_timedelta_column(self, dtype: np.dtype) -> TimeDeltaColumn:
-        return self.cast(dtype=dtype)  # type: ignore[return-value]
+        new_plc_column = self._as_temporal_column(dtype)
+        return cudf.core.column.timedelta.TimeDeltaColumn(
+            plc_column=new_plc_column,
+            size=new_plc_column.size(),
+            dtype=dtype,
+            offset=new_plc_column.offset(),
+            null_count=new_plc_column.null_count(),
+        )
         # return cudf.core.column.TimeDeltaColumn(
         #     data=self.astype(np.dtype(np.int64)).base_data,  # type: ignore[arg-type]
         #     dtype=dtype,

python/cudf/cudf/core/column/temporal_base.py
@@ -12,6 +12,8 @@ import numpy as np
 import pandas as pd
 import pyarrow as pa
 
+import pylibcudf as plc
+
 import cudf
 from cudf.api.types import is_scalar
 from cudf.core.column.column import ColumnBase, as_column, column_empty
@@ -19,6 +21,7 @@ from cudf.utils.dtypes import (
     CUDF_STRING_DTYPE,
     cudf_dtype_from_pa_type,
     cudf_dtype_to_pa_type,
+    dtype_to_pylibcudf_type,
     find_common_type,
     is_pandas_nullable_extension_dtype,
 )
@@ -28,8 +31,6 @@ if TYPE_CHECKING:
     from collections.abc import Sequence
 
     from typing_extensions import Self
-
-    import pylibcudf as plc
 
     from cudf._typing import ColumnLike, DtypeObj, ScalarLike
     from cudf.core.column.numerical import NumericalColumn
@@ -223,7 +224,25 @@ class TemporalBaseColumn(ColumnBase):
             )
 
     def as_numerical_column(self, dtype: np.dtype) -> NumericalColumn:
-        return self.cast(dtype=self._UNDERLYING_DTYPE).astype(dtype)  # type: ignore[return-value]
+        new_plc_column = plc.Column(
+            data_type=dtype_to_pylibcudf_type(self._UNDERLYING_DTYPE),
+            size=self.size,
+            data=plc.gpumemoryview(self.base_data),
+            mask=plc.gpemoryview(self.base_mask)
+            if self.base_mask is not None
+            else None,
+            null_count=self.null_count,
+            offset=self.offset,
+            children=[],
+        )
+        new_column = cudf.core.column.numerical.NumericalColumn(
+            plc_column=new_plc_column,
+            size=new_plc_column.size(),
+            dtype=self._UNDERLYING_DTYPE,
+            offset=new_plc_column.offset(),
+            null_count=new_plc_column.null_count(),
+        )
+        return new_column.astype(dtype)  # type: ignore[return-value]
         # col = NumericalColumn(
         #     data=self.base_data,  # type: ignore[arg-type]
         #     dtype=self._UNDERLYING_DTYPE,