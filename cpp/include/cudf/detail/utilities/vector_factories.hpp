b2c4e0c2a3ad487f0297eefffe2616b03b2d8b14
Jigao Luo <jigao.luo@outlook.com>
2025-09-25 21:49:16 +0200

    update comment during reading
    Signed-off-by: Jigao Luo <jigao.luo@outlook.com>

--- cpp/include/cudf/detail/utilities/vector_factories.hpp
+++ cpp/include/cudf/detail/utilities/vector_factories.hpp
@@ -267,7 +267,7 @@ rmm::device_uvector<typename Container::value_type> make_device_uvector(
  * @note This function does not synchronize `stream` after the copy.
  *
  * @tparam T The type of the data to copy
- * @param source_data The device data to copy
+ * @param source_data The device_span of data to copy
  * @param stream The stream on which to perform the copy
  * @return The data copied to the host
  */
@@ -307,7 +307,7 @@ std::vector<typename Container::value_type> make_std_vector_async(Container cons
  * @note This function does a synchronize on `stream` after the copy.
  *
  * @tparam T The type of the data to copy
- * @param source_data The device data to copy
+ * @param source_data The device_span of data to copy
  * @param stream The stream on which to perform the copy
  * @return The data copied to the host
  */
@@ -381,7 +381,7 @@ host_vector<T> make_empty_host_vector(size_t capacity, rmm::cuda_stream_view str
  * using a pinned memory resource.
  *
  * @tparam T The type of the data to copy
- * @param source_data The device data to copy
+ * @param source_data The device_span of data to copy
  * @param stream The stream on which to perform the copy
  * @return The data copied to the host
  */
@@ -422,7 +422,7 @@ host_vector<typename Container::value_type> make_host_vector_async(Container con
  * using a pinned memory resource.
  *
  * @tparam T The type of the data to copy
- * @param source_data The device data to copy
+ * @param source_data The device_span of data to copy
  * @param stream The stream on which to perform the copy
  * @return The data copied to the host
  */
