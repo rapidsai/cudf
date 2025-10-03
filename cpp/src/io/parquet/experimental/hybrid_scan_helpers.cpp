41cd1e2569aa84b84c0f42f35b4e5b93ec6d3ba8
Muhammad Haseeb <mhaseeb@nvidia.com>
2025-10-03 21:01:22 +0000

    Apply suggestions from code review

--- cpp/src/io/parquet/experimental/hybrid_scan_helpers.cpp
+++ cpp/src/io/parquet/experimental/hybrid_scan_helpers.cpp
@@ -423,15 +423,17 @@ std::vector<byte_range_info> aggregate_reader_metadata::get_dictionary_page_byte
             auto dictionary_offset = int64_t{0};
             auto dictionary_size   = int64_t{0};
 
-            // Make sure that the column chunk doesn't have any non-dictionary encoded pages
-            auto const has_only_dict_encoded_pages = std::all_of(
-              col_meta.encodings.cbegin(), col_meta.encodings.cend(), [](auto const& encoding) {
-                return encoding == Encoding::PLAIN_DICTIONARY or
-                       encoding == Encoding::RLE_DICTIONARY;
-              });
+            // Make sure that we have page index and the column chunk doesn't have any
+            // non-dictionary encoded pages
+            auto const has_page_index_and_only_dict_encoded_pages =
+              col_chunk.offset_index.has_value() and col_chunk.column_index.has_value() and
+              std::all_of(
+                col_meta.encodings.cbegin(), col_meta.encodings.cend(), [](auto const& encoding) {
+                  return encoding == Encoding::PLAIN_DICTIONARY or
+                         encoding == Encoding::RLE_DICTIONARY;
+                });
 
-            if (has_only_dict_encoded_pages and col_chunk.offset_index.has_value() and
-                col_chunk.column_index.has_value()) {
+            if (has_page_index_and_only_dict_encoded_pages) {
               auto const& offset_index = col_chunk.offset_index.value();
               auto const num_pages     = offset_index.page_locations.size();
 
