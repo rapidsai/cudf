c351f8e8e23a8408556505f3ddab618ae7b72e08
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-10-06 16:28:54 -0700

    Add comments

--- python/cudf/cudf/pandas/scripts/conftest-patch.py
+++ python/cudf/cudf/pandas/scripts/conftest-patch.py
@@ -12852,6 +12852,9 @@ def pytest_runtest_makereport(item, call):
         and type(call.excinfo.value).__name__ == "FallbackWarning"
         and item.nodeid in NODEIDS_THAT_FALLBACK_TO_PANDAS
     ):
+        # TODO: check if we can use the pytest_warning_recorded hook
+        # If we expect a test to raise a cudf.pandas.FallbackWarning,
+        # we consider the test "passed" (assuming we are running with warning as errors)
         outcome = pytest.TestReport(
             nodeid=outcome.nodeid,
             location=outcome.location,
