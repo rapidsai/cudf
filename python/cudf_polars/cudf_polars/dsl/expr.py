efd7914f8d03afddcd4a9ddff25f29ab06b85295
Bradley Dice <bdice@bradleydice.com>
2025-10-10 16:36:57 -0500

    Remove TODO

--- python/cudf_polars/cudf_polars/dsl/expr.py
+++ python/cudf_polars/cudf_polars/dsl/expr.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
-# TODO: remove need for this
 """
 DSL nodes for the polars expression language.
 
