1b1c30f5aefb8614dd720e759e5d374608901710
David Wendt <dwendt@nvidia.com>
2025-10-15 11:23:21 -0400

    Performance improvement for nvtext::edit_distance for long strings

--- cpp/benchmarks/text/edit_distance.cpp
+++ cpp/benchmarks/text/edit_distance.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024, NVIDIA CORPORATION.
+ * Copyright (c) 2023-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 
 #include <benchmarks/common/generate_input.hpp>
 
+#include <cudf/aggregation.hpp>
+#include <cudf/reduction.hpp>
 #include <cudf/strings/strings_column_view.hpp>
 
 #include <nvtext/edit_distance.hpp>
@@ -24,7 +26,7 @@
 
 #include <nvbench/nvbench.cuh>
 
-static void bench_edit_distance(nvbench::state& state)
+static void bench_edit_distance_utf8(nvbench::state& state)
 {
   auto const num_rows  = static_cast<cudf::size_type>(state.get_int64("num_rows"));
   auto const min_width = static_cast<cudf::size_type>(state.get_int64("min_width"));
@@ -34,23 +36,84 @@ static void bench_edit_distance(nvbench::state& state)
     cudf::type_id::STRING, distribution_id::NORMAL, min_width, max_width);
   auto const strings_table = create_random_table(
     {cudf::type_id::STRING, cudf::type_id::STRING}, row_count{num_rows}, strings_profile);
-  cudf::strings_column_view input1(strings_table->view().column(0));
-  cudf::strings_column_view input2(strings_table->view().column(1));
+  auto input1 = strings_table->get_column(0);
+  auto input2 = strings_table->get_column(0);
+  auto sv1    = cudf::strings_column_view(input1.view());
+  auto sv2    = cudf::strings_column_view(input2.view());
 
   state.set_cuda_stream(nvbench::make_cuda_stream_view(cudf::get_default_stream().value()));
 
-  auto chars_size =
-    input1.chars_size(cudf::get_default_stream()) + input2.chars_size(cudf::get_default_stream());
-  state.add_global_memory_reads<nvbench::int8_t>(chars_size);
+  state.add_global_memory_reads<nvbench::int8_t>(input1.alloc_size() + input2.alloc_size());
   // output are integers (one per row)
   state.add_global_memory_writes<nvbench::int32_t>(num_rows);
 
   state.exec(nvbench::exec_tag::sync,
-             [&](nvbench::launch& launch) { auto result = nvtext::edit_distance(input1, input2); });
+             [&](nvbench::launch& launch) { auto result = nvtext::edit_distance(sv1, sv2); });
+}
+
+static void bench_edit_distance_ascii(nvbench::state& state)
+{
+  auto const num_rows  = static_cast<cudf::size_type>(state.get_int64("num_rows"));
+  auto const min_width = static_cast<cudf::size_type>(state.get_int64("min_width"));
+  auto const max_width = static_cast<cudf::size_type>(state.get_int64("max_width"));
+
+  data_profile profile = data_profile_builder().no_validity().cardinality(0).distribution(
+    cudf::type_id::INT32, distribution_id::NORMAL, min_width, max_width);
+  data_profile ascii_profile = data_profile_builder().no_validity().cardinality(0).distribution(
+    cudf::type_id::INT8, distribution_id::UNIFORM, 32, 126);  // nice ASCII range
+
+  auto offsets = create_random_column(cudf::type_id::INT32, row_count{num_rows + 1}, profile);
+  offsets      = cudf::scan(offsets->view(),
+                       *cudf::make_sum_aggregation<cudf::scan_aggregation>(),
+                       cudf::scan_type::EXCLUSIVE);
+  auto ascii_data1 =
+    create_random_column(cudf::type_id::INT8, row_count{num_rows * max_width}, ascii_profile);
+  auto ascii_data2 =
+    create_random_column(cudf::type_id::INT8, row_count{num_rows * max_width}, ascii_profile);
+
+  auto input1 = cudf::column_view(cudf::data_type{cudf::type_id::STRING},
+                                  num_rows,
+                                  ascii_data1->view().data<char>(),
+                                  nullptr,
+                                  0,
+                                  0,
+                                  {offsets->view()});
+  auto input2 = cudf::column_view(cudf::data_type{cudf::type_id::STRING},
+                                  num_rows,
+                                  ascii_data2->view().data<char>(),
+                                  nullptr,
+                                  0,
+                                  0,
+                                  {offsets->view()});
+
+  state.set_cuda_stream(nvbench::make_cuda_stream_view(cudf::get_default_stream().value()));
+
+  auto sv1 = cudf::strings_column_view(input1);
+  auto sv2 = cudf::strings_column_view(input2);
+  auto chars_size =
+    sv1.chars_size(cudf::get_default_stream()) + sv2.chars_size(cudf::get_default_stream());
+  auto offsets_size = offsets->alloc_size();
+  state.add_global_memory_reads<nvbench::int8_t>(chars_size + 2 * offsets_size);
+  // output are integers (one per row)
+  state.add_global_memory_writes<nvbench::int32_t>(num_rows);
+
+  state.exec(nvbench::exec_tag::sync,
+             [&](nvbench::launch& launch) { auto result = nvtext::edit_distance(sv1, sv2); });
+}
+
+static void bench_edit_distance(nvbench::state& state)
+{
+  auto const encode = state.get_string("encode");
+  if (encode == "ascii") {
+    bench_edit_distance_ascii(state);
+  } else {
+    bench_edit_distance_utf8(state);
+  }
 }
 
 NVBENCH_BENCH(bench_edit_distance)
   .set_name("edit_distance")
   .add_int64_axis("min_width", {0})
-  .add_int64_axis("max_width", {32, 64, 128, 256})
-  .add_int64_axis("num_rows", {32768, 262144});
+  .add_int64_axis("max_width", {32, 64, 128, 256, 512})
+  .add_int64_axis("num_rows", {262144, 524288, 1048576})
+  .add_string_axis("encode", {"utf8", "ascii"});
--- cpp/tests/text/edit_distance_tests.cpp
+++ cpp/tests/text/edit_distance_tests.cpp
@@ -17,6 +17,7 @@
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_utilities.hpp>
 #include <cudf_test/column_wrapper.hpp>
+#include <cudf_test/debug_utilities.hpp>
 
 #include <cudf/column/column.hpp>
 #include <cudf/strings/strings_column_view.hpp>
@@ -36,35 +37,59 @@ TEST_F(TextEditDistanceTest, EditDistance)
     h_strings.begin(),
     h_strings.end(),
     thrust::make_transform_iterator(h_strings.begin(), [](auto str) { return str != nullptr; }));
+  auto sv = cudf::strings_column_view(strings);
 
-  std::vector<char const*> h_targets{"hog", "not", "cake", "house", "fox", nullptr, "puppy", "the"};
-  cudf::test::strings_column_wrapper targets(
-    h_targets.begin(),
-    h_targets.end(),
-    thrust::make_transform_iterator(h_targets.begin(), [](auto str) { return str != nullptr; }));
   {
-    auto results =
-      nvtext::edit_distance(cudf::strings_column_view(strings), cudf::strings_column_view(targets));
+    std::vector<char const*> h_targets{
+      "hog", "not", "cake", "house", "fox", nullptr, "puppy", "the"};
+    cudf::test::strings_column_wrapper targets(
+      h_targets.begin(),
+      h_targets.end(),
+      thrust::make_transform_iterator(h_targets.begin(), [](auto str) { return str != nullptr; }));
+    auto tv = cudf::strings_column_view(targets);
+
+    auto results = nvtext::edit_distance(sv, tv);
     cudf::test::fixed_width_column_wrapper<int32_t> expected({1, 3, 2, 1, 3, 0, 0, 1});
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected);
   }
   {
     cudf::test::strings_column_wrapper single({"pup"});
-    auto results =
-      nvtext::edit_distance(cudf::strings_column_view(strings), cudf::strings_column_view(single));
+    auto tv      = cudf::strings_column_view(single);
+    auto results = nvtext::edit_distance(sv, tv);
     cudf::test::fixed_width_column_wrapper<int32_t> expected({3, 3, 3, 4, 0, 3, 2, 3});
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected);
   }
   {
     cudf::test::strings_column_wrapper single({"pup"}, {1});
+    auto tv = cudf::strings_column_view(single);
     std::vector<char const*> h_input(516, "cup");
-    auto input = cudf::test::strings_column_wrapper(h_input.begin(), h_input.end());
-    auto results =
-      nvtext::edit_distance(cudf::strings_column_view(input), cudf::strings_column_view(single));
+    auto input    = cudf::test::strings_column_wrapper(h_input.begin(), h_input.end());
+    auto sv       = cudf::strings_column_view(input);
+    auto results  = nvtext::edit_distance(sv, tv);
     auto begin    = thrust::constant_iterator<int32_t>(1);
     auto expected = cudf::test::fixed_width_column_wrapper<int32_t>(begin, begin + h_input.size());
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected);
   }
+}
+
+TEST_F(TextEditDistanceTest, EditDistanceLong)
+{
+  auto input1 = cudf::test::strings_column_wrapper(
+    {"the lady brown fox jumps down the wall of the castle with wide windows",
+     "the lady brown fox jumps down the wall of thé castlé with wide windows",
+     "thé lady brown fox jumps down the wall of the castle with wide windows",
+     "the lazy brown dog jumps upon the hill of the castle with long windows",  // exact one
+     "why the lazy brown dog jumps upon the hill of the castle with long windows",
+     "the lazy brown dog jumps upon the hill of the castle",
+     "lazy brown dog jumps upon hill"});
+  auto input2 = cudf::test::strings_column_wrapper(
+    {"the lazy brown dog jumps upon the hill of the castle with long windows"});
+  auto sv1 = cudf::strings_column_view(input1);
+  auto sv2 = cudf::strings_column_view(input2);
+
+  auto expected = cudf::test::fixed_width_column_wrapper<int32_t>({12, 14, 13, 0, 4, 18, 40});
+  auto results  = nvtext::edit_distance(sv1, sv2);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(*results, expected);
 }
 
 TEST_F(TextEditDistanceTest, EditDistanceMatrix)
