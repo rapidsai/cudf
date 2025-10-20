397a64dd35df82994083d604be040f59457c04af
GALI PREM SAGAR <sagarprem75@gmail.com>
2025-10-20 17:04:39 -0500

    Add `inferred_type` and missing `IntervalIndex` properties (#20294)
    Closes: #20290 
    
    This PR adds :
    
    1. `Index._constructor`
    2. `Index.inferred_type`
    3. `IntervalIndex.closed_right`
    4. `IntervalIndex.closed_left`
    
    Authors:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
      - Lawrence Mitchell (https://github.com/wence-)
      - Matthew Roeschke (https://github.com/mroeschke)
      - Bradley Dice (https://github.com/bdice)
      - Nghia Truong (https://github.com/ttnghia)
    
    Approvers:
      - Matthew Roeschke (https://github.com/mroeschke)
    
    URL: https://github.com/rapidsai/cudf/pull/20294

---
+++ python/cudf/cudf/tests/indexes/intervalindex/test_attributes.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2025, NVIDIA CORPORATION.
+import pandas as pd
+import pytest
+
+import cudf
+from cudf.testing import assert_eq
+
+
+def test_intervalindex_constructor():
+    gidx = cudf.IntervalIndex.from_breaks([0, 1, 2, 3])
+
+    assert gidx._constructor is cudf.IntervalIndex
+
+
+@pytest.mark.parametrize(
+    "closed",
+    ["left", "right", "both", "neither"],
+)
+def test_intervalindex_inferred_type(closed):
+    gidx = cudf.IntervalIndex.from_breaks([0, 1, 2, 3], closed=closed)
+    pidx = pd.IntervalIndex.from_breaks([0, 1, 2, 3], closed=closed)
+    assert_eq(gidx.inferred_type, pidx.inferred_type)
+
+
+@pytest.mark.parametrize(
+    "closed",
+    ["left", "right", "both", "neither"],
+)
+def test_intervalindex_closed_left_right(closed):
+    """Test closed_left and closed_right properties."""
+    gidx = cudf.IntervalIndex.from_breaks([0, 1, 2, 3], closed=closed)
+    pidx = pd.IntervalIndex.from_breaks([0, 1, 2, 3], closed=closed)
+
+    assert gidx.closed_left == pidx.closed_left
+    assert gidx.closed_right == pidx.closed_right
