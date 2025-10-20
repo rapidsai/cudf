3dbd163ae1a02389710eeaecfb080ec2eea50b9c
galipremsagar <sagarprem75@gmail.com>
2025-10-20 16:56:00 +0000

    update

--- python/cudf/cudf/core/series.py
+++ python/cudf/cudf/core/series.py
@@ -4564,7 +4564,7 @@ class DatetimeProperties(BaseDatelikeProperties):
 
     @property
     def tz(self) -> str | None:
-        raise NotImplementedError("tz is not yet implemented.")
+        raise self.series._column.tz
 
     @property
     def date(self):
