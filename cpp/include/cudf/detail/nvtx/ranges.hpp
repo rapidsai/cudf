fa8cee60098bfe71f1a35af333b63b9d542db9eb
Nghia Truong <nghiat@nvidia.com>
2025-10-21 13:45:24 -0700

    Rename macros, and add more
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- cpp/include/cudf/detail/nvtx/ranges.hpp
+++ cpp/include/cudf/detail/nvtx/ranges.hpp
@@ -18,7 +18,7 @@
 
 #include <nvtx3/nvtx3.hpp>
 
-#ifdef CUDF_ENABLE_NVTX_DEBUG_RANGE
+#ifdef CUDF_ENABLE_NVTX_DEV_UTILS
 #include <rmm/cuda_stream_view.hpp>
 #endif
 
@@ -45,7 +45,7 @@ struct libcudf_domain {
  */
 using scoped_range = ::nvtx3::scoped_range_in<libcudf_domain>;
 
-#ifdef CUDF_ENABLE_NVTX_DEBUG_RANGE
+#ifdef CUDF_ENABLE_NVTX_DEV_UTILS
 struct scoped_range_sync : scoped_range {
   rmm::cuda_stream_view stream;
   scoped_range_sync(char const* str, rmm::cuda_stream_view stream_)
@@ -76,26 +76,45 @@ struct scoped_range_sync : scoped_range {
 #define CUDF_FUNC_RANGE() NVTX3_FUNC_RANGE_IN(cudf::libcudf_domain)
 
 /**
+ * @brief Conditionally compiled into `CUDF_FUNC_RANGE()` in debug build, and no-op otherwise.
+ */
+#ifndef NDEBUG
+#define CUDF_FUNC_RANGE_DEBUG() CUDF_FUNC_RANGE()
+#else  // no-op
+#define CUDF_FUNC_RANGE_DEBUG()
+#endif
+
+/**
+ * @brief Conditionally generate an instance of `cudf::scoped_range` in debug build, and no-op
+ * otherwise.
+ */
+#ifndef NDEBUG
+#define CUDF_SCOPED_RANGE_DEBUG(str) [[maybe_unused]] cudf::scoped_range __range{str};
+#else  // no-op
+#define CUDF_SCOPED_RANGE_DEBUG(str)
+#endif
+
+/**
  * @brief Convenience macro for generating an NVTX scoped range in the `libcudf` domain with stream
  * synchronization at the end of the scope.
  *
  * This is an internal development utility, which should be compiled to nothing in production
  * builds. The NVTX range is generated and stream is synchronized only if the macro
- * `CUDF_ENABLE_NVTX_DEBUG_RANGE` is explicitly defined.
+ * `CUDF_ENABLE_NVTX_DEV_UTILS` is explicitly defined.
  *
  * Example:
  * ```
  * {
- *    CUDF_SCOPED_RANGE_SYNC_DEBUG("Search keys", stream);
+ *    CUDF_SCOPED_RANGE_SYNC_DEV("Search keys", stream);
  *    ...
  * }
  * ```
  */
-#ifdef CUDF_ENABLE_NVTX_DEBUG_RANGE
-#define CUDF_SCOPED_RANGE_SYNC_DEBUG(str, stream) \
+#ifdef CUDF_ENABLE_NVTX_DEV_UTILS
+#define CUDF_SCOPED_RANGE_SYNC_DEV(str, stream) \
   [[maybe_unused]] cudf::scoped_range_sync __range_sync{str, stream};
 #else  // no-op
-#define CUDF_SCOPED_RANGE_SYNC_DEBUG(str, stream)
+#define CUDF_SCOPED_RANGE_SYNC_DEV(str, stream)
 #endif
 
 /**
@@ -104,7 +123,7 @@ struct scoped_range_sync : scoped_range {
  *
  * This is an internal development utility, which should be compiled to nothing in production
  * builds. The NVTX range is generated and stream is synchronized only if the macro
- * `CUDF_ENABLE_NVTX_DEBUG_RANGE` is explicitly defined.
+ * `CUDF_ENABLE_NVTX_DEV_UTILS` is explicitly defined.
  *
  * Similar to `CUDF_FUNC_RANGE`, this uses the name of the immediately enclosing function returned
  * by `__func__` to name the range.
@@ -112,14 +131,14 @@ struct scoped_range_sync : scoped_range {
  * Example:
  * ```
  * void some_function(rmm::cuda_stream_view stream) {
- *    CUDF_FUNC_RANGE_SYNC_DEBUG(stream);
+ *    CUDF_FUNC_RANGE_SYNC_DEV(stream);
  *    ...
  * }
  * ```
  */
-#ifdef CUDF_ENABLE_NVTX_DEBUG_RANGE
-#define CUDF_FUNC_RANGE_SYNC_DEBUG(stream) \
+#ifdef CUDF_ENABLE_NVTX_DEV_UTILS
+#define CUDF_FUNC_RANGE_SYNC_DEV(stream) \
   [[maybe_unused]] cudf::scoped_range_sync __range_sync{__func__, stream};
 #else  // no-op
-#define CUDF_FUNC_RANGE_SYNC_DEBUG(stream)
+#define CUDF_FUNC_RANGE_SYNC_DEV(stream)
 #endif
