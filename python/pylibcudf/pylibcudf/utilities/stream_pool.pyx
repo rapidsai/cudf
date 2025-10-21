b1e310465b78c060d2a3096be08d011e8586bcb7
Tom Augspurger <toaugspurger@nvidia.com>
2025-10-21 14:10:31 +0000

    compiles

--- python/pylibcudf/pylibcudf/utilities/stream_pool.pyx
+++ python/pylibcudf/pylibcudf/utilities/stream_pool.pyx
@@ -1,10 +1,12 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 from libcpp.vector cimport vector
 from pylibcudf.libcudf.utilities cimport stream_pool as cpp_stream_pool
+from pylibcudf.libcudf.utilities.span cimport host_span
 from rmm.librmm.cuda_stream_view cimport cuda_stream_view
 from rmm.pylibrmm.stream cimport Stream
 
 ctypedef const cuda_stream_view const_cuda_stream_view
+
 
 __all__ = ["join_streams"]
 
@@ -22,9 +24,18 @@ cpdef void join_streams(list streams, Stream stream):
     stream
         Joined stream that synchronizes with the waited-on streams.
     """
-    cdef vector[cuda_stream_view] c_streams = [
-        (<Stream?>stream).view() for stream in streams
-    ]
     cdef Stream c_stream = <Stream?>stream
+    cdef vector[cuda_stream_view] c_streams
+
+    c_streams.reserve(len(streams))
+    for s in streams:
+        c_streams.push_back((<Stream?>s).view())
+
+    cdef host_span[const_cuda_stream_view] c_streams_span = (
+        host_span[const_cuda_stream_view](
+            c_streams.data(), c_streams.size()
+        )
+    )
+
     with nogil:
-        cpp_stream_pool.join_streams(c_streams, c_stream.view())
+        cpp_stream_pool.join_streams(c_streams_span, c_stream.view())
