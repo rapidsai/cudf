rapidsai/cudf

commit f79ea64d32424fca3a696aff60f529e838e4fee7

Improve hash-based groupby aggregation: direct write to the dense output columns whenever possible (#19764)
This performs a major refactor to the hash-based groupby aggregation framework, trying to remove the intermediate sparse output result buffers. By doing so, the aggregation kernels write to the final, dense output columns directly instead of writing to the sparse intermediate result columns then gathering to the final dense result columns. This approach enhances writing efficiency and reduce memory footprint, especially in cases when we have high multiplicity and large number of aggregations.

However, for some situations such as when there are only a couple of simple aggregations, the old code approach (writing to the sparse intermediate result columns then gathering to the final dense result columns) is still be used as it run faster due to less contention in atomic write.

Beyond the main goal, this PR also makes some additional small contributions:
 * Fixing a bug reported in https://github.com/rapidsai/cudf/issues/19744
 * Use a single thread for processing each row of each output column, instead of one thread for processing one row of all output columns (requested in https://github.com/rapidsai/cudf/issues/19513)

Contribute to https://github.com/rapidsai/cudf/issues/19513.

Authors:
  - Nghia Truong (https://github.com/ttnghia)

Approvers:
  - Muhammad Haseeb (https://github.com/mhaseeb123)
  - Yunsong Wang (https://github.com/PointKernel)
  - Lawrence Mitchell (https://github.com/wence-)

URL: https://github.com/rapidsai/cudf/pull/19764

cpp/src/groupby/hash/compute_mapping_indices.cuh
@@ -19,10 +19,8 @@
 #include "helpers.cuh"
 
 #include <cudf/detail/cuco_helpers.hpp>
-#include <cudf/detail/utilities/cuda.cuh>
 #include <cudf/detail/utilities/cuda.hpp>
 #include <cudf/detail/utilities/grid_1d.cuh>
-#include <cudf/detail/utilities/integer_utils.hpp>
 #include <cudf/types.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
@@ -36,13 +34,13 @@
 namespace cudf::groupby::detail::hash {
 template <typename SetType>
 __device__ void find_local_mapping(cooperative_groups::thread_block const& block,
-                                   cudf::size_type idx,
-                                   cudf::size_type num_input_rows,
+                                   size_type idx,
+                                   size_type num_input_rows,
                                    SetType shared_set,
                                    bitmask_type const* row_bitmask,
-                                   cudf::size_type* cardinality,
-                                   cudf::size_type* local_mapping_index,
-                                   cudf::size_type* shared_set_indices)
+                                   size_type* cardinality,
+                                   size_type* local_mapping_indices,
+                                   size_type* shared_set_indices)
 {
   auto const is_valid_input =
     idx < num_input_rows and (not row_bitmask or cudf::bit_is_set(row_bitmask, idx));
@@ -51,104 +49,98 @@ __device__ void find_local_mapping(cooperative_groups::thread_block const& block
       auto const result      = shared_set.insert_and_find(idx);
       auto const matched_idx = *result.first;
       auto const inserted    = result.second;
-      // inserted a new element
-      if (result.second) {
-        auto const shared_set_index          = atomicAdd(cardinality, 1);
+      if (inserted) {  // inserted a new element
+        auto const ref_cardinality =
+          cuda::atomic_ref<size_type, cuda::thread_scope_block>{*cardinality};
+        auto const shared_set_index = ref_cardinality.fetch_add(1, cuda::std::memory_order_relaxed);
         shared_set_indices[shared_set_index] = idx;
-        local_mapping_index[idx]             = shared_set_index;
+        local_mapping_indices[idx]           = shared_set_index;
       }
       return cuda::std::pair{matched_idx, inserted};
     }
     return cuda::std::pair{0, false};  // dummy values
   }();
-  // Syncing the thread block is needed so that updates in `local_mapping_index` are visible to all
-  // threads in the thread block.
+  // Syncing the thread block is needed so that updates in `local_mapping_indices` are visible to
+  // all threads in the thread block.
   block.sync();
   if (is_valid_input) {
     // element was already in set
-    if (!inserted) { local_mapping_index[idx] = local_mapping_index[result_idx]; }
+    if (!inserted) { local_mapping_indices[idx] = local_mapping_indices[result_idx]; }
   }
 }
 
 template <typename SetRef>
 __device__ void find_global_mapping(cooperative_groups::thread_block const& block,
-                                    cudf::size_type cardinality,
+                                    size_type cardinality,
                                     SetRef global_set,
-                                    cudf::size_type* shared_set_indices,
-                                    cudf::size_type* global_mapping_index)
+                                    size_type* shared_set_indices,
+                                    size_type* global_mapping_indices)
 {
   // for all unique keys in shared memory hash set, stores their matches in
-  // global hash set to `global_mapping_index`
+  // global hash set to `global_mapping_indices`
   for (auto idx = block.thread_rank(); idx < cardinality; idx += block.num_threads()) {
     auto const input_idx = shared_set_indices[idx];
-    global_mapping_index[block.group_index().x * GROUPBY_SHM_MAX_ELEMENTS + idx] =
-      *global_set.insert_and_find(input_idx).first;
+    auto const key_idx   = *global_set.insert_and_find(input_idx).first;
+
+    global_mapping_indices[block.group_index().x * GROUPBY_SHM_MAX_ELEMENTS + idx] = key_idx;
   }
 }
 
 /*
  * @brief Inserts keys into the shared memory hash set, and stores the block-wise rank for a given
- * row index in `local_mapping_index`. If the number of unique keys found in a threadblock exceeds
+ * row index in `local_mapping_indices`. If the number of unique keys found in a threadblock exceeds
  * `GROUPBY_CARDINALITY_THRESHOLD`, the threads in that block will exit without updating
- * `global_set` or setting `global_mapping_index`. Else, we insert the unique keys found to the
- * global hash set, and save the row index of the global sparse table in `global_mapping_index`.
+ * `global_set` or setting `global_mapping_indices`. Else, we insert the unique keys found to the
+ * global hash set, and save the row index of the global sparse table in `global_mapping_indices`.
  */
 template <class SetRef>
-CUDF_KERNEL void mapping_indices_kernel(cudf::size_type num_input_rows,
+CUDF_KERNEL void mapping_indices_kernel(size_type num_input_rows,
                                         SetRef global_set,
                                         bitmask_type const* row_bitmask,
-                                        cudf::size_type* local_mapping_index,
-                                        cudf::size_type* global_mapping_index,
-                                        cudf::size_type* block_cardinality,
-                                        cuda::std::atomic_flag* needs_global_memory_fallback)
+                                        size_type* local_mapping_indices,
+                                        size_type* global_mapping_indices,
+                                        size_type* block_cardinality)
 {
-  __shared__ cudf::size_type shared_set_indices[GROUPBY_SHM_MAX_ELEMENTS];
+  __shared__ size_type shared_set_indices[GROUPBY_SHM_MAX_ELEMENTS];
 
   // Shared set initialization
-  __shared__ cudf::size_type slots[valid_extent.value()];
+  __shared__ size_type slots[valid_extent.value()];
 
   auto raw_set = cuco::static_set_ref{
-    cuco::empty_key<cudf::size_type>{cudf::detail::CUDF_SIZE_TYPE_SENTINEL},
+    cuco::empty_key<size_type>{cudf::detail::CUDF_SIZE_TYPE_SENTINEL},
     global_set.key_eq(),
     probing_scheme_t{global_set.hash_function()},
     cuco::thread_scope_block,
-    cuco::bucket_storage_ref<cudf::size_type, GROUPBY_BUCKET_SIZE, decltype(valid_extent)>{
-      valid_extent, slots}};
+    cuco::bucket_storage_ref<size_type, GROUPBY_BUCKET_SIZE, decltype(valid_extent)>{valid_extent,
+                                                                                     slots}};
   auto shared_set = raw_set.rebind_operators(cuco::insert_and_find);
 
   auto const block = cooperative_groups::this_thread_block();
   shared_set.initialize(block);
 
-  __shared__ cudf::size_type cardinality;
+  __shared__ size_type cardinality;
   if (block.thread_rank() == 0) { cardinality = 0; }
   block.sync();
 
   auto const stride = cudf::detail::grid_1d::grid_stride();
-
-  for (auto idx = cudf::detail::grid_1d::global_thread_id();
-       idx - block.thread_rank() < num_input_rows;
-       idx += stride) {
+  for (auto idx = cudf::detail::grid_1d::global_thread_id(); idx < num_input_rows; idx += stride) {
     find_local_mapping(block,
                        idx,
                        num_input_rows,
                        shared_set,
                        row_bitmask,
                        &cardinality,
-                       local_mapping_index,
+                       local_mapping_indices,
                        shared_set_indices);
 
     block.sync();
-
-    if (cardinality >= GROUPBY_CARDINALITY_THRESHOLD) {
-      if (block.thread_rank() == 0) { needs_global_memory_fallback->test_and_set(); }
-      break;
-    }
+    if (cardinality >= GROUPBY_CARDINALITY_THRESHOLD) { break; }
   }
 
   // Insert unique keys from shared to global hash set if block-cardinality
   // doesn't exceed the threshold upper-limit
   if (cardinality < GROUPBY_CARDINALITY_THRESHOLD) {
-    find_global_mapping(block, cardinality, global_set, shared_set_indices, global_mapping_index);
+    find_global_mapping(block, cardinality, global_set, shared_set_indices, global_mapping_indices);
   }
 
   if (block.thread_rank() == 0) { block_cardinality[block.group_index().x] = cardinality; }
@@ -164,23 +156,20 @@ int32_t max_active_blocks_mapping_kernel()
 }
 
 template <class SetRef>
-void compute_mapping_indices(cudf::size_type grid_size,
-                             cudf::size_type num,
+void compute_mapping_indices(size_type grid_size,
+                             size_type num_rows,
                              SetRef global_set,
                              bitmask_type const* row_bitmask,
-                             cudf::size_type* local_mapping_index,
-                             cudf::size_type* global_mapping_index,
-                             cudf::size_type* block_cardinality,
-                             cuda::std::atomic_flag* needs_global_memory_fallback,
+                             size_type* local_mapping_indices,
+                             size_type* global_mapping_indices,
+                             size_type* block_cardinality,
                              rmm::cuda_stream_view stream)
 {
-  mapping_indices_kernel<<<grid_size, GROUPBY_BLOCK_SIZE, 0, stream>>>(
-    num,
-    global_set,
-    row_bitmask,
-    local_mapping_index,
-    global_mapping_index,
-    block_cardinality,
-    needs_global_memory_fallback);
+  mapping_indices_kernel<<<grid_size, GROUPBY_BLOCK_SIZE, 0, stream>>>(num_rows,
+                                                                       global_set,
+                                                                       row_bitmask,
+                                                                       local_mapping_indices,
+                                                                       global_mapping_indices,
+                                                                       block_cardinality);
 }
 }  // namespace cudf::groupby::detail::hash

cpp/CMakeLists.txt
@@ -428,19 +428,18 @@ add_library(
   src/filling/sequence.cu
   src/groupby/common/m2_var_std.cu
   src/groupby/groupby.cu
-  src/groupby/hash/compute_aggregations.cu
-  src/groupby/hash/compute_aggregations_null.cu
   src/groupby/hash/compute_global_memory_aggs.cu
   src/groupby/hash/compute_global_memory_aggs_null.cu
   src/groupby/hash/compute_groupby.cu
   src/groupby/hash/compute_mapping_indices.cu
   src/groupby/hash/compute_mapping_indices_null.cu
   src/groupby/hash/compute_shared_memory_aggs.cu
-  src/groupby/hash/create_sparse_results_table.cu
-  src/groupby/hash/flatten_single_pass_aggs.cpp
+  src/groupby/hash/compute_single_pass_aggs.cu
+  src/groupby/hash/compute_single_pass_aggs_null.cu
+  src/groupby/hash/extract_single_pass_aggs.cpp
   src/groupby/hash/groupby.cu
   src/groupby/hash/hash_compound_agg_finalizer.cu
-  src/groupby/hash/sparse_to_dense_results.cu
+  src/groupby/hash/output_utils.cu
   src/groupby/sort/aggregate.cpp
   src/groupby/sort/group_argmax.cu
   src/groupby/sort/group_argmin.cu

cpp/benchmarks/groupby/group_max.cpp
@@ -104,5 +104,5 @@ NVBENCH_BENCH_TYPES(bench_groupby_max,
 
 NVBENCH_BENCH_TYPES(bench_groupby_max_cardinality, NVBENCH_TYPE_AXES(nvbench::type_list<int32_t>))
   .set_name("groupby_max_cardinality")
-  .add_int64_axis("num_aggregations", {1})
-  .add_int64_axis("cardinality", {10, 20, 50, 100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000});
+  .add_int64_axis("num_aggregations", {1, 2, 3, 4, 5, 6, 7, 8})
+  .add_int64_axis("cardinality", {20, 50, 100, 1'000, 10'000, 100'000, 1'000'000});

cpp/include/cudf/detail/aggregation/aggregation.hpp
@@ -116,6 +116,8 @@ class simple_aggregations_collector {  // Declares the interface for the simple 
 
 class aggregation_finalizer {  // Declares the interface for the finalizer
  public:
+  virtual ~aggregation_finalizer() = default;
+
   // Declare overloads for each kind of a agg to dispatch
   virtual void visit(aggregation const& agg);
   virtual void visit(class sum_aggregation const& agg);
@@ -1817,7 +1819,7 @@ bool is_valid_aggregation(data_type source, aggregation::Kind k);
  * columns. The aggregations determine the identity value for each column.
  * @param stream CUDA stream used for device memory operations and kernel launches.
  */
-void initialize_with_identity(mutable_table_view& table,
+void initialize_with_identity(mutable_table_view const& table,
                               host_span<cudf::aggregation::Kind const> aggs,
                               rmm::cuda_stream_view stream);
 

cpp/include/cudf/detail/aggregation/device_aggregators.cuh
@@ -333,8 +333,30 @@ struct update_target_element<Source, aggregation::ARGMIN> {
  * @brief Function object to update a single element in a target column by
  * performing an aggregation operation with a single element from a source
  * column.
+ *
+ * This functor only supports aggregations that can be done in a "single pass",
+ * i.e., given an initial value `r = R`, the aggregation `op` can be computed on a series
+ * of elements `e[i] for i in [0,n)` just by updating `r = op(r, e[i])` using any order
+ * of the values `e[i]`.
+ *
+ * The initial value and validity of `R` depends on the specific aggregation. For example:
+ *  - SUM: 0 and NULL
+ *  - MIN: Max value of type and NULL
+ *  - MAX: Min value of type and NULL
+ *  - COUNT_VALID: 0 and VALID
+ *  - COUNT_ALL:   0 and VALID
+ *  - ARGMAX: `ARGMAX_SENTINEL` and NULL
+ *  - ARGMIN: `ARGMIN_SENTINEL` and NULL
+ *
+ * It is required that the elements of `target` be initialized with the corresponding
+ * initial values and validity specified above.
+ *
+ * Handling of null elements in both `source` and `target` depends on the aggregation. For example:
+ *  - SUM, MIN, MAX, ARGMIN, ARGMAX:  `source` is skipped, `target` is updated from null to valid
+ *    upon first successful aggregation
+ *  - COUNT_VALID, COUNT_ALL:  `source` is skipped, `target` cannot be null
  */
-struct elementwise_aggregator {
+struct element_aggregator {
   template <typename Source, aggregation::Kind k>
   __device__ void operator()(mutable_column_device_view target,
                              size_type target_index,
@@ -354,64 +376,4 @@ struct elementwise_aggregator {
   }
 };
 
-/**
- * @brief Updates a row in `target` by performing elementwise aggregation
- * operations with a row in `source`.
- *
- * For the row in `target` specified by `target_index`, each element at `i` is
- * updated by:
- * ```c++
- * target_row[i] = aggs[i](target_row[i], source_row[i])
- * ```
- *
- * This function only supports aggregations that can be done in a "single pass",
- * i.e., given an initial value `R`, the aggregation `op` can be computed on a series
- * of elements `e[i] for i in [0,n)` by computing `R = op(e[i],R)` for any order
- * of the values of `i`.
- *
- * The initial value and validity of `R` depends on the aggregation:
- * SUM: 0 and NULL
- * MIN: Max value of type and NULL
- * MAX: Min value of type and NULL
- * COUNT_VALID: 0 and VALID
- * COUNT_ALL:   0 and VALID
- * ARGMAX: `ARGMAX_SENTINEL` and NULL
- * ARGMIN: `ARGMIN_SENTINEL` and NULL
- *
- * It is required that the elements of `target` be initialized with the corresponding
- * initial values and validity specified above.
- *
- * Handling of null elements in both `source` and `target` depends on the aggregation:
- * SUM, MIN, MAX, ARGMIN, ARGMAX:
- *  - `source`: Skipped
- *  - `target`: Updated from null to valid upon first successful aggregation
- * COUNT_VALID, COUNT_ALL:
- *  - `source`: Skipped
- *  - `target`: Cannot be null
- *
- * @param target Table containing the row to update
- * @param target_index Index of the row to update in `target`
- * @param source Table containing the row used to update the row in `target`.
- * The invariant `source.num_columns() >= target.num_columns()` must hold.
- * @param source_index Index of the row to use in `source`
- * @param aggs Array of aggregations to perform between elements of the `target`
- * and `source` rows. Must contain at least `target.num_columns()` valid
- * `aggregation::Kind` values.
- */
-__device__ inline void aggregate_row(mutable_table_device_view target,
-                                     size_type target_index,
-                                     table_device_view source,
-                                     size_type source_index,
-                                     aggregation::Kind const* aggs)
-{
-  for (auto i = 0; i < target.num_columns(); ++i) {
-    dispatch_type_and_aggregation(source.column(i).type(),
-                                  aggs[i],
-                                  elementwise_aggregator{},
-                                  target.column(i),
-                                  target_index,
-                                  source.column(i),
-                                  source_index);
-  }
-}
 }  // namespace cudf::detail

cpp/src/aggregation/aggregation.cu
@@ -128,7 +128,7 @@ struct identity_initializer {
 };
 }  // namespace
 
-void initialize_with_identity(mutable_table_view& table,
+void initialize_with_identity(mutable_table_view const& table,
                               host_span<cudf::aggregation::Kind const> aggs,
                               rmm::cuda_stream_view stream)
 {

cpp/src/groupby/hash/compute_aggregations.cu
@@ -1,28 +1,0 @@
-/*
- * Copyright (c) 2024-2025, NVIDIA CORPORATION.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include "compute_aggregations.cuh"
-#include "compute_aggregations.hpp"
-
-namespace cudf::groupby::detail::hash {
-template rmm::device_uvector<cudf::size_type> compute_aggregations<global_set_t>(
-  int64_t num_rows,
-  bitmask_type const* row_bitmask,
-  global_set_t& global_set,
-  cudf::host_span<cudf::groupby::aggregation_request const> requests,
-  cudf::detail::result_cache* sparse_results,
-  rmm::cuda_stream_view stream);
-}  // namespace cudf::groupby::detail::hash

commit b311cf94e2f56662df28b9b5be8b357a852b4d48

Fix missed-sync for `mapping_indices_kernel` in hash-based groupby aggregation (#20370)
This fixes a missed sync issue in `mapping_indices_kernel`, where the idle threads that do not sync to update a shared value stored in a shared memory location. This issue was introduced in https://github.com/rapidsai/cudf/pull/19764. Before that PR, all threads paritcipated in the loop (although some threads do nothing there) but all threads sync at the end of the loop.

Closes https://github.com/rapidsai/cudf/issues/20345, https://github.com/rapidsai/cudf/issues/20361, https://github.com/NVIDIA/spark-rapids/issues/13652.

Authors:
  - Nghia Truong (https://github.com/ttnghia)

Approvers:
  - Raza Jafri (https://github.com/razajafri)
  - Matthew Murray (https://github.com/Matt711)
  - Yunsong Wang (https://github.com/PointKernel)
  - MithunR (https://github.com/mythrocks)

URL: https://github.com/rapidsai/cudf/pull/20370

cpp/src/groupby/hash/compute_mapping_indices.cuh
@@ -111,8 +111,11 @@ CUDF_KERNEL void mapping_indices_kernel(size_type num_input_rows,
   if (block.thread_rank() == 0) { cardinality = 0; }
   block.sync();
 
+  // All threads in the block will participate in the loop, and sync.
   auto const stride = cudf::detail::grid_1d::grid_stride();
-  for (auto idx = cudf::detail::grid_1d::global_thread_id(); idx < num_input_rows; idx += stride) {
+  for (auto idx = cudf::detail::grid_1d::global_thread_id();
+       idx - block.thread_rank() < num_input_rows;
+       idx += stride) {
     find_local_mapping(block,
                        idx,
                        num_input_rows,