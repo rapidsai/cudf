bf69b610c41cc0afef7ef039ed3714b8833923f0
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-10-29 14:05:55 -0700

    Add q8

--- python/cudf/cudf/pandas/_benchmarks/pdsh.py
+++ python/cudf/cudf/pandas/_benchmarks/pdsh.py
@@ -314,6 +314,61 @@ class PDSHQueries:
 
         return agg.sort_values(by=["supp_nation", "cust_nation", "l_year"])
 
+    @staticmethod
+    def q8(run_config: RunConfig) -> pd.DataFrame:
+        """Query 8."""
+        customer = get_data(
+            run_config.dataset_path, "customer", run_config.suffix
+        )
+        lineitem = get_data(
+            run_config.dataset_path, "lineitem", run_config.suffix
+        )
+        nation = get_data(run_config.dataset_path, "nation", run_config.suffix)
+        orders = get_data(run_config.dataset_path, "orders", run_config.suffix)
+        part = get_data(run_config.dataset_path, "part", run_config.suffix)
+        region = get_data(run_config.dataset_path, "region", run_config.suffix)
+        supplier = get_data(
+            run_config.dataset_path, "supplier", run_config.suffix
+        )
+
+        var1 = "BRAZIL"
+        var2 = "AMERICA"
+        var3 = "ECONOMY ANODIZED STEEL"
+        var4 = date(1995, 1, 1)
+        var5 = date(1996, 12, 31)
+
+        n1 = nation.loc[:, ["n_nationkey", "n_regionkey"]]
+        n2 = nation.loc[:, ["n_nationkey", "n_name"]]
+
+        jn1 = part.merge(lineitem, left_on="p_partkey", right_on="l_partkey")
+        jn2 = jn1.merge(supplier, left_on="l_suppkey", right_on="s_suppkey")
+        jn3 = jn2.merge(orders, left_on="l_orderkey", right_on="o_orderkey")
+        jn4 = jn3.merge(customer, left_on="o_custkey", right_on="c_custkey")
+        jn5 = jn4.merge(n1, left_on="c_nationkey", right_on="n_nationkey")
+        jn6 = jn5.merge(region, left_on="n_regionkey", right_on="r_regionkey")
+
+        jn6 = jn6[(jn6["r_name"] == var2)]
+
+        jn7 = jn6.merge(n2, left_on="s_nationkey", right_on="n_nationkey")
+
+        jn7 = jn7[(jn7["o_orderdate"] >= var4) & (jn7["o_orderdate"] <= var5)]
+        jn7 = jn7[jn7["p_type"] == var3]
+
+        jn7["o_year"] = jn7["o_orderdate"].dt.year
+        jn7["volume"] = jn7["l_extendedprice"] * (1.0 - jn7["l_discount"])
+        jn7 = jn7.rename(columns={"n_name": "nation"})
+
+        def udf(df: pd.DataFrame) -> float:
+            demonimator: float = df["volume"].sum()
+            df = df[df["nation"] == var1]
+            numerator: float = df["volume"].sum()
+            return round(numerator / demonimator, 2)
+
+        gb = jn7.groupby("o_year", as_index=False)
+        agg = gb.apply(udf, include_groups=False)
+        agg.columns = ["o_year", "mkt_share"]
+        return agg.sort_values("o_year")
+
 
 if __name__ == "__main__":
     run_pandas(PDSHQueries)
