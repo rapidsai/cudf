30a91f0170bf5fd94a19a883f9d9d95ea0426f44
anon <users.noreply.github.com>
2025-10-29 23:51:41 +0000

    remove round

--- cpp/include/cudf/round.hpp
+++ cpp/include/cudf/round.hpp
@@ -31,48 +31,6 @@ enum class rounding_method : int32_t { HALF_UP, HALF_EVEN };
 /**
  * @brief Rounds all the values in a column to the specified number of decimal places.
  *
- * @deprecated Use cudf::round_decimal
- *
- * `cudf::round` currently supports HALF_UP and HALF_EVEN rounding for integer, floating point and
- * `decimal32` and `decimal64` numbers. For `decimal32` and `decimal64` numbers, negated
- * `numeric::scale` is equivalent to `decimal_places`.
- *
- * Example:
- * ```
- * using namespace cudf;
- *
- * column_view a; // contains { 1.729, 17.29, 172.9, 1729 };
- *
- * auto result1 = round(a);     // { 2,   17,   173,   1729 }
- * auto result2 = round(a, 1);  // { 1.7, 17.3, 172.9, 1729 }
- * auto result3 = round(a, -1); // { 0,   20,   170,   1730 }
- *
- * column_view b; // contains { 1.5, 2.5, 1.35, 1.45, 15, 25 };
- *
- * auto result4 = round(b,  0, rounding_method::HALF_EVEN); // { 2,   2,   1,   1,   15, 25};
- * auto result5 = round(b,  1, rounding_method::HALF_EVEN); // { 1.5, 2.5, 1.4, 1.4, 15, 25};
- * auto result6 = round(b, -1, rounding_method::HALF_EVEN); // { 0,   0,   0,   0,   20, 20};
- * ```
- *
- * @param input          Column of values to be rounded
- * @param decimal_places Number of decimal places to round to (default 0). If negative, this
- * specifies the number of positions to the left of the decimal point.
- * @param method         Rounding method
- * @param stream         CUDA stream used for device memory operations and kernel launches
- * @param mr             Device memory resource used to allocate the returned column's device memory
- *
- * @return Column with each of the values rounded
- */
-[[deprecated]] std::unique_ptr<column> round(
-  column_view const& input,
-  int32_t decimal_places            = 0,
-  rounding_method method            = rounding_method::HALF_UP,
-  rmm::cuda_stream_view stream      = cudf::get_default_stream(),
-  rmm::device_async_resource_ref mr = cudf::get_current_device_resource_ref());
-
-/**
- * @brief Rounds all the values in a column to the specified number of decimal places.
- *
  * Supports HALF_UP and HALF_EVEN rounding for integer and fixed-point (decimal)
  * numbers. For decimal numbers, negated `numeric::scale` is equivalent to `decimal_places`.
  *
