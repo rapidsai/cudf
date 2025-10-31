0d5629acaa530b87493b5ae60adc818fcfa4043a
Muhammad Haseeb <mhaseeb@nvidia.com>
2025-10-31 23:59:40 +0000

    style fix

--- cpp/src/io/utilities/block_utils.cuh
+++ cpp/src/io/utilities/block_utils.cuh
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2019-2023, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2019-2025, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 
