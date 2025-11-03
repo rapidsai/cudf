3c80948bd7a54381020821221bfa84ce2cb39581
Bradley Dice <bdice@bradleydice.com>
2025-11-03 12:21:44 -0600

    Remove outdated CCCL compatibility shims

--- cpp/src/utilities/host_memory.cpp
+++ cpp/src/utilities/host_memory.cpp
@@ -74,11 +74,7 @@ class fixed_pinned_pool_memory_resource {
   void* allocate(std::size_t bytes, std::size_t alignment = rmm::RMM_DEFAULT_HOST_ALIGNMENT)
   {
     auto const result = allocate_async(bytes, alignment, stream_);
-#if CCCL_MAJOR_VERSION > 3 || (CCCL_MAJOR_VERSION == 3 && CCCL_MINOR_VERSION >= 1)
     stream_.sync();
-#else
-    stream_.wait();
-#endif
     return result;
   }
 
