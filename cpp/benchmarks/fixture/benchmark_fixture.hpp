rapidsai/cudf

commit 414668e3b70a7217ab4b6c656b617d1276266e41

Deprecate `get_current_device_resource` in favor of `get_current_device_resource_ref` (#20386)
This PR replaces all usages of `get_current_device_resource` with `get_current_device_resource_ref` and marks the former as deprecated in 25.12. The deprecated API will be removed in release 26.02.

Authors:
  - Yunsong Wang (https://github.com/PointKernel)

Approvers:
  - Nghia Truong (https://github.com/ttnghia)
  - Bradley Dice (https://github.com/bdice)
  - David Wendt (https://github.com/davidwendt)

URL: https://github.com/rapidsai/cudf/pull/20386

cpp/benchmarks/fixture/benchmark_fixture.hpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2019-2024, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2019-2025, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -96,13 +96,14 @@ class benchmark : public ::benchmark::Fixture {
 class memory_stats_logger {
  public:
   memory_stats_logger()
-    : existing_mr(cudf::get_current_device_resource()),
-      statistics_mr(rmm::mr::statistics_resource_adaptor(existing_mr))
+    : existing_mr(cudf::get_current_device_resource_ref()),
+      statistics_mr(
+        rmm::mr::statistics_resource_adaptor<rmm::mr::device_memory_resource>(existing_mr))
   {
     cudf::set_current_device_resource(&statistics_mr);
   }
 
-  ~memory_stats_logger() { cudf::set_current_device_resource(existing_mr); }
+  ~memory_stats_logger() { cudf::set_current_device_resource_ref(existing_mr); }
 
   [[nodiscard]] size_t peak_memory_usage() const noexcept
   {
@@ -110,8 +111,7 @@ class memory_stats_logger {
   }
 
  private:
-  // TODO change to resource_ref once set_current_device_resource supports it
-  rmm::mr::device_memory_resource* existing_mr;
+  rmm::device_async_resource_ref existing_mr;
   rmm::mr::statistics_resource_adaptor<rmm::mr::device_memory_resource> statistics_mr;
 };
 

cpp/benchmarks/ndsh/utilities.cpp
@@ -401,7 +401,7 @@ void generate_parquet_data_sources(double scale_factor,
   CUDF_BENCHMARK_RANGE();
 
   // Set the memory resource to the managed pool
-  auto old_mr = cudf::get_current_device_resource();
+  auto* old_mr = rmm::mr::get_current_device_resource();
   // if already managed pool or managed, don't create new one.
   using managed_pool_mr_t = decltype(make_managed_pool());
   managed_pool_mr_t managed_pool_mr;

cpp/include/cudf/utilities/memory_resource.hpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -21,8 +21,12 @@ namespace cudf {
 /**
  * @brief Get the current device memory resource.
  *
+ * @deprecated Since 25.12. Use get_current_device_resource_ref() instead.
+ * This function will be removed in 26.02.
+ *
  * @return The current device memory resource.
  */
+[[deprecated("Use get_current_device_resource_ref() instead. Will be removed in 26.02.")]]
 inline rmm::mr::device_memory_resource* get_current_device_resource()
 {
   return rmm::mr::get_current_device_resource();

cpp/include/cudf_test/testing_main.hpp
@@ -227,7 +227,7 @@ inline void init_cudf_test(int argc, char** argv, cudf::test::config const& conf
     init_cudf_test(argc, argv);                                                                  \
     if (std::getenv("GTEST_CUDF_MEMORY_PEAK")) {                                                 \
       auto mr = rmm::mr::statistics_resource_adaptor<rmm::mr::device_memory_resource>(           \
-        cudf::get_current_device_resource());                                                    \
+        cudf::get_current_device_resource_ref());                                                \
       cudf::set_current_device_resource(&mr);                                                    \
       auto rc = RUN_ALL_TESTS();                                                                 \
       std::cout << "Peak memory usage " << mr.get_bytes_counter().peak << " bytes" << std::endl; \

cpp/src/io/parquet/predicate_pushdown.cpp
@@ -237,8 +237,8 @@ aggregate_reader_metadata::filter_row_groups(
   }
 
   // Aligned resource adaptor to allocate bloom filter buffers with
-  auto aligned_mr = rmm::mr::aligned_resource_adaptor(cudf::get_current_device_resource(),
-                                                      get_bloom_filter_alignment());
+  auto aligned_mr = rmm::mr::aligned_resource_adaptor<rmm::device_async_resource_ref>(
+    cudf::get_current_device_resource_ref(), get_bloom_filter_alignment());
 
   // Read a vector of bloom filter bitset device buffers for all columns with equality
   // predicate(s) across all row groups

cpp/tests/io/experimental/hybrid_scan_common.cpp
@@ -172,8 +172,8 @@ auto apply_parquet_filters(cudf::host_span<uint8_t const> file_buffer_span,
   bloom_filtered_row_group_indices.reserve(current_row_group_indices.size());
   if (bloom_filter_byte_ranges.size()) {
     // Fetch 32 byte aligned bloom filter data buffers from the input file buffer
-    auto aligned_mr = rmm::mr::aligned_resource_adaptor(cudf::get_current_device_resource(),
-                                                        bloom_filter_alignment);
+    auto aligned_mr = rmm::mr::aligned_resource_adaptor<rmm::device_async_resource_ref>(
+      cudf::get_current_device_resource_ref(), bloom_filter_alignment);
 
     std::vector<rmm::device_buffer> bloom_filter_data =
       fetch_byte_ranges(file_buffer_span, bloom_filter_byte_ranges, stream, aligned_mr);

cpp/tests/io/experimental/hybrid_scan_test.cpp
@@ -125,7 +125,7 @@ void test_hybrid_scan(std::vector<cudf::column_view> const& columns)
   auto stream     = cudf::get_default_stream();
   auto mr         = cudf::get_current_device_resource_ref();
   auto aligned_mr = rmm::mr::aligned_resource_adaptor<rmm::mr::device_memory_resource>(
-    cudf::get_current_device_resource(), bloom_filter_alignment);
+    cudf::get_current_device_resource_ref(), bloom_filter_alignment);
 
   // Read parquet using the hybrid scan reader
   auto [read_filter_table, read_payload_table, read_filter_meta, read_payload_meta, row_mask] =
@@ -199,7 +199,7 @@ TEST_F(HybridScanTest, PruneRowGroupsOnlyAndScanAllColumns)
   auto stream     = cudf::get_default_stream();
   auto mr         = cudf::get_current_device_resource_ref();
   auto aligned_mr = rmm::mr::aligned_resource_adaptor<rmm::mr::device_memory_resource>(
-    cudf::get_current_device_resource(), bloom_filter_alignment);
+    cudf::get_current_device_resource_ref(), bloom_filter_alignment);
 
   // Read parquet using the hybrid scan reader
   auto [read_filter_table, read_payload_table, read_filter_meta, read_payload_meta, row_mask] =
@@ -256,7 +256,7 @@ TEST_F(HybridScanTest, PruneRowGroupsOnlyAndScanSelectColumns)
   auto stream     = cudf::get_default_stream();
   auto mr         = cudf::get_current_device_resource_ref();
   auto aligned_mr = rmm::mr::aligned_resource_adaptor<rmm::mr::device_memory_resource>(
-    cudf::get_current_device_resource(), bloom_filter_alignment);
+    cudf::get_current_device_resource_ref(), bloom_filter_alignment);
 
   {
     auto const payload_column_names = std::vector<std::string>{"col0", "col2"};
@@ -345,7 +345,7 @@ TEST_F(HybridScanTest, PruneDataPagesOnlyAndScanAllColumns)
   auto stream     = cudf::get_default_stream();
   auto mr         = cudf::get_current_device_resource_ref();
   auto aligned_mr = rmm::mr::aligned_resource_adaptor<rmm::mr::device_memory_resource>(
-    cudf::get_current_device_resource(), bloom_filter_alignment);
+    cudf::get_current_device_resource_ref(), bloom_filter_alignment);
 
   // Read parquet using the hybrid scan reader
   auto [read_filter_table, read_payload_table, read_filter_meta, read_payload_meta, row_mask] =

cpp/tests/large_strings/large_strings_fixture.cpp
@@ -118,7 +118,7 @@ int main(int argc, char** argv)
 
   if (std::getenv("GTEST_CUDF_MEMORY_PEAK")) {
     auto mr = rmm::mr::statistics_resource_adaptor<rmm::mr::device_memory_resource>(
-      cudf::get_current_device_resource());
+      cudf::get_current_device_resource_ref());
     cudf::set_current_device_resource(&mr);
     auto rc = RUN_ALL_TESTS();
     std::cout << "Peak memory usage " << mr.get_bytes_counter().peak << " bytes" << std::endl;

commit 6beccbc2dbeaf8e52dac423dbc26fc1159936755

Fix the statistics_mr in benchmark fixture (#20496)
This PR fixes a bug introduced in #20386, where the data memory resource was updated to use the new async resource reference, but the statistics based on the new ref was still calling the legacy memory resource setter, leading to a runtime crash.

Authors:
  - Yunsong Wang (https://github.com/PointKernel)

Approvers:
  - David Wendt (https://github.com/davidwendt)
  - Bradley Dice (https://github.com/bdice)
  - Nghia Truong (https://github.com/ttnghia)
  - Muhammad Haseeb (https://github.com/mhaseeb123)

URL: https://github.com/rapidsai/cudf/pull/20496

cpp/benchmarks/fixture/benchmark_fixture.hpp
@@ -98,9 +98,9 @@ class memory_stats_logger {
   memory_stats_logger()
     : existing_mr(cudf::get_current_device_resource_ref()),
       statistics_mr(
-        rmm::mr::statistics_resource_adaptor<rmm::mr::device_memory_resource>(existing_mr))
+        rmm::mr::statistics_resource_adaptor<rmm::device_async_resource_ref>(existing_mr))
   {
-    cudf::set_current_device_resource(&statistics_mr);
+    cudf::set_current_device_resource_ref(&statistics_mr);
   }
 
   ~memory_stats_logger() { cudf::set_current_device_resource_ref(existing_mr); }
@@ -112,7 +112,7 @@ class memory_stats_logger {
 
  private:
   rmm::device_async_resource_ref existing_mr;
-  rmm::mr::statistics_resource_adaptor<rmm::mr::device_memory_resource> statistics_mr;
+  rmm::mr::statistics_resource_adaptor<rmm::device_async_resource_ref> statistics_mr;
 };
 
 }  // namespace cudf