7dc8283a41203d3175c620c3632cd22cfecc5ef5
anon <users.noreply.github.com>
2025-11-06 00:12:04 +0000

    remove more garbage

--- cpp/benchmarks/io/table/table_reader.cpp
+++ cpp/benchmarks/io/table/table_reader.cpp
@@ -31,11 +31,8 @@ void table_read_common(cudf::size_type num_rows_to_read,
 
       timer.start();
       auto result = cudf::io::read_table(source_info);
-      // read_table already synchronizes internally, so result is ready
       timer.stop();
 
-      // Access result.table while result is still in scope
-      // result owns the packed_columns which contains the memory that table_view points to
       CUDF_EXPECTS(result.table.num_columns() == num_cols_to_read, "Unexpected number of columns");
       CUDF_EXPECTS(result.table.num_rows() == num_rows_to_read, "Unexpected number of rows");
     });
@@ -45,61 +42,23 @@ void table_read_common(cudf::size_type num_rows_to_read,
   state.add_buffer_size(source_sink.size(), "encoded_file_size", "encoded_file_size");
 }
 
-void BM_table_read_io_paths(nvbench::state& state)
+void BM_table_read_data_sizes(nvbench::state& state)
 {
   auto const d_type      = get_type_or_group(static_cast<int32_t>(data_type::INTEGRAL));
-  auto const source_type = retrieve_io_type_enum(state.get_string("io_type"));
-  auto const data_size   = static_cast<size_t>(state.get_int64("data_size"));
-  cuio_source_sink_pair source_sink(source_type);
-
-  auto const num_rows_written = [&]() {
-    auto const tbl =
-      create_random_table(cycle_dtypes(d_type, num_cols), table_size_bytes{data_size});
-    auto const view = tbl->view();
-
-    cudf::io::write_table(view, source_sink.make_sink_info());
-    return view.num_rows();
-  }();
-
-  table_read_common(num_rows_written, num_cols, source_sink, state);
-}
-
-void BM_table_read_size_scaling(nvbench::state& state)
-{
-  auto const d_type      = get_type_or_group(static_cast<int32_t>(data_type::INTEGRAL));
-  auto const source_type = io_type::DEVICE_BUFFER;  // Use DEVICE_BUFFER to isolate unpack overhead
-  auto const data_size   = static_cast<size_t>(state.get_int64("data_size"));
-  cuio_source_sink_pair source_sink(source_type);
-
-  auto const num_rows_written = [&]() {
-    auto const tbl =
-      create_random_table(cycle_dtypes(d_type, num_cols), table_size_bytes{data_size});
-    auto const view = tbl->view();
-
-    cudf::io::write_table(view, source_sink.make_sink_info());
-    return view.num_rows();
-  }();
-
-  table_read_common(num_rows_written, num_cols, source_sink, state);
-}
-
-void BM_table_read_column_complexity(nvbench::state& state)
-{
-  auto const d_type      = get_type_or_group(static_cast<int32_t>(data_type::INTEGRAL));
-  auto const n_col       = static_cast<cudf::size_type>(state.get_int64("num_cols"));
   auto const source_type = io_type::DEVICE_BUFFER;
-  auto const data_size   = static_cast<size_t>(512 << 20);
+  auto const data_size   = static_cast<size_t>(state.get_int64("data_size"));
   cuio_source_sink_pair source_sink(source_type);
 
   auto const num_rows_written = [&]() {
-    auto const tbl  = create_random_table(cycle_dtypes(d_type, n_col), table_size_bytes{data_size});
+    auto const tbl =
+      create_random_table(cycle_dtypes(d_type, num_cols), table_size_bytes{data_size});
     auto const view = tbl->view();
 
     cudf::io::write_table(view, source_sink.make_sink_info());
     return view.num_rows();
   }();
 
-  table_read_common(num_rows_written, n_col, source_sink, state);
+  table_read_common(num_rows_written, num_cols, source_sink, state);
 }
 
 template <data_type DataType>
@@ -125,10 +84,9 @@ void BM_table_read_data_common(nvbench::state& state,
 }
 
 template <data_type DataType>
-void BM_table_read_data(nvbench::state& state,
-                        nvbench::type_list<nvbench::enum_type<DataType>> type_list)
+void BM_table_read_data_types(nvbench::state& state,
+                              nvbench::type_list<nvbench::enum_type<DataType>> type_list)
 {
-  // Cardinality and run_length don't affect table format read performance
   BM_table_read_data_common<DataType>(state, data_profile{}, type_list);
 }
 
@@ -144,7 +102,6 @@ void BM_table_read_wide_tables(nvbench::state& state,
   cuio_source_sink_pair source_sink(source_type);
 
   auto const num_rows_written = [&]() {
-    // Cardinality and run_length don't affect table format read performance
     auto const tbl =
       create_random_table(cycle_dtypes(d_type, n_col), table_size_bytes{data_size_bytes});
     auto const view = tbl->view();
@@ -156,17 +113,6 @@ void BM_table_read_wide_tables(nvbench::state& state,
   table_read_common(num_rows_written, n_col, source_sink, state);
 }
 
-NVBENCH_BENCH(BM_table_read_size_scaling)
-  .set_name("table_read_size_scaling")
-  .set_min_samples(4)
-  .add_int64_axis("data_size", {256LL << 20, 512LL << 20, 1024LL << 20, 2048LL << 20});
-
-NVBENCH_BENCH(BM_table_read_column_complexity)
-  .set_name("table_read_column_complexity")
-  .set_min_samples(4)
-  .add_int64_axis("num_cols", {32, 128, 512, 1024})
-  .add_int64_axis("data_size", {512 << 20});  // Fixed size for column complexity test
-
 using d_type_list_reduced = nvbench::enum_type_list<data_type::INTEGRAL,
                                                     data_type::FLOAT,
                                                     data_type::BOOL8,
@@ -176,12 +122,17 @@ using d_type_list_reduced = nvbench::enum_type_list<data_type::INTEGRAL,
                                                     data_type::STRING,
                                                     data_type::LIST,
                                                     data_type::STRUCT>;
-NVBENCH_BENCH_TYPES(BM_table_read_data, NVBENCH_TYPE_AXES(d_type_list_reduced))
-  .set_name("table_read_decode")
+NVBENCH_BENCH_TYPES(BM_table_read_data_types, NVBENCH_TYPE_AXES(d_type_list_reduced))
+  .set_name("table_read_data_types")
   .set_type_axes_names({"data_type"})
   .add_string_axis("io_type", {"DEVICE_BUFFER"})
   .set_min_samples(4)
   .add_int64_axis("data_size", {512 << 20});
+
+NVBENCH_BENCH(BM_table_read_data_sizes)
+  .set_name("table_read_data_sizes")
+  .set_min_samples(4)
+  .add_int64_power_of_two_axis("data_size", nvbench::range(24, 31, 1));  // 16MB to 2GB
 
 NVBENCH_BENCH_TYPES(BM_table_read_wide_tables,
                     NVBENCH_TYPE_AXES(nvbench::enum_type_list<data_type::STRING>))
@@ -189,4 +140,4 @@ NVBENCH_BENCH_TYPES(BM_table_read_wide_tables,
   .set_type_axes_names({"data_type"})
   .set_min_samples(4)
   .add_int64_axis("data_size", {512 << 20})
-  .add_int64_axis("num_cols", {256, 512, 1024, 2048});
+  .add_int64_axis("num_cols", {128, 256, 512, 1024, 2048});
