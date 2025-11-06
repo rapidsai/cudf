762f445c8b27f5070a1b33b554167978dadc76bc
Bradley Dice <bdice@bradleydice.com>
2025-11-06 15:59:48 -0600

    Run all nvbench benchmarks with timeout in smoketest

--- ci/run_cudf_benchmark_smoketests.sh
+++ ci/run_cudf_benchmark_smoketests.sh
@@ -10,5 +10,11 @@ cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/benchmarks/libcudf/";
 # Ensure that benchmarks are runnable
 # Run a small Google benchmark
 ./MERGE_BENCH --benchmark_filter=/2/
-# Run a small nvbench benchmark
-./STRINGS_NVBENCH --profile --benchmark 0 --devices 0
+
+# Run all nvbench benchmarks with --profile and 1 minute timeout
+for bench in *_NVBENCH; do
+  if [[ -x "$bench" && -f "$bench" ]]; then
+    echo "Running $bench with --profile..."
+    timeout 1m "./$bench" --profile
+  fi
+done
