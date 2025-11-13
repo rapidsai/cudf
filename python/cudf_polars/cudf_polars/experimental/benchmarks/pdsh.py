8e4d395a86199e3421b52004a8a3b5e83774a8fa
Matthew Murray <matthewmurray711@gmail.com>
2025-11-13 15:39:21 -0800

    Print duckDB query plan and change Q17 join type

--- python/cudf_polars/cudf_polars/experimental/benchmarks/pdsh.py
+++ python/cudf_polars/cudf_polars/experimental/benchmarks/pdsh.py
@@ -617,7 +617,7 @@ class PDSHQueries:
             q1.group_by("p_partkey")
             .agg((0.2 * pl.col("l_quantity").mean()).alias("avg_quantity"))
             .select(pl.col("p_partkey").alias("key"), pl.col("avg_quantity"))
-            .join(q1, left_on="key", right_on="p_partkey")
+            .join(q1, left_on="key", right_on="p_partkey", how="inner")
             .filter(pl.col("l_quantity") < pl.col("avg_quantity"))
             .select(
                 (pl.col("l_extendedprice").sum() / 7.0).round(2).alias("avg_yearly")
