4e266b389e5fb43b0109817480dc443b4cdd19df
Matthew Murray <matthewmurray711@gmail.com>
2025-11-14 12:39:14 -0500

    wrong place

--- python/cudf_polars/cudf_polars/experimental/benchmarks/pdsh.py
+++ python/cudf_polars/cudf_polars/experimental/benchmarks/pdsh.py
@@ -610,14 +610,14 @@ class PDSHQueries:
         q1 = (
             part.filter(pl.col("p_brand") == var1)
             .filter(pl.col("p_container") == var2)
-            .join(lineitem, how="left", left_on="p_partkey", right_on="l_partkey")
+            .join(lineitem, how="inner", left_on="p_partkey", right_on="l_partkey")
         )
 
         return (
             q1.group_by("p_partkey")
             .agg((0.2 * pl.col("l_quantity").mean()).alias("avg_quantity"))
             .select(pl.col("p_partkey").alias("key"), pl.col("avg_quantity"))
-            .join(q1, left_on="key", right_on="p_partkey", how="inner")
+            .join(q1, left_on="key", right_on="p_partkey")
             .filter(pl.col("l_quantity") < pl.col("avg_quantity"))
             .select(
                 (pl.col("l_extendedprice").sum() / 7.0).round(2).alias("avg_yearly")
