8c7d62c94d0b934d394f33522327efc3e83cd6c7
Vyas Ramasubramani <vyasr@nvidia.com>
2025-11-15 00:45:13 +0000

    Add testing to CI

--- ci/run_pylibcudf_pytests.sh
+++ ci/run_pylibcudf_pytests.sh
@@ -9,4 +9,39 @@ set -euo pipefail
 # Support invoking run_cudf_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/pylibcudf/
 
-pytest --cache-clear --ignore="benchmarks" "$@" tests
+# Find the libcudf_identify_stream_usage_mode_testing.so library
+TESTING_LIB=$(python3 <<'EOF'
+import os
+
+# Import pylibcudf to ensure libcudf.so is loaded
+import pylibcudf
+
+with open("/proc/self/maps") as f:
+    for line in f:
+        if "libcudf.so" in line:
+            libcudf_lib_path = line.split()[-1]
+            break
+    else:
+        raise RuntimeError(
+            "Could not find libcudf.so in /proc/self/maps"
+        )
+
+# Search for the testing library in that directory
+testing_lib_pattern = os.path.join(
+    os.path.dirname(libcudf_lib_path),
+    "libcudf_identify_stream_usage_mode_testing.so"
+)
+if os.path.isfile(testing_lib_pattern):
+    print(testing_lib_pattern)
+else:
+    print("")
+EOF
+)
+
+if [ -n "$TESTING_LIB" ] && [ -f "$TESTING_LIB" ]; then
+    # If the stream testing library was found, split the tests into two passes.
+    LD_PRELOAD="$TESTING_LIB" PYLIBCUDF_STREAM_TESTING=1 pytest --cache-clear -m "not uses_custom_stream" --ignore="benchmarks" "$@" tests
+    pytest --cache-clear -m "uses_custom_stream" --ignore="benchmarks" "$@" tests
+else
+    pytest --cache-clear --ignore="benchmarks" "$@" tests
+fi
