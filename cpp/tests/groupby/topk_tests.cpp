f7ef0255e2cba7f8ed3968bb839be60f3877ee42
David Wendt <dwendt@nvidia.com>
2025-11-17 15:33:33 -0500

    add tests with nulls k-bigger-than-segsize

--- cpp/tests/groupby/topk_tests.cpp
+++ cpp/tests/groupby/topk_tests.cpp
@@ -23,13 +23,13 @@ TYPED_TEST(groupby_topk_test_types, FixedWidth)
   using K = int32_t;
   using V = TypeParam;
 
-  auto keys   = cudf::test::fixed_width_column_wrapper<K, int32_t>{1, 1, 1, 2, 2, 2};
-  auto values = cudf::test::fixed_width_column_wrapper<V, int32_t>{1, 2, 3, 4, 5, 6};
+  auto keys   = cudf::test::fixed_width_column_wrapper<K, int32_t>{1, 2, 1, 2, 1, 2};
+  auto values = cudf::test::fixed_width_column_wrapper<V, int32_t>{1, 4, 2, 5, 3, 6};
 
   auto expect_keys = cudf::test::fixed_width_column_wrapper<K, int32_t>{1, 2};
-  auto expect_vals = cudf::test::lists_column_wrapper<V, int32_t>{{3, 2, 1}, {6, 5, 4}};
 
-  auto agg = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(3);
+  auto expect_vals = cudf::test::lists_column_wrapper<V, int32_t>{{3, 2, 1}, {6, 5, 4}};
+  auto agg         = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(3);
   test_single_agg(keys, values, expect_keys, expect_vals, std::move(agg));
 
   expect_vals = cudf::test::lists_column_wrapper<V, int32_t>{{1, 2, 3}, {4, 5, 6}};
@@ -39,19 +39,57 @@ TYPED_TEST(groupby_topk_test_types, FixedWidth)
 
 struct groupby_topk_test : public cudf::test::BaseFixture {};
 
-TEST_F(groupby_topk_test, Strings)
+TEST_F(groupby_topk_test, WithNulls)
 {
-  auto keys   = cudf::test::fixed_width_column_wrapper<int32_t>({1, 1, 1, 2, 2, 2});
-  auto values = cudf::test::strings_column_wrapper({"a", "b", "c", "d", "e", "f"});
+  // clang-format off
+  auto keys   = cudf::test::fixed_width_column_wrapper<int32_t>({1, 2, 1, 2, 1, 2, 3, 3, 2, 1, 3, 3});
+  auto values = cudf::test::fixed_width_column_wrapper<int32_t>({1, 4, 2, 5, 3, 6, 7, 8, 0, 9, 0, 10},
+    {true, true, true, true, true, true, true, true, false, true, false, true});
+  // clang-format on
 
-  auto expect_keys = cudf::test::fixed_width_column_wrapper<int32_t>({1, 2});
-  auto expect_vals = cudf::test::lists_column_wrapper<cudf::string_view>({{"c", "b"}, {"f", "e"}});
+  auto expect_keys = cudf::test::fixed_width_column_wrapper<int32_t>({1, 2, 3});
 
-  auto agg = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(2);
+  auto expect_vals = cudf::test::lists_column_wrapper<int32_t>({{9, 3, 2}, {6, 5, 4}, {10, 8, 7}});
+  auto agg         = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(3);
   test_single_agg(keys, values, expect_keys, expect_vals, std::move(agg));
 
   agg         = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(2, cudf::order::ASCENDING);
-  expect_vals = cudf::test::lists_column_wrapper<cudf::string_view>({{"a", "b"}, {"d", "e"}});
+  expect_vals = cudf::test::lists_column_wrapper<int32_t>({{1, 2}, {4, 5}, {7, 8}});
+  test_single_agg(keys, values, expect_keys, expect_vals, std::move(agg));
+}
+
+TEST_F(groupby_topk_test, KGreaterThanGroupSize)
+{
+  auto keys   = cudf::test::fixed_width_column_wrapper<int32_t>({1, 2, 1, 2, 1, 2, 1, 2, 1});
+  auto values = cudf::test::fixed_width_column_wrapper<int32_t>({1, 4, 2, 5, 3, 6, 7, 8, 9});
+
+  auto expect_keys = cudf::test::fixed_width_column_wrapper<int32_t>({1, 2});
+
+  auto expect_vals = cudf::test::lists_column_wrapper<int32_t>({{9, 7, 3, 2, 1}, {8, 6, 5, 4}});
+  auto agg         = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(6);
+  test_single_agg(keys, values, expect_keys, expect_vals, std::move(agg));
+  agg = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(5);
+  test_single_agg(keys, values, expect_keys, expect_vals, std::move(agg));
+
+  agg         = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(5, cudf::order::ASCENDING);
+  expect_vals = cudf::test::lists_column_wrapper<int32_t>({{1, 2, 3, 7, 9}, {4, 5, 6, 8}});
+  test_single_agg(keys, values, expect_keys, expect_vals, std::move(agg));
+}
+
+TEST_F(groupby_topk_test, Strings)
+{
+  auto keys   = cudf::test::fixed_width_column_wrapper<int32_t>({1, 2, 1, 2, 1, 2, 1, 2, 1});
+  auto values = cudf::test::strings_column_wrapper({"a", "d", "b", "e", "c", "f", "g", "h", "i"});
+
+  auto expect_keys = cudf::test::fixed_width_column_wrapper<int32_t>({1, 2});
+
+  auto expect_vals = cudf::test::lists_column_wrapper<cudf::string_view>({{"i", "g"}, {"h", "f"}});
+  auto agg         = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(2);
+  test_single_agg(keys, values, expect_keys, expect_vals, std::move(agg));
+
+  agg         = cudf::make_top_k_aggregation<cudf::groupby_aggregation>(5, cudf::order::ASCENDING);
+  expect_vals = cudf::test::lists_column_wrapper<cudf::string_view>(
+    {{"a", "b", "c", "g", "i"}, {"d", "e", "f", "h"}});
   test_single_agg(keys, values, expect_keys, expect_vals, std::move(agg));
 }
 
