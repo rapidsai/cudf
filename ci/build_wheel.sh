61260d16a490890317448fcf75f70c31378d1051
ptaylor <paul.e.taylor@me.com>
2025-11-19 20:00:22 +0000

    use S3 as the preprocessor cache location in devcontainers

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -11,7 +11,6 @@ source rapids-configure-sccache
 source rapids-date-string
 source rapids-init-pip
 
-# shellcheck disable=SC2155
 export SCCACHE_S3_PREPROCESSOR_CACHE_KEY_PREFIX="${package_name}-${RAPIDS_CONDA_ARCH}-cuda${RAPIDS_CUDA_VERSION%%.*}-wheel-preprocessor-cache"
 export SCCACHE_S3_USE_PREPROCESSOR_CACHE_MODE=true
 
