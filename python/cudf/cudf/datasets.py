rapidsai/cudf

commit e97450311b38b1b22dfcabc5f7b456d72aaca853

Make Column.set_mask go through pylibcudf (#20103)
Broken off from https://github.com/rapidsai/cudf/pull/20087

Authors:
  - Matthew Roeschke (https://github.com/mroeschke)

Approvers:
  - Tom Augspurger (https://github.com/TomAugspurger)

URL: https://github.com/rapidsai/cudf/pull/20103

python/cudf/cudf/datasets.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2024, NVIDIA CORPORATION.
+# Copyright (c) 2020-2025, NVIDIA CORPORATION.
 
 import numpy as np
 import pandas as pd
@@ -69,10 +69,7 @@ def timeseries(
             size=len(index),
             p=[1 - nulls_frequency, nulls_frequency],
         )
-        mask_buf = cudf.core.column.as_column(mask).as_mask()
-        masked_col = gdf[col]._column.set_mask(mask_buf)
-        gdf[col] = cudf.Series._from_column(masked_col, index=gdf.index)
-
+        gdf.loc[mask, col] = None
     return gdf
 
 

python/cudf/cudf/core/column/categorical.py
@@ -331,13 +331,8 @@ class CategoricalColumn(column.ColumnBase):
             raise NotImplementedError(f"{arrow_type=} is not supported.")
 
         if self.categories.dtype.kind == "f":
-            col = type(self)(
-                data=self.data,  # type: ignore[arg-type]
-                size=self.size,
-                dtype=self.dtype,
-                mask=self.notnull().fillna(False).as_mask(),
-                children=self.children,
-            )
+            new_mask = self.notnull().fillna(False).as_mask()
+            col = self.set_mask(new_mask)
         else:
             col = self
 

python/cudf/cudf/core/column/column.py
@@ -398,14 +398,22 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
                 dbuf.copy_from_host(value)
                 mask = as_buffer(dbuf)
 
-        return build_column(  # type: ignore[return-value]
-            data=self.data,
-            dtype=self.dtype,
-            mask=mask,
-            size=self.size,
-            offset=0,
-            children=self.children,
-        )
+        if mask is not None:
+            new_mask: plc.gpumemoryview | None = plc.gpumemoryview(mask)
+            new_null_count = plc.null_mask.null_count(
+                new_mask,
+                0,
+                self.size,
+            )
+        else:
+            new_mask = None
+            new_null_count = 0
+        new_plc_column = self.to_pylibcudf(
+            mode="read", use_base=False
+        ).with_mask(new_mask, new_null_count)
+        return self.from_pylibcudf(  # type: ignore[return-value]
+            new_plc_column,
+        )._with_type_metadata(self.dtype)
 
     @property
     def null_count(self) -> int:
@@ -488,7 +496,9 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
     # underlying buffers as exposed before this function can itself be exposed
     # publicly.  User requests to convert to pylibcudf must assume that the
     # data may be modified afterwards.
-    def to_pylibcudf(self, mode: Literal["read", "write"]) -> plc.Column:
+    def to_pylibcudf(
+        self, mode: Literal["read", "write"], *, use_base: bool = True
+    ) -> plc.Column:
         """Convert this Column to a pylibcudf.Column.
 
         This function will generate a pylibcudf Column pointing to the same
@@ -501,6 +511,9 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
             to may be modified by the caller. If "read", the data pointed to
             must not be modified by the caller.  Failure to fulfill this
             contract will cause incorrect behavior.
+        use_base : bool, default True
+            Whether to use the column's base data, mask, and children,
+            or data, mask, and children relative to a 0 offset.
 
         Returns
         -------
@@ -522,10 +535,14 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
 
         data = None
         if col.base_data is not None:
+            if use_base:
+                data_buff = col.base_data
+            else:
+                data_buff = col.data  # type: ignore[assignment]
             cai = cuda_array_interface_wrapper(
-                ptr=col.base_data.get_ptr(mode=mode),
-                size=col.base_data.size,
-                owner=col.base_data,
+                ptr=data_buff.get_ptr(mode=mode),
+                size=data_buff.size,
+                owner=data_buff,
             )
             data = plc.gpumemoryview(cai)
 
@@ -533,18 +550,24 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
         if self.nullable:
             # TODO: Are we intentionally use self's mask instead of col's?
             # Where is the mask stored for categoricals?
+            if use_base:
+                mask_buff = self.base_mask
+            else:
+                mask_buff = self.mask
             cai = cuda_array_interface_wrapper(
-                ptr=self.base_mask.get_ptr(mode=mode),  # type: ignore[union-attr]
-                size=self.base_mask.size,  # type: ignore[union-attr]
-                owner=self.base_mask,
+                ptr=mask_buff.get_ptr(mode=mode),  # type: ignore[union-attr]
+                size=mask_buff.size,  # type: ignore[union-attr]
+                owner=mask_buff,
             )
             mask = plc.gpumemoryview(cai)
 
         children = []
         if col.base_children:
             children = [
-                child_column.to_pylibcudf(mode=mode)
-                for child_column in col.base_children
+                child_column.to_pylibcudf(mode=mode, use_base=use_base)
+                for child_column in (
+                    col.base_children if use_base else col.children
+                )
             ]
 
         return plc.Column(
@@ -553,7 +576,7 @@ class ColumnBase(Serializable, BinaryOperand, Reducible):
             data,
             mask,
             self.null_count,
-            self.offset,
+            self.offset if use_base else 0,
             children,
         )
 

python/cudf/cudf/tests/dataframe/methods/test_reductions.py
@@ -198,7 +198,10 @@ def test_dataframe_reduction_error():
 
 
 def test_mean_timeseries(numeric_only):
-    gdf = cudf.datasets.timeseries()
+    gdf = cudf.DataFrame(
+        {"a": ["a", "b", "c"], "b": range(3), "c": [-1.0, 12.2, 0.0]},
+        index=pd.date_range("2020-01-01", periods=3, name="timestamp"),
+    )
     if not numeric_only:
         gdf = gdf.select_dtypes(include="number")
     pdf = gdf.to_pandas()

commit ccc2653da1d4c51e60ef94cc7565f858ca87edda

Resolve pandas test failures (#20704)
This PR fixes pandas test failures and also fixes an unrelated issue in dataset generation introduced in https://github.com/rapidsai/cudf/pull/20103.

Authors:
  - GALI PREM SAGAR (https://github.com/galipremsagar)

Approvers:
  - Vyas Ramasubramani (https://github.com/vyasr)
  - Matthew Murray (https://github.com/Matt711)

URL: https://github.com/rapidsai/cudf/pull/20704

python/cudf/cudf/datasets.py
@@ -66,7 +66,7 @@ def timeseries(
     gdf = cudf.from_pandas(df)
     for col in gdf:
         mask = state.choice(
-            [True, False],
+            [False, True],
             size=len(index),
             p=[1 - nulls_frequency, nulls_frequency],
         )

python/cudf/cudf/pandas/scripts/conftest-patch.py
@@ -859,10 +859,8 @@ NODEIDS_THAT_FAIL_WITH_CUDF_PANDAS = {
     "tests/arrays/string_/test_string.py::test_repr[string=str[pyarrow]]",
     "tests/arrays/string_/test_string.py::test_repr[string=str[python]]",
     "tests/arrays/string_/test_string.py::test_to_numpy_returns_pdna_default[pyarrow_numpy]",
-    "tests/arrays/string_/test_string.py::test_value_counts_sort_false[string=string[pyarrow]]",
     "tests/arrays/string_/test_string.py::test_value_counts_with_normalize[pyarrow]",
     "tests/arrays/string_/test_string.py::test_value_counts_with_normalize[python]",
-    "tests/arrays/string_/test_string.py::test_value_counts_with_normalize[string=string[pyarrow]]",
     "tests/arrays/test_array.py::test_array_inference[data7-expected7]",
     "tests/arrays/test_datetimelike.py::TestDatetimeArray::test_array_i8_dtype['+01:15'-B]",
     "tests/arrays/test_datetimelike.py::TestDatetimeArray::test_array_i8_dtype['+01:15'-D]",
@@ -2559,18 +2557,6 @@ NODEIDS_THAT_FAIL_WITH_CUDF_PANDAS = {
     "tests/extension/test_string.py::TestStringArray::test_compare_array[pyarrow_numpy-True-le]",
     "tests/extension/test_string.py::TestStringArray::test_compare_array[pyarrow_numpy-True-lt]",
     "tests/extension/test_string.py::TestStringArray::test_compare_array[pyarrow_numpy-True-ne]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-False-eq]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-False-ge]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-False-gt]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-False-le]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-False-lt]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-False-ne]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-True-eq]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-True-ge]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-True-gt]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-True-le]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-True-lt]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_array[string=string[pyarrow]-True-ne]",
     "tests/extension/test_string.py::TestStringArray::test_compare_scalar[pyarrow-False-eq]",
     "tests/extension/test_string.py::TestStringArray::test_compare_scalar[pyarrow-False-ge]",
     "tests/extension/test_string.py::TestStringArray::test_compare_scalar[pyarrow-False-gt]",
@@ -2595,18 +2581,6 @@ NODEIDS_THAT_FAIL_WITH_CUDF_PANDAS = {
     "tests/extension/test_string.py::TestStringArray::test_compare_scalar[pyarrow_numpy-True-le]",
     "tests/extension/test_string.py::TestStringArray::test_compare_scalar[pyarrow_numpy-True-lt]",
     "tests/extension/test_string.py::TestStringArray::test_compare_scalar[pyarrow_numpy-True-ne]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-False-eq]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-False-ge]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-False-gt]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-False-le]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-False-lt]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-False-ne]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-True-eq]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-True-ge]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-True-gt]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-True-le]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-True-lt]",
-    "tests/extension/test_string.py::TestStringArray::test_compare_scalar[string=string[pyarrow]-True-ne]",
     "tests/extension/test_string.py::TestStringArray::test_container_shift[pyarrow-False--2-indices0-False]",
     "tests/extension/test_string.py::TestStringArray::test_container_shift[pyarrow-False--2-indices0-True]",
     "tests/extension/test_string.py::TestStringArray::test_container_shift[pyarrow-False-0-indices1-False]",
@@ -2727,8 +2701,6 @@ NODEIDS_THAT_FAIL_WITH_CUDF_PANDAS = {
     "tests/extension/test_string.py::TestStringArray::test_value_counts_with_normalize[pyarrow-True]",
     "tests/extension/test_string.py::TestStringArray::test_value_counts_with_normalize[python-False]",
     "tests/extension/test_string.py::TestStringArray::test_value_counts_with_normalize[python-True]",
-    "tests/extension/test_string.py::TestStringArray::test_value_counts_with_normalize[string=string[pyarrow]-False]",
-    "tests/extension/test_string.py::TestStringArray::test_value_counts_with_normalize[string=string[pyarrow]-True]",
     "tests/frame/constructors/test_from_records.py::TestFromRecords::test_from_records_misc_brokenness3",
     "tests/frame/constructors/test_from_records.py::TestFromRecords::test_from_records_with_datetimes",
     "tests/frame/indexing/test_getitem.py::TestGetitemBooleanMask::test_getitem_returns_view_when_column_is_unique_in_df",
@@ -7818,7 +7790,6 @@ NODEIDS_THAT_FAIL_WITH_CUDF_PANDAS = {
     "tests/series/test_logical_ops.py::TestSeriesLogicalOps::test_logical_operators_int_dtype_with_bool_dtype_and_reindex",
     "tests/series/test_logical_ops.py::TestSeriesLogicalOps::test_logical_ops_df_compat",
     "tests/series/test_logical_ops.py::TestSeriesLogicalOps::test_logical_ops_label_based",
-    "tests/series/test_logical_ops.py::TestSeriesLogicalOps::test_pyarrow_numpy_string_invalid",
     "tests/series/test_logical_ops.py::TestSeriesLogicalOps::test_reversed_xor_with_index_returns_series",
     "tests/series/test_missing.py::TestSeriesMissingData::test_timedelta64_nan",
     "tests/series/test_missing.py::test_hasnans_uncached_for_series",
@@ -7906,6 +7877,7 @@ NODEIDS_THAT_FAIL_WITH_CUDF_PANDAS = {
     "tests/strings/test_extract.py::test_extract_series[string[python]-series_name]",
     "tests/strings/test_find_replace.py::test_contains[string=str[pyarrow]]",
     "tests/strings/test_find_replace.py::test_contains[string=str[python]]",
+    "tests/strings/test_find_replace.py::test_contains[string=string[pyarrow]]",
     "tests/strings/test_find_replace.py::test_contains_moar[string=str[pyarrow]]",
     "tests/strings/test_find_replace.py::test_contains_moar[string=str[python]]",
     "tests/strings/test_find_replace.py::test_contains_nan[string=str[pyarrow]]",
@@ -7941,6 +7913,13 @@ NODEIDS_THAT_FAIL_WITH_CUDF_PANDAS = {
     "tests/strings/test_find_replace.py::test_replace_not_case_sensitive_not_regex[string[pyarrow_numpy]]",
     "tests/strings/test_find_replace.py::test_replace_unicode[string[pyarrow]]",
     "tests/strings/test_find_replace.py::test_replace_unicode[string[pyarrow_numpy]]",
+    "tests/strings/test_find_replace.py::test_contains_na_kwarg_for_nullable_string_dtype[string[pyarrow]-False-nan-expected5]",
+    "tests/strings/test_find_replace.py::test_findall[string=string[pyarrow]]",
+    "tests/strings/test_split_partition.py::test_split_more_than_one_char[string=string[pyarrow]-split]",
+    "tests/strings/test_split_partition.py::test_split_more_regex_split[string=string[pyarrow]]",
+    "tests/strings/test_split_partition.py::test_split_regex[string=string[pyarrow]]",
+    "tests/strings/test_split_partition.py::test_split_regex_explicit[string=string[pyarrow]]",
+    "tests/strings/test_string_array.py::test_string_array[string[pyarrow]-findall]",
     "tests/strings/test_split_partition.py::test_partition_series_stdlib[string=object-partition]",
     "tests/strings/test_split_partition.py::test_partition_series_stdlib[string=object-rpartition]",
     "tests/strings/test_split_partition.py::test_partition_series_stdlib[string=str[pyarrow]-partition]",