06c2e4e583e0c5fee2ee0c0e8c4eb415f48ac848
Benjamin Zaitlen <quasiben@users.noreply.github.com>
2025-11-21 20:29:52 -0500

    Add configuration option for max-io-threads (#20606)
    Authors:
      - Benjamin Zaitlen (https://github.com/quasiben)
      - Richard (Rick) Zamora (https://github.com/rjzamora)
    
    Approvers:
      - Richard (Rick) Zamora (https://github.com/rjzamora)
      - Matthew Murray (https://github.com/Matt711)
      - Tom Augspurger (https://github.com/TomAugspurger)
    
    URL: https://github.com/rapidsai/cudf/pull/20606

--- python/cudf_polars/tests/test_config.py
+++ python/cudf_polars/tests/test_config.py
@@ -382,9 +382,10 @@ def test_validate_shuffle_method_defaults(
         "rapidsmpf_spill",
         "sink_to_directory",
         "client_device_threshold",
+        "max_io_threads",
     ],
 )
-def test_validate_max_rows_per_partition(option: str) -> None:
+def test_validate_streaming_executor_options(option: str) -> None:
     with pytest.raises(TypeError, match=f"{option} must be"):
         ConfigOptions.from_polars_engine(
             pl.GPUEngine(
