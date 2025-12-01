1e09a6b6591fb55bcbe9157921e192a68b64329e
Nghia Truong <nghiat@nvidia.com>
2025-12-01 15:16:06 -0800

    Fix copyright year
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- cpp/tests/groupby/rank_scan_tests.cpp
+++ cpp/tests/groupby/rank_scan_tests.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2021-2024, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2025, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 
