03228782059282de355a7ada358293f1505090c1
Richard (Rick) Zamora <rzamora217@gmail.com>
2025-12-01 09:22:54 -0600

    Revert "Simplify broadcast-join algorithm in cudf-polars (#20724)"
    This reverts commit 67d4a41dda7cee02bddab249bc5fbe0b2ec44903.

--- python/cudf_polars/cudf_polars/experimental/rapidsmpf/join.py
+++ python/cudf_polars/cudf_polars/experimental/rapidsmpf/join.py
@@ -149,11 +149,11 @@ async def broadcast_join_node(
         )
         await ch_out.send_metadata(context, output_metadata)
 
-        # Collect small-side
-        small_df = _concat(
-            *await get_small_table(context, small_child, small_ch),
-            context=ir_context,
-        )
+        # Collect small-side chunks
+        small_dfs = await get_small_table(context, small_child, small_ch)
+        if ir.options[0] != "Inner":
+            # TODO: Use local repartitioning for non-inner joins
+            small_dfs = [_concat(*small_dfs, context=ir_context)]
 
         # Stream through large side, joining with the small-side
         while (msg := await large_ch.data.recv(context)) is not None:
@@ -169,14 +169,22 @@ async def broadcast_join_node(
             )
 
             # Perform the join
-            df = await asyncio.to_thread(
-                ir.do_evaluate,
-                *ir._non_child_args,
-                *(
-                    [large_df, small_df]
-                    if broadcast_side == "right"
-                    else [small_df, large_df]
-                ),
+            df = _concat(
+                *[
+                    (
+                        await asyncio.to_thread(
+                            ir.do_evaluate,
+                            *ir._non_child_args,
+                            *(
+                                [large_df, small_df]
+                                if broadcast_side == "right"
+                                else [small_df, large_df]
+                            ),
+                            context=ir_context,
+                        )
+                    )
+                    for small_df in small_dfs
+                ],
                 context=ir_context,
             )
 
