da46b2772aa5586b9175ae0a09e478be2dca7d38
Bradley Dice <bdice@bradleydice.com>
2025-12-02 13:52:45 -0600

    Test with rapids-cmake updates

--- cmake/rapids_config.cmake
+++ cmake/rapids_config.cmake
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 # =============================================================================
+set(rapids-cmake-repo "bdice/rapids-cmake")
+set(rapids-cmake-branch "nvcomp-5.1")
 file(READ "${CMAKE_CURRENT_LIST_DIR}/../VERSION" _rapids_version)
 if(_rapids_version MATCHES [[^([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9])]])
   set(RAPIDS_VERSION_MAJOR "${CMAKE_MATCH_1}")
