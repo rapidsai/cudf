e210317a17a2b7c8ccd22cd2c7ea6f8c176a8051
Matthew Murray <matthewmurray711@gmail.com>
2025-12-02 15:03:17 -0500

    update copyright header

--- ci/utils/get_device_and_worker_count.py
+++ ci/utils/get_device_and_worker_count.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-License-Identifier: Apache-2.0
 
 from __future__ import annotations
 
