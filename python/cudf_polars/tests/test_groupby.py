834d9a6b9cf98d3a812ef26b79967845fdac30ce
Matt711 <matthewmurray711@gmail.com>
2025-12-05 17:25:40 -0500

    update groupby test

--- python/cudf_polars/tests/test_groupby.py
+++ python/cudf_polars/tests/test_groupby.py
@@ -15,7 +15,7 @@ from cudf_polars.testing.asserts import (
     assert_gpu_result_equal,
     assert_ir_translation_raises,
 )
-from cudf_polars.utils.versions import POLARS_VERSION_LT_132, POLARS_VERSION_LT_1321
+from cudf_polars.utils.versions import POLARS_VERSION_LT_132
 
 
 @pytest.fixture
