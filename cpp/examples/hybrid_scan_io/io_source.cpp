4d2bec78edc50a0a937f4b56e1cf138e923ba803
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-12-09 15:09:39 -0500

    Add clarifying remarks. Cleanup

--- cpp/examples/hybrid_scan_io/io_source.cpp
+++ cpp/examples/hybrid_scan_io/io_source.cpp
@@ -16,7 +16,6 @@
 #include <thrust/host_vector.h>
 
 #include <cstdint>
-#include <fstream>
 #include <string>
 
 rmm::host_async_resource_ref pinned_memory_resource()
@@ -56,11 +55,40 @@ cudf::host_span<uint8_t const> io_source::get_host_buffer_span() const
 }
 
 namespace {
+/**
+ * @brief A concept for containers with resizing capability.
+ *
+ * This concept requires the container to support:
+ * - `data()`: Returns a pointer to the underlying contiguous storage
+ * - `resize(n)`: Resizes the container to hold `n` elements
+ * - `size()`: Returns the current number of elements
+ *
+ * Satisfied by standard containers such as `std::vector` and `std::string`.
+ *
+ * @tparam T The container type to check.
+ */
 template <typename T>
+concept ResizableContainer = requires(T& container, std::size_t n) {
+  { container.data() };
+  { container.resize(n) } -> std::same_as<void>;
+  { container.size() } -> std::same_as<std::size_t>;
+};
+
+/**
+ * @brief Create a `cudf::io::source_info` object using the container as the underlying storage. The
+ * container must initially be empty, and this function will resize it to hold the file data.
+ *
+ * @tparam T Container type that must satisfy the `ResizableContainer` concept
+ * @param file_path Path of the file to be read.
+ * @param container Container that will be resized to hold the file data
+ * @return A `cudf::io::source_info` object
+ */
+template <ResizableContainer T>
 cudf::io::source_info generate_source_info_from_file(std::string const& file_path, T&& container)
 {
   CUDF_EXPECTS(container.size() == 0, "Container must have an initial size of 0");
 
+  // Use KvikIO to read the file efficiently
   auto kvikio_datasource = cudf::io::datasource::create(file_path);
   std::size_t const offset{0};
   auto const file_size = kvikio_datasource->size();
@@ -70,6 +98,7 @@ cudf::io::source_info generate_source_info_from_file(std::string const& file_pat
     kvikio_datasource->host_read(offset, file_size, reinterpret_cast<uint8_t*>(container.data()));
   CUDF_EXPECTS(num_bytes_read == file_size, "Failed to read expected number of bytes");
 
+  // Create a `source_info` object from the container data
   auto source_info = cudf::io::source_info(cudf::host_span<std::byte const>(
     reinterpret_cast<std::byte const*>(container.data()), file_size));
   return source_info;
@@ -81,7 +110,7 @@ io_source::io_source(std::string_view file_path, io_source_type type, rmm::cuda_
 {
   std::string const file_name{file_path};
 
-  // Copy file contents to the specified io source buffer
+  // Copy file contents to the specified io source buffer for HOST_BUFFER and PINNED_BUFFER
   switch (type) {
     case io_source_type::HOST_BUFFER: {
       _source_info = generate_source_info_from_file(file_name, _h_buffer);
