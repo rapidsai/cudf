dd129491d2e788ef6cc13887e7e80c4040c39951
Yunsong Wang <yunsongw@nvidia.com>
2025-12-10 16:48:02 -0800

    Test cuco HLL fix

--- cmake/rapids_config.cmake
+++ cmake/rapids_config.cmake
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 # =============================================================================
+
+set(rapids-cmake-repo PointKernel/rapids-cmake)
+set(rapids-cmake-branch fetch-cuco-hll-fix)
+
 file(READ "${CMAKE_CURRENT_LIST_DIR}/../VERSION" _rapids_version)
 if(_rapids_version MATCHES [[^([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9])]])
   set(RAPIDS_VERSION_MAJOR "${CMAKE_MATCH_1}")
