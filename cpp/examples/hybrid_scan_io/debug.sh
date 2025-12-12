4e2bb9792efa9cdfacc8113aa4847562f5752e16
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-12-12 15:29:15 -0500

    Remove debug file. Cleanup

--- cpp/examples/hybrid_scan_io/debug.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-
-# gdb --ex start --args ./build/hybrid_scan_io example.parquet string_col 0000001 PINNED_BUFFER
-./build/hybrid_scan_io example.parquet string_col 0000001 FILEPATH
-# ./build/hybrid_scan_io example.parquet string_col 0000001 HOST_BUFFER
-# ./build/hybrid_scan_io example.parquet string_col 0000001 PINNED_BUFFER
