f675162ceaf5de307a8a77280096c1405af0450c
Tom Augspurger <toaugspurger@nvidia.com>
2025-12-18 13:21:49 -0800

    Different seaborn approach

--- python/cudf/cudf_pandas_tests/third_party_integration_tests/tests/test_seaborn.py
+++ python/cudf/cudf_pandas_tests/third_party_integration_tests/tests/test_seaborn.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
-import io
 
 import pandas as pd
 import pytest
@@ -10,13 +9,6 @@ import seaborn as sns
 def assert_plots_equal(expect: bytes, got: bytes):
     # these are the PNGs that we saved in-memory.
     assert expect == got
-
-
-def _save_figure(ax) -> bytes:
-    buf = io.BytesIO()
-    ax.get_figure().savefig(buf, format="png")
-    buf.seek(0)
-    return buf.getvalue()
 
 
 pytestmark = pytest.mark.assert_eq(fn=assert_plots_equal)
@@ -36,15 +28,20 @@ def df():
 
 def test_bar(df):
     ax = sns.barplot(data=df, x="x", y="y")
-    return _save_figure(ax)
+    return [x.get_height() for x in ax.patches]
 
 
 def test_scatter(df):
     ax = sns.scatterplot(data=df, x="x", y="y", hue="hue")
-    return _save_figure(ax)
+    assert len(ax.collections) == 1
+    paths = ax.collections[0].get_paths()
+    assert len(paths) == 1
+    return paths[0].vertices.tolist()
 
 
 def test_lineplot_with_sns_data():
     df = sns.load_dataset("flights")
     ax = sns.lineplot(data=df, x="month", y="passengers")
-    return _save_figure(ax)
+    paths = ax.collections[0].get_paths()
+    assert len(paths) == 1
+    return paths[0].vertices.tolist()
