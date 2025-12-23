f283a95dd1928e08c30fc893c8361ebc9e690d3b
Vyas Ramasubramani <vyasr@nvidia.com>
2025-12-23 08:07:57 -0800

    Push more arrow conversion logic down to pylibcudf (#20919)
    The various column subtypes have legacy arrow conversion logic that can largely be replaced by using pylibcudf directly. The main challenge is that pandas<->pyarrow conversions have different semantics around all-null arrays and nullability than libcudf, so we need to insert a recursive post-order traversal of nested types that replaces the results.
    
    Authors:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Matthew Roeschke (https://github.com/mroeschke)
    
    URL: https://github.com/rapidsai/cudf/pull/20919

--- python/cudf/cudf/core/column/lists.py
+++ python/cudf/cudf/core/column/lists.py
@@ -175,28 +175,6 @@ class ListColumn(ColumnBase):
         """
         return cast(NumericalColumn, self.children[0])
 
-    def to_arrow(self) -> pa.Array:
-        offsets = self.offsets.to_arrow()
-        elements = (
-            pa.nulls(len(self.elements))
-            if len(self.elements) == self.elements.null_count
-            else self.elements.to_arrow()
-        )
-        pa_type = pa.list_(elements.type)
-
-        if self.nullable:
-            nbuf = pa.py_buffer(self.mask.memoryview())  # type: ignore[union-attr]
-            buffers = [nbuf, offsets.buffers()[1]]
-        else:
-            buffers = list(offsets.buffers())
-        return pa.ListArray.from_buffers(
-            pa_type,
-            len(self),
-            # PyArrow stubs are too strict - from_buffers should accept None for missing buffers
-            buffers,  # type: ignore[arg-type]
-            children=[elements],
-        )
-
     @property
     def __cuda_array_interface__(self) -> Mapping[str, Any]:
         raise NotImplementedError(
