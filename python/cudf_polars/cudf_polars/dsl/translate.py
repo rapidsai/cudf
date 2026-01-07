ad2ac429487db1904ce298eab9917ee0090644d3
Matthew Murray <matthewmurray711@gmail.com>
2026-01-07 13:21:19 +0000

    pre-commit check

--- python/cudf_polars/cudf_polars/dsl/translate.py
+++ python/cudf_polars/cudf_polars/dsl/translate.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
 
 """Translate polars IR representation to ours."""
