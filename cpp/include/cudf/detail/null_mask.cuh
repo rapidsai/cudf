7589554d2930718c21ca18ea14cb5b41fb261b12
Bradley Dice <bdice@bradleydice.com>
2026-01-07 00:32:51 +0000

    Update copyrights

--- cpp/include/cudf/detail/null_mask.cuh
+++ cpp/include/cudf/detail/null_mask.cuh
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2021-2025, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2021-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
--- cpp/src/replace/nans.cu
+++ cpp/src/replace/nans.cu
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2020-2025, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2020-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 #include <cudf/column/column.hpp>
