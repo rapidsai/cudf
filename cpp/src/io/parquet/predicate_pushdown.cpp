61303ccd6001417c724a0a208ac76db5ed149870
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2026-01-09 20:42:13 +0000

    copyright year fix please

--- cpp/src/io/parquet/predicate_pushdown.cpp
+++ cpp/src/io/parquet/predicate_pushdown.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "reader_impl_helpers.hpp"
