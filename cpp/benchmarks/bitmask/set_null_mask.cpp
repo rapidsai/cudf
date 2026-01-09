afd8842b01f5207d3e0d7700357685efbadb484b
David Wendt <dwendt@nvidia.com>
2026-01-09 11:54:52 -0500

    remove fake error

--- cpp/benchmarks/bitmask/set_null_mask.cpp
+++ cpp/benchmarks/bitmask/set_null_mask.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2020-2026, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2020-2025, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -9,7 +9,6 @@
 #include <nvbench/nvbench.cuh>
 
 #include <random>
-#include <stdexcept>
 
 namespace {
 
