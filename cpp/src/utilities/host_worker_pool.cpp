76f5905903386442223ef9dff4997aeb510e3288
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2026-01-16 16:45:39 -0800

    explicit cast
    Co-authored-by: Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>

--- cpp/src/utilities/host_worker_pool.cpp
+++ cpp/src/utilities/host_worker_pool.cpp
@@ -35,7 +35,7 @@ std::shared_mutex g_pools_mutex;
 [[nodiscard]] std::size_t pool_size()
 {
   static std::size_t const default_pool_size =
-    std::min(16u, std::thread::hardware_concurrency() / 4);
+    std::min<std::size_t>(16, std::thread::hardware_concurrency() / 4);
   return getenv_or("LIBCUDF_NUM_HOST_WORKERS", default_pool_size);
 }
 
