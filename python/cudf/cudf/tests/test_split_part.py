6ad0baef8a3b1e4fc3b9c7a01189fba309b25f19
Umang <umang2001umang@gmail.com>
2026-01-19 19:38:32 +0530

    Update python/cudf/cudf/tests/test_split_part.py
    Co-authored-by: David Wendt <45795991+davidwendt@users.noreply.github.com>

--- python/cudf/cudf/tests/test_split_part.py
+++ python/cudf/cudf/tests/test_split_part.py
@@ -1,4 +1,5 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES.
+
 # SPDX-License-Identifier: Apache-2.0
 
 import cudf
