e9f95dc2e235add9fdf2853d1b7ac93ded8d8c59
Umang-projects <umang2001umang@gmail.com>
2026-01-20 15:43:41 +0000

    STYLE: Fixed String.py

--- python/cudf/cudf/core/column/string.py
+++ python/cudf/cudf/core/column/string.py
@@ -1224,25 +1224,26 @@ class StringColumn(ColumnBase, Scannable):
 
     def rsplit(self, delimiter: plc.Scalar, maxsplit: int) -> dict[int, Self]:
         return self._split(delimiter, maxsplit, plc.strings.split.split.rsplit)
-
+    
     def split_part(self, delimiter: str | None = None, index: int = 0) -> Self:
-        if delimiter is None:
-            delimiter = ""
+        with self.access(mode="read", scope="internal"):
+            if delimiter is None:
+                delimiter = ""
+            
+            delimiter_scalar = pa_scalar_to_plc_scalar(
+                pa.scalar(delimiter, type=pa.string())
+            )
+            
+            plc_column = plc_split.split_part(
+                self.plc_column, 
+                delimiter_scalar, 
+                index
+            )
+            return cast(
+                Self,
+                type(self).from_pylibcudf(plc_column)._with_type_metadata(self.dtype)
+            )
 
-        input_col = self.to_pylibcudf(mode="read")
-
-        delim_scalar = pa_scalar_to_plc_scalar(
-            pa.scalar(delimiter, type=pa.string())
-        )
-
-        plc_column = plc_split.split_part(input_col, delim_scalar, index)
-
-        return cast(
-            Self,
-            type(self)
-            .from_pylibcudf(plc_column)
-            ._with_type_metadata(self.dtype),
-        )
 
     def _partition(
         self,
