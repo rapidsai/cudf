edff74ddf51ad69d23ccbe27c5e202161c583f41
David Wendt <dwendt@nvidia.com>
2026-01-20 17:46:39 -0500

    first set of fix for set_keys and match

--- cpp/src/dictionary/set_keys.cu
+++ cpp/src/dictionary/set_keys.cu
@@ -12,6 +12,7 @@
 #include <cudf/detail/nvtx/ranges.hpp>
 #include <cudf/detail/row_operator/equality.cuh>
 #include <cudf/detail/row_operator/hashing.cuh>
+#include <cudf/detail/sorting.hpp>
 #include <cudf/detail/valid_if.cuh>
 #include <cudf/dictionary/detail/encode.hpp>
 #include <cudf/dictionary/detail/iterator.cuh>
@@ -24,6 +25,7 @@
 #include <cudf/utilities/memory_resource.hpp>
 #include <cudf/utilities/traits.hpp>
 #include <cudf/utilities/type_checks.hpp>
+#include <cudf/utilities/type_dispatcher.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/exec_policy.hpp>
@@ -46,6 +48,118 @@ namespace dictionary {
 namespace detail {
 namespace {
 
+template <typename T, typename Iterator>
+struct create_indices_map_fn {
+  cudf::column_device_view d_old_keys;
+  Iterator d_begin;
+  Iterator d_end;
+  cudf::size_type* d_map;
+
+  __device__ void operator()(size_type idx)
+  {
+    auto key   = d_old_keys.element<T>(idx);
+    auto itr   = thrust::lower_bound(thrust::seq, d_begin, d_end, key);
+    d_map[idx] = (itr != d_end && key == *itr)
+                   ? static_cast<cudf::size_type>(cuda::std::distance(d_begin, itr))
+                   : -1;
+  }
+};
+
+struct apply_indices_map_fn {
+  cudf::column_device_view d_input;
+  cudf::size_type const* d_map;
+  cudf::detail::output_indexalator d_output;
+
+  __device__ void operator()(cudf::size_type idx)
+  {
+    if (d_input.is_null(idx)) {
+      d_output[idx] = -1;
+      return;
+    }
+    auto const indices = d_input.child(cudf::dictionary_column_view::indices_column_index);
+    auto indices_itr =
+      cudf::detail::input_indexalator(indices.head(), indices.type(), d_input.offset());
+    auto old_idx  = indices_itr[idx];
+    d_output[idx] = d_map[old_idx];
+  }
+};
+
+struct set_keys_dispatch_fn {
+  template <typename T>
+  std::unique_ptr<cudf::column> operator()(cudf::dictionary_column_view const& input,
+                                           cudf::column_view const& new_keys,
+                                           rmm::cuda_stream_view stream,
+                                           rmm::device_async_resource_ref mr)
+    requires(cudf::is_relationally_comparable<T, T>())
+  {
+    auto tmr = cudf::get_current_device_resource_ref();  // temporary mr
+    // temporarily sort the new keys so they can be searched more quickly
+    auto sorted_indices = cudf::detail::sorted_order(table_view({new_keys}), {}, {}, stream, tmr);
+
+    auto d_old_keys       = column_device_view::create(input.keys(), stream);
+    auto d_new_keys       = column_device_view::create(new_keys, stream);
+    auto d_sorted_indices = column_device_view::create(sorted_indices->view(), stream);
+    auto keys_itr         = thrust::make_permutation_iterator(d_new_keys->begin<T>(),
+                                                      d_sorted_indices->begin<size_type>());
+    auto zero             = thrust::make_counting_iterator<cudf::size_type>(0);
+
+    // create a map from the old key indices to the new ones
+    auto indices_map = rmm::device_uvector<size_type>(input.keys().size(), stream);
+    create_indices_map_fn<T, decltype(keys_itr)> map_fn{
+      *d_old_keys, keys_itr, keys_itr + new_keys.size(), indices_map.data()};
+    thrust::for_each(rmm::exec_policy_nosync(stream), zero, zero + input.keys().size(), map_fn);
+
+    // build the indices column using the same type as the input
+    auto indices_column = cudf::make_numeric_column(
+      input.indices().type(), input.size(), cudf::mask_state::UNALLOCATED, stream, mr);
+    auto d_new_indices =
+      cudf::detail::indexalator_factory::make_output_iterator(indices_column->mutable_view());
+
+    auto d_input = cudf::column_device_view::create(input.parent(), stream);
+    apply_indices_map_fn apply_fn{*d_input, indices_map.data(), d_new_indices};
+    thrust::for_each(rmm::exec_policy_nosync(stream), zero, zero + input.size(), apply_fn);
+    auto d_indices = cudf::detail::indexalator_factory::make_input_iterator(indices_column->view());
+
+    // compute the new nulls (any indices < 0)
+    auto [null_mask, null_count] = cudf::detail::valid_if(
+      zero,
+      zero + input.size(),
+      [d_input = *d_input, d_indices] __device__(size_type idx) { return d_indices[idx] >= 0; },
+      stream,
+      mr);
+
+    auto keys_column = std::make_unique<cudf::column>(new_keys, stream, mr);
+    return make_dictionary_column(
+      std::move(keys_column), std::move(indices_column), std::move(null_mask), null_count);
+  }
+
+  template <typename T>
+  std::unique_ptr<cudf::column> operator()(cudf::dictionary_column_view const&,
+                                           cudf::column_view const&,
+                                           rmm::cuda_stream_view,
+                                           rmm::device_async_resource_ref)
+    requires(not cudf::is_relationally_comparable<T, T>())
+  {
+    CUDF_UNREACHABLE("not a valid dictionary key type");
+  }
+};
+}  // namespace
+
+std::unique_ptr<column> set_keys(dictionary_column_view const& input,
+                                 column_view const& new_keys,
+                                 rmm::cuda_stream_view stream,
+                                 rmm::device_async_resource_ref mr)
+{
+  CUDF_EXPECTS(!new_keys.has_nulls(), "keys parameter must not have nulls", std::invalid_argument);
+  CUDF_EXPECTS(!new_keys.is_empty(), "keys cannot be empty", std::invalid_argument);
+  CUDF_EXPECTS(
+    cudf::have_same_types(input.keys(), new_keys), "keys types must match", cudf::data_type_error);
+
+  return type_dispatcher<dispatch_storage_type>(
+    new_keys.type(), set_keys_dispatch_fn{}, input, new_keys, stream, mr);
+}
+
+namespace {
 template <typename T>
 using hasher_type = cudf::hashing::detail::MurmurHash3_x86_32<T>;
 template <typename T>
@@ -83,127 +197,67 @@ struct set_keys_equal {
   }
 };
 
-template <typename T, typename SetRef>
-CUDF_KERNEL void set_keys_kernel(SetRef set_ref, column_device_view d_input, size_type* d_indices)
-{
-  auto const idx = cudf::detail::grid_1d::global_thread_id();
-  if (idx < d_input.size() && d_input.is_valid(idx)) {
-    auto const elem = d_input.child(1).element<T>(d_input.element<dictionary32>(idx).value());
-    auto const look = cuda::std::pair<bool, T>{true, elem};
-    auto const slot = set_ref.find(look);
-    d_indices[idx]  = slot != set_ref.end() ? *slot : -1;
-  }
-}
-
-struct set_keys_dispatch_fn {
+struct unique_keys_dispatch_fn {
   template <typename T>
-  std::unique_ptr<column> operator()(dictionary_column_view const& input,
-                                     column_view const& new_keys,
-                                     rmm::cuda_stream_view stream,
-                                     rmm::device_async_resource_ref mr)
+  std::unique_ptr<cudf::column> operator()(cudf::column_view const& all_keys,
+                                           rmm::cuda_stream_view stream,
+                                           rmm::device_async_resource_ref mr)
     requires(cudf::is_relationally_comparable<T, T>())
   {
-    auto d_keys    = column_device_view::create(new_keys, stream);
-    auto empty_key = cuco::empty_key{cudf::detail::CUDF_SIZE_TYPE_SENTINEL};
-    auto d_equal   = set_keys_equal<T>{*d_keys};
-    auto probe     = cuco::linear_probing<1, set_keys_hasher<T>>{set_keys_hasher<T>{*d_keys}};
-    auto allocator = rmm::mr::polymorphic_allocator<char>{};
-    auto set       = cuco::static_set{
-      new_keys.size(), 0.5, empty_key, d_equal, probe, {}, {}, allocator, stream.value()};
+    auto d_all_keys = column_device_view::create(all_keys, stream);
+    auto empty_key  = cuco::empty_key{cudf::detail::CUDF_SIZE_TYPE_SENTINEL};
+    auto d_equal    = set_keys_equal<T>{*d_all_keys};
+    auto probe      = cuco::linear_probing<1, set_keys_hasher<T>>{set_keys_hasher<T>{*d_all_keys}};
+    auto allocator  = rmm::mr::polymorphic_allocator<char>{};
+    auto set        = cuco::static_set{
+      all_keys.size(), 0.5, empty_key, d_equal, probe, {}, {}, allocator, stream.value()};
 
     auto const iter = thrust::counting_iterator<cudf::size_type>{0};
-    set.insert_async(iter, iter + new_keys.size(), stream.value());
+    set.insert_async(iter, iter + all_keys.size(), stream.value());
 
-    auto keys_indices = rmm::device_uvector<size_type>(new_keys.size(), stream);
+    auto keys_indices = rmm::device_uvector<size_type>(all_keys.size(), stream);
     auto keys_end     = set.retrieve_all(keys_indices.begin(), stream.value());
     keys_indices.resize(cuda::std::distance(keys_indices.begin(), keys_end), stream);
-
-    auto set_ref    = set.ref(cuco::find);
-    using set_ref_t = decltype(set_ref);
-
-    // resolve the new indices by using the heterogenous set interface
-    auto d_indices = rmm::device_uvector<size_type>(input.size(), stream);
-    auto d_input   = column_device_view::create(input.parent(), stream);
-    auto grid      = cudf::detail::grid_1d(input.size(), 256);
-    set_keys_kernel<T, set_ref_t>
-      <<<grid.num_blocks, grid.num_threads_per_block, 0, stream.value()>>>(
-        set_ref, *d_input, d_indices.data());
-
-    // compute the new nulls (existing nulls and any indices < 0)
-    auto [null_mask, null_count] = cudf::detail::valid_if(
-      thrust::make_counting_iterator<size_type>(0),
-      thrust::make_counting_iterator<size_type>(input.size()),
-      [d_input = *d_input, d_indices = d_indices.data()] __device__(size_type idx) {
-        return (d_input.is_valid(idx) && d_indices[idx] >= 0);
-      },
-      stream,
-      mr);
-
-    // sort the keys_indices so we can use lower-bound on them
-    thrust::sort(rmm::exec_policy_nosync(stream), keys_indices.begin(), keys_indices.end());
 
     // gather the keys using the keys_indices
     auto const oob_policy   = cudf::out_of_bounds_policy::DONT_CHECK;
     auto const index_policy = cudf::detail::negative_index_policy::NOT_ALLOWED;
-    auto const keys_tv      = table_view({new_keys});
+    auto const keys_tv      = table_view({all_keys});
     auto keys_column =
       std::move(cudf::detail::gather(keys_tv, keys_indices, oob_policy, index_policy, stream, mr)
                   ->release()
                   .front());
-
-    // build the indices column using the same type as the input
-    auto indices_column = cudf::make_numeric_column(
-      input.indices().type(), input.size(), cudf::mask_state::UNALLOCATED, stream, mr);
-    auto d_result =
-      cudf::detail::indexalator_factory::make_output_iterator(indices_column->mutable_view());
-
-    // call lower-bound with keys_indices and d_indices to get the new indices values
-    auto const begin = keys_indices.begin();
-    auto const end   = keys_indices.end();
-    thrust::lower_bound(
-      rmm::exec_policy_nosync(stream), begin, end, d_indices.begin(), d_indices.end(), d_result);
-
-    // create column with keys_column and indices_column
-    return make_dictionary_column(
-      std::move(keys_column), std::move(indices_column), std::move(null_mask), null_count);
+    return keys_column;
   }
 
   template <typename T>
-  std::unique_ptr<column> operator()(dictionary_column_view const&,
-                                     column_view const&,
-                                     rmm::cuda_stream_view,
-                                     rmm::device_async_resource_ref)
+  std::unique_ptr<cudf::column> operator()(cudf::column_view const&,
+                                           rmm::cuda_stream_view,
+                                           rmm::device_async_resource_ref)
     requires(not cudf::is_relationally_comparable<T, T>())
   {
-    CUDF_UNREACHABLE("keys are not a valid dictionary key type");
+    CUDF_UNREACHABLE("invalid dictionary key type");
   }
 };
-
 }  // namespace
-
-std::unique_ptr<column> set_keys(dictionary_column_view const& input,
-                                 column_view const& new_keys,
-                                 rmm::cuda_stream_view stream,
-                                 rmm::device_async_resource_ref mr)
-{
-  CUDF_EXPECTS(!new_keys.has_nulls(), "keys parameter must not have nulls", std::invalid_argument);
-  CUDF_EXPECTS(!new_keys.is_empty(), "keys cannot be empty", std::invalid_argument);
-  CUDF_EXPECTS(
-    cudf::have_same_types(input.keys(), new_keys), "keys types must match", cudf::data_type_error);
-
-  return type_dispatcher<dispatch_storage_type>(
-    new_keys.type(), set_keys_dispatch_fn{}, input, new_keys, stream, mr);
-}
 
 std::vector<std::unique_ptr<column>> match_dictionaries(
   cudf::host_span<dictionary_column_view const> input,
   rmm::cuda_stream_view stream,
   rmm::device_async_resource_ref mr)
 {
-  std::vector<column_view> keys(input.size());
+  CUDF_EXPECTS(not input.empty(), "expect at least one dictionary", std::invalid_argument);
+  // special case when input.size()==1 ?
+
+  auto tmr = cudf::get_current_device_resource_ref();
+  std::vector<cudf::column_view> keys(input.size());
   std::transform(input.begin(), input.end(), keys.begin(), [](auto& col) { return col.keys(); });
-  auto new_keys  = cudf::detail::concatenate(keys, stream, cudf::get_current_device_resource_ref());
+  auto all_keys = cudf::detail::concatenate(keys, stream, tmr);
+
+  auto new_keys = cudf::type_dispatcher(
+    keys.front().type(), unique_keys_dispatch_fn{}, all_keys->view(), stream, tmr);
   auto keys_view = new_keys->view();
+
   std::vector<std::unique_ptr<column>> result(input.size());
   std::transform(input.begin(), input.end(), result.begin(), [keys_view, mr, stream](auto& col) {
     return set_keys(col, keys_view, stream, mr);
