85115821b0257a61d74fe534940f0c959950addf
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2026-01-22 22:53:12 +0000

    The style fix

--- python/cudf/cudf/tests/input_output/test_parquet.py
+++ python/cudf/cudf/tests/input_output/test_parquet.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 import datetime
