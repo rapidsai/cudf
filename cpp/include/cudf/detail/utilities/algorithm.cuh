7dc37704d70c25fa954851542c6ec679b1a18986
David Wendt <dwendt@nvidia.com>
2026-01-23 14:11:08 -0500

    fixed wording; removed unneeded comments

--- cpp/include/cudf/detail/utilities/algorithm.cuh
+++ cpp/include/cudf/detail/utilities/algorithm.cuh
@@ -65,7 +65,7 @@ template <typename InputIterator,
  * @brief Helper to copy elements satisfying a predicate/stencil using CUB with pinned memory
  *
  * This function copies elements from the input range that satisfy the given predicate/stencil
- * to the output range, using CUB's DeviceSelect::If implementation with pinned memory
+ * to the output range, using CUB's DeviceSelect::FlaggedIf implementation with pinned memory
  * for efficient device-to-host transfer of the number of selected elements.
  *
  * @tparam InputIterator **[inferred]** Type of device-accessible input iterator
@@ -94,12 +94,10 @@ OutputIterator copy_if(InputIterator begin,
 {
   auto const num_items = cuda::std::distance(begin, end);
 
-  // Device scalar to store the number of selected elements
   auto num_selected =
-    cudf::detail::device_scalar<cuda::std::size_t>(stream, cudf::get_current_device_resource_ref());
+    cudf::detail::device_scalar<std::size_t>(stream, cudf::get_current_device_resource_ref());
 
-  // First call to get temporary storage size
-  size_t temp_storage_bytes = 0;
+  auto temp_storage_bytes = std::size_t{0};
   CUDF_CUDA_TRY(cub::DeviceSelect::FlaggedIf(nullptr,
                                              temp_storage_bytes,
                                              begin,
@@ -110,9 +108,8 @@ OutputIterator copy_if(InputIterator begin,
                                              predicate,
                                              stream.value()));
 
-  // Allocate temporary storage
-  rmm::device_buffer d_temp_storage(
-    temp_storage_bytes, stream, cudf::get_current_device_resource_ref());
+  auto d_temp_storage =
+    rmm::device_buffer(temp_storage_bytes, stream, cudf::get_current_device_resource_ref());
 
   CUDF_CUDA_TRY(cub::DeviceSelect::FlaggedIf(d_temp_storage.data(),
                                              temp_storage_bytes,
@@ -124,7 +121,6 @@ OutputIterator copy_if(InputIterator begin,
                                              predicate,
                                              stream.value()));
 
-  // Copy number of selected elements back to host via pinned memory
   return result + num_selected.value(stream);
 }
 
