03c7773e1ad7fe4940d82aa4091559591fb99c80
Matthew Murray <matthewmurray711@gmail.com>
2026-01-24 14:40:35 +0000

    xfail test and update xfail set

--- python/cudf/cudf/tests/series/test_unary.py
+++ python/cudf/cudf/tests/series/test_unary.py
@@ -6,8 +6,10 @@ from decimal import Decimal
 import numpy as np
 import pandas as pd
 import pyarrow as pa
+import pytest
 
 from cudf import Series, from_pandas, option_context
+from cudf.core._compat import PANDAS_GE_210
 from cudf.testing import assert_eq
 
 
@@ -45,7 +47,13 @@ def test_series_decimal_neg():
     assert_eq((-sr).to_pandas(), -psr, check_dtype=True)
 
 
-def test_series_invert_arrow_dtype():
+def test_series_invert_arrow_dtype(request):
+    request.applymarker(
+        pytest.mark.xfail(
+            condition=not PANDAS_GE_210,
+            reason="PyArrow-backed integer dtypes now support bitwise operations in Pandas 2.1 and above",
+        )
+    )
     ps = pd.Series(
         pa.array([1, 0, 1, 0, None, 2, 1, 2], type=pa.uint32()),
         dtype="uint32[pyarrow]",
