b18c175a847b16f4997632f7569c27b09fec8ee0
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2026-01-30 14:36:14 -0800

    Fix `pl.Expr.concat_str` with boolean columns in cudf_polars (#21145)
    closes https://github.com/pola-rs/polars/issues/26186
    
    Authors:
      - Matthew Roeschke (https://github.com/mroeschke)
      - Matthew Murray (https://github.com/Matt711)
    
    Approvers:
      - Matthew Murray (https://github.com/Matt711)
    
    URL: https://github.com/rapidsai/cudf/pull/21145

--- python/cudf_polars/cudf_polars/containers/column.py
+++ python/cudf_polars/cudf_polars/containers/column.py
@@ -10,6 +10,7 @@ from typing import TYPE_CHECKING
 from polars.exceptions import InvalidOperationError
 
 import pylibcudf as plc
+from pylibcudf.strings.convert.convert_booleans import from_booleans
 from pylibcudf.strings.convert.convert_floats import from_floats, is_float, to_floats
 from pylibcudf.strings.convert.convert_integers import (
     from_integers,
@@ -352,6 +353,13 @@ class Column:
                 return from_floats(self.obj, stream=stream)
             elif plc.traits.is_integral_not_bool(self.obj.type()):
                 return from_integers(self.obj, stream=stream)
+            elif plc.traits.is_boolean(self.obj.type()):
+                return from_booleans(
+                    self.obj,
+                    plc.Scalar.from_py("true", dtype, stream=stream),
+                    plc.Scalar.from_py("false", dtype, stream=stream),
+                    stream=stream,
+                )
             else:
                 raise InvalidOperationError(
                     f"Unsupported casting from {self.dtype.id()} to {dtype.id()}."
