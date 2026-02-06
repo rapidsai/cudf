a955a0462102c42f7513cec9f58307a9dfec2af8
Basit Ayantunde <rlamarrr@gmail.com>
2026-02-06 16:50:17 +0000

    Update copyright year in get_lz4.cmake and improve formatting

--- cpp/cmake/thirdparty/get_lz4.cmake
+++ cpp/cmake/thirdparty/get_lz4.cmake
@@ -1,6 +1,6 @@
 # =============================================================================
 # cmake-format: off
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 # =============================================================================
@@ -10,22 +10,20 @@ function(find_and_configure_lz4)
 
   set(CPM_DOWNLOAD_lz4 ON)
   rapids_cpm_find(
-    lz4
-    1.5.7
-    GLOBAL_TARGETS
-    lz4
+    lz4 1.5.7
+    GLOBAL_TARGETS lz4
     CPM_ARGS
-    GIT_REPOSITORY
-    https://github.com/lz4/lz4.git
-    GIT_TAG
-    446a35f
-    GIT_SHALLOW
-    TRUE)
+    GIT_REPOSITORY https://github.com/lz4/lz4.git
+    GIT_TAG 446a35f
+    GIT_SHALLOW TRUE
+  )
 
   if(lz4_ADDED)
-    add_library(lz4_static STATIC lz4file.c lz4frame.c lz4hc.c xxhash.c)
-    target_include_directories(lz4_static
-                               PUBLIC $<BUILD_INTERFACE:${lz4_SOURCE_DIR}/lib>)
+    add_library(
+      lz4_static STATIC ${lz4_SOURCE_DIR}/lib/lz4file.c ${lz4_SOURCE_DIR}/lib/lz4frame.c
+                        ${lz4_SOURCE_DIR}/lib/lz4hc.c ${lz4_SOURCE_DIR}/lib/xxhash.c
+    )
+    target_include_directories(lz4_static PUBLIC $<BUILD_INTERFACE:${lz4_SOURCE_DIR}/lib>)
 
     set_target_properties(lz4_static PROPERTIES POSITION_INDEPENDENT_CODE ON)
   endif()
