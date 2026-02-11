c06ffe41b489bd81bb425abf67947b435a5729db
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2026-02-10 17:49:16 -0800

    Allow np.dtype(str) to coerce to object type for cudf column construction (#21403)
    Fixes a bug seen in cuML where we were disallowing `np.dtype(str)` outright instead of coercing it to `np.dtype(object)` when _setting_ it as the result dtype metadata like pandas
    
    https://github.com/rapidsai/cuml/actions/runs/21872781515/job/63134572384?pr=7787
    
    I have added some failing tests to `conftest-patch.py`, mostly related to `astype`ing to/from strings compared to the pandas test suite. I don't anticipate that these failures will further impact cuml.
    
    Authors:
      - Matthew Roeschke (https://github.com/mroeschke)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/cudf/pull/21403

--- python/cudf/cudf/core/column/string.py
+++ python/cudf/cudf/core/column/string.py
@@ -118,7 +118,13 @@ class StringColumn(ColumnBase, Scannable):
     ) -> tuple[plc.Column, np.dtype]:
         plc_column, dtype = super()._validate_args(plc_column, dtype)
         if not is_dtype_obj_string(dtype):
-            raise ValueError("dtype must be a valid cuDF string dtype")
+            if dtype.kind == "U":
+                # User _passed_ e.g. np.dtype(str), but we store np.dtype(object) like pandas
+                # >>> pd.Series(["a"], dtype=np.dtype(str)).dtype
+                # dtype('O')
+                dtype = np.dtype(object)
+            else:
+                raise ValueError("dtype must be a valid cuDF string dtype")
         return plc_column, dtype
 
     @property
