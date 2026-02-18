a14d2390ae770128c6cd8ca3aee2c0a191836409
David Wendt <dwendt@nvidia.com>
2026-02-18 16:07:37 -0500

    fix pattern variable

--- java/src/main/native/src/ColumnViewJni.cpp
+++ java/src/main/native/src/ColumnViewJni.cpp
@@ -1684,7 +1684,7 @@ JNIEXPORT jlong JNICALL Java_ai_rapids_cudf_ColumnView_like(
     auto const column_view    = reinterpret_cast<cudf::column_view const*>(j_view_handle);
     auto const strings_column = cudf::strings_column_view{*column_view};
     auto const pattern_scalar = reinterpret_cast<cudf::string_scalar const*>(pattern);
-    auto const pattern_str    = pattern->to_string();
+    auto const pattern_str    = pattern_scalar->to_string();
     auto const escape_scalar  = reinterpret_cast<cudf::string_scalar const*>(escapeChar);
     return release_as_jlong(cudf::strings::like(strings_column, pattern_str, *escape_scalar));
   }
