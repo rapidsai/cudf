83edda541d611edec713da9f10dc09c61b0612ef
Vyas Ramasubramani <vyasr@nvidia.com>
2026-02-19 19:25:19 -0800

    Let Series normalize pylibcudf text

--- python/cudf/cudf/io/text.py
+++ python/cudf/cudf/io/text.py
@@ -5,7 +5,6 @@ from io import BytesIO, StringIO, TextIOBase
 
 import pylibcudf as plc
 
-from cudf.core.column.column import _normalize_types_column
 from cudf.core.series import Series
 from cudf.utils import ioutils
 from cudf.utils.performance_tracking import _performance_tracking
@@ -65,5 +64,4 @@ def read_text(
         byte_range=byte_range, strip_delimiters=strip_delimiters
     )
     plc_column = plc.io.text.multibyte_split(datasource, delimiter, options)
-    normalized = _normalize_types_column(plc_column)
-    return Series.from_pylibcudf(normalized)
+    return Series.from_pylibcudf(plc_column)
