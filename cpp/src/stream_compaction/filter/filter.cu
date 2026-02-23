5fedf2e8281dc721331aa65cef43b07a215ad3f8
Basit Ayantunde <rlamarrr@gmail.com>
2026-02-23 20:50:53 +0000

    code review changes

--- cpp/src/stream_compaction/filter/filter.cu
+++ cpp/src/stream_compaction/filter/filter.cu
@@ -22,7 +22,7 @@ namespace cudf {
 
 namespace detail {
 std::vector<std::unique_ptr<column>> filter(
-  std::vector<std::variant<column_view, scalar_column_view>> const& predicate_inputs,
+  std::span<std::variant<column_view, scalar_column_view> const> predicate_inputs,
   std::string const& predicate_udf,
   std::vector<column_view> const& filter_columns,
   bool is_ptx,
