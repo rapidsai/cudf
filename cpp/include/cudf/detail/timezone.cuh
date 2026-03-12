f8dc81e258dfd8bab39fd7966da228e3ffaa74b4
anon <users.noreply.github.com>
2026-03-12 17:53:10 +0000

    style

--- cpp/include/cudf/detail/timezone.cuh
+++ cpp/include/cudf/detail/timezone.cuh
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2023, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
