d0a6e9309e190ba6420de17a3fe61dcbc2e80a33
Yunsong Wang <yunsongw@nvidia.com>
2026-03-13 12:00:44 -0700

    Add join selectivity benchmarks

--- cpp/benchmarks/join/left_join.cu
+++ cpp/benchmarks/join/left_join.cu
@@ -9,7 +9,8 @@
 #include <cudf/join/mark_join.hpp>
 #include <cudf/utilities/default_stream.hpp>
 
-auto const num_keys = 1;
+auto const num_keys                       = 1;
+auto constexpr selectivity_num_operations = cudf::size_type{4};
 
 template <bool Nullable, cudf::null_equality NullEquality, data_type DataType>
 void nvbench_left_anti_join(nvbench::state& state,
@@ -20,6 +21,7 @@ void nvbench_left_anti_join(nvbench::state& state,
   auto const num_operations = static_cast<cudf::size_type>(state.get_int64("num_operations"));
   auto const left_size      = state.get_int64("left_size");
   auto const right_size     = state.get_int64("right_size");
+  auto const selectivity    = state.get_float64("selectivity");
   auto const join_type      = state.get_string("join_type");
   if (join_type == "mark_join" && (left_size > right_size || left_size > 100'000)) {
     state.skip("mark_join: build (left) should be smaller than probe (right) and <= 100K");
@@ -51,7 +53,8 @@ void nvbench_left_anti_join(nvbench::state& state,
   };
 
   auto const skip_large_right = (join_type == "filtered_join");
-  BM_join<Nullable, join_t::HASH, NullEquality>(state, dtypes, join, 1, 0.3, skip_large_right);
+  BM_join<Nullable, join_t::HASH, NullEquality>(
+    state, dtypes, join, 1, selectivity, skip_large_right);
 }
 
 template <bool Nullable, cudf::null_equality NullEquality, data_type DataType>
@@ -63,6 +66,7 @@ void nvbench_left_semi_join(nvbench::state& state,
   auto const num_operations = static_cast<cudf::size_type>(state.get_int64("num_operations"));
   auto const left_size      = state.get_int64("left_size");
   auto const right_size     = state.get_int64("right_size");
+  auto const selectivity    = state.get_float64("selectivity");
   auto const join_type      = state.get_string("join_type");
   if (join_type == "mark_join" && (left_size > right_size || left_size > 100'000)) {
     state.skip("mark_join: build (left) should be smaller than probe (right) and <= 100K");
@@ -93,7 +97,52 @@ void nvbench_left_semi_join(nvbench::state& state,
     }
   };
   auto const skip_large_right = (join_type == "filtered_join");
-  BM_join<Nullable, join_t::HASH, NullEquality>(state, dtypes, join, 1, 0.3, skip_large_right);
+  BM_join<Nullable, join_t::HASH, NullEquality>(
+    state, dtypes, join, 1, selectivity, skip_large_right);
+}
+
+template <cudf::null_equality NullEquality, data_type DataType>
+void nvbench_filtered_left_anti_join_selectivity(
+  nvbench::state& state,
+  nvbench::type_list<nvbench::enum_type<NullEquality>, nvbench::enum_type<DataType>>)
+{
+  auto const selectivity = state.get_float64("selectivity");
+  auto dtypes = cycle_dtypes(get_type_or_group(static_cast<int32_t>(DataType)), num_keys);
+
+  auto join = [](cudf::table_view const& left,
+                 cudf::table_view const& right,
+                 cudf::null_equality compare_nulls) {
+    cudf::filtered_join obj(
+      right, compare_nulls, cudf::set_as_build_table::RIGHT, cudf::get_default_stream());
+    for (auto i = 0; i < selectivity_num_operations - 1; i++) {
+      [[maybe_unused]] auto result = obj.anti_join(left);
+    }
+    return obj.anti_join(left);
+  };
+
+  BM_join<false, join_t::HASH, NullEquality>(state, dtypes, join, 1, selectivity);
+}
+
+template <cudf::null_equality NullEquality, data_type DataType>
+void nvbench_filtered_left_semi_join_selectivity(
+  nvbench::state& state,
+  nvbench::type_list<nvbench::enum_type<NullEquality>, nvbench::enum_type<DataType>>)
+{
+  auto const selectivity = state.get_float64("selectivity");
+  auto dtypes = cycle_dtypes(get_type_or_group(static_cast<int32_t>(DataType)), num_keys);
+
+  auto join = [](cudf::table_view const& left,
+                 cudf::table_view const& right,
+                 cudf::null_equality compare_nulls) {
+    cudf::filtered_join obj(
+      right, compare_nulls, cudf::set_as_build_table::RIGHT, cudf::get_default_stream());
+    for (auto i = 0; i < selectivity_num_operations - 1; i++) {
+      [[maybe_unused]] auto result = obj.semi_join(left);
+    }
+    return obj.semi_join(left);
+  };
+
+  BM_join<false, join_t::HASH, NullEquality>(state, dtypes, join, 1, selectivity);
 }
 
 NVBENCH_BENCH_TYPES(nvbench_left_anti_join,
@@ -105,6 +154,7 @@ NVBENCH_BENCH_TYPES(nvbench_left_anti_join,
   .add_int64_axis("left_size", JOIN_SIZE_RANGE)
   .add_int64_axis("right_size", JOIN_SIZE_RANGE)
   .add_int64_axis("num_operations", {4})
+  .add_float64_axis("selectivity", {0.3})
   .add_string_axis("join_type", {"mark_join", "filtered_join"});
 
 NVBENCH_BENCH_TYPES(nvbench_left_semi_join,
@@ -116,4 +166,21 @@ NVBENCH_BENCH_TYPES(nvbench_left_semi_join,
   .add_int64_axis("left_size", JOIN_SIZE_RANGE)
   .add_int64_axis("right_size", JOIN_SIZE_RANGE)
   .add_int64_axis("num_operations", {4})
+  .add_float64_axis("selectivity", {0.3})
   .add_string_axis("join_type", {"mark_join", "filtered_join"});
+
+NVBENCH_BENCH_TYPES(nvbench_filtered_left_anti_join_selectivity,
+                    NVBENCH_TYPE_AXES(DEFAULT_JOIN_NULL_EQUALITY, SELECTIVITY_JOIN_DATATYPES))
+  .set_name("filtered_left_anti_join_selectivity")
+  .set_type_axes_names({"NullEquality", "DataType"})
+  .add_int64_axis("left_size", {100'000'000})
+  .add_int64_axis("right_size", {100'000})
+  .add_float64_axis("selectivity", JOIN_SELECTIVITY_RANGE);
+
+NVBENCH_BENCH_TYPES(nvbench_filtered_left_semi_join_selectivity,
+                    NVBENCH_TYPE_AXES(DEFAULT_JOIN_NULL_EQUALITY, SELECTIVITY_JOIN_DATATYPES))
+  .set_name("filtered_left_semi_join_selectivity")
+  .set_type_axes_names({"NullEquality", "DataType"})
+  .add_int64_axis("left_size", {100'000'000})
+  .add_int64_axis("right_size", {100'000})
+  .add_float64_axis("selectivity", JOIN_SELECTIVITY_RANGE);
