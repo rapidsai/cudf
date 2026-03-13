550f4bdf14f4a804a0cd4d561cdccdfaa8b0210d
anon <users.noreply.github.com>
2026-03-13 19:01:14 +0000

    style

--- cpp/src/io/csv/reader_impl.cu
+++ cpp/src/io/csv/reader_impl.cu
@@ -20,8 +20,8 @@
 #include <cudf/detail/iterator.cuh>
 #include <cudf/detail/utilities/cuda.cuh>
 #include <cudf/detail/utilities/cuda_memcpy.hpp>
-#include <cudf/detail/utilities/integer_utils.hpp>
 #include <cudf/detail/utilities/host_worker_pool.hpp>
+#include <cudf/detail/utilities/integer_utils.hpp>
 #include <cudf/detail/utilities/stream_pool.hpp>
 #include <cudf/detail/utilities/vector_factories.hpp>
 #include <cudf/detail/utilities/visitor_overload.hpp>
