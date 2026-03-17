2aad7719da6262fbb3090f2203db8a67b5b78bd7
James Lamb <jlamb@nvidia.com>
2026-03-16 20:53:48 -0500

    merge test-older-ctk

--- ci/build_wheel_pylibcudf.sh
+++ ci/build_wheel_pylibcudf.sh
@@ -6,6 +6,9 @@ set -euo pipefail
 
 source rapids-init-pip
 
+# TODO(jameslamb): revert before merging
+source ci/use_wheels_from_prs.sh
+
 package_dir="python/pylibcudf"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
