a99742f557190a0d71d95ee59f1e7b7790f3733e
Tim Head <betatim@gmail.com>
2026-03-24 18:02:32 +0100

    Easy as ABC

--- ci/test_cuml_compat.sh
+++ ci/test_cuml_compat.sh
@@ -9,24 +9,24 @@ rapids-mamba-retry install -n test -c rapidsai-nightly cuml
 
 rapids-logger "pytest cuml cuDF-compat subset"
 timeout 10m python -m pytest --pyargs --cache-clear \
-  cuml.tests.test_input_utils \
   cuml.tests.test_array \
-  cuml.tests.test_reflection \
-  cuml.tests.test_validation \
-  cuml.tests.test_label_encoder \
-  cuml.tests.test_target_encoder \
-  cuml.tests.test_one_hot_encoder \
-  cuml.tests.test_ordinal_encoder \
-  cuml.tests.test_train_test_split \
-  cuml.tests.test_split \
   cuml.tests.test_compose \
-  cuml.tests.test_text_feature_extraction \
+  cuml.tests.test_input_utils \
   cuml.tests.test_kneighbors_classifier \
   cuml.tests.test_kneighbors_regressor \
-  cuml.tests.test_nearest_neighbors \
-  cuml.tests.test_metrics \
+  cuml.tests.test_label_encoder \
   cuml.tests.test_linear_model \
-  cuml.tests.test_sgd \
-  cuml.tests.test_svm \
+  cuml.tests.test_metrics \
+  cuml.tests.test_nearest_neighbors \
+  cuml.tests.test_one_hot_encoder \
+  cuml.tests.test_ordinal_encoder \
   cuml.tests.test_pickle \
-  cuml.tests.test_random_forest
+  cuml.tests.test_random_forest \
+  cuml.tests.test_reflection \
+  cuml.tests.test_sgd \
+  cuml.tests.test_split \
+  cuml.tests.test_svm \
+  cuml.tests.test_target_encoder \
+  cuml.tests.test_text_feature_extraction \
+  cuml.tests.test_train_test_split \
+  cuml.tests.test_validation
