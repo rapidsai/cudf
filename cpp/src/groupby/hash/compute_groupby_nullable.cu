3f38e660960fb86ff9a392aebfc0de965c4968a6
Bradley Dice <bdice@bradleydice.com>
2026-03-27 20:04:21 +0000

    Split compute_groupby.cu by nullable template parameter
    Move template definition to compute_groupby.cuh and split the two
    explicit instantiations (row_comparator_t, nullable_row_comparator_t)
    into separate TUs so they compile in parallel.
    
    compute_groupby.cu (340s) -> compute_groupby.cu (177s) +
      compute_groupby_nullable.cu (227s), critical path 227s.
    
    All 4272 groupby tests pass.

--- cpp/src/groupby/hash/compute_groupby_nullable.cu
+++ cpp/src/groupby/hash/compute_groupby_nullable.cu
@@ -7,11 +7,11 @@
 
 namespace cudf::groupby::detail::hash {
 
-template std::unique_ptr<table> compute_groupby<row_comparator_t, row_hash_t>(
+template std::unique_ptr<table> compute_groupby<nullable_row_comparator_t, row_hash_t>(
   table_view const& keys,
   host_span<aggregation_request const> requests,
   bool skip_rows_with_nulls,
-  row_comparator_t const& d_row_equal,
+  nullable_row_comparator_t const& d_row_equal,
   row_hash_t const& d_row_hash,
   cudf::detail::result_cache* cache,
   rmm::cuda_stream_view stream,
