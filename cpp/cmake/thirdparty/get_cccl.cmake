4c4755483b1926a5aef399726992e5bfdb947d70
David Wendt <dwendt@nvidia.com>
2026-03-30 15:05:25 -0400

    change back to CCCL 3.3

--- cpp/cmake/thirdparty/get_cccl.cmake
+++ cpp/cmake/thirdparty/get_cccl.cmake
@@ -1,6 +1,6 @@
 # =============================================================================
 # cmake-format: off
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 # cmake-format: on
 # =============================================================================
@@ -8,6 +8,12 @@
 # Use CPM to find or clone CCCL
 function(find_and_configure_cccl)
   include(${rapids-cmake-dir}/cpm/cccl.cmake)
+
+  include(${rapids-cmake-dir}/cpm/package_override.cmake)
+
+  set(rmm_patch_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/patches")
+  rapids_cpm_package_override("${rmm_patch_dir}/cccl_override.json")
+
   rapids_cpm_cccl(BUILD_EXPORT_SET cudf-exports INSTALL_EXPORT_SET cudf-exports)
 endfunction()
 
