98bdbf60e0be432d0dc80577746d539676a53b1e
Nick Becker <nickb500@gmail.com>
2026-03-31 18:03:22 -0700

    Revert "Update python/cudf_polars/cudf_polars/experimental/benchmarks/pdsds_queries/q50.py"
    This reverts commit b15866052ac745094cd171d79a01ce0846941572.

--- python/cudf_polars/cudf_polars/experimental/benchmarks/pdsds_queries/q50.py
+++ python/cudf_polars/cudf_polars/experimental/benchmarks/pdsds_queries/q50.py
@@ -215,6 +215,6 @@ def polars_impl(run_config: RunConfig) -> QueryResult:
             .sort(group_cols, nulls_last=True)
             .limit(limit)
         ),
-        sort_by=group_cols,
+        sort_by=list(sort_by.items()),
         limit=limit,
     )
