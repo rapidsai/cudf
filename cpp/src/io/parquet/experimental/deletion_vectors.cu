d7d02429fd55c04fb20aa861f4fdd9766cac0555
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2026-04-01 20:16:31 +0000

    Refactor

--- cpp/src/io/parquet/experimental/deletion_vectors.cu
+++ cpp/src/io/parquet/experimental/deletion_vectors.cu
@@ -20,6 +20,160 @@
 
 namespace cudf::io::parquet::experimental {
 
+namespace detail {
+
+[[nodiscard]] table_with_metadata read_parquet(parquet_reader_options const& options,
+                                               deletion_vector_info const& deletion_vector_info,
+                                               rmm::cuda_stream_view stream,
+                                               rmm::device_async_resource_ref mr)
+{
+  auto const& serialized_roaring_bitmaps = deletion_vector_info.serialized_roaring_bitmaps;
+  auto const& deletion_vector_row_counts = deletion_vector_info.deletion_vector_row_counts;
+  auto const& row_group_offsets          = deletion_vector_info.row_group_offsets;
+  auto const& row_group_num_rows         = deletion_vector_info.row_group_num_rows;
+
+  CUDF_EXPECTS(
+    row_group_offsets.size() == row_group_num_rows.size(),
+    "Encountered a mismatch in the number of row group offsets and row group row counts");
+  CUDF_EXPECTS(
+    not options.get_filter().has_value(),
+    "Encountered a non-empty AST filter expression. Use a roaring64 bitmap deletion vector to "
+    "filter the table instead");
+  CUDF_EXPECTS(serialized_roaring_bitmaps.empty() or
+                 serialized_roaring_bitmaps.size() == deletion_vector_row_counts.size(),
+               "Encountered a mismatch in the number of deletion vectors and the number of rows "
+               "per deletion vector");
+
+  // Use default mr to read parquet table and build row index column if we will be applying the
+  // deletion vector to produce a new table later
+  auto const table_mr =
+    serialized_roaring_bitmaps.empty() ? mr : cudf::get_current_device_resource_ref();
+
+  auto [table, metadata] = cudf::io::read_parquet(options, stream, table_mr);
+  auto const num_rows    = table->num_rows();
+
+  CUDF_EXPECTS(
+    row_group_num_rows.empty() or
+      std::cmp_equal(metadata.num_input_row_groups, row_group_num_rows.size()),
+    "Encountered a mismatch in the number of row groups in parquet file and the specified "
+    "row group offsets/row counts vectors");
+
+  // Compute a row index column from the specified row group offsets and counts
+  auto row_index_column = compute_row_index_column(
+    row_group_offsets, row_group_num_rows, std::nullopt, num_rows, stream, table_mr);
+
+  // Prepend row index column to the table columns
+  auto table_with_index =
+    prepend_index_column_to_table(std::move(table), std::move(row_index_column));
+
+  prepend_index_column_to_table_metadata(metadata);
+
+  if (serialized_roaring_bitmaps.empty()) {
+    return table_with_metadata{std::move(table_with_index), std::move(metadata)};
+  }
+
+  // Construct all deletion vectors and store their references
+  auto deletion_vectors      = std::vector<roaring_bitmap_type>{};
+  auto deletion_vectors_refs = std::vector<std::reference_wrapper<roaring_bitmap_type>>{};
+  deletion_vectors.reserve(serialized_roaring_bitmaps.size());
+  deletion_vectors_refs.reserve(serialized_roaring_bitmaps.size());
+  std::transform(serialized_roaring_bitmaps.begin(),
+                 serialized_roaring_bitmaps.end(),
+                 std::back_inserter(deletion_vectors_refs),
+                 [&](auto const& serialized_roaring_bitmap) {
+                   deletion_vectors.emplace_back(serialized_roaring_bitmap.data(),
+                                                 rmm::mr::polymorphic_allocator<char>{},
+                                                 stream);
+                   return std::ref(deletion_vectors.back());
+                 });
+  auto row_mask = compute_row_mask_column(table_with_index->get_column(0).view(),
+                                          deletion_vectors_refs,
+                                          deletion_vector_row_counts,
+                                          stream,
+                                          cudf::get_current_device_resource_ref());
+  // Filter the table using the deletion vector
+  return table_with_metadata{
+    // Supply user-provided mr to apply_boolean_mask to allocate output table's memory
+    cudf::apply_boolean_mask(table_with_index->view(), row_mask->view(), stream, mr),
+    std::move(metadata)};
+}
+
+[[nodiscard]] size_t compute_num_deleted_rows(deletion_vector_info const& deletion_vector_info,
+                                              cudf::size_type max_chunk_rows,
+                                              rmm::cuda_stream_view stream)
+{
+  auto const& serialized_roaring_bitmaps = deletion_vector_info.serialized_roaring_bitmaps;
+  auto const& deletion_vector_row_counts = deletion_vector_info.deletion_vector_row_counts;
+  auto const& row_group_offsets          = deletion_vector_info.row_group_offsets;
+  auto const& row_group_num_rows         = deletion_vector_info.row_group_num_rows;
+
+  if (serialized_roaring_bitmaps.empty()) { return 0; }
+
+  // Validate input
+  CUDF_EXPECTS(std::cmp_equal(serialized_roaring_bitmaps.size(), deletion_vector_row_counts.size()),
+               "Encountered a mismatch in the number of deletion vector data spans and the number "
+               "of rows per deletion vector");
+  CUDF_EXPECTS(
+    row_group_offsets.size() == row_group_num_rows.size(),
+    "Encountered a mismatch in the number of row group offsets and row group row counts");
+  CUDF_EXPECTS(max_chunk_rows > 0, "Encountered an invalid chunk size");
+
+  auto const is_row_group_data_unspecified = row_group_offsets.empty();
+
+  auto const num_rows = [&]() {
+    auto const rows_in_dvs = std::accumulate(
+      deletion_vector_row_counts.begin(), deletion_vector_row_counts.end(), size_t{0});
+    if (not is_row_group_data_unspecified) {
+      auto const rows_in_rgs =
+        std::accumulate(row_group_num_rows.begin(), row_group_num_rows.end(), size_t{0});
+      CUDF_EXPECTS(std::cmp_equal(rows_in_dvs, rows_in_rgs),
+                   "Encountered a mismatch in the number of rows across deletion vectors and the "
+                   "number of rows across row groups");
+    }
+    return rows_in_dvs;
+  }();
+
+  std::queue<size_t> rg_offsets_queue;
+  std::queue<size_type> rg_counts_queue;
+  for (size_t i = 0; i < row_group_offsets.size(); ++i) {
+    rg_offsets_queue.push(row_group_offsets[i]);
+    rg_counts_queue.push(row_group_num_rows[i]);
+  }
+
+  std::queue<chunked_parquet_reader::roaring_bitmap_impl> dv_queue;
+  std::queue<cudf::size_type> dv_row_counts_queue;
+  for (size_t i = 0; i < serialized_roaring_bitmaps.size(); ++i) {
+    dv_queue.emplace(serialized_roaring_bitmaps[i]);
+    dv_row_counts_queue.push(deletion_vector_row_counts[i]);
+  }
+
+  size_t deleted_rows   = 0;
+  size_t remaining_rows = num_rows;
+  size_t start_row      = 0;
+
+  while (remaining_rows > 0) {
+    // Maximum number of rows to process in this chunk
+    auto const chunk_rows = std::min<size_t>(remaining_rows, max_chunk_rows);
+    auto row_index_column =
+      compute_partial_row_index_column(rg_offsets_queue,
+                                       rg_counts_queue,
+                                       start_row,
+                                       static_cast<size_type>(chunk_rows),
+                                       is_row_group_data_unspecified,
+                                       stream,
+                                       cudf::get_current_device_resource_ref());
+    deleted_rows += compute_partial_deleted_row_count(
+      row_index_column->view(), dv_queue, dv_row_counts_queue, stream);
+
+    start_row += chunk_rows;
+    remaining_rows -= chunk_rows;
+  }
+
+  return deleted_rows;
+}
+
+}  // namespace detail
+
 /**
  * @copydoc
  * cudf::io::parquet::experimental::chunked_parquet_reader::chunked_parquet_reader
@@ -160,155 +314,18 @@ table_with_metadata read_parquet(parquet_reader_options const& options,
                                  rmm::device_async_resource_ref mr)
 {
   CUDF_FUNC_RANGE();
-
-  auto const& serialized_roaring_bitmaps = deletion_vector_info.serialized_roaring_bitmaps;
-  auto const& deletion_vector_row_counts = deletion_vector_info.deletion_vector_row_counts;
-  auto const& row_group_offsets          = deletion_vector_info.row_group_offsets;
-  auto const& row_group_num_rows         = deletion_vector_info.row_group_num_rows;
-
-  CUDF_EXPECTS(
-    row_group_offsets.size() == row_group_num_rows.size(),
-    "Encountered a mismatch in the number of row group offsets and row group row counts");
-  CUDF_EXPECTS(
-    not options.get_filter().has_value(),
-    "Encountered a non-empty AST filter expression. Use a roaring64 bitmap deletion vector to "
-    "filter the table instead");
-  CUDF_EXPECTS(serialized_roaring_bitmaps.empty() or
-                 serialized_roaring_bitmaps.size() == deletion_vector_row_counts.size(),
-               "Encountered a mismatch in the number of deletion vectors and the number of rows "
-               "per deletion vector");
-
-  // Use default mr to read parquet table and build row index column if we will be applying the
-  // deletion vector to produce a new table later
-  auto const table_mr =
-    serialized_roaring_bitmaps.empty() ? mr : cudf::get_current_device_resource_ref();
-
-  auto [table, metadata] = cudf::io::read_parquet(options, stream, table_mr);
-  auto const num_rows    = table->num_rows();
-
-  CUDF_EXPECTS(
-    row_group_num_rows.empty() or
-      std::cmp_equal(metadata.num_input_row_groups, row_group_num_rows.size()),
-    "Encountered a mismatch in the number of row groups in parquet file and the specified "
-    "row group offsets/row counts vectors");
-
-  // Compute a row index column from the specified row group offsets and counts
-  auto row_index_column = compute_row_index_column(
-    row_group_offsets, row_group_num_rows, std::nullopt, num_rows, stream, table_mr);
-
-  // Prepend row index column to the table columns
-  auto table_with_index =
-    prepend_index_column_to_table(std::move(table), std::move(row_index_column));
-
-  prepend_index_column_to_table_metadata(metadata);
-
-  if (serialized_roaring_bitmaps.empty()) {
-    return table_with_metadata{std::move(table_with_index), std::move(metadata)};
-  }
-
-  // Construct all deletion vectors and store their references
-  auto deletion_vectors      = std::vector<roaring_bitmap_type>{};
-  auto deletion_vectors_refs = std::vector<std::reference_wrapper<roaring_bitmap_type>>{};
-  deletion_vectors.reserve(serialized_roaring_bitmaps.size());
-  deletion_vectors_refs.reserve(serialized_roaring_bitmaps.size());
-  std::transform(serialized_roaring_bitmaps.begin(),
-                 serialized_roaring_bitmaps.end(),
-                 std::back_inserter(deletion_vectors_refs),
-                 [&](auto const& serialized_roaring_bitmap) {
-                   deletion_vectors.emplace_back(serialized_roaring_bitmap.data(),
-                                                 rmm::mr::polymorphic_allocator<char>{},
-                                                 stream);
-                   return std::ref(deletion_vectors.back());
-                 });
-  auto row_mask = compute_row_mask_column(table_with_index->get_column(0).view(),
-                                          deletion_vectors_refs,
-                                          deletion_vector_row_counts,
-                                          stream,
-                                          cudf::get_current_device_resource_ref());
-  // Filter the table using the deletion vector
-  return table_with_metadata{
-    // Supply user-provided mr to apply_boolean_mask to allocate output table's memory
-    cudf::apply_boolean_mask(table_with_index->view(), row_mask->view(), stream, mr),
-    std::move(metadata)};
+  return detail::read_parquet(options, deletion_vector_info, stream, mr);
 }
 
 /**
  * @copydoc cudf::io::parquet::experimental::compute_num_deleted_rows
  */
-[[nodiscard]] size_t compute_num_deleted_rows(deletion_vector_info const& deletion_vector_info,
-                                              cudf::size_type max_chunk_rows,
-                                              rmm::cuda_stream_view stream)
+size_t compute_num_deleted_rows(deletion_vector_info const& deletion_vector_info,
+                                cudf::size_type max_chunk_rows,
+                                rmm::cuda_stream_view stream)
 {
   CUDF_FUNC_RANGE();
-
-  auto const& serialized_roaring_bitmaps = deletion_vector_info.serialized_roaring_bitmaps;
-  auto const& deletion_vector_row_counts = deletion_vector_info.deletion_vector_row_counts;
-  auto const& row_group_offsets          = deletion_vector_info.row_group_offsets;
-  auto const& row_group_num_rows         = deletion_vector_info.row_group_num_rows;
-
-  if (serialized_roaring_bitmaps.empty()) { return 0; }
-
-  // Validate input
-  CUDF_EXPECTS(std::cmp_equal(serialized_roaring_bitmaps.size(), deletion_vector_row_counts.size()),
-               "Encountered a mismatch in the number of deletion vector data spans and the number "
-               "of rows per deletion vector");
-  CUDF_EXPECTS(
-    row_group_offsets.size() == row_group_num_rows.size(),
-    "Encountered a mismatch in the number of row group offsets and row group row counts");
-  CUDF_EXPECTS(max_chunk_rows > 0, "Encountered an invalid chunk size");
-
-  auto const is_row_group_data_unspecified = row_group_offsets.empty();
-
-  auto const num_rows = [&]() {
-    auto const rows_in_dvs = std::accumulate(
-      deletion_vector_row_counts.begin(), deletion_vector_row_counts.end(), size_t{0});
-    if (not is_row_group_data_unspecified) {
-      auto const rows_in_rgs =
-        std::accumulate(row_group_num_rows.begin(), row_group_num_rows.end(), size_t{0});
-      CUDF_EXPECTS(std::cmp_equal(rows_in_dvs, rows_in_rgs),
-                   "Encountered a mismatch in the number of rows across deletion vectors and the "
-                   "number of rows across row groups");
-    }
-    return rows_in_dvs;
-  }();
-
-  std::queue<size_t> rg_offsets_queue;
-  std::queue<size_type> rg_counts_queue;
-  for (size_t i = 0; i < row_group_offsets.size(); ++i) {
-    rg_offsets_queue.push(row_group_offsets[i]);
-    rg_counts_queue.push(row_group_num_rows[i]);
-  }
-
-  std::queue<chunked_parquet_reader::roaring_bitmap_impl> dv_queue;
-  std::queue<cudf::size_type> dv_row_counts_queue;
-  for (size_t i = 0; i < serialized_roaring_bitmaps.size(); ++i) {
-    dv_queue.emplace(serialized_roaring_bitmaps[i]);
-    dv_row_counts_queue.push(deletion_vector_row_counts[i]);
-  }
-
-  size_t deleted_rows   = 0;
-  size_t remaining_rows = num_rows;
-  size_t start_row      = 0;
-
-  while (remaining_rows > 0) {
-    // Maximum number of rows to process in this chunk
-    auto const chunk_rows = std::min<size_t>(remaining_rows, max_chunk_rows);
-    auto row_index_column =
-      compute_partial_row_index_column(rg_offsets_queue,
-                                       rg_counts_queue,
-                                       start_row,
-                                       static_cast<size_type>(chunk_rows),
-                                       is_row_group_data_unspecified,
-                                       stream,
-                                       cudf::get_current_device_resource_ref());
-    deleted_rows += compute_partial_deleted_row_count(
-      row_index_column->view(), dv_queue, dv_row_counts_queue, stream);
-
-    start_row += chunk_rows;
-    remaining_rows -= chunk_rows;
-  }
-
-  return deleted_rows;
+  return detail::compute_num_deleted_rows(deletion_vector_info, max_chunk_rows, stream);
 }
 
 }  // namespace cudf::io::parquet::experimental
