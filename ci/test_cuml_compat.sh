4a94ceda3e8fe10449a59dafe5af76e51764d07d
Tim Head <betatim@gmail.com>
2026-04-02 09:52:50 +0200

    Checkout cuml's source directly
    The conda/wheel packages do not ship the tests, so we need to go to the
    source.

--- ci/test_cuml_compat.sh
+++ ci/test_cuml_compat.sh
@@ -10,26 +10,37 @@ source ./ci/test_python_common.sh test_python_cudf
 # Install cuml nightly (gets cuml + its dependencies from RAPIDS nightly channel)
 rapids-mamba-retry install -n test -c rapidsai-nightly cuml
 
+# Clone cuml at the matching RAPIDS branch to get test files
+# (the conda package doesn't ship cuml/tests/)
+
+# This makes sure we read RAPIDS_BRANCH from the top level of the repo, no matter
+# what the current working directory is.
+RAPIDS_BRANCH="$(cat "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../RAPIDS_BRANCH)"
+rapids-logger "Cloning cuml at branch ${RAPIDS_BRANCH}"
+git clone https://github.com/rapidsai/cuml.git --branch "${RAPIDS_BRANCH}" --depth 1 /tmp/cuml
+
+CUML_TESTS_DIR=/tmp/cuml/python/cuml/cuml/tests
+
 rapids-logger "pytest cuml cuDF-compat subset"
-timeout 10m python -m pytest --pyargs --cache-clear \
-  cuml.tests.test_array \
-  cuml.tests.test_compose \
-  cuml.tests.test_input_utils \
-  cuml.tests.test_kneighbors_classifier \
-  cuml.tests.test_kneighbors_regressor \
-  cuml.tests.test_label_encoder \
-  cuml.tests.test_linear_model \
-  cuml.tests.test_metrics \
-  cuml.tests.test_nearest_neighbors \
-  cuml.tests.test_one_hot_encoder \
-  cuml.tests.test_ordinal_encoder \
-  cuml.tests.test_pickle \
-  cuml.tests.test_random_forest \
-  cuml.tests.test_reflection \
-  cuml.tests.test_sgd \
-  cuml.tests.test_split \
-  cuml.tests.test_svm \
-  cuml.tests.test_target_encoder \
-  cuml.tests.test_text_feature_extraction \
-  cuml.tests.test_train_test_split \
-  cuml.tests.test_validation
+timeout 10m python -m pytest --cache-clear \
+  "${CUML_TESTS_DIR}/test_array.py" \
+  "${CUML_TESTS_DIR}/test_compose.py" \
+  "${CUML_TESTS_DIR}/test_input_utils.py" \
+  "${CUML_TESTS_DIR}/test_kneighbors_classifier.py" \
+  "${CUML_TESTS_DIR}/test_kneighbors_regressor.py" \
+  "${CUML_TESTS_DIR}/test_label_encoder.py" \
+  "${CUML_TESTS_DIR}/test_linear_model.py" \
+  "${CUML_TESTS_DIR}/test_metrics.py" \
+  "${CUML_TESTS_DIR}/test_nearest_neighbors.py" \
+  "${CUML_TESTS_DIR}/test_one_hot_encoder.py" \
+  "${CUML_TESTS_DIR}/test_ordinal_encoder.py" \
+  "${CUML_TESTS_DIR}/test_pickle.py" \
+  "${CUML_TESTS_DIR}/test_random_forest.py" \
+  "${CUML_TESTS_DIR}/test_reflection.py" \
+  "${CUML_TESTS_DIR}/test_sgd.py" \
+  "${CUML_TESTS_DIR}/test_split.py" \
+  "${CUML_TESTS_DIR}/test_svm.py" \
+  "${CUML_TESTS_DIR}/test_target_encoder.py" \
+  "${CUML_TESTS_DIR}/test_text_feature_extraction.py" \
+  "${CUML_TESTS_DIR}/test_train_test_split.py" \
+  "${CUML_TESTS_DIR}/test_validation.py"
