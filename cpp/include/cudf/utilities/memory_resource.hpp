c0648ca00487eb946d7d121269e4d088a2d8dfe3
Bradley Dice <bdice@bradleydice.com>
2026-04-15 11:56:18 -0500

    Deprecate set_current_device_resource_ref and reset_current_device_resource_ref

--- cpp/include/cudf/utilities/memory_resource.hpp
+++ cpp/include/cudf/utilities/memory_resource.hpp
@@ -42,6 +42,24 @@ inline cuda::mr::any_resource<cuda::mr::device_accessible> set_current_device_re
 }
 
 /**
+ * @brief Set the current device memory resource reference.
+ *
+ * @deprecated Use `set_current_device_resource` instead.
+ *
+ * The object referenced by `mr` must outlive the last use of the resource, otherwise behavior is
+ * undefined. It is the caller's responsibility to maintain the lifetime of the resource object.
+ *
+ * @param mr The new device memory resource reference.
+ * @return An owning any_resource holding the previous resource.
+ */
+[[deprecated("Use set_current_device_resource instead.")]]  //
+inline cuda::mr::any_resource<cuda::mr::device_accessible>
+set_current_device_resource_ref(rmm::device_async_resource_ref mr)
+{
+  return set_current_device_resource(cuda::mr::any_resource<cuda::mr::device_accessible>{mr});
+}
+
+/**
  * @brief Reset the current device memory resource to the initial resource.
  *
  * @return An owning any_resource holding the previous resource.
@@ -49,6 +67,20 @@ inline cuda::mr::any_resource<cuda::mr::device_accessible> set_current_device_re
 inline cuda::mr::any_resource<cuda::mr::device_accessible> reset_current_device_resource()
 {
   return rmm::mr::reset_current_device_resource();
+}
+
+/**
+ * @brief Reset the current device memory resource reference to the initial resource.
+ *
+ * @deprecated Use `reset_current_device_resource` instead.
+ *
+ * @return An owning any_resource holding the previous resource.
+ */
+[[deprecated("Use reset_current_device_resource instead.")]]  //
+inline cuda::mr::any_resource<cuda::mr::device_accessible>
+reset_current_device_resource_ref()
+{
+  return reset_current_device_resource();
 }
 
 /** @} */  // end of group
