297cc818b26e4624302aa2f79397f478b54014c2
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2026-04-16 21:28:47 +0000

    Ignore ResouceWarnings and enable other test runs

--- ci/run_cudf_polars_polars_tests.sh
+++ ci/run_cudf_polars_polars_tests.sh
@@ -62,24 +62,7 @@ DESELECTED_TESTS_STR=$(printf -- " --deselect %s" "${DESELECTED_TESTS[@]}")
 # Don't quote the `DESELECTED_...` variable because `pytest` can't handle
 # multiple quoted arguments inline
 # shellcheck disable=SC2086
-# echo "Run polars tests with the streaming executor"
-# CUDF_POLARS__EXECUTOR__TARGET_PARTITION_SIZE=805306368 \
-# CUDF_POLARS__EXECUTOR__FALLBACK_MODE=silent \
-#     python -m pytest \
-#        --import-mode=importlib \
-#        --cache-clear \
-#        -m "" \
-#        -p cudf_polars.testing.plugin \
-#        -n 8 \
-#        --dist=worksteal \
-#        -vv \
-#        --tb=native \
-#        $DESELECTED_TESTS_STR \
-#        "$@" \
-#        py-polars/tests \
-#        --executor streaming
-
-echo "Run polars tests with the streaming executor and rapidsmpf runtime"
+echo "Run polars tests with the streaming executor"
 CUDF_POLARS__EXECUTOR__TARGET_PARTITION_SIZE=805306368 \
 CUDF_POLARS__EXECUTOR__FALLBACK_MODE=silent \
     python -m pytest \
@@ -94,21 +77,41 @@ CUDF_POLARS__EXECUTOR__FALLBACK_MODE=silent \
        $DESELECTED_TESTS_STR \
        "$@" \
        py-polars/tests \
+       --executor streaming
+
+
+# TODO(ResourceWarning): https://github.com/rapidsai/cudf/issues/22181
+echo "Run polars tests with the streaming executor and rapidsmpf runtime"
+CUDF_POLARS__EXECUTOR__TARGET_PARTITION_SIZE=805306368 \
+CUDF_POLARS__EXECUTOR__FALLBACK_MODE=silent \
+    python -m pytest \
+       --import-mode=importlib \
+       --cache-clear \
+       -m "" \
+       -p cudf_polars.testing.plugin \
+       -W ignore::ResourceWarning \
+       -n 8 \
+       --dist=worksteal \
+       -vv \
+       --tb=native \
+       $DESELECTED_TESTS_STR \
+       "$@" \
+       py-polars/tests \
        --executor streaming \
        --blocksize-mode small \
        --runtime rapidsmpf
 
-# echo "Run polars tests with the in-memory executor"
-# python -m pytest \
-#        --import-mode=importlib \
-#        --cache-clear \
-#        -m "" \
-#        -p cudf_polars.testing.plugin \
-#        -n 8 \
-#        --dist=worksteal \
-#        -vv \
-#        --tb=native \
-#        $DESELECTED_TESTS_STR \
-#        "$@" \
-#        py-polars/tests \
-#        --executor in-memory
+echo "Run polars tests with the in-memory executor"
+python -m pytest \
+       --import-mode=importlib \
+       --cache-clear \
+       -m "" \
+       -p cudf_polars.testing.plugin \
+       -n 8 \
+       --dist=worksteal \
+       -vv \
+       --tb=native \
+       $DESELECTED_TESTS_STR \
+       "$@" \
+       py-polars/tests \
+       --executor in-memory
