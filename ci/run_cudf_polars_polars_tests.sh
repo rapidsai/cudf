2c366416b8ff82cdc1fd4aa22b0995339c9846b1
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2026-04-17 10:21:23 -0700

    Run Polars unit tests with RapidsMPF (#21677)
    This PR adds a Polars test suite run using `--runtime rapidsmpf` (modified in our Polars testing plugin). Note this is a **blocking** job in CI.
    
    Tests are run with `--block-size small` to emulate multi-partition execution even for tests with small data; however, this dramatically increases the runtime of some tests with larger data and are therefore skipped in `RAPIDSMPF_TESTS_TO_SKIP` to keep the runtime manageable.
    
    xfailing test are listed in `RAPIDSMPF_ONLY_EXPECTED_FAILURES` with corresponding issues in https://github.com/rapidsai/cudf/issues/21468. If the failure occurs during the runtime of query with rapidsmpf, they tend to be in `RAPIDSMPF_TESTS_TO_SKIP` instead due to https://github.com/rapidsai/cudf/issues/22181.
    
    Minor, but also replaces `rapids-logger` with `echo` in `ci/run_cudf_polars_polars_tests.sh` so this script can more easily run without CI rapids utilities installed.
    
    Authors:
      - Matthew Roeschke (https://github.com/mroeschke)
      - Matthew Murray (https://github.com/Matt711)
    
    Approvers:
      - James Lamb (https://github.com/jameslamb)
      - Matthew Murray (https://github.com/Matt711)
    
    URL: https://github.com/rapidsai/cudf/pull/21677

--- ci/run_cudf_polars_polars_tests.sh
+++ ci/run_cudf_polars_polars_tests.sh
@@ -62,7 +62,7 @@ DESELECTED_TESTS_STR=$(printf -- " --deselect %s" "${DESELECTED_TESTS[@]}")
 # Don't quote the `DESELECTED_...` variable because `pytest` can't handle
 # multiple quoted arguments inline
 # shellcheck disable=SC2086
-rapids-logger "Run polars tests with the streaming executor"
+echo "Run polars tests with the streaming executor"
 CUDF_POLARS__EXECUTOR__TARGET_PARTITION_SIZE=805306368 \
 CUDF_POLARS__EXECUTOR__FALLBACK_MODE=silent \
     python -m pytest \
@@ -79,7 +79,29 @@ CUDF_POLARS__EXECUTOR__FALLBACK_MODE=silent \
        py-polars/tests \
        --executor streaming
 
-rapids-logger "Run polars tests with the in-memory executor"
+
+# TODO(ResourceWarning): https://github.com/rapidsai/cudf/issues/22181
+echo "Run polars tests with the streaming executor and rapidsmpf runtime"
+CUDF_POLARS__EXECUTOR__TARGET_PARTITION_SIZE=805306368 \
+CUDF_POLARS__EXECUTOR__FALLBACK_MODE=silent \
+    python -m pytest \
+       --import-mode=importlib \
+       --cache-clear \
+       -m "" \
+       -p cudf_polars.testing.plugin \
+       -W ignore::ResourceWarning \
+       -n 8 \
+       --dist=worksteal \
+       -vv \
+       --tb=native \
+       $DESELECTED_TESTS_STR \
+       "$@" \
+       py-polars/tests \
+       --executor streaming \
+       --blocksize-mode small \
+       --runtime rapidsmpf
+
+echo "Run polars tests with the in-memory executor"
 python -m pytest \
        --import-mode=importlib \
        --cache-clear \
