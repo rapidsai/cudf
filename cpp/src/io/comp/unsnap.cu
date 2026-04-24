abddbb66f076238fa718b1cef60c5da6b8c180c0
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2026-04-24 02:23:17 +0000

    Parquet unsnap kernel throws if copy element is malformed

--- cpp/src/io/comp/unsnap.cu
+++ cpp/src/io/comp/unsnap.cu
@@ -319,19 +319,25 @@ __device__ void snappy_decode_symbols(unsnap_state_s* s, uint32_t t)
         if (batch_len != 0) {
           uint32_t blen = 0;
           int32_t ofs   = 0;
+          // Check and reject lanes with malformed copy elements with offset not in range [1,
+          // current dst position]. This way the batch is truncated before warp 2 executes the
+          // invalid copy.
+          bool is_copy_offset_bad = false;
           if (t < batch_len) {
-            blen        = (b0 & 1) ? ((b0 >> 2) & 7) + 4 : ((b0 >> 2) + 1);
-            ofs         = (b0 & 1)   ? ((b0 & 0xe0) << 3) | byte_access(s, cur_t + 1)
-                          : (b0 & 2) ? byte_access(s, cur_t + 1) | (byte_access(s, cur_t + 2) << 8)
-                                     : -(int32_t)(cur_t + 1);
-            b[t].len    = blen;
-            b[t].offset = ofs;
+            blen               = (b0 & 1) ? ((b0 >> 2) & 7) + 4 : ((b0 >> 2) + 1);
+            ofs                = (b0 & 1) ? ((b0 & 0xe0) << 3) | byte_access(s, cur_t + 1)
+                                 : (b0 & 2) ? byte_access(s, cur_t + 1) | (byte_access(s, cur_t + 2) << 8)
+                                            : -(int32_t)(cur_t + 1);
+            is_copy_offset_bad = (b0 & 3) != 0 && ofs == 0;
+            b[t].len           = blen;
+            b[t].offset        = ofs;
             ofs += blen;  // for correct out-of-range detection below
           }
           blen           = warp_reduce_pos<cudf::detail::warp_size>(blen, t);
           bytes_left     = shuffle(bytes_left);
           dst_pos        = shuffle(dst_pos);
-          short_sym_mask = __ffs(ballot(blen > bytes_left || ofs > (int32_t)(dst_pos + blen)));
+          short_sym_mask = __ffs(
+            ballot(blen > bytes_left || ofs > (int32_t)(dst_pos + blen) || is_copy_offset_bad));
           if (short_sym_mask != 0) { batch_len = min(batch_len, short_sym_mask - 1); }
           if (batch_len != 0) {
             blen = shuffle(blen, batch_len - 1);
@@ -363,13 +369,15 @@ __device__ void snappy_decode_symbols(unsnap_state_s* s, uint32_t t)
                         (batch_len + t >= batch_size);
           batch_add = __ffs(ballot(is_long_sym)) - 1;
           if (batch_add != 0) {
-            uint32_t blen = 0;
-            int32_t ofs   = 0;
+            uint32_t blen           = 0;
+            int32_t ofs             = 0;
+            bool is_copy_offset_bad = false;
             if (t < batch_add) {
               blen                    = (b0 & 1) ? ((b0 >> 2) & 7) + 4 : ((b0 >> 2) + 1);
               ofs                     = (b0 & 1) ? ((b0 & 0xe0) << 3) | byte_access(s, cur_t + 1)
                                         : (b0 & 2) ? byte_access(s, cur_t + 1) | (byte_access(s, cur_t + 2) << 8)
                                                    : -(int32_t)(cur_t + 1);
+              is_copy_offset_bad      = (b0 & 3) != 0 && ofs == 0;
               b[batch_len + t].len    = blen;
               b[batch_len + t].offset = ofs;
               ofs += blen;  // for correct out-of-range detection below
@@ -377,7 +385,8 @@ __device__ void snappy_decode_symbols(unsnap_state_s* s, uint32_t t)
             blen           = warp_reduce_pos<cudf::detail::warp_size>(blen, t);
             bytes_left     = shuffle(bytes_left);
             dst_pos        = shuffle(dst_pos);
-            short_sym_mask = __ffs(ballot(blen > bytes_left || ofs > (int32_t)(dst_pos + blen)));
+            short_sym_mask = __ffs(
+              ballot(blen > bytes_left || ofs > (int32_t)(dst_pos + blen) || is_copy_offset_bad));
             if (short_sym_mask != 0) { batch_add = min(batch_add, short_sym_mask - 1); }
             if (batch_add != 0) {
               blen = shuffle(blen, batch_add - 1);
