1d241d40dc1194421ed91c190420ab3d214b55c9
Gil Forsyth <gforsyth@nvidia.com>
2026-04-29 09:58:58 -0400

    refactor: switch to `rapids-artifact-name` for consistent artifact naming
    fix: test `shared-workflows` post-build-check
    
    fix(artifact): set `--arch` manually for pure python packages
    
    chore: add env-var for explicit cuda version
    
    refactor: use artifact name with new streaming package
    
    fix(test.yaml): add package_name to conda-cpp-checks

--- ci/test_cpp.sh
+++ ci/test_cpp.sh
@@ -12,7 +12,6 @@ source ./ci/test_cpp_common.sh
 EXITCODE=0
 trap "EXITCODE=1" ERR
 set +e
-
 # Run libcudf and libcudf_kafka gtests from libcudf-tests package
 export GTEST_OUTPUT=xml:${RAPIDS_TESTS_DIR}/
 
