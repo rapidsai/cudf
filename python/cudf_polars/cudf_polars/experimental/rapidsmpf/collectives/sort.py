62cb286fa9ea91a2fa73500fb577e77562d30a91
rjzamora <rzamora217@gmail.com>
2026-05-01 07:56:23 -0700

    refactor sort_actor (round 2)

--- python/cudf_polars/cudf_polars/experimental/rapidsmpf/collectives/sort.py
+++ python/cudf_polars/cudf_polars/experimental/rapidsmpf/collectives/sort.py
@@ -337,16 +337,25 @@ async def _receive_and_buffer_chunks(
     return local_candidates_list
 
 
+async def _forward_from_chunk_store(
+    context: Context, ch_out: Channel[TableChunk], chunk_store: ChunkStore
+) -> None:
+    """Forward buffered messages from a ChunkStore into a channel."""
+    for msg in chunk_store:
+        await ch_out.send(context, msg)
+    await ch_out.drain(context)
+
+
 async def _insert_chunks_into_shuffle(
     context: Context,
     comm: Communicator,
+    ir: Sort,
+    ir_context: IRExecutionContext,
+    ch_in: Channel[TableChunk],
     num_partitions: int,
     collective_ids: list[int],
     metadata_in: ChannelMetadata,
-    chunk_store: ChunkStore,
     sort_boundaries_df: DataFrame,
-    ir: Sort,
-    ir_context: IRExecutionContext,
     by: list[str],
 ) -> tuple[ShuffleManager, Sort]:
     """Create shuffle manager and insert each buffered chunk with sort-based splits."""
@@ -364,7 +373,7 @@ async def _insert_chunks_into_shuffle(
         partition_assignment=PartitionAssignment.CONTIGUOUS,
     )
     async with shuffle.inserting() as inserter:
-        for msg in chunk_store:
+        while (msg := await ch_in.recv(context)) is not None:
             if skip_insert:
                 continue
             seq_num = msg.sequence_number
@@ -379,6 +388,8 @@ async def _insert_chunks_into_shuffle(
                 upstreams=(available_chunk.stream, sort_boundaries_df.stream),
             )
 
+            # TODO: Pre-sort chunks if they do not originate from the ChunkStore.
+            # (Not possible until we use _global_sort outside of sort_actor.)
             splits = find_sort_splits(
                 sort_cols_tbl,
                 sort_boundaries_df.table,
@@ -453,6 +464,52 @@ async def _extract_partitions_and_send(
     await ch_out.drain(context)
 
 
+async def _global_sort(
+    context: Context,
+    comm: Communicator,
+    ir: Sort,
+    ir_context: IRExecutionContext,
+    ch_out: Channel[TableChunk],
+    ch_in: Channel[TableChunk],
+    metadata_in: ChannelMetadata,
+    by: list[str],
+    num_partitions: int,
+    sort_boundaries_df: DataFrame,
+    collective_ids: list[int],
+    *,
+    tracer: ActorTracer | None,
+) -> None:
+    """Global sort."""
+    # TODO: Attach OrderScheme metadata here.
+    output_metadata = ChannelMetadata(
+        local_count=max(1, num_partitions // comm.nranks),
+        partitioning=Partitioning(inter_rank=None, local="inherit"),
+    )
+    await send_metadata(ch_out, context, output_metadata)
+
+    shuffle, post_sort_ir = await _insert_chunks_into_shuffle(
+        context,
+        comm,
+        ir,
+        ir_context,
+        ch_in,
+        num_partitions,
+        collective_ids,
+        metadata_in,
+        sort_boundaries_df,
+        by,
+    )
+    await _extract_partitions_and_send(
+        context,
+        ch_out,
+        shuffle,
+        post_sort_ir,
+        ir_context,
+        ir.schema,
+        tracer=tracer,
+    )
+
+
 @define_actor()
 async def sort_actor(
     context: Context,
@@ -467,10 +524,18 @@ async def sort_actor(
     collective_ids: list[int],
 ) -> None:
     """Streaming sort actor."""
-    ch_replay = context.create_channel()
+    ch_sample_replay = context.create_channel()
+    ch_chunk_store = context.create_channel()
     async with shutdown_on_error(
-        context, ch_in, ch_out, ch_replay, trace_ir=ir, ir_context=ir_context
+        context,
+        ch_in,
+        ch_out,
+        ch_sample_replay,
+        ch_chunk_store,
+        trace_ir=ir,
+        ir_context=ir_context,
     ) as tracer:
+        # TODO: Skip sort if OrderScheme metadata is present and compatible.
         metadata_in = await recv_metadata(ch_in, context)
 
         if ir.zlice is not None:
@@ -494,20 +559,19 @@ async def sort_actor(
             context, comm, ch_in, num_partitions, metadata_in, executor, collective_ids
         )
 
-        output_metadata = ChannelMetadata(
-            local_count=max(1, num_partitions // comm.nranks),
-            partitioning=Partitioning(inter_rank=None, local="inherit"),
-        )
-        await send_metadata(ch_out, context, output_metadata)
-
         chunk_store = ChunkStore(context)
         _, local_candidates_list = await gather_in_task_group(
             replay_buffered_channel(
-                context, ch_replay, ch_in, sampled_chunks, metadata_in, trace_ir=ir
+                context,
+                ch_sample_replay,
+                ch_in,
+                sampled_chunks,
+                metadata_in,
+                trace_ir=ir,
             ),
             _receive_and_buffer_chunks(
                 context,
-                ch_replay,
+                ch_sample_replay,
                 chunk_store,
                 ir,
                 by,
@@ -529,27 +593,22 @@ async def sort_actor(
             collective_ids.pop() if need_allgather else None,
         )
 
-        shuffle, post_sort_ir = await _insert_chunks_into_shuffle(
-            context,
-            comm,
-            num_partitions,
-            collective_ids,
-            metadata_in,
-            chunk_store,
-            sort_boundaries_df,
-            ir,
-            ir_context,
-            by,
-        )
-
-        await _extract_partitions_and_send(
-            context,
-            ch_out,
-            shuffle,
-            post_sort_ir,
-            ir_context,
-            ir.schema,
-            tracer=tracer,
+        await gather_in_task_group(
+            _forward_from_chunk_store(context, ch_chunk_store, chunk_store),
+            _global_sort(
+                context,
+                comm,
+                ir,
+                ir_context,
+                ch_out,
+                ch_chunk_store,
+                metadata_in,
+                by,
+                num_partitions,
+                sort_boundaries_df,
+                collective_ids,
+                tracer=tracer,
+            ),
         )
 
 
