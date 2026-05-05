7b85d4f4d36e6ce40e68d9c1c2a1eda943e1bc9b
Vyas Ramasubramani <vyasr@nvidia.com>
2026-05-05 11:10:48 -0700

    Fix and clean up rmm args

--- cpp/cmake/thirdparty/get_rmm.cmake
+++ cpp/cmake/thirdparty/get_rmm.cmake
@@ -9,19 +9,15 @@
 function(find_and_configure_rmm BUILD_SHARED EXCLUDE_FROM_ALL)
   include(${rapids-cmake-dir}/cpm/rmm.cmake)
 
-  if(EXCLUDE_FROM_ALL)
-    set(_exclude_flag EXCLUDE_FROM_ALL)
-  else()
-    set(_exclude_flag)
-  endif()
-
   # Find or install RMM.
   set(_rmm_args BUILD_EXPORT_SET cudf-exports)
-  if(NOT EXCLUDE_FROM_ALL)
+  if(EXCLUDE_FROM_ALL)
+    list(APPEND _rmm_args EXCLUDE_FROM_ALL)
+  else()
     list(APPEND _rmm_args INSTALL_EXPORT_SET cudf-exports)
   endif()
-  rapids_cpm_rmm(${_rmm_args} ${_exclude_flag} CPM_ARGS OPTIONS "BUILD_SHARED_LIBS ${BUILD_SHARED}")
+  rapids_cpm_rmm(${_rmm_args} CPM_ARGS OPTIONS "BUILD_SHARED_LIBS ${BUILD_SHARED}")
 
 endfunction()
 
-find_and_configure_rmm(${CUDF_DEPS_BUILD_SHARED} ${CUDF_EXCLUDE_DEPS_FROM_ALL})
+find_and_configure_rmm(${CUDF_DEPS_BUILD_SHARED})
