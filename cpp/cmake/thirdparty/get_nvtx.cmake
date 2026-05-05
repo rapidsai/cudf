039fbf7d338297067b0fbc22b770a6720b30227f
Vyas Ramasubramani <vyasr@nvidia.com>
2026-05-05 10:47:17 -0700

    Just export nvtx for now

--- cpp/cmake/thirdparty/get_nvtx.cmake
+++ cpp/cmake/thirdparty/get_nvtx.cmake
@@ -10,6 +10,10 @@
 function(find_and_configure_nvtx)
   include(${rapids-cmake-dir}/cpm/nvtx3.cmake)
 
+  # TODO: nvtx is linked privately, so we shouldn't have to export it, but it is part of rmm's
+  # public export set and we run into ordering/first find wins issues if we don't export it in cudf
+  # as well. We should be able to remove this once we have a better solution for handling rmm's
+  # export sets consistently from cudf.
   rapids_cpm_nvtx3(
     BUILD_EXPORT_SET cudf-exports INSTALL_EXPORT_SET cudf-exports
                                                      ${CUDF_EXCLUDE_DEPS_FROM_ALL_FLAG}
