0a10efd2085f3f19bb1b57906b5f8e2079c0c54f
Mads R. B. Kristensen <madsbk@gmail.com>
2026-05-13 14:45:45 +0200

    remove remove assert_collect_raises()

--- python/cudf_polars/tests/testing/test_asserts.py
+++ python/cudf_polars/tests/testing/test_asserts.py
@@ -16,7 +16,6 @@ from cudf_polars.experimental.benchmarks.asserts import (
     assert_tpch_result_equal,
 )
 from cudf_polars.testing.asserts import (
-    assert_collect_raises,
     assert_gpu_result_equal,
     assert_ir_translation_raises,
     assert_sink_ir_translation_raises,
@@ -53,55 +52,6 @@ def test_translation_assert_raises(engine: pl.GPUEngine):
     with pytest.raises(AssertionError):
         # This should fail, because we can't translate this query, but it doesn't raise E.
         assert_ir_translation_raises(unsupported, E)
-
-
-def test_collect_assert_raises():
-    df = pl.LazyFrame({"a": [1, 2, 3], "b": ["a", "b", "c"]})
-
-    with pytest.raises(AssertionError, match="CPU execution DID NOT RAISE"):
-        # This should raise, because polars CPU can run this query,
-        # but we expect an error.
-        assert_collect_raises(
-            df,
-            polars_except=pl.exceptions.InvalidOperationError,
-            cudf_except=(),
-        )
-
-    with pytest.raises(AssertionError, match="GPU execution DID NOT RAISE"):
-        # This should raise, because polars GPU can run this query,
-        # but we expect an error.
-        assert_collect_raises(
-            df,
-            polars_except=(),
-            cudf_except=pl.exceptions.InvalidOperationError,
-        )
-
-    # Here's an invalid query that gets caught at IR optimisation time.
-    q = df.select(pl.col("a") * pl.col("b"))
-
-    # This exception is raised in preprocessing, so is the same for
-    # both CPU and GPU engines.
-    assert_collect_raises(
-        q,
-        polars_except=pl.exceptions.InvalidOperationError,
-        cudf_except=pl.exceptions.InvalidOperationError,
-    )
-
-    with pytest.raises(AssertionError, match="GPU execution RAISED"):
-        # This should raise because the expected GPU error is wrong
-        assert_collect_raises(
-            q,
-            polars_except=pl.exceptions.InvalidOperationError,
-            cudf_except=NotImplementedError,
-        )
-
-    with pytest.raises(AssertionError, match="CPU execution RAISED"):
-        # This should raise because the expected CPU error is wrong
-        assert_collect_raises(
-            q,
-            polars_except=NotImplementedError,
-            cudf_except=pl.exceptions.InvalidOperationError,
-        )
 
 
 def test_sink_ir_translation_raises_bad_extension():
