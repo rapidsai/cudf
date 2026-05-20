c294e310c520745b0cb9cbfa41636c537b18a4c1
joannam <joannam@nvidia.com>
2026-05-19 21:48:15 -0400

    migrate interop

--- cpp/src/interop/arrow_data_structures.cpp
+++ cpp/src/interop/arrow_data_structures.cpp
@@ -294,7 +294,7 @@ void arrow_column::to_arrow(ArrowDeviceArray* output,
 column_view arrow_column::view() const { return cached_view; }
 
 arrow_table::arrow_table(cudf::table&& input,
-                         cudf::host_span<column_metadata const> metadata,
+                         std::span<column_metadata const> metadata,
                          rmm::cuda_stream_view stream,
                          rmm::device_async_resource_ref mr)
   : container{[&]() {
