b210af6b24ce4e3d5c7c3c21de00d21e071b89bc
joannam <joannam@nvidia.com>
2026-05-28 09:32:01 -0400

    copyright

--- cpp/src/interop/arrow_data_structures.cpp
+++ cpp/src/interop/arrow_data_structures.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 
