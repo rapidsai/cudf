da536670578ca70dabd10c81fb3bbb7681aec7a1
Basit Ayantunde <rlamarrr@gmail.com>
2026-05-29 11:13:00 +0000

    Add all.cuh header file with operator includes

---
+++ cpp/include/cudf/operators/all.cuh
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <cudf/operators/ansi_arithmetic.cuh>
+#include <cudf/operators/arithmetic.cuh>
+#include <cudf/operators/bitwise.cuh>
+#include <cudf/operators/casts.cuh>
+#include <cudf/operators/comparison.cuh>
+#include <cudf/operators/error.hpp>
+#include <cudf/operators/identity.cuh>
+#include <cudf/operators/logic.cuh>
+#include <cudf/operators/math.cuh>
+#include <cudf/operators/null_handling.cuh>
+#include <cudf/operators/trigonometric.cuh>
