7b802c72bb991302381743510e9d6f9c6eea3eac
David Wendt <45795991+davidwendt@users.noreply.github.com>
2026-05-29 16:46:38 +0000

    Remove deprecated nvtext::edit_distance_matrix (#22644)
    Removes the `nvtext::edit_distance_matrix()` API  and pylibcudf and python equivalents deprecated in 26.04.
    
    Authors:
      - David Wendt (https://github.com/davidwendt)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - Igor Peshansky (https://github.com/igorpeshansky)
    
    URL: https://github.com/rapidsai/cudf/pull/22644

--- python/cudf/cudf/core/accessors/string.py
+++ python/cudf/cudf/core/accessors/string.py
@@ -5371,45 +5371,6 @@ class StringMethods(BaseAccessor):
             self._column.edit_distance(targets_column)  # type: ignore[arg-type]
         )
 
-    def edit_distance_matrix(self) -> Series | Index:
-        """Computes the edit distance between strings in the series.
-
-        The series to compute the matrix should have more than 2 strings and
-        should not contain nulls.
-
-        Edit distance is measured based on the `Levenshtein edit distance
-        algorithm <https://www.cuelogic.com/blog/the-levenshtein-algorithm>`_.
-
-        Returns
-        -------
-        Series of ListDtype(int64)
-            Assume ``N`` is the length of this series. The return series
-            contains ``N`` lists of size ``N``, where the ``j`` th number in
-            the ``i`` th row of the series tells the edit distance between the
-            ``i`` th string and the ``j`` th string of this series.  The matrix
-            is symmetric. Diagonal elements are 0.
-
-        Examples
-        --------
-        >>> import cudf
-        >>> s = cudf.Series(['abc', 'bc', 'cba'])
-        >>> s.str.edit_distance_matrix()
-        0    [0, 1, 2]
-        1    [1, 0, 2]
-        2    [2, 2, 0]
-        dtype: list
-        """
-        if self._column.size < 2:
-            raise ValueError(
-                "Require size >= 2 to compute edit distance matrix."
-            )
-        if self._column.has_nulls():
-            raise ValueError(
-                "Cannot compute edit distance between null strings. "
-                "Consider removing them using `dropna` or fill with `fillna`."
-            )
-        return self._return_or_inplace(self._column.edit_distance_matrix())
-
     def minhash(
         self, seed: int, a: ColumnLike, b: ColumnLike, width: int
     ) -> Series | Index:
