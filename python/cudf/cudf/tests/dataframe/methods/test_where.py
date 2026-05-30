0a1e247345ea802064c811411a2c10864dae8fc5
Aryan Srivastava <your_github_email@example.com>
2026-05-30 14:45:36 +0530

    Apply ruff formatting

--- python/cudf/cudf/tests/dataframe/methods/test_where.py
+++ python/cudf/cudf/tests/dataframe/methods/test_where.py
@@ -116,9 +116,7 @@ def test_frame_series_where_other():
 
 @pytest.mark.parametrize("method", ["where", "mask"])
 def test_frame_series_condition_nondefault_index(method):
-    pdf = pd.DataFrame(
-        {"a": [1, 2], "b": [3, 4]}, index=["i0", "i1"]
-    )
+    pdf = pd.DataFrame({"a": [1, 2], "b": [3, 4]}, index=["i0", "i1"])
     gdf = cudf.from_pandas(pdf)
     pcond = pd.Series([True, False], index=pdf.index)
     gcond = cudf.from_pandas(pcond)
