7a5dc7ada3fda9fa9ec24dbfedc90a903a09cd3b
Vyas Ramasubramani <vyasr@nvidia.com>
2026-06-02 00:37:25 +0000

    feat(libcudf_streaming): add C++ tests

---
+++ cpp/libcudf_streaming/tests/environment.hpp
@@ -0,0 +1,39 @@
+/**
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights
+ * reserved. SPDX-License-Identifier: Apache-2.0
+ */
+#pragma once
+
+#include <gtest/gtest.h>
+#include <rapidsmpf/communicator/communicator.hpp>
+
+enum class TestEnvironmentType : int {
+  SINGLE,
+};
+
+class Environment : public ::testing::Environment {
+ public:
+  Environment(int argc, char** argv);
+
+  void SetUp() override;
+
+  void TearDown() override;
+
+  void barrier();
+
+  [[nodiscard]] TestEnvironmentType type() const;
+
+  constexpr rapidsmpf::config::Options& options() { return options_; }
+
+  std::shared_ptr<rapidsmpf::Communicator> split_comm();
+
+  std::shared_ptr<rapidsmpf::Communicator> comm_;
+
+ private:
+  int argc_;
+  char** argv_;
+  std::shared_ptr<rapidsmpf::Communicator> split_comm_{nullptr};
+  rapidsmpf::config::Options options_;
+};
+
+extern Environment* GlobalEnvironment;
