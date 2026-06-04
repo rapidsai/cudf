903bf7a435e1d82da1611573e4183cd8b62735d9
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2026-06-04 09:06:45 -0700

    Enable chunked row masks in hybrid scan reader (#22716)
    Closes #22672
    
    This PR enables the hybrid scan reader to accept and use chunked row masks to support two-step materialization of more than `std::numeric_limits<cudf::size_type>::max()` rows. Row mask columns (or their views) must be chunked across the same row boundaries as row group passes being materialized.
    
    Authors:
      - Muhammad Haseeb (https://github.com/mhaseeb123)
    
    Approvers:
      - Qi Chen (https://github.com/qbacpey)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/cudf/pull/22716

--- cpp/src/io/parquet/experimental/hybrid_scan_impl.hpp
+++ cpp/src/io/parquet/experimental/hybrid_scan_impl.hpp
@@ -523,7 +523,7 @@ class hybrid_scan_reader_impl : public parquet::detail::reader_impl {
    */
   [[nodiscard]] bool is_first_output_chunk() const
   {
-    return _file_itm_data._output_chunk_count == 0 and _rows_processed_so_far == 0;
+    return _file_itm_data._output_chunk_count == 0 and not _output_chunk_produced;
   }
 
  private:
@@ -531,7 +531,8 @@ class hybrid_scan_reader_impl : public parquet::detail::reader_impl {
 
   std::optional<std::vector<std::string>> _filter_columns_names;
 
-  cudf::size_type _rows_processed_so_far{0};
+  cudf::size_type _row_mask_offset{0};
+  bool _output_chunk_produced{false};
 
   bool _use_pandas_metadata{false};
 
