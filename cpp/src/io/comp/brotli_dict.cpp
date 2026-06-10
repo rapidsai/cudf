35ab3a9d2a8217fe076c0ec72d4ad390a3791bcf
Yunsong Wang <yunsongw@nvidia.com>
2026-06-10 02:12:05 +0000

    Update copyright years

--- cpp/src/io/comp/brotli_dict.cpp
+++ cpp/src/io/comp/brotli_dict.cpp
@@ -1,7 +1,7 @@
 /*
  * SPDX-FileCopyrightText: Copyright 2013 Google Inc. All Rights Reserved.
  * SPDX-FileCopyrightText: Copyright(c) 2009, 2010, 2013 - 2016 by the Brotli Authors.
- * SPDX-FileCopyrightText: Copyright (c) 2018-2025, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2018-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0 AND MIT
  */
 
