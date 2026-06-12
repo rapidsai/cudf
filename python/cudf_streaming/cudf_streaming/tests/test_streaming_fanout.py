d59ba979374ae61e2476b44bb7aa07832d45573c
Vyas Ramasubramani <vyasr@nvidia.com>
2026-06-11 22:42:08 -0700

    Prepare librtcx for standalone usage (#22856)
    ## Description
    <!-- Provide a standalone description of changes in this PR. -->
    <!-- Reference any issues closed by this PR with "closes #1234". -->
    <!-- Note: The pull request title will be included in the CHANGELOG. -->
    This PR makes a number of changes to rtcx's CMake to support using an
    out-of-tree version of it.
    
    ## Checklist
    - [x] I am familiar with the [Contributing
    Guidelines](https://github.com/rapidsai/cudf/blob/HEAD/CONTRIBUTING.md).
    - [x] New or existing tests cover these changes.
    - [x] The documentation is up to date with these changes.

--- python/cudf_streaming/cudf_streaming/tests/test_streaming_fanout.py
+++ python/cudf_streaming/cudf_streaming/tests/test_streaming_fanout.py
@@ -11,6 +11,7 @@ import pylibcudf as plc
 import pytest
 
 from cudf_streaming.streaming.table_chunk import TableChunk
+from cudf_streaming.testing import assert_eq
 from rapidsmpf.streaming.core.actor import run_actor_network
 from rapidsmpf.streaming.core.fanout import FanoutPolicy, fanout
 from rapidsmpf.streaming.core.leaf_actor import (
@@ -18,7 +19,6 @@ from rapidsmpf.streaming.core.leaf_actor import (
     push_to_channel,
 )
 from rapidsmpf.streaming.core.message import Message
-from rapidsmpf.testing import assert_eq
 
 _INT64 = plc.DataType(plc.TypeId.INT64)
 
