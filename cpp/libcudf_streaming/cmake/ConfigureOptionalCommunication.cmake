558a74dcca2e75d1e0547f4d284575b331809cc6
Peter Andreas Entschev <peter@entschev.com>
2026-06-12 05:43:40 -0700

    Separate logic to find and have support for MPI/UCXX

--- cpp/libcudf_streaming/cmake/ConfigureOptionalCommunication.cmake
+++ cpp/libcudf_streaming/cmake/ConfigureOptionalCommunication.cmake
@@ -7,6 +7,8 @@
 
 include_guard(GLOBAL)
 
+set(CUDF_STREAMING_FOUND_MPI OFF)
+set(CUDF_STREAMING_FOUND_UCXX OFF)
 set(CUDF_STREAMING_HAVE_MPI OFF)
 set(CUDF_STREAMING_HAVE_UCXX OFF)
 
@@ -17,7 +19,7 @@ if(BUILD_BENCHMARKS OR BUILD_EXAMPLES)
     INSTALL_EXPORT_SET cudf_streaming-exports
   )
   if(TARGET MPI::MPI_CXX)
-    set(CUDF_STREAMING_HAVE_MPI ON)
+    set(CUDF_STREAMING_FOUND_MPI ON)
   endif()
 endif()
 
@@ -28,31 +30,81 @@ if(BUILD_BENCHMARKS)
     INSTALL_EXPORT_SET cudf_streaming-exports
   )
   if(TARGET ucxx::ucxx)
-    set(CUDF_STREAMING_HAVE_UCXX ON)
+    set(CUDF_STREAMING_FOUND_UCXX ON)
   endif()
 endif()
 
-set(CUDF_STREAMING_HAVE_COMM OFF)
-if(CUDF_STREAMING_HAVE_MPI OR CUDF_STREAMING_HAVE_UCXX)
-  set(CUDF_STREAMING_HAVE_COMM ON)
-endif()
+# Check whether rapidsmpf exports a compile definition for an optional feature.
+function(cudf_streaming_rapidsmpf_exports_feature feature result)
+  set(has_feature OFF)
+  get_target_property(rapidsmpf_compile_definitions rapidsmpf::rapidsmpf INTERFACE_COMPILE_DEFINITIONS)
+  if(rapidsmpf_compile_definitions)
+    foreach(definition IN LISTS rapidsmpf_compile_definitions)
+      if(definition STREQUAL "${feature}")
+        set(has_feature ON)
+      elseif(definition MATCHES "^\\$<\\$<BOOL:([^>]*)>:${feature}>$")
+        set(feature_condition "${CMAKE_MATCH_1}")
+        if(feature_condition)
+          set(has_feature ON)
+        endif()
+      endif()
+    endforeach()
+  endif()
+  set(${result}
+      "${has_feature}"
+      PARENT_SCOPE
+  )
+endfunction()
 
-add_library(cudf_streaming_optional_communication INTERFACE)
-target_compile_definitions(
-  cudf_streaming_optional_communication
-  INTERFACE $<$<BOOL:${CUDF_STREAMING_HAVE_MPI}>:CUDF_STREAMING_HAVE_MPI>
-            $<$<BOOL:${CUDF_STREAMING_HAVE_UCXX}>:CUDF_STREAMING_HAVE_UCXX>
-)
-if(CUDF_STREAMING_HAVE_MPI)
-  target_link_libraries(cudf_streaming_optional_communication INTERFACE MPI::MPI_CXX)
-endif()
-if(CUDF_STREAMING_HAVE_UCXX)
-  target_link_libraries(cudf_streaming_optional_communication INTERFACE ucxx::ucxx)
-endif()
-add_library(cudf_streaming::optional_communication ALIAS cudf_streaming_optional_communication)
+# Configure the optional communication target after rapidsmpf reports exported feature support.
+function(cudf_streaming_configure_optional_communication)
+  set(CUDF_STREAMING_HAVE_MPI OFF)
+  set(CUDF_STREAMING_HAVE_UCXX OFF)
+  set(CUDF_STREAMING_HAVE_COMM OFF)
 
-message(STATUS "CUDF_STREAMING: MPI support: ${CUDF_STREAMING_HAVE_MPI}")
-message(STATUS "CUDF_STREAMING: UCXX support: ${CUDF_STREAMING_HAVE_UCXX}")
+  cudf_streaming_rapidsmpf_exports_feature(RAPIDSMPF_HAVE_MPI rapidsmpf_have_mpi)
+  cudf_streaming_rapidsmpf_exports_feature(RAPIDSMPF_HAVE_UCXX rapidsmpf_have_ucxx)
+
+  if(CUDF_STREAMING_FOUND_MPI AND rapidsmpf_have_mpi)
+    set(CUDF_STREAMING_HAVE_MPI ON)
+  endif()
+  if(CUDF_STREAMING_FOUND_UCXX AND rapidsmpf_have_ucxx)
+    set(CUDF_STREAMING_HAVE_UCXX ON)
+  endif()
+  if(CUDF_STREAMING_HAVE_MPI OR CUDF_STREAMING_HAVE_UCXX)
+    set(CUDF_STREAMING_HAVE_COMM ON)
+  endif()
+
+  add_library(cudf_streaming_optional_communication INTERFACE)
+  target_compile_definitions(
+    cudf_streaming_optional_communication
+    INTERFACE $<$<BOOL:${CUDF_STREAMING_HAVE_MPI}>:CUDF_STREAMING_HAVE_MPI>
+              $<$<BOOL:${CUDF_STREAMING_HAVE_UCXX}>:CUDF_STREAMING_HAVE_UCXX>
+  )
+  if(CUDF_STREAMING_HAVE_MPI)
+    target_link_libraries(cudf_streaming_optional_communication INTERFACE MPI::MPI_CXX)
+  endif()
+  if(CUDF_STREAMING_HAVE_UCXX)
+    target_link_libraries(cudf_streaming_optional_communication INTERFACE ucxx::ucxx)
+  endif()
+  add_library(cudf_streaming::optional_communication ALIAS cudf_streaming_optional_communication)
+
+  message(STATUS "CUDF_STREAMING: MPI support: ${CUDF_STREAMING_HAVE_MPI}")
+  message(STATUS "CUDF_STREAMING: UCXX support: ${CUDF_STREAMING_HAVE_UCXX}")
+
+  set(CUDF_STREAMING_HAVE_MPI
+      "${CUDF_STREAMING_HAVE_MPI}"
+      PARENT_SCOPE
+  )
+  set(CUDF_STREAMING_HAVE_UCXX
+      "${CUDF_STREAMING_HAVE_UCXX}"
+      PARENT_SCOPE
+  )
+  set(CUDF_STREAMING_HAVE_COMM
+      "${CUDF_STREAMING_HAVE_COMM}"
+      PARENT_SCOPE
+  )
+endfunction()
 
 # Emit a standardized status message for targets disabled by unavailable optional dependencies.
 function(cudf_streaming_skip_optional_target target reason)
