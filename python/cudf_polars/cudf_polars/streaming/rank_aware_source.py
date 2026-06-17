d1214fedaa3e20b8416f8183250ff1913c527853
Mads R. B. Kristensen <madsbk@gmail.com>
2026-06-17 11:06:07 +0200

    style

--- python/cudf_polars/cudf_polars/streaming/rank_aware_source.py
+++ python/cudf_polars/cudf_polars/streaming/rank_aware_source.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 """Rank-aware Python scan source for cudf-polars."""
 
