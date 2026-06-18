d8f5f9c7371e8a7897751ec5b0efb762182ea920
galipremsagar <sagarprem75@gmail.com>
2026-06-18 19:42:04 +0000

    Use canonical copyright notice to fix style check
    Update SPDX copyright lines to the canonical 'NVIDIA CORPORATION & AFFILIATES. All rights reserved.' form expected by the verify-copyright pre-commit hook.

--- python/cudf/cudf/tests/series/test_attributes.py
+++ python/cudf/cudf/tests/series/test_attributes.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 import re
 
