9110d0934e6932422463e3087f7a7269d8751864
Basit Ayantunde <rlamarrr@gmail.com>
2026-06-22 18:36:41 +0000

    Add checked arithmetic operations with overflow detection

--- cpp/include/cudf/detail/operators/operators.cuh
+++ cpp/include/cudf/detail/operators/operators.cuh
@@ -5,10 +5,10 @@
 
 #pragma once
 
-#include <cudf/detail/operators/ansi_arithmetic.cuh>
 #include <cudf/detail/operators/arithmetic.cuh>
 #include <cudf/detail/operators/bitwise.cuh>
 #include <cudf/detail/operators/casts.cuh>
+#include <cudf/detail/operators/checked_arithmetic.cuh>
 #include <cudf/detail/operators/comparison.cuh>
 #include <cudf/detail/operators/identity.cuh>
 #include <cudf/detail/operators/logic.cuh>
