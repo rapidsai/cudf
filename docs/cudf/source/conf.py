15049ef00ade7dabe497b47b3196cf04dc758c08
Vyas Ramasubramani <vyasr@nvidia.com>
2026-06-23 08:23:27 -0700

    Fix copyright notice in docs/cudf/source/conf.py

--- docs/cudf/source/conf.py
+++ docs/cudf/source/conf.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2018-2026, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2018-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # cudf documentation build configuration file, created by
