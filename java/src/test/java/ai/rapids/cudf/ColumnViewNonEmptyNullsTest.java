76dd4588596880c3700bb024134b341991e3bd18
Rishi Chandra <rishic@nvidia.com>
2026-06-30 08:41:18 -0700

    fix license headers

--- java/src/test/java/ai/rapids/cudf/ColumnViewNonEmptyNullsTest.java
+++ java/src/test/java/ai/rapids/cudf/ColumnViewNonEmptyNullsTest.java
@@ -1,6 +1,6 @@
 /*
  *
- *  SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
+ *  SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  *  SPDX-License-Identifier: Apache-2.0
  *
  */
