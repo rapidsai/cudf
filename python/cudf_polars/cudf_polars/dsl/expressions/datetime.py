5a1e417cf3157c3ce81677d18efdc48a597dbed7
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2026-07-08 22:15:10 +0000

    Use astype instead

--- python/cudf_polars/cudf_polars/dsl/expressions/datetime.py
+++ python/cudf_polars/cudf_polars/dsl/expressions/datetime.py
@@ -236,13 +236,9 @@ class TemporalFunction(Expr):
         elif self.name is TemporalFunction.Name.DaysInMonth:
             (column,) = columns
             return Column(
-                plc.unary.cast(
-                    plc.datetime.days_in_month(column.obj, stream=df.stream),
-                    self.dtype.plc_type,
-                    stream=df.stream,
-                ),
-                dtype=self.dtype,
-            )
+                plc.datetime.days_in_month(column.obj, stream=df.stream),
+                dtype=DataType(pl.Int16()),
+            ).astype(self.dtype, stream=df.stream)
         elif self.name in self._CENTURY_MILLENNIUM_DIVISOR:
             (column,) = columns
             int32 = plc.DataType(plc.TypeId.INT32)
