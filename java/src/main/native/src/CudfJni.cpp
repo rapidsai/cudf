3aeba52574bf051eaf02ac2760237d757c405dbe
Haoyang Li <haoyangl@nvidia.com>
2026-07-20 10:29:41 +0800

    style
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- java/src/main/native/src/CudfJni.cpp
+++ java/src/main/native/src/CudfJni.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2019-2026, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2019-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
