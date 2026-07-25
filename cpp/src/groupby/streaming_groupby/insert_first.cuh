eb94e925f9b98a131361afb23c133ec7b1087eb9
Yunsong Wang <yunsongw@nvidia.com>
2026-07-25 00:36:44 +0000

    Reduce streaming insert build time with indirect equality

--- cpp/src/groupby/streaming_groupby/insert_first.cuh
+++ cpp/src/groupby/streaming_groupby/insert_first.cuh
@@ -7,30 +7,26 @@
 
 #include "common.cuh"
 
-#include <cudf/detail/device_scalar.hpp>
+#include <cudf/detail/utilities/cuda_memcpy.hpp>
 #include <cudf/utilities/memory_resource.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/device_buffer.hpp>
+#include <rmm/exec_policy.hpp>
 
-#include <cub/device/dispatch/dispatch_select_if.cuh>
 #include <cuda/iterator>
+#include <thrust/copy.h>
 
 namespace cudf::groupby {
 
-// Limit the number of copies of the state-heavy insert predicate in each CUB agent. This retains
-// stable, single-pass selection while reducing register pressure and device-code optimization time.
-struct first_batch_select_policy {
-  struct Policy900 : cub::ChainedPolicy<900, Policy900, Policy900> {
-    using SelectIfPolicyT = cub::AgentSelectIfPolicy<128,
-                                                     5,
-                                                     cub::BLOCK_LOAD_DIRECT,
-                                                     cub::LOAD_DEFAULT,
-                                                     cub::BLOCK_SCAN_WARP_SCANS,
-                                                     cub::detail::no_delay_constructor_t<0>>;
-  };
+template <typename RowEquality>
+struct indirect_row_equality {
+  RowEquality const* row_equal;
 
-  using MaxPolicy = Policy900;
+  __attribute__((noinline)) __device__ bool operator()(size_type lhs, size_type rhs) const noexcept
+  {
+    return (*row_equal)(lhs, rhs);
+  }
 };
 
 template <bool has_nested>
@@ -47,52 +43,31 @@ size_type streaming_groupby::impl::probe_and_insert_first_batch(
 {
   auto const temp_mr        = cudf::get_current_device_resource_ref();
   auto const batch_self_cmp = cudf::detail::row::equality::self_comparator{preprocessed_batch};
-  auto const batch_self_eq  = batch_self_cmp.equal_to<has_nested>(has_null, null_equality::EQUAL);
-  auto const hasher         = offset_cache_hasher{batch_hash_cache, _max_distinct_keys};
-  auto const set_ref_base   = _key_set->ref(cuco::op::insert_and_find).rebind_hash_function(hasher);
-  auto const first_batch_cmp = first_batch_comparator{batch_self_eq, _max_distinct_keys};
-  auto* const base           = _key_set->data();
-  auto const input           = cuda::counting_iterator<size_type>(0);
-  auto const predicate       = insert_and_check_fn{set_ref_base.rebind_key_eq(first_batch_cmp),
-                                             batch_bitmask,
-                                             _max_distinct_keys,
-                                             base,
-                                             target_indices,
-                                             slot_offsets};
-  cudf::detail::device_scalar<size_type> output_count(0, stream, temp_mr);
+  auto batch_self_eq        = batch_self_cmp.equal_to<has_nested>(has_null, null_equality::EQUAL);
+  rmm::device_buffer d_batch_self_eq(sizeof(batch_self_eq), stream, temp_mr);
+  auto* const d_batch_self_eq_ptr = static_cast<decltype(batch_self_eq)*>(d_batch_self_eq.data());
+  cudf::detail::cuda_memcpy_async(
+    cudf::device_span<decltype(batch_self_eq)>{d_batch_self_eq_ptr, 1},
+    cudf::host_span<decltype(batch_self_eq) const>{&batch_self_eq, 1},
+    stream);
+  auto const hasher       = offset_cache_hasher{batch_hash_cache, _max_distinct_keys};
+  auto const set_ref_base = _key_set->ref(cuco::op::insert_and_find).rebind_hash_function(hasher);
+  auto const first_batch_cmp = first_batch_comparator{
+    indirect_row_equality<decltype(batch_self_eq)>{d_batch_self_eq_ptr}, _max_distinct_keys};
+  auto* const base = _key_set->data();
 
-  using dispatch_t = cub::DispatchSelectIf<decltype(input),
-                                           cub::NullType*,
-                                           size_type*,
-                                           size_type*,
-                                           decltype(predicate),
-                                           cub::NullType,
-                                           size_type,
-                                           cub::SelectImpl::Select,
-                                           first_batch_select_policy>;
-  std::size_t temporary_storage_bytes{};
-  CUDF_CUDA_TRY(dispatch_t::Dispatch(nullptr,
-                                     temporary_storage_bytes,
-                                     input,
-                                     nullptr,
-                                     batch_local_indices,
-                                     output_count.data(),
-                                     predicate,
-                                     cub::NullType{},
-                                     batch_size,
-                                     stream.value()));
-  rmm::device_buffer temporary_storage(temporary_storage_bytes, stream, temp_mr);
-  CUDF_CUDA_TRY(dispatch_t::Dispatch(temporary_storage.data(),
-                                     temporary_storage_bytes,
-                                     input,
-                                     nullptr,
-                                     batch_local_indices,
-                                     output_count.data(),
-                                     predicate,
-                                     cub::NullType{},
-                                     batch_size,
-                                     stream.value()));
-  return output_count.value(stream);
+  auto const out_end =
+    thrust::copy_if(rmm::exec_policy_nosync(stream, temp_mr),
+                    cuda::counting_iterator<size_type>(0),
+                    cuda::counting_iterator<size_type>(batch_size),
+                    batch_local_indices,
+                    insert_and_check_fn{set_ref_base.rebind_key_eq(first_batch_cmp),
+                                        batch_bitmask,
+                                        _max_distinct_keys,
+                                        base,
+                                        target_indices,
+                                        slot_offsets});
+  return static_cast<size_type>(out_end - batch_local_indices);
 }
 
 }  // namespace cudf::groupby
