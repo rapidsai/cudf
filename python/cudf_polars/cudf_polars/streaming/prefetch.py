afe910e74db47fffa3b3b25f0c96375bf893bedc
Matthew Murray <matthewmurray711@gmail.com>
2026-08-03 15:16:35 +0000

    make hybrid scan stats pruning option private

--- python/cudf_polars/cudf_polars/streaming/prefetch.py
+++ python/cudf_polars/cudf_polars/streaming/prefetch.py
@@ -173,7 +173,7 @@ def _plan_hybrid_scan_prefetch(
 
     reader = cached_info[0].hybrid_scan_reader(id(scan.base_scan), options)
 
-    if scan.parquet_options.hybrid_scan_stats_pruning:
+    if scan.parquet_options._hybrid_scan_stats_pruning:
         with nvtx_annotate_cudf_polars(message="filter_row_groups_with_stats"):
             row_group_indices = reader.filter_row_groups_with_stats(
                 row_group_indices, options, stream=stream
