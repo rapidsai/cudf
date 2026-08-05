401c60e3945f2492c0ce13583809062e71478219
Yunsong Wang <yunsongw@nvidia.com>
2026-08-05 18:47:01 +0000

    Update copyright notices

--- cpp/src/text/deduplicate.cu
+++ cpp/src/text/deduplicate.cu
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
