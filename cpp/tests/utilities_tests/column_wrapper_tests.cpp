3f518afe0621cc5108ee8d6c7768543ad0aa4635
niranda perera <niranda.perera@gmail.com>
2026-08-07 16:26:23 -0700

    fix wrapper tests
    Signed-off-by: niranda perera <niranda.perera@gmail.com>

--- cpp/tests/utilities_tests/column_wrapper_tests.cpp
+++ cpp/tests/utilities_tests/column_wrapper_tests.cpp
@@ -24,74 +24,6 @@ auto const uses_temporary = cudf::test::memory_resource_expectations{
   cudf::test::output_allocation_expectation::EXACT, temporary_allocation_expectation::SOME};
 }  // namespace
 
-TEST(FixedWidthColumnWrapperMemoryResourceTest, DistinctOutputAndTemporaryResources)
-{
-  auto stream         = cudf::test::get_default_stream();
-  auto const elements = std::vector<int32_t>{1, 2, 3, 4};
-  auto const validity = std::vector<bool>{true, false, true, false};
-
-  expect_output_uses_distinct_resources(
-    [&](auto mr) { return cudf::test::fixed_width_column_wrapper<int32_t>(stream, mr); });
-
-  expect_output_uses_distinct_resources([&](auto mr) {
-    return cudf::test::fixed_width_column_wrapper<int32_t>(
-      elements.begin(), elements.end(), stream, mr);
-  });
-
-  expect_output_uses_distinct_resources([&](auto mr) {
-    return cudf::test::fixed_width_column_wrapper<int32_t>(
-      {1, 2, 3, 4}, stream, mr.get_output_mr());
-  });
-
-  expect_output_uses_distinct_resources([&](auto mr) {
-    return cudf::test::fixed_width_column_wrapper<int32_t>(
-      elements.begin(), elements.end(), validity.begin(), stream, mr);
-  });
-
-  expect_output_uses_distinct_resources([&](auto mr) {
-    return cudf::test::fixed_width_column_wrapper<int32_t>(
-      {1, 2, 3, 4}, {true, false, true, false}, stream, mr);
-  });
-
-  expect_output_uses_distinct_resources([&](auto mr) {
-    return cudf::test::fixed_width_column_wrapper<int32_t>(
-      {1, 2, 3, 4}, validity.begin(), stream, mr);
-  });
-
-  expect_output_uses_distinct_resources([&](auto mr) {
-    return cudf::test::fixed_width_column_wrapper<int32_t>(
-      elements.begin(), elements.end(), {true, false, true, false}, stream, mr);
-  });
-
-  expect_output_uses_distinct_resources([&](auto mr) {
-    using pair_type = std::pair<int32_t, bool>;
-    return cudf::test::fixed_width_column_wrapper<int32_t>(
-      {pair_type{1, true}, pair_type{2, false}, pair_type{3, true}}, stream, mr);
-  });
-}
-
-TEST(FixedWidthColumnWrapperMemoryResourceTest, FixedPointElementPaths)
-{
-  auto stream     = cudf::test::get_default_stream();
-  using decimal32 = numeric::decimal32;
-
-  auto const reps     = std::vector<int32_t>{1, 2, 3, 4};
-  auto const decimals = std::vector<decimal32>{decimal32{1, numeric::scale_type{0}},
-                                               decimal32{2, numeric::scale_type{0}},
-                                               decimal32{3, numeric::scale_type{0}},
-                                               decimal32{4, numeric::scale_type{0}}};
-
-  expect_output_uses_distinct_resources([&](auto mr) {
-    return cudf::test::fixed_width_column_wrapper<decimal32, int32_t>(
-      reps.begin(), reps.end(), stream, mr);
-  });
-
-  expect_output_uses_distinct_resources([&](auto mr) {
-    return cudf::test::fixed_width_column_wrapper<decimal32>(
-      decimals.begin(), decimals.end(), stream, mr);
-  });
-}
-
 TEST(FixedPointColumnWrapperMemoryResourceTest, DistinctOutputAndTemporaryResources)
 {
   auto stream         = cudf::test::get_default_stream();
@@ -273,8 +205,38 @@ TEST(DictionaryColumnWrapperMemoryResourceTest, StringDistinctOutputAndTemporary
     uses_temporary);
 }
 
+/**
+ * @brief Base fixture that instruments column-wrapper tests with a memory-resource harness.
+ *
+ * Each test instantiates a fresh harness. Tests should construct wrappers with `resources()` and
+ * pass the released column to `validate_with_harness()` before returning. `TearDown` asserts that
+ * no output or temporary allocations remain live.
+ */
+struct ColumnWrapperTestWithHarness : public cudf::test::BaseFixture {
+  void TearDown() override { _harness.expect_no_live_allocations(this->stream()); }
+
+  rmm::cuda_stream_view stream() const { return cudf::test::get_default_stream(); }
+
+  cudf::memory_resources resources() { return _harness.resources(); }
+
+  /**
+   * @brief Validate that the harness owns the given result.
+   *
+   * Assert that the harness output resource holds bytes equal to `col->alloc_size()` and that no
+   * temporary allocations remain live. `col` is destroyed on return, so `TearDown` can additionally
+   * confirm that the output bytes were released.
+   */
+  void validate_with_harness(std::unique_ptr<cudf::column> col)
+  {
+    _harness.expect_resource_usage(col->alloc_size(), {}, this->stream());
+  }
+
+ private:
+  cudf::test::memory_resource_test_harness _harness{};
+};
+
 template <typename T>
-struct FixedWidthColumnWrapperTest : public cudf::test::BaseFixture,
+struct FixedWidthColumnWrapperTest : public ColumnWrapperTestWithHarness,
                                      cudf::test::UniformRandomGenerator<cudf::size_type> {
   FixedWidthColumnWrapperTest() : cudf::test::UniformRandomGenerator<cudf::size_type>{1000, 5000} {}
 
@@ -289,7 +251,7 @@ TYPED_TEST(FixedWidthColumnWrapperTest, EmptyIterator)
 {
   auto sequence = cuda::counting_iterator{0};
   cudf::test::fixed_width_column_wrapper<TypeParam, typename decltype(sequence)::value_type> col(
-    sequence, sequence);
+    sequence, sequence, this->stream(), this->resources());
   cudf::column_view view = col;
   EXPECT_EQ(view.size(), 0);
   EXPECT_EQ(view.head(), nullptr);
@@ -297,10 +259,12 @@ TYPED_TEST(FixedWidthColumnWrapperTest, EmptyIterator)
   EXPECT_FALSE(view.nullable());
   EXPECT_FALSE(view.has_nulls());
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 TYPED_TEST(FixedWidthColumnWrapperTest, EmptyList)
 {
-  cudf::test::fixed_width_column_wrapper<TypeParam> col{};
+  cudf::test::fixed_width_column_wrapper<TypeParam> col(this->stream(), this->resources());
   cudf::column_view view = col;
   EXPECT_EQ(view.size(), 0);
   EXPECT_EQ(view.head(), nullptr);
@@ -308,6 +272,8 @@ TYPED_TEST(FixedWidthColumnWrapperTest, EmptyList)
   EXPECT_FALSE(view.nullable());
   EXPECT_FALSE(view.has_nulls());
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, NonNullableIteratorConstructor)
@@ -317,7 +283,7 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NonNullableIteratorConstructor)
   auto size = this->size();
 
   cudf::test::fixed_width_column_wrapper<TypeParam, typename decltype(sequence)::value_type> col(
-    sequence, sequence + size);
+    sequence, sequence + size, this->stream(), this->resources());
   cudf::column_view view = col;
   EXPECT_EQ(view.size(), size);
   EXPECT_NE(nullptr, view.head());
@@ -325,11 +291,14 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NonNullableIteratorConstructor)
   EXPECT_FALSE(view.nullable());
   EXPECT_FALSE(view.has_nulls());
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, NonNullableListConstructor)
 {
-  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col({1, 2, 3, 4, 5});
+  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col(
+    {1, 2, 3, 4, 5}, this->stream(), this->resources());
 
   cudf::column_view view = col;
   EXPECT_EQ(view.size(), 5);
@@ -338,6 +307,8 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NonNullableListConstructor)
   EXPECT_FALSE(view.nullable());
   EXPECT_FALSE(view.has_nulls());
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, NullableIteratorConstructorAllValid)
@@ -349,7 +320,7 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NullableIteratorConstructorAllValid)
   auto size = this->size();
 
   cudf::test::fixed_width_column_wrapper<TypeParam, typename decltype(sequence)::value_type> col(
-    sequence, sequence + size, all_valid);
+    sequence, sequence + size, all_valid, this->stream(), this->resources());
   cudf::column_view view = col;
   EXPECT_EQ(view.size(), size);
   EXPECT_NE(nullptr, view.head());
@@ -357,13 +328,16 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NullableIteratorConstructorAllValid)
   EXPECT_TRUE(view.nullable());
   EXPECT_FALSE(view.has_nulls());
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, NullableListConstructorAllValid)
 {
   auto all_valid = cudf::test::iterators::no_nulls();
 
-  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col({1, 2, 3, 4, 5}, all_valid);
+  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col(
+    {1, 2, 3, 4, 5}, all_valid, this->stream(), this->resources());
   cudf::column_view view = col;
   EXPECT_EQ(view.size(), 5);
   EXPECT_NE(nullptr, view.head());
@@ -371,6 +345,8 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NullableListConstructorAllValid)
   EXPECT_TRUE(view.nullable());
   EXPECT_FALSE(view.has_nulls());
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, NullableIteratorConstructorAllNull)
@@ -382,7 +358,7 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NullableIteratorConstructorAllNull)
   auto size = this->size();
 
   cudf::test::fixed_width_column_wrapper<TypeParam, typename decltype(sequence)::value_type> col(
-    sequence, sequence + size, all_null);
+    sequence, sequence + size, all_null, this->stream(), this->resources());
   cudf::column_view view = col;
   EXPECT_EQ(view.size(), size);
   EXPECT_NE(nullptr, view.head());
@@ -391,13 +367,16 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NullableIteratorConstructorAllNull)
   EXPECT_TRUE(view.has_nulls());
   EXPECT_EQ(view.null_count(), size);
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, NullableListConstructorAllNull)
 {
   auto all_null = cudf::test::iterators::all_nulls();
 
-  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col({1, 2, 3, 4, 5}, all_null);
+  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col(
+    {1, 2, 3, 4, 5}, all_null, this->stream(), this->resources());
   cudf::column_view view = col;
   EXPECT_EQ(view.size(), 5);
   EXPECT_NE(nullptr, view.head());
@@ -406,13 +385,17 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NullableListConstructorAllNull)
   EXPECT_TRUE(view.has_nulls());
   EXPECT_EQ(view.null_count(), 5);
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, NullablePairListConstructorAllNull)
 {
   using p = std::pair<int32_t, bool>;
   cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col(
-    {p{1, false}, p{2, false}, p{3, false}, p{4, false}, p{5, false}});
+    {p{1, false}, p{2, false}, p{3, false}, p{4, false}, p{5, false}},
+    this->stream(),
+    this->resources());
   cudf::column_view view = col;
 
   EXPECT_EQ(view.size(), 5);
@@ -422,13 +405,16 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NullablePairListConstructorAllNull)
   EXPECT_TRUE(view.has_nulls());
   EXPECT_EQ(view.null_count(), 5);
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, NullablePairListConstructorAllNullMatch)
 {
   auto odd_valid = cudf::test::iterators::nulls_at_multiples_of(2);
 
-  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> match_col({1, 2, 3, 4, 5}, odd_valid);
+  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> match_col(
+    {1, 2, 3, 4, 5}, odd_valid, this->stream(), this->resources());
   cudf::column_view match_view = match_col;
 
   using p = std::pair<int32_t, bool>;
@@ -436,17 +422,24 @@ TYPED_TEST(FixedWidthColumnWrapperTest, NullablePairListConstructorAllNullMatch)
                                                                   p{2, odd_valid[1]},
                                                                   p{3, odd_valid[2]},
                                                                   p{4, odd_valid[3]},
-                                                                  p{5, odd_valid[4]}});
+                                                                  p{5, odd_valid[4]}},
+                                                                 this->stream(),
+                                                                 this->resources());
   cudf::column_view view = col;
 
-  CUDF_TEST_EXPECT_COLUMNS_EQUAL(view, match_view);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(view,
+                                 match_view,
+                                 cudf::test::debug_output_level::FIRST_ERROR,
+                                 this->stream(),
+                                 this->resources());
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, ReleaseWrapperAllValid)
 {
   auto all_valid = cudf::test::iterators::no_nulls();
 
-  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col({1, 2, 3, 4, 5}, all_valid);
+  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col(
+    {1, 2, 3, 4, 5}, all_valid, this->stream(), this->resources());
   auto colPtr            = col.release();
   cudf::column_view view = *colPtr;
   EXPECT_EQ(view.size(), 5);
@@ -455,13 +448,16 @@ TYPED_TEST(FixedWidthColumnWrapperTest, ReleaseWrapperAllValid)
   EXPECT_TRUE(view.nullable());
   EXPECT_FALSE(view.has_nulls());
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(std::move(colPtr));
 }
 
 TYPED_TEST(FixedWidthColumnWrapperTest, ReleaseWrapperAllNull)
 {
   auto all_null = cudf::test::iterators::all_nulls();
 
-  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col({1, 2, 3, 4, 5}, all_null);
+  cudf::test::fixed_width_column_wrapper<TypeParam, int32_t> col(
+    {1, 2, 3, 4, 5}, all_null, this->stream(), this->resources());
   auto colPtr            = col.release();
   cudf::column_view view = *colPtr;
   EXPECT_EQ(view.size(), 5);
@@ -471,11 +467,12 @@ TYPED_TEST(FixedWidthColumnWrapperTest, ReleaseWrapperAllNull)
   EXPECT_TRUE(view.has_nulls());
   EXPECT_EQ(view.null_count(), 5);
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(std::move(colPtr));
 }
 
 template <typename T>
-struct StringsColumnWrapperTest : public cudf::test::BaseFixture,
-                                  cudf::test::UniformRandomGenerator<cudf::size_type> {
+struct StringsColumnWrapperTest : public ColumnWrapperTestWithHarness {
   auto data_type() { return cudf::data_type{cudf::type_to_id<T>()}; }
 };
 
@@ -483,7 +480,7 @@ TYPED_TEST_SUITE(StringsColumnWrapperTest, cudf::test::StringTypes);
 
 TYPED_TEST(StringsColumnWrapperTest, EmptyList)
 {
-  cudf::test::strings_column_wrapper col;
+  cudf::test::strings_column_wrapper col(this->stream(), this->resources());
   cudf::column_view view = col;
   EXPECT_EQ(view.size(), 0);
   EXPECT_EQ(view.head(), nullptr);
@@ -491,13 +488,17 @@ TYPED_TEST(StringsColumnWrapperTest, EmptyList)
   EXPECT_FALSE(view.nullable());
   EXPECT_FALSE(view.has_nulls());
   EXPECT_EQ(view.offset(), 0);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(StringsColumnWrapperTest, NullablePairListConstructorAllNull)
 {
   using p = std::pair<std::string, bool>;
   cudf::test::strings_column_wrapper col(
-    {p{"a", false}, p{"string", false}, p{"test", false}, p{"for", false}, p{"nulls", false}});
+    {p{"a", false}, p{"string", false}, p{"test", false}, p{"for", false}, p{"nulls", false}},
+    this->stream(),
+    this->resources());
   cudf::strings_column_view view = cudf::column_view(col);
 
   constexpr auto count = 5;
@@ -508,14 +509,16 @@ TYPED_TEST(StringsColumnWrapperTest, NullablePairListConstructorAllNull)
   EXPECT_NE(nullptr, view.offsets().head());
   EXPECT_TRUE(view.has_nulls());
   EXPECT_EQ(view.null_count(), 5);
+
+  this->validate_with_harness(col.release());
 }
 
 TYPED_TEST(StringsColumnWrapperTest, NullablePairListConstructorAllNullMatch)
 {
   auto odd_valid = cudf::test::iterators::nulls_at_multiples_of(2);
 
-  cudf::test::strings_column_wrapper match_col({"a", "string", "", "test", "for", "nulls"},
-                                               odd_valid);
+  cudf::test::strings_column_wrapper match_col(
+    {"a", "string", "", "test", "for", "nulls"}, odd_valid, this->stream(), this->resources());
   cudf::column_view match_view = match_col;
 
   using p = std::pair<std::string, bool>;
@@ -524,8 +527,14 @@ TYPED_TEST(StringsColumnWrapperTest, NullablePairListConstructorAllNullMatch)
                                           p{"", odd_valid[2]},
                                           p{"test", odd_valid[3]},
                                           p{"for", odd_valid[4]},
-                                          p{"nulls", odd_valid[5]}});
+                                          p{"nulls", odd_valid[5]}},
+                                         this->stream(),
+                                         this->resources());
   cudf::column_view view = col;
 
-  CUDF_TEST_EXPECT_COLUMNS_EQUAL(view, match_view);
+  CUDF_TEST_EXPECT_COLUMNS_EQUAL(view,
+                                 match_view,
+                                 cudf::test::debug_output_level::FIRST_ERROR,
+                                 this->stream(),
+                                 this->resources());
 }
