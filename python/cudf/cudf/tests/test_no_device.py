74b9f11e3870b57934829ecd089d3738f825844d
Vyas Ramasubramani <vyasr@nvidia.com>
2026-09-06 11:46:34 -0700

    TEST: Merge import subprocess checks

--- python/cudf/cudf/tests/test_no_device.py
+++ python/cudf/cudf/tests/test_no_device.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 import subprocess
 import sys
@@ -8,7 +8,11 @@ def test_cudf_import_no_device(monkeypatch):
     with monkeypatch.context() as m:
         m.setenv("CUDA_VISIBLE_DEVICES", "-1")
         output = subprocess.check_call(
-            [sys.executable, "-c", "import cudf"],
+            [
+                sys.executable,
+                "-c",
+                "import cudf, sys; assert 'pyarrow._s3fs' not in sys.modules",
+            ],
             cwd="/",
         )
     assert output == 0
