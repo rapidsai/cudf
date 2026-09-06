5a3d1372b4ea105c8b0a943f40f6663084a8e352
Vyas Ramasubramani <vyasr@nvidia.com>
2026-09-06 09:11:40 -0700

    CI: Trigger complete PR workflow for timing

--- python/cudf/cudf/pandas/__init__.py
+++ python/cudf/cudf/pandas/__init__.py
@@ -1,5 +1,7 @@
-# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
+
+# Temporary no-op marker for a complete PR-workflow timing run.
 
 import os
 import warnings
