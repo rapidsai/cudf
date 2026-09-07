1be9aaf8246c33d442a800cface8586297f28536
Vyas Ramasubramani <vyasr@nvidia.com>
2026-09-06 20:12:19 -0700

    TEST: Focus reflected DateOffset binary-op coverage

--- python/cudf/cudf/tests/series/test_binops.py
+++ python/cudf/cudf/tests/series/test_binops.py
@@ -1779,27 +1779,17 @@ def test_datetime_dateoffset_binaryop_multiple(kwargs, op):
     assert_eq(expect, got)
 
 
-@pytest.mark.parametrize("n_periods", [0, 1, -12])
 @pytest.mark.parametrize(
-    "frequency",
+    "n_periods, frequency, dtype, components",
     [
-        "months",
-        "years",
-        "days",
-        "hours",
-        "minutes",
-        "seconds",
-        "microseconds",
-        "nanoseconds",
-    ],
-)
-@pytest.mark.parametrize(
-    "dtype, components",
-    [
-        ["datetime64[ns]", "00.012345678"],
-        ["datetime64[us]", "00.012345"],
-        ["datetime64[ms]", "00.012"],
-        ["datetime64[s]", "00"],
+        (0, "months", "datetime64[ns]", "00.012345678"),
+        (1, "years", "datetime64[us]", "00.012345"),
+        (-12, "days", "datetime64[ms]", "00.012"),
+        (0, "hours", "datetime64[s]", "00"),
+        (1, "minutes", "datetime64[ns]", "00.012345678"),
+        (-12, "seconds", "datetime64[us]", "00.012345"),
+        (0, "microseconds", "datetime64[ms]", "00.012"),
+        (1, "nanoseconds", "datetime64[s]", "00"),
     ],
 )
 def test_datetime_dateoffset_binaryop_reflected(
