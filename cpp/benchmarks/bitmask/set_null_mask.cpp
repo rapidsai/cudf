117fa54b0ed30c96ba811ca7ecb62d8a9c0b21e3
Kyle Edwards <kyedwards@nvidia.com>
2026-09-08 12:48:20 +0000

    Fix

--- cpp/benchmarks/bitmask/set_null_mask.cpp
+++ cpp/benchmarks/bitmask/set_null_mask.cpp
@@ -43,9 +43,9 @@ auto generate_test_data(cudf::size_type num_masks,
   masks.reserve(num_masks);
   std::vector<cudf::bitmask_type*> masks_ptr;
   masks_ptr.reserve(num_masks);
-  for (auto const& mask : masks) {
+  for (cudf::size_type i = 0; i < num_masks; ++i) {
     masks.emplace_back(cudf::create_null_mask(mask_size, cudf::mask_state::UNINITIALIZED));
-    masks_ptr.emplace_back(reinterpret_cast<cudf::bitmask_type*>(mask.data()));
+    masks_ptr.emplace_back(reinterpret_cast<cudf::bitmask_type*>(masks.back().data()));
   }
 
   return std::make_tuple(std::move(begin_bits),
